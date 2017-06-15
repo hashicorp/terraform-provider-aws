11d2bad1ede88fd0b934957799d9c2fe5c5779d6
Martins Sipenko <martins.sipenko@gmail.com>
2017-06-15 18:40:43 +0300

    Allow ipv6_cidr_block to be assigned to peering_connection

--- aws/resource_aws_route_test.go
+++ aws/resource_aws_route_test.go
@@ -106,6 +106,27 @@ func TestAccAWSRoute_ipv6ToInternetGateway(t *testing.T) {
 	})
 }
 
+func TestAccAWSRoute_ipv6ToPeeringConnection(t *testing.T) {
+ 	var route ec2.Route
+
+ 	resource.Test(t, resource.TestCase{
+ 		PreCheck: func() {
+ 			testAccPreCheck(t)
+ 		},
+ 		Providers:    testAccProviders,
+ 		CheckDestroy: testAccCheckAWSRouteDestroy,
+ 		Steps: []resource.TestStep{
+ 			{
+ 				Config: testAccAWSRouteConfigIpv6PeeringConnection,
+ 				Check: resource.ComposeTestCheckFunc(
+ 					testAccCheckAWSRouteExists("aws_route.pc", &route),
+ 				),
+ 			},
+ 		},
+ 	})
+ }
+
+
 func TestAccAWSRoute_changeCidr(t *testing.T) {
 	var route ec2.Route
 	var routeTable ec2.RouteTable
@@ -333,6 +354,36 @@ resource "aws_route" "igw" {
 }
 
 `)
+
+var testAccAWSRouteConfigIpv6PeeringConnection = fmt.Sprintf(`
+resource "aws_vpc" "foo" {
+	cidr_block = "10.0.0.0/16"
+	assign_generated_ipv6_cidr_block = true
+}
+
+resource "aws_vpc" "bar" {
+	cidr_block = "10.1.0.0/16"
+	assign_generated_ipv6_cidr_block = true
+}
+
+resource "aws_vpc_peering_connection" "foo" {
+	vpc_id = "${aws_vpc.foo.id}"
+	peer_vpc_id = "${aws_vpc.bar.id}"
+	auto_accept = true
+}
+
+resource "aws_route_table" "peering" {
+	vpc_id = "${aws_vpc.foo.id}"
+}
+
+resource "aws_route" "pc" {
+  route_table_id = "${aws_route_table.peering.id}"
+  destination_ipv6_cidr_block = "${aws_vpc.bar.ipv6_cidr_block}"
+  vpc_peering_connection_id = "${aws_vpc_peering_connection.foo.id}"
+}
+
+`)
+
 
 var testAccAWSRouteConfigIpv6 = fmt.Sprintf(`
 resource "aws_vpc" "foo" {
