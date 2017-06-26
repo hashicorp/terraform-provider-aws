d141a2eab5323d150f6a844968efcc8fc52d4e2f
Chris Marchesi <cmarchesi@paybyphone.com>
2017-06-25 21:34:32 -0700

    resource/aws_network_interface_sg_attachment: refactor tests
    Make the test configs a bit easier to understand. Each case (via
    resource or data source) now has its own config, but we still
    parameterize on enabling/disabling the security group resource for the
    removal check.

--- aws/resource_aws_network_interface_sg_attachment_test.go
+++ aws/resource_aws_network_interface_sg_attachment_test.go
@@ -10,16 +10,19 @@ import (
 
 func TestAccAwsNetworkInterfaceSGAttachment(t *testing.T) {
 	cases := []struct {
-		Name     string
-		External bool
+		Name                      string
+		CheckPrimaryInterfaceAttr bool
+		Config                    func(bool) string
 	}{
 		{
-			Name:     "instance primary interface",
-			External: false,
+			Name: "instance primary interface",
+			CheckPrimaryInterfaceAttr: false,
+			Config: testAccAwsNetworkInterfaceSGAttachmentViaInstance,
 		},
 		{
-			Name:     "externally supplied instance through data source",
-			External: true,
+			Name: "externally supplied instance through data source",
+			CheckPrimaryInterfaceAttr: true,
+			Config: testAccAwsNetworkInterfaceSGAttachmentViaDataSource,
 		},
 	}
 	for _, tc := range cases {
@@ -29,12 +32,12 @@ func TestAccAwsNetworkInterfaceSGAttachment(t *testing.T) {
 				Providers: testAccProviders,
 				Steps: []resource.TestStep{
 					resource.TestStep{
-						Config: testAccAwsNetworkInterfaceSGAttachment(tc.External, true),
-						Check:  checkSecurityGroupAttachment(tc.External, true),
+						Config: tc.Config(true),
+						Check:  checkSecurityGroupAttachment(tc.CheckPrimaryInterfaceAttr, true),
 					},
 					resource.TestStep{
-						Config: testAccAwsNetworkInterfaceSGAttachment(tc.External, false),
-						Check:  checkSecurityGroupAttachment(tc.External, false),
+						Config: tc.Config(false),
+						Check:  checkSecurityGroupAttachment(tc.CheckPrimaryInterfaceAttr, false),
 					},
 				},
 			})
@@ -42,8 +45,54 @@ func TestAccAwsNetworkInterfaceSGAttachment(t *testing.T) {
 	}
 }
 
-func testAccAwsNetworkInterfaceSGAttachment(external bool, attach bool) string {
-	baseConfig := `
+func testAccAwsNetworkInterfaceSGAttachmentViaInstance(attachmentEnabled bool) string {
+	return fmt.Sprintf(`
+variable "sg_attachment_enabled" {
+  type    = "string"
+  default = "%t"
+}
+
+data "aws_ami" "ami" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm-*"]
+  }
+
+  owners = ["amazon"]
+}
+
+resource "aws_instance" "instance" {
+  instance_type = "t2.micro"
+  ami           = "${data.aws_ami.ami.id}"
+
+  tags = {
+    "type" = "terraform-test-instance"
+  }
+}
+
+resource "aws_security_group" "sg" {
+  tags = {
+    "type" = "terraform-test-security-group"
+  }
+}
+
+resource "aws_network_interface_sg_attachment" "sg_attachment" {
+  count                = "${var.sg_attachment_enabled == "true" ? 1 : 0}"
+  security_group_id    = "${aws_security_group.sg.id}"
+  network_interface_id = "${aws_instance.instance.primary_network_interface_id}"
+}
+`, attachmentEnabled)
+}
+
+func testAccAwsNetworkInterfaceSGAttachmentViaDataSource(attachmentEnabled bool) string {
+	return fmt.Sprintf(`
+variable "sg_attachment_enabled" {
+  type    = "string"
+  default = "%t"
+}
+
 data "aws_ami" "ami" {
   most_recent = true
 
@@ -65,7 +114,7 @@ resource "aws_instance" "instance" {
 }
 
 data "aws_instance" "external_instance" {
-	instance_id = "${aws_instance.instance.id}"
+  instance_id = "${aws_instance.instance.id}"
 }
 
 resource "aws_security_group" "sg" {
@@ -74,35 +123,23 @@ resource "aws_security_group" "sg" {
   }
 }
 
-`
-	optionalConfig := `
 resource "aws_network_interface_sg_attachment" "sg_attachment" {
+  count                = "${var.sg_attachment_enabled == "true" ? 1 : 0}"
   security_group_id    = "${aws_security_group.sg.id}"
-  network_interface_id = "${%saws_instance.%sinstance.%snetwork_interface_id}"
+  network_interface_id = "${data.aws_instance.external_instance.network_interface_id}"
 }
-`
-
-	if attach {
-		externalResPre := ""
-		externalDataPre := ""
-		externalAttrPre := "primary_"
-		if external {
-			externalResPre = "data."
-			externalDataPre = "external_"
-			externalAttrPre = ""
-		}
-		return baseConfig + fmt.Sprintf(optionalConfig, externalResPre, externalDataPre, externalAttrPre)
-	}
-	return baseConfig
+`, attachmentEnabled)
 }
 
-func checkSecurityGroupAttachment(external bool, expected bool) resource.TestCheckFunc {
+func checkSecurityGroupAttachment(checkPrimaryInterfaceAttr bool, expected bool) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		conn := testAccProvider.Meta().(*AWSClient).ec2conn
 
-		ifAttr := "primary_network_interface_id"
-		if external {
+		var ifAttr string
+		if checkPrimaryInterfaceAttr {
 			ifAttr = "network_interface_id"
+		} else {
+			ifAttr = "primary_network_interface_id"
 		}
 		interfaceID := s.Modules[0].Resources["aws_instance.instance"].Primary.Attributes[ifAttr]
 		sgID := s.Modules[0].Resources["aws_security_group.sg"].Primary.ID
