c9c786c1e973d518f0cb7b429ffbfa138cd7da8a
Danniel Magno <dennyloko@gmail.com>
2017-07-03 16:50:37 -0300

    Add support for aws_wafregional_rule

--- aws/provider.go
+++ aws/provider.go
@@ -475,6 +475,7 @@ func Provider() terraform.ResourceProvider {
 			"aws_waf_sql_injection_match_set":              resourceAwsWafSqlInjectionMatchSet(),
 			"aws_wafregional_byte_match_set":               resourceAwsWafRegionalByteMatchSet(),
 			"aws_wafregional_ipset":                        resourceAwsWafRegionalIPSet(),
+			"aws_wafregional_rule":                         resourceAwsWafRegionalRule(),
 		},
 		ConfigureFunc: providerConfigure,
 	}
