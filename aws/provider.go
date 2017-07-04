a87c997da7fa7478ce237f4f309ef8526d453173
Danniel Magno <dennyloko@gmail.com>
2017-07-03 21:00:21 -0300

    Add support for aws_wafregional_web_acl

--- aws/provider.go
+++ aws/provider.go
@@ -527,6 +527,7 @@ func Provider() terraform.ResourceProvider {
 			"aws_waf_sql_injection_match_set":              resourceAwsWafSqlInjectionMatchSet(),
 			"aws_wafregional_byte_match_set":               resourceAwsWafRegionalByteMatchSet(),
 			"aws_wafregional_ipset":                        resourceAwsWafRegionalIPSet(),
+			"aws_wafregional_web_acl":                      resourceAwsWafRegionalWebAcl(),
 			"aws_batch_compute_environment":                resourceAwsBatchComputeEnvironment(),
 			"aws_batch_job_definition":                     resourceAwsBatchJobDefinition(),
 			"aws_batch_job_queue":                          resourceAwsBatchJobQueue(),
