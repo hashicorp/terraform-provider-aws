cdba86be569d17a2385e2b840e32f68af8c8adc5
Andy Chan <andy.chan@ticketmaster.com>
2017-07-07 11:58:07 -0700

    SHA256 hash the password for RDS and DB instance

--- aws/resource_aws_rds_cluster.go
+++ aws/resource_aws_rds_cluster.go
@@ -278,6 +278,7 @@ func resourceAwsRDSCluster() *schema.Resource {
 				Type:      schema.TypeString,
 				Optional:  true,
 				Sensitive: true,
+				StateFunc: hashSum,
 			},
 
 			"snapshot_identifier": {
