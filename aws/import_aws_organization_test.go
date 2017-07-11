54f122bfb5a984805b2290e1199470fdaed91fb4
Ninir <gauthier.wallet@gmail.com>
2017-07-11 22:05:49 +0200

    Added Organization import test

---
+++ aws/import_aws_organization_test.go
@@ -0,0 +1,28 @@
+package aws
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccAWSOrganizationImportBasic(t *testing.T) {
+	resourceName := "aws_organization.test"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAWSOrganizationDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAWSOrganizationConfig(),
+			},
+
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
