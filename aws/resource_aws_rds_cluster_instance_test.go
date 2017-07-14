e6dec321ca75923d83ecf6b217c70d8c5496cad4
Paul Stack <public@paulstack.co.uk>
2017-07-14 13:56:12 +0300

    resource/rds_cluster_instance: Export the RDI Resource ID for the instance (#1142)
    Fixes: #1081

--- aws/resource_aws_rds_cluster_instance_test.go
+++ aws/resource_aws_rds_cluster_instance_test.go
@@ -32,6 +32,7 @@ func TestAccAWSRDSClusterInstance_basic(t *testing.T) {
 					resource.TestCheckResourceAttr("aws_rds_cluster_instance.cluster_instances", "auto_minor_version_upgrade", "true"),
 					resource.TestCheckResourceAttrSet("aws_rds_cluster_instance.cluster_instances", "preferred_maintenance_window"),
 					resource.TestCheckResourceAttrSet("aws_rds_cluster_instance.cluster_instances", "preferred_backup_window"),
+					resource.TestCheckResourceAttrSet("aws_rds_cluster_instance.cluster_instances", "dbi_resource_id"),
 				),
 			},
 			{
