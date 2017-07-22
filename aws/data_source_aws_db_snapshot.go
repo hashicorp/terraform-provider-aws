3a34ec3af9d1a1228d4e4fb9f1e4206fe97b6dfc
Kelvin Jasperson <github@mqp3.com>
2017-07-22 12:37:42 -0600

    Fix spelling in error message for aws_db_snapshot data source

--- aws/data_source_aws_db_snapshot.go
+++ aws/data_source_aws_db_snapshot.go
@@ -135,7 +135,7 @@ func dataSourceAwsDbSnapshotRead(d *schema.ResourceData, meta interface{}) error
 	snapshotIdentifier, snapshotIdentifierOk := d.GetOk("db_snapshot_identifier")
 
 	if !instanceIdentifierOk && !snapshotIdentifierOk {
-		return fmt.Errorf("One of db_snapshot_indentifier or db_instance_identifier must be assigned")
+		return fmt.Errorf("One of db_snapshot_identifier or db_instance_identifier must be assigned")
 	}
 
 	params := &rds.DescribeDBSnapshotsInput{
