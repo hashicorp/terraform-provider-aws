7d79ebc87af3de85d35bc6958b6fb10ff99c919a
stack72 <public@paulstack.co.uk>
2017-07-27 12:20:25 +0300

    resource/aws_ssm_association: Add support for OutputLocation and Schedule Expression
    Fixes: #1246
    
    ```
    % make testacc TEST=./aws TESTARGS='-run=TestAccAWSSSMAssociation_'
    ==> Checking that code complies with gofmt requirements...
    TF_ACC=1 go test ./aws -v -run=TestAccAWSSSMAssociation_ -timeout 120m
    === RUN   TestAccAWSSSMAssociation_basic
    --- PASS: TestAccAWSSSMAssociation_basic (135.40s)
    === RUN   TestAccAWSSSMAssociation_withTargets
    --- PASS: TestAccAWSSSMAssociation_withTargets (32.06s)
    === RUN   TestAccAWSSSMAssociation_withOutputLocation
    --- PASS: TestAccAWSSSMAssociation_withOutputLocation (58.07s)
    === RUN   TestAccAWSSSMAssociation_withScheduleExpression
    --- PASS: TestAccAWSSSMAssociation_withScheduleExpression (31.17s)
    PASS
    ok  	github.com/terraform-providers/terraform-provider-aws/aws	256.727s
    ```

--- aws/resource_aws_ssm_association_test.go
+++ aws/resource_aws_ssm_association_test.go
@@ -46,6 +46,46 @@ func TestAccAWSSSMAssociation_withTargets(t *testing.T) {
 	})
 }
 
+func TestAccAWSSSMAssociation_withOutputLocation(t *testing.T) {
+	name := acctest.RandString(10)
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAWSSSMAssociationDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAWSSSMAssociationBasicConfigWithOutPutLocation(name),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAWSSSMAssociationExists("aws_ssm_association.foo"),
+					resource.TestCheckResourceAttr(
+						"aws_ssm_association.foo", "output_location.0.s3_bucket_name", fmt.Sprintf("tf-acc-test-ssmoutput-%s", name)),
+					resource.TestCheckResourceAttr(
+						"aws_ssm_association.foo", "output_location.0.s3_key_prefix", "SSMAssociation"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccAWSSSMAssociation_withScheduleExpression(t *testing.T) {
+	name := acctest.RandString(10)
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAWSSSMAssociationDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAWSSSMAssociationBasicConfigWithScheduleExpression(name),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAWSSSMAssociationExists("aws_ssm_association.foo"),
+					resource.TestCheckResourceAttr(
+						"aws_ssm_association.foo", "schedule_expression", "cron(0 16 ? * TUE *)"),
+				),
+			},
+		},
+	})
+}
+
 func testAccCheckAWSSSMAssociationExists(n string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		rs, ok := s.RootModule().Resources[n]
@@ -185,4 +225,84 @@ resource "aws_ssm_association" "foo" {
   instance_id = "${aws_instance.foo.id}"
 }
 `, rName, rName, rName)
+}
+
+func testAccAWSSSMAssociationBasicConfigWithScheduleExpression(rName string) string {
+	return fmt.Sprintf(`
+resource "aws_ssm_document" "foo_document" {
+  name = "test_document_association-%s",
+  document_type = "Command"
+  content = <<DOC
+  {
+    "schemaVersion": "1.2",
+    "description": "Check ip configuration of a Linux instance.",
+    "parameters": {
+
+    },
+    "runtimeConfig": {
+      "aws:runShellScript": {
+        "properties": [
+          {
+            "id": "0.aws:runShellScript",
+            "runCommand": ["ifconfig"]
+          }
+        ]
+      }
+    }
+  }
+DOC
+}
+
+resource "aws_ssm_association" "foo" {
+  name = "${aws_ssm_document.foo_document.name}",
+  schedule_expression = "cron(0 16 ? * TUE *)"
+  targets {
+    key = "tag:Name"
+    values = ["acceptanceTest"]
+  }
+}`, rName)
+}
+
+func testAccAWSSSMAssociationBasicConfigWithOutPutLocation(rName string) string {
+	return fmt.Sprintf(`
+resource "aws_s3_bucket" "output_location" {
+  bucket = "tf-acc-test-ssmoutput-%s"
+  force_destroy = true
+}
+
+resource "aws_ssm_document" "foo_document" {
+  name = "test_document_association-%s",
+  document_type = "Command"
+  content = <<DOC
+  {
+    "schemaVersion": "1.2",
+    "description": "Check ip configuration of a Linux instance.",
+    "parameters": {
+
+    },
+    "runtimeConfig": {
+      "aws:runShellScript": {
+        "properties": [
+          {
+            "id": "0.aws:runShellScript",
+            "runCommand": ["ifconfig"]
+          }
+        ]
+      }
+    }
+  }
+DOC
+}
+
+resource "aws_ssm_association" "foo" {
+  name = "${aws_ssm_document.foo_document.name}",
+  targets {
+    key = "tag:Name"
+    values = ["acceptanceTest"]
+  }
+  output_location {
+    s3_bucket_name = "${aws_s3_bucket.output_location.id}"
+    s3_key_prefix = "SSMAssociation"
+  }
+}`, rName, rName)
 }
