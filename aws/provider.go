63a73e769f48ff50bc379763355c3528fcbce295
Colin Edwards <colin@recursivepenguin.com>
2017-07-29 15:01:01 -0500

    Add Waf RateBasedRule
    https://docs.aws.amazon.com/waf/latest/APIReference/API_regional_RateBasedRule.html#WAF-Type-regional_RateBasedRule-MatchPredicates

--- aws/provider.go
+++ aws/provider.go
@@ -478,6 +478,7 @@ func Provider() terraform.ResourceProvider {
 			"aws_waf_byte_match_set":                       resourceAwsWafByteMatchSet(),
 			"aws_waf_ipset":                                resourceAwsWafIPSet(),
 			"aws_waf_rule":                                 resourceAwsWafRule(),
+			"aws_waf_rate_based_rule":                      resourceAwsWafRateBasedRule(),
 			"aws_waf_size_constraint_set":                  resourceAwsWafSizeConstraintSet(),
 			"aws_waf_web_acl":                              resourceAwsWafWebAcl(),
 			"aws_waf_xss_match_set":                        resourceAwsWafXssMatchSet(),
