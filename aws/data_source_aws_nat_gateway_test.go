3740a02099337a99e8dc8e47463690c7b33c1212
Levi <shadycuz@gmail.com>
2017-07-31 16:24:43 -0400

    replaced s/vpn/nat

--- aws/data_source_aws_nat_gateway_test.go
+++ aws/data_source_aws_nat_gateway_test.go
@@ -20,14 +20,14 @@ func TestAccDataSourceAwsNatGateway_unattached(t *testing.T) {
 				Config: testAccDataSourceAwsNatGatewayUnattachedConfig(rInt),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrPair(
-						"data.aws_vpn_gateway.test_by_id", "id",
-						"aws_vpn_gateway.unattached", "id"),
+						"data.aws_nat_gateway.test_by_id", "id",
+						"aws_nat_gateway.unattached", "id"),
 					resource.TestCheckResourceAttrPair(
-						"data.aws_vpn_gateway.test_by_tags", "id",
-						"aws_vpn_gateway.unattached", "id"),
-					resource.TestCheckResourceAttrSet("data.aws_vpn_gateway.test_by_id", "state"),
-					resource.TestCheckResourceAttr("data.aws_vpn_gateway.test_by_tags", "tags.%", "3"),
-					resource.TestCheckNoResourceAttr("data.aws_vpn_gateway.test_by_id", "attached_vpc_id"),
+						"data.aws_nat_gateway.test_by_tags", "id",
+						"aws_nat_gateway.unattached", "id"),
+					resource.TestCheckResourceAttrSet("data.aws_nat_gateway.test_by_id", "state"),
+					resource.TestCheckResourceAttr("data.aws_nat_gateway.test_by_tags", "tags.%", "3"),
+					resource.TestCheckNoResourceAttr("data.aws_nat_gateway.test_by_id", "attached_vpc_id"),
 				),
 			},
 		},
@@ -45,12 +45,12 @@ func TestAccDataSourceAwsNatGateway_attached(t *testing.T) {
 				Config: testAccDataSourceAwsNatGatewayAttachedConfig(rInt),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrPair(
-						"data.aws_vpn_gateway.test_by_attached_vpc_id", "id",
-						"aws_vpn_gateway.attached", "id"),
+						"data.aws_nat_gateway.test_by_attached_vpc_id", "id",
+						"aws_nat_gateway.attached", "id"),
 					resource.TestCheckResourceAttrPair(
-						"data.aws_vpn_gateway.test_by_attached_vpc_id", "attached_vpc_id",
+						"data.aws_nat_gateway.test_by_attached_vpc_id", "attached_vpc_id",
 						"aws_vpc.foo", "id"),
-					resource.TestMatchResourceAttr("data.aws_vpn_gateway.test_by_attached_vpc_id", "state", regexp.MustCompile("(?i)available")),
+					resource.TestMatchResourceAttr("data.aws_nat_gateway.test_by_attached_vpc_id", "state", regexp.MustCompile("(?i)available")),
 				),
 			},
 		},
@@ -63,20 +63,20 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_vpn_gateway" "unattached" {
+resource "aws_nat_gateway" "unattached" {
     tags {
-		Name = "terraform-testacc-vpn-gateway-data-source-unattached-%d"
+		Name = "terraform-testacc-nat-gateway-data-source-unattached-%d"
       	ABC  = "testacc-%d"
 		XYZ  = "testacc-%d"
     }
 }
 
-data "aws_vpn_gateway" "test_by_id" {
-	id = "${aws_vpn_gateway.unattached.id}"
+data "aws_nat_gateway" "test_by_id" {
+	id = "${aws_nat_gateway.unattached.id}"
 }
 
-data "aws_vpn_gateway" "test_by_tags" {
-	tags = "${aws_vpn_gateway.unattached.tags}"
+data "aws_nat_gateway" "test_by_tags" {
+	tags = "${aws_nat_gateway.unattached.tags}"
 }
 `, rInt, rInt+1, rInt-1)
 }
@@ -91,23 +91,23 @@ resource "aws_vpc" "foo" {
 	cidr_block = "10.1.0.0/16"
 
   	tags {
-    	Name = "terraform-testacc-vpn-gateway-data-source-foo-%d"
+    	Name = "terraform-testacc-nat-gateway-data-source-foo-%d"
   	}
 }
 
-resource "aws_vpn_gateway" "attached" {
+resource "aws_nat_gateway" "attached" {
     tags {
-		Name = "terraform-testacc-vpn-gateway-data-source-attached-%d"
+		Name = "terraform-testacc-nat-gateway-data-source-attached-%d"
     }
 }
 
-resource "aws_vpn_gateway_attachment" "vpn_attachment" {
+resource "aws_nat_gateway_attachment" "nat_attachment" {
   vpc_id = "${aws_vpc.foo.id}"
-  vpn_gateway_id = "${aws_vpn_gateway.attached.id}"
+  nat_gateway_id = "${aws_nat_gateway.attached.id}"
 }
 
-data "aws_vpn_gateway" "test_by_attached_vpc_id" {
-	attached_vpc_id = "${aws_vpn_gateway_attachment.vpn_attachment.vpc_id}"
+data "aws_nat_gateway" "test_by_attached_vpc_id" {
+	attached_vpc_id = "${aws_nat_gateway_attachment.nat_attachment.vpc_id}"
 }
 `, rInt, rInt)
 }
