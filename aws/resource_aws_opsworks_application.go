2066ac2be6097053b405255212372ee0526f6146
jtsaito <jahn.saito@gmail.com>
2017-08-21 16:27:56 +0200

    Fix resource aws ospworks application secure, ssh_key, password handling

--- aws/resource_aws_opsworks_application.go
+++ aws/resource_aws_opsworks_application.go
@@ -448,10 +448,12 @@ func resourceAwsOpsworksSetApplicationSource(d *schema.ResourceData, v *opsworks
 			m["revision"] = *v.Revision
 		}
 
-		// v.Password and v.SshKey will, on read, contain the placeholder string
+		// v.Password will, on read, contain the placeholder string
 		// "*****FILTERED*****", so we ignore it on read and let persist
 		// the value already in the state.
 		m["password"] = d.Get("app_source.0.password").(string)
+		// ssh_key can not be read via api, so we ignore it on read and let persist
+		// the value already in the state.
 		m["ssh_key"] = d.Get("app_source.0.ssh_key").(string)
 
 		nv = append(nv, m)
