8226851d791276e6a3ed8c4fdf03df07dcaca664
Brett W <brett_weaver@intuit.com>
2017-09-19 07:29:00 -0700

    adding destroy and dissapear test

--- aws/resource_aws_servicecatalog_portfolio_test.go
+++ aws/resource_aws_servicecatalog_portfolio_test.go
@@ -1,15 +1,22 @@
 package aws
 
 import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/servicecatalog"
+
 	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
 
 	"testing"
 )
 
 func TestAccAWSServiceCatalogPortfolio_basic(t *testing.T) {
 	resource.Test(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
-		Providers: testAccProviders,
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckServiceCatlaogPortfolioDestroy,
 		Steps: []resource.TestStep{
 			resource.TestStep{
 				Config: testAccCheckAwsServiceCatalogPortfolioResourceConfig_basic1,
@@ -37,6 +44,85 @@ func TestAccAWSServiceCatalogPortfolio_basic(t *testing.T) {
 			},
 		},
 	})
+}
+
+func TestAccAWSServiceCatalogPortfolio_disappears(t *testing.T) {
+	var portfolioDetail servicecatalog.PortfolioDetail
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckServiceCatlaogPortfolioDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccCheckAwsServiceCatalogPortfolioResourceConfig_basic1,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckPortfolio("aws_servicecatalog_portfolio.test", &portfolioDetail),
+					testAccCheckServiceCatlaogPortfolioDisappears(&portfolioDetail),
+				),
+				ExpectNonEmptyPlan: true,
+			},
+		},
+	})
+}
+
+func testAccCheckPortfolio(pr string, pd *servicecatalog.PortfolioDetail) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		conn := testAccProvider.Meta().(*AWSClient).scconn
+		rs, ok := s.RootModule().Resources[pr]
+		if !ok {
+			return fmt.Errorf("Not found: %s", pr)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No ID is set")
+		}
+
+		input := servicecatalog.DescribePortfolioInput{}
+		input.Id = aws.String(rs.Primary.ID)
+
+		resp, err := conn.DescribePortfolio(&input)
+		if err != nil {
+			return err
+		}
+
+		*pd = *resp.PortfolioDetail
+		return nil
+	}
+}
+
+func testAccCheckServiceCatlaogPortfolioDisappears(pd *servicecatalog.PortfolioDetail) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		conn := testAccProvider.Meta().(*AWSClient).scconn
+
+		input := servicecatalog.DeletePortfolioInput{}
+		input.Id = pd.Id
+
+		_, err := conn.DeletePortfolio(&input)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckServiceCatlaogPortfolioDestroy(s *terraform.State) error {
+	conn := testAccProvider.Meta().(*AWSClient).scconn
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "aws_servicecatalog_portfolio" {
+			continue
+		}
+		input := servicecatalog.DescribePortfolioInput{}
+		input.Id = aws.String(rs.Primary.ID)
+
+		_, err := conn.DescribePortfolio(&input)
+		if err == nil {
+			return fmt.Errorf("Portfolio still exists")
+		}
+	}
+
+	return nil
 }
 
 const testAccCheckAwsServiceCatalogPortfolioResourceConfig_basic1 = `
