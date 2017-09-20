90c1c226a20bbdf6f7d72b23ebde6144a02e3510
trung <trung@mdkt.org>
2017-09-20 14:54:36 -0500

    test case to cover gp2 with iops

--- aws/resource_aws_instance_test.go
+++ aws/resource_aws_instance_test.go
@@ -189,6 +189,11 @@ func TestAccAWSInstance_GP2IopsDevice(t *testing.T) {
 					testCheck(),
 				),
 			},
+			{
+				Config:             testAccInstanceGP2IopsDeviceExplicit,
+				PlanOnly:           true,
+				ExpectNonEmptyPlan: false,
+			},
 		},
 	})
 }
@@ -1509,6 +1514,25 @@ resource "aws_instance" "foo" {
 	root_block_device {
 		volume_type = "gp2"
 		volume_size = 11
+	}
+}
+`
+
+const testAccInstanceGP2IopsDeviceExplicit = `
+resource "aws_instance" "foo" {
+	# us-west-2
+	ami = "ami-55a7ea65"
+
+	# In order to attach an encrypted volume to an instance you need to have an
+	# m3.medium or larger. See "Supported Instance Types" in:
+	# http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/EBSEncryption.html
+	instance_type = "m3.medium"
+
+	root_block_device {
+		volume_type = "gp2"
+		volume_size = 11
+        # demo a test scenario
+		iops        = 10
 	}
 }
 `
