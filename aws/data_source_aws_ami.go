7e4a4a57bfdd9133fac6e328d2e7f5476dbdd6b9
Ninir <gauthier.wallet@gmail.com>
2017-09-20 13:52:31 +0200

    Update the AWS AMI Data Source Tags management

--- aws/data_source_aws_ami.go
+++ aws/data_source_aws_ami.go
@@ -165,7 +165,7 @@ func dataSourceAwsAmi() *schema.Resource {
 				Type:     schema.TypeMap,
 				Computed: true,
 			},
-			"tags": dataSourceTagsSchema(),
+			"tags": tagsSchemaComputed(),
 		},
 	}
 }
@@ -299,7 +299,7 @@ func amiDescriptionAttributes(d *schema.ResourceData, image *ec2.Image) error {
 	if err := d.Set("state_reason", amiStateReason(image.StateReason)); err != nil {
 		return err
 	}
-	if err := d.Set("tags", dataSourceTags(image.Tags)); err != nil {
+	if err := d.Set("tags", tagsToMap(image.Tags)); err != nil {
 		return err
 	}
 	return nil
