1a49dc08a7125d5e5a5b813603315e62a20bd9ab
Roberto Acevedo <rxacevedo@fastmail.com>
2017-10-04 09:39:52 -0400

    Fix ECS cluster ARN attribute name/value

--- aws/resource_aws_batch_compute_environment.go
+++ aws/resource_aws_batch_compute_environment.go
@@ -121,7 +121,7 @@ func resourceAwsBatchComputeEnvironment() *schema.Resource {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
-			"ecc_cluster_arn": {
+			"ecs_cluster_arn": {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
@@ -266,7 +266,7 @@ func resourceAwsBatchComputeEnvironmentRead(d *schema.ResourceData, meta interfa
 	}
 
 	d.Set("arn", computeEnvironment.ComputeEnvironmentArn)
-	d.Set("ecc_cluster_arn", computeEnvironment.ComputeEnvironmentArn)
+	d.Set("ecs_cluster_arn", computeEnvironment.EcsClusterArn)
 	d.Set("status", computeEnvironment.Status)
 	d.Set("status_reason", computeEnvironment.StatusReason)
 
