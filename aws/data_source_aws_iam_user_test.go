f1cb2807b10e233dd97f1b17bdd2c8d118f1f682
Radek Simko <radek.simko@gmail.com>
2017-10-09 16:16:17 +0100

    d/iam_user: Remove fragile 404 test

--- aws/data_source_aws_iam_user_test.go
+++ aws/data_source_aws_iam_user_test.go
@@ -20,14 +20,6 @@ func TestAccAWSDataSourceIAMUser_basic(t *testing.T) {
 					resource.TestCheckResourceAttr("data.aws_iam_user.test", "user_name", "test-datasource-user"),
 					resource.TestMatchResourceAttr("data.aws_iam_user.test", "arn", regexp.MustCompile("^arn:aws:iam::[0-9]{12}:user/test-datasource-user")),
 				),
-			}, {
-				Config: testAccAwsDataSourceIAMUserUnknownConfig,
-				// Should fail with an error that looks like this:
-				//   Error refreshing: 1 error(s) occurred:
-				//   * data.aws_iam_user.test: 1 error(s) occurred:
-				//   * data.aws_iam_user.test: data.aws_iam_user.test: error getting user: NoSuchEntity: The user with name test-datasource-user-unknown cannot be found.
-				//           status code: 404, request id: 16760dec-a88d-11e7-afb8-bbe194fb2bc8
-				ExpectError: regexp.MustCompile("(?s)\\berror getting user\\b.*\\btest-datasource-user-unknown\\b.*\\b404\\b"),
 			},
 		},
 	})
@@ -41,11 +33,5 @@ resource "aws_iam_user" "user" {
 
 data "aws_iam_user" "test" {
 	  user_name = "${aws_iam_user.user.name}"
-}
-`
-
-const testAccAwsDataSourceIAMUserUnknownConfig = `
-data "aws_iam_user" "test" {
-	  user_name = "test-datasource-user-unknown"
 }
 `
