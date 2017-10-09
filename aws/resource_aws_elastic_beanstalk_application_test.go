db846ff2e30889287ebfb6aaf992a727fc3beac0
Clint Shryock <clint@ctshryock.com>
2017-10-09 15:20:30 -0500

    r/beanstalk_environment, r/beanstalk_application: Add sweepers
    Adds sweepers for Beanstalk ENV/Application. We had some leaks

--- aws/resource_aws_elastic_beanstalk_application_test.go
+++ aws/resource_aws_elastic_beanstalk_application_test.go
@@ -2,6 +2,8 @@ package aws
 
 import (
 	"fmt"
+	"log"
+	"strings"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -10,6 +12,67 @@ import (
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
 )
+
+// initialize sweeper
+func init() {
+	resource.AddTestSweepers("aws_beanstalk_application", &resource.Sweeper{
+		Name:         "aws_beanstalk_application",
+		Dependencies: []string{"aws_beanstalk_environment"},
+		F:            testSweepBeanstalkApplications,
+	})
+}
+
+func testSweepBeanstalkApplications(region string) error {
+	client, err := sharedClientForRegion(region)
+	if err != nil {
+		return fmt.Errorf("error getting client: %s", err)
+	}
+	beanstalkconn := client.(*AWSClient).elasticbeanstalkconn
+
+	resp, err := beanstalkconn.DescribeApplications(&elasticbeanstalk.DescribeApplicationsInput{})
+	if err != nil {
+		return fmt.Errorf("Error retrieving beanstalk application: %s", err)
+	}
+
+	if len(resp.Applications) == 0 {
+		log.Print("[DEBUG] No aws beanstalk applications to sweep")
+		return nil
+	}
+
+	for _, bsa := range resp.Applications {
+		var testOptGroup bool
+		for _, testName := range []string{
+			"terraform-",
+			"tf-test-",
+			"tf_acc_",
+			"tf-acc-",
+		} {
+			if strings.HasPrefix(*bsa.ApplicationName, testName) {
+				testOptGroup = true
+			}
+		}
+
+		if !testOptGroup {
+			continue
+		}
+
+		_, err := beanstalkconn.DeleteApplication(
+			&elasticbeanstalk.DeleteApplicationInput{
+				ApplicationName: bsa.ApplicationName,
+			})
+		if err != nil {
+			elasticbeanstalkerr, ok := err.(awserr.Error)
+			if ok && (elasticbeanstalkerr.Code() == "InvalidConfiguration.NotFound" || elasticbeanstalkerr.Code() == "ValidationError") {
+				log.Printf("[DEBUG] beanstalk application (%s) not found", *bsa.ApplicationName)
+				return nil
+			}
+
+			return err
+		}
+	}
+
+	return nil
+}
 
 func TestAccAWSBeanstalkApp_basic(t *testing.T) {
 	var app elasticbeanstalk.ApplicationDescription
