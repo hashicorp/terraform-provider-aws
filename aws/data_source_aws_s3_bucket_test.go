5f3605b9807c6ba4e57a4cdd6faa9bd787a6dac4
Kash <ka.shing.law@gmail.com>
2017-10-11 07:59:30 -0400

    Add s3 bucket data source (#1505)
    * Create s3 bucket data source
    
    * Don't return website endpoints if it doesn't exist
    
    * Add s3 bucket data source documentation
    
    * d/s3_bucket: Reorder imports according to conventions
    
    * d/s3_bucket: Rename files according to conventions

---
+++ aws/data_source_aws_s3_bucket_test.go
@@ -0,0 +1,85 @@
+package aws
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccDataSourceS3Bucket_basic(t *testing.T) {
+	rInt := acctest.RandInt()
+	arnRegexp := regexp.MustCompile(
+		"^arn:aws:s3:::")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAWSDataSourceS3BucketConfig_basic(rInt),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAWSS3BucketExists("data.aws_s3_bucket.bucket"),
+					resource.TestMatchResourceAttr("data.aws_s3_bucket.bucket", "arn", arnRegexp),
+					resource.TestCheckResourceAttr("data.aws_s3_bucket.bucket", "region", "us-west-2"),
+					resource.TestCheckResourceAttr(
+						"data.aws_s3_bucket.bucket", "bucket_domain_name", testAccBucketDomainName(rInt)),
+					resource.TestCheckResourceAttr(
+						"data.aws_s3_bucket.bucket", "hosted_zone_id", HostedZoneIDForRegion("us-west-2")),
+					resource.TestCheckNoResourceAttr("data.aws_s3_bucket.bucket", "website_endpoint"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDataSourceS3Bucket_website(t *testing.T) {
+	rInt := acctest.RandInt()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAWSDataSourceS3BucketWebsiteConfig(rInt),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAWSS3BucketExists("data.aws_s3_bucket.bucket"),
+					testAccCheckAWSS3BucketWebsite(
+						"data.aws_s3_bucket.bucket", "index.html", "error.html", "", ""),
+					resource.TestCheckResourceAttr(
+						"data.aws_s3_bucket.bucket", "website_endpoint", testAccWebsiteEndpoint(rInt)),
+				),
+			},
+		},
+	})
+}
+
+func testAccAWSDataSourceS3BucketConfig_basic(randInt int) string {
+	return fmt.Sprintf(`
+resource "aws_s3_bucket" "bucket" {
+	bucket = "tf-test-bucket-%d"
+}
+
+data "aws_s3_bucket" "bucket" {
+	bucket = "${aws_s3_bucket.bucket.id}"
+}`, randInt)
+}
+
+func testAccAWSDataSourceS3BucketWebsiteConfig(randInt int) string {
+	return fmt.Sprintf(`
+resource "aws_s3_bucket" "bucket" {
+	bucket = "tf-test-bucket-%d"
+	acl = "public-read"
+
+	website {
+		index_document = "index.html"
+		error_document = "error.html"
+	}
+}
+
+data "aws_s3_bucket" "bucket" {
+	bucket = "${aws_s3_bucket.bucket.id}"
+}`, randInt)
+}
