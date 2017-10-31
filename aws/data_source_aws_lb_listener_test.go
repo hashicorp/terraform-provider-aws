971ec8195c10a78cfa581cd07089eb3a0a1bcaa0
Tom Elliff <tome@cloudnc.com>
2017-10-31 15:46:52 +0000

    Find LB listener by the LB ARN
    Right now the aws_lb_listener data source isn't particularly useful unless you somehow know the ARN of the listener.
    Being able to search by the load balancer ARN means you can chain the aws_lb data source to find the LB listener from an LB name.
    
    Note that if a load balancer has multiple listeners this will error when searching by load_balancer_arn.

--- aws/data_source_aws_lb_listener_test.go
+++ aws/data_source_aws_lb_listener_test.go
@@ -34,6 +34,30 @@ func TestAccDataSourceAWSLBListener_basic(t *testing.T) {
 	})
 }
 
+func TestAccDataSourceAWSLBListenerByLBArn(t *testing.T) {
+	lbName := fmt.Sprintf("testlistener-lbarn-%s", acctest.RandStringFromCharSet(13, acctest.CharSetAlphaNum))
+	targetGroupName := fmt.Sprintf("testtargetgroup-%s", acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceAWSLBListenerConfigByLBArn(lbName, targetGroupName),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.aws_lb_listener.front_end", "load_balancer_arn"),
+					resource.TestCheckResourceAttrSet("data.aws_lb_listener.front_end", "arn"),
+					resource.TestCheckResourceAttr("data.aws_lb_listener.front_end", "protocol", "HTTP"),
+					resource.TestCheckResourceAttr("data.aws_lb_listener.front_end", "port", "80"),
+					resource.TestCheckResourceAttr("data.aws_lb_listener.front_end", "default_action.#", "1"),
+					resource.TestCheckResourceAttr("data.aws_lb_listener.front_end", "default_action.0.type", "forward"),
+					resource.TestCheckResourceAttrSet("data.aws_lb_listener.front_end", "default_action.0.target_group_arn"),
+				),
+			},
+		},
+	})
+}
+
 func TestAccDataSourceAWSLBListenerBackwardsCompatibility(t *testing.T) {
 	lbName := fmt.Sprintf("testlistener-basic-%s", acctest.RandStringFromCharSet(13, acctest.CharSetAlphaNum))
 	targetGroupName := fmt.Sprintf("testtargetgroup-%s", acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum))
@@ -181,6 +205,106 @@ resource "aws_security_group" "alb_test" {
 
 data "aws_lb_listener" "front_end" {
 	arn = "${aws_lb_listener.front_end.arn}"
+}`, lbName, targetGroupName)
+}
+
+func testAccDataSourceAWSLBListenerConfigByLBArn(lbName, targetGroupName string) string {
+	return fmt.Sprintf(`resource "aws_lb_listener" "front_end" {
+   load_balancer_arn = "${aws_lb.alb_test.id}"
+   protocol = "HTTP"
+   port = "80"
+
+   default_action {
+     target_group_arn = "${aws_lb_target_group.test.id}"
+     type = "forward"
+   }
+}
+
+resource "aws_lb" "alb_test" {
+  name            = "%s"
+  internal        = true
+  security_groups = ["${aws_security_group.alb_test.id}"]
+  subnets         = ["${aws_subnet.alb_test.*.id}"]
+
+  idle_timeout = 30
+  enable_deletion_protection = false
+
+  tags {
+    TestName = "TestAccAWSALB_basic"
+  }
+}
+
+resource "aws_lb_target_group" "test" {
+  name = "%s"
+  port = 8080
+  protocol = "HTTP"
+  vpc_id = "${aws_vpc.alb_test.id}"
+
+  health_check {
+    path = "/health"
+    interval = 60
+    port = 8081
+    protocol = "HTTP"
+    timeout = 3
+    healthy_threshold = 3
+    unhealthy_threshold = 3
+    matcher = "200-299"
+  }
+}
+
+variable "subnets" {
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+  type    = "list"
+}
+
+data "aws_availability_zones" "available" {}
+
+resource "aws_vpc" "alb_test" {
+  cidr_block = "10.0.0.0/16"
+
+  tags {
+    TestName = "TestAccAWSALB_basic"
+  }
+}
+
+resource "aws_subnet" "alb_test" {
+  count                   = 2
+  vpc_id                  = "${aws_vpc.alb_test.id}"
+  cidr_block              = "${element(var.subnets, count.index)}"
+  map_public_ip_on_launch = true
+  availability_zone       = "${element(data.aws_availability_zones.available.names, count.index)}"
+
+  tags {
+    TestName = "TestAccAWSALB_basic"
+  }
+}
+
+resource "aws_security_group" "alb_test" {
+  name        = "allow_all_alb_test"
+  description = "Used for ALB Testing"
+  vpc_id      = "${aws_vpc.alb_test.id}"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    TestName = "TestAccAWSALB_basic"
+  }
+}
+
+data "aws_lb_listener" "front_end" {
+	load_balancer_arn = "${aws_lb_listener.front_end.load_balancer_arn}"
 }`, lbName, targetGroupName)
 }
 
