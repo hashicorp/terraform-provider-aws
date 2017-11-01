6632beb8f01cea9b58c00d55abd73dd42299b180
Atsushi Ishibashi <atsushi.ishibashi@finatext.com>
2017-11-01 10:45:29 +0900

    fix

--- aws/data_source_aws_elasticache_replication_group_test.go
+++ aws/data_source_aws_elasticache_replication_group_test.go
@@ -3,19 +3,21 @@ package aws
 import (
 	"fmt"
 	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
 )
 
 func TestAccDataSourceAwsElasticacheReplicationGroup_basic(t *testing.T) {
-  resource.Test(t, resource.TestCase{
+	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccDataSourceAwsElasticacheReplicationGroupConfig_basic(),
-				Check: resource.ComposeAggregateTestCheckFunc(),
-      },
-    },
-  }
+				Check:  resource.ComposeAggregateTestCheckFunc(),
+			},
+		},
+	})
 }
 
 func testAccDataSourceAwsElasticacheReplicationGroupConfig_basic() string {
