4c79647626065e7fa58375ec5098f3f384af0271
Drew J. Sonne <drew.sonne@beamly.com>
2017-11-04 15:31:55 +0000

    Added initial test cases

---
+++ aws/resource_aws_glue_catalog_database_test.go
@@ -0,0 +1,94 @@
+package aws
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/glue"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func TestAccAWSGlueCatalogDatabase_basic(t *testing.T) {
+	rInt := acctest.RandInt()
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckGlueDatabase,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccGlueCatalogDatabase_basic(rInt),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckGlueCatalogDatabaseExists("aws_glue_catalog_database.test"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckGlueDatabase(s *terraform.State) error {
+	conn := testAccProvider.Meta().(*AWSClient).glueconn
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "aws_glue_catalog_database" {
+			continue
+		}
+
+		input := &glue.GetDatabaseInput{
+		}
+		if _, err := conn.GetDatabase(input); err != nil {
+			//Verify the error is what we want
+			if ae, ok := err.(awserr.Error); ok && ae.Code() == "" {
+				continue
+			}
+
+			return err
+		}
+		return fmt.Errorf("still exists")
+	}
+	return nil
+}
+
+func testAccGlueCatalogDatabase_basic(rInt int) string {
+	return fmt.Sprintf(`
+resource "aws_glue_catalog_database" "test" {
+  name = "my_test_catalog_database_%d"
+}
+`, rInt)
+}
+
+func testAccCheckGlueCatalogDatabaseExists(name string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[name]
+		if !ok {
+			return fmt.Errorf("Not found: %s", name)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No ID is set")
+		}
+
+		glueconn := testAccProvider.Meta().(*AWSClient).glueconn
+		out, err := glueconn.GetDatabase(&glue.GetDatabaseInput{
+
+		})
+
+		if err != nil {
+			return err
+		}
+
+		if out.Database == nil {
+			return fmt.Errorf("No Glue Database Found")
+		}
+
+		if *out.Database.Name != rs.Primary.ID {
+			return fmt.Errorf("Glue Database Mismatch - existing: %q, state: %q",
+				*out.Database, rs.Primary.ID)
+		}
+
+		return nil
+	}
+}
