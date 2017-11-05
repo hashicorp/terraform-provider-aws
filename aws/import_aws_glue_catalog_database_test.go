19ab44c425ec55ad5ef295bbc58dfbe1dc31cdcf
Drew J. Sonne <drew.sonne@beamly.com>
2017-11-05 08:54:00 +0000

    Added import test

---
+++ aws/import_aws_glue_catalog_database_test.go
@@ -0,0 +1,30 @@
+package aws
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccAWSGlueCatalogDatabaseVault_importBasic(t *testing.T) {
+	resourceName := "aws_glue_catalog_database.test"
+	rInt := acctest.RandInt()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckGlueDatabaseDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccGlueCatalogDatabase_basic(rInt),
+			},
+
+			resource.TestStep{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
