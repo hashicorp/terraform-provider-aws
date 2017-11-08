7c572c3c74509396a56e4e231eb95155fb4cb8c7
trung <trung.n.k@gmail.com>
2017-11-08 16:53:38 -0500

    #2009: support DescribeKey

--- aws/data_source_aws_kms_key.go
+++ aws/data_source_aws_kms_key.go
@@ -1,6 +1,10 @@
 package aws
 
-import "github.com/hashicorp/terraform/helper/schema"
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/aws/aws-sdk-go/service/kms"
+	"fmt"
+)
 
 func dataSourceAwsKmsKey() *schema.Resource {
 	return &schema.Resource{
@@ -17,51 +21,51 @@ func dataSourceAwsKmsKey() *schema.Resource {
 				Elem:     &schema.Schema{Type: schema.TypeString},
 			},
 			"arn": {
-				Type: schema.TypeString,
+				Type:     schema.TypeString,
 				Computed: true,
 			},
 			"aws_account_id": {
-				Type: schema.TypeString,
+				Type:     schema.TypeString,
 				Computed: true,
 			},
 			"creation_date": {
-				Type: schema.TypeFloat,
+				Type:     schema.TypeFloat,
 				Computed: true,
 			},
 			"deletion_date": {
-				Type: schema.TypeFloat,
+				Type:     schema.TypeFloat,
 				Computed: true,
 			},
 			"description": {
-				Type: schema.TypeString,
+				Type:     schema.TypeString,
 				Computed: true,
 			},
 			"enabled": {
-				Type: schema.TypeBool,
+				Type:     schema.TypeBool,
 				Computed: true,
 			},
 			"expiration_model": {
-				Type: schema.TypeString,
+				Type:     schema.TypeString,
 				Computed: true,
 			},
 			"key_manager": {
-				Type: schema.TypeString,
+				Type:     schema.TypeString,
 				Computed: true,
 			},
 			"key_state": {
-				Type: schema.TypeString,
+				Type:     schema.TypeString,
 				Computed: true,
 			},
 			"key_usage": {
-				Type: schema.TypeString,
+				Type:     schema.TypeString,
 				Computed: true,
 			},
 			"origin": {
-				Type: schema.TypeString,
+				Type:     schema.TypeString,
 				Computed: true,
 			},
 			"valid_to": {
-				Type: schema.TypeFloat,
+				Type:     schema.TypeFloat,
 				Computed: true,
 			},
 		},
@@ -69,5 +73,17 @@ func dataSourceAwsKmsKey() *schema.Resource {
 }
 
 func dataSourceAwsKmsKeyRead(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*AWSClient).kmsconn
+	keyId := d.Get("key_id")
+	input := &kms.DescribeKeyInput{
+		KeyId: keyId,
+		GrantTokens: d.Get("grant_tokens"),
+	}
+	output, err := conn.DescribeKey(input)
+	if err != nil {
+		return fmt.Errorf("Error while describing key [%s]: %s", keyId, err)
+	}
+	d.SetId(keyId)
+	d.Set("arn", output.KeyMetadata.Arn)
 	return nil
 }
