a808ddc28e6afc25e4d65c6a351fcfb8e4ede970
Radek Simko <radek.simko@gmail.com>
2017-12-18 20:32:04 +0000

    New Resource: aws_gamelift_alias

--- aws/provider.go
+++ aws/provider.go
@@ -368,6 +368,7 @@ func Provider() terraform.ResourceProvider {
 			"aws_emr_instance_group":                       resourceAwsEMRInstanceGroup(),
 			"aws_emr_security_configuration":               resourceAwsEMRSecurityConfiguration(),
 			"aws_flow_log":                                 resourceAwsFlowLog(),
+			"aws_gamelift_alias":                           resourceAwsGameliftAlias(),
 			"aws_gamelift_build":                           resourceAwsGameliftBuild(),
 			"aws_gamelift_fleet":                           resourceAwsGameliftFleet(),
 			"aws_glacier_vault":                            resourceAwsGlacierVault(),
