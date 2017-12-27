45112c893ddc174575eba20219d1a93e227105bc
Modestas Vainius <modestas@vainius.eu>
2017-12-27 12:55:20 +0200

    d/aws_ssm_parameter: Simplify test case.
    I think there is no need to add another test case just for in parameter
    check. Simply randomize it to be 100% sure.

--- aws/data_source_aws_ssm_parameter_test.go
+++ aws/data_source_aws_ssm_parameter_test.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
 )
 
 func TestAccAwsSsmParameterDataSource_basic(t *testing.T) {
 	name := "test.parameter"
+	with_decryption := []string{"true", "false"}[acctest.RandIntRange(0, 2)]
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
 			testAccPreCheck(t)
@@ -16,23 +18,13 @@ func TestAccAwsSsmParameterDataSource_basic(t *testing.T) {
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckAwsSsmParameterDataSourceConfig(name, "true"),
+				Config: testAccCheckAwsSsmParameterDataSourceConfig(name, with_decryption),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttrSet("data.aws_ssm_parameter.test", "arn"),
 					resource.TestCheckResourceAttr("data.aws_ssm_parameter.test", "name", name),
 					resource.TestCheckResourceAttr("data.aws_ssm_parameter.test", "type", "String"),
 					resource.TestCheckResourceAttr("data.aws_ssm_parameter.test", "value", "TestValue"),
-					resource.TestCheckResourceAttr("data.aws_ssm_parameter.test", "with_decryption", "true"),
-				),
-			},
-			{
-				Config: testAccCheckAwsSsmParameterDataSourceConfig(name, "false"),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttrSet("data.aws_ssm_parameter.test", "arn"),
-					resource.TestCheckResourceAttr("data.aws_ssm_parameter.test", "name", name),
-					resource.TestCheckResourceAttr("data.aws_ssm_parameter.test", "type", "String"),
-					resource.TestCheckResourceAttr("data.aws_ssm_parameter.test", "value", "TestValue"),
-					resource.TestCheckResourceAttr("data.aws_ssm_parameter.test", "with_decryption", "false"),
+					resource.TestCheckResourceAttr("data.aws_ssm_parameter.test", "with_decryption", with_decryption),
 				),
 			},
 		},
