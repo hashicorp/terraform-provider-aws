0c975ae75b9630d744ecd366c759cb83743606fe
Colin Hebert <hebert.colin@gmail.com>
2017-12-28 09:20:31 +0100

    Create validateLambdaFunctionNameArn
    Lambda "function name" is an overloaded term for both the ARN of the function and its actual name.
    
    Create a validator for the actual name and the ARN.

--- aws/validators.go
+++ aws/validators.go
@@ -349,6 +349,23 @@ func validateCloudWatchEventTargetId(v interface{}, k string) (ws []string, erro
 
 func validateLambdaFunctionName(v interface{}, k string) (ws []string, errors []error) {
 	value := v.(string)
+	if len(value) > 64 {
+		errors = append(errors, fmt.Errorf(
+			"%q cannot be longer than 140 characters: %q", k, value))
+	}
+	// http://docs.aws.amazon.com/lambda/latest/dg/API_AddPermission.html
+	pattern := `^[a-zA-Z0-9-_]+$`
+	if !regexp.MustCompile(pattern).MatchString(value) {
+		errors = append(errors, fmt.Errorf(
+			"%q doesn't comply with restrictions (%q): %q",
+			k, pattern, value))
+	}
+
+	return
+}
+
+func validateLambdaFunctionNameArn(v interface{}, k string) (ws []string, errors []error) {
+	value := v.(string)
 	if len(value) > 140 {
 		errors = append(errors, fmt.Errorf(
 			"%q cannot be longer than 140 characters: %q", k, value))
