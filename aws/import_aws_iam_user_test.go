0e99fac806a6cf61113a43956116d2b4838ef4e8
Erran Carey <ecarey@rapid7.com>
2018-01-10 17:37:15 +0000

    test/aws_iam_user: Create failing test for iam user import with policy

--- aws/import_aws_iam_user_test.go
+++ aws/import_aws_iam_user_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
 )
 
 func TestAccAWSUser_importBasic(t *testing.T) {
@@ -25,6 +26,57 @@ func TestAccAWSUser_importBasic(t *testing.T) {
 			resource.TestStep{
 				ResourceName:      resourceName,
 				ImportState:       true,
+				ImportStateVerify: true,
+				ImportStateVerifyIgnore: []string{
+					"force_destroy"},
+			},
+		},
+	})
+}
+
+func TestAccAWSUser_importWithPolicy(t *testing.T) {
+	resourceName := "aws_iam_user.user"
+
+	rInt := acctest.RandInt()
+
+	checkFn := func(s []*terraform.InstanceState) error {
+		// Expect 2: user + policy
+		if len(s) != 2 {
+			return fmt.Errorf("expected 2 states: %#v", s)
+		}
+
+		// TODO: Is this order guaranteed?
+		policyState, userState := s[0], s[1]
+
+		expectedUserId := fmt.Sprintf("test_user_%d", rInt)
+		expectedPolicyId := fmt.Sprintf("test_user_%d:foo_policy_%d", rInt, rInt)
+
+		if userState.ID != expectedUserId {
+			return fmt.Errorf("expected user of ID %s, %s received",
+				expectedUserId, userState.ID)
+		}
+
+		if policyState.ID != expectedPolicyId {
+			return fmt.Errorf("expected policy of ID %s, %s received",
+				expectedPolicyId, policyState.ID)
+		}
+
+		return nil
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAWSUserDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccIAMUserPolicyConfig(rInt),
+			},
+
+			resource.TestStep{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateCheck:  checkFn,
 				ImportStateVerify: true,
 				ImportStateVerifyIgnore: []string{
 					"force_destroy"},
