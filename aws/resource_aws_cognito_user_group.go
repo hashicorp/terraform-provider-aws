8c141966cbc386466232d7225b99df2e8521e04a
Tom Elliff <tomelliff@gmail.com>
2018-01-11 08:26:50 +0000

    New resource aws_cognito_user_group

---
+++ aws/resource_aws_cognito_user_group.go
@@ -0,0 +1,145 @@
+package aws
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
+	"github.com/hashicorp/errwrap"
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourceAwsCognitoUserGroup() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceAwsCognitoUserGroupCreate,
+		Read:   resourceAwsCognitoUserGroupRead,
+		Update: resourceAwsCognitoUserGroupUpdate,
+		Delete: resourceAwsCognitoUserGroupDelete,
+
+		Schema: map[string]*schema.Schema{
+			"description": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"precedence": {
+				Type:     schema.TypeInt,
+				Optional: true,
+			},
+			"role_arn": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"user_pool_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+		},
+	}
+}
+
+func resourceAwsCognitoUserGroupCreate(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*AWSClient).cognitoidpconn
+
+	params := &cognitoidentityprovider.CreateGroupInput{
+		GroupName:  aws.String(d.Get("name").(string)),
+		UserPoolId: aws.String(d.Get("user_pool_id").(string)),
+	}
+
+	if v, ok := d.GetOk("description"); ok {
+		params.Description = aws.String(v.(string))
+	}
+
+	if v, ok := d.GetOk("precedence"); ok {
+		params.Precedence = aws.Int64(int64(v.(int)))
+	}
+
+	if v, ok := d.GetOk("role_arn"); ok {
+		params.RoleArn = aws.String(v.(string))
+	}
+
+	resp, err := conn.CreateGroup(params)
+	if err != nil {
+		return errwrap.Wrapf("Error creating Cognito User Group: {{err}}", err)
+	}
+
+	d.SetId(*resp.Group.GroupName)
+
+	return resourceAwsCognitoUserGroupRead(d, meta)
+}
+
+func resourceAwsCognitoUserGroupRead(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*AWSClient).cognitoidpconn
+
+	params := &cognitoidentityprovider.GetGroupInput{
+		GroupName:  aws.String(d.Get("name").(string)),
+		UserPoolId: aws.String(d.Get("user_pool_id").(string)),
+	}
+
+	resp, err := conn.GetGroup(params)
+	if err != nil {
+		return errwrap.Wrapf("Error reading Cognito User Group: {{err}}", err)
+	}
+
+	if resp.Group.Description != nil {
+		d.Set("description", *resp.Group.Description)
+	}
+
+	if resp.Group.Precedence != nil {
+		d.Set("precedence", *resp.Group.Precedence)
+	}
+
+	if resp.Group.RoleArn != nil {
+		d.Set("role_arn", *resp.Group.RoleArn)
+	}
+
+	return nil
+}
+
+func resourceAwsCognitoUserGroupUpdate(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*AWSClient).cognitoidpconn
+
+	params := &cognitoidentityprovider.UpdateGroupInput{
+		GroupName:  aws.String(d.Get("name").(string)),
+		UserPoolId: aws.String(d.Get("user_pool_id").(string)),
+	}
+
+	if d.HasChange("description") {
+		params.Description = aws.String(d.Get("description").(string))
+	}
+
+	if d.HasChange("precedence") {
+		params.Precedence = aws.Int64(int64(d.Get("precedence").(int)))
+	}
+
+	if d.HasChange("role_arn") {
+		params.RoleArn = aws.String(d.Get("description").(string))
+	}
+
+	_, err := conn.UpdateGroup(params)
+	if err != nil {
+		return errwrap.Wrapf("Error updating Cognito User Group: {{err}}", err)
+	}
+
+	return resourceAwsCognitoUserGroupRead(d, meta)
+}
+
+func resourceAwsCognitoUserGroupDelete(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*AWSClient).cognitoidpconn
+
+	params := &cognitoidentityprovider.DeleteGroupInput{
+		GroupName:  aws.String(d.Get("name").(string)),
+		UserPoolId: aws.String(d.Get("user_pool_id").(string)),
+	}
+
+	_, err := conn.DeleteGroup(params)
+
+	if err != nil {
+		return errwrap.Wrapf("Error deleting Cognito User Group: {{err}}", err)
+	}
+
+	return nil
+}
---
+++ aws/resource_aws_cognito_user_group_test.go
@@ -0,0 +1,190 @@
+package aws
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func TestAccAWSCognitoUserGroup_basic(t *testing.T) {
+	poolName := fmt.Sprintf("%s", acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum))
+	groupName := fmt.Sprintf("%s", acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum))
+	updatedGroupName := fmt.Sprintf("%s", acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAWSCognitoUserGroupDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAWSCognitoUserGroupConfig_basic(poolName, groupName),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccCheckAWSCognitoUserGroupExists("aws_cognito_user_group.main"),
+					resource.TestCheckResourceAttr("aws_cognito_user_group.main", "name", fmt.Sprintf("user-group-%s", groupName)),
+				),
+			},
+			{
+				Config: testAccAWSCognitoUserGroupConfig_basic(poolName, updatedGroupName),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccCheckAWSCognitoUserGroupExists("aws_cognito_user_group.main"),
+					resource.TestCheckResourceAttr("aws_cognito_user_group.main", "name", fmt.Sprintf("user-group-%s", updatedGroupName)),
+				),
+			},
+		},
+	})
+}
+
+func TestAccAWSCognitoUserGroup_complex(t *testing.T) {
+	poolName := fmt.Sprintf("%s", acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum))
+	groupName := fmt.Sprintf("%s", acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum))
+	updatedGroupName := fmt.Sprintf("%s", acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAWSCognitoUserGroupDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAWSCognitoUserGroupConfig_complex(poolName, groupName, "This is the user group description", 1),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccCheckAWSCognitoUserGroupExists("aws_cognito_user_group.main"),
+					resource.TestCheckResourceAttr("aws_cognito_user_group.main", "name", fmt.Sprintf("user-group-%s", groupName)),
+					resource.TestCheckResourceAttr("aws_cognito_user_group.main", "description", "This is the user group description"),
+					resource.TestCheckResourceAttr("aws_cognito_user_group.main", "precedence", "1"),
+					resource.TestCheckResourceAttrSet("aws_cognito_user_group.main", "role_arn"),
+				),
+			},
+			{
+				Config: testAccAWSCognitoUserGroupConfig_complex(poolName, updatedGroupName, "This is the updated user group description", 42),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccCheckAWSCognitoUserGroupExists("aws_cognito_user_group.main"),
+					resource.TestCheckResourceAttr("aws_cognito_user_group.main", "name", fmt.Sprintf("user-group-%s", updatedGroupName)),
+					resource.TestCheckResourceAttr("aws_cognito_user_group.main", "description", "This is the updated user group description"),
+					resource.TestCheckResourceAttr("aws_cognito_user_group.main", "precedence", "42"),
+					resource.TestCheckResourceAttrSet("aws_cognito_user_group.main", "role_arn"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckAWSCognitoUserGroupExists(name string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[name]
+		if !ok {
+			return fmt.Errorf("Not found: %s", name)
+		}
+
+		if rs.Primary.ID == "" {
+			return errors.New("No Cognito User Group Name set")
+		}
+
+		if rs.Primary.Attributes["user_pool_id"] == "" {
+			return errors.New("No Cognito User Pool Id set")
+		}
+
+		conn := testAccProvider.Meta().(*AWSClient).cognitoidpconn
+
+		params := &cognitoidentityprovider.GetGroupInput{
+			GroupName:  aws.String(rs.Primary.ID),
+			UserPoolId: aws.String(rs.Primary.Attributes["user_pool_id"]),
+		}
+
+		_, err := conn.GetGroup(params)
+
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckAWSCognitoUserGroupDestroy(s *terraform.State) error {
+	conn := testAccProvider.Meta().(*AWSClient).cognitoidpconn
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "aws_cognito_user_group" {
+			continue
+		}
+
+		params := &cognitoidentityprovider.GetGroupInput{
+			GroupName:  aws.String(rs.Primary.ID),
+			UserPoolId: aws.String(rs.Primary.Attributes["user_pool_id"]),
+		}
+
+		_, err := conn.GetGroup(params)
+
+		if err != nil {
+			if awsErr, ok := err.(awserr.Error); ok && awsErr.Code() == "ResourceNotFoundException" {
+				return nil
+			}
+			return err
+		}
+	}
+
+	return nil
+}
+
+func testAccAWSCognitoUserGroupConfig_basic(poolName, groupName string) string {
+	return fmt.Sprintf(`
+resource "aws_cognito_user_pool" "main" {
+  name = "identity pool %s"
+}
+
+resource "aws_cognito_user_group" "main" {
+  name         = "user-group-%s"
+  user_pool_id = "${aws_cognito_user_pool.main.id}"
+}
+`, poolName, groupName)
+}
+
+func testAccAWSCognitoUserGroupConfig_complex(poolName, groupName, groupDescription string, precedence int) string {
+	return fmt.Sprintf(`
+resource "aws_cognito_user_pool" "main" {
+  name = "identity pool %s"
+}
+
+resource "aws_iam_role" "group_role" {
+  name = "user-group-%s"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Federated": "cognito-identity.amazonaws.com"
+      },
+      "Action": "sts:AssumeRoleWithWebIdentity",
+      "Condition": {
+        "StringEquals": {
+          "cognito-identity.amazonaws.com:aud": "us-east-1:12345678-dead-beef-cafe-123456790ab"
+        },
+        "ForAnyValue:StringLike": {
+          "cognito-identity.amazonaws.com:amr": "authenticated"
+        }
+      }
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_cognito_user_group" "main" {
+  name         = "user-group-%s"
+  user_pool_id = "${aws_cognito_user_pool.main.id}"
+  description  = "%s"
+  precedence   = %v
+  role_arn     = "${aws_iam_role.group_role.arn}"
+}
+`, poolName, groupName, groupName, groupDescription, precedence)
+}
