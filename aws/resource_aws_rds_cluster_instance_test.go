bdf96539ff54c9075942a9328cb52c8bfaa6b6ef
kwent <contact@quent.in>
2018-01-22 20:27:17 -0800

    Lint

--- aws/resource_aws_rds_cluster_instance_test.go
+++ aws/resource_aws_rds_cluster_instance_test.go
@@ -245,7 +245,7 @@ func TestAccAWSRDSClusterInstance_withInstancePerformanceInsights(t *testing.T) 
 					testAccCheckAWSClusterInstanceExists("aws_rds_cluster_instance.cluster_instances", &v),
 					testAccCheckAWSDBClusterInstanceAttributes(&v),
 					resource.TestCheckResourceAttr(
-						"aws_rds_cluster_instance.cluster_instances", "performance_insights_enabled", "true")
+						"aws_rds_cluster_instance.cluster_instances", "performance_insights_enabled", "true"),
 				),
 			},
 		},
