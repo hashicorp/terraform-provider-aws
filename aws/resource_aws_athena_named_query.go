1f07afeaeab3f28cd2346706acd259b7edc2b12c
Atsushi Ishibashi <atsushi.ishibashi@finatext.com>
2018-02-02 09:21:03 +0900

    remove &schema.Schema

--- aws/resource_aws_athena_named_query.go
+++ aws/resource_aws_athena_named_query.go
@@ -19,22 +19,22 @@ func resourceAwsAthenaNamedQuery() *schema.Resource {
 		},
 
 		Schema: map[string]*schema.Schema{
-			"name": &schema.Schema{
+			"name": {
 				Type:     schema.TypeString,
 				Required: true,
 				ForceNew: true,
 			},
-			"query": &schema.Schema{
+			"query": {
 				Type:     schema.TypeString,
 				Required: true,
 				ForceNew: true,
 			},
-			"database": &schema.Schema{
+			"database": {
 				Type:     schema.TypeString,
 				Required: true,
 				ForceNew: true,
 			},
-			"description": &schema.Schema{
+			"description": {
 				Type:     schema.TypeString,
 				Optional: true,
 				ForceNew: true,
