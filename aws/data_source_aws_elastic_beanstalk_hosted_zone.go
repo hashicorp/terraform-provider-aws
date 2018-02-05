38e5f8710a1294a406786bfeb5780590f0c0c8ef
Stuart Auld <sauld@cozero.com.au>
2018-02-06 07:34:07 +1100

    remove unsupported region support, lexically sort region map

--- aws/data_source_aws_elastic_beanstalk_hosted_zone.go
+++ aws/data_source_aws_elastic_beanstalk_hosted_zone.go
@@ -6,26 +6,23 @@ import (
 	"github.com/hashicorp/terraform/helper/schema"
 )
 
-const unsupportedElasticBeanstalkRegion = "UnsupportedRegion"
-
 // See # http://docs.aws.amazon.com/general/latest/gr/rande.html#elasticbeanstalk_region
 var elasticBeanstalkHostedZoneIds = map[string]string{
-	"us-east-2":      "Z14LCN19Q5QHIC",
-	"us-east-1":      "Z117KPS5GTRQ2G",
-	"us-west-1":      "Z1LQECGX5PH1X",
-	"us-west-2":      "Z38NKT9BP95V3O",
-	"ca-central-1":   "ZJFCZL7SSZB5I",
-	"ap-south-1":     "Z18NTBI3Y7N9TZ",
-	"ap-northeast-2": "Z3JE5OI70TWKCP",
 	"ap-southeast-1": "Z16FZ9L249IFLT",
 	"ap-southeast-2": "Z2PCDNR3VC2G1N",
 	"ap-northeast-1": "Z1R25G3KIG2GBW",
-	"cn-northwest-1": unsupportedElasticBeanstalkRegion,
+	"ap-northeast-2": "Z3JE5OI70TWKCP",
+	"ap-south-1":     "Z18NTBI3Y7N9TZ",
+	"ca-central-1":   "ZJFCZL7SSZB5I",
 	"eu-central-1":   "Z1FRNW7UH4DEZJ",
 	"eu-west-1":      "Z2NYPWQ7DFZAZH",
 	"eu-west-2":      "Z1GKAAAUGATPF1",
 	"eu-west-3":      "Z5WN6GAYWG5OB",
 	"sa-east-1":      "Z10X7K2B4QSOFV",
+	"us-east-1":      "Z117KPS5GTRQ2G",
+	"us-east-2":      "Z14LCN19Q5QHIC",
+	"us-west-1":      "Z1LQECGX5PH1X",
+	"us-west-2":      "Z38NKT9BP95V3O",
 }
 
 func dataSourceAwsElasticBeanstalkHostedZone() *schema.Resource {
@@ -49,12 +46,8 @@ func dataSourceAwsElasticBeanstalkHostedZoneRead(d *schema.ResourceData, meta in
 
 	zoneID := elasticBeanstalkHostedZoneIds[region]
 
-	if zoneID == unsupportedElasticBeanstalkRegion {
-		return fmt.Errorf("Unsupported region (%q)", region)
-	}
-
 	if zoneID == "" {
-		return fmt.Errorf("Unknown region (%q)", region)
+		return fmt.Errorf("Unknown region or elasticbeanstalk not supported (%q)", region)
 	}
 
 	d.SetId(zoneID)
