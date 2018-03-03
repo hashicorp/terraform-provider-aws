44d5e6857090194ca8d25176169775c90c73bc75
Dominik Froehlich <dominik.froehlich@sap.com>
2018-03-03 09:03:20 +0100

    remove update timeout

--- aws/resource_aws_security_group.go
+++ aws/resource_aws_security_group.go
@@ -29,7 +29,6 @@ func resourceAwsSecurityGroup() *schema.Resource {
 
 		Timeouts: &schema.ResourceTimeout{
 			Create: schema.DefaultTimeout(10 * time.Minute),
-			Update: schema.DefaultTimeout(10 * time.Minute),
 			Delete: schema.DefaultTimeout(10 * time.Minute),
 		},
 
