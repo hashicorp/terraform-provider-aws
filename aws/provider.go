490bd3ac4e99320089342d92483977d0830ff213
Jarred Trainor <jarred@soundcloud.com>
2018-03-05 12:48:40 +0100

    New resource: aws_dx_gateway_association

--- aws/provider.go
+++ aws/provider.go
@@ -337,6 +337,7 @@ func Provider() terraform.ResourceProvider {
 			"aws_dx_connection":                            resourceAwsDxConnection(),
 			"aws_dx_connection_association":                resourceAwsDxConnectionAssociation(),
 			"aws_dx_gateway":                               resourceAwsDxGateway(),
+			"aws_dx_gateway_association":                   resourceAwsDxGatewayAssociation(),
 			"aws_dynamodb_table":                           resourceAwsDynamoDbTable(),
 			"aws_dynamodb_table_item":                      resourceAwsDynamoDbTableItem(),
 			"aws_dynamodb_global_table":                    resourceAwsDynamoDbGlobalTable(),
