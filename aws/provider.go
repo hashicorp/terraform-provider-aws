f617d80dfb35a4992341e7806d1844acdb6a163d
Danniel Magno <dennyloko@gmail.com>
2018-03-15 05:43:34 -0300

    New Resource: aws_wafregional_sql_injection_match_set (#1013)
    * Add support for resource_aws_wafregional_sql_injection_match_set
    
    * Implement feedback and clean up code

--- aws/provider.go
+++ aws/provider.go
@@ -556,6 +556,7 @@ func Provider() terraform.ResourceProvider {
 			"aws_waf_geo_match_set":                        resourceAwsWafGeoMatchSet(),
 			"aws_wafregional_byte_match_set":               resourceAwsWafRegionalByteMatchSet(),
 			"aws_wafregional_ipset":                        resourceAwsWafRegionalIPSet(),
+			"aws_wafregional_sql_injection_match_set":      resourceAwsWafRegionalSqlInjectionMatchSet(),
 			"aws_wafregional_xss_match_set":                resourceAwsWafRegionalXssMatchSet(),
 			"aws_batch_compute_environment":                resourceAwsBatchComputeEnvironment(),
 			"aws_batch_job_definition":                     resourceAwsBatchJobDefinition(),
