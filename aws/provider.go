b66fcae6c9a9198d546b82588edec7eb75c7b61f
Alex Pilon <apilon@hashicorp.com>
2018-03-15 17:01:39 -0400

    implement acceptance tests

--- aws/provider.go
+++ aws/provider.go
@@ -556,7 +556,7 @@ func Provider() terraform.ResourceProvider {
 			"aws_waf_geo_match_set":                        resourceAwsWafGeoMatchSet(),
 			"aws_wafregional_byte_match_set":               resourceAwsWafRegionalByteMatchSet(),
 			"aws_wafregional_ipset":                        resourceAwsWafRegionalIPSet(),
-			"aws_wafregional_size_constraint_set":      	resourceAwsWafRegionalSizeConstraintSet(),
+			"aws_wafregional_size_constraint_set":          resourceAwsWafRegionalSizeConstraintSet(),
 			"aws_wafregional_sql_injection_match_set":      resourceAwsWafRegionalSqlInjectionMatchSet(),
 			"aws_wafregional_xss_match_set":                resourceAwsWafRegionalXssMatchSet(),
 			"aws_wafregional_rule":                         resourceAwsWafRegionalRule(),
