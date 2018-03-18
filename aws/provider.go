cecf1d860e8dd28e7696803a954213511dad1353
Pascal van Buijtene <pvanbuijtene@users.noreply.github.com>
2018-03-18 13:24:56 +0100

    New Resource: aws_wafregional_rule (#3756)
    * Add support for aws_wafregional_rule
    
    * Updating docs
    
    * Use singular name for predicate field
    
    * Use helpers
    
    * Use flatten func
    
    * Add test for rule without predicates
    
    * Add test for predicate changes
    
    * Require data_id for predicate
    
    * Reformat imports + remove redundant code

--- aws/provider.go
+++ aws/provider.go
@@ -558,6 +558,7 @@ func Provider() terraform.ResourceProvider {
 			"aws_wafregional_ipset":                        resourceAwsWafRegionalIPSet(),
 			"aws_wafregional_sql_injection_match_set":      resourceAwsWafRegionalSqlInjectionMatchSet(),
 			"aws_wafregional_xss_match_set":                resourceAwsWafRegionalXssMatchSet(),
+			"aws_wafregional_rule":                         resourceAwsWafRegionalRule(),
 			"aws_batch_compute_environment":                resourceAwsBatchComputeEnvironment(),
 			"aws_batch_job_definition":                     resourceAwsBatchJobDefinition(),
 			"aws_batch_job_queue":                          resourceAwsBatchJobQueue(),
