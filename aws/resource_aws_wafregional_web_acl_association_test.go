ba79fde5647a29361fb6b510cd4ffe9e3f5bb149
Radek Simko <radek.simko@gmail.com>
2018-03-23 07:14:01 +0000

    Fix indentation in example + test

--- aws/resource_aws_wafregional_web_acl_association_test.go
+++ aws/resource_aws_wafregional_web_acl_association_test.go
@@ -136,19 +136,19 @@ resource "aws_wafregional_web_acl" "foo" {
 }
 
 resource "aws_vpc" "foo" {
-	cidr_block = "10.1.0.0/16"
+  cidr_block = "10.1.0.0/16"
 }
 
 data "aws_availability_zones" "available" {}
 
 resource "aws_subnet" "foo" {
-	vpc_id = "${aws_vpc.foo.id}"
+  vpc_id = "${aws_vpc.foo.id}"
   cidr_block = "10.1.1.0/24"
   availability_zone = "${data.aws_availability_zones.available.names[0]}"
 }
 
 resource "aws_subnet" "bar" {
-	vpc_id = "${aws_vpc.foo.id}"
+  vpc_id = "${aws_vpc.foo.id}"
   cidr_block = "10.1.2.0/24"
   availability_zone = "${data.aws_availability_zones.available.names[1]}"
 }
