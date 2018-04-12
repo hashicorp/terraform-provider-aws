dc980ced9a8caf02964fc41d2d06060f3c9a9920
Brian Flad <bflad417@gmail.com>
2018-04-12 11:40:14 -0400

    tests/data-source/aws_cloudwatch_log_group: Randomize naming

--- aws/data_source_aws_cloudwatch_log_group_test.go
+++ aws/data_source_aws_cloudwatch_log_group_test.go
@@ -4,18 +4,21 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
 )
 
 func TestAccAWSCloudwatchLogGroupDataSource(t *testing.T) {
+	rName := acctest.RandomWithPrefix("tf-acc-test")
+
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckAWSCloudwatchLogGroupDataSourceConfig,
+				Config: testAccCheckAWSCloudwatchLogGroupDataSourceConfig(rName),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.aws_cloudwatch_log_group.test", "name", "Test"),
+					resource.TestCheckResourceAttr("data.aws_cloudwatch_log_group.test", "name", rName),
 					resource.TestCheckResourceAttrSet("data.aws_cloudwatch_log_group.test", "arn"),
 					resource.TestCheckResourceAttrSet("data.aws_cloudwatch_log_group.test", "creation_time"),
 				),
@@ -25,12 +28,14 @@ func TestAccAWSCloudwatchLogGroupDataSource(t *testing.T) {
 	return
 }
 
-var testAccCheckAWSCloudwatchLogGroupDataSourceConfig = fmt.Sprintf(`
+func testAccCheckAWSCloudwatchLogGroupDataSourceConfig(rName string) string {
+	return fmt.Sprintf(`
 resource aws_cloudwatch_log_group "test" {
-  name = "Test"
+  name = "%s"
 }
 
 data aws_cloudwatch_log_group "test" {
   name = "${aws_cloudwatch_log_group.test.name}"
 }
-`)
+`, rName)
+}
