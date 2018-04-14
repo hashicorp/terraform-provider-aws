0f9f57d035951a2a8e05abed7ec1909e7c76fad8
Tom Elliff <tomelliff@gmail.com>
2018-04-14 20:11:00 +0100

    Fix vet issue

--- aws/resource_aws_organizations_account_test.go
+++ aws/resource_aws_organizations_account_test.go
@@ -21,7 +21,7 @@ func testAccAwsOrganizationsAccount_basic(t *testing.T) {
 	}
 
 	rInt := acctest.RandInt()
-	name := fmt.Sprintf("tf_acctest_%s", rInt)
+	name := fmt.Sprintf("tf_acctest_%d", rInt)
 	email := fmt.Sprintf("tf-acctest+%d@%s", rInt, orgsEmailDomain)
 
 	resource.Test(t, resource.TestCase{
