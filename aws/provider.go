a327b8e37362581f3a00988549fa5915ca461e7f
Brian Flad <bflad417@gmail.com>
2018-04-18 13:51:24 -0400

    New Resource: aws_organizations_policy_attachment

--- aws/provider.go
+++ aws/provider.go
@@ -476,6 +476,7 @@ func Provider() terraform.ResourceProvider {
 			"aws_organizations_organization":               resourceAwsOrganizationsOrganization(),
 			"aws_organizations_account":                    resourceAwsOrganizationsAccount(),
 			"aws_organizations_policy":                     resourceAwsOrganizationsPolicy(),
+			"aws_organizations_policy_attachment":          resourceAwsOrganizationsPolicyAttachment(),
 			"aws_placement_group":                          resourceAwsPlacementGroup(),
 			"aws_proxy_protocol_policy":                    resourceAwsProxyProtocolPolicy(),
 			"aws_rds_cluster":                              resourceAwsRDSCluster(),
