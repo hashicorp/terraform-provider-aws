c9ccda055c33f6c6519280fe1e92d4ce97c59a13
Brian Flad <bflad417@gmail.com>
2018-04-24 12:40:17 -0400

    resource/aws_secretsmanager_secret: Fix imports formatting

--- aws/resource_aws_secretsmanager_secret.go
+++ aws/resource_aws_secretsmanager_secret.go
@@ -5,10 +5,9 @@ import (
 	"log"
 	"time"
 
-	"github.com/hashicorp/terraform/helper/resource"
-
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/secretsmanager"
+	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/helper/validation"
 )
