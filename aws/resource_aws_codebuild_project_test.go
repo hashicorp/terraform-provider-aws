7bd7d5866c4e00c284427f669cdc379cb59ce10a
Ryan Kennedy <rkennedy@2ndwatch.com>
2018-06-22 11:57:32 -0700

    adding acceptance test (TestAccAWSCodeBuildProject_WindowsContainer) for #4940. credit: @callmeradical

--- aws/resource_aws_codebuild_project_test.go
+++ aws/resource_aws_codebuild_project_test.go
@@ -418,6 +418,32 @@ func TestAccAWSCodeBuildProject_VpcConfig(t *testing.T) {
 	})
 }
 
+func TestAccAWSCodeBuildProject_WindowsContainer(t *testing.T) {
+	var project codebuild.Project
+	rName := acctest.RandomWithPrefix("tf-acc-test")
+	resourceName := "aws_codebuild_project.test"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAWSCodeBuildProjectDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAWSCodeBuildProjectConfig_WindowsContainer(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAWSCodeBuildProjectExists(resourceName, &project),
+					resource.TestCheckResourceAttr(resourceName, "environment.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "environment.3935046469.compute_type", "BUILD_GENERAL1_MEDIUM"),
+					resource.TestCheckResourceAttr(resourceName, "environment.3935046469.environment_variable.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "environment.3935046469.image", "2"),
+					resource.TestCheckResourceAttr(resourceName, "environment.3935046469.privileged_mode", "false"),
+					resource.TestCheckResourceAttr(resourceName, "environment.3935046469.type", "WINDOWS_CONTAINER"),
+				),
+			},
+		},
+	})
+}
+
 func TestAWSCodeBuildProject_nameValidation(t *testing.T) {
 	cases := []struct {
 		Value    string
@@ -911,4 +937,28 @@ resource "aws_codebuild_project" "test" {
   }
 }
 `, subnetCount, rName)
+}
+
+func testAccAWSCodeBuildProjectConfig_WindowsContainer(rName string) string {
+	return testAccAWSCodeBuildProjectConfig_Base_ServiceRole(rName) + fmt.Sprintf(`
+resource "aws_codebuild_project" "test" {
+  name         = "%s"
+  service_role = "${aws_iam_role.test.arn}"
+
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+
+  environment {
+    compute_type = "BUILD_GENERAL1_MEDIUM"
+    image        = "2"
+    type         = "WINDOWS_CONTAINER"
+  }
+
+  source {
+    location = "%s"
+    type     = "GITHUB"
+  }
+}
+`, rName, testAccAWSCodeBuildGitHubSourceLocationFromEnv())
 }
