93e11382b83c1ceb6e1f49f56d6b7e2ca8a534cd
Daniel White <dawhite@seek.com.au>
2018-06-25 18:41:43 +1000

    New Resource: aws_s3_bucket_inventory

--- aws/provider.go
+++ aws/provider.go
@@ -559,6 +559,7 @@ func Provider() terraform.ResourceProvider {
 			"aws_s3_bucket_object":                             resourceAwsS3BucketObject(),
 			"aws_s3_bucket_notification":                       resourceAwsS3BucketNotification(),
 			"aws_s3_bucket_metric":                             resourceAwsS3BucketMetric(),
+			"aws_s3_bucket_inventory":                          resourceAwsS3BucketInventory(),
 			"aws_security_group":                               resourceAwsSecurityGroup(),
 			"aws_network_interface_sg_attachment":              resourceAwsNetworkInterfaceSGAttachment(),
 			"aws_default_security_group":                       resourceAwsDefaultSecurityGroup(),
