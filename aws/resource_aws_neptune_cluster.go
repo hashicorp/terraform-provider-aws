cc5b78eedea468b37a81dfc389bb7a94a6c6c713
saravanan30erd <saravanan30erd@gmail.com>
2018-06-28 07:38:01 +0400

    create cluster from snapshot

--- aws/resource_aws_neptune_cluster.go
+++ aws/resource_aws_neptune_cluster.go
@@ -251,3 +251,112 @@ func resourceAwsNeptuneCluster() *schema.Resource {
 		},
 	}
 }
+
+func resourceAwsNeptuneClusterCreate(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*AWSClient).neptuneconn
+	tags := tagsFromMapNeptune(d.Get("tags").(map[string]interface{}))
+
+	var identifier string
+	if v, ok := d.GetOk("cluster_identifier"); ok {
+		identifier = v.(string)
+	} else {
+		if v, ok := d.GetOk("cluster_identifier_prefix"); ok {
+			identifier = resource.PrefixedUniqueId(v.(string))
+		} else {
+			identifier = resource.PrefixedUniqueId("tf-")
+		}
+
+		d.Set("cluster_identifier", identifier)
+	}
+
+	if _, ok := d.GetOk("snapshot_identifier"); ok {
+		opts := neptune.RestoreDBClusterFromSnapshotInput{
+			DBClusterIdentifier: aws.String(d.Get("cluster_identifier").(string)),
+			Engine:              aws.String(d.Get("engine").(string)),
+			SnapshotIdentifier:  aws.String(d.Get("snapshot_identifier").(string)),
+			Tags:                tags,
+		}
+
+		if attr, ok := d.GetOk("engine_version"); ok {
+			opts.EngineVersion = aws.String(attr.(string))
+		}
+
+		if attr := d.Get("availability_zones").(*schema.Set); attr.Len() > 0 {
+			opts.AvailabilityZones = expandStringList(attr.List())
+		}
+
+		if attr, ok := d.GetOk("db_subnet_group_name"); ok {
+			opts.DBSubnetGroupName = aws.String(attr.(string))
+		}
+
+		if attr, ok := d.GetOk("database_name"); ok {
+			opts.DatabaseName = aws.String(attr.(string))
+		}
+
+		if attr, ok := d.GetOk("port"); ok {
+			opts.Port = aws.Int64(int64(attr.(int)))
+		}
+
+		// Check if any of the parameters that require a cluster modification after creation are set
+		var clusterUpdate bool
+		if attr := d.Get("vpc_security_group_ids").(*schema.Set); attr.Len() > 0 {
+			clusterUpdate = true
+			opts.VpcSecurityGroupIds = expandStringList(attr.List())
+		}
+
+		if _, ok := d.GetOk("db_cluster_parameter_group_name"); ok {
+			clusterUpdate = true
+		}
+
+		if _, ok := d.GetOk("backup_retention_period"); ok {
+			clusterUpdate = true
+		}
+
+		log.Printf("[DEBUG] Neptune Cluster restore from snapshot configuration: %s", opts)
+		err := resource.Retry(1*time.Minute, func() *resource.RetryError {
+			_, err := conn.RestoreDBClusterFromSnapshot(&opts)
+			if err != nil {
+				if isAWSErr(err, "InvalidParameterValue", "IAM role ARN value is invalid or does not include the required permissions") {
+					return resource.RetryableError(err)
+				}
+				return resource.NonRetryableError(err)
+			}
+			return nil
+		})
+		if err != nil {
+			return fmt.Errorf("Error creating Neptune Cluster: %s", err)
+		}
+
+		if clusterUpdate {
+			log.Printf("[INFO] Neptune Cluster is restoring from snapshot with default db_cluster_parameter_group_name, backup_retention_period and vpc_security_group_ids" +
+				"but custom values should be set, will now update after snapshot is restored!")
+
+			d.SetId(d.Get("cluster_identifier").(string))
+
+			log.Printf("[INFO] Neptune Cluster ID: %s", d.Id())
+
+			log.Println("[INFO] Waiting for Neptune Cluster to be available")
+
+			stateConf := &resource.StateChangeConf{
+				Pending:    resourceAwsNeptuneClusterCreatePendingStates,
+				Target:     []string{"available"},
+				Refresh:    resourceAwsNeptuneClusterStateRefreshFunc(d, meta),
+				Timeout:    d.Timeout(schema.TimeoutCreate),
+				MinTimeout: 10 * time.Second,
+				Delay:      30 * time.Second,
+			}
+
+			// Wait, catching any errors
+			_, err := stateConf.WaitForState()
+			if err != nil {
+				return err
+			}
+
+			err = resourceAwsNeptuneClusterUpdate(d, meta)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+}
