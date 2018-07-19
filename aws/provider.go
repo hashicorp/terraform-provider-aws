c875593be277f2917edb2a49b9de25887eafe155
Brian Flad <bflad417@gmail.com>
2018-07-19 11:37:38 -0400

    New Resource: aws_storagegateway_nfs_file_share

--- aws/provider.go
+++ aws/provider.go
@@ -583,6 +583,7 @@ func Provider() terraform.ResourceProvider {
 			"aws_ssm_parameter":                                resourceAwsSsmParameter(),
 			"aws_ssm_resource_data_sync":                       resourceAwsSsmResourceDataSync(),
 			"aws_storagegateway_gateway":                       resourceAwsStorageGatewayGateway(),
+			"aws_storagegateway_nfs_file_share":                resourceAwsStorageGatewayNfsFileShare(),
 			"aws_spot_datafeed_subscription":                   resourceAwsSpotDataFeedSubscription(),
 			"aws_spot_instance_request":                        resourceAwsSpotInstanceRequest(),
 			"aws_spot_fleet_request":                           resourceAwsSpotFleetRequest(),
