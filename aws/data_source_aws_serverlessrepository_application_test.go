9d762ebd71baddb3d3070da818c21ef7c5220faa
Graham Davison <gdavison@paybyphone.com>
2018-08-01 14:31:05 -0700

    Handle non-existent applications in the repo

--- aws/data_source_aws_serverlessrepository_application_test.go
+++ aws/data_source_aws_serverlessrepository_application_test.go
@@ -1,9 +1,10 @@
 package aws
 
 import (
+	"fmt"
+	"regexp"
 	"testing"
 
-	"fmt"
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
 )
@@ -22,6 +23,10 @@ func TestAccDataSourceAwsServerlessRepositoryApplication_Basic(t *testing.T) {
 					resource.TestCheckResourceAttrSet("data.aws_serverlessrepository_application.secrets_manager_postgres_single_user_rotator", "source_code_url"),
 					resource.TestCheckResourceAttrSet("data.aws_serverlessrepository_application.secrets_manager_postgres_single_user_rotator", "template_url"),
 				),
+			},
+			{
+				Config:      testAccCheckAwsServerlessRepositoryApplicationDataSourceConfig_NonExistent,
+				ExpectError: regexp.MustCompile(`error reading application`),
 			},
 		},
 	})
@@ -44,5 +49,11 @@ func testAccCheckAwsServerlessRepositoryApplicationDataSourceID(n string) resour
 const testAccCheckAwsServerlessRepositoryApplicationDataSourceConfig = `
 data "aws_serverlessrepository_application" "secrets_manager_postgres_single_user_rotator" {
 	application_id = "arn:aws:serverlessrepo:us-east-1:297356227824:applications/SecretsManagerRDSPostgreSQLRotationSingleUser"
+}
+`
+
+const testAccCheckAwsServerlessRepositoryApplicationDataSourceConfig_NonExistent = `
+data "aws_serverlessrepository_application" "no_such_function" {
+	application_id = "arn:aws:serverlessrepo:us-east-1:297356227824:applications/ThisFunctionDoesNotExist"
 }
 `
