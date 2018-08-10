ecdebee87ef092fce4a2a8fd78b337e8caad6c77
saravanan30erd <saravanan30erd@gmail.com>
2018-08-10 21:17:22 +0400

    add datasource in provider conf

--- aws/provider.go
+++ aws/provider.go
@@ -181,6 +181,7 @@ func Provider() terraform.ResourceProvider {
 			"aws_cognito_user_pools":               dataSourceAwsCognitoUserPools(),
 			"aws_codecommit_repository":            dataSourceAwsCodeCommitRepository(),
 			"aws_db_cluster_snapshot":              dataSourceAwsDbClusterSnapshot(),
+			"aws_db_event_categories":              dataSourceAwsDbEventCategories(),
 			"aws_db_instance":                      dataSourceAwsDbInstance(),
 			"aws_db_snapshot":                      dataSourceAwsDbSnapshot(),
 			"aws_dx_gateway":                       dataSourceAwsDxGateway(),
