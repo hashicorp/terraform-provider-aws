973370e60b26206076f0b5856944bc44fb1b6504
Brian Flad <bflad417@gmail.com>
2018-08-16 00:27:56 -0400

    resource/aws_api_gateway_method_response: Support resource import
    * Support, test, and document resource import
    
    make testacc TEST=./aws TESTARGS='-run=TestAccAWSAPIGatewayMethodResponse_'
    ==> Checking that code complies with gofmt requirements...
    TF_ACC=1 go test ./aws -v -run=TestAccAWSAPIGatewayMethodResponse_ -timeout 120m
    === RUN   TestAccAWSAPIGatewayMethodResponse_basic
    --- PASS: TestAccAWSAPIGatewayMethodResponse_basic (28.66s)
    PASS
    ok  	github.com/terraform-providers/terraform-provider-aws/aws	29.358s

--- aws/resource_aws_api_gateway_method_response_test.go
+++ aws/resource_aws_api_gateway_method_response_test.go
@@ -42,6 +42,12 @@ func TestAccAWSAPIGatewayMethodResponse_basic(t *testing.T) {
 						"aws_api_gateway_method_response.error", "response_models.application/json", "Empty"),
 				),
 			},
+			{
+				ResourceName:      "aws_api_gateway_method_response.error",
+				ImportState:       true,
+				ImportStateIdFunc: testAccAWSAPIGatewayMethodResponseImportStateIdFunc("aws_api_gateway_method_response.error"),
+				ImportStateVerify: true,
+			},
 		},
 	})
 }
@@ -150,6 +156,17 @@ func testAccCheckAWSAPIGatewayMethodResponseDestroy(s *terraform.State) error {
 	}
 
 	return nil
+}
+
+func testAccAWSAPIGatewayMethodResponseImportStateIdFunc(resourceName string) resource.ImportStateIdFunc {
+	return func(s *terraform.State) (string, error) {
+		rs, ok := s.RootModule().Resources[resourceName]
+		if !ok {
+			return "", fmt.Errorf("Not found: %s", resourceName)
+		}
+
+		return fmt.Sprintf("%s/%s/%s/%s", rs.Primary.Attributes["rest_api_id"], rs.Primary.Attributes["resource_id"], rs.Primary.Attributes["http_method"], rs.Primary.Attributes["status_code"]), nil
+	}
 }
 
 const testAccAWSAPIGatewayMethodResponseConfig = `
