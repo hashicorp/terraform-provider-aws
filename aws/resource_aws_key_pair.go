1d223b8437fd85e7ef89002563bc41d1149ac89d
mahsoud <mahsoud@users.noreply.github.com>
2018-08-30 14:56:26 +0300

    5731 save key pair fingerprint attribute from response
    5731 save key pair fingerprint attribute from the response to create keypair request.

--- aws/resource_aws_key_pair.go
+++ aws/resource_aws_key_pair.go
@@ -85,7 +85,8 @@ func resourceAwsKeyPairCreate(d *schema.ResourceData, meta interface{}) error {
 	if err != nil {
 		return fmt.Errorf("Error import KeyPair: %s", err)
 	}
-
+	
+        d.Set("fingerprint", *resp.KeyFingerprint)
 	d.SetId(*resp.KeyName)
 	return nil
 }
