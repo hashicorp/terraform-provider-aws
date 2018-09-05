hashicorp/terraform-provider-aws

commit ddaaae1d95fbb931b6c5edb4c2e727939fdb1089

fix the panic error

aws/resource_aws_lambda_function.go
@@ -308,26 +308,28 @@ func resourceAwsLambdaFunctionCreate(d *schema.ResourceData, meta interface{}) e
 	if v, ok := d.GetOk("vpc_config"); ok {
 
 		configs := v.([]interface{})
-		config, ok := configs[0].(map[string]interface{})
+		if len(configs) == 1 {
+			config, ok := configs[0].(map[string]interface{})
 
-		if !ok {
-			return errors.New("vpc_config is <nil>")
-		}
-
-		if config != nil {
-			var subnetIds []*string
-			for _, id := range config["subnet_ids"].(*schema.Set).List() {
-				subnetIds = append(subnetIds, aws.String(id.(string)))
+			if !ok {
+				return errors.New("vpc_config is <nil>")
 			}
 
-			var securityGroupIds []*string
-			for _, id := range config["security_group_ids"].(*schema.Set).List() {
-				securityGroupIds = append(securityGroupIds, aws.String(id.(string)))
-			}
+			if config != nil {
+				var subnetIds []*string
+				for _, id := range config["subnet_ids"].(*schema.Set).List() {
+					subnetIds = append(subnetIds, aws.String(id.(string)))
+				}
 
-			params.VpcConfig = &lambda.VpcConfig{
-				SubnetIds:        subnetIds,
-				SecurityGroupIds: securityGroupIds,
+				var securityGroupIds []*string
+				for _, id := range config["security_group_ids"].(*schema.Set).List() {
+					securityGroupIds = append(securityGroupIds, aws.String(id.(string)))
+				}
+
+				params.VpcConfig = &lambda.VpcConfig{
+					SubnetIds:        subnetIds,
+					SecurityGroupIds: securityGroupIds,
+				}
 			}
 		}
 	}
@@ -681,27 +683,29 @@ func resourceAwsLambdaFunctionUpdate(d *schema.ResourceData, meta interface{}) e
 	}
 	if d.HasChange("vpc_config") {
 		vpcConfigRaw := d.Get("vpc_config").([]interface{})
-		vpcConfig, ok := vpcConfigRaw[0].(map[string]interface{})
-		if !ok {
-			return errors.New("vpc_config is <nil>")
-		}
-
-		if vpcConfig != nil {
-			var subnetIds []*string
-			for _, id := range vpcConfig["subnet_ids"].(*schema.Set).List() {
-				subnetIds = append(subnetIds, aws.String(id.(string)))
+		if len(vpcConfigRaw) == 1 { // Schema guarantees either 0 or 1
+			vpcConfig, ok := vpcConfigRaw[0].(map[string]interface{})
+			if !ok {
+				return errors.New("vpc_config is <nil>")
 			}
 
-			var securityGroupIds []*string
-			for _, id := range vpcConfig["security_group_ids"].(*schema.Set).List() {
-				securityGroupIds = append(securityGroupIds, aws.String(id.(string)))
-			}
+			if vpcConfig != nil {
+				var subnetIds []*string
+				for _, id := range vpcConfig["subnet_ids"].(*schema.Set).List() {
+					subnetIds = append(subnetIds, aws.String(id.(string)))
+				}
 
-			configReq.VpcConfig = &lambda.VpcConfig{
-				SubnetIds:        subnetIds,
-				SecurityGroupIds: securityGroupIds,
+				var securityGroupIds []*string
+				for _, id := range vpcConfig["security_group_ids"].(*schema.Set).List() {
+					securityGroupIds = append(securityGroupIds, aws.String(id.(string)))
+				}
+
+				configReq.VpcConfig = &lambda.VpcConfig{
+					SubnetIds:        subnetIds,
+					SecurityGroupIds: securityGroupIds,
+				}
+				configUpdate = true
 			}
-			configUpdate = true
 		}
 	}
 

commit 2c9433d1eb168a3b3129900e29d358fed3bee80a

resource/aws_lambda_function: Properly handle `vpc_config` removal
Includes preventing panics as started by https://github.com/terraform-providers/terraform-provider-aws/pull/5792

* Adds acceptance test (`TestAccAWSLambdaFunction_VPCRemoval`) to cover `vpc_config` removal
* Prevents panic when removing `vpc_config`
* Properly handles vpc_config removal to prevent perpetual difference

Previously:

```
--- FAIL: TestAccAWSLambdaFunction_VPCRemoval (42.52s)
    testing.go:527: Step 1 error: Check failed: Check 2/2 error: aws_lambda_function.lambda_function_test: Attribute 'vpc_config.#' expected "0", got "1"
```

Now:

```
--- PASS: TestAccAWSLambdaFunction_VPCRemoval (51.23s)
```

aws/resource_aws_lambda_function.go
@@ -305,32 +305,12 @@ func resourceAwsLambdaFunctionCreate(d *schema.ResourceData, meta interface{}) e
 		}
 	}
 
-	if v, ok := d.GetOk("vpc_config"); ok {
+	if v, ok := d.GetOk("vpc_config"); ok && len(v.([]interface{})) > 0 {
+		config := v.([]interface{})[0].(map[string]interface{})
 
-		configs := v.([]interface{})
-		if len(configs) == 1 {
-			config, ok := configs[0].(map[string]interface{})
-
-			if !ok {
-				return errors.New("vpc_config is <nil>")
-			}
-
-			if config != nil {
-				var subnetIds []*string
-				for _, id := range config["subnet_ids"].(*schema.Set).List() {
-					subnetIds = append(subnetIds, aws.String(id.(string)))
-				}
-
-				var securityGroupIds []*string
-				for _, id := range config["security_group_ids"].(*schema.Set).List() {
-					securityGroupIds = append(securityGroupIds, aws.String(id.(string)))
-				}
-
-				params.VpcConfig = &lambda.VpcConfig{
-					SubnetIds:        subnetIds,
-					SecurityGroupIds: securityGroupIds,
-				}
-			}
+		params.VpcConfig = &lambda.VpcConfig{
+			SecurityGroupIds: expandStringSet(config["security_group_ids"].(*schema.Set)),
+			SubnetIds:        expandStringSet(config["subnet_ids"].(*schema.Set)),
 		}
 	}
 
@@ -682,31 +662,16 @@ func resourceAwsLambdaFunctionUpdate(d *schema.ResourceData, meta interface{}) e
 		}
 	}
 	if d.HasChange("vpc_config") {
-		vpcConfigRaw := d.Get("vpc_config").([]interface{})
-		if len(vpcConfigRaw) == 1 { // Schema guarantees either 0 or 1
-			vpcConfig, ok := vpcConfigRaw[0].(map[string]interface{})
-			if !ok {
-				return errors.New("vpc_config is <nil>")
-			}
-
-			if vpcConfig != nil {
-				var subnetIds []*string
-				for _, id := range vpcConfig["subnet_ids"].(*schema.Set).List() {
-					subnetIds = append(subnetIds, aws.String(id.(string)))
-				}
-
-				var securityGroupIds []*string
-				for _, id := range vpcConfig["security_group_ids"].(*schema.Set).List() {
-					securityGroupIds = append(securityGroupIds, aws.String(id.(string)))
-				}
-
-				configReq.VpcConfig = &lambda.VpcConfig{
-					SubnetIds:        subnetIds,
-					SecurityGroupIds: securityGroupIds,
-				}
-				configUpdate = true
-			}
+		configReq.VpcConfig = &lambda.VpcConfig{
+			SecurityGroupIds: []*string{},
+			SubnetIds:        []*string{},
 		}
+		if v, ok := d.GetOk("vpc_config"); ok && len(v.([]interface{})) > 0 {
+			vpcConfig := v.([]interface{})[0].(map[string]interface{})
+			configReq.VpcConfig.SecurityGroupIds = expandStringSet(vpcConfig["security_group_ids"].(*schema.Set))
+			configReq.VpcConfig.SubnetIds = expandStringSet(vpcConfig["subnet_ids"].(*schema.Set))
+		}
+		configUpdate = true
 	}
 
 	if d.HasChange("runtime") {

aws/resource_aws_lambda_function_test.go
@@ -534,6 +534,35 @@ func TestAccAWSLambdaFunction_VPC(t *testing.T) {
 	})
 }
 
+func TestAccAWSLambdaFunction_VPCRemoval(t *testing.T) {
+	var conf lambda.GetFunctionOutput
+
+	rName := acctest.RandomWithPrefix("tf-acc-test")
+	resourceName := "aws_lambda_function.lambda_function_test"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckLambdaFunctionDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAWSLambdaConfigWithVPC(rName, rName, rName, rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAwsLambdaFunctionExists(resourceName, rName, &conf),
+					resource.TestCheckResourceAttr(resourceName, "vpc_config.#", "1"),
+				),
+			},
+			{
+				Config: testAccAWSLambdaConfigBasic(rName, rName, rName, rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAwsLambdaFunctionExists(resourceName, rName, &conf),
+					resource.TestCheckResourceAttr(resourceName, "vpc_config.#", "0"),
+				),
+			},
+		},
+	})
+}
+
 func TestAccAWSLambdaFunction_VPCUpdate(t *testing.T) {
 	var conf lambda.GetFunctionOutput
 