4dcbe897f08141d732a03127faf085f3a25896c3
Mark Butcher <macbutch@users.noreply.github.com>
2018-09-12 23:57:51 +1000

    Issue #4633 retry lambda creation
    Treat the error 'Lambda was unable to configure access to your
    environment variables because the KMS key is invalid for CreateGrant' as
    retryable. The root cause seems to be (?) that the required role hasn't
    fully been created when the lamdba creation is attmpted (but hard to
    know). The failure is intermittent and does not occur on retry.

--- aws/resource_aws_lambda_function.go
+++ aws/resource_aws_lambda_function.go
@@ -382,6 +382,10 @@ func resourceAwsLambdaFunctionCreate(d *schema.ResourceData, meta interface{}) e
 				log.Printf("[DEBUG] Received %s, retrying CreateFunction", err)
 				return resource.RetryableError(err)
 			}
+			if isAWSErr(err, "InvalidParameterValueException", "Lambda was unable to configure access to your environment variables because the KMS key is invalid for CreateGrant") {
+				log.Printf("[DEBUG] Received %s, retrying CreateFunction", err)
+				return resource.RetryableError(err)
+			}
 
 			return resource.NonRetryableError(err)
 		}
