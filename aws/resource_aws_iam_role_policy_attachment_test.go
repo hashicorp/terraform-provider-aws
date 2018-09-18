471c29adfc0857ba2e46f801207deab48eac945b
Brian Flad <bflad417@gmail.com>
2018-09-18 16:09:15 -0400

    tests/resource/aws_iam_role_policy_attachment: Parameterize testAccAWSIAMRolePolicyAttachmentImportStateIdFunc() and add simple ImportStateCheck

--- aws/resource_aws_iam_role_policy_attachment_test.go
+++ aws/resource_aws_iam_role_policy_attachment_test.go
@@ -34,7 +34,23 @@ func TestAccAWSRolePolicyAttachment_basic(t *testing.T) {
 			{
 				ResourceName:      "aws_iam_role_policy_attachment.test-attach",
 				ImportState:       true,
-				ImportStateIdFunc: testAccAWSIAMRolePolicyAttachmentImportStateIdFunc,
+				ImportStateIdFunc: testAccAWSIAMRolePolicyAttachmentImportStateIdFunc("aws_iam_role_policy_attachment.test-attach"),
+				// We do not have a way to align IDs since the Create function uses resource.PrefixedUniqueId()
+				// Failed state verification, resource with ID ROLE-POLICYARN not found
+				// ImportStateVerify: true,
+				ImportStateCheck: func(s []*terraform.InstanceState) error {
+					if len(s) != 1 {
+						return fmt.Errorf("expected 1 state: %#v", s)
+					}
+
+					rs := s[0]
+
+					if !strings.HasPrefix(rs.Attributes["policy_arn"], "arn:") {
+						return fmt.Errorf("expected policy_arn attribute to be set and begin with arn:, received: %s", rs.Attributes["policy_arn"])
+					}
+
+					return nil
+				},
 			},
 			{
 				Config: testAccAWSRolePolicyAttachConfigUpdate(rInt),
@@ -98,19 +114,15 @@ func testAccCheckAWSRolePolicyAttachmentAttributes(policies []string, out *iam.L
 	}
 }
 
-func testAccAWSIAMRolePolicyAttachmentImportStateIdFunc(s *terraform.State) (string, error) {
-	resources := s.RootModule().Resources
+func testAccAWSIAMRolePolicyAttachmentImportStateIdFunc(resourceName string) resource.ImportStateIdFunc {
+	return func(s *terraform.State) (string, error) {
+		rs, ok := s.RootModule().Resources[resourceName]
+		if !ok {
+			return "", fmt.Errorf("Not found: %s", resourceName)
+		}
 
-	roleResource, ok := resources["aws_iam_role.role"]
-	if !ok {
-		return "", fmt.Errorf("role not found: aws_iam_role.role")
+		return fmt.Sprintf("%s/%s", rs.Primary.Attributes["role"], rs.Primary.Attributes["policy_arn"]), nil
 	}
-	policyResource, ok := resources["aws_iam_policy.policy"]
-	if !ok {
-		return "", fmt.Errorf("policy not found: aws_iam_policy.policy")
-	}
-
-	return fmt.Sprintf("%s/%s", roleResource.Primary.Attributes["name"], policyResource.Primary.Attributes["arn"]), nil
 }
 
 func testAccAWSRolePolicyAttachConfig(rInt int) string {
