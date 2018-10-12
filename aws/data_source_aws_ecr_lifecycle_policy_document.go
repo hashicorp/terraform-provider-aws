819b1d6f7261ef4468b4c45755d1301b92f292ff
Aaron J. Smith <ajsmith10381@gmail.com>
2018-10-12 00:02:39 -0500

    removing unneeded out

--- aws/data_source_aws_ecr_lifecycle_policy_document.go
+++ aws/data_source_aws_ecr_lifecycle_policy_document.go
@@ -167,9 +167,8 @@ func dataSourceAwsEcrLifecyclePolicyDocumentRead(d *schema.ResourceData, meta in
 }
 
 func dataSourceAwsEcrLifecyclePolicyDocumentMakeSelection(in []interface{}) EcrLifecyclePolicyStatementSelectionSet {
-	out := EcrLifecyclePolicySelection{}
 	item := in[0].(map[string]interface{})
-	out = EcrLifecyclePolicySelection{
+	out := EcrLifecyclePolicySelection{
 		TagStatus:     item["tag_status"].(string),
 		TagPrefixList: item["tag_prefix_list"].([]interface{}),
 		CountType:     item["count_type"].(string),
