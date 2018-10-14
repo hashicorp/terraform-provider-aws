942b2dd390fe348705213cafc796c787b6a0e67a
Aaron J. Smith <ajsmith10381@gmail.com>
2018-10-14 08:37:32 -0500

    data_source/aws_route53_delegation_set: adding new data source for route53

---
+++ aws/data_source_aws_route53_delegation_set.go
@@ -0,0 +1,63 @@
+package aws
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/route53"
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func dataSourceAwsDelegationSet() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceAwsDelegationSetRead,
+
+		Schema: map[string]*schema.Schema{
+			"id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"caller_reference": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"name_servers": {
+				Type:     schema.TypeList,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceAwsDelegationSetRead(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*AWSClient).r53conn
+
+	dSetID := d.Get("id").(string)
+
+	input := &route53.GetReusableDelegationSetInput{
+		Id: aws.String(dSetID),
+	}
+
+	log.Printf("[DEBUG] Reading Route53 delegation set: %s", input)
+
+	req, resp := conn.GetReusableDelegationSetRequest(input)
+	err := req.Send()
+	if err != nil {
+		return fmt.Errorf("Failed getting Route53 delegation set: %s Set: %q", err, dSetID)
+	}
+
+	d.SetId(dSetID)
+	d.Set("caller_reference", resp.DelegationSet.CallerReference)
+
+	servers := []string{}
+	for _, server := range resp.DelegationSet.NameServers {
+		if server != nil {
+			servers = append(servers, "nope")
+		}
+	}
+	d.Set("name_servers", servers)
+
+	return nil
+}
---
+++ aws/data_source_aws_route53_delegation_set_test.go
@@ -0,0 +1,42 @@
+package aws
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccDataSourceRoute53DelegationSet_basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAWSDataSourceAWSRoute53DelegationSetConfig_basic,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestMatchResourceAttr(
+						"data.aws_route53_delegation_set.dset",
+						"caller_reference",
+						regexp.MustCompile("DynDNS(.*)"),
+					),
+				),
+			},
+		},
+	})
+}
+
+const testAccAWSDataSourceAWSRoute53DelegationSetConfig_basic = `
+resource "aws_route53_delegation_set" "dset" {
+  reference_name = "DynDNS"
+}
+
+resource "aws_route53_zone" "primary" {
+  name              = "example.xyz"
+  delegation_set_id = "${aws_route53_delegation_set.dset.id}"
+}
+
+data "aws_route53_delegation_set" "dset" {
+  id = "${aws_route53_delegation_set.dset.id}"
+}
+`
