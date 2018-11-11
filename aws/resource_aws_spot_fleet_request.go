42d717f4f004fa0060e5cd556880fcf8f00d606b
Stephen Hallett <stephen.hallett@catchgroup.com.au>
2018-11-11 14:20:16 +1100

    remove commented validatefuncs

--- aws/resource_aws_spot_fleet_request.go
+++ aws/resource_aws_spot_fleet_request.go
@@ -362,34 +362,29 @@ func resourceAwsSpotFleetRequest() *schema.Resource {
 										Type:     schema.TypeString,
 										Optional: true,
 										ForceNew: true,
-										//ValidateFunc:  validateLaunchTemplateId,
 									},
 									"instance_type": {
 										Type:     schema.TypeString,
 										Optional: true,
 										ForceNew: true,
-										//ValidateFunc:  validateLaunchTemplateName,
 									},
 									"spot_price": {
 										Type:     schema.TypeString,
 										Optional: true,
 										Computed: true,
 										ForceNew: true,
-										//ValidateFunc: validation.StringLenBetween(1, 255),
 									},
 									"subnet_id": {
 										Type:     schema.TypeString,
 										Optional: true,
 										Computed: true,
 										ForceNew: true,
-										//ValidateFunc: validation.StringLenBetween(1, 255),
 									},
 									"weighted_capacity": {
 										Type:     schema.TypeFloat,
 										Optional: true,
 										Computed: true,
 										ForceNew: true,
-										//ValidateFunc: validation.StringLenBetween(1, 255),
 									},
 								},
 							},
@@ -398,7 +393,8 @@ func resourceAwsSpotFleetRequest() *schema.Resource {
 					},
 				},
 			},
-			// Everything on a spot fleet is ForceNew except target_capacity
+			// Everything on a spot fleet is ForceNew except target_capacity and excess_capacity_termination_policy,
+			// see https://docs.aws.amazon.com/sdk-for-go/api/service/ec2/#ModifySpotFleetRequestInput
 			"target_capacity": {
 				Type:     schema.TypeInt,
 				Required: true,
@@ -408,7 +404,7 @@ func resourceAwsSpotFleetRequest() *schema.Resource {
 				Type:          schema.TypeInt,
 				ConflictsWith: []string{"launch_specification"},
 				Optional:      true,
-				ForceNew:      false,
+				ForceNew:      true,
 			},
 			"allocation_strategy": {
 				Type:     schema.TypeString,
