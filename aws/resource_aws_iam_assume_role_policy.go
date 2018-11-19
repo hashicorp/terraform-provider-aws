12464f316d171ea83a993458466b10ab5c3e93c5
Charles Treatman <charles.treatman@gmail.com>
2018-11-19 14:59:09 -0500

    Closes #6399: Adds an aws_iam_assume_role_policy resource
    This adds a resource that can be used to update the assume role
    policy for an IAM role created outside of terraform, without the
    need to import that role into terraform state manually.
    
    This is useful in cases where a team using terraform is allowed
    to manage permissions for IAM roles, but does not have access to
    create or delete IAM roles.

---
+++ aws/resource_aws_iam_assume_role_policy.go
@@ -0,0 +1,114 @@
+package aws
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/iam"
+
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourceAwsIamAssumeRolePolicy() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceAwsIamAssumeRolePolicyCreate,
+		Read:   resourceAwsIamAssumeRolePolicyRead,
+		Update: resourceAwsIamAssumeRolePolicyUpdate,
+		Delete: resourceAwsIamAssumeRolePolicyDelete,
+
+		Schema: map[string]*schema.Schema{
+			"role_name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+
+			"assume_role_policy": {
+				Type:             schema.TypeString,
+				Required:         true,
+				DiffSuppressFunc: suppressEquivalentAwsPolicyDiffs,
+				ValidateFunc:     validateJsonString,
+			},
+		},
+	}
+}
+
+func resourceAwsIamAssumeRolePolicyCreate(d *schema.ResourceData, meta interface{}) error {
+	iamconn := meta.(*AWSClient).iamconn
+
+	assumeRolePolicyInput := &iam.UpdateAssumeRolePolicyInput{
+		RoleName:       aws.String(d.Get("role_name").(string)),
+		PolicyDocument: aws.String(d.Get("assume_role_policy").(string)),
+	}
+	_, err := iamconn.UpdateAssumeRolePolicy(assumeRolePolicyInput)
+	if err != nil {
+		if iamerr, ok := err.(awserr.Error); ok && iamerr.Code() == "NoSuchEntity" {
+			d.SetId("")
+			return nil
+		}
+		return fmt.Errorf("Error Updating IAM Role (%s) Assume Role Policy: %s", d.Id(), err)
+	}
+
+	d.SetId(d.Get("role_name").(string))
+
+	return resourceAwsIamAssumeRolePolicyRead(d, meta)
+}
+
+func resourceAwsIamAssumeRolePolicyRead(d *schema.ResourceData, meta interface{}) error {
+	iamconn := meta.(*AWSClient).iamconn
+
+	request := &iam.GetRoleInput{
+		RoleName: aws.String(d.Id()),
+	}
+
+	getResp, err := iamconn.GetRole(request)
+	if err != nil {
+		if iamerr, ok := err.(awserr.Error); ok && iamerr.Code() == "NoSuchEntity" { // XXX test me
+			d.SetId("")
+			return nil
+		}
+		return fmt.Errorf("Error reading IAM Role %s: %s", d.Id(), err)
+	}
+
+	role := getResp.Role
+
+	if err := d.Set("role_name", role.RoleName); err != nil {
+		return err
+	}
+
+	assumeRolePolicy, err := url.QueryUnescape(*role.AssumeRolePolicyDocument)
+	if err != nil {
+		return err
+	}
+	if err := d.Set("assume_role_policy", assumeRolePolicy); err != nil {
+		return err
+	}
+	return nil
+}
+
+func resourceAwsIamAssumeRolePolicyUpdate(d *schema.ResourceData, meta interface{}) error {
+	iamconn := meta.(*AWSClient).iamconn
+
+	if d.HasChange("assume_role_policy") {
+		assumeRolePolicyInput := &iam.UpdateAssumeRolePolicyInput{
+			RoleName:       aws.String(d.Get("role_name").(string)),
+			PolicyDocument: aws.String(d.Get("assume_role_policy").(string)),
+		}
+		_, err := iamconn.UpdateAssumeRolePolicy(assumeRolePolicyInput)
+		if err != nil {
+			if iamerr, ok := err.(awserr.Error); ok && iamerr.Code() == "NoSuchEntity" {
+				d.SetId("")
+				return nil
+			}
+			return fmt.Errorf("Error Updating IAM Role (%s) Assume Role Policy: %s", d.Id(), err)
+		}
+	}
+
+	return nil
+}
+
+func resourceAwsIamAssumeRolePolicyDelete(d *schema.ResourceData, meta interface{}) error {
+	// No need to do anything, assume role policy will be deleted when the role is deleted
+	return nil
+}
