5a59d914feb0058ac820f0cd4880e6c7238ce9ad
Matthew Bamber <matthewbamber@icloud.com>
2018-11-21 15:08:56 +0000

    Fix typo

--- aws/resource_aws_elastic_transcoder_preset.go
+++ aws/resource_aws_elastic_transcoder_preset.go
@@ -327,7 +327,7 @@ func resourceAwsElasticTranscoderPresetCreate(d *schema.ResourceData, meta inter
 		Container:   aws.String(d.Get("container").(string)),
 		Description: getStringPtr(d, "description"),
 		Thumbnails:  expandETThumbnails(d),
-		Video:       exapandETVideoParams(d),
+		Video:       expandETVideoParams(d),
 	}
 
 	if name, ok := d.GetOk("name"); ok {
@@ -418,7 +418,7 @@ func expandETAudioCodecOptions(d *schema.ResourceData) *elastictranscoder.AudioC
 	return codecOpts
 }
 
-func exapandETVideoParams(d *schema.ResourceData) *elastictranscoder.VideoParameters {
+func expandETVideoParams(d *schema.ResourceData) *elastictranscoder.VideoParameters {
 	s := d.Get("video").(*schema.Set)
 	if s == nil || s.Len() == 0 {
 		return nil
