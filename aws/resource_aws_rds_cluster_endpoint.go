5d454b2ad55140a1ebfdefa3476759fb34b97c73
KOJIMA Kazunori <kjm.kznr@gmail.com>
2018-12-09 09:17:00 +0000

    Add available and destroy waiter func

--- aws/resource_aws_rds_cluster_endpoint.go
+++ aws/resource_aws_rds_cluster_endpoint.go
@@ -3,11 +3,18 @@ package aws
 import (
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/rds"
+	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/helper/validation"
+)
+
+const (
+	AWSRDSClusterEndpointRetryDelay      = 5 * time.Second
+	AWSRDSClusterEndpointRetryMinTimeout = 3 * time.Second
 )
 
 func resourceAwsRDSClusterEndpoint() *schema.Resource {
@@ -93,6 +100,12 @@ func resourceAwsRDSClusterEndpointCreate(d *schema.ResourceData, meta interface{
 	}
 
 	d.SetId(endpointId)
+
+	_, err = resourceAwsRDSClusterEndpointWaitForAvailable(d.Timeout(schema.TimeoutDelete), d.Id(), conn)
+	if err != nil {
+		return err
+	}
+
 	return resourceAwsRDSClusterEndpointRead(d, meta)
 }
 
@@ -183,5 +196,72 @@ func resourceAwsRDSClusterEndpointDelete(d *schema.ResourceData, meta interface{
 	if err != nil {
 		return fmt.Errorf("Error deleting RDS Cluster Endpoint: %s", err)
 	}
+
+	if err := resourceAwsRDSClusterEndpointWaitForDestroy(d.Timeout(schema.TimeoutDelete), d.Id(), conn); err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func resourceAwsRDSClusterEndpointWaitForDestroy(timeout time.Duration, id string, conn *rds.RDS) error {
+	log.Printf("Waiting for RDS Cluster Endpoint %s to be deleted...", id)
+	stateConf := &resource.StateChangeConf{
+		Pending:    []string{"available", "deleting"},
+		Target:     []string{"destroyed"},
+		Refresh:    DBClusterEndpointStateRefreshFunc(conn, id),
+		Timeout:    timeout,
+		Delay:      AWSRDSClusterEndpointRetryDelay,
+		MinTimeout: AWSRDSClusterEndpointRetryMinTimeout,
+	}
+	_, err := stateConf.WaitForState()
+	if err != nil {
+		return fmt.Errorf("Error waiting for RDS Cluster Endpoint (%s) to be deleted: %v", id, err)
+	}
+	return nil
+}
+
+func resourceAwsRDSClusterEndpointWaitForAvailable(timeout time.Duration, id string, conn *rds.RDS) (*rds.DBClusterEndpoint, error) {
+	log.Printf("Waiting for RDS Cluster Endpoint %s to become available...", id)
+
+	stateConf := &resource.StateChangeConf{
+		Pending:    []string{"creating"},
+		Target:     []string{"available"},
+		Refresh:    DBClusterEndpointStateRefreshFunc(conn, id),
+		Timeout:    timeout,
+		Delay:      AWSRDSClusterEndpointRetryDelay,
+		MinTimeout: AWSRDSClusterEndpointRetryMinTimeout,
+	}
+
+	info, err := stateConf.WaitForState()
+	if err != nil {
+		return nil, fmt.Errorf("Error waiting for RDS Cluster Endpoint (%s) to be ready: %v", id, err)
+	}
+	return info.(*rds.DBClusterEndpoint), nil
+}
+
+func DBClusterEndpointStateRefreshFunc(conn *rds.RDS, id string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		emptyResp := &rds.DescribeDBClusterEndpointsOutput{}
+
+		resp, err := conn.DescribeDBClusterEndpoints(
+			&rds.DescribeDBClusterEndpointsInput{
+				DBClusterEndpointIdentifier: aws.String(id),
+			})
+		if err != nil {
+			if isAWSErr(err, rds.ErrCodeDBClusterNotFoundFault, "") {
+				return emptyResp, "destroyed", nil
+			} else if resp != nil && len(resp.DBClusterEndpoints) == 0 {
+				return emptyResp, "destroyed", nil
+			} else {
+				return emptyResp, "", fmt.Errorf("Error on refresh: %+v", err)
+			}
+		}
+
+		if resp == nil || resp.DBClusterEndpoints == nil || len(resp.DBClusterEndpoints) == 0 {
+			return emptyResp, "destroyed", nil
+		}
+
+		return resp.DBClusterEndpoints[0], *resp.DBClusterEndpoints[0].Status, nil
+	}
 }
