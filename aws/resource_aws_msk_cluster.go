674ca26360d508750427061e05d5fff8dcb5185b
Justin Refi <justinrefi@granular.ag>
2018-12-11 17:12:40 -0600

    Initial MSK implementation with tests

--- aws/resource_aws_msk_cluster.go
+++ aws/resource_aws_msk_cluster.go
@@ -1,9 +1,14 @@
 package aws
 
 import (
+	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/kafka"
+	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/helper/validation"
+	"time"
 )
 
 func resourceAwsMskCluster() *schema.Resource {
@@ -16,13 +21,17 @@ func resourceAwsMskCluster() *schema.Resource {
 			State: schema.ImportStatePassthrough,
 		},
 		Schema: map[string]*schema.Schema{
-			"cluster_name": {
+			"name": {
 				Type:     schema.TypeString,
 				Required: true,
 				ForceNew: true,
 			},
 			"broker_node_group_info": {
 				Type:     schema.TypeString,
+				Required: true,
+			},
+			"encryption_enabled": {
+				Type:     schema.TypeBool,
 				Required: true,
 			},
 			"encryption_info": {
@@ -32,6 +41,12 @@ func resourceAwsMskCluster() *schema.Resource {
 			"enhanced_monitoring": {
 				Type:     schema.TypeString,
 				Required: true,
+				Default:  kafka.EnhancedMonitoringDefault,
+				ValidateFunc: validation.StringInSlice([]string{
+					kafka.EnhancedMonitoringDefault,
+					kafka.EnhancedMonitoringPerBroker,
+					kafka.EnhancedMonitoringPerTopicPerBroker,
+				}, true),
 			},
 			"kafka_version": {
 				Type:     schema.TypeString,
@@ -42,15 +57,143 @@ func resourceAwsMskCluster() *schema.Resource {
 				Type:     schema.TypeInt,
 				Required: true,
 			},
+			"arn": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
 		},
 	}
 }
 func resourceAwsMskClusterCreate(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*AWSClient).kafkaconn
+	cn := d.Get("name").(string)
+
+	createOpts := &kafka.CreateClusterInput{
+		ClusterName:         aws.String(cn),
+		EnhancedMonitoring:  aws.String(d.Get("enhanced_monitoring").(string)),
+		NumberOfBrokerNodes: aws.Int64(int64(d.Get("broker_count").(int))),
+	}
+
+	cluster, err := conn.CreateCluster(createOpts)
+	if err != nil {
+		return fmt.Errorf("Unable to create cluster: %s", err)
+	}
+
+	arn := aws.StringValue(cluster.ClusterArn)
+
+	// No error, wait for ACTIVE state
+	stateConf := &resource.StateChangeConf{
+		Pending:    []string{"CREATING"},
+		Target:     []string{"ACTIVE"},
+		Refresh:    clusterStateRefreshFunc(conn, arn),
+		Timeout:    d.Timeout(schema.TimeoutCreate),
+		Delay:      10 * time.Second,
+		MinTimeout: 3 * time.Second,
+	}
+
+	clusterRaw, err := stateConf.WaitForState()
+
+	if err != nil {
+		return fmt.Errorf(
+			"Error waiting for MSK cluster (%s) to become active: %s",
+			cn, err)
+	}
+
+	c := clusterRaw.(*mskClusterState)
+	d.SetId(c.arn)
+	d.Set("arn", c.arn)
+
+	return resourceAwsMskClusterUpdate(d, meta)
 }
 func resourceAwsMskClusterRead(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*AWSClient).kafkaconn
+	arn := d.Get("arn").(string)
+
+	state, err := readMskClusterState(conn, arn)
+	if err != nil {
+		if awsErr, ok := err.(awserr.Error); ok {
+			if awsErr.Code() == "ResourceNotFoundException" {
+				d.SetId("")
+				return nil
+			}
+			return fmt.Errorf("Error reading MSK cluster: \"%s\", code: \"%s\"", awsErr.Message(), awsErr.Code())
+		}
+		return err
+
+	}
+	d.SetId(state.arn)
+	d.Set("arn", state.arn)
+
+	return nil
 }
+
 func resourceAwsMskClusterUpdate(d *schema.ResourceData, meta interface{}) error {
+	return nil
 }
+
 func resourceAwsMskClusterDelete(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*AWSClient).kafkaconn
+	arn := d.Get("arn").(string)
+
+	_, err := conn.DeleteCluster(&kafka.DeleteClusterInput{
+		ClusterArn: aws.String(arn),
+	})
+	if err != nil {
+		return err
+	}
+
+	stateConf := &resource.StateChangeConf{
+		Pending:    []string{"DELETING"},
+		Target:     []string{"DESTROYED"},
+		Refresh:    clusterStateRefreshFunc(conn, arn),
+		Timeout:    d.Timeout(schema.TimeoutDelete),
+		Delay:      10 * time.Second,
+		MinTimeout: 3 * time.Second,
+	}
+
+	_, err = stateConf.WaitForState()
+	if err != nil {
+		return fmt.Errorf(
+			"Error waiting for MSK Cluster (%s) to be destroyed: %s",
+			arn, err)
+	}
+
+	return nil
+}
+
+type mskClusterState struct {
+	arn               string
+	creationTimestamp int64
+	status            string
+}
+
+func readMskClusterState(conn *kafka.Kafka, arn string) (*mskClusterState, error) {
+	describeOpts := &kafka.DescribeClusterInput{
+		ClusterArn: aws.String(arn),
+	}
+
+	state := &mskClusterState{}
+	cluster, err := conn.DescribeCluster(describeOpts)
+
+	state.arn = aws.StringValue(cluster.ClusterInfo.ClusterArn)
+	state.status = aws.StringValue(cluster.ClusterInfo.State)
+	return state, err
+}
+
+func clusterStateRefreshFunc(conn *kafka.Kafka, arn string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		state, err := readMskClusterState(conn, arn)
+		if err != nil {
+			if awsErr, ok := err.(awserr.Error); ok {
+				if awsErr.Code() == "ResourceNotFoundException" {
+					return 42, "DESTROYED", nil
+				}
+				return nil, awsErr.Code(), err
+			}
+			return nil, "failed", err
+		}
+
+		return state, state.status, nil
+	}
 }
--- aws/resource_aws_msk_cluster_test.go
+++ aws/resource_aws_msk_cluster_test.go
@@ -6,11 +6,111 @@ import (
 	"strings"
 	"testing"
 
-	"regexp"
-
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/kafka"
 	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
 )
+
+func TestAccAWSMskCluster_basic(t *testing.T) {
+	var cluster kafka.ClusterInfo
+
+	rInt := acctest.RandInt()
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckKinesisStreamDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testMskClusterConfig(rInt),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckMskClusterExists("aws_msk_cluster.test_cluster", &cluster),
+					testAccCheckAWSMskClusterAttributes(&cluster),
+				),
+			},
+		},
+	})
+}
+
+func testMskClusterConfig(rInt int) string {
+	return fmt.Sprintf(`
+resource "aws_msk_cluster" "test_cluster" {
+	name = "terraform-msk-test-%d"
+	broker_count = 1
+	tags = {
+		Name = "tf-test"
+	}
+}`, rInt)
+}
+
+func testAccCheckMskClusterExists(n string, cluster *kafka.ClusterInfo) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No MSK Cluster ID is set")
+		}
+
+		conn := testAccProvider.Meta().(*AWSClient).kafkaconn
+		describeOpts := &kafka.DescribeClusterInput{
+			ClusterArn: aws.String(rs.Primary.Attributes["arn"]),
+		}
+		resp, err := conn.DescribeCluster(describeOpts)
+		if err != nil {
+			return err
+		}
+
+		*cluster = *resp.ClusterInfo
+
+		return nil
+	}
+}
+
+func testAccCheckAWSMskClusterAttributes(cluster *kafka.ClusterInfo) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		if !strings.HasPrefix(*cluster.ClusterName, "terraform-msk-test") {
+			return fmt.Errorf("Bad Cluster name: %s", *cluster.ClusterName)
+		}
+		for _, rs := range s.RootModule().Resources {
+			if rs.Type != "aws_msk_cluster" {
+				continue
+			}
+			if *cluster.ClusterArn != rs.Primary.Attributes["arn"] {
+				return fmt.Errorf("Bad Cluster ARN\n\t expected: %s\n\tgot: %s\n", rs.Primary.Attributes["arn"], *cluster.ClusterArn)
+			}
+			broker_count := strconv.Itoa(int(aws.Int64Value(cluster.NumberOfBrokerNodes)))
+			if broker_count != rs.Primary.Attributes["broker_count"] {
+				return fmt.Errorf("Bad Cluster Broker Count\n\t expected: %s\n\tgot: %s\n", rs.Primary.Attributes["broker_count"], broker_count)
+			}
+		}
+		return nil
+	}
+}
+
+func testAccCheckMskClusterDestroy(s *terraform.State) error {
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "aws_msk_cluster" {
+			continue
+		}
+		conn := testAccProvider.Meta().(*AWSClient).kafkaconn
+		describeOpts := &kafka.DescribeClusterInput{
+			ClusterArn: aws.String(rs.Primary.Attributes["arn"]),
+		}
+		resp, err := conn.DescribeCluster(describeOpts)
+		if err == nil {
+			if resp.ClusterInfo != nil && *resp.ClusterInfo.State != "DELETING" {
+				return fmt.Errorf("Error: Cluster still exists")
+			}
+		}
+
+		return nil
+
+	}
+
+	return nil
+}
