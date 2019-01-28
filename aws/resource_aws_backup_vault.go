d72ef92f73617cb84751eae102d10056839bb85a
Aaron J. Smith <ajsmith10381@gmail.com>
2019-01-28 00:16:53 -0600

    removing double-quotes from regex

--- aws/resource_aws_backup_vault.go
+++ aws/resource_aws_backup_vault.go
@@ -22,7 +22,7 @@ func resourceAwsBackupVault() *schema.Resource {
 				Type:         schema.TypeString,
 				Required:     true,
 				ForceNew:     true,
-				ValidateFunc: validation.StringMatch(regexp.MustCompile("[a-z0-9\\-]+"), "must contain alphanumeric characters or underscores"),
+				ValidateFunc: validation.StringMatch(regexp.MustCompile(`[a-z0-9\-]+`), "must contain alphanumeric characters or underscores"),
 			},
 			"tags": {
 				Type:     schema.TypeMap,
