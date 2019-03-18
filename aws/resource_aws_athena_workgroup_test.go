3761b0ea8abcd9f76ff09f62127cc25995528235
Marco Tesch <mtesch@tecracer.de>
2019-03-18 21:08:28 +0100

    Added TestCase TestAccAWSAthenaWorkGroup_basic
    Signed-off-by: Marco Tesch <mtesch@tecracer.de>

---
+++ aws/resource_aws_athena_workgroup_test.go
@@ -0,0 +1,80 @@
+package aws
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/athena"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func TestAccAWSAthenaWorkGroup_basic(t *testing.T) {
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAWSAthenaWorkGroupDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAthenaWorkGroupConfig(acctest.RandString(5)),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAWSAthenaWorkGroupExists("aws_athena_workgroup.foo"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckAWSAthenaWorkGroupDestroy(s *terraform.State) error {
+	conn := testAccProvider.Meta().(*AWSClient).athenaconn
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "aws_athena_workgroup" {
+			continue
+		}
+
+		input := &athena.GetWorkGroupInput{
+			WorkGroup: aws.String(rs.Primary.ID),
+		}
+
+		resp, err := conn.GetWorkGroup(input)
+		if err != nil {
+			if isAWSErr(err, athena.ErrCodeInvalidRequestException, rs.Primary.ID) {
+				return nil
+			}
+			return err
+		}
+		if resp.WorkGroup != nil {
+			return fmt.Errorf("Athena WorkGroup (%s) found", rs.Primary.ID)
+		}
+	}
+	return nil
+}
+
+func testAccCheckAWSAthenaWorkGroupExists(name string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[name]
+		if !ok {
+			return fmt.Errorf("Not found: %s", name)
+		}
+
+		conn := testAccProvider.Meta().(*AWSClient).athenaconn
+
+		input := &athena.GetWorkGroupInput{
+			WorkGroup: aws.String(rs.Primary.ID),
+		}
+
+		_, err := conn.GetWorkGroup(input)
+		return err
+	}
+}
+
+func testAccAthenaWorkGroupConfig(rName string) string {
+	return fmt.Sprintf(`
+resource "aws_athena_workgroup" "foo" {
+  name = "tf-athena-workgroup-%s"
+}
+		`, rName)
+}
