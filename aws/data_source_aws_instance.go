e0682db72fc20f36b172a4fdb52f69948d1c31d3
Brian Flad <bflad417@gmail.com>
2019-03-18 20:41:32 -0400

    data-source/aws_instance: Add get_user_data argument and user_data_base64 attribute
    Reference: https://github.com/terraform-providers/terraform-provider-aws/issues/5927
    
    As contents may be large and not necessarily safe for the Terraform state, which requires valid UTF-8, we allow retrieving the Base64 contents of EC2 Instance user data into the `user_data_base64` attribute when a new `get_user_data` argument is set to `true`.
    
    Output from acceptance testing:
    
    ```
    --- PASS: TestAccAWSInstanceDataSource_basic (149.59s)
    --- PASS: TestAccAWSInstanceDataSource_GetUserData (158.67s)
    ```

--- aws/data_source_aws_instance.go
+++ aws/data_source_aws_instance.go
@@ -112,7 +112,16 @@ func dataSourceAwsInstance() *schema.Resource {
 				Type:     schema.TypeBool,
 				Computed: true,
 			},
+			"get_user_data": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+			},
 			"user_data": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"user_data_base64": {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
@@ -423,8 +432,11 @@ func instanceDescriptionAttributes(d *schema.ResourceData, instance *ec2.Instanc
 		if err != nil {
 			return err
 		}
-		if attr.UserData.Value != nil {
-			d.Set("user_data", userDataHashSum(*attr.UserData.Value))
+		if attr != nil && attr.UserData != nil && attr.UserData.Value != nil {
+			d.Set("user_data", userDataHashSum(aws.StringValue(attr.UserData.Value)))
+			if d.Get("get_user_data").(bool) {
+				d.Set("user_data_base64", aws.StringValue(attr.UserData.Value))
+			}
 		}
 	}
 	{
