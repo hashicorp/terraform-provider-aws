b15a4dd4871dc83f9e388779bc599f7f94c6953e
G Panula <github@kablah.com>
2019-03-23 21:34:19 -0500

    fix networking example

--- examples/networking/region/variables.tf
+++ examples/networking/region/variables.tf
@@ -7,3 +7,13 @@ variable "base_cidr_block" {}
 provider "aws" {
   region = "${var.region}"
 }
+
+variable "region_numbers" {
+  default = {
+    us-east-1 = 1
+    us-west-1 = 2
+    us-west-2 = 3
+    eu-west-1 = 4
+  }
+}
+
