41544ff49660d2c1d67b8ae09f9df42517698671
Brian Flad <bflad417@gmail.com>
2019-03-28 19:16:33 -0400

    resource/aws_s3_bucket: Adjust SDK constant for lifecycle transition storage class consistency

--- aws/validators.go
+++ aws/validators.go
@@ -903,7 +903,7 @@ func validateS3BucketLifecycleTransitionStorageClass() schema.SchemaValidateFunc
 		s3.TransitionStorageClassStandardIa,
 		s3.TransitionStorageClassOnezoneIa,
 		s3.TransitionStorageClassIntelligentTiering,
-		s3.StorageClassDeepArchive,
+		s3.TransitionStorageClassDeepArchive,
 	}, false)
 }
 
