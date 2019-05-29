7029bae922268d608d709cbdae61b78c05c585b0
Oscar Utbult <oscar.utbult@gmail.com>
2019-05-29 16:20:01 +0200

    Upgrade lambda example to 0.12

--- examples/lambda/outputs.tf
+++ examples/lambda/outputs.tf
@@ -1,3 +1,4 @@
 output "lambda" {
-  value = "${aws_lambda_function.lambda.qualified_arn}"
+  value = aws_lambda_function.lambda.qualified_arn
 }
+
--- examples/lambda/variables.tf
+++ examples/lambda/variables.tf
@@ -2,3 +2,4 @@ variable "aws_region" {
   description = "The AWS region to create things in."
   default     = "us-east-1"
 }
+
