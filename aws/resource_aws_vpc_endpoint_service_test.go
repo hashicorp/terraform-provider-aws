d5f72ee7dde8014e59f327c2cfad54d7e3266701
Kit Ewbank <Christopher.Ewbank@capitalone.com>
2019-05-30 15:43:27 -0400

    Add a real '_basic' acceptance test.

--- aws/resource_aws_vpc_endpoint_service_test.go
+++ aws/resource_aws_vpc_endpoint_service_test.go
@@ -92,7 +92,38 @@ func TestAccAWSVpcEndpointService_basic(t *testing.T) {
 		CheckDestroy: testAccCheckVpcEndpointServiceDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccVpcEndpointServiceConfig_basic(rName1),
+				Config: testAccVpcEndpointServiceConfig_basic(rName1, rName2),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckVpcEndpointServiceExists(resourceName, &svcCfg),
+					resource.TestCheckResourceAttr(resourceName, "acceptance_required", "false"),
+					resource.TestCheckResourceAttr(resourceName, "network_load_balancer_arns.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "allowed_principals.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "manages_vpc_endpoints", "false"),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", "0"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccAWSVpcEndpointService_AllowedPrincipalsAndTags(t *testing.T) {
+	var svcCfg ec2.ServiceConfiguration
+	resourceName := "aws_vpc_endpoint_service.test"
+	rName1 := fmt.Sprintf("tf-testacc-vpcesvc-%s", acctest.RandStringFromCharSet(13, acctest.CharSetAlphaNum))
+	rName2 := fmt.Sprintf("tf-testacc-vpcesvc-%s", acctest.RandStringFromCharSet(13, acctest.CharSetAlphaNum))
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckVpcEndpointServiceDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccVpcEndpointServiceConfig_allowedPrincipalsAndTags(rName1, rName2),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckVpcEndpointServiceExists(resourceName, &svcCfg),
 					resource.TestCheckResourceAttr(resourceName, "acceptance_required", "false"),
@@ -111,7 +142,7 @@ func TestAccAWSVpcEndpointService_basic(t *testing.T) {
 				ImportStateVerify: true,
 			},
 			{
-				Config: testAccVpcEndpointServiceConfig_modified(rName1, rName2),
+				Config: testAccVpcEndpointServiceConfig_allowedPrincipalsAndTagsUpdated(rName1, rName2),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckVpcEndpointServiceExists(resourceName, &svcCfg),
 					resource.TestCheckResourceAttr(resourceName, "acceptance_required", "true"),
@@ -129,7 +160,8 @@ func TestAccAWSVpcEndpointService_basic(t *testing.T) {
 func TestAccAWSVpcEndpointService_removed(t *testing.T) {
 	var svcCfg ec2.ServiceConfiguration
 	resourceName := "aws_vpc_endpoint_service.test"
-	rName := fmt.Sprintf("tf-testacc-vpcesvc-%s", acctest.RandStringFromCharSet(13, acctest.CharSetAlphaNum))
+	rName1 := fmt.Sprintf("tf-testacc-vpcesvc-%s", acctest.RandStringFromCharSet(13, acctest.CharSetAlphaNum))
+	rName2 := fmt.Sprintf("tf-testacc-vpcesvc-%s", acctest.RandStringFromCharSet(13, acctest.CharSetAlphaNum))
 
 	testDestroy := func(*terraform.State) error {
 		conn := testAccProvider.Meta().(*AWSClient).ec2conn
@@ -147,7 +179,7 @@ func TestAccAWSVpcEndpointService_removed(t *testing.T) {
 		CheckDestroy: testAccCheckVpcEndpointServiceDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccVpcEndpointServiceConfig_basic(rName),
+				Config: testAccVpcEndpointServiceConfig_basic(rName1, rName2),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckVpcEndpointServiceExists(resourceName, &svcCfg),
 					testDestroy,
@@ -215,79 +247,7 @@ func testAccCheckVpcEndpointServiceExists(n string, svcCfg *ec2.ServiceConfigura
 	}
 }
 
-func testAccVpcEndpointServiceConfig_basic(rName string) string {
-	return fmt.Sprintf(`
-resource "aws_vpc" "test" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = %[1]q
-  }
-}
-
-resource "aws_lb" "test1" {
-  name = %[1]q
-
-  subnets = [
-    "${aws_subnet.test1.id}",
-    "${aws_subnet.test2.id}",
-  ]
-
-  load_balancer_type         = "network"
-  internal                   = true
-  idle_timeout               = 60
-  enable_deletion_protection = false
-
-  tags = {
-    Name = %[1]q
-  }
-}
-
-data "aws_availability_zones" "available" {}
-
-resource "aws_subnet" "test1" {
-  vpc_id            = "${aws_vpc.test.id}"
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "${data.aws_availability_zones.available.names[0]}"
-
-  tags = {
-    Name = %[1]q
-  }
-}
-
-resource "aws_subnet" "test2" {
-  vpc_id            = "${aws_vpc.test.id}"
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "${data.aws_availability_zones.available.names[1]}"
-
-  tags = {
-    Name = %[1]q
-  }
-}
-
-data "aws_caller_identity" "current" {}
-
-resource "aws_vpc_endpoint_service" "test" {
-  acceptance_required = false
-
-  network_load_balancer_arns = [
-    "${aws_lb.test1.arn}",
-  ]
-
-  allowed_principals = [
-    "${data.aws_caller_identity.current.arn}",
-  ]
-
-  tags = {
-    Environment = "test"
-    Usage       = "original"
-    Name        = %[1]q
-  }
-}
-`, rName)
-}
-
-func testAccVpcEndpointServiceConfig_modified(rName1, rName2 string) string {
+func testAccVpcEndpointServiceConfig_base(rName1, rName2 string) string {
 	return fmt.Sprintf(`
 resource "aws_vpc" "test" {
   cidr_block = "10.0.0.0/16"
@@ -356,7 +316,45 @@ resource "aws_subnet" "test2" {
 }
 
 data "aws_caller_identity" "current" {}
+`, rName1, rName2)
+}
 
+func testAccVpcEndpointServiceConfig_basic(rName1, rName2 string) string {
+	return testAccVpcEndpointServiceConfig_base(rName1, rName2) + fmt.Sprintf(`
+resource "aws_vpc_endpoint_service" "test" {
+  acceptance_required = false
+
+  network_load_balancer_arns = [
+    "${aws_lb.test1.arn}",
+  ]
+}
+`)
+}
+
+func testAccVpcEndpointServiceConfig_allowedPrincipalsAndTags(rName1, rName2 string) string {
+	return testAccVpcEndpointServiceConfig_base(rName1, rName2) + fmt.Sprintf(`
+resource "aws_vpc_endpoint_service" "test" {
+  acceptance_required = false
+
+  network_load_balancer_arns = [
+    "${aws_lb.test1.arn}",
+  ]
+
+  allowed_principals = [
+    "${data.aws_caller_identity.current.arn}",
+  ]
+
+  tags = {
+    Environment = "test"
+    Usage       = "original"
+    Name        = %[1]q
+  }
+}
+`, rName1)
+}
+
+func testAccVpcEndpointServiceConfig_allowedPrincipalsAndTagsUpdated(rName1, rName2 string) string {
+	return testAccVpcEndpointServiceConfig_base(rName1, rName2) + fmt.Sprintf(`
 resource "aws_vpc_endpoint_service" "test" {
   acceptance_required = true
 
@@ -372,5 +370,5 @@ resource "aws_vpc_endpoint_service" "test" {
     Name  = %[1]q
   }
 }
-`, rName1, rName2)
+`, rName1)
 }
