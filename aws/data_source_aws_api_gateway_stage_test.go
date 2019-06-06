5f8e403b9fe6ccfd9d71ea71df909a4375c021a1
Dean <deangalvin3@gmail.com>
2019-06-06 15:41:07 -0400

    Use stage name over basename

--- aws/data_source_aws_api_gateway_stage_test.go
+++ aws/data_source_aws_api_gateway_stage_test.go
@@ -90,7 +90,7 @@ resource "aws_api_gateway_stage" "example" {
 
 data "aws_api_gateway_stage" "example" {
   rest_api_id = "${aws_api_gateway_rest_api.test.id}"
-  stage_name 	= "${basename(aws_api_gateway_stage.example.invoke_url)}"
+  stage_name 	= "${aws_api_gateway_stage.example.stage_name}"
 }
 `, r)
 }
