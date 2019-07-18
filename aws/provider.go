758f50c988ececb1985c1938cef2fa439bee0630
Ryn Daniels <ryn@hashicorp.com>
2019-07-18 17:09:04 +0100

    Fixing whitespace after fixing merge conflicts via the web

--- aws/provider.go
+++ aws/provider.go
@@ -273,7 +273,7 @@ func Provider() terraform.ResourceProvider {
 			"aws_vpc_peering_connection":                    dataSourceAwsVpcPeeringConnection(),
 			"aws_vpn_gateway":                               dataSourceAwsVpnGateway(),
 			"aws_waf_web_acl":                               dataSourceAwsWafWebAcl(),
-      "aws_wafregional_rule":                          dataSourceAwsWafRegionalRule(),
+			"aws_wafregional_rule":                          dataSourceAwsWafRegionalRule(),
 			"aws_wafregional_web_acl":                       dataSourceAwsWafRegionalWebAcl(),
 			"aws_workspaces_bundle":                         dataSourceAwsWorkspaceBundle(),
 
