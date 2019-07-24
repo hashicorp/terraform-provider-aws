015cc8f0cb5e8b8d696a8c6ea619035215bf1fc9
Ryn Daniels <ryn@hashicorp.com>
2019-07-24 19:21:12 +0200

    Import ordering

--- aws/data_source_aws_waf_ipset_test.go
+++ aws/data_source_aws_waf_ipset_test.go
@@ -2,10 +2,10 @@ package aws
 
 import (
 	"fmt"
-	"github.com/hashicorp/terraform/helper/acctest"
 	"regexp"
 	"testing"
 
+	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
 )
 
