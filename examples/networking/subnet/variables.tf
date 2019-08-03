3388b2d3d38b3e80eda0fc124fcf363b128f82cf
Aidan Melen <amelen@exactsciences.com>
2019-08-03 17:00:57 -0600

    fixed issue https://github.com/terraform-providers/terraform-provider-aws/issues/9612

--- examples/networking/subnet/variables.tf
+++ examples/networking/subnet/variables.tf
@@ -9,3 +9,22 @@ data "aws_availability_zone" "target" {
 data "aws_vpc" "target" {
   id = "${var.vpc_id}"
 }
+
+variable "az_numbers" {
+  default = {
+    a = 1
+    b = 2
+    c = 3
+    d = 4
+    e = 5
+    f = 6
+    g = 7
+    h = 8
+    i = 9
+    j = 10
+    k = 11
+    l = 12
+    m = 13
+    n = 14
+  }
+}
