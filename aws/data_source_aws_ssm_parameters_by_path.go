e6ceacf64f1ad07bb021ec037d779a604b0fc5dd
katainaka0503 <katainaka0503@gmail.com>
2019-08-04 11:41:21 +0900

    Add datasource ssm_parameters_by_path

---
+++ aws/data_source_aws_ssm_parameters_by_path.go
@@ -0,0 +1,114 @@
+package aws
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform/helper/resource"
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ssm"
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func dataSourceAwsSsmParametersByPath() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceAwsSsmParametersReadByPath,
+
+		Schema: map[string]*schema.Schema{
+			"path": {
+				Type:     schema.TypeString,
+				Required: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"arns": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"names": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"types": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"values": {
+				Type:      schema.TypeList,
+				Computed:  true,
+				Sensitive: true,
+				Elem:      &schema.Schema{Type: schema.TypeString},
+			},
+			"with_decryption": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
+		},
+	}
+}
+
+func dataSourceAwsSsmParametersReadByPath(d *schema.ResourceData, meta interface{}) error {
+	ssmconn := meta.(*AWSClient).ssmconn
+
+	path := d.Get("path").(string)
+
+	arns := make([]string, 0)
+	names := make([]string, 0)
+	types := make([]string, 0)
+	values := make([]string, 0)
+
+	for {
+		paramInput := &ssm.GetParametersByPathInput{
+			Path:           aws.String(path),
+			WithDecryption: aws.Bool(d.Get("with_decryption").(bool)),
+		}
+
+		log.Printf("[DEBUG] Reading SSM Parameters by path: %s", paramInput)
+		resp, err := ssmconn.GetParametersByPath(paramInput)
+
+		if err != nil {
+			return fmt.Errorf("Error reading SSM parameters by path: %s", err)
+		}
+
+		params := resp.Parameters
+
+		for _, param := range params {
+			arns = append(arns, *param.ARN)
+			names = append(names, *param.Name)
+			types = append(types, *param.Type)
+			values = append(values, *param.Value)
+		}
+
+		if resp.NextToken == nil {
+			break
+		}
+		paramInput.NextToken = resp.NextToken
+	}
+
+	d.SetId(resource.UniqueId())
+
+	err := d.Set("arns", arns)
+	if err != nil {
+		return err
+	}
+
+	err = d.Set("names", names)
+	if err != nil {
+		return err
+	}
+
+	err = d.Set("types", types)
+	if err != nil {
+		return err
+	}
+
+	err = d.Set("values", values)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ aws/data_source_aws_ssm_parameters_by_path_test.go
@@ -0,0 +1,60 @@
+package aws
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccAWSSsmParametersByPathDataSource_basic(t *testing.T) {
+	resourceName := "data.aws_ssm_parameters_by_path.test"
+	prefix := "path"
+	otherPrefix := "dummy"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckAwsSsmParametersByPathDataSourceConfig(prefix, otherPrefix, "false"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "arns.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "names.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "types.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "values.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "with_decryption", "false"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckAwsSsmParametersByPathDataSourceConfig(prefix string, otherPrefix string, withDecryption string) string {
+	return fmt.Sprintf(`
+resource "aws_ssm_parameter" "test_a" {
+	name = "/%s/param-a"
+	type = "String"
+	value = "TestValueA"
+}
+
+resource "aws_ssm_parameter" "test_b" {
+	name = "/%s/param-b"
+	type = "String"
+	value = "TestValueB"
+}
+
+resource "aws_ssm_parameter" "test_c" {
+	name = "/%s/param-c"
+	type = "String"
+	value = "TestValueC"
+}
+
+data "aws_ssm_parameters_by_path" "test" {
+	path = "/${element(split("/", aws_ssm_parameter.test_a.name), 1)}"
+	with_decryption = "%s"
+}
+`, prefix, prefix, otherPrefix, withDecryption)
+}
