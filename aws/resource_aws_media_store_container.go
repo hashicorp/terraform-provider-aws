3c14a1992e60ad5752a9e4a16e13ef4f1529527e
Ryn Daniels <ryn@hashicorp.com>
2019-08-05 18:50:13 +0200

    Final retries after timeouts deleting media resources

--- aws/resource_aws_media_store_container.go
+++ aws/resource_aws_media_store_container.go
@@ -132,10 +132,10 @@ func resourceAwsMediaStoreContainerDelete(d *schema.ResourceData, meta interface
 		return err
 	}
 
+	dcinput := &mediastore.DescribeContainerInput{
+		ContainerName: aws.String(d.Id()),
+	}
 	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
-		dcinput := &mediastore.DescribeContainerInput{
-			ContainerName: aws.String(d.Id()),
-		}
 		_, err := conn.DescribeContainer(dcinput)
 		if err != nil {
 			if isAWSErr(err, mediastore.ErrCodeContainerNotFoundException, "") {
@@ -145,6 +145,9 @@ func resourceAwsMediaStoreContainerDelete(d *schema.ResourceData, meta interface
 		}
 		return resource.RetryableError(fmt.Errorf("Media Store Container (%s) still exists", d.Id()))
 	})
+	if isResourceTimeoutError(err) {
+		_, err = conn.DescribeContainer(dcinput)
+	}
 	if err != nil {
 		return fmt.Errorf("error waiting for Media Store Container (%s) deletion: %s", d.Id(), err)
 	}
