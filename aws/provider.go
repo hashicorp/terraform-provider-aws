17b322844601cf872a13f0f6f0f6030a5183f300
Kamil Szczygieł <kamil@szczygiel.io>
2019-09-05 11:39:11 +0200

    remove member/invite resources as they're already in different PR

--- aws/provider.go
+++ aws/provider.go
@@ -699,8 +699,6 @@ func Provider() terraform.ResourceProvider {
 			"aws_security_group_rule":                                 resourceAwsSecurityGroupRule(),
 			"aws_securityhub_account":                                 resourceAwsSecurityHubAccount(),
 			"aws_securityhub_accept_invitation":                       resourceAwsSecurityHubAcceptInvitation(),
-			"aws_securityhub_invite":                                  resourceAwsSecurityHubInvite(),
-			"aws_securityhub_member":                                  resourceAwsSecurityHubMember(),
 			"aws_securityhub_product_subscription":                    resourceAwsSecurityHubProductSubscription(),
 			"aws_securityhub_standards_subscription":                  resourceAwsSecurityHubStandardsSubscription(),
 			"aws_servicecatalog_portfolio":                            resourceAwsServiceCatalogPortfolio(),
