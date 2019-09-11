941e92827cf7f9535c5753c6327d386b07be6b12
Ryn Daniels <ryn@hashicorp.com>
2019-09-11 17:17:42 +0200

    Automatically run make fmt if the tests need a make fmt

--- scripts/gofmtcheck.sh
+++ scripts/gofmtcheck.sh
@@ -6,8 +6,8 @@ gofmt_files=$(find . -name '*.go' | grep -v vendor | xargs gofmt -l -s)
 if [[ -n ${gofmt_files} ]]; then
     echo 'gofmt needs running on the following files:'
     echo "${gofmt_files}"
-    echo "You can use the command: \`make fmt\` to reformat code."
-    exit 1
+    echo "Running \`make fmt\`..."
+    make fmt
 fi
 
 exit 0
