9cc7e9fef9a6c5511bb5b11d9993102a94964c90
Julien Duchesne <jduchesne@coveo.com>
2019-09-27 09:07:51 -0400

    Add data source to download a s3 bucket object

---
+++ aws/data_source_aws_s3_download_bucket_object_test.go
@@ -0,0 +1,82 @@
+package aws
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"io/ioutil"
+	"path"
+	"testing"
+)
+
+func TestAccDataSourceAWSS3DownloadBucketObject_basic(t *testing.T) {
+	dirName, _ := ioutil.TempDir("", "")
+	fileName := path.Join(dirName, "testfile.txt")
+	rInt := acctest.RandInt()
+	resourceOnlyConf, conf := testAccAWSDataSourceS3DownloadObjectConfig_basic(rInt, fileName)
+
+	var rObj s3.GetObjectOutput
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                  func() { testAccPreCheck(t) },
+		Providers:                 testAccProviders,
+		PreventPostDestroyRefresh: true,
+		Steps: []resource.TestStep{
+			{
+				Config: resourceOnlyConf,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAWSS3BucketObjectExists("aws_s3_bucket_object.object", &rObj),
+				),
+			},
+			{
+				Config: conf,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAwsS3DownloadedObjectExists(fileName, "Hello World"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckAwsS3DownloadedObjectExists(fileName string, expectedContent string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		content, err := ioutil.ReadFile(fileName)
+		if err != nil {
+			return fmt.Errorf("Error while reading the downloaded file: %s", err)
+		}
+
+		gottenContent := string(content)
+		if gottenContent != expectedContent {
+			return fmt.Errorf("Gotten content was `%s`, not `%s` as expected", gottenContent, expectedContent)
+		}
+
+		return nil
+
+	}
+
+}
+
+func testAccAWSDataSourceS3DownloadObjectConfig_basic(randInt int, fileName string) (string, string) {
+	resources := fmt.Sprintf(`
+resource "aws_s3_bucket" "object_bucket" {
+	bucket = "tf-object-test-bucket-%d"
+}
+resource "aws_s3_bucket_object" "object" {
+	bucket = "${aws_s3_bucket.object_bucket.bucket}"
+	key = "tf-testing-obj-%d"
+	content = "Hello World"
+}
+`, randInt, randInt)
+
+	both := fmt.Sprintf(`%s
+data "aws_s3_download_bucket_object" "obj" {
+	bucket = "tf-object-test-bucket-%d"
+	key = "tf-testing-obj-%d"
+	filename = "%s"
+}
+`, resources, randInt, randInt, fileName)
+
+	return resources, both
+}
---
+++ website/docs/d/s3_download_bucket_object.html.markdown
@@ -0,0 +1,42 @@
+---
+layout: "aws"
+page_title: "AWS: aws_s3_download_bucket_object"
+sidebar_current: "docs-aws-datasource-s3-download-bucket-object"
+description: |-
+    Downloads a bucket object to be used as a local file
+---
+
+# Data Source: aws_s3_download_bucket_object
+
+The S3 Download Bucket Object Data Source allows download of a file to the disk to be used as a local file.
+
+## Example Usage
+
+The following example retrieves a text object (which must have a `Content-Type`
+value starting with `text/`) and uses it as the `user_data` for an EC2 instance:
+
+```hcl
+data "aws_s3_download_bucket_object" "lambda_file" {
+  bucket   = "ourcorp-deploy-config"
+  key      = "lambda_file.zip"
+  filename = "lambda_file.zip"
+}
+
+resource "aws_lambda_function" "example" {
+  filename      = "lambda_file.zip"
+  function_name = "example"
+  handler       = "lambda_handler.lambda_handler"
+  runtime       = "python3.7"
+}
+```
+
+This is especially useful when using a S3 file to deploy a Lambda function in multiple regions. Deployment from S3 can only occur in the same region as the lambda.
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `bucket` - (Required) The name of the bucket to read the object from
+* `key` - (Required) The full path to the object inside the bucket
+* `version_id` - (Optional) Specific version ID of the object returned (defaults to latest version)
+* `filename` - (Required) The path where the file should be downloaded
