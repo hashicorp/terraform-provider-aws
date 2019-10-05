e790475a86546dbb202935bce71e2b7fd7153957
Kristian.Drucker@perspectum.com <Kristian.Drucker@perspectum.com>
2019-10-05 16:10:59 +0100

    Format QLDB Ledger and fix code

--- aws/data_source_aws_qldb_ledger.go
+++ aws/data_source_aws_qldb_ledger.go
@@ -1,74 +1,74 @@
 package aws
 
 import (
-    "fmt"
-    "github.com/aws/aws-sdk-go/aws"
-    "github.com/aws/aws-sdk-go/service/qldb"
-    "log"
-    "regexp"
-    "time"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/qldb"
+	"log"
+	"regexp"
+	"time"
 
-    "github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
 func dataSourceAwsQLDBLedger() *schema.Resource {
-    return &schema.Resource{
-        Read: dataSourceAwsQLDBLedgerRead,
-        Schema: map[string]*schema.Schema{
-            "arn": {
-                Type:     schema.TypeString,
-                Computed: true,
-            },
+	return &schema.Resource{
+		Read: dataSourceAwsQLDBLedgerRead,
+		Schema: map[string]*schema.Schema{
+			"arn": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
 
-            "name": {
-                Type:     schema.TypeString,
-                Required: true,
-                ValidateFunc: func(v interface{}, k string) (ws []string, errors []error) {
-                    value := v.(string)
-                    validNamePattern := "^[A-Za-z0-9_-]+$"
-                    validName, nameMatchErr := regexp.MatchString(validNamePattern, value)
-                    if !validName || nameMatchErr != nil {
-                        errors = append(errors, fmt.Errorf(
-                            "%q must match regex '%v'", k, validNamePattern))
-                    }
-                    return
-                },
-            },
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+				ValidateFunc: func(v interface{}, k string) (ws []string, errors []error) {
+					value := v.(string)
+					validNamePattern := "^[A-Za-z0-9_-]+$"
+					validName, nameMatchErr := regexp.MatchString(validNamePattern, value)
+					if !validName || nameMatchErr != nil {
+						errors = append(errors, fmt.Errorf(
+							"%q must match regex '%v'", k, validNamePattern))
+					}
+					return
+				},
+			},
 
-            "permissions_mode": {
-                Type:     schema.TypeString,
-                Computed: true,
-            },
+			"permissions_mode": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
 
-            "deletion_protection": {
-                Type:     schema.TypeBool,
-                Computed: true,
-            },
-        },
-    }
+			"deletion_protection": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+		},
+	}
 }
 
 func dataSourceAwsQLDBLedgerRead(d *schema.ResourceData, meta interface{}) error {
-    conn := meta.(*AWSClient).qldbconn
+	conn := meta.(*AWSClient).qldbconn
 
-    target := d.Get("name")
+	target := d.Get("name")
 
-    req := &qldb.DescribeLedgerInput{
-        Name: aws.String(target.(string)),
-    }
+	req := &qldb.DescribeLedgerInput{
+		Name: aws.String(target.(string)),
+	}
 
-    log.Printf("[DEBUG] Reading QLDB Ledger: %s", req)
-    resp, err := conn.DescribeLedger(req)
+	log.Printf("[DEBUG] Reading QLDB Ledger: %s", req)
+	resp, err := conn.DescribeLedger(req)
 
-    if err != nil {
-        return fmt.Errorf("Error describing ledger: %s", err)
-    }
+	if err != nil {
+		return fmt.Errorf("Error describing ledger: %s", err)
+	}
 
-    d.SetId(time.Now().UTC().String())
-    d.Set("arn", resp.Arn)
-    // This is hardcoded because AWS SDK does not allow returning Permissions Mode
-    d.Set("permissions_mode", qldb.PermissionsModeAllowAll)
-    d.Set("deletion_protection", resp.DeletionProtection)
+	d.SetId(time.Now().UTC().String())
+	d.Set("arn", resp.Arn)
+	// This is hardcoded because AWS SDK does not allow returning Permissions Mode
+	d.Set("permissions_mode", qldb.PermissionsModeAllowAll)
+	d.Set("deletion_protection", resp.DeletionProtection)
 
-    return nil
+	return nil
 }
--- aws/data_source_aws_qldb_ledger_test.go
+++ aws/data_source_aws_qldb_ledger_test.go
@@ -1,63 +1,66 @@
 package aws
 
 import (
-    "fmt"
-    "testing"
+	"fmt"
+	"testing"
 
-    "github.com/hashicorp/terraform-plugin-sdk/helper/resource"
-    "github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
 func TestAccDataSourceAwsQLDBLedger(t *testing.T) {
-    resource.ParallelTest(t, resource.TestCase{
-        PreCheck:  func() { testAccPreCheck(t) },
-        Providers: testAccProviders,
-        Steps: []resource.TestStep{
-            {
-                Config: testAccDataSourceAwsQLDBLedgerConfig,
-                Check: resource.ComposeTestCheckFunc(
-                    testAccDataSourceAwsQLDBLedgerCheck("data.aws_qldb_ledger.by_name"),
-                ),
-            },
-        },
-    })
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceAwsQLDBLedgerConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccDataSourceAwsQLDBLedgerCheck("data.aws_qldb_ledger.by_name"),
+				),
+			},
+		},
+	})
 }
 
 func testAccDataSourceAwsQLDBLedgerCheck(name string) resource.TestCheckFunc {
-    return func(s *terraform.State) error {
-        rs, ok := s.RootModule().Resources[name]
-        if !ok {
-            return fmt.Errorf("root module has no resource called %s", name)
-        }
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[name]
+		if !ok {
+			return fmt.Errorf("root module has no resource called %s", name)
+		}
 
-        ledgerRs, ok := s.RootModule().Resources["aws_qldb_ledger.tf_test"]
-        if !ok {
-            return fmt.Errorf("can't find aws_qldb_ledger.tf_test in state")
-        }
+		ledgerRs, ok := s.RootModule().Resources["aws_qldb_ledger.tf_test"]
+		if !ok {
+			return fmt.Errorf("can't find aws_qldb_ledger.tf_test in state")
+		}
 
-        attr := rs.Primary.Attributes
+		attr := rs.Primary.Attributes
 
-        if attr["name"] != ledgerRs.Primary.Attributes["name"] {
-            return fmt.Errorf(
-                "name is %s; want %s",
-                attr["name"],
-                ledgerRs.Primary.Attributes["name"],
-            )
-        }
+		if attr["name"] != ledgerRs.Primary.Attributes["name"] {
+			return fmt.Errorf(
+				"name is %s; want %s",
+				attr["name"],
+				ledgerRs.Primary.Attributes["name"],
+			)
+		}
 
-        return nil
-    }
+		return nil
+	}
 }
 
 const testAccDataSourceAwsQLDBLedgerConfig = `
 resource "aws_qldb_ledger" "tf_wrong1" {
   name = "wrong1"
+  deletion_protection = false
 }
 resource "aws_qldb_ledger" "tf_test" {
-  name = "tf_test"
+  name = "tf-test"
+  deletion_protection = false
 }
 resource "aws_qldb_ledger" "tf_wrong2" {
   name = "wrong2"
+  deletion_protection = false
 }
 
 data "aws_qldb_ledger" "by_name" {
