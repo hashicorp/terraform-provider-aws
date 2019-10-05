f1ec286b60b9caba9ffe94c0c86f831335959931
Kit Ewbank <Kit_Ewbank@hotmail.com>
2019-10-05 17:18:14 -0400

    Terraform Plugin SDK migration.

--- aws/tags_apigateway2.go
+++ aws/tags_apigateway2.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/apigatewayv2"
-	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
 // setTags is a helper to set the tags for a resource. It expects the
