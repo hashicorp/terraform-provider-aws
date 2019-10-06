02e1f2a05322393d6aba39e5b123519266d78cb3
Kit Ewbank <Kit_Ewbank@hotmail.com>
2019-10-06 17:18:41 -0400

    r/aws_appmesh_route: Add support for HTTP header-based routing and route priorities.

--- aws/structure.go
+++ aws/structure.go
@@ -5261,6 +5261,10 @@ func expandAppmeshRouteSpec(vSpec []interface{}) *appmesh.RouteSpec {
 	}
 	mSpec := vSpec[0].(map[string]interface{})
 
+	if vPriority, ok := mSpec["priority"].(int); ok && vPriority > 0 {
+		spec.Priority = aws.Int64(int64(vPriority))
+	}
+
 	if vHttpRoute, ok := mSpec["http_route"].([]interface{}); ok && len(vHttpRoute) > 0 && vHttpRoute[0] != nil {
 		mHttpRoute := vHttpRoute[0].(map[string]interface{})
 
@@ -5280,7 +5284,7 @@ func expandAppmeshRouteSpec(vSpec []interface{}) *appmesh.RouteSpec {
 					if vVirtualNode, ok := mWeightedTarget["virtual_node"].(string); ok && vVirtualNode != "" {
 						weightedTarget.VirtualNode = aws.String(vVirtualNode)
 					}
-					if vWeight, ok := mWeightedTarget["weight"].(int); ok {
+					if vWeight, ok := mWeightedTarget["weight"].(int); ok && vWeight > 0 {
 						weightedTarget.Weight = aws.Int64(int64(vWeight))
 					}
 
@@ -5294,13 +5298,74 @@ func expandAppmeshRouteSpec(vSpec []interface{}) *appmesh.RouteSpec {
 		}
 
 		if vHttpRouteMatch, ok := mHttpRoute["match"].([]interface{}); ok && len(vHttpRouteMatch) > 0 && vHttpRouteMatch[0] != nil {
+			httpRouteMatch := &appmesh.HttpRouteMatch{}
+
 			mHttpRouteMatch := vHttpRouteMatch[0].(map[string]interface{})
 
-			if vPrefix, ok := mHttpRouteMatch["prefix"].(string); ok && vPrefix != "" {
-				spec.HttpRoute.Match = &appmesh.HttpRouteMatch{
-					Prefix: aws.String(vPrefix),
-				}
+			if vMethod, ok := mHttpRouteMatch["method"].(string); ok && vMethod != "" {
+				httpRouteMatch.Method = aws.String(vMethod)
 			}
+			if vPrefix, ok := mHttpRouteMatch["prefix"].(string); ok && vPrefix != "" {
+				httpRouteMatch.Prefix = aws.String(vPrefix)
+			}
+			if vScheme, ok := mHttpRouteMatch["scheme"].(string); ok && vScheme != "" {
+				httpRouteMatch.Scheme = aws.String(vScheme)
+			}
+
+			if vHttpRouteHeaders, ok := mHttpRouteMatch["header"].(*schema.Set); ok && vHttpRouteHeaders.Len() > 0 {
+				httpRouteHeaders := []*appmesh.HttpRouteHeader{}
+
+				for _, vHttpRouteHeader := range vHttpRouteHeaders.List() {
+					httpRouteHeader := &appmesh.HttpRouteHeader{}
+
+					mHttpRouteHeader := vHttpRouteHeader.(map[string]interface{})
+
+					if vInvert, ok := mHttpRouteHeader["invert"].(bool); ok {
+						httpRouteHeader.Invert = aws.Bool(vInvert)
+					}
+					if vName, ok := mHttpRouteHeader["name"].(string); ok && vName != "" {
+						httpRouteHeader.Name = aws.String(vName)
+					}
+
+					if vMatch, ok := mHttpRouteHeader["match"].([]interface{}); ok && len(vMatch) > 0 && vMatch[0] != nil {
+						httpRouteHeader.Match = &appmesh.HeaderMatchMethod{}
+
+						mMatch := vMatch[0].(map[string]interface{})
+
+						if vExact, ok := mMatch["exact"].(string); ok && vExact != "" {
+							httpRouteHeader.Match.Exact = aws.String(vExact)
+						}
+						if vPrefix, ok := mMatch["prefix"].(string); ok && vPrefix != "" {
+							httpRouteHeader.Match.Prefix = aws.String(vPrefix)
+						}
+						if vRegex, ok := mMatch["regex"].(string); ok && vRegex != "" {
+							httpRouteHeader.Match.Regex = aws.String(vRegex)
+						}
+						if vSuffix, ok := mMatch["suffix"].(string); ok && vSuffix != "" {
+							httpRouteHeader.Match.Suffix = aws.String(vSuffix)
+						}
+
+						if vRange, ok := mMatch["range"].([]interface{}); ok && len(vRange) > 0 && vRange[0] != nil {
+							httpRouteHeader.Match.Range = &appmesh.MatchRange{}
+
+							mRange := vRange[0].(map[string]interface{})
+
+							if vEnd, ok := mRange["end"].(int); ok && vEnd > 0 {
+								httpRouteHeader.Match.Range.End = aws.Int64(int64(vEnd))
+							}
+							if vStart, ok := mRange["start"].(int); ok && vStart > 0 {
+								httpRouteHeader.Match.Range.Start = aws.Int64(int64(vStart))
+							}
+						}
+					}
+
+					httpRouteHeaders = append(httpRouteHeaders, httpRouteHeader)
+				}
+
+				httpRouteMatch.Headers = httpRouteHeaders
+			}
+
+			spec.HttpRoute.Match = httpRouteMatch
 		}
 	}
 
@@ -5323,7 +5388,7 @@ func expandAppmeshRouteSpec(vSpec []interface{}) *appmesh.RouteSpec {
 					if vVirtualNode, ok := mWeightedTarget["virtual_node"].(string); ok && vVirtualNode != "" {
 						weightedTarget.VirtualNode = aws.String(vVirtualNode)
 					}
-					if vWeight, ok := mWeightedTarget["weight"].(int); ok {
+					if vWeight, ok := mWeightedTarget["weight"].(int); ok && vWeight > 0 {
 						weightedTarget.Weight = aws.Int64(int64(vWeight))
 					}
 
@@ -5345,34 +5410,72 @@ func flattenAppmeshRouteSpec(spec *appmesh.RouteSpec) []interface{} {
 		return []interface{}{}
 	}
 
-	mSpec := map[string]interface{}{}
+	mSpec := map[string]interface{}{
+		"priority": int(aws.Int64Value(spec.Priority)),
+	}
 
-	if spec.HttpRoute != nil {
+	if httpRoute := spec.HttpRoute; httpRoute != nil {
 		mHttpRoute := map[string]interface{}{}
 
-		if spec.HttpRoute.Action != nil && spec.HttpRoute.Action.WeightedTargets != nil {
-			vWeightedTargets := []interface{}{}
+		if action := httpRoute.Action; action != nil {
+			if weightedTargets := action.WeightedTargets; weightedTargets != nil {
+				vWeightedTargets := []interface{}{}
 
-			for _, weightedTarget := range spec.HttpRoute.Action.WeightedTargets {
-				mWeightedTarget := map[string]interface{}{
-					"virtual_node": aws.StringValue(weightedTarget.VirtualNode),
-					"weight":       int(aws.Int64Value(weightedTarget.Weight)),
+				for _, weightedTarget := range weightedTargets {
+					mWeightedTarget := map[string]interface{}{
+						"virtual_node": aws.StringValue(weightedTarget.VirtualNode),
+						"weight":       int(aws.Int64Value(weightedTarget.Weight)),
+					}
+
+					vWeightedTargets = append(vWeightedTargets, mWeightedTarget)
 				}
 
-				vWeightedTargets = append(vWeightedTargets, mWeightedTarget)
-			}
-
-			mHttpRoute["action"] = []interface{}{
-				map[string]interface{}{
-					"weighted_target": schema.NewSet(appmeshRouteWeightedTargetHash, vWeightedTargets),
-				},
+				mHttpRoute["action"] = []interface{}{
+					map[string]interface{}{
+						"weighted_target": schema.NewSet(appmeshWeightedTargetHash, vWeightedTargets),
+					},
+				}
 			}
 		}
 
-		if spec.HttpRoute.Match != nil {
+		if httpRouteMatch := httpRoute.Match; httpRouteMatch != nil {
+			vHttpRouteHeaders := []interface{}{}
+
+			for _, httpRouteHeader := range httpRouteMatch.Headers {
+				mHttpRouteHeader := map[string]interface{}{
+					"invert": aws.BoolValue(httpRouteHeader.Invert),
+					"name":   aws.StringValue(httpRouteHeader.Name),
+				}
+
+				if match := httpRouteHeader.Match; match != nil {
+					mMatch := map[string]interface{}{
+						"exact":  aws.StringValue(match.Exact),
+						"prefix": aws.StringValue(match.Prefix),
+						"regex":  aws.StringValue(match.Regex),
+						"suffix": aws.StringValue(match.Suffix),
+					}
+
+					if r := match.Range; r != nil {
+						mRange := map[string]interface{}{
+							"end":   int(aws.Int64Value(r.End)),
+							"start": int(aws.Int64Value(r.Start)),
+						}
+
+						mMatch["range"] = []interface{}{mRange}
+					}
+
+					mHttpRouteHeader["match"] = []interface{}{mMatch}
+				}
+
+				vHttpRouteHeaders = append(vHttpRouteHeaders, mHttpRouteHeader)
+			}
+
 			mHttpRoute["match"] = []interface{}{
 				map[string]interface{}{
-					"prefix": aws.StringValue(spec.HttpRoute.Match.Prefix),
+					"header": schema.NewSet(appmeshHttpRouteHeaderHash, vHttpRouteHeaders),
+					"method": aws.StringValue(httpRouteMatch.Method),
+					"prefix": aws.StringValue(httpRouteMatch.Prefix),
+					"scheme": aws.StringValue(httpRouteMatch.Scheme),
 				},
 			}
 		}
@@ -5380,25 +5483,27 @@ func flattenAppmeshRouteSpec(spec *appmesh.RouteSpec) []interface{} {
 		mSpec["http_route"] = []interface{}{mHttpRoute}
 	}
 
-	if spec.TcpRoute != nil {
+	if tcpRoute := spec.TcpRoute; tcpRoute != nil {
 		mTcpRoute := map[string]interface{}{}
 
-		if spec.TcpRoute.Action != nil && spec.TcpRoute.Action.WeightedTargets != nil {
-			vWeightedTargets := []interface{}{}
+		if action := tcpRoute.Action; action != nil {
+			if weightedTargets := action.WeightedTargets; weightedTargets != nil {
+				vWeightedTargets := []interface{}{}
 
-			for _, weightedTarget := range spec.TcpRoute.Action.WeightedTargets {
-				mWeightedTarget := map[string]interface{}{
-					"virtual_node": aws.StringValue(weightedTarget.VirtualNode),
-					"weight":       int(aws.Int64Value(weightedTarget.Weight)),
+				for _, weightedTarget := range weightedTargets {
+					mWeightedTarget := map[string]interface{}{
+						"virtual_node": aws.StringValue(weightedTarget.VirtualNode),
+						"weight":       int(aws.Int64Value(weightedTarget.Weight)),
+					}
+
+					vWeightedTargets = append(vWeightedTargets, mWeightedTarget)
 				}
 
-				vWeightedTargets = append(vWeightedTargets, mWeightedTarget)
-			}
-
-			mTcpRoute["action"] = []interface{}{
-				map[string]interface{}{
-					"weighted_target": schema.NewSet(appmeshRouteWeightedTargetHash, vWeightedTargets),
-				},
+				mTcpRoute["action"] = []interface{}{
+					map[string]interface{}{
+						"weighted_target": schema.NewSet(appmeshWeightedTargetHash, vWeightedTargets),
+					},
+				}
 			}
 		}
 
