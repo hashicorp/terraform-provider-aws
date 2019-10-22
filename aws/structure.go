91077d79a7598af09451586b7f9f82cbadec1855
Michael Lin <mike@luminal.io>
2019-10-22 15:30:11 -0400

    Remove previous version

--- aws/structure.go
+++ aws/structure.go
@@ -621,23 +621,6 @@ func flattenEcsVolumes(list []*ecs.Volume) []map[string]interface{} {
 			"name": *volume.Name,
 		}
 
-		if volume.Host.SourcePath != nil {
-			l["host_path"] = *volume.Host.SourcePath
-		}
-
-		result = append(result, l)
-	}
-	return result
-}
-
-// Flattens an array of Volumes into a []map[string]interface{}
-func flattenEcsVolumes(list []*ecs.Volume) []map[string]interface{} {
-	result := make([]map[string]interface{}, 0, len(list))
-	for _, volume := range list {
-		l := map[string]interface{}{
-			"name": *volume.Name,
-		}
-
 		if volume.Host != nil && volume.Host.SourcePath != nil {
 			l["host_path"] = *volume.Host.SourcePath
 		}
