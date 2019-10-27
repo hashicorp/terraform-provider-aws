0d2068e47e877922d20495ec0693fb3291cb0fe3
NaotoSuzuki <suzuki.z2o.naoto@gmail.com>
2019-10-27 20:17:43 +0900

    Add validateDocDBIdentifier test

--- aws/validators_test.go
+++ aws/validators_test.go
@@ -1403,6 +1403,37 @@ func TestValidateApiGatewayUsagePlanQuotaSettings(t *testing.T) {
 	}
 }
 
+func TestValidateDocDBIdentifier(t *testing.T) {
+	validNames := []string{
+		"a",
+		"hello-world",
+		"hello-world-0123456789",
+	}
+	for _, v := range validNames {
+		_, errors := validateDocDBIdentifier(v, "name")
+		if len(errors) != 0 {
+			t.Fatalf("%q should be a valid DocDB Identifier: %q", v, errors)
+		}
+	}
+
+	invalidNames := []string{
+		"",
+		"special@character",
+		"slash/in-the-middle",
+		"dot.in-the-middle",
+		"two-hyphen--in-the-middle",
+		"0-first-numeric",
+		"-first-hyphen",
+		"end-hyphen-",
+	}
+	for _, v := range invalidNames {
+		_, errors := validateDocDBIdentifier(v, "name")
+		if len(errors) == 0 {
+			t.Fatalf("%q should be an invalid DocDB Identifier", v)
+		}
+	}
+}
+
 func TestValidateElbName(t *testing.T) {
 	validNames := []string{
 		"tf-test-elb",
