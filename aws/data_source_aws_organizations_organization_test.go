1caf6314a91c464d82deb84587e0ea7ff3e5d0ae
Bohdan Tsikhanovskyi <bohdan.tsikhanovskyi@grammarly.com>
2019-10-28 12:29:57 +0200

    add status attribute to resource/datasource aws_organizaion

--- aws/data_source_aws_organizations_organization_test.go
+++ aws/data_source_aws_organizations_organization_test.go
@@ -29,6 +29,7 @@ func testAccDataSourceAwsOrganizationsOrganization_basic(t *testing.T) {
 					resource.TestCheckResourceAttrPair(resourceName, "enabled_policy_types.#", dataSourceName, "enabled_policy_types.#"),
 					resource.TestCheckResourceAttrPair(resourceName, "feature_set", dataSourceName, "feature_set"),
 					resource.TestCheckResourceAttrPair(resourceName, "id", dataSourceName, "id"),
+					resource.TestCheckResourceAttrPair(resourceName, "status", dataSourceName, "status"),
 					resource.TestCheckResourceAttrPair(resourceName, "master_account_arn", dataSourceName, "master_account_arn"),
 					resource.TestCheckResourceAttrPair(resourceName, "master_account_email", dataSourceName, "master_account_email"),
 					resource.TestCheckResourceAttrPair(resourceName, "master_account_id", dataSourceName, "master_account_id"),
