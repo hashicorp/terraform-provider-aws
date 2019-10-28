74326983cf2d80d88b16950967c08a39cd070ce7
Kirill Zonov <kzonov@babbel.com>
2019-10-28 17:46:19 +0100

    Just moved things around

--- aws/resource_aws_guardduty_filter.go
+++ aws/resource_aws_guardduty_filter.go
@@ -98,144 +98,6 @@ func resourceAwsGuardDutyFilter() *schema.Resource {
 	}
 }
 
-func criteriaFields() []string {
-	criteria := make([]string, 0, len(criteriaMap()))
-	for criterion := range criteriaMap() {
-		criteria = append(criteria, criterion)
-	}
-	return criteria
-}
-
-func criteriaMap() map[string][]string {
-	return map[string][]string{
-		"confidence": {"equals", "not_equals"},
-		"id":         {"equals", "not_equals"},
-		"account_id": {"equals", "not_equals"},
-		"region":     {"equals", "not_equals"},
-		"resource.accessKeyDetails.accessKeyId":                                          {"equals", "not_equals"},
-		"resource.accessKeyDetails.principalId":                                          {"equals", "not_equals"},
-		"resource.accessKeyDetails.userName":                                             {"equals", "not_equals"},
-		"resource.accessKeyDetails.userType":                                             {"equals", "not_equals"},
-		"resource.instanceDetails.iamInstanceProfile.id":                                 {"equals", "not_equals"},
-		"resource.instanceDetails.imageId":                                               {"equals", "not_equals"},
-		"resource.instanceDetails.instanceId":                                            {"equals", "not_equals"},
-		"resource.instanceDetails.networkInterfaces.ipv6Addresses":                       {"equals", "not_equals"},
-		"resource.instanceDetails.networkInterfaces.privateIpAddresses.privateIpAddress": {"equals", "not_equals"},
-		"resource.instanceDetails.networkInterfaces.publicDnsName":                       {"equals", "not_equals"},
-		"resource.instanceDetails.networkInterfaces.publicIp":                            {"equals", "not_equals"},
-		"resource.instanceDetails.networkInterfaces.securityGroups.groupId":              {"equals", "not_equals"},
-		"resource.instanceDetails.networkInterfaces.securityGroups.groupName":            {"equals", "not_equals"},
-		"resource.instanceDetails.networkInterfaces.subnetId":                            {"equals", "not_equals"},
-		"resource.instanceDetails.networkInterfaces.vpcId":                               {"equals", "not_equals"},
-		"resource.instanceDetails.tags.key":                                              {"equals", "not_equals"},
-		"resource.instanceDetails.tags.value":                                            {"equals", "not_equals"},
-		"resource.resourceType":                                                          {"equals", "not_equals"},
-		"service.action.actionType":                                                      {"equals", "not_equals"},
-		"service.action.awsApiCallAction.api":                                            {"equals", "not_equals"},
-		"service.action.awsApiCallAction.callerType":                                     {"equals", "not_equals"},
-		"service.action.awsApiCallAction.remoteIpDetails.city.cityName":                  {"equals", "not_equals"},
-		"service.action.awsApiCallAction.remoteIpDetails.country.countryName":            {"equals", "not_equals"},
-		"service.action.awsApiCallAction.remoteIpDetails.ipAddressV4":                    {"equals", "not_equals"},
-		"service.action.awsApiCallAction.remoteIpDetails.organization.asn":               {"equals", "not_equals"},
-		"service.action.awsApiCallAction.remoteIpDetails.organization.asnOrg":            {"equals", "not_equals"},
-		"service.action.awsApiCallAction.serviceName":                                    {"equals", "not_equals"},
-		"service.action.dnsRequestAction.domain":                                         {"equals", "not_equals"},
-		"service.action.networkConnectionAction.blocked":                                 {"equals", "not_equals"},
-		"service.action.networkConnectionAction.connectionDirection":                     {"equals", "not_equals"},
-		"service.action.networkConnectionAction.localPortDetails.port":                   {"equals", "not_equals"},
-		"service.action.networkConnectionAction.protocol":                                {"equals", "not_equals"},
-		"service.action.networkConnectionAction.remoteIpDetails.city.cityName":           {"equals", "not_equals"},
-		"service.action.networkConnectionAction.remoteIpDetails.country.countryName":     {"equals", "not_equals"},
-		"service.action.networkConnectionAction.remoteIpDetails.ipAddressV4":             {"equals", "not_equals"},
-		"service.action.networkConnectionAction.remoteIpDetails.organization.asn":        {"equals", "not_equals"},
-		"service.action.networkConnectionAction.remoteIpDetails.organization.asnOrg":     {"equals", "not_equals"},
-		"service.action.networkConnectionAction.remotePortDetails.port":                  {"equals", "not_equals"},
-		"service.additionalInfo.threatListName":                                          {"equals", "not_equals"},
-		"service.archived":                                                               {"equals", "not_equals"},
-		"service.resourceRole":                                                           {"equals", "not_equals"},
-		"severity":                                                                       {"equals", "not_equals"},
-		"type":                                                                           {"equals", "not_equals"},
-		"updatedAt":                                                                      {"equals", "not_equals", "greater_than", "greater_than_or_equal", "less_than", "less_than_or_equal"},
-	}
-}
-
-func conditionAllowedForCriterion(criterion map[string]interface{}) bool {
-	availableConditions := criteriaMap()[criterion["field"].(string)]
-	conditionToCheck := criterion["condition"].(string)
-
-	for _, availableCondition := range availableConditions {
-		if availableCondition == conditionToCheck {
-			return true
-		}
-	}
-	return false
-}
-
-func serializeFindingCriteria(findingCriteria map[string]interface{}) (*guardduty.FindingCriteria, error) {
-	inputFindingCriteria := findingCriteria["criterion"].(*schema.Set).List()
-	criteria := map[string]*guardduty.Condition{}
-	for _, criterion := range inputFindingCriteria {
-		typedCriterion := criterion.(map[string]interface{})
-
-		if !conditionAllowedForCriterion(typedCriterion) {
-			return nil, fmt.Errorf("The condition is not supported for the given field. Supported conditions are: %v", criteriaMap()[typedCriterion["field"].(string)])
-		}
-
-		switch typedCriterion["condition"].(string) {
-		case "equals":
-			criteria[typedCriterion["field"].(string)] = &guardduty.Condition{
-				Equals: aws.StringSlice(conditionValueToStrings(typedCriterion["values"].([]interface{}))),
-			}
-		case "greater_than":
-			criteria[typedCriterion["field"].(string)] = &guardduty.Condition{
-				GreaterThan: aws.Int64(conditionValueToInt(typedCriterion["values"].([]interface{})).(int64)),
-			}
-		case "greater_than_or_equals":
-			criteria[typedCriterion["field"].(string)] = &guardduty.Condition{
-				GreaterThanOrEqual: aws.Int64(conditionValueToInt(typedCriterion["values"].([]interface{})).(int64)),
-			}
-		case "less_than":
-			criteria[typedCriterion["field"].(string)] = &guardduty.Condition{
-				LessThan: aws.Int64(conditionValueToInt(typedCriterion["values"].([]interface{})).(int64)),
-			}
-		case "less_than_or_equals":
-			criteria[typedCriterion["field"].(string)] = &guardduty.Condition{
-				LessThanOrEqual: aws.Int64(conditionValueToInt(typedCriterion["values"].([]interface{})).(int64)),
-			}
-		case "not_equals":
-			criteria[typedCriterion["field"].(string)] = &guardduty.Condition{
-				NotEquals: aws.StringSlice(conditionValueToStrings(typedCriterion["values"].([]interface{}))),
-			}
-		}
-
-	}
-	log.Printf("[DEBUG] Creating FindingCriteria map: %#v", findingCriteria)
-
-	return &guardduty.FindingCriteria{Criterion: criteria}, nil
-}
-
-func conditionValueToStrings(untypedValues []interface{}) []string {
-	values := make([]string, len(untypedValues))
-	for i, v := range untypedValues {
-		values[i] = v.(string)
-	}
-	return values
-}
-
-func conditionValueToInt(untypedValues []interface{}) interface{} {
-	if len(untypedValues) != 1 {
-		return fmt.Errorf("Exactly one value must be given for conditions like less_ or greater_than. Instead given: %v", untypedValues)
-	}
-
-	untypedValue := untypedValues[0]
-	typedValue, err := strconv.ParseInt(untypedValue.(string), 10, 64)
-	if err != nil {
-		return fmt.Errorf("Parsing condition value failed: %s", err.Error())
-	}
-
-	return typedValue
-}
-
 func resourceAwsGuardDutyFilterCreate(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*AWSClient).guarddutyconn
 
@@ -376,6 +238,144 @@ func resourceAwsGuardDutyFilterDelete(d *schema.ResourceData, meta interface{}) 
 		return fmt.Errorf("Deleting GuardDuty Filter '%s' failed: %s", d.Id(), err.Error())
 	}
 	return nil
+}
+
+func criteriaFields() []string {
+	criteria := make([]string, 0, len(criteriaMap()))
+	for criterion := range criteriaMap() {
+		criteria = append(criteria, criterion)
+	}
+	return criteria
+}
+
+func criteriaMap() map[string][]string {
+	return map[string][]string{
+		"confidence": {"equals", "not_equals"},
+		"id":         {"equals", "not_equals"},
+		"account_id": {"equals", "not_equals"},
+		"region":     {"equals", "not_equals"},
+		"resource.accessKeyDetails.accessKeyId":                                          {"equals", "not_equals"},
+		"resource.accessKeyDetails.principalId":                                          {"equals", "not_equals"},
+		"resource.accessKeyDetails.userName":                                             {"equals", "not_equals"},
+		"resource.accessKeyDetails.userType":                                             {"equals", "not_equals"},
+		"resource.instanceDetails.iamInstanceProfile.id":                                 {"equals", "not_equals"},
+		"resource.instanceDetails.imageId":                                               {"equals", "not_equals"},
+		"resource.instanceDetails.instanceId":                                            {"equals", "not_equals"},
+		"resource.instanceDetails.networkInterfaces.ipv6Addresses":                       {"equals", "not_equals"},
+		"resource.instanceDetails.networkInterfaces.privateIpAddresses.privateIpAddress": {"equals", "not_equals"},
+		"resource.instanceDetails.networkInterfaces.publicDnsName":                       {"equals", "not_equals"},
+		"resource.instanceDetails.networkInterfaces.publicIp":                            {"equals", "not_equals"},
+		"resource.instanceDetails.networkInterfaces.securityGroups.groupId":              {"equals", "not_equals"},
+		"resource.instanceDetails.networkInterfaces.securityGroups.groupName":            {"equals", "not_equals"},
+		"resource.instanceDetails.networkInterfaces.subnetId":                            {"equals", "not_equals"},
+		"resource.instanceDetails.networkInterfaces.vpcId":                               {"equals", "not_equals"},
+		"resource.instanceDetails.tags.key":                                              {"equals", "not_equals"},
+		"resource.instanceDetails.tags.value":                                            {"equals", "not_equals"},
+		"resource.resourceType":                                                          {"equals", "not_equals"},
+		"service.action.actionType":                                                      {"equals", "not_equals"},
+		"service.action.awsApiCallAction.api":                                            {"equals", "not_equals"},
+		"service.action.awsApiCallAction.callerType":                                     {"equals", "not_equals"},
+		"service.action.awsApiCallAction.remoteIpDetails.city.cityName":                  {"equals", "not_equals"},
+		"service.action.awsApiCallAction.remoteIpDetails.country.countryName":            {"equals", "not_equals"},
+		"service.action.awsApiCallAction.remoteIpDetails.ipAddressV4":                    {"equals", "not_equals"},
+		"service.action.awsApiCallAction.remoteIpDetails.organization.asn":               {"equals", "not_equals"},
+		"service.action.awsApiCallAction.remoteIpDetails.organization.asnOrg":            {"equals", "not_equals"},
+		"service.action.awsApiCallAction.serviceName":                                    {"equals", "not_equals"},
+		"service.action.dnsRequestAction.domain":                                         {"equals", "not_equals"},
+		"service.action.networkConnectionAction.blocked":                                 {"equals", "not_equals"},
+		"service.action.networkConnectionAction.connectionDirection":                     {"equals", "not_equals"},
+		"service.action.networkConnectionAction.localPortDetails.port":                   {"equals", "not_equals"},
+		"service.action.networkConnectionAction.protocol":                                {"equals", "not_equals"},
+		"service.action.networkConnectionAction.remoteIpDetails.city.cityName":           {"equals", "not_equals"},
+		"service.action.networkConnectionAction.remoteIpDetails.country.countryName":     {"equals", "not_equals"},
+		"service.action.networkConnectionAction.remoteIpDetails.ipAddressV4":             {"equals", "not_equals"},
+		"service.action.networkConnectionAction.remoteIpDetails.organization.asn":        {"equals", "not_equals"},
+		"service.action.networkConnectionAction.remoteIpDetails.organization.asnOrg":     {"equals", "not_equals"},
+		"service.action.networkConnectionAction.remotePortDetails.port":                  {"equals", "not_equals"},
+		"service.additionalInfo.threatListName":                                          {"equals", "not_equals"},
+		"service.archived":                                                               {"equals", "not_equals"},
+		"service.resourceRole":                                                           {"equals", "not_equals"},
+		"severity":                                                                       {"equals", "not_equals"},
+		"type":                                                                           {"equals", "not_equals"},
+		"updatedAt":                                                                      {"equals", "not_equals", "greater_than", "greater_than_or_equal", "less_than", "less_than_or_equal"},
+	}
+}
+
+func conditionAllowedForCriterion(criterion map[string]interface{}) bool {
+	availableConditions := criteriaMap()[criterion["field"].(string)]
+	conditionToCheck := criterion["condition"].(string)
+
+	for _, availableCondition := range availableConditions {
+		if availableCondition == conditionToCheck {
+			return true
+		}
+	}
+	return false
+}
+
+func serializeFindingCriteria(findingCriteria map[string]interface{}) (*guardduty.FindingCriteria, error) {
+	inputFindingCriteria := findingCriteria["criterion"].(*schema.Set).List()
+	criteria := map[string]*guardduty.Condition{}
+	for _, criterion := range inputFindingCriteria {
+		typedCriterion := criterion.(map[string]interface{})
+
+		if !conditionAllowedForCriterion(typedCriterion) {
+			return nil, fmt.Errorf("The condition is not supported for the given field. Supported conditions are: %v", criteriaMap()[typedCriterion["field"].(string)])
+		}
+
+		switch typedCriterion["condition"].(string) {
+		case "equals":
+			criteria[typedCriterion["field"].(string)] = &guardduty.Condition{
+				Equals: aws.StringSlice(conditionValueToStrings(typedCriterion["values"].([]interface{}))),
+			}
+		case "greater_than":
+			criteria[typedCriterion["field"].(string)] = &guardduty.Condition{
+				GreaterThan: aws.Int64(conditionValueToInt(typedCriterion["values"].([]interface{})).(int64)),
+			}
+		case "greater_than_or_equals":
+			criteria[typedCriterion["field"].(string)] = &guardduty.Condition{
+				GreaterThanOrEqual: aws.Int64(conditionValueToInt(typedCriterion["values"].([]interface{})).(int64)),
+			}
+		case "less_than":
+			criteria[typedCriterion["field"].(string)] = &guardduty.Condition{
+				LessThan: aws.Int64(conditionValueToInt(typedCriterion["values"].([]interface{})).(int64)),
+			}
+		case "less_than_or_equals":
+			criteria[typedCriterion["field"].(string)] = &guardduty.Condition{
+				LessThanOrEqual: aws.Int64(conditionValueToInt(typedCriterion["values"].([]interface{})).(int64)),
+			}
+		case "not_equals":
+			criteria[typedCriterion["field"].(string)] = &guardduty.Condition{
+				NotEquals: aws.StringSlice(conditionValueToStrings(typedCriterion["values"].([]interface{}))),
+			}
+		}
+
+	}
+	log.Printf("[DEBUG] Creating FindingCriteria map: %#v", findingCriteria)
+
+	return &guardduty.FindingCriteria{Criterion: criteria}, nil
+}
+
+func conditionValueToStrings(untypedValues []interface{}) []string {
+	values := make([]string, len(untypedValues))
+	for i, v := range untypedValues {
+		values[i] = v.(string)
+	}
+	return values
+}
+
+func conditionValueToInt(untypedValues []interface{}) interface{} {
+	if len(untypedValues) != 1 {
+		return fmt.Errorf("Exactly one value must be given for conditions like less_ or greater_than. Instead given: %v", untypedValues)
+	}
+
+	untypedValue := untypedValues[0]
+	typedValue, err := strconv.ParseInt(untypedValue.(string), 10, 64)
+	if err != nil {
+		return fmt.Errorf("Parsing condition value failed: %s", err.Error())
+	}
+
+	return typedValue
 }
 
 func parseImportedId(importedId string) (string, string, error) {
