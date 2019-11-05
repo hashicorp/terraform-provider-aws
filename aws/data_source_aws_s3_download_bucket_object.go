316f87bf1e9adce4c832b1f9cd9fc3de3ab77df9
Julien Duchesne <jduchesne@coveo.com>
2019-11-05 09:36:27 -0500

    Move to plugin SDK

--- aws/data_source_aws_s3_download_bucket_object.go
+++ aws/data_source_aws_s3_download_bucket_object.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
 func dataSourceAwsS3DownloadBucketObject() *schema.Resource {
