9b6b18072d9debe6d540913d56b60567e8f2c10d
Vincent Chenal <vincent.chenal@protonmail.com>
2019-11-06 18:33:57 +0100

    [data_source_aws_iam_group_test] test group users retrieval

--- aws/data_source_aws_iam_group_test.go
+++ aws/data_source_aws_iam_group_test.go
@@ -10,34 +10,52 @@ import (
 )
 
 func TestAccAWSDataSourceIAMGroup_basic(t *testing.T) {
-	groupName := fmt.Sprintf("test-datasource-user-%d", acctest.RandInt())
+	groupName := fmt.Sprintf("test-datasource-group-%d", acctest.RandInt())
+	userName := fmt.Sprintf("test-datasource-user-%d", acctest.RandInt())
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccAwsIAMGroupConfig(groupName),
+				Config: testAccAwsIAMGroupConfig(groupName, userName),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrSet("data.aws_iam_group.test", "group_id"),
 					resource.TestCheckResourceAttr("data.aws_iam_group.test", "path", "/"),
 					resource.TestCheckResourceAttr("data.aws_iam_group.test", "group_name", groupName),
 					resource.TestMatchResourceAttr("data.aws_iam_group.test", "arn", regexp.MustCompile("^arn:aws:iam::[0-9]{12}:group/"+groupName)),
+					resource.TestCheckResourceAttr("data.aws_iam_group.test", "users.#", "1"),
 				),
 			},
 		},
 	})
 }
 
-func testAccAwsIAMGroupConfig(name string) string {
+func testAccAwsIAMGroupConfig(groupname string, username string) string {
 	return fmt.Sprintf(`
 resource "aws_iam_group" "group" {
   name = "%s"
   path = "/"
 }
 
-data "aws_iam_group" "test" {
-  group_name = "${aws_iam_group.group.name}"
+resource "aws_iam_user" "user" {
+	name = "%s"
 }
-`, name)
+
+resource "aws_iam_user_group_membership" "user_membership" {
+	user = "${aws_iam_user.user.name}"
+
+	groups = [
+		"${aws_iam_group.group.name}",
+	]
+}
+
+data "aws_iam_group" "test" {
+  /*
+  Getting the group_name from the aws_iam_user_group_membership
+  enforce an implicit dependency which is needed for the test
+  */
+  group_name = "${element(tolist(aws_iam_user_group_membership.user_membership.groups), 0)}"
+}
+`, groupname, username)
 }
