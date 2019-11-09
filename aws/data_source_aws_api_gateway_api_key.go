912830e4932ae58870745fdda887f7ec66a58533
DrFaust92 <ilia.lazebnik@gmail.com>
2019-11-09 16:46:55 +0200

    added attributes to api gateway data source

--- aws/data_source_aws_api_gateway_api_key.go
+++ aws/data_source_aws_api_gateway_api_key.go
@@ -1,9 +1,13 @@
 package aws
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/apigateway"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/terraform-providers/terraform-provider-aws/aws/internal/keyvaluetags"
 )
 
 func dataSourceAwsApiGatewayApiKey() *schema.Resource {
@@ -18,11 +22,28 @@ func dataSourceAwsApiGatewayApiKey() *schema.Resource {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+			"description": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"created_date": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"last_updated_date": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"enabled": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
 			"value": {
 				Type:      schema.TypeString,
 				Computed:  true,
 				Sensitive: true,
 			},
+			"tags": tagsSchemaComputed(),
 		},
 	}
 }
@@ -41,5 +62,13 @@ func dataSourceAwsApiGatewayApiKeyRead(d *schema.ResourceData, meta interface{})
 	d.SetId(aws.StringValue(apiKey.Id))
 	d.Set("name", apiKey.Name)
 	d.Set("value", apiKey.Value)
+	d.Set("created_date", aws.TimeValue(apiKey.CreatedDate).Format(time.RFC3339))
+	d.Set("description", apiKey.Description)
+	d.Set("enabled", apiKey.Enabled)
+	d.Set("last_updated_date", aws.TimeValue(apiKey.LastUpdatedDate).Format(time.RFC3339))
+
+	if err := d.Set("tags", keyvaluetags.ApigatewayKeyValueTags(apiKey.Tags).IgnoreAws().Map()); err != nil {
+		return fmt.Errorf("error setting tags: %s", err)
+	}
 	return nil
 }
