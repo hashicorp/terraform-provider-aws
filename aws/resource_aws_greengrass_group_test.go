1b7e6130f4c6731c13bfe485056408551c6111af
Oleksiy Veretiuk <oleksiy.veretiuk@quintagroup.org>
2019-11-13 10:36:15 +0200

    Update tests

--- aws/resource_aws_greengrass_group_test.go
+++ aws/resource_aws_greengrass_group_test.go
@@ -95,12 +95,106 @@ resource "aws_greengrass_group" "test" {
 
 func testAccAWSGreengrassGroupConfig_groupVersion(rString string) string {
 	return fmt.Sprintf(`
+resource "aws_greengrass_connector_definition" "test" {
+	name = "connector_definition_%[1]s"
+	connector_definition_version {
+		connector {
+			connector_arn = "arn:aws:greengrass:eu-west-1::/connectors/RaspberryPiGPIO/versions/5"
+			id = "connector_id"
+			parameters = {
+				"key" = "value",
+			}
+		}
+	}
+}
+
+resource "aws_greengrass_logger_definition" "test" {
+	name = "logger_definition_%[1]s"
+	logger_definition_version {
+		logger {
+			component = "GreengrassSystem"
+			id = "test_id"
+			type = "FileSystem"
+			level = "DEBUG"
+			space = 3	
+		}
+	}
+}
+
+resource "aws_iot_thing" "test" {
+	name = "%[1]s"
+}
+resource "aws_iot_certificate" "foo_cert" {
+	csr = "${file("test-fixtures/iot-csr.pem")}"
+	active = true
+}
+resource "aws_greengrass_core_definition" "test" {
+	name = "core_definition_%[1]s"
+	core_definition_version {
+		core {
+			certificate_arn = "${aws_iot_certificate.foo_cert.arn}"
+			id = "core_id"
+			sync_shadow = false
+			thing_arn = "${aws_iot_thing.test.arn}"
+		}
+	}
+}
+
+resource "aws_greengrass_device_definition" "test" {
+	name = "device_definition_%[1]s"
+	device_definition_version {
+		device {
+			certificate_arn = "${aws_iot_certificate.foo_cert.arn}"
+			id = "device_id"
+			sync_shadow = false
+			thing_arn = "${aws_iot_thing.test.arn}"
+		}
+	}
+}
+
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_greengrass_function_definition" "test" {
+	name = "function_definition_%[1]s"
+	function_definition_version {
+		default_config {
+			isolation_mode = "GreengrassContainer"
+			run_as {
+				gid = 1
+				uid = 1
+			}
+		}
+		function {
+			function_arn = "arn:aws:lambda:us-west-2:${data.aws_caller_identity.current.account_id}:function:test_lambda_wv8l0glb:test"
+			id = "test_id"
+			function {}
+		}
+	}
+}
+
+resource "aws_greengrass_subscription_definition" "test" {
+	name = "subscription_definition_%[1]s"
+	subscription_definition_version {
+		subscription {
+			id = "test_id"
+			subject = "test_subject"
+			source = "arn:aws:iot:eu-west-1:111111111111:thing/Source"
+			target = "arn:aws:iot:eu-west-1:222222222222:thing/Target"	
+		}
+	}
+}
+
 resource "aws_greengrass_group" "test" {
-  name = "group_%s"
+  name = "group_%[1]s"
 
   group_version {
-	amzn_client_token = "token"
-	connector_definition_version_arn = "arn:aws:greengrass:eu-west-1:123456789012:connector-definition-version:test_cd"
+	core_definition_version_arn = "${aws_greengrass_core_definition.test.latest_definition_version_arn}" 
+	connector_definition_version_arn = "${aws_greengrass_connector_definition.test.latest_definition_version_arn}"
+	function_definition_version_arn = "${aws_greengrass_function_definition.test.latest_definition_version_arn}"
+	subscription_definition_version_arn = "${aws_greengrass_subscription_definition.test.latest_definition_version_arn}"
+	logger_definition_version_arn = "${aws_greengrass_logger_definition.test.latest_definition_version_arn}"
+	device_definition_version_arn = "${aws_greengrass_device_definition.test.latest_definition_version_arn}"
   }
 }
 `, rString)
