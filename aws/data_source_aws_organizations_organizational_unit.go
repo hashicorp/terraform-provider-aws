da7b03e2cb534c98c34d6737835c79843347068d
David Fluck <david@davidfluck.com>
2019-11-20 22:59:39 -0500

    Import correct module.

--- aws/data_source_aws_organizations_organizational_unit.go
+++ aws/data_source_aws_organizations_organizational_unit.go
@@ -5,8 +5,8 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/organizations"
-	"github.com/hashicorp/terraform/helper/resource"
-	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
 func dataSourceAwsOrganizationsOrganizationalUnit() *schema.Resource {
