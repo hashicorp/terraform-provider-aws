4b026efa3e6781115a413816873c5c53c909ae65
Oliver Grimm <ogrimm@babbel.com>
2019-11-22 14:09:03 +0100

    Adjust imports

--- aws/resource_aws_guardduty_filter.go
+++ aws/resource_aws_guardduty_filter.go
@@ -9,8 +9,8 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/guardduty"
-	"github.com/hashicorp/terraform/helper/schema"
-	"github.com/hashicorp/terraform/helper/validation"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 )
 
 func resourceAwsGuardDutyFilter() *schema.Resource {
