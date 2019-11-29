4c449c8a76361411fafd62e2174300bfb53eb74a
Oleksiy Veretiuk <oleksiy.veretiuk@quintagroup.org>
2019-11-29 12:08:40 +0200

    Add amzn_client_token to schema as sensitive field

--- aws/resource_aws_greengrass_core_definition.go
+++ aws/resource_aws_greengrass_core_definition.go
@@ -3,7 +3,6 @@ package aws
 import (
 	"fmt"
 	"log"
-	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/greengrass"
@@ -30,6 +29,11 @@ func resourceAwsGreengrassCoreDefinition() *schema.Resource {
 			"arn": {
 				Type:     schema.TypeString,
 				Computed: true,
+			},
+			"amzn_client_token": {
+				Type:      schema.TypeString,
+				Optional:  true,
+				Sensitive: true,
 			},
 			"tags": tagsSchema(),
 			"latest_definition_version_arn": {
@@ -86,7 +90,7 @@ func createCoreDefinitionVersion(d *schema.ResourceData, conn *greengrass.Greeng
 		CoreDefinitionId: aws.String(d.Id()),
 	}
 
-	if v := os.Getenv("AMZN_CLIENT_TOKEN"); v != "" {
+	if v := d.Get("amzn_client_token").(string); v != "" {
 		params.AmznClientToken = aws.String(v)
 	}
 
