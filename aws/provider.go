c5efdda24b5f8874a7c390247383f7ce7177ffed
Brian Flad <bflad417@gmail.com>
2019-12-03 18:31:05 -0800

    New Resource: aws_lambda_provisioned_concurrency_config (#11129)
    Output from acceptance testing:
    
    ```
    --- PASS: TestAccAWSLambdaProvisionedConcurrencyConfig_disappears_LambdaProvisionedConcurrencyConfig (240.38s)
    --- PASS: TestAccAWSLambdaProvisionedConcurrencyConfig_Qualifier_AliasName (246.42s)
    --- PASS: TestAccAWSLambdaProvisionedConcurrencyConfig_basic (253.46s)
    --- PASS: TestAccAWSLambdaProvisionedConcurrencyConfig_disappears_LambdaFunction (259.60s)
    --- PASS: TestAccAWSLambdaProvisionedConcurrencyConfig_ProvisionedConcurrentExecutions (380.90s)
    ```

--- aws/provider.go
+++ aws/provider.go
@@ -598,6 +598,7 @@ func Provider() terraform.ResourceProvider {
 			"aws_lambda_event_source_mapping":                         resourceAwsLambdaEventSourceMapping(),
 			"aws_lambda_alias":                                        resourceAwsLambdaAlias(),
 			"aws_lambda_permission":                                   resourceAwsLambdaPermission(),
+			"aws_lambda_provisioned_concurrency_config":               resourceAwsLambdaProvisionedConcurrencyConfig(),
 			"aws_lambda_layer_version":                                resourceAwsLambdaLayerVersion(),
 			"aws_launch_configuration":                                resourceAwsLaunchConfiguration(),
 			"aws_launch_template":                                     resourceAwsLaunchTemplate(),
