144b987a34c2bdd7011687546f322851b0ad79f8
Brian Flad <bflad417@gmail.com>
2019-12-19 21:47:02 -0500

    deps: Update github.com/aws/aws-sdk-go/service/wafv2 vendor directory to match current dependency
    Updated via `go mod vendor`

--- vendor/github.com/aws/aws-sdk-go/service/wafv2/service.go
+++ vendor/github.com/aws/aws-sdk-go/service/wafv2/service.go
@@ -31,7 +31,7 @@ var initRequest func(*request.Request)
 const (
 	ServiceName = "WAFV2" // Name of service.
 	EndpointsID = "wafv2" // ID to lookup a service endpoint with.
-	ServiceID   = "WAFV2" // ServiceID is a unique identifer of a specific service.
+	ServiceID   = "WAFV2" // ServiceID is a unique identifier of a specific service.
 )
 
 // New creates a new instance of the WAFV2 client with a session.
