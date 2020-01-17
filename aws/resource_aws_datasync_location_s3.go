b73646ccdc10eaf5b53e3af5488390124b86e6e4
Kit Ewbank <Christopher.Ewbank@capitalone.com>
2020-01-17 01:09:59 -0500

    service/datasync: Refactor to use keyvaluetags package (#11626)
    Output from acceptance testing:
    
    ```
    --- PASS: TestAccAWSDataSyncAgent_AgentName (158.49s)
    --- PASS: TestAccAWSDataSyncAgent_basic (137.86s)
    --- PASS: TestAccAWSDataSyncAgent_disappears (163.37s)
    --- PASS: TestAccAWSDataSyncAgent_Tags (187.19s)
    
    --- PASS: TestAccAWSDataSyncLocationEfs_basic (114.51s)
    --- PASS: TestAccAWSDataSyncLocationEfs_disappears (139.85s)
    --- PASS: TestAccAWSDataSyncLocationEfs_Subdirectory (158.66s)
    --- PASS: TestAccAWSDataSyncLocationEfs_Tags (172.35s)
    
    --- PASS: TestAccAWSDataSyncLocationNfs_AgentARNs_Multple (183.23s)
    --- PASS: TestAccAWSDataSyncLocationNfs_basic (174.64s)
    --- PASS: TestAccAWSDataSyncLocationNfs_disappears (176.43s)
    --- PASS: TestAccAWSDataSyncLocationNfs_Subdirectory (184.84s)
    --- PASS: TestAccAWSDataSyncLocationNfs_Tags (178.33s)
    
    --- PASS: TestAccAWSDataSyncLocationS3_basic (28.03s)
    --- PASS: TestAccAWSDataSyncLocationS3_disappears (27.83s)
    --- PASS: TestAccAWSDataSyncLocationS3_Tags (40.93s)
    
    --- PASS: TestAccAWSDataSyncTask_basic (477.22s)
    --- PASS: TestAccAWSDataSyncTask_CloudWatchLogGroupARN (449.73s)
    --- PASS: TestAccAWSDataSyncTask_DefaultSyncOptions_AtimeMtime (475.48s)
    --- PASS: TestAccAWSDataSyncTask_DefaultSyncOptions_BytesPerSecond (443.54s)
    --- PASS: TestAccAWSDataSyncTask_DefaultSyncOptions_Gid (445.18s)
    --- PASS: TestAccAWSDataSyncTask_DefaultSyncOptions_PosixPermissions (416.73s)
    --- PASS: TestAccAWSDataSyncTask_DefaultSyncOptions_PreserveDeletedFiles (415.94s)
    --- PASS: TestAccAWSDataSyncTask_DefaultSyncOptions_PreserveDevices (440.38s)
    --- PASS: TestAccAWSDataSyncTask_DefaultSyncOptions_Uid (500.35s)
    --- PASS: TestAccAWSDataSyncTask_DefaultSyncOptions_VerifyMode (447.15s)
    --- PASS: TestAccAWSDataSyncTask_disappears (263.23s)
    ```

--- aws/resource_aws_datasync_location_s3.go
+++ aws/resource_aws_datasync_location_s3.go
@@ -11,6 +11,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+	"github.com/terraform-providers/terraform-provider-aws/aws/internal/keyvaluetags"
 )
 
 func resourceAwsDataSyncLocationS3() *schema.Resource {
@@ -65,11 +66,7 @@ func resourceAwsDataSyncLocationS3() *schema.Resource {
 					return false
 				},
 			},
-			"tags": {
-				Type:     schema.TypeMap,
-				Optional: true,
-				Elem:     &schema.Schema{Type: schema.TypeString},
-			},
+			"tags": tagsSchema(),
 			"uri": {
 				Type:     schema.TypeString,
 				Computed: true,
@@ -85,7 +82,7 @@ func resourceAwsDataSyncLocationS3Create(d *schema.ResourceData, meta interface{
 		S3BucketArn:  aws.String(d.Get("s3_bucket_arn").(string)),
 		S3Config:     expandDataSyncS3Config(d.Get("s3_config").([]interface{})),
 		Subdirectory: aws.String(d.Get("subdirectory").(string)),
-		Tags:         expandDataSyncTagListEntry(d.Get("tags").(map[string]interface{})),
+		Tags:         keyvaluetags.New(d.Get("tags").(map[string]interface{})).IgnoreAws().DatasyncTags(),
 	}
 
 	log.Printf("[DEBUG] Creating DataSync Location S3: %s", input)
@@ -147,17 +144,6 @@ func resourceAwsDataSyncLocationS3Read(d *schema.ResourceData, meta interface{})
 		return fmt.Errorf("error reading DataSync Location S3 (%s): %s", d.Id(), err)
 	}
 
-	tagsInput := &datasync.ListTagsForResourceInput{
-		ResourceArn: output.LocationArn,
-	}
-
-	log.Printf("[DEBUG] Reading DataSync Location S3 tags: %s", tagsInput)
-	tagsOutput, err := conn.ListTagsForResource(tagsInput)
-
-	if err != nil {
-		return fmt.Errorf("error reading DataSync Location S3 (%s) tags: %s", d.Id(), err)
-	}
-
 	subdirectory, err := dataSyncParseLocationURI(aws.StringValue(output.LocationUri))
 
 	if err != nil {
@@ -171,12 +157,17 @@ func resourceAwsDataSyncLocationS3Read(d *schema.ResourceData, meta interface{})
 	}
 
 	d.Set("subdirectory", subdirectory)
+	d.Set("uri", output.LocationUri)
 
-	if err := d.Set("tags", flattenDataSyncTagListEntry(tagsOutput.Tags)); err != nil {
-		return fmt.Errorf("error setting tags: %s", err)
+	tags, err := keyvaluetags.DatasyncListTags(conn, d.Id())
+
+	if err != nil {
+		return fmt.Errorf("error listing tags for DataSync Location S3 (%s): %s", d.Id(), err)
 	}
 
-	d.Set("uri", output.LocationUri)
+	if err := d.Set("tags", tags.IgnoreAws().Map()); err != nil {
+		return fmt.Errorf("error setting tags: %s", err)
+	}
 
 	return nil
 }
@@ -185,31 +176,10 @@ func resourceAwsDataSyncLocationS3Update(d *schema.ResourceData, meta interface{
 	conn := meta.(*AWSClient).datasyncconn
 
 	if d.HasChange("tags") {
-		oldRaw, newRaw := d.GetChange("tags")
-		createTags, removeTags := dataSyncTagsDiff(expandDataSyncTagListEntry(oldRaw.(map[string]interface{})), expandDataSyncTagListEntry(newRaw.(map[string]interface{})))
+		o, n := d.GetChange("tags")
 
-		if len(removeTags) > 0 {
-			input := &datasync.UntagResourceInput{
-				Keys:        dataSyncTagsKeys(removeTags),
-				ResourceArn: aws.String(d.Id()),
-			}
-
-			log.Printf("[DEBUG] Untagging DataSync Location S3: %s", input)
-			if _, err := conn.UntagResource(input); err != nil {
-				return fmt.Errorf("error untagging DataSync Location S3 (%s): %s", d.Id(), err)
-			}
-		}
-
-		if len(createTags) > 0 {
-			input := &datasync.TagResourceInput{
-				ResourceArn: aws.String(d.Id()),
-				Tags:        createTags,
-			}
-
-			log.Printf("[DEBUG] Tagging DataSync Location S3: %s", input)
-			if _, err := conn.TagResource(input); err != nil {
-				return fmt.Errorf("error tagging DataSync Location S3 (%s): %s", d.Id(), err)
-			}
+		if err := keyvaluetags.DatasyncUpdateTags(conn, d.Id(), o, n); err != nil {
+			return fmt.Errorf("error updating DataSync Location S3 (%s) tags: %s", d.Id(), err)
 		}
 	}
 
