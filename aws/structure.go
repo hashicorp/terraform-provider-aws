e64113ee05d167b57a8d374943919194a2cc1c46
Kit Ewbank <Kit_Ewbank@hotmail.com>
2020-01-18 18:36:12 -0500

    r/aws_appmesh_route: Support gRPC and HTTP/2 services.

--- aws/structure.go
+++ aws/structure.go
@@ -5618,6 +5618,15 @@ func expandAppmeshHttpRoute(vHttpRoute []interface{}) *appmesh.HttpRoute {
 					if vSuffix, ok := mMatch["suffix"].(string); ok && vSuffix != "" {
 						httpRouteHeader.Match.Suffix = aws.String(vSuffix)
 					}
+					if vRegex, ok := mMatch["regex"].(string); ok && vRegex != "" {
+						httpRouteHeader.Match.Regex = aws.String(vRegex)
+					}
+					if vSuffix, ok := mMatch["suffix"].(string); ok && vSuffix != "" {
+						httpRouteHeader.Match.Suffix = aws.String(vSuffix)
+					}
+
+					if vRange, ok := mMatch["range"].([]interface{}); ok && len(vRange) > 0 && vRange[0] != nil {
+						httpRouteHeader.Match.Range = &appmesh.MatchRange{}
 
 					if vRange, ok := mMatch["range"].([]interface{}); ok && len(vRange) > 0 && vRange[0] != nil {
 						httpRouteHeader.Match.Range = &appmesh.MatchRange{}
