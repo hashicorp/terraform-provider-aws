8ec2c1077a2ea9802218f037c354cbeaa30d4b0c
Ilia Lazebnik <Ilia.lazebnik@gmail.com>
2020-01-18 07:23:15 +0200

    resource/aws_key_pair: Add tags argument and key_pair_id attribute (#11481)
    Output from acceptance testing:
    
    ```
    --- PASS: TestAccAWSKeyPair_basic (8.22s)
    --- PASS: TestAccAWSKeyPair_tags (21.40s)
    --- PASS: TestAccAWSKeyPair_generatedName (7.79s)
    --- PASS: TestAccAWSKeyPair_namePrefix (7.74s)
    --- PASS: TestAccAWSKeyPair_disappears (5.99s)
    ```

--- aws/resource_aws_key_pair.go
+++ aws/resource_aws_key_pair.go
@@ -7,6 +7,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+	"github.com/terraform-providers/terraform-provider-aws/aws/internal/keyvaluetags"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
@@ -17,7 +18,7 @@ func resourceAwsKeyPair() *schema.Resource {
 	return &schema.Resource{
 		Create: resourceAwsKeyPairCreate,
 		Read:   resourceAwsKeyPairRead,
-		Update: nil,
+		Update: resourceAwsKeyPairUpdate,
 		Delete: resourceAwsKeyPairDelete,
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
@@ -59,6 +60,11 @@ func resourceAwsKeyPair() *schema.Resource {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+			"key_pair_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"tags": tagsSchema(),
 		},
 	}
 }
@@ -88,6 +94,30 @@ func resourceAwsKeyPairCreate(d *schema.ResourceData, meta interface{}) error {
 	}
 
 	d.SetId(*resp.KeyName)
+
+	if v := d.Get("tags").(map[string]interface{}); len(v) > 0 {
+		readReq := &ec2.DescribeKeyPairsInput{
+			KeyNames: []*string{aws.String(d.Id())},
+		}
+		readResp, err := conn.DescribeKeyPairs(readReq)
+		if err != nil {
+			awsErr, ok := err.(awserr.Error)
+			if ok && awsErr.Code() == "InvalidKeyPair.NotFound" {
+				d.SetId("")
+				return nil
+			}
+			return fmt.Errorf("Error retrieving KeyPair: %s", err)
+		}
+
+		for _, keyPair := range readResp.KeyPairs {
+			if *keyPair.KeyName == d.Id() {
+				if err := keyvaluetags.Ec2UpdateTags(conn, aws.StringValue(keyPair.KeyPairId), nil, v); err != nil {
+					return fmt.Errorf("error adding tags: %s", err)
+				}
+			}
+		}
+
+	}
 	return resourceAwsKeyPairRead(d, meta)
 }
 
@@ -110,11 +140,28 @@ func resourceAwsKeyPairRead(d *schema.ResourceData, meta interface{}) error {
 		if *keyPair.KeyName == d.Id() {
 			d.Set("key_name", keyPair.KeyName)
 			d.Set("fingerprint", keyPair.KeyFingerprint)
+			d.Set("key_pair_id", keyPair.KeyPairId)
+			if err := d.Set("tags", keyvaluetags.Ec2KeyValueTags(keyPair.Tags).IgnoreAws().Map()); err != nil {
+				return fmt.Errorf("error setting tags: %s", err)
+			}
 			return nil
 		}
 	}
 
 	return fmt.Errorf("Unable to find key pair within: %#v", resp.KeyPairs)
+}
+
+func resourceAwsKeyPairUpdate(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*AWSClient).ec2conn
+
+	if d.HasChange("tags") {
+		o, n := d.GetChange("tags")
+		if err := keyvaluetags.Ec2UpdateTags(conn, d.Get("key_pair_id").(string), o, n); err != nil {
+			return fmt.Errorf("error adding tags: %s", err)
+		}
+	}
+
+	return resourceAwsKeyPairRead(d, meta)
 }
 
 func resourceAwsKeyPairDelete(d *schema.ResourceData, meta interface{}) error {
