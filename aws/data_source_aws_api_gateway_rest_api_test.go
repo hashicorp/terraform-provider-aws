e5a2ce4b60c3044202fdad29b3ca74ba1855f494
DrFaust92 <ilia.lazebnik@gmail.com>
2020-02-04 16:15:48 +0200

    rebase + add more attributes

--- aws/data_source_aws_api_gateway_rest_api_test.go
+++ aws/data_source_aws_api_gateway_rest_api_test.go
@@ -22,46 +22,20 @@ func TestAccDataSourceAwsApiGatewayRestApi(t *testing.T) {
 					resource.TestCheckResourceAttrPair(dataSourceName, "name", resourceName, "name"),
 					resource.TestCheckResourceAttrPair(dataSourceName, "arn", resourceName, "arn"),
 					resource.TestCheckResourceAttrPair(dataSourceName, "root_resource_id", resourceName, "root_resource_id"),
-					resource.TestCheckResourceAttr(dataSourceName, "tags.%", "0"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "tags", resourceName, "tags"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "version", resourceName, "version"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "description", resourceName, "description"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "policy", resourceName, "policy"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "api_key_source", resourceName, "api_key_source"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "minimum_compression_size", resourceName, "minimum_compression_size"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "binary_media_types", resourceName, "binary_media_types"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "endpoint_configuration", resourceName, "endpoint_configuration"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "execution_arn", resourceName, "execution_arn"),
 				),
 			},
 		},
 	})
 }
-
-//func testAccDataSourceAwsApiGatewayRestApiCheck(name string) resource.TestCheckFunc {
-//	return func(s *terraform.State) error {
-//		resources, ok := s.RootModule().Resources[name]
-//		if !ok {
-//			return fmt.Errorf("root module has no resource called %s", name)
-//		}
-//
-//		apiGatewayRestApiResources, ok := s.RootModule().Resources["aws_api_gateway_rest_api.tf_test"]
-//		if !ok {
-//			return fmt.Errorf("can't find aws_api_gateway_rest_api.tf_test in state")
-//		}
-//
-//		attr := resources.Primary.Attributes
-//
-//		if attr["name"] != apiGatewayRestApiResources.Primary.Attributes["name"] {
-//			return fmt.Errorf(
-//				"name is %s; want %s",
-//				attr["name"],
-//				apiGatewayRestApiResources.Primary.Attributes["name"],
-//			)
-//		}
-//
-//		if attr["root_resource_id"] != apiGatewayRestApiResources.Primary.Attributes["root_resource_id"] {
-//			return fmt.Errorf(
-//				"root_resource_id is %s; want %s",
-//				attr["root_resource_id"],
-//				apiGatewayRestApiResources.Primary.Attributes["root_resource_id"],
-//			)
-//		}
-//
-//		return nil
-//	}
-//}
 
 func testAccDataSourceAwsApiGatewayRestApiConfig(r string) string {
 	return fmt.Sprintf(`
