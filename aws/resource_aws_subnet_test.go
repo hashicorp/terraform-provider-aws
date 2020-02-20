84e8e39e6e9c9f87fa3992718704d27e3ee1222b
John Barney <barneyjo@amazon.com>
2020-02-21 01:04:41 +0800

    Add Outpost acceptance test that is skipped if no Outpost ARN

--- aws/resource_aws_subnet_test.go
+++ aws/resource_aws_subnet_test.go
@@ -3,6 +3,7 @@ package aws
 import (
 	"fmt"
 	"log"
+	"os"
 	"regexp"
 	"testing"
 	"time"
@@ -312,6 +313,42 @@ func TestAccAWSSubnet_availabilityZoneId(t *testing.T) {
 	})
 }
 
+func TestAccAWSSubnet_outpost(t *testing.T) {
+	var v ec2.Subnet
+	resourceName := "aws_subnet.test"
+
+	outpostArn := os.Getenv("AWS_OUTPOST_ARN")
+	if outpostArn == "" {
+		t.Skip(
+			"Environment variable AWS_OUTPOST_ARN is not set. " +
+				"This environment variable must be set to the ARN of " +
+				"a deployed Outpost to enable this test.")
+	}
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:      func() { testAccPreCheck(t) },
+		IDRefreshName: resourceName,
+		Providers:     testAccProviders,
+		CheckDestroy:  testAccCheckSubnetDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSubnetConfigOutpost(outpostArn),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckSubnetExists(
+						resourceName, &v),
+					resource.TestCheckResourceAttr(
+						resourceName, "outpost_arn", outpostArn),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
 func testAccCheckAwsSubnetIpv6BeforeUpdate(t *testing.T, subnet *ec2.Subnet) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		if subnet.Ipv6CidrBlockAssociationSet == nil {
@@ -532,3 +569,23 @@ resource "aws_subnet" "test" {
   }
 }
 `
+
+func testAccSubnetConfigOutpost(outpostArn string) string {
+	return fmt.Sprintf(`
+resource "aws_vpc" "test" {
+  cidr_block = "10.1.0.0/16"
+  tags = {
+    Name = "terraform-testacc-subnet-outpost"
+  }
+}
+
+resource "aws_subnet" "test" {
+  cidr_block = "10.1.1.0/24"
+  vpc_id = "${aws_vpc.test.id}"
+  outpost_arn = "%s"
+  tags = {
+    Name = "tf-acc-subnet-outpost"
+  }
+}
+`, outpostArn)
+}
