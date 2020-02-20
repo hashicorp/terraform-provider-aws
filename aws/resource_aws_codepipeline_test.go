0d78dfa46fd77538c16da68fe4d2ed4fa9c83838
Alan Jenkins <alan.jenkins@uk.clara.net>
2020-02-20 17:51:16 +0000

    [WIP] Test for multi region codepipeline.

--- aws/resource_aws_codepipeline_test.go
+++ aws/resource_aws_codepipeline_test.go
@@ -182,6 +182,34 @@ func TestAccAWSCodePipeline_tags(t *testing.T) {
 	})
 }
 
+func TestAccAWSCodePipeline_basic_multiregion(t *testing.T) {
+	if os.Getenv("GITHUB_TOKEN") == "" {
+		t.Skip("Environment variable GITHUB_TOKEN is not set")
+	}
+
+	name := acctest.RandString(10)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAWSCodePipelineDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAWSCodePipelineConfig_multiregion(name),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAWSCodePipelineExists("aws_codepipeline.bar"),
+					resource.TestCheckResourceAttr("aws_codepipeline.bar", "stage.2.name", "Build"),
+					resource.TestCheckResourceAttr("aws_codepipeline.bar", "stage.2.action.0.region", "eu-west-1"),
+					resource.TestCheckResourceAttr("aws_codepipeline.bar", "stage.2.action.1.region", "eu-central-1"),
+					resource.TestMatchResourceAttr(
+						"aws_codepipeline.bar", "stage.2.action.0.role_arn",
+						regexp.MustCompile("^arn:aws:iam::[0-9]{12}:role/codepipeline-action-role.*")),
+				),
+			},
+		},
+	})
+}
+
 func testAccCheckAWSCodePipelineExists(n string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		rs, ok := s.RootModule().Resources[n]
@@ -875,4 +903,148 @@ resource "aws_codepipeline" "test" {
   }
 }
 `, rName, tag1, tag2)
+}
+
+func testAccAWSCodePipelineConfig_multiregion(rName string) string {
+	return fmt.Sprintf(`
+resource "aws_s3_bucket" "eu-west-1" {
+  bucket = "tf-test-pipeline-eu-west-1-%s"
+  acl    = "private"
+}
+
+resource "aws_s3_bucket" "eu-central-1" {
+  bucket = "tf-test-pipeline-eu-central-1-%s"
+  acl    = "private"
+}
+
+resource "aws_iam_role" "codepipeline_role" {
+  name = "codepipeline-role-%s"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codepipeline.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "codepipeline_policy" {
+  name = "codepipeline_policy"
+  role = "${aws_iam_role.codepipeline_role.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect":"Allow",
+      "Action": [
+        "s3:GetObject",
+        "s3:GetObjectVersion",
+        "s3:GetBucketVersioning"
+      ],
+      "Resource": [
+        "${aws_s3_bucket.foo.arn}",
+        "${aws_s3_bucket.foo.arn}/*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "codebuild:BatchGetBuilds",
+        "codebuild:StartBuild"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_codepipeline" "bar" {
+  name     = "test-pipeline-%s"
+  role_arn = "${aws_iam_role.codepipeline_role.arn}"
+
+  artifact_stores {
+	  eu-west-1 = {
+			location = "${aws_s3_bucket.eu-west-1.bucket}"
+			type     = "S3"
+
+			encryption_key {
+				id   = "1234"
+				type = "KMS"
+			}
+		}
+
+		eu-central-1 = {
+			location = "${aws_s3_bucket.eu-central-1.bucket}"
+			type     = "S3"
+
+			encryption_key {
+				id   = "1234"
+				type = "KMS"
+			}
+		}
+	}
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "ThirdParty"
+      provider         = "GitHub"
+      version          = "1"
+      output_artifacts = ["test"]
+
+      configuration = {
+        Owner  = "lifesum-terraform"
+        Repo   = "test"
+        Branch = "master"
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+
+    action {
+		  region          = "eu-west-1"
+      name            = "EuWest1Build"
+      category        = "Build"
+      owner           = "AWS"
+      provider        = "CodeBuild"
+      input_artifacts = ["test"]
+      version         = "1"
+
+      configuration = {
+        ProjectName = "EuWest1Test"
+      }
+    }
+
+    action {
+		  region          = "eu-central-1"
+      name            = "EuCentral1Build"
+      category        = "Build"
+      owner           = "AWS"
+      provider        = "CodeBuild"
+      input_artifacts = ["test"]
+      version         = "1"
+
+      configuration = {
+        ProjectName = "EuCentral1Test"
+      }
+    }
+  }
+}
+`, rName, rName, rName, rName)
 }
