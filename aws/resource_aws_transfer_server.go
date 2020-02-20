89e3de2aa0988978ecf4048c2a52434ad824d34c
HIRAMATSU Kentaro <hiramatsu_kentaro@cyberagent.co.jp>
2020-02-21 06:52:40 +0900

    resource/aws_transfer_server: Add host_key argument and host_key_fingerprint attribute (#8913)
    Output from acceptance testing:
    
    ```
    --- PASS: TestAccAWSTransferServer_disappears (8.15s)
    --- PASS: TestAccAWSTransferServer_hostKey (9.79s)
    --- PASS: TestAccAWSTransferServer_apigateway (12.53s)
    --- PASS: TestAccAWSTransferServer_forcedestroy (13.37s)
    --- PASS: TestAccAWSTransferServer_basic (17.71s)
    --- PASS: TestAccAWSTransferServer_vpcEndpointId (74.66s)
    ```

--- aws/resource_aws_transfer_server.go
+++ aws/resource_aws_transfer_server.go
@@ -69,6 +69,16 @@ func resourceAwsTransferServer() *schema.Resource {
 				},
 			},
 
+			"host_key": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+
+			"host_key_fingerprint": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
 			"invocation_role": {
 				Type:         schema.TypeString,
 				Optional:     true,
@@ -146,6 +156,10 @@ func resourceAwsTransferServerCreate(d *schema.ResourceData, meta interface{}) e
 		createOpts.EndpointDetails = expandTransferServerEndpointDetails(attr.([]interface{}))
 	}
 
+	if attr, ok := d.GetOk("host_key"); ok {
+		createOpts.HostKey = aws.String(attr.(string))
+	}
+
 	log.Printf("[DEBUG] Create Transfer Server Option: %#v", createOpts)
 
 	resp, err := conn.CreateServer(createOpts)
@@ -191,6 +205,7 @@ func resourceAwsTransferServerRead(d *schema.ResourceData, meta interface{}) err
 	d.Set("endpoint_details", flattenTransferServerEndpointDetails(resp.Server.EndpointDetails))
 	d.Set("identity_provider_type", resp.Server.IdentityProviderType)
 	d.Set("logging_role", resp.Server.LoggingRole)
+	d.Set("host_key_fingerprint", resp.Server.HostKeyFingerprint)
 
 	if err := d.Set("tags", keyvaluetags.TransferKeyValueTags(resp.Server.Tags).IgnoreAws().Map()); err != nil {
 		return fmt.Errorf("Error setting tags: %s", err)
@@ -234,6 +249,13 @@ func resourceAwsTransferServerUpdate(d *schema.ResourceData, meta interface{}) e
 		updateFlag = true
 		if attr, ok := d.GetOk("endpoint_details"); ok {
 			updateOpts.EndpointDetails = expandTransferServerEndpointDetails(attr.([]interface{}))
+		}
+	}
+
+	if d.HasChange("host_key") {
+		updateFlag = true
+		if attr, ok := d.GetOk("host_key"); ok {
+			updateOpts.HostKey = aws.String(attr.(string))
 		}
 	}
 
