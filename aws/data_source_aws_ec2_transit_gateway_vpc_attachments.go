231fa0394843e517a087240f7f5cb628aaf0a89d
Marc Duthoit <escasgorille@aol.com>
2020-03-12 09:54:45 +0100

    feat(add-data-ec2-transit-gateway-vpc-attatchments): be able to get a list of all vpnc attachments matching criterias and not only one

---
+++ aws/data_source_aws_ec2_transit_gateway_vpc_attachments.go
@@ -0,0 +1,115 @@
+package aws
+
+import (
+	"errors"
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/terraform-providers/terraform-provider-aws/aws/internal/keyvaluetags"
+)
+
+func dataSourceAwsEc2TransitGatewayVpcAttachments() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceAwsEc2TransitGatewayVpcAttachmentsRead,
+
+		Schema: map[string]*schema.Schema{
+			"filter": dataSourceFiltersSchema(),
+			"transit_gateway_vpc_attachments": {
+				Type:  schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"dns_support": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"id": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"ipv6_support": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"subnet_ids": {
+							Type:     schema.TypeSet,
+							Computed: true,
+							Elem:     &schema.Schema{Type: schema.TypeString},
+						},
+						"transit_gateway_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"tags": tagsSchemaComputed(),
+						"vpc_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"vpc_owner_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+				        },
+				},
+			},
+		},
+	}
+}
+
+func dataSourceAwsEc2TransitGatewayVpcAttachmentsRead(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*AWSClient).ec2conn
+
+	input := &ec2.DescribeTransitGatewayVpcAttachmentsInput{}
+	items := []
+
+	if v, ok := d.GetOk("filter"); ok {
+		input.Filters = buildAwsDataSourceFilters(v.(*schema.Set))
+	}
+
+	if v, ok := d.GetOk("id"); ok {
+		input.TransitGatewayAttachmentIds = []*string{aws.String(v.(string))}
+	}
+
+	log.Printf("[DEBUG] Reading EC2 Transit Gateways: %s", input)
+	output, err := conn.DescribeTransitGatewayVpcAttachments(input)
+
+	if err != nil {
+		return fmt.Errorf("error reading EC2 Transit Gateway VPC Attachments: %s", err)
+	}
+
+	if output == nil || len(output.TransitGatewayVpcAttachments) == 0 {
+		return errors.New("error reading EC2 Transit Gateway VPC Attachments: no results found")
+	}
+
+	for index, value := range output.TransitGatewayVpcAttachments {
+		transitGatewayVpcAttachment := output.TransitGatewayVpcAttachments[index]
+		if transitGatewayVpcAttachment != nil {
+			var item *schema.ResourceData
+			if transitGatewayVpcAttachment.Options == nil {
+				return fmt.Errorf("error reading EC2 Transit Gateway VPC Attachment (%s): missing options", d.Id())
+			}
+
+			item.Set("dns_support", transitGatewayVpcAttachment.Options.DnsSupport)
+			item.Set("ipv6_support", transitGatewayVpcAttachment.Options.Ipv6Support)
+
+			if err := item.Set("subnet_ids", aws.StringValueSlice(transitGatewayVpcAttachment.SubnetIds)); err != nil {
+				return fmt.Errorf("error setting subnet_ids: %s", err)
+			}
+			if err := item.Set("tags", keyvaluetags.Ec2KeyValueTags(transitGatewayVpcAttachment.Tags).IgnoreAws().Map()); err != nil {
+				return fmt.Errorf("error setting tags: %s", err)
+			}
+
+			item.Set("transit_gateway_id", aws.StringValue(transitGatewayVpcAttachment.TransitGatewayId))
+			item.Set("vpc_id", aws.StringValue(transitGatewayVpcAttachment.VpcId))
+			item.Set("vpc_owner_id", aws.StringValue(transitGatewayVpcAttachment.VpcOwnerId))
+
+			item.SetId(aws.StringValue(transitGatewayVpcAttachment.TransitGatewayAttachmentId))
+		}
+		items.append(item)
+	}
+	d.set("transit_gateway_vpc_attachments",items)
+	return nil
+}
