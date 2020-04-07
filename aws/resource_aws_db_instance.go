d27fe1e96707c2e77a875fd228d48bb0ade28d57
Cameron Ross <cameronr@nulogy.com>
2020-04-07 11:56:12 -0400

    Update aws/resource_aws_db_instance.go
    Co-Authored-By: Brian Flad <bflad417@gmail.com>

--- aws/resource_aws_db_instance.go
+++ aws/resource_aws_db_instance.go
@@ -642,11 +642,7 @@ func resourceAwsDbInstanceCreate(d *schema.ResourceData, meta interface{}) error
 		}
 
 		if attr := d.Get("vpc_security_group_ids").(*schema.Set); attr.Len() > 0 {
-			var s []*string
-			for _, v := range attr.List() {
-				s = append(s, aws.String(v.(string)))
-			}
-			opts.VpcSecurityGroupIds = s
+			opts.VpcSecurityGroupIds = expandStringSet(attr)
 		}
 
 		if attr, ok := d.GetOk("performance_insights_enabled"); ok {
