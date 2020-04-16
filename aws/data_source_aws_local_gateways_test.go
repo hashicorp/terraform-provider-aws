536ae75d3213387e18bb34309bda75b63bc4f4e5
John Barney <barneyjo@amazon.com>
2020-04-16 21:22:24 +0800

    Break dependency on VPC tests

--- aws/data_source_aws_local_gateways_test.go
+++ aws/data_source_aws_local_gateways_test.go
@@ -1,7 +1,9 @@
 package aws
 
 import (
+	"fmt"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 	"testing"
 )
 
@@ -13,11 +15,25 @@ func TestAccDataSourceAwsLocalGateways_basic(t *testing.T) {
 			{
 				Config: testAccDataSourceAwsLocalGatewaysConfig,
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckAwsVpcsDataSourceExists("data.aws_local_gateways.all"),
+					testAccCheckAwsLocalGatewaysDataSourceExists("data.aws_local_gateways.all"),
 				),
 			},
 		},
 	})
+}
+
+func testAccCheckAwsLocalGatewaysDataSourceExists(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Can't find aws_local_gateways data source: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("aws_local_gateways data source ID not set")
+		}
+		return nil
+	}
 }
 
 const testAccDataSourceAwsLocalGatewaysConfig = `data "aws_local_gateways" "all" {}`
