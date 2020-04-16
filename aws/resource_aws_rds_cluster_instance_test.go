70f6ed434b17ab4b1b69088590200e28922d78e3
jbarnes <jbarnes7952@gmail.com>
2020-04-16 10:31:07 -0400

    remove_duplicate_test

--- aws/resource_aws_rds_cluster_instance_test.go
+++ aws/resource_aws_rds_cluster_instance_test.go
@@ -853,13 +853,6 @@ func TestAccAWSRDSClusterInstance_CACertificateIdentifier(t *testing.T) {
 		CheckDestroy: testAccCheckAWSClusterDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccAWSRDSClusterInstanceConfig_CACertificateIdentifier(rName, "rds-ca-2019"),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckAWSClusterInstanceExists(resourceName, &dbInstance),
-					resource.TestCheckResourceAttr(resourceName, "ca_cert_identifier", "rds-ca-2019"),
-				),
-			},
-			{
 				ResourceName:      resourceName,
 				ImportState:       true,
 				ImportStateVerify: true,
