f7e9818ad2d62345b00828da14d61babe54c1974
Kit Ewbank <Kit_Ewbank@hotmail.com>
2020-04-18 19:04:46 -0400

    Updated resource names (aws_apigatewayv2_).

--- examples/api-gateway-websocket-chat-app/main.tf
+++ examples/api-gateway-websocket-chat-app/main.tf
@@ -78,81 +78,81 @@ EOT
 # WebSocket API resources.
 #
 
-resource "aws_api_gateway_v2_api" "SimpleChatWebSocket" {
+resource "aws_apigatewayv2_api" "SimpleChatWebSocket" {
   name                       = "SimpleChatWebSocket"
   protocol_type              = "WEBSOCKET"
   route_selection_expression = "$request.body.message"
 }
 
-resource "aws_api_gateway_v2_deployment" "Deployment" {
-  api_id = "${aws_api_gateway_v2_api.SimpleChatWebSocket.id}"
+resource "aws_apigatewayv2_deployment" "Deployment" {
+  api_id = "${aws_apigatewayv2_api.SimpleChatWebSocket.id}"
 
   depends_on = [
-    "aws_api_gateway_v2_route.ConnectRoute",
-    "aws_api_gateway_v2_route.DisconnectRoute",
-    "aws_api_gateway_v2_route.SendRoute",
+    "aws_apigatewayv2_route.ConnectRoute",
+    "aws_apigatewayv2_route.DisconnectRoute",
+    "aws_apigatewayv2_route.SendRoute",
   ]
 }
 
-resource "aws_api_gateway_v2_stage" "Stage" {
-  api_id        = "${aws_api_gateway_v2_api.SimpleChatWebSocket.id}"
+resource "aws_apigatewayv2_stage" "Stage" {
+  api_id        = "${aws_apigatewayv2_api.SimpleChatWebSocket.id}"
   name          = "Prod"
   description   = "Prod Stage"
-  deployment_id = "${aws_api_gateway_v2_deployment.Deployment.id}"
+  deployment_id = "${aws_apigatewayv2_deployment.Deployment.id}"
 }
 
 ###########
 # OnConnect
 ###########
-resource "aws_api_gateway_v2_integration" "ConnectIntegrat" {
-  api_id             = "${aws_api_gateway_v2_api.SimpleChatWebSocket.id}"
+resource "aws_apigatewayv2_integration" "ConnectIntegrat" {
+  api_id             = "${aws_apigatewayv2_api.SimpleChatWebSocket.id}"
   integration_type   = "AWS_PROXY"
   description        = "Connect Integration"
   integration_uri    = "${aws_lambda_function.OnConnectFunction.invoke_arn}"
   integration_method = "POST"
 }
 
-resource "aws_api_gateway_v2_route" "ConnectRoute" {
-  api_id         = "${aws_api_gateway_v2_api.SimpleChatWebSocket.id}"
+resource "aws_apigatewayv2_route" "ConnectRoute" {
+  api_id         = "${aws_apigatewayv2_api.SimpleChatWebSocket.id}"
   route_key      = "$connect"
   operation_name = "ConnectRoute"
-  target         = "integrations/${aws_api_gateway_v2_integration.ConnectIntegrat.id}"
+  target         = "integrations/${aws_apigatewayv2_integration.ConnectIntegrat.id}"
 }
 
 ##############
 # OnDisconnect
 ##############
-resource "aws_api_gateway_v2_integration" "DisconnectInteg" {
-  api_id             = "${aws_api_gateway_v2_api.SimpleChatWebSocket.id}"
+resource "aws_apigatewayv2_integration" "DisconnectInteg" {
+  api_id             = "${aws_apigatewayv2_api.SimpleChatWebSocket.id}"
   integration_type   = "AWS_PROXY"
   description        = "Disconnect Integration"
   integration_uri    = "${aws_lambda_function.OnDisconnectFunction.invoke_arn}"
   integration_method = "POST"
 }
 
-resource "aws_api_gateway_v2_route" "DisconnectRoute" {
-  api_id         = "${aws_api_gateway_v2_api.SimpleChatWebSocket.id}"
+resource "aws_apigatewayv2_route" "DisconnectRoute" {
+  api_id         = "${aws_apigatewayv2_api.SimpleChatWebSocket.id}"
   route_key      = "$disconnect"
   operation_name = "DisconnectRoute"
-  target         = "integrations/${aws_api_gateway_v2_integration.DisconnectInteg.id}"
+  target         = "integrations/${aws_apigatewayv2_integration.DisconnectInteg.id}"
 }
 
 #############
 # SendMessage
 #############
-resource "aws_api_gateway_v2_integration" "SendInteg" {
-  api_id             = "${aws_api_gateway_v2_api.SimpleChatWebSocket.id}"
+resource "aws_apigatewayv2_integration" "SendInteg" {
+  api_id             = "${aws_apigatewayv2_api.SimpleChatWebSocket.id}"
   integration_type   = "AWS_PROXY"
   description        = "Send Integration"
   integration_uri    = "${aws_lambda_function.SendMessageFunction.invoke_arn}"
   integration_method = "POST"
 }
 
-resource "aws_api_gateway_v2_route" "SendRoute" {
-  api_id         = "${aws_api_gateway_v2_api.SimpleChatWebSocket.id}"
+resource "aws_apigatewayv2_route" "SendRoute" {
+  api_id         = "${aws_apigatewayv2_api.SimpleChatWebSocket.id}"
   route_key      = "sendmessage"
   operation_name = "SendRoute"
-  target         = "integrations/${aws_api_gateway_v2_integration.SendInteg.id}"
+  target         = "integrations/${aws_apigatewayv2_integration.SendInteg.id}"
 }
 
 #
@@ -420,7 +420,7 @@ resource "aws_iam_policy" "SendMessageManageConnectionsPolicy" {
   "Statement": [{
     "Effect": "Allow",
     "Action": "execute-api:ManageConnections",
-    "Resource": "${aws_api_gateway_v2_api.SimpleChatWebSocket.execution_arn}/*"
+    "Resource": "${aws_apigatewayv2_api.SimpleChatWebSocket.execution_arn}/*"
   }]
 }
 EOT
@@ -452,5 +452,5 @@ output "SendMessageFunctionArn" {
 }
 
 output "WebSocketURI" {
-  value = "${aws_api_gateway_v2_stage.Stage.invoke_url}"
+  value = "${aws_apigatewayv2_stage.Stage.invoke_url}"
 }
