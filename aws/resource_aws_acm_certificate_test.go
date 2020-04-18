0d85573e5bd131daf97a3447277f2f2e156ff12e
Kit Ewbank <Kit_Ewbank@hotmail.com>
2020-04-18 18:47:53 -0400

    r/aws_acm_certificate: Add test sweeper.

--- aws/resource_aws_acm_certificate_test.go
+++ aws/resource_aws_acm_certificate_test.go
@@ -2,6 +2,7 @@ package aws
 
 import (
 	"fmt"
+	"log"
 	"strconv"
 	"strings"
 	"testing"
@@ -11,10 +12,75 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/acm"
+	"github.com/hashicorp/go-multierror"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
+
+func init() {
+	resource.AddTestSweepers("aws_acm_certificate", &resource.Sweeper{
+		Name: "aws_acm_certificate",
+		F:    testSweepAcmCertificates,
+	})
+}
+
+func testSweepAcmCertificates(region string) error {
+	client, err := sharedClientForRegion(region)
+	if err != nil {
+		return fmt.Errorf("error getting client: %s", err)
+	}
+	conn := client.(*AWSClient).acmconn
+	var sweeperErrs *multierror.Error
+
+	err = conn.ListCertificatesPages(&acm.ListCertificatesInput{}, func(page *acm.ListCertificatesOutput, isLast bool) bool {
+		if page == nil {
+			return !isLast
+		}
+
+		for _, certificate := range page.CertificateSummaryList {
+			arn := aws.StringValue(certificate.CertificateArn)
+
+			output, err := conn.DescribeCertificate(&acm.DescribeCertificateInput{
+				CertificateArn: aws.String(arn),
+			})
+			if err != nil {
+				sweeperErr := fmt.Errorf("error describing ACM certificate (%s): %w", arn, err)
+				log.Printf("[ERROR] %s", sweeperErr)
+				sweeperErrs = multierror.Append(sweeperErrs, sweeperErr)
+				continue
+			}
+
+			if len(output.Certificate.InUseBy) > 0 {
+				log.Printf("[INFO] ACM certificate (%s) is in-use, skipping", arn)
+				continue
+			}
+
+			log.Printf("[INFO] Deleting ACM certificate: %s", arn)
+			_, err = conn.DeleteCertificate(&acm.DeleteCertificateInput{
+				CertificateArn: aws.String(arn),
+			})
+			if err != nil {
+				sweeperErr := fmt.Errorf("error deleting ACM certificate (%s): %w", arn, err)
+				log.Printf("[ERROR] %s", sweeperErr)
+				sweeperErrs = multierror.Append(sweeperErrs, sweeperErr)
+				continue
+			}
+		}
+
+		return !isLast
+	})
+
+	if err != nil {
+		if testSweepSkipSweepError(err) {
+			log.Printf("[WARN] Skipping ACM certificate sweep for %s: %s", region, err)
+			return sweeperErrs.ErrorOrNil() // In case we have completed some pages, but had errors
+		}
+		return fmt.Errorf("error retrieving ACM certificates: %s", err)
+	}
+
+	return sweeperErrs.ErrorOrNil()
+}
 
 func testAccAwsAcmCertificateDomainFromEnv(t *testing.T) string {
 	rootDomain := os.Getenv("ACM_CERTIFICATE_ROOT_DOMAIN")
