3a909722443cb371412b8f391b7ad9e13d5beab4
DrFaust92 <ilia.lazebnik@gmail.com>
2020-04-26 14:33:43 +0300

    use rname

--- aws/resource_aws_spot_fleet_request_test.go
+++ aws/resource_aws_spot_fleet_request_test.go
@@ -1143,7 +1143,7 @@ data "aws_availability_zones" "available" {
   }
 }
 resource "aws_key_pair" "test" {
-  key_name   = "tmp-key-%[1]s"
+  key_name   = %[1]q
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 phodgson@thoughtworks.com"
 
   tags = {
@@ -1152,7 +1152,7 @@ resource "aws_key_pair" "test" {
 }
 
 resource "aws_iam_role" "test-role" {
-  name = "test-role-%[1]s"
+  name = %[1]q
 
   assume_role_policy = <<EOF
 {
