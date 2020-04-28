7b9dcb8c39649adf93e33822d43f0aec9caf800b
Graham Davison <gdavison@hashicorp.com>
2020-04-27 17:26:53 -0700

    Adds one more ARN test fix and replaces many `acctest.RandInt()` calls with `acctest.RandomWithPrefix()`

--- aws/data_source_aws_s3_bucket_test.go
+++ aws/data_source_aws_s3_bucket_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func TestAccDataSourceS3Bucket_basic(t *testing.T) {
-	rInt := acctest.RandInt()
+	bucketName := acctest.RandomWithPrefix("tf-test-bucket")
 	region := testAccGetRegion()
 	hostedZoneID, _ := HostedZoneIDForRegion(region)
 
@@ -18,13 +18,13 @@ func TestAccDataSourceS3Bucket_basic(t *testing.T) {
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccAWSDataSourceS3BucketConfig_basic(rInt),
+				Config: testAccAWSDataSourceS3BucketConfig_basic(bucketName),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckAWSS3BucketExists("data.aws_s3_bucket.bucket"),
 					resource.TestCheckResourceAttrPair("data.aws_s3_bucket.bucket", "arn", "aws_s3_bucket.bucket", "arn"),
 					resource.TestCheckResourceAttr("data.aws_s3_bucket.bucket", "region", region),
-					testAccCheckS3BucketDomainName("data.aws_s3_bucket.bucket", "bucket_domain_name", testAccBucketName(rInt)),
-					resource.TestCheckResourceAttr("data.aws_s3_bucket.bucket", "bucket_regional_domain_name", testAccBucketRegionalDomainName(rInt, region)),
+					testAccCheckS3BucketDomainName("data.aws_s3_bucket.bucket", "bucket_domain_name", bucketName),
+					resource.TestCheckResourceAttr("data.aws_s3_bucket.bucket", "bucket_regional_domain_name", testAccBucketRegionalDomainName(bucketName, region)),
 					resource.TestCheckResourceAttr("data.aws_s3_bucket.bucket", "hosted_zone_id", hostedZoneID),
 					resource.TestCheckNoResourceAttr("data.aws_s3_bucket.bucket", "website_endpoint"),
 				),
@@ -34,7 +34,7 @@ func TestAccDataSourceS3Bucket_basic(t *testing.T) {
 }
 
 func TestAccDataSourceS3Bucket_website(t *testing.T) {
-	rInt := acctest.RandInt()
+	bucketName := acctest.RandomWithPrefix("tf-test-bucket")
 	region := testAccGetRegion()
 
 	resource.ParallelTest(t, resource.TestCase{
@@ -42,33 +42,33 @@ func TestAccDataSourceS3Bucket_website(t *testing.T) {
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccAWSDataSourceS3BucketWebsiteConfig(rInt),
+				Config: testAccAWSDataSourceS3BucketWebsiteConfig(bucketName),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckAWSS3BucketExists("data.aws_s3_bucket.bucket"),
 					testAccCheckAWSS3BucketWebsite("data.aws_s3_bucket.bucket", "index.html", "error.html", "", ""),
-					testAccCheckS3BucketWebsiteEndpoint("data.aws_s3_bucket.bucket", "website_endpoint", testAccBucketName(rInt), region),
+					testAccCheckS3BucketWebsiteEndpoint("data.aws_s3_bucket.bucket", "website_endpoint", bucketName, region),
 				),
 			},
 		},
 	})
 }
 
-func testAccAWSDataSourceS3BucketConfig_basic(randInt int) string {
+func testAccAWSDataSourceS3BucketConfig_basic(bucketName string) string {
 	return fmt.Sprintf(`
 resource "aws_s3_bucket" "bucket" {
-  bucket = "tf-test-bucket-%d"
+  bucket = %[1]q
 }
 
 data "aws_s3_bucket" "bucket" {
   bucket = "${aws_s3_bucket.bucket.id}"
 }
-`, randInt)
+`, bucketName)
 }
 
-func testAccAWSDataSourceS3BucketWebsiteConfig(randInt int) string {
+func testAccAWSDataSourceS3BucketWebsiteConfig(bucketName string) string {
 	return fmt.Sprintf(`
 resource "aws_s3_bucket" "bucket" {
-  bucket = "tf-test-bucket-%d"
+  bucket = %[1]q
   acl    = "public-read"
 
   website {
@@ -80,5 +80,5 @@ resource "aws_s3_bucket" "bucket" {
 data "aws_s3_bucket" "bucket" {
   bucket = "${aws_s3_bucket.bucket.id}"
 }
-`, randInt)
+`, bucketName)
 }
