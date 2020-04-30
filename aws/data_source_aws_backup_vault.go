6ad7809adc8d6b8539a49de001dbcc1d988a3a84
Kit Ewbank <Kit_Ewbank@hotmail.com>
2020-04-30 10:12:58 -0400

    Fix tfproviderlint AWSR002 errors.

--- aws/data_source_aws_backup_vault.go
+++ aws/data_source_aws_backup_vault.go
@@ -36,6 +36,8 @@ func dataSourceAwsBackupVault() *schema.Resource {
 
 func dataSourceAwsBackupVaultRead(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*AWSClient).backupconn
+	ignoreTagsConfig := meta.(*AWSClient).IgnoreTagsConfig
+
 	name := d.Get("name").(string)
 	input := &backup.DescribeBackupVaultInput{
 		BackupVaultName: aws.String(name),
@@ -56,7 +58,7 @@ func dataSourceAwsBackupVaultRead(d *schema.ResourceData, meta interface{}) erro
 	if err != nil {
 		return fmt.Errorf("error listing tags for Backup Vault (%s): %s", name, err)
 	}
-	if err := d.Set("tags", tags.IgnoreAws().Map()); err != nil {
+	if err := d.Set("tags", tags.IgnoreAws().IgnoreConfig(ignoreTagsConfig).Map()); err != nil {
 		return fmt.Errorf("error setting tags: %s", err)
 	}
 
