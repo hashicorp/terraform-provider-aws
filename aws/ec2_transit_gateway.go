4cee185cc9ee2b3b4329bf767b22d5f21aaf1653
Omarimcblack <43746683+Omarimcblack@users.noreply.github.com>
2020-04-30 18:44:33 +0100

    merge changes from master for service/ec2: Add Resource & Data Source aws_ec2_transit_gateway_peering_attachment (#11162)
    Output from acceptance testing:
    
    ```
    --- PASS: TestAccAWSEc2TransitGatewayPeeringAttachment_basic (279.30s)
    --- PASS: TestAccAWSEc2TransitGatewayPeeringAttachment_differentAccount (274.76s)
    --- PASS: TestAccAWSEc2TransitGatewayPeeringAttachment_disappears (298.80s)
    --- PASS: TestAccAWSEc2TransitGatewayPeeringAttachment_Tags_sameAccount (341.73s)
    
    --- PASS: TestAccAWSEc2TransitGatewayPeeringAttachmentDataSource_Filter_differentAccount (290.68s)
    --- PASS: TestAccAWSEc2TransitGatewayPeeringAttachmentDataSource_Filter_sameAccount (281.60s)
    --- PASS: TestAccAWSEc2TransitGatewayPeeringAttachmentDataSource_ID_differentAccount (291.70s)
    --- PASS: TestAccAWSEc2TransitGatewayPeeringAttachmentDataSource_ID_sameAccount (308.69s)
    ```

--- aws/ec2_transit_gateway.go
+++ aws/ec2_transit_gateway.go
@@ -427,6 +427,10 @@ func ec2TransitGatewayPeeringAttachmentRefreshFunc(conn *ec2.EC2, transitGateway
 			return nil, ec2.TransitGatewayAttachmentStateDeleted, nil
 		}
 
+		if aws.StringValue(transitGatewayPeeringAttachment.State) == ec2.TransitGatewayAttachmentStateFailed && transitGatewayPeeringAttachment.Status != nil {
+			return transitGatewayPeeringAttachment, aws.StringValue(transitGatewayPeeringAttachment.State), fmt.Errorf("%s: %s", aws.StringValue(transitGatewayPeeringAttachment.Status.Code), aws.StringValue(transitGatewayPeeringAttachment.Status.Message))
+		}
+
 		return transitGatewayPeeringAttachment, aws.StringValue(transitGatewayPeeringAttachment.State), nil
 	}
 }
@@ -615,16 +619,38 @@ func waitForEc2TransitGatewayPeeringAttachmentAcceptance(conn *ec2.EC2, transitG
 	return err
 }
 
+func waitForEc2TransitGatewayPeeringAttachmentCreation(conn *ec2.EC2, transitGatewayAttachmentID string) error {
+	stateConf := &resource.StateChangeConf{
+		Pending: []string{
+			ec2.TransitGatewayAttachmentStateFailing,
+			ec2.TransitGatewayAttachmentStatePending,
+			"initiatingRequest", // No ENUM currently exists in the SDK for the state given by AWS
+		},
+		Target: []string{
+			ec2.TransitGatewayAttachmentStateAvailable,
+			ec2.TransitGatewayAttachmentStatePendingAcceptance,
+		},
+		Refresh: ec2TransitGatewayPeeringAttachmentRefreshFunc(conn, transitGatewayAttachmentID),
+		Timeout: 10 * time.Minute,
+	}
+
+	log.Printf("[DEBUG] Waiting for EC2 Transit Gateway Peering Attachment (%s) availability", transitGatewayAttachmentID)
+	_, err := stateConf.WaitForState()
+
+	return err
+}
+
 func waitForEc2TransitGatewayPeeringAttachmentDeletion(conn *ec2.EC2, transitGatewayAttachmentID string) error {
 	stateConf := &resource.StateChangeConf{
 		Pending: []string{
 			ec2.TransitGatewayAttachmentStateAvailable,
 			ec2.TransitGatewayAttachmentStateDeleting,
+			ec2.TransitGatewayAttachmentStatePendingAcceptance,
+			ec2.TransitGatewayAttachmentStateRejected,
 		},
-		Target:         []string{ec2.TransitGatewayAttachmentStateDeleted},
-		Refresh:        ec2TransitGatewayVpcAttachmentRefreshFunc(conn, transitGatewayAttachmentID),
-		Timeout:        10 * time.Minute,
-		NotFoundChecks: 1,
+		Target:  []string{ec2.TransitGatewayAttachmentStateDeleted},
+		Refresh: ec2TransitGatewayPeeringAttachmentRefreshFunc(conn, transitGatewayAttachmentID),
+		Timeout: 10 * time.Minute,
 	}
 
 	log.Printf("[DEBUG] Waiting for EC2 Transit Gateway Peering Attachment (%s) deletion", transitGatewayAttachmentID)
