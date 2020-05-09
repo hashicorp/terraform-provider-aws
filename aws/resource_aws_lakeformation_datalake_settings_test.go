9d54d249bd8ebb59231c68c8b9e7cfe80b1b451f
Gaylord Mazelier <gaylord@mazelier.fr>
2020-05-09 19:21:03 +0200

    Check resource state on destroy

--- aws/resource_aws_lakeformation_datalake_settings_test.go
+++ aws/resource_aws_lakeformation_datalake_settings_test.go
@@ -1,9 +1,14 @@
 package aws
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/lakeformation"
+
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
 func TestAccAWSLakeFormationDataLakeSettings_basic(t *testing.T) {
@@ -11,13 +16,14 @@ func TestAccAWSLakeFormationDataLakeSettings_basic(t *testing.T) {
 	resourceName := "aws_lakeformation_datalake_settings.test"
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
-		Providers: testAccProviders,
-		// TODO: CheckDestroy: testAccCheckAWSLakeFormationDataLakeSettingsDestroy,
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAWSLakeFormationDataLakeSettingsEmpty,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccAWSLakeFormationDataLakeSettingsConfig_basic,
 				Check: resource.ComposeTestCheckFunc(
+					testAccCheckResourceAttrAccountID(resourceName, "catalog_id"),
 					resource.TestCheckResourceAttrPair(callerIdentityName, "account_id", resourceName, "catalog_id"),
 					resource.TestCheckResourceAttr(resourceName, "admins.#", "1"),
 					resource.TestCheckResourceAttrPair(callerIdentityName, "arn", resourceName, "admins.0"),
@@ -40,13 +46,14 @@ func TestAccAWSLakeFormationDataLakeSettings_withCatalogId(t *testing.T) {
 	resourceName := "aws_lakeformation_datalake_settings.test"
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
-		Providers: testAccProviders,
-		// TODO: CheckDestroy: testAccCheckAWSLakeFormationDataLakeSettingsDestroy,
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAWSLakeFormationDataLakeSettingsEmpty,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccAWSLakeFormationDataLakeSettingsConfig_withCatalogId,
 				Check: resource.ComposeTestCheckFunc(
+					testAccCheckResourceAttrAccountID(resourceName, "catalog_id"),
 					resource.TestCheckResourceAttrPair(callerIdentityName, "account_id", resourceName, "catalog_id"),
 					resource.TestCheckResourceAttr(resourceName, "admins.#", "1"),
 					resource.TestCheckResourceAttrPair(callerIdentityName, "arn", resourceName, "admins.0"),
@@ -64,3 +71,28 @@ resource "aws_lakeformation_datalake_settings" "test" {
   admins = ["${data.aws_caller_identity.current.arn}"]
 }
 `
+
+func testAccCheckAWSLakeFormationDataLakeSettingsEmpty(s *terraform.State) error {
+	conn := testAccProvider.Meta().(*AWSClient).lakeformationconn
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "aws_lakeformation_datalake_settings" {
+			continue
+		}
+
+		input := &lakeformation.GetDataLakeSettingsInput{
+			CatalogId: aws.String(testAccProvider.Meta().(*AWSClient).accountid),
+		}
+
+		out, err := conn.GetDataLakeSettings(input)
+		if err != nil {
+			return fmt.Errorf("Error reading DataLakeSettings: %s", err)
+		}
+
+		if len(out.DataLakeSettings.DataLakeAdmins) > 0 {
+			return fmt.Errorf("Error admins list not empty in DataLakeSettings: %s", out)
+		}
+	}
+
+	return nil
+}
