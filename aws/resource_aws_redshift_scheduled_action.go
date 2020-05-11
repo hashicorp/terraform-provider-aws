dc6c8c126958e05715e94a357b8438db6418017a
hitofuji <email@example.com>
2020-05-11 09:33:32 +0900

    add create method excepted target_action

--- aws/resource_aws_redshift_scheduled_action.go
+++ aws/resource_aws_redshift_scheduled_action.go
@@ -1,10 +1,14 @@
 package aws
 
 import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/glue"
 	"github.com/aws/aws-sdk-go/service/redshift"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+	"time"
 )
 
 func resourceAwsRedshiftScheduledAction() *schema.Resource {
@@ -31,7 +35,6 @@ func resourceAwsRedshiftScheduledAction() *schema.Resource {
 			"description": {
 				Type:     schema.TypeString,
 				Optional: true,
-				ForceNew: true,
 			},
 			"active": {
 				Type:     schema.TypeBool,
@@ -93,5 +96,32 @@ func resourceAwsRedshiftScheduledAction() *schema.Resource {
 			},
 		},
 	}
+}
 
+func resourceAwsRedshiftScheduledActionCreate(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*AWSClient).redshiftconn
+	var name string
+	if v, ok := d.GetOk("name"); ok {
+		name = v.(string)
+	} else {
+		name = resource.UniqueId()
+	}
+	createOpts := &redshift.CreateScheduledActionInput{
+		ScheduledActionName: aws.String(name),
+		Schedule:            aws.String(d.Get("schedule").(string)),
+		IamRole:             aws.String(d.Get("iam_role").(string)),
+	}
+	// TODO: add target_action
+	if attr, ok := d.GetOk("description"); ok {
+		createOpts.ScheduledActionDescription = aws.String(attr.(string))
+	}
+	if attr, ok := d.GetOk("active"); ok {
+		createOpts.Enable = aws.Bool(attr.(bool))
+	}
+	if attr, ok := d.GetOk("start_time"); ok {
+		createOpts.StartTime = aws.Time(attr.(time.Time))
+	}
+	if attr, ok := d.GetOk("end_time"); ok {
+		createOpts.EndTime = aws.Time(attr.(time.Time))
+	}
 }
