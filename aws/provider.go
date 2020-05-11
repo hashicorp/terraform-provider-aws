c2cad95cb988ce4a0e182c335da13195db76a706
schwamster <bastian.toepfer@gmail.com>
2020-05-11 14:06:39 +0200

    added aws_iot_greengrass_service_role resource

--- aws/provider.go
+++ aws/provider.go
@@ -635,6 +635,7 @@ func Provider() terraform.ResourceProvider {
 			"aws_instance":                                            resourceAwsInstance(),
 			"aws_internet_gateway":                                    resourceAwsInternetGateway(),
 			"aws_iot_certificate":                                     resourceAwsIotCertificate(),
+			"aws_iot_greengrass_service_role":                         resourceAwsIotGreengrassServiceRole(),
 			"aws_iot_policy":                                          resourceAwsIotPolicy(),
 			"aws_iot_policy_attachment":                               resourceAwsIotPolicyAttachment(),
 			"aws_iot_thing":                                           resourceAwsIotThing(),
