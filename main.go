b261bbd78e4ae4ca8500dcdd00f4bf409457a44e
Felix Candelario <fcandela@amazon.com>
2020-05-23 19:24:21 -0400

    found the work of @trung and getting it up and running

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
-	"github.com/frc9/terraform-provider-aws/aws"
+	"github.com/terraform-providers/terraform-provider-aws/aws"
 )
 
 func main() {
--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_servicecatalog_provisioned_product" "test" {
+resource "aws_servicecatalog_product" "test" {
   product_id = "parameter"
 }
