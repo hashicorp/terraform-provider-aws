99d96fc913dae7dc51b2d4cd077d50b81b2d6007
Felix Chan <firecat701@yahoo.com>
2020-05-23 23:05:26 -0700

    added enable_default_standards flag

--- aws/resource_aws_securityhub_account.go
+++ aws/resource_aws_securityhub_account.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/securityhub"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
@@ -12,20 +13,30 @@ func resourceAwsSecurityHubAccount() *schema.Resource {
 	return &schema.Resource{
 		Create: resourceAwsSecurityHubAccountCreate,
 		Read:   resourceAwsSecurityHubAccountRead,
+		Update: resourceAwsSecurityHubAccountUpdate,
 		Delete: resourceAwsSecurityHubAccountDelete,
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
 		},
 
-		Schema: map[string]*schema.Schema{},
+		Schema: map[string]*schema.Schema{
+			"enable_default_standards": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
+		},
 	}
 }
 
 func resourceAwsSecurityHubAccountCreate(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*AWSClient).securityhubconn
+	defaultStandard := d.Get("enable_default_standards").(bool)
 	log.Print("[DEBUG] Enabling Security Hub for account")
 
-	_, err := conn.EnableSecurityHub(&securityhub.EnableSecurityHubInput{})
+	_, err := conn.EnableSecurityHub(&securityhub.EnableSecurityHubInput{
+		EnableDefaultStandards: aws.Bool(defaultStandard),
+	})
 
 	if err != nil {
 		return fmt.Errorf("Error enabling Security Hub for account: %s", err)
@@ -49,6 +60,26 @@ func resourceAwsSecurityHubAccountRead(d *schema.ResourceData, meta interface{})
 			return nil
 		}
 		return fmt.Errorf("Error checking if Security Hub is enabled: %s", err)
+	}
+
+	return nil
+}
+
+func resourceAwsSecurityHubAccountUpdate(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*AWSClient).securityhubconn
+
+	name := d.Id()
+	log.Printf("[DEBUG] Updating Security Hub account: %q", name)
+
+	if d.HasChange("enable_default_standards") {
+		defaultStandard := d.Get("enable_default_standards").(bool)
+		input := securityhub.EnableSecurityHubInput{
+			EnableDefaultStandards: aws.Bool(defaultStandard),
+		}
+		log.Printf("[DEBUG] Setting default standards for Security Hub: %q: %s", name, input)
+		if _, err := conn.EnableSecurityHub(&input); err != nil {
+			return fmt.Errorf("[ERROR] Error updating Security Hub account (%s): %s", d.Id(), err)
+		}
 	}
 
 	return nil
