c0028558e8413fad7027e270c80b692699487696
Israel Sotomayor <zot24@users.noreply.github.com>
2020-05-30 17:08:52 -0700

    Add node group names data source
    This will show a list of node group names attached to a specific cluster

---
+++ aws/data_source_aws_eks_node_group_names.go
@@ -0,0 +1,59 @@
+package aws
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/eks"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func dataSourceAwsEksNodeGroupNames() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceAwsEksNodeGroupNamesRead,
+
+		Schema: map[string]*schema.Schema{
+			"cluster_name": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validation.NoZeroValues,
+			},
+			"names": {
+				Type:     schema.TypeSet,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Set:      schema.HashString,
+			},
+		},
+	}
+}
+
+func dataSourceAwsEksNodeGroupNamesRead(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*AWSClient).eksconn
+
+	clusterName := d.Get("cluster_name").(string)
+
+	input := &eks.ListNodegroupsInput{
+		ClusterName: aws.String(clusterName),
+	}
+
+	log.Printf("[DEBUG] Reading EKS Node Groups: %s", input)
+	output, err := conn.ListNodegroups(input)
+	if err != nil {
+		return err
+	}
+
+	if output == nil || output.Nodegroups == nil {
+		return fmt.Errorf("EKS Node Groups on cluster (%s) not found", clusterName)
+	}
+
+	nodeGroups := output.Nodegroups
+
+	d.SetId(clusterName)
+	d.Set("cluster_name", clusterName)
+	d.Set("names", nodeGroups)
+
+	return nil
+}
---
+++ aws/data_source_aws_eks_node_group_names_test.go
@@ -0,0 +1,85 @@
+package aws
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"testing"
+)
+
+func TestAccAWSEksNodegroupNamesDataSource_basic(t *testing.T) {
+	rName := acctest.RandomWithPrefix("tf-acc-test")
+	dataSourceResourceName := "data.aws_eks_node_group_names.test"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t); testAccPreCheckAWSEks(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAWSEksClusterDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAWSEksNodeGroupNamesConfig(rName),
+				Check:  resource.ComposeTestCheckFunc(),
+			},
+			{
+				Config: testAccAWSEksNodeGroupNamesDataSourceConfig(rName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(dataSourceResourceName, "cluster_name", rName),
+					resource.TestCheckResourceAttr(dataSourceResourceName, "names.#", "2"),
+				),
+			},
+		},
+	})
+}
+
+func testAccAWSEksNodeGroupNamesDataSourceConfig(rName string) string {
+	return composeConfig(
+		testAccAWSEksNodeGroupNamesConfig(rName),
+		fmt.Sprintf(`
+data "aws_eks_node_group_names" "test" {
+  cluster_name = aws_eks_cluster.test.name
+}
+`))
+}
+
+func testAccAWSEksNodeGroupNamesConfig(rName string) string {
+	return testAccAWSEksNodeGroupConfigBase(rName) + fmt.Sprintf(`
+resource "aws_eks_node_group" "test_a" {
+  cluster_name    = aws_eks_cluster.test.name
+  node_group_name = "%[1]s-test-a"
+  node_role_arn   = aws_iam_role.node.arn
+  subnet_ids      = aws_subnet.test[*].id
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+
+  depends_on = [
+    "aws_iam_role_policy_attachment.node-AmazonEKSWorkerNodePolicy",
+    "aws_iam_role_policy_attachment.node-AmazonEKS_CNI_Policy",
+    "aws_iam_role_policy_attachment.node-AmazonEC2ContainerRegistryReadOnly",
+  ]
+}
+
+resource "aws_eks_node_group" "test_b" {
+	cluster_name    = aws_eks_cluster.test.name
+	node_group_name = "%[1]s-test-b"
+	node_role_arn   = aws_iam_role.node.arn
+	subnet_ids      = aws_subnet.test[*].id
+
+	scaling_config {
+	  desired_size = 1
+	  max_size     = 1
+	  min_size     = 1
+	}
+
+	depends_on = [
+	  "aws_iam_role_policy_attachment.node-AmazonEKSWorkerNodePolicy",
+	  "aws_iam_role_policy_attachment.node-AmazonEKS_CNI_Policy",
+	  "aws_iam_role_policy_attachment.node-AmazonEC2ContainerRegistryReadOnly",
+	]
+  }
+`, rName)
+}
