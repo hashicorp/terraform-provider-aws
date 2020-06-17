f78c6d927b6cef3b464d596dd5462c8357872f6e
Ilia Lazebnik <Ilia.lazebnik@gmail.com>
2020-06-17 22:04:00 +0300

    Update waiter.go

--- aws/internal/service/sfn/waiter/waiter.go
+++ aws/internal/service/sfn/waiter/waiter.go
@@ -1,9 +1,9 @@
 package waiter
 
 import (
-	"github.com/aws/aws-sdk-go/service/sfn"
 	"time"
-
+	
+	"github.com/aws/aws-sdk-go/service/sfn"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 )
 
