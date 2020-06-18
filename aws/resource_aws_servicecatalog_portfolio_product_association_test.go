038fd4a962189337205c2cd9b652d2300275bd62
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2020-06-18 16:43:57 +0100

    restore portfolio-product-association additions for now, focus on incremental PR with just this
    This reverts commit 2dcb0833ce31f3a731163d0649d58bcf432ed00f.

---
+++ aws/resource_aws_servicecatalog_portfolio_product_association_test.go
@@ -0,0 +1,98 @@
+package aws
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/servicecatalog"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+)
+
+func TestAccAWSServiceCatalogPortfolioProductAssociation_Basic(t *testing.T) {
+	salt := acctest.RandString(5)
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckServiceCatalogPortfolioProductAssociationDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckAwsServiceCatalogPortfolioProductAssociationConfigBasic("test_product", salt),
+				Check:  testAccCheckAwsServiceCatalogPortfolioProductAssociation(),
+			},
+			{
+				ResourceName:      "aws_servicecatalog_portfolio_product_association.association",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccCheckServiceCatalogPortfolioProductAssociationDestroy(s *terraform.State) error {
+	conn := testAccProvider.Meta().(*AWSClient).scconn
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "aws_servicecatalog_portfolio_product_association" {
+			continue // not our monkey
+		}
+		_, productId, portfolioId := parseServiceCatalogPortfolioProductAssociationResourceId(rs.Primary.ID)
+		input := servicecatalog.ListPortfoliosForProductInput{ProductId: &productId}
+		portfolios, err := conn.ListPortfoliosForProduct(&input)
+		if err != nil {
+			if isAWSErr(err, servicecatalog.ErrCodeResourceNotFoundException, "") {
+				return nil // not found for product is good
+			}
+			return err // some other unexpected error
+		}
+		for _, portfolioDetail := range portfolios.PortfolioDetails {
+			if aws.StringValue(portfolioDetail.Id) == portfolioId {
+				return fmt.Errorf("expected AWS Service Catalog Portfolio Product Association to be gone, but it was still found")
+			}
+		}
+	}
+	return nil
+}
+
+func testAccCheckAwsServiceCatalogPortfolioProductAssociation() resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		conn := testAccProvider.Meta().(*AWSClient).scconn
+		for _, rs := range s.RootModule().Resources {
+			if rs.Type != "aws_servicecatalog_portfolio_product_association" {
+				continue // not our monkey
+			}
+			_, portfolioId, productId := parseServiceCatalogPortfolioProductAssociationResourceId(rs.Primary.ID)
+			input := servicecatalog.ListPortfoliosForProductInput{ProductId: &productId}
+			portfolios, err := conn.ListPortfoliosForProduct(&input)
+			if err != nil {
+				return err
+			}
+			for _, portfolioDetail := range portfolios.PortfolioDetails {
+				if aws.StringValue(portfolioDetail.Id) == portfolioId {
+					return nil //is good
+				}
+			}
+			return fmt.Errorf("association not found between portfolio %s and product %s", portfolioId, productId)
+		}
+		return fmt.Errorf("no associations found")
+	}
+}
+
+func testAccCheckAwsServiceCatalogPortfolioProductAssociationConfigBasic(productName, salt string) string {
+	portfolio_cfg := testAccCheckAwsServiceCatalogPortfolioResourceConfigBasic("tfm-test")
+
+	arbitraryBucketName := fmt.Sprintf("bucket-%s", salt)
+	arbitraryProductName := fmt.Sprintf("product-%s", salt)
+	arbitraryProvisionArtifactName := fmt.Sprintf("pa-%s", salt)
+	p_tag1 := "FooKey = \"bar\""
+	p_tag2 := "BarKey = \"foo\""
+	productResourceFqn := "aws_servicecatalog_product." + productName
+	productCfg := testAccCheckAwsServiceCatalogProductResourceConfigTemplate(productResourceFqn, arbitraryBucketName, arbitraryProductName, arbitraryProvisionArtifactName, p_tag1, p_tag2)
+
+	return portfolio_cfg + "\n" + productCfg + "\n" + fmt.Sprintf(`
+resource "aws_servicecatalog_portfolio_product_association" "association" {
+    portfolio_id = aws_servicecatalog_portfolio.test.id
+    product_id = %s.id
+}`, productResourceFqn)
+}
---
+++ website/docs/r/servicecatalog_portfolio_product_association.html.markdown
@@ -0,0 +1,55 @@
+---
+subcategory: "Service Catalog"
+layout: "aws"
+page_title: "AWS: aws_servicecatalog_portfolio_product_association"
+description: |-
+  Provides a resource to control the association of a Service Catalog Product with a Portfolio
+---
+
+# aws_servicecatalog_portfolio_product_association
+
+Provides a resource to control the association of a Service Catalog Product with a Portfolio.
+
+This is necessary for a product to be provisioned, as it must be in at least one portfolio.
+
+In most cases this is simple and straightforward. 
+However, there are some pathological edge cases that can arise 
+as the association is not an identifiable resource in the usual sense. 
+For instance if an association between a given portfolio and product were created twice,
+with two instances in Terraform, and then one of them is deleted, 
+there would be one instance remaining in Terraform but
+the association be absent in AWS until the remaining Terraform instance is re-applied.   
+
+
+## Example Usage
+
+```hcl
+resource "aws_servicecatalog_portfolio_product_association" "test" {
+  portfolio_id = "port-01234567890abc"
+  product_id = "prod-abcdefghijklm"
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `portfolio_id` - (Required) The portfolio identifier
+* `product_id` - (Required) The product identifier
+
+
+## Attributes Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - A generated ID to represent this association, of the form `${portfolio_id}--${product_id}`.
+
+
+## Import
+
+Service Catalog Portfolio-Product Associations can be imported using the ID constructed 
+from the portfolio and product ids, e.g.
+
+```
+$ terraform import aws_servicecatalog_portfolio_product_association.test port-01234567890abc--prod-abcdefghijklm
+```
