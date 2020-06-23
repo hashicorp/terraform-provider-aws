667babca0824104b9f37cd0003b88312a0341689
Ilia Lazebnik <Ilia.lazebnik@gmail.com>
2020-06-23 19:14:31 +0300

    tests/resource/aws_ec2_transit_gateway: Change disappears test + arn check (#13899)
    Output from acceptance testing:
    
    ```
    --- PASS: TestAccAWSEc2TransitGateway_basic (168.37s)
    --- PASS: TestAccAWSEc2TransitGateway_disappears (175.88s)
    --- PASS: TestAccAWSEc2TransitGatewayRouteTable_disappears_TransitGateway (189.25s)
    ```

--- aws/resource_aws_ec2_transit_gateway_test.go
+++ aws/resource_aws_ec2_transit_gateway_test.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"regexp"
 	"testing"
 	"time"
 
@@ -121,7 +122,7 @@ func TestAccAWSEc2TransitGateway_basic(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckAWSEc2TransitGatewayExists(resourceName, &transitGateway1),
 					resource.TestCheckResourceAttr(resourceName, "amazon_side_asn", "64512"),
-					resource.TestCheckResourceAttrSet(resourceName, "arn"),
+					testAccMatchResourceAttrRegionalARN(resourceName, "arn", "ec2", regexp.MustCompile(`transit-gateway/tgw-.+`)),
 					resource.TestCheckResourceAttrSet(resourceName, "association_default_route_table_id"),
 					resource.TestCheckResourceAttr(resourceName, "auto_accept_shared_attachments", ec2.AutoAcceptSharedAttachmentsValueDisable),
 					resource.TestCheckResourceAttr(resourceName, "default_route_table_association", ec2.DefaultRouteTableAssociationValueEnable),
@@ -156,7 +157,7 @@ func TestAccAWSEc2TransitGateway_disappears(t *testing.T) {
 				Config: testAccAWSEc2TransitGatewayConfig(),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckAWSEc2TransitGatewayExists(resourceName, &transitGateway1),
-					testAccCheckAWSEc2TransitGatewayDisappears(&transitGateway1),
+					testAccCheckResourceDisappears(testAccProvider, resourceAwsEc2TransitGateway(), resourceName),
 				),
 				ExpectNonEmptyPlan: true,
 			},
@@ -527,24 +528,6 @@ func testAccCheckAWSEc2TransitGatewayDestroy(s *terraform.State) error {
 	}
 
 	return nil
-}
-
-func testAccCheckAWSEc2TransitGatewayDisappears(transitGateway *ec2.TransitGateway) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		conn := testAccProvider.Meta().(*AWSClient).ec2conn
-
-		input := &ec2.DeleteTransitGatewayInput{
-			TransitGatewayId: transitGateway.TransitGatewayId,
-		}
-
-		_, err := conn.DeleteTransitGateway(input)
-
-		if err != nil {
-			return err
-		}
-
-		return waitForEc2TransitGatewayDeletion(conn, aws.StringValue(transitGateway.TransitGatewayId))
-	}
 }
 
 func testAccCheckAWSEc2TransitGatewayNotRecreated(i, j *ec2.TransitGateway) resource.TestCheckFunc {
