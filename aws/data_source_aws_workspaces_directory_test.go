3bb8b5cb2f78cbcc7be9e6a4902584c6869cff42
Andrew Babichev <andrew.babichev@gmail.com>
2020-06-24 15:37:52 +0300

    @review Fix resource and data source creation race

--- aws/data_source_aws_workspaces_directory_test.go
+++ aws/data_source_aws_workspaces_directory_test.go
@@ -11,9 +11,8 @@ import (
 func TestAccDataSourceAwsWorkspacesDirectory_basic(t *testing.T) {
 	rName := acctest.RandString(8)
 
-	iamRoleDataSourceName := "data.aws_iam_role.workspaces-default"
-	directoryResourceName := "aws_directory_service_directory.main"
-	dataSourceName := "data.aws_workspaces_directory.main"
+	resourceName := "aws_workspaces_directory.test"
+	dataSourceName := "data.aws_workspaces_directory.test"
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t); testAccPreCheckHasIAMRole(t, "workspaces_DefaultRole") },
@@ -22,22 +21,22 @@ func TestAccDataSourceAwsWorkspacesDirectory_basic(t *testing.T) {
 			{
 				Config: testAccDataSourceAwsWorkspacesDirectoryConfig(rName),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr(dataSourceName, "subnet_ids.#", "2"),
-					resource.TestCheckResourceAttr(dataSourceName, "self_service_permissions.#", "1"),
-					resource.TestCheckResourceAttr(dataSourceName, "self_service_permissions.0.change_compute_type", "false"),
-					resource.TestCheckResourceAttr(dataSourceName, "self_service_permissions.0.increase_volume_size", "true"),
-					resource.TestCheckResourceAttr(dataSourceName, "self_service_permissions.0.rebuild_workspace", "true"),
-					resource.TestCheckResourceAttr(dataSourceName, "self_service_permissions.0.restart_workspace", "false"),
-					resource.TestCheckResourceAttr(dataSourceName, "self_service_permissions.0.switch_running_mode", "true"),
-					resource.TestCheckResourceAttr(dataSourceName, "dns_ip_addresses.#", "2"),
-					resource.TestCheckResourceAttr(dataSourceName, "directory_type", "SIMPLE_AD"),
-					resource.TestCheckResourceAttrPair(dataSourceName, "directory_name", directoryResourceName, "name"),
-					resource.TestCheckResourceAttrPair(dataSourceName, "alias", directoryResourceName, "alias"),
-					resource.TestCheckResourceAttrPair(dataSourceName, "directory_id", directoryResourceName, "id"),
-					resource.TestCheckResourceAttrPair(dataSourceName, "iam_role_id", iamRoleDataSourceName, "arn"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "workspace_security_group_id"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "registration_code"),
-					resource.TestCheckResourceAttr(dataSourceName, "tags.%", "0"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "subnet_ids.#", resourceName, "subnet_ids.#"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "self_service_permissions.#", resourceName, "self_service_permissions.#"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "self_service_permissions.0.change_compute_type", resourceName, "self_service_permissions.0.change_compute_type"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "self_service_permissions.0.increase_volume_size", resourceName, "self_service_permissions.0.increase_volume_size"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "self_service_permissions.0.rebuild_workspace", resourceName, "self_service_permissions.0.rebuild_workspace"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "self_service_permissions.0.restart_workspace", resourceName, "self_service_permissions.0.restart_workspace"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "self_service_permissions.0.switch_running_mode", resourceName, "self_service_permissions.0.switch_running_mode"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "dns_ip_addresses.#", resourceName, "dns_ip_addresses.#"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "directory_type", resourceName, "directory_type"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "directory_name", resourceName, "directory_name"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "alias", resourceName, "alias"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "directory_id", resourceName, "directory_id"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "iam_role_id", resourceName, "iam_role_id"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "workspace_security_group_id", resourceName, "workspace_security_group_id"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "registration_code", resourceName, "registration_code"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "tags.%", resourceName, "tags.%"),
 				),
 			},
 		},
@@ -46,7 +45,7 @@ func TestAccDataSourceAwsWorkspacesDirectory_basic(t *testing.T) {
 
 func testAccDataSourceAwsWorkspacesDirectoryConfig(rName string) string {
 	return testAccAwsWorkspacesDirectoryConfig_Prerequisites(rName) + fmt.Sprintf(`
-resource "aws_workspaces_directory" "main" {
+resource "aws_workspaces_directory" "test" {
   directory_id = "${aws_directory_service_directory.main.id}"
 
   self_service_permissions {
@@ -58,8 +57,8 @@ resource "aws_workspaces_directory" "main" {
   }
 }
 
-data "aws_workspaces_directory" "main" {
-  directory_id = "${aws_directory_service_directory.main.id}"
+data "aws_workspaces_directory" "test" {
+  directory_id = "${aws_workspaces_directory.test.id}"
 }
 
 data "aws_iam_role" "workspaces-default" {
