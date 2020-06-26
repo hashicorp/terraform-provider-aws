ae11dde05d453e9081319cca0077a6d35231b805
Kit Ewbank <Kit_Ewbank@hotmail.com>
2020-06-26 15:53:32 -0400

    r/aws_route: Add 'TestAccAWSRoute_IPv4_To_NetworkInterface'.
    Acceptance test output:
    
    $ make testacc TEST=./aws/ TESTARGS='-run=TestAccAWSRoute_IPv4_To_NetworkInterface'
    ==> Checking that code complies with gofmt requirements...
    TF_ACC=1 go test ./aws/ -v -count 1 -parallel 20 -run=TestAccAWSRoute_IPv4_To_NetworkInterface -timeout 120m
    === RUN   TestAccAWSRoute_IPv4_To_NetworkInterface
    === PAUSE TestAccAWSRoute_IPv4_To_NetworkInterface
    === CONT  TestAccAWSRoute_IPv4_To_NetworkInterface
    --- PASS: TestAccAWSRoute_IPv4_To_NetworkInterface (47.04s)
    PASS
    ok  	github.com/terraform-providers/terraform-provider-aws/aws	47.100s

--- aws/resource_aws_route_test.go
+++ aws/resource_aws_route_test.go
@@ -309,6 +309,39 @@ func TestAccAWSRoute_IPv4_To_Instance(t *testing.T) {
 	})
 }
 
+func TestAccAWSRoute_IPv4_To_NetworkInterface(t *testing.T) {
+	var route ec2.Route
+	resourceName := "aws_route.test"
+	eniResourceName := "aws_network_interface.test"
+	rName := acctest.RandomWithPrefix("tf-acc-test")
+	destinationCidr := "10.3.0.0/16"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAWSRouteDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAWSRouteConfigIpv4NetworkInterface(rName, destinationCidr),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAWSRouteExists(resourceName, &route),
+					testAccCheckAWSRouteNetworkInterfaceRoute(eniResourceName, &route),
+					resource.TestCheckResourceAttr(resourceName, "destination_cidr_block", destinationCidr),
+					resource.TestCheckResourceAttr(resourceName, "destination_ipv6_cidr_block", ""),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateIdFunc: testAccAWSRouteImportStateIdFunc(resourceName),
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
 func TestAccAWSRoute_DoesNotCrashWithVpcEndpoint(t *testing.T) {
 	var route ec2.Route
 	var routeTable ec2.RouteTable
@@ -1095,9 +1128,65 @@ resource "aws_route_table" "test" {
 }
 
 resource "aws_route" "test" {
-  route_table_id              = aws_route_table.test.id
-  destination_cidr_block      = %[2]q
-  instance_id                 = aws_instance.test.id
+  route_table_id         = aws_route_table.test.id
+  destination_cidr_block = %[2]q
+  instance_id            = aws_instance.test.id
 }
 `, rName, destinationCidr))
+}
+
+func testAccAWSRouteConfigIpv4NetworkInterface(rName, destinationCidr string) string {
+	return fmt.Sprintf(`
+data "aws_availability_zones" "current" {
+  # Exclude usw2-az4 (us-west-2d) as it has limited instance types.
+  blacklisted_zone_ids = ["usw2-az4"]
+  state                = "available"
+
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+resource "aws_vpc" "test" {
+  cidr_block = "10.1.0.0/16"
+
+  tags = {
+    Name = %[1]q
+  }
+}
+
+resource "aws_subnet" "test" {
+  cidr_block        = "10.1.1.0/24"
+  vpc_id            = aws_vpc.test.id
+  availability_zone = data.aws_availability_zones.current.names[0]
+
+  tags = {
+    Name = %[1]q
+  }
+}
+
+resource "aws_network_interface" "test" {
+  subnet_id = aws_subnet.test.id
+
+  tags = {
+    Name = %[1]q
+  }
+}
+
+
+resource "aws_route_table" "test" {
+  vpc_id = aws_vpc.test.id
+
+  tags = {
+    Name = %[1]q
+  }
+}
+
+resource "aws_route" "test" {
+  route_table_id         = aws_route_table.test.id
+  destination_cidr_block = %[2]q
+  network_interface_id   = aws_network_interface.test.id
+}
+`, rName, destinationCidr)
 }
