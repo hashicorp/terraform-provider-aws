a0d6c6a8cc2bae5ad5a747bcaa01d681f6d0a640
Alex Pilon <apilon@hashicorp.com>
2020-06-25 22:31:43 -0400

    Fix import

--- aws/resource_aws_dx_gateway_association_test.go
+++ aws/resource_aws_dx_gateway_association_test.go
@@ -13,6 +13,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/terraform-providers/terraform-provider-aws/aws/internal/tfawsresource"
 )
 
 func init() {
