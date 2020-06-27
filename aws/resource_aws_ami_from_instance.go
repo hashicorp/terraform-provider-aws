46a91018ec5c7102ef0bb72bac0612074343e54b
DrFaust92 <ilia.lazebnik@gmail.com>
2020-06-27 21:04:38 +0300

    ami copy from instance resource add attributes

--- aws/resource_aws_ami_from_instance.go
+++ aws/resource_aws_ami_from_instance.go
@@ -177,6 +177,38 @@ func resourceAwsAmiFromInstance() *schema.Resource {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+			"usage_operation": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"platform_details": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"image_owner_alias": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"image_type": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"hypervisor": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"owner_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"platform": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"public": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
 		},
 
 		// The remaining operations are shared with the generic aws_ami resource,
@@ -202,7 +234,12 @@ func resourceAwsAmiFromInstanceCreate(d *schema.ResourceData, meta interface{}) 
 		return err
 	}
 
+<<<<<<< HEAD
 	d.SetId(aws.StringValue(res.ImageId))
+=======
+	id := aws.StringValue(res.ImageId)
+	d.SetId(id)
+>>>>>>> e196a4433 (ami copy from instance resource add attributes)
 	d.Set("manage_ebs_snapshots", true)
 
 	if v := d.Get("tags").(map[string]interface{}); len(v) > 0 {
