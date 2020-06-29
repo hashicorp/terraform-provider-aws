d8c5e5b959129e3d628710b4ad1c723afed03b72
Israel Sotomayor <zot24@users.noreply.github.com>
2020-06-29 13:23:37 -0700

    Add providers data reference

--- aws/provider.go
+++ aws/provider.go
@@ -287,6 +287,8 @@ func Provider() *schema.Provider {
 			"aws_eks_cluster":                                dataSourceAwsEksCluster(),
 			"aws_eks_clusters":                               dataSourceAwsEksClusters(),
 			"aws_eks_cluster_auth":                           dataSourceAwsEksClusterAuth(),
+			"aws_eks_node_group_names":                       dataSourceAwsEksNodeGroupNames(),
+			"aws_eks_node_group":                             dataSourceAwsEksNodeGroup(),
 			"aws_elastic_beanstalk_application":              dataSourceAwsElasticBeanstalkApplication(),
 			"aws_elastic_beanstalk_hosted_zone":              dataSourceAwsElasticBeanstalkHostedZone(),
 			"aws_elastic_beanstalk_solution_stack":           dataSourceAwsElasticBeanstalkSolutionStack(),
