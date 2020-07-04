9b910c3787eb8099ff026b11c5c30358087e2e79
DrFaust92 <ilia.lazebnik@gmail.com>
2020-07-05 00:48:00 +0300

    add disappers test + minor refactor

--- aws/resource_aws_sqs_queue_policy_test.go
+++ aws/resource_aws_sqs_queue_policy_test.go
@@ -20,9 +20,9 @@ func TestAccAWSSQSQueuePolicy_basic(t *testing.T) {
 		CheckDestroy: testAccCheckAWSSQSQueueDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccAWSSQSPolicyConfig_basic(queueName),
+				Config: testAccAWSSQSPolicyConfigBasic(queueName),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckAWSSQSQueueExists("aws_sqs_queue.q", &queueAttributes),
+					testAccCheckAWSSQSQueueExists("aws_sqs_queue.test", &queueAttributes),
 					testAccCheckAWSSQSQueueDefaultAttributes(&queueAttributes),
 					resource.TestMatchResourceAttr("aws_sqs_queue_policy.test", "policy",
 						regexp.MustCompile("^{\"Version\":\"2012-10-17\".+")),
@@ -37,31 +37,74 @@ func TestAccAWSSQSQueuePolicy_basic(t *testing.T) {
 	})
 }
 
-func testAccAWSSQSPolicyConfig_basic(r string) string {
-	return fmt.Sprintf(testAccAWSSQSPolicyConfig_basic_tpl, r)
+func TestAccAWSSQSQueuePolicy_disappears_queue(t *testing.T) {
+	var queueAttributes map[string]*string
+	resourceName := "aws_sqs_queue_policy.test"
+	queueName := fmt.Sprintf("sqs-queue-%s", acctest.RandString(5))
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAWSSQSQueueDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAWSSQSPolicyConfigBasic(queueName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAWSSQSQueueExists("aws_sqs_queue.test", &queueAttributes),
+					testAccCheckAWSSQSQueueDefaultAttributes(&queueAttributes),
+					testAccCheckResourceDisappears(testAccProvider, resourceAwsSqsQueue(), resourceName),
+				),
+				ExpectNonEmptyPlan: true,
+			},
+		},
+	})
 }
 
-const testAccAWSSQSPolicyConfig_basic_tpl = `
-resource "aws_sqs_queue" "q" {
-  name = "%s"
+func TestAccAWSSQSQueuePolicy_disappears(t *testing.T) {
+	var queueAttributes map[string]*string
+	resourceName := "aws_sqs_queue_policy.test"
+	queueName := fmt.Sprintf("sqs-queue-%s", acctest.RandString(5))
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAWSSQSQueueDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAWSSQSPolicyConfigBasic(queueName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAWSSQSQueueExists("aws_sqs_queue.test", &queueAttributes),
+					testAccCheckAWSSQSQueueDefaultAttributes(&queueAttributes),
+					testAccCheckResourceDisappears(testAccProvider, resourceAwsSqsQueuePolicy(), resourceName),
+				),
+				ExpectNonEmptyPlan: true,
+			},
+		},
+	})
+}
+
+func testAccAWSSQSPolicyConfigBasic(rName string) string {
+	return fmt.Sprintf(`
+resource "aws_sqs_queue" "test" {
+  name = %[1]q
 }
 
 resource "aws_sqs_queue_policy" "test" {
-  queue_url = "${aws_sqs_queue.q.id}"
+  queue_url = "${aws_sqs_queue.test.id}"
   policy = <<POLICY
 {
-  "Version": "2012-10-17",
-  "Id": "sqspolicy",
-  "Statement": [
+  "Version":"2012-10-17",
+  "Id":"sqspolicy",
+  "Statement":[
     {
-      "Sid": "First",
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": "sqs:SendMessage",
-      "Resource": "${aws_sqs_queue.q.arn}",
-      "Condition": {
-        "ArnEquals": {
-          "aws:SourceArn": "${aws_sqs_queue.q.arn}"
+      "Sid":"First",
+      "Effect":"Allow",
+      "Principal":"*",
+      "Action":"sqs:SendMessage",
+      "Resource":"${aws_sqs_queue.test.arn}",
+      "Condition":{
+        "ArnEquals":{
+          "aws:SourceArn":"${aws_sqs_queue.test.arn}"
         }
       }
     }
@@ -69,4 +112,5 @@ resource "aws_sqs_queue_policy" "test" {
 }
 POLICY
 }
-`
+`, rName)
+}
