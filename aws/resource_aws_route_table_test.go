c2e0693513e51d4599382ad89d539be60e18f150
Kit Ewbank <Kit_Ewbank@hotmail.com>
2020-07-15 16:29:34 -0400

    r/aws_route_table: Use Amazon NAT instance AMI for instance tests.
    Acceptance test output:
    
    $ make testacc TEST=./aws/ TESTARGS='-run=TestAccAWSRouteTable_IPv4_To_Instance'
    ==> Checking that code complies with gofmt requirements...
    TF_ACC=1 go test ./aws/ -v -count 1 -parallel 20 -run=TestAccAWSRouteTable_IPv4_To_Instance -timeout 120m
    === RUN   TestAccAWSRouteTable_IPv4_To_Instance
    === PAUSE TestAccAWSRouteTable_IPv4_To_Instance
    === CONT  TestAccAWSRouteTable_IPv4_To_Instance
    --- PASS: TestAccAWSRouteTable_IPv4_To_Instance (109.91s)
    PASS
    ok  	github.com/terraform-providers/terraform-provider-aws/aws	110.478s
    $ make testacc TEST=./aws/ TESTARGS='-run=TestAccAWSRouteTable_MultipleRoutes'
    ==> Checking that code complies with gofmt requirements...
    TF_ACC=1 go test ./aws/ -v -count 1 -parallel 20 -run=TestAccAWSRouteTable_MultipleRoutes -timeout 120m
    === RUN   TestAccAWSRouteTable_MultipleRoutes
    === PAUSE TestAccAWSRouteTable_MultipleRoutes
    === CONT  TestAccAWSRouteTable_MultipleRoutes
    --- PASS: TestAccAWSRouteTable_MultipleRoutes (204.15s)
    PASS
    ok  	github.com/terraform-providers/terraform-provider-aws/aws	204.201s

--- aws/resource_aws_route_table_test.go
+++ aws/resource_aws_route_table_test.go
@@ -1273,7 +1273,7 @@ resource "aws_route_table" "test" {
 
 func testAccAWSRouteTableConfigIpv4Instance(rName, destinationCidr string) string {
 	return composeConfig(
-		testAccLatestAmazonLinuxHvmEbsAmiConfig(),
+		testAccLatestAmazonNatInstanceAmiConfig(),
 		testAccAvailableEc2InstanceTypeForRegion("t3.micro", "t2.micro"),
 		fmt.Sprintf(`
 data "aws_availability_zones" "available" {
@@ -1306,7 +1306,7 @@ resource "aws_subnet" "test" {
 }
 
 resource "aws_instance" "test" {
-  ami           = data.aws_ami.amzn-ami-minimal-hvm-ebs.id
+  ami           = data.aws_ami.amzn-ami-nat-instance.id
   instance_type = data.aws_ec2_instance_type_offering.available.instance_type
   subnet_id     = aws_subnet.test.id
 
@@ -2110,7 +2110,7 @@ func testAccAWSRouteTableConfigMultipleRoutes(rName,
 	destinationAttr2, destinationValue2, targetAttribute2, targetValue2,
 	destinationAttr3, destinationValue3, targetAttribute3, targetValue3 string) string {
 	return composeConfig(
-		testAccLatestAmazonLinuxHvmEbsAmiConfig(),
+		testAccLatestAmazonNatInstanceAmiConfig(),
 		testAccAvailableEc2InstanceTypeForRegion("t3.micro", "t2.micro"),
 		fmt.Sprintf(`
 data "aws_availability_zones" "available" {
@@ -2168,7 +2168,7 @@ resource "aws_internet_gateway" "test" {
 }
 
 resource "aws_instance" "test" {
-  ami           = data.aws_ami.amzn-ami-minimal-hvm-ebs.id
+  ami           = data.aws_ami.amzn-ami-nat-instance.id
   instance_type = data.aws_ec2_instance_type_offering.available.instance_type
   subnet_id     = aws_subnet.test.id
 
@@ -2208,4 +2208,22 @@ resource "aws_route_table" "test" {
   }
 }
 `, rName, destinationAttr1, destinationValue1, targetAttribute1, targetValue1, destinationAttr2, destinationValue2, targetAttribute2, targetValue2, destinationAttr3, destinationValue3, targetAttribute3, targetValue3))
+}
+
+// testAccLatestAmazonNatInstanceAmiConfig returns the configuration for a data source that
+// describes the latest Amazon NAT instance AMI.
+// See https://docs.aws.amazon.com/vpc/latest/userguide/VPC_NAT_Instance.html#nat-instance-ami.
+// The data source is named 'amzn-ami-nat-instance'.
+func testAccLatestAmazonNatInstanceAmiConfig() string {
+	return fmt.Sprintf(`
+data "aws_ami" "amzn-ami-nat-instance" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-vpc-nat-*"]
+  }
+}
+`)
 }
