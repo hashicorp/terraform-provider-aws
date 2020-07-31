8f53c4819502f6f4d418275fbc75bd5ba4e8d044
Ilia Lazebnik <Ilia.lazebnik@gmail.com>
2020-07-31 18:28:59 +0300

    data-source/aws_ecr_repository: Make `registry_id` optional to allow to filter by account + add missing attributes (#14368)
    Output from acceptance testing:
    
    ```
    --- PASS: TestAccAWSEcrRepositoryDataSource_nonExistent (1.90s)
    --- PASS: TestAccAWSEcrRepositoryDataSource_basic (8.77s)
    ```

--- aws/data_source_aws_ecr_repository_test.go
+++ aws/data_source_aws_ecr_repository_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 )
 
-func TestAccAWSEcrDataSource_ecrRepository(t *testing.T) {
+func TestAccAWSEcrRepositoryDataSource_basic(t *testing.T) {
 	rName := acctest.RandomWithPrefix("tf-acc-test")
 	resourceName := "aws_ecr_repository.test"
 	dataSourceName := "data.aws_ecr_repository.test"
@@ -25,13 +25,15 @@ func TestAccAWSEcrDataSource_ecrRepository(t *testing.T) {
 					resource.TestCheckResourceAttrPair(resourceName, "registry_id", dataSourceName, "registry_id"),
 					resource.TestCheckResourceAttrPair(resourceName, "repository_url", dataSourceName, "repository_url"),
 					resource.TestCheckResourceAttrPair(resourceName, "tags", dataSourceName, "tags"),
+					resource.TestCheckResourceAttrPair(resourceName, "image_scanning_configuration.#", dataSourceName, "image_scanning_configuration.#"),
+					resource.TestCheckResourceAttrPair(resourceName, "image_tag_mutability", dataSourceName, "image_tag_mutability"),
 				),
 			},
 		},
 	})
 }
 
-func TestAccAWSEcrDataSource_nonExistent(t *testing.T) {
+func TestAccAWSEcrRepositoryDataSource_nonExistent(t *testing.T) {
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
