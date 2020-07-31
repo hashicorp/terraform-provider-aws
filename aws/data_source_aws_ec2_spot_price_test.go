dafa7b0d82e7eac270c201cbb17f715e4944a1ce
Alex Pilon <apilon@hashicorp.com>
2020-07-31 11:29:23 -0400

    Upgrade to TF SDK V2

--- aws/data_source_aws_ec2_spot_price_test.go
+++ aws/data_source_aws_ec2_spot_price_test.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
 func TestAccAwsEc2SpotPriceDataSource_basic(t *testing.T) {
