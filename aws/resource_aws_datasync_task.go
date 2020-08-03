2257a26940c47c00db34de7b1c37ff59d1f7104e
DrFaust92 <ilia.lazebnik@gmail.com>
2020-08-03 18:36:17 +0300

    fix log group update

--- aws/resource_aws_datasync_task.go
+++ aws/resource_aws_datasync_task.go
@@ -300,7 +300,7 @@ func resourceAwsDataSyncTaskUpdate(d *schema.ResourceData, meta interface{}) err
 	}
 
 	if d.HasChanges("cloudwatch_log_group_arn") {
-		input.CloudWatchLogGroupArn = aws.String(d.Get("name").(string))
+		input.CloudWatchLogGroupArn = aws.String(d.Get("cloudwatch_log_group_arn").(string))
 		needsUpdate = true
 	}
 
