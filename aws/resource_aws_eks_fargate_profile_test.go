a7d34b8a9c15d187ea797c7f4cc70131b0260074
Wedge Jarrad <wedge.jarrad@gmail.com>
2020-08-10 11:29:20 -0700

    service/eks: Update EKS tests for new versions and supported regions (#14533)
    * Update EKS Fargate supported regions
    
    Per https://docs.aws.amazon.com/eks/latest/userguide/fargate.html.
    
    * Fix failing EKS test - v1.13 no longer supported
    
    Per https://docs.aws.amazon.com/eks/latest/userguide/kubernetes-versions.html
    Kubernetes 1.13 is no longer supported and 1.14 is not long for this world.
    Update test to create a cluster at 1.16 and upgrade to 1.17 which are the two
    newest versions supported as of 2020-08-09.

--- aws/resource_aws_eks_fargate_profile_test.go
+++ aws/resource_aws_eks_fargate_profile_test.go
@@ -317,9 +317,13 @@ func testAccPreCheckAWSEksFargateProfile(t *testing.T) {
 	// we take the least desirable approach of hardcoding allowed regions.
 	allowedRegions := []string{
 		"ap-northeast-1",
+		"ap-southeast-1",
+		"ap-southeast-2",
+		"eu-central-1",
 		"eu-west-1",
 		"us-east-1",
 		"us-east-2",
+		"us-west-2",
 	}
 	region := testAccProvider.Meta().(*AWSClient).region
 
