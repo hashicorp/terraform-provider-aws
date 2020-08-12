df76ebac1ab3bc1f7e292c5d7d7b55fd81610a44
DrFaust92 <ilia.lazebnik@gmail.com>
2020-08-12 18:53:16 +0300

    lint

--- aws/data_source_aws_lb.go
+++ aws/data_source_aws_lb.go
@@ -9,7 +9,6 @@ import (
 	"github.com/aws/aws-sdk-go/service/elbv2"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/hashcode"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/hashcode"
 )
 
 func dataSourceAwsLb() *schema.Resource {
