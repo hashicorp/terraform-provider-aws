5c79926d44897cfbbde6165d4c2bb8b882f50213
Ilia Lazebnik <Ilia.lazebnik@gmail.com>
2020-08-12 16:36:44 +0300

    Update data_source_aws_lb.go

--- aws/data_source_aws_lb.go
+++ aws/data_source_aws_lb.go
@@ -9,6 +9,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/elbv2"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/hashcode"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/terraform-providers/terraform-provider-aws/aws/internal/hashcode"
 )
 
 func dataSourceAwsLb() *schema.Resource {
