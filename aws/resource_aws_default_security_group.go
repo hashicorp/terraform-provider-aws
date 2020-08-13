0dc54ffb25055ef6f62bfbd476bc2d7544bb473c
Graham Davison <gdavison@hashicorp.com>
2020-08-13 16:59:28 -0700

    Adds state migration function, returns errors instead of warnings for setting rule state

--- aws/resource_aws_default_security_group.go
+++ aws/resource_aws_default_security_group.go
@@ -10,6 +10,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws/arn"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/terraform-providers/terraform-provider-aws/aws/internal/hashcode"
 	"github.com/terraform-providers/terraform-provider-aws/aws/internal/keyvaluetags"
 	"github.com/terraform-providers/terraform-provider-aws/aws/internal/service/ec2/finder"
@@ -28,6 +29,7 @@ func resourceAwsDefaultSecurityGroup() *schema.Resource {
 		},
 
 		SchemaVersion: 1,
+		MigrateState:  resourceAwsDefaultSecurityGroupMigrateState,
 
 		Schema: map[string]*schema.Schema{
 			"name": {
@@ -293,15 +295,15 @@ func resourceAwsDefaultSecurityGroupRead(d *schema.ResourceData, meta interface{
 	d.Set("vpc_id", group.VpcId)
 
 	if err := d.Set("ingress", ingressRules); err != nil {
-		log.Printf("[WARN] Error setting Ingress rule set for (%s): %s", d.Id(), err)
+		return fmt.Errorf("error setting Ingress rule set for (%s): %w", d.Id(), err)
 	}
 
 	if err := d.Set("egress", egressRules); err != nil {
-		log.Printf("[WARN] Error setting Egress rule set for (%s): %s", d.Id(), err)
+		return fmt.Errorf("error setting Egress rule set for (%s): %w", d.Id(), err)
 	}
 
 	if err := d.Set("tags", keyvaluetags.Ec2KeyValueTags(group.Tags).IgnoreAws().IgnoreConfig(ignoreTagsConfig).Map()); err != nil {
-		return fmt.Errorf("error setting tags: %s", err)
+		return fmt.Errorf("error setting tags for (%s): %w", d.Id(), err)
 	}
 
 	return nil
@@ -452,4 +454,30 @@ func resourceAwsDefaultSecurityGroupRuleHash(v interface{}) int {
 	}
 
 	return hashcode.String(buf.String())
+}
+
+func resourceAwsDefaultSecurityGroupMigrateState(
+	v int, is *terraform.InstanceState, meta interface{}) (*terraform.InstanceState, error) {
+	switch v {
+	case 0:
+		log.Println("[INFO] Found AWS Default Security Group state v0; migrating to v1")
+		return migrateAwsDefaultSecurityGroupStateV0toV1(is)
+	default:
+		return is, fmt.Errorf("Unexpected schema version: %d", v)
+	}
+}
+
+func migrateAwsDefaultSecurityGroupStateV0toV1(is *terraform.InstanceState) (*terraform.InstanceState, error) {
+	if is.Empty() || is.Attributes == nil {
+		log.Println("[DEBUG] Empty InstanceState; nothing to migrate.")
+		return is, nil
+	}
+
+	log.Printf("[DEBUG] Attributes before migration: %#v", is.Attributes)
+
+	// set default for revoke_rules_on_delete
+	is.Attributes["revoke_rules_on_delete"] = "false"
+
+	log.Printf("[DEBUG] Attributes after migration: %#v", is.Attributes)
+	return is, nil
 }
--- aws/resource_aws_default_security_group_test.go
+++ aws/resource_aws_default_security_group_test.go
@@ -47,7 +47,7 @@ func TestAccAWSDefaultSecurityGroup_Vpc_basic(t *testing.T) {
 						"cidr_blocks.0": "10.0.0.0/8",
 					}),
 					testAccCheckAWSDefaultSecurityGroupARN(resourceName, &group),
-					testAccCheckAWSDefaultSecurityGroupOwnerID(resourceName),
+					testAccCheckResourceAttrAccountID(resourceName, "owner_id"),
 					resource.TestCheckResourceAttr(resourceName, "tags.%", "1"),
 					resource.TestCheckResourceAttr(resourceName, "tags.Name", "tf-acc-test"),
 				),
@@ -102,7 +102,7 @@ func TestAccAWSDefaultSecurityGroup_Classic_basic(t *testing.T) {
 	var group ec2.SecurityGroup
 	resourceName := "aws_default_security_group.test"
 
-	resource.ParallelTest(t, resource.TestCase{
+	resource.Test(t, resource.TestCase{
 		PreCheck:      func() { testAccPreCheck(t); testAccEC2ClassicPreCheck(t) },
 		IDRefreshName: resourceName,
 		Providers:     testAccProviders,
@@ -125,7 +125,7 @@ func TestAccAWSDefaultSecurityGroup_Classic_basic(t *testing.T) {
 					}),
 					resource.TestCheckResourceAttr(resourceName, "egress.#", "0"),
 					testAccCheckAWSDefaultSecurityGroupARN(resourceName, &group),
-					testAccCheckAWSDefaultSecurityGroupOwnerID(resourceName),
+					testAccCheckResourceAttrAccountID(resourceName, "owner_id"),
 					resource.TestCheckResourceAttr(resourceName, "tags.%", "1"),
 					resource.TestCheckResourceAttr(resourceName, "tags.Name", "tf-acc-test"),
 				),
@@ -144,34 +144,36 @@ func TestAccAWSDefaultSecurityGroup_Classic_basic(t *testing.T) {
 	})
 }
 
-// This test currently fails because it does not clear tags when adopting the resource, resulting
-// in a non-empty plan.
-// https://github.com/terraform-providers/terraform-provider-aws/issues/14631
-// func TestAccAWSDefaultSecurityGroup_Classic_empty(t *testing.T) {
-// 	oldvar := os.Getenv("AWS_DEFAULT_REGION")
-// 	os.Setenv("AWS_DEFAULT_REGION", "us-east-1")
-// 	defer os.Setenv("AWS_DEFAULT_REGION", oldvar)
+func TestAccAWSDefaultSecurityGroup_Classic_empty(t *testing.T) {
 
-// 	var group ec2.SecurityGroup
-// 	resourceName := "aws_default_security_group.test"
+	TestAccSkip(t, "This resource does not currently clear tags when adopting the resource")
+	// Additional references:
+	//  * https://github.com/terraform-providers/terraform-provider-aws/issues/14631
 
-// 	resource.ParallelTest(t, resource.TestCase{
-// 		PreCheck:      func() { testAccPreCheck(t); testAccEC2ClassicPreCheck(t) },
-// 		IDRefreshName: resourceName,
-// 		Providers:     testAccProviders,
-// 		CheckDestroy:  testAccCheckAWSDefaultSecurityGroupDestroy,
-// 		Steps: []resource.TestStep{
-// 			{
-// 				Config: testAccAWSDefaultSecurityGroupConfig_Classic_empty,
-// 				Check: resource.ComposeTestCheckFunc(
-// 					testAccCheckAWSDefaultSecurityGroupExists(resourceName, &group),
-// 					resource.TestCheckResourceAttr(resourceName, "ingress.#", "0"),
-// 					resource.TestCheckResourceAttr(resourceName, "egress.#", "0"),
-// 				),
-// 			},
-// 		},
-// 	})
-// }
+	oldvar := os.Getenv("AWS_DEFAULT_REGION")
+	os.Setenv("AWS_DEFAULT_REGION", "us-east-1")
+	defer os.Setenv("AWS_DEFAULT_REGION", oldvar)
+
+	var group ec2.SecurityGroup
+	resourceName := "aws_default_security_group.test"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:      func() { testAccPreCheck(t); testAccEC2ClassicPreCheck(t) },
+		IDRefreshName: resourceName,
+		Providers:     testAccProviders,
+		CheckDestroy:  testAccCheckAWSDefaultSecurityGroupDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAWSDefaultSecurityGroupConfig_Classic_empty,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAWSDefaultSecurityGroupExists(resourceName, &group),
+					resource.TestCheckResourceAttr(resourceName, "ingress.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "egress.#", "0"),
+				),
+			},
+		},
+	})
+}
 
 func testAccCheckAWSDefaultSecurityGroupDestroy(s *terraform.State) error {
 	// We expect Security Group to still exist
@@ -210,12 +212,6 @@ func testAccCheckAWSDefaultSecurityGroupExists(n string, group *ec2.SecurityGrou
 func testAccCheckAWSDefaultSecurityGroupARN(resourceName string, group *ec2.SecurityGroup) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		return testAccCheckResourceAttrRegionalARN(resourceName, "arn", "ec2", fmt.Sprintf("security-group/%s", aws.StringValue(group.GroupId)))(s)
-	}
-}
-
-func testAccCheckAWSDefaultSecurityGroupOwnerID(resourceName string) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		return resource.TestCheckResourceAttr(resourceName, "owner_id", testAccGetAccountID())(s)
 	}
 }
 
@@ -280,8 +276,72 @@ resource "aws_default_security_group" "test" {
 }
 `
 
-// const testAccAWSDefaultSecurityGroupConfig_Classic_empty = `
-// resource "aws_default_security_group" "test" {
-//   # No attributes set.
-// }
-// `
+const testAccAWSDefaultSecurityGroupConfig_Classic_empty = `
+resource "aws_default_security_group" "test" {
+  # No attributes set.
+}
+`
+
+func TestAWSDefaultSecurityGroupMigrateState(t *testing.T) {
+	cases := map[string]struct {
+		StateVersion int
+		Attributes   map[string]string
+		Expected     map[string]string
+		Meta         interface{}
+	}{
+		"v0": {
+			StateVersion: 0,
+			Attributes: map[string]string{
+				"name": "test",
+			},
+			Expected: map[string]string{
+				"name":                   "test",
+				"revoke_rules_on_delete": "false",
+			},
+		},
+	}
+
+	for tn, tc := range cases {
+		is := &terraform.InstanceState{
+			ID:         "i-abc123",
+			Attributes: tc.Attributes,
+		}
+		is, err := resourceAwsDefaultSecurityGroupMigrateState(
+			tc.StateVersion, is, tc.Meta)
+
+		if err != nil {
+			t.Fatalf("bad: %s, err: %#v", tn, err)
+		}
+
+		for k, v := range tc.Expected {
+			if is.Attributes[k] != v {
+				t.Fatalf(
+					"bad: %s\n\n expected: %#v -> %#v\n got: %#v -> %#v\n in: %#v",
+					tn, k, v, k, is.Attributes[k], is.Attributes)
+			}
+		}
+	}
+}
+
+func TestAWSDefaultSecurityGroupMigrateState_empty(t *testing.T) {
+	var is *terraform.InstanceState
+	var meta interface{}
+
+	// should handle nil
+	is, err := resourceAwsDefaultSecurityGroupMigrateState(0, is, meta)
+
+	if err != nil {
+		t.Fatalf("err: %#v", err)
+	}
+	if is != nil {
+		t.Fatalf("expected nil instancestate, got: %#v", is)
+	}
+
+	// should handle non-nil but empty
+	is = &terraform.InstanceState{}
+	_, err = resourceAwsDefaultSecurityGroupMigrateState(0, is, meta)
+
+	if err != nil {
+		t.Fatalf("err: %#v", err)
+	}
+}
