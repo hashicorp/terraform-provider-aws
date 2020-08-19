663fd99be68945cce4aa808a7246af249d54eccc
Kit Ewbank <Kit_Ewbank@hotmail.com>
2020-08-19 17:38:04 -0400

    r/aws_service_discovery_http_namespace: Correct name validation.

--- aws/validators_test.go
+++ aws/validators_test.go
@@ -3146,3 +3146,36 @@ func TestValidateRoute53ResolverName(t *testing.T) {
 		}
 	}
 }
+
+func TestValidateServiceDiscoveryNamespaceName(t *testing.T) {
+	validNames := []string{
+		"ValidName",
+		"V_-.dN01e",
+		"0",
+		".",
+		"-",
+		"_",
+		strings.Repeat("x", 1024),
+	}
+	for _, v := range validNames {
+		_, errors := validateServiceDiscoveryNamespaceName(v, "name")
+		if len(errors) != 0 {
+			t.Fatalf("%q should be a valid namespace name: %q", v, errors)
+		}
+	}
+
+	invalidNames := []string{
+		"Inval:dName",
+		"Invalid Name",
+		"*",
+		"",
+		// length > 512
+		strings.Repeat("x", 1025),
+	}
+	for _, v := range invalidNames {
+		_, errors := validateServiceDiscoveryNamespaceName(v, "name")
+		if len(errors) == 0 {
+			t.Fatalf("%q should be an invalid namespace name", v)
+		}
+	}
+}
