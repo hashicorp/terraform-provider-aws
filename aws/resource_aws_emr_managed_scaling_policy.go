c8b52093b2f244e4cd4abb528d20d04473ef1138
Max Cai <zhiyuan.cai@gmail.com>
2020-08-19 11:28:21 -0400

    add missing package

--- aws/resource_aws_emr_managed_scaling_policy.go
+++ aws/resource_aws_emr_managed_scaling_policy.go
@@ -5,6 +5,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/emr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"log"
 )
 
