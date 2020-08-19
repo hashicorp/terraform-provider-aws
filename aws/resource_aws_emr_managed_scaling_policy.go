dde888ef9dc211ed487d5449c819fd0777d186b3
Max Cai <zhiyuan.cai@gmail.com>
2020-08-19 10:00:32 -0400

    Terraform Plugin SDK V2

--- aws/resource_aws_emr_managed_scaling_policy.go
+++ aws/resource_aws_emr_managed_scaling_policy.go
@@ -6,9 +6,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/emr"
-	// "github.com/hashicorp/terraform-plugin-sdk/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	// "github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 func resourceAwsEMRManagedScalingPolicy() *schema.Resource {
