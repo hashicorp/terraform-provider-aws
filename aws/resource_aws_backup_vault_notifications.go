68bc9aa82cecfa3322ae9e233d01b6b568e4c9b2
DrFaust92 <ilia.lazebnik@gmail.com>
2020-08-23 00:26:44 +0300

    revert validation

--- aws/resource_aws_backup_vault_notifications.go
+++ aws/resource_aws_backup_vault_notifications.go
@@ -22,13 +22,10 @@ func resourceAwsBackupVaultNotifications() *schema.Resource {
 
 		Schema: map[string]*schema.Schema{
 			"backup_vault_name": {
-				Type:     schema.TypeString,
-				Required: true,
-				ForceNew: true,
-				ValidateFunc: validation.All(
-					validation.StringMatch(regexp.MustCompile(`^[a-zA-Z0-9\-\_\.]$`), "must consist of lowercase letters, numbers, and hyphens."),
-					validation.StringLenBetween(1, 50),
-				),
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringMatch(regexp.MustCompile(`^[a-zA-Z0-9\-\_\.]{1,50}$`), "must consist of lowercase letters, numbers, and hyphens."),
 			},
 			"sns_topic_arn": {
 				Type:         schema.TypeString,
