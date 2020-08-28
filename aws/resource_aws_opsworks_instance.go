db4b17448097b1aada885d04bfca6f0998318444
Angie Pinilla <angelinepinilla@gmail.com>
2020-08-27 22:37:20 -0400

    refactor opsworks_slack tests and update enumerated values

--- aws/resource_aws_opsworks_instance.go
+++ aws/resource_aws_opsworks_instance.go
@@ -46,22 +46,16 @@ func resourceAwsOpsworksInstance() *schema.Resource {
 			},
 
 			"architecture": {
-				Type:     schema.TypeString,
-				Optional: true,
-				Default:  "x86_64",
-				ValidateFunc: validation.StringInSlice([]string{
-					opsworks.ArchitectureX8664,
-					opsworks.ArchitectureI386,
-				}, false),
+				Type:         schema.TypeString,
+				Optional:     true,
+				Default:      "x86_64",
+				ValidateFunc: validation.StringInSlice(opsworks.Architecture_Values(), false),
 			},
 
 			"auto_scaling_type": {
-				Type:     schema.TypeString,
-				Optional: true,
-				ValidateFunc: validation.StringInSlice([]string{
-					opsworks.AutoScalingTypeLoad,
-					opsworks.AutoScalingTypeTimer,
-				}, false),
+				Type:         schema.TypeString,
+				Optional:     true,
+				ValidateFunc: validation.StringInSlice(opsworks.AutoScalingType_Values(), false),
 			},
 
 			"availability_zone": {
@@ -223,14 +217,11 @@ func resourceAwsOpsworksInstance() *schema.Resource {
 			},
 
 			"root_device_type": {
-				Type:     schema.TypeString,
-				Optional: true,
-				ForceNew: true,
-				Computed: true,
-				ValidateFunc: validation.StringInSlice([]string{
-					opsworks.RootDeviceTypeEbs,
-					opsworks.RootDeviceTypeInstanceStore,
-				}, false),
+				Type:         schema.TypeString,
+				Optional:     true,
+				ForceNew:     true,
+				Computed:     true,
+				ValidateFunc: validation.StringInSlice(opsworks.RootDeviceType_Values(), false),
 			},
 
 			"root_device_volume_id": {
@@ -305,14 +296,11 @@ func resourceAwsOpsworksInstance() *schema.Resource {
 			},
 
 			"virtualization_type": {
-				Type:     schema.TypeString,
-				Optional: true,
-				Computed: true,
-				ForceNew: true,
-				ValidateFunc: validation.StringInSlice([]string{
-					opsworks.VirtualizationTypeParavirtual,
-					opsworks.VirtualizationTypeHvm,
-				}, false),
+				Type:         schema.TypeString,
+				Optional:     true,
+				Computed:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringInSlice(opsworks.VirtualizationType_Values(), false),
 			},
 
 			"ebs_block_device": {
