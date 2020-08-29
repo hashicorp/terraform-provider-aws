9f8dfd33319a0d15be4c84c015ab4768c5fb5386
Rob Houghton <roberth@lt-14117.lan>
2020-08-29 15:33:20 +0100

    Updates to provider version change

--- aws/resource_aws_ecs_account_setting_default_test.go
+++ aws/resource_aws_ecs_account_setting_default_test.go
@@ -7,8 +7,8 @@ import (
 	"regexp"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 func TestAccAWSEcsAccountDefaultSetting_containerInstanceLongArnFormat(t *testing.T) {
