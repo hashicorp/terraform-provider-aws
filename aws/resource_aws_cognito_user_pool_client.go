c9a6b2a357f559f36d53aa0aeda376b0ab248e0a
DrFaust92 <ilia.lazebnik@gmail.com>
2020-09-01 16:21:43 +0300

    add `token_validity_units`

--- aws/resource_aws_cognito_user_pool_client.go
+++ aws/resource_aws_cognito_user_pool_client.go
@@ -178,6 +178,33 @@ func resourceAwsCognitoUserPoolClient() *schema.Resource {
 					),
 				},
 			},
+			"token_validity_units": {
+				Type:     schema.TypeList,
+				Optional: true,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"access_token": {
+							Type:         schema.TypeString,
+							Optional:     true,
+							Default:      cognitoidentityprovider.TimeUnitsTypeHours,
+							ValidateFunc: validation.StringInSlice(cognitoidentityprovider.TimeUnitsType_Values(), false),
+						},
+						"id_token": {
+							Type:         schema.TypeString,
+							Optional:     true,
+							Default:      cognitoidentityprovider.TimeUnitsTypeHours,
+							ValidateFunc: validation.StringInSlice(cognitoidentityprovider.TimeUnitsType_Values(), false),
+						},
+						"refresh_token": {
+							Type:         schema.TypeString,
+							Optional:     true,
+							Default:      cognitoidentityprovider.TimeUnitsTypeDays,
+							ValidateFunc: validation.StringInSlice(cognitoidentityprovider.TimeUnitsType_Values(), false),
+						},
+					},
+				},
+			},
 			"analytics_configuration": {
 				Type:     schema.TypeList,
 				Optional: true,
@@ -288,6 +315,10 @@ func resourceAwsCognitoUserPoolClientCreate(d *schema.ResourceData, meta interfa
 		params.AnalyticsConfiguration = expandAwsCognitoUserPoolClientAnalyticsConfig(v.([]interface{}))
 	}
 
+	if v, ok := d.GetOk("token_validity_units"); ok {
+		params.TokenValidityUnits = expandAwsCognitoUserPoolClientTokenValidityUnitsType(v.([]interface{}))
+	}
+
 	if v, ok := d.GetOk("prevent_user_existence_errors"); ok {
 		params.PreventUserExistenceErrors = aws.String(v.(string))
 	}
@@ -347,6 +378,10 @@ func resourceAwsCognitoUserPoolClientRead(d *schema.ResourceData, meta interface
 
 	if err := d.Set("analytics_configuration", flattenAwsCognitoUserPoolClientAnalyticsConfig(userPoolClient.AnalyticsConfiguration)); err != nil {
 		return fmt.Errorf("error setting analytics_configuration: %s", err)
+	}
+
+	if err := d.Set("token_validity_units", flattenAwsCognitoUserPoolClientTokenValidityUnitsType(userPoolClient.TokenValidityUnits)); err != nil {
+		return fmt.Errorf("error setting token_validity_units: %w", err)
 	}
 
 	return nil
@@ -422,6 +457,10 @@ func resourceAwsCognitoUserPoolClientUpdate(d *schema.ResourceData, meta interfa
 
 	if v, ok := d.GetOk("analytics_configuration"); ok {
 		params.AnalyticsConfiguration = expandAwsCognitoUserPoolClientAnalyticsConfig(v.([]interface{}))
+	}
+
+	if v, ok := d.GetOk("token_validity_units"); ok {
+		params.TokenValidityUnits = expandAwsCognitoUserPoolClientTokenValidityUnitsType(v.([]interface{}))
 	}
 
 	log.Printf("[DEBUG] Updating Cognito User Pool Client: %s", params)
@@ -521,6 +560,57 @@ func flattenAwsCognitoUserPoolClientAnalyticsConfig(analyticsConfig *cognitoiden
 
 	if analyticsConfig.ApplicationArn != nil {
 		m["application_arn"] = aws.StringValue(analyticsConfig.ApplicationArn)
+	}
+
+	return []interface{}{m}
+}
+
+func expandAwsCognitoUserPoolClientTokenValidityUnitsType(l []interface{}) *cognitoidentityprovider.TokenValidityUnitsType {
+	if len(l) == 0 {
+		return nil
+	}
+
+	m := l[0].(map[string]interface{})
+
+	tokenValidityConfig := &cognitoidentityprovider.TokenValidityUnitsType{}
+
+	if v, ok := m["access_token"]; ok {
+		tokenValidityConfig.AccessToken = aws.String(v.(string))
+	}
+
+	if v, ok := m["id_token"]; ok {
+		tokenValidityConfig.IdToken = aws.String(v.(string))
+	}
+
+	if v, ok := m["refresh_token"]; ok {
+		tokenValidityConfig.RefreshToken = aws.String(v.(string))
+	}
+
+	return tokenValidityConfig
+}
+
+func flattenAwsCognitoUserPoolClientTokenValidityUnitsType(tokenValidityConfig *cognitoidentityprovider.TokenValidityUnitsType) []interface{} {
+	if tokenValidityConfig == nil {
+		return nil
+	}
+
+	//tokenValidityConfig is never nil and if everything is empty it causes diffs
+	if tokenValidityConfig.IdToken == nil && tokenValidityConfig.AccessToken == nil && tokenValidityConfig.RefreshToken == nil {
+		return nil
+	}
+
+	m := map[string]interface{}{}
+
+	if tokenValidityConfig.IdToken != nil {
+		m["id_token"] = aws.StringValue(tokenValidityConfig.IdToken)
+	}
+
+	if tokenValidityConfig.AccessToken != nil {
+		m["access_token"] = aws.StringValue(tokenValidityConfig.AccessToken)
+	}
+
+	if tokenValidityConfig.RefreshToken != nil {
+		m["refresh_token"] = aws.StringValue(tokenValidityConfig.RefreshToken)
 	}
 
 	return []interface{}{m}
