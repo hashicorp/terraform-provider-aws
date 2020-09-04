a7c8cffa515faddd74227520726c0a4f64298a8f
DrFaust92 <ilia.lazebnik@gmail.com>
2020-09-04 23:23:30 +0300

    add updated checks

--- aws/resource_aws_cloudfront_field_level_encryption_config_test.go
+++ aws/resource_aws_cloudfront_field_level_encryption_config_test.go
@@ -45,6 +45,25 @@ func TestAccAWSCloudfrontFieldLevelEncryptionConfig_basic(t *testing.T) {
 				ImportState:       true,
 				ImportStateVerify: true,
 			},
+			{
+				Config: testAccAWSCloudfrontFieldLevelEncryptionConfigUpdated(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckCloudfrontFieldLevelEncryptionConfigExists(resourceName, &profile),
+					resource.TestCheckResourceAttr(resourceName, "comment", "some comment"),
+					resource.TestCheckResourceAttr(resourceName, "content_type_profile_config.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "content_type_profile_config.0.content_type_profile.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "content_type_profile_config.0.content_type_profile.0.content_type", "application/x-www-form-urlencoded"),
+					resource.TestCheckResourceAttr(resourceName, "content_type_profile_config.0.content_type_profile.0.format", "URLEncoded"),
+					resource.TestCheckResourceAttr(resourceName, "content_type_profile_config.0.forward_when_content_type_is_unknown", "false"),
+					resource.TestCheckResourceAttrPair(resourceName, "content_type_profile_config.0.content_type_profile.0.profile_id", profileResourceName, "id"),
+					resource.TestCheckResourceAttr(resourceName, "query_arg_profile_config.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "query_arg_profile_config.0.query_arg_profile.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "query_arg_profile_config.0.query_arg_profile.0.query_arg", "URLEncoded2"),
+					resource.TestCheckResourceAttrPair(resourceName, "query_arg_profile_config.0.query_arg_profile.0.profile_id", profileResourceName, "id"),
+					resource.TestCheckResourceAttr(resourceName, "query_arg_profile_config.0.forward_when_query_arg_is_unknown", "false"),
+					resource.TestCheckResourceAttrSet(resourceName, "etag"),
+				),
+			},
 		},
 	})
 }
@@ -166,23 +185,29 @@ resource "aws_cloudfront_field_level_encryption_config" "test" {
 `)
 }
 
-//func testAccAWSCloudfrontFieldLevelEncryptionConfigExtendedConfig(rName string) string {
-//	return fmt.Sprintf(`
-//resource "aws_cloudfront_public_key" "test" {
-//  comment     = "test key"
-//  encoded_key = file("test-fixtures/cloudfront-public-key.pem")
-//  name        = %[1]q
-//}
-//
-//resource "aws_cloudfront_field_level_encryption_profile" "test" {
-//  comment = "some other comment"
-//  name    = %[1]q
-//
-//  encryption_entities {
-//    public_key_id  = aws_cloudfront_public_key.test.id
-//    provider_id    = %[1]q
-//    field_patterns = ["FirstName", "DateOfBirth"]
-//  }
-//}
-//`, rName)
-//}
+func testAccAWSCloudfrontFieldLevelEncryptionConfigUpdated(rName string) string {
+	return testAccAWSCloudfrontFieldLevelEncryptionConfigBase(rName) + fmt.Sprintf(`
+resource "aws_cloudfront_field_level_encryption_config" "test" {
+  comment = "some comment"
+
+  content_type_profile_config {
+    forward_when_content_type_is_unknown = false
+    
+    content_type_profile {
+      content_type = "application/x-www-form-urlencoded"
+      format       = "URLEncoded"
+      profile_id   = aws_cloudfront_field_level_encryption_profile.test.id
+    }
+  }
+
+  query_arg_profile_config {
+    forward_when_query_arg_is_unknown = false
+    
+    query_arg_profile {
+      profile_id = aws_cloudfront_field_level_encryption_profile.test.id
+      query_arg  = "URLEncoded2"
+    }
+  }
+}
+`)
+}
