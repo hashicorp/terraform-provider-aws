3c87bee1f648a3c16e615f90d7944d987e3ecf98
Omarimcblack <omarimcblack@gmail.com>
2020-09-10 23:46:58 +0100

    networkmanager support Terraform Plugin SDK V2

--- aws/resource_aws_networkmanager_transit_gateway_registration.go
+++ aws/resource_aws_networkmanager_transit_gateway_registration.go
@@ -9,8 +9,8 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/arn"
 	"github.com/aws/aws-sdk-go/service/networkmanager"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 func resourceAwsNetworkManagerTransitGatewayRegistration() *schema.Resource {
--- aws/resource_aws_networkmanager_transit_gateway_registration_test.go
+++ aws/resource_aws_networkmanager_transit_gateway_registration_test.go
@@ -2,13 +2,14 @@ package aws
 
 import (
 	"fmt"
+	"log"
+	"testing"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/networkmanager"
 	"github.com/hashicorp/go-multierror"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
-	"log"
-	"testing"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 func init() {
