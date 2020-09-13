041e73c6cda7d20cf9ffb71e438f7ab6d63e608a
Fabian Heymann <fabianheymann1010@gmail.com>
2020-09-13 10:12:01 +0200

    Config Service rule: Prevent duplication on name change

--- internal/service/configservice/config_rule.go
+++ internal/service/configservice/config_rule.go
@@ -36,6 +36,7 @@ func ResourceConfigRule() *schema.Resource {
 				Type:         schema.TypeString,
 				Required:     true,
 				ValidateFunc: validation.StringLenBetween(0, 64),
+				ForceNew:     true,
 			},
 			"rule_id": {
 				Type:     schema.TypeString,
