ba127292862845dbc78e3678566166776c2d3b40
Brian Flad <bflad417@gmail.com>
2020-09-24 13:57:01 -0400

    tests/resource/aws_transfer_server: Refactor VPC endpoint testing configuration to remove error, 0.12 syntax, and formatting
    Previously:
    
    ```
    === CONT  TestAccAWSTransferServer_Vpc
        resource_aws_transfer_server_test.go:119: Step 1/3 error: terraform failed: exit status 1
    
            stderr:
    
            Error: Reference to undeclared resource
    
              on ../../../../../../../../var/folders/w8/05f3x02n27x72g0mc2jy6_180000gp/T/tftest338659464/work499087943/config361699947/terraform_plugin_test.tf line 43, in resource "aws_security_group" "test":
              43:   vpc_id      = data.aws_vpc.test.id
    
            A data resource "aws_vpc" "test" has not been declared in the root module.
    ```
    
    Output from acceptance testing:
    
    ```
    --- PASS: TestAccAWSTransferServer_Vpc (231.03s)
    ```

--- aws/resource_aws_transfer_server_test.go
+++ aws/resource_aws_transfer_server_test.go
@@ -702,81 +702,81 @@ const testAccAWSTransferServerConfig_VpcDefault = `
 data "aws_region" "current" {}
 
 resource "aws_vpc" "test" {
-	cidr_block = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
 
-	tags = {
-		Name = "terraform-testacc-vpc"
-	}
+  tags = {
+    Name = "terraform-testacc-vpc"
+  }
 }
 
 resource "aws_internet_gateway" "test" {
-	vpc_id = "${aws_vpc.test.id}"
+  vpc_id = "${aws_vpc.test.id}"
 
-	tags = {
-		Name = "terraform-testacc-igw"
-	}
+  tags = {
+    Name = "terraform-testacc-igw"
+  }
 }
 
 resource "aws_subnet" "test" {
-  vpc_id                  = "${aws_vpc.test.id}"
+  vpc_id                  = aws_vpc.test.id
   cidr_block              = "10.0.0.0/24"
   map_public_ip_on_launch = true
 
-  depends_on = ["aws_internet_gateway.test"]
+  depends_on = [aws_internet_gateway.test]
 }
 
 resource "aws_default_route_table" "test" {
-	default_route_table_id = "${aws_vpc.test.default_route_table_id}"
+  default_route_table_id = aws_vpc.test.default_route_table_id
 
-	route {
-		cidr_block = "0.0.0.0/0"
-		gateway_id = "${aws_internet_gateway.test.id}"
-	}
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.test.id
+  }
 
-	tags = {
-		Name = "terraform-testacc-subnet"
-	}
+  tags = {
+    Name = "terraform-testacc-subnet"
+  }
 }
 
 resource "aws_security_group" "test" {
-  name        = "terraform-testacc-security-group"
-  vpc_id      = data.aws_vpc.test.id
+  name   = "terraform-testacc-security-group"
+  vpc_id = aws_vpc.test.id
 
-	tags = {
-		Name = "terraform-testacc-security-group"
-	}
+  tags = {
+    Name = "terraform-testacc-security-group"
+  }
 }
 
 resource "aws_eip" "testa" {
-  vpc      = true
+  vpc = true
 }
 
 resource "aws_eip" "testb" {
-  vpc      = true
+  vpc = true
 }
 `
 
 const testAccAWSTransferServerConfig_Vpc = testAccAWSTransferServerConfig_VpcDefault + `
-
 resource "aws_transfer_server" "test" {
-	endpoint_type = "VPC"
-	endpoint_details {
-		vpc_id = "${aws_vpc.test.id}"
-		address_allocation_ids = ["${aws_eip.testa.id}"]
-		subnet_ids = ["${aws_subnet.test.id}"]
-	}
+  endpoint_type = "VPC"
+
+  endpoint_details {
+    address_allocation_ids = [aws_eip.testa.id]
+    subnet_ids             = [aws_subnet.test.id]
+    vpc_id                 = aws_vpc.test.id
+  }
 }
 `
 
 const testAccAWSTransferServerConfig_VpcUpdate = testAccAWSTransferServerConfig_VpcDefault + `
-
 resource "aws_transfer_server" "test" {
-	endpoint_type = "VPC"
-	endpoint_details {
-		vpc_id = "${aws_vpc.test.id}"
-		address_allocation_ids = ["${aws_eip.testb.id}"]
-		subnet_ids = ["${aws_subnet.test.id}"]
-	}
+  endpoint_type = "VPC"
+
+  endpoint_details {
+    address_allocation_ids = [aws_eip.testb.id]
+    subnet_ids             = [aws_subnet.test.id]
+    vpc_id                 = aws_vpc.test.id
+  }
 }
 `
 
