3f6dbdd041ca6001347ea8a96594e4391d7c3136
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2020-09-26 23:57:12 +0100

    Update imports to use v2/ of plugin SDK

--- aws/resource_aws_servicecatalog_launch_template_constraint.go
+++ aws/resource_aws_servicecatalog_launch_template_constraint.go
@@ -9,8 +9,8 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/servicecatalog"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 )
 
 func resourceAwsServiceCatalogLaunchTemplateConstraint() *schema.Resource {
