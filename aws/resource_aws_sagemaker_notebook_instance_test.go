258ac0575940118400aacda721471bd33b156aa7
drfaust92 <ilia.lazebnik@gmail.com>
2020-09-29 16:38:54 +0300

    fix tags test - the last one!

--- aws/resource_aws_sagemaker_notebook_instance_test.go
+++ aws/resource_aws_sagemaker_notebook_instance_test.go
@@ -538,7 +538,7 @@ resource "aws_sagemaker_notebook_instance" "test" {
 
   tags = {
     %[2]q = %[3]q
-    %[3]q = %[4]q
+    %[4]q = %[5]q
   }
 }
 `, rName, tagKey1, tagValue1, tagKey2, tagValue2)
