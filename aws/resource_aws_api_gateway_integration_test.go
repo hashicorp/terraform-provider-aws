9b5a0d2af8684321e60161ac72df3fa2a59ff6a5
Tadhg McGahern <tfjmcgahern@gmail.com>
2020-10-05 20:21:34 +0100

    Add TLS config to apigw integration
    Co-authored-by: jake-mcdermott <mail@jake.ie>

--- aws/resource_aws_api_gateway_integration_test.go
+++ aws/resource_aws_api_gateway_integration_test.go
@@ -820,11 +820,13 @@ resource "aws_api_gateway_integration" "test" {
   rest_api_id = aws_api_gateway_rest_api.test.id
   resource_id = aws_api_gateway_resource.test.id
   http_method = aws_api_gateway_method.test.http_method
+
   type                    = "HTTP"
   uri                     = "https://www.google.de"
   integration_http_method = "GET"
   passthrough_behavior    = "WHEN_NO_MATCH"
   content_handling        = "CONVERT_TO_TEXT"
+
   tls_config {
     insecure_skip_verification = true
   }
