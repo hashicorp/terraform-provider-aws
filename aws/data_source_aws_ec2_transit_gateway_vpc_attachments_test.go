72473444ad667c803eb0050d81f4cc3ba4d72cb6
Maarc-D <escasgorille@aol.com>
2020-10-12 11:49:46 +0200

    feat(add-data-ec2-transit-gateway-vpc-attatchments): syntax tf 0.12 for test

--- aws/data_source_aws_ec2_transit_gateway_vpc_attachments_test.go
+++ aws/data_source_aws_ec2_transit_gateway_vpc_attachments_test.go
@@ -50,9 +50,9 @@ resource "aws_vpc" "test2" {
 }
 
 resource "aws_subnet" "test" {
-  availability_zone = "${data.aws_availability_zones.available.names[0]}"
+  availability_zone = data.aws_availability_zones.available.names[0]
   cidr_block        = "10.0.0.0/24"
-  vpc_id            = "${aws_vpc.test.id}"
+  vpc_id            = aws_vpc.test.id
 
   tags = {
     Name = "tf-acc-test-ec2-transit-gateway-vpc-attachment"
@@ -60,9 +60,9 @@ resource "aws_subnet" "test" {
 }
 
 resource "aws_subnet" "test2" {
-  availability_zone = "${data.aws_availability_zones.available.names[0]}"
+  availability_zone = data.aws_availability_zones.available.names[0]
   cidr_block        = "192.168.1.0/24"
-  vpc_id            = "${aws_vpc.test2.id}"
+  vpc_id            = aws_vpc.test2.id
 
   tags = {
     Name = "tf-acc-test-ec2-transit-gateway-vpc-attachment"
@@ -72,21 +72,21 @@ resource "aws_subnet" "test2" {
 resource "aws_ec2_transit_gateway" "test" {}
 
 resource "aws_ec2_transit_gateway_vpc_attachment" "test" {
-  subnet_ids         = ["${aws_subnet.test.id}"]
-  transit_gateway_id = "${aws_ec2_transit_gateway.test.id}"
-  vpc_id             = "${aws_vpc.test.id}"
+  subnet_ids         = [aws_subnet.test.id]
+  transit_gateway_id = aws_ec2_transit_gateway.test.id
+  vpc_id             = aws_vpc.test.id
 }
 
 resource "aws_ec2_transit_gateway_vpc_attachment" "test2" {
-  subnet_ids         = ["${aws_subnet.test2.id}"]
-  transit_gateway_id = "${aws_ec2_transit_gateway.test.id}"
-  vpc_id             = "${aws_vpc.test2.id}"
+  subnet_ids         = [aws_subnet.test2.id]
+  transit_gateway_id = aws_ec2_transit_gateway.test.id
+  vpc_id             = aws_vpc.test2.id
 }
 
 data "aws_ec2_transit_gateway_vpc_attachments" "test" {
   filter {
     name   = "transit-gateway-attachment-id"
-    values = ["${aws_ec2_transit_gateway_vpc_attachments.test.id}"]
+    values = [aws_ec2_transit_gateway_vpc_attachments.test.id]
   }
 }
 `)
