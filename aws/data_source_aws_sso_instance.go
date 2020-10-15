70e789dce72ebad6281f60ee1570a9dc8f6f3e48
Alex Burck <me@alexburck.com>
2020-10-15 12:37:00 -0500

    update to use paging with data.aws_sso_instance

--- aws/data_source_aws_sso_instance.go
+++ aws/data_source_aws_sso_instance.go
@@ -31,22 +31,28 @@ func dataSourceAwsSsoInstanceRead(d *schema.ResourceData, meta interface{}) erro
 	conn := meta.(*AWSClient).ssoadminconn
 
 	log.Printf("[DEBUG] Reading AWS SSO Instances")
-	resp, err := conn.ListInstances(&ssoadmin.ListInstancesInput{})
+	instances := []*ssoadmin.InstanceMetadata{}
+	err := conn.ListInstancesPages(&ssoadmin.ListInstancesInput{}, func(page *ssoadmin.ListInstancesOutput, lastPage bool) bool {
+		if page != nil && page.Instances != nil && len(page.Instances) != 0 {
+			instances = append(instances, page.Instances...)
+		}
+		return !lastPage
+	})
 	if err != nil {
 		return fmt.Errorf("Error getting AWS SSO Instances: %s", err)
 	}
 
-	if resp == nil || len(resp.Instances) == 0 {
+	if instances == nil || len(instances) == 0 {
 		log.Printf("[DEBUG] No AWS SSO Instance found")
 		d.SetId("")
 		return nil
 	}
 
-	if len(resp.Instances) > 1 {
-		return fmt.Errorf("Found multiple AWS SSO Instances. Not sure which one to use. %s", resp.Instances)
+	if len(instances) > 1 {
+		return fmt.Errorf("Found multiple AWS SSO Instances. Not sure which one to use. %s", instances)
 	}
 
-	instance := resp.Instances[0]
+	instance := instances[0]
 	log.Printf("[DEBUG] Received AWS SSO Instance: %s", instance)
 
 	d.SetId(time.Now().UTC().String())
