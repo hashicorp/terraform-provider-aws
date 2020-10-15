26a3e3989526a8fe5d580e159e8a05c3cb397b8f
Alex Burck <me@alexburck.com>
2020-10-14 19:29:55 -0500

    fix id and import

--- aws/resource_aws_sso_assignment.go
+++ aws/resource_aws_sso_assignment.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/arn"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/ssoadmin"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -30,7 +31,7 @@ func resourceAwsSsoAssignment() *schema.Resource {
 
 		// TODO
 		Importer: &schema.ResourceImporter{
-			State: schema.ImportStatePassthrough,
+			State: resourceAwsSsoAssignmentImport,
 		},
 
 		Timeouts: &schema.ResourceTimeout{
@@ -39,11 +40,6 @@ func resourceAwsSsoAssignment() *schema.Resource {
 		},
 
 		Schema: map[string]*schema.Schema{
-			"created_date": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-
 			"instance_arn": {
 				Type:     schema.TypeString,
 				Required: true,
@@ -64,6 +60,28 @@ func resourceAwsSsoAssignment() *schema.Resource {
 				),
 			},
 
+			"target_type": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ForceNew:     true,
+				Default:      ssoadmin.TargetTypeAwsAccount,
+				ValidateFunc: validation.StringInSlice([]string{ssoadmin.TargetTypeAwsAccount}, false),
+			},
+
+			"target_id": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validateAwsAccountId,
+			},
+
+			"principal_type": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringInSlice([]string{ssoadmin.PrincipalTypeUser, ssoadmin.PrincipalTypeGroup}, false),
+			},
+
 			"principal_id": {
 				Type:     schema.TypeString,
 				Required: true,
@@ -74,26 +92,9 @@ func resourceAwsSsoAssignment() *schema.Resource {
 				),
 			},
 
-			"principal_type": {
-				Type:         schema.TypeString,
-				Required:     true,
-				ForceNew:     true,
-				ValidateFunc: validation.StringInSlice([]string{"USER", "GROUP"}, false),
-			},
-
-			"target_id": {
-				Type:         schema.TypeString,
-				Required:     true,
-				ForceNew:     true,
-				ValidateFunc: validateAwsAccountId,
-			},
-
-			"target_type": {
-				Type:         schema.TypeString,
-				Optional:     true,
-				ForceNew:     true,
-				Default:      "AWS_ACCOUNT",
-				ValidateFunc: validation.StringInSlice([]string{"AWS_ACCOUNT"}, false),
+			"created_date": {
+				Type:     schema.TypeString,
+				Computed: true,
 			},
 		},
 	}
@@ -104,18 +105,18 @@ func resourceAwsSsoAssignmentCreate(d *schema.ResourceData, meta interface{}) er
 
 	instanceArn := d.Get("instance_arn").(string)
 	permissionSetArn := d.Get("permission_set_arn").(string)
-	principalID := d.Get("principal_id").(string)
-	principalType := d.Get("principal_type").(string)
-	targetID := d.Get("target_id").(string)
 	targetType := d.Get("target_type").(string)
+	targetID := d.Get("target_id").(string)
+	principalType := d.Get("principal_type").(string)
+	principalID := d.Get("principal_id").(string)
 
 	req := &ssoadmin.CreateAccountAssignmentInput{
 		InstanceArn:      aws.String(instanceArn),
 		PermissionSetArn: aws.String(permissionSetArn),
-		PrincipalId:      aws.String(principalID),
-		PrincipalType:    aws.String(principalType),
-		TargetId:         aws.String(targetID),
 		TargetType:       aws.String(targetType),
+		TargetId:         aws.String(targetID),
+		PrincipalType:    aws.String(principalType),
+		PrincipalId:      aws.String(principalID),
 	}
 
 	log.Printf("[INFO] Creating AWS SSO Assignment")
@@ -135,14 +136,11 @@ func resourceAwsSsoAssignmentCreate(d *schema.ResourceData, meta interface{}) er
 		return waitErr
 	}
 
-	vars := []string{
-		permissionSetArn,
-		targetType,
-		targetID,
-		principalType,
-		principalID,
+	id, idErr := resourceAwsSsoAssignmentID(instanceArn, permissionSetArn, targetType, targetID, principalType, principalID)
+	if idErr != nil {
+		return idErr
 	}
-	d.SetId(strings.Join(vars, "_"))
+	d.SetId(id)
 
 	if waitResp.CreatedDate != nil {
 		d.Set("created_date", waitResp.CreatedDate.Format(time.RFC3339))
@@ -156,10 +154,10 @@ func resourceAwsSsoAssignmentRead(d *schema.ResourceData, meta interface{}) erro
 
 	instanceArn := d.Get("instance_arn").(string)
 	permissionSetArn := d.Get("permission_set_arn").(string)
-	principalID := d.Get("principal_id").(string)
-	principalType := d.Get("principal_type").(string)
-	targetID := d.Get("target_id").(string)
 	targetType := d.Get("target_type").(string)
+	targetID := d.Get("target_id").(string)
+	principalType := d.Get("principal_type").(string)
+	principalID := d.Get("principal_id").(string)
 
 	req := &ssoadmin.ListAccountAssignmentsInput{
 		InstanceArn:      aws.String(instanceArn),
@@ -182,14 +180,11 @@ func resourceAwsSsoAssignmentRead(d *schema.ResourceData, meta interface{}) erro
 	for _, accountAssignment := range resp.AccountAssignments {
 		if aws.StringValue(accountAssignment.PrincipalType) == principalType {
 			if aws.StringValue(accountAssignment.PrincipalId) == principalID {
-				vars := []string{
-					permissionSetArn,
-					targetType,
-					targetID,
-					principalType,
-					principalID,
+				id, idErr := resourceAwsSsoAssignmentID(instanceArn, permissionSetArn, targetType, targetID, principalType, principalID)
+				if idErr != nil {
+					return idErr
 				}
-				d.SetId(strings.Join(vars, "_"))
+				d.SetId(id)
 				return nil
 			}
 		}
@@ -208,18 +203,18 @@ func resourceAwsSsoAssignmentDelete(d *schema.ResourceData, meta interface{}) er
 
 	instanceArn := d.Get("instance_arn").(string)
 	permissionSetArn := d.Get("permission_set_arn").(string)
-	principalID := d.Get("principal_id").(string)
-	principalType := d.Get("principal_type").(string)
-	targetID := d.Get("target_id").(string)
 	targetType := d.Get("target_type").(string)
+	targetID := d.Get("target_id").(string)
+	principalType := d.Get("principal_type").(string)
+	principalID := d.Get("principal_id").(string)
 
 	req := &ssoadmin.DeleteAccountAssignmentInput{
 		InstanceArn:      aws.String(instanceArn),
 		PermissionSetArn: aws.String(permissionSetArn),
-		PrincipalId:      aws.String(principalID),
-		PrincipalType:    aws.String(principalType),
-		TargetId:         aws.String(targetID),
 		TargetType:       aws.String(targetType),
+		TargetId:         aws.String(targetID),
+		PrincipalType:    aws.String(principalType),
+		PrincipalId:      aws.String(principalID),
 	}
 
 	log.Printf("[INFO] Deleting AWS SSO Assignment")
@@ -243,6 +238,111 @@ func resourceAwsSsoAssignmentDelete(d *schema.ResourceData, meta interface{}) er
 
 	d.SetId("")
 	return nil
+}
+
+func resourceAwsSsoAssignmentImport(d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
+	// id = ${InstanceID}/${PermissionSetID}/${TargetType}/${TargetID}/${PrincipalType}/${PrincipalID}
+	idParts := strings.Split(d.Id(), "/")
+	if len(idParts) != 6 || idParts[0] == "" || idParts[1] == "" || idParts[2] == "" || idParts[3] == "" || idParts[4] == "" || idParts[5] == "" {
+		return nil, fmt.Errorf("Unexpected format of id (%s), expected ${InstanceID}/${PermissionSetID}/${TargetType}/${TargetID}/${PrincipalType}/${PrincipalID}", d.Id())
+	}
+
+	instanceID := idParts[0]
+	permissionSetID := idParts[1]
+	targetType := idParts[2]
+	targetID := idParts[3]
+	principalType := idParts[4]
+	principalID := idParts[5]
+
+	var err error
+
+	// arn:${Partition}:sso:::instance/${InstanceId}
+	instanceArn := arn.ARN{
+		Partition: meta.(*AWSClient).partition,
+		Service:   "sso",
+		Resource:  fmt.Sprintf("instance/%s", instanceID),
+	}.String()
+
+	// arn:${Partition}:sso:::permissionSet/${InstanceId}/${PermissionSetId}
+	permissionSetArn := arn.ARN{
+		Partition: meta.(*AWSClient).partition,
+		Service:   "sso",
+		Resource:  fmt.Sprintf("permissionSet/%s/%s", instanceID, permissionSetID),
+	}.String()
+
+	err = d.Set("instance_arn", instanceArn)
+	if err != nil {
+		return nil, err
+	}
+	err = d.Set("permission_set_arn", permissionSetArn)
+	if err != nil {
+		return nil, err
+	}
+	err = d.Set("target_type", targetType)
+	if err != nil {
+		return nil, err
+	}
+	err = d.Set("target_id", targetID)
+	if err != nil {
+		return nil, err
+	}
+	err = d.Set("principal_type", principalType)
+	if err != nil {
+		return nil, err
+	}
+	err = d.Set("principal_id", principalID)
+	if err != nil {
+		return nil, err
+	}
+
+	id, idErr := resourceAwsSsoAssignmentID(instanceArn, permissionSetArn, targetType, targetID, principalType, principalID)
+	if idErr != nil {
+		return nil, idErr
+	}
+	d.SetId(id)
+
+	return []*schema.ResourceData{d}, nil
+}
+
+func resourceAwsSsoAssignmentID(
+	instanceArn string,
+	permissionSetArn string,
+	targetType string,
+	targetID string,
+	principalType string,
+	principalID string,
+) (string, error) {
+	// arn:${Partition}:sso:::instance/${InstanceId}
+	iArn, err := arn.Parse(instanceArn)
+	if err != nil {
+		return "", err
+	}
+	iArnResourceParts := strings.Split(iArn.Resource, "/")
+	if len(iArnResourceParts) != 2 || iArnResourceParts[0] != "instance" || iArnResourceParts[1] == "" {
+		return "", fmt.Errorf("Unexpected format of ARN (%s), expected arn:${Partition}:sso:::instance/${InstanceId}", instanceArn)
+	}
+	instanceID := iArnResourceParts[1]
+
+	// arn:${Partition}:sso:::permissionSet/${InstanceId}/${PermissionSetId}
+	pArn, err := arn.Parse(permissionSetArn)
+	if err != nil {
+		return "", err
+	}
+	pArnResourceParts := strings.Split(pArn.Resource, "/")
+	if len(iArnResourceParts) != 3 || pArnResourceParts[0] != "permissionSet" || pArnResourceParts[1] == "" || pArnResourceParts[2] == "" {
+		return "", fmt.Errorf("Unexpected format of ARN (%s), expected arn:${Partition}:sso:::permissionSet/${InstanceId}/${PermissionSetId}", instanceArn)
+	}
+	permissionSetID := pArnResourceParts[2]
+
+	vars := []string{
+		instanceID,
+		permissionSetID,
+		targetType,
+		targetID,
+		principalType,
+		principalID,
+	}
+	return strings.Join(vars, "/"), nil
 }
 
 func waitForAssignmentCreation(conn *ssoadmin.SSOAdmin, instanceArn string, requestID string, timeout time.Duration) (*ssoadmin.AccountAssignmentOperationStatus, error) {
