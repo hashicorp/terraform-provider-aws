543e5d37c86b41c4f57de5bf3108fd4c903b0cb1
Kit Ewbank <Kit_Ewbank@hotmail.com>
2020-10-21 06:12:04 -0400

    Update aws/internal/service/autoscalingplans/finder/finder.go
    Co-authored-by: angie pinilla <angelinepinilla@gmail.com>

--- aws/internal/service/autoscalingplans/finder/finder.go
+++ aws/internal/service/autoscalingplans/finder/finder.go
@@ -6,7 +6,7 @@ import (
 )
 
 // ScalingPlan returns the scaling plan corresponding to the specified name and version.
-// Returns returns nil and potentially an API error if no scaling plan is found.
+// Returns nil and potentially an API error if no scaling plan is found.
 func ScalingPlan(conn *autoscalingplans.AutoScalingPlans, scalingPlanName string, scalingPlanVersion int) (*autoscalingplans.ScalingPlan, error) {
 	input := &autoscalingplans.DescribeScalingPlansInput{
 		ScalingPlanNames:   aws.StringSlice([]string{scalingPlanName}),
