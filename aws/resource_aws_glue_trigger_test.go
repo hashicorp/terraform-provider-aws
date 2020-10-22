1a2c717f5e1627a5fed23f121ee557f8826c1936
drfaust92 <ilia.lazebnik@gmail.com>
2020-10-23 00:02:12 +0300

    son of fmt

--- aws/resource_aws_glue_trigger_test.go
+++ aws/resource_aws_glue_trigger_test.go
@@ -719,16 +719,16 @@ func testAccAWSGlueTriggerConfigActionsSecurityConfiguration(rName string) strin
 	return composeConfig(testAccAWSGlueJobConfig_Required(rName), fmt.Sprintf(`
 resource "aws_glue_security_configuration" "test" {
   name = %[1]q
-  
+
   encryption_configuration {
     cloudwatch_encryption {
       cloudwatch_encryption_mode = "DISABLED"
     }
-  
+
     job_bookmarks_encryption {
       job_bookmarks_encryption_mode = "DISABLED"
     }
-  
+
     s3_encryption {
       s3_encryption_mode = "DISABLED"
     }
