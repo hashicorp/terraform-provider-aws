74ea51f4f179fd5f6925fa6162b3277b9719bed4
Piotr Kruk <piotr@kruk.co>
2020-10-25 00:23:41 +0200

    fix test

--- aws/data_source_aws_key_pair_test.go
+++ aws/data_source_aws_key_pair_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestAccDataSourceAwsKeyPair_basic(t *testing.T) {
-	keyName, publicKey := "testKey", "ssh-rsa testKey"
+	keyName, publicKey := "testKey", "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
 	resourceName := "data.aws_key_pair.default"
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
