3fd1d58bad0c1df1d1b01e83c267cb4ddf47662a
mycodeself <drafee@gmail.com>
2020-10-26 21:08:41 +0100

    fix: missing newline after argument

--- aws/resource_aws_ssm_maintenance_window_task_test.go
+++ aws/resource_aws_ssm_maintenance_window_task_test.go
@@ -817,20 +817,25 @@ resource "aws_ssm_maintenance_window_task" "test" {
     task_type = "RUN_COMMAND"
     task_arn = "AWS-RunShellScript"
     priority = 1
-    service_role_arn = aws_iam_role.test.arn}
+    service_role_arn = aws_iam_role.test.arn
     max_concurrency = "2"
     max_errors = "1"
+
     targets {
       key    = "WindowTargetIds"
       values = [aws_ssm_maintenance_window_target.test.id]
     }
+
     task_invocation_parameters {
+
       run_command_parameters {
         timeout_seconds = 600
+
         cloudwatch_output_config {
           cloudwatch_output_enabled = true
           cloudwatch_log_group_name = aws_cloudwatch_log_group.test.name
         }
+        
         parameter {
           name = "commands"
           values = ["date"]
