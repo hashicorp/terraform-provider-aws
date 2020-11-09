c87b6121b03228683b97c0cb0cbecb522b18e2e5
Brian Flad <bflad417@gmail.com>
2020-11-09 08:11:27 -0500

    tests/resource/aws_kinesis_firehose_delivery_stream: Remove extraneous comments

--- aws/resource_aws_kinesis_firehose_delivery_stream_test.go
+++ aws/resource_aws_kinesis_firehose_delivery_stream_test.go
@@ -2889,11 +2889,6 @@ resource "aws_kinesis_firehose_delivery_stream" "test" {
           parameter_value = "${aws_lambda_function.lambda_function_test.arn}:$LATEST"
         }
 
-        // parameters {
-        //   parameter_name  = "RoleArn"
-        //   parameter_value = aws_iam_role.firehose.arn
-        // }
-
         parameters {
           parameter_name  = "BufferSizeInMBs"
           parameter_value = 1
