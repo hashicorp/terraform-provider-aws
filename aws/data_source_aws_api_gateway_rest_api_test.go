62d2e652495428c1683397d5864fb3c6826f68ab
Dirk Avery <dirk.avery@gmail.com>
2020-11-12 20:33:42 -0500

    tests/provider: Add API Gateway EDGE PreCheck

--- aws/data_source_aws_api_gateway_rest_api_test.go
+++ aws/data_source_aws_api_gateway_rest_api_test.go
@@ -12,7 +12,7 @@ func TestAccDataSourceAwsApiGatewayRestApi_basic(t *testing.T) {
 	dataSourceName := "data.aws_api_gateway_rest_api.test"
 	resourceName := "aws_api_gateway_rest_api.test"
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
+		PreCheck:  func() { testAccPreCheck(t); testAccAPIGatewayTypeEDGEPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
