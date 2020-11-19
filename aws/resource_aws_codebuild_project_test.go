746cc9c18f123916b7d9d47c00dfb16964b21abf
Brian Flad <bflad417@gmail.com>
2020-11-18 22:25:26 -0500

    tests/resource/aws_codebuild_project: Remove WINDOWS_CONTAINER acceptance test (#16231)
    * tests/resource/aws_codebuild_project: Remove WINDOWS_CONTAINER acceptance test
    
    Reference: https://github.com/hashicorp/terraform-provider-aws/issues/16163
    
    The API no longer allows the creation of CodeBuild Project environments using the `WINDOWS_CONTAINER` value:
    
    ```
    === CONT  TestAccAWSCodeBuildProject_WindowsContainer
    TestAccAWSCodeBuildProject_WindowsContainer: resource_aws_codebuild_project_test.go:1282: Step 1/2 error: Error running apply: 2020/11/12 06:11:09 [DEBUG] Using modified User-Agent: Terraform/0.12.29 HashiCorp-terraform-exec/0.10.0
    Error: Error creating CodeBuild project: InvalidInputException: The environment type WINDOWS_CONTAINER is deprecated for new projects or existing project environment updates. Please consider using Windows Server 2019 instead.
    --- FAIL: TestAccAWSCodeBuildProject_WindowsContainer (20.91s)
    ```
    
    The resource should continue supporting the value in the schema validation (so existing environments can still work), however the acceptance test should be removed to prevent the recurring failure.
    
    * docs/resource/aws_codebuild_project: Denote WINDOWS_CONTAINER as deprecated and link to CodeBuild User Guide
    
    Reference: https://github.com/hashicorp/terraform-provider-aws/pull/16231#pullrequestreview-533950377

--- aws/resource_aws_codebuild_project_test.go
+++ aws/resource_aws_codebuild_project_test.go
@@ -1273,38 +1273,6 @@ func TestAccAWSCodeBuildProject_VpcConfig(t *testing.T) {
 	})
 }
 
-func TestAccAWSCodeBuildProject_WindowsContainer(t *testing.T) {
-	var project codebuild.Project
-	rName := acctest.RandomWithPrefix("tf-acc-test")
-	resourceName := "aws_codebuild_project.test"
-
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:     func() { testAccPreCheck(t); testAccPreCheckAWSCodeBuild(t) },
-		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckAWSCodeBuildProjectDestroy,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccAWSCodeBuildProjectConfig_WindowsContainer(rName),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckAWSCodeBuildProjectExists(resourceName, &project),
-					resource.TestCheckResourceAttr(resourceName, "environment.#", "1"),
-					resource.TestCheckResourceAttr(resourceName, "environment.0.compute_type", codebuild.ComputeTypeBuildGeneral1Medium),
-					resource.TestCheckResourceAttr(resourceName, "environment.0.environment_variable.#", "0"),
-					resource.TestCheckResourceAttr(resourceName, "environment.0.image", "2"),
-					resource.TestCheckResourceAttr(resourceName, "environment.0.privileged_mode", "false"),
-					resource.TestCheckResourceAttr(resourceName, "environment.0.image_pull_credentials_type", codebuild.ImagePullCredentialsTypeCodebuild),
-					resource.TestCheckResourceAttr(resourceName, "environment.0.type", codebuild.EnvironmentTypeWindowsContainer),
-				),
-			},
-			{
-				ResourceName:      resourceName,
-				ImportState:       true,
-				ImportStateVerify: true,
-			},
-		},
-	})
-}
-
 func TestAccAWSCodeBuildProject_WindowsServer2019Container(t *testing.T) {
 	var project codebuild.Project
 	rName := acctest.RandomWithPrefix("tf-acc-test")
@@ -3547,30 +3515,6 @@ resource "aws_codebuild_project" "test" {
   }
 }
 `, rName)
-}
-
-func testAccAWSCodeBuildProjectConfig_WindowsContainer(rName string) string {
-	return testAccAWSCodeBuildProjectConfig_Base_ServiceRole(rName) + fmt.Sprintf(`
-resource "aws_codebuild_project" "test" {
-  name         = "%s"
-  service_role = aws_iam_role.test.arn
-
-  artifacts {
-    type = "NO_ARTIFACTS"
-  }
-
-  environment {
-    compute_type = "BUILD_GENERAL1_MEDIUM"
-    image        = "2"
-    type         = "WINDOWS_CONTAINER"
-  }
-
-  source {
-    location = "%s"
-    type     = "GITHUB"
-  }
-}
-`, rName, testAccAWSCodeBuildGitHubSourceLocationFromEnv())
 }
 
 func testAccAWSCodeBuildProjectConfig_WindowsServer2019Container(rName string) string {
