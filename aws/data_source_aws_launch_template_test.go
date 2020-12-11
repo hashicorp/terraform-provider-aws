02f60650780fe2acdfe5244c08ee7953641250ab
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2020-12-11 09:23:45 -0600

    data-source/aws_launch_template: Support id as argument (#16457)

--- aws/data_source_aws_launch_template_test.go
+++ aws/data_source_aws_launch_template_test.go
@@ -33,6 +33,30 @@ func TestAccAWSLaunchTemplateDataSource_basic(t *testing.T) {
 	})
 }
 
+func TestAccAWSLaunchTemplateDataSource_id_basic(t *testing.T) {
+	rName := acctest.RandomWithPrefix("tf-acc-test")
+	dataSourceName := "data.aws_launch_template.test"
+	resourceName := "aws_launch_template.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAWSLaunchTemplateDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAWSLaunchTemplateDataSourceConfig_BasicId(rName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrPair(resourceName, "arn", dataSourceName, "arn"),
+					resource.TestCheckResourceAttrPair(resourceName, "default_version", dataSourceName, "default_version"),
+					resource.TestCheckResourceAttrPair(resourceName, "latest_version", dataSourceName, "latest_version"),
+					resource.TestCheckResourceAttrPair(resourceName, "name", dataSourceName, "name"),
+					resource.TestCheckResourceAttrPair(resourceName, "hibernation_options", dataSourceName, "hibernation_options"),
+				),
+			},
+		},
+	})
+}
+
 func TestAccAWSLaunchTemplateDataSource_filter_basic(t *testing.T) {
 	rName := acctest.RandomWithPrefix("tf-acc-test")
 	dataSourceName := "data.aws_launch_template.test"
@@ -252,6 +276,18 @@ resource "aws_launch_template" "test" {
 
 data "aws_launch_template" "test" {
   name = aws_launch_template.test.name
+}
+`, rName)
+}
+
+func testAccAWSLaunchTemplateDataSourceConfig_BasicId(rName string) string {
+	return fmt.Sprintf(`
+resource "aws_launch_template" "test" {
+  name = %q
+}
+
+data "aws_launch_template" "test" {
+  id = aws_launch_template.test.id
 }
 `, rName)
 }
