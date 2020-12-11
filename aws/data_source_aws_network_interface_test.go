73aba829034b4e6f42268a031e4821ea26c1766c
Kit Ewbank <Kit_Ewbank@hotmail.com>
2020-12-11 10:50:37 -0500

    d/aws_network_interface: Refactor existing acceptance tests before testing new functionality.
    Acceptance test output:
    
    $ make testacc TEST=./aws/ TESTARGS='-run=TestAccDataSourceAwsNetworkInterface_' ACCTEST_PARALLELISM=2
    ==> Checking that code complies with gofmt requirements...
    TF_ACC=1 go test ./aws -v -count 1 -parallel 2 -run=TestAccDataSourceAwsNetworkInterface_ -timeout 120m
    === RUN   TestAccDataSourceAwsNetworkInterface_basic
    === PAUSE TestAccDataSourceAwsNetworkInterface_basic
    === RUN   TestAccDataSourceAwsNetworkInterface_filters
    === PAUSE TestAccDataSourceAwsNetworkInterface_filters
    === CONT  TestAccDataSourceAwsNetworkInterface_basic
    === CONT  TestAccDataSourceAwsNetworkInterface_filters
    --- PASS: TestAccDataSourceAwsNetworkInterface_filters (57.63s)
    --- PASS: TestAccDataSourceAwsNetworkInterface_basic (57.84s)
    PASS
    ok  	github.com/terraform-providers/terraform-provider-aws/aws	57.929s

--- aws/data_source_aws_network_interface_test.go
+++ aws/data_source_aws_network_interface_test.go
@@ -9,24 +9,27 @@ import (
 )
 
 func TestAccDataSourceAwsNetworkInterface_basic(t *testing.T) {
-	rName := acctest.RandString(5)
+	datasourceName := "data.aws_network_interface.test"
+	resourceName := "aws_network_interface.test"
+	rName := acctest.RandomWithPrefix("tf-acc-test")
+
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataSourceAwsNetworkInterface_basic(rName),
+				Config: testAccDataSourceAwsNetworkInterfaceConfigBasic(rName),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("data.aws_network_interface.test", "private_ips.#", "1"),
-					resource.TestCheckResourceAttr("data.aws_network_interface.test", "security_groups.#", "1"),
-					resource.TestCheckResourceAttrPair("data.aws_network_interface.test", "private_ip", "aws_network_interface.test", "private_ip"),
-					resource.TestCheckResourceAttrSet("data.aws_network_interface.test", "availability_zone"),
-					resource.TestCheckResourceAttrPair("data.aws_network_interface.test", "description", "aws_network_interface.test", "description"),
-					resource.TestCheckResourceAttrSet("data.aws_network_interface.test", "interface_type"),
-					resource.TestCheckResourceAttrPair("data.aws_network_interface.test", "private_dns_name", "aws_network_interface.test", "private_dns_name"),
-					resource.TestCheckResourceAttrPair("data.aws_network_interface.test", "subnet_id", "aws_network_interface.test", "subnet_id"),
-					resource.TestCheckResourceAttr("data.aws_network_interface.test", "outpost_arn", ""),
-					resource.TestCheckResourceAttrSet("data.aws_network_interface.test", "vpc_id"),
+					resource.TestCheckResourceAttr(datasourceName, "private_ips.#", "1"),
+					resource.TestCheckResourceAttr(datasourceName, "security_groups.#", "1"),
+					resource.TestCheckResourceAttrPair(datasourceName, "private_ip", resourceName, "private_ip"),
+					resource.TestCheckResourceAttrSet(datasourceName, "availability_zone"),
+					resource.TestCheckResourceAttrPair(datasourceName, "description", resourceName, "description"),
+					resource.TestCheckResourceAttrSet(datasourceName, "interface_type"),
+					resource.TestCheckResourceAttrPair(datasourceName, "private_dns_name", resourceName, "private_dns_name"),
+					resource.TestCheckResourceAttrPair(datasourceName, "subnet_id", resourceName, "subnet_id"),
+					resource.TestCheckResourceAttr(datasourceName, "outpost_arn", ""),
+					resource.TestCheckResourceAttrSet(datasourceName, "vpc_id"),
 				),
 			},
 		},
@@ -34,38 +37,31 @@ func TestAccDataSourceAwsNetworkInterface_basic(t *testing.T) {
 }
 
 func TestAccDataSourceAwsNetworkInterface_filters(t *testing.T) {
-	rName := acctest.RandString(5)
+	datasourceName := "data.aws_network_interface.test"
+	rName := acctest.RandomWithPrefix("tf-acc-test")
+
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataSourceAwsNetworkInterface_filters(rName),
+				Config: testAccDataSourceAwsNetworkInterfaceConfigFilters(rName),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("data.aws_network_interface.test", "private_ips.#", "1"),
-					resource.TestCheckResourceAttr("data.aws_network_interface.test", "security_groups.#", "1"),
+					resource.TestCheckResourceAttr(datasourceName, "private_ips.#", "1"),
+					resource.TestCheckResourceAttr(datasourceName, "security_groups.#", "1"),
 				),
 			},
 		},
 	})
 }
 
-func testAccDataSourceAwsNetworkInterface_basic(rName string) string {
+func testAccDataSourceAwsNetworkInterfaceConfigBase(rName string) string {
 	return fmt.Sprintf(`
-data "aws_availability_zones" "available" {
-  state = "available"
-
-  filter {
-    name   = "opt-in-status"
-    values = ["opt-in-not-required"]
-  }
-}
-
 resource "aws_vpc" "test" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "terraform-testacc-eni-data-source-basic"
+    Name = %[1]q
   }
 }
 
@@ -75,12 +71,12 @@ resource "aws_subnet" "test" {
   vpc_id            = aws_vpc.test.id
 
   tags = {
-    Name = "tf-acc-eni-data-source-basic"
+    Name = %[1]q
   }
 }
 
 resource "aws_security_group" "test" {
-  name   = "tf-sg-%s"
+  name   = %[1]q
   vpc_id = aws_vpc.test.id
 }
 
@@ -88,59 +84,35 @@ resource "aws_network_interface" "test" {
   subnet_id       = aws_subnet.test.id
   private_ips     = ["10.0.0.50"]
   security_groups = [aws_security_group.test.id]
-}
 
-data "aws_network_interface" "test" {
-  id = aws_network_interface.test.id
+  tags = {
+    Name = %[1]q
+  }
 }
 `, rName)
 }
 
-func testAccDataSourceAwsNetworkInterface_filters(rName string) string {
-	return fmt.Sprintf(`
-data "aws_availability_zones" "available" {
-  state = "available"
-
-  filter {
-    name   = "opt-in-status"
-    values = ["opt-in-not-required"]
-  }
+func testAccDataSourceAwsNetworkInterfaceConfigBasic(rName string) string {
+	return composeConfig(
+		testAccAvailableAZsNoOptInConfig(),
+		testAccDataSourceAwsNetworkInterfaceConfigBase(rName),
+		`
+data "aws_network_interface" "test" {
+  id = aws_network_interface.test.id
+}
+`)
 }
 
-resource "aws_vpc" "test" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "terraform-testacc-eni-data-source-filters"
-  }
-}
-
-resource "aws_subnet" "test" {
-  cidr_block        = "10.0.0.0/24"
-  availability_zone = data.aws_availability_zones.available.names[0]
-  vpc_id            = aws_vpc.test.id
-
-  tags = {
-    Name = "tf-acc-eni-data-source-filters"
-  }
-}
-
-resource "aws_security_group" "test" {
-  name   = "tf-sg-%s"
-  vpc_id = aws_vpc.test.id
-}
-
-resource "aws_network_interface" "test" {
-  subnet_id       = aws_subnet.test.id
-  private_ips     = ["10.0.0.60"]
-  security_groups = [aws_security_group.test.id]
-}
-
+func testAccDataSourceAwsNetworkInterfaceConfigFilters(rName string) string {
+	return composeConfig(
+		testAccAvailableAZsNoOptInConfig(),
+		testAccDataSourceAwsNetworkInterfaceConfigBase(rName),
+		`
 data "aws_network_interface" "test" {
   filter {
     name   = "network-interface-id"
     values = [aws_network_interface.test.id]
   }
 }
-`, rName)
+`)
 }
