5f2d218e7c206b9b978e596d90a0647299396e6d
Damir Bagapov <bonzaster@gmail.com>
2020-12-14 16:53:50 +0200

    add EngineTypeRabbitmq engine type
    See https://docs.aws.amazon.com/amazon-mq/latest/developer-guide/getting-started-rabbitmq.html

--- aws/resource_aws_mq_broker.go
+++ aws/resource_aws_mq_broker.go
@@ -103,7 +103,7 @@ func resourceAwsMqBroker() *schema.Resource {
 				Required: true,
 				ForceNew: true,
 				ValidateFunc: validation.StringInSlice([]string{
-					mq.EngineTypeActivemq,
+					mq.EngineTypeActivemq, mq.EngineTypeRabbitmq,
 				}, true),
 			},
 			"engine_version": {
