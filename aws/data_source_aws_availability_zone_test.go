4d33eab5b85d504507bc23e73c0c07e47c793e58
Kit Ewbank <Kit_Ewbank@hotmail.com>
2020-12-15 09:55:08 -0500

    d/aws_availability_zone: Add Local Zone test.
    Acceptance test output:
    
    $ make testacc TEST=./aws/ TESTARGS='-run=TestAccDataSourceAwsAvailabilityZone_LocalZone'
    ==> Checking that code complies with gofmt requirements...
    TF_ACC=1 go test ./aws -v -count 1 -parallel 20 -run=TestAccDataSourceAwsAvailabilityZone_LocalZone -timeout 120m
    === RUN   TestAccDataSourceAwsAvailabilityZone_LocalZone
    === PAUSE TestAccDataSourceAwsAvailabilityZone_LocalZone
    === CONT  TestAccDataSourceAwsAvailabilityZone_LocalZone
    --- PASS: TestAccDataSourceAwsAvailabilityZone_LocalZone (10.55s)
    PASS
    ok  	github.com/terraform-providers/terraform-provider-aws/aws	10.625s

--- aws/data_source_aws_availability_zone_test.go
+++ aws/data_source_aws_availability_zone_test.go
@@ -1,6 +1,7 @@
 package aws
 
 import (
+	"fmt"
 	"regexp"
 	"testing"
 
@@ -58,6 +59,31 @@ func TestAccDataSourceAwsAvailabilityZone_Filter(t *testing.T) {
 	})
 }
 
+func TestAccDataSourceAwsAvailabilityZone_LocalZone(t *testing.T) {
+	availabilityZonesDataSourceName := "data.aws_availability_zones.available"
+	dataSourceName := "data.aws_availability_zone.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t); testAccPreCheckAWSLocalZoneAvailable(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceAwsAvailabilityZoneConfigZoneType("local-zone"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(dataSourceName, "group_name"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "name", availabilityZonesDataSourceName, "names.0"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "name_suffix"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "network_border_group"),
+					resource.TestCheckResourceAttr(dataSourceName, "opt_in_status", ec2.AvailabilityZoneOptInStatusOptedIn),
+					resource.TestCheckResourceAttr(dataSourceName, "region", testAccGetRegion()),
+					resource.TestCheckResourceAttrPair(dataSourceName, "zone_id", availabilityZonesDataSourceName, "zone_ids.0"),
+					resource.TestCheckResourceAttr(dataSourceName, "zone_type", "local-zone"),
+				),
+			},
+		},
+	})
+}
+
 func TestAccDataSourceAwsAvailabilityZone_Name(t *testing.T) {
 	availabilityZonesDataSourceName := "data.aws_availability_zones.available"
 	dataSourceName := "data.aws_availability_zone.test"
@@ -108,6 +134,31 @@ func TestAccDataSourceAwsAvailabilityZone_ZoneId(t *testing.T) {
 	})
 }
 
+func testAccPreCheckAWSLocalZoneAvailable(t *testing.T) {
+	conn := testAccProvider.Meta().(*AWSClient).ec2conn
+
+	input := &ec2.DescribeAvailabilityZonesInput{
+		Filters: buildEC2AttributeFilterList(map[string]string{
+			"zone-type":     "local-zone",
+			"opt-in-status": "opted-in",
+		}),
+	}
+
+	output, err := conn.DescribeAvailabilityZones(input)
+
+	if testAccPreCheckSkipError(err) {
+		t.Skipf("skipping acceptance testing: %s", err)
+	}
+
+	if err != nil {
+		t.Fatalf("unexpected PreCheck error: %s", err)
+	}
+
+	if output == nil || len(output.AvailabilityZones) == 0 {
+		t.Skip("skipping since no Local Zones are available")
+	}
+}
+
 func testAccDataSourceAwsAvailabilityZoneConfigAllAvailabilityZones() string {
 	return composeConfig(
 		testAccAvailableAZsNoOptInConfig(),
@@ -150,4 +201,26 @@ data "aws_availability_zone" "test" {
   zone_id = data.aws_availability_zones.available.zone_ids[0]
 }
 `)
+}
+
+func testAccDataSourceAwsAvailabilityZoneConfigZoneType(zoneType string) string {
+	return fmt.Sprintf(`
+data "aws_availability_zones" "available" {
+  state = "available"
+
+  filter {
+    name   = "zone-type"
+    values = [%[1]q]
+  }
+
+  filter {
+    name   = "opt-in-status"
+    values = ["opted-in"]
+  }
+}
+
+data "aws_availability_zone" "test" {
+  zone_id = data.aws_availability_zones.available.zone_ids[0]
+}
+`, zoneType)
 }
