417fa7e99b01fbe1444d5c97627c2c334ff16cd1
Dan Cohen <dan@devshell.co.uk>
2020-12-18 17:30:09 +0000

    implement the event bus policy resource

---
+++ aws/resource_aws_cloudwatch_event_bus_policy_test.go
@@ -0,0 +1,102 @@
+package aws
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
+	events "github.com/aws/aws-sdk-go/service/cloudwatchevents"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+func TestAccAWSCloudwatchEventBusPolicy_basic(t *testing.T) {
+	resourceName := "aws_cloudwatch_event_bus_policy.test"
+	rstring := acctest.RandString(5)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAWSCloudWatchEventBusDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAWSCloudwatchEventBusPolicyConfig(rstring),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAWSCloudwatchEventBusPolicyExists(resourceName),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccCheckAWSCloudwatchEventBusPolicyExists(pr string) resource.TestCheckFunc {
+	return func(state *terraform.State) error {
+		eventBusResource, ok := state.RootModule().Resources[pr]
+		if !ok {
+			return fmt.Errorf("Not found: %s", pr)
+		}
+
+		if eventBusResource.Primary.ID == "" {
+			return fmt.Errorf("No ID is set")
+		}
+
+		eventBusName := eventBusResource.Primary.ID
+
+		input := &events.DescribeEventBusInput{
+			Name: aws.String(eventBusName),
+		}
+
+		cloudWatchEventsConnection := testAccProvider.Meta().(*AWSClient).cloudwatcheventsconn
+		describedEventBus, err := cloudWatchEventsConnection.DescribeEventBus(input)
+
+		if err != nil {
+			return fmt.Errorf("Reading CloudWatch Events bus policy for '%s' failed: %w", pr, err)
+		}
+		if describedEventBus.Policy == nil {
+			return fmt.Errorf("Not found: %s", pr)
+		}
+
+		return nil
+	}
+}
+
+func testAccAWSCloudwatchEventBusPolicyConfig(name string) string {
+	return fmt.Sprintf(`
+resource "aws_cloudwatch_event_bus" "test" {
+  name = %[1]q
+}
+
+data "aws_iam_policy_document" "access" {
+  statement {
+	sid = "test-resource-policy"
+
+    effect = "Allow"
+
+    principals {
+      identifiers = ["ecs.amazonaws.com"]
+      type        = "Service"
+    }
+
+    actions = [
+      "events:PutEvents",
+      "events:PutRule"
+    ]
+
+    resources = [
+      aws_cloudwatch_event_bus.test.arn,
+    ]
+  }
+}
+
+resource "aws_cloudwatch_event_bus_policy" "test" {
+  policy    = data.aws_iam_policy_document.access.json
+  event_bus_name = aws_cloudwatch_event_bus.test.name
+}
+`, name)
+}
