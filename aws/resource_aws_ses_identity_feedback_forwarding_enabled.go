71c236cfe0174fa7be2524ed79b8dce16149a770
yuonoda <yuonoa95@gmail.com>
2020-12-23 21:54:10 +0900

    Created a basic structure

---
+++ aws/resource_aws_ses_identity_feedback_forwarding_enabled.go
@@ -0,0 +1,37 @@
+package aws
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceAwsSesIdentityFeedbackForwardingEnabled() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceAwsSesIdentityFeedbackForwardingEnabledCreate,
+		Read:   resourceAwsSesIdentityFeedbackForwardingEnabledRead,
+		Delete: resourceAwsSesIdentityFeedbackForwardingEnabledDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"identity": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"enabled": {
+				Type:     schema.TypeBool,
+				Required: true,
+			},
+		},
+	}
+}
+
+func resourceAwsSesIdentityFeedbackForwardingEnabledCreate(d *schema.ResourceData, meta interface{}) error {
+}
+
+func resourceAwsSesIdentityFeedbackForwardingEnabledRead(d *schema.ResourceData, meta interface{}) error {
+}
+
+func resourceAwsSesIdentityFeedbackForwardingEnabledDelete(d *schema.ResourceData, meta interface{}) error {
+
+}
