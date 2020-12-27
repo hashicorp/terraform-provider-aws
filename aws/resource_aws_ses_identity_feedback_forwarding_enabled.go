a741d75662bcef13c14d8cf5c377e683556fa79a
yuonoda <yuonoa95@gmail.com>
2020-12-27 12:31:30 +0900

    Created a resource create function

--- aws/resource_aws_ses_identity_feedback_forwarding_enabled.go
+++ aws/resource_aws_ses_identity_feedback_forwarding_enabled.go
@@ -1,13 +1,19 @@
 package aws
 
 import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/arn"
+	"github.com/aws/aws-sdk-go/service/ses"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"log"
+	"strings"
 )
 
 func resourceAwsSesIdentityFeedbackForwardingEnabled() *schema.Resource {
 	return &schema.Resource{
-		//Create: resourceAwsSesIdentityFeedbackForwardingEnabledCreate,
-		//Read:   resourceAwsSesIdentityFeedbackForwardingEnabledRead,
+		Create: resourceAwsSesIdentityFeedbackForwardingEnabledCreate,
+		Read:   resourceAwsSesIdentityFeedbackForwardingEnabledRead,
 		//Delete: resourceAwsSesIdentityFeedbackForwardingEnabledDelete,
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
@@ -26,11 +32,64 @@ func resourceAwsSesIdentityFeedbackForwardingEnabled() *schema.Resource {
 	}
 }
 
-//func resourceAwsSesIdentityFeedbackForwardingEnabledCreate(d *schema.ResourceData, meta interface{}) error {
-//}
-//
-//func resourceAwsSesIdentityFeedbackForwardingEnabledRead(d *schema.ResourceData, meta interface{}) error {
-//}
+func resourceAwsSesIdentityFeedbackForwardingEnabledCreate(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*AWSClient).sesconn
+
+	domainName := d.Get("domain").(string)
+	enabled := d.Get("enabled").(bool)
+
+	input := &ses.SetIdentityFeedbackForwardingEnabledInput{
+		Identity:          aws.String(domainName),
+		ForwardingEnabled: aws.Bool(enabled),
+	}
+
+	_, err := conn.SetIdentityFeedbackForwardingEnabled(input)
+	if err != nil {
+		return fmt.Errorf("Error setting Feedback Forwarding domain: %s", err)
+	}
+
+	d.SetId(domainName)
+
+	return resourceAwsSesIdentityFeedbackForwardingEnabledRead(d, meta)
+}
+
+func resourceAwsSesIdentityFeedbackForwardingEnabledRead(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*AWSClient).sesconn
+
+	email := d.Id()
+	d.Set("identity", domain)
+
+	//readOpts := &ses.GetIdentityVerificationAttributesInput{
+	//	Identities: []*string{
+	//		aws.String(email),
+	//	},
+	//}
+	//
+	//response, err := conn.GetIdentityVerificationAttributes(readOpts)
+	//if err != nil {
+	//	log.Printf("[WARN] Error fetching identity verification attributes for %s: %s", d.Id(), err)
+	//	return err
+	//}
+
+	//_, ok := response.VerificationAttributes[email]
+	//if !ok {
+	//	log.Printf("[WARN] Email not listed in response when fetching verification attributes for %s", d.Id())
+	//	d.SetId("")
+	//	return nil
+	//}
+
+	//arn := arn.ARN{
+	//	AccountID: meta.(*AWSClient).accountid,
+	//	Partition: meta.(*AWSClient).partition,
+	//	Region:    meta.(*AWSClient).region,
+	//	Resource:  fmt.Sprintf("identity/%s", d.Id()),
+	//	Service:   "ses",
+	//}.String()
+	//d.Set("arn", arn)
+	d.Set("")
+	return nil
+}
+
 //
 //func resourceAwsSesIdentityFeedbackForwardingEnabledDelete(d *schema.ResourceData, meta interface{}) error {
 //
