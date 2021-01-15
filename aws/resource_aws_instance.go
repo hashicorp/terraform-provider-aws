14c19280a95e6cadf6aa6d5dfbff79bed9a87ca4
Dirk Avery <dirk.avery@gmail.com>
2021-01-15 13:29:07 -0500

    resource/instance: Adjust volume tags logic

--- aws/resource_aws_instance.go
+++ aws/resource_aws_instance.go
@@ -1790,7 +1790,7 @@ func readBlockDevicesFromInstance(d *schema.ResourceData, instance *ec2.Instance
 		if instanceBd.DeviceName != nil {
 			bd["device_name"] = aws.StringValue(instanceBd.DeviceName)
 		}
-		if _, ok := d.GetOk("volume_tags"); !ok && vol.Tags != nil {
+		if v, ok := d.GetOk("volume_tags"); (!ok || v == nil || len(v.(map[string]interface{})) == 0) && vol.Tags != nil {
 			bd["tags"] = keyvaluetags.Ec2KeyValueTags(vol.Tags).IgnoreAws().Map()
 		}
 
