1481bf466d4a75666f94aa252f89fd06a28e280d
Graham Davison <gdavison@hashicorp.com>
2021-01-20 14:47:15 -0800

    Update aws/resource_aws_elasticache_cluster.go
    Co-authored-by: Brian Flad <bflad417@gmail.com>

--- aws/resource_aws_elasticache_cluster.go
+++ aws/resource_aws_elasticache_cluster.go
@@ -444,7 +444,7 @@ func resourceAwsElasticacheClusterRead(d *schema.ResourceData, meta interface{})
 	d.Set("engine_version", c.EngineVersion)
 	if c.ConfigurationEndpoint != nil {
 		d.Set("port", c.ConfigurationEndpoint.Port)
-		d.Set("configuration_endpoint", aws.String(fmt.Sprintf("%s:%d", *c.ConfigurationEndpoint.Address, *c.ConfigurationEndpoint.Port)))
+		d.Set("configuration_endpoint", aws.String(fmt.Sprintf("%s:%d", aws.StringValue(c.ConfigurationEndpoint.Address), aws.Int64Value(c.ConfigurationEndpoint.Port))))
 		d.Set("cluster_address", aws.String((*c.ConfigurationEndpoint.Address)))
 	} else if len(c.CacheNodes) > 0 {
 		d.Set("port", int(aws.Int64Value(c.CacheNodes[0].Endpoint.Port)))
