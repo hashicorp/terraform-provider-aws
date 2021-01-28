e0e96cab5ad54d95df1f94c1d6e5719553c5e6b1
Brian Flad <bflad417@gmail.com>
2021-01-27 19:22:07 -0500

    resource/aws_iam_access_key: Support resource import
    Reference: https://github.com/hashicorp/terraform-provider-aws/issues/17288
    
    Output from acceptance testing:
    
    ```
    --- PASS: TestAccAWSAccessKey_encrypted (12.92s)
    --- PASS: TestAccAWSAccessKey_basic (12.92s)
    --- PASS: TestAccAWSAccessKey_inactive (19.64s)
    ```

--- aws/resource_aws_iam_access_key_test.go
+++ aws/resource_aws_iam_access_key_test.go
@@ -33,6 +33,12 @@ func TestAccAWSAccessKey_basic(t *testing.T) {
 					resource.TestCheckResourceAttrSet("aws_iam_access_key.a_key", "secret"),
 				),
 			},
+			{
+				ResourceName:            "aws_iam_access_key.a_key",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"encrypted_secret", "key_fingerprint", "pgp_key", "secret", "ses_smtp_password_v4"},
+			},
 		},
 	})
 }
@@ -60,6 +66,12 @@ func TestAccAWSAccessKey_encrypted(t *testing.T) {
 						"aws_iam_access_key.a_key", "key_fingerprint"),
 				),
 			},
+			{
+				ResourceName:            "aws_iam_access_key.a_key",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"encrypted_secret", "key_fingerprint", "pgp_key", "secret", "ses_smtp_password_v4"},
+			},
 		},
 	})
 }
@@ -80,6 +92,12 @@ func TestAccAWSAccessKey_inactive(t *testing.T) {
 					testAccCheckAWSAccessKeyAttributes(&conf, "Active"),
 					resource.TestCheckResourceAttrSet("aws_iam_access_key.a_key", "secret"),
 				),
+			},
+			{
+				ResourceName:            "aws_iam_access_key.a_key",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"encrypted_secret", "key_fingerprint", "pgp_key", "secret", "ses_smtp_password_v4"},
 			},
 			{
 				Config: testAccAWSAccessKeyConfig_inactive(rName),
