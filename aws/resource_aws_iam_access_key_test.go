11525f3c10326147d538203dd97903d05a0e8c07
Brian Flad <bflad417@gmail.com>
2021-01-29 13:42:59 -0500

    resource/aws_iam_access_key: Ensure Inactive status is properly configured during resource creation (#17322)
    * resource/aws_iam_access_key: Ensure Inactive status is properly configured during resource creation
    
    Reference: https://github.com/hashicorp/terraform-provider-aws/issues/16818
    
    Previously before code updates:
    
    ```
    === CONT  TestAccAWSAccessKey_Status
        resource_aws_iam_access_key_test.go:71: Step 1/3 error: Check failed: Check 2/2 error: aws_iam_access_key.a_key: Attribute 'status' expected "Inactive", got "Active"
    --- FAIL: TestAccAWSAccessKey_Status (5.97s)
    ```
    
    Output from acceptance testing:
    
    ```
    --- PASS: TestAccAWSAccessKey_basic (8.96s)
    --- PASS: TestAccAWSAccessKey_encrypted (9.13s)
    --- PASS: TestAccAWSAccessKey_Status (27.74s)
    ```
    
    * Update CHANGELOG for #17322

--- aws/resource_aws_iam_access_key_test.go
+++ aws/resource_aws_iam_access_key_test.go
@@ -77,7 +77,7 @@ func TestAccAWSAccessKey_encrypted(t *testing.T) {
 	})
 }
 
-func TestAccAWSAccessKey_inactive(t *testing.T) {
+func TestAccAWSAccessKey_Status(t *testing.T) {
 	var conf iam.AccessKeyMetadata
 	rName := fmt.Sprintf("test-user-%d", acctest.RandInt())
 
@@ -87,11 +87,10 @@ func TestAccAWSAccessKey_inactive(t *testing.T) {
 		CheckDestroy: testAccCheckAWSAccessKeyDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccAWSAccessKeyConfig(rName),
+				Config: testAccAWSAccessKeyConfig_Status(rName, iam.StatusTypeInactive),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckAWSAccessKeyExists("aws_iam_access_key.a_key", &conf),
-					testAccCheckAWSAccessKeyAttributes(&conf, "Active"),
-					resource.TestCheckResourceAttrSet("aws_iam_access_key.a_key", "secret"),
+					resource.TestCheckResourceAttr("aws_iam_access_key.a_key", "status", iam.StatusTypeInactive),
 				),
 			},
 			{
@@ -101,11 +100,17 @@ func TestAccAWSAccessKey_inactive(t *testing.T) {
 				ImportStateVerifyIgnore: []string{"encrypted_secret", "key_fingerprint", "pgp_key", "secret", "ses_smtp_password_v4"},
 			},
 			{
-				Config: testAccAWSAccessKeyConfig_inactive(rName),
+				Config: testAccAWSAccessKeyConfig_Status(rName, iam.StatusTypeActive),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckAWSAccessKeyExists("aws_iam_access_key.a_key", &conf),
-					testAccCheckAWSAccessKeyAttributes(&conf, "Inactive"),
-					resource.TestCheckResourceAttrSet("aws_iam_access_key.a_key", "secret"),
+					resource.TestCheckResourceAttr("aws_iam_access_key.a_key", "status", iam.StatusTypeActive),
+				),
+			},
+			{
+				Config: testAccAWSAccessKeyConfig_Status(rName, iam.StatusTypeInactive),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAWSAccessKeyExists("aws_iam_access_key.a_key", &conf),
+					resource.TestCheckResourceAttr("aws_iam_access_key.a_key", "status", iam.StatusTypeInactive),
 				),
 			},
 		},
@@ -241,17 +246,17 @@ EOF
 `, rName, key)
 }
 
-func testAccAWSAccessKeyConfig_inactive(rName string) string {
+func testAccAWSAccessKeyConfig_Status(rName string, status string) string {
 	return fmt.Sprintf(`
 resource "aws_iam_user" "a_user" {
-  name = "%s"
+  name = %[1]q
 }
 
 resource "aws_iam_access_key" "a_key" {
   user   = aws_iam_user.a_user.name
-  status = "Inactive"
+  status = %[2]q
 }
-`, rName)
+`, rName, status)
 }
 
 func TestSesSmtpPasswordFromSecretKeySigV4(t *testing.T) {
