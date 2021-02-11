5191dcfc937f3261d11d693a481496b8ffda495b
Brian Flad <bflad417@gmail.com>
2021-02-11 15:00:19 -0500

    Update aws/resource_aws_cloudwatch_log_group.go

--- aws/resource_aws_cloudwatch_log_group.go
+++ aws/resource_aws_cloudwatch_log_group.go
@@ -44,7 +44,7 @@ func resourceAwsCloudWatchLogGroup() *schema.Resource {
 				Type:         schema.TypeInt,
 				Optional:     true,
 				Default:      0,
-				ValidateFunc: validation.IntInSlice([]int{1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, 3653}),
+				ValidateFunc: validation.IntInSlice([]int{0, 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, 3653}),
 			},
 
 			"kms_key_id": {
