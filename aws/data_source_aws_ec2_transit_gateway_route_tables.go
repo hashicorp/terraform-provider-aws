e41ea31340841a56d57441a265a903013c9eef39
Matt Robinson <matthew.robinson@anaplan.com>
2021-02-12 10:24:19 +0000

    Greetings.  This is my first attempt to contribute to terraform and go is not even slightly my first language.
    This is to resolve 12373 which I would like to resolve for selfish reasons as it affects me.  It seemed like a good place to start.
    
    The new data source "aws_ec2_transit_gateway_route_tables" is modelled after "aws_route_tables".  The code is shamelessly plagiarized from the aws_route_tables and the aws_transit_gateway_route_table resources.
    
    I've included tests which I think are right, but I'm not sure on protocol for running them.
    
    The code compiles.  Which is a good start.

---
+++ aws/data_source_aws_ec2_transit_gateway_route_tables.go
@@ -0,0 +1,75 @@
+package aws
+
+import (
+	"errors"
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/terraform-providers/terraform-provider-aws/aws/internal/keyvaluetags"
+)
+
+func dataSourceAwsEc2TransitGatewayRouteTables() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceAwsEc2TransitGatewayRouteTablesRead,
+
+		Schema: map[string]*schema.Schema{
+			"transit_gateway_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"filter": ec2CustomFiltersSchema(),
+			"ids": {
+				Type:     schema.TypeSet,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Set:      schema.HashString,
+			},
+		},
+	}
+}
+
+func dataSourceAwsEc2TransitGatewayRouteTablesRead(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*AWSClient).ec2conn
+
+	input := &ec2.DescribeTransitGatewayRouteTablesInput{}
+
+	if v, ok := d.GetOk("transit_gateway_id"); ok {
+		input.Filters = buildEC2AttributeFilterList(
+			map[string]string{
+				"transit-gateway-id": v.(string),
+			},
+		)
+	}
+
+	input.Filters = append(input.Filters, buildEC2TagFilterList(
+		keyvaluetags.New(d.Get("tags").(map[string]interface{})).Ec2Tags(),
+	)...)
+
+	log.Printf("[DEBUG] Reading EC2 Transit Gateway Route Tables: %s", input)
+	output, err := conn.DescribeTransitGatewayRouteTables(input)
+
+	if err != nil {
+		return fmt.Errorf("error reading EC2 Transit Gateway Route Table: %s", err)
+	}
+
+	if output == nil || len(output.TransitGatewayRouteTables) == 0 {
+		return errors.New("error reading EC2 Transit Gateway Route Table: no results found")
+	}
+
+	d.SetId(meta.(*AWSClient).region)
+
+	routeTables := make([]string, 0)
+
+	for _, routeTable := range output.TransitGatewayRouteTables {
+		routeTables = append(routeTables, aws.StringValue(routeTable.TransitGatewayRouteTableId))
+	}
+
+	if err = d.Set("ids", routeTables); err != nil {
+		return fmt.Errorf("error setting ids: %s", err)
+	}
+
+	return nil
+}
