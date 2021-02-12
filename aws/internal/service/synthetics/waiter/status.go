ae3e310a0517b94bea157531b9817042c97dec06
Dirk Avery <dirk.avery@gmail.com>
2021-02-12 09:43:00 -0500

    resource/synthetics_canary: Use finder in waiter status

--- aws/internal/service/synthetics/waiter/status.go
+++ aws/internal/service/synthetics/waiter/status.go
@@ -6,15 +6,12 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/synthetics"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/terraform-providers/terraform-provider-aws/aws/internal/service/synthetics/finder"
 )
 
 func CanaryStatus(conn *synthetics.Synthetics, name string) resource.StateRefreshFunc {
 	return func() (interface{}, string, error) {
-		input := &synthetics.GetCanaryInput{
-			Name: aws.String(name),
-		}
-
-		output, err := conn.GetCanary(input)
+		output, err := finder.CanaryByName(conn, name)
 
 		if err != nil {
 			return nil, synthetics.CanaryStateError, err
