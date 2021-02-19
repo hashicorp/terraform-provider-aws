31daddd5a1c806ed81d7e61de5c931bfb1a4062e
nikhil <nplusg1993@gmail.com>
2021-02-19 20:46:56 +0530

    LogEncryptionKmsKeyId for EMR Clusters

--- aws/resource_aws_emr_cluster_test.go
+++ aws/resource_aws_emr_cluster_test.go
@@ -1185,6 +1185,7 @@ func TestAccAWSEMRCluster_s3Logging(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckAWSEmrClusterExists(resourceName, &cluster),
 					resource.TestCheckResourceAttr(resourceName, "log_uri", bucketName),
+					resource.TestMatchResourceAttr(resourceName, "log_encryption_kms_key_id", regexp.MustCompile("^arn")),
 				),
 			},
 			{
@@ -3053,7 +3054,7 @@ resource "aws_emr_cluster" "tf-test-cluster" {
   }
 
   log_encryption_kms_key_id = aws_kms_key.foo.key_id
-  log_uri = "s3://${aws_s3_bucket.test.bucket}/"
+  log_uri                   = "s3://${aws_s3_bucket.test.bucket}/"
 
   ec2_attributes {
     instance_profile                  = aws_iam_instance_profile.emr_instance_profile.arn
