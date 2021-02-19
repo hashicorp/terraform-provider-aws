2f3199c39d8efbf5cc22b173755847fdb18dc4ba
nikhil <nplusg1993@gmail.com>
2021-02-19 14:14:03 +0530

    glue_data_catalog_encryption_settings KMS_KEY_ID renamed

--- aws/resource_aws_glue_data_catalog_encryption_settings_test.go
+++ aws/resource_aws_glue_data_catalog_encryption_settings_test.go
@@ -123,13 +123,13 @@ POLICY
 resource "aws_glue_data_catalog_encryption_settings" "test" {
   data_catalog_encryption_settings {
     connection_password_encryption {
-      aws_kms_key_arn                       = aws_kms_key.test.arn
+      aws_kms_key_arn                      = aws_kms_key.test.arn
       return_connection_password_encrypted = true
     }
 
     encryption_at_rest {
       catalog_encryption_mode = "SSE-KMS"
-      sse_aws_kms_key_arn      = aws_kms_key.test.arn
+      sse_aws_kms_key_arn     = aws_kms_key.test.arn
     }
   }
 }
