a2ae2088b30aa59114560e7170bcdd242b5e923a
Ilia Lazebnik <Ilia.lazebnik@gmail.com>
2021-02-26 01:59:39 +0200

    r/sagemaker_app - new resource (#17251)

--- aws/resource_aws_sagemaker_domain_test.go
+++ aws/resource_aws_sagemaker_domain_test.go
@@ -3,16 +3,65 @@ package aws
 import (
 	"fmt"
 	"log"
+	"os"
 	"regexp"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/sagemaker"
+	"github.com/hashicorp/go-multierror"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/terraform-providers/terraform-provider-aws/aws/internal/service/sagemaker/finder"
 )
+
+// Tests are serialized as SagmMaker Domain resources are limited to 1 per account by default.
+// SageMaker UserProfile and App depend on the Domain resources and as such are also part of the serialized test suite.
+func TestAccAWSSagemakerDomain_serial(t *testing.T) {
+	testCases := map[string]map[string]func(t *testing.T){
+		"Domain": {
+			"basic":                                testAccAWSSagemakerDomain_basic,
+			"disappears":                           testAccAWSSagemakerDomain_tags,
+			"tags":                                 testAccAWSSagemakerDomain_disappears,
+			"tensorboardAppSettings":               testAccAWSSagemakerDomain_tensorboardAppSettings,
+			"tensorboardAppSettingsWithImage":      testAccAWSSagemakerDomain_tensorboardAppSettingsWithImage,
+			"kernelGatewayAppSettings":             testAccAWSSagemakerDomain_kernelGatewayAppSettings,
+			"kernelGatewayAppSettings_customImage": testAccAWSSagemakerDomain_kernelGatewayAppSettings_customImage,
+			"jupyterServerAppSettings":             testAccAWSSagemakerDomain_jupyterServerAppSettings,
+			"kms":                                  testAccAWSSagemakerDomain_kms,
+			"securityGroup":                        testAccAWSSagemakerDomain_securityGroup,
+			"sharingSettings":                      testAccAWSSagemakerDomain_sharingSettings,
+		},
+		"UserProfile": {
+			"basic":                           testAccAWSSagemakerUserProfile_basic,
+			"disappears":                      testAccAWSSagemakerUserProfile_tags,
+			"tags":                            testAccAWSSagemakerUserProfile_disappears,
+			"tensorboardAppSettings":          testAccAWSSagemakerUserProfile_tensorboardAppSettings,
+			"tensorboardAppSettingsWithImage": testAccAWSSagemakerUserProfile_tensorboardAppSettingsWithImage,
+			"kernelGatewayAppSettings":        testAccAWSSagemakerUserProfile_kernelGatewayAppSettings,
+			"jupyterServerAppSettings":        testAccAWSSagemakerUserProfile_jupyterServerAppSettings,
+		},
+		"App": {
+			"basic":        testAccAWSSagemakerApp_basic,
+			"disappears":   testAccAWSSagemakerApp_tags,
+			"tags":         testAccAWSSagemakerApp_disappears,
+			"resourceSpec": testAccAWSSagemakerApp_resourceSpec,
+		},
+	}
+
+	for group, m := range testCases {
+		m := m
+		t.Run(group, func(t *testing.T) {
+			for name, tc := range m {
+				tc := tc
+				t.Run(name, func(t *testing.T) {
+					tc(t)
+				})
+			}
+		})
+	}
+}
 
 func init() {
 	resource.AddTestSweepers("aws_sagemaker_domain", &resource.Sweeper{
@@ -21,6 +70,7 @@ func init() {
 		Dependencies: []string{
 			"aws_efs_mount_target",
 			"aws_efs_file_system",
+			"aws_sagemaker_user_profile",
 		},
 	})
 }
@@ -31,21 +81,18 @@ func testSweepSagemakerDomains(region string) error {
 		return fmt.Errorf("error getting client: %s", err)
 	}
 	conn := client.(*AWSClient).sagemakerconn
+	var sweeperErrs *multierror.Error
 
 	err = conn.ListDomainsPages(&sagemaker.ListDomainsInput{}, func(page *sagemaker.ListDomainsOutput, lastPage bool) bool {
-		for _, instance := range page.Domains {
-			domainArn := aws.StringValue(instance.DomainArn)
-			domainID, err := decodeSagemakerDomainID(domainArn)
-			if err != nil {
-				log.Printf("[ERROR] Error parsing sagemaker domain arn (%s): %s", domainArn, err)
-			}
-			input := &sagemaker.DeleteDomainInput{
-				DomainId: aws.String(domainID),
-			}
+		for _, domain := range page.Domains {
 
-			log.Printf("[INFO] Deleting SageMaker domain: %s", domainArn)
-			if _, err := conn.DeleteDomain(input); err != nil {
-				log.Printf("[ERROR] Error deleting SageMaker domain (%s): %s", domainArn, err)
+			r := resourceAwsSagemakerDomain()
+			d := r.Data(nil)
+			d.SetId(aws.StringValue(domain.DomainId))
+			err = r.Delete(d, client)
+			if err != nil {
+				log.Printf("[ERROR] %s", err)
+				sweeperErrs = multierror.Append(sweeperErrs, err)
 				continue
 			}
 		}
@@ -55,22 +102,22 @@ func testSweepSagemakerDomains(region string) error {
 
 	if testSweepSkipSweepError(err) {
 		log.Printf("[WARN] Skipping SageMaker domain sweep for %s: %s", region, err)
-		return nil
+		return sweeperErrs.ErrorOrNil()
 	}
 
 	if err != nil {
-		return fmt.Errorf("Error retrieving SageMaker domains: %w", err)
+		sweeperErrs = multierror.Append(sweeperErrs, fmt.Errorf("error retrieving Sagemaker Domains: %w", err))
 	}
 
-	return nil
+	return sweeperErrs.ErrorOrNil()
 }
 
-func TestAccAWSSagemakerDomain_basic(t *testing.T) {
+func testAccAWSSagemakerDomain_basic(t *testing.T) {
 	var domain sagemaker.DescribeDomainOutput
 	rName := acctest.RandomWithPrefix("tf-acc-test")
 	resourceName := "aws_sagemaker_domain.test"
 
-	resource.ParallelTest(t, resource.TestCase{
+	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
 		CheckDestroy: testAccCheckAWSSagemakerDomainDestroy,
@@ -101,12 +148,12 @@ func TestAccAWSSagemakerDomain_basic(t *testing.T) {
 	})
 }
 
-func TestAccAWSSagemakerDomain_kms(t *testing.T) {
+func testAccAWSSagemakerDomain_kms(t *testing.T) {
 	var domain sagemaker.DescribeDomainOutput
 	rName := acctest.RandomWithPrefix("tf-acc-test")
 	resourceName := "aws_sagemaker_domain.test"
 
-	resource.ParallelTest(t, resource.TestCase{
+	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
 		CheckDestroy: testAccCheckAWSSagemakerDomainDestroy,
@@ -127,12 +174,12 @@ func TestAccAWSSagemakerDomain_kms(t *testing.T) {
 	})
 }
 
-func TestAccAWSSagemakerDomain_tags(t *testing.T) {
+func testAccAWSSagemakerDomain_tags(t *testing.T) {
 	var domain sagemaker.DescribeDomainOutput
 	rName := acctest.RandomWithPrefix("tf-acc-test")
 	resourceName := "aws_sagemaker_domain.test"
 
-	resource.ParallelTest(t, resource.TestCase{
+	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
 		CheckDestroy: testAccCheckAWSSagemakerDomainDestroy,
@@ -171,12 +218,12 @@ func TestAccAWSSagemakerDomain_tags(t *testing.T) {
 	})
 }
 
-func TestAccAWSSagemakerDomain_securityGroup(t *testing.T) {
+func testAccAWSSagemakerDomain_securityGroup(t *testing.T) {
 	var domain sagemaker.DescribeDomainOutput
 	rName := acctest.RandomWithPrefix("tf-acc-test")
 	resourceName := "aws_sagemaker_domain.test"
 
-	resource.ParallelTest(t, resource.TestCase{
+	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
 		CheckDestroy: testAccCheckAWSSagemakerDomainDestroy,
@@ -206,12 +253,12 @@ func TestAccAWSSagemakerDomain_securityGroup(t *testing.T) {
 	})
 }
 
-func TestAccAWSSagemakerDomain_sharingSettings(t *testing.T) {
+func testAccAWSSagemakerDomain_sharingSettings(t *testing.T) {
 	var domain sagemaker.DescribeDomainOutput
 	rName := acctest.RandomWithPrefix("tf-acc-test")
 	resourceName := "aws_sagemaker_domain.test"
 
-	resource.ParallelTest(t, resource.TestCase{
+	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
 		CheckDestroy: testAccCheckAWSSagemakerDomainDestroy,
@@ -236,12 +283,12 @@ func TestAccAWSSagemakerDomain_sharingSettings(t *testing.T) {
 	})
 }
 
-func TestAccAWSSagemakerDomain_tensorboardAppSettings(t *testing.T) {
+func testAccAWSSagemakerDomain_tensorboardAppSettings(t *testing.T) {
 	var domain sagemaker.DescribeDomainOutput
 	rName := acctest.RandomWithPrefix("tf-acc-test")
 	resourceName := "aws_sagemaker_domain.test"
 
-	resource.ParallelTest(t, resource.TestCase{
+	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
 		CheckDestroy: testAccCheckAWSSagemakerDomainDestroy,
@@ -265,12 +312,12 @@ func TestAccAWSSagemakerDomain_tensorboardAppSettings(t *testing.T) {
 	})
 }
 
-func TestAccAWSSagemakerDomain_tensorboardAppSettingsWithImage(t *testing.T) {
+func testAccAWSSagemakerDomain_tensorboardAppSettingsWithImage(t *testing.T) {
 	var domain sagemaker.DescribeDomainOutput
 	rName := acctest.RandomWithPrefix("tf-acc-test")
 	resourceName := "aws_sagemaker_domain.test"
 
-	resource.ParallelTest(t, resource.TestCase{
+	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
 		CheckDestroy: testAccCheckAWSSagemakerDomainDestroy,
@@ -295,12 +342,12 @@ func TestAccAWSSagemakerDomain_tensorboardAppSettingsWithImage(t *testing.T) {
 	})
 }
 
-func TestAccAWSSagemakerDomain_kernelGatewayAppSettings(t *testing.T) {
+func testAccAWSSagemakerDomain_kernelGatewayAppSettings(t *testing.T) {
 	var domain sagemaker.DescribeDomainOutput
 	rName := acctest.RandomWithPrefix("tf-acc-test")
 	resourceName := "aws_sagemaker_domain.test"
 
-	resource.ParallelTest(t, resource.TestCase{
+	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
 		CheckDestroy: testAccCheckAWSSagemakerDomainDestroy,
@@ -324,12 +371,49 @@ func TestAccAWSSagemakerDomain_kernelGatewayAppSettings(t *testing.T) {
 	})
 }
 
-func TestAccAWSSagemakerDomain_jupyterServerAppSettings(t *testing.T) {
+func testAccAWSSagemakerDomain_kernelGatewayAppSettings_customImage(t *testing.T) {
+
+	if os.Getenv("SAGEMAKER_IMAGE_VERSION_BASE_IMAGE") == "" {
+		t.Skip("Environment variable SAGEMAKER_IMAGE_VERSION_BASE_IMAGE is not set")
+	}
+
+	var domain sagemaker.DescribeDomainOutput
+	rName := acctest.RandomWithPrefix("tf-acc-test")
+	resourceName := "aws_sagemaker_domain.test"
+	baseImage := os.Getenv("SAGEMAKER_IMAGE_VERSION_BASE_IMAGE")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAWSSagemakerDomainDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAWSSagemakerDomainConfigKernelGatewayAppSettingsCustomImage(rName, baseImage),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAWSSagemakerDomainExists(resourceName, &domain),
+					resource.TestCheckResourceAttr(resourceName, "default_user_settings.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "default_user_settings.0.kernel_gateway_app_settings.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "default_user_settings.0.kernel_gateway_app_settings.0.default_resource_spec.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "default_user_settings.0.kernel_gateway_app_settings.0.custom_image.#", "1"),
+					resource.TestCheckResourceAttrPair(resourceName, "default_user_settings.0.kernel_gateway_app_settings.0.custom_image.0.app_image_config_name", "aws_sagemaker_app_image_config.test", "app_image_config_name"),
+					resource.TestCheckResourceAttrPair(resourceName, "default_user_settings.0.kernel_gateway_app_settings.0.custom_image.0.image_name", "aws_sagemaker_image.test", "image_name"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccAWSSagemakerDomain_jupyterServerAppSettings(t *testing.T) {
 	var domain sagemaker.DescribeDomainOutput
 	rName := acctest.RandomWithPrefix("tf-acc-test")
 	resourceName := "aws_sagemaker_domain.test"
 
-	resource.ParallelTest(t, resource.TestCase{
+	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
 		CheckDestroy: testAccCheckAWSSagemakerDomainDestroy,
@@ -353,12 +437,12 @@ func TestAccAWSSagemakerDomain_jupyterServerAppSettings(t *testing.T) {
 	})
 }
 
-func TestAccAWSSagemakerDomain_disappears(t *testing.T) {
+func testAccAWSSagemakerDomain_disappears(t *testing.T) {
 	var domain sagemaker.DescribeDomainOutput
 	rName := acctest.RandomWithPrefix("tf-acc-test")
 	resourceName := "aws_sagemaker_domain.test"
 
-	resource.ParallelTest(t, resource.TestCase{
+	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
 		CheckDestroy: testAccCheckAWSSagemakerDomainDestroy,
@@ -427,6 +511,8 @@ func testAccCheckAWSSagemakerDomainExists(n string, codeRepo *sagemaker.Describe
 
 func testAccAWSSagemakerDomainConfigBase(rName string) string {
 	return fmt.Sprintf(`
+data "aws_partition" "current" {}
+
 resource "aws_vpc" "test" {
   cidr_block = "10.0.0.0/16"
 
@@ -456,9 +542,14 @@ data "aws_iam_policy_document" "test" {
 
     principals {
       type        = "Service"
-      identifiers = ["sagemaker.amazonaws.com"]
+      identifiers = ["sagemaker.${data.aws_partition.current.dns_suffix}"]
     }
   }
+}
+
+resource "aws_iam_role_policy_attachment" "test" {
+  role       = aws_iam_role.test.name
+  policy_arn = "arn:${data.aws_partition.current.partition}:iam::aws:policy/AmazonSageMakerFullAccess"
 }
 `, rName)
 }
@@ -702,4 +793,48 @@ resource "aws_sagemaker_domain" "test" {
   }
 }
 `, rName)
+}
+
+func testAccAWSSagemakerDomainConfigKernelGatewayAppSettingsCustomImage(rName, baseImage string) string {
+	return testAccAWSSagemakerDomainConfigBase(rName) + fmt.Sprintf(`
+resource "aws_sagemaker_image" "test" {
+  image_name = %[1]q
+  role_arn   = aws_iam_role.test.arn
+
+  depends_on = [aws_iam_role_policy_attachment.test]
+}
+
+resource "aws_sagemaker_app_image_config" "test" {
+  app_image_config_name = %[1]q
+
+  kernel_gateway_image_config {
+    kernel_spec {
+      name = %[1]q
+    }
+  }
+}
+
+resource "aws_sagemaker_image_version" "test" {
+  image_name = aws_sagemaker_image.test.id
+  base_image = %[2]q
+}
+
+resource "aws_sagemaker_domain" "test" {
+  domain_name = %[1]q
+  auth_mode   = "IAM"
+  vpc_id      = aws_vpc.test.id
+  subnet_ids  = [aws_subnet.test.id]
+
+  default_user_settings {
+    execution_role = aws_iam_role.test.arn
+
+    kernel_gateway_app_settings {
+      custom_image {
+        app_image_config_name = aws_sagemaker_app_image_config.test.app_image_config_name
+        image_name            = aws_sagemaker_image_version.test.image_name
+      }
+    }
+  }
+}
+`, rName, baseImage)
 }
