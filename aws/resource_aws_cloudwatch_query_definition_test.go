2144f2ab07eaf53e799f137d40a2f3492b41d23d
Traci Kamp <traci.kamp.ffx4@statefarm.com>
2021-02-26 16:04:04 -0700

    test(aws_cloudwatch_query_definition): test disappears case

--- aws/resource_aws_cloudwatch_query_definition_test.go
+++ aws/resource_aws_cloudwatch_query_definition_test.go
@@ -34,6 +34,45 @@ func TestAccAWSCloudWatchQueryDefinition_basic(t *testing.T) {
 	})
 }
 
+func TestAccAWSCloudWatchQueryDefinition_disappears(t *testing.T) {
+	resourceName := "aws_cloudwatch_query_definition.query"
+	queryName := "test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: testAccProviderFactories,
+		CheckDestroy:      testAccCheckAWSCloudWatchQueryDefinitionDestroy(resourceName, queryName),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAWSCloudWatchQueryDefinitionConfig(queryName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAWSCloudWatchQueryDefinitionExists(resourceName, queryName),
+					testAccCheckAWSCloudWatchQueryDefinitionDisappears(resourceName),
+				),
+				ExpectNonEmptyPlan: true,
+			},
+		},
+	})
+}
+
+func testAccCheckAWSCloudWatchQueryDefinitionDisappears(rName string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		conn := testAccProvider.Meta().(*AWSClient).cloudwatchlogsconn
+
+		rs, ok := s.RootModule().Resources[rName]
+		if !ok {
+			return fmt.Errorf("resource not found: %s", rName)
+		}
+
+		qID := rs.Primary.ID
+
+		input := &cloudwatchlogs.DeleteQueryDefinitionInput{QueryDefinitionId: &qID}
+		_, err := conn.DeleteQueryDefinition(input)
+
+		return err
+	}
+}
+
 func testAccCheckAWSCloudWatchQueryDefinitionExists(rName, qName string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		rs, ok := s.RootModule().Resources[rName]
