c2f28d02e8a2e480cb353d2fd12fadbb5b2fe372
Will Varney <github@willvrny.co.uk>
2021-03-02 10:29:00 +0000

    New resource: aws_eks_identity_provider_config.
    This adds a new resource for associating an identity provider with an EKS cluster. This commit implements only the required fields.

---
+++ aws/resource_aws_eks_identity_provider_config.go
@@ -0,0 +1,312 @@
+package aws
+
+import (
+	"context"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/eks"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"log"
+	"strings"
+	"time"
+)
+
+const typeOidc string = "oidc"
+
+func resourceAwsEksIdentityProviderConfig() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceAwsEksIdentityProviderConfigCreate,
+		ReadContext:   resourceAwsEksIdentityProviderConfigRead,
+		DeleteContext: resourceAwsEksIdentityProviderConfigDelete,
+
+		// TODO - Not sure if I can enable importing?
+
+		// TODO - AWS is suggesting it can take 15-20 minutes to associate an identity provider with EKS. Seeing `context deadline exceeded`.
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(25 * time.Minute),
+			Delete: schema.DefaultTimeout(25 * time.Minute),
+		},
+
+		Schema: map[string]*schema.Schema{
+			"cluster_name": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.NoZeroValues,
+			},
+			// TODO - Do I set ForceNew here to true as it doesn't look like you can update a identity provider config
+			"oidc": {
+				Type:     schema.TypeList,
+				Required: true,
+				ForceNew: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"client_id": {
+							Type:         schema.TypeString,
+							Required:     true,
+							ForceNew:     true,
+							ValidateFunc: validation.NoZeroValues,
+						},
+						"identity_provider_config_name": {
+							Type:         schema.TypeString,
+							Required:     true,
+							ForceNew:     true,
+							ValidateFunc: validation.NoZeroValues,
+						},
+						"issuer_url": {
+							Type:         schema.TypeString,
+							Required:     true,
+							ForceNew:     true,
+							ValidateFunc: validation.IsURLWithHTTPS,
+						},
+					},
+				},
+			},
+			"status": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceAwsEksIdentityProviderConfigCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn := meta.(*AWSClient).eksconn
+	clusterName := d.Get("cluster_name").(string)
+
+	//TODO - Should I break away from the aws sdk api and move the name outside of the oidc map?
+	configName, oidcRequest := expandEksOidcIdentityProviderConfigRequest(d.Get("oidc").([]interface{}))
+
+	id := fmt.Sprintf("%s:%s", clusterName, configName)
+
+	input := &eks.AssociateIdentityProviderConfigInput{
+		ClientRequestToken: aws.String(resource.UniqueId()),
+		ClusterName:        aws.String(clusterName),
+		Oidc:               oidcRequest,
+	}
+
+	//if v := d.Get("tags").(map[string]interface{}); len(v) > 0 {
+	//	input.Tags = keyvaluetags.New(v).IgnoreAws().EksTags()
+	//}
+
+	_, err := conn.AssociateIdentityProviderConfig(input)
+	if err != nil {
+		return diag.Errorf("error associating EKS Identity Provider Config (%s): %s", id, err)
+	}
+
+	d.SetId(id)
+
+	stateConf := resource.StateChangeConf{
+		Pending: []string{eks.ConfigStatusCreating},
+		Target:  []string{eks.ConfigStatusActive},
+		Timeout: d.Timeout(schema.TimeoutCreate),
+		Refresh: refreshEksIdentityProviderConfigStatus(conn, clusterName, &eks.IdentityProviderConfig{
+			Name: aws.String(configName),
+			Type: aws.String(typeOidc),
+		}),
+	}
+
+	if _, err := stateConf.WaitForStateContext(ctx); err != nil {
+		return diag.Errorf("error waiting for EKS Identity Provider Config (%s) association: %s", d.Id(), err)
+	}
+
+	return resourceAwsEksIdentityProviderConfigRead(ctx, d, meta)
+}
+
+func resourceAwsEksIdentityProviderConfigRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn := meta.(*AWSClient).eksconn
+	//ignoreTagsConfig := meta.(*AWSClient).IgnoreTagsConfig
+
+	clusterName, configName, err := resourceAwsEksIdentityProviderConfigParseId(d.Id())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	input := &eks.DescribeIdentityProviderConfigInput{
+		ClusterName: aws.String(clusterName),
+		IdentityProviderConfig: &eks.IdentityProviderConfig{
+			Name: aws.String(configName),
+			Type: aws.String(typeOidc),
+		},
+	}
+
+	output, err := conn.DescribeIdentityProviderConfigWithContext(ctx, input)
+
+	if isAWSErr(err, eks.ErrCodeResourceNotFoundException, "") {
+		log.Printf("[WARN] EKS Identity Provider Config (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	if err != nil {
+		return diag.Errorf("error reading EKS Identity Provider Config (%s): %s", d.Id(), err)
+	}
+
+	config := output.IdentityProviderConfig
+
+	if config == nil {
+		log.Printf("[WARN] EKS Identity Provider Config (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	// TODO - Do I need the nil check here?
+	if config.Oidc == nil {
+		log.Printf("[WARN] EKS OIDC Identity Provider Config (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	d.Set("cluster_name", clusterName)
+
+	if err := d.Set("oidc", flattenEksOidcIdentityProviderConfig(config.Oidc)); err != nil {
+		return diag.Errorf("error setting oidc: %s", err)
+	}
+
+	d.Set("status", config.Oidc.Status)
+
+	//if err := d.Set("tags", keyvaluetags.EksKeyValueTags(fargateProfile.Tags).IgnoreAws().IgnoreConfig(ignoreTagsConfig).Map()); err != nil {
+	//	return fmt.Errorf("error setting tags: %s", err)
+	//}
+
+	return nil
+}
+
+func resourceAwsEksIdentityProviderConfigDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn := meta.(*AWSClient).eksconn
+
+	clusterName, configName, err := resourceAwsEksIdentityProviderConfigParseId(d.Id())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	config := &eks.IdentityProviderConfig{
+		Name: aws.String(configName),
+		Type: aws.String(typeOidc),
+	}
+
+	log.Printf("[DEBUG] Disassociating EKS Identity Provider Config: %s", d.Id())
+	input := &eks.DisassociateIdentityProviderConfigInput{
+		ClusterName:            aws.String(clusterName),
+		IdentityProviderConfig: config,
+	}
+
+	_, err = conn.DisassociateIdentityProviderConfigWithContext(ctx, input)
+
+	if isAWSErr(err, eks.ErrCodeResourceNotFoundException, "") {
+		return nil
+	}
+
+	// TODO - if we timeout on the association whilst acc testing this will fail with:
+	// {
+	//   RespMetadata: {
+	//     StatusCode: 400,
+	// 	   RequestID: "abf3f851-04e7-4d66-867e-f871897efbb3"
+	//   },
+	//   ClusterName: "tf-acc-test-387064423719985885",
+	// 	 Message_: "Identity provider config is not associated with cluster tf-acc-test-387064423719985885"
+	// }
+	if err != nil {
+		return diag.Errorf("error disassociating EKS Identity Provider Config (%s): %s", d.Id(), err)
+	}
+
+	if err := waitForEksIdentityProviderConfigDisassociation(ctx, conn, clusterName, config, d.Timeout(schema.TimeoutDelete)); err != nil {
+		return diag.Errorf("error waiting for EKS Identity Provider Config (%s) disassociation: %s", d.Id(), err)
+	}
+
+	return nil
+}
+
+func refreshEksIdentityProviderConfigStatus(conn *eks.EKS, clusterName string, config *eks.IdentityProviderConfig) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		input := &eks.DescribeIdentityProviderConfigInput{
+			ClusterName:            aws.String(clusterName),
+			IdentityProviderConfig: config,
+		}
+
+		output, err := conn.DescribeIdentityProviderConfig(input)
+
+		if err != nil {
+			return "", "", err
+		}
+
+		identityProviderConfig := output.IdentityProviderConfig
+
+		// TODO: not sure about the use of StringValue here.
+		if identityProviderConfig == nil {
+			return identityProviderConfig, "", fmt.Errorf("EKS Identity Provider Config (%s:%s) missing", clusterName, aws.StringValue(config.Name))
+		}
+
+		oidc := identityProviderConfig.Oidc
+
+		// TODO: Should I be checking for nil on OIDC here too?
+		if oidc == nil {
+			return identityProviderConfig, "", fmt.Errorf("EKS OIDC Identity Provider Config (%s:%s) missing", clusterName, aws.StringValue(config.Name))
+		}
+
+		return identityProviderConfig, aws.StringValue(oidc.Status), nil
+	}
+}
+
+func waitForEksIdentityProviderConfigDisassociation(ctx context.Context, conn *eks.EKS, clusterName string, config *eks.IdentityProviderConfig, timeout time.Duration) error {
+	stateConf := resource.StateChangeConf{
+		Pending: []string{
+			eks.ConfigStatusActive,
+			eks.ConfigStatusDeleting,
+		},
+		Target:  []string{""},
+		Timeout: timeout,
+		Refresh: refreshEksIdentityProviderConfigStatus(conn, clusterName, config),
+	}
+	_, err := stateConf.WaitForStateContext(ctx)
+
+	if isAWSErr(err, eks.ErrCodeResourceNotFoundException, "") {
+		return nil
+	}
+
+	return err
+}
+
+func resourceAwsEksIdentityProviderConfigParseId(id string) (string, string, error) {
+	parts := strings.Split(id, ":")
+
+	if len(parts) != 2 || parts[0] == "" || parts[1] == "" {
+		return "", "", fmt.Errorf("unexpected format of ID (%s), expected cluster-name:identity-provider-config-name", id)
+	}
+
+	return parts[0], parts[1], nil
+}
+
+func expandEksOidcIdentityProviderConfigRequest(l []interface{}) (string, *eks.OidcIdentityProviderConfigRequest) {
+	if len(l) == 0 {
+		return "", nil
+	}
+
+	m := l[0].(map[string]interface{})
+
+	configName := m["identity_provider_config_name"].(string)
+	oidcIdentityProviderConfigRequest := &eks.OidcIdentityProviderConfigRequest{
+		ClientId:                   aws.String(m["client_id"].(string)),
+		IdentityProviderConfigName: aws.String(configName),
+		IssuerUrl:                  aws.String(m["issuer_url"].(string)),
+	}
+
+	return configName, oidcIdentityProviderConfigRequest
+}
+
+func flattenEksOidcIdentityProviderConfig(config *eks.OidcIdentityProviderConfig) []map[string]interface{} {
+	if config == nil {
+		return []map[string]interface{}{}
+	}
+
+	m := map[string]interface{}{
+		"client_id":                     aws.StringValue(config.ClientId),
+		"identity_provider_config_name": aws.StringValue(config.IdentityProviderConfigName),
+		"issuer_url":                    aws.StringValue(config.IssuerUrl),
+	}
+
+	return []map[string]interface{}{m}
+}
---
+++ aws/resource_aws_eks_identity_provider_config_test.go
@@ -0,0 +1,276 @@
+package aws
+
+import (
+	"context"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/eks"
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"log"
+	"testing"
+	"time"
+)
+
+func init() {
+	resource.AddTestSweepers("aws_eks_identity_provider_config", &resource.Sweeper{
+		Name: "aws_eks_identity_provider_config",
+		F:    testSweepEksIdentityProviderConfigs,
+	})
+}
+
+func testSweepEksIdentityProviderConfigs(region string) error {
+	client, err := sharedClientForRegion(region)
+	if err != nil {
+		return fmt.Errorf("error getting client: %s", err)
+	}
+	conn := client.(*AWSClient).eksconn
+
+	var errors error
+	input := &eks.ListClustersInput{}
+	err = conn.ListClustersPages(input, func(page *eks.ListClustersOutput, lastPage bool) bool {
+		for _, cluster := range page.Clusters {
+			clusterName := aws.StringValue(cluster)
+			input := &eks.ListIdentityProviderConfigsInput{
+				ClusterName: cluster,
+			}
+			err := conn.ListIdentityProviderConfigsPages(input, func(page *eks.ListIdentityProviderConfigsOutput, lastPage bool) bool {
+				for _, config := range page.IdentityProviderConfigs {
+					configName := aws.StringValue(config.Name)
+					log.Printf("[INFO] Disassociating Identity Provider Config %q", configName)
+					input := &eks.DisassociateIdentityProviderConfigInput{
+						ClusterName:            cluster,
+						IdentityProviderConfig: config,
+					}
+					_, err := conn.DisassociateIdentityProviderConfig(input)
+
+					if err != nil && !isAWSErr(err, eks.ErrCodeResourceNotFoundException, "") {
+						errors = multierror.Append(errors, fmt.Errorf("error disassociating Identity Provider Config %q: %w", configName, err))
+						continue
+					}
+
+					// TODO - Should I use context.Background here?
+					if err := waitForEksIdentityProviderConfigDisassociation(context.Background(), conn, clusterName, config, 10*time.Minute); err != nil {
+						errors = multierror.Append(errors, fmt.Errorf("error waiting for EKS Identity Provider Config %q disassociation: %w", configName, err))
+						continue
+					}
+				}
+				return true
+			})
+			if err != nil {
+				errors = multierror.Append(errors, fmt.Errorf("error listing Identity Provider Configs for EKS Cluster %s: %w", clusterName, err))
+			}
+		}
+
+		return true
+	})
+	if testSweepSkipSweepError(err) {
+		log.Printf("[WARN] Skipping EKS Clusters sweep for %s: %s", region, err)
+		return errors // In case we have completed some pages, but had errors
+	}
+	if err != nil {
+		errors = multierror.Append(errors, fmt.Errorf("error retrieving EKS Clusters: %w", err))
+	}
+
+	return errors
+}
+
+func TestAccAWSEksIdentityProviderConfig_basic(t *testing.T) {
+	var config eks.IdentityProviderConfig
+	rName := acctest.RandomWithPrefix("tf-acc-test")
+	eksClusterResourceName := "aws_eks_cluster.test"
+	resourceName := "aws_eks_identity_provider_config.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t); testAccPreCheckAWSEks(t) },
+		ProviderFactories: testAccProviderFactories,
+		CheckDestroy:      testAccCheckAWSEksIdentityProviderConfigDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAWSEksIdentityProviderConfigProviderConfigName(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAWSEksIdentityProviderConfigExists(resourceName, &config),
+					resource.TestCheckResourceAttrPair(resourceName, "cluster_name", eksClusterResourceName, "name"),
+					resource.TestCheckResourceAttr(resourceName, "oidc.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "oidc.0.identity_provider_config_name", rName),
+					resource.TestCheckResourceAttr(resourceName, "oidc.0.client_id", "test-url.apps.googleusercontent.com"),
+					resource.TestCheckResourceAttr(resourceName, "oidc.0.issuer_url", "https://accounts.google.com/.well-known/openid-configuration"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckAWSEksIdentityProviderConfigExists(resourceName string, config *eks.IdentityProviderConfig) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[resourceName]
+		if !ok {
+			return fmt.Errorf("Not found: %s", resourceName)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No EKS Identity Profile Config is set")
+		}
+
+		clusterName, configName, err := resourceAwsEksIdentityProviderConfigParseId(rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+
+		conn := testAccProvider.Meta().(*AWSClient).eksconn
+
+		input := &eks.DescribeIdentityProviderConfigInput{
+			ClusterName: aws.String(clusterName),
+			IdentityProviderConfig: &eks.IdentityProviderConfig{
+				Name: aws.String(configName),
+				Type: aws.String(typeOidc),
+			},
+		}
+
+		output, err := conn.DescribeIdentityProviderConfig(input)
+
+		if err != nil {
+			return err
+		}
+
+		if output == nil || output.IdentityProviderConfig == nil {
+			return fmt.Errorf("EKS Identity Provider Config (%s) not found", rs.Primary.ID)
+		}
+
+		if aws.StringValue(output.IdentityProviderConfig.Oidc.IdentityProviderConfigName) != configName {
+			return fmt.Errorf("EKS OIDC Identity Provider Config (%s) not found", rs.Primary.ID)
+		}
+
+		if got, want := aws.StringValue(output.IdentityProviderConfig.Oidc.Status), eks.ConfigStatusActive; got != want {
+			return fmt.Errorf("EKS OIDC Identity Provider Config (%s) not in %s status, got: %s", rs.Primary.ID, want, got)
+		}
+
+		*config = eks.IdentityProviderConfig{
+			Name: output.IdentityProviderConfig.Oidc.IdentityProviderConfigName,
+			Type: aws.String(typeOidc),
+		}
+
+		return nil
+	}
+}
+
+// TODO - still needs some work
+func testAccCheckAWSEksIdentityProviderConfigDestroy(s *terraform.State) error {
+	conn := testAccProvider.Meta().(*AWSClient).eksconn
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "aws_eks_identity_provider_config" {
+			continue
+		}
+
+		clusterName, configName, err := resourceAwsEksIdentityProviderConfigParseId(rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+
+		input := &eks.DescribeIdentityProviderConfigInput{
+			ClusterName: aws.String(clusterName),
+			IdentityProviderConfig: &eks.IdentityProviderConfig{
+				Name: aws.String(configName),
+				Type: aws.String(typeOidc),
+			},
+		}
+
+		output, err := conn.DescribeIdentityProviderConfig(input)
+
+		if isAWSErr(err, eks.ErrCodeResourceNotFoundException, "") {
+			continue
+		}
+
+		if output != nil && output.IdentityProviderConfig != nil && aws.StringValue(output.IdentityProviderConfig.Oidc.IdentityProviderConfigName) == configName {
+			return fmt.Errorf("EKS Identity Provider Config (%s) still exists", rs.Primary.ID)
+		}
+	}
+
+	return nil
+}
+
+func testAccAWSEksIdentityProviderConfigBase(rName string) string {
+	return fmt.Sprintf(`
+data "aws_availability_zones" "available" {
+  state = "available"
+
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+data "aws_partition" "current" {}
+
+resource "aws_iam_role" "cluster" {
+  name = "%[1]s-cluster"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.${data.aws_partition.current.dns_suffix}"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cluster-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:${data.aws_partition.current.partition}:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.cluster.name
+}
+
+resource "aws_vpc" "test" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name                          = "tf-acc-test-eks-identity-provider-config"
+    "kubernetes.io/cluster/%[1]s" = "shared"
+  }
+}
+
+resource "aws_subnet" "test" {
+  count = 2
+
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  cidr_block        = "10.0.${count.index}.0/24"
+  vpc_id            = aws_vpc.test.id
+
+  tags = {
+    Name                          = "tf-acc-test-eks-identity-provider-config"
+    "kubernetes.io/cluster/%[1]s" = "shared"
+  }
+}
+
+resource "aws_eks_cluster" "test" {
+  name     = %[1]q
+  role_arn = aws_iam_role.cluster.arn
+
+  vpc_config {
+    subnet_ids = aws_subnet.test[*].id
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.cluster-AmazonEKSClusterPolicy]
+}
+`, rName)
+}
+
+func testAccAWSEksIdentityProviderConfigProviderConfigName(rName string) string {
+	return testAccAWSEksIdentityProviderConfigBase(rName) + fmt.Sprintf(`
+resource "aws_eks_identity_provider_config" "test" {
+  cluster_name           = aws_eks_cluster.test.name
+  oidc {
+    client_id = "test-url.apps.googleusercontent.com"
+    identity_provider_config_name = %[1]q
+    issuer_url = "https://accounts.google.com/.well-known/openid-configuration"
+  }
+}
+`, rName)
+}
