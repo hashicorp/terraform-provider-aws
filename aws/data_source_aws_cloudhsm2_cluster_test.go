7f5eb0027e7c9f209910cca7f10fed2eb2c41e0c
Dirk Avery <dirk.avery@gmail.com>
2021-03-04 11:07:25 -0500

    tests/cloudhsmv2: Move to compose config, make parallel

--- aws/data_source_aws_cloudhsm2_cluster_test.go
+++ aws/data_source_aws_cloudhsm2_cluster_test.go
@@ -12,7 +12,7 @@ func TestAccDataSourceCloudHsmV2Cluster_basic(t *testing.T) {
 	resourceName := "aws_cloudhsm_v2_cluster.cluster"
 	dataSourceName := "data.aws_cloudhsm_v2_cluster.default"
 
-	resource.Test(t, resource.TestCase{
+	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
@@ -31,7 +31,7 @@ func TestAccDataSourceCloudHsmV2Cluster_basic(t *testing.T) {
 	})
 }
 
-var testAccCheckCloudHsmV2ClusterDataSourceConfig = testAccAvailableAZsNoOptInConfig() + fmt.Sprintf(`
+var testAccCheckCloudHsmV2ClusterDataSourceConfig = composeConfig(testAccAvailableAZsNoOptInConfig(), fmt.Sprintf(`
 variable "subnets" {
   default = ["10.0.1.0/24", "10.0.2.0/24"]
   type    = list(string)
@@ -69,4 +69,4 @@ resource "aws_cloudhsm_v2_cluster" "cluster" {
 data "aws_cloudhsm_v2_cluster" "default" {
   cluster_id = aws_cloudhsm_v2_cluster.cluster.cluster_id
 }
-`, acctest.RandInt())
+`, acctest.RandInt()))
