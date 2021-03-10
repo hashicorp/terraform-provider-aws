be0d7b488808ffbdb8489f06d24ddf47ca258c2b
Isaiah-Turner <42742035+Isaiah-Turner@users.noreply.github.com>
2021-03-10 18:42:35 -0500

    Fixed spacing in acceptance test

--- aws/resource_aws_appconfig_application_test.go
+++ aws/resource_aws_appconfig_application_test.go
@@ -191,8 +191,8 @@ func testAccCheckAWSAppConfigApplicationARN(resourceName string, application *ap
 func testAccAWSAppConfigApplicationName(rName, rDesc string) string {
 	return fmt.Sprintf(`
 resource "aws_appconfig_application" "test" {
-	name        = %[1]q
-	description = %[2]q
+  name        = %[1]q
+  description = %[2]q
 }
 `, rName, rDesc)
 }
