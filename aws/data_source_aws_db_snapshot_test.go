f87b0f955d266bbd6503671eda83fed017be0013
Dirk Avery <dirk.avery@gmail.com>
2021-03-18 03:02:37 -0400

    tests/ds/db_snapshot: Add ErrorCheck

--- aws/data_source_aws_db_snapshot_test.go
+++ aws/data_source_aws_db_snapshot_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/aws/aws-sdk-go/service/rds"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
@@ -12,8 +13,9 @@ import (
 func TestAccAWSDbSnapshotDataSource_basic(t *testing.T) {
 	rInt := acctest.RandInt()
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
-		Providers: testAccProviders,
+		PreCheck:   func() { testAccPreCheck(t) },
+		ErrorCheck: testAccErrorCheck(t, rds.EndpointsID),
+		Providers:  testAccProviders,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccCheckAwsDbSnapshotDataSourceConfig(rInt),
