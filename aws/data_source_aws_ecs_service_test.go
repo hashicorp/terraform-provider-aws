890f9bc42f4db3569219b3f0a0d1c79fd3cb86c2
Dirk Avery <dirk.avery@gmail.com>
2021-03-18 20:16:44 -0400

    tests/ds/ecs_service: Add ErrorCheck

--- aws/data_source_aws_ecs_service_test.go
+++ aws/data_source_aws_ecs_service_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/aws/aws-sdk-go/service/ecs"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
@@ -13,8 +14,9 @@ func TestAccAWSEcsServiceDataSource_basic(t *testing.T) {
 	resourceName := "aws_ecs_service.test"
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
-		Providers: testAccProviders,
+		PreCheck:   func() { testAccPreCheck(t) },
+		ErrorCheck: testAccErrorCheck(t, ecs.EndpointsID),
+		Providers:  testAccProviders,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccCheckAwsEcsServiceDataSourceConfig,
