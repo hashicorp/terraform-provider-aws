98d0b8c4f1f077d964d0bf12f2d1194a7a0fdb9d
Graham Davison <gdavison@hashicorp.com>
2021-03-25 13:03:54 -0700

    Adds NullableBool for MQ Broker audit log configuration

--- aws/internal/experimental/nullable/bool_test.go
+++ aws/internal/experimental/nullable/bool_test.go
@@ -7,33 +7,38 @@ import (
 	"testing"
 )
 
-func TestNullableInt(t *testing.T) {
+func TestNullableBool(t *testing.T) {
 	cases := []struct {
 		val           string
 		expectNull    bool
-		expectedValue int64
+		expectedValue bool
 		expectedErr   error
 	}{
 		{
-			val:           "1",
+			val:           "true",
 			expectNull:    false,
-			expectedValue: 1,
+			expectedValue: true,
+		},
+		{
+			val:           "false",
+			expectNull:    false,
+			expectedValue: false,
 		},
 		{
 			val:           "",
 			expectNull:    true,
-			expectedValue: 0,
+			expectedValue: false,
 		},
 		{
 			val:           "A",
 			expectNull:    false,
-			expectedValue: 0,
+			expectedValue: false,
 			expectedErr:   strconv.ErrSyntax,
 		},
 	}
 
 	for i, tc := range cases {
-		v := Int(tc.val)
+		v := Bool(tc.val)
 
 		if null := v.IsNull(); null != tc.expectNull {
 			t.Fatalf("expected test case %d IsNull to return %t, got %t", i, null, tc.expectNull)
@@ -41,7 +46,7 @@ func TestNullableInt(t *testing.T) {
 
 		value, null, err := v.Value()
 		if value != tc.expectedValue {
-			t.Fatalf("expected test case %d Value to be %d, got %d", i, tc.expectedValue, value)
+			t.Fatalf("expected test case %d Value to be %t, got %t", i, tc.expectedValue, value)
 		}
 		if null != tc.expectNull {
 			t.Fatalf("expected test case %d Value null flag to be %t, got %t", i, tc.expectNull, null)
@@ -57,43 +62,20 @@ func TestNullableInt(t *testing.T) {
 	}
 }
 
-func TestValidationInt(t *testing.T) {
+func TestValidationBool(t *testing.T) {
 	runTestCases(t, []testCase{
 		{
-			val: "1",
-			f:   ValidateTypeStringNullableInt,
+			val: "true",
+			f:   ValidateTypeStringNullableBool,
 		},
 		{
 			val:         "A",
-			f:           ValidateTypeStringNullableInt,
-			expectedErr: regexp.MustCompile(`[\w]+: cannot parse 'A' as int: .*`),
+			f:           ValidateTypeStringNullableBool,
+			expectedErr: regexp.MustCompile(`[\w]+: cannot parse 'A' as boolean: .*`),
 		},
 		{
 			val:         1,
-			f:           ValidateTypeStringNullableInt,
-			expectedErr: regexp.MustCompile(`expected type of [\w]+ to be string`),
-		},
-	})
-}
-
-func TestValidationIntAtLeast(t *testing.T) {
-	runTestCases(t, []testCase{
-		{
-			val: "1",
-			f:   ValidateTypeStringNullableIntAtLeast(1),
-		},
-		{
-			val: "1",
-			f:   ValidateTypeStringNullableIntAtLeast(0),
-		},
-		{
-			val:         "1",
-			f:           ValidateTypeStringNullableIntAtLeast(2),
-			expectedErr: regexp.MustCompile(`expected [\w]+ to be at least \(2\), got 1`),
-		},
-		{
-			val:         1,
-			f:           ValidateTypeStringNullableIntAtLeast(2),
+			f:           ValidateTypeStringNullableBool,
 			expectedErr: regexp.MustCompile(`expected type of [\w]+ to be string`),
 		},
 	})
