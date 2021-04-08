d7925e444d88f4646f168d7f28fcf0c9a072ff28
Brian Flad <bflad417@gmail.com>
2021-04-08 13:07:14 -0400

    tests/service/iam: Refactor and fix IAM Account Alias testing (#18632)
    * tests/service/iam: Refactor and fix IAM Account Alias testing
    
    Reference: https://github.com/hashicorp/terraform-provider-aws/issues/18617
    
    Output from acceptance testing:
    
    ```
    --- PASS: TestAccAWSIAMAccountAlias_serial (24.54s)
        --- PASS: TestAccAWSIAMAccountAlias_serial/DataSource (13.72s)
            --- PASS: TestAccAWSIAMAccountAlias_serial/DataSource/basic (13.72s)
        --- PASS: TestAccAWSIAMAccountAlias_serial/Resource (10.82s)
            --- PASS: TestAccAWSIAMAccountAlias_serial/Resource/basic (10.82s)
    ```
    
    * tests/resource/aws_iam_account_alias: Ensure basic test calls CheckExists function
    
    Output from acceptance testing:
    
    ```
    --- PASS: TestAccAWSIAMAccountAlias_serial (23.99s)
        --- PASS: TestAccAWSIAMAccountAlias_serial/DataSource (13.28s)
            --- PASS: TestAccAWSIAMAccountAlias_serial/DataSource/basic (13.28s)
        --- PASS: TestAccAWSIAMAccountAlias_serial/Resource (10.71s)
            --- PASS: TestAccAWSIAMAccountAlias_serial/Resource/basic (10.71s)
    ```

--- aws/resource_aws_iam_account_alias_test.go
+++ aws/resource_aws_iam_account_alias_test.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/iam"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -13,11 +12,11 @@ import (
 
 func TestAccAWSIAMAccountAlias_serial(t *testing.T) {
 	testCases := map[string]map[string]func(t *testing.T){
-		"Basic": {
-			"basic": testAccAWSIAMAccountAlias_basic_with_datasource,
+		"DataSource": {
+			"basic": testAccAWSIAMAccountAliasDataSource_basic,
 		},
-		"Import": {
-			"import": testAccAWSIAMAccountAlias_importBasic,
+		"Resource": {
+			"basic": testAccAWSIAMAccountAlias_basic,
 		},
 	}
 
@@ -34,10 +33,10 @@ func TestAccAWSIAMAccountAlias_serial(t *testing.T) {
 	}
 }
 
-func testAccAWSIAMAccountAlias_importBasic(t *testing.T) {
+func testAccAWSIAMAccountAlias_basic(t *testing.T) {
 	resourceName := "aws_iam_account_alias.test"
 
-	rstring := acctest.RandString(5)
+	rName := acctest.RandomWithPrefix("tf-acc-test")
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
@@ -46,46 +45,15 @@ func testAccAWSIAMAccountAlias_importBasic(t *testing.T) {
 		CheckDestroy: testAccCheckAWSIAMAccountAliasDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccAWSIAMAccountAliasConfig(rstring),
+				Config: testAccAWSIAMAccountAliasConfig(rName),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccCheckAWSIAMAccountAliasExists(resourceName),
+				),
 			},
-
 			{
 				ResourceName:      resourceName,
 				ImportState:       true,
 				ImportStateVerify: true,
-			},
-		},
-	})
-}
-
-func testAccAWSIAMAccountAlias_basic_with_datasource(t *testing.T) {
-	var account_alias string
-
-	rstring := acctest.RandString(5)
-
-	resource.Test(t, resource.TestCase{
-		PreCheck:     func() { testAccPreCheck(t) },
-		ErrorCheck:   testAccErrorCheck(t, iam.EndpointsID),
-		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckAWSIAMAccountAliasDestroy,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccAWSIAMAccountAliasConfig(rstring),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckAWSIAMAccountAliasExists("aws_iam_account_alias.test", &account_alias),
-				),
-			},
-			{
-				Config: testAccAWSIAMAccountAliasConfig_with_datasource(rstring),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckAWSIAMAccountAliasExists("aws_iam_account_alias.test", &account_alias),
-					testAccCheckAWSIAMAccountAliasDataExists("data.aws_iam_account_alias.current", &account_alias),
-				),
-				// We expect a non-empty plan due to the way data sources and depends_on
-				// work, or don't work. See https://github.com/hashicorp/terraform/issues/11139#issuecomment-275121893
-				// We accept this limitation and feel this test is OK because of the
-				// explicitly check above
-				ExpectNonEmptyPlan: true,
 			},
 		},
 	})
@@ -116,22 +84,7 @@ func testAccCheckAWSIAMAccountAliasDestroy(s *terraform.State) error {
 
 }
 
-func testAccCheckAWSIAMAccountAliasDataExists(n string, a *string) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		rs, ok := s.RootModule().Resources[n]
-		if !ok {
-			return fmt.Errorf("Not found: %s", n)
-		}
-
-		if rs.Primary.Attributes["account_alias"] != *a {
-			return fmt.Errorf("Data Source account_alias didn't match, expected (%s), got (%s)", *a, rs.Primary.Attributes["account_alias"])
-		}
-
-		return nil
-	}
-}
-
-func testAccCheckAWSIAMAccountAliasExists(n string, a *string) resource.TestCheckFunc {
+func testAccCheckAWSIAMAccountAliasExists(n string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		rs, ok := s.RootModule().Resources[n]
 		if !ok {
@@ -151,28 +104,14 @@ func testAccCheckAWSIAMAccountAliasExists(n string, a *string) resource.TestChec
 			return fmt.Errorf("Bad: Account alias %q does not exist", rs.Primary.ID)
 		}
 
-		*a = aws.StringValue(resp.AccountAliases[0])
-
 		return nil
 	}
 }
 
-func testAccAWSIAMAccountAliasConfig_with_datasource(rstring string) string {
+func testAccAWSIAMAccountAliasConfig(rName string) string {
 	return fmt.Sprintf(`
 resource "aws_iam_account_alias" "test" {
-  account_alias = "terraform-%s-alias"
+  account_alias = %[1]q
 }
-
-data "aws_iam_account_alias" "current" {
-  depends_on = [aws_iam_account_alias.test]
-}
-`, rstring)
-}
-
-func testAccAWSIAMAccountAliasConfig(rstring string) string {
-	return fmt.Sprintf(`
-resource "aws_iam_account_alias" "test" {
-  account_alias = "terraform-%s-alias"
-}
-`, rstring)
+`, rName)
 }
