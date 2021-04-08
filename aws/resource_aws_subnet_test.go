576d420115eecc769c331f59853536f56de44e3f
Dirk Avery <dirk.avery@gmail.com>
2021-04-08 12:36:26 -0400

    tests/r/subnet: Remove unneeded len check

--- aws/resource_aws_subnet_test.go
+++ aws/resource_aws_subnet_test.go
@@ -98,11 +98,8 @@ func testSweepSubnets(region string) error {
 		errs = multierror.Append(errs, fmt.Errorf("error describing EC2 Subnets for %s: %w", region, err))
 	}
 
-	if len(sweepResources) > 0 {
-		// Any errors didn't prevent gathering some sweeping work, so do it.
-		if err := testSweepResourceOrchestrator(sweepResources); err != nil {
-			errs = multierror.Append(errs, fmt.Errorf("error sweeping EC2 Subnets for %s: %w", region, err))
-		}
+	if err = testSweepResourceOrchestrator(sweepResources); err != nil {
+		errs = multierror.Append(errs, fmt.Errorf("error sweeping EC2 Subnets for %s: %w", region, err))
 	}
 
 	if testSweepSkipSweepError(errs.ErrorOrNil()) {
