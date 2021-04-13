8949a06eaf72da036a09d1d3d39f719aa0202bd7
Dirk Avery <dirk.avery@gmail.com>
2021-04-13 00:20:41 -0400

    r/dynamodb_table: Add status checkers

--- aws/internal/service/dynamodb/waiter/status.go
+++ aws/internal/service/dynamodb/waiter/status.go
@@ -3,21 +3,16 @@ package waiter
 import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/terraform-providers/terraform-provider-aws/aws/internal/service/dynamodb/finder"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/tfresource"
-)
-
-const (
-	ReplicaStatusNotFound    = "NOT_FOUND"
-	ReplicaStatusEmptyResult = "EMPTY_RESULT"
 )
 
 func DynamoDBTableStatus(conn *dynamodb.DynamoDB, tableName string) resource.StateRefreshFunc {
 	return func() (interface{}, string, error) {
 		table, err := finder.DynamoDBTableByName(conn, tableName)
 
-		if tfresource.NotFound(err) {
+		if tfawserr.ErrCodeEquals(err, dynamodb.ErrCodeResourceNotFoundException) {
 			return nil, "", nil
 		}
 
@@ -37,8 +32,8 @@ func DynamoDBReplicaStatus(conn *dynamodb.DynamoDB, tableName, region string) re
 	return func() (interface{}, string, error) {
 		replica, err := finder.DynamoDBReplicaByTableNameRegion(conn, tableName, region)
 
-		if tfresource.NotFound(err) {
-			return nil, ReplicaStatusNotFound, err
+		if tfawserr.ErrCodeEquals(err, dynamodb.ErrCodeResourceNotFoundException) {
+			return nil, "", nil
 		}
 
 		if err != nil {
@@ -46,9 +41,94 @@ func DynamoDBReplicaStatus(conn *dynamodb.DynamoDB, tableName, region string) re
 		}
 
 		if replica == nil {
-			return nil, ReplicaStatusEmptyResult, nil
+			return nil, dynamodb.ReplicaStatusCreating, nil
 		}
 
 		return replica, aws.StringValue(replica.ReplicaStatus), nil
+	}
+}
+
+func DynamoDBGSIStatus(conn *dynamodb.DynamoDB, tableName, indexName string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		gsi, err := finder.DynamoDBGSIByTableNameIndexName(conn, tableName, indexName)
+
+		if tfawserr.ErrCodeEquals(err, dynamodb.ErrCodeResourceNotFoundException) {
+			return nil, "", nil
+		}
+
+		if err != nil {
+			return nil, "", err
+		}
+
+		if gsi == nil {
+			return nil, "", nil
+		}
+
+		return gsi, aws.StringValue(gsi.IndexStatus), nil
+	}
+}
+
+func DynamoDBPITRStatus(conn *dynamodb.DynamoDB, tableName string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		pitr, err := finder.DynamoDBPITRDescriptionByTableName(conn, tableName)
+
+		if tfawserr.ErrCodeEquals(err, dynamodb.ErrCodeResourceNotFoundException) {
+			return nil, "", nil
+		}
+
+		if err != nil {
+			return nil, "", err
+		}
+
+		if pitr == nil {
+			return nil, "", nil
+		}
+
+		return pitr, aws.StringValue(pitr.PointInTimeRecoveryStatus), nil
+	}
+}
+
+func DynamoDBTTLStatus(conn *dynamodb.DynamoDB, tableName string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		ttl, err := finder.DynamoDBTTLRDescriptionByTableName(conn, tableName)
+
+		if tfawserr.ErrCodeEquals(err, dynamodb.ErrCodeResourceNotFoundException) {
+			return nil, "", nil
+		}
+
+		if err != nil {
+			return nil, "", err
+		}
+
+		if ttl == nil {
+			return nil, "", nil
+		}
+
+		return ttl, aws.StringValue(ttl.TimeToLiveStatus), nil
+	}
+}
+
+func DynamoDBTableSESStatus(conn *dynamodb.DynamoDB, tableName string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		table, err := finder.DynamoDBTableByName(conn, tableName)
+
+		if tfawserr.ErrCodeEquals(err, dynamodb.ErrCodeResourceNotFoundException) {
+			return nil, "", nil
+		}
+
+		if err != nil {
+			return nil, "", err
+		}
+
+		if table == nil {
+			return nil, "", nil
+		}
+
+		// Disabling SSE returns null SSEDescription
+		if table.SSEDescription == nil {
+			return table, dynamodb.SSEStatusDisabled, nil
+		}
+
+		return table, aws.StringValue(table.SSEDescription.Status), nil
 	}
 }
