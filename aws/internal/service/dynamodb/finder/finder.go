92729ac055528696d736690e3cbd0b57aaaca03a
Dirk Avery <dirk.avery@gmail.com>
2021-04-13 00:20:14 -0400

    r/dynamodb_table: Add finders

--- aws/internal/service/dynamodb/finder/finder.go
+++ aws/internal/service/dynamodb/finder/finder.go
@@ -3,7 +3,6 @@ package finder
 import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
 func DynamoDBTableByName(conn *dynamodb.DynamoDB, tableName string) (*dynamodb.TableDescription, error) {
@@ -32,9 +31,7 @@ func DynamoDBReplicaByTableNameRegion(conn *dynamodb.DynamoDB, tableName, region
 	}
 
 	if table == nil {
-		return nil, &resource.NotFoundError{
-			Message: dynamodb.ErrCodeTableNotFoundException,
-		}
+		return nil, nil
 	}
 
 	for _, replica := range table.Replicas {
@@ -44,4 +41,68 @@ func DynamoDBReplicaByTableNameRegion(conn *dynamodb.DynamoDB, tableName, region
 	}
 
 	return nil, nil
+}
+
+func DynamoDBGSIByTableNameIndexName(conn *dynamodb.DynamoDB, tableName, indexName string) (*dynamodb.GlobalSecondaryIndexDescription, error) {
+	table, err := DynamoDBTableByName(conn, tableName)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if table == nil {
+		return nil, nil
+	}
+
+	for _, gsi := range table.GlobalSecondaryIndexes {
+		if aws.StringValue(gsi.IndexName) == indexName {
+			return gsi, nil
+		}
+	}
+
+	return nil, nil
+}
+
+func DynamoDBPITRDescriptionByTableName(conn *dynamodb.DynamoDB, tableName string) (*dynamodb.PointInTimeRecoveryDescription, error) {
+	input := &dynamodb.DescribeContinuousBackupsInput{
+		TableName: aws.String(tableName),
+	}
+
+	output, err := conn.DescribeContinuousBackups(input)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if output == nil {
+		return nil, nil
+	}
+
+	if output.ContinuousBackupsDescription == nil || output.ContinuousBackupsDescription.PointInTimeRecoveryDescription == nil {
+		return nil, nil
+	}
+
+	return output.ContinuousBackupsDescription.PointInTimeRecoveryDescription, nil
+}
+
+func DynamoDBTTLRDescriptionByTableName(conn *dynamodb.DynamoDB, tableName string) (*dynamodb.TimeToLiveDescription, error) {
+	input := &dynamodb.DescribeTimeToLiveInput{
+		TableName: aws.String(tableName),
+	}
+
+	output, err := conn.DescribeTimeToLive(input)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if output == nil {
+		return nil, nil
+	}
+
+	if output.TimeToLiveDescription == nil {
+		return nil, nil
+	}
+
+	return output.TimeToLiveDescription, nil
 }
