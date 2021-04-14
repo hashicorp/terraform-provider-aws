dc9e0b3eadeb6c4002a0c0aee23d1e2a5048e3a1
drfaust92 <ilia.lazebnik@gmail.com>
2021-04-14 20:13:14 +0300

    waiter

--- aws/internal/service/rds/waiter/waiter.go
+++ aws/internal/service/rds/waiter/waiter.go
@@ -10,9 +10,13 @@ import (
 const (
 	// Maximum amount of time to wait for an EventSubscription to return Deleted
 	EventSubscriptionDeletedTimeout = 10 * time.Minute
+	// Maximum amount of time to wait for an DBProxyEndpoint to return Available
+	DBProxyEndpointAvailableTimeout = 10 * time.Minute
+	// Maximum amount of time to wait for an DBProxyEndpoint to return Deleted
+	DBProxyEndpointDeletedTimeout = 10 * time.Minute
 )
 
-// DeploymentDeployed waits for a EventSubscription to return Deleted
+// EventSubscriptionDeleted waits for a EventSubscription to return Deleted
 func EventSubscriptionDeleted(conn *rds.RDS, subscriptionName string) (*rds.EventSubscription, error) {
 	stateConf := &resource.StateChangeConf{
 		Pending: []string{"deleting"},
@@ -24,6 +28,45 @@ func EventSubscriptionDeleted(conn *rds.RDS, subscriptionName string) (*rds.Even
 	outputRaw, err := stateConf.WaitForState()
 
 	if v, ok := outputRaw.(*rds.EventSubscription); ok {
+		return v, err
+	}
+
+	return nil, err
+}
+
+// DBProxyEndpointAvailable waits for a DBProxyEndpoint to return Available
+func DBProxyEndpointAvailable(conn *rds.RDS, dbProxyName, dbProxyEndpointName, arn string) (*rds.DBProxyEndpoint, error) {
+	stateConf := &resource.StateChangeConf{
+		Pending: []string{
+			rds.DBProxyEndpointStatusCreating,
+			rds.DBProxyEndpointStatusModifying,
+		},
+		Target:  []string{rds.DBProxyEndpointStatusAvailable},
+		Refresh: DBProxyEndpointStatus(conn, dbProxyName, dbProxyEndpointName, arn),
+		Timeout: DBProxyEndpointAvailableTimeout,
+	}
+
+	outputRaw, err := stateConf.WaitForState()
+
+	if v, ok := outputRaw.(*rds.DBProxyEndpoint); ok {
+		return v, err
+	}
+
+	return nil, err
+}
+
+// DBProxyEndpointDeleted waits for a DBProxyEndpoint to return Deleted
+func DBProxyEndpointDeleted(conn *rds.RDS, dbProxyName, dbProxyEndpointName, arn string) (*rds.DBProxyEndpoint, error) {
+	stateConf := &resource.StateChangeConf{
+		Pending: []string{rds.DBProxyEndpointStatusDeleting},
+		Target:  []string{},
+		Refresh: DBProxyEndpointStatus(conn, dbProxyName, dbProxyEndpointName, arn),
+		Timeout: DBProxyEndpointDeletedTimeout,
+	}
+
+	outputRaw, err := stateConf.WaitForState()
+
+	if v, ok := outputRaw.(*rds.DBProxyEndpoint); ok {
 		return v, err
 	}
 
