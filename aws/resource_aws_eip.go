0540ed2e74b1e9b7d9dd5cf0543284e3a20b41b7
Dirk Avery <dirk.avery@gmail.com>
2021-04-15 22:28:46 -0400

    r/eip: Fix error message

--- aws/resource_aws_eip.go
+++ aws/resource_aws_eip.go
@@ -148,7 +148,7 @@ func resourceAwsEipCreate(d *schema.ResourceData, meta interface{}) error {
 	if v := d.Get("tags").(map[string]interface{}); len(v) > 0 {
 		supportedPlatforms := meta.(*AWSClient).supportedplatforms
 		if domainOpt != ec2.DomainTypeVpc && len(supportedPlatforms) > 0 && hasEc2Classic(supportedPlatforms) {
-			return fmt.Errorf("tags can not be set for an EIP in EC2 Classic")
+			return fmt.Errorf("tags cannot be set for a standard-domain EIP - must be a VPC-domain EIP")
 		}
 		allocOpts.TagSpecifications = ec2TagSpecificationsFromMap(d.Get("tags").(map[string]interface{}), ec2.ResourceTypeElasticIp)
 	}
@@ -400,7 +400,7 @@ func resourceAwsEipUpdate(d *schema.ResourceData, meta interface{}) error {
 
 	if d.HasChange("tags") && !d.IsNewResource() {
 		if d.Get("domain").(string) == ec2.DomainTypeStandard {
-			return fmt.Errorf("tags can not be set for an EIP in EC2 Classic")
+			return fmt.Errorf("tags cannot be set for a standard-domain EIP - must be a VPC-domain EIP")
 		}
 		o, n := d.GetChange("tags")
 		if err := keyvaluetags.Ec2UpdateTags(ec2conn, d.Id(), o, n); err != nil {
