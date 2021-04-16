82beb3c4ac744ac7a560cc31e0ed73c12c9aacca
Dirk Avery <dirk.avery@gmail.com>
2021-04-16 18:58:23 -0400

    tests/ds/kms_public_key: Minor fixes to tests

--- aws/data_source_aws_kms_public_key_test.go
+++ aws/data_source_aws_kms_public_key_test.go
@@ -13,7 +13,7 @@ import (
 func TestAccDataSourceAwsKmsPublicKey_basic(t *testing.T) {
 	resourceName := "aws_kms_key.test"
 	datasourceName := "data.aws_kms_public_key.test"
-	rName := fmt.Sprintf("tf-testacc-kms-public-key-%s", acctest.RandString(13))
+	rName := acctest.RandomWithPrefix("tf-acc-test")
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:   func() { testAccPreCheck(t) },
@@ -24,7 +24,36 @@ func TestAccDataSourceAwsKmsPublicKey_basic(t *testing.T) {
 				Config: testAccDataSourceAwsKmsPublicKeyConfig(rName),
 				Check: resource.ComposeTestCheckFunc(
 					testAccDataSourceAwsKmsPublicKeyCheck(datasourceName),
-					resource.TestCheckResourceAttrPair(datasourceName, "id", resourceName, "arn"),
+					resource.TestCheckResourceAttrPair(datasourceName, "arn", resourceName, "arn"),
+					resource.TestCheckResourceAttrPair(datasourceName, "customer_master_key_spec", resourceName, "customer_master_key_spec"),
+					resource.TestCheckResourceAttrPair(datasourceName, "key_id", resourceName, "arn"),
+					resource.TestCheckResourceAttrPair(datasourceName, "key_usage", resourceName, "key_usage"),
+					resource.TestCheckResourceAttrSet(datasourceName, "public_key"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDataSourceAwsKmsPublicKey_encrypt(t *testing.T) {
+	resourceName := "aws_kms_key.test"
+	datasourceName := "data.aws_kms_public_key.test"
+	rName := acctest.RandomWithPrefix("tf-acc-test")
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:   func() { testAccPreCheck(t) },
+		ErrorCheck: testAccErrorCheck(t, kms.EndpointsID),
+		Providers:  testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceAwsKmsPublicKeyEncryptConfig(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccDataSourceAwsKmsPublicKeyCheck(datasourceName),
+					resource.TestCheckResourceAttrPair(datasourceName, "arn", resourceName, "arn"),
+					resource.TestCheckResourceAttrPair(datasourceName, "key_id", resourceName, "arn"),
+					resource.TestCheckResourceAttrPair(datasourceName, "customer_master_key_spec", resourceName, "customer_master_key_spec"),
+					resource.TestCheckResourceAttrPair(datasourceName, "key_usage", resourceName, "key_usage"),
+					resource.TestCheckResourceAttrSet(datasourceName, "public_key"),
 				),
 			},
 		},
@@ -49,6 +78,21 @@ resource "aws_kms_key" "test" {
   deletion_window_in_days  = 7
   customer_master_key_spec = "RSA_2048"
   key_usage                = "SIGN_VERIFY"
+}
+
+data "aws_kms_public_key" "test" {
+  key_id = aws_kms_key.test.arn
+}
+`, rName)
+}
+
+func testAccDataSourceAwsKmsPublicKeyEncryptConfig(rName string) string {
+	return fmt.Sprintf(`
+resource "aws_kms_key" "test" {
+  description              = %[1]q
+  deletion_window_in_days  = 7
+  customer_master_key_spec = "RSA_2048"
+  key_usage                = "ENCRYPT_DECRYPT"
 }
 
 data "aws_kms_public_key" "test" {
