46c7e0552828176e860b457a8ba95938260d8242
Phil Nichol <35630607+philnichol@users.noreply.github.com>
2021-04-23 14:36:56 +0100

    added eks cluster_name validation

--- aws/validators_test.go
+++ aws/validators_test.go
@@ -3246,3 +3246,55 @@ func TestValidateTypeStringIsDateOrInt(t *testing.T) {
 		}
 	}
 }
+
+func TestResourceAWSEKSClusterNameValidation(t *testing.T) {
+	cases := []struct {
+		Value    string
+		ErrCount int
+	}{
+		{
+			Value:    "my-valid-eks-cluster_1_dev",
+			ErrCount: 0,
+		},
+		{
+			Value:    `_invalid`,
+			ErrCount: 1,
+		},
+		{
+			Value:    `-invalid`,
+			ErrCount: 1,
+		},
+		{
+			Value:    `invalid@`,
+			ErrCount: 1,
+		},
+		{
+			Value:    `invalid*`,
+			ErrCount: 1,
+		},
+		{
+			Value:    `invalid:`,
+			ErrCount: 1,
+		},
+		{
+			Value:    `invalid$`,
+			ErrCount: 1,
+		},
+		{
+			Value:    ``,
+			ErrCount: 2,
+		},
+		{
+			Value:    acctest.RandStringFromCharSet(101, acctest.CharSetAlpha),
+			ErrCount: 1,
+		},
+	}
+
+	for _, tc := range cases {
+		_, errors := validateEKSClusterName(tc.Value, "cluster_name")
+
+		if len(errors) != tc.ErrCount {
+			t.Fatalf("Expected the EKS Cluster Name to trigger a validation error: %s, expected %d, got %d errors", tc.Value, tc.ErrCount, len(errors))
+		}
+	}
+}
