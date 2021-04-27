1beedfefcbcf9567d682f68736948256acd06c48
Dirk Avery <dirk.avery@gmail.com>
2021-04-27 11:05:22 -0400

    provider: Add Service Catalog Product

--- aws/provider.go
+++ aws/provider.go
@@ -995,6 +995,7 @@ func Provider() *schema.Provider {
 			"aws_securityhub_product_subscription":                    resourceAwsSecurityHubProductSubscription(),
 			"aws_securityhub_standards_subscription":                  resourceAwsSecurityHubStandardsSubscription(),
 			"aws_servicecatalog_portfolio":                            resourceAwsServiceCatalogPortfolio(),
+			"aws_servicecatalog_product":                              resourceAwsServiceCatalogProduct(),
 			"aws_service_discovery_http_namespace":                    resourceAwsServiceDiscoveryHttpNamespace(),
 			"aws_service_discovery_private_dns_namespace":             resourceAwsServiceDiscoveryPrivateDnsNamespace(),
 			"aws_service_discovery_public_dns_namespace":              resourceAwsServiceDiscoveryPublicDnsNamespace(),
