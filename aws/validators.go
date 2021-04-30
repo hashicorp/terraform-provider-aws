9589fb61c63ebdca0152c5eb4d257728375f7fff
drfaust92 <ilia.lazebnik@gmail.com>
2021-04-30 11:39:59 +0300

    add map key in slice validation func

--- aws/validators.go
+++ aws/validators.go
@@ -2446,6 +2446,29 @@ func MapKeysDoNotMatch(r *regexp.Regexp, message string) schema.SchemaValidateFu
 	}
 }
 
+func MapKeyInSlice(valid []string, ignoreCase bool) schema.SchemaValidateFunc {
+	return func(i interface{}, k string) (warnings []string, errors []error) {
+		v, ok := i.(map[string]interface{})
+		if !ok {
+			errors = append(errors, fmt.Errorf("expected type of %[1]q to be Map, got %[1]T", k))
+			return warnings, errors
+		}
+
+		for key := range v {
+			for _, str := range valid {
+				if key == str || (ignoreCase && strings.ToLower(key) == strings.ToLower(str)) {
+					return warnings, errors
+				}
+			}
+
+			errors = append(errors, fmt.Errorf("expected %s to be one of %v, got %s", k, valid, key))
+			return warnings, errors
+		}
+
+		return warnings, errors
+	}
+}
+
 var validateTypeStringIsDateOrPositiveInt = validation.Any(
 	validation.IsRFC3339Time,
 	validation.StringMatch(regexp.MustCompile(`^\d+$`), "must be a positive integer value"),
