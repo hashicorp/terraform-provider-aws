7fbba43b8b3e6d44357dbc00a90ed0a77fcc0420
Dirk Avery <dirk.avery@gmail.com>
2021-05-07 14:57:08 -0400

    validators: Add validator for servicecatalog principal

--- aws/validators.go
+++ aws/validators.go
@@ -697,6 +697,32 @@ func validatePrincipal(v interface{}, k string) (ws []string, errors []error) {
 	return ws, errors
 }
 
+func validateServiceCatalogSharePrincipal(v interface{}, k string) (ws []string, errors []error) {
+	value := v.(string)
+	// either account ID, or organization or organization unit
+
+	wsAccount, errorsAccount := validateAwsAccountId(v, k)
+
+	if len(errorsAccount) == 0 {
+		return wsAccount, errorsAccount
+	}
+
+	wsARN, errorsARN := validateArn(v, k)
+	ws = append(ws, wsARN...)
+	errors = append(errors, errorsARN...)
+
+	pattern := `^arn:[\w-]+:organizations:.*:(ou|organization)/`
+	if !regexp.MustCompile(pattern).MatchString(value) {
+		errors = append(errors, fmt.Errorf("%q does not look like an OU or organization: %q", k, value))
+	}
+
+	if len(errors) > 0 {
+		errors = append(errors, errorsAccount...)
+	}
+
+	return ws, errors
+}
+
 func validateArn(v interface{}, k string) (ws []string, errors []error) {
 	value := v.(string)
 
