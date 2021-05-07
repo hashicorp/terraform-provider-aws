4a6285513abf72c6ec23165daee2d0f8bcd4a75d
Dirk Avery <dirk.avery@gmail.com>
2021-05-07 12:47:26 -0400

    servicecatalog: Add waiter for orgs/portfolio_share

--- aws/internal/service/servicecatalog/waiter/waiter.go
+++ aws/internal/service/servicecatalog/waiter/waiter.go
@@ -15,11 +15,17 @@ const (
 	TagOptionReadyTimeout  = 3 * time.Minute
 	TagOptionDeleteTimeout = 3 * time.Minute
 
+	PortfolioShareCreateTimeout = 3 * time.Minute
+
+	OrganizationsAccessStableTimeout = 3 * time.Minute
+
 	StatusNotFound    = "NOT_FOUND"
 	StatusUnavailable = "UNAVAILABLE"
 
 	// AWS documentation is wrong, says that status will be "AVAILABLE" but it is actually "CREATED"
 	ProductStatusCreated = "CREATED"
+
+	OrganizationAccessStatusError = "ERROR"
 )
 
 func ProductReady(conn *servicecatalog.ServiceCatalog, acceptLanguage, productID string) (*servicecatalog.DescribeProductAsAdminOutput, error) {
@@ -88,4 +94,89 @@ func TagOptionDeleted(conn *servicecatalog.ServiceCatalog, id string) error {
 	}
 
 	return err
+}
+
+func PortfolioShareReady(conn *servicecatalog.ServiceCatalog, portfolioID, shareType, accountID, orgNodeValue string) (*servicecatalog.PortfolioShareDetail, error) {
+	stateConf := &resource.StateChangeConf{
+		Pending: []string{servicecatalog.ShareStatusNotStarted, servicecatalog.ShareStatusInProgress, StatusNotFound, StatusUnavailable},
+		Target:  []string{servicecatalog.ShareStatusCompleted},
+		Refresh: PortfolioShareStatus(conn, portfolioID, shareType, accountID, orgNodeValue),
+		Timeout: PortfolioShareCreateTimeout,
+	}
+
+	outputRaw, err := stateConf.WaitForState()
+
+	if output, ok := outputRaw.(*servicecatalog.PortfolioShareDetail); ok {
+		return output, err
+	}
+
+	return nil, err
+}
+
+func PortfolioShareCreatedWithToken(conn *servicecatalog.ServiceCatalog, token string) (*servicecatalog.DescribePortfolioShareStatusOutput, error) {
+	stateConf := &resource.StateChangeConf{
+		Pending: []string{servicecatalog.ShareStatusNotStarted, servicecatalog.ShareStatusInProgress, StatusNotFound, StatusUnavailable},
+		Target:  []string{servicecatalog.ShareStatusCompleted},
+		Refresh: PortfolioShareStatusWithToken(conn, token),
+		Timeout: PortfolioShareCreateTimeout,
+	}
+
+	outputRaw, err := stateConf.WaitForState()
+
+	if output, ok := outputRaw.(*servicecatalog.DescribePortfolioShareStatusOutput); ok {
+		return output, err
+	}
+
+	return nil, err
+}
+
+func PortfolioShareDeleted(conn *servicecatalog.ServiceCatalog, portfolioID, shareType, accountID, orgNodeValue string) (*servicecatalog.PortfolioShareDetail, error) {
+	stateConf := &resource.StateChangeConf{
+		Pending: []string{servicecatalog.ShareStatusNotStarted, servicecatalog.ShareStatusInProgress, servicecatalog.ShareStatusCompleted, StatusUnavailable},
+		Target:  []string{StatusNotFound},
+		Refresh: PortfolioShareStatus(conn, portfolioID, shareType, accountID, orgNodeValue),
+		Timeout: PortfolioShareCreateTimeout,
+	}
+
+	outputRaw, err := stateConf.WaitForState()
+
+	if output, ok := outputRaw.(*servicecatalog.PortfolioShareDetail); ok {
+		return output, err
+	}
+
+	return nil, err
+}
+
+func PortfolioShareDeletedWithToken(conn *servicecatalog.ServiceCatalog, token string) (*servicecatalog.DescribePortfolioShareStatusOutput, error) {
+	stateConf := &resource.StateChangeConf{
+		Pending: []string{servicecatalog.ShareStatusCompleted, servicecatalog.ShareStatusNotStarted, servicecatalog.ShareStatusInProgress, StatusNotFound, StatusUnavailable},
+		Target:  []string{StatusNotFound},
+		Refresh: PortfolioShareStatusWithToken(conn, token),
+		Timeout: PortfolioShareCreateTimeout,
+	}
+
+	outputRaw, err := stateConf.WaitForState()
+
+	if output, ok := outputRaw.(*servicecatalog.DescribePortfolioShareStatusOutput); ok {
+		return output, err
+	}
+
+	return nil, err
+}
+
+func OrganizationsAccessStable(conn *servicecatalog.ServiceCatalog) (string, error) {
+	stateConf := &resource.StateChangeConf{
+		Pending: []string{servicecatalog.AccessStatusUnderChange, StatusNotFound, StatusUnavailable},
+		Target:  []string{servicecatalog.AccessStatusEnabled, servicecatalog.AccessStatusDisabled},
+		Refresh: OrganizationsAccessStatus(conn),
+		Timeout: OrganizationsAccessStableTimeout,
+	}
+
+	outputRaw, err := stateConf.WaitForState()
+
+	if output, ok := outputRaw.(string); ok {
+		return output, err
+	}
+
+	return "", err
 }
