48f46814beb69134e5dab5138b4d78b3de871ebc
Jonathan Brown <frumpledbiscuit@gmail.com>
2021-05-10 20:12:49 -0600

    Add untested tests

--- aws/resource_aws_qldb_stream_test.go
+++ aws/resource_aws_qldb_stream_test.go
@@ -3,11 +3,16 @@ package aws
 import (
 	"fmt"
 	"log"
+	"regexp"
+	"testing"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/qldb"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 func init() {
@@ -25,7 +30,33 @@ func testSweepQLDBStreams(region string) error {
 	}
 
 	conn := client.(*AWSClient).qldbconn
-	input := &qldb.ListJournalKinesisStreamsForLedgerInput{}
+	input := &qldb.ListLedgersInput{}
+	page, err := conn.ListLedgers(input)
+
+	if err != nil {
+		if testSweepSkipSweepError(err) {
+			log.Printf("[WARN] Skipping QLDB Stream sweep for %s: %s", region, err)
+			return nil
+		}
+		return fmt.Errorf("Error listing QLDB Ledgers for QLDB Stream Sweep: %s", err)
+	}
+
+	for _, item := range page.Ledgers {
+		ledgerName := aws.StringValue(item.Name)
+		err := testSweepQLDBLedgerStreams(conn, region, ledgerName)
+		if err != nil {
+			log.Printf("[ERROR] Failed to delete QLDB Stream for Ledger %s: %s", ledgerName, err)
+			continue
+		}
+	}
+
+	return nil
+}
+
+func testSweepQLDBLedgerStreams(conn *qldb.QLDB, region string, ledgerName string) error {
+	input := &qldb.ListJournalKinesisStreamsForLedgerInput{
+		LedgerName: aws.String(ledgerName),
+	}
 	page, err := conn.ListJournalKinesisStreamsForLedger(input)
 
 	if err != nil {
@@ -64,14 +95,13 @@ func testSweepQLDBStreams(region string) error {
 func waitForQLDBStreamCancellation(conn *qldb.QLDB, ledgerName string, streamID string) error {
 	stateConf := resource.StateChangeConf{
 		Pending: []string{
-			// qldb.StreamStatusFailed,
 			qldb.StreamStatusActive,
 			qldb.StreamStatusImpaired,
 		},
 		Target: []string{
 			qldb.StreamStatusCanceled,
 			qldb.StreamStatusCompleted,
-			qldb.StreamStatusFailed, // TODO: Double check, but this is also a terminal state?
+			qldb.StreamStatusFailed,
 		},
 		Timeout:    5 * time.Minute,
 		MinTimeout: 1 * time.Second,
@@ -89,7 +119,7 @@ func waitForQLDBStreamCancellation(conn *qldb.QLDB, ledgerName string, streamID 
 				return nil, qldb.ErrCodeResourceInUseException, err
 			}
 
-			return resp, aws.StringValue(resp.State), nil
+			return resp, aws.StringValue(resp.Stream.Status), nil
 		},
 	}
 
@@ -98,102 +128,109 @@ func waitForQLDBStreamCancellation(conn *qldb.QLDB, ledgerName string, streamID 
 	return err
 }
 
-// func TestAccAWSQLDBStream_basic(t *testing.T) {
-// 	var qldbCluster qldb.DescribeLedgerOutput
-// 	rInt := acctest.RandInt()
-// 	resourceName := "aws_qldb_ledger.test"
+func TestAccAWSQLDBStream_basic(t *testing.T) {
+	var qldbCluster qldb.DescribeJournalKinesisStreamOutput
+	rInt := acctest.RandInt()
+	resourceName := "aws_qldb_stream.test"
 
-// 	resource.ParallelTest(t, resource.TestCase{
-// 		PreCheck:     func() { testAccPreCheck(t); testAccPartitionHasServicePreCheck(qldb.EndpointsID, t) },
-// 		ErrorCheck:   testAccErrorCheck(t, qldb.EndpointsID),
-// 		Providers:    testAccProviders,
-// 		CheckDestroy: testAccCheckAWSQLDBLedgerDestroy,
-// 		Steps: []resource.TestStep{
-// 			{
-// 				Config: testAccAWSQLDBLedgerConfig(rInt),
-// 				Check: resource.ComposeTestCheckFunc(
-// 					testAccCheckAWSQLDBLedgerExists(resourceName, &qldbCluster),
-// 					testAccMatchResourceAttrRegionalARN(resourceName, "arn", "qldb", regexp.MustCompile(`ledger/.+`)),
-// 					resource.TestMatchResourceAttr(resourceName, "name", regexp.MustCompile("test-ledger-[0-9]+")),
-// 					resource.TestCheckResourceAttr(resourceName, "deletion_protection", "false"),
-// 					resource.TestCheckResourceAttr(resourceName, "tags.%", "0"),
-// 				),
-// 			},
-// 			{
-// 				ResourceName:      resourceName,
-// 				ImportState:       true,
-// 				ImportStateVerify: true,
-// 			},
-// 		},
-// 	})
-// }
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t); testAccPartitionHasServicePreCheck(qldb.EndpointsID, t) },
+		ErrorCheck:   testAccErrorCheck(t, qldb.EndpointsID),
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAWSQLDBStreamCancel,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAWSQLDBLedgerConfig(rInt),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAWSQLDBStreamExists(resourceName, &qldbCluster),
+					testAccMatchResourceAttrRegionalARN(resourceName, "arn", "qldb", regexp.MustCompile(`stream/.+`)),
+					resource.TestMatchResourceAttr(resourceName, "stream_name", regexp.MustCompile("test-stream-[0-9]+")),
+					resource.TestCheckResourceAttr(resourceName, "deletion_protection", "false"),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", "0"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
 
-// func testAccCheckAWSQLDBStreamCancel(s *terraform.State) error {
-// 	return testAccCheckAWSLedgerDestroyWithProvider(s, testAccProvider)
-// }
+// reference: testAccCheckAWSLedgerDestroyWithProvider
+func testAccCheckAWSQLDBStreamCancel(s *terraform.State) error {
+	return testAccCheckAWSQLDBStreamCancelWithProvider(s, testAccProvider)
+}
 
-// func testAccCheckAWSStreamCancelWithProvider(s *terraform.State, provider *schema.Provider) error {
-// 	conn := provider.Meta().(*AWSClient).qldbconn
+func testAccCheckAWSQLDBStreamCancelWithProvider(s *terraform.State, provider *schema.Provider) error {
+	conn := provider.Meta().(*AWSClient).qldbconn
 
-// 	for _, rs := range s.RootModule().Resources {
-// 		if rs.Type != "aws_qldb_ledger" {
-// 			continue
-// 		}
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "aws_qldb_ledger" {
+			continue
+		}
 
-// 		// Try to find the Group
-// 		var err error
-// 		resp, err := conn.DescribeLedger(
-// 			&qldb.DescribeLedgerInput{
-// 				Name: aws.String(rs.Primary.ID),
-// 			})
+		// Try to find the Group
+		var err error
+		resp, err := conn.DescribeLedger(
+			&qldb.DescribeLedgerInput{
+				Name: aws.String(rs.Primary.ID),
+			})
 
-// 		if err == nil {
-// 			if len(aws.StringValue(resp.Name)) != 0 && aws.StringValue(resp.Name) == rs.Primary.ID {
-// 				return fmt.Errorf("QLDB Ledger %s still exists", rs.Primary.ID)
-// 			}
-// 		}
+		if err == nil {
+			if len(aws.StringValue(resp.Name)) != 0 && aws.StringValue(resp.Name) == rs.Primary.ID {
+				return fmt.Errorf("QLDB Ledger %s still exists", rs.Primary.ID)
+			}
+		}
 
-// 		// Return nil if the cluster is already destroyed
-// 		if isAWSErr(err, qldb.ErrCodeResourceNotFoundException, "") {
-// 			continue
-// 		}
+		// Return nil if the cluster is already destroyed
+		if isAWSErr(err, qldb.ErrCodeResourceNotFoundException, "") {
+			continue
+		}
 
-// 		if err != nil {
-// 			return err
-// 		}
-// 	}
+		if err != nil {
+			return err
+		}
+	}
 
-// 	return nil
-// }
+	return nil
+}
 
-// func testAccCheckAWSQLDBStreamExists(n string, v *qldb.DescribeLedgerOutput) resource.TestCheckFunc {
-// 	return func(s *terraform.State) error {
-// 		rs, ok := s.RootModule().Resources[n]
-// 		if !ok {
-// 			return fmt.Errorf("Not found: %s", n)
-// 		}
+func testAccCheckAWSQLDBStreamExists(n string, v *qldb.DescribeJournalKinesisStreamOutput) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
 
-// 		if rs.Primary.ID == "" {
-// 			return fmt.Errorf("No QLDB Ledger ID is set")
-// 		}
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No QLDB Stream ID is set")
+		}
 
-// 		conn := testAccProvider.Meta().(*AWSClient).qldbconn
-// 		resp, err := conn.DescribeLedger(&qldb.DescribeLedgerInput{
-// 			Name: aws.String(rs.Primary.ID),
-// 		})
+		ledgerName, ok := rs.Primary.Attributes["ledger_name"]
+		if !ok {
+			return fmt.Errorf("No ledger name value has been set")
+		}
 
-// 		if err != nil {
-// 			return err
-// 		}
+		conn := testAccProvider.Meta().(*AWSClient).qldbconn
+		resp, err := conn.DescribeJournalKinesisStream(&qldb.DescribeJournalKinesisStreamInput{
+			LedgerName: aws.String(ledgerName),
+			StreamId:   aws.String(rs.Primary.ID),
+		})
 
-// 		if *resp.Name == rs.Primary.ID {
-// 			*v = *resp
-// 			return nil
-// 		}
+		if err != nil {
+			return err
+		}
 
-// 		return fmt.Errorf("QLDB Ledger (%s) not found", rs.Primary.ID)
-// 	}
-// }
+		if *resp.Stream.StreamId == rs.Primary.ID {
+			*v = *resp
+			return nil
+		}
+
+		return fmt.Errorf("QLDB Stream (%s) not found", rs.Primary.ID)
+	}
+}
 
 func testAccAWSQLDBStreamConfig(n int) string {
 	return fmt.Sprintf(`
@@ -204,50 +241,69 @@ resource "aws_qldb_stream" "test" {
 `, n)
 }
 
-// func TestAccAWSQLDBStream_Tags(t *testing.T) {
-// 	var cluster1, cluster2, cluster3 qldb.DescribeLedgerOutput
-// 	rName := acctest.RandomWithPrefix("tf-acc-test")
-// 	resourceName := "aws_qldb_ledger.test"
+func TestAccAWSQLDBStream_Tags(t *testing.T) {
+	var cluster1 qldb.DescribeLedgerOutput
+	var cluster2, cluster3, cluster4 qldb.DescribeJournalKinesisStreamOutput
+	rLedgerName := acctest.RandomWithPrefix("tf-acc-test")
+	rStreamName := acctest.RandomWithPrefix("tf-acc-test")
 
-// 	resource.ParallelTest(t, resource.TestCase{
-// 		PreCheck:     func() { testAccPreCheck(t); testAccPartitionHasServicePreCheck(qldb.EndpointsID, t) },
-// 		ErrorCheck:   testAccErrorCheck(t, qldb.EndpointsID),
-// 		Providers:    testAccProviders,
-// 		CheckDestroy: testAccCheckAWSQLDBLedgerDestroy,
-// 		Steps: []resource.TestStep{
-// 			{
-// 				Config: testAccAWSQLDBLedgerConfigTags1(rName, "key1", "value1"),
-// 				Check: resource.ComposeTestCheckFunc(
-// 					testAccCheckAWSQLDBLedgerExists(resourceName, &cluster1),
-// 					resource.TestCheckResourceAttr(resourceName, "tags.%", "1"),
-// 					resource.TestCheckResourceAttr(resourceName, "tags.key1", "value1"),
-// 				),
-// 			},
-// 			{
-// 				ResourceName:      resourceName,
-// 				ImportState:       true,
-// 				ImportStateVerify: true,
-// 			},
-// 			{
-// 				Config: testAccAWSQLDBLedgerConfigTags2(rName, "key1", "value1updated", "key2", "value2"),
-// 				Check: resource.ComposeTestCheckFunc(
-// 					testAccCheckAWSQLDBLedgerExists(resourceName, &cluster2),
-// 					resource.TestCheckResourceAttr(resourceName, "tags.%", "2"),
-// 					resource.TestCheckResourceAttr(resourceName, "tags.key1", "value1updated"),
-// 					resource.TestCheckResourceAttr(resourceName, "tags.key2", "value2"),
-// 				),
-// 			},
-// 			{
-// 				Config: testAccAWSQLDBLedgerConfigTags1(rName, "key2", "value2"),
-// 				Check: resource.ComposeTestCheckFunc(
-// 					testAccCheckAWSQLDBLedgerExists(resourceName, &cluster3),
-// 					resource.TestCheckResourceAttr(resourceName, "tags.%", "1"),
-// 					resource.TestCheckResourceAttr(resourceName, "tags.key2", "value2"),
-// 				),
-// 			},
-// 		},
-// 	})
-// }
+	resourceNameForQLDBLedger := "aws_qldb_ledger.test"
+	resourceNameForQLDBStream := "aws_qldb_stream.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t); testAccPartitionHasServicePreCheck(qldb.EndpointsID, t) },
+		ErrorCheck:   testAccErrorCheck(t, qldb.EndpointsID),
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAWSQLDBStreamCancel,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAWSQLDBLedgerForStreamsConfig(rLedgerName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAWSQLDBLedgerExists(resourceNameForQLDBLedger, &cluster1),
+				),
+			},
+			{
+				Config: testAccAWSQLDBStreamConfigTags1(rLedgerName, rStreamName, "key1", "value1"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAWSQLDBStreamExists(resourceNameForQLDBStream, &cluster2),
+					resource.TestCheckResourceAttr(resourceNameForQLDBStream, "tags.%", "1"),
+					resource.TestCheckResourceAttr(resourceNameForQLDBStream, "tags.key1", "value1"),
+				),
+			},
+			{
+				ResourceName:      resourceNameForQLDBStream,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccAWSQLDBStreamConfigTags2(rLedgerName, rStreamName, "key1", "value1updated", "key2", "value2"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAWSQLDBStreamExists(resourceNameForQLDBStream, &cluster3),
+					resource.TestCheckResourceAttr(resourceNameForQLDBStream, "tags.%", "2"),
+					resource.TestCheckResourceAttr(resourceNameForQLDBStream, "tags.key1", "value1updated"),
+					resource.TestCheckResourceAttr(resourceNameForQLDBStream, "tags.key2", "value2"),
+				),
+			},
+			{
+				Config: testAccAWSQLDBStreamConfigTags1(rLedgerName, rStreamName, "key2", "value2"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAWSQLDBStreamExists(resourceNameForQLDBStream, &cluster4),
+					resource.TestCheckResourceAttr(resourceNameForQLDBStream, "tags.%", "1"),
+					resource.TestCheckResourceAttr(resourceNameForQLDBStream, "tags.key2", "value2"),
+				),
+			},
+		},
+	})
+}
+
+func testAccAWSQLDBLedgerForStreamsConfig(rLedgerName string) string {
+	return fmt.Sprintf(`
+	resource "aws_qldb_ledger" "test" {
+		name                = "%s"
+		deletion_protection = false
+	}
+	`, rLedgerName)
+}
 
 func testAccAWSQLDBStreamConfigTags1(rLedgerName, rStreamName, tagKey1, tagValue1 string) string {
 	return fmt.Sprintf(`
