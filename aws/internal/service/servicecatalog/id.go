a84e54a5786610592295d68ce2f36a99148d237c
Dirk Avery <dirk.avery@gmail.com>
2021-05-17 14:32:12 -0400

    r/servicecat_product_portfolio_assoc: Add ID funcs

--- aws/internal/service/servicecatalog/id.go
+++ aws/internal/service/servicecatalog/id.go
@@ -18,3 +18,17 @@ func PortfolioShareParseResourceID(id string) (string, string, string, error) {
 func PortfolioShareCreateResourceID(portfolioID, shareType, principalID string) string {
 	return strings.Join([]string{portfolioID, shareType, principalID}, ":")
 }
+
+func ProductPortfolioAssociationParseID(id string) (string, string, string, error) {
+	parts := strings.SplitN(id, ":", 3)
+
+	if len(parts) != 3 || parts[0] == "" || parts[1] == "" || parts[2] == "" {
+		return "", "", "", fmt.Errorf("unexpected format of ID (%s), expected acceptLanguage:portfolioID:productID", id)
+	}
+
+	return parts[0], parts[1], parts[2], nil
+}
+
+func ProductPortfolioAssociationCreateID(acceptLanguage, portfolioID, productID string) string {
+	return strings.Join([]string{acceptLanguage, portfolioID, productID}, ":")
+}
