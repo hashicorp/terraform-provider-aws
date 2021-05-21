a7f8304c13305e699c00b27011212a705aa89d9a
Dirk Avery <dirk.avery@gmail.com>
2021-05-21 13:27:22 -0400

    tests/ds/default_tags: Test a bit more

--- aws/data_source_aws_default_tags_test.go
+++ aws/data_source_aws_default_tags_test.go
@@ -33,6 +33,56 @@ func TestAccAWSDefaultTagsDataSource_basic(t *testing.T) {
 	})
 }
 
+func TestAccAWSDefaultTagsDataSource_empty(t *testing.T) {
+	var providers []*schema.Provider
+
+	dataSourceName := "data.aws_default_tags.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ErrorCheck:        testAccErrorCheck(t, ec2.EndpointsID),
+		ProviderFactories: testAccProviderFactoriesInternal(&providers),
+		CheckDestroy:      nil,
+		Steps: []resource.TestStep{
+			{
+				Config: composeConfig(
+					testAccAWSProviderConfigDefaultTags_Tags0(),
+					testAccAWSDefaultTagsDataSource(),
+				),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(dataSourceName, "tags.%", "0"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccAWSDefaultTagsDataSource_multiple(t *testing.T) {
+	var providers []*schema.Provider
+
+	dataSourceName := "data.aws_default_tags.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ErrorCheck:        testAccErrorCheck(t, ec2.EndpointsID),
+		ProviderFactories: testAccProviderFactoriesInternal(&providers),
+		CheckDestroy:      nil,
+		Steps: []resource.TestStep{
+			{
+				Config: composeConfig(
+					testAccAWSProviderConfigDefaultTags_Tags2("nuera", "hijo", "escalofrios", "calambres"),
+					testAccAWSDefaultTagsDataSource(),
+				),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(dataSourceName, "tags.%", "2"),
+					resource.TestCheckResourceAttr(dataSourceName, "tags.nuera", "hijo"),
+					resource.TestCheckResourceAttr(dataSourceName, "tags.escalofrios", "calambres"),
+				),
+			},
+		},
+	})
+}
+
 func testAccAWSDefaultTagsDataSource() string {
 	return `data "aws_default_tags" "test" {}`
 }
