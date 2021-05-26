691222720d2bc7467b5d0b0035ddb7f4c41b03fb
Dirk Avery <dirk.avery@gmail.com>
2021-05-26 09:41:18 -0400

    servicecat: Work with RecordTags

--- aws/internal/keyvaluetags/servicecatalog_tags.go
+++ aws/internal/keyvaluetags/servicecatalog_tags.go
@@ -60,3 +60,38 @@ func ServiceCatalogProductUpdateTags(conn *servicecatalog.ServiceCatalog, identi
 
 	return nil
 }
+
+func ServicecatalogRecordKeyValueTags(tags []*servicecatalog.RecordTag) KeyValueTags {
+	m := make(map[string]*string, len(tags))
+
+	for _, tag := range tags {
+		m[aws.StringValue(tag.Key)] = tag.Value
+	}
+
+	return New(m)
+}
+
+func ServiceCatalogProvisionedProductUpdateTags(conn *servicecatalog.ServiceCatalog, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+	oldTags := New(oldTagsMap)
+	newTags := New(newTagsMap)
+
+	input := &servicecatalog.UpdateProductInput{
+		Id: aws.String(identifier),
+	}
+
+	if removedTags := oldTags.Removed(newTags); len(removedTags) > 0 {
+		input.RemoveTags = aws.StringSlice(removedTags.IgnoreAws().Keys())
+	}
+
+	if updatedTags := oldTags.Updated(newTags); len(updatedTags) > 0 {
+		input.AddTags = updatedTags.IgnoreAws().ServicecatalogTags()
+	}
+
+	_, err := conn.UpdateProduct(input)
+
+	if err != nil {
+		return fmt.Errorf("error updating tags for Service Catalog Product (%s): %w", identifier, err)
+	}
+
+	return nil
+}
