fd24595aed1eb6da3f3f69be0c86bc406cc569d9
drfaust92 <ilia.lazebnik@gmail.com>
2021-05-29 01:19:06 +0300

    fmt

--- aws/resource_aws_devicefarm_project_test.go
+++ aws/resource_aws_devicefarm_project_test.go
@@ -257,7 +257,7 @@ resource "aws_devicefarm_project" "test" {
 
   tags = {
     %[2]q = %[3]q
-  }  
+  }
 }
 `, rName, tagKey1, tagValue1)
 }
@@ -270,7 +270,7 @@ resource "aws_devicefarm_project" "test" {
   tags = {
     %[2]q = %[3]q
     %[4]q = %[5]q
-  }  
+  }
 }
 `, rName, tagKey1, tagValue1, tagKey2, tagValue2)
 }
