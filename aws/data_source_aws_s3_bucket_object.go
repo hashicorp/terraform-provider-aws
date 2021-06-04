3811552cf544eab16ac62ea9193f2cbfbf799faf
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:52:54 -0400

    ds/aws_s3_bucket_object: Restructure packages

--- aws/data_source_aws_s3_bucket_object.go
+++ aws/data_source_aws_s3_bucket_object.go
@@ -11,7 +11,8 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/keyvaluetags"
+	"github.com/terraform-providers/terraform-provider-aws/aws/keyvaluetags"
+	awsprovider "github.com/terraform-providers/terraform-provider-aws/provider"
 )
 
 func dataSourceAwsS3BucketObject() *schema.Resource {
@@ -124,8 +125,8 @@ func dataSourceAwsS3BucketObject() *schema.Resource {
 }
 
 func dataSourceAwsS3BucketObjectRead(d *schema.ResourceData, meta interface{}) error {
-	conn := meta.(*AWSClient).s3conn
-	ignoreTagsConfig := meta.(*AWSClient).IgnoreTagsConfig
+	conn := meta.(*awsprovider.AWSClient).S3Conn
+	ignoreTagsConfig := meta.(*awsprovider.AWSClient).IgnoreTagsConfig
 
 	bucket := d.Get("bucket").(string)
 	key := d.Get("key").(string)
