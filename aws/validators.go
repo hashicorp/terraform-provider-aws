058d96d137cc6a9ca64a5d9fa52363d565b20f6f
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:59:34 -0400

    validators: Restructure packages

--- aws/validators.go
+++ aws/validators.go
@@ -20,29 +20,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/structure"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	awsprovider "github.com/terraform-providers/terraform-provider-aws/provider"
 )
-
-const (
-	awsAccountIDRegexpInternalPattern = `(aws|\d{12})`
-	awsPartitionRegexpInternalPattern = `aws(-[a-z]+)*`
-	awsRegionRegexpInternalPattern    = `[a-z]{2}(-[a-z]+)+-\d`
-
-	versionStringRegexpInternalPattern = `[[:digit:]]+(\.[[:digit:]]+){2}`
-)
-
-const (
-	awsAccountIDRegexpPattern = "^" + awsAccountIDRegexpInternalPattern + "$"
-	awsPartitionRegexpPattern = "^" + awsPartitionRegexpInternalPattern + "$"
-	awsRegionRegexpPattern    = "^" + awsRegionRegexpInternalPattern + "$"
-
-	versionStringRegexpPattern = "^" + versionStringRegexpInternalPattern + "$"
-)
-
-var awsAccountIDRegexp = regexp.MustCompile(awsAccountIDRegexpPattern)
-var awsPartitionRegexp = regexp.MustCompile(awsPartitionRegexpPattern)
-var awsRegionRegexp = regexp.MustCompile(awsRegionRegexpPattern)
-
-var versionStringRegexp = regexp.MustCompile(versionStringRegexpPattern)
 
 // validateTypeStringNullableBoolean provides custom error messaging for TypeString booleans
 // Some arguments require three values: true, false, and "" (unspecified).
@@ -681,7 +660,7 @@ func validatePrincipal(v interface{}, k string) (ws []string, errors []error) {
 		return wsAccount, errorsAccount
 	}
 
-	wsARN, errorsARN := validateArn(v, k)
+	wsARN, errorsARN := ValidateArn(v, k)
 	ws = append(ws, wsARN...)
 	errors = append(errors, errorsARN...)
 
@@ -707,7 +686,7 @@ func validateServiceCatalogSharePrincipal(v interface{}, k string) (ws []string,
 		return wsAccount, errorsAccount
 	}
 
-	wsARN, errorsARN := validateArn(v, k)
+	wsARN, errorsARN := ValidateArn(v, k)
 	ws = append(ws, wsARN...)
 	errors = append(errors, errorsARN...)
 
@@ -723,7 +702,7 @@ func validateServiceCatalogSharePrincipal(v interface{}, k string) (ws []string,
 	return ws, errors
 }
 
-func validateArn(v interface{}, k string) (ws []string, errors []error) {
+func ValidateArn(v interface{}, k string) (ws []string, errors []error) {
 	value := v.(string)
 
 	if value == "" {
@@ -739,16 +718,16 @@ func validateArn(v interface{}, k string) (ws []string, errors []error) {
 
 	if parsedARN.Partition == "" {
 		errors = append(errors, fmt.Errorf("%q (%s) is an invalid ARN: missing partition value", k, value))
-	} else if !awsPartitionRegexp.MatchString(parsedARN.Partition) {
-		errors = append(errors, fmt.Errorf("%q (%s) is an invalid ARN: invalid partition value (expecting to match regular expression: %s)", k, value, awsPartitionRegexpPattern))
+	} else if !awsprovider.PartitionRegexp.MatchString(parsedARN.Partition) {
+		errors = append(errors, fmt.Errorf("%q (%s) is an invalid ARN: invalid partition value (expecting to match regular expression: %s)", k, value, awsprovider.PartitionRegexpPattern))
 	}
 
-	if parsedARN.Region != "" && !awsRegionRegexp.MatchString(parsedARN.Region) {
-		errors = append(errors, fmt.Errorf("%q (%s) is an invalid ARN: invalid region value (expecting to match regular expression: %s)", k, value, awsRegionRegexpPattern))
+	if parsedARN.Region != "" && !awsprovider.RegionRegexp.MatchString(parsedARN.Region) {
+		errors = append(errors, fmt.Errorf("%q (%s) is an invalid ARN: invalid region value (expecting to match regular expression: %s)", k, value, awsprovider.RegionRegexpPattern))
 	}
 
-	if parsedARN.AccountID != "" && !awsAccountIDRegexp.MatchString(parsedARN.AccountID) {
-		errors = append(errors, fmt.Errorf("%q (%s) is an invalid ARN: invalid account ID value (expecting to match regular expression: %s)", k, value, awsAccountIDRegexpPattern))
+	if parsedARN.AccountID != "" && !awsprovider.AccountIDRegexp.MatchString(parsedARN.AccountID) {
+		errors = append(errors, fmt.Errorf("%q (%s) is an invalid ARN: invalid account ID value (expecting to match regular expression: %s)", k, value, awsprovider.AccountIDRegexpPattern))
 	}
 
 	if parsedARN.Resource == "" {
@@ -2419,7 +2398,7 @@ var validateCloudWatchEventCustomEventBusEventSourceName = validation.All(
 )
 
 var validateCloudWatchEventBusNameOrARN = validation.Any(
-	validateArn,
+	ValidateArn,
 	validation.All(
 		validation.StringLenBetween(1, 256),
 		validation.StringMatch(regexp.MustCompile(`^[a-zA-Z0-9._\-/]+$`), ""),
@@ -2520,7 +2499,7 @@ var validateTypeStringIsDateOrPositiveInt = validation.Any(
 func validateVersionString(v interface{}, k string) (ws []string, errors []error) {
 	value := v.(string)
 
-	if !versionStringRegexp.MatchString(value) {
+	if !awsprovider.VersionStringRegexp.MatchString(value) {
 		errors = append(errors, fmt.Errorf("%s: must be a version string matching x.y.z", k))
 	}
 
