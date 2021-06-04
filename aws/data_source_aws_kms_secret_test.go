b1397b46fae3d2c4617da300b85271d62e3e9fb3
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:51:55 -0400

    tests/ds/aws_kms_secret: Restructure packages

--- aws/data_source_aws_kms_secret_test.go
+++ aws/data_source_aws_kms_secret_test.go
@@ -6,13 +6,14 @@ import (
 
 	"github.com/aws/aws-sdk-go/service/kms"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/terraform-providers/terraform-provider-aws/atest"
 )
 
 func TestAccAWSKmsSecretDataSource_removed(t *testing.T) {
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:   func() { testAccPreCheck(t) },
-		ErrorCheck: testAccErrorCheck(t, kms.EndpointsID),
-		Providers:  testAccProviders,
+		PreCheck:   func() { atest.PreCheck(t) },
+		ErrorCheck: atest.ErrorCheck(t, kms.EndpointsID),
+		Providers:  atest.Providers,
 		Steps: []resource.TestStep{
 			{
 				Config:      testAccAwsKmsSecretDataSourceConfig,
