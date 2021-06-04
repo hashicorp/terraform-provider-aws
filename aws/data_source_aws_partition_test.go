536c70fec154a93576581071915561275e307724
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:52:29 -0400

    tests/ds/aws_partition: Restructure packages

--- aws/data_source_aws_partition_test.go
+++ aws/data_source_aws_partition_test.go
@@ -6,20 +6,22 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/terraform-providers/terraform-provider-aws/atest"
+	awsprovider "github.com/terraform-providers/terraform-provider-aws/provider"
 )
 
 func TestAccAWSPartition_basic(t *testing.T) {
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:   func() { testAccPreCheck(t) },
-		ErrorCheck: testAccErrorCheck(t),
-		Providers:  testAccProviders,
+		PreCheck:   func() { atest.PreCheck(t) },
+		ErrorCheck: atest.ErrorCheck(t),
+		Providers:  atest.Providers,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccCheckAwsPartitionConfig_basic,
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckAwsPartition("data.aws_partition.current"),
 					testAccCheckAwsDnsSuffix("data.aws_partition.current"),
-					resource.TestCheckResourceAttr("data.aws_partition.current", "reverse_dns_prefix", testAccGetPartitionReverseDNSPrefix()),
+					resource.TestCheckResourceAttr("data.aws_partition.current", "reverse_dns_prefix", atest.PartitionReverseDNSPrefix()),
 				),
 			},
 		},
@@ -33,7 +35,7 @@ func testAccCheckAwsPartition(n string) resource.TestCheckFunc {
 			return fmt.Errorf("Can't find resource: %s", n)
 		}
 
-		expected := testAccProvider.Meta().(*AWSClient).partition
+		expected := atest.Provider.Meta().(*awsprovider.AWSClient).Partition
 		if rs.Primary.Attributes["partition"] != expected {
 			return fmt.Errorf("Incorrect Partition: expected %q, got %q", expected, rs.Primary.Attributes["partition"])
 		}
@@ -49,7 +51,7 @@ func testAccCheckAwsDnsSuffix(n string) resource.TestCheckFunc {
 			return fmt.Errorf("Can't find resource: %s", n)
 		}
 
-		expected := testAccProvider.Meta().(*AWSClient).dnsSuffix
+		expected := atest.Provider.Meta().(*awsprovider.AWSClient).DNSSuffix
 		if rs.Primary.Attributes["dns_suffix"] != expected {
 			return fmt.Errorf("Incorrect DNS Suffix: expected %q, got %q", expected, rs.Primary.Attributes["dns_suffix"])
 		}
