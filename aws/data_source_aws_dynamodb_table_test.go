fcaf72e1932519dadc182f5d0805ff19d2a1198c
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:51:16 -0400

    tests/ds/aws_dynamodb_table: Restructure packages

--- aws/data_source_aws_dynamodb_table_test.go
+++ aws/data_source_aws_dynamodb_table_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/terraform-providers/terraform-provider-aws/atest"
 )
 
 func TestAccDataSourceAwsDynamoDbTable_basic(t *testing.T) {
@@ -14,9 +15,9 @@ func TestAccDataSourceAwsDynamoDbTable_basic(t *testing.T) {
 	tableName := fmt.Sprintf("testaccawsdynamodbtable-basic-%s", acctest.RandString(10))
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:   func() { testAccPreCheck(t) },
-		ErrorCheck: testAccErrorCheck(t, dynamodb.EndpointsID),
-		Providers:  testAccProviders,
+		PreCheck:   func() { atest.PreCheck(t) },
+		ErrorCheck: atest.ErrorCheck(t, dynamodb.EndpointsID),
+		Providers:  atest.Providers,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccDataSourceAwsDynamoDbTableConfigBasic(tableName),
