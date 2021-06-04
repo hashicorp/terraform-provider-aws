726aa66c0326a16f9263a091b0e6016557d87395
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:52:58 -0400

    tests/ds/aws_ssm_parameter: Restructure packages

--- aws/data_source_aws_ssm_parameter_test.go
+++ aws/data_source_aws_ssm_parameter_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/ssm"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/terraform-providers/terraform-provider-aws/atest"
 )
 
 func TestAccAWSSsmParameterDataSource_basic(t *testing.T) {
@@ -14,9 +15,9 @@ func TestAccAWSSsmParameterDataSource_basic(t *testing.T) {
 	name := acctest.RandomWithPrefix("tf-acc-test")
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:   func() { testAccPreCheck(t) },
-		ErrorCheck: testAccErrorCheck(t, ssm.EndpointsID),
-		Providers:  testAccProviders,
+		PreCheck:   func() { atest.PreCheck(t) },
+		ErrorCheck: atest.ErrorCheck(t, ssm.EndpointsID),
+		Providers:  atest.Providers,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccCheckAwsSsmParameterDataSourceConfig(name, "false"),
@@ -48,9 +49,9 @@ func TestAccAWSSsmParameterDataSource_fullPath(t *testing.T) {
 	name := acctest.RandomWithPrefix("/tf-acc-test/tf-acc-test")
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:   func() { testAccPreCheck(t) },
-		ErrorCheck: testAccErrorCheck(t, ssm.EndpointsID),
-		Providers:  testAccProviders,
+		PreCheck:   func() { atest.PreCheck(t) },
+		ErrorCheck: atest.ErrorCheck(t, ssm.EndpointsID),
+		Providers:  atest.Providers,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccCheckAwsSsmParameterDataSourceConfig(name, "false"),
