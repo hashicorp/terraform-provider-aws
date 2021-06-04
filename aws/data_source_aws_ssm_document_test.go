1321074316c4e9161674fedb8e91d6680711d0c8
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:52:58 -0400

    tests/ds/aws_ssm_document: Restructure packages

--- aws/data_source_aws_ssm_document_test.go
+++ aws/data_source_aws_ssm_document_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/ssm"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/terraform-providers/terraform-provider-aws/atest"
 )
 
 func TestAccAWSSsmDocumentDataSource_basic(t *testing.T) {
@@ -14,9 +15,9 @@ func TestAccAWSSsmDocumentDataSource_basic(t *testing.T) {
 	name := fmt.Sprintf("test_document-%d", acctest.RandInt())
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:   func() { testAccPreCheck(t) },
-		ErrorCheck: testAccErrorCheck(t, ssm.EndpointsID),
-		Providers:  testAccProviders,
+		PreCheck:   func() { atest.PreCheck(t) },
+		ErrorCheck: atest.ErrorCheck(t, ssm.EndpointsID),
+		Providers:  atest.Providers,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccCheckAwsSsmDocumentDataSourceConfig(name, "JSON"),
