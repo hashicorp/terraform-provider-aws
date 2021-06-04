4aa11618d0c9abc2301070b056b441b2831cf654
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:51:22 -0400

    ds/aws_ecs_task_definition: Restructure packages

--- aws/data_source_aws_ecs_task_definition.go
+++ aws/data_source_aws_ecs_task_definition.go
@@ -7,6 +7,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ecs"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	awsprovider "github.com/terraform-providers/terraform-provider-aws/provider"
 )
 
 func dataSourceAwsEcsTaskDefinition() *schema.Resource {
@@ -44,7 +45,7 @@ func dataSourceAwsEcsTaskDefinition() *schema.Resource {
 }
 
 func dataSourceAwsEcsTaskDefinitionRead(d *schema.ResourceData, meta interface{}) error {
-	conn := meta.(*AWSClient).ecsconn
+	conn := meta.(*awsprovider.AWSClient).ECSConn
 
 	params := &ecs.DescribeTaskDefinitionInput{
 		TaskDefinition: aws.String(d.Get("task_definition").(string)),
