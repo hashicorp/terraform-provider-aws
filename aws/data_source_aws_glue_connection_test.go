9f339bc0e7be95b19cfe97b81ff289d922607d62
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:51:49 -0400

    tests/ds/aws_glue_connection: Restructure packages

--- aws/data_source_aws_glue_connection_test.go
+++ aws/data_source_aws_glue_connection_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/terraform-providers/terraform-provider-aws/atest"
 )
 
 func TestAccDataSourceAwsGlueConnection_basic(t *testing.T) {
@@ -16,9 +17,9 @@ func TestAccDataSourceAwsGlueConnection_basic(t *testing.T) {
 	rName := fmt.Sprintf("tf-testacc-glue-connection-%s", acctest.RandString(13))
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:   func() { testAccPreCheck(t) },
-		ErrorCheck: testAccErrorCheck(t, glue.EndpointsID),
-		Providers:  testAccProviders,
+		PreCheck:   func() { atest.PreCheck(t) },
+		ErrorCheck: atest.ErrorCheck(t, glue.EndpointsID),
+		Providers:  atest.Providers,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccDataSourceAwsGlueConnectionConfig(rName),
