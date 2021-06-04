18e875c061c82da1a294e8c44a74897140f880f1
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:54:59 -0400

    r/aws_datasync_agent: Restructure packages

--- aws/resource_aws_datasync_agent.go
+++ aws/resource_aws_datasync_agent.go
@@ -12,10 +12,11 @@ import (
 	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/keyvaluetags"
 	"github.com/terraform-providers/terraform-provider-aws/aws/internal/service/datasync/finder"
 	"github.com/terraform-providers/terraform-provider-aws/aws/internal/service/datasync/waiter"
 	"github.com/terraform-providers/terraform-provider-aws/aws/internal/tfresource"
+	"github.com/terraform-providers/terraform-provider-aws/aws/keyvaluetags"
+	awsprovider "github.com/terraform-providers/terraform-provider-aws/provider"
 )
 
 func resourceAwsDataSyncAgent() *schema.Resource {
@@ -87,8 +88,8 @@ func resourceAwsDataSyncAgent() *schema.Resource {
 }
 
 func resourceAwsDataSyncAgentCreate(d *schema.ResourceData, meta interface{}) error {
-	conn := meta.(*AWSClient).datasyncconn
-	defaultTagsConfig := meta.(*AWSClient).DefaultTagsConfig
+	conn := meta.(*awsprovider.AWSClient).DataSyncConn
+	defaultTagsConfig := meta.(*awsprovider.AWSClient).DefaultTagsConfig
 	tags := defaultTagsConfig.MergeTags(keyvaluetags.New(d.Get("tags").(map[string]interface{})))
 
 	activationKey := d.Get("activation_key").(string)
@@ -102,7 +103,7 @@ func resourceAwsDataSyncAgentCreate(d *schema.ResourceData, meta interface{}) er
 			},
 			Timeout: time.Second * 10,
 		}
-		region := meta.(*AWSClient).region
+		region := meta.(*awsprovider.AWSClient).Region
 
 		var requestURL string
 		if v, ok := d.GetOk("private_link_endpoint"); ok {
@@ -205,9 +206,9 @@ func resourceAwsDataSyncAgentCreate(d *schema.ResourceData, meta interface{}) er
 }
 
 func resourceAwsDataSyncAgentRead(d *schema.ResourceData, meta interface{}) error {
-	conn := meta.(*AWSClient).datasyncconn
-	defaultTagsConfig := meta.(*AWSClient).DefaultTagsConfig
-	ignoreTagsConfig := meta.(*AWSClient).IgnoreTagsConfig
+	conn := meta.(*awsprovider.AWSClient).DataSyncConn
+	defaultTagsConfig := meta.(*awsprovider.AWSClient).DefaultTagsConfig
+	ignoreTagsConfig := meta.(*awsprovider.AWSClient).IgnoreTagsConfig
 
 	output, err := finder.AgentByARN(conn, d.Id())
 
@@ -256,7 +257,7 @@ func resourceAwsDataSyncAgentRead(d *schema.ResourceData, meta interface{}) erro
 }
 
 func resourceAwsDataSyncAgentUpdate(d *schema.ResourceData, meta interface{}) error {
-	conn := meta.(*AWSClient).datasyncconn
+	conn := meta.(*awsprovider.AWSClient).DataSyncConn
 
 	if d.HasChange("name") {
 		input := &datasync.UpdateAgentInput{
@@ -284,7 +285,7 @@ func resourceAwsDataSyncAgentUpdate(d *schema.ResourceData, meta interface{}) er
 }
 
 func resourceAwsDataSyncAgentDelete(d *schema.ResourceData, meta interface{}) error {
-	conn := meta.(*AWSClient).datasyncconn
+	conn := meta.(*awsprovider.AWSClient).DataSyncConn
 
 	log.Printf("[DEBUG] Deleting DataSync Agent: %s", d.Id())
 	_, err := conn.DeleteAgent(&datasync.DeleteAgentInput{
