a8753520d1b9f1ff1c38a0a72ec74eba00f8530b
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:50:47 -0400

    tests/ds/aws_cloudtrail_service_account: Restructure packages

--- aws/data_source_aws_cloudtrail_service_account_test.go
+++ aws/data_source_aws_cloudtrail_service_account_test.go
@@ -4,23 +4,24 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/terraform-providers/terraform-provider-aws/atest"
 )
 
 func TestAccAWSCloudTrailServiceAccount_basic(t *testing.T) {
-	expectedAccountID := cloudTrailServiceAccountPerRegionMap[testAccGetRegion()]
+	expectedAccountID := cloudTrailServiceAccountPerRegionMap[atest.Region()]
 
 	dataSourceName := "data.aws_cloudtrail_service_account.main"
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:   func() { testAccPreCheck(t) },
-		ErrorCheck: testAccErrorCheck(t),
-		Providers:  testAccProviders,
+		PreCheck:   func() { atest.PreCheck(t) },
+		ErrorCheck: atest.ErrorCheck(t),
+		Providers:  atest.Providers,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccCheckAwsCloudTrailServiceAccountConfig,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(dataSourceName, "id", expectedAccountID),
-					testAccCheckResourceAttrGlobalARNAccountID(dataSourceName, "arn", expectedAccountID, "iam", "root"),
+					atest.CheckAttrGlobalARNAccountID(dataSourceName, "arn", expectedAccountID, "iam", "root"),
 				),
 			},
 		},
@@ -28,20 +29,20 @@ func TestAccAWSCloudTrailServiceAccount_basic(t *testing.T) {
 }
 
 func TestAccAWSCloudTrailServiceAccount_Region(t *testing.T) {
-	expectedAccountID := cloudTrailServiceAccountPerRegionMap[testAccGetRegion()]
+	expectedAccountID := cloudTrailServiceAccountPerRegionMap[atest.Region()]
 
 	dataSourceName := "data.aws_cloudtrail_service_account.regional"
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:   func() { testAccPreCheck(t) },
-		ErrorCheck: testAccErrorCheck(t),
-		Providers:  testAccProviders,
+		PreCheck:   func() { atest.PreCheck(t) },
+		ErrorCheck: atest.ErrorCheck(t),
+		Providers:  atest.Providers,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccCheckAwsCloudTrailServiceAccountConfigRegion,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(dataSourceName, "id", expectedAccountID),
-					testAccCheckResourceAttrGlobalARNAccountID(dataSourceName, "arn", expectedAccountID, "iam", "root"),
+					atest.CheckAttrGlobalARNAccountID(dataSourceName, "arn", expectedAccountID, "iam", "root"),
 				),
 			},
 		},
