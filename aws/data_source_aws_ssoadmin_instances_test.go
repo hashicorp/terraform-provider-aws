2addaa863d4eff2d7b6751006d2e6f3fc16ca52d
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:52:58 -0400

    tests/ds/aws_ssoadmin_instances: Restructure packages

--- aws/data_source_aws_ssoadmin_instances_test.go
+++ aws/data_source_aws_ssoadmin_instances_test.go
@@ -6,10 +6,12 @@ import (
 
 	"github.com/aws/aws-sdk-go/service/ssoadmin"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/terraform-providers/terraform-provider-aws/atest"
+	awsprovider "github.com/terraform-providers/terraform-provider-aws/provider"
 )
 
 func testAccPreCheckAWSSSOAdminInstances(t *testing.T) {
-	conn := testAccProvider.Meta().(*AWSClient).ssoadminconn
+	conn := atest.Provider.Meta().(*awsprovider.AWSClient).SSOAdminConn
 
 	var instances []*ssoadmin.InstanceMetadata
 	err := conn.ListInstancesPages(&ssoadmin.ListInstancesInput{}, func(page *ssoadmin.ListInstancesOutput, lastPage bool) bool {
@@ -22,7 +24,7 @@ func testAccPreCheckAWSSSOAdminInstances(t *testing.T) {
 		return !lastPage
 	})
 
-	if testAccPreCheckSkipError(err) {
+	if atest.PreCheckSkipError(err) {
 		t.Skipf("skipping acceptance testing: %s", err)
 	}
 
@@ -39,16 +41,16 @@ func TestAccDataSourceAWSSSOAdminInstances_basic(t *testing.T) {
 	dataSourceName := "data.aws_ssoadmin_instances.test"
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:   func() { testAccPreCheck(t); testAccPreCheckAWSSSOAdminInstances(t) },
-		ErrorCheck: testAccErrorCheck(t, ssoadmin.EndpointsID),
-		Providers:  testAccProviders,
+		PreCheck:   func() { atest.PreCheck(t); testAccPreCheckAWSSSOAdminInstances(t) },
+		ErrorCheck: atest.ErrorCheck(t, ssoadmin.EndpointsID),
+		Providers:  atest.Providers,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccDataSourceAWSSSOAdminInstancesConfigBasic,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(dataSourceName, "arns.#", "1"),
 					resource.TestCheckResourceAttr(dataSourceName, "identity_store_ids.#", "1"),
-					testAccMatchResourceAttrGlobalARNNoAccount(dataSourceName, "arns.0", "sso", regexp.MustCompile("instance/(sso)?ins-[a-zA-Z0-9-.]{16}")),
+					atest.MatchAttrGlobalARNNoAccount(dataSourceName, "arns.0", "sso", regexp.MustCompile("instance/(sso)?ins-[a-zA-Z0-9-.]{16}")),
 					resource.TestMatchResourceAttr(dataSourceName, "identity_store_ids.0", regexp.MustCompile("^[a-zA-Z0-9-]*")),
 				),
 			},
