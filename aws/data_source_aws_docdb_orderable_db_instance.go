bdc5ac02220430f85c5956a454113406f568eb35
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:51:15 -0400

    ds/aws_docdb_orderable_db_instance: Restructure packages

--- aws/data_source_aws_docdb_orderable_db_instance.go
+++ aws/data_source_aws_docdb_orderable_db_instance.go
@@ -7,6 +7,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/docdb"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	awsprovider "github.com/terraform-providers/terraform-provider-aws/provider"
 )
 
 func dataSourceAwsDocdbOrderableDbInstance() *schema.Resource {
@@ -61,7 +62,7 @@ func dataSourceAwsDocdbOrderableDbInstance() *schema.Resource {
 }
 
 func dataSourceAwsDocdbOrderableDbInstanceRead(d *schema.ResourceData, meta interface{}) error {
-	conn := meta.(*AWSClient).docdbconn
+	conn := meta.(*awsprovider.AWSClient).DocDBConn
 
 	input := &docdb.DescribeOrderableDBInstanceOptionsInput{}
 
