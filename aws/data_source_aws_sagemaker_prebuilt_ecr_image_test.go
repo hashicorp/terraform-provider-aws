c8f363283a48c16580aac4bfb64203c1af1f5f21
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:52:54 -0400

    tests/ds/aws_sagemaker_prebuilt_ecr_image: Restructure packages

--- aws/data_source_aws_sagemaker_prebuilt_ecr_image_test.go
+++ aws/data_source_aws_sagemaker_prebuilt_ecr_image_test.go
@@ -5,24 +5,25 @@ import (
 
 	"github.com/aws/aws-sdk-go/service/sagemaker"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/terraform-providers/terraform-provider-aws/atest"
 )
 
 func TestAccAWSSageMakerPrebuiltECRImage_basic(t *testing.T) {
-	expectedID := sageMakerPrebuiltECRImageIDByRegion_FactorMachines[testAccGetRegion()]
+	expectedID := sageMakerPrebuiltECRImageIDByRegion_FactorMachines[atest.Region()]
 
 	dataSourceName := "data.aws_sagemaker_prebuilt_ecr_image.test"
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:   func() { testAccPreCheck(t) },
-		ErrorCheck: testAccErrorCheck(t, sagemaker.EndpointsID),
-		Providers:  testAccProviders,
+		PreCheck:   func() { atest.PreCheck(t) },
+		ErrorCheck: atest.ErrorCheck(t, sagemaker.EndpointsID),
+		Providers:  atest.Providers,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccCheckAwsSageMakerPrebuiltECRImageConfig,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(dataSourceName, "id", expectedID),
 					resource.TestCheckResourceAttr(dataSourceName, "registry_id", expectedID),
-					resource.TestCheckResourceAttr(dataSourceName, "registry_path", dataSourceAwsSageMakerPrebuiltECRImageCreatePath(expectedID, testAccGetRegion(), testAccGetPartitionDNSSuffix(), "kmeans", "1")),
+					resource.TestCheckResourceAttr(dataSourceName, "registry_path", dataSourceAwsSageMakerPrebuiltECRImageCreatePath(expectedID, atest.Region(), atest.PartitionDNSSuffix(), "kmeans", "1")),
 				),
 			},
 		},
@@ -30,21 +31,21 @@ func TestAccAWSSageMakerPrebuiltECRImage_basic(t *testing.T) {
 }
 
 func TestAccAWSSageMakerPrebuiltECRImage_region(t *testing.T) {
-	expectedID := sageMakerPrebuiltECRImageIDByRegion_SparkML[testAccGetRegion()]
+	expectedID := sageMakerPrebuiltECRImageIDByRegion_SparkML[atest.Region()]
 
 	dataSourceName := "data.aws_sagemaker_prebuilt_ecr_image.test"
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:   func() { testAccPreCheck(t) },
-		ErrorCheck: testAccErrorCheck(t, sagemaker.EndpointsID),
-		Providers:  testAccProviders,
+		PreCheck:   func() { atest.PreCheck(t) },
+		ErrorCheck: atest.ErrorCheck(t, sagemaker.EndpointsID),
+		Providers:  atest.Providers,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccCheckAwsSageMakerPrebuiltECRImageExplicitRegionConfig,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(dataSourceName, "id", expectedID),
 					resource.TestCheckResourceAttr(dataSourceName, "registry_id", expectedID),
-					resource.TestCheckResourceAttr(dataSourceName, "registry_path", dataSourceAwsSageMakerPrebuiltECRImageCreatePath(expectedID, testAccGetRegion(), testAccGetPartitionDNSSuffix(), "sagemaker-scikit-learn", "2.2-1.0.11.0")),
+					resource.TestCheckResourceAttr(dataSourceName, "registry_path", dataSourceAwsSageMakerPrebuiltECRImageCreatePath(expectedID, atest.Region(), atest.PartitionDNSSuffix(), "sagemaker-scikit-learn", "2.2-1.0.11.0")),
 				),
 			},
 		},
