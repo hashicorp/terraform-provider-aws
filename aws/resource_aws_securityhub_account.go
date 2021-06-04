3f8cb820bbc4b3974beb46a648a9a09eabc00252
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:58:18 -0400

    r/aws_securityhub_account: Restructure packages

--- aws/resource_aws_securityhub_account.go
+++ aws/resource_aws_securityhub_account.go
@@ -10,6 +10,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/terraform-providers/terraform-provider-aws/aws/internal/service/securityhub/waiter"
 	"github.com/terraform-providers/terraform-provider-aws/aws/internal/tfresource"
+	awsprovider "github.com/terraform-providers/terraform-provider-aws/provider"
 )
 
 func resourceAwsSecurityHubAccount() *schema.Resource {
@@ -26,7 +27,7 @@ func resourceAwsSecurityHubAccount() *schema.Resource {
 }
 
 func resourceAwsSecurityHubAccountCreate(d *schema.ResourceData, meta interface{}) error {
-	conn := meta.(*AWSClient).securityhubconn
+	conn := meta.(*awsprovider.AWSClient).SecurityHubConn
 	log.Print("[DEBUG] Enabling Security Hub for account")
 
 	_, err := conn.EnableSecurityHub(&securityhub.EnableSecurityHubInput{})
@@ -35,20 +36,20 @@ func resourceAwsSecurityHubAccountCreate(d *schema.ResourceData, meta interface{
 		return fmt.Errorf("Error enabling Security Hub for account: %s", err)
 	}
 
-	d.SetId(meta.(*AWSClient).accountid)
+	d.SetId(meta.(*awsprovider.AWSClient).AccountID)
 
 	return resourceAwsSecurityHubAccountRead(d, meta)
 }
 
 func resourceAwsSecurityHubAccountRead(d *schema.ResourceData, meta interface{}) error {
-	conn := meta.(*AWSClient).securityhubconn
+	conn := meta.(*awsprovider.AWSClient).SecurityHubConn
 
 	log.Printf("[DEBUG] Checking if Security Hub is enabled")
 	_, err := conn.GetEnabledStandards(&securityhub.GetEnabledStandardsInput{})
 
 	if err != nil {
 		// Can only read enabled standards if Security Hub is enabled
-		if isAWSErr(err, "InvalidAccessException", "not subscribed to AWS Security Hub") {
+		if tfawserr.ErrMessageContains(err, "InvalidAccessException", "not subscribed to AWS Security Hub") {
 			d.SetId("")
 			return nil
 		}
@@ -59,7 +60,7 @@ func resourceAwsSecurityHubAccountRead(d *schema.ResourceData, meta interface{})
 }
 
 func resourceAwsSecurityHubAccountDelete(d *schema.ResourceData, meta interface{}) error {
-	conn := meta.(*AWSClient).securityhubconn
+	conn := meta.(*awsprovider.AWSClient).SecurityHubConn
 	log.Print("[DEBUG] Disabling Security Hub for account")
 
 	err := resource.Retry(waiter.AdminAccountNotFoundTimeout, func() *resource.RetryError {
