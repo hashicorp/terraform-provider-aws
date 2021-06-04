4cdf68787324fccd7e9f6f469c1d68614452c45e
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:50:41 -0400

    tests/ds/aws_api_gateway_api_key: Restructure packages

--- aws/data_source_aws_api_gateway_api_key_test.go
+++ aws/data_source_aws_api_gateway_api_key_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/apigateway"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/terraform-providers/terraform-provider-aws/atest"
 )
 
 func TestAccDataSourceAwsApiGatewayApiKey_basic(t *testing.T) {
@@ -15,9 +16,9 @@ func TestAccDataSourceAwsApiGatewayApiKey_basic(t *testing.T) {
 	dataSourceName1 := "data.aws_api_gateway_api_key.test_key"
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:   func() { testAccPreCheck(t) },
-		ErrorCheck: testAccErrorCheck(t, apigateway.EndpointsID),
-		Providers:  testAccProviders,
+		PreCheck:   func() { atest.PreCheck(t) },
+		ErrorCheck: atest.ErrorCheck(t, apigateway.EndpointsID),
+		Providers:  atest.Providers,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccDataSourceAwsApiGatewayApiKeyConfig(rName),
