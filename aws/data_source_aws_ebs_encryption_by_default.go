f7d4f35f1a551c8562a22a5343b16f9cc09c39c9
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:51:16 -0400

    ds/aws_ebs_encryption_by_default: Restructure packages

--- aws/data_source_aws_ebs_encryption_by_default.go
+++ aws/data_source_aws_ebs_encryption_by_default.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	awsprovider "github.com/terraform-providers/terraform-provider-aws/provider"
 )
 
 func dataSourceAwsEbsEncryptionByDefault() *schema.Resource {
@@ -20,14 +21,14 @@ func dataSourceAwsEbsEncryptionByDefault() *schema.Resource {
 	}
 }
 func dataSourceAwsEbsEncryptionByDefaultRead(d *schema.ResourceData, meta interface{}) error {
-	conn := meta.(*AWSClient).ec2conn
+	conn := meta.(*awsprovider.AWSClient).EC2Conn
 
 	res, err := conn.GetEbsEncryptionByDefault(&ec2.GetEbsEncryptionByDefaultInput{})
 	if err != nil {
 		return fmt.Errorf("Error reading default EBS encryption toggle: %w", err)
 	}
 
-	d.SetId(meta.(*AWSClient).region)
+	d.SetId(meta.(*awsprovider.AWSClient).Region)
 	d.Set("enabled", res.EbsEncryptionByDefault)
 
 	return nil
