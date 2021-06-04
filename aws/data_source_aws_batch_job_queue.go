d758ef4abfbaca69dcf7cd903a5d0303800ba6ac
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:50:44 -0400

    ds/aws_batch_job_queue: Restructure packages

--- aws/data_source_aws_batch_job_queue.go
+++ aws/data_source_aws_batch_job_queue.go
@@ -7,7 +7,8 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/batch"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/keyvaluetags"
+	"github.com/terraform-providers/terraform-provider-aws/aws/keyvaluetags"
+	awsprovider "github.com/terraform-providers/terraform-provider-aws/provider"
 )
 
 func dataSourceAwsBatchJobQueue() *schema.Resource {
@@ -68,8 +69,8 @@ func dataSourceAwsBatchJobQueue() *schema.Resource {
 }
 
 func dataSourceAwsBatchJobQueueRead(d *schema.ResourceData, meta interface{}) error {
-	conn := meta.(*AWSClient).batchconn
-	ignoreTagsConfig := meta.(*AWSClient).IgnoreTagsConfig
+	conn := meta.(*awsprovider.AWSClient).BatchConn
+	ignoreTagsConfig := meta.(*awsprovider.AWSClient).IgnoreTagsConfig
 
 	params := &batch.DescribeJobQueuesInput{
 		JobQueues: []*string{aws.String(d.Get("name").(string))},
