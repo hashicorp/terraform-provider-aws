f36fc200153688925376813078e0200dc2257ea0
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:52:58 -0400

    ds/aws_ssm_patch_baseline: Restructure packages

--- aws/data_source_aws_ssm_patch_baseline.go
+++ aws/data_source_aws_ssm_patch_baseline.go
@@ -8,6 +8,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/ssm"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	awsprovider "github.com/terraform-providers/terraform-provider-aws/provider"
 )
 
 func dataSourceAwsSsmPatchBaseline() *schema.Resource {
@@ -47,7 +48,7 @@ func dataSourceAwsSsmPatchBaseline() *schema.Resource {
 }
 
 func dataAwsSsmPatchBaselineRead(d *schema.ResourceData, meta interface{}) error {
-	ssmconn := meta.(*AWSClient).ssmconn
+	SSMConn := meta.(*awsprovider.AWSClient).SSMConn
 
 	filters := []*ssm.PatchOrchestratorFilter{
 		{
@@ -73,7 +74,7 @@ func dataAwsSsmPatchBaselineRead(d *schema.ResourceData, meta interface{}) error
 
 	log.Printf("[DEBUG] Reading DescribePatchBaselines: %s", params)
 
-	resp, err := ssmconn.DescribePatchBaselines(params)
+	resp, err := SSMConn.DescribePatchBaselines(params)
 
 	if err != nil {
 		return fmt.Errorf("Error describing SSM PatchBaselines: %w", err)
