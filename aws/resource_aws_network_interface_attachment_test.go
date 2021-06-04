b9c087c2d9ff7f2b75f815d9d566d77af0952e16
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:57:16 -0400

    tests/r/aws_network_interface_attachment: Restructure packages

--- aws/resource_aws_network_interface_attachment_test.go
+++ aws/resource_aws_network_interface_attachment_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/terraform-providers/terraform-provider-aws/atest"
 )
 
 func TestAccAWSNetworkInterfaceAttachment_basic(t *testing.T) {
@@ -14,9 +15,9 @@ func TestAccAWSNetworkInterfaceAttachment_basic(t *testing.T) {
 	rInt := acctest.RandInt()
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:     func() { testAccPreCheck(t) },
-		ErrorCheck:   testAccErrorCheck(t, ec2.EndpointsID),
-		Providers:    testAccProviders,
+		PreCheck:     func() { atest.PreCheck(t) },
+		ErrorCheck:   atest.ErrorCheck(t, ec2.EndpointsID),
+		Providers:    atest.Providers,
 		CheckDestroy: testAccCheckAWSENIDestroy,
 		Steps: []resource.TestStep{
 			{
