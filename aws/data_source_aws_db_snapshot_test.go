2046f5bfcde26f2227bccc3ce651a22805ac2cc6
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:51:14 -0400

    tests/ds/aws_db_snapshot: Restructure packages

--- aws/data_source_aws_db_snapshot_test.go
+++ aws/data_source_aws_db_snapshot_test.go
@@ -8,14 +8,15 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/terraform-providers/terraform-provider-aws/atest"
 )
 
 func TestAccAWSDbSnapshotDataSource_basic(t *testing.T) {
 	rInt := acctest.RandInt()
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:   func() { testAccPreCheck(t) },
-		ErrorCheck: testAccErrorCheck(t, rds.EndpointsID),
-		Providers:  testAccProviders,
+		PreCheck:   func() { atest.PreCheck(t) },
+		ErrorCheck: atest.ErrorCheck(t, rds.EndpointsID),
+		Providers:  atest.Providers,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccCheckAwsDbSnapshotDataSourceConfig(rInt),
