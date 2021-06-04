9a3336a65906e5b7441fb63553a1d15af2223888
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:52:57 -0400

    tests/ds/aws_sfn_state_machine: Restructure packages

--- aws/data_source_aws_sfn_state_machine_test.go
+++ aws/data_source_aws_sfn_state_machine_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/sfn"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/terraform-providers/terraform-provider-aws/atest"
 )
 
 func TestAccDataSourceAwsSfnStateMachine_basic(t *testing.T) {
@@ -15,9 +16,9 @@ func TestAccDataSourceAwsSfnStateMachine_basic(t *testing.T) {
 	resourceName := "aws_sfn_state_machine.test"
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:   func() { testAccPreCheck(t) },
-		ErrorCheck: testAccErrorCheck(t, sfn.EndpointsID),
-		Providers:  testAccProviders,
+		PreCheck:   func() { atest.PreCheck(t) },
+		ErrorCheck: atest.ErrorCheck(t, sfn.EndpointsID),
+		Providers:  atest.Providers,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccDataSourceAwsSfnStateMachineConfig(rName),
