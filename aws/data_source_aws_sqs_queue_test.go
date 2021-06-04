9a2097c81cd36a4c45255dbc58ff93953abd860c
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:52:58 -0400

    tests/ds/aws_sqs_queue: Restructure packages

--- aws/data_source_aws_sqs_queue_test.go
+++ aws/data_source_aws_sqs_queue_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/terraform-providers/terraform-provider-aws/atest"
 )
 
 func TestAccDataSourceAwsSqsQueue_basic(t *testing.T) {
@@ -16,9 +17,9 @@ func TestAccDataSourceAwsSqsQueue_basic(t *testing.T) {
 	datasourceName := "data.aws_sqs_queue.by_name"
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:   func() { testAccPreCheck(t) },
-		ErrorCheck: testAccErrorCheck(t, sqs.EndpointsID),
-		Providers:  testAccProviders,
+		PreCheck:   func() { atest.PreCheck(t) },
+		ErrorCheck: atest.ErrorCheck(t, sqs.EndpointsID),
+		Providers:  atest.Providers,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccDataSourceAwsSqsQueueConfig(rName),
@@ -37,9 +38,9 @@ func TestAccDataSourceAwsSqsQueue_tags(t *testing.T) {
 	datasourceName := "data.aws_sqs_queue.by_name"
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:   func() { testAccPreCheck(t) },
-		ErrorCheck: testAccErrorCheck(t, sqs.EndpointsID),
-		Providers:  testAccProviders,
+		PreCheck:   func() { atest.PreCheck(t) },
+		ErrorCheck: atest.ErrorCheck(t, sqs.EndpointsID),
+		Providers:  atest.Providers,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccDataSourceAwsSqsQueueConfigTags(rName),
