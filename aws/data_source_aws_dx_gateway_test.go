8f996b0d5f9daa7d02c81149b4453418106f884e
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:51:15 -0400

    tests/ds/aws_dx_gateway: Restructure packages

--- aws/data_source_aws_dx_gateway_test.go
+++ aws/data_source_aws_dx_gateway_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/directconnect"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/terraform-providers/terraform-provider-aws/atest"
 )
 
 func TestAccDataSourceAwsDxGateway_basic(t *testing.T) {
@@ -16,9 +17,9 @@ func TestAccDataSourceAwsDxGateway_basic(t *testing.T) {
 	datasourceName := "data.aws_dx_gateway.test"
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:   func() { testAccPreCheck(t) },
-		ErrorCheck: testAccErrorCheck(t, directconnect.EndpointsID),
-		Providers:  testAccProviders,
+		PreCheck:   func() { atest.PreCheck(t) },
+		ErrorCheck: atest.ErrorCheck(t, directconnect.EndpointsID),
+		Providers:  atest.Providers,
 		Steps: []resource.TestStep{
 			{
 				Config:      testAccDataSourceAwsDxGatewayConfig_NonExistent,
