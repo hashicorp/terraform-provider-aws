9817c4ba75a4a7a618541c10f982cfbe89c07ede
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:50:44 -0400

    tests/ds/aws_batch_job_queue: Restructure packages

--- aws/data_source_aws_batch_job_queue_test.go
+++ aws/data_source_aws_batch_job_queue_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/batch"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/terraform-providers/terraform-provider-aws/atest"
 )
 
 func TestAccDataSourceAwsBatchJobQueue_basic(t *testing.T) {
@@ -15,9 +16,9 @@ func TestAccDataSourceAwsBatchJobQueue_basic(t *testing.T) {
 	datasourceName := "data.aws_batch_job_queue.by_name"
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:   func() { testAccPreCheck(t); testAccPreCheckAWSBatch(t) },
-		ErrorCheck: testAccErrorCheck(t, batch.EndpointsID),
-		Providers:  testAccProviders,
+		PreCheck:   func() { atest.PreCheck(t); testAccPreCheckAWSBatch(t) },
+		ErrorCheck: atest.ErrorCheck(t, batch.EndpointsID),
+		Providers:  atest.Providers,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccDataSourceAwsBatchJobQueueConfig(rName),
