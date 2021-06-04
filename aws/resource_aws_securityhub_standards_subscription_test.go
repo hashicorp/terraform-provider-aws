1e8bbcff71f9040bd4edfc0e9409b5cd07044a72
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:58:20 -0400

    tests/r/aws_securityhub_standards_subscription: Restructure packages

--- aws/resource_aws_securityhub_standards_subscription_test.go
+++ aws/resource_aws_securityhub_standards_subscription_test.go
@@ -6,17 +6,20 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/securityhub"
+	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/terraform-providers/terraform-provider-aws/atest"
+	awsprovider "github.com/terraform-providers/terraform-provider-aws/provider"
 )
 
 func testAccAWSSecurityHubStandardsSubscription_basic(t *testing.T) {
 	var standardsSubscription *securityhub.StandardsSubscription
 
 	resource.Test(t, resource.TestCase{
-		PreCheck:     func() { testAccPreCheck(t) },
-		ErrorCheck:   testAccErrorCheck(t, securityhub.EndpointsID),
-		Providers:    testAccProviders,
+		PreCheck:     func() { atest.PreCheck(t) },
+		ErrorCheck:   atest.ErrorCheck(t, securityhub.EndpointsID),
+		Providers:    atest.Providers,
 		CheckDestroy: testAccCheckAWSSecurityHubAccountDestroy,
 		Steps: []resource.TestStep{
 			{
@@ -47,7 +50,7 @@ func testAccCheckAWSSecurityHubStandardsSubscriptionExists(n string, standardsSu
 			return fmt.Errorf("Not found: %s", n)
 		}
 
-		conn := testAccProvider.Meta().(*AWSClient).securityhubconn
+		conn := atest.Provider.Meta().(*awsprovider.AWSClient).SecurityHubConn
 
 		resp, err := conn.GetEnabledStandards(&securityhub.GetEnabledStandardsInput{
 			StandardsSubscriptionArns: []*string{aws.String(rs.Primary.ID)},
@@ -68,7 +71,7 @@ func testAccCheckAWSSecurityHubStandardsSubscriptionExists(n string, standardsSu
 }
 
 func testAccCheckAWSSecurityHubStandardsSubscriptionDestroy(s *terraform.State) error {
-	conn := testAccProvider.Meta().(*AWSClient).securityhubconn
+	conn := atest.Provider.Meta().(*awsprovider.AWSClient).SecurityHubConn
 
 	for _, rs := range s.RootModule().Resources {
 		if rs.Type != "aws_securityhub_standards_subscription" {
@@ -80,7 +83,7 @@ func testAccCheckAWSSecurityHubStandardsSubscriptionDestroy(s *terraform.State) 
 		})
 
 		if err != nil {
-			if isAWSErr(err, securityhub.ErrCodeResourceNotFoundException, "") {
+			if tfawserr.ErrMessageContains(err, securityhub.ErrCodeResourceNotFoundException, "") {
 				continue
 			}
 			return err
