97c342471fe806258eee67da53cebc1ab5a83e9e
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:51:53 -0400

    tests/ds/aws_inspector_rules_packages: Restructure packages

--- aws/data_source_aws_inspector_rules_packages_test.go
+++ aws/data_source_aws_inspector_rules_packages_test.go
@@ -5,13 +5,14 @@ import (
 
 	"github.com/aws/aws-sdk-go/service/inspector"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/terraform-providers/terraform-provider-aws/atest"
 )
 
 func TestAccAWSInspectorRulesPackages_basic(t *testing.T) {
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:   func() { testAccPreCheck(t) },
-		ErrorCheck: testAccErrorCheck(t, inspector.EndpointsID),
-		Providers:  testAccProviders,
+		PreCheck:   func() { atest.PreCheck(t) },
+		ErrorCheck: atest.ErrorCheck(t, inspector.EndpointsID),
+		Providers:  atest.Providers,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccCheckAWSInspectorRulesPackagesConfig,
