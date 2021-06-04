362ebf815f3099cc00a41aa6e820698597f69518
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:52:56 -0400

    tests/ds/aws_servicequotas_service: Restructure packages

--- aws/data_source_aws_servicequotas_service_test.go
+++ aws/data_source_aws_servicequotas_service_test.go
@@ -6,15 +6,16 @@ import (
 
 	"github.com/aws/aws-sdk-go/service/servicequotas"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/terraform-providers/terraform-provider-aws/atest"
 )
 
 func TestAccAwsServiceQuotasServiceDataSource_ServiceName(t *testing.T) {
 	dataSourceName := "data.aws_servicequotas_service.test"
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:   func() { testAccPreCheck(t); testAccPartitionHasServicePreCheck(servicequotas.EndpointsID, t) },
-		ErrorCheck: testAccErrorCheck(t, servicequotas.EndpointsID),
-		Providers:  testAccProviders,
+		PreCheck:   func() { atest.PreCheck(t); atest.PreCheckPartitionService(servicequotas.EndpointsID, t) },
+		ErrorCheck: atest.ErrorCheck(t, servicequotas.EndpointsID),
+		Providers:  atest.Providers,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccAwsServiceQuotasServiceDataSourceConfigServiceName("Amazon Virtual Private Cloud (Amazon VPC)"),
