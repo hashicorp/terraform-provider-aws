d854f32b82c90f3c2270cf96241bf1a41dc4a5d8
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:50:41 -0400

    tests/ds/aws_acmpca_certificate_authority: Restructure packages

--- aws/data_source_aws_acmpca_certificate_authority_test.go
+++ aws/data_source_aws_acmpca_certificate_authority_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/service/acmpca"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/terraform-providers/terraform-provider-aws/atest"
 )
 
 func TestAccDataSourceAwsAcmpcaCertificateAuthority_basic(t *testing.T) {
@@ -13,9 +14,9 @@ func TestAccDataSourceAwsAcmpcaCertificateAuthority_basic(t *testing.T) {
 	datasourceName := "data.aws_acmpca_certificate_authority.test"
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:   func() { testAccPreCheck(t) },
-		ErrorCheck: testAccErrorCheck(t, acmpca.EndpointsID),
-		Providers:  testAccProviders,
+		PreCheck:   func() { atest.PreCheck(t) },
+		ErrorCheck: atest.ErrorCheck(t, acmpca.EndpointsID),
+		Providers:  atest.Providers,
 		Steps: []resource.TestStep{
 			{
 				Config:      testAccDataSourceAwsAcmpcaCertificateAuthorityConfig_NonExistent,
