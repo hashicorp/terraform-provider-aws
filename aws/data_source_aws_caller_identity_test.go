4168efdfeb23543b78d32fea099060e32a580644
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:50:45 -0400

    tests/ds/aws_caller_identity: Restructure packages

--- aws/data_source_aws_caller_identity_test.go
+++ aws/data_source_aws_caller_identity_test.go
@@ -1,56 +1,27 @@
 package aws
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/service/sts"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/terraform-providers/terraform-provider-aws/atest"
 )
 
 func TestAccAWSCallerIdentity_basic(t *testing.T) {
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:   func() { testAccPreCheck(t) },
-		ErrorCheck: testAccErrorCheck(t, sts.EndpointsID),
-		Providers:  testAccProviders,
+		PreCheck:   func() { atest.PreCheck(t) },
+		ErrorCheck: atest.ErrorCheck(t, sts.EndpointsID),
+		Providers:  atest.Providers,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccCheckAwsCallerIdentityConfig_basic,
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckAwsCallerIdentityAccountId("data.aws_caller_identity.current"),
+					atest.CheckAwsCallerIdentityAccountId("data.aws_caller_identity.current"),
 				),
 			},
 		},
 	})
-}
-
-func testAccCheckAwsCallerIdentityAccountId(n string) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		rs, ok := s.RootModule().Resources[n]
-		if !ok {
-			return fmt.Errorf("Can't find AccountID resource: %s", n)
-		}
-
-		if rs.Primary.ID == "" {
-			return fmt.Errorf("Account Id resource ID not set.")
-		}
-
-		expected := testAccProvider.Meta().(*AWSClient).accountid
-		if rs.Primary.Attributes["account_id"] != expected {
-			return fmt.Errorf("Incorrect Account ID: expected %q, got %q", expected, rs.Primary.Attributes["account_id"])
-		}
-
-		if rs.Primary.Attributes["user_id"] == "" {
-			return fmt.Errorf("UserID expected to not be nil")
-		}
-
-		if rs.Primary.Attributes["arn"] == "" {
-			return fmt.Errorf("ARN expected to not be nil")
-		}
-
-		return nil
-	}
 }
 
 const testAccCheckAwsCallerIdentityConfig_basic = `
