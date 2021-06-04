74b738e84bb4276c461104e86be783b5fd2af755
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:51:21 -0400

    tests/ds/aws_ecr_authorization_token: Restructure packages

--- aws/data_source_aws_ecr_authorization_token_test.go
+++ aws/data_source_aws_ecr_authorization_token_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/ecr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/terraform-providers/terraform-provider-aws/atest"
 )
 
 func TestAccAWSEcrAuthorizationTokenDataSource_basic(t *testing.T) {
@@ -15,9 +16,9 @@ func TestAccAWSEcrAuthorizationTokenDataSource_basic(t *testing.T) {
 	dataSourceName := "data.aws_ecr_authorization_token.repo"
 
 	resource.Test(t, resource.TestCase{
-		PreCheck:   func() { testAccPreCheck(t) },
-		ErrorCheck: testAccErrorCheck(t, ecr.EndpointsID),
-		Providers:  testAccProviders,
+		PreCheck:   func() { atest.PreCheck(t) },
+		ErrorCheck: atest.ErrorCheck(t, ecr.EndpointsID),
+		Providers:  atest.Providers,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccCheckAwsEcrAuthorizationTokenDataSourceBasicConfig,
