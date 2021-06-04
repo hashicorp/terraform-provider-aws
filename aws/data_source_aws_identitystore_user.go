fc9d6d2de2af9c1a9d31a2b507c1f1fa75126bba
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:51:52 -0400

    ds/aws_identitystore_user: Restructure packages

--- aws/data_source_aws_identitystore_user.go
+++ aws/data_source_aws_identitystore_user.go
@@ -8,6 +8,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/identitystore"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	awsprovider "github.com/terraform-providers/terraform-provider-aws/provider"
 )
 
 func dataSourceAwsIdentityStoreUser() *schema.Resource {
@@ -60,7 +61,7 @@ func dataSourceAwsIdentityStoreUser() *schema.Resource {
 }
 
 func dataSourceAwsIdentityStoreUserRead(d *schema.ResourceData, meta interface{}) error {
-	conn := meta.(*AWSClient).identitystoreconn
+	conn := meta.(*awsprovider.AWSClient).IdentityStoreConn
 
 	input := &identitystore.ListUsersInput{
 		IdentityStoreId: aws.String(d.Get("identity_store_id").(string)),
