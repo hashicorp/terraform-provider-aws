aab5aa27f7ecb6bc59708ebfd7d88da1e89213e9
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:51:48 -0400

    tests/ds/aws_elasticache_cluster: Restructure packages

--- aws/data_source_aws_elasticache_cluster_test.go
+++ aws/data_source_aws_elasticache_cluster_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/elasticache"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/terraform-providers/terraform-provider-aws/atest"
 )
 
 func TestAccAWSDataElasticacheCluster_basic(t *testing.T) {
@@ -15,9 +16,9 @@ func TestAccAWSDataElasticacheCluster_basic(t *testing.T) {
 	dataSourceName := "data.aws_elasticache_cluster.test"
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:   func() { testAccPreCheck(t) },
-		ErrorCheck: testAccErrorCheck(t, elasticache.EndpointsID),
-		Providers:  testAccProviders,
+		PreCheck:   func() { atest.PreCheck(t) },
+		ErrorCheck: atest.ErrorCheck(t, elasticache.EndpointsID),
+		Providers:  atest.Providers,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccAWSElastiCacheClusterConfigWithDataSource(rName),
