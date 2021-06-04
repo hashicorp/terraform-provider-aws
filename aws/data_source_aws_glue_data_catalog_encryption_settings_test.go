161d37b7ae02e9a2e74ea39372216f7d5c75097b
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:51:50 -0400

    tests/ds/aws_glue_data_catalog_encryption_settings: Restructure packages

--- aws/data_source_aws_glue_data_catalog_encryption_settings_test.go
+++ aws/data_source_aws_glue_data_catalog_encryption_settings_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/glue"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/terraform-providers/terraform-provider-aws/atest"
 )
 
 func TestAccDataSourceAwsGlueDataCatalogEncryptionSettings_basic(t *testing.T) {
@@ -14,9 +15,9 @@ func TestAccDataSourceAwsGlueDataCatalogEncryptionSettings_basic(t *testing.T) {
 	datasourceName := "data.aws_glue_data_catalog_encryption_settings.test"
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:   func() { testAccPreCheck(t) },
-		ErrorCheck: testAccErrorCheck(t, glue.EndpointsID),
-		Providers:  testAccProviders,
+		PreCheck:   func() { atest.PreCheck(t) },
+		ErrorCheck: atest.ErrorCheck(t, glue.EndpointsID),
+		Providers:  atest.Providers,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccDataSourceAwsGlueDataCatalogEncryptionSettingsConfig(),
