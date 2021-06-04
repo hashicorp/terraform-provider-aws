bf9867e1e1a79f71b71696f948d560e2f3fda54f
Dirk Avery <dirk.avery@gmail.com>
2021-06-04 00:51:50 -0400

    ds/aws_glue_script: Restructure packages

--- aws/data_source_aws_glue_script.go
+++ aws/data_source_aws_glue_script.go
@@ -9,6 +9,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/glue"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	awsprovider "github.com/terraform-providers/terraform-provider-aws/provider"
 )
 
 func dataSourceAwsGlueScript() *schema.Resource {
@@ -98,7 +99,7 @@ func dataSourceAwsGlueScript() *schema.Resource {
 }
 
 func dataSourceAwsGlueScriptRead(d *schema.ResourceData, meta interface{}) error {
-	conn := meta.(*AWSClient).glueconn
+	conn := meta.(*awsprovider.AWSClient).GlueConn
 
 	dagEdge := d.Get("dag_edge").([]interface{})
 	dagNode := d.Get("dag_node").([]interface{})
@@ -122,7 +123,7 @@ func dataSourceAwsGlueScriptRead(d *schema.ResourceData, meta interface{}) error
 		return errors.New("script not created")
 	}
 
-	d.SetId(meta.(*AWSClient).region)
+	d.SetId(meta.(*awsprovider.AWSClient).Region)
 	d.Set("python_script", output.PythonScript)
 	d.Set("scala_code", output.ScalaCode)
 
