e05971353ac0466a03543ddd730982479b981a70
drfaust92 <ilia.lazebnik@gmail.com>
2021-06-14 11:31:17 +0300

    fmt

--- aws/resource_aws_ecs_cluster_test.go
+++ aws/resource_aws_ecs_cluster_test.go
@@ -632,14 +632,14 @@ resource "aws_ecs_cluster" "test" {
 
   configuration {
     execute_command_configuration {
-	  kms_key_id = aws_kms_key.test.arn
-	  logging    = "OVERRIDE"
+      kms_key_id = aws_kms_key.test.arn
+      logging    = "OVERRIDE"
 
-	  log_configuration {
+      log_configuration {
         cloud_watch_encryption_enabled = %[2]t
-		cloud_watch_log_group_name     = aws_cloudwatch_log_group.test.name
-	  }
-	}
+        cloud_watch_log_group_name     = aws_cloudwatch_log_group.test.name
+      }
+    }
   }
 }
 `, rName, enable)
