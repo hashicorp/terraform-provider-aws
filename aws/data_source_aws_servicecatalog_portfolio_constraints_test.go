64cbda168fd922830b167c0ceaa40e6943dbe33f
Dirk Avery <dirk.avery@gmail.com>
2021-06-15 13:57:15 -0400

    tests/d/servicecat_port_contraints: New data source

---
+++ aws/data_source_aws_servicecatalog_portfolio_constraints_test.go
@@ -0,0 +1,44 @@
+package aws
+
+import (
+	"testing"
+
+	"github.com/aws/aws-sdk-go/service/servicecatalog"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccAWSServiceCatalogPortfolioConstraintDataSource_basic(t *testing.T) {
+	resourceName := "aws_servicecatalog_portfolio_constraint.test"
+	dataSourceName := "data.aws_servicecatalog_portfolio_constraint.test"
+	rName := acctest.RandomWithPrefix("tf-acc-test")
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:   func() { testAccPreCheck(t) },
+		ErrorCheck: testAccErrorCheck(t, servicecatalog.EndpointsID),
+		Providers:  testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAWSServiceCatalogPortfolioConstraintDataSourceConfig_basic(rName, rName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrPair(resourceName, "accept_language", dataSourceName, "accept_language"),
+					resource.TestCheckResourceAttrPair(resourceName, "description", dataSourceName, "description"),
+					resource.TestCheckResourceAttrPair(resourceName, "owner", dataSourceName, "owner"),
+					resource.TestCheckResourceAttrPair(resourceName, "parameters", dataSourceName, "parameters"),
+					resource.TestCheckResourceAttrPair(resourceName, "portfolio_id", dataSourceName, "portfolio_id"),
+					resource.TestCheckResourceAttrPair(resourceName, "product_id", dataSourceName, "product_id"),
+					resource.TestCheckResourceAttrPair(resourceName, "status", dataSourceName, "status"),
+					resource.TestCheckResourceAttrPair(resourceName, "type", dataSourceName, "type"),
+				),
+			},
+		},
+	})
+}
+
+func testAccAWSServiceCatalogPortfolioConstraintDataSourceConfig_basic(rName, description string) string {
+	return composeConfig(testAccAWSServiceCatalogPortfolioConstraintConfig_basic(rName, description), `
+data "aws_servicecatalog_portfolio_constraints" "test" {
+  id = aws_servicecatalog_portfolio_constraint.test.id
+}
+`)
+}
