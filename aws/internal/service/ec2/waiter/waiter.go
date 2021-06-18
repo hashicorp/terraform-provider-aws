46eea7a6e7e3c268fca9fc22e7cd2b007c27748f
Kit Ewbank <Kit_Ewbank@hotmail.com>
2021-06-18 13:58:20 -0400

    Tweak use of 'tfresource.SetLastError'.

--- aws/internal/service/ec2/waiter/waiter.go
+++ aws/internal/service/ec2/waiter/waiter.go
@@ -358,7 +358,7 @@ func RouteTableAssociationCreated(conn *ec2.EC2, id string) (*ec2.RouteTableAsso
 	outputRaw, err := stateConf.WaitForState()
 
 	if output, ok := outputRaw.(*ec2.RouteTableAssociationState); ok {
-		if output != nil && aws.StringValue(output.State) == ec2.RouteTableAssociationStateCodeFailed {
+		if state := aws.StringValue(output.State); state == ec2.RouteTableAssociationStateCodeFailed {
 			tfresource.SetLastError(err, errors.New(aws.StringValue(output.StatusMessage)))
 		}
 
@@ -379,7 +379,7 @@ func RouteTableAssociationDeleted(conn *ec2.EC2, id string) (*ec2.RouteTableAsso
 	outputRaw, err := stateConf.WaitForState()
 
 	if output, ok := outputRaw.(*ec2.RouteTableAssociationState); ok {
-		if output != nil && aws.StringValue(output.State) == ec2.RouteTableAssociationStateCodeFailed {
+		if state := aws.StringValue(output.State); state == ec2.RouteTableAssociationStateCodeFailed {
 			tfresource.SetLastError(err, errors.New(aws.StringValue(output.StatusMessage)))
 		}
 
@@ -400,7 +400,7 @@ func RouteTableAssociationUpdated(conn *ec2.EC2, id string) (*ec2.RouteTableAsso
 	outputRaw, err := stateConf.WaitForState()
 
 	if output, ok := outputRaw.(*ec2.RouteTableAssociationState); ok {
-		if output != nil && aws.StringValue(output.State) == ec2.RouteTableAssociationStateCodeFailed {
+		if state := aws.StringValue(output.State); state == ec2.RouteTableAssociationStateCodeFailed {
 			tfresource.SetLastError(err, errors.New(aws.StringValue(output.StatusMessage)))
 		}
 
@@ -704,8 +704,8 @@ func VpcEndpointAccepted(conn *ec2.EC2, vpcEndpointID string, timeout time.Durat
 	outputRaw, err := stateConf.WaitForState()
 
 	if output, ok := outputRaw.(*ec2.VpcEndpoint); ok {
-		if output != nil && aws.StringValue(output.State) == tfec2.VpcEndpointStateFailed && output.LastError != nil {
-			tfresource.SetLastError(err, fmt.Errorf("%s: %s", aws.StringValue(output.LastError.Code), aws.StringValue(output.LastError.Message)))
+		if state, lastError := aws.StringValue(output.State), output.LastError; state == tfec2.VpcEndpointStateFailed && lastError != nil {
+			tfresource.SetLastError(err, fmt.Errorf("%s: %s", aws.StringValue(lastError.Code), aws.StringValue(lastError.Message)))
 		}
 
 		return output, err
