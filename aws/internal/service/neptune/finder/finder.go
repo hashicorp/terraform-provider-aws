a4b72484a56228893deb6550dc27cc59d1e20d39
drfaust92 <ilia.lazebnik@gmail.com>
2021-06-19 19:29:32 +0300

    initial commit

---
+++ aws/internal/service/neptune/finder/finder.go
@@ -0,0 +1,44 @@
+package finder
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/neptune"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	tfneptune "github.com/terraform-providers/terraform-provider-aws/aws/internal/service/neptune"
+)
+
+func EndpointById(conn *neptune.Neptune, id string) (*neptune.DBClusterEndpoint, error) {
+	clusterId, endpointId, err := tfneptune.ReadAwsNeptuneClusterEndpointId(id)
+	if err != nil {
+		return nil, err
+	}
+	input := &neptune.DescribeDBClusterEndpointsInput{
+		DBClusterIdentifier:         aws.String(clusterId),
+		DBClusterEndpointIdentifier: aws.String(endpointId),
+	}
+
+	output, err := conn.DescribeDBClusterEndpoints(input)
+	if err != nil {
+		return nil, &resource.NotFoundError{
+			LastError:   err,
+			LastRequest: input,
+		}
+	}
+
+	if output == nil {
+		return nil, &resource.NotFoundError{
+			Message:     "Empty result",
+			LastRequest: input,
+		}
+	}
+
+	endpoints := output.DBClusterEndpoints
+	if len(endpoints) == 0 {
+		return nil, &resource.NotFoundError{
+			Message:     "Empty result",
+			LastRequest: input,
+		}
+	}
+
+	return endpoints[0], nil
+}
