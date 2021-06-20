5489dc60a35b9f488261b92b79f8b97333392095
Adithya Ranganath <kradikar@gmail.com>
2021-06-21 00:34:55 +0800

    Update variables.tf

--- examples/two-tier/variables.tf
+++ examples/two-tier/variables.tf
@@ -14,7 +14,7 @@ variable "key_name" {
 
 variable "aws_region" {
   description = "AWS region to launch servers."
-  default     = "us-west-2"
+  default     = "ap-southeast-1"
 }
 
 # Ubuntu Precise 12.04 LTS (x64)
