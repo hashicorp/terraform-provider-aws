11d04ceb643920e95317a4ec870581757452f733
Dirk Avery <dirk.avery@gmail.com>
2021-06-24 16:58:51 -0400

    tests/d/caller_identity: Unit test source_arn

--- aws/data_source_aws_caller_identity_test.go
+++ aws/data_source_aws_caller_identity_test.go
@@ -9,6 +9,65 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
+func TestSourceARN(t *testing.T) {
+	testCases := []struct {
+		Name     string
+		ARN      string
+		Expected string
+	}{
+		{
+			Name:     "not an ARN",
+			ARN:      "abcd",
+			Expected: "abcd",
+		},
+		{
+			Name:     "regular ARN",
+			ARN:      "arn:aws:iam::111122223333:role/role_name",
+			Expected: "arn:aws:iam::111122223333:role/role_name",
+		},
+		{
+			Name:     "assumed role ARN",
+			ARN:      "arn:aws:sts::444433332222:assumed-role/something_something-admin/sessionIDNotPartOfRoleARN",
+			Expected: "arn:aws:iam::444433332222:role/something_something-admin",
+		},
+		{
+			Name:     "'assumed-role' part of ARN resource",
+			ARN:      "arn:aws:iam::444433332222:user/assumed-role-but-not-really",
+			Expected: "arn:aws:iam::444433332222:user/assumed-role-but-not-really",
+		},
+		{
+			Name:     "user ARN",
+			ARN:      "arn:aws:iam::123456789012:user/Bob",
+			Expected: "arn:aws:iam::123456789012:user/Bob",
+		},
+		{
+			Name:     "assumed role from AWS example",
+			ARN:      "arn:aws:sts::123456789012:assumed-role/example-role/AWSCLI-Session",
+			Expected: "arn:aws:iam::123456789012:role/example-role",
+		},
+		{
+			Name:     "multiple slashes in resource", // not sure this is even valid
+			ARN:      "arn:aws:sts::123456789012:assumed-role/example-role/also-part-of-role-or-no/AWSCLI-Session",
+			Expected: "arn:aws:iam::123456789012:role/example-role/also-part-of-role-or-no",
+		},
+		{
+			Name:     "not an sts ARN",
+			ARN:      "arn:aws:iam::123456789012:assumed-role/example-role/AWSCLI-Session",
+			Expected: "arn:aws:iam::123456789012:assumed-role/example-role/AWSCLI-Session",
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			got := sourceARN(testCase.ARN)
+
+			if got != testCase.Expected {
+				t.Errorf("for %s: got %s, expected %s", testCase.Name, got, testCase.Expected)
+			}
+		})
+	}
+}
+
 func TestAccAWSCallerIdentity_basic(t *testing.T) {
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:   func() { testAccPreCheck(t) },
