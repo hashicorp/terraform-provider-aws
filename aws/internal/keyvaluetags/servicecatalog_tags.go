488fc844c266bc9f004a79e8cfc1827bd56c6cc0
Dirk Avery <dirk.avery@gmail.com>
2021-06-29 14:01:41 -0400

    internal/keyvaluetags: Remove unused func

--- aws/internal/keyvaluetags/servicecatalog_tags.go
+++ aws/internal/keyvaluetags/servicecatalog_tags.go
@@ -70,28 +70,3 @@ func ServicecatalogRecordKeyValueTags(tags []*servicecatalog.RecordTag) KeyValue
 
 	return New(m)
 }
-
-func ServiceCatalogProvisionedProductUpdateTags(conn *servicecatalog.ServiceCatalog, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
-	oldTags := New(oldTagsMap)
-	newTags := New(newTagsMap)
-
-	input := &servicecatalog.UpdateProductInput{
-		Id: aws.String(identifier),
-	}
-
-	if removedTags := oldTags.Removed(newTags); len(removedTags) > 0 {
-		input.RemoveTags = aws.StringSlice(removedTags.IgnoreAws().Keys())
-	}
-
-	if updatedTags := oldTags.Updated(newTags); len(updatedTags) > 0 {
-		input.AddTags = updatedTags.IgnoreAws().ServicecatalogTags()
-	}
-
-	_, err := conn.UpdateProduct(input)
-
-	if err != nil {
-		return fmt.Errorf("error updating tags for Service Catalog Product (%s): %w", identifier, err)
-	}
-
-	return nil
-}
