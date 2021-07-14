df4fb0e7709b9bde453b502181850a821ae790c8
Tyler Lynch <adamtylerlynch@gmail.com>
2021-07-14 11:12:11 -0400

    SG-FSx extract the lookup logic to the relevant finder package.

--- aws/internal/service/storagegateway/finder/finder.go
+++ aws/internal/service/storagegateway/finder/finder.go
@@ -1,8 +1,13 @@
 package finder
 
 import (
+	"errors"
+	"fmt"
+	"log"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/storagegateway"
+	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
 )
 
 func LocalDiskByDiskId(conn *storagegateway.StorageGateway, gatewayARN string, diskID string) (*storagegateway.Disk, error) {
@@ -78,4 +83,38 @@ func UploadBufferDisk(conn *storagegateway.StorageGateway, gatewayARN string, di
 	}
 
 	return result, err
+}
+
+func FileSystemAssociationByARN(conn *storagegateway.StorageGateway, fileSystemAssociationARN string) (*storagegateway.FileSystemAssociationInfo, error) {
+
+	input := &storagegateway.DescribeFileSystemAssociationsInput{
+		FileSystemAssociationARNList: []*string{aws.String(fileSystemAssociationARN)},
+	}
+	log.Printf("[DEBUG] Reading Storage Gateway FSx File Associations: %s", input)
+
+	output, err := conn.DescribeFileSystemAssociations(input)
+	if err != nil {
+		if tfawserr.ErrCodeEquals(err, storagegateway.ErrCodeInvalidGatewayRequestException) {
+			var igrex *storagegateway.InvalidGatewayRequestException
+			if ok := errors.As(err, &igrex); ok {
+				if err := igrex.Error_; err != nil {
+					if aws.StringValue(err.ErrorCode) == "FileSystemAssociationNotFound" {
+						log.Printf("[WARN] FSX File System %q not found", fileSystemAssociationARN)
+						return nil, nil
+					}
+				}
+			}
+		}
+
+		return nil, fmt.Errorf("error reading Storage Gateway FSx File System: %w", err)
+	}
+
+	if output == nil || len(output.FileSystemAssociationInfoList) == 0 || output.FileSystemAssociationInfoList[0] == nil {
+		log.Printf("[WARN] FSX File System %q not found", fileSystemAssociationARN)
+		return nil, nil
+	}
+
+	filesystem := output.FileSystemAssociationInfoList[0]
+
+	return filesystem, nil
 }
