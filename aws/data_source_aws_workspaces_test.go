739cf42a659b625641c3f4bb6f62c11b6dcb7237
Kit Ewbank <Kit_Ewbank@hotmail.com>
2021-08-06 12:09:10 -0400

    Serialize Workspaces tests: d/aws_workspaces_bundle.

---
+++ aws/data_source_aws_workspaces_test.go
@@ -0,0 +1,31 @@
+package aws
+
+import (
+	"testing"
+)
+
+func TestAccDataSourceAwsWorkspaces_serial(t *testing.T) {
+	testCases := map[string]map[string]func(t *testing.T){
+		"Bundle": {
+			"basic":                   testAccDataSourceAwsWorkspaceBundle_basic,
+			"bundleIDAndNameConflict": testAccDataSourceAwsWorkspaceBundle_bundleIDAndNameConflict,
+			"byOwnerName":             testAccDataSourceAwsWorkspaceBundle_byOwnerName,
+			"privateOwner":            testAccDataSourceAwsWorkspaceBundle_privateOwner,
+		},
+		"Directory": {},
+		"Image":     {},
+		"Workspace": {},
+	}
+
+	for group, m := range testCases {
+		m := m
+		t.Run(group, func(t *testing.T) {
+			for name, tc := range m {
+				tc := tc
+				t.Run(name, func(t *testing.T) {
+					tc(t)
+				})
+			}
+		})
+	}
+}
