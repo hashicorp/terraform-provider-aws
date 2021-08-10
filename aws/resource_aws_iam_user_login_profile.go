f48c3a3695b7a58d04ad84ea084670d74d3fb071
Dirk Avery <dirk.avery@gmail.com>
2021-08-10 13:02:01 -0400

    r/iam_user_login_profile: Fix wrong import

--- aws/resource_aws_iam_user_login_profile.go
+++ aws/resource_aws_iam_user_login_profile.go
@@ -16,7 +16,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/terraform-providers/terraform-provider-aws/aws/internal/encryption"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/service/ec2/waiter"
+	"github.com/terraform-providers/terraform-provider-aws/aws/internal/service/iam/waiter"
 	"github.com/terraform-providers/terraform-provider-aws/aws/internal/tfresource"
 )
 
