c6c53a8489b45050511699a935b3079d1db9c262
Aleks1001 <alexey.kozyachiy@gmail.com>
2021-08-13 18:52:32 -0400

    Add new aws_chime_voice_connector_group resource

--- aws/provider.go
+++ aws/provider.go
@@ -549,6 +549,7 @@ func Provider() *schema.Provider {
 			"aws_budgets_budget":                                      resourceAwsBudgetsBudget(),
 			"aws_budgets_budget_action":                               resourceAwsBudgetsBudgetAction(),
 			"aws_chime_voice_connector":                               resourceAwsChimeVoiceConnector(),
+			"aws_chime_voice_connector_group":                         resourceAwsChimeVoiceConnectorGroup(),
 			"aws_cloud9_environment_ec2":                              resourceAwsCloud9EnvironmentEc2(),
 			"aws_cloudformation_stack":                                resourceAwsCloudFormationStack(),
 			"aws_cloudformation_stack_set":                            resourceAwsCloudFormationStackSet(),
