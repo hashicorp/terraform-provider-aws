c453bf386809e659834ffa577cb01c852e404332
Dirk Avery <dirk.avery@gmail.com>
2021-08-23 20:32:57 -0400

    tests/r/route53rcc_control_panel: Fix resource

--- aws/resource_aws_route53recoverycontrolconfig_control_panel.go
+++ aws/resource_aws_route53recoverycontrolconfig_control_panel.go
@@ -6,9 +6,10 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/route53recoverycontrolconfig"
+	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/service/route53recoverycontrolconfig"
+	waiter "github.com/terraform-providers/terraform-provider-aws/aws/internal/service/route53recoverycontrolconfig"
 )
 
 func resourceAwsRoute53RecoveryControlConfigControlPanel() *schema.Resource {
@@ -21,7 +22,7 @@ func resourceAwsRoute53RecoveryControlConfigControlPanel() *schema.Resource {
 			State: schema.ImportStatePassthrough,
 		},
 		Schema: map[string]*schema.Schema{
-			"control_panel_arn": {
+			"arn": {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
@@ -34,13 +35,13 @@ func resourceAwsRoute53RecoveryControlConfigControlPanel() *schema.Resource {
 				Type:     schema.TypeBool,
 				Computed: true,
 			},
-			"routing_control_count": {
-				Type:     schema.TypeInt,
-				Computed: true,
-			},
 			"name": {
 				Type:     schema.TypeString,
 				Required: true,
+			},
+			"routing_control_count": {
+				Type:     schema.TypeInt,
+				Computed: true,
 			},
 			"status": {
 				Type:     schema.TypeString,
@@ -67,7 +68,7 @@ func resourceAwsRoute53RecoveryControlConfigControlPanelCreate(d *schema.Resourc
 	}
 
 	if result == nil {
-		return fmt.Errorf("Error creating Route53 Recovery Control Config Control Panel empty response")
+		return fmt.Errorf("Error creating Route53 Recovery Control Config Control Panel: empty response")
 	}
 
 	d.SetId(aws.StringValue(result.ControlPanelArn))
@@ -87,23 +88,27 @@ func resourceAwsRoute53RecoveryControlConfigControlPanelRead(d *schema.ResourceD
 	}
 
 	output, err := conn.DescribeControlPanel(input)
-	result := output.ControlPanel
 
-	if err != nil {
-		return fmt.Errorf("Error describing Route53 Recovery Control Config Control Panel: %s", err)
-	}
-
-	if !d.IsNewResource() && result == nil {
+	if !d.IsNewResource() && tfawserr.ErrCodeEquals(err, route53recoverycontrolconfig.ErrCodeResourceNotFoundException) {
 		log.Printf("[WARN] Route53 Recovery Control Config Control Panel (%s) not found, removing from state", d.Id())
 		d.SetId("")
 		return nil
 	}
 
-	d.Set("control_panel_arn", result.ControlPanelArn)
+	if err != nil {
+		return fmt.Errorf("Error describing Route53 Recovery Control Config Control Panel: %s", err)
+	}
+
+	if output == nil || output.ControlPanel == nil {
+		return fmt.Errorf("Error describing Route53 Recovery Control Config Control Panel: %s", "empty response")
+	}
+
+	result := output.ControlPanel
+	d.Set("arn", result.ControlPanelArn)
 	d.Set("cluster_arn", result.ClusterArn)
 	d.Set("default_control_panel", result.DefaultControlPanel)
-	d.Set("routing_control_count", result.RoutingControlCount)
 	d.Set("name", result.Name)
+	d.Set("routing_control_count", result.RoutingControlCount)
 	d.Set("status", result.Status)
 
 	return nil
@@ -114,10 +119,11 @@ func resourceAwsRoute53RecoveryControlConfigControlPanelUpdate(d *schema.Resourc
 
 	input := &route53recoverycontrolconfig.UpdateControlPanelInput{
 		ControlPanelName: aws.String(d.Get("name").(string)),
-		ControlPanelArn:  aws.String(d.Get("control_panel_arn").(string)),
+		ControlPanelArn:  aws.String(d.Get("arn").(string)),
 	}
 
 	_, err := conn.UpdateControlPanel(input)
+
 	if err != nil {
 		return fmt.Errorf("error updating Route53 Recovery Control Config Control Panel: %s", err)
 	}
@@ -134,17 +140,21 @@ func resourceAwsRoute53RecoveryControlConfigControlPanelDelete(d *schema.Resourc
 
 	_, err := conn.DeleteControlPanel(input)
 
+	if tfawserr.ErrCodeEquals(err, route53recoverycontrolconfig.ErrCodeResourceNotFoundException) {
+		return nil
+	}
+
 	if err != nil {
-		if isAWSErr(err, route53recoverycontrolconfig.ErrCodeResourceNotFoundException, "") {
-			return nil
-		}
 		return fmt.Errorf("error deleting Route53 Recovery Control Config Control Panel: %s", err)
 	}
 
-	if _, err := waiter.Route53RecoveryControlConfigControlPanelDeleted(conn, d.Id()); err != nil {
-		if isAWSErr(err, route53recoverycontrolconfig.ErrCodeResourceNotFoundException, "") {
-			return nil
-		}
+	_, err = waiter.Route53RecoveryControlConfigControlPanelDeleted(conn, d.Id())
+
+	if tfawserr.ErrCodeEquals(err, route53recoverycontrolconfig.ErrCodeResourceNotFoundException) {
+		return nil
+	}
+
+	if err != nil {
 		return fmt.Errorf("Error waiting for Route53 Recovery Control Config Control Panel (%s) to be deleted: %w", d.Id(), err)
 	}
 
