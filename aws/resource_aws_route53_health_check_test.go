40ff2717ab9c084c98b8afec9906bdf6aff555fb
nargua <nargua@amazom.com>
2021-08-31 13:09:14 -0700

    minor fixes

--- aws/resource_aws_route53_health_check_test.go
+++ aws/resource_aws_route53_health_check_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws/endpoints"
 	"github.com/aws/aws-sdk-go/service/route53"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
