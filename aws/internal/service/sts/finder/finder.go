a94531c26787506194ff8ec1e5e1ca597542ffe0
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 11:00:04 -0400

    sts: Create verify package

--- aws/internal/service/sts/finder/finder.go
+++ aws/internal/service/sts/finder/finder.go
@@ -3,6 +3,7 @@ package finder
 import (
 	"github.com/aws/aws-sdk-go/service/sts"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 func CallerIdentity(conn *sts.STS) (*sts.GetCallerIdentityOutput, error) {
