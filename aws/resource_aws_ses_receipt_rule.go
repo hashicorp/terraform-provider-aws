6c2bd41cb175e6728f398d1e19d18dafd58a9249
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:34:59 -0400

    ses: Change from terraform-providers to hashicorp

--- aws/resource_aws_ses_receipt_rule.go
+++ aws/resource_aws_ses_receipt_rule.go
@@ -13,7 +13,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/ses"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/hashcode"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/hashcode"
 )
 
 func resourceAwsSesReceiptRule() *schema.Resource {
