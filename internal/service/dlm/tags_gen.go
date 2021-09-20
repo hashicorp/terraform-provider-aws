8db5d1250c08fa9ed6590522f43f90f4955d3042
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 12:34:29 -0400

    dlm: Fix more import problems

--- internal/service/dlm/tags_gen.go
+++ internal/service/dlm/tags_gen.go
@@ -4,15 +4,10 @@ package dlm
 
 import (
 	"fmt"
-	"strconv"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dlm"
-	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
-	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 )
 
 // ListTags lists dlm service tags.
