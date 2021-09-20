d1233f190b9e27ed5c96626f2372df771b5d1be6
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 12:34:42 -0400

    macie2: Fix more import problems

--- internal/service/macie2/tags_gen.go
+++ internal/service/macie2/tags_gen.go
@@ -3,16 +3,8 @@
 package macie2
 
 import (
-	"fmt"
-	"strconv"
-
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/macie2"
-	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
-	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 )
 
 // map[string]*string handling
