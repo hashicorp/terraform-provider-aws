7415b26f1e70714ebc93ba75b9417abd100002cd
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 12:34:22 -0400

    cloud9: Fix more import problems

--- internal/service/cloud9/tags_gen.go
+++ internal/service/cloud9/tags_gen.go
@@ -4,15 +4,10 @@ package cloud9
 
 import (
 	"fmt"
-	"strconv"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/cloud9"
-	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
-	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 )
 
 // ListTags lists cloud9 service tags.
