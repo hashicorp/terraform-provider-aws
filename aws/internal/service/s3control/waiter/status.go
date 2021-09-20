29e910a60f6499d22d775b6b629d114678ed50bf
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:34:58 -0400

    s3control: Change from terraform-providers to hashicorp

--- aws/internal/service/s3control/waiter/status.go
+++ aws/internal/service/s3control/waiter/status.go
@@ -6,7 +6,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3control"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/service/s3control/finder"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/s3control/finder"
 )
 
 // PublicAccessBlockConfigurationBlockPublicAcls fetches the PublicAccessBlockConfiguration and its BlockPublicAcls
