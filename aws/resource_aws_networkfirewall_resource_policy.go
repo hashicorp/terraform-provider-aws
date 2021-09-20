5da8d49361026f156105db237799b9e8ffb6504b
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:55:25 -0400

    networkfirewall: Begin long road to new tags

--- aws/resource_aws_networkfirewall_resource_policy.go
+++ aws/resource_aws_networkfirewall_resource_policy.go
@@ -13,6 +13,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/networkfirewall/finder"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 func ResourceResourcePolicy() *schema.Resource {
