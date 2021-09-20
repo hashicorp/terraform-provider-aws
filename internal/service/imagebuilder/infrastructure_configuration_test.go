f318087d58a99c66648e92a01287e2155d6c1c2a
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 12:16:39 -0400

    imagebuilder: Export many entities for use by test, cross-package

--- internal/service/imagebuilder/infrastructure_configuration_test.go
+++ internal/service/imagebuilder/infrastructure_configuration_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	tfimagebuilder "github.com/hashicorp/terraform-provider-aws/internal/service/imagebuilder"
 )
 
 func init() {
@@ -46,7 +47,7 @@ func testSweepImageBuilderInfrastructureConfigurations(region string) error {
 
 			arn := aws.StringValue(infrastructureConfigurationSummary.Arn)
 
-			r := ResourceInfrastructureConfiguration()
+			r := tfimagebuilder.ResourceInfrastructureConfiguration()
 			d := r.Data(nil)
 			d.SetId(arn)
 
@@ -130,7 +131,7 @@ func TestAccAwsImageBuilderInfrastructureConfiguration_disappears(t *testing.T) 
 				Config: testAccAwsImageBuilderInfrastructureConfigurationConfigName(rName),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckAwsImageBuilderInfrastructureConfigurationExists(resourceName),
-					acctest.CheckResourceDisappears(acctest.Provider, ResourceInfrastructureConfiguration(), resourceName),
+					acctest.CheckResourceDisappears(acctest.Provider, tfimagebuilder.ResourceInfrastructureConfiguration(), resourceName),
 				),
 				ExpectNonEmptyPlan: true,
 			},
