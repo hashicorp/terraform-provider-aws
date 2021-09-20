3d9823331597672e8b903b84404b6acfb93c96a1
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:34:45 -0400

    elasticbeanstalk: Change from terraform-providers to hashicorp

--- aws/resource_aws_elastic_beanstalk_environment.go
+++ aws/resource_aws_elastic_beanstalk_environment.go
@@ -15,8 +15,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/structure"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/hashcode"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/keyvaluetags"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/hashcode"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/keyvaluetags"
 )
 
 func resourceAwsElasticBeanstalkOptionSetting() *schema.Resource {
