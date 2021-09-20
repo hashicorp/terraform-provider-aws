43c77df04ef756e0e0e22da36cc4f3f135b90aaa
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 11:26:45 -0400

    serverlessapprepo: Fix more import problems

--- internal/service/serverlessapprepo/cloudformation_stack.go
+++ internal/service/serverlessapprepo/cloudformation_stack.go
@@ -13,13 +13,12 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/flex"
+	tfcloudformation "github.com/hashicorp/terraform-provider-aws/internal/service/cloudformation"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
-	"github.com/hashicorp/terraform-provider-aws/internal/conns"
-	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
-	tfcloudformation "github.com/hashicorp/terraform-provider-aws/internal/service/cloudformation"
-	tfcloudformation "github.com/hashicorp/terraform-provider-aws/internal/service/cloudformation"
 )
 
 const (
@@ -355,4 +354,3 @@ func flattenCloudFormationOutputs(cfOutputs []*cloudformation.Output) map[string
 	}
 	return outputs
 }
-
