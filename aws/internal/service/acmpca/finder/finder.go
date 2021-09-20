bae2bf07866c6dba1cb31ef45259ac0a9f853b16
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:59:32 -0400

    acmpca: Create verify package

--- aws/internal/service/acmpca/finder/finder.go
+++ aws/internal/service/acmpca/finder/finder.go
@@ -5,6 +5,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/acmpca"
 	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 // CertificateAuthorityByARN returns the certificate authority corresponding to the specified ARN.
