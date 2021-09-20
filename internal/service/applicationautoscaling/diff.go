dc664e7e0daf97a10b1995b8c79c8946e22f6e3b
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:59:33 -0400

    applicationautoscaling: Create verify package

---
+++ internal/service/applicationautoscaling/diff.go
@@ -0,0 +1,24 @@
+package applicationautoscaling
+
+import (
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
+)
+
+// suppressEquivalentTime suppresses differences for time values that represent the same
+// instant in different timezones.
+func suppressEquivalentTime(k, old, new string, d *schema.ResourceData) bool {
+	oldTime, err := time.Parse(time.RFC3339, old)
+	if err != nil {
+		return false
+	}
+
+	newTime, err := time.Parse(time.RFC3339, new)
+	if err != nil {
+		return false
+	}
+
+	return oldTime.Equal(newTime)
+}
