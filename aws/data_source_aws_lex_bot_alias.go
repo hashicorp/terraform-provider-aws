d052458874062d98711cdb8c3fabd0961d5cee5d
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:55:22 -0400

    lexmodelbuilding: Begin long road to new tags

--- aws/data_source_aws_lex_bot_alias.go
+++ aws/data_source_aws_lex_bot_alias.go
@@ -9,6 +9,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/lexmodelbuildingservice"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 func DataSourceBotAlias() *schema.Resource {
