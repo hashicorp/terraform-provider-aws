8f12878f04ee26c6e046b76bc30a788991528bc9
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 11:26:43 -0400

    ram: Fix more import problems

--- internal/service/ram/find.go
+++ internal/service/ram/find.go
@@ -8,7 +8,6 @@ import (
 	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
-	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 const (
