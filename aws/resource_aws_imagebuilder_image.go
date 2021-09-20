827e8fedd627923baac1118a7b6c529523c15f04
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:34:48 -0400

    imagebuilder: Change from terraform-providers to hashicorp

--- aws/resource_aws_imagebuilder_image.go
+++ aws/resource_aws_imagebuilder_image.go
@@ -12,8 +12,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/keyvaluetags"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/service/imagebuilder/waiter"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/keyvaluetags"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/imagebuilder/waiter"
 )
 
 func resourceAwsImageBuilderImage() *schema.Resource {
