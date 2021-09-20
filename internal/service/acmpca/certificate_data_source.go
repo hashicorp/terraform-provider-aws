e93c9c217d31375c020c84f396fc062da796d9eb
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 11:26:25 -0400

    acmpca: Fix more import problems

--- internal/service/acmpca/certificate_data_source.go
+++ internal/service/acmpca/certificate_data_source.go
@@ -8,7 +8,6 @@ import (
 	"github.com/aws/aws-sdk-go/service/acmpca"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
-	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
--- internal/service/acmpca/wait.go
+++ internal/service/acmpca/wait.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/aws/aws-sdk-go/service/acmpca"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 // waitCertificateAuthorityCreated waits for a CertificateAuthority to return Active or PendingCertificate
