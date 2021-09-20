372d9bb436781e1c92ef5b2c5ab442df81e340ca
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:51:45 -0400

    kms: Migrate to service, global flex

--- aws/resource_aws_kms_ciphertext.go
+++ aws/resource_aws_kms_ciphertext.go
@@ -59,7 +59,7 @@ func resourceAwsKmsCiphertextCreate(d *schema.ResourceData, meta interface{}) er
 	}
 
 	if ec := d.Get("context"); ec != nil {
-		req.EncryptionContext = expandStringMap(ec.(map[string]interface{}))
+		req.EncryptionContext = flex.ExpandStringMap(ec.(map[string]interface{}))
 	}
 
 	log.Printf("[DEBUG] KMS encrypt for key: %s", d.Get("key_id").(string))
