2d3b6cbfb00695018c7405e59d51a6e06667636f
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 12:34:43 -0400

    mediaconvert: Fix more import problems

--- internal/service/mediaconvert/tags_gen.go
+++ internal/service/mediaconvert/tags_gen.go
@@ -4,15 +4,10 @@ package mediaconvert
 
 import (
 	"fmt"
-	"strconv"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/mediaconvert"
-	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
-	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 )
 
 // ListTags lists mediaconvert service tags.
