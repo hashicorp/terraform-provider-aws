71001ba0f7a70080c3b3c06349a7c87342c9c746
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:55:22 -0400

    lambda: Begin long road to new tags

--- aws/resource_aws_lambda_alias.go
+++ aws/resource_aws_lambda_alias.go
@@ -10,6 +10,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/lambda"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 func ResourceAlias() *schema.Resource {
