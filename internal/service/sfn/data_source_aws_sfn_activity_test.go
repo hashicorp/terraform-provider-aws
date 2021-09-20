ea3842253103e352a94b2373c86864b49069b749
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 11:01:55 -0400

    sfn: Move resources and data sources

---
+++ internal/service/sfn/data_source_aws_sfn_activity_test.go
@@ -0,0 +1,69 @@
+package aws
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/service/sfn"
+	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/provider"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
+)
+
+func TestAccAWSStepFunctionsActivityDataSource_basic(t *testing.T) {
+	rName := sdkacctest.RandomWithPrefix("tf-acc-test")
+	resourceName := "aws_sfn_activity.test"
+	dataName := "data.aws_sfn_activity.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:   func() { acctest.PreCheck(t) },
+		ErrorCheck: acctest.ErrorCheck(t, sfn.EndpointsID),
+		Providers:  acctest.Providers,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckAWSStepFunctionsActivityDataSourceConfig_ActivityArn(rName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrPair(resourceName, "id", dataName, "id"),
+					resource.TestCheckResourceAttrPair(resourceName, "creation_date", dataName, "creation_date"),
+					resource.TestCheckResourceAttrPair(resourceName, "name", dataName, "name"),
+				),
+			},
+			{
+				Config: testAccCheckAWSStepFunctionsActivityDataSourceConfig_ActivityName(rName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrPair(resourceName, "id", dataName, "id"),
+					resource.TestCheckResourceAttrPair(resourceName, "creation_date", dataName, "creation_date"),
+					resource.TestCheckResourceAttrPair(resourceName, "name", dataName, "name"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckAWSStepFunctionsActivityDataSourceConfig_ActivityArn(rName string) string {
+	return fmt.Sprintf(`
+resource aws_sfn_activity "test" {
+  name = "%s"
+}
+
+data aws_sfn_activity "test" {
+  arn = aws_sfn_activity.test.id
+}
+`, rName)
+}
+
+func testAccCheckAWSStepFunctionsActivityDataSourceConfig_ActivityName(rName string) string {
+	return fmt.Sprintf(`
+resource aws_sfn_activity "test" {
+  name = "%s"
+}
+
+data aws_sfn_activity "test" {
+  name = aws_sfn_activity.test.name
+}
+`, rName)
+}
---
+++ internal/service/sfn/data_source_aws_sfn_state_machine_test.go
@@ -0,0 +1,85 @@
+package aws
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/service/sfn"
+	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/provider"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
+)
+
+func TestAccDataSourceAwsSfnStateMachine_basic(t *testing.T) {
+	rName := sdkacctest.RandString(5)
+	dataSourceName := "data.aws_sfn_state_machine.test"
+	resourceName := "aws_sfn_state_machine.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:   func() { acctest.PreCheck(t) },
+		ErrorCheck: acctest.ErrorCheck(t, sfn.EndpointsID),
+		Providers:  acctest.Providers,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceAwsSfnStateMachineConfig(rName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrPair(resourceName, "id", dataSourceName, "arn"),
+					resource.TestCheckResourceAttrPair(resourceName, "creation_date", dataSourceName, "creation_date"),
+					resource.TestCheckResourceAttrPair(resourceName, "definition", dataSourceName, "definition"),
+					resource.TestCheckResourceAttrPair(resourceName, "name", dataSourceName, "name"),
+					resource.TestCheckResourceAttrPair(resourceName, "role_arn", dataSourceName, "role_arn"),
+					resource.TestCheckResourceAttrPair(resourceName, "status", dataSourceName, "status"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceAwsSfnStateMachineConfig(rName string) string {
+	return fmt.Sprintf(`
+data "aws_region" "current" {}
+
+resource "aws_iam_role" "iam_for_sfn" {
+  name = "iam_for_sfn_%s"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "states.${data.aws_region.current.name}.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_sfn_state_machine" "test" {
+  name     = "test_sfn_%s"
+  role_arn = aws_iam_role.iam_for_sfn.arn
+
+  definition = <<EOF
+{
+  "StartAt": "HelloWorld",
+  "States": {
+    "HelloWorld": {
+      "Type": "Succeed"
+    }
+  }
+}
+EOF
+}
+
+data "aws_sfn_state_machine" "test" {
+  name = aws_sfn_state_machine.test.name
+}
+`, rName, rName)
+}
