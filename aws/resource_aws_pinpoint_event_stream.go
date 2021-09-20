d14d7f8da3fbb8eca0f60fb589b70d10849a982d
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:55:26 -0400

    pinpoint: Begin long road to new tags

--- aws/resource_aws_pinpoint_event_stream.go
+++ aws/resource_aws_pinpoint_event_stream.go
@@ -12,6 +12,7 @@ import (
 	iamwaiter "github.com/hashicorp/terraform-provider-aws/aws/internal/service/iam/waiter"
 	"github.com/hashicorp/terraform-provider-aws/aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 func ResourceEventStream() *schema.Resource {
