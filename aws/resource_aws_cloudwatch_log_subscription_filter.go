53373c41e385c96a2804ca04a4d5bdb8c6ceea57
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:55:11 -0400

    cloudwatchlogs: Begin long road to new tags

--- aws/resource_aws_cloudwatch_log_subscription_filter.go
+++ aws/resource_aws_cloudwatch_log_subscription_filter.go
@@ -14,6 +14,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/create"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 func ResourceSubscriptionFilter() *schema.Resource {
