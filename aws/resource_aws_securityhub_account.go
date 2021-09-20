b48256259dc8d61a636a282e43c6146afeeb0bdf
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:55:30 -0400

    securityhub: Begin long road to new tags

--- aws/resource_aws_securityhub_account.go
+++ aws/resource_aws_securityhub_account.go
@@ -11,6 +11,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/securityhub/waiter"
 	"github.com/hashicorp/terraform-provider-aws/aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 func ResourceAccount() *schema.Resource {
