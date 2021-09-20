138fd40f6e61274e7b928ffe688de7566924f909
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 11:26:37 -0400

    inspector: Fix more import problems

--- internal/service/inspector/assessment_template.go
+++ internal/service/inspector/assessment_template.go
@@ -8,8 +8,8 @@ import (
 	// "github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/inspector"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/flex"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
