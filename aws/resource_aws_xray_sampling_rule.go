398fb3e96ee7b7889c21e2701cbcceaa96a201ef
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:55:33 -0400

    xray: Begin long road to new tags

--- aws/resource_aws_xray_sampling_rule.go
+++ aws/resource_aws_xray_sampling_rule.go
@@ -8,8 +8,9 @@ import (
 	"github.com/aws/aws-sdk-go/service/xray"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-	"github.com/hashicorp/terraform-provider-aws/aws/internal/keyvaluetags"
+	tftags "github.com/hashicorp/terraform-provider-aws/aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 func ResourceSamplingRule() *schema.Resource {
@@ -23,7 +24,7 @@ func ResourceSamplingRule() *schema.Resource {
 			State: schema.ImportStatePassthrough,
 		},
 
-		CustomizeDiff: SetTagsDiff,
+		CustomizeDiff: verify.SetTagsDiff,
 
 		Schema: map[string]*schema.Schema{
 			"rule_name": {
@@ -93,8 +94,8 @@ func ResourceSamplingRule() *schema.Resource {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
-			"tags":     tagsSchema(),
-			"tags_all": tagsSchemaComputed(),
+			"tags":     tftags.TagsSchema(),
+			"tags_all": tftags.TagsSchemaComputed(),
 		},
 	}
 }
@@ -102,7 +103,7 @@ func ResourceSamplingRule() *schema.Resource {
 func resourceSamplingRuleCreate(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*conns.AWSClient).XRayConn
 	defaultTagsConfig := meta.(*conns.AWSClient).DefaultTagsConfig
-	tags := defaultTagsConfig.MergeTags(keyvaluetags.New(d.Get("tags").(map[string]interface{})))
+	tags := defaultTagsConfig.MergeTags(tftags.New(d.Get("tags").(map[string]interface{})))
 
 	samplingRule := &xray.SamplingRule{
 		RuleName:      aws.String(d.Get("rule_name").(string)),
@@ -169,7 +170,7 @@ func resourceSamplingRuleRead(d *schema.ResourceData, meta interface{}) error {
 	d.Set("attributes", aws.StringValueMap(samplingRule.Attributes))
 	d.Set("arn", arn)
 
-	tags, err := keyvaluetags.XrayListTags(conn, arn)
+	tags, err := tftags.XrayListTags(conn, arn)
 	if err != nil {
 		return fmt.Errorf("error listing tags for Xray Sampling group (%q): %s", d.Id(), err)
 	}
@@ -193,7 +194,7 @@ func resourceSamplingRuleUpdate(d *schema.ResourceData, meta interface{}) error 
 
 	if d.HasChange("tags_all") {
 		o, n := d.GetChange("tags_all")
-		if err := keyvaluetags.XrayUpdateTags(conn, d.Get("arn").(string), o, n); err != nil {
+		if err := tftags.XrayUpdateTags(conn, d.Get("arn").(string), o, n); err != nil {
 			return fmt.Errorf("error updating tags: %w", err)
 		}
 	}
