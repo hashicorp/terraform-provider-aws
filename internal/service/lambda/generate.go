986e30bbe086179af5a5828498891a65fd51014e
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 12:22:39 -0400

    lambda: Create and run new generators

---
+++ internal/service/lambda/generate.go
@@ -0,0 +1,3 @@
+//go:generate go run -tags generate ../../generate/tags/main.go -ListTags=yes -ListTagsOp=ListTags -ListTagsInIDElem=Resource -ServiceTagsMap=yes -TagInIDElem=Resource -UpdateTags=yes
+
+package lambda
