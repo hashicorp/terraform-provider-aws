5cc2ca966e0045c3313cb9eaa50a774f7435e55b
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 11:01:42 -0400

    elasticbeanstalk: Move resources and data sources

---
+++ internal/service/elasticbeanstalk/data_source_aws_elastic_beanstalk_application_test.go
@@ -0,0 +1,79 @@
+package aws
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/service/elasticbeanstalk"
+	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/eks/finder"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/tfresource"
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/provider"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
+)
+
+func TestAccAwsElasticBeanstalkApplicationDataSource_basic(t *testing.T) {
+	rName := fmt.Sprintf("tf-acc-test-%s", sdkacctest.RandString(5))
+	dataSourceResourceName := "data.aws_elastic_beanstalk_application.test"
+	resourceName := "aws_elastic_beanstalk_application.tftest"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { acctest.PreCheck(t) },
+		ErrorCheck:   acctest.ErrorCheck(t, elasticbeanstalk.EndpointsID),
+		Providers:    acctest.Providers,
+		CheckDestroy: testAccCheckAWSEksClusterDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAwsElasticBeanstalkApplicationDataSourceConfig_Basic(rName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(dataSourceResourceName, "arn"),
+					resource.TestCheckResourceAttrPair(resourceName, "name", dataSourceResourceName, "name"),
+					resource.TestCheckResourceAttrPair(resourceName, "description", dataSourceResourceName, "description"),
+					resource.TestCheckResourceAttr(dataSourceResourceName, "appversion_lifecycle.#", "1"),
+					resource.TestCheckResourceAttrPair(resourceName, "appversion_lifecycle.0.service_role", dataSourceResourceName, "appversion_lifecycle.0.service_role"),
+					resource.TestCheckResourceAttrPair(resourceName, "appversion_lifecycle.0.max_age_in_days", dataSourceResourceName, "appversion_lifecycle.0.max_age_in_days"),
+					resource.TestCheckResourceAttrPair(resourceName, "appversion_lifecycle.0.delete_source_from_s3", dataSourceResourceName, "appversion_lifecycle.0.delete_source_from_s3"),
+				),
+			},
+		},
+	})
+}
+
+func testAccAwsElasticBeanstalkApplicationDataSourceConfig_Basic(rName string) string {
+	return fmt.Sprintf(`
+%s
+
+data "aws_elastic_beanstalk_application" "test" {
+  name = aws_elastic_beanstalk_application.tftest.name
+}
+`, testAccBeanstalkAppConfigWithMaxAge(rName))
+}
+
+func testAccCheckAWSEksClusterDestroy(s *terraform.State) error {
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "aws_eks_cluster" {
+			continue
+		}
+
+		conn := acctest.Provider.Meta().(*conns.AWSClient).EKSConn
+
+		_, err := finder.ClusterByName(conn, rs.Primary.ID)
+
+		if tfresource.NotFound(err) {
+			continue
+		}
+
+		if err != nil {
+			return err
+		}
+
+		return fmt.Errorf("EKS Cluster %s still exists", rs.Primary.ID)
+	}
+
+	return nil
+}
---
+++ internal/service/elasticbeanstalk/data_source_aws_elastic_beanstalk_hosted_zone_test.go
@@ -0,0 +1,86 @@
+package aws
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/service/elasticbeanstalk"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/provider"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
+)
+
+func TestAccAWSDataSourceElasticBeanstalkHostedZone_basic(t *testing.T) {
+	dataSourceName := "data.aws_elastic_beanstalk_hosted_zone.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:   func() { acctest.PreCheck(t) },
+		ErrorCheck: acctest.ErrorCheck(t, elasticbeanstalk.EndpointsID),
+		Providers:  acctest.Providers,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckAwsElasticBeanstalkHostedZoneDataSource_currentRegion,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAwsElasticBeanstalkHostedZone(dataSourceName, acctest.Region()),
+				),
+			},
+		},
+	})
+}
+
+func TestAccAWSDataSourceElasticBeanstalkHostedZone_Region(t *testing.T) {
+	dataSourceName := "data.aws_elastic_beanstalk_hosted_zone.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:   func() { acctest.PreCheck(t) },
+		ErrorCheck: acctest.ErrorCheck(t, elasticbeanstalk.EndpointsID),
+		Providers:  acctest.Providers,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckAwsElasticBeanstalkHostedZoneDataSource_byRegion("ap-southeast-2"), //lintignore:AWSAT003 // passes in GovCloud
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAwsElasticBeanstalkHostedZone(dataSourceName, "ap-southeast-2"), //lintignore:AWSAT003 // passes in GovCloud
+				),
+			},
+			{
+				Config: testAccCheckAwsElasticBeanstalkHostedZoneDataSource_byRegion("eu-west-1"), //lintignore:AWSAT003 // passes in GovCloud
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAwsElasticBeanstalkHostedZone(dataSourceName, "eu-west-1"), //lintignore:AWSAT003 // passes in GovCloud
+				),
+			},
+			{
+				Config:      testAccCheckAwsElasticBeanstalkHostedZoneDataSource_byRegion("ss-pluto-1"),
+				ExpectError: regexp.MustCompile("Unsupported region"),
+			},
+		},
+	})
+}
+
+func testAccCheckAwsElasticBeanstalkHostedZone(resourceName string, region string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		expectedValue, ok := elasticBeanstalkHostedZoneIds[region]
+
+		if !ok {
+			return fmt.Errorf("Unsupported Region: %s", region)
+		}
+
+		return resource.TestCheckResourceAttr(resourceName, "id", expectedValue)(s)
+	}
+}
+
+const testAccCheckAwsElasticBeanstalkHostedZoneDataSource_currentRegion = `
+data "aws_elastic_beanstalk_hosted_zone" "test" {}
+`
+
+func testAccCheckAwsElasticBeanstalkHostedZoneDataSource_byRegion(r string) string {
+	return fmt.Sprintf(`
+data "aws_elastic_beanstalk_hosted_zone" "test" {
+  region = "%s"
+}
+`, r)
+}
