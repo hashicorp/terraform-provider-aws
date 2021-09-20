8029fb81eeddea45b42a5d68952da0fd48efd68a
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 11:26:42 -0400

    pinpoint: Fix more import problems

--- internal/service/pinpoint/apns_sandbox_channel.go
+++ internal/service/pinpoint/apns_sandbox_channel.go
@@ -7,10 +7,9 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/pinpoint"
+	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
-	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
-	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 func ResourceAPNSSandboxChannel() *schema.Resource {
