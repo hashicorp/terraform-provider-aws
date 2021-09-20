13353882b0e81a693320ca5f329ad5fa6c9c2d95
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:34:47 -0400

    glacier: Change from terraform-providers to hashicorp

--- aws/resource_aws_glacier_vault.go
+++ aws/resource_aws_glacier_vault.go
@@ -11,7 +11,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/structure"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/keyvaluetags"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/keyvaluetags"
 )
 
 func resourceAwsGlacierVault() *schema.Resource {
