ff08bf0a917626b45e5caaec4d28ecdc36fe8ba8
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 11:22:47 -0400

    timestreamwrite: Fix many import problems

--- internal/service/timestreamwrite/resource_aws_timestreamwrite_database.go
+++ internal/service/timestreamwrite/resource_aws_timestreamwrite_database.go
@@ -1,4 +1,4 @@
-package aws
+package timestreamwrite
 
 import (
 	"context"
@@ -12,7 +12,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-	tftags "github.com/hashicorp/terraform-provider-aws/aws/internal/tags"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
