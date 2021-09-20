5220d350534b87344fbf7609bfd802bf0156fa58
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 12:22:42 -0400

    mediaconvert: Create and run new generators

---
+++ internal/service/mediaconvert/generate.go
@@ -0,0 +1,3 @@
+//go:generate go run -tags generate ../../generate/tags/main.go -ListTags=yes -ListTagsInIDElem=Arn -ListTagsOutTagsElem=ResourceTags.Tags -ServiceTagsMap=yes -TagInIDElem=Arn -UpdateTags=yes
+
+package mediaconvert
