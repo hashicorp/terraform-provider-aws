0b53ff441562c678962d81861aa222d66da29df7
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:55:10 -0400

    cloudtrail: Begin long road to new tags

--- aws/data_source_aws_cloudtrail_service_account.go
+++ aws/data_source_aws_cloudtrail_service_account.go
@@ -7,6 +7,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws/endpoints"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 // See http://docs.aws.amazon.com/awscloudtrail/latest/userguide/cloudtrail-supported-regions.html
