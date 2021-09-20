cafb3aee5c0914ab193e6591300c7ae997bcb67e
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 11:19:05 -0400

    waf: Move services out of aws/internal

--- internal/service/waf/list.go
+++ internal/service/waf/list.go
@@ -1,3 +1,3 @@
 //go:generate go run ../../../generators/listpages/main.go -function=ListByteMatchSets,ListGeoMatchSets,ListIPSets,ListRateBasedRules,ListRegexMatchSets,ListRegexPatternSets,ListRuleGroups,ListRules,ListSizeConstraintSets,ListSqlInjectionMatchSets,ListWebACLs,ListXssMatchSets -paginator=NextMarker github.com/aws/aws-sdk-go/service/waf
 
-package lister
+package waf
