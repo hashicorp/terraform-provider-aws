cdfb0fcb58574aba32d77f38e974d15d5d297371
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:34:46 -0400

    elb: Change from terraform-providers to hashicorp

--- aws/resource_aws_db_proxy_target_test.go
+++ aws/resource_aws_db_proxy_target_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/service/rds/finder"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/rds/finder"
 )
 
 func TestAccAWSDBProxyTarget_Instance(t *testing.T) {
