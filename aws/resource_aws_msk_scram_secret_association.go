81206e0f969d1e7b300f1deb37302ed9a0465d2d
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:34:49 -0400

    kafka: Change from terraform-providers to hashicorp

--- aws/resource_aws_msk_scram_secret_association.go
+++ aws/resource_aws_msk_scram_secret_association.go
@@ -9,7 +9,7 @@ import (
 	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
 	"github.com/hashicorp/go-multierror"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/service/msk/finder"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/msk/finder"
 )
 
 const (
--- aws/resource_aws_msk_scram_secret_association_test.go
+++ aws/resource_aws_msk_scram_secret_association_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/service/msk/finder"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/msk/finder"
 )
 
 func TestAccAwsMskScramSecretAssociation_basic(t *testing.T) {
