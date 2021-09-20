972ed8cdb1f397e6e7132aaa26292d22cbe3f0f1
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 11:01:57 -0400

    wafregional: Move resources and data sources

---
+++ internal/service/wafregional/data_source_aws_wafregional_ipset_test.go
@@ -0,0 +1,59 @@
+package aws
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/service/wafregional"
+	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/provider"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
+)
+
+func TestAccDataSourceAwsWafRegionalIPSet_basic(t *testing.T) {
+	name := sdkacctest.RandomWithPrefix("tf-acc-test")
+	resourceName := "aws_wafregional_ipset.ipset"
+	datasourceName := "data.aws_wafregional_ipset.ipset"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:   func() { acctest.PreCheck(t); acctest.PreCheckPartitionHasService(wafregional.EndpointsID, t) },
+		ErrorCheck: acctest.ErrorCheck(t, wafregional.EndpointsID),
+		Providers:  acctest.Providers,
+		Steps: []resource.TestStep{
+			{
+				Config:      testAccDataSourceAwsWafRegionalIPSet_NonExistent,
+				ExpectError: regexp.MustCompile(`WAF Regional IP Set not found`),
+			},
+			{
+				Config: testAccDataSourceAwsWafRegionalIPSet_Name(name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrPair(datasourceName, "id", resourceName, "id"),
+					resource.TestCheckResourceAttrPair(datasourceName, "name", resourceName, "name"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceAwsWafRegionalIPSet_Name(name string) string {
+	return fmt.Sprintf(`
+resource "aws_wafregional_ipset" "ipset" {
+  name = %[1]q
+}
+
+data "aws_wafregional_ipset" "ipset" {
+  name = aws_wafregional_ipset.ipset.name
+}
+`, name)
+}
+
+const testAccDataSourceAwsWafRegionalIPSet_NonExistent = `
+data "aws_wafregional_ipset" "ipset" {
+  name = "tf-acc-test-does-not-exist"
+}
+`
---
+++ internal/service/wafregional/data_source_aws_wafregional_rate_based_rule_test.go
@@ -0,0 +1,62 @@
+package aws
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/service/wafregional"
+	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/provider"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
+)
+
+func TestAccDataSourceAwsWafRegionalRateBasedRule_basic(t *testing.T) {
+	name := sdkacctest.RandomWithPrefix("tf-acc-test")
+	resourceName := "aws_wafregional_rate_based_rule.wafrule"
+	datasourceName := "data.aws_wafregional_rate_based_rule.wafrule"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:   func() { acctest.PreCheck(t); acctest.PreCheckPartitionHasService(wafregional.EndpointsID, t) },
+		ErrorCheck: acctest.ErrorCheck(t, wafregional.EndpointsID),
+		Providers:  acctest.Providers,
+		Steps: []resource.TestStep{
+			{
+				Config:      testAccDataSourceAwsWafRegionalRateBasedRuleConfig_NonExistent,
+				ExpectError: regexp.MustCompile(`WAF Rate Based Rules not found`),
+			},
+			{
+				Config: testAccDataSourceAwsWafRegionalRateBasedRuleConfig_Name(name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrPair(datasourceName, "id", resourceName, "id"),
+					resource.TestCheckResourceAttrPair(datasourceName, "name", resourceName, "name"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceAwsWafRegionalRateBasedRuleConfig_Name(name string) string {
+	return fmt.Sprintf(`
+resource "aws_wafregional_rate_based_rule" "wafrule" {
+  name        = %[1]q
+  metric_name = "WafruleTest"
+  rate_key    = "IP"
+  rate_limit  = 2000
+}
+
+data "aws_wafregional_rate_based_rule" "wafrule" {
+  name = aws_wafregional_rate_based_rule.wafrule.name
+}
+`, name)
+}
+
+const testAccDataSourceAwsWafRegionalRateBasedRuleConfig_NonExistent = `
+data "aws_wafregional_rate_based_rule" "wafrule" {
+  name = "tf-acc-test-does-not-exist"
+}
+`
