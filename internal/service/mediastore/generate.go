67a9581577e703c8f12c435a350d1c9e1de8b997
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 12:22:42 -0400

    mediastore: Create and run new generators

---
+++ internal/service/mediastore/generate.go
@@ -0,0 +1,3 @@
+//go:generate go run -tags generate ../../generate/tags/main.go -ListTags=yes -ListTagsInIDElem=Resource -ServiceTagsSlice=yes -TagInIDElem=Resource -UpdateTags=yes
+
+package mediastore
