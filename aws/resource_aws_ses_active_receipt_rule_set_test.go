2fb8cdf1fcedbf21d824944e795fed568de568af
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:55:30 -0400

    ses: Begin long road to new tags

--- aws/resource_aws_ses_active_receipt_rule_set_test.go
+++ aws/resource_aws_ses_active_receipt_rule_set_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/provider"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 // Only one SES Receipt RuleSet can be active at a time, so run serially
