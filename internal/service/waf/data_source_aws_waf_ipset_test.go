e4d7527784bd4d7cf06fa64d8038abc0762d9193
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 11:01:57 -0400

    waf: Move resources and data sources

---
+++ internal/service/waf/data_source_aws_waf_ipset_test.go
@@ -0,0 +1,59 @@
+package aws
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/service/waf"
+	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/provider"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
+)
+
+func TestAccDataSourceAwsWafIPSet_basic(t *testing.T) {
+	name := sdkacctest.RandomWithPrefix("tf-acc-test")
+	resourceName := "aws_waf_ipset.ipset"
+	datasourceName := "data.aws_waf_ipset.ipset"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:   func() { acctest.PreCheck(t); acctest.PreCheckPartitionHasService(waf.EndpointsID, t) },
+		ErrorCheck: acctest.ErrorCheck(t, waf.EndpointsID),
+		Providers:  acctest.Providers,
+		Steps: []resource.TestStep{
+			{
+				Config:      testAccDataSourceAwsWafIPSet_NonExistent,
+				ExpectError: regexp.MustCompile(`WAF IP Set not found`),
+			},
+			{
+				Config: testAccDataSourceAwsWafIPSet_Name(name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrPair(datasourceName, "id", resourceName, "id"),
+					resource.TestCheckResourceAttrPair(datasourceName, "name", resourceName, "name"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceAwsWafIPSet_Name(name string) string {
+	return fmt.Sprintf(`
+resource "aws_waf_ipset" "ipset" {
+  name = %[1]q
+}
+
+data "aws_waf_ipset" "ipset" {
+  name = aws_waf_ipset.ipset.name
+}
+`, name)
+}
+
+const testAccDataSourceAwsWafIPSet_NonExistent = `
+data "aws_waf_ipset" "ipset" {
+  name = "tf-acc-test-does-not-exist"
+}
+`
---
+++ internal/service/waf/data_source_aws_waf_rate_based_rule_test.go
@@ -0,0 +1,62 @@
+package aws
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/service/waf"
+	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/provider"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
+)
+
+func TestAccDataSourceAwsWafRateBasedRule_basic(t *testing.T) {
+	name := sdkacctest.RandomWithPrefix("tf-acc-test")
+	resourceName := "aws_waf_rate_based_rule.wafrule"
+	datasourceName := "data.aws_waf_rate_based_rule.wafrule"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:   func() { acctest.PreCheck(t); acctest.PreCheckPartitionHasService(waf.EndpointsID, t) },
+		ErrorCheck: acctest.ErrorCheck(t, waf.EndpointsID),
+		Providers:  acctest.Providers,
+		Steps: []resource.TestStep{
+			{
+				Config:      testAccDataSourceAwsWafRateBasedRuleConfig_NonExistent,
+				ExpectError: regexp.MustCompile(`WAF Rate Based Rules not found`),
+			},
+			{
+				Config: testAccDataSourceAwsWafRateBasedRuleConfig_Name(name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrPair(datasourceName, "id", resourceName, "id"),
+					resource.TestCheckResourceAttrPair(datasourceName, "name", resourceName, "name"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceAwsWafRateBasedRuleConfig_Name(name string) string {
+	return fmt.Sprintf(`
+resource "aws_waf_rate_based_rule" "wafrule" {
+  name        = %[1]q
+  metric_name = "WafruleTest"
+  rate_key    = "IP"
+  rate_limit  = 2000
+}
+
+data "aws_waf_rate_based_rule" "wafrule" {
+  name = aws_waf_rate_based_rule.wafrule.name
+}
+`, name)
+}
+
+const testAccDataSourceAwsWafRateBasedRuleConfig_NonExistent = `
+data "aws_waf_rate_based_rule" "wafrule" {
+  name = "tf-acc-test-does-not-exist"
+}
+`
