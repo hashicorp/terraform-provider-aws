ac6f3f35fd0d190675e139f585561a99c9198c95
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 11:00:01 -0400

    s3outposts: Create verify package

--- aws/internal/service/s3outposts/waiter/status.go
+++ aws/internal/service/s3outposts/waiter/status.go
@@ -5,6 +5,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3outposts"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/s3outposts/finder"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 const (
