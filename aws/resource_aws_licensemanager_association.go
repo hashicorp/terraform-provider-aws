6df72e70389c5f91815b7ec4b27db3e80ca34ef0
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:59:52 -0400

    licensemanager: Create verify package

--- aws/resource_aws_licensemanager_association.go
+++ aws/resource_aws_licensemanager_association.go
@@ -10,6 +10,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 func ResourceAssociation() *schema.Resource {
@@ -26,13 +27,13 @@ func ResourceAssociation() *schema.Resource {
 				Type:         schema.TypeString,
 				Required:     true,
 				ForceNew:     true,
-				ValidateFunc: validateArn,
+				ValidateFunc: verify.ValidARN,
 			},
 			"license_configuration_arn": {
 				Type:         schema.TypeString,
 				Required:     true,
 				ForceNew:     true,
-				ValidateFunc: validateArn,
+				ValidateFunc: verify.ValidARN,
 			},
 		},
 	}
