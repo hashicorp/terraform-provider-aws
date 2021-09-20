2169c3e122603c98960268668e0482abe932b1dc
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:34:36 -0400

    apigatewayv2: Change from terraform-providers to hashicorp

--- aws/data_source_aws_apigatewayv2_apis.go
+++ aws/data_source_aws_apigatewayv2_apis.go
@@ -6,8 +6,8 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/apigatewayv2"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/keyvaluetags"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/service/apigatewayv2/finder"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/keyvaluetags"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/apigatewayv2/finder"
 )
 
 func dataSourceAwsApiGatewayV2Apis() *schema.Resource {
