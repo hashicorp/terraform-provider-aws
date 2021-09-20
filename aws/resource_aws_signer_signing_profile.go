89945ca08ba5642ab092496d67a3689b93bc07c7
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:34:59 -0400

    signer: Change from terraform-providers to hashicorp

--- aws/resource_aws_signer_signing_profile.go
+++ aws/resource_aws_signer_signing_profile.go
@@ -12,8 +12,8 @@ import (
 	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/keyvaluetags"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/naming"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/keyvaluetags"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/naming"
 )
 
 func resourceAwsSignerSigningProfile() *schema.Resource {
--- aws/resource_aws_signer_signing_profile_permission.go
+++ aws/resource_aws_signer_signing_profile_permission.go
@@ -12,8 +12,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/naming"
-	iamwaiter "github.com/terraform-providers/terraform-provider-aws/aws/internal/service/iam/waiter"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/naming"
+	iamwaiter "github.com/hashicorp/terraform-provider-aws/aws/internal/service/iam/waiter"
 )
 
 func resourceAwsSignerSigningProfilePermission() *schema.Resource {
