ee0a634fe5a5399c11b16265de0d6413d791329c
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:34:51 -0400

    lambda: Change from terraform-providers to hashicorp

--- aws/internal/service/lambda/waiter/status.go
+++ aws/internal/service/lambda/waiter/status.go
@@ -4,8 +4,8 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/lambda"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/service/lambda/finder"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/tfresource"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/lambda/finder"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/tfresource"
 )
 
 const (
