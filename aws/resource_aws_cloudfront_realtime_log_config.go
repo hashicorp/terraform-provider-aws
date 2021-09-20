661ce6fc2517bd88a984fbcd35bdd7e8ca72b6b5
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:34:38 -0400

    cloudfront: Change from terraform-providers to hashicorp

--- aws/resource_aws_cloudfront_realtime_log_config.go
+++ aws/resource_aws_cloudfront_realtime_log_config.go
@@ -9,7 +9,7 @@ import (
 	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/service/cloudfront/finder"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/cloudfront/finder"
 )
 
 func resourceAwsCloudFrontRealtimeLogConfig() *schema.Resource {
--- aws/resource_aws_cloudfront_realtime_log_config_test.go
+++ aws/resource_aws_cloudfront_realtime_log_config_test.go
@@ -12,7 +12,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/service/cloudfront/finder"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/cloudfront/finder"
 )
 
 func init() {
