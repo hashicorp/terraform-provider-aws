34b7e14bda49fa585f475d658e657faaa80b566d
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:55:26 -0400

    pricing: Begin long road to new tags

--- aws/data_source_aws_pricing_product.go
+++ aws/data_source_aws_pricing_product.go
@@ -10,6 +10,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/create"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 func DataSourceProduct() *schema.Resource {
