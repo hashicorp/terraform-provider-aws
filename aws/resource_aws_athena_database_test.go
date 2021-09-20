092195e22540c5ad5e6ad0356314d16c42c8b019
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:55:08 -0400

    athena: Begin long road to new tags

--- aws/resource_aws_athena_database_test.go
+++ aws/resource_aws_athena_database_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/provider"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 func TestAccAWSAthenaDatabase_basic(t *testing.T) {
