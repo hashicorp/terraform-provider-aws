35b490fa877f924a15330b331016b1fae34ff577
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:34:55 -0400

    pinpoint: Change from terraform-providers to hashicorp

--- aws/resource_aws_pinpoint_event_stream.go
+++ aws/resource_aws_pinpoint_event_stream.go
@@ -9,8 +9,8 @@ import (
 	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	iamwaiter "github.com/terraform-providers/terraform-provider-aws/aws/internal/service/iam/waiter"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/tfresource"
+	iamwaiter "github.com/hashicorp/terraform-provider-aws/aws/internal/service/iam/waiter"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/tfresource"
 )
 
 func resourceAwsPinpointEventStream() *schema.Resource {
