4a96c712084dbba06ed505e2bbd4137ddfa353e5
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 11:01:38 -0400

    cloudtrail: Move resources and data sources

---
+++ internal/service/cloudtrail/data_source_aws_cloudtrail_service_account_test.go
@@ -0,0 +1,66 @@
+package aws
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/provider"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
+)
+
+func TestAccAWSCloudTrailServiceAccount_basic(t *testing.T) {
+	expectedAccountID := cloudTrailServiceAccountPerRegionMap[acctest.Region()]
+
+	dataSourceName := "data.aws_cloudtrail_service_account.main"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:   func() { acctest.PreCheck(t) },
+		ErrorCheck: acctest.ErrorCheck(t),
+		Providers:  acctest.Providers,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckAwsCloudTrailServiceAccountConfig,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(dataSourceName, "id", expectedAccountID),
+					acctest.CheckResourceAttrGlobalARNAccountID(dataSourceName, "arn", expectedAccountID, "iam", "root"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccAWSCloudTrailServiceAccount_Region(t *testing.T) {
+	expectedAccountID := cloudTrailServiceAccountPerRegionMap[acctest.Region()]
+
+	dataSourceName := "data.aws_cloudtrail_service_account.regional"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:   func() { acctest.PreCheck(t) },
+		ErrorCheck: acctest.ErrorCheck(t),
+		Providers:  acctest.Providers,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckAwsCloudTrailServiceAccountConfigRegion,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(dataSourceName, "id", expectedAccountID),
+					acctest.CheckResourceAttrGlobalARNAccountID(dataSourceName, "arn", expectedAccountID, "iam", "root"),
+				),
+			},
+		},
+	})
+}
+
+const testAccCheckAwsCloudTrailServiceAccountConfig = `
+data "aws_cloudtrail_service_account" "main" {}
+`
+
+const testAccCheckAwsCloudTrailServiceAccountConfigRegion = `
+data "aws_region" "current" {}
+
+data "aws_cloudtrail_service_account" "regional" {
+  region = data.aws_region.current.name
+}
+`
