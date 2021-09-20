5efc054325edfce33dd2b02b79fd41bd1590b580
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 11:01:52 -0400

    redshift: Move resources and data sources

---
+++ internal/service/redshift/data_source_aws_redshift_service_account_test.go
@@ -0,0 +1,67 @@
+package aws
+
+import (
+	"testing"
+
+	"github.com/aws/aws-sdk-go/service/redshift"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/provider"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
+)
+
+func TestAccAWSRedshiftServiceAccount_basic(t *testing.T) {
+	expectedAccountID := redshiftServiceAccountPerRegionMap[acctest.Region()]
+
+	dataSourceName := "data.aws_redshift_service_account.main"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:   func() { acctest.PreCheck(t) },
+		ErrorCheck: acctest.ErrorCheck(t, redshift.EndpointsID),
+		Providers:  acctest.Providers,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckAwsRedshiftServiceAccountConfig,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(dataSourceName, "id", expectedAccountID),
+					acctest.CheckResourceAttrGlobalARNAccountID(dataSourceName, "arn", expectedAccountID, "iam", "user/logs"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccAWSRedshiftServiceAccount_Region(t *testing.T) {
+	expectedAccountID := redshiftServiceAccountPerRegionMap[acctest.Region()]
+
+	dataSourceName := "data.aws_redshift_service_account.regional"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:   func() { acctest.PreCheck(t) },
+		ErrorCheck: acctest.ErrorCheck(t, redshift.EndpointsID),
+		Providers:  acctest.Providers,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckAwsRedshiftServiceAccountExplicitRegionConfig,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(dataSourceName, "id", expectedAccountID),
+					acctest.CheckResourceAttrGlobalARNAccountID(dataSourceName, "arn", expectedAccountID, "iam", "user/logs"),
+				),
+			},
+		},
+	})
+}
+
+const testAccCheckAwsRedshiftServiceAccountConfig = `
+data "aws_redshift_service_account" "main" {}
+`
+
+const testAccCheckAwsRedshiftServiceAccountExplicitRegionConfig = `
+data "aws_region" "current" {}
+
+data "aws_redshift_service_account" "regional" {
+  region = data.aws_region.current.name
+}
+`
