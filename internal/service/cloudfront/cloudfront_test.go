6c26a7458de06253ec543368f5d3d27544953497
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 11:02:19 -0400

    cloudfront: Move odds and ends out of aws

---
+++ internal/service/cloudfront/cloudfront_test.go
@@ -0,0 +1,18 @@
+package aws
+
+import "github.com/aws/aws-sdk-go/aws/endpoints"
+
+// testAccCloudfrontRegionProviderConfig is the Terraform provider configuration for CloudFront region testing
+//
+// Testing CloudFront assumes no other provider configurations
+// are necessary and overwrites the "aws" provider configuration.
+func testAccCloudfrontRegionProviderConfig() string {
+	switch acctest.Partition() {
+	case endpoints.AwsPartitionID:
+		return acctest.ConfigRegionalProvider(endpoints.UsEast1RegionID)
+	case endpoints.AwsCnPartitionID:
+		return acctest.ConfigRegionalProvider(endpoints.CnNorthwest1RegionID)
+	default:
+		return acctest.ConfigRegionalProvider(acctest.Region())
+	}
+}
