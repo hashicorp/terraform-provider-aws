e4c8db1f3eee873248dac9e25c85e7095ead48bc
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 11:22:30 -0400

    codepipeline: Fix many import problems

--- internal/service/codepipeline/resource_aws_codepipeline.go
+++ internal/service/codepipeline/resource_aws_codepipeline.go
@@ -1,4 +1,4 @@
-package aws
+package codepipeline
 
 import (
 	"crypto/sha256"
@@ -15,11 +15,11 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-	tftags "github.com/hashicorp/terraform-provider-aws/aws/internal/tags"
-	iamwaiter "github.com/hashicorp/terraform-provider-aws/aws/internal/service/iam/waiter"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
+	tfiam "github.com/hashicorp/terraform-provider-aws/internal/service/iam"
 )
 
 const (
@@ -195,7 +195,7 @@ func resourceCreate(d *schema.ResourceData, meta interface{}) error {
 	}
 
 	var resp *codepipeline.CreatePipelineOutput
-	err = resource.Retry(iamwaiter.PropagationTimeout, func() *resource.RetryError {
+	err = resource.Retry(tfiam.PropagationTimeout, func() *resource.RetryError {
 		var err error
 
 		resp, err = conn.CreatePipeline(params)
