b538f632e5484449fd005be4bea82b875d9205f8
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:34:38 -0400

    batch: Change from terraform-providers to hashicorp

--- aws/data_source_aws_batch_compute_environment.go
+++ aws/data_source_aws_batch_compute_environment.go
@@ -7,7 +7,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/batch"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/keyvaluetags"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/keyvaluetags"
 )
 
 func dataSourceAwsBatchComputeEnvironment() *schema.Resource {
--- aws/internal/service/batch/waiter/status.go
+++ aws/internal/service/batch/waiter/status.go
@@ -4,8 +4,8 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/batch"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/service/batch/finder"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/tfresource"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/batch/finder"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/tfresource"
 )
 
 func ComputeEnvironmentStatus(conn *batch.Batch, name string) resource.StateRefreshFunc {
