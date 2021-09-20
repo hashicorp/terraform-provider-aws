5666480235327ccd587e8c0d48e78c62ff750fd8
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:55:25 -0400

    opsworks: Begin long road to new tags

--- aws/resource_aws_opsworks_permission.go
+++ aws/resource_aws_opsworks_permission.go
@@ -11,6 +11,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	iamwaiter "github.com/hashicorp/terraform-provider-aws/aws/internal/service/iam/waiter"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 func ResourcePermission() *schema.Resource {
