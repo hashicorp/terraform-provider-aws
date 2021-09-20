16eb9caddbacd40c9f33a7a846e4b850dae2fae0
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 12:34:54 -0400

    swf: Fix more import problems

--- internal/service/swf/tags_gen.go
+++ internal/service/swf/tags_gen.go
@@ -4,15 +4,10 @@ package swf
 
 import (
 	"fmt"
-	"strconv"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/swf"
-	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
-	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 )
 
 // ListTags lists swf service tags.
