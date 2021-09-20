f77acbdc42ee94b3ee5d91194735db89c60c47bd
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 11:19:05 -0400

    wafv2: Move services out of aws/internal

--- internal/service/wafv2/list.go
+++ internal/service/wafv2/list.go
@@ -1,3 +1,3 @@
 //go:generate go run ../../../generators/listpages/main.go -function=ListIPSets,ListRegexPatternSets,ListRuleGroups,ListWebACLs -paginator=NextMarker github.com/aws/aws-sdk-go/service/wafv2
 
-package lister
+package wafv2
