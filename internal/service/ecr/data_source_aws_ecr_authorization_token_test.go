e10b177fbaa31c9f9c736c0d01e218d3458a0314
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 11:01:42 -0400

    ecr: Move resources and data sources

---
+++ internal/service/ecr/data_source_aws_ecr_authorization_token_test.go
@@ -0,0 +1,68 @@
+package aws
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/service/ecr"
+	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/provider"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
+)
+
+func TestAccAWSEcrAuthorizationTokenDataSource_basic(t *testing.T) {
+	rName := sdkacctest.RandomWithPrefix("tf-acc-test")
+	dataSourceName := "data.aws_ecr_authorization_token.repo"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:   func() { acctest.PreCheck(t) },
+		ErrorCheck: acctest.ErrorCheck(t, ecr.EndpointsID),
+		Providers:  acctest.Providers,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckAwsEcrAuthorizationTokenDataSourceBasicConfig,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(dataSourceName, "authorization_token"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "proxy_endpoint"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "expires_at"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "user_name"),
+					resource.TestMatchResourceAttr(dataSourceName, "user_name", regexp.MustCompile(`AWS`)),
+					resource.TestCheckResourceAttrSet(dataSourceName, "password"),
+				),
+			},
+			{
+				Config: testAccCheckAwsEcrAuthorizationTokenDataSourceRepositoryConfig(rName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrPair(dataSourceName, "registry_id", "aws_ecr_repository.repo", "registry_id"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "authorization_token"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "proxy_endpoint"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "expires_at"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "user_name"),
+					resource.TestMatchResourceAttr(dataSourceName, "user_name", regexp.MustCompile(`AWS`)),
+					resource.TestCheckResourceAttrSet(dataSourceName, "password"),
+				),
+			},
+		},
+	})
+}
+
+var testAccCheckAwsEcrAuthorizationTokenDataSourceBasicConfig = `
+data "aws_ecr_authorization_token" "repo" {}
+`
+
+func testAccCheckAwsEcrAuthorizationTokenDataSourceRepositoryConfig(rName string) string {
+	return fmt.Sprintf(`
+resource "aws_ecr_repository" "repo" {
+  name = %q
+}
+
+data "aws_ecr_authorization_token" "repo" {
+  registry_id = aws_ecr_repository.repo.registry_id
+}
+`, rName)
+}
---
+++ internal/service/ecr/resource_aws_ecr_repository_policy_test.go
@@ -0,0 +1,272 @@
+package aws
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ecr"
+	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/provider"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
+)
+
+func TestAccAWSEcrRepositoryPolicy_basic(t *testing.T) {
+	rName := sdkacctest.RandomWithPrefix("tf-acc-test")
+	resourceName := "aws_ecr_repository_policy.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { acctest.PreCheck(t) },
+		ErrorCheck:   acctest.ErrorCheck(t, ecr.EndpointsID),
+		Providers:    acctest.Providers,
+		CheckDestroy: testAccCheckAWSEcrRepositoryPolicyDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAWSEcrRepositoryPolicyConfig(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAWSEcrRepositoryPolicyExists(resourceName),
+					resource.TestCheckResourceAttrPair(resourceName, "repository", "aws_ecr_repository.test", "name"),
+					resource.TestMatchResourceAttr(resourceName, "policy", regexp.MustCompile(rName)),
+					acctest.CheckResourceAttrAccountID(resourceName, "registry_id"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccAWSEcrRepositoryPolicyConfigUpdated(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAWSEcrRepositoryPolicyExists(resourceName),
+					resource.TestCheckResourceAttrPair(resourceName, "repository", "aws_ecr_repository.test", "name"),
+					resource.TestMatchResourceAttr(resourceName, "policy", regexp.MustCompile(rName)),
+					resource.TestMatchResourceAttr(resourceName, "policy", regexp.MustCompile("ecr:DescribeImages")),
+					acctest.CheckResourceAttrAccountID(resourceName, "registry_id"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccAWSEcrRepositoryPolicy_iam(t *testing.T) {
+	rName := sdkacctest.RandomWithPrefix("tf-acc-test")
+	resourceName := "aws_ecr_repository_policy.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { acctest.PreCheck(t) },
+		ErrorCheck:   acctest.ErrorCheck(t, ecr.EndpointsID),
+		Providers:    acctest.Providers,
+		CheckDestroy: testAccCheckAWSEcrRepositoryPolicyDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAWSEcrRepositoryPolicyWithIAMRoleConfig(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAWSEcrRepositoryPolicyExists(resourceName),
+					resource.TestMatchResourceAttr(resourceName, "policy", regexp.MustCompile(rName)),
+					resource.TestMatchResourceAttr(resourceName, "policy", regexp.MustCompile("iam")),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccAWSEcrRepositoryPolicy_disappears(t *testing.T) {
+	rName := sdkacctest.RandomWithPrefix("tf-acc-test")
+	resourceName := "aws_ecr_repository_policy.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { acctest.PreCheck(t) },
+		ErrorCheck:   acctest.ErrorCheck(t, ecr.EndpointsID),
+		Providers:    acctest.Providers,
+		CheckDestroy: testAccCheckAWSEcrRepositoryPolicyDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAWSEcrRepositoryPolicyConfig(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAWSEcrRepositoryPolicyExists(resourceName),
+					acctest.CheckResourceDisappears(acctest.Provider, ResourceRepositoryPolicy(), resourceName),
+				),
+				ExpectNonEmptyPlan: true,
+			},
+		},
+	})
+}
+
+func TestAccAWSEcrRepositoryPolicy_disappears_repository(t *testing.T) {
+	rName := sdkacctest.RandomWithPrefix("tf-acc-test")
+	resourceName := "aws_ecr_repository_policy.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { acctest.PreCheck(t) },
+		ErrorCheck:   acctest.ErrorCheck(t, ecr.EndpointsID),
+		Providers:    acctest.Providers,
+		CheckDestroy: testAccCheckAWSEcrRepositoryPolicyDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAWSEcrRepositoryPolicyConfig(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAWSEcrRepositoryPolicyExists(resourceName),
+					acctest.CheckResourceDisappears(acctest.Provider, ResourceRepository(), resourceName),
+				),
+				ExpectNonEmptyPlan: true,
+			},
+		},
+	})
+}
+
+func testAccCheckAWSEcrRepositoryPolicyDestroy(s *terraform.State) error {
+	conn := acctest.Provider.Meta().(*conns.AWSClient).ECRConn
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "aws_ecr_repository_policy" {
+			continue
+		}
+
+		_, err := conn.GetRepositoryPolicy(&ecr.GetRepositoryPolicyInput{
+			RegistryId:     aws.String(rs.Primary.Attributes["registry_id"]),
+			RepositoryName: aws.String(rs.Primary.ID),
+		})
+		if err != nil {
+			if tfawserr.ErrMessageContains(err, ecr.ErrCodeRepositoryNotFoundException, "") ||
+				tfawserr.ErrMessageContains(err, ecr.ErrCodeRepositoryPolicyNotFoundException, "") {
+				return nil
+			}
+			return err
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckAWSEcrRepositoryPolicyExists(name string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		_, ok := s.RootModule().Resources[name]
+		if !ok {
+			return fmt.Errorf("Not found: %s", name)
+		}
+
+		return nil
+	}
+}
+
+func testAccAWSEcrRepositoryPolicyConfig(rName string) string {
+	return fmt.Sprintf(`
+resource "aws_ecr_repository" "test" {
+  name = %[1]q
+}
+
+resource "aws_ecr_repository_policy" "test" {
+  repository = aws_ecr_repository.test.name
+
+  policy = <<EOF
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Sid": "%[1]s",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "ecr:ListImages"
+            ]
+        }
+    ]
+}
+EOF
+}
+`, rName)
+}
+
+func testAccAWSEcrRepositoryPolicyConfigUpdated(rName string) string {
+	return fmt.Sprintf(`
+resource "aws_ecr_repository" "test" {
+  name = %[1]q
+}
+
+resource "aws_ecr_repository_policy" "test" {
+  repository = aws_ecr_repository.test.name
+
+  policy = <<EOF
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Sid": "%[1]s",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "ecr:ListImages",
+                "ecr:DescribeImages"
+            ]
+        }
+    ]
+}
+EOF
+}
+`, rName)
+}
+
+// testAccAWSEcrRepositoryPolicyWithIAMRoleConfig creates a new IAM Role and tries
+// to use it's ARN in an ECR Repository Policy. IAM changes need some time to
+// be propagated to other services - like ECR. So the following code should
+// exercise our retry logic, since we try to use the new resource instantly.
+func testAccAWSEcrRepositoryPolicyWithIAMRoleConfig(rName string) string {
+	return fmt.Sprintf(`
+resource "aws_ecr_repository" "test" {
+  name = %[1]q
+}
+
+resource "aws_iam_role" "test" {
+  name = %[1]q
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      }
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_ecr_repository_policy" "test" {
+  repository = aws_ecr_repository.test.name
+
+  policy = <<EOF
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Sid": "%[1]s",
+            "Effect": "Allow",
+            "Principal": {
+              "AWS": "${aws_iam_role.test.arn}"
+            },
+            "Action": [
+                "ecr:ListImages"
+            ]
+        }
+    ]
+}
+EOF
+}
+`, rName)
+}
