a38e77d63cb1e697b7272343fb5c6485cdf11dc9
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:34:48 -0400

    guardduty: Change from terraform-providers to hashicorp

--- aws/resource_aws_guardduty_filter.go
+++ aws/resource_aws_guardduty_filter.go
@@ -13,7 +13,7 @@ import (
 	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/keyvaluetags"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/keyvaluetags"
 )
 
 func resourceAwsGuardDutyFilter() *schema.Resource {
--- aws/resource_aws_guardduty_publishing_destination.go
+++ aws/resource_aws_guardduty_publishing_destination.go
@@ -9,7 +9,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/guardduty"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/service/guardduty/waiter"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/guardduty/waiter"
 )
 
 func resourceAwsGuardDutyPublishingDestination() *schema.Resource {
