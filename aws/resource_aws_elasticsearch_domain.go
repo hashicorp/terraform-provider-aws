781a5e7e5a287fa7f82c293c45f3d8d65bbbd701
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:34:45 -0400

    elasticsearch: Change from terraform-providers to hashicorp

--- aws/resource_aws_elasticsearch_domain.go
+++ aws/resource_aws_elasticsearch_domain.go
@@ -16,8 +16,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/structure"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/keyvaluetags"
-	iamwaiter "github.com/terraform-providers/terraform-provider-aws/aws/internal/service/iam/waiter"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/keyvaluetags"
+	iamwaiter "github.com/hashicorp/terraform-provider-aws/aws/internal/service/iam/waiter"
 )
 
 func resourceAwsElasticSearchDomain() *schema.Resource {
