56e06d9651d04850d2cc218e1dd4b6a50511de57
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:55:09 -0400

    chime: Begin long road to new tags

--- aws/resource_aws_chime_voice_connector_logging.go
+++ aws/resource_aws_chime_voice_connector_logging.go
@@ -9,6 +9,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 func ResourceVoiceConnectorLogging() *schema.Resource {
