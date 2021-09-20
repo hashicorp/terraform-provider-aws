c6acb4619c1e2f049f7acf10f3d4db35e95651a1
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:34:36 -0400

    appconfig: Change from terraform-providers to hashicorp

--- aws/resource_aws_appconfig_deployment_strategy.go
+++ aws/resource_aws_appconfig_deployment_strategy.go
@@ -10,7 +10,7 @@ import (
 	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/keyvaluetags"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/keyvaluetags"
 )
 
 func resourceAwsAppconfigDeploymentStrategy() *schema.Resource {
