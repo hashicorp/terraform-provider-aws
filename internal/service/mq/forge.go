aeb1edee0434daba421094d5175dfdefd78ac1a3
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:51:48 -0400

    mq: Migrate to service, global flex

---
+++ internal/service/mq/forge.go
@@ -0,0 +1,26 @@
+package mq
+
+import (
+	"regexp"
+
+	"github.com/beevik/etree"
+)
+
+// cannonicalXML reads XML in a string and re-writes it canonically, used for
+// comparing XML for logical equivalency
+func canonicalXML(s string) (string, error) {
+	doc := etree.NewDocument()
+	doc.WriteSettings.CanonicalEndTags = true
+	if err := doc.ReadFromString(s); err != nil {
+		return "", err
+	}
+
+	rawString, err := doc.WriteToString()
+	if err != nil {
+		return "", err
+	}
+
+	re := regexp.MustCompile(`\s`)
+	results := re.ReplaceAllString(rawString, "")
+	return results, nil
+}
