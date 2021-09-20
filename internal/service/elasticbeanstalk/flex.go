0f01f4edbb0ec8c996dea45eef8e3beb4e086176
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:59:45 -0400

    elasticbeanstalk: Create verify package

--- internal/service/elasticbeanstalk/flex.go
+++ internal/service/elasticbeanstalk/flex.go
@@ -2,6 +2,7 @@ package elasticbeanstalk
 
 import (
 	"github.com/aws/aws-sdk-go/service/elasticbeanstalk"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 func flattenAsg(list []*elasticbeanstalk.AutoScalingGroup) []string {
