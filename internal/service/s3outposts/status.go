485964417222be89d25f7e920f78eaad5fb07ebb
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 11:26:45 -0400

    s3outposts: Fix more import problems

--- internal/service/s3outposts/status.go
+++ internal/service/s3outposts/status.go
@@ -4,7 +4,6 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3outposts"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 const (
