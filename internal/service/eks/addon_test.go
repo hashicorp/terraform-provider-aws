76ecef6ed1da1182e57eccbbde010aa890db4bb1
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 12:34:31 -0400

    eks: Fix more import problems

--- internal/service/eks/addon_test.go
+++ internal/service/eks/addon_test.go
@@ -17,7 +17,6 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	tfeks "github.com/hashicorp/terraform-provider-aws/internal/service/eks"
-	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 )
 
