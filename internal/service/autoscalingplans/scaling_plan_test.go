33402375dae95684f34a74ce6697bc65a4bbd431
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 12:32:32 -0400

    autoscalingplans: Rename tests, fix misc naming

--- internal/service/autoscalingplans/scaling_plan_test.go
+++ internal/service/autoscalingplans/scaling_plan_test.go
@@ -73,7 +73,7 @@ func testSweepAutoScalingPlansScalingPlans(region string) error {
 	return sweeperErrs.ErrorOrNil()
 }
 
-func TestAccAwsAutoScalingPlansScalingPlan_basicDynamicScaling(t *testing.T) {
+func TestAccAutoScalingPlansScalingPlan_basicDynamicScaling(t *testing.T) {
 	var scalingPlan autoscalingplans.ScalingPlan
 	resourceName := "aws_autoscalingplans_scaling_plan.test"
 	rName := sdkacctest.RandomWithPrefix("tf-acc-test")
@@ -122,7 +122,7 @@ func TestAccAwsAutoScalingPlansScalingPlan_basicDynamicScaling(t *testing.T) {
 	})
 }
 
-func TestAccAwsAutoScalingPlansScalingPlan_basicPredictiveScaling(t *testing.T) {
+func TestAccAutoScalingPlansScalingPlan_basicPredictiveScaling(t *testing.T) {
 	var scalingPlan autoscalingplans.ScalingPlan
 	resourceName := "aws_autoscalingplans_scaling_plan.test"
 	rName := sdkacctest.RandomWithPrefix("tf-acc-test")
@@ -177,7 +177,7 @@ func TestAccAwsAutoScalingPlansScalingPlan_basicPredictiveScaling(t *testing.T) 
 	})
 }
 
-func TestAccAwsAutoScalingPlansScalingPlan_basicUpdate(t *testing.T) {
+func TestAccAutoScalingPlansScalingPlan_basicUpdate(t *testing.T) {
 	var scalingPlan autoscalingplans.ScalingPlan
 	resourceName := "aws_autoscalingplans_scaling_plan.test"
 	rName := sdkacctest.RandomWithPrefix("tf-acc-test")
@@ -259,7 +259,7 @@ func TestAccAwsAutoScalingPlansScalingPlan_basicUpdate(t *testing.T) {
 	})
 }
 
-func TestAccAwsAutoScalingPlansScalingPlan_disappears(t *testing.T) {
+func TestAccAutoScalingPlansScalingPlan_disappears(t *testing.T) {
 	var scalingPlan autoscalingplans.ScalingPlan
 	resourceName := "aws_autoscalingplans_scaling_plan.test"
 	rName := sdkacctest.RandomWithPrefix("tf-acc-test")
