bbafc7659a0531f98069b4702b320abccf6722a8
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 11:22:44 -0400

    route53recoveryreadiness: Fix many import problems

--- internal/service/route53recoveryreadiness/resource_aws_route53recoveryreadiness_readiness_check.go
+++ internal/service/route53recoveryreadiness/resource_aws_route53recoveryreadiness_readiness_check.go
@@ -1,4 +1,4 @@
-package aws
+package route53recoveryreadiness
 
 import (
 	"fmt"
@@ -10,7 +10,7 @@ import (
 	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	tftags "github.com/hashicorp/terraform-provider-aws/aws/internal/tags"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
