f1226aa48a28344b7df6ae43b6bd174d89dd09b3
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 12:34:30 -0400

    dynamodb: Fix more import problems

--- internal/service/dynamodb/tags_gen.go
+++ internal/service/dynamodb/tags_gen.go
@@ -4,13 +4,9 @@ package dynamodb
 
 import (
 	"fmt"
-	"strconv"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 )
