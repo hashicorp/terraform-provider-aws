96e6db8ea193c799d288acbb72e4084092736a82
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:34:56 -0400

    ram: Change from terraform-providers to hashicorp

--- aws/resource_aws_ram_principal_association.go
+++ aws/resource_aws_ram_principal_association.go
@@ -12,8 +12,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/service/ram/finder"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/service/ram/waiter"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/ram/finder"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/ram/waiter"
 )
 
 func resourceAwsRamPrincipalAssociation() *schema.Resource {
--- aws/resource_aws_ram_resource_share.go
+++ aws/resource_aws_ram_resource_share.go
@@ -8,8 +8,8 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ram"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/keyvaluetags"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/service/ram/waiter"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/keyvaluetags"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/ram/waiter"
 )
 
 func resourceAwsRamResourceShare() *schema.Resource {
