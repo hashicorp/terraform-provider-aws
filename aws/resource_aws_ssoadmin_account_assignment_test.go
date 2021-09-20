00c2551b3a289a2f0ab600d20359a084b7228513
Dirk Avery <dirk.avery@gmail.com>
2021-09-20 10:35:00 -0400

    ssoadmin: Change from terraform-providers to hashicorp

--- aws/resource_aws_ssoadmin_account_assignment_test.go
+++ aws/resource_aws_ssoadmin_account_assignment_test.go
@@ -15,7 +15,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/service/ssoadmin/finder"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/ssoadmin/finder"
 )
 
 func init() {
--- aws/resource_aws_ssoadmin_managed_policy_attachment.go
+++ aws/resource_aws_ssoadmin_managed_policy_attachment.go
@@ -9,7 +9,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/ssoadmin"
 	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/service/ssoadmin/finder"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/ssoadmin/finder"
 )
 
 func resourceAwsSsoAdminManagedPolicyAttachment() *schema.Resource {
