a1f72704a61b8ede23619207ee27a9aa62f2af95
Kamil Turek <kamil.turek@hotmail.com>
2021-09-26 23:08:14 +0200

    Sort imports

--- aws/data_source_aws_db_proxy_test.go
+++ aws/data_source_aws_db_proxy_test.go
@@ -2,10 +2,10 @@ package aws
 
 import (
 	"fmt"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/service/rds"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
