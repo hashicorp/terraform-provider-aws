922200d3fff0b097cdcd85c1551b45cdd9a8b1ca
Ahmed Bebars <ahmed.bebars@nytimes.com>
2021-09-28 00:40:48 -0400

    create id functions for lambda function associations

---
+++ aws/internal/service/connect/id.go
@@ -0,0 +1,23 @@
+package connect
+
+import (
+	"fmt"
+	"strings"
+)
+
+const lambdaFunctionAssociationIDSeparator = "_"
+
+func LambdaFunctionAssociationParseID(id string) (string, string, error) {
+	parts := strings.SplitN(id, lambdaFunctionAssociationIDSeparator, 2)
+	if len(parts) == 2 && parts[0] != "" && parts[1] != "" {
+		return parts[0], parts[1], nil
+	}
+
+	return "", "",
+		fmt.Errorf("unexpected format for ID (%q), expected instanceID"+lambdaFunctionAssociationIDSeparator+
+			"function-arn", id)
+}
+
+func LambdaFunctionAssociationID(instanceID string, functionArn string) string {
+	return strings.Join([]string{instanceID, functionArn}, lambdaFunctionAssociationIDSeparator)
+}
