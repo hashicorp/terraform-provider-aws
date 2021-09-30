ac4b98b0b90a3fa8cd1fc888bd3bebd9225873fd
Kit Ewbank <Kit_Ewbank@hotmail.com>
2021-09-30 16:32:27 -0400

    Corrections after rebase.

--- aws/data_source_aws_ssm_parameters_by_path.go
+++ aws/data_source_aws_ssm_parameters_by_path.go
@@ -6,9 +6,8 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ssm"
-
-	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 func dataSourceAwsSsmParametersByPath() *schema.Resource {
--- aws/data_source_aws_ssm_parameters_by_path_test.go
+++ aws/data_source_aws_ssm_parameters_by_path_test.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
 func TestAccAWSSsmParametersByPathDataSource_basic(t *testing.T) {
