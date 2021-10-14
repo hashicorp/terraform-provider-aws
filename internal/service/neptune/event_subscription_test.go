4a03aa83e65ae785c8728436d1f1137f71b79a27
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 12:58:27 -0400

    neptune: Making service sweeps

--- internal/service/neptune/event_subscription_test.go
+++ internal/service/neptune/event_subscription_test.go
@@ -19,67 +19,9 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 )
 
-func init() {
-	resource.AddTestSweepers("aws_neptune_event_subscription", &resource.Sweeper{
-		Name: "aws_neptune_event_subscription",
-		F:    sweepEventSubscriptions,
-	})
-}
 
-func sweepEventSubscriptions(region string) error {
-	client, err := sweep.SharedRegionalSweepClient(region)
-	if err != nil {
-		return fmt.Errorf("error getting client: %w", err)
-	}
-	conn := client.(*conns.AWSClient).NeptuneConn
-	var sweeperErrs *multierror.Error
 
-	err = conn.DescribeEventSubscriptionsPages(&neptune.DescribeEventSubscriptionsInput{}, func(page *neptune.DescribeEventSubscriptionsOutput, lastPage bool) bool {
-		if page == nil {
-			return !lastPage
-		}
 
-		for _, eventSubscription := range page.EventSubscriptionsList {
-			name := aws.StringValue(eventSubscription.CustSubscriptionId)
-
-			log.Printf("[INFO] Deleting Neptune Event Subscription: %s", name)
-			_, err = conn.DeleteEventSubscription(&neptune.DeleteEventSubscriptionInput{
-				SubscriptionName: aws.String(name),
-			})
-			if tfawserr.ErrMessageContains(err, neptune.ErrCodeSubscriptionNotFoundFault, "") {
-				continue
-			}
-			if err != nil {
-				sweeperErr := fmt.Errorf("error deleting Neptune Event Subscription (%s): %w", name, err)
-				log.Printf("[ERROR] %s", sweeperErr)
-				sweeperErrs = multierror.Append(sweeperErrs, sweeperErr)
-				continue
-			}
-
-			_, err = tfneptune.WaitEventSubscriptionDeleted(conn, name)
-			if tfawserr.ErrMessageContains(err, neptune.ErrCodeSubscriptionNotFoundFault, "") {
-				continue
-			}
-			if err != nil {
-				sweeperErr := fmt.Errorf("error waiting for Neptune Event Subscription (%s) deletion: %w", name, err)
-				log.Printf("[ERROR] %s", sweeperErr)
-				sweeperErrs = multierror.Append(sweeperErrs, sweeperErr)
-				continue
-			}
-		}
-
-		return !lastPage
-	})
-	if sweep.SkipSweepError(err) {
-		log.Printf("[WARN] Skipping Neptune Event Subscriptions sweep for %s: %s", region, err)
-		return sweeperErrs.ErrorOrNil() // In case we have completed some pages, but had errors
-	}
-	if err != nil {
-		sweeperErrs = multierror.Append(sweeperErrs, fmt.Errorf("error retrieving Neptune Event Subscriptions: %w", err))
-	}
-
-	return sweeperErrs.ErrorOrNil()
-}
 
 func TestAccNeptuneEventSubscription_basic(t *testing.T) {
 	var v neptune.EventSubscription
