38135860de3dde99595f7e0f893d4d5ba9326370
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 12:55:36 -0400

    macie2: Clean up and standardize acceptance test names

--- internal/service/macie2/macie2_test.go
+++ internal/service/macie2/macie2_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestAccAWSMacie2_serial(t *testing.T) {
+func TestAccMacie2_serial(t *testing.T) {
 	testCases := map[string]map[string]func(t *testing.T){
 		"Account": {
 			"basic":                        testAccAccount_basic,
