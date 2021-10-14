d544d1254ce1e64bf2e09b95c268fe28f0dbd8f5
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:43:46 -0400

    dynamodb: Fix more import problems

--- internal/service/dynamodb/find.go
+++ internal/service/dynamodb/find.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 func FindDynamoDBKinesisDataStreamDestination(ctx context.Context, conn *dynamodb.DynamoDB, streamArn, tableName string) (*dynamodb.KinesisDataStreamDestination, error) {
