720bb66cbbdb0865f75e5423da9560c680699b30
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 12:55:15 -0400

    appmesh: Clean up and standardize acceptance test names

--- internal/service/appmesh/appmesh_test.go
+++ internal/service/appmesh/appmesh_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestAccAWSAppmesh_serial(t *testing.T) {
+func TestAccAppMesh_serial(t *testing.T) {
 	testCases := map[string]map[string]func(t *testing.T){
 		"GatewayRoute": {
 			"basic":      testAccGatewayRoute_basic,
