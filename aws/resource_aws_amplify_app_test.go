4b1cbf6ed6e7cb2142221c747bed6e7fc935e171
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:21:10 -0400

    amplify: Create sweep package

--- aws/resource_aws_amplify_app_test.go
+++ aws/resource_aws_amplify_app_test.go
@@ -20,6 +20,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/provider"
+	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 )
 
 func init() {
@@ -30,7 +31,7 @@ func init() {
 }
 
 func testSweepAmplifyApps(region string) error {
-	client, err := sharedClientForRegion(region)
+	client, err := sweep.SharedRegionalSweepClient(region)
 	if err != nil {
 		return fmt.Errorf("error getting client: %s", err)
 	}
@@ -59,7 +60,7 @@ func testSweepAmplifyApps(region string) error {
 		return !lastPage
 	})
 
-	if testSweepSkipSweepError(err) {
+	if sweep.SkipSweepError(err) {
 		log.Printf("[WARN] Skipping Amplify Apps sweep for %s: %s", region, err)
 		return sweeperErrs.ErrorOrNil() // In case we have completed some pages, but had errors
 	}
