da97497533c4a5cfcfd820754d53d61aca354d4c
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:43:36 -0400

    accessanalyzer: Fix more import problems

--- internal/service/accessanalyzer/accessanalyzer_test.go
+++ internal/service/accessanalyzer/accessanalyzer_test.go
@@ -6,9 +6,6 @@ import (
 	"github.com/aws/aws-sdk-go/service/accessanalyzer"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
-	"github.com/hashicorp/terraform-provider-aws/internal/provider"
-	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
-	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 // AccessAnalyzer is limited to one per region, so run serially
