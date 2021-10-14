ead58b39f4e3d7182f72d8140392b656e632353e
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:21:22 -0400

    glacier: Create sweep package

--- aws/resource_aws_glacier_vault_test.go
+++ aws/resource_aws_glacier_vault_test.go
@@ -15,6 +15,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/provider"
+	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 )
 
 func init() {
@@ -25,7 +26,7 @@ func init() {
 }
 
 func testSweepGlacierVaults(region string) error {
-	client, err := sharedClientForRegion(region)
+	client, err := sweep.SharedRegionalSweepClient(region)
 	if err != nil {
 		return fmt.Errorf("error getting client: %w", err)
 	}
@@ -66,7 +67,7 @@ func testSweepGlacierVaults(region string) error {
 
 		return !lastPage
 	})
-	if testSweepSkipSweepError(err) {
+	if sweep.SkipSweepError(err) {
 		log.Printf("[WARN] Skipping Glacier Vaults sweep for %s: %s", region, err)
 		return sweeperErrs.ErrorOrNil() // In case we have completed some pages, but had errors
 	}
