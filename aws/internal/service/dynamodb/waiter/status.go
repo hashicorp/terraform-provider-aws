d26cf176102c93b0df5a713cc47298e1c38f8e52
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:25:48 -0400

    dynamodb: Create verify package

--- aws/internal/service/dynamodb/waiter/status.go
+++ aws/internal/service/dynamodb/waiter/status.go
@@ -9,6 +9,7 @@ import (
 	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/dynamodb/finder"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 func DynamoDBKinesisStreamingDestinationStatus(ctx context.Context, conn *dynamodb.DynamoDB, streamArn, tableName string) resource.StateRefreshFunc {
--- aws/resource_aws_dynamodb_global_table.go
+++ aws/resource_aws_dynamodb_global_table.go
@@ -11,6 +11,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 func ResourceGlobalTable() *schema.Resource {
@@ -34,7 +35,7 @@ func ResourceGlobalTable() *schema.Resource {
 				Type:         schema.TypeString,
 				Required:     true,
 				ForceNew:     true,
-				ValidateFunc: validateAwsDynamoDbGlobalTableName,
+				ValidateFunc: validGlobalTableName,
 			},
 
 			"replica": {
