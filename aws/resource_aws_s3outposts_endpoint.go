656adae3e725b6e6d658f515f1e5098b7f20c610
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:22:44 -0400

    s3outposts: Begin long road to new tags

--- aws/resource_aws_s3outposts_endpoint.go
+++ aws/resource_aws_s3outposts_endpoint.go
@@ -14,6 +14,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/s3outposts/finder"
 	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/s3outposts/waiter"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 func ResourceEndpoint() *schema.Resource {
