1b3d7ceabab55c72b207fdacfb71f06a98c68cbb
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 12:58:20 -0400

    glacier: Making service sweeps

--- internal/service/glacier/vault_test.go
+++ internal/service/glacier/vault_test.go
@@ -19,65 +19,9 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 )
 
-func init() {
-	resource.AddTestSweepers("aws_glacier_vault", &resource.Sweeper{
-		Name: "aws_glacier_vault",
-		F:    sweepVaults,
-	})
-}
 
-func sweepVaults(region string) error {
-	client, err := sweep.SharedRegionalSweepClient(region)
-	if err != nil {
-		return fmt.Errorf("error getting client: %w", err)
-	}
-	conn := client.(*conns.AWSClient).GlacierConn
-	var sweeperErrs *multierror.Error
 
-	err = conn.ListVaultsPages(&glacier.ListVaultsInput{}, func(page *glacier.ListVaultsOutput, lastPage bool) bool {
-		if page == nil {
-			return !lastPage
-		}
 
-		for _, vault := range page.VaultList {
-			name := aws.StringValue(vault.VaultName)
-
-			// First attempt to delete the vault's notification configuration in case the vault deletion fails.
-			log.Printf("[INFO] Deleting Glacier Vault (%s) Notifications", name)
-			_, err := conn.DeleteVaultNotifications(&glacier.DeleteVaultNotificationsInput{
-				VaultName: aws.String(name),
-			})
-			if err != nil {
-				sweeperErr := fmt.Errorf("error deleting Glacier Vault (%s) Notifications: %w", name, err)
-				log.Printf("[ERROR] %s", sweeperErr)
-				sweeperErrs = multierror.Append(sweeperErrs, sweeperErr)
-				continue
-			}
-
-			log.Printf("[INFO] Deleting Glacier Vault: %s", name)
-			_, err = conn.DeleteVault(&glacier.DeleteVaultInput{
-				VaultName: aws.String(name),
-			})
-			if err != nil {
-				sweeperErr := fmt.Errorf("error deleting Glacier Vault (%s): %w", name, err)
-				log.Printf("[ERROR] %s", sweeperErr)
-				sweeperErrs = multierror.Append(sweeperErrs, sweeperErr)
-				continue
-			}
-		}
-
-		return !lastPage
-	})
-	if sweep.SkipSweepError(err) {
-		log.Printf("[WARN] Skipping Glacier Vaults sweep for %s: %s", region, err)
-		return sweeperErrs.ErrorOrNil() // In case we have completed some pages, but had errors
-	}
-	if err != nil {
-		sweeperErrs = multierror.Append(sweeperErrs, fmt.Errorf("error retrieving Glacier Vaults: %w", err))
-	}
-
-	return sweeperErrs.ErrorOrNil()
-}
 
 func TestAccGlacierVault_basic(t *testing.T) {
 	var vault glacier.DescribeVaultOutput
