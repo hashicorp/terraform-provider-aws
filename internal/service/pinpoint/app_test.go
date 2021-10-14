5b722dbd3f3790430469029242467bc9efedeb54
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 12:58:28 -0400

    pinpoint: Making service sweeps

--- internal/service/pinpoint/app_test.go
+++ internal/service/pinpoint/app_test.go
@@ -17,57 +17,9 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 )
 
-func init() {
-	resource.AddTestSweepers("aws_pinpoint_app", &resource.Sweeper{
-		Name: "aws_pinpoint_app",
-		F:    sweepApps,
-	})
-}
 
-func sweepApps(region string) error {
-	client, err := sweep.SharedRegionalSweepClient(region)
-	if err != nil {
-		return fmt.Errorf("error getting client: %s", err)
-	}
-	conn := client.(*conns.AWSClient).PinpointConn
 
-	input := &pinpoint.GetAppsInput{}
 
-	for {
-		output, err := conn.GetApps(input)
-		if err != nil {
-			if sweep.SkipSweepError(err) {
-				log.Printf("[WARN] Skipping Pinpoint app sweep for %s: %s", region, err)
-				return nil
-			}
-			return fmt.Errorf("Error retrieving Pinpoint apps: %s", err)
-		}
-
-		if len(output.ApplicationsResponse.Item) == 0 {
-			log.Print("[DEBUG] No Pinpoint apps to sweep")
-			return nil
-		}
-
-		for _, item := range output.ApplicationsResponse.Item {
-			name := aws.StringValue(item.Name)
-
-			log.Printf("[INFO] Deleting Pinpoint app %s", name)
-			_, err := conn.DeleteApp(&pinpoint.DeleteAppInput{
-				ApplicationId: item.Id,
-			})
-			if err != nil {
-				return fmt.Errorf("Error deleting Pinpoint app %s: %s", name, err)
-			}
-		}
-
-		if output.ApplicationsResponse.NextToken == nil {
-			break
-		}
-		input.Token = output.ApplicationsResponse.NextToken
-	}
-
-	return nil
-}
 
 func TestAccPinpointApp_basic(t *testing.T) {
 	var application pinpoint.ApplicationResponse
