61ef4e9743258e88b67960264bb52309c2e0aed7
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:43:43 -0400

    cognitoidp: Fix more import problems

--- internal/service/cognitoidp/find.go
+++ internal/service/cognitoidp/find.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
-	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 // FindCognitoUserPoolUICustomization returns the UI Customization corresponding to the UserPoolId and ClientId.
