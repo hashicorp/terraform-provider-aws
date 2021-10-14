26ff9426894121694dafb9a20eed1ff5da3f556b
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:26:03 -0400

    pricing: Create verify package

--- aws/data_source_aws_pricing_product.go
+++ aws/data_source_aws_pricing_product.go
@@ -11,6 +11,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/create"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 func DataSourceProduct() *schema.Resource {
