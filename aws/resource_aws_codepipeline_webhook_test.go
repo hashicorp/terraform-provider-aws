d23136195c599556c763b8ccc6b19da1efb5b20d
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:25:44 -0400

    codepipeline: Create verify package

--- aws/resource_aws_codepipeline_webhook_test.go
+++ aws/resource_aws_codepipeline_webhook_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/provider"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 const envVarGithubTokenUsageCodePipelineWebhook = "token with GitHub permissions to repository for CodePipeline webhook creation"
