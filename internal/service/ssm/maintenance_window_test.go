8b55890a4e430353c2f2c232c3edcd23bd213e1d
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 13:00:03 -0400

    ssm: Fix more import problems

--- internal/service/ssm/maintenance_window_test.go
+++ internal/service/ssm/maintenance_window_test.go
@@ -2,20 +2,17 @@ package ssm_test
 
 import (
 	"fmt"
-	"log"
 	"testing"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ssm"
 	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
-	"github.com/hashicorp/go-multierror"
 	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
-	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 )
 
 func TestAccSSMMaintenanceWindow_basic(t *testing.T) {
--- internal/service/ssm/resource_data_sync_test.go
+++ internal/service/ssm/resource_data_sync_test.go
@@ -5,16 +5,13 @@ import (
 	"log"
 	"testing"
 
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ssm"
-	multierror "github.com/hashicorp/go-multierror"
 	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	tfssm "github.com/hashicorp/terraform-provider-aws/internal/service/ssm"
-	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 )
 
 func TestAccSSMResourceDataSync_basic(t *testing.T) {
