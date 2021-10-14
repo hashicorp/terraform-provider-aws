452d3c3db24e8ff239725799266fa50d3c53c543
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:21:38 -0400

    synthetics: Create sweep package

--- aws/resource_aws_synthetics_canary_test.go
+++ aws/resource_aws_synthetics_canary_test.go
@@ -20,6 +20,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/provider"
+	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 )
 
 func init() {
@@ -35,7 +36,7 @@ func init() {
 }
 
 func testSweepSyntheticsCanaries(region string) error {
-	client, err := sharedClientForRegion(region)
+	client, err := sweep.SharedRegionalSweepClient(region)
 	if err != nil {
 		return fmt.Errorf("error getting client: %s", err)
 	}
@@ -45,7 +46,7 @@ func testSweepSyntheticsCanaries(region string) error {
 
 	for {
 		output, err := conn.DescribeCanaries(input)
-		if testSweepSkipSweepError(err) {
+		if sweep.SkipSweepError(err) {
 			log.Printf("[WARN] Skipping Synthetics Canary sweep for %s: %s", region, err)
 			return nil
 		}
