1705f54b76e735da4295ed74571f6322025fa0cf
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 12:59:35 -0400

    acmpca: Fix more import problems

--- internal/service/acmpca/certificate_authority_test.go
+++ internal/service/acmpca/certificate_authority_test.go
@@ -2,21 +2,18 @@ package acmpca_test
 
 import (
 	"fmt"
-	"log"
 	"regexp"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/acmpca"
 	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
-	"github.com/hashicorp/go-multierror"
 	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	tfacmpca "github.com/hashicorp/terraform-provider-aws/internal/service/acmpca"
-	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 )
 
 func TestAccACMPCACertificateAuthority_basic(t *testing.T) {
