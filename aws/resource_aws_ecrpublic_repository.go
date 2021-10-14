d06d11a7578df307c9b7a47afad6f5a2f9e14473
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:17:43 -0400

    ecrpublic: Migrate to service, global flex

--- aws/resource_aws_ecrpublic_repository.go
+++ aws/resource_aws_ecrpublic_repository.go
@@ -327,7 +327,7 @@ func expandEcrPublicRepositoryCatalogData(tfMap map[string]interface{}) *ecrpubl
 	}
 
 	if v, ok := tfMap["architectures"].(*schema.Set); ok {
-		repositoryCatalogDataInput.Architectures = expandStringSet(v)
+		repositoryCatalogDataInput.Architectures = flex.ExpandStringSet(v)
 	}
 
 	if v, ok := tfMap["description"].(string); ok && v != "" {
@@ -340,7 +340,7 @@ func expandEcrPublicRepositoryCatalogData(tfMap map[string]interface{}) *ecrpubl
 	}
 
 	if v, ok := tfMap["operating_systems"].(*schema.Set); ok {
-		repositoryCatalogDataInput.OperatingSystems = expandStringSet(v)
+		repositoryCatalogDataInput.OperatingSystems = flex.ExpandStringSet(v)
 	}
 
 	if v, ok := tfMap["usage_text"].(string); ok && v != "" {
