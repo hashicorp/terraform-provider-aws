a7710f063cd8fe74ffb08a67cb02b3006fa4a9c9
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:21:32 -0400

    quicksight: Create sweep package

--- aws/resource_aws_quicksight_data_source_test.go
+++ aws/resource_aws_quicksight_data_source_test.go
@@ -18,6 +18,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/provider"
+	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 )
 
 func init() {
@@ -28,14 +29,14 @@ func init() {
 }
 
 func testSweepQuickSightDataSources(region string) error {
-	client, err := sharedClientForRegion(region)
+	client, err := sweep.SharedRegionalSweepClient(region)
 
 	if err != nil {
 		return fmt.Errorf("error getting client: %w", err)
 	}
 
 	conn := client.(*conns.AWSClient).QuickSightConn
-	sweepResources := make([]*testSweepResource, 0)
+	sweepResources := make([]*sweep.SweepResource, 0)
 	var errs *multierror.Error
 
 	ctx := context.Background()
@@ -61,7 +62,7 @@ func testSweepQuickSightDataSources(region string) error {
 
 			d.SetId(fmt.Sprintf("%s/%s", awsAccountId, aws.StringValue(ds.DataSourceId)))
 
-			sweepResources = append(sweepResources, NewTestSweepResource(r, d, client))
+			sweepResources = append(sweepResources, sweep.NewSweepResource(r, d, client))
 		}
 
 		return !lastPage
@@ -71,11 +72,11 @@ func testSweepQuickSightDataSources(region string) error {
 		errs = multierror.Append(errs, fmt.Errorf("error describing QuickSigth Data Sources: %w", err))
 	}
 
-	if err := testSweepResourceOrchestrator(sweepResources); err != nil {
+	if err := sweep.SweepOrchestrator(sweepResources); err != nil {
 		errs = multierror.Append(errs, fmt.Errorf("error sweeping QuickSight Data Sources for %s: %w", region, err))
 	}
 
-	if testSweepSkipSweepError(errs.ErrorOrNil()) {
+	if sweep.SkipSweepError(errs.ErrorOrNil()) {
 		log.Printf("[WARN] Skipping QuickSight Data Source sweep for %s: %s", region, errs)
 		return nil
 	}
