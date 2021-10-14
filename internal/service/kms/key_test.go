9f8565ef6ca0ae41876c5bc7ad0231dcaec2ea4c
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 12:59:54 -0400

    kms: Fix more import problems

--- internal/service/kms/key_test.go
+++ internal/service/kms/key_test.go
@@ -2,7 +2,6 @@ package kms_test
 
 import (
 	"fmt"
-	"log"
 	"regexp"
 	"testing"
 
@@ -14,7 +13,6 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	tfkms "github.com/hashicorp/terraform-provider-aws/internal/service/kms"
-	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	awspolicy "github.com/jen20/awspolicyequivalence"
 )
