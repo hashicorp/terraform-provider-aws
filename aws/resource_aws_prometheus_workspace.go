ac6f8fb00a6f627ab06059bdf121fe73e2649d68
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:22:40 -0400

    prometheus: Begin long road to new tags

--- aws/resource_aws_prometheus_workspace.go
+++ aws/resource_aws_prometheus_workspace.go
@@ -12,6 +12,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/prometheusservice/waiter"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 func ResourceWorkspace() *schema.Resource {
