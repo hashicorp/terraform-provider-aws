8bb4815f0a1ea08c5956c12f44fe0ce0985ed26a
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:38:49 -0400

    cur: Fix many import problems

--- internal/service/cur/data_source_aws_cur_report_definition.go
+++ internal/service/cur/data_source_aws_cur_report_definition.go
@@ -1,15 +1,13 @@
-package aws
+package cur
 
 import (
 	"fmt"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/costandusagereportservice/finder"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
-	tfcur "github.com/hashicorp/terraform-provider-aws/internal/service/cur"
 )
 
 func DataSourceReportDefinition() *schema.Resource {
@@ -74,7 +72,7 @@ func dataSourceReportDefinitionRead(d *schema.ResourceData, meta interface{}) er
 
 	reportName := d.Get("report_name").(string)
 
-	reportDefinition, err := tfcur.FindReportDefinitionByName(conn, reportName)
+	reportDefinition, err := FindReportDefinitionByName(conn, reportName)
 
 	if err != nil {
 		return fmt.Errorf("error reading Report Definition (%s): %w", reportName, err)
