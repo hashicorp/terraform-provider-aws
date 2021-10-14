13ccf60bc92dc78c901ca14ceae6b34f159507f8
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:21:40 -0400

    workspaces: Create sweep package

--- aws/resource_aws_workspaces_workspace_test.go
+++ aws/resource_aws_workspaces_workspace_test.go
@@ -17,6 +17,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/provider"
+	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 )
 
 func init() {
@@ -27,7 +28,7 @@ func init() {
 }
 
 func testSweepWorkspacesWorkspace(region string) error {
-	client, err := sharedClientForRegion(region)
+	client, err := sweep.SharedRegionalSweepClient(region)
 	if err != nil {
 		return fmt.Errorf("error getting client: %w", err)
 	}
@@ -45,7 +46,7 @@ func testSweepWorkspacesWorkspace(region string) error {
 		}
 		return true
 	})
-	if testSweepSkipSweepError(err) {
+	if sweep.SkipSweepError(err) {
 		log.Printf("[WARN] Skipping workspaces sweep for %s: %s", region, err)
 		return errors // In case we have completed some pages, but had errors
 	}
