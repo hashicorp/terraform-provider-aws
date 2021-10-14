af49185c2d97137a2a8889a23bb2b35b761c1a41
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 12:59:58 -0400

    pinpoint: Fix more import problems

--- internal/service/pinpoint/app_test.go
+++ internal/service/pinpoint/app_test.go
@@ -2,7 +2,6 @@ package pinpoint_test
 
 import (
 	"fmt"
-	"log"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -14,7 +13,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
-	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 )
 
 func TestAccPinpointApp_basic(t *testing.T) {
