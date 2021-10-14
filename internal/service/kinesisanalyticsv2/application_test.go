358812b7d946215900cce7f2bd0b8264efec9fc5
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 12:58:23 -0400

    kinesisanalyticsv2: Making service sweeps

--- internal/service/kinesisanalyticsv2/application_test.go
+++ internal/service/kinesisanalyticsv2/application_test.go
@@ -20,68 +20,9 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 )
 
-func init() {
-	resource.AddTestSweepers("aws_kinesisanalyticsv2_application", &resource.Sweeper{
-		Name: "aws_kinesisanalyticsv2_application",
-		F:    sweepApplication,
-	})
-}
 
-func sweepApplication(region string) error {
-	client, err := sweep.SharedRegionalSweepClient(region)
-	if err != nil {
-		return fmt.Errorf("error getting client: %s", err)
-	}
-	conn := client.(*conns.AWSClient).KinesisAnalyticsV2Conn
-	input := &kinesisanalyticsv2.ListApplicationsInput{}
-	var sweeperErrs *multierror.Error
 
-	err = tfkinesisanalyticsv2.ListApplicationsPages(conn, input, func(page *kinesisanalyticsv2.ListApplicationsOutput, lastPage bool) bool {
-		if page == nil {
-			return !lastPage
-		}
 
-		for _, applicationSummary := range page.ApplicationSummaries {
-			arn := aws.StringValue(applicationSummary.ApplicationARN)
-			name := aws.StringValue(applicationSummary.ApplicationName)
-
-			application, err := tfkinesisanalyticsv2.FindApplicationDetailByName(conn, name)
-
-			if err != nil {
-				sweeperErr := fmt.Errorf("error reading Kinesis Analytics v2 Application (%s): %w", arn, err)
-				log.Printf("[ERROR] %s", err)
-				sweeperErrs = multierror.Append(sweeperErrs, sweeperErr)
-				continue
-			}
-
-			r := tfkinesisanalyticsv2.ResourceApplication()
-			d := r.Data(nil)
-			d.SetId(arn)
-			d.Set("create_timestamp", aws.TimeValue(application.CreateTimestamp).Format(time.RFC3339))
-			d.Set("name", name)
-			err = r.Delete(d, client)
-
-			if err != nil {
-				log.Printf("[ERROR] %s", err)
-				sweeperErrs = multierror.Append(sweeperErrs, err)
-				continue
-			}
-		}
-
-		return !lastPage
-	})
-
-	if sweep.SkipSweepError(err) {
-		log.Printf("[WARN] Skipping Kinesis Analytics v2 Application sweep for %s: %s", region, err)
-		return sweeperErrs.ErrorOrNil() // In case we have completed some pages, but had errors
-	}
-
-	if err != nil {
-		sweeperErrs = multierror.Append(sweeperErrs, fmt.Errorf("error listing Kinesis Analytics v2 Applications: %w", err))
-	}
-
-	return sweeperErrs.ErrorOrNil()
-}
 
 func TestAccKinesisAnalyticsV2Application_basicFlinkApplication(t *testing.T) {
 	var v kinesisanalyticsv2.ApplicationDetail
