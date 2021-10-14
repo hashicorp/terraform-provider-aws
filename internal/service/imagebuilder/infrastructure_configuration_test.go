80b94cf5d0da3a152605c5406c0cd900fb2a5cc7
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 12:59:47 -0400

    imagebuilder: Fix more import problems

--- internal/service/imagebuilder/infrastructure_configuration_test.go
+++ internal/service/imagebuilder/infrastructure_configuration_test.go
@@ -2,20 +2,17 @@ package imagebuilder_test
 
 import (
 	"fmt"
-	"log"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/imagebuilder"
 	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
-	"github.com/hashicorp/go-multierror"
 	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	tfimagebuilder "github.com/hashicorp/terraform-provider-aws/internal/service/imagebuilder"
-	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 )
 
 func TestAccImageBuilderInfrastructureConfiguration_basic(t *testing.T) {
