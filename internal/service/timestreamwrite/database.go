8505d664e7bdf6e5af072b74682d16be528f945c
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:44:07 -0400

    timestreamwrite: Fix more import problems

--- internal/service/timestreamwrite/database.go
+++ internal/service/timestreamwrite/database.go
@@ -12,7 +12,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
