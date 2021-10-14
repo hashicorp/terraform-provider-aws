6dc228de48bfbf592352fbe577becb1e4d8aae59
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:43:54 -0400

    kinesisanalytics: Fix more import problems

--- internal/service/kinesisanalytics/list.go
+++ internal/service/kinesisanalytics/list.go
@@ -3,7 +3,6 @@ package kinesisanalytics
 import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/kinesisanalytics"
-	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 // Custom Kinesisanalytics listing functions using similar formatting as other service generated code.
