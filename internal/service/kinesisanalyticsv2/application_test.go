f6444936b1d10035cc33125c6733e7205d34e380
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 12:59:53 -0400

    kinesisanalyticsv2: Fix more import problems

--- internal/service/kinesisanalyticsv2/application_test.go
+++ internal/service/kinesisanalyticsv2/application_test.go
@@ -2,21 +2,16 @@ package kinesisanalyticsv2_test
 
 import (
 	"fmt"
-	"log"
 	"strconv"
 	"testing"
-	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/kinesisanalyticsv2"
-	"github.com/hashicorp/go-multierror"
 	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	tfkinesisanalyticsv2 "github.com/hashicorp/terraform-provider-aws/internal/service/kinesisanalyticsv2"
-	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 )
 
