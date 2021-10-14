c9c831b87fcf24844c83489de167b8a19f9cb1ff
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 12:58:24 -0400

    kms: Making service sweeps

--- internal/service/kms/key_test.go
+++ internal/service/kms/key_test.go
@@ -19,62 +19,9 @@ import (
 	awspolicy "github.com/jen20/awspolicyequivalence"
 )
 
-func init() {
-	resource.AddTestSweepers("aws_kms_key", &resource.Sweeper{
-		Name: "aws_kms_key",
-		F:    sweepKeys,
-	})
-}
 
-func sweepKeys(region string) error {
-	client, err := sweep.SharedRegionalSweepClient(region)
-	if err != nil {
-		return fmt.Errorf("error getting client: %w", err)
-	}
-	conn := client.(*conns.AWSClient).KMSConn
 
-	err = conn.ListKeysPages(&kms.ListKeysInput{Limit: aws.Int64(int64(1000))}, func(out *kms.ListKeysOutput, lastPage bool) bool {
-		for _, k := range out.Keys {
-			kKeyId := aws.StringValue(k.KeyId)
-			kOut, err := conn.DescribeKey(&kms.DescribeKeyInput{
-				KeyId: k.KeyId,
-			})
-			if err != nil {
-				log.Printf("Error: Failed to describe key %q: %s", kKeyId, err)
-				return false
-			}
-			if aws.StringValue(kOut.KeyMetadata.KeyManager) == kms.KeyManagerTypeAws {
-				// Skip (default) keys which are managed by AWS
-				continue
-			}
-			if aws.StringValue(kOut.KeyMetadata.KeyState) == kms.KeyStatePendingDeletion {
-				// Skip keys which are already scheduled for deletion
-				continue
-			}
 
-			r := tfkms.ResourceKey()
-			d := r.Data(nil)
-			d.SetId(kKeyId)
-			d.Set("key_id", kKeyId)
-			d.Set("deletion_window_in_days", "7")
-			err = r.Delete(d, client)
-			if err != nil {
-				log.Printf("Error: Failed to schedule key %q for deletion: %s", kKeyId, err)
-				return false
-			}
-		}
-		return !lastPage
-	})
-	if err != nil {
-		if sweep.SkipSweepError(err) {
-			log.Printf("[WARN] Skipping KMS Key sweep for %s: %s", region, err)
-			return nil
-		}
-		return fmt.Errorf("Error describing KMS keys: %w", err)
-	}
-
-	return nil
-}
 
 func TestAccKMSKey_basic(t *testing.T) {
 	var key kms.KeyMetadata
