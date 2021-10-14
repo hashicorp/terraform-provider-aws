3297f57cee469cdd42100629b43e03d3a16f61fc
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 12:58:10 -0400

    amplify: Making service sweeps

--- internal/service/amplify/app_test.go
+++ internal/service/amplify/app_test.go
@@ -21,54 +21,9 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 )
 
-func init() {
-	resource.AddTestSweepers("aws_amplify_app", &resource.Sweeper{
-		Name: "aws_amplify_app",
-		F:    sweepApps,
-	})
-}
 
-func sweepApps(region string) error {
-	client, err := sweep.SharedRegionalSweepClient(region)
-	if err != nil {
-		return fmt.Errorf("error getting client: %s", err)
-	}
-	conn := client.(*conns.AWSClient).AmplifyConn
-	input := &amplify.ListAppsInput{}
-	var sweeperErrs *multierror.Error
 
-	err = tfamplify.ListAppsPages(conn, input, func(page *amplify.ListAppsOutput, lastPage bool) bool {
-		if page == nil {
-			return !lastPage
-		}
 
-		for _, app := range page.Apps {
-			r := tfamplify.ResourceApp()
-			d := r.Data(nil)
-			d.SetId(aws.StringValue(app.AppId))
-			err = r.Delete(d, client)
-
-			if err != nil {
-				log.Printf("[ERROR] %s", err)
-				sweeperErrs = multierror.Append(sweeperErrs, err)
-				continue
-			}
-		}
-
-		return !lastPage
-	})
-
-	if sweep.SkipSweepError(err) {
-		log.Printf("[WARN] Skipping Amplify Apps sweep for %s: %s", region, err)
-		return sweeperErrs.ErrorOrNil() // In case we have completed some pages, but had errors
-	}
-
-	if err != nil {
-		sweeperErrs = multierror.Append(sweeperErrs, fmt.Errorf("error listing Amplify Apps: %w", err))
-	}
-
-	return sweeperErrs.ErrorOrNil()
-}
 
 func testAccApp_basic(t *testing.T) {
 	var app amplify.App
