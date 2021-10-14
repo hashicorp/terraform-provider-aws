49e60de389f0ed913e7769cef62281f3fae8a5ec
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:44:04 -0400

    sfn: Fix more import problems

--- internal/service/sfn/activity.go
+++ internal/service/sfn/activity.go
@@ -10,7 +10,6 @@ import (
 	"github.com/aws/aws-sdk-go/service/sfn"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
