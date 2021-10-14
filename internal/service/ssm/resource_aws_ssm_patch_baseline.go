08b5244b0d6c01d4d40f29d114f5d805fc3b8c64
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:39:10 -0400

    ssm: Fix many import problems

--- internal/service/ssm/resource_aws_ssm_patch_baseline.go
+++ internal/service/ssm/resource_aws_ssm_patch_baseline.go
@@ -1,4 +1,4 @@
-package aws
+package ssm
 
 import (
 	"fmt"
@@ -11,7 +11,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/ssm"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-	tftags "github.com/hashicorp/terraform-provider-aws/aws/internal/tags"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
--- internal/service/ssm/status.go
+++ internal/service/ssm/status.go
@@ -4,10 +4,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ssm"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/ssm/finder"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
-	tfssm "github.com/hashicorp/terraform-provider-aws/internal/service/ssm"
-	tfssm "github.com/hashicorp/terraform-provider-aws/internal/service/ssm"
 )
 
 const (
@@ -17,7 +14,7 @@ const (
 // statusDocument fetches the Document and its Status
 func statusDocument(conn *ssm.SSM, name string) resource.StateRefreshFunc {
 	return func() (interface{}, string, error) {
-		output, err := tfssm.FindDocumentByName(conn, name)
+		output, err := FindDocumentByName(conn, name)
 
 		if err != nil {
 			return nil, ssm.DocumentStatusFailed, err
