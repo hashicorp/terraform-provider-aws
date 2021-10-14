41cad0075d13f81a53ba1f63ee04ca8054dda395
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:21:23 -0400

    globalaccelerator: Create sweep package

--- aws/resource_aws_globalaccelerator_accelerator_test.go
+++ aws/resource_aws_globalaccelerator_accelerator_test.go
@@ -17,6 +17,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/provider"
+	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 )
 
 func init() {
@@ -27,7 +28,7 @@ func init() {
 }
 
 func testSweepGlobalAcceleratorAccelerators(region string) error {
-	client, err := sharedClientForRegion(region)
+	client, err := sweep.SharedRegionalSweepClient(region)
 	if err != nil {
 		return fmt.Errorf("Error getting client: %s", err)
 	}
@@ -39,7 +40,7 @@ func testSweepGlobalAcceleratorAccelerators(region string) error {
 	for {
 		output, err := conn.ListAccelerators(input)
 
-		if testSweepSkipSweepError(err) {
+		if sweep.SkipSweepError(err) {
 			log.Printf("[WARN] Skipping Global Accelerator Accelerator sweep for %s: %s", region, err)
 			return nil
 		}
