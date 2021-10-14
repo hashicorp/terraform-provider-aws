b62083eb10349120dbb9072c0ecd061f69233f4e
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:21:26 -0400

    kinesis: Create sweep package

--- aws/resource_aws_kinesis_stream_test.go
+++ aws/resource_aws_kinesis_stream_test.go
@@ -17,6 +17,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/provider"
+	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 )
 
 func init() {
@@ -27,7 +28,7 @@ func init() {
 }
 
 func testSweepKinesisStreams(region string) error {
-	client, err := sharedClientForRegion(region)
+	client, err := sweep.SharedRegionalSweepClient(region)
 	if err != nil {
 		return fmt.Errorf("error getting client: %s", err)
 	}
@@ -58,7 +59,7 @@ func testSweepKinesisStreams(region string) error {
 		return !lastPage
 	})
 
-	if testSweepSkipSweepError(err) {
+	if sweep.SkipSweepError(err) {
 		log.Printf("[WARN] Skipping Kinesis Stream sweep for %s: %s", region, err)
 		return nil
 	}
