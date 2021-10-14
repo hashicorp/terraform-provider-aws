1ba3c2ad6cd994f43783de9fdbbab8a0968ce7e2
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:22:45 -0400

    ses: Begin long road to new tags

--- aws/resource_aws_ses_receipt_rule.go
+++ aws/resource_aws_ses_receipt_rule.go
@@ -15,6 +15,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/create"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 func ResourceReceiptRule() *schema.Resource {
