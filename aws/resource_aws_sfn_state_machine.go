4ec6cdc379a4c09811558a3408a75d0a1ae9c610
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:22:45 -0400

    sfn: Begin long road to new tags

--- aws/resource_aws_sfn_state_machine.go
+++ aws/resource_aws_sfn_state_machine.go
@@ -11,11 +11,12 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-	"github.com/hashicorp/terraform-provider-aws/aws/internal/keyvaluetags"
+	tftags "github.com/hashicorp/terraform-provider-aws/aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/sfn/finder"
 	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/sfn/waiter"
 	"github.com/hashicorp/terraform-provider-aws/aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 func ResourceStateMachine() *schema.Resource {
@@ -88,8 +89,8 @@ func ResourceStateMachine() *schema.Resource {
 				Computed: true,
 			},
 
-			"tags":     tagsSchema(),
-			"tags_all": tagsSchemaComputed(),
+			"tags":     tftags.TagsSchema(),
+			"tags_all": tftags.TagsSchemaComputed(),
 
 			"type": {
 				Type:         schema.TypeString,
@@ -116,14 +117,14 @@ func ResourceStateMachine() *schema.Resource {
 			},
 		},
 
-		CustomizeDiff: SetTagsDiff,
+		CustomizeDiff: verify.SetTagsDiff,
 	}
 }
 
 func resourceStateMachineCreate(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*conns.AWSClient).SFNConn
 	defaultTagsConfig := meta.(*conns.AWSClient).DefaultTagsConfig
-	tags := defaultTagsConfig.MergeTags(keyvaluetags.New(d.Get("tags").(map[string]interface{})))
+	tags := defaultTagsConfig.MergeTags(tftags.New(d.Get("tags").(map[string]interface{})))
 
 	name := d.Get("name").(string)
 	input := &sfn.CreateStateMachineInput{
@@ -227,7 +228,7 @@ func resourceStateMachineRead(d *schema.ResourceData, meta interface{}) error {
 		d.Set("tracing_configuration", nil)
 	}
 
-	tags, err := keyvaluetags.SfnListTags(conn, d.Id())
+	tags, err := tftags.SfnListTags(conn, d.Id())
 
 	if err != nil {
 		if tfawserr.ErrCodeEquals(err, "UnknownOperationException") {
@@ -307,7 +308,7 @@ func resourceStateMachineUpdate(d *schema.ResourceData, meta interface{}) error 
 
 	if d.HasChange("tags_all") {
 		o, n := d.GetChange("tags_all")
-		if err := keyvaluetags.SfnUpdateTags(conn, d.Id(), o, n); err != nil {
+		if err := tftags.SfnUpdateTags(conn, d.Id(), o, n); err != nil {
 			return fmt.Errorf("error updating tags: %w", err)
 		}
 	}
