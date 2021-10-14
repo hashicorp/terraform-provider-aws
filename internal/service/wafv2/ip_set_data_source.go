bbb23171be908b63643402c5d3a454fb3ca3417e
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:44:07 -0400

    wafv2: Fix more import problems

--- internal/service/wafv2/ip_set_data_source.go
+++ internal/service/wafv2/ip_set_data_source.go
@@ -8,8 +8,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
-	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
-	"github.com/hashicorp/terraform-provider-aws/internal/verify"
+	"github.com/hashicorp/terraform-provider-aws/internal/flex"
 )
 
 func DataSourceIPSet() *schema.Resource {
