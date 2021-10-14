51cc91e5b14a39a3d9f346c7c10c3e1c3e586551
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:43:58 -0400

    nas: Fix more import problems

--- internal/service/nas/ip_ranges_data_source.go
+++ internal/service/nas/ip_ranges_data_source.go
@@ -11,9 +11,6 @@ import (
 
 	"github.com/hashicorp/go-cleanhttp"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hashicorp/terraform-provider-aws/internal/conns"
-	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
-	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 type dataSourceAwsIPRangesResult struct {
