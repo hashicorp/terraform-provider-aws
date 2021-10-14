2828eddefa640574f91e94bee2cb11bd935d8eea
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:39:09 -0400

    sfn: Fix many import problems

--- internal/service/sfn/status.go
+++ internal/service/sfn/status.go
@@ -4,15 +4,13 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/sfn"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/sfn/finder"
-	"github.com/hashicorp/terraform-provider-aws/aws/internal/tfresource"
+	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
-	tfsfn "github.com/hashicorp/terraform-provider-aws/internal/service/sfn"
 )
 
 func statusStateMachine(conn *sfn.SFN, stateMachineArn string) resource.StateRefreshFunc {
 	return func() (interface{}, string, error) {
-		output, err := tfsfn.FindStateMachineByARN(conn, stateMachineArn)
+		output, err := FindStateMachineByARN(conn, stateMachineArn)
 
 		if tfresource.NotFound(err) {
 			return nil, "", nil
