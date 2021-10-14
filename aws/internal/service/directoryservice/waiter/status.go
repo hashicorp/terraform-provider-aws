2a4117e8512432ea895f4dfd56b716ff990c23e9
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:25:48 -0400

    ds: Create verify package

--- aws/internal/service/directoryservice/waiter/status.go
+++ aws/internal/service/directoryservice/waiter/status.go
@@ -6,6 +6,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/directoryservice/finder"
 	"github.com/hashicorp/terraform-provider-aws/aws/internal/tfresource"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 func DirectoryStage(conn *directoryservice.DirectoryService, id string) resource.StateRefreshFunc {
--- aws/internal/service/directoryservice/waiter/waiter.go
+++ aws/internal/service/directoryservice/waiter/waiter.go
@@ -8,6 +8,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/directoryservice"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-provider-aws/aws/internal/tfresource"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 const (
