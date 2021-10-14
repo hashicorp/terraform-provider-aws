fcaaa81c099e34cdaedbf996757c8b40e348f45d
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 12:58:14 -0400

    cloudwatch: Making service sweeps

--- internal/service/cloudwatch/composite_alarm_test.go
+++ internal/service/cloudwatch/composite_alarm_test.go
@@ -21,70 +21,9 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 )
 
-func init() {
-	resource.AddTestSweepers("aws_cloudwatch_composite_alarm", &resource.Sweeper{
-		Name: "aws_cloudwatch_composite_alarm",
-		F:    sweepCompositeAlarms,
-	})
-}
 
-func sweepCompositeAlarms(region string) error {
-	client, err := sweep.SharedRegionalSweepClient(region)
-	if err != nil {
-		return fmt.Errorf("error getting client: %w", err)
-	}
 
-	conn := client.(*conns.AWSClient).CloudWatchConn
-	ctx := context.Background()
 
-	input := &cloudwatch.DescribeAlarmsInput{
-		AlarmTypes: aws.StringSlice([]string{cloudwatch.AlarmTypeCompositeAlarm}),
-	}
-
-	var sweeperErrs *multierror.Error
-
-	err = conn.DescribeAlarmsPagesWithContext(ctx, input, func(page *cloudwatch.DescribeAlarmsOutput, isLast bool) bool {
-		if page == nil {
-			return !isLast
-		}
-
-		for _, compositeAlarm := range page.CompositeAlarms {
-			if compositeAlarm == nil {
-				continue
-			}
-
-			name := aws.StringValue(compositeAlarm.AlarmName)
-
-			log.Printf("[INFO] Deleting CloudWatch Composite Alarm: %s", name)
-
-			r := tfcloudwatch.ResourceCompositeAlarm()
-			d := r.Data(nil)
-			d.SetId(name)
-
-			diags := r.DeleteContext(ctx, d, client)
-
-			for i := range diags {
-				if diags[i].Severity == diag.Error {
-					log.Printf("[ERROR] %s", diags[i].Summary)
-					sweeperErrs = multierror.Append(sweeperErrs, fmt.Errorf(diags[i].Summary))
-					continue
-				}
-			}
-		}
-
-		return !isLast
-	})
-
-	if sweep.SkipSweepError(err) {
-		log.Printf("[WARN] Skipping CloudWatch Composite Alarms sweep for %s: %s", region, err)
-		return sweeperErrs.ErrorOrNil() // In case we have completed some pages, but had errors
-	}
-	if err != nil {
-		sweeperErrs = multierror.Append(sweeperErrs, fmt.Errorf("error retrieving CloudWatch Composite Alarms: %w", err))
-	}
-
-	return sweeperErrs.ErrorOrNil()
-}
 
 func TestAccCloudWatchCompositeAlarm_basic(t *testing.T) {
 	suffix := sdkacctest.RandString(8)
