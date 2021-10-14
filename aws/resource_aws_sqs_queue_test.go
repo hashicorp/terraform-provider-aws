5515474ab8ef042ca2f9eb16707cc901f93063dc
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:21:37 -0400

    sqs: Create sweep package

--- aws/resource_aws_sqs_queue_test.go
+++ aws/resource_aws_sqs_queue_test.go
@@ -21,6 +21,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/provider"
+	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 )
 
 func init() {
@@ -40,7 +41,7 @@ func init() {
 }
 
 func testSweepSqsQueues(region string) error {
-	client, err := sharedClientForRegion(region)
+	client, err := sweep.SharedRegionalSweepClient(region)
 	if err != nil {
 		return fmt.Errorf("error getting client: %w", err)
 	}
@@ -69,7 +70,7 @@ func testSweepSqsQueues(region string) error {
 		return !lastPage
 	})
 
-	if testSweepSkipSweepError(err) {
+	if sweep.SkipSweepError(err) {
 		log.Printf("[WARN] Skipping SQS Queue sweep for %s: %s", region, err)
 		return sweeperErrs.ErrorOrNil() // In case we have completed some pages, but had errors
 	}
