6311875c7391fc330e22703f799aeee545eab32b
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 12:59:40 -0400

    codedeploy: Fix more import problems

--- internal/service/codedeploy/app_test.go
+++ internal/service/codedeploy/app_test.go
@@ -3,20 +3,17 @@ package codedeploy_test
 import (
 	"errors"
 	"fmt"
-	"log"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/codedeploy"
 	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
-	"github.com/hashicorp/go-multierror"
 	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	tfcodedeploy "github.com/hashicorp/terraform-provider-aws/internal/service/codedeploy"
-	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 )
 
 func TestAccCodeDeployApp_basic(t *testing.T) {
