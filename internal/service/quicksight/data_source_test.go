5e530ae3b1b381110240827832b502a49ad078be
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 12:58:29 -0400

    quicksight: Making service sweeps

--- internal/service/quicksight/data_source_test.go
+++ internal/service/quicksight/data_source_test.go
@@ -22,68 +22,9 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 )
 
-func init() {
-	resource.AddTestSweepers("aws_quicksight_data_source", &resource.Sweeper{
-		Name: "aws_quicksight_data_source",
-		F:    sweepsDataSource,
-	})
-}
 
-func sweepsDataSource(region string) error {
-	client, err := sweep.SharedRegionalSweepClient(region)
 
-	if err != nil {
-		return fmt.Errorf("error getting client: %w", err)
-	}
 
-	conn := client.(*conns.AWSClient).QuickSightConn
-	sweepResources := make([]*sweep.SweepResource, 0)
-	var errs *multierror.Error
-
-	ctx := context.Background()
-	awsAccountId := client.(*conns.AWSClient).AccountID
-
-	input := &quicksight.ListDataSourcesInput{
-		AwsAccountId: aws.String(awsAccountId),
-	}
-
-	err = conn.ListDataSourcesPagesWithContext(ctx, input, func(page *quicksight.ListDataSourcesOutput, lastPage bool) bool {
-		if page == nil {
-			return !lastPage
-		}
-
-		for _, ds := range page.DataSources {
-			if ds == nil {
-				continue
-			}
-
-			r := tfquicksight.ResourceDataSource()
-
-			d := r.Data(nil)
-
-			d.SetId(fmt.Sprintf("%s/%s", awsAccountId, aws.StringValue(ds.DataSourceId)))
-
-			sweepResources = append(sweepResources, sweep.NewSweepResource(r, d, client))
-		}
-
-		return !lastPage
-	})
-
-	if err != nil {
-		errs = multierror.Append(errs, fmt.Errorf("error describing QuickSigth Data Sources: %w", err))
-	}
-
-	if err := sweep.SweepOrchestrator(sweepResources); err != nil {
-		errs = multierror.Append(errs, fmt.Errorf("error sweeping QuickSight Data Sources for %s: %w", region, err))
-	}
-
-	if sweep.SkipSweepError(errs.ErrorOrNil()) {
-		log.Printf("[WARN] Skipping QuickSight Data Source sweep for %s: %s", region, errs)
-		return nil
-	}
-
-	return errs.ErrorOrNil()
-}
 
 func TestQuickSightDataSourcePermissionsDiff(t *testing.T) {
 	testCases := []struct {
