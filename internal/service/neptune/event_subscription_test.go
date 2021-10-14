f34b8c93686c199771e3568f2f2078ab20f23359
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 12:59:57 -0400

    neptune: Fix more import problems

--- internal/service/neptune/event_subscription_test.go
+++ internal/service/neptune/event_subscription_test.go
@@ -2,21 +2,17 @@ package neptune_test
 
 import (
 	"fmt"
-	"log"
 	"regexp"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/neptune"
 	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
-	"github.com/hashicorp/go-multierror"
 	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
-	tfneptune "github.com/hashicorp/terraform-provider-aws/internal/service/neptune"
-	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 )
 
 func TestAccNeptuneEventSubscription_basic(t *testing.T) {
