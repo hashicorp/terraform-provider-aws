e5f4403af275019439b453b8248196187c266c0d
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:43:42 -0400

    codebuild: Fix more import problems

--- internal/service/codebuild/report_group_test.go
+++ internal/service/codebuild/report_group_test.go
@@ -13,12 +13,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
-	"github.com/hashicorp/terraform-provider-aws/internal/provider"
+	tfcodebuild "github.com/hashicorp/terraform-provider-aws/internal/service/codebuild"
 	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
-	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
-	"github.com/hashicorp/terraform-provider-aws/internal/verify"
-	tfcodebuild "github.com/hashicorp/terraform-provider-aws/internal/service/codebuild"
-	tfcodebuild "github.com/hashicorp/terraform-provider-aws/internal/service/codebuild"
 )
 
 func init() {
