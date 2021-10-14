ea3c440ca00fa62f89424d96ec40e08d67b46032
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:51:06 -0400

    transfer: Create and run new generators

---
+++ internal/service/transfer/generate.go
@@ -0,0 +1,4 @@
+//go:generate go run -tags generate ../../generate/tags/main.go -ListTags=yes -ListTagsInIDElem=Arn -ServiceTagsSlice=yes -TagInIDElem=Arn -UpdateTags=yes
+// ONLY generate directives and package declaration! Do not add anything else to this file.
+
+package transfer
