3b578443e2ea67326c1c434b533f2c4734e55031
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 13:00:04 -0400

    storagegateway: Fix more import problems

--- internal/service/storagegateway/gateway_test.go
+++ internal/service/storagegateway/gateway_test.go
@@ -2,20 +2,17 @@ package storagegateway_test
 
 import (
 	"fmt"
-	"log"
 	"regexp"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/storagegateway"
-	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
 	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	tfstoragegateway "github.com/hashicorp/terraform-provider-aws/internal/service/storagegateway"
-	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 )
 
 func TestAccStorageGatewayGateway_GatewayType_cached(t *testing.T) {
