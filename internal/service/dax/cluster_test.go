16617a6c0386982b220ed0e27d89cea75ccc46f7
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 12:58:16 -0400

    dax: Making service sweeps

--- internal/service/dax/cluster_test.go
+++ internal/service/dax/cluster_test.go
@@ -17,50 +17,9 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 )
 
-func init() {
-	resource.AddTestSweepers("aws_dax_cluster", &resource.Sweeper{
-		Name: "aws_dax_cluster",
-		F:    sweepClusters,
-	})
-}
 
-func sweepClusters(region string) error {
-	client, err := sweep.SharedRegionalSweepClient(region)
-	if err != nil {
-		return fmt.Errorf("Error getting client: %s", err)
-	}
-	conn := client.(*conns.AWSClient).DAXConn
 
-	resp, err := conn.DescribeClusters(&dax.DescribeClustersInput{})
-	if err != nil {
-		// GovCloud (with no DAX support) has an endpoint that responds with:
-		// InvalidParameterValueException: Access Denied to API Version: DAX_V3
-		if sweep.SkipSweepError(err) || tfawserr.ErrMessageContains(err, "InvalidParameterValueException", "Access Denied to API Version: DAX_V3") {
-			log.Printf("[WARN] Skipping DAX Cluster sweep for %s: %s", region, err)
-			return nil
-		}
-		return fmt.Errorf("Error retrieving DAX clusters: %s", err)
-	}
 
-	if len(resp.Clusters) == 0 {
-		log.Print("[DEBUG] No DAX clusters to sweep")
-		return nil
-	}
-
-	log.Printf("[INFO] Found %d DAX clusters", len(resp.Clusters))
-
-	for _, cluster := range resp.Clusters {
-		log.Printf("[INFO] Deleting DAX cluster %s", *cluster.ClusterName)
-		_, err := conn.DeleteCluster(&dax.DeleteClusterInput{
-			ClusterName: cluster.ClusterName,
-		})
-		if err != nil {
-			return fmt.Errorf("Error deleting DAX cluster %s: %s", *cluster.ClusterName, err)
-		}
-	}
-
-	return nil
-}
 
 func TestAccDAXCluster_basic(t *testing.T) {
 	var dc dax.Cluster
