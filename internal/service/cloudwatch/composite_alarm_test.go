81df60ede2d9cbd05ad540ad43f77622118c73e7
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 12:59:39 -0400

    cloudwatch: Fix more import problems

--- internal/service/cloudwatch/composite_alarm_test.go
+++ internal/service/cloudwatch/composite_alarm_test.go
@@ -3,22 +3,17 @@ package cloudwatch_test
 import (
 	"context"
 	"fmt"
-	"log"
 	"regexp"
 	"testing"
 
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/cloudwatch"
 	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
-	"github.com/hashicorp/go-multierror"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	tfcloudwatch "github.com/hashicorp/terraform-provider-aws/internal/service/cloudwatch"
-	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 )
 
 func TestAccCloudWatchCompositeAlarm_basic(t *testing.T) {
