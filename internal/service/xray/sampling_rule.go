050c661f94e1449c08e333ad1fb5aed006e34284
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:44:08 -0400

    xray: Fix more import problems

--- internal/service/xray/sampling_rule.go
+++ internal/service/xray/sampling_rule.go
@@ -8,8 +8,8 @@ import (
 	"github.com/aws/aws-sdk-go/service/xray"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/flex"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
