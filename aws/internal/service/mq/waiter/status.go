a6734f5f88bdb8395837a4b254984240cc5618ec
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:26:01 -0400

    mq: Create verify package

--- aws/internal/service/mq/waiter/status.go
+++ aws/internal/service/mq/waiter/status.go
@@ -5,6 +5,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/mq"
 	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 func BrokerStatus(conn *mq.MQ, id string) resource.StateRefreshFunc {
