feccd61826a1c722995ec8fc2c225f589c255bf2
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 12:59:44 -0400

    ecr: Fix more import problems

--- internal/service/ecr/tags_gen.go
+++ internal/service/ecr/tags_gen.go
@@ -7,7 +7,6 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ecr"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
-	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 )
 
 // ListTags lists ecr service tags.
