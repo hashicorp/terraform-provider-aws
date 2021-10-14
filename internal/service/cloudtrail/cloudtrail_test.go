b36700e535255bc47c180554e1df1e7f35c2bdaf
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 12:58:14 -0400

    cloudtrail: Making service sweeps

--- internal/service/cloudtrail/cloudtrail_test.go
+++ internal/service/cloudtrail/cloudtrail_test.go
@@ -18,81 +18,9 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 )
 
-func init() {
-	resource.AddTestSweepers("aws_cloudtrail", &resource.Sweeper{
-		Name: "aws_cloudtrail",
-		F:    sweeps,
-	})
-}
 
-func sweeps(region string) error {
-	client, err := sweep.SharedRegionalSweepClient(region)
-	if err != nil {
-		return fmt.Errorf("error getting client: %w", err)
-	}
-	conn := client.(*conns.AWSClient).CloudTrailConn
-	var sweeperErrs *multierror.Error
 
-	err = conn.ListTrailsPages(&cloudtrail.ListTrailsInput{}, func(page *cloudtrail.ListTrailsOutput, lastPage bool) bool {
-		if page == nil {
-			return !lastPage
-		}
 
-		for _, trail := range page.Trails {
-			name := aws.StringValue(trail.Name)
-
-			if name == "AWSMacieTrail-DO-NOT-EDIT" {
-				log.Printf("[INFO] Skipping AWSMacieTrail-DO-NOT-EDIT for Macie Classic, which is not automatically recreated by the service")
-				continue
-			}
-
-			output, err := conn.DescribeTrails(&cloudtrail.DescribeTrailsInput{
-				TrailNameList: aws.StringSlice([]string{name}),
-			})
-			if err != nil {
-				sweeperErr := fmt.Errorf("error describing CloudTrail (%s): %w", name, err)
-				log.Printf("[ERROR] %s", sweeperErr)
-				sweeperErrs = multierror.Append(sweeperErrs, sweeperErr)
-				continue
-			}
-
-			if len(output.TrailList) == 0 {
-				log.Printf("[INFO] CloudTrail (%s) not found, skipping", name)
-				continue
-			}
-
-			if aws.BoolValue(output.TrailList[0].IsOrganizationTrail) {
-				log.Printf("[INFO] CloudTrail (%s) is an organization trail, skipping", name)
-				continue
-			}
-
-			log.Printf("[INFO] Deleting CloudTrail: %s", name)
-			_, err = conn.DeleteTrail(&cloudtrail.DeleteTrailInput{
-				Name: aws.String(name),
-			})
-			if tfawserr.ErrMessageContains(err, cloudtrail.ErrCodeTrailNotFoundException, "") {
-				continue
-			}
-			if err != nil {
-				sweeperErr := fmt.Errorf("error deleting CloudTrail (%s): %w", name, err)
-				log.Printf("[ERROR] %s", sweeperErr)
-				sweeperErrs = multierror.Append(sweeperErrs, sweeperErr)
-				continue
-			}
-		}
-
-		return !lastPage
-	})
-	if sweep.SkipSweepError(err) {
-		log.Printf("[WARN] Skipping CloudTrail sweep for %s: %s", region, err)
-		return sweeperErrs.ErrorOrNil() // In case we have completed some pages, but had errors
-	}
-	if err != nil {
-		sweeperErrs = multierror.Append(sweeperErrs, fmt.Errorf("error retrieving CloudTrails: %w", err))
-	}
-
-	return sweeperErrs.ErrorOrNil()
-}
 
 func TestAccCloudTrail_serial(t *testing.T) {
 	testCases := map[string]map[string]func(t *testing.T){
