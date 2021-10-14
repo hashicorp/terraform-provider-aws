3539bc848f68588bf1ff3eb06906a56d7b65a2ea
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:43:55 -0400

    lightsail: Fix more import problems

--- internal/service/lightsail/static_ip_attachment.go
+++ internal/service/lightsail/static_ip_attachment.go
@@ -8,8 +8,6 @@ import (
 	"github.com/aws/aws-sdk-go/service/lightsail"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
-	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
-	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 func ResourceStaticIPAttachment() *schema.Resource {
