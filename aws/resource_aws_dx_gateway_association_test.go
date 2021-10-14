4d3a2e67691dd3cb37a1e5927d76fdf011bd40ce
Angie Pinilla <angie@hashicorp.com>
2021-10-14 10:05:00 -0400

    set dx gateway association ID in cross-account sweeper

--- aws/resource_aws_dx_gateway_association_test.go
+++ aws/resource_aws_dx_gateway_association_test.go
@@ -137,6 +137,7 @@ func testSweepDirectConnectGatewayAssociations(region string) error {
 					r := resourceAwsDxGatewayAssociation()
 					d := r.Data(nil)
 					d.SetId(tfdirectconnect.GatewayAssociationCreateResourceID(directConnectGatewayID, transitGatewayID))
+					d.Set("dx_gateway_association_id", association.AssociationId)
 
 					sweepResources = append(sweepResources, NewTestSweepResource(r, d, client))
 				}
