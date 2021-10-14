d54166487f6c4fa9a70b82e15679343a2bdce03e
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:44:02 -0400

    route53recoverycontrolconfig: Fix more import problems

--- internal/service/route53recoverycontrolconfig/wait.go
+++ internal/service/route53recoverycontrolconfig/wait.go
@@ -5,7 +5,6 @@ import (
 
 	r53rcc "github.com/aws/aws-sdk-go/service/route53recoverycontrolconfig"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 const (
