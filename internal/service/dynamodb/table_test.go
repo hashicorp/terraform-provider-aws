42e40d9c640e296bfe8f46cab8fe26bc002c5acd
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 12:58:17 -0400

    dynamodb: Making service sweeps

--- internal/service/dynamodb/table_test.go
+++ internal/service/dynamodb/table_test.go
@@ -21,84 +21,9 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 )
 
-func init() {
-	resource.AddTestSweepers("aws_dynamodb_table", &resource.Sweeper{
-		Name: "aws_dynamodb_table",
-		F:    sweepTables,
-	})
-}
 
-func sweepTables(region string) error {
-	client, err := sweep.SharedRegionalSweepClient(region)
 
-	if err != nil {
-		return fmt.Errorf("error getting client: %s", err)
-	}
 
-	conn := client.(*conns.AWSClient).DynamoDBConn
-	sweepResources := make([]*sweep.SweepResource, 0)
-	var errs *multierror.Error
-	var g multierror.Group
-	var mutex = &sync.Mutex{}
-
-	err = conn.ListTablesPages(&dynamodb.ListTablesInput{}, func(page *dynamodb.ListTablesOutput, lastPage bool) bool {
-		if page == nil {
-			return !lastPage
-		}
-
-		for _, tableName := range page.TableNames {
-			r := tfdynamodb.ResourceTable()
-			d := r.Data(nil)
-
-			id := aws.StringValue(tableName)
-			d.SetId(id)
-
-			// read concurrently and gather errors
-			g.Go(func() error {
-				// Need to Read first to fill in byte_match_tuples attribute
-				err := r.Read(d, client)
-
-				if err != nil {
-					sweeperErr := fmt.Errorf("error reading DynamoDB Table (%s): %w", id, err)
-					log.Printf("[ERROR] %s", sweeperErr)
-					return sweeperErr
-				}
-
-				// In case it was already deleted
-				if d.Id() == "" {
-					return nil
-				}
-
-				mutex.Lock()
-				defer mutex.Unlock()
-				sweepResources = append(sweepResources, sweep.NewSweepResource(r, d, client))
-
-				return nil
-			})
-		}
-
-		return !lastPage
-	})
-
-	if err != nil {
-		errs = multierror.Append(errs, fmt.Errorf("error listing DynamoDB Tables for %s: %w", region, err))
-	}
-
-	if err = g.Wait().ErrorOrNil(); err != nil {
-		errs = multierror.Append(errs, fmt.Errorf("error concurrently reading DynamoDB Tables: %w", err))
-	}
-
-	if err = sweep.SweepOrchestrator(sweepResources); err != nil {
-		errs = multierror.Append(errs, fmt.Errorf("error sweeping DynamoDB Tables for %s: %w", region, err))
-	}
-
-	if sweep.SkipSweepError(errs.ErrorOrNil()) {
-		log.Printf("[WARN] Skipping DynamoDB Tables sweep for %s: %s", region, errs)
-		return nil
-	}
-
-	return errs.ErrorOrNil()
-}
 
 func TestUpdateDynamoDbDiffGSI(t *testing.T) {
 	testCases := []struct {
