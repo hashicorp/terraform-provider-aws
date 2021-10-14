2ccfd7cbd69e534c626c13d128215ab8a61b25cd
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:43:50 -0400

    globalaccelerator: Fix more import problems

--- internal/service/globalaccelerator/wait.go
+++ internal/service/globalaccelerator/wait.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/aws/aws-sdk-go/service/globalaccelerator"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 // waitAcceleratorDeployed waits for an Accelerator to return Deployed
