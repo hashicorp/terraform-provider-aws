40ed6d75248971ccc3fad4f70cfbc1245c3fecb3
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 12:58:20 -0400

    firehose: Making service sweeps

--- internal/service/firehose/delivery_stream_test.go
+++ internal/service/firehose/delivery_stream_test.go
@@ -21,56 +21,9 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 )
 
-func init() {
-	resource.AddTestSweepers("aws_kinesis_firehose_delivery_stream", &resource.Sweeper{
-		Name: "aws_kinesis_firehose_delivery_stream",
-		F:    sweepDeliveryStreams,
-	})
-}
 
-func sweepDeliveryStreams(region string) error {
-	client, err := sweep.SharedRegionalSweepClient(region)
-	if err != nil {
-		return fmt.Errorf("error getting client: %s", err)
-	}
-	conn := client.(*conns.AWSClient).FirehoseConn
-	input := &firehose.ListDeliveryStreamsInput{}
-	sweepResources := make([]*sweep.SweepResource, 0)
 
-	for {
-		page, err := conn.ListDeliveryStreams(input)
 
-		if sweep.SkipSweepError(err) {
-			log.Printf("[WARN] Skipping Kinesis Firehose Delivery Streams sweep for %s: %s", region, err)
-			return nil
-		}
-
-		if err != nil {
-			return fmt.Errorf("error listing Kinesis Firehose Delivery Streams: %w", err)
-		}
-
-		for _, sn := range page.DeliveryStreamNames {
-			r := tffirehose.ResourceDeliveryStream()
-			d := r.Data(nil)
-			d.SetId("???")
-			d.Set("name", sn)
-
-			sweepResources = append(sweepResources, sweep.NewSweepResource(r, d, client))
-		}
-
-		if !aws.BoolValue(page.HasMoreDeliveryStreams) {
-			break
-		}
-	}
-
-	err = sweep.SweepOrchestrator(sweepResources)
-
-	if err != nil {
-		return fmt.Errorf("error sweeping Kinesis Firehose Delivery Streams (%s): %w", region, err)
-	}
-
-	return nil
-}
 
 func TestAccFirehoseDeliveryStream_basic(t *testing.T) {
 	resourceName := "aws_kinesis_firehose_delivery_stream.test"
