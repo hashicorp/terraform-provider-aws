0665c93ef74e74ed83f1de23917bf7cc4fb47adf
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 12:58:17 -0400

    ds: Making service sweeps

--- internal/service/ds/directory_test.go
+++ internal/service/ds/directory_test.go
@@ -19,71 +19,9 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 )
 
-func init() {
-	resource.AddTestSweepers("aws_directory_service_directory", &resource.Sweeper{
-		Name: "aws_directory_service_directory",
-		F:    sweepDirectories,
-		Dependencies: []string{
-			"aws_db_instance",
-			"aws_ec2_client_vpn_endpoint",
-			"aws_fsx_windows_file_system",
-			"aws_transfer_server",
-			"aws_workspaces_directory",
-		},
-	})
-}
 
-func sweepDirectories(region string) error {
-	client, err := sweep.SharedRegionalSweepClient(region)
 
-	if err != nil {
-		return fmt.Errorf("error getting client: %w", err)
-	}
 
-	conn := client.(*conns.AWSClient).DirectoryServiceConn
-
-	var sweeperErrs *multierror.Error
-
-	input := &directoryservice.DescribeDirectoriesInput{}
-
-	err = tfds.DescribeDirectoriesPagesWithContext(context.TODO(), conn, input, func(page *directoryservice.DescribeDirectoriesOutput, lastPage bool) bool {
-		if page == nil {
-			return !lastPage
-		}
-
-		for _, directory := range page.DirectoryDescriptions {
-			id := aws.StringValue(directory.DirectoryId)
-
-			r := tfds.ResourceDirectory()
-			d := r.Data(nil)
-			d.SetId(id)
-
-			err := r.Delete(d, client)
-
-			if err != nil {
-				sweeperErr := fmt.Errorf("error deleting Directory Service Directory (%s): %w", id, err)
-				log.Printf("[ERROR] %s", sweeperErr)
-				sweeperErrs = multierror.Append(sweeperErrs, sweeperErr)
-				continue
-			}
-		}
-
-		return !lastPage
-	})
-
-	if sweep.SkipSweepError(err) {
-		log.Printf("[WARN] Skipping Directory Service Directory sweep for %s: %s", region, err)
-		return sweeperErrs.ErrorOrNil()
-	}
-
-	if err != nil {
-		sweeperErr := fmt.Errorf("error listing Directory Service Directories: %w", err)
-		log.Printf("[ERROR] %s", sweeperErr)
-		sweeperErrs = multierror.Append(sweeperErrs, sweeperErr)
-	}
-
-	return sweeperErrs.ErrorOrNil()
-}
 
 func TestAccDirectoryServiceDirectory_basic(t *testing.T) {
 	var ds directoryservice.DirectoryDescription
