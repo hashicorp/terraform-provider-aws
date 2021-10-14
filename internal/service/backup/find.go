1290c1fbe16e2396316aac8903333fedcb941cbd
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:43:40 -0400

    backup: Fix more import problems

--- internal/service/backup/find.go
+++ internal/service/backup/find.go
@@ -5,7 +5,6 @@ import (
 	"github.com/aws/aws-sdk-go/service/backup"
 	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 func FindBackupVaultAccessPolicyByName(conn *backup.Backup, name string) (*backup.GetBackupVaultAccessPolicyOutput, error) {
