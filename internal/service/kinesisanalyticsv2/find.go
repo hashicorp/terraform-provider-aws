97bb47c498a9667c14be2dfaff22e0d4b1871468
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:43:54 -0400

    kinesisanalyticsv2: Fix more import problems

--- internal/service/kinesisanalyticsv2/find.go
+++ internal/service/kinesisanalyticsv2/find.go
@@ -5,7 +5,6 @@ import (
 	"github.com/aws/aws-sdk-go/service/kinesisanalyticsv2"
 	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 // FindApplicationDetailByName returns the application corresponding to the specified name.
--- internal/service/kinesisanalyticsv2/id.go
+++ internal/service/kinesisanalyticsv2/id.go
@@ -3,7 +3,6 @@ package kinesisanalyticsv2
 import (
 	"fmt"
 	"strings"
-	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 const applicationSnapshotIDSeparator = "/"
