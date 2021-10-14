082e40a8bde9efd5af838d0ea8996722ea2d5b73
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:21:14 -0400

    cloudtrail: Create sweep package

--- aws/resource_aws_cloudtrail_test.go
+++ aws/resource_aws_cloudtrail_test.go
@@ -15,6 +15,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/provider"
+	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 )
 
 func init() {
@@ -25,7 +26,7 @@ func init() {
 }
 
 func testSweepCloudTrails(region string) error {
-	client, err := sharedClientForRegion(region)
+	client, err := sweep.SharedRegionalSweepClient(region)
 	if err != nil {
 		return fmt.Errorf("error getting client: %w", err)
 	}
@@ -82,7 +83,7 @@ func testSweepCloudTrails(region string) error {
 
 		return !lastPage
 	})
-	if testSweepSkipSweepError(err) {
+	if sweep.SkipSweepError(err) {
 		log.Printf("[WARN] Skipping CloudTrail sweep for %s: %s", region, err)
 		return sweeperErrs.ErrorOrNil() // In case we have completed some pages, but had errors
 	}
