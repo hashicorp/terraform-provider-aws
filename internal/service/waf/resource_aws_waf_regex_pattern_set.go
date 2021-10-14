5048c50071eba8598bc62edac975e81e3826cc79
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:39:11 -0400

    waf: Fix many import problems

--- internal/service/waf/resource_aws_waf_regex_pattern_set.go
+++ internal/service/waf/resource_aws_waf_regex_pattern_set.go
@@ -1,4 +1,4 @@
-package aws
+package waf
 
 import (
 	"fmt"
--- internal/service/waf/resource_aws_waf_rule_group.go
+++ internal/service/waf/resource_aws_waf_rule_group.go
@@ -1,4 +1,4 @@
-package aws
+package waf
 
 import (
 	"fmt"
@@ -8,7 +8,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws/arn"
 	"github.com/aws/aws-sdk-go/service/waf"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	tftags "github.com/hashicorp/terraform-provider-aws/aws/internal/tags"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
