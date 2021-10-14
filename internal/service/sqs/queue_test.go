27cace1cc8095ee41422a12f510a327f0fc3c863
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 12:58:33 -0400

    sqs: Making service sweeps

--- internal/service/sqs/queue_test.go
+++ internal/service/sqs/queue_test.go
@@ -22,63 +22,9 @@ import (
 	awspolicy "github.com/jen20/awspolicyequivalence"
 )
 
-func init() {
-	resource.AddTestSweepers("aws_sqs_queue", &resource.Sweeper{
-		Name: "aws_sqs_queue",
-		F:    sweepQueues,
-		Dependencies: []string{
-			"aws_autoscaling_group",
-			"aws_cloudwatch_event_rule",
-			"aws_elastic_beanstalk_environment",
-			"aws_iot_topic_rule",
-			"aws_lambda_function",
-			"aws_s3_bucket",
-			"aws_sns_topic",
-		},
-	})
-}
 
-func sweepQueues(region string) error {
-	client, err := sweep.SharedRegionalSweepClient(region)
-	if err != nil {
-		return fmt.Errorf("error getting client: %w", err)
-	}
-	conn := client.(*conns.AWSClient).SQSConn
-	input := &sqs.ListQueuesInput{}
-	var sweeperErrs *multierror.Error
 
-	err = conn.ListQueuesPages(input, func(page *sqs.ListQueuesOutput, lastPage bool) bool {
-		if page == nil {
-			return !lastPage
-		}
 
-		for _, queueUrl := range page.QueueUrls {
-			r := tfsqs.ResourceQueue()
-			d := r.Data(nil)
-			d.SetId(aws.StringValue(queueUrl))
-			err = r.Delete(d, client)
-
-			if err != nil {
-				log.Printf("[ERROR] %s", err)
-				sweeperErrs = multierror.Append(sweeperErrs, err)
-				continue
-			}
-		}
-
-		return !lastPage
-	})
-
-	if sweep.SkipSweepError(err) {
-		log.Printf("[WARN] Skipping SQS Queue sweep for %s: %s", region, err)
-		return sweeperErrs.ErrorOrNil() // In case we have completed some pages, but had errors
-	}
-
-	if err != nil {
-		sweeperErrs = multierror.Append(sweeperErrs, fmt.Errorf("error listing SQS Queues: %w", err))
-	}
-
-	return sweeperErrs.ErrorOrNil()
-}
 
 func TestAccSQSQueue_basic(t *testing.T) {
 	var queueAttributes map[string]string
