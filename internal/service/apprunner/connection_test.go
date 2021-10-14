c77b19ac23a4e23505a7f23e9dee73a8fa9a1a93
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 12:59:37 -0400

    apprunner: Fix more import problems

--- internal/service/apprunner/connection_test.go
+++ internal/service/apprunner/connection_test.go
@@ -3,21 +3,17 @@ package apprunner_test
 import (
 	"context"
 	"fmt"
-	"log"
 	"regexp"
 	"testing"
 
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/apprunner"
 	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
-	"github.com/hashicorp/go-multierror"
 	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	tfapprunner "github.com/hashicorp/terraform-provider-aws/internal/service/apprunner"
-	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 )
 
 func TestAccAppRunnerConnection_basic(t *testing.T) {
