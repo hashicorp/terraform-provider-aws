aeba9e1fff5e9913f386c1206960d1baf3030817
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 12:58:18 -0400

    ecr: Making service sweeps

--- internal/service/ecr/repository_test.go
+++ internal/service/ecr/repository_test.go
@@ -19,58 +19,9 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
-func init() {
-	resource.AddTestSweepers("aws_ecr_repository", &resource.Sweeper{
-		Name: "aws_ecr_repository",
-		F:    sweepRepositories,
-	})
-}
 
-func sweepRepositories(region string) error {
-	client, err := sweep.SharedRegionalSweepClient(region)
-	if err != nil {
-		return fmt.Errorf("error getting client: %w", err)
-	}
-	conn := client.(*conns.AWSClient).ECRConn
 
-	var errors error
-	err = conn.DescribeRepositoriesPages(&ecr.DescribeRepositoriesInput{}, func(page *ecr.DescribeRepositoriesOutput, lastPage bool) bool {
-		if page == nil {
-			return !lastPage
-		}
 
-		for _, repository := range page.Repositories {
-			repositoryName := aws.StringValue(repository.RepositoryName)
-			log.Printf("[INFO] Deleting ECR repository: %s", repositoryName)
-
-			_, err = conn.DeleteRepository(&ecr.DeleteRepositoryInput{
-				// We should probably sweep repositories even if there are images.
-				Force:          aws.Bool(true),
-				RegistryId:     repository.RegistryId,
-				RepositoryName: repository.RepositoryName,
-			})
-			if err != nil {
-				if !tfawserr.ErrMessageContains(err, ecr.ErrCodeRepositoryNotFoundException, "") {
-					sweeperErr := fmt.Errorf("Error deleting ECR repository (%s): %w", repositoryName, err)
-					log.Printf("[ERROR] %s", sweeperErr)
-					errors = multierror.Append(errors, sweeperErr)
-				}
-				continue
-			}
-		}
-
-		return !lastPage
-	})
-	if err != nil {
-		if sweep.SkipSweepError(err) {
-			log.Printf("[WARN] Skipping ECR repository sweep for %s: %s", region, err)
-			return nil
-		}
-		errors = multierror.Append(errors, fmt.Errorf("Error retreiving ECR repositories: %w", err))
-	}
-
-	return errors
-}
 
 func TestAccECRRepository_basic(t *testing.T) {
 	var v ecr.Repository
