2d0b6c10802bd0edae5c8f58b6a35489a0d86e3e
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:43:48 -0400

    elasticache: Fix more import problems

--- internal/service/elasticache/parameter_group.go
+++ internal/service/elasticache/parameter_group.go
@@ -13,10 +13,10 @@ import (
 	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/create"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
-	"github.com/hashicorp/terraform-provider-aws/internal/conns"
-	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
+	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
--- internal/service/elasticache/replication_group_data_source.go
+++ internal/service/elasticache/replication_group_data_source.go
@@ -9,8 +9,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/elasticache"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
-	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
-	"github.com/hashicorp/terraform-provider-aws/internal/verify"
+	"github.com/hashicorp/terraform-provider-aws/internal/flex"
 )
 
 func DataSourceReplicationGroup() *schema.Resource {
