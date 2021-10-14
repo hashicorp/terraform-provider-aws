d71a019695ca23e57f3128f3365aa5f1e413ffd1
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:22:49 -0400

    worklink: Begin long road to new tags

--- aws/resource_aws_worklink_website_certificate_authority_association.go
+++ aws/resource_aws_worklink_website_certificate_authority_association.go
@@ -12,6 +12,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 func ResourceWebsiteCertificateAuthorityAssociation() *schema.Resource {
