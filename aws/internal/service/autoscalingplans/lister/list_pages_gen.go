434777eee25ee960912e6df23bd4ce82b3129008
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:25:41 -0400

    autoscalingplans: Create verify package

--- aws/internal/service/autoscalingplans/lister/list_pages_gen.go
+++ aws/internal/service/autoscalingplans/lister/list_pages_gen.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/autoscalingplans"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 func DescribeScalingPlansPages(conn *autoscalingplans.AutoScalingPlans, input *autoscalingplans.DescribeScalingPlansInput, fn func(*autoscalingplans.DescribeScalingPlansOutput, bool) bool) error {
--- aws/internal/service/autoscalingplans/waiter/status.go
+++ aws/internal/service/autoscalingplans/waiter/status.go
@@ -6,6 +6,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/autoscalingplans/finder"
 	"github.com/hashicorp/terraform-provider-aws/aws/internal/tfresource"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 func ScalingPlanStatusCode(conn *autoscalingplans.AutoScalingPlans, scalingPlanName string, scalingPlanVersion int) resource.StateRefreshFunc {
