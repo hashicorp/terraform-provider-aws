2a07e5636d3b2a16a12f0ba435b825b0459d6fc8
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 12:58:23 -0400

    kinesis: Making service sweeps

--- internal/service/kinesis/stream_test.go
+++ internal/service/kinesis/stream_test.go
@@ -20,56 +20,9 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 )
 
-func init() {
-	resource.AddTestSweepers("aws_kinesis_stream", &resource.Sweeper{
-		Name: "aws_kinesis_stream",
-		F:    sweepStreams,
-	})
-}
 
-func sweepStreams(region string) error {
-	client, err := sweep.SharedRegionalSweepClient(region)
-	if err != nil {
-		return fmt.Errorf("error getting client: %s", err)
-	}
-	conn := client.(*conns.AWSClient).KinesisConn
-	input := &kinesis.ListStreamsInput{}
-	var sweeperErrs *multierror.Error
 
-	err = conn.ListStreamsPages(input, func(page *kinesis.ListStreamsOutput, lastPage bool) bool {
-		for _, streamName := range page.StreamNames {
-			if streamName == nil {
-				continue
-			}
 
-			r := tfkinesis.ResourceStream()
-			d := r.Data(nil)
-			d.Set("name", streamName)
-			d.Set("enforce_consumer_deletion", true)
-
-			err := r.Delete(d, client)
-
-			if err != nil {
-				sweeperErr := fmt.Errorf("error deleting Kinesis Stream (%s): %w", aws.StringValue(streamName), err)
-				log.Printf("[ERROR] %s", sweeperErr)
-				sweeperErrs = multierror.Append(sweeperErrs, sweeperErr)
-			}
-		}
-
-		return !lastPage
-	})
-
-	if sweep.SkipSweepError(err) {
-		log.Printf("[WARN] Skipping Kinesis Stream sweep for %s: %s", region, err)
-		return nil
-	}
-
-	if err != nil {
-		return fmt.Errorf("Error listing Kinesis Streams: %s", err)
-	}
-
-	return sweeperErrs.ErrorOrNil()
-}
 
 func TestAccKinesisStream_basic(t *testing.T) {
 	var stream kinesis.StreamDescription
