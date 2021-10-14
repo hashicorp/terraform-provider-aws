165d3a9f0603e4c52f70a1167b1d63f07c92aa64
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:22:43 -0400

    route53recoverycontrolconfig: Begin long road to new tags

--- aws/resource_aws_route53recoverycontrolconfig_safety_rule.go
+++ aws/resource_aws_route53recoverycontrolconfig_safety_rule.go
@@ -12,6 +12,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/route53recoverycontrolconfig/waiter"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 func ResourceSafetyRule() *schema.Resource {
