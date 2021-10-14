e7087649e5d060b594020765c1b66488c3a11811
Dirk Avery <dirk.avery@gmail.com>
2021-10-14 11:39:00 -0400

    macie2: Fix many import problems

--- internal/service/macie2/status.go
+++ internal/service/macie2/status.go
@@ -4,15 +4,13 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/macie2"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/macie2/finder"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
-	tfmacie2 "github.com/hashicorp/terraform-provider-aws/internal/service/macie2"
 )
 
 // statusMemberRelationship fetches the Member and its relationship status
 func statusMemberRelationship(conn *macie2.Macie2, adminAccountID string) resource.StateRefreshFunc {
 	return func() (interface{}, string, error) {
-		adminAccount, err := tfmacie2.findMemberNotAssociated(conn, adminAccountID)
+		adminAccount, err := findMemberNotAssociated(conn, adminAccountID)
 
 		if err != nil {
 			return nil, "Unknown", err
