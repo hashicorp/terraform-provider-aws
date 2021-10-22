b2eb2a261ee7607bb9762b1f76018f2a58b4dd45
Edgar Lopez <edgarlopez@pop-os.localdomain>
2021-10-22 17:33:52 -0600

    feat: added resource and docs for appstream directory config

---
+++ internal/service/appstream/directory_config.go
@@ -0,0 +1,205 @@
+package appstream
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/appstream"
+	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/flex"
+	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
+)
+
+func ResourceDirectoryConfig() *schema.Resource {
+	return &schema.Resource{
+		CreateWithoutTimeout: resourceDirectoryConfigCreate,
+		ReadWithoutTimeout:   resourceDirectoryConfigRead,
+		UpdateWithoutTimeout: resourceDirectoryConfigUpdate,
+		DeleteWithoutTimeout: resourceDirectoryConfigDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+		Schema: map[string]*schema.Schema{
+			"created_time": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"directory_name": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"organizational_unit_distinguished_names": {
+				Type:     schema.TypeSet,
+				Required: true,
+				Elem: &schema.Schema{
+					Type:         schema.TypeString,
+					ValidateFunc: validation.StringLenBetween(0, 2000),
+				},
+				Set: schema.HashString,
+			},
+			"service_account_credentials": {
+				Type:     schema.TypeList,
+				MaxItems: 1,
+				Required: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"account_name": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"account_password": {
+							Type:      schema.TypeString,
+							Required:  true,
+							Sensitive: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func resourceDirectoryConfigCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn := meta.(*conns.AWSClient).AppStreamConn
+	input := &appstream.CreateDirectoryConfigInput{
+		DirectoryName:                        aws.String(d.Get("directory_name").(string)),
+		OrganizationalUnitDistinguishedNames: flex.ExpandStringSet(d.Get("organizational_unit_distinguished_names").(*schema.Set)),
+		ServiceAccountCredentials:            expandServiceAccountCredentials(d.Get("service_account_credentials").([]interface{})),
+	}
+
+	var err error
+	var output *appstream.CreateDirectoryConfigOutput
+	err = resource.RetryContext(ctx, directoryConfigTimeout, func() *resource.RetryError {
+		output, err = conn.CreateDirectoryConfigWithContext(ctx, input)
+		if err != nil {
+			if tfawserr.ErrCodeEquals(err, appstream.ErrCodeResourceNotFoundException) {
+				return resource.RetryableError(err)
+			}
+
+			return resource.NonRetryableError(err)
+		}
+
+		return nil
+	})
+
+	if tfresource.TimedOut(err) {
+		output, err = conn.CreateDirectoryConfigWithContext(ctx, input)
+	}
+	if err != nil {
+		return diag.FromErr(fmt.Errorf("error creating Appstream DirectoryConfig (%s): %w", d.Id(), err))
+	}
+
+	d.SetId(aws.StringValue(output.DirectoryConfig.DirectoryName))
+
+	return resourceDirectoryConfigRead(ctx, d, meta)
+}
+
+func resourceDirectoryConfigRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn := meta.(*conns.AWSClient).AppStreamConn
+
+	resp, err := conn.DescribeDirectoryConfigsWithContext(ctx, &appstream.DescribeDirectoryConfigsInput{DirectoryNames: []*string{aws.String(d.Id())}})
+
+	if !d.IsNewResource() && tfawserr.ErrCodeEquals(err, appstream.ErrCodeResourceNotFoundException) {
+		log.Printf("[WARN] Appstream DirectoryConfig (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	if err != nil {
+		return diag.FromErr(fmt.Errorf("error reading Appstream DirectoryConfig (%s): %w", d.Id(), err))
+	}
+
+	if len(resp.DirectoryConfigs) == 0 {
+		return diag.FromErr(fmt.Errorf("error reading Appstream DirectoryConfig (%s): %s", d.Id(), "empty response"))
+	}
+
+	if len(resp.DirectoryConfigs) > 1 {
+		return diag.FromErr(fmt.Errorf("error reading Appstream DirectoryConfig (%s): %s", d.Id(), "multiple directories config found"))
+	}
+
+	directoryConfig := resp.DirectoryConfigs[0]
+
+	d.Set("created_time", aws.TimeValue(directoryConfig.CreatedTime).Format(time.RFC3339))
+	d.Set("directory_name", directoryConfig.DirectoryName)
+	d.Set("organizational_unit_distinguished_names", flex.FlattenStringSet(directoryConfig.OrganizationalUnitDistinguishedNames))
+
+	if err = d.Set("service_account_credentials", flattenServiceAccountCredentials(directoryConfig.ServiceAccountCredentials, d)); err != nil {
+		return diag.FromErr(fmt.Errorf("error setting `%s` for AppStream DirectoryConfig (%s): %w", "service_account_credentials", d.Id(), err))
+	}
+
+	return nil
+}
+
+func resourceDirectoryConfigUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn := meta.(*conns.AWSClient).AppStreamConn
+	input := &appstream.UpdateDirectoryConfigInput{
+		DirectoryName: aws.String(d.Id()),
+	}
+
+	if d.HasChange("organizational_unit_distinguished_names") {
+		input.OrganizationalUnitDistinguishedNames = flex.ExpandStringSet(d.Get("organizational_unit_distinguished_names").(*schema.Set))
+	}
+
+	if d.HasChange("service_account_credentials") {
+		input.ServiceAccountCredentials = expandServiceAccountCredentials(d.Get("service_account_credentials").([]interface{}))
+	}
+
+	_, err := conn.UpdateDirectoryConfigWithContext(ctx, input)
+	if err != nil {
+		return diag.FromErr(fmt.Errorf("error updating Appstream DirectoryConfig (%s): %w", d.Id(), err))
+	}
+
+	return resourceDirectoryConfigRead(ctx, d, meta)
+}
+
+func resourceDirectoryConfigDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn := meta.(*conns.AWSClient).AppStreamConn
+
+	_, err := conn.DeleteDirectoryConfigWithContext(ctx, &appstream.DeleteDirectoryConfigInput{
+		DirectoryName: aws.String(d.Id()),
+	})
+
+	if err != nil {
+		if tfawserr.ErrCodeEquals(err, appstream.ErrCodeResourceNotFoundException) {
+			return nil
+		}
+		return diag.FromErr(fmt.Errorf("error deleting Appstream DirectoryConfig (%s): %w", d.Id(), err))
+	}
+	return nil
+}
+
+func expandServiceAccountCredentials(tfList []interface{}) *appstream.ServiceAccountCredentials {
+	if len(tfList) == 0 {
+		return nil
+	}
+
+	attr := tfList[0].(map[string]interface{})
+
+	apiObject := &appstream.ServiceAccountCredentials{
+		AccountName:     aws.String(attr["account_name"].(string)),
+		AccountPassword: aws.String(attr["account_password"].(string)),
+	}
+
+	return apiObject
+}
+
+func flattenServiceAccountCredentials(apiObject *appstream.ServiceAccountCredentials, d *schema.ResourceData) []interface{} {
+	if apiObject == nil {
+		return nil
+	}
+
+	tfList := map[string]interface{}{}
+	tfList["account_name"] = aws.StringValue(apiObject.AccountName)
+	tfList["account_password"] = d.Get("service_account_credentials.0.account_password").(string)
+
+	return []interface{}{tfList}
+}
---
+++ internal/service/appstream/directory_config_test.go
@@ -0,0 +1,156 @@
+package appstream_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/appstream"
+	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
+	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	tfappstream "github.com/hashicorp/terraform-provider-aws/internal/service/appstream"
+)
+
+func TestAccAppStreamDirectoryConfig_basic(t *testing.T) {
+	var directoryOutput appstream.DirectoryConfig
+	resourceName := "aws_appstream_directory_config.test"
+	rName := acctest.RandomDomainName()
+	rUserName := fmt.Sprintf("%s\\%s", rName, sdkacctest.RandString(10))
+	rPassword := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	rUserNameUpdated := fmt.Sprintf("%s\\%s", rName, sdkacctest.RandString(10))
+	rPasswordUpdated := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheck(t)
+			acctest.PreCheckHasIAMRole(t, "AmazonAppStreamServiceAccess")
+		},
+		ProviderFactories: acctest.ProviderFactories,
+		CheckDestroy:      testAccCheckDirectoryConfigDestroy,
+		ErrorCheck:        acctest.ErrorCheck(t, appstream.EndpointsID),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDirectoryConfigConfig(rName, rUserName, rPassword),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDirectoryConfigExists(resourceName, &directoryOutput),
+					resource.TestCheckResourceAttr(resourceName, "directory_name", rName),
+					acctest.CheckResourceAttrRFC3339(resourceName, "created_time"),
+				),
+			},
+			{
+				Config: testAccDirectoryConfigConfig(rName, rUserNameUpdated, rPasswordUpdated),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDirectoryConfigExists(resourceName, &directoryOutput),
+					resource.TestCheckResourceAttr(resourceName, "directory_name", rName),
+					acctest.CheckResourceAttrRFC3339(resourceName, "created_time"),
+				),
+			},
+			{
+				ResourceName:            resourceName,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"service_account_credentials.0.account_password"},
+			},
+		},
+	})
+}
+
+func TestAccAppStreamDirectoryConfig_disappears(t *testing.T) {
+	var directoryOutput appstream.DirectoryConfig
+	resourceName := "aws_appstream_directory_config.test"
+	rName := acctest.RandomDomainName()
+	rUserName := fmt.Sprintf("%s\\%s", rName, sdkacctest.RandString(10))
+	rPassword := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheck(t)
+			acctest.PreCheckHasIAMRole(t, "AmazonAppStreamServiceAccess")
+		},
+		ProviderFactories: acctest.ProviderFactories,
+		CheckDestroy:      testAccCheckDirectoryConfigDestroy,
+		ErrorCheck:        acctest.ErrorCheck(t, appstream.EndpointsID),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDirectoryConfigConfig(rName, rUserName, rPassword),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDirectoryConfigExists(resourceName, &directoryOutput),
+					acctest.CheckResourceDisappears(acctest.Provider, tfappstream.ResourceDirectoryConfig(), resourceName),
+				),
+				ExpectNonEmptyPlan: true,
+			},
+		},
+	})
+}
+func testAccCheckDirectoryConfigExists(resourceName string, appStreamDirectoryConfig *appstream.DirectoryConfig) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[resourceName]
+		if !ok {
+			return fmt.Errorf("not found: %s", resourceName)
+		}
+
+		conn := acctest.Provider.Meta().(*conns.AWSClient).AppStreamConn
+		resp, err := conn.DescribeDirectoryConfigs(&appstream.DescribeDirectoryConfigsInput{DirectoryNames: []*string{aws.String(rs.Primary.ID)}})
+
+		if err != nil {
+			return err
+		}
+
+		if resp == nil && len(resp.DirectoryConfigs) == 0 {
+			return fmt.Errorf("appstream directory config %q does not exist", rs.Primary.ID)
+		}
+
+		*appStreamDirectoryConfig = *resp.DirectoryConfigs[0]
+
+		return nil
+	}
+}
+
+func testAccCheckDirectoryConfigDestroy(s *terraform.State) error {
+	conn := acctest.Provider.Meta().(*conns.AWSClient).AppStreamConn
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "aws_appstream_directory_config" {
+			continue
+		}
+
+		resp, err := conn.DescribeDirectoryConfigs(&appstream.DescribeDirectoryConfigsInput{DirectoryNames: []*string{aws.String(rs.Primary.ID)}})
+
+		if tfawserr.ErrCodeEquals(err, appstream.ErrCodeResourceNotFoundException) {
+			continue
+		}
+
+		if err != nil {
+			return err
+		}
+
+		if resp != nil && len(resp.DirectoryConfigs) > 0 {
+			return fmt.Errorf("appstream directory config %q still exists", rs.Primary.ID)
+		}
+	}
+
+	return nil
+}
+
+func testAccDirectoryConfigConfig(name, userName, password string) string {
+	return fmt.Sprintf(`
+data "aws_organizations_organization" "test" {}
+
+data "aws_organizations_organizational_units" "test" {
+  parent_id = data.aws_organizations_organization.test.roots[0].id
+}
+
+resource "aws_appstream_directory_config" "test" {
+  directory_name                          = %[1]q
+  organizational_unit_distinguished_names = data.aws_organizations_organizational_units.test.children.*.id
+  service_account_credentials{
+    account_name     = %[2]q
+    account_password = %[3]q
+  }
+}
+`, name, userName, password)
+}
