74403802fd3ff60a07558613c8925690cdb34c06
Kit Ewbank <Kit_Ewbank@hotmail.com>
2021-10-25 17:14:34 -0400

    r/aws-s3_bucket: Incorporate RTC modifications after Service Package restructure.

--- internal/service/s3/bucket.go
+++ internal/service/s3/bucket.go
@@ -456,6 +456,49 @@ func ResourceBucket() *schema.Resource {
 														},
 													},
 												},
+												"replication_time": {
+													Type:     schema.TypeList,
+													Optional: true,
+													MaxItems: 1,
+													Elem: &schema.Resource{
+														Schema: map[string]*schema.Schema{
+															"minutes": {
+																Type:         schema.TypeInt,
+																Optional:     true,
+																Default:      15,
+																ValidateFunc: validation.IntBetween(15, 15),
+															},
+															"status": {
+																Type:         schema.TypeString,
+																Optional:     true,
+																Default:      s3.ReplicationTimeStatusEnabled,
+																ValidateFunc: validation.StringInSlice(s3.ReplicationTimeStatus_Values(), false),
+															},
+														},
+													},
+												},
+												"metrics": {
+													Type:     schema.TypeList,
+													Optional: true,
+													MinItems: 1,
+													MaxItems: 1,
+													Elem: &schema.Resource{
+														Schema: map[string]*schema.Schema{
+															"minutes": {
+																Type:         schema.TypeInt,
+																Optional:     true,
+																Default:      15,
+																ValidateFunc: validation.IntBetween(10, 15),
+															},
+															"status": {
+																Type:         schema.TypeString,
+																Optional:     true,
+																Default:      s3.MetricsStatusEnabled,
+																ValidateFunc: validation.StringInSlice(s3.MetricsStatus_Values(), false),
+															},
+														},
+													},
+												},
 											},
 										},
 									},
@@ -2043,6 +2086,7 @@ func resourceBucketReplicationConfigurationUpdate(conn *s3.S3, d *schema.Resourc
 			rcRule.ID = aws.String(rrid.(string))
 		}
 
+		rtcEnabled := false // Note: RTC requires schemaV2
 		ruleDestination := &s3.Destination{}
 		if dest, ok := rr["destination"].([]interface{}); ok && len(dest) > 0 {
 			if dest[0] != nil {
@@ -2069,6 +2113,40 @@ func resourceBucketReplicationConfigurationUpdate(conn *s3.S3, d *schema.Resourc
 					ruleAclTranslation.Owner = aws.String(aclTranslationValues["owner"].(string))
 					ruleDestination.AccessControlTranslation = ruleAclTranslation
 				}
+
+				// replication metrics (required for RTC)
+				metricsEnabled := false
+				if metrics, ok := bd["metrics"].([]interface{}); ok && len(metrics) > 0 {
+					metricsConfig := &s3.Metrics{}
+					metricsValues := metrics[0].(map[string]interface{})
+					metricsConfig.EventThreshold = &s3.ReplicationTimeValue{}
+
+					metricsConfig.Status = aws.String(metricsValues["status"].(string))
+					metricsConfig.EventThreshold.Minutes = aws.Int64(int64(metricsValues["minutes"].(int)))
+					ruleDestination.Metrics = metricsConfig
+
+					if *metricsConfig.Status == s3.MetricsStatusEnabled {
+						metricsEnabled = true
+					}
+				}
+
+				// replication time control (RTC)
+				if rtc, ok := bd["replication_time"].([]interface{}); ok && len(rtc) > 0 {
+					rtcValues := rtc[0].(map[string]interface{})
+					rtcConfig := &s3.ReplicationTime{}
+					rtcConfig.Status = aws.String(rtcValues["status"].(string))
+					rtcConfig.Time = &s3.ReplicationTimeValue{}
+					rtcConfig.Time.Minutes = aws.Int64(int64(rtcValues["minutes"].(int)))
+
+					if *rtcConfig.Status == s3.ReplicationTimeStatusEnabled {
+						rtcEnabled = true
+						if !metricsEnabled {
+							return fmt.Errorf("Metrics must be enabled to allow S3 RTC. Enable by adding a block like: metrics {}")
+						}
+					}
+
+					ruleDestination.ReplicationTime = rtcConfig
+				}
 			}
 		}
 		rcRule.Destination = ruleDestination
@@ -2093,8 +2171,13 @@ func resourceBucketReplicationConfigurationUpdate(conn *s3.S3, d *schema.Resourc
 			}
 		}
 
-		if f, ok := rr["filter"].([]interface{}); ok && len(f) > 0 && f[0] != nil {
-			// XML schema V2.
+		f, ok := rr["filter"].([]interface{})
+		if ok && (len(f) == 0 || f[0] == nil) {
+			// an empty filter means apply the rule to all objects in the bucket
+			rcRule.Priority = aws.Int64(int64(rr["priority"].(int)))
+			rcRule.Filter = &s3.ReplicationRuleFilter{}
+		} else if ok && len(f) > 0 && f[0] != nil {
+			// XML schema V2 (required for S3 RTC).
 			rcRule.Priority = aws.Int64(int64(rr["priority"].(int)))
 			rcRule.Filter = &s3.ReplicationRuleFilter{}
 			filter := f[0].(map[string]interface{})
@@ -2117,8 +2200,10 @@ func resourceBucketReplicationConfigurationUpdate(conn *s3.S3, d *schema.Resourc
 					Status: aws.String(s3.DeleteMarkerReplicationStatusDisabled),
 				}
 			}
+		} else if rtcEnabled {
+			return fmt.Errorf("A filter block must be present to enable RTC due to differences between V1 and V2 of the replication configuration schema.")
 		} else {
-			// XML schema V1.
+			// XML schema V1. (Note: Incompatible with RTC)
 			rcRule.Prefix = aws.String(rr["prefix"].(string))
 		}
 
@@ -2354,6 +2439,23 @@ func flattenBucketReplicationConfiguration(r *s3.ReplicationConfiguration) []map
 			}
 			if v.Destination.StorageClass != nil {
 				rd["storage_class"] = aws.StringValue(v.Destination.StorageClass)
+			}
+			if v.Destination.ReplicationTime != nil {
+				rtc := map[string]interface{}{
+					"minutes": aws.Int64Value(v.Destination.ReplicationTime.Time.Minutes),
+					"status":  aws.StringValue(v.Destination.ReplicationTime.Status),
+				}
+				rd["replication_time"] = []interface{}{rtc}
+				if v.Destination.Metrics == nil {
+					log.Printf("[WARN] Metrics must be enabled to enable RTC")
+				}
+			}
+			if v.Destination.Metrics != nil {
+				metrics := map[string]interface{}{
+					"minutes": aws.Int64Value(v.Destination.Metrics.EventThreshold.Minutes),
+					"status":  aws.StringValue(v.Destination.Metrics.Status),
+				}
+				rd["metrics"] = []interface{}{metrics}
 			}
 			if v.Destination.EncryptionConfiguration != nil {
 				if v.Destination.EncryptionConfiguration.ReplicaKmsKeyID != nil {
