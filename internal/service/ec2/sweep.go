73bffbbc0636ccc9ad9560c9e3f2e8ad41fbb859
Graham Davison <gdavison@hashicorp.com>
2021-10-29 17:15:14 -0700

    Adds dependency on `aws_dms_replication_instance` to `aws_subnet` sweeper

--- internal/service/ec2/sweep.go
+++ internal/service/ec2/sweep.go
@@ -170,6 +170,7 @@ func init() {
 			"aws_cloudhsm_v2_cluster",
 			"aws_db_subnet_group",
 			"aws_directory_service_directory",
+			"aws_dms_replication_instance",
 			"aws_ec2_client_vpn_endpoint",
 			"aws_ec2_transit_gateway_vpc_attachment",
 			"aws_efs_file_system",
