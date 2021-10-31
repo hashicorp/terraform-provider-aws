bf66452f2ee84f79f68bd8169707c84ad0c0e94c
Kamil Turek <kamil.turek@hotmail.com>
2021-10-31 09:51:27 +0100

    Remove deadcode

--- internal/service/elb/load_balancer.go
+++ internal/service/elb/load_balancer.go
@@ -976,23 +976,3 @@ func CleanupNetworkInterfaces(conn *ec2.EC2, name string) error {
 	err = tfec2.DeleteNetworkInterfaces(conn, out.NetworkInterfaces)
 	return err
 }
-
-func networkInterfaceAttachmentRefreshFunc(conn *ec2.EC2, id string) resource.StateRefreshFunc {
-	return func() (interface{}, string, error) {
-
-		describe_network_interfaces_request := &ec2.DescribeNetworkInterfacesInput{
-			NetworkInterfaceIds: []*string{aws.String(id)},
-		}
-		describeResp, err := conn.DescribeNetworkInterfaces(describe_network_interfaces_request)
-
-		if err != nil {
-			log.Printf("[ERROR] Could not find network interface %s. %s", id, err)
-			return nil, "", err
-		}
-
-		eni := describeResp.NetworkInterfaces[0]
-		hasAttachment := strconv.FormatBool(eni.Attachment != nil)
-		log.Printf("[DEBUG] ENI %s has attachment state %s", id, hasAttachment)
-		return eni, hasAttachment, nil
-	}
-}
--- internal/service/elbv2/load_balancer.go
+++ internal/service/elbv2/load_balancer.go
@@ -878,23 +878,3 @@ func customizeDiffNLBSubnets(_ context.Context, diff *schema.ResourceDiff, v int
 	}
 	return nil
 }
-
-func networkInterfaceAttachmentRefreshFunc(conn *ec2.EC2, id string) resource.StateRefreshFunc {
-	return func() (interface{}, string, error) {
-
-		describe_network_interfaces_request := &ec2.DescribeNetworkInterfacesInput{
-			NetworkInterfaceIds: []*string{aws.String(id)},
-		}
-		describeResp, err := conn.DescribeNetworkInterfaces(describe_network_interfaces_request)
-
-		if err != nil {
-			log.Printf("[ERROR] Could not find network interface %s. %s", id, err)
-			return nil, "", err
-		}
-
-		eni := describeResp.NetworkInterfaces[0]
-		hasAttachment := strconv.FormatBool(eni.Attachment != nil)
-		log.Printf("[DEBUG] ENI %s has attachment state %s", id, hasAttachment)
-		return eni, hasAttachment, nil
-	}
-}
