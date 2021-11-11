bf608c5175b4574a4c431c34168f931f9b44602f
Kit Ewbank <Kit_Ewbank@hotmail.com>
2021-11-11 10:52:44 -0500

    Run 'make fmt'.

--- internal/service/docdb/sweep.go
+++ internal/service/docdb/sweep.go
@@ -5,13 +5,12 @@ package docdb
 
 import (
 	"fmt"
-	
 	"log"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/docdb"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-provider-aws/internal/conns"	
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 )
 
 func init() {
