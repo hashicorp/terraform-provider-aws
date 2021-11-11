dbe21803393dc51157146951dba0f1d97eaa003d
philof <philof@users.noreply.github.com>
2021-11-11 17:15:56 -0500

    r/aws_sagemaker_app: add attributes to ResourceSpec
        Acceptance test output:
    
        $ make testacc TESTARGS='-run=TestAccApp' PKG_NAME=internal/service/sagemaker
        TF_ACC=1 go test ./internal/service/sagemaker/... -v -count 1 -parallel 20 -run=TestAccApp -timeout 180m
        === RUN   TestAccApp_resourceSpec
        --- PASS: TestAccApp_resourceSpec (624.88s)
        === RUN   TestAccApp_resourceSpecLifecycle
        --- PASS: TestAccApp_resourceSpecLifecycle (603.75s)
        PASS
        ok       github.com/hashicorp/terraform-provider-aws/internal/service/sagemaker  1230.041s

--- internal/service/sagemaker/app.go
+++ internal/service/sagemaker/app.go
@@ -65,7 +65,19 @@ func ResourceApp() *schema.Resource {
 							Optional:     true,
 							ValidateFunc: validation.StringInSlice(sagemaker.AppInstanceType_Values(), false),
 						},
+						"lifecycle_config_arn": {
+							Type:         schema.TypeString,
+							Optional:     true,
+							Computed:     true,
+							ValidateFunc: verify.ValidARN,
+						},
 						"sagemaker_image_arn": {
+							Type:         schema.TypeString,
+							Optional:     true,
+							Computed:     true,
+							ValidateFunc: verify.ValidARN,
+						},
+						"sagemaker_image_version_arn": {
 							Type:         schema.TypeString,
 							Optional:     true,
 							Computed:     true,
