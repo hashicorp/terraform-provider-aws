fadac94ce864051f4f97dc39a092cd3cb123bd32
Graham Davison <gdavison@hashicorp.com>
2021-11-15 11:08:09 -0800

    Fixes error where empty `checkpoint_percentages` is submitted to the API

--- internal/service/autoscaling/group_test.go
+++ internal/service/autoscaling/group_test.go
@@ -974,6 +974,19 @@ func TestAccAutoScalingGroup_InstanceRefresh_basic(t *testing.T) {
 				},
 			},
 			{
+				Config: testAccGroupConfig_InstanceRefresh_MinHealthyPercentage(),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckGroupExists(resourceName, &group),
+					resource.TestCheckResourceAttr(resourceName, "instance_refresh.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "instance_refresh.0.strategy", "Rolling"),
+					resource.TestCheckResourceAttr(resourceName, "instance_refresh.0.preferences.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "instance_refresh.0.preferences.0.instance_warmup", ""),
+					resource.TestCheckResourceAttr(resourceName, "instance_refresh.0.preferences.0.min_healthy_percentage", "0"),
+					resource.TestCheckResourceAttr(resourceName, "instance_refresh.0.preferences.0.checkpoint_delay", ""),
+					resource.TestCheckResourceAttr(resourceName, "instance_refresh.0.preferences.0.checkpoint_percentages.#", "0"),
+				),
+			},
+			{
 				Config: testAccGroupConfig_InstanceRefresh_Full(),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckGroupExists(resourceName, &group),
@@ -4474,6 +4487,49 @@ resource "aws_launch_configuration" "test" {
 `
 }
 
+func testAccGroupConfig_InstanceRefresh_MinHealthyPercentage() string {
+	return `
+resource "aws_autoscaling_group" "test" {
+  availability_zones   = [data.aws_availability_zones.current.names[0]]
+  max_size             = 2
+  min_size             = 1
+  desired_capacity     = 1
+  launch_configuration = aws_launch_configuration.test.name
+
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage = 0
+    }
+  }
+}
+
+data "aws_ami" "test" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+data "aws_availability_zones" "current" {
+  state = "available"
+
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+resource "aws_launch_configuration" "test" {
+  image_id      = data.aws_ami.test.id
+  instance_type = "t3.nano"
+}
+`
+}
+
 func testAccGroupConfig_InstanceRefresh_Full() string {
 	return `
 resource "aws_autoscaling_group" "test" {
@@ -4965,6 +5021,27 @@ func TestCreateAutoScalingGroupInstanceRefreshInput(t *testing.T) {
 					},
 					InstanceWarmup:       nil,
 					MinHealthyPercentage: nil,
+				},
+			},
+		},
+		{
+			name: "checkpoint_percentages empty",
+			input: []interface{}{map[string]interface{}{
+				"strategy": "Rolling",
+				"preferences": []interface{}{
+					map[string]interface{}{
+						"checkpoint_percentages": []interface{}{},
+					},
+				},
+			}},
+			expected: &autoscaling.StartInstanceRefreshInput{
+				AutoScalingGroupName: aws.String(asgName),
+				Strategy:             aws.String("Rolling"),
+				Preferences: &autoscaling.RefreshPreferences{
+					CheckpointDelay:       nil,
+					CheckpointPercentages: nil,
+					InstanceWarmup:        nil,
+					MinHealthyPercentage:  nil,
 				},
 			},
 		},
