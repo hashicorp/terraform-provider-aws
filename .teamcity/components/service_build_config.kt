0d2079d7c27bf2cc25591366e7d3b87a36364498
Graham Davison <gdavison@hashicorp.com>
2021-11-16 12:17:28 -0800

    Adds VPC lock

--- .teamcity/components/service_build_config.kt
+++ .teamcity/components/service_build_config.kt
@@ -5,26 +5,30 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.ParameterDisplay
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import java.io.File
 
-data class ServiceSpec(val readableName: String, val patternOverride: String = "")
+data class ServiceSpec(
+    val readableName: String,
+    val patternOverride: String = "",
+    val vpcLock: Boolean = false,
+)
 
-class Service(name: String, displayName: ServiceSpec) {
+class Service(name: String, spec: ServiceSpec) {
     val packageName = name
-    val displayName = displayName
+    val spec = spec
 
     fun buildType(): BuildType {
         return BuildType {
             id = DslContext.createId("ServiceTest_$packageName")
 
-            name = "2. ${displayName.readableName} - Tests"
+            name = "2. ${spec.readableName} - Tests"
 
             vcs {
                 root(AbsoluteId(DslContext.getParameter("vcs_root_id")))
                 cleanCheckout = true
             }
 
-            if (displayName.patternOverride != "") {
+            if (spec.patternOverride != "") {
                 params {
-                    text("TEST_PATTERN", displayName.patternOverride, display = ParameterDisplay.HIDDEN)
+                    text("TEST_PATTERN", spec.patternOverride, display = ParameterDisplay.HIDDEN)
                 }
             }
 
@@ -48,6 +52,15 @@ class Service(name: String, displayName: ServiceSpec) {
                     name = "Run Acceptance Tests"
                     workingDir = serviceDir
                     scriptContent = File("./scripts/service_tests/acceptance_tests.sh").readText()
+                }
+            }
+
+            if (spec.vpcLock) {
+                features {
+                    feature {
+                        type = "JetBrains.SharedResources"
+                        param("locks-param", "${DslContext.getParameter("aws_account.vpc_lock_id")} readLock")
+                    }
                 }
             }
         }
