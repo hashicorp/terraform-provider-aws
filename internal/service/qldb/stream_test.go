cfa904e9b77aedc1aec230cc543262d2879a3f76
Jonathan Brown <frumpledbiscuit@gmail.com>
2021-11-16 17:56:13 -0700

    move sweept test bits

--- internal/service/qldb/stream_test.go
+++ internal/service/qldb/stream_test.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"regexp"
 	"testing"
-	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/qldb"
@@ -17,121 +16,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
-	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 )
-
-func init() {
-	resource.AddTestSweepers("aws_qldb_stream", &resource.Sweeper{
-		Name: "aws_qldb_stream",
-		F:    testSweepQLDBStreams,
-	})
-}
-
-func testSweepQLDBStreams(region string) error {
-	client, err := sweep.SharedRegionalSweepClient(region)(region)
-
-	if err != nil {
-		return fmt.Errorf("error getting client: %s", err)
-	}
-
-	conn := client.(*AWSClient).qldbconn
-	input := &qldb.ListLedgersInput{}
-	page, err := conn.ListLedgers(input)
-
-	if err != nil {
-		if sweep.SkipSweepError(err) {
-			log.Printf("[WARN] Skipping QLDB Stream sweep for %s: %s", region, err)
-			return nil
-		}
-		return fmt.Errorf("Error listing QLDB Ledgers for QLDB Stream Sweep: %s", err)
-	}
-
-	for _, item := range page.Ledgers {
-		ledgerName := aws.StringValue(item.Name)
-		err := testSweepQLDBLedgerStreams(conn, region, ledgerName)
-		if err != nil {
-			log.Printf("[ERROR] Failed to delete QLDB Stream for Ledger %s: %s", ledgerName, err)
-			continue
-		}
-	}
-
-	return nil
-}
-
-func testSweepQLDBLedgerStreams(conn *qldb.QLDB, region string, ledgerName string) error {
-	input := &qldb.ListJournalKinesisStreamsForLedgerInput{
-		LedgerName: aws.String(ledgerName),
-	}
-	page, err := conn.ListJournalKinesisStreamsForLedger(input)
-
-	if err != nil {
-		if sweep.SkipSweepError(err) {
-			log.Printf("[WARN] Skipping QLDB Stream sweep for %s: %s", region, err)
-			return nil
-		}
-		return fmt.Errorf("Error listing QLDB Streams: %s", err)
-	}
-
-	for _, item := range page.Streams {
-		input := &qldb.CancelJournalKinesisStreamInput{
-			LedgerName: item.LedgerName,
-			StreamId:   item.StreamId,
-		}
-
-		ledgerName := aws.StringValue(item.LedgerName)
-		streamID := aws.StringValue(item.StreamId)
-
-		log.Printf("[INFO] Cancelling QLDB Stream: (%s, %s)", ledgerName, streamID)
-		_, err = conn.CancelJournalKinesisStream(input)
-
-		if err != nil {
-			log.Printf("[ERROR] Failed to cancel QLDB Ledger: (%s, %s): %s", ledgerName, streamID, err)
-			continue
-		}
-
-		if err := waitForQLDBStreamCancellation(conn, ledgerName, streamID); err != nil {
-			log.Printf("[ERROR] Error waiting for QLDB Ledger (%s, %s) deletion: %s", ledgerName, streamID, err)
-		}
-	}
-
-	return nil
-}
-
-func waitForQLDBStreamCancellation(conn *qldb.QLDB, ledgerName string, streamID string) error {
-	stateConf := resource.StateChangeConf{
-		Pending: []string{
-			qldb.StreamStatusActive,
-			qldb.StreamStatusImpaired,
-		},
-		Target: []string{
-			qldb.StreamStatusCanceled,
-			qldb.StreamStatusCompleted,
-			qldb.StreamStatusFailed,
-		},
-		Timeout:    5 * time.Minute,
-		MinTimeout: 1 * time.Second,
-		Refresh: func() (interface{}, string, error) {
-			resp, err := conn.DescribeJournalKinesisStream(&qldb.DescribeJournalKinesisStreamInput{
-				LedgerName: aws.String(ledgerName),
-				StreamId:   aws.String(streamID),
-			})
-
-			if tfawserr.ErrMessageContains(err, qldb.ErrCodeResourceNotFoundException, "") {
-				return 1, "", nil
-			}
-
-			if err != nil {
-				return nil, qldb.ErrCodeResourceInUseException, err
-			}
-
-			return resp, aws.StringValue(resp.Stream.Status), nil
-		},
-	}
-
-	_, err := stateConf.WaitForState()
-
-	return err
-}
 
 func TestAccAWSQLDBStream_basic(t *testing.T) {
 	var qldbCluster qldb.DescribeJournalKinesisStreamOutput
--- internal/service/qldb/sweep.go
+++ internal/service/qldb/sweep.go
@@ -19,6 +19,11 @@ func init() {
 		Name: "aws_qldb_ledger",
 		F:    sweepLedgers,
 	})
+	resource.AddTestSweepers("aws_qldb_stream", &resource.Sweeper{
+		Name: "aws_qldb_stream",
+		F:    testSweepQLDBStreams,
+	})
+
 }
 
 func sweepLedgers(region string) error {
@@ -60,4 +65,110 @@ func sweepLedgers(region string) error {
 	}
 
 	return nil
+}
+
+func testSweepQLDBStreams(region string) error {
+	client, err := sweep.SharedRegionalSweepClient(region)(region)
+
+	if err != nil {
+		return fmt.Errorf("error getting client: %s", err)
+	}
+
+	conn := client.(*AWSClient).qldbconn
+	input := &qldb.ListLedgersInput{}
+	page, err := conn.ListLedgers(input)
+
+	if err != nil {
+		if sweep.SkipSweepError(err) {
+			log.Printf("[WARN] Skipping QLDB Stream sweep for %s: %s", region, err)
+			return nil
+		}
+		return fmt.Errorf("Error listing QLDB Ledgers for QLDB Stream Sweep: %s", err)
+	}
+
+	for _, item := range page.Ledgers {
+		ledgerName := aws.StringValue(item.Name)
+		err := testSweepQLDBLedgerStreams(conn, region, ledgerName)
+		if err != nil {
+			log.Printf("[ERROR] Failed to delete QLDB Stream for Ledger %s: %s", ledgerName, err)
+			continue
+		}
+	}
+
+	return nil
+}
+
+func testSweepQLDBLedgerStreams(conn *qldb.QLDB, region string, ledgerName string) error {
+	input := &qldb.ListJournalKinesisStreamsForLedgerInput{
+		LedgerName: aws.String(ledgerName),
+	}
+	page, err := conn.ListJournalKinesisStreamsForLedger(input)
+
+	if err != nil {
+		if sweep.SkipSweepError(err) {
+			log.Printf("[WARN] Skipping QLDB Stream sweep for %s: %s", region, err)
+			return nil
+		}
+		return fmt.Errorf("Error listing QLDB Streams: %s", err)
+	}
+
+	for _, item := range page.Streams {
+		input := &qldb.CancelJournalKinesisStreamInput{
+			LedgerName: item.LedgerName,
+			StreamId:   item.StreamId,
+		}
+
+		ledgerName := aws.StringValue(item.LedgerName)
+		streamID := aws.StringValue(item.StreamId)
+
+		log.Printf("[INFO] Cancelling QLDB Stream: (%s, %s)", ledgerName, streamID)
+		_, err = conn.CancelJournalKinesisStream(input)
+
+		if err != nil {
+			log.Printf("[ERROR] Failed to cancel QLDB Ledger: (%s, %s): %s", ledgerName, streamID, err)
+			continue
+		}
+
+		if err := waitForQLDBStreamCancellation(conn, ledgerName, streamID); err != nil {
+			log.Printf("[ERROR] Error waiting for QLDB Ledger (%s, %s) deletion: %s", ledgerName, streamID, err)
+		}
+	}
+
+	return nil
+}
+
+func waitForQLDBStreamCancellation(conn *qldb.QLDB, ledgerName string, streamID string) error {
+	stateConf := resource.StateChangeConf{
+		Pending: []string{
+			qldb.StreamStatusActive,
+			qldb.StreamStatusImpaired,
+		},
+		Target: []string{
+			qldb.StreamStatusCanceled,
+			qldb.StreamStatusCompleted,
+			qldb.StreamStatusFailed,
+		},
+		Timeout:    5 * time.Minute,
+		MinTimeout: 1 * time.Second,
+		Refresh: func() (interface{}, string, error) {
+			resp, err := conn.DescribeJournalKinesisStream(&qldb.DescribeJournalKinesisStreamInput{
+				LedgerName: aws.String(ledgerName),
+				StreamId:   aws.String(streamID),
+			})
+
+			if tfawserr.ErrMessageContains(err, qldb.ErrCodeResourceNotFoundException, "") {
+				return 1, "", nil
+			}
+
+			if err != nil {
+				return nil, qldb.ErrCodeResourceInUseException, err
+			}
+
+			return resp, aws.StringValue(resp.Stream.Status), nil
+		},
+	}
+
+	_, err := stateConf.WaitForState()
+
+	return err
 }
