75b887ee034655fe4a7f192bfe7b53b50f2ce107
ialidzhikov <i.alidjikov@gmail.com>
2021-11-19 17:57:25 +0200

    r/route_table: Increase the default timeout values
    Signed-off-by: ialidzhikov <i.alidjikov@gmail.com>

--- internal/service/ec2/route_table.go
+++ internal/service/ec2/route_table.go
@@ -50,8 +50,8 @@ func ResourceRouteTable() *schema.Resource {
 		},
 
 		Timeouts: &schema.ResourceTimeout{
-			Create: schema.DefaultTimeout(2 * time.Minute),
-			Update: schema.DefaultTimeout(2 * time.Minute),
+			Create: schema.DefaultTimeout(10 * time.Minute),
+			Update: schema.DefaultTimeout(5 * time.Minute),
 			Delete: schema.DefaultTimeout(5 * time.Minute),
 		},
 
