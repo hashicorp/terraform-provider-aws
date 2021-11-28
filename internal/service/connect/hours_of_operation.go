3e468db867eddc2bfdf67ed46bb7bd03c5b471c8
GlennChia <glennchia7@gmail.com>
2021-11-28 17:45:24 +0800

    feat(connect): add HoursOfOp expand, flatten conf
    expand and flatten config property

--- internal/service/connect/hours_of_operation.go
+++ internal/service/connect/hours_of_operation.go
@@ -106,3 +106,60 @@ func ResourceHoursOfOperation() *schema.Resource {
 		},
 	}
 }
+
+
+func expandConfigs(configs []interface{}) ([]*connect.HoursOfOperationConfig, error) {
+	if len(configs) == 0 {
+		return nil, nil
+	}
+
+	hoursOfOperationConfigs := []*connect.HoursOfOperationConfig{}
+	for _, config := range configs {
+		data := config.(map[string]interface{})
+		hoursOfOperationConfig := &connect.HoursOfOperationConfig{
+			Day: aws.String(data["day"].(string)),
+		}
+
+		tet := data["end_time"].([]interface{})
+		vet := tet[0].(map[string]interface{})
+		et := connect.HoursOfOperationTimeSlice{
+			Hours:   aws.Int64(int64(vet["hours"].(int))),
+			Minutes: aws.Int64(int64(vet["minutes"].(int))),
+		}
+		hoursOfOperationConfig.EndTime = &et
+
+		tst := data["start_time"].([]interface{})
+		vst := tst[0].(map[string]interface{})
+		st := connect.HoursOfOperationTimeSlice{
+			Hours:   aws.Int64(int64(vst["hours"].(int))),
+			Minutes: aws.Int64(int64(vst["minutes"].(int))),
+		}
+		hoursOfOperationConfig.StartTime = &st
+
+		hoursOfOperationConfigs = append(hoursOfOperationConfigs, hoursOfOperationConfig)
+	}
+
+	return hoursOfOperationConfigs, nil
+}
+
+func flattenConfigs(configs []*connect.HoursOfOperationConfig, d *schema.ResourceData) []interface{} {
+	configsList := []interface{}{}
+	for _, config := range configs {
+		values := map[string]interface{}{}
+		values["day"] = aws.StringValue(config.Day)
+
+		et := map[string]interface{}{
+			"hours":   aws.Int64Value(config.EndTime.Hours),
+			"minutes": aws.Int64Value(config.EndTime.Minutes),
+		}
+		values["end_time"] = []interface{}{et}
+
+		st := map[string]interface{}{
+			"hours":   aws.Int64Value(config.StartTime.Hours),
+			"minutes": aws.Int64Value(config.StartTime.Minutes),
+		}
+		values["start_time"] = []interface{}{st}
+		configsList = append(configsList, values)
+	}
+	return configsList
+}
