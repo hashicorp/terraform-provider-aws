f2f1f30bd435e7b8b71769ad51a9fde82db6f5b0
GlennChia <glennchia7@gmail.com>
2021-11-28 17:45:41 +0800

    feat(connect): add HoursOfOperation ParseID func

--- internal/service/connect/hours_of_operation.go
+++ internal/service/connect/hours_of_operation.go
@@ -163,3 +163,13 @@ func flattenConfigs(configs []*connect.HoursOfOperationConfig, d *schema.Resourc
 	}
 	return configsList
 }
+
+func HoursOfOperationParseID(id string) (string, string, error) {
+	parts := strings.SplitN(id, ":", 2)
+
+	if len(parts) != 2 || parts[0] == "" || parts[1] == "" {
+		return "", "", fmt.Errorf("unexpected format of ID (%s), expected instanceID:hoursOfOperationID", id)
+	}
+
+	return parts[0], parts[1], nil
+}
