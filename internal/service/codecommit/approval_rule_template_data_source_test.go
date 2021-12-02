7e7765f48410cf40728b32e5b876005a45612819
Angie Pinilla <angie@hashicorp.com>
2021-12-01 19:43:04 -0500

    CR updates to align with AWS API

--- internal/service/codecommit/approval_rule_template_data_source_test.go
+++ internal/service/codecommit/approval_rule_template_data_source_test.go
@@ -12,8 +12,8 @@ import (
 
 func TestAccCodeCommitApprovalRuleTemplateDataSource_basic(t *testing.T) {
 	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
-	resourceName := "aws_codecommit_approval_rule.default"
-	datasourceName := "data.aws_codecommit_approval_rule.default"
+	resourceName := "aws_codecommit_approval_rule_template.test"
+	datasourceName := "data.aws_codecommit_approval_rule_template.test"
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:   func() { acctest.PreCheck(t) },
@@ -23,8 +23,13 @@ func TestAccCodeCommitApprovalRuleTemplateDataSource_basic(t *testing.T) {
 			{
 				Config: testAccCheckCodeCommitApprovalRuleTemplateDataSourceConfig(rName),
 				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrPair(datasourceName, "description", resourceName, "description"),
 					resource.TestCheckResourceAttrPair(datasourceName, "name", resourceName, "name"),
 					resource.TestCheckResourceAttrPair(datasourceName, "content", resourceName, "content"),
+					resource.TestCheckResourceAttrPair(datasourceName, "creation_date", resourceName, "creation_date"),
+					resource.TestCheckResourceAttrPair(datasourceName, "last_modified_date", resourceName, "last_modified_date"),
+					resource.TestCheckResourceAttrPair(datasourceName, "last_modified_user", resourceName, "last_modified_user"),
+					resource.TestCheckResourceAttrPair(datasourceName, "rule_content_sha256", resourceName, "rule_content_sha256"),
 				),
 			},
 		},
@@ -33,22 +38,28 @@ func TestAccCodeCommitApprovalRuleTemplateDataSource_basic(t *testing.T) {
 
 func testAccCheckCodeCommitApprovalRuleTemplateDataSourceConfig(rName string) string {
 	return fmt.Sprintf(`
-resource "aws_codecommit_approval_rule" "default" {
-	name = "%s"
-	content = <<EOF
+data "aws_caller_identity" "current" {}
+
+data "aws_partition" "current" {}
+
+resource "aws_codecommit_approval_rule_template" "test" {
+  description = %[1]q
+  name        = %[1]q
+  content     = <<EOF
 {
+	"Version": "2018-11-08",
 	"DestinationReferences": ["refs/heads/master"],
 	"Statements": [{
 			"Type": "Approvers",
 			"NumberOfApprovalsNeeded": 2,
-			"ApprovalPoolMembers": ["arn:aws:sts::444194214726:assumed-role/CodeCommitReview/*"]}],
-	"Version": "2018-11-08"
+			"ApprovalPoolMembers": ["arn:${data.aws_partition.current.partition}:sts::${data.aws_caller_identity.current.account_id}:assumed-role/CodeCommitReview/*"]
+	}]
 }
 EOF
 }
 
-data "aws_codecommit_approval_rule" "default" {
-  name = "${aws_codecommit_approval_rule.default.name}"
+data "aws_codecommit_approval_rule_template" "test" {
+  name = aws_codecommit_approval_rule_template.test.name
 }
 `, rName)
 }
