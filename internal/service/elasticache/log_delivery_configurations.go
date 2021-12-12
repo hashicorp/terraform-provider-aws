b64b39275e23260702f1ee7cfb29aa4272a4812a
nickyamanaka <n@nicolasrivera.ca>
2021-12-11 20:27:07 -0700

    hashicorp#20068 logdeliveryconfigurations add expand methods

--- internal/service/elasticache/log_delivery_configurations.go
+++ internal/service/elasticache/log_delivery_configurations.go
@@ -138,3 +138,49 @@ func getLogDeliveryConfigurationsComputedSchema() *schema.Resource {
 
 }
 
+func expandLogDeliveryConfigurations(d *schema.ResourceData) []*elasticache.LogDeliveryConfigurationRequest {
+	logDeliveryConfigurationRequest := elasticache.LogDeliveryConfigurationRequest{}
+
+	if _, ok := d.GetOk("log_delivery_configurations"); !ok { // if d.HasChange() removed the block, we must send a `delete` request to the API.
+		logDeliveryConfigurationRequest.SetEnabled(false)
+		logDeliveryConfigurationRequest.SetLogType(elasticache.LogTypeSlowLog)
+		logDeliveryConfigurations := []*elasticache.LogDeliveryConfigurationRequest{
+			&logDeliveryConfigurationRequest,
+		}
+		return logDeliveryConfigurations
+	}
+
+	if _, ok := d.GetOk("log_delivery_configurations.0.destination_details"); ok {
+		logDeliveryConfigurationRequest.DestinationDetails = expandDestinationDetails(d)
+	}
+	if v, ok := d.GetOk("log_delivery_configurations.0.destination_type"); ok {
+		logDeliveryConfigurationRequest.DestinationType = aws.String(v.(string))
+	}
+	if v, ok := d.GetOk("log_delivery_configurations.0.log_format"); ok {
+		logDeliveryConfigurationRequest.LogFormat = aws.String(v.(string))
+	}
+	if v, ok := d.GetOk("log_delivery_configurations.0.log_type"); ok {
+		logDeliveryConfigurationRequest.LogType = aws.String(v.(string))
+	}
+
+	logDeliveryConfigurations := []*elasticache.LogDeliveryConfigurationRequest{
+		&logDeliveryConfigurationRequest,
+	}
+	return logDeliveryConfigurations
+}
+
+func expandDestinationDetails(d *schema.ResourceData) *elasticache.DestinationDetails {
+	destinationDetails := elasticache.DestinationDetails{}
+	if v, ok := d.GetOk("log_delivery_configurations.0.destination_details.0.cloudwatch_logs.0.log_group"); ok {
+		destinationDetails.CloudWatchLogsDetails = &elasticache.CloudWatchLogsDestinationDetails{
+			LogGroup: aws.String(v.(string)),
+		}
+	}
+	if v, ok := d.GetOk("log_delivery_configurations.0.destination_details.0.kinesis_firehose.0.delivery_stream"); ok {
+		destinationDetails.KinesisFirehoseDetails = &elasticache.KinesisFirehoseDestinationDetails{
+			DeliveryStream: aws.String(v.(string)),
+		}
+	}
+	return &destinationDetails
+}
+
