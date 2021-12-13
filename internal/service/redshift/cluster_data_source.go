b808677f4bcfdb73b4623d272f00c7816a328f2d
nickyamanaka <n@nicolasrivera.ca>
2021-12-12 17:52:37 -0700

    #19098 cluster_data_source: add read option

--- internal/service/redshift/cluster_data_source.go
+++ internal/service/redshift/cluster_data_source.go
@@ -206,6 +206,12 @@ func dataSourceClusterRead(d *schema.ResourceData, meta interface{}) error {
 	d.Set("allow_version_upgrade", rsc.AllowVersionUpgrade)
 	d.Set("automated_snapshot_retention_period", rsc.AutomatedSnapshotRetentionPeriod)
 	d.Set("availability_zone", rsc.AvailabilityZone)
+	d.Set("availability_zone_relocation_status", rsc.AvailabilityZoneRelocationStatus)
+	azr, err := availabilityZoneRelocationStatus(rsc)
+	if err != nil {
+		return fmt.Errorf("error reading Redshift Cluster (%s): %w", d.Id(), err)
+	}
+	d.Set("availability_zone_relocation", azr)
 	d.Set("cluster_identifier", rsc.ClusterIdentifier)
 
 	if len(rsc.ClusterParameterGroups) > 0 {
