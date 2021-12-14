89ef24b5077edf52173aa63e86664dacdcecb4e4
Andrea Quintino <andreaquin1990@gmail.com>
2021-12-14 22:19:22 +0100

    ok
    Signed-off-by: Andrea Quintino <andreaquin1990@gmail.com>

--- internal/service/ec2/default_vpc_test.go
+++ internal/service/ec2/default_vpc_test.go
@@ -3,10 +3,12 @@ package ec2_test
 import (
 	"testing"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 )
 
 func TestAccEC2DefaultVPC_basic(t *testing.T) {
@@ -19,7 +21,8 @@ func TestAccEC2DefaultVPC_basic(t *testing.T) {
 		CheckDestroy: testAccCheckDefaultVPCDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDefaultVPCBasicConfig,
+				PreConfig: func() { destroyDefaultVPCBeforeTest(t) },
+				Config:    testAccDefaultVPCBasicConfig,
 				Check: resource.ComposeTestCheckFunc(
 					acctest.CheckVPCExists("aws_default_vpc.foo", &vpc),
 					resource.TestCheckResourceAttr("aws_default_vpc.foo", "cidr_block", "172.31.0.0/16"),
@@ -42,6 +45,59 @@ func TestAccEC2DefaultVPC_basic(t *testing.T) {
 			},
 		},
 	})
+}
+
+func destroyDefaultVPCBeforeTest(t *testing.T) {
+	conn := acctest.Provider.Meta().(*conns.AWSClient).EC2Conn
+	req := &ec2.DescribeVpcsInput{
+		Filters: []*ec2.Filter{
+			{
+				Name:   aws.String("isDefault"),
+				Values: aws.StringSlice([]string{"true"}),
+			},
+		},
+	}
+
+	resp, err := conn.DescribeVpcs(req)
+	if err != nil {
+		t.Fatalf("Error describing default vpcs: %s", err)
+	}
+
+	if resp.Vpcs == nil || len(resp.Vpcs) == 0 {
+		return // No vpc, all good
+	}
+
+	//delete all subnets
+	reqDS := &ec2.DescribeSubnetsInput{
+		Filters: []*ec2.Filter{
+			{
+				Name:   aws.String("vpc-id"),
+				Values: aws.StringSlice([]string{aws.StringValue(resp.Vpcs[0].VpcId)}),
+			},
+		},
+	}
+
+	respDS, err := conn.DescribeSubnets(reqDS)
+	if err != nil {
+		t.Fatalf("Error describing default subnets: %s", err)
+	}
+	for _, subnet := range respDS.Subnets {
+		_, err := conn.DeleteSubnet(&ec2.DeleteSubnetInput{
+			SubnetId: subnet.SubnetId,
+		})
+		if err != nil {
+			t.Fatalf("Error deleting default subnet: %s", err)
+		}
+	}
+
+	delReq := &ec2.DeleteVpcInput{
+		VpcId: resp.Vpcs[0].VpcId,
+	}
+
+	_, err = conn.DeleteVpc(delReq)
+	if err != nil {
+		t.Fatalf("Error deleting default VPC: %s", err)
+	}
 }
 
 func testAccCheckDefaultVPCDestroy(s *terraform.State) error {
