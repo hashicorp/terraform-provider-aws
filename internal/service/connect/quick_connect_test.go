c431aabefde25f87998e7cfe48dc74994942c321
GlennChia <glennchia7@gmail.com>
2021-12-16 14:21:36 +0800

    test(connect): config for instance, QuickConnect

---
+++ internal/service/connect/quick_connect_test.go
@@ -0,0 +1,52 @@
+package connect_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/connect"
+	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	tfconnect "github.com/hashicorp/terraform-provider-aws/internal/service/connect"
+)
+
+
+func testAccQuickConnectBaseConfig(rName string) string {
+	return fmt.Sprintf(`
+resource "aws_connect_instance" "test" {
+  identity_management_type = "CONNECT_MANAGED"
+  inbound_calls_enabled    = true
+  instance_alias           = %[1]q
+  outbound_calls_enabled   = true
+}
+`, rName)
+}
+
+func testAccQuickConnectPhoneNumberConfig(rName, rName2, label string) string {
+	return acctest.ConfigCompose(
+		testAccQuickConnectBaseConfig(rName),
+		fmt.Sprintf(`
+resource "aws_connect_quick_connect" "test" {
+  instance_id = aws_connect_instance.test.id
+  name        = %[1]q
+  description = %[2]q
+
+  quick_connect_config {
+    quick_connect_type = "PHONE_NUMBER"
+
+    phone_config {
+	  phone_number   = "+12345678912"
+    }
+  }
+
+  tags = {
+    "Name" = "Test Quick Connect"
+  }
+}
+`, rName2, label))
+}
