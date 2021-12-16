07a2836b338c7ffefdce51acc876a1a45ba840cb
GlennChia <glennchia7@gmail.com>
2021-12-16 14:16:39 +0800

    feat(connect): add QuickConnect expand, flatten
    condition on QuickConnectType that has values USER, QUEUE, PHONE_NUMBER

--- internal/service/connect/quick_connect.go
+++ internal/service/connect/quick_connect.go
@@ -133,3 +133,107 @@ func ResourceQuickConnect() *schema.Resource {
 		},
 	}
 }
+
+
+func expandQuickConnectConfig(quickConnectConfig []interface{}) *connect.QuickConnectConfig {
+	if len(quickConnectConfig) == 0 || quickConnectConfig[0] == nil {
+		return nil
+	}
+
+	tfMap, ok := quickConnectConfig[0].(map[string]interface{})
+	if !ok {
+		return nil
+	}
+
+	quickConnectType := tfMap["quick_connect_type"].(string)
+
+	result := &connect.QuickConnectConfig{
+		QuickConnectType: aws.String(quickConnectType),
+	}
+
+	switch quickConnectType {
+	case connect.QuickConnectTypePhoneNumber:
+		tpc := tfMap["phone_config"].([]interface{})
+		if len(tpc) == 0 || tpc[0] == nil {
+			log.Printf("[ERR] 'phone_config' must be set when 'quick_connect_type' is '%s'", quickConnectType)
+			return nil
+		}
+		vpc := tpc[0].(map[string]interface{})
+		pc := connect.PhoneNumberQuickConnectConfig{
+			PhoneNumber: aws.String(vpc["phone_number"].(string)),
+		}
+		result.PhoneConfig = &pc
+
+	case connect.QuickConnectTypeQueue:
+		tqc := tfMap["queue_config"].([]interface{})
+		if len(tqc) == 0 || tqc[0] == nil {
+			log.Printf("[ERR] 'queue_config' must be set when 'quick_connect_type' is '%s'", quickConnectType)
+			return nil
+		}
+		vqc := tqc[0].(map[string]interface{})
+		qc := connect.QueueQuickConnectConfig{
+			ContactFlowId: aws.String(vqc["contact_flow_id"].(string)),
+			QueueId:       aws.String(vqc["queue_id"].(string)),
+		}
+		result.QueueConfig = &qc
+
+	case connect.QuickConnectTypeUser:
+		tuc := tfMap["user_config"].([]interface{})
+		if len(tuc) == 0 || tuc[0] == nil {
+			log.Printf("[ERR] 'user_config' must be set when 'quick_connect_type' is '%s'", quickConnectType)
+			return nil
+		}
+		vuc := tuc[0].(map[string]interface{})
+		uc := connect.UserQuickConnectConfig{
+			ContactFlowId: aws.String(vuc["contact_flow_id"].(string)),
+			UserId:        aws.String(vuc["user_id"].(string)),
+		}
+		result.UserConfig = &uc
+
+	default:
+		log.Printf("[ERR] quick_connect_type is invalid")
+		return nil
+	}
+
+	return result
+}
+
+func flattenQuickConnectConfig(quickConnectConfig *connect.QuickConnectConfig) []interface{} {
+	if quickConnectConfig == nil {
+		return []interface{}{}
+	}
+
+	quickConnectType := aws.StringValue(quickConnectConfig.QuickConnectType)
+
+	values := map[string]interface{}{
+		"quick_connect_type": quickConnectType,
+	}
+
+	switch quickConnectType {
+	case connect.QuickConnectTypePhoneNumber:
+		pc := map[string]interface{}{
+			"phone_number": aws.StringValue(quickConnectConfig.PhoneConfig.PhoneNumber),
+		}
+		values["phone_config"] = []interface{}{pc}
+
+	case connect.QuickConnectTypeQueue:
+		qc := map[string]interface{}{
+			"contact_flow_id": aws.StringValue(quickConnectConfig.QueueConfig.ContactFlowId),
+			"queue_id":        aws.StringValue(quickConnectConfig.QueueConfig.QueueId),
+		}
+		values["queue_config"] = []interface{}{qc}
+
+	case connect.QuickConnectTypeUser:
+		uc := map[string]interface{}{
+			"contact_flow_id": aws.StringValue(quickConnectConfig.UserConfig.ContactFlowId),
+			"user_id":         aws.StringValue(quickConnectConfig.UserConfig.UserId),
+		}
+		values["user_config"] = []interface{}{uc}
+
+	default:
+		log.Printf("[ERR] quick_connect_type is invalid")
+		return nil
+	}
+
+	return []interface{}{values}
+}
