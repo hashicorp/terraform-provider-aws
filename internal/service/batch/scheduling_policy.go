ac665e152b2f1a5c532ec07400fa56a8dd2ef5af
GlennChia <glennchia7@gmail.com>
2021-12-17 14:50:55 +0800

    feat(batch): add SchedulingPolicy schema

---
+++ internal/service/batch/scheduling_policy.go
@@ -0,0 +1,92 @@
+package batch
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/batch"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/create"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
+)
+
+func ResourceSchedulingPolicy() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceSchedulingPolicyCreate,
+		ReadContext:   resourceSchedulingPolicyRead,
+		UpdateContext: resourceSchedulingPolicyUpdate,
+		DeleteContext: resourceSchedulingPolicyDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+		Schema: map[string]*schema.Schema{
+			"arn": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"fair_share_policy": {
+				Type:     schema.TypeList,
+				Optional: true,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"compute_reservation": {
+							Type:         schema.TypeInt,
+							Optional:     true,
+							ValidateFunc: validation.IntBetween(0, 99),
+						},
+						"share_decay_seconds": {
+							Type:         schema.TypeInt,
+							Optional:     true,
+							ValidateFunc: validation.IntBetween(0, 604800),
+						},
+						"share_distribution": {
+							Type: schema.TypeSet,
+							// There can be no more than 500 fair share identifiers active in a job queue.
+							MaxItems: 500,
+							Optional: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"share_identifier": {
+										Type:     schema.TypeString,
+										Required: true,
+										// limited to 255 alphanumeric characters, optionally followed by an asterisk (*).
+										ValidateFunc: validation.StringLenBetween(1, 256),
+									},
+									"weight_factor": {
+										Type:         schema.TypeFloat,
+										Optional:     true,
+										ValidateFunc: validation.FloatBetween(0.0001, 999.9999),
+									},
+								},
+							},
+							Set: func(v interface{}) int {
+								var buf bytes.Buffer
+								m := v.(map[string]interface{})
+								buf.WriteString(m["share_identifier"].(string))
+								if v, ok := m["weight_factor"]; ok {
+									buf.WriteString(fmt.Sprintf("%s-", v))
+								}
+								return create.StringHashcode(buf.String())
+							},
+						},
+					},
+				},
+			},
+			"name": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringLenBetween(1, 127),
+			},
+			"tags":     tftags.TagsSchema(),
+			"tags_all": tftags.TagsSchemaComputed(),
+		},
+	}
+}
