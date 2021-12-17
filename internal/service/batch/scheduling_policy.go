19b0a8dcc58aa9a2043a20211641fff76bb2b98a
GlennChia <glennchia7@gmail.com>
2021-12-17 14:51:20 +0800

    feat(batch): add SchedulingPolicy expand, flatten

--- internal/service/batch/scheduling_policy.go
+++ internal/service/batch/scheduling_policy.go
@@ -90,3 +90,63 @@ func ResourceSchedulingPolicy() *schema.Resource {
 		},
 	}
 }
+
+
+func expandFairsharePolicy(fairsharePolicy []interface{}) *batch.FairsharePolicy {
+	if len(fairsharePolicy) == 0 || fairsharePolicy[0] == nil {
+		return nil
+	}
+
+	tfMap, ok := fairsharePolicy[0].(map[string]interface{})
+	if !ok {
+		return nil
+	}
+
+	result := &batch.FairsharePolicy{
+		ComputeReservation: aws.Int64(int64(tfMap["compute_reservation"].(int))),
+		ShareDecaySeconds:  aws.Int64(int64(tfMap["share_decay_seconds"].(int))),
+	}
+
+	shareDistributions := tfMap["share_distribution"].(*schema.Set).List()
+
+	fairsharePolicyShareDistributions := []*batch.ShareAttributes{}
+
+	for _, shareDistribution := range shareDistributions {
+		data := shareDistribution.(map[string]interface{})
+
+		schedulingPolicyConfig := &batch.ShareAttributes{
+			ShareIdentifier: aws.String(data["share_identifier"].(string)),
+			WeightFactor:    aws.Float64(float64(data["weight_factor"].(float64))),
+		}
+		fairsharePolicyShareDistributions = append(fairsharePolicyShareDistributions, schedulingPolicyConfig)
+	}
+
+	result.ShareDistribution = fairsharePolicyShareDistributions
+
+	return result
+}
+
+func flattenFairsharePolicy(fairsharePolicy *batch.FairsharePolicy) []interface{} {
+	if fairsharePolicy == nil {
+		return []interface{}{}
+	}
+
+	values := map[string]interface{}{
+		"compute_reservation": aws.Int64Value(fairsharePolicy.ComputeReservation),
+		"share_decay_seconds": aws.Int64Value(fairsharePolicy.ShareDecaySeconds),
+	}
+
+	shareDistributions := fairsharePolicy.ShareDistribution
+
+	fairsharePolicyShareDistributions := []interface{}{}
+	for _, shareDistribution := range shareDistributions {
+		sdValues := map[string]interface{}{
+			"share_identifier": aws.StringValue(shareDistribution.ShareIdentifier),
+			"weight_factor":    aws.Float64Value(shareDistribution.WeightFactor),
+		}
+		fairsharePolicyShareDistributions = append(fairsharePolicyShareDistributions, sdValues)
+	}
+	values["share_distribution"] = fairsharePolicyShareDistributions
+
+	return []interface{}{values}
+}
