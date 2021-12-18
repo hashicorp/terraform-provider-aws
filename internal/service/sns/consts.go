de6ad2754362501bbfdca36c8b13e43272055073
Kit Ewbank <Kit_Ewbank@hotmail.com>
2021-12-18 17:40:12 -0500

    Add TopicAttributeName constants.

--- internal/service/sns/consts.go
+++ internal/service/sns/consts.go
@@ -3,3 +3,29 @@ package sns
 const (
 	FIFOTopicNameSuffix = ".fifo"
 )
+
+const (
+	TopicAttributeNameApplicationFailureFeedbackRoleArn    = "ApplicationFailureFeedbackRoleArn"
+	TopicAttributeNameApplicationSuccessFeedbackRoleArn    = "ApplicationSuccessFeedbackRoleArn"
+	TopicAttributeNameApplicationSuccessFeedbackSampleRate = "ApplicationSuccessFeedbackSampleRate"
+	TopicAttributeNameContentBasedDeduplication            = "ContentBasedDeduplication"
+	TopicAttributeNameDeliveryPolicy                       = "DeliveryPolicy"
+	TopicAttributeNameDisplayName                          = "DisplayName"
+	TopicAttributeNameFifoTopic                            = "FifoTopic"
+	TopicAttributeNameFirehoseFailureFeedbackRoleArn       = "FirehoseFailureFeedbackRoleArn"
+	TopicAttributeNameFirehoseSuccessFeedbackRoleArn       = "FirehoseSuccessFeedbackRoleArn"
+	TopicAttributeNameFirehoseSuccessFeedbackSampleRate    = "FirehoseSuccessFeedbackSampleRate"
+	TopicAttributeNameHTTPFailureFeedbackRoleArn           = "HTTPFailureFeedbackRoleArn"
+	TopicAttributeNameHTTPSuccessFeedbackRoleArn           = "HTTPSuccessFeedbackRoleArn"
+	TopicAttributeNameHTTPSuccessFeedbackSampleRate        = "HTTPSuccessFeedbackSampleRate"
+	TopicAttributeNameKmsMasterKeyId                       = "KmsMasterKeyId"
+	TopicAttributeNameLambdaFailureFeedbackRoleArn         = "LambdaFailureFeedbackRoleArn"
+	TopicAttributeNameLambdaSuccessFeedbackRoleArn         = "LambdaSuccessFeedbackRoleArn"
+	TopicAttributeNameLambdaSuccessFeedbackSampleRate      = "LambdaSuccessFeedbackSampleRate"
+	TopicAttributeNameOwner                                = "Owner"
+	TopicAttributeNamePolicy                               = "Policy"
+	TopicAttributeNameSQSFailureFeedbackRoleArn            = "SQSFailureFeedbackRoleArn"
+	TopicAttributeNameSQSSuccessFeedbackRoleArn            = "SQSSuccessFeedbackRoleArn"
+	TopicAttributeNameSQSSuccessFeedbackSampleRate         = "SQSSuccessFeedbackSampleRate"
+	TopicAttributeNameTopicArn                             = "TopicArn"
+)
