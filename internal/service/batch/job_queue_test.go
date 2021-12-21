7b9696a75aec5736d0f009b2bc381d4145cce610
GlennChia <glennchia7@gmail.com>
2021-12-21 23:06:13 +0800

    refactor(batch): underscores instead of camel case

--- internal/service/batch/job_queue_test.go
+++ internal/service/batch/job_queue_test.go
@@ -492,14 +492,14 @@ func testAccBatchJobQueueConfigSchedulingPolicy(rName string, schedulingPolicyNa
 		testAccBatchJobQueueSchedulingPolicy(schedulingPolicyName1, schedulingPolicyName2),
 		fmt.Sprintf(`
 locals {
-  selectSchedulingPolicy = %[2]q
+  select_scheduling_policy = %[2]q
 }
 
 resource "aws_batch_job_queue" "test" {
   compute_environments  = [aws_batch_compute_environment.test.arn]
   name                  = %[1]q
   priority              = 1
-  scheduling_policy_arn = local.selectSchedulingPolicy == "first" ? aws_batch_scheduling_policy.test1.arn : aws_batch_scheduling_policy.test2.arn
+  scheduling_policy_arn = local.select_scheduling_policy == "first" ? aws_batch_scheduling_policy.test1.arn : aws_batch_scheduling_policy.test2.arn
   state                 = "ENABLED"
 }
 `, rName, selectSchedulingPolicy))
