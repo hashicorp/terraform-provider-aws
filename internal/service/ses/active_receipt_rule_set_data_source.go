2c3b99d99a90a6963191696dd3239630c4220240
Daniel Quackenbush <25692880+danquack@users.noreply.github.com>
2021-12-21 22:16:07 -0500

    fix semgrep

--- internal/service/ses/active_receipt_rule_set_data_source.go
+++ internal/service/ses/active_receipt_rule_set_data_source.go
@@ -3,6 +3,7 @@ package ses
 import (
 	"fmt"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/arn"
 	"github.com/aws/aws-sdk-go/service/ses"
 	"github.com/hashicorp/aws-sdk-go-base/tfawserr"
@@ -36,14 +37,15 @@ func dataSourceActiveReceiptRuleSetRead(d *schema.ResourceData, meta interface{}
 		}
 		return fmt.Errorf("Error getting SES Active Receipt Rule Set: %s", err)
 	}
-	d.SetId(*data.Metadata.Name)
-	d.Set("rule_set_name", data.Metadata.Name)
+	name := aws.StringValue(data.Metadata.Name)
+	d.SetId(name)
+	d.Set("rule_set_name", name)
 	arn := arn.ARN{
 		Partition: meta.(*conns.AWSClient).Partition,
 		Service:   "ses",
 		Region:    meta.(*conns.AWSClient).Region,
 		AccountID: meta.(*conns.AWSClient).AccountID,
-		Resource:  fmt.Sprintf("receipt-rule-set/%s", *data.Metadata.Name),
+		Resource:  fmt.Sprintf("receipt-rule-set/%s", name),
 	}.String()
 	d.Set("arn", arn)
 	return nil
