eacba5dac9d1b9cd4bef6cf8505044aef1e254ae
GlennChia <glennchia7@gmail.com>
2021-12-26 14:55:45 +0800

    feat(connect): add ContactFlowModule read

--- internal/service/connect/contact_flow_module.go
+++ internal/service/connect/contact_flow_module.go
@@ -86,6 +86,68 @@ func ResourceContactFlowModule() *schema.Resource {
 	}
 }
 
+
+func resourceContactFlowModuleRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn := meta.(*conns.AWSClient).ConnectConn
+	defaultTagsConfig := meta.(*conns.AWSClient).DefaultTagsConfig
+	ignoreTagsConfig := meta.(*conns.AWSClient).IgnoreTagsConfig
+
+	instanceID, contactFlowModuleID, err := ContactFlowModuleParseID(d.Id())
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	resp, err := conn.DescribeContactFlowModuleWithContext(ctx, &connect.DescribeContactFlowModuleInput{
+		ContactFlowModuleId: aws.String(contactFlowModuleID),
+		InstanceId:          aws.String(instanceID),
+	})
+
+	if !d.IsNewResource() && tfawserr.ErrMessageContains(err, connect.ErrCodeResourceNotFoundException, "") {
+		log.Printf("[WARN] Connect Contact Flow Module (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	if err != nil {
+		return diag.FromErr(fmt.Errorf("error getting Connect Contact Flow Module (%s): %w", d.Id(), err))
+	}
+
+	if resp == nil || resp.ContactFlowModule == nil {
+		return diag.FromErr(fmt.Errorf("error getting Connect Contact Flow Module (%s): empty response", d.Id()))
+	}
+
+	d.Set("arn", resp.ContactFlowModule.Arn)
+	d.Set("contact_flow_module_id", resp.ContactFlowModule.Id)
+	d.Set("instance_id", instanceID)
+	d.Set("name", resp.ContactFlowModule.Name)
+	d.Set("description", resp.ContactFlowModule.Description)
+	d.Set("content", resp.ContactFlowModule.Content)
+
+	tags := KeyValueTags(resp.ContactFlowModule.Tags).IgnoreAWS().IgnoreConfig(ignoreTagsConfig)
+
+	//lintignore:AWSR002
+	if err := d.Set("tags", tags.RemoveDefaultConfig(defaultTagsConfig).Map()); err != nil {
+		return diag.FromErr(fmt.Errorf("error setting tags: %w", err))
+	}
+
+	if err := d.Set("tags_all", tags.Map()); err != nil {
+		return diag.FromErr(fmt.Errorf("error setting tags_all: %w", err))
+	}
+
+	return nil
+}
+
+func ContactFlowModuleParseID(id string) (string, string, error) {
+	parts := strings.SplitN(id, ":", 2)
+
+	if len(parts) != 2 || parts[0] == "" || parts[1] == "" {
+		return "", "", fmt.Errorf("unexpected format of ID (%s), expected instanceID:contactFlowModuleID", id)
+	}
+
+	return parts[0], parts[1], nil
+}
+
 func resourceContactFlowModuleLoadFileContent(filename string) (string, error) {
 	filename, err := homedir.Expand(filename)
 	if err != nil {
