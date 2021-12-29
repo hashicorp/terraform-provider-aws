28dedea205c51f57e81b3fbf6e6cf426b0b69948
awsaxeman <34073510+awsaxeman@users.noreply.github.com>
2021-12-29 08:31:23 -0500

    test lint

--- internal/service/fsx/backup_test.go
+++ internal/service/fsx/backup_test.go
@@ -369,7 +369,7 @@ resource "aws_fsx_openzfs_file_system" "test" {
   subnet_ids          = [aws_subnet.test1.id]
   deployment_type     = "SINGLE_AZ_1"
   throughput_capacity = 64
-	  
+
 
   tags = {
     Name = %[1]q
