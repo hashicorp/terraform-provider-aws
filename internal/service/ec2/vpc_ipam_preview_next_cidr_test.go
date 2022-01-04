a760780e0180d3f82fb55335dbadbedc27003212
GlennChia <glennchia7@gmail.com>
2022-01-04 23:12:36 +0800

    test(ec2): allocate ipam pool cidr from preview

--- internal/service/ec2/vpc_ipam_preview_next_cidr_test.go
+++ internal/service/ec2/vpc_ipam_preview_next_cidr_test.go
@@ -32,6 +32,40 @@ func TestAccVPCIpamPreviewNextCidr_ipv4Basic(t *testing.T) {
 	})
 }
 
+func TestAccVPCIpamPreviewNextCidr_ipv4Allocated(t *testing.T) {
+	resourceName := "aws_vpc_ipam_preview_next_cidr.test"
+	netmaskLength := "28"
+	allocatedCidr := "172.2.0.0/28"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { acctest.PreCheck(t); testAccIPAMPreCheck(t) },
+		ErrorCheck:   acctest.ErrorCheck(t, ec2.EndpointsID),
+		Providers:    acctest.Providers,
+		CheckDestroy: nil,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccVPCIpamPreviewNextCidrIpv4Basic(netmaskLength),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "cidr", allocatedCidr),
+					resource.TestCheckResourceAttrSet(resourceName, "id"),
+					resource.TestCheckResourceAttrPair(resourceName, "ipam_pool_id", "aws_vpc_ipam_pool.test", "id"),
+					resource.TestCheckResourceAttr(resourceName, "netmask_length", netmaskLength),
+				),
+			},
+			{
+				Config: testAccVPCIpamPreviewNextCidrIpv4Allocated(netmaskLength),
+				Check: resource.ComposeTestCheckFunc(
+					// cidr should not change even after allocation
+					resource.TestCheckResourceAttr(resourceName, "cidr", allocatedCidr),
+					resource.TestCheckResourceAttrSet(resourceName, "id"),
+					resource.TestCheckResourceAttrPair(resourceName, "ipam_pool_id", "aws_vpc_ipam_pool.test", "id"),
+					resource.TestCheckResourceAttr(resourceName, "netmask_length", netmaskLength),
+				),
+			},
+		},
+	})
+}
+
 // // temp comment out till bug is resolved
 // func TestAccVPCIpamPreviewNextCidr_ipv4DisallowedCidr(t *testing.T) {
 // 	resourceName := "aws_vpc_ipam_preview_next_cidr.test"
@@ -92,6 +126,26 @@ resource "aws_vpc_ipam_preview_next_cidr" "test" {
   depends_on = [
     aws_vpc_ipam_pool_cidr.test
   ]
+}
+`, netmaskLength))
+}
+
+func testAccVPCIpamPreviewNextCidrIpv4Allocated(netmaskLength string) string {
+	return acctest.ConfigCompose(
+		testAccVPCIpamPreviewNextCidrIpv4Base,
+		fmt.Sprintf(`
+resource "aws_vpc_ipam_preview_next_cidr" "test" {
+  ipam_pool_id   = aws_vpc_ipam_pool.test.id
+  netmask_length = %[1]q
+
+  depends_on = [
+    aws_vpc_ipam_pool_cidr.test
+  ]
+}
+
+resource "aws_vpc_ipam_pool_cidr_allocation" "test" {
+  ipam_pool_id = aws_vpc_ipam_pool.test.id
+  cidr         = aws_vpc_ipam_preview_next_cidr.test.cidr
 }
 `, netmaskLength))
 }
