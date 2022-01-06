a285c8f65acb5b4c7b55dc521da0836b850f3e9f
DrFaust92 <ilia.lazebnik@gmail.com>
2022-01-06 15:42:18 +0200

    env member

--- internal/service/cloud9/find.go
+++ internal/service/cloud9/find.go
@@ -39,3 +39,36 @@ func FindEnvironmentByID(conn *cloud9.Cloud9, id string) (*cloud9.Environment, e
 
 	return env, nil
 }
+
+func FindEnvironmentMembershipByID(conn *cloud9.Cloud9, envId, userArn string) (*cloud9.EnvironmentMember, error) {
+	input := &cloud9.DescribeEnvironmentMembershipsInput{
+		EnvironmentId: aws.String(envId),
+		UserArn:       aws.String(userArn),
+	}
+	out, err := conn.DescribeEnvironmentMemberships(input)
+
+	if tfawserr.ErrCodeEquals(err, cloud9.ErrCodeNotFoundException) {
+		return nil, &resource.NotFoundError{
+			LastError:   err,
+			LastRequest: input,
+		}
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	envs := out.Memberships
+
+	if len(envs) == 0 {
+		return nil, tfresource.NewEmptyResultError(input)
+	}
+
+	env := envs[0]
+
+	if env == nil {
+		return nil, tfresource.NewEmptyResultError(input)
+	}
+
+	return env, nil
+}
