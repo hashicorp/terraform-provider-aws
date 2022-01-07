ab6fdceebd811146d9b4e6a21627a9a0abcd7a44
Dirk Avery <dirk.avery@gmail.com>
2022-01-07 15:59:43 -0500

    Better skip message for long test

--- internal/service/ecs/task_definition_test.go
+++ internal/service/ecs/task_definition_test.go
@@ -430,7 +430,7 @@ func TestAccECSTaskDefinition_fsxWinFileSystem(t *testing.T) {
 	domainName := acctest.RandomDomainName()
 
 	if testing.Short() {
-		t.Skip("skipping testing in short mode")
+		t.Skip("skipping ~3400s test in short mode")
 	}
 
 	if acctest.Partition() == "aws-us-gov" {
