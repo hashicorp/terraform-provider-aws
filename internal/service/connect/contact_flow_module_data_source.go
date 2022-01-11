a3a7d20865b5b9b6e9584011e2add7d5b270b48f
GlennChia <glennchia7@gmail.com>
2022-01-11 12:14:04 +0800

    feat(connect): contact flow module datasrc reads
    uses either contact_flow_module_id or name
    DescribeContactFlowModuleWithContext api
    ListContactFlowModulesPagesWithContext api

--- internal/service/connect/contact_flow_module_data_source.go
+++ internal/service/connect/contact_flow_module_data_source.go
@@ -56,3 +56,93 @@ func DataSourceContactFlowModule() *schema.Resource {
 		},
 	}
 }
+
+func dataSourceContactFlowModuleRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn := meta.(*conns.AWSClient).ConnectConn
+	ignoreTagsConfig := meta.(*conns.AWSClient).IgnoreTagsConfig
+
+	instanceID := d.Get("instance_id").(string)
+
+	input := &connect.DescribeContactFlowModuleInput{
+		InstanceId: aws.String(instanceID),
+	}
+
+	if v, ok := d.GetOk("contact_flow_module_id"); ok {
+		input.ContactFlowModuleId = aws.String(v.(string))
+	} else if v, ok := d.GetOk("name"); ok {
+		name := v.(string)
+		contactFlowModuleSummary, err := dataSourceGetConnectContactFlowModuleSummaryByName(ctx, conn, instanceID, name)
+
+		if err != nil {
+			return diag.FromErr(fmt.Errorf("error finding Connect Contact Flow Module Summary by name (%s): %w", name, err))
+		}
+
+		if contactFlowModuleSummary == nil {
+			return diag.FromErr(fmt.Errorf("error finding Connect Contact Flow Module Summary by name (%s): not found", name))
+		}
+
+		input.ContactFlowModuleId = contactFlowModuleSummary.Id
+	}
+
+	resp, err := conn.DescribeContactFlowModuleWithContext(ctx, input)
+
+	if err != nil {
+		return diag.FromErr(fmt.Errorf("error getting Connect Contact Flow Module: %w", err))
+	}
+
+	if resp == nil || resp.ContactFlowModule == nil {
+		return diag.FromErr(fmt.Errorf("error getting Connect Contact Flow Module: empty response"))
+	}
+
+	contactFlowModule := resp.ContactFlowModule
+
+	d.Set("arn", contactFlowModule.Arn)
+	d.Set("contact_flow_module_id", contactFlowModule.Id)
+	d.Set("content", contactFlowModule.Content)
+	d.Set("description", contactFlowModule.Description)
+	d.Set("name", contactFlowModule.Name)
+	d.Set("state", contactFlowModule.State)
+	d.Set("status", contactFlowModule.Status)
+
+	if err := d.Set("tags", KeyValueTags(contactFlowModule.Tags).IgnoreAWS().IgnoreConfig(ignoreTagsConfig).Map()); err != nil {
+		return diag.FromErr(fmt.Errorf("error setting tags: %s", err))
+	}
+
+	d.SetId(fmt.Sprintf("%s:%s", instanceID, aws.StringValue(contactFlowModule.Id)))
+
+	return nil
+}
+
+func dataSourceGetConnectContactFlowModuleSummaryByName(ctx context.Context, conn *connect.Connect, instanceID, name string) (*connect.ContactFlowModuleSummary, error) {
+	var result *connect.ContactFlowModuleSummary
+
+	input := &connect.ListContactFlowModulesInput{
+		InstanceId: aws.String(instanceID),
+		MaxResults: aws.Int64(ListContactFlowModulesMaxResults),
+	}
+
+	err := conn.ListContactFlowModulesPagesWithContext(ctx, input, func(page *connect.ListContactFlowModulesOutput, lastPage bool) bool {
+		if page == nil {
+			return !lastPage
+		}
+
+		for _, cf := range page.ContactFlowModulesSummaryList {
+			if cf == nil {
+				continue
+			}
+
+			if aws.StringValue(cf.Name) == name {
+				result = cf
+				return false
+			}
+		}
+
+		return !lastPage
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
--- internal/service/connect/enum.go
+++ internal/service/connect/enum.go
@@ -8,7 +8,11 @@ const BotAssociationStatusNotFound = "ResourceNotFoundException"
 const (
 	ListInstancesMaxResults = 10
 	// MaxResults Valid Range: Minimum value of 1. Maximum value of 1000
+	// https://docs.aws.amazon.com/connect/latest/APIReference/API_ListContactFlows.html
 	ListContactFlowsMaxResults = 60
+	// MaxResults Valid Range: Minimum value of 1. Maximum value of 1000
+	// https://docs.aws.amazon.com/connect/latest/APIReference/API_ListContactFlowModules.html
+	ListContactFlowModulesMaxResults = 60
 	// MaxResults Valid Range: Minimum value of 1. Maximum value of 25
 	ListBotsMaxResults = 25
 	// MaxResults Valid Range: Minimum value of 1. Maximum value of 1000
