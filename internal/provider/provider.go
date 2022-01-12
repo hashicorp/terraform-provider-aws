5e381d59c1321d7c0ebf2aeaf7ac026a41b37108
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-01-12 09:28:22 -0500

    Alphabetic order for resource names.

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -1595,8 +1595,8 @@ func Provider() *schema.Provider {
 			"aws_sfn_state_machine": sfn.ResourceStateMachine(),
 
 			"aws_shield_protection":                          shield.ResourceProtection(),
-			"aws_shield_protection_health_check_association": shield.ResourceProtectionHealthCheckAssociation(),
 			"aws_shield_protection_group":                    shield.ResourceProtectionGroup(),
+			"aws_shield_protection_health_check_association": shield.ResourceProtectionHealthCheckAssociation(),
 
 			"aws_signer_signing_job":                signer.ResourceSigningJob(),
 			"aws_signer_signing_profile":            signer.ResourceSigningProfile(),
