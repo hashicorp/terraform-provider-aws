a57b2faa9675ba3ccb3168b644931786227d9878
GlennChia <glennchia7@gmail.com>
2022-01-13 16:39:30 +0800

    test(connect): add basic queue config

--- internal/service/connect/queue_test.go
+++ internal/service/connect/queue_test.go
@@ -20,3 +20,20 @@ data "aws_connect_hours_of_operation" "test" {
 }
 `, rName)
 }
+
+func testAccQueueBasicConfig(rName, rName2, label string) string {
+	return acctest.ConfigCompose(
+		testAccQueueBaseConfig(rName),
+		fmt.Sprintf(`
+resource "aws_connect_queue" "test" {
+  instance_id           = aws_connect_instance.test.id
+  name                  = %[1]q
+  description           = %[2]q
+  hours_of_operation_id = data.aws_connect_hours_of_operation.test.hours_of_operation_id
+
+  tags = {
+    "Name" = "Test Queue",
+  }
+}
+`, rName2, label))
+}
