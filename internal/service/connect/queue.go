e2c2b3bd45a96d4d690f39431741e0d5b4362b4e
GlennChia <glennchia7@gmail.com>
2022-01-13 16:28:10 +0800

    feat(connect): add queue read

--- internal/service/connect/queue.go
+++ internal/service/connect/queue.go
@@ -104,6 +104,62 @@ func ResourceQueue() *schema.Resource {
 	}
 }
 
+func resourceQueueRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn := meta.(*conns.AWSClient).ConnectConn
+	defaultTagsConfig := meta.(*conns.AWSClient).DefaultTagsConfig
+	ignoreTagsConfig := meta.(*conns.AWSClient).IgnoreTagsConfig
+
+	instanceID, queueID, err := QueueParseID(d.Id())
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	resp, err := conn.DescribeQueueWithContext(ctx, &connect.DescribeQueueInput{
+		InstanceId: aws.String(instanceID),
+		QueueId:    aws.String(queueID),
+	})
+
+	if !d.IsNewResource() && tfawserr.ErrMessageContains(err, connect.ErrCodeResourceNotFoundException, "") {
+		log.Printf("[WARN] Connect Queue (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	if err != nil {
+		return diag.FromErr(fmt.Errorf("error getting Connect Queue (%s): %w", d.Id(), err))
+	}
+
+	if resp == nil || resp.Queue == nil {
+		return diag.FromErr(fmt.Errorf("error getting Connect Queue (%s): empty response", d.Id()))
+	}
+
+	if err := d.Set("outbound_caller_config", flattenOutboundCallerConfig(resp.Queue.OutboundCallerConfig)); err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.Set("arn", resp.Queue.QueueArn)
+	d.Set("description", resp.Queue.Description)
+	d.Set("hours_of_operation_id", resp.Queue.HoursOfOperationId)
+	d.Set("instance_id", instanceID)
+	d.Set("max_contacts", resp.Queue.MaxContacts)
+	d.Set("name", resp.Queue.Name)
+	d.Set("queue_id", resp.Queue.QueueId)
+	d.Set("status", resp.Queue.Status)
+
+	tags := KeyValueTags(resp.Queue.Tags).IgnoreAWS().IgnoreConfig(ignoreTagsConfig)
+
+	//lintignore:AWSR002
+	if err := d.Set("tags", tags.RemoveDefaultConfig(defaultTagsConfig).Map()); err != nil {
+		return diag.FromErr(fmt.Errorf("error setting tags: %w", err))
+	}
+
+	if err := d.Set("tags_all", tags.Map()); err != nil {
+		return diag.FromErr(fmt.Errorf("error setting tags_all: %w", err))
+	}
+
+	return nil
+}
 func expandOutboundCallerConfig(outboundCallerConfig []interface{}) *connect.OutboundCallerConfig {
 	if len(outboundCallerConfig) == 0 || outboundCallerConfig[0] == nil {
 		return nil
@@ -153,4 +209,14 @@ func flattenOutboundCallerConfig(outboundCallerConfig *connect.OutboundCallerCon
 	}
 
 	return []interface{}{values}
+}
+
+func QueueParseID(id string) (string, string, error) {
+	parts := strings.SplitN(id, ":", 2)
+
+	if len(parts) != 2 || parts[0] == "" || parts[1] == "" {
+		return "", "", fmt.Errorf("unexpected format of ID (%s), expected instanceID:queueID", id)
+	}
+
+	return parts[0], parts[1], nil
 }
