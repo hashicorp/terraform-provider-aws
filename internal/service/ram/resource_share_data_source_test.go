66b5c1acc4b89dcf866924aec293242694e6e1af
Nicolas Harlem Eide <nicolas@harlemeide.net>
2022-01-14 11:58:58 +0100

    Add acceptance tests

--- internal/service/ram/resource_share_data_source_test.go
+++ internal/service/ram/resource_share_data_source_test.go
@@ -31,6 +31,7 @@ func TestAccRAMResourceShareDataSource_basic(t *testing.T) {
 					resource.TestCheckResourceAttrPair(datasourceName, "name", resourceName, "name"),
 					resource.TestCheckResourceAttrPair(datasourceName, "id", resourceName, "id"),
 					resource.TestCheckResourceAttrSet(datasourceName, "owning_account_id"),
+					resource.TestCheckResourceAttr(datasourceName, "resources.%", "0"),
 				),
 			},
 		},
@@ -53,6 +54,29 @@ func TestAccRAMResourceShareDataSource_tags(t *testing.T) {
 					resource.TestCheckResourceAttrPair(datasourceName, "name", resourceName, "name"),
 					resource.TestCheckResourceAttrPair(datasourceName, "id", resourceName, "id"),
 					resource.TestCheckResourceAttrPair(datasourceName, "tags.%", resourceName, "tags.%"),
+					resource.TestCheckResourceAttr(datasourceName, "resources.%", "0"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccRAMResourceShareDataSource_resources(t *testing.T) {
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	resourceName := "aws_ram_resource_share.test"
+	datasourceName := "data.aws_ram_resource_share.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:   func() { acctest.PreCheck(t) },
+		ErrorCheck: acctest.ErrorCheck(t, ram.EndpointsID),
+		Providers:  acctest.Providers,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceShareDataSourceConfig_Resource(rName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrPair(datasourceName, "name", resourceName, "name"),
+					resource.TestCheckResourceAttrPair(datasourceName, "id", resourceName, "id"),
+					resource.TestCheckResourceAttr(datasourceName, "resources.%", "1"),
 				),
 			},
 		},
@@ -94,6 +118,41 @@ data "aws_ram_resource_share" "test" {
     name   = "Name"
     values = ["%s-Tags"]
   }
+}
+`, rName, rName, rName)
+}
+
+func testAccResourceShareDataSourceConfig_Resource(rName string) string {
+	return fmt.Sprintf(`
+resource "aws_ram_resource_share" "test" {
+  name = "tf-acc-test-ram-%s"
+}
+
+resource "aws_vpc" "test" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "tf-acc-test-ram-%s"
+  }
+}
+
+resource "aws_subnet" "test" {
+  cidr_block = "10.0.0.0/24"
+  vpc_id     = aws_vpc.test.id
+
+  tags = {
+    Name = "tf-acc-test-ram-%s"
+  }
+}
+
+resource "aws_ram_resource_association" "test" {
+	resource_arn = aws_subnet.test.arn
+	resource_share_arn = aws_ram_resource_share.test.arn
+}
+
+data "aws_ram_resource_share" "test" {
+  name           = aws_ram_resource_share.test.name
+  resource_owner = "SELF"
 }
 `, rName, rName, rName)
 }
