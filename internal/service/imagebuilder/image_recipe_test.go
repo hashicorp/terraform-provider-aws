b467fde58aff2a91e98835509fc54a17ba297a03
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-01-18 11:59:51 -0500

    Fix compilation error.

--- internal/service/imagebuilder/image_recipe_test.go
+++ internal/service/imagebuilder/image_recipe_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	tfimagebuilder "github.com/hashicorp/terraform-provider-aws/internal/service/imagebuilder"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 func TestAccImageBuilderImageRecipe_basic(t *testing.T) {
