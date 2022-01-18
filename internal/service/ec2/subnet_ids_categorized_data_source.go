5a80f75f0e2fab3f650d9fab7af01cdfbb9b4e1f
fireflycons <34012094+fireflycons@users.noreply.github.com>
2022-01-18 21:12:44 +0000

    WIP - NAT gw routing

--- internal/service/ec2/subnet_ids_categorized_data_source.go
+++ internal/service/ec2/subnet_ids_categorized_data_source.go
@@ -11,6 +11,13 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 )
 
+type subnetInfo struct {
+	publicSubnetIds       []string
+	privateSubnetIds      []string
+	privateNatSubnetIds   []string
+	privateNoNatSubnetIds []string
+}
+
 func DataSourceSubnetIDsCategorized() *schema.Resource {
 	return &schema.Resource{
 		Read: dataSourceSubnetIDsCategorizedRead,
@@ -29,23 +36,39 @@ func DataSourceSubnetIDsCategorized() *schema.Resource {
 				Computed: true,
 				Elem:     &schema.Schema{Type: schema.TypeString},
 			},
+			"private_subnet_nat_ids": {
+				Type:     schema.TypeSet,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"private_subnet_nonat_ids": {
+				Type:     schema.TypeSet,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
 		},
 	}
 }
 
 func dataSourceSubnetIDsCategorizedRead(d *schema.ResourceData, meta interface{}) error {
+	var vpcId string
 	conn := meta.(*conns.AWSClient).EC2Conn
 
 	if vpc, vpcOk := d.GetOk("vpc_id"); vpcOk {
-		publicSubnets, privateSubnets, err := getCategorizedSubnets(conn, vpc.(string))
+		vpcId = vpc.(string)
+		pSubnetInfo, err := getCategorizedSubnets(conn, vpcId)
 
 		if err != nil {
 			return err
 		}
 
-		d.SetId(d.Get("vpc_id").(string))
-		d.Set("public_subnet_ids", publicSubnets)
-		d.Set("private_subnet_ids", privateSubnets)
+		log.Printf("[INFO] aws_subnet_ids_categorized: %s: %+v\n", vpcId, *pSubnetInfo)
+
+		d.SetId(vpcId)
+		d.Set("public_subnet_ids", (*pSubnetInfo).publicSubnetIds)
+		d.Set("private_subnet_ids", (*pSubnetInfo).privateSubnetIds)
+		d.Set("private_subnet_nat_ids", (*pSubnetInfo).privateNatSubnetIds)
+		d.Set("private_subnet_nonat_ids", (*pSubnetInfo).privateNoNatSubnetIds)
 	} else {
 		return errors.New("Error reading vpc_id argument")
 	}
@@ -55,33 +78,40 @@ func dataSourceSubnetIDsCategorizedRead(d *schema.ResourceData, meta interface{}
 
 // Read IGW, subnets and route tables for specified VPC, and then categorize to public and private.
 // Returns: Two arrays - one of public subnet IDs and one of private subnet IDs.
-func getCategorizedSubnets(conn *ec2.EC2, vpcId string) ([]string, []string, error) {
+func getCategorizedSubnets(conn *ec2.EC2, vpcId string) (*subnetInfo, error) {
+	si := subnetInfo{
+		publicSubnetIds:       []string{},
+		privateSubnetIds:      []string{},
+		privateNatSubnetIds:   []string{},
+		privateNoNatSubnetIds: []string{},
+	}
+
 	allSubnetIds, err := getAllSubnetIds(conn, vpcId)
 
 	if err != nil {
-		return nil, nil, fmt.Errorf("error reading EC2 Subnets: %w", err)
+		return nil, fmt.Errorf("error reading EC2 Subnets: %w", err)
 	}
 
 	log.Printf("[INFO] aws_subnet_ids_categorized: %s: %d subnets retrieved\n", vpcId, len(allSubnetIds))
 
 	if len(allSubnetIds) == 0 {
-		// No subnets at all - return empty arrays.
-		return []string{}, []string{}, nil
+		// No subnets at all - nothing more to do.
+		return &si, nil
 	}
 
-	igw, err := getInternetGateway(conn, vpcId)
+	internetGateway, err := findInternetGateway(conn, vpcId)
 
 	if err != nil {
-		return nil, nil, fmt.Errorf("error reading EC2 Internet Gateways: %w", err)
+		return nil, fmt.Errorf("error reading EC2 Internet Gateways: %w", err)
 	}
 
-	if igw == nil {
+	haveInternetGateway := internetGateway != nil
+
+	if haveInternetGateway {
+		log.Printf("[INFO] aws_subnet_ids_categorized: %s: IGW retrieved: %s\n", vpcId, aws.StringValue(internetGateway.InternetGatewayId))
+	} else {
 		log.Printf("[INFO] aws_subnet_ids_categorized: %s: No IGW retrieved\n", vpcId)
-		// No IGW, then all subnets are private.
-		return []string{}, allSubnetIds, nil
 	}
-
-	log.Printf("[INFO] aws_subnet_ids_categorized: %s: IGW retrieved: %s\n", vpcId, aws.StringValue(igw.InternetGatewayId))
 
 	routeTables, err := FindRouteTables(conn, &ec2.DescribeRouteTablesInput{
 		Filters: BuildAttributeFilterList(map[string]string{
@@ -90,44 +120,51 @@ func getCategorizedSubnets(conn *ec2.EC2, vpcId string) ([]string, []string, err
 	})
 
 	if err != nil {
-		return nil, nil, fmt.Errorf("error reading EC2 Route Tables: %w", err)
+		return nil, fmt.Errorf("error reading EC2 Route Tables: %w", err)
 	}
+
+	haveRouteTables := len(routeTables) > 0
 
 	log.Printf("[INFO] aws_subnet_ids_categorized: %s: %d Route tables retrieved\n", vpcId, len(routeTables))
 
-	if len(routeTables) == 0 {
-		// No route tables (unlikely), then all subnets are private.
-		return []string{}, allSubnetIds, nil
+	// Condition for all subnets are private and isolated
+	// NAT won't work without IGW
+	if !(haveRouteTables && haveInternetGateway) {
+		si.privateNoNatSubnetIds = allSubnetIds
+		si.privateSubnetIds = allSubnetIds
+
+		return &si, nil
 	}
 
-	publicSubnetIds, privateSubnetIds := categorizeSubnetIds(igw, routeTables, allSubnetIds)
+	si.publicSubnetIds, si.privateSubnetIds = categorizeSubnetIds(internetGateway, routeTables, allSubnetIds)
 
-	log.Printf("[INFO] aws_subnet_ids_categorized: %s: Public subnets: %+q\n", vpcId, publicSubnetIds)
-	log.Printf("[INFO] aws_subnet_ids_categorized: %s: Private subnets: %+q\n", vpcId, privateSubnetIds)
+	// Now read NAT gateways
+	natGateways, err := findNatGateways(conn, vpcId)
 
-	return publicSubnetIds, privateSubnetIds, nil
+	if err != nil {
+		return nil, fmt.Errorf("error reading EC2 NAT gateways: %w", err)
+	}
+
+	log.Printf("[INFO] aws_subnet_ids_categorized: %s: %d NAT gateways retrieved\n", vpcId, len(natGateways))
+
+	if len(natGateways) > 0 {
+		// further categorize private subnets
+		si.privateNatSubnetIds, si.privateNoNatSubnetIds = categorizePrivateSubnetIds(natGateways, routeTables, si.privateSubnetIds)
+	}
+
+	return &si, nil
 }
 
 // Categorize subnets into public and private given an IGW, at least one route table and at least one subnet.
 // Returns: Two arrays - one of public subnet IDs and one of private subnet IDs.
 func categorizeSubnetIds(igw *ec2.InternetGateway, routeTables []*ec2.RouteTable, allSubnetIds []string) ([]string, []string) {
-	var publicRouteTable *ec2.RouteTable
-
-	publicRouteTable = nil
+	if igw == nil {
+		// No IGW = All private.
+		return []string{}, allSubnetIds
+	}
 
 	// Find the public route table (routes to IGW)
-	for _, t := range routeTables {
-		for _, r := range t.Routes {
-			if r.GatewayId != nil && aws.StringValue(r.GatewayId) == aws.StringValue(igw.InternetGatewayId) {
-				publicRouteTable = t
-				break
-			}
-		}
-
-		if publicRouteTable != nil {
-			break
-		}
-	}
+	publicRouteTable := findRouteTableForGateway(routeTables, aws.StringValue(igw.InternetGatewayId))
 
 	if publicRouteTable == nil {
 		// All subnets are private
@@ -171,6 +208,65 @@ func categorizeSubnetIds(igw *ec2.InternetGateway, routeTables []*ec2.RouteTable
 	return setDifference(allSubnetIds, privateSubnetIds), privateSubnetIds
 }
 
+// Categorize private subnets into those which have a route to a NAT gateway and those which don't
+// Returns: Two arrays - one of routed private subnet IDs and one of isolated private subnet IDs.
+func categorizePrivateSubnetIds(natGateways []*ec2.NatGateway, routeTables []*ec2.RouteTable, privateSubnetIds []string) ([]string, []string) {
+	routedSubnetIds := []string{}
+
+	for _, gw := range natGateways {
+		routeTable := findRouteTableForGateway(routeTables, aws.StringValue(gw.NatGatewayId))
+
+		if routeTable == nil {
+			continue
+		}
+
+		for _, assoc := range routeTable.Associations {
+			if assoc.SubnetId != nil {
+				routedSubnetIds = append(routedSubnetIds, aws.StringValue(assoc.SubnetId))
+			}
+		}
+	}
+
+	return routedSubnetIds, setDifference(privateSubnetIds, routedSubnetIds)
+}
+
+// Find route table with route to specified gateway
+// Returns: Pointer to route table; else nil if no route found
+func findRouteTableForGateway(routeTables []*ec2.RouteTable, gatewayId string) *ec2.RouteTable {
+	for _, t := range routeTables {
+		for _, r := range t.Routes {
+			if r.GatewayId != nil && aws.StringValue(r.GatewayId) == gatewayId {
+				return t
+			}
+		}
+	}
+
+	return nil
+}
+
+// Find all NAT gateways (if any) on specified VPC
+// Returns: Array of NAT gateways
+func findNatGateways(conn *ec2.EC2, vpcId string) ([]*ec2.NatGateway, error) {
+	req := &ec2.DescribeNatGatewaysInput{}
+
+	req.Filter = BuildAttributeFilterList(
+		map[string]string{
+			"vpc-id": vpcId,
+		},
+	)
+
+	resp, err := conn.DescribeNatGateways(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp == nil || len(resp.NatGateways) == 0 {
+		return []*ec2.NatGateway{}, nil
+	}
+
+	return resp.NatGateways, nil
+}
+
 // Get subnet IDs of all subnets in specified VPC
 // Returns: Array of subnet IDs
 func getAllSubnetIds(conn *ec2.EC2, vpcId string) ([]string, error) {
@@ -200,7 +296,7 @@ func getAllSubnetIds(conn *ec2.EC2, vpcId string) ([]string, error) {
 
 // Get the internet gateway for the specified VPC
 // Returns: InternetGateway, or nil if there isn't one
-func getInternetGateway(conn *ec2.EC2, vpcId string) (*ec2.InternetGateway, error) {
+func findInternetGateway(conn *ec2.EC2, vpcId string) (*ec2.InternetGateway, error) {
 
 	igws, err := FindInternetGateways(conn, &ec2.DescribeInternetGatewaysInput{
 		Filters: BuildAttributeFilterList(map[string]string{
