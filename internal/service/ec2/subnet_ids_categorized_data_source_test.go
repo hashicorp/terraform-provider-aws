3be2763198ba3af9ffb56bb4eb6ebbda8cde89fa
fireflycons <34012094+fireflycons@users.noreply.github.com>
2022-01-19 11:54:32 +0000

    Make NAT tests have 2 gateways

--- internal/service/ec2/subnet_ids_categorized_data_source_test.go
+++ internal/service/ec2/subnet_ids_categorized_data_source_test.go
@@ -27,7 +27,9 @@ func TestAccEC2SubnetIDsCategorizedDataSource_basic(t *testing.T) {
 				Config: testAccSubnetIDsWithDataSourceCategorizedDataSourceConfig(rName, rInt),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("data.aws_subnet_ids_categorized.categorized", "public_subnet_ids.#", "0"),
-					resource.TestCheckResourceAttr("data.aws_subnet_ids_categorized.categorized", "private_subnet_ids.#", "3"),
+					resource.TestCheckResourceAttr("data.aws_subnet_ids_categorized.categorized", "private_subnet_ids.#", "4"),
+					resource.TestCheckResourceAttr("data.aws_subnet_ids_categorized.categorized", "private_subnet_nat_ids.#", "0"),
+					resource.TestCheckResourceAttr("data.aws_subnet_ids_categorized.categorized", "private_subnet_nonat_ids.#", "4"),
 				),
 			},
 		},
@@ -48,7 +50,9 @@ func TestAccEC2SubnetIDsCategorizedDataSourceWithExplicitRoute_ids(t *testing.T)
 				Config: testAccSubnetIDsCategorizedDataSourceWithExplicitPublicRoute_ids(rName, rInt),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("data.aws_subnet_ids_categorized.categorized", "public_subnet_ids.#", "1"),
-					resource.TestCheckResourceAttr("data.aws_subnet_ids_categorized.categorized", "private_subnet_ids.#", "2"),
+					resource.TestCheckResourceAttr("data.aws_subnet_ids_categorized.categorized", "private_subnet_ids.#", "3"),
+					resource.TestCheckResourceAttr("data.aws_subnet_ids_categorized.categorized", "private_subnet_nat_ids.#", "0"),
+					resource.TestCheckResourceAttr("data.aws_subnet_ids_categorized.categorized", "private_subnet_nonat_ids.#", "3"),
 				),
 			},
 		},
@@ -68,8 +72,10 @@ func TestAccEC2SubnetIDsCategorizedDataSourceWithImplicitRoute_ids(t *testing.T)
 			{
 				Config: testAccSubnetIDsCategorizedDataSourceWithImplicitPublicRoute_ids(rName, rInt),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.aws_subnet_ids_categorized.categorized", "public_subnet_ids.#", "2"),
+					resource.TestCheckResourceAttr("data.aws_subnet_ids_categorized.categorized", "public_subnet_ids.#", "3"),
 					resource.TestCheckResourceAttr("data.aws_subnet_ids_categorized.categorized", "private_subnet_ids.#", "1"),
+					resource.TestCheckResourceAttr("data.aws_subnet_ids_categorized.categorized", "private_subnet_nat_ids.#", "0"),
+					resource.TestCheckResourceAttr("data.aws_subnet_ids_categorized.categorized", "private_subnet_nonat_ids.#", "1"),
 				),
 			},
 		},
@@ -90,7 +96,32 @@ func TestAccEC2SubnetIDsCategorizedDataSourceWithNoGateway_ids(t *testing.T) {
 				Config: testAccSubnetIDsCategorizedDataSourceWithNoGateway_ids(rName, rInt),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("data.aws_subnet_ids_categorized.categorized", "public_subnet_ids.#", "0"),
+					resource.TestCheckResourceAttr("data.aws_subnet_ids_categorized.categorized", "private_subnet_ids.#", "4"),
+					resource.TestCheckResourceAttr("data.aws_subnet_ids_categorized.categorized", "private_subnet_nat_ids.#", "0"),
+					resource.TestCheckResourceAttr("data.aws_subnet_ids_categorized.categorized", "private_subnet_nonat_ids.#", "4"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccEC2SubnetIDsCategorizedDataSourceWithNATGateways_ids(t *testing.T) {
+	rInt := sdkacctest.RandIntRange(0, 256)
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { acctest.PreCheck(t) },
+		ErrorCheck:   acctest.ErrorCheck(t, ec2.EndpointsID),
+		Providers:    acctest.Providers,
+		CheckDestroy: testAccCheckVpcDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSubnetIDsCategorizedDataSourceWithNATGateways_ids(rName, rInt),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.aws_subnet_ids_categorized.categorized", "public_subnet_ids.#", "2"),
 					resource.TestCheckResourceAttr("data.aws_subnet_ids_categorized.categorized", "private_subnet_ids.#", "3"),
+					resource.TestCheckResourceAttr("data.aws_subnet_ids_categorized.categorized", "private_subnet_nat_ids.#", "2"),
+					resource.TestCheckResourceAttr("data.aws_subnet_ids_categorized.categorized", "private_subnet_nonat_ids.#", "1"),
 				),
 			},
 		},
@@ -127,9 +158,19 @@ resource "aws_subnet" "test_a_two" {
   }
 }
 
-resource "aws_subnet" "test_b" {
+resource "aws_subnet" "test_b_one" {
   vpc_id            = aws_vpc.test.id
   cidr_block        = "172.%[2]d.126.0/24"
+  availability_zone = data.aws_availability_zones.available.names[1]
+
+  tags = {
+    Name = %[1]q
+  }
+}
+
+resource "aws_subnet" "test_b_two" {
+  vpc_id            = aws_vpc.test.id
+  cidr_block        = "172.%[2]d.127.0/24"
   availability_zone = data.aws_availability_zones.available.names[1]
 
   tags = {
@@ -149,7 +190,7 @@ data "aws_subnet_ids_categorized" "categorized" {
   depends_on = [
     aws_subnet.test_a_one,
     aws_subnet.test_a_two,
-    aws_subnet.test_b,
+    aws_subnet.test_b_one,
   ]
 }
 `)
@@ -276,9 +317,113 @@ data "aws_subnet_ids_categorized" "categorized" {
   depends_on = [
     aws_subnet.test_a_one,
     aws_subnet.test_a_two,
-    aws_subnet.test_b,
+    aws_subnet.test_b_one,
   ]
 }
-
 `, rName))
+}
+
+func testAccSubnetIDsCategorizedDataSourceWithNATGateways_ids(rName string, rInt int) string {
+	return acctest.ConfigCompose(testAccSubnetIDsCategorizedDataSourceConfigBase(rName, rInt), fmt.Sprintf(`
+# This one is private and isolated
+resource "aws_subnet" "test_a_three" {
+  vpc_id            = aws_vpc.test.id
+  cidr_block        = "172.%[2]d.128.0/24"
+  availability_zone = data.aws_availability_zones.available.names[1]
+  tags = {
+    Name = %[1]q
+  }
+}
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.test.id
+  tags = {
+    Name = %[1]q
+  }
+}
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.test.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = %[1]q
+  }
+}
+# Make subnets test_a_one and test_b_one public for NATs
+resource "aws_route_table_association" "a_one" {
+  subnet_id      = aws_subnet.test_a_one.id
+  route_table_id = aws_route_table.public.id
+}
+resource "aws_route_table_association" "b_one" {
+  subnet_id      = aws_subnet.test_b_one.id
+  route_table_id = aws_route_table.public.id
+}
+resource "aws_eip" "nat_eip_a" {
+  tags = {
+    Name = %[1]q
+  }
+}
+resource "aws_eip" "nat_eip_b" {
+  tags = {
+    Name = %[1]q
+  }
+}
+resource "aws_nat_gateway" "nat_a" {
+  allocation_id = aws_eip.nat_eip_a.id
+  subnet_id     = aws_subnet.test_a_one.id
+  depends_on = [
+    aws_internet_gateway.gw
+  ]
+  tags = {
+    Name = %[1]q
+  }
+}
+resource "aws_nat_gateway" "nat_b" {
+  allocation_id = aws_eip.nat_eip_b.id
+  subnet_id     = aws_subnet.test_b_one.id
+  depends_on = [
+    aws_internet_gateway.gw
+  ]
+  tags = {
+    Name = %[1]q
+  }
+}
+resource "aws_route_table" "private_a" {
+  vpc_id = aws_vpc.test.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_a.id
+  }
+  tags = {
+    Name = %[1]q
+  }
+}
+resource "aws_route_table" "private_b" {
+  vpc_id = aws_vpc.test.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_b.id
+  }
+  tags = {
+    Name = %[1]q
+  }
+}
+resource "aws_route_table_association" "a_two" {
+  subnet_id      = aws_subnet.test_a_two.id
+  route_table_id = aws_route_table.private_a.id
+}
+resource "aws_route_table_association" "b_two" {
+  subnet_id      = aws_subnet.test_b_two.id
+  route_table_id = aws_route_table.private_b.id
+}
+data "aws_subnet_ids_categorized" "categorized" {
+  vpc_id = aws_vpc.test.id
+  # Ensure data source not read till all referenced resources exist
+  depends_on = [
+    aws_route_table_association.a_one,
+    aws_route_table_association.a_two,
+    aws_route_table_association.b_two,
+  ]
+}`, rName, rInt))
 }
