96d8d9ca88b16fd432fd82b13a13f71cb05ac7d2
Angie Pinilla <angie@hashicorp.com>
2022-01-21 10:14:58 -0500

    CR updates: align with API and update overall provider design patterns

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -1577,6 +1577,7 @@ func Provider() *schema.Provider {
 			"aws_s3_bucket_policy":                            s3.ResourceBucketPolicy(),
 			"aws_s3_bucket_public_access_block":               s3.ResourceBucketPublicAccessBlock(),
 			"aws_s3_bucket_replication_configuration":         s3.ResourceBucketReplicationConfiguration(),
+			"aws_s3_bucket_versioning":                        s3.ResourceBucketVersioning(),
 			"aws_s3_object_copy":                              s3.ResourceObjectCopy(),
 
 			"aws_s3_access_point":                             s3control.ResourceAccessPoint(),
