fcee144eb3dec311f793bd9f75f2abe8b0d51303
drfaust92 <ilia.lazebnik@gmail.com>
2022-01-22 18:42:44 +0200

    apigw datasource

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -354,6 +354,7 @@ func Provider() *schema.Provider {
 			"aws_api_gateway_export":      apigateway.DataSourceExport(),
 			"aws_api_gateway_resource":    apigateway.DataSourceResource(),
 			"aws_api_gateway_rest_api":    apigateway.DataSourceRestAPI(),
+			"aws_api_gateway_sdk":         apigateway.DataSourceSdk(),
 			"aws_api_gateway_vpc_link":    apigateway.DataSourceVPCLink(),
 
 			"aws_apigatewayv2_api":  apigatewayv2.DataSourceAPI(),
