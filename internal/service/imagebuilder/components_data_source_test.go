58f6dd8c4166ef4c2c7ef4203220f6c62e65547c
Kamil Turek <kamil.turek@hotmail.com>
2022-01-22 22:36:55 +0100

    Split the acceptance test into two steps
    It takes some time for newly created image component to appear in the
    ListComponents response. Therefore, we needed to a separate test step to
    make sure the component will be already visible.

--- internal/service/imagebuilder/components_data_source_test.go
+++ internal/service/imagebuilder/components_data_source_test.go
@@ -10,57 +10,31 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 )
 
-func TestAccImageBuilderComponentsDataSource_owner(t *testing.T) {
-	dataSourceName := "data.aws_imagebuilder_components.test"
-
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:          func() { acctest.PreCheck(t) },
-		ErrorCheck:        acctest.ErrorCheck(t, imagebuilder.EndpointsID),
-		ProviderFactories: acctest.ProviderFactories,
-		CheckDestroy:      testAccCheckImageRecipeDestroy,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccComponentsOwnerDataSourceConfig,
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(dataSourceName, "arns.#", "0"),
-					resource.TestCheckResourceAttr(dataSourceName, "names.#", "0"),
-				),
-			},
-		},
-	})
-}
-
 func TestAccImageBuilderComponentsDataSource_filter(t *testing.T) {
 	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
 	dataSourceName := "data.aws_imagebuilder_components.test"
-	resourceName := "aws_imagebuilder_component.test"
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:          func() { acctest.PreCheck(t) },
 		ErrorCheck:        acctest.ErrorCheck(t, imagebuilder.EndpointsID),
 		ProviderFactories: acctest.ProviderFactories,
-		CheckDestroy:      testAccCheckImageRecipeDestroy,
+		CheckDestroy:      testAccCheckComponentDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccComponentsFilterDataSourceConfig(rName),
+				Config: testAccConfigComponent(rName),
+			},
+			{
+				Config: testAccConfigComponentWithDataSource(rName),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(dataSourceName, "arns.#", "1"),
 					resource.TestCheckResourceAttr(dataSourceName, "names.#", "1"),
-					resource.TestCheckResourceAttrPair(dataSourceName, "arns.0", resourceName, "arn"),
-					resource.TestCheckResourceAttrPair(dataSourceName, "names.0", resourceName, "name"),
 				),
 			},
 		},
 	})
 }
 
-const testAccComponentsOwnerDataSourceConfig = `
-data "aws_imagebuilder_components" "test" {
-  owner = "Self"
-}
-`
-
-func testAccComponentsFilterDataSourceConfig(rName string) string {
+func testAccConfigComponent(rName string) string {
 	return fmt.Sprintf(`
 resource "aws_imagebuilder_component" "test" {
   data = yamlencode({
@@ -79,14 +53,20 @@ resource "aws_imagebuilder_component" "test" {
   })
   name     = %[1]q
   platform = "Linux"
-  version  = "1.0.1"
-}
-
-data "aws_imagebuilder_components" "test" {
-  filter {
-    name   = "name"
-    values = [aws_imagebuilder_component.test.name]
-  }
+  version  = "1.0.0"
 }
 `, rName)
+}
+
+func testAccConfigComponentWithDataSource(rName string) string {
+	return acctest.ConfigCompose(
+		testAccConfigComponent(rName),
+		`
+data "aws_imagebuilder_components" "test" {
+  filter {
+    name = "name"
+    values = [aws_imagebuilder_component.test.name]  
+  }
+}
+`)
 }
