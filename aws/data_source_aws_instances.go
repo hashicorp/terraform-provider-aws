ccb27a0ca70c563c038184bbc09e68053c8782bc
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-01-25 11:08:12 -0500

    Revert "Add option to allow no results when querying for instances data"
    This reverts commit 9c81e9debb19f759f35fd6f4c039bcdc586bff92.

--- aws/data_source_aws_instances.go
+++ aws/data_source_aws_instances.go
@@ -49,11 +49,6 @@ func dataSourceAwsInstances() *schema.Resource {
 				Computed: true,
 				Elem:     &schema.Schema{Type: schema.TypeString},
 			},
-			"allow_no_results": {
-				Type:     schema.TypeBool,
-				Optional: true,
-				Default:  false,
-			},
 		},
 	}
 }
@@ -63,7 +58,6 @@ func dataSourceAwsInstancesRead(d *schema.ResourceData, meta interface{}) error 
 
 	filters, filtersOk := d.GetOk("filter")
 	tags, tagsOk := d.GetOk("instance_tags")
-	allowNoResults := d.Get("allow_no_results").(bool)
 
 	if !filtersOk && !tagsOk {
 		return fmt.Errorf("One of filters or instance_tags must be assigned")
@@ -113,7 +107,7 @@ func dataSourceAwsInstancesRead(d *schema.ResourceData, meta interface{}) error 
 		return err
 	}
 
-	if len(instanceIds) < 1 && allowNoResults == false {
+	if len(instanceIds) < 1 {
 		return fmt.Errorf("Your query returned no results. Please change your search criteria and try again.")
 	}
 
