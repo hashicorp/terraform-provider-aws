356c3f21a7759915adda04570c86b3609a0e44bc
GlennChia <glennchia7@gmail.com>
2022-01-26 11:52:38 +0800

    feat(connect): add read for permissions attribute

--- internal/service/connect/enum.go
+++ internal/service/connect/enum.go
@@ -27,6 +27,9 @@ const (
 	// ListQuickConnectsMaxResults Valid Range: Minimum value of 1. Maximum value of 1000.
 	// https://docs.aws.amazon.com/connect/latest/APIReference/API_ListQuickConnects.html
 	ListQuickConnectsMaxResults = 60
+	// ListSecurityProfilePermissionsMaxResults Valid Range: Minimum value of 1. Maximum value of 1000.
+	// https://docs.aws.amazon.com/connect/latest/APIReference/API_ListSecurityProfilePermissions.html
+	ListSecurityProfilePermissionsMaxResults = 60
 )
 
 func InstanceAttributeMapping() map[string]string {
