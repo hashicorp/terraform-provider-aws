a61ba090e982466f977f105620a5e6149f5f575a
Angie Pinilla <angie@hashicorp.com>
2022-01-26 16:28:25 -0500

    add missing import and update data source tests

--- internal/service/cloudwatchlogs/group_data_source.go
+++ internal/service/cloudwatchlogs/group_data_source.go
@@ -3,6 +3,7 @@ package cloudwatchlogs
 import (
 	"fmt"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
