bbff1fd3ac0ac141e99e369940a927bf661afa99
GlennChia <glennchia7@gmail.com>
2022-01-26 16:04:23 +0800

    test(connect): name for queue datasrc

--- internal/service/connect/queue_data_source_test.go
+++ internal/service/connect/queue_data_source_test.go
@@ -41,6 +41,38 @@ func TestAccConnectQueueDataSource_QueueID(t *testing.T) {
 	})
 }
 
+func TestAccConnectQueueDataSource_name(t *testing.T) {
+	rName := sdkacctest.RandomWithPrefix("resource-test-terraform")
+	rName2 := sdkacctest.RandomWithPrefix("resource-test-terraform")
+	resourceName := "aws_connect_queue.test"
+	datasourceName := "data.aws_connect_queue.test"
+	outboundCallerConfigName := "exampleOutboundCallerConfigName"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:   func() { acctest.PreCheck(t) },
+		ErrorCheck: acctest.ErrorCheck(t, connect.EndpointsID),
+		Providers:  acctest.Providers,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccQueueDataSourceConfig_Name(rName, rName2, outboundCallerConfigName),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrPair(datasourceName, "arn", resourceName, "arn"),
+					resource.TestCheckResourceAttrPair(datasourceName, "description", resourceName, "description"),
+					resource.TestCheckResourceAttrPair(datasourceName, "hours_of_operation_id", resourceName, "hours_of_operation_id"),
+					resource.TestCheckResourceAttrPair(datasourceName, "instance_id", resourceName, "instance_id"),
+					resource.TestCheckResourceAttrPair(datasourceName, "max_contacts", resourceName, "max_contacts"),
+					resource.TestCheckResourceAttrPair(datasourceName, "name", resourceName, "name"),
+					resource.TestCheckResourceAttrPair(datasourceName, "outbound_caller_config.#", resourceName, "outbound_caller_config.#"),
+					resource.TestCheckResourceAttrPair(datasourceName, "outbound_caller_config.0.outbound_caller_id_name", resourceName, "outbound_caller_config.0.outbound_caller_id_name"),
+					resource.TestCheckResourceAttrPair(datasourceName, "queue_id", resourceName, "queue_id"),
+					resource.TestCheckResourceAttrPair(datasourceName, "status", resourceName, "status"),
+					resource.TestCheckResourceAttrPair(datasourceName, "tags.%", resourceName, "tags.%"),
+				),
+			},
+		},
+	})
+}
+
 func testAccQueueBaseDataSourceConfig(rName, rName2, outboundCallerConfigName string) string {
 	return fmt.Sprintf(`
 resource "aws_connect_instance" "test" {
@@ -79,6 +111,17 @@ func testAccQueueDataSourceConfig_QueueID(rName, rName2, outboundCallerConfigNam
 data "aws_connect_queue" "test" {
   instance_id = aws_connect_instance.test.id
   queue_id    = aws_connect_queue.test.queue_id
+}
+`)
+}
+
+func testAccQueueDataSourceConfig_Name(rName, rName2, outboundCallerConfigName string) string {
+	return acctest.ConfigCompose(
+		testAccQueueBaseDataSourceConfig(rName, rName2, outboundCallerConfigName),
+		`
+data "aws_connect_queue" "test" {
+  instance_id = aws_connect_instance.test.id
+  name        = aws_connect_queue.test.name
 }
 `)
 }
