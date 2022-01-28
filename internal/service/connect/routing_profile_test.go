1b1db52a50e8439b5bd2a86170db0e7ce46da84f
GlennChia <glennchia7@gmail.com>
2022-01-28 18:33:25 +0800

    test(connect): RoutingProfile update concurrency

--- internal/service/connect/routing_profile_test.go
+++ internal/service/connect/routing_profile_test.go
@@ -18,8 +18,9 @@ import (
 // Serialized acceptance tests due to Connect account limits (max 2 parallel tests)
 func TestAccConnectRoutingProfile_serial(t *testing.T) {
 	testCases := map[string]func(t *testing.T){
-		"basic":      testAccRoutingProfile_basic,
-		"disappears": testAccRoutingProfile_disappears,
+		"basic":              testAccRoutingProfile_basic,
+		"disappears":         testAccRoutingProfile_disappears,
+		"update_concurrency": testAccRoutingProfile_updateConcurrency,
 	}
 
 	for name, tc := range testCases {
@@ -107,6 +108,59 @@ func testAccRoutingProfile_disappears(t *testing.T) {
 					acctest.CheckResourceDisappears(acctest.Provider, tfconnect.ResourceRoutingProfile(), resourceName),
 				),
 				ExpectNonEmptyPlan: true,
+			},
+		},
+	})
+}
+
+func testAccRoutingProfile_updateConcurrency(t *testing.T) {
+	var v connect.DescribeRoutingProfileOutput
+	rName := sdkacctest.RandomWithPrefix("resource-test-terraform")
+	rName2 := sdkacctest.RandomWithPrefix("resource-test-terraform")
+	rName3 := sdkacctest.RandomWithPrefix("resource-test-terraform")
+	resourceName := "aws_connect_routing_profile.test"
+	description := "testMediaConcurrencies"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { acctest.PreCheck(t) },
+		ErrorCheck:   acctest.ErrorCheck(t, connect.EndpointsID),
+		Providers:    acctest.Providers,
+		CheckDestroy: testAccCheckRoutingProfileDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccRoutingProfileBasicConfig(rName, rName2, rName3, description),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckRoutingProfileExists(resourceName, &v),
+					resource.TestCheckResourceAttrSet(resourceName, "arn"),
+					resource.TestCheckResourceAttrPair(resourceName, "default_outbound_queue_id", "aws_connect_queue.default_outbound_queue", "queue_id"),
+					resource.TestCheckResourceAttr(resourceName, "description", description),
+					resource.TestCheckResourceAttrPair(resourceName, "instance_id", "aws_connect_instance.test", "id"),
+					resource.TestCheckResourceAttr(resourceName, "media_concurrencies.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "media_concurrencies.0.channel", connect.ChannelVoice),
+					resource.TestCheckResourceAttr(resourceName, "media_concurrencies.0.concurrency", "1"),
+					resource.TestCheckResourceAttr(resourceName, "name", rName3),
+					resource.TestCheckResourceAttrSet(resourceName, "routing_profile_id"),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", "1"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccRoutingProfileMediaConcurrenciesConfig(rName, rName2, rName3, description),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccCheckRoutingProfileExists(resourceName, &v),
+					resource.TestCheckResourceAttrSet(resourceName, "arn"),
+					resource.TestCheckResourceAttrPair(resourceName, "default_outbound_queue_id", "aws_connect_queue.default_outbound_queue", "queue_id"),
+					resource.TestCheckResourceAttr(resourceName, "description", description),
+					resource.TestCheckResourceAttrPair(resourceName, "instance_id", "aws_connect_instance.test", "id"),
+					resource.TestCheckResourceAttr(resourceName, "media_concurrencies.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "name", rName3),
+					resource.TestCheckResourceAttrSet(resourceName, "routing_profile_id"),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", "1"),
+				),
 			},
 		},
 	})
@@ -213,6 +267,33 @@ resource "aws_connect_routing_profile" "test" {
   media_concurrencies {
     channel     = "VOICE"
     concurrency = 1
+  }
+
+  tags = {
+    "Name" = "Test Routing Profile",
+  }
+}
+`, rName3, label))
+}
+
+func testAccRoutingProfileMediaConcurrenciesConfig(rName, rName2, rName3, label string) string {
+	return acctest.ConfigCompose(
+		testAccRoutingProfileBaseConfig(rName, rName2),
+		fmt.Sprintf(`
+resource "aws_connect_routing_profile" "test" {
+  instance_id               = aws_connect_instance.test.id
+  name                      = %[1]q
+  default_outbound_queue_id = aws_connect_queue.default_outbound_queue.queue_id
+  description               = %[2]q
+
+  media_concurrencies {
+    channel     = "VOICE"
+    concurrency = 1
+  }
+
+  media_concurrencies {
+    channel     = "CHAT"
+    concurrency = 2
   }
 
   tags = {
