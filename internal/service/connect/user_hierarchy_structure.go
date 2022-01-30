63e424c511d7748ead62450adf8214f2cd34d5ae
GlennChia <glennchia7@gmail.com>
2022-01-30 13:55:34 +0800

    feat(connect): userHierarchyStruc create

--- internal/service/connect/user_hierarchy_structure.go
+++ internal/service/connect/user_hierarchy_structure.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"strings"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/connect"
@@ -17,7 +18,8 @@ import (
 
 func ResourceUserHierarchyStructure() *schema.Resource {
 	return &schema.Resource{
-		ReadContext: resourceUserHierarchyStructureRead,
+		CreateContext: resourceUserHierarchyStructureCreate,
+		ReadContext:   resourceUserHierarchyStructureRead,
 		Importer: &schema.ResourceImporter{
 			StateContext: schema.ImportStatePassthroughContext,
 		},
@@ -145,6 +147,31 @@ func ResourceUserHierarchyStructure() *schema.Resource {
 	}
 }
 
+func resourceUserHierarchyStructureCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn := meta.(*conns.AWSClient).ConnectConn
+
+	instanceID := d.Get("instance_id").(string)
+
+	input := &connect.UpdateUserHierarchyStructureInput{
+		HierarchyStructure: expandUserHierarchyStructure(d.Get("hierarchy_structure").([]interface{})),
+		InstanceId:         aws.String(instanceID),
+	}
+
+	log.Printf("[DEBUG] Creating Connect User Hierarchy Structure %s", input)
+	_, err := conn.UpdateUserHierarchyStructureWithContext(ctx, input)
+
+	if err != nil {
+		return diag.FromErr(fmt.Errorf("error creating Connect User Hierarchy Structure for Connect Instance (%s): %w", instanceID, err))
+	}
+
+	// since user hierarchy structures do not have an ID or ARN, use the timestamp to generate a unique value
+	timestamp := time.Now().UTC()
+
+	d.SetId(fmt.Sprintf("%s:%s", instanceID, timestamp.Format(time.RFC3339)))
+
+	return resourceUserHierarchyStructureRead(ctx, d, meta)
+}
+
 func resourceUserHierarchyStructureRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	conn := meta.(*conns.AWSClient).ConnectConn
 
@@ -179,6 +206,46 @@ func resourceUserHierarchyStructureRead(ctx context.Context, d *schema.ResourceD
 	d.Set("instance_id", instanceID)
 
 	return nil
+}
+
+func expandUserHierarchyStructure(userHierarchyStructure []interface{}) *connect.HierarchyStructureUpdate {
+	if len(userHierarchyStructure) == 0 {
+		return &connect.HierarchyStructureUpdate{}
+	}
+
+	tfMap, ok := userHierarchyStructure[0].(map[string]interface{})
+
+	if !ok {
+		return nil
+	}
+
+	result := &connect.HierarchyStructureUpdate{
+		LevelOne:   expandUserHierarchyStructureLevel(tfMap["level_one"].([]interface{})),
+		LevelTwo:   expandUserHierarchyStructureLevel(tfMap["level_two"].([]interface{})),
+		LevelThree: expandUserHierarchyStructureLevel(tfMap["level_three"].([]interface{})),
+		LevelFour:  expandUserHierarchyStructureLevel(tfMap["level_four"].([]interface{})),
+		LevelFive:  expandUserHierarchyStructureLevel(tfMap["level_five"].([]interface{})),
+	}
+
+	return result
+}
+
+func expandUserHierarchyStructureLevel(userHierarchyStructureLevel []interface{}) *connect.HierarchyLevelUpdate {
+	if len(userHierarchyStructureLevel) == 0 {
+		return nil
+	}
+
+	tfMap, ok := userHierarchyStructureLevel[0].(map[string]interface{})
+
+	if !ok {
+		return nil
+	}
+
+	result := &connect.HierarchyLevelUpdate{
+		Name: aws.String(tfMap["name"].(string)),
+	}
+
+	return result
 }
 
 func flattenUserHierarchyStructure(userHierarchyStructure *connect.HierarchyStructure) []interface{} {
