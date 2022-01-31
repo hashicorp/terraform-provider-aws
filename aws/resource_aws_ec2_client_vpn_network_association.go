474c03b2d1b1901ff0f8beb9faba7cb2cd619186
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-01-31 14:03:40 -0500

    Revert "adding support for timeouts block"
    This reverts commit 3de43c67c856e229a859b04ef0958cedcf13199c.

--- aws/resource_aws_ec2_client_vpn_network_association.go
+++ aws/resource_aws_ec2_client_vpn_network_association.go
@@ -21,11 +21,6 @@ func resourceAwsEc2ClientVpnNetworkAssociation() *schema.Resource {
 			State: resourceAwsEc2ClientVpnNetworkAssociationImport,
 		},
 
-		Timeouts: &schema.ResourceTimeout{
-			Create: schema.DefaultTimeout(waiter.ClientVpnNetworkAssociationAssociatedTimeout),
-			Delete: schema.DefaultTimeout(waiter.ClientVpnNetworkAssociationDisassociatedTimeout),
-		},
-
 		Schema: map[string]*schema.Schema{
 			"association_id": {
 				Type:     schema.TypeString,
