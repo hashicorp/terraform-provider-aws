fd0609e2ffa1785f40bdc3b94f9d6c1ae2e386a2
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-01-31 13:15:30 -0500

    Revert "r/aws_client_vpn_endpoint: Add `vpc_id` and `security_group_ids` arguments."
    This reverts commit 327df16eefe0fa8627a8ee6613cfa3ebe7900c09.

--- internal/service/ec2/client_vpn_endpoint.go
+++ internal/service/ec2/client_vpn_endpoint.go
@@ -155,12 +155,6 @@ func ResourceClientVPNEndpoint() *schema.Resource {
 				Optional: true,
 				Elem:     &schema.Schema{Type: schema.TypeString},
 			},
-			"security_group_ids": {
-				Type:         schema.TypeSet,
-				Optional:     true,
-				Elem:         &schema.Schema{Type: schema.TypeString},
-				RequiredWith: []string{"vpc_id"},
-			},
 			"self_service_portal": {
 				Type:         schema.TypeString,
 				Optional:     true,
@@ -195,11 +189,6 @@ func ResourceClientVPNEndpoint() *schema.Resource {
 				ForceNew:     true,
 				Default:      ec2.TransportProtocolUdp,
 				ValidateFunc: validation.StringInSlice(ec2.TransportProtocol_Values(), false),
-			},
-			"vpc_id": {
-				Type:         schema.TypeString,
-				Optional:     true,
-				RequiredWith: []string{"security_group_ids"},
 			},
 			"vpn_port": {
 				Type:     schema.TypeInt,
@@ -252,20 +241,12 @@ func resourceClientVPNEndpointCreate(d *schema.ResourceData, meta interface{}) e
 		input.DnsServers = flex.ExpandStringSet(v.(*schema.Set))
 	}
 
-	if v, ok := d.GetOk("security_group_ids"); ok {
-		input.SecurityGroupIds = flex.ExpandStringSet(v.(*schema.Set))
-	}
-
 	if v, ok := d.GetOk("self_service_portal"); ok {
 		input.SelfServicePortal = aws.String(v.(string))
 	}
 
 	if v, ok := d.GetOk("session_timeout_hours"); ok {
 		input.SessionTimeoutHours = aws.Int64(int64(v.(int)))
-	}
-
-	if v, ok := d.GetOk("vpc_id"); ok {
-		input.VpcId = aws.String(v.(string))
 	}
 
 	log.Printf("[DEBUG] Creating EC2 Client VPN Endpoint: %s", input)
@@ -332,8 +313,7 @@ func resourceClientVPNEndpointRead(d *schema.ResourceData, meta interface{}) err
 	}
 	d.Set("description", ep.Description)
 	d.Set("dns_name", ep.DnsName)
-	d.Set("dns_servers", aws.StringValueSlice(ep.DnsServers))
-	d.Set("security_group_ids", aws.StringValueSlice(ep.SecurityGroupIds))
+	d.Set("dns_servers", ep.DnsServers)
 	if aws.StringValue(ep.SelfServicePortalUrl) != "" {
 		d.Set("self_service_portal", ec2.SelfServicePortalEnabled)
 	} else {
@@ -344,7 +324,6 @@ func resourceClientVPNEndpointRead(d *schema.ResourceData, meta interface{}) err
 	d.Set("split_tunnel", ep.SplitTunnel)
 	d.Set("status", ep.Status.Code)
 	d.Set("transport_protocol", ep.TransportProtocol)
-	d.Set("vpc_id", ep.VpcId)
 	d.Set("vpn_port", ep.VpnPort)
 
 	tags := KeyValueTags(ep.Tags).IgnoreAWS().IgnoreConfig(ignoreTagsConfig)
@@ -406,12 +385,6 @@ func resourceClientVPNEndpointUpdate(d *schema.ResourceData, meta interface{}) e
 			}
 		}
 
-		if d.HasChange("security_group_ids") {
-			if v, ok := d.GetOk("security_group_ids"); ok {
-				input.SecurityGroupIds = flex.ExpandStringSet(v.(*schema.Set))
-			}
-		}
-
 		if d.HasChange("self_service_portal") {
 			input.SelfServicePortal = aws.String(d.Get("self_service_portal").(string))
 		}
@@ -426,10 +399,6 @@ func resourceClientVPNEndpointUpdate(d *schema.ResourceData, meta interface{}) e
 
 		if d.HasChange("split_tunnel") {
 			input.SplitTunnel = aws.Bool(d.Get("split_tunnel").(bool))
-		}
-
-		if d.HasChange("vpc_id") {
-			input.VpcId = aws.String(d.Get("vpc_id").(string))
 		}
 
 		if d.HasChange("vpn_port") {
