3851e80a68a5fd10eb5c744051ec1d882c2f326a
Graham Davison <g.m.davison@computer.org>
2022-02-01 14:50:33 -0800

    Removes unneeded `gomnd` ignored functions

--- internal/service/events/target.go
+++ internal/service/events/target.go
@@ -335,7 +335,7 @@ func ResourceTarget() *schema.Resource {
 							Optional: true,
 							Elem:     &schema.Schema{Type: schema.TypeString},
 							ValidateFunc: validation.All(
-								mapMaxItems(mapMaxItemsCount),
+								mapMaxItems(targetInputTransformerMaxInputPaths),
 								mapKeysDoNotMatch(regexp.MustCompile(`^AWS.*$`), "input_path must not start with \"AWS\""),
 							),
 						},
