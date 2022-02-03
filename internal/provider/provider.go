d8640c1b79bfa4f369a40b5a8d49a53f2fdcf3c9
angie pinilla <angelinepinilla@gmail.com>
2022-02-03 13:01:17 -0500

    r/s3_bucket_lifecycle_configuration: new resource (#22579)
    * feat: new resource s3_bucket_lifecycle_configuration
    
    * only wait on update and format docs
    
    * remove conflicting validations
    
    * update imports referencing tfawserr
    
    * r/s3_bucket_lifecycle_configuration: re-use share id create/parse method; update docs
    
    * rename test and remove extra update
    
    * r/s3_bucket_lifecycle_configuration: change 'rule' to TypeList to respect order
    
    * add retry method in Exists test check for occassional flakiness

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -1626,6 +1626,7 @@ func Provider() *schema.Provider {
 			"aws_s3_bucket_cors_configuration":                s3.ResourceBucketCorsConfiguration(),
 			"aws_s3_bucket_intelligent_tiering_configuration": s3.ResourceBucketIntelligentTieringConfiguration(),
 			"aws_s3_bucket_inventory":                         s3.ResourceBucketInventory(),
+			"aws_s3_bucket_lifecycle_configuration":           s3.ResourceBucketLifecycleConfiguration(),
 			"aws_s3_bucket_logging":                           s3.ResourceBucketLogging(),
 			"aws_s3_bucket_metric":                            s3.ResourceBucketMetric(),
 			"aws_s3_bucket_notification":                      s3.ResourceBucketNotification(),
