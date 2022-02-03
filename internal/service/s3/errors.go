7288456e2a24ff4dc84ab4153736429b3795bcf1
Angie Pinilla <angie@hashicorp.com>
2022-02-02 20:22:03 -0500

    add missing error code

--- internal/service/s3/errors.go
+++ internal/service/s3/errors.go
@@ -11,4 +11,5 @@ const (
 	ErrCodeNoSuchWebsiteConfiguration           = "NoSuchWebsiteConfiguration"
 	ErrCodeObjectLockConfigurationNotFound      = "ObjectLockConfigurationNotFoundError"
 	ErrCodeOperationAborted                     = "OperationAborted"
+	ErrCodeServerSideEncryptionConfigurationNotFound = "ServerSideEncryptionConfigurationNotFoundError"
 )
