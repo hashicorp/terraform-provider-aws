b1842d089993b62c7c3d1e84835f0a9a3186b50a
Graham Davison <gdavison@hashicorp.com>
2022-02-03 14:23:40 -0800

    Ensures `goenv` is up-to-date

--- .teamcity/scripts/setup_goenv.sh
+++ .teamcity/scripts/setup_goenv.sh
@@ -2,4 +2,9 @@
 
 set -euo pipefail
 
+pushd "$GOENV_ROOT"
+printf '\nUpdating goenv to %s...\n' "${GOENV_TOOL_VERSION}"
+git pull origin "${GOENV_TOOL_VERSION}"
+popd
+
 goenv install -s "$(goenv local)" && goenv rehash
