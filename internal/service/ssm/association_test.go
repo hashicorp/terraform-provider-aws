4314a8c8780b6f34374ea7c9404708d39baea2d2
drfaust92 <ilia.lazebnik@gmail.com>
2022-02-04 18:51:13 +0200

    assocation add arn + wait

--- internal/service/ssm/association_test.go
+++ internal/service/ssm/association_test.go
@@ -767,7 +767,7 @@ resource "aws_ssm_association" "test" {
 func testAccAssociationBasicWithParametersUpdatedConfig(rName string) string {
 	return fmt.Sprintf(`
 resource "aws_ssm_document" "test" {
-  name          = %[1]q
+  name          = "%[1]s-2"
   document_type = "Command"
 
   content = <<-DOC
