0c0127c19bf452c0809e3d5ea16aa7819dd87112
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-02-04 09:44:31 -0500

    r/aws_network_acl_association: Start improvement of acceptance tests.
    Acceptance test output:
    
    % make testacc TESTS=TestAccEC2NetworkACLAssociation_ PKG=ec2
    ==> Checking that code complies with gofmt requirements...
    TF_ACC=1 go test ./internal/service/ec2/... -v -count 1 -parallel 20 -run='TestAccEC2NetworkACLAssociation_'  -timeout 180m
    === RUN   TestAccEC2NetworkACLAssociation_basic
    --- PASS: TestAccEC2NetworkACLAssociation_basic (34.96s)
    PASS
    ok      github.com/hashicorp/terraform-provider-aws/internal/service/ec2        38.706s

--- internal/service/ec2/network_acl_association_test.go
+++ internal/service/ec2/network_acl_association_test.go
@@ -1,28 +1,38 @@
 package ec2_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/service/ec2"
+	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	tfec2 "github.com/hashicorp/terraform-provider-aws/internal/service/ec2"
+	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 )
 
 func TestAccEC2NetworkACLAssociation_basic(t *testing.T) {
-	var networkAcl ec2.NetworkAcl
-	resourceName := "aws_network_acl.acl_a"
+	var v ec2.NetworkAclAssociation
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	resourceName := "aws_network_acl_association.test"
+	naclResourceName := "aws_network_acl.test"
+	subnetResourceName := "aws_subnet.test"
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { acctest.PreCheck(t) },
 		ErrorCheck:   acctest.ErrorCheck(t, ec2.EndpointsID),
 		Providers:    acctest.Providers,
-		CheckDestroy: testAccCheckNetworkACLDestroy,
+		CheckDestroy: testAccCheckNetworkACLAssociationDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccNetworkACLAssociationConfig,
+				Config: testAccNetworkACLAssociationConfig(rName),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					testAccCheckNetworkACLExists(resourceName, &networkAcl),
-					testAccCheckSubnetIsAssociatedWithAcl(resourceName, "aws_subnet.subnet_a"),
+					testAccCheckNetworkACLAssociationExists(resourceName, &v),
+					resource.TestCheckResourceAttrPair(resourceName, "network_acl_id", naclResourceName, "id"),
+					resource.TestCheckResourceAttrPair(resourceName, "subnet_id", subnetResourceName, "id"),
 				),
 			},
 			{
@@ -34,28 +44,136 @@ func TestAccEC2NetworkACLAssociation_basic(t *testing.T) {
 	})
 }
 
-const testAccNetworkACLAssociationConfig = `
-resource "aws_vpc" "test_vpc" {
+func testAccCheckNetworkACLAssociationDestroy(s *terraform.State) error {
+	conn := acctest.Provider.Meta().(*conns.AWSClient).EC2Conn
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "aws_network_acl_association" {
+			continue
+		}
+
+		_, err := tfec2.FindNetworkACLAssociationByID(conn, rs.Primary.ID)
+
+		if tfresource.NotFound(err) {
+			continue
+		}
+
+		if err != nil {
+			return err
+		}
+
+		return fmt.Errorf("Network ACL Association %s still exists", rs.Primary.ID)
+	}
+
+	return nil
+}
+
+func testAccCheckNetworkACLAssociationExists(n string, v *ec2.NetworkAclAssociation) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No Network ACL Association ID is set")
+		}
+
+		conn := acctest.Provider.Meta().(*conns.AWSClient).EC2Conn
+
+		output, err := tfec2.FindNetworkACLAssociationByID(conn, rs.Primary.ID)
+
+		if err != nil {
+			return err
+		}
+
+		*v = *output
+
+		return nil
+	}
+}
+
+func testAccNetworkACLAssociationConfig(rName string) string {
+	return fmt.Sprintf(`
+resource "aws_vpc" "test" {
   cidr_block = "10.1.0.0/16"
+
   tags = {
-    Name = "testAccAWSNetworkAclEsp"
+    Name = %[1]q
   }
 }
-resource "aws_network_acl" "acl_a" {
-  vpc_id = aws_vpc.test_vpc.id
+
+resource "aws_network_acl" "test" {
+  vpc_id = aws_vpc.test.id
+
   tags = {
-    Name = "terraform test"
+    Name = %[1]q
   }
 }
-resource "aws_subnet" "subnet_a" {
-  vpc_id     = aws_vpc.test_vpc.id
+
+resource "aws_subnet" "test" {
+  vpc_id     = aws_vpc.test.id
+
   cidr_block = "10.1.33.0/24"
+
   tags = {
-    Name = "terraform test"
+    Name = %[1]q
   }
 }
+
 resource "aws_network_acl_association" "test" {
-  network_acl_id = aws_network_acl.acl_a.id
-  subnet_id      = aws_subnet.subnet_a.id
+  network_acl_id = aws_network_acl.test.id
+  subnet_id      = aws_subnet.test.id
 }
-`
+`, rName)
+}
+
+func testAccNetworkACLAssociationTwoAssociationsConfig(rName string) string {
+	return fmt.Sprintf(`
+resource "aws_vpc" "test" {
+  cidr_block = "10.1.0.0/16"
+
+  tags = {
+    Name = %[1]q
+  }
+}
+
+resource "aws_network_acl" "test" {
+  vpc_id = aws_vpc.test.id
+
+  tags = {
+    Name = %[1]q
+  }
+}
+
+resource "aws_subnet" "test1" {
+  vpc_id     = aws_vpc.test.id
+
+  cidr_block = "10.1.33.0/24"
+
+  tags = {
+    Name = %[1]q
+  }
+}
+
+resource "aws_subnet" "test2" {
+  vpc_id     = aws_vpc.test.id
+
+  cidr_block = "10.1.34.0/24"
+
+  tags = {
+    Name = %[1]q
+  }
+}
+
+resource "aws_network_acl_association" "test1" {
+  network_acl_id = aws_network_acl.test.id
+  subnet_id      = aws_subnet.test1.id
+}
+
+resource "aws_network_acl_association" "test2" {
+  network_acl_id = aws_network_acl.test.id
+  subnet_id      = aws_subnet.test2.id
+}
+`, rName)
+}
