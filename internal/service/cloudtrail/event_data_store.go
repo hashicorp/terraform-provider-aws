2d45163fcdff72432fc33f0e21478019c146de53
Bruno Schaatsbergen <git@bschaatsbergen.com>
2022-02-05 00:34:43 +0100

    cloudtrail-lake: add tests

--- internal/service/cloudtrail/event_data_store.go
+++ internal/service/cloudtrail/event_data_store.go
@@ -3,6 +3,7 @@ package cloudtrail
 import (
 	"context"
 	"log"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/cloudtrail"
@@ -320,8 +321,8 @@ func resourceEventDataStoreRead(ctx context.Context, d *schema.ResourceData, met
 
 	d.Set("arn", eventDataStore.EventDataStoreArn)
 
-	d.Set("created_timestamp", eventDataStore.CreatedTimestamp)
-	d.Set("Updated_timestamp", eventDataStore.UpdatedTimestamp)
+	d.Set("created_timestamp", aws.TimeValue(eventDataStore.CreatedTimestamp).Format(time.RFC3339))
+	d.Set("updated_timestamp", aws.TimeValue(eventDataStore.UpdatedTimestamp).Format(time.RFC3339))
 	d.Set("multi_region_enabled", eventDataStore.MultiRegionEnabled)
 	d.Set("organization_enabled", eventDataStore.OrganizationEnabled)
 	d.Set("retention_period", eventDataStore.RetentionPeriod)
---
+++ internal/service/cloudtrail/event_data_store_test.go
@@ -0,0 +1,346 @@
+package cloudtrail_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/service/cloudtrail"
+	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	tfcloudtrail "github.com/hashicorp/terraform-provider-aws/internal/service/cloudtrail"
+	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
+)
+
+func TestAccEventDataStore_basic(t *testing.T) {
+	rName := "tf-test-" + sdkacctest.RandString(8)
+	resourceName := "aws_cloudtrail_event_data_store.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { acctest.PreCheck(t) },
+		ErrorCheck:   acctest.ErrorCheck(t, cloudtrail.EndpointsID),
+		Providers:    acctest.Providers,
+		CheckDestroy: testAccCheckEventDataStoreDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccEventDataStoreConfig(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckEventDataStoreExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "retention_period", "7"),
+					resource.TestCheckResourceAttr(resourceName, "multi_region_enabled", "false"),
+					resource.TestCheckResourceAttr(resourceName, "organization_enabled", "false"),
+					resource.TestCheckResourceAttr(resourceName, "termination_protection_enabled", "false"),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", "1"),
+					resource.TestCheckResourceAttr(resourceName, "tags.Test", "test"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+				ImportStateVerifyIgnore: []string{
+					// Need this workaround due to import behavior change introduced by https://github.com/hashicorp/terraform/issues/20985
+					"name",
+				},
+			},
+		},
+	})
+}
+
+func TestAccEventDataStore_multi_region_enabled(t *testing.T) {
+	rName := "tf-test-" + sdkacctest.RandString(8)
+	resourceName := "aws_cloudtrail_event_data_store.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { acctest.PreCheck(t) },
+		ErrorCheck:   acctest.ErrorCheck(t, cloudtrail.EndpointsID),
+		Providers:    acctest.Providers,
+		CheckDestroy: testAccCheckEventDataStoreDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccEventDataStoreConfig_multi_region_enabled(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckEventDataStoreExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "retention_period", "7"),
+					resource.TestCheckResourceAttr(resourceName, "multi_region_enabled", "true"),
+					resource.TestCheckResourceAttr(resourceName, "termination_protection_enabled", "false"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+				ImportStateVerifyIgnore: []string{
+					// Need this workaround due to import behavior change introduced by https://github.com/hashicorp/terraform/issues/20985
+					"name",
+				},
+			},
+		},
+	})
+}
+
+func TestAccEventDataStore_advanced_event_selector(t *testing.T) {
+	rName := "tf-test-" + sdkacctest.RandString(8)
+	resourceName := "aws_cloudtrail_event_data_store.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { acctest.PreCheck(t) },
+		ErrorCheck:   acctest.ErrorCheck(t, cloudtrail.EndpointsID),
+		Providers:    acctest.Providers,
+		CheckDestroy: testAccCheckEventDataStoreDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccEventDataStoreConfig_advancedEventSelector(rName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "advanced_event_selector.#", "5"),
+					resource.TestCheckResourceAttr(resourceName, "advanced_event_selector.0.name", "s3Custom"),
+					resource.TestCheckResourceAttr(resourceName, "advanced_event_selector.0.field_selector.#", "4"),
+					resource.TestCheckTypeSetElemNestedAttrs(resourceName, "advanced_event_selector.0.field_selector.*", map[string]string{
+						"field":    "eventCategory",
+						"equals.#": "1",
+						"equals.0": "Data",
+					}),
+					resource.TestCheckTypeSetElemNestedAttrs(resourceName, "advanced_event_selector.0.field_selector.*", map[string]string{
+						"field":    "eventName",
+						"equals.#": "1",
+						"equals.0": "DeleteObject",
+					}),
+					resource.TestCheckTypeSetElemNestedAttrs(resourceName, "advanced_event_selector.0.field_selector.*", map[string]string{
+						"field":    "readOnly",
+						"equals.#": "1",
+						"equals.0": "false",
+					}),
+					resource.TestCheckTypeSetElemNestedAttrs(resourceName, "advanced_event_selector.0.field_selector.*", map[string]string{
+						"field":    "resources.type",
+						"equals.#": "1",
+						"equals.0": "AWS::S3::Object",
+					}),
+					resource.TestCheckResourceAttr(resourceName, "advanced_event_selector.1.name", "lambdaLogAllEvents"),
+					resource.TestCheckResourceAttr(resourceName, "advanced_event_selector.1.field_selector.#", "2"),
+					resource.TestCheckTypeSetElemNestedAttrs(resourceName, "advanced_event_selector.1.field_selector.*", map[string]string{
+						"field":    "eventCategory",
+						"equals.#": "1",
+						"equals.0": "Data",
+					}),
+					resource.TestCheckTypeSetElemNestedAttrs(resourceName, "advanced_event_selector.1.field_selector.*", map[string]string{
+						"field":    "resources.type",
+						"equals.#": "1",
+						"equals.0": "AWS::Lambda::Function",
+					}),
+					resource.TestCheckResourceAttr(resourceName, "advanced_event_selector.2.name", "dynamoDbReadOnlyEvents"),
+					resource.TestCheckTypeSetElemNestedAttrs(resourceName, "advanced_event_selector.2.field_selector.*", map[string]string{
+						"field":    "readOnly",
+						"equals.#": "1",
+						"equals.0": "true",
+					}),
+					resource.TestCheckTypeSetElemNestedAttrs(resourceName, "advanced_event_selector.2.field_selector.*", map[string]string{
+						"field":    "resources.type",
+						"equals.#": "1",
+						"equals.0": "AWS::DynamoDB::Table",
+					}),
+					resource.TestCheckResourceAttr(resourceName, "advanced_event_selector.3.name", "s3OutpostsWriteOnlyEvents"),
+					resource.TestCheckResourceAttr(resourceName, "advanced_event_selector.3.field_selector.#", "3"),
+					resource.TestCheckTypeSetElemNestedAttrs(resourceName, "advanced_event_selector.3.field_selector.*", map[string]string{
+						"field":    "eventCategory",
+						"equals.#": "1",
+						"equals.0": "Data",
+					}),
+					resource.TestCheckTypeSetElemNestedAttrs(resourceName, "advanced_event_selector.3.field_selector.*", map[string]string{
+						"field":    "readOnly",
+						"equals.#": "1",
+						"equals.0": "false",
+					}),
+					resource.TestCheckTypeSetElemNestedAttrs(resourceName, "advanced_event_selector.3.field_selector.*", map[string]string{
+						"field":    "resources.type",
+						"equals.#": "1",
+						"equals.0": "AWS::S3Outposts::Object",
+					}),
+					resource.TestCheckResourceAttr(resourceName, "advanced_event_selector.4.name", "managementEventsSelector"),
+					resource.TestCheckResourceAttr(resourceName, "advanced_event_selector.4.field_selector.#", "1"),
+					resource.TestCheckTypeSetElemNestedAttrs(resourceName, "advanced_event_selector.4.field_selector.*", map[string]string{
+						"field":    "eventCategory",
+						"equals.#": "1",
+						"equals.0": "Management",
+					}),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+				ImportStateVerifyIgnore: []string{
+					// Need this workaround due to import behavior change introduced by https://github.com/hashicorp/terraform/issues/20985
+					"name",
+				},
+			},
+		},
+	})
+}
+
+func testAccEventDataStoreConfig(rName string) string {
+	return fmt.Sprintf(`
+resource "aws_cloudtrail_event_data_store" "test" {
+  name = %[1]q
+  retention_period     = 7
+  termination_protection_enabled = false
+  tags = {
+    Test = "test"
+  }
+}
+`, rName)
+}
+
+func testAccEventDataStoreConfig_multi_region_enabled(rName string) string {
+	return fmt.Sprintf(`
+resource "aws_cloudtrail_event_data_store" "test" {
+  name = %[1]q
+  retention_period     = 7
+  multi_region_enabled = true
+  termination_protection_enabled = false
+}
+`, rName)
+}
+
+func testAccEventDataStoreConfig_advancedEventSelector(rName string) string {
+	return fmt.Sprintf(`
+resource "aws_cloudtrail_event_data_store" "test" {
+  name = %[1]q
+  retention_period     = 7
+
+  termination_protection_enabled = false
+
+  advanced_event_selector {
+    name = "s3Custom"
+    field_selector {
+      field  = "eventCategory"
+      equals = ["Data"]
+    }
+
+    field_selector {
+      field  = "eventName"
+      equals = ["DeleteObject"]
+    }
+
+    field_selector {
+      field  = "readOnly"
+      equals = ["false"]
+    }
+
+    field_selector {
+      field  = "resources.type"
+      equals = ["AWS::S3::Object"]
+    }
+  }
+
+  advanced_event_selector {
+    name = "lambdaLogAllEvents"
+    field_selector {
+      field  = "eventCategory"
+      equals = ["Data"]
+    }
+
+    field_selector {
+      field  = "resources.type"
+      equals = ["AWS::Lambda::Function"]
+    }
+  }
+
+  advanced_event_selector {
+    name = "dynamoDbReadOnlyEvents"
+    field_selector {
+      field  = "eventCategory"
+      equals = ["Data"]
+    }
+
+    field_selector {
+      field  = "readOnly"
+      equals = ["true"]
+    }
+
+    field_selector {
+      field  = "resources.type"
+      equals = ["AWS::DynamoDB::Table"]
+    }
+  }
+
+  advanced_event_selector {
+    name = "s3OutpostsWriteOnlyEvents"
+    field_selector {
+      field  = "eventCategory"
+      equals = ["Data"]
+    }
+
+    field_selector {
+      field  = "readOnly"
+      equals = ["false"]
+    }
+
+    field_selector {
+      field  = "resources.type"
+      equals = ["AWS::S3Outposts::Object"]
+    }
+  }
+
+  advanced_event_selector {
+    name = "managementEventsSelector"
+    field_selector {
+      field  = "eventCategory"
+      equals = ["Management"]
+    }
+  }
+}
+`, rName)
+}
+
+func testAccCheckEventDataStoreExists(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No CloudTrail Event Data Store ARN is set")
+		}
+
+		conn := acctest.Provider.Meta().(*conns.AWSClient).CloudTrailConn
+
+		_, err := tfcloudtrail.FindEventDataStoreByArn(context.Background(), conn, rs.Primary.ID)
+
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckEventDataStoreDestroy(s *terraform.State) error {
+	conn := acctest.Provider.Meta().(*conns.AWSClient).CloudTrailConn
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "aws_cloudtrail_event_data_store" {
+			continue
+		}
+
+		output, err := tfcloudtrail.FindEventDataStoreByArn(context.Background(), conn, rs.Primary.ID)
+
+		if tfresource.NotFound(err) {
+			continue
+		}
+
+		deletionState := cloudtrail.EventDataStoreStatusPendingDeletion
+		if output.Status != &deletionState {
+			return err
+		}
+
+		return fmt.Errorf("CloudTrail Event Data Store %s still exists", rs.Primary.ID)
+	}
+
+	return nil
+}
