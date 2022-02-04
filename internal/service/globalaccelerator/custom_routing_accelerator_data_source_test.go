c13828c68023693359c7c62af5b0a5885277c619
Yvan Guidoin <yvan.guidoin@gmail.com>
2022-02-04 15:28:30 +0000

    Add datasource testing

---
+++ internal/service/globalaccelerator/custom_routing_accelerator_data_source_test.go
@@ -0,0 +1,65 @@
+package globalaccelerator_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/service/globalaccelerator"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+)
+
+func TestAccGlobalAcceleratorCustomRoutingAcceleratorDataSource_basic(t *testing.T) {
+	resourceName := "aws_globalaccelerator_customroutingaccelerator.test"
+	dataSourceName := "data.aws_globalaccelerator_customroutingaccelerator.test_by_arn"
+	dataSourceName2 := "data.aws_globalaccelerator_customroutingaccelerator.test_by_name"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:   func() { acctest.PreCheck(t); testAccPreCheckGlobalAccelerator(t) },
+		ErrorCheck: acctest.ErrorCheck(t, globalaccelerator.EndpointsID),
+		Providers:  acctest.Providers,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccGlobalAcceleratorCustomRoutingAcceleratorDataSourceConfig(resourceName),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrPair(dataSourceName, "name", resourceName, "name"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "enabled", resourceName, "enabled"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "dns_name", resourceName, "dns_name"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "hosted_zone_id", resourceName, "hosted_zone_id"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "ip_sets.#", resourceName, "ip_sets.#"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "ip_sets.0.ip_addresses.#", resourceName, "ip_sets.0.ip_addresses.#"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "ip_sets.0.ip_addresses.0", resourceName, "ip_sets.0.ip_addresses.0"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "ip_sets.0.ip_addresses.1", resourceName, "ip_sets.0.ip_addresses.1"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "ip_sets.0.ip_family", resourceName, "ip_sets.0.ip_family"),
+					resource.TestCheckResourceAttrPair(dataSourceName2, "name", resourceName, "name"),
+					resource.TestCheckResourceAttrPair(dataSourceName2, "enabled", resourceName, "enabled"),
+					resource.TestCheckResourceAttrPair(dataSourceName2, "dns_name", resourceName, "dns_name"),
+					resource.TestCheckResourceAttrPair(dataSourceName2, "hosted_zone_id", resourceName, "hosted_zone_id"),
+					resource.TestCheckResourceAttrPair(dataSourceName2, "ip_sets.#", resourceName, "ip_sets.#"),
+					resource.TestCheckResourceAttrPair(dataSourceName2, "ip_sets.0.ip_addresses.#", resourceName, "ip_sets.0.ip_addresses.#"),
+					resource.TestCheckResourceAttrPair(dataSourceName2, "ip_sets.0.ip_addresses.0", resourceName, "ip_sets.0.ip_addresses.0"),
+					resource.TestCheckResourceAttrPair(dataSourceName2, "ip_sets.0.ip_addresses.1", resourceName, "ip_sets.0.ip_addresses.1"),
+					resource.TestCheckResourceAttrPair(dataSourceName2, "ip_sets.0.ip_family", resourceName, "ip_sets.0.ip_family"),
+				),
+			},
+		},
+	})
+
+}
+
+func testAccGlobalAcceleratorCustomRoutingAcceleratorDataSourceConfig(rName string) string {
+	return fmt.Sprintf(`
+resource "aws_globalaccelerator_customroutingaccelerator" "test" {
+  name = %[1]q
+}
+
+
+data "aws_globalaccelerator_customroutingaccelerator" "test_by_arn" {
+	arn = aws_globalaccelerator_accelerator.test.id
+  }
+  
+  data "aws_globalaccelerator_customroutingaccelerator" "test_by_name" {
+	name = aws_globalaccelerator_accelerator.test.name
+  }
+`, rName)
+}
