a6494f9a193b0fb7a70e0460c442746913ed2889
Alex Schiff <alex.schiff@gmail.com>
2022-02-05 08:59:51 -0500

    f(workspace_test.go): running terraform fmt

--- internal/service/grafana/workspace_test.go
+++ internal/service/grafana/workspace_test.go
@@ -76,92 +76,91 @@ func TestAccGrafanaWorkspace_organization(t *testing.T) {
 func testAccWorkspaceConfigBasic(name string) string {
 	return fmt.Sprintf(`
 resource "aws_grafana_workspace" "test" {
-    account_access_type = "CURRENT_ACCOUNT"
-    authentication_providers = ["SAML"]
-    permission_type = "CUSTOMER_MANAGED"
-    name = %[1]q
-    role_arn = aws_iam_role.test.arn
+  account_access_type      = "CURRENT_ACCOUNT"
+  authentication_providers = ["SAML"]
+  permission_type          = "CUSTOMER_MANAGED"
+  name                     = %[1]q
+  role_arn                 = aws_iam_role.test.arn
 }
 
 resource "aws_iam_role" "test" {
-    name = %[1]q
-    assume_role_policy = jsonencode({
-      Version = "2012-10-17"
-      Statement = [
-        {
-          Action = "sts:AssumeRole"
-          Effect = "Allow"
-          Sid    = ""
-          Principal = {
-            Service = "grafana.amazonaws.com"
-          }
-        },
-      ]
-    })
-  }
+  name = %[1]q
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "grafana.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
 `, name)
 }
 
 func testAccWorkspaceConfigOrganization(name string) string {
 	return fmt.Sprintf(`
 resource "aws_grafana_workspace" "test" {
-    account_access_type = "ORGANIZATION"
-    authentication_providers = ["SAML"]
-    permission_type = "CUSTOMER_MANAGED"
-    name = %[1]q
-    role_arn = aws_iam_role.assume.arn
-    organizational_units = [ aws_organizations_organizational_unit.test.id ]
+  account_access_type      = "ORGANIZATION"
+  authentication_providers = ["SAML"]
+  permission_type          = "CUSTOMER_MANAGED"
+  name                     = %[1]q
+  role_arn                 = aws_iam_role.assume.arn
+  organizational_units     = [aws_organizations_organizational_unit.test.id]
 }
 
 resource "aws_iam_role" "assume" {
-    name = "${%[1]q}-assume"
-    assume_role_policy = jsonencode({
-        Version = "2012-10-17"
-        Statement = [
-        {
-            Action = "sts:AssumeRole"
-            Effect = "Allow"
-            Sid    = ""
-            Principal = {
-                Service = "grafana.amazonaws.com"
-            }
-        },
-      ]
-    })
+  name = "$%[1]q-assume"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "grafana.amazonaws.com"
+        }
+      },
+    ]
+  })
 }
 
 resource "aws_organizations_organization" "test" {
-    aws_service_access_principals = [
-        "cloudtrail.amazonaws.com",
-        "config.amazonaws.com"
-    ]
+  aws_service_access_principals = [
+    "cloudtrail.amazonaws.com",
+    "config.amazonaws.com"
+  ]
 
-    feature_set = "ALL"
+  feature_set = "ALL"
 }
 
 resource "aws_organizations_organizational_unit" "test" {
-    name = %[1]q
-    parent_id = aws_organizations_organization.test.roots[0].id
+  name      = %[1]q
+  parent_id = aws_organizations_organization.test.roots[0].id
 }
 
 resource "aws_iam_role" "org" {
-    name = "${%[1]q}-org"
-    assume_role_policy = jsonencode({
-        Version = "2012-10-17"
-        Statement = [
-        {
-            Action = "sts:AssumeRole"
-            Effect = "Allow"
-            Sid    = ""
-            Principal = {
-                Service = "organizations.amazonaws.com"
-            }
-        },
-      ]
-    })
-    managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AWSConfigRoleForOrganizations"]
+  name = "%[1]q-org"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "organizations.amazonaws.com"
+        }
+      },
+    ]
+  })
+  managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AWSConfigRoleForOrganizations"]
 }
-
 `, name)
 }
 
