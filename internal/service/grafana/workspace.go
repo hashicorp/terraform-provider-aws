3c771d2ff397a990bd4c6d5b8b470c877e97a3a7
Alex Schiff <alex.schiff@gmail.com>
2022-02-09 14:32:14 -0500

    updating imports

--- internal/service/grafana/workspace.go
+++ internal/service/grafana/workspace.go
@@ -2,16 +2,16 @@ package grafana
 
 import (
 	"fmt"
-	"github.com/aws/aws-sdk-go-v2/aws/arn"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"log"
 	"time"
 )
 
 import (
+	"github.com/aws/aws-sdk-go-v2/aws/arn"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/managedgrafana"
 	"github.com/hashicorp/aws-sdk-go-base/v2/awsv1shim/v2/tfawserr"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
