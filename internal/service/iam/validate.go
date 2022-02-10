bdbc1eac0ea2ff62288a22de38d0555b9785d82f
Graham Davison <gdavison@hashicorp.com>
2022-02-09 16:37:54 -0800

    Resolves `providerlint` V001 errors for IAM service

--- internal/service/iam/validate.go
+++ internal/service/iam/validate.go
@@ -5,50 +5,34 @@ import (
 	"net/url"
 	"regexp"
 	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 )
 
-func validRolePolicyName(v interface{}, k string) (ws []string, errors []error) {
-	// https://github.com/boto/botocore/blob/2485f5c/botocore/data/iam/2010-05-08/service-2.json#L8291-L8296
-	value := v.(string)
-	if len(value) > 128 {
-		errors = append(errors, fmt.Errorf(
-			"%q cannot be longer than 128 characters", k))
-	}
-	if !regexp.MustCompile(`^[\w+=,.@-]+$`).MatchString(value) {
-		errors = append(errors, fmt.Errorf(`%q must match [\w+=,.@-]`, k))
-	}
-	return
-}
+var validRolePolicyName = validation.All(
+	validation.StringLenBetween(1, 128),
+	validation.StringMatch(regexp.MustCompile(`^[\w+=,.@-]*$`), "must match [\\w+=,.@-]"),
+)
 
-func validRolePolicyNamePrefix(v interface{}, k string) (ws []string, errors []error) {
-	value := v.(string)
-	if len(value) > 100 {
-		errors = append(errors, fmt.Errorf(
-			"%q cannot be longer than 100 characters", k))
-	}
-	if !regexp.MustCompile(`^[\w+=,.@-]+$`).MatchString(value) {
-		errors = append(errors, fmt.Errorf(`%q must match [\w+=,.@-]`, k))
-	}
-	return
-}
+var validRolePolicyNamePrefix = validation.All(
+	validation.StringLenBetween(1, 100),
+	validation.StringMatch(regexp.MustCompile(`^[\w+=,.@-]*$`), "must match [\\w+=,.@-]"),
+)
 
-func validAccountAlias(v interface{}, k string) (ws []string, es []error) {
-	val := v.(string)
-
-	if (len(val) < 3) || (len(val) > 63) {
-		es = append(es, fmt.Errorf("%q must contain from 3 to 63 alphanumeric characters or hyphens", k))
-	}
-	if !regexp.MustCompile("^[a-z0-9][a-z0-9-]+$").MatchString(val) {
-		es = append(es, fmt.Errorf("%q must start with an alphanumeric character and only contain lowercase alphanumeric characters and hyphens", k))
-	}
-	if strings.Contains(val, "--") {
-		es = append(es, fmt.Errorf("%q must not contain consecutive hyphens", k))
-	}
-	if strings.HasSuffix(val, "-") {
-		es = append(es, fmt.Errorf("%q must not end in a hyphen", k))
-	}
-	return
-}
+var validAccountAlias = validation.All(
+	validation.StringLenBetween(3, 63),
+	validation.StringMatch(regexp.MustCompile(`^[a-z0-9][a-z0-9-]+$`), "must start with an alphanumeric character and only contain lowercase alphanumeric characters and hyphens"),
+	func(v interface{}, k string) (ws []string, es []error) {
+		val := v.(string)
+		if strings.Contains(val, "--") {
+			es = append(es, fmt.Errorf("%q must not contain consecutive hyphens", k))
+		}
+		if strings.HasSuffix(val, "-") {
+			es = append(es, fmt.Errorf("%q must not end in a hyphen", k))
+		}
+		return
+	},
+)
 
 func validOpenIDURL(v interface{}, k string) (ws []string, errors []error) {
 	value := v.(string)
