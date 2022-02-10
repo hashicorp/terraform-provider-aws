1af0af669a362acc911f640711a935c300fb134b
GlennChia <glennchia7@gmail.com>
2022-02-10 16:49:53 +0800

    test(backup): check ReportPlan update tags

--- internal/service/backup/report_plan_test.go
+++ internal/service/backup/report_plan_test.go
@@ -72,6 +72,91 @@ func TestAccBackupReportPlan_basic(t *testing.T) {
 		},
 	})
 }
+
+func TestAccBackupReportPlan_updateTags(t *testing.T) {
+	var reportPlan backup.DescribeReportPlanOutput
+
+	rName := sdkacctest.RandomWithPrefix("tf-test-bucket")
+	rName2 := fmt.Sprintf("tf_acc_test_%s", sdkacctest.RandString(7))
+	description := "example description"
+	resourceName := "aws_backup_report_plan.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { acctest.PreCheck(t); testAccPreCheck(t) },
+		ErrorCheck:   acctest.ErrorCheck(t, backup.EndpointsID),
+		Providers:    acctest.Providers,
+		CheckDestroy: testAccCheckReportPlanDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccBackupReportPlanConfig_basic(rName, rName2, description),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckReportPlanExists(resourceName, &reportPlan),
+					resource.TestCheckResourceAttrSet(resourceName, "arn"),
+					resource.TestCheckResourceAttrSet(resourceName, "creation_time"),
+					resource.TestCheckResourceAttrSet(resourceName, "deployment_status"),
+					resource.TestCheckResourceAttr(resourceName, "description", description),
+					resource.TestCheckResourceAttr(resourceName, "report_delivery_channel.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "report_delivery_channel.0.formats.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "report_delivery_channel.0.formats.0", "CSV"),
+					resource.TestCheckResourceAttrPair(resourceName, "report_delivery_channel.0.s3_bucket_name", "aws_s3_bucket.test", "id"),
+					resource.TestCheckResourceAttr(resourceName, "report_setting.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "report_setting.0.report_template", "RESTORE_JOB_REPORT"),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", "1"),
+					resource.TestCheckResourceAttr(resourceName, "tags.Name", "Test Report Plan"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccBackupReportPlanConfig_tags(rName, rName2, description),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckReportPlanExists(resourceName, &reportPlan),
+					resource.TestCheckResourceAttrSet(resourceName, "arn"),
+					resource.TestCheckResourceAttrSet(resourceName, "creation_time"),
+					resource.TestCheckResourceAttrSet(resourceName, "deployment_status"),
+					resource.TestCheckResourceAttr(resourceName, "description", description),
+					resource.TestCheckResourceAttr(resourceName, "report_delivery_channel.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "report_delivery_channel.0.formats.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "report_delivery_channel.0.formats.0", "CSV"),
+					resource.TestCheckResourceAttrPair(resourceName, "report_delivery_channel.0.s3_bucket_name", "aws_s3_bucket.test", "id"),
+					resource.TestCheckResourceAttr(resourceName, "report_setting.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "report_setting.0.report_template", "RESTORE_JOB_REPORT"),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", "2"),
+					resource.TestCheckResourceAttr(resourceName, "tags.Name", "Test Report Plan"),
+					resource.TestCheckResourceAttr(resourceName, "tags.Key2", "Value2a"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccBackupReportPlanConfig_tagsUpdated(rName, rName2, description),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckReportPlanExists(resourceName, &reportPlan),
+					resource.TestCheckResourceAttrSet(resourceName, "arn"),
+					resource.TestCheckResourceAttrSet(resourceName, "creation_time"),
+					resource.TestCheckResourceAttrSet(resourceName, "deployment_status"),
+					resource.TestCheckResourceAttr(resourceName, "description", description),
+					resource.TestCheckResourceAttr(resourceName, "report_delivery_channel.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "report_delivery_channel.0.formats.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "report_delivery_channel.0.formats.0", "CSV"),
+					resource.TestCheckResourceAttrPair(resourceName, "report_delivery_channel.0.s3_bucket_name", "aws_s3_bucket.test", "id"),
+					resource.TestCheckResourceAttr(resourceName, "report_setting.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "report_setting.0.report_template", "RESTORE_JOB_REPORT"),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", "3"),
+					resource.TestCheckResourceAttr(resourceName, "tags.Name", "Test Report Plan"),
+					resource.TestCheckResourceAttr(resourceName, "tags.Key2", "Value2b"),
+					resource.TestCheckResourceAttr(resourceName, "tags.Key3", "Value3"),
+				),
+			},
+		},
+	})
+}
 func testAccCheckReportPlanDestroy(s *terraform.State) error {
 	conn := acctest.Provider.Meta().(*conns.AWSClient).BackupConn
 	for _, rs := range s.RootModule().Resources {
@@ -160,3 +245,59 @@ resource "aws_backup_report_plan" "test" {
 }
 `, rName2, label))
 }
+
+func testAccBackupReportPlanConfig_tags(rName, rName2, label string) string {
+	return acctest.ConfigCompose(
+		testAccBackupReportPlanBaseConfig(rName),
+		fmt.Sprintf(`
+resource "aws_backup_report_plan" "test" {
+  name        = %[1]q
+  description = %[2]q
+
+  report_delivery_channel {
+    formats = [
+      "CSV"
+    ]
+    s3_bucket_name = aws_s3_bucket.test.id
+  }
+
+  report_setting {
+    report_template = "RESTORE_JOB_REPORT"
+  }
+
+  tags = {
+    "Name" = "Test Report Plan"
+    "Key2" = "Value2a"
+  }
+}
+`, rName2, label))
+}
+
+func testAccBackupReportPlanConfig_tagsUpdated(rName, rName2, label string) string {
+	return acctest.ConfigCompose(
+		testAccBackupReportPlanBaseConfig(rName),
+		fmt.Sprintf(`
+resource "aws_backup_report_plan" "test" {
+  name        = %[1]q
+  description = %[2]q
+
+  report_delivery_channel {
+    formats = [
+      "CSV"
+    ]
+    s3_bucket_name = aws_s3_bucket.test.id
+  }
+
+  report_setting {
+    report_template = "RESTORE_JOB_REPORT"
+  }
+
+  tags = {
+    "Name" = "Test Report Plan"
+    "Key2" = "Value2b"
+    "Key3" = "Value3"
+  }
+}
+`, rName2, label))
+}
+
