c4bc9ca36f5e310cf666d6ea4e4d1ad607859ba8
GlennChia <glennchia7@gmail.com>
2022-02-10 16:38:14 +0800

    feat(backup): add ReportPlan read

--- internal/service/backup/report_plan.go
+++ internal/service/backup/report_plan.go
@@ -1,14 +1,24 @@
 package backup
 
 import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/backup"
+	"github.com/hashicorp/aws-sdk-go-base/v2/awsv1shim/v2/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/flex"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 func ResourceReportPlan() *schema.Resource {
 	return &schema.Resource{
+		Read: resourceReportPlanRead,
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
 		},
@@ -103,4 +113,97 @@ func ResourceReportPlan() *schema.Resource {
 
 		CustomizeDiff: verify.SetTagsDiff,
 	}
+}
+
+func resourceReportPlanRead(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*conns.AWSClient).BackupConn
+	defaultTagsConfig := meta.(*conns.AWSClient).DefaultTagsConfig
+	ignoreTagsConfig := meta.(*conns.AWSClient).IgnoreTagsConfig
+
+	resp, err := conn.DescribeReportPlan(&backup.DescribeReportPlanInput{
+		ReportPlanName: aws.String(d.Id()),
+	})
+
+	if tfawserr.ErrMessageContains(err, backup.ErrCodeResourceNotFoundException, "") {
+		log.Printf("[WARN] Backup Report Plan (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+	if err != nil {
+		return fmt.Errorf("error reading Backup Report Plan (%s): %w", d.Id(), err)
+	}
+
+	d.Set("arn", resp.ReportPlan.ReportPlanArn)
+	d.Set("deployment_status", resp.ReportPlan.DeploymentStatus)
+	d.Set("description", resp.ReportPlan.ReportPlanDescription)
+	d.Set("name", resp.ReportPlan.ReportPlanName)
+
+	if err := d.Set("creation_time", resp.ReportPlan.CreationTime.Format(time.RFC3339)); err != nil {
+		return fmt.Errorf("error setting creation_time: %s", err)
+	}
+
+	if err := d.Set("report_delivery_channel", flattenReportDeliveryChannel(resp.ReportPlan.ReportDeliveryChannel)); err != nil {
+		return fmt.Errorf("error setting report_delivery_channel: %w", err)
+	}
+
+	if err := d.Set("report_setting", flattenReportSetting(resp.ReportPlan.ReportSetting)); err != nil {
+		return fmt.Errorf("error setting report_delivery_channel: %w", err)
+	}
+
+	tags, err := ListTags(conn, d.Get("arn").(string))
+	if err != nil {
+		return fmt.Errorf("error listing tags for Backup Report Plan (%s): %w", d.Id(), err)
+	}
+	tags = tags.IgnoreAWS().IgnoreConfig(ignoreTagsConfig)
+
+	//lintignore:AWSR002
+	if err := d.Set("tags", tags.RemoveDefaultConfig(defaultTagsConfig).Map()); err != nil {
+		return fmt.Errorf("error setting tags: %w", err)
+	}
+
+	if err := d.Set("tags_all", tags.Map()); err != nil {
+		return fmt.Errorf("error setting tags_all: %w", err)
+	}
+
+	return nil
+}
+
+func flattenReportDeliveryChannel(reportDeliveryChannel *backup.ReportDeliveryChannel) []interface{} {
+	if reportDeliveryChannel == nil {
+		return []interface{}{}
+	}
+
+	values := map[string]interface{}{
+		"s3_bucket_name": aws.StringValue(reportDeliveryChannel.S3BucketName),
+	}
+
+	if reportDeliveryChannel.Formats != nil && len(reportDeliveryChannel.Formats) > 0 {
+		values["formats"] = flex.FlattenStringSet(reportDeliveryChannel.Formats)
+	}
+
+	if v := reportDeliveryChannel.S3KeyPrefix; v != nil {
+		values["s3_key_prefix"] = aws.StringValue(v)
+	}
+
+	return []interface{}{values}
+}
+
+func flattenReportSetting(reportSetting *backup.ReportSetting) []interface{} {
+	if reportSetting == nil {
+		return []interface{}{}
+	}
+
+	values := map[string]interface{}{
+		"report_template": aws.StringValue(reportSetting.ReportTemplate),
+	}
+
+	if reportSetting.FrameworkArns != nil && len(reportSetting.FrameworkArns) > 0 {
+		values["framework_arns"] = flex.FlattenStringSet(reportSetting.FrameworkArns)
+	}
+
+	if reportSetting.NumberOfFrameworks != nil {
+		values["number_of_frameworks"] = aws.Int64Value(reportSetting.NumberOfFrameworks)
+	}
+
+	return []interface{}{values}
 }
