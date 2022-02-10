00b493324030fd48466a21476ec55182c537242a
drfaust92 <ilia.lazebnik@gmail.com>
2022-02-11 00:52:51 +0200

    fmt

--- internal/service/iam/virtual_mfa_device_test.go
+++ internal/service/iam/virtual_mfa_device_test.go
@@ -192,7 +192,7 @@ resource "aws_iam_virtual_mfa_device" "test" {
 
   tags = {
     %[2]q = %[3]q
-    %[4]q = %[5]q	
+    %[4]q = %[5]q
   }  
 }
 `, rName, tagKey1, tagValue1, tagKey2, tagValue2)
