9c9eacaacc604ad283aac06cf4f4f681e42037e0
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-02-11 17:39:35 -0500

    r/aws_route53domains_registered_domain: Add 'name_servers'.

--- internal/service/route53domains/registered_domain.go
+++ internal/service/route53domains/registered_domain.go
@@ -1,15 +1,20 @@
 package route53domains
 
 import (
+	"errors"
 	"fmt"
 	"log"
+	"regexp"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/route53domains"
 	"github.com/hashicorp/aws-sdk-go-base/v2/awsv1shim/v2/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/flex"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
@@ -22,10 +27,40 @@ func ResourceRegisteredDomain() *schema.Resource {
 		Update: resourceRegisteredDomainUpdate,
 		Delete: resourceRegisteredDomainDelete,
 
+		Timeouts: &schema.ResourceTimeout{
+			Update: schema.DefaultTimeout(30 * time.Minute),
+		},
+
 		Schema: map[string]*schema.Schema{
 			"domain_name": {
 				Type:     schema.TypeString,
 				Required: true,
+			},
+			"name_servers": {
+				Type:     schema.TypeString,
+				Optional: true,
+				MaxItems: 6,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"glue_ips": {
+							Type:     schema.TypeSet,
+							Optional: true,
+							MaxItems: 2,
+							Elem: &schema.Schema{
+								Type:         schema.TypeString,
+								ValidateFunc: validation.IsIPAddress,
+							},
+						},
+						"name": {
+							Type:     schema.TypeString,
+							Required: true,
+							ValidateFunc: validation.All(
+								validation.StringLenBetween(1, 255),
+								validation.StringMatch(regexp.MustCompile(`[a-zA-Z0-9_\-.]*`), "can contain only alphabetical characters (A-Z or a-z), numeric characters (0-9), underscore (_), the minus sign (-), and the period (.)"),
+							),
+						},
+					},
+				},
 			},
 			"tags":     tftags.TagsSchema(),
 			"tags_all": tftags.TagsSchemaComputed(),
@@ -39,7 +74,7 @@ func resourceRegisteredDomainCreate(d *schema.ResourceData, meta interface{}) er
 	conn := meta.(*conns.AWSClient).Route53DomainsConn
 
 	domainName := d.Get("domain_name").(string)
-	domainDetail, err := FindDomainDetailByName(conn, domainName)
+	domainDetail, err := findDomainDetailByName(conn, domainName)
 
 	if err != nil {
 		return fmt.Errorf("error reading Route 53 Domains Domain (%s): %w", domainName, err)
@@ -72,7 +107,7 @@ func resourceRegisteredDomainRead(d *schema.ResourceData, meta interface{}) erro
 	defaultTagsConfig := meta.(*conns.AWSClient).DefaultTagsConfig
 	ignoreTagsConfig := meta.(*conns.AWSClient).IgnoreTagsConfig
 
-	domainDetail, err := FindDomainDetailByName(conn, d.Id())
+	domainDetail, err := findDomainDetailByName(conn, d.Id())
 
 	if !d.IsNewResource() && tfresource.NotFound(err) {
 		log.Printf("[WARN] Route 53 Domains Domain %s not found, removing from state", d.Id())
@@ -85,6 +120,9 @@ func resourceRegisteredDomainRead(d *schema.ResourceData, meta interface{}) erro
 	}
 
 	d.Set("domain_name", domainDetail.DomainName)
+	if err := d.Set("name_servers", flattenNameservers(domainDetail.Nameservers)); err != nil {
+		return fmt.Errorf("error setting name_servers: %w", err)
+	}
 
 	tags, err := ListTags(conn, d.Id())
 
@@ -109,6 +147,24 @@ func resourceRegisteredDomainRead(d *schema.ResourceData, meta interface{}) erro
 func resourceRegisteredDomainUpdate(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*conns.AWSClient).Route53DomainsConn
 
+	if d.HasChange("nameservers") {
+		input := &route53domains.UpdateDomainNameserversInput{
+			DomainName:  aws.String(d.Id()),
+			Nameservers: expandNameservers(d.Get("name_servers").([]interface{})),
+		}
+
+		log.Printf("[DEBUG] Updating Route 53 Domains Domain name dervers: %s", input)
+		output, err := conn.UpdateDomainNameservers(input)
+
+		if err != nil {
+			return fmt.Errorf("error updating Route 53 Domains Domain (%s) name servers: %w", d.Id(), err)
+		}
+
+		if _, err := waitOperationSucceeded(conn, aws.StringValue(output.OperationId), d.Timeout(schema.TimeoutUpdate)); err != nil {
+			return fmt.Errorf("error waiting for Route 53 Domains Domain (%s) name servers update: %w", d.Id(), err)
+		}
+	}
+
 	if d.HasChange("tags_all") {
 		o, n := d.GetChange("tags_all")
 
@@ -126,7 +182,7 @@ func resourceRegisteredDomainDelete(d *schema.ResourceData, meta interface{}) er
 	return nil
 }
 
-func FindDomainDetailByName(conn *route53domains.Route53Domains, name string) (*route53domains.GetDomainDetailOutput, error) {
+func findDomainDetailByName(conn *route53domains.Route53Domains, name string) (*route53domains.GetDomainDetailOutput, error) {
 	input := &route53domains.GetDomainDetailInput{
 		DomainName: aws.String(name),
 	}
@@ -149,4 +205,144 @@ func FindDomainDetailByName(conn *route53domains.Route53Domains, name string) (*
 	}
 
 	return output, nil
+}
+
+func findOperationDetailByID(conn *route53domains.Route53Domains, id string) (*route53domains.GetOperationDetailOutput, error) {
+	input := &route53domains.GetOperationDetailInput{
+		OperationId: aws.String(id),
+	}
+
+	output, err := conn.GetOperationDetail(input)
+
+	if tfawserr.ErrMessageContains(err, route53domains.ErrCodeInvalidInput, "No operation found") {
+		return nil, &resource.NotFoundError{
+			LastError:   err,
+			LastRequest: input,
+		}
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	if output == nil {
+		return nil, tfresource.NewEmptyResultError(input)
+	}
+
+	return output, nil
+}
+
+func statusOperation(conn *route53domains.Route53Domains, id string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		output, err := findOperationDetailByID(conn, id)
+
+		if tfresource.NotFound(err) {
+			return nil, "", nil
+		}
+
+		if err != nil {
+			return nil, "", err
+		}
+
+		return output, aws.StringValue(output.Status), nil
+	}
+}
+
+func waitOperationSucceeded(conn *route53domains.Route53Domains, id string, timeout time.Duration) (*route53domains.GetOperationDetailOutput, error) {
+	stateConf := &resource.StateChangeConf{
+		Pending: []string{route53domains.OperationStatusSubmitted, route53domains.OperationStatusInProgress},
+		Target:  []string{route53domains.OperationStatusSuccessful},
+		Timeout: timeout,
+		Refresh: statusOperation(conn, id),
+	}
+
+	outputRaw, err := stateConf.WaitForState()
+
+	if output, ok := outputRaw.(*route53domains.GetOperationDetailOutput); ok {
+		tfresource.SetLastError(err, errors.New(aws.StringValue(output.Message)))
+
+		return output, err
+	}
+
+	return nil, err
+}
+
+func flattenNameserver(apiObject *route53domains.Nameserver) map[string]interface{} {
+	if apiObject == nil {
+		return nil
+	}
+
+	tfMap := map[string]interface{}{}
+
+	if v := apiObject.GlueIps; v != nil {
+		tfMap["glue_ips"] = aws.StringValueSlice(v)
+	}
+
+	if v := apiObject.Name; v != nil {
+		tfMap["name"] = aws.StringValue(v)
+	}
+
+	return tfMap
+}
+
+func expandNameserver(tfMap map[string]interface{}) *route53domains.Nameserver {
+	if tfMap == nil {
+		return nil
+	}
+
+	apiObject := &route53domains.Nameserver{}
+
+	if v, ok := tfMap["glue_ips"].(*schema.Set); ok && v.Len() > 0 {
+		apiObject.GlueIps = flex.ExpandStringSet(v)
+	}
+
+	if v, ok := tfMap["name"].(string); ok && v != "" {
+		apiObject.Name = aws.String(v)
+	}
+
+	return apiObject
+}
+
+func expandNameservers(tfList []interface{}) []*route53domains.Nameserver {
+	if len(tfList) == 0 {
+		return nil
+	}
+
+	var apiObjects []*route53domains.Nameserver
+
+	for _, tfMapRaw := range tfList {
+		tfMap, ok := tfMapRaw.(map[string]interface{})
+
+		if !ok {
+			continue
+		}
+
+		apiObject := expandNameserver(tfMap)
+
+		if apiObject == nil {
+			continue
+		}
+
+		apiObjects = append(apiObjects, apiObject)
+	}
+
+	return apiObjects
+}
+
+func flattenNameservers(apiObjects []*route53domains.Nameserver) []interface{} {
+	if len(apiObjects) == 0 {
+		return nil
+	}
+
+	var tfList []interface{}
+
+	for _, apiObject := range apiObjects {
+		if apiObject == nil {
+			continue
+		}
+
+		tfList = append(tfList, flattenNameserver(apiObject))
+	}
+
+	return tfList
 }
