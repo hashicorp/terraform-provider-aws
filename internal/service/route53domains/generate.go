73ac0d71f48eec9ee7cd4c0357cf3d39468f631b
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-02-15 17:25:37 -0500

    Hand massage tagging code for AWS Go SDK v2.

--- internal/service/route53domains/generate.go
+++
@@ -1,4 +0,0 @@
-//go:generate go run ../../generate/tags/main.go -GetTag -ListTags -ListTagsOp=ListTagsForDomain -ListTagsInIDElem=DomainName -ListTagsOutTagsElem=TagList -UntagOp=DeleteTagsForDomain -UntagInTagsElem=TagsToDelete -TagOp=UpdateTagsForDomain -TagInIDElem=DomainName -TagInTagsElem=TagsToUpdate -ServiceTagsSlice -UpdateTags
-// ONLY generate directives and package declaration! Do not add anything else to this file.
-
-package route53domains
