89b80340fac73ccbd331b60e82c16801c1152e5a
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-02-16 09:22:49 -0500

    d/aws_ssm_instances: Add VPC networking to allow SSM Agent to communicate with the SSM service.

--- internal/service/ssm/instances_data_source_test.go
+++ internal/service/ssm/instances_data_source_test.go
@@ -48,8 +48,11 @@ func TestAccSSMInstancesDataSource_filter(t *testing.T) {
 
 func testAccCheckInstancesDataSourceConfig_filter_instance(rName string) string {
 	return acctest.ConfigCompose(
+		acctest.ConfigAvailableAZsNoOptInDefaultExclude(),
 		acctest.AvailableEC2InstanceTypeForRegion("t2.micro", "t3.micro"),
 		fmt.Sprintf(`
+data "aws_partition" "current" {}
+
 data "aws_iam_policy" "test" {
   name = "AmazonSSMManagedInstanceCore"
 }
@@ -66,7 +69,7 @@ resource "aws_iam_role" "test" {
       "Effect": "Allow",
       "Principal": {
         "Service": [
-          "ec2.amazonaws.com"
+          "ec2.${data.aws_partition.current.dns_suffix}"
         ]
       },
       "Action": [
@@ -83,6 +86,52 @@ resource "aws_iam_instance_profile" "test" {
   role = aws_iam_role.test.name
 }
 
+resource "aws_vpc" "test" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = %[1]q
+  }
+}
+
+resource "aws_subnet" "test" {
+  vpc_id            = aws_vpc.test.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = %[1]q
+  }
+}
+
+resource "aws_internet_gateway" "test" {
+  vpc_id = aws_vpc.test.id
+
+  tags = {
+    Name = %[1]q
+  }
+}
+
+resource "aws_route_table" "test" {
+  vpc_id = aws_vpc.test.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.test.id
+  }
+
+  tags = {
+    Name = %[1]q
+  }
+}
+
+resource "aws_main_route_table_association" "test" {
+  route_table_id = aws_route_table.test.id
+  vpc_id         = aws_vpc.test.id
+}
+
 data "aws_ami" "test" {
   most_recent = true
   owners      = ["amazon"]
@@ -97,6 +146,15 @@ resource "aws_instance" "test" {
   ami                  = data.aws_ami.test.id
   instance_type        = data.aws_ec2_instance_type_offering.available.instance_type
   iam_instance_profile = aws_iam_instance_profile.test.name
+
+  vpc_security_group_ids      = [aws_vpc.test.default_security_group_id]
+  subnet_id                   = aws_subnet.test.id
+  associate_public_ip_address = true
+  depends_on                  = [aws_main_route_table_association.test]
+
+  tags = {
+    Name = %[1]q
+  }
 }
 `, rName))
 }
