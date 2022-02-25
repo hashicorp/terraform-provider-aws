9050480f02ea570d33c33084d678dfe38486b97b
Sam.Goodliff <sam.goodliff@iress.com>
2022-02-25 14:19:40 +0000

    fix: remove dotnet6 from eol list

--- internal/service/lambda/function_test.go
+++ internal/service/lambda/function_test.go
@@ -2083,8 +2083,6 @@ func TestAccLambdaFunction_runtimes(t *testing.T) {
 	for _, runtime := range lambda.Runtime_Values() {
 		// EOL runtimes.
 		switch runtime {
-		case lambda.RuntimeDotnet6:
-			fallthrough
 		case lambda.RuntimeNodejs43Edge:
 			fallthrough
 		case lambda.RuntimeDotnetcore20:
