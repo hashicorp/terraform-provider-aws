f48ffdbb134bed0d29e3b074bc3018490fac45b6
Khader Syed <geek@sidcarter.com>
2022-02-26 17:10:58 -0500

    update changes so far

--- internal/service/workmail/organization.go
+++ internal/service/workmail/organization.go
@@ -2,19 +2,13 @@ package workmail
 
 import (
 	"context"
-	"crypto/sha1"
-	"encoding/hex"
-	"errors"
 	"fmt"
 	"log"
-	"regexp"
-	"strings"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/workmail"
 	"github.com/hashicorp/aws-sdk-go-base/v2/awsv1shim/v2/tfawserr"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/customdiff"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
@@ -23,16 +17,6 @@ import (
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
-)
-
-const (
-	// Maximum amount of time for ACM Organization cross-service reference propagation.
-	// Removal of ACM Organizations from API Gateway Custom Domains can take >15 minutes.
-	AcmOrganizationCrossServicePropagationTimeout = 20 * time.Minute
-
-	// Maximum amount of time for ACM Organization asynchronous DNS validation record assignment.
-	// This timeout is unrelated to any creation or validation of those assigned DNS records.
-	AcmOrganizationDnsValidationAssignmentTimeout = 5 * time.Minute
 )
 
 func ResourceOrganization() *schema.Resource {
@@ -58,8 +42,8 @@ func ResourceOrganization() *schema.Resource {
 				Optional: true,
 			},
 			"directory_id": {
-				Type:     schema.TypeString,
-				Optional: true,
+				Type:         schema.TypeString,
+				Optional:     true,
 				ValidateFunc: validation.StringLenBetween(1, 12),
 			},
 			"kms_key_arn": {
@@ -76,15 +60,7 @@ func ResourceOrganization() *schema.Resource {
 							Type:     schema.TypeString,
 							Computed: true,
 						},
-						"resource_record_name": {
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"resource_record_type": {
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"resource_record_value": {
+						"hosted_zone_id": {
 							Type:     schema.TypeString,
 							Computed: true,
 						},
