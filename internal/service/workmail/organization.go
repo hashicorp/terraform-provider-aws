fa3b404937e500b6bdf59f8d748629dcd02bb0e0
Khader Syed <geek@sidcarter.com>
2022-02-26 17:32:05 -0500

    more updates

--- internal/service/workmail/organization.go
+++ internal/service/workmail/organization.go
@@ -44,7 +44,7 @@ func ResourceOrganization() *schema.Resource {
 			"directory_id": {
 				Type:         schema.TypeString,
 				Optional:     true,
-				ValidateFunc: validation.StringLenBetween(1, 12),
+				ValidateFunc: validation.StringLenBetween(12, 12),
 			},
 			"kms_key_arn": {
 				Type:         schema.TypeString,
@@ -72,4 +72,61 @@ func ResourceOrganization() *schema.Resource {
 			"tags_all": tftags.TagsSchemaComputed(),
 		},
 	}
+}
+
+func resourceOrganizationCreate(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*conns.AWSClient).WorkMailConn
+	acceleratorARN := d.Get("accelerator_arn").(string)
+
+	opts := &globalaccelerator.CreateOrganizationInput{
+		AcceleratorArn: aws.String(acceleratorARN),
+		ClientToken:    aws.String(resource.UniqueId()),
+	}
+
+	log.Printf("[DEBUG] Create WorkMail Organization: %s", opts)
+
+	resp, err := conn.CreateOrganization(opts)
+	if err != nil {
+		return fmt.Errorf("error creating WorkMail Organization: %w", err)
+	}
+
+	d.SetId(aws.StringValue(resp.Organization.OrganizationArn))
+
+	// Creating a listener triggers the accelerator to change status to InPending.
+	if _, err := waitAcceleratorDeployed(conn, acceleratorARN, d.Timeout(schema.TimeoutCreate)); err != nil {
+		return fmt.Errorf("error waiting for Global Accelerator Accelerator (%s) deployment: %w", acceleratorARN, err)
+	}
+
+	return resourceOrganizationRead(d, meta)
+}
+
+func resourceOrganizationRead(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*conns.AWSClient).WorkMailConn
+
+	listener, err := FindOrganizationByARN(conn, d.Id())
+
+	if !d.IsNewResource() && tfresource.NotFound(err) {
+		log.Printf("[WARN] WorkMail Organization (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	if err != nil {
+		return fmt.Errorf("error reading WorkMail Organization (%s): %w", d.Id(), err)
+	}
+
+	acceleratorARN, err := OrganizationOrEndpointGroupARNToAcceleratorARN(d.Id())
+
+	if err != nil {
+		return err
+	}
+
+	d.Set("accelerator_arn", acceleratorARN)
+	d.Set("client_affinity", listener.ClientAffinity)
+	d.Set("protocol", listener.Protocol)
+	if err := d.Set("port_range", resourceOrganizationFlattenPortRanges(listener.PortRanges)); err != nil {
+		return fmt.Errorf("error setting port_range: %w", err)
+	}
+
+	return nil
 }
