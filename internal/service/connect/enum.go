3ae2621d23cc408e929fcf0e7c99cf1946c4bb9d
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-02-28 13:18:00 -0500

    'go fmt'.

--- internal/service/connect/enum.go
+++ internal/service/connect/enum.go
@@ -24,7 +24,7 @@ const (
 	// MaxResults Valid Range: Minimum value of 1. Maximum value of 1000
 	// https://docs.aws.amazon.com/connect/latest/APIReference/API_ListPrompts.html
 	ListPromptsMaxResults = 60
-  // ListQueueQuickConnectsMaxResults Valid Range: Minimum value of 1. Maximum value of 100.
+	// ListQueueQuickConnectsMaxResults Valid Range: Minimum value of 1. Maximum value of 100.
 	// https://docs.aws.amazon.com/connect/latest/APIReference/API_ListQueueQuickConnects.html
 	ListQueueQuickConnectsMaxResults = 60
 	// ListQuickConnectsMaxResults Valid Range: Minimum value of 1. Maximum value of 1000.
