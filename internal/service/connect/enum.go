95d0ba1f4babcaff5cd54c55ba602d5121dc71a0
Glenn <31919569+GlennChia@users.noreply.github.com>
2022-03-01 00:29:14 +0800

    r/aws_connect_security_profile-read new attribute (#22761)
    * chore(connect): fix comment typo for max results
    
    * feat(connect): add read for permissions attribute
    
    * test(connect): basic sec profile with no perm
    
    length should be 0 since empty list is returned
    
    * test(connect): sec profile permissions update
    
    test updating of permissions from original 0 to 2 new permissions
    
    * ci(connect): changelog for permissions attribute
    
    * test(connect): rm ImportStateVerifyIgnore for perm

--- internal/service/connect/enum.go
+++ internal/service/connect/enum.go
@@ -24,9 +24,12 @@ const (
 	// MaxResults Valid Range: Minimum value of 1. Maximum value of 1000
 	// https://docs.aws.amazon.com/connect/latest/APIReference/API_ListPrompts.html
 	ListPromptsMaxResults = 60
-	// ListLambdaFunctionsMaxResults Valid Range: Minimum value of 1. Maximum value of 1000.
+	// ListQuickConnectsMaxResults Valid Range: Minimum value of 1. Maximum value of 1000.
 	// https://docs.aws.amazon.com/connect/latest/APIReference/API_ListQuickConnects.html
 	ListQuickConnectsMaxResults = 60
+	// ListSecurityProfilePermissionsMaxResults Valid Range: Minimum value of 1. Maximum value of 1000.
+	// https://docs.aws.amazon.com/connect/latest/APIReference/API_ListSecurityProfilePermissions.html
+	ListSecurityProfilePermissionsMaxResults = 60
 )
 
 func InstanceAttributeMapping() map[string]string {
