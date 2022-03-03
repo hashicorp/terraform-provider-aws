335c5c42211ed66f11cb8a1fc4c80520ffa09df4
jordanfelle <jfelle@tenable.com>
2022-03-03 05:06:48 -0500

    allow any character in networkfirewall/stateful_rule/header source/destination

--- internal/service/networkfirewall/rule_group.go
+++ internal/service/networkfirewall/rule_group.go
@@ -185,10 +185,7 @@ func ResourceRuleGroup() *schema.Resource {
 															"destination": {
 																Type:     schema.TypeString,
 																Required: true,
-																ValidateFunc: validation.Any(
-																	verify.ValidIPv4CIDRNetworkAddress,
-																	validation.StringInSlice([]string{networkfirewall.StatefulRuleDirectionAny}, false),
-																),
+																Elem:     &schema.Schema{Type: schema.TypeString},
 															},
 															"destination_port": {
 																Type:     schema.TypeString,
@@ -207,10 +204,7 @@ func ResourceRuleGroup() *schema.Resource {
 															"source": {
 																Type:     schema.TypeString,
 																Required: true,
-																ValidateFunc: validation.Any(
-																	verify.ValidIPv4CIDRNetworkAddress,
-																	validation.StringInSlice([]string{networkfirewall.StatefulRuleDirectionAny}, false),
-																),
+																Elem:     &schema.Schema{Type: schema.TypeString},
 															},
 															"source_port": {
 																Type:     schema.TypeString,
