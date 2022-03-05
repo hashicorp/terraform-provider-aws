aa046c3b8ebeba8583360cabc267dd4f1e7ca58b
GlennChia <glennchia7@gmail.com>
2022-03-05 16:29:38 +0000

    feat(connect): userHierarchyStruc datasrc reads

--- internal/service/connect/user_hierarchy_structure_data_source.go
+++ internal/service/connect/user_hierarchy_structure_data_source.go
@@ -1,12 +1,20 @@
 package connect
 
 import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/connect"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 )
 
 func DataSourceUserHierarchyStructure() *schema.Resource {
 	return &schema.Resource{
+		ReadContext: dataSourceUserHierarchyStructureRead,
 		Schema: map[string]*schema.Schema{
 			"hierarchy_structure": {
 				Type:     schema.TypeList,
@@ -69,3 +77,28 @@ func connectUserHierarchyLevelDataSourceSchema() *schema.Schema {
 	}
 }
 
+func dataSourceUserHierarchyStructureRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn := meta.(*conns.AWSClient).ConnectConn
+
+	instanceID := d.Get("instance_id").(string)
+
+	resp, err := conn.DescribeUserHierarchyStructureWithContext(ctx, &connect.DescribeUserHierarchyStructureInput{
+		InstanceId: aws.String(instanceID),
+	})
+
+	if err != nil {
+		return diag.FromErr(fmt.Errorf("error getting Connect User Hierarchy Structure for Connect Instance (%s): %w", instanceID, err))
+	}
+
+	if resp == nil || resp.HierarchyStructure == nil {
+		return diag.FromErr(fmt.Errorf("error getting Connect User Hierarchy Structure for Connect Instance (%s): empty response", instanceID))
+	}
+
+	if err := d.Set("hierarchy_structure", flattenUserHierarchyStructure(resp.HierarchyStructure)); err != nil {
+		return diag.FromErr(fmt.Errorf("error setting Connect User Hierarchy Structure for Connect Instance: (%s)", instanceID))
+	}
+
+	d.SetId(instanceID)
+
+	return nil
+}
