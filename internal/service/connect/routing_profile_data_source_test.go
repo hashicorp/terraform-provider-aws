816c0a451d88e0ed7621afcb7056f17598301a4e
GlennChia <glennchia7@gmail.com>
2022-03-05 15:33:34 +0000

    test(connect): name for RoutingProfile datasrc

--- internal/service/connect/routing_profile_data_source_test.go
+++ internal/service/connect/routing_profile_data_source_test.go
@@ -62,6 +62,58 @@ func TestAccConnectRoutingProfileDataSource_routingProfileID(t *testing.T) {
 	})
 }
 
+func TestAccConnectRoutingProfileDataSource_name(t *testing.T) {
+	rName := sdkacctest.RandomWithPrefix("resource-test-terraform")
+	rName2 := sdkacctest.RandomWithPrefix("resource-test-terraform")
+	rName3 := sdkacctest.RandomWithPrefix("resource-test-terraform")
+	rName4 := sdkacctest.RandomWithPrefix("resource-test-terraform")
+	resourceName := "aws_connect_routing_profile.test"
+	datasourceName := "data.aws_connect_routing_profile.test"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:   func() { acctest.PreCheck(t) },
+		ErrorCheck: acctest.ErrorCheck(t, connect.EndpointsID),
+		Providers:  acctest.Providers,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccRoutingProfileDataSourceConfig_Name(rName, rName2, rName3, rName4),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrPair(datasourceName, "arn", resourceName, "arn"),
+					resource.TestCheckResourceAttrPair(datasourceName, "default_outbound_queue_id", resourceName, "default_outbound_queue_id"),
+					resource.TestCheckResourceAttrPair(datasourceName, "description", resourceName, "description"),
+					resource.TestCheckResourceAttrPair(datasourceName, "instance_id", resourceName, "instance_id"),
+					resource.TestCheckResourceAttrPair(datasourceName, "media_concurrencies.#", resourceName, "media_concurrencies.#"),
+					resource.TestCheckTypeSetElemNestedAttrs(datasourceName, "media_concurrencies.*", map[string]string{
+						"channel":     "VOICE",
+						"concurrency": "1",
+					}),
+					resource.TestCheckTypeSetElemNestedAttrs(datasourceName, "media_concurrencies.*", map[string]string{
+						"channel":     "CHAT",
+						"concurrency": "2",
+					}),
+					resource.TestCheckResourceAttrPair(datasourceName, "name", resourceName, "name"),
+					resource.TestCheckResourceAttrPair(datasourceName, "queue_configs.#", resourceName, "queue_configs.#"),
+					resource.TestCheckResourceAttrSet(datasourceName, "queue_configs.0.channel"),
+					resource.TestCheckResourceAttr(datasourceName, "queue_configs.0.delay", "1"),
+					resource.TestCheckResourceAttrSet(datasourceName, "queue_configs.0.priority"),
+					resource.TestCheckTypeSetElemAttrPair(datasourceName, "queue_configs.*.queue_arn", "aws_connect_queue.default_outbound_queue", "arn"),
+					resource.TestCheckTypeSetElemAttrPair(datasourceName, "queue_configs.*.queue_id", "aws_connect_queue.default_outbound_queue", "queue_id"),
+					resource.TestCheckTypeSetElemAttrPair(datasourceName, "queue_configs.*.queue_name", "aws_connect_queue.default_outbound_queue", "name"),
+					resource.TestCheckResourceAttrSet(datasourceName, "queue_configs.1.channel"),
+					resource.TestCheckResourceAttr(datasourceName, "queue_configs.1.delay", "1"),
+					resource.TestCheckResourceAttrSet(datasourceName, "queue_configs.1.priority"),
+					resource.TestCheckTypeSetElemAttrPair(datasourceName, "queue_configs.*.queue_arn", "aws_connect_queue.test", "arn"),
+					resource.TestCheckTypeSetElemAttrPair(datasourceName, "queue_configs.*.queue_id", "aws_connect_queue.test", "queue_id"),
+					resource.TestCheckTypeSetElemAttrPair(datasourceName, "queue_configs.*.queue_name", "aws_connect_queue.test", "name"),
+					resource.TestCheckResourceAttrPair(datasourceName, "routing_profile_id", resourceName, "routing_profile_id"),
+					resource.TestCheckResourceAttrPair(datasourceName, "tags.%", resourceName, "tags.%"),
+					resource.TestCheckResourceAttrPair(datasourceName, "tags.Name", resourceName, "tags.Name"),
+				),
+			},
+		},
+	})
+}
+
 func testAccRoutingProfileBaseDataSourceConfig(rName, rName2, rName3, rName4 string) string {
 	return fmt.Sprintf(`
 resource "aws_connect_instance" "test" {
@@ -134,6 +186,17 @@ func testAccRoutingProfileDataSourceConfig_RoutingProfileID(rName, rName2, rName
 data "aws_connect_routing_profile" "test" {
   instance_id        = aws_connect_instance.test.id
   routing_profile_id = aws_connect_routing_profile.test.routing_profile_id
+}
+`)
+}
+
+func testAccRoutingProfileDataSourceConfig_Name(rName, rName2, rName3, rName4 string) string {
+	return acctest.ConfigCompose(
+		testAccRoutingProfileBaseDataSourceConfig(rName, rName2, rName3, rName4),
+		`
+data "aws_connect_routing_profile" "test" {
+  instance_id = aws_connect_instance.test.id
+  name        = aws_connect_routing_profile.test.name
 }
 `)
 }
