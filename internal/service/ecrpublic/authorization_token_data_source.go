cfc4fd4b55a04054d328c1d6ab70039a580778b3
Paul Rutledge <paul.rutledge@rate.com>
2022-03-06 10:57:00 -0600

    add aws_ecrpublic_authorization_token data source

---
+++ internal/service/ecrpublic/authorization_token_data_source.go
@@ -0,0 +1,72 @@
+package ecrpublic
+
+import (
+	"encoding/base64"
+	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ecrpublic"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+)
+
+func DataSourceAuthorizationToken() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceAuthorizationTokenRead,
+
+		Schema: map[string]*schema.Schema{
+			"authorization_token": {
+				Type:      schema.TypeString,
+				Computed:  true,
+				Sensitive: true,
+			},
+			"expires_at": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"user_name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"password": {
+				Type:      schema.TypeString,
+				Computed:  true,
+				Sensitive: true,
+			},
+		},
+	}
+}
+
+func dataSourceAuthorizationTokenRead(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*conns.AWSClient).ECRPublicConn
+	params := &ecrpublic.GetAuthorizationTokenInput{}
+	log.Printf("[DEBUG] Getting Public ECR authorization token")
+	out, err := conn.GetAuthorizationToken(params)
+	if err != nil {
+		return fmt.Errorf("error getting Public ECR authorization token: %w", err)
+	}
+	log.Printf("[DEBUG] Received Public ECR AuthorizationData %v", out.AuthorizationData)
+	authorizationData := out.AuthorizationData
+	authorizationToken := aws.StringValue(authorizationData.AuthorizationToken)
+	expiresAt := aws.TimeValue(authorizationData.ExpiresAt).Format(time.RFC3339)
+	authBytes, err := base64.URLEncoding.DecodeString(authorizationToken)
+	if err != nil {
+		d.SetId("")
+		return fmt.Errorf("error decoding Public ECR authorization token: %w", err)
+	}
+	basicAuthorization := strings.Split(string(authBytes), ":")
+	if len(basicAuthorization) != 2 {
+		return fmt.Errorf("unknown Public ECR authorization token format")
+	}
+	userName := basicAuthorization[0]
+	password := basicAuthorization[1]
+	d.SetId(meta.(*conns.AWSClient).Region)
+	d.Set("authorization_token", authorizationToken)
+	d.Set("expires_at", expiresAt)
+	d.Set("user_name", userName)
+	d.Set("password", password)
+	return nil
+}
