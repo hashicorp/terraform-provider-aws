8fdcf6f0491f4ed1e14ff43b1ebbe9a8c9748c8c
Jordan <jfelle@tenable.com>
2022-03-07 06:56:28 -0500

    fix #23543 Ignore suffix for domains in aws_route53_resolver_firewall_domain_list

--- internal/service/route53resolver/firewall_domain_list.go
+++ internal/service/route53resolver/firewall_domain_list.go
@@ -114,7 +114,11 @@ func resourceFirewallDomainListRead(d *schema.ResourceData, meta interface{}) er
 	domains := []*string{}
 
 	err = conn.ListFirewallDomainsPages(input, func(output *route53resolver.ListFirewallDomainsOutput, lastPage bool) bool {
-		domains = append(domains, output.Domains...)
+		// AWS' response contains a dot at the end, this causes the domains to never match.
+		for _, domain := range output.Domains {
+			var sanitizedDomain := strings.TrimSuffix(*domain, ".")
+			domains = append(domains, &sanitizedDomain)
+		}
 		return !lastPage
 	})
 
