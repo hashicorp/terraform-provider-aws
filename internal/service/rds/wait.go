ef15f7b92a16a884b150e6b42ec3d9d8a02abf0f
Dirk Avery <dirk.avery@gmail.com>
2022-03-08 15:36:26 -0500

    Additional test fixes

--- internal/service/rds/wait.go
+++ internal/service/rds/wait.go
@@ -161,11 +161,12 @@ func waitDBInstanceDeleted(conn *rds.RDS, id string, timeout time.Duration) (*rd
 			InstanceStatusStorageFull,
 			InstanceStatusStorageOptimization,
 		},
-		Target:     []string{},
-		Refresh:    statusDBInstance(conn, id),
-		Timeout:    timeout,
-		MinTimeout: 10 * time.Second,
-		Delay:      30 * time.Second,
+		Target:                    []string{},
+		Refresh:                   statusDBInstance(conn, id),
+		Timeout:                   timeout,
+		MinTimeout:                10 * time.Second,
+		Delay:                     30 * time.Second,
+		ContinuousTargetOccurence: 3,
 	}
 
 	outputRaw, err := stateConf.WaitForState()
