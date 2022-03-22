10fc816eba9b2e72067936fe08aefe588315fb9c
Roberth Kulbin <6707630+roberth-k@users.noreply.github.com>
2022-03-22 20:27:11 +0000

    d/aws_memorydb_parameter_group: new data source

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -716,7 +716,8 @@ func Provider() *schema.Provider {
 			"aws_regions":                 meta.DataSourceRegions(),
 			"aws_service":                 meta.DataSourceService(),
 
-			"aws_memorydb_subnet_group": memorydb.DataSourceSubnetGroup(),
+			"aws_memorydb_parameter_group": memorydb.DataSourceParameterGroup(),
+			"aws_memorydb_subnet_group":    memorydb.DataSourceSubnetGroup(),
 
 			"aws_mq_broker": mq.DataSourceBroker(),
 
