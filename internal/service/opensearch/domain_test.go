f54da1db6b19ed1decbabdb0f498a1015bc22b9e
Dirk Avery <dirk.avery@gmail.com>
2022-03-28 14:45:24 -0400

    Arm in a lint

--- internal/service/opensearch/domain_test.go
+++ internal/service/opensearch/domain_test.go
@@ -2755,7 +2755,7 @@ data "aws_iam_policy_document" "test" {
     effect  = "Allow"
 
     principals {
-      type        = "Service"
+      type = "Service"
       identifiers = [
         "es.${data.aws_partition.current.dns_suffix}",
       ]
