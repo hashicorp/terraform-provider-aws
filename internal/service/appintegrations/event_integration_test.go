942343762a942e6c6bcd8b9d0d9fdf59e5ff3ec0
GlennChia <glennchia7@gmail.com>
2022-03-28 15:39:57 +0000

    test(appint): eventIntegration update tags

--- internal/service/appintegrations/event_integration_test.go
+++ internal/service/appintegrations/event_integration_test.go
@@ -71,6 +71,82 @@ func TestAccEventIntegration_basic(t *testing.T) {
 	})
 }
 
+func TestAccEventIntegration_updateTags(t *testing.T) {
+	var eventIntegration appintegrationsservice.GetEventIntegrationOutput
+
+	rName := sdkacctest.RandomWithPrefix("resource-test-terraform")
+	description := "example description"
+	resourceName := "aws_appintegrations_event_integration.test"
+
+	key := "EVENT_BRIDGE_PARTNER_EVENT_SOURCE_NAME"
+	var sourceName string
+	sourceName = os.Getenv(key)
+	if sourceName == "" {
+		sourceName = "aws.partner/examplepartner.com"
+	}
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { acctest.PreCheck(t) },
+		ErrorCheck:   acctest.ErrorCheck(t, appintegrationsservice.EndpointsID),
+		Providers:    acctest.Providers,
+		CheckDestroy: testAccCheckEventIntegrationDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccEventIntegrationConfig_basic(rName, description, sourceName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckEventIntegrationExists(resourceName, &eventIntegration),
+					resource.TestCheckResourceAttrSet(resourceName, "arn"),
+					resource.TestCheckResourceAttr(resourceName, "description", description),
+					resource.TestCheckResourceAttr(resourceName, "eventbridge_bus", "default"),
+					resource.TestCheckResourceAttr(resourceName, "event_filter.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "event_filter.0.source", sourceName),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", "1"),
+					resource.TestCheckResourceAttr(resourceName, "tags.Name", "Test Event Integration"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccEventIntegrationConfig_tags(rName, description, sourceName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckEventIntegrationExists(resourceName, &eventIntegration),
+					resource.TestCheckResourceAttrSet(resourceName, "arn"),
+					resource.TestCheckResourceAttr(resourceName, "description", description),
+					resource.TestCheckResourceAttr(resourceName, "eventbridge_bus", "default"),
+					resource.TestCheckResourceAttr(resourceName, "event_filter.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "event_filter.0.source", sourceName),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", "2"),
+					resource.TestCheckResourceAttr(resourceName, "tags.Name", "Test Event Integration"),
+					resource.TestCheckResourceAttr(resourceName, "tags.Key2", "Value2a"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccEventIntegrationConfig_tagsUpdated(rName, description, sourceName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckEventIntegrationExists(resourceName, &eventIntegration),
+					resource.TestCheckResourceAttrSet(resourceName, "arn"),
+					resource.TestCheckResourceAttr(resourceName, "description", description),
+					resource.TestCheckResourceAttr(resourceName, "eventbridge_bus", "default"),
+					resource.TestCheckResourceAttr(resourceName, "event_filter.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "event_filter.0.source", sourceName),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", "3"),
+					resource.TestCheckResourceAttr(resourceName, "tags.Name", "Test Event Integration"),
+					resource.TestCheckResourceAttr(resourceName, "tags.Key2", "Value2b"),
+					resource.TestCheckResourceAttr(resourceName, "tags.Key3", "Value3"),
+				),
+			},
+		},
+	})
+}
+
 func TestAccEventIntegration_disappears(t *testing.T) {
 	var eventIntegration appintegrationsservice.GetEventIntegrationOutput
 
@@ -163,6 +239,45 @@ resource "aws_appintegrations_event_integration" "test" {
 
   tags = {
     "Name" = "Test Event Integration"
+  }
+}
+`, rName, label, sourceName)
+}
+
+func testAccEventIntegrationConfig_tags(rName, label, sourceName string) string {
+	return fmt.Sprintf(`
+resource "aws_appintegrations_event_integration" "test" {
+  name            = %[1]q
+  description     = %[2]q
+  eventbridge_bus = "default"
+
+  event_filter {
+    source = %[3]q
+  }
+
+  tags = {
+    "Name" = "Test Event Integration"
+    "Key2" = "Value2a"
+  }
+}
+`, rName, label, sourceName)
+}
+
+func testAccEventIntegrationConfig_tagsUpdated(rName, label, sourceName string) string {
+	return fmt.Sprintf(`
+resource "aws_appintegrations_event_integration" "test" {
+  name            = %[1]q
+  description     = %[2]q
+  eventbridge_bus = "default"
+
+  event_filter {
+    source = %[3]q
+  }
+
+  tags = {
+    "Name" = "Test Event Integration"
+    "Key2" = "Value2b"
+    "Key3" = "Value3"
   }
 }
 `, rName, label, sourceName)
