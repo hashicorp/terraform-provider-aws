ec74e68fa5ba4528d5e9122e0f5b793b6c79b6c9
GlennChia <glennchia7@gmail.com>
2022-03-29 00:45:18 +0000

    fix(appint): add CustomizeDiff to fix update tags
    CustomizeDiff: verify.SetTagsDiff,

--- internal/service/appintegrations/event_integration.go
+++ internal/service/appintegrations/event_integration.go
@@ -15,6 +15,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 func ResourceEventIntegration() *schema.Resource {
@@ -67,6 +68,7 @@ func ResourceEventIntegration() *schema.Resource {
 			"tags":     tftags.TagsSchema(),
 			"tags_all": tftags.TagsSchemaComputed(),
 		},
+		CustomizeDiff: verify.SetTagsDiff,
 	}
 }
 
