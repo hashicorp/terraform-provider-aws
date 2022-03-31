658f663f4c4af50066a9e6c7ff4baceeede553ef
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-03-31 16:51:37 -0400

    Add 'deleteDeleteMarkerListIterator'.

--- internal/service/s3/delete.go
+++ internal/service/s3/delete.go
@@ -21,7 +21,7 @@ type objectVersionDeleter struct {
 func NewObjectVersionDeleter(conn *s3.S3, bucket, key string) *objectVersionDeleter {
 	return &objectVersionDeleter{
 		batchDelete:         s3manager.NewBatchDeleteWithClient(conn, func(o *s3manager.BatchDelete) { o.BatchSize = deleteBatchSize }),
-		batchDeleteIterator: NewDeleteVersionListIterator(conn, bucket, key),
+		batchDeleteIterator: NewDeleteObjectVersionListIterator(conn, bucket, key),
 	}
 }
 
@@ -29,49 +29,43 @@ func (self *objectVersionDeleter) DeleteAll(ctx context.Context) error {
 	return self.batchDelete.Delete(ctx, self.batchDeleteIterator)
 }
 
-// deleteVersionListIterator implements s3manager.BatchDeleteIterator.
-// It is inspired by s3manager.DeleteListIterator.
-type deleteVersionListIterator struct {
-	Bucket    *string
-	Paginator request.Pagination
+// listIterator is intended to be embedded inside iterators.
+type listIterator struct {
+	bucket    string
 	key       string
-	objects   []*s3.ObjectVersion
+	paginator request.Pagination
 }
 
-func NewDeleteVersionListIterator(conn *s3.S3, bucket, key string) s3manager.BatchDeleteIterator {
-	return &deleteVersionListIterator{
-		Bucket: aws.String(bucket),
-		Paginator: request.Pagination{
-			NewRequest: func() (*request.Request, error) {
-				input := &s3.ListObjectVersionsInput{
-					Bucket: aws.String(bucket),
-				}
+// deleteVersionListIterator implements s3manager.BatchDeleteIterator.
+// It iterates through a list of S3 Object versions and delete them.
+// It is inspired by s3manager.DeleteListIterator.
+type deleteObjectVersionListIterator struct {
+	listIterator
+	objects []*s3.ObjectVersion
+}
 
-				if key != "" {
-					input.Prefix = aws.String(key)
-				}
-
-				request, _ := conn.ListObjectVersionsRequest(input)
-
-				return request, nil
-			},
+func NewDeleteObjectVersionListIterator(conn *s3.S3, bucket, key string) s3manager.BatchDeleteIterator {
+	return &deleteObjectVersionListIterator{
+		listIterator: listIterator{
+			bucket:    bucket,
+			key:       key,
+			paginator: listObjectVersionsPaginator(conn, bucket, key),
 		},
-		key: key,
 	}
 }
 
-func (iter *deleteVersionListIterator) Next() bool {
+func (iter *deleteObjectVersionListIterator) Next() bool {
 	if len(iter.objects) > 0 {
 		iter.objects = iter.objects[1:]
 	}
 
-	if len(iter.objects) == 0 && iter.Paginator.Next() {
+	if len(iter.objects) == 0 && iter.listIterator.paginator.Next() {
 		if iter.key == "" {
-			iter.objects = iter.Paginator.Page().(*s3.ListObjectVersionsOutput).Versions
+			iter.objects = iter.listIterator.paginator.Page().(*s3.ListObjectVersionsOutput).Versions
 		} else {
 			// ListObjectVersions uses Prefix as an argument but we use Key.
 			// Ignore any object versions that do not have the required Key.
-			for _, v := range iter.Paginator.Page().(*s3.ListObjectVersionsOutput).Versions {
+			for _, v := range iter.listIterator.paginator.Page().(*s3.ListObjectVersionsOutput).Versions {
 				if iter.key != aws.StringValue(v.Key) {
 					continue
 				}
@@ -84,16 +78,91 @@ func (iter *deleteVersionListIterator) Next() bool {
 	return len(iter.objects) > 0
 }
 
-func (iter *deleteVersionListIterator) Err() error {
-	return iter.Paginator.Err()
+func (iter *deleteObjectVersionListIterator) Err() error {
+	return iter.listIterator.paginator.Err()
 }
 
-func (iter *deleteVersionListIterator) DeleteObject() s3manager.BatchDeleteObject {
+func (iter *deleteObjectVersionListIterator) DeleteObject() s3manager.BatchDeleteObject {
 	return s3manager.BatchDeleteObject{
 		Object: &s3.DeleteObjectInput{
-			Bucket:    iter.Bucket,
+			Bucket:    aws.String(iter.listIterator.bucket),
 			Key:       iter.objects[0].Key,
 			VersionId: iter.objects[0].VersionId,
+		},
+	}
+}
+
+// deleteDeleteMarkerListIterator implements s3manager.BatchDeleteIterator.
+// It iterates through a list of S3 Object delete markers and delete them.
+// It is inspired by s3manager.DeleteListIterator.
+type deleteDeleteMarkerListIterator struct {
+	listIterator
+	deleteMarkers []*s3.DeleteMarkerEntry
+}
+
+func NewDeleteDeletMarkerListIterator(conn *s3.S3, bucket, key string) s3manager.BatchDeleteIterator {
+	return &deleteDeleteMarkerListIterator{
+		listIterator: listIterator{
+			bucket:    bucket,
+			key:       key,
+			paginator: listObjectVersionsPaginator(conn, bucket, key),
+		},
+	}
+}
+
+func (iter *deleteDeleteMarkerListIterator) Next() bool {
+	if len(iter.deleteMarkers) > 0 {
+		iter.deleteMarkers = iter.deleteMarkers[1:]
+	}
+
+	if len(iter.deleteMarkers) == 0 && iter.listIterator.paginator.Next() {
+		if iter.key == "" {
+			iter.deleteMarkers = iter.listIterator.paginator.Page().(*s3.ListObjectVersionsOutput).DeleteMarkers
+		} else {
+			// ListObjectVersions uses Prefix as an argument but we use Key.
+			// Ignore any delete markers that do not have the required Key.
+			for _, v := range iter.listIterator.paginator.Page().(*s3.ListObjectVersionsOutput).DeleteMarkers {
+				if iter.key != aws.StringValue(v.Key) {
+					continue
+				}
+
+				iter.deleteMarkers = append(iter.deleteMarkers, v)
+			}
+		}
+	}
+
+	return len(iter.deleteMarkers) > 0
+}
+
+func (iter *deleteDeleteMarkerListIterator) Err() error {
+	return iter.listIterator.paginator.Err()
+}
+
+func (iter *deleteDeleteMarkerListIterator) DeleteObject() s3manager.BatchDeleteObject {
+	return s3manager.BatchDeleteObject{
+		Object: &s3.DeleteObjectInput{
+			Bucket:    aws.String(iter.listIterator.bucket),
+			Key:       iter.deleteMarkers[0].Key,
+			VersionId: iter.deleteMarkers[0].VersionId,
+		},
+	}
+}
+
+// listObjectVersionsPaginator returns a paginator that lists S3 object versions for the specified bucket and optional key.
+func listObjectVersionsPaginator(conn *s3.S3, bucket, key string) request.Pagination {
+	return request.Pagination{
+		NewRequest: func() (*request.Request, error) {
+			input := &s3.ListObjectVersionsInput{
+				Bucket: aws.String(bucket),
+			}
+
+			if key != "" {
+				input.Prefix = aws.String(key)
+			}
+
+			request, _ := conn.ListObjectVersionsRequest(input)
+
+			return request, nil
 		},
 	}
 }
