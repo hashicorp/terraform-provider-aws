11668204fb9716055accc339857ea78e586c41c4
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-04-01 14:54:26 -0400

    Add 'RetryWhenAWSErrMessageContains'.

--- internal/tfresource/retry_test.go
+++ internal/tfresource/retry_test.go
@@ -75,6 +75,68 @@ func TestRetryWhenAWSErrCodeEquals(t *testing.T) {
 	}
 }
 
+func TestRetryWhenAWSErrMessageContains(t *testing.T) {
+	var retryCount int32
+
+	testCases := []struct {
+		Name        string
+		F           func() (interface{}, error)
+		ExpectError bool
+	}{
+		{
+			Name: "no error",
+			F: func() (interface{}, error) {
+				return nil, nil
+			},
+		},
+		{
+			Name: "non-retryable other error",
+			F: func() (interface{}, error) {
+				return nil, errors.New("TestCode")
+			},
+			ExpectError: true,
+		},
+		{
+			Name: "non-retryable AWS error",
+			F: func() (interface{}, error) {
+				return nil, awserr.New("TestCode1", "Testing", nil)
+			},
+			ExpectError: true,
+		},
+		{
+			Name: "retryable AWS error timeout",
+			F: func() (interface{}, error) {
+				return nil, awserr.New("TestCode1", "TestMessage1", nil)
+			},
+			ExpectError: true,
+		},
+		{
+			Name: "retryable AWS error success",
+			F: func() (interface{}, error) {
+				if atomic.CompareAndSwapInt32(&retryCount, 0, 1) {
+					return nil, awserr.New("TestCode1", "TestMessage1", nil)
+				}
+
+				return nil, nil
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			retryCount = 0
+
+			_, err := tfresource.RetryWhenAWSErrMessageContains(5*time.Second, testCase.F, "TestCode1", "TestMessage1")
+
+			if testCase.ExpectError && err == nil {
+				t.Fatal("expected error")
+			} else if !testCase.ExpectError && err != nil {
+				t.Fatalf("unexpected error: %s", err)
+			}
+		})
+	}
+}
+
 func TestRetryWhenNewResourceNotFound(t *testing.T) {
 	var retryCount int32
 
