40fc98bbf5154915d58d8782da49786d90131a1d
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-04-05 12:25:22 -0400

    Add 'FindAccountByID'.

--- internal/service/organizations/find.go
+++ internal/service/organizations/find.go
@@ -1,9 +1,37 @@
 package organizations
 
 import (
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/organizations"
+	"github.com/hashicorp/aws-sdk-go-base/v2/awsv1shim/v2/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 )
+
+func FindAccountByID(conn *organizations.Organizations, id string) (*organizations.Account, error) {
+	input := &organizations.DescribeAccountInput{
+		AccountId: aws.String(id),
+	}
+
+	output, err := conn.DescribeAccount(input)
+
+	if tfawserr.ErrCodeEquals(err, organizations.ErrCodeAccountNotFoundException) {
+		return nil, &resource.NotFoundError{
+			LastError:   err,
+			LastRequest: input,
+		}
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	if output == nil || output.Account == nil {
+		return nil, tfresource.NewEmptyResultError(input)
+	}
+
+	return output.Account, nil
+}
 
 func FindOrganization(conn *organizations.Organizations) (*organizations.Organization, error) {
 	input := &organizations.DescribeOrganizationInput{}
@@ -15,10 +43,7 @@ func FindOrganization(conn *organizations.Organizations) (*organizations.Organiz
 	}
 
 	if output == nil || output.Organization == nil {
-		return nil, &resource.NotFoundError{
-			Message:     "Empty result",
-			LastRequest: input,
-		}
+		return nil, tfresource.NewEmptyResultError(input)
 	}
 
 	return output.Organization, nil
