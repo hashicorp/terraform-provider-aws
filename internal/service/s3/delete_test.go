731c6971b12f7705f902bede863edd903a7edb92
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-04-05 10:25:58 -0400

    Add 'TestDeleteAllObjectVersions'.

--- internal/service/s3/delete_test.go
+++ internal/service/s3/delete_test.go
@@ -10,6 +10,8 @@ import (
 	tfs3 "github.com/hashicorp/terraform-provider-aws/internal/service/s3"
 )
 
+// AWS_REGION=us-west-2 go test -v ./internal/service/s3 -run=TestEmptyBucket -b ewbankkit-test-empty-bucket-001 -f
+
 var bucket = flag.String("b", "", "bucket")
 var force = flag.Bool("f", false, "force")
 
@@ -23,6 +25,23 @@ func TestEmptyBucket(t *testing.T) {
 	ctx := context.Background()
 
 	n, err := tfs3.EmptyBucket(ctx, svc, *bucket, *force)
+
+	if err != nil {
+		t.Fatalf("error emptying S3 bucket (%s): %s", *bucket, err)
+	}
+
+	t.Logf("%d S3 objects deleted", n)
+}
+
+func TestDeleteAllObjectVersions(t *testing.T) {
+	if *bucket == "" {
+		t.Skip("bucket not specified")
+	}
+
+	sess := session.Must(session.NewSession())
+	svc := s3.New(sess)
+
+	n, err := tfs3.DeleteAllObjectVersions(svc, *bucket, "", *force, false)
 
 	if err != nil {
 		t.Fatalf("error emptying S3 bucket (%s): %s", *bucket, err)
