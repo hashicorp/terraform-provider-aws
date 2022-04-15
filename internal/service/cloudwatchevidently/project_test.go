c5927955fa72b08878034bf65e8a8323fd6f790e
GlennChia <glennchia7@gmail.com>
2022-04-15 12:59:50 +0000

    test(cwevi): check project destroy

--- internal/service/cloudwatchevidently/project_test.go
+++ internal/service/cloudwatchevidently/project_test.go
@@ -11,6 +11,29 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 )
 
+func testAccCheckProjectDestroy(s *terraform.State) error {
+	conn := acctest.Provider.Meta().(*conns.AWSClient).CloudWatchEvidentlyConn
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "aws_cloudwatchevidently_project" {
+			continue
+		}
+
+		input := &cloudwatchevidently.GetProjectInput{
+			Project: aws.String(rs.Primary.ID),
+		}
+
+		resp, err := conn.GetProject(input)
+
+		if err == nil {
+			if aws.StringValue(resp.Project.Arn) == rs.Primary.ID {
+				return fmt.Errorf("Project '%s' was not deleted properly", rs.Primary.ID)
+			}
+		}
+	}
+
+	return nil
+}
+
 func testAccCheckProjectExists(name string, project *cloudwatchevidently.GetProjectOutput) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		rs, ok := s.RootModule().Resources[name]
