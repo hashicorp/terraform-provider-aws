2e4bf840be91dfc296ca37fca9cd0f437a417bb4
Andrew Hubler <ahubler@rapidsos.com>
2022-04-21 15:16:11 -0600

    Use random email addresses in acc test

--- internal/service/organizations/organizational_unit_descendant_accounts_data_source_test.go
+++ internal/service/organizations/organizational_unit_descendant_accounts_data_source_test.go
@@ -9,10 +9,17 @@ import (
 )
 
 func testAccOrganizationalUnitDescendantAccountsDataSource_basic(t *testing.T) {
-	firstResourceName := "aws_organizations_account.test1"
-	secondResourceName := "aws_organizations_account.test2"
-	thirdResourceName := "aws_organizations_account.test3"
+	resourceName1 := "aws_organizations_account.test1"
+	resourceName2 := "aws_organizations_account.test2"
+	resourceName3 := "aws_organizations_account.test3"
+
 	dataSourceName := "data.aws_organizations_organizational_unit_descendant_accounts.test"
+
+	domain := acctest.RandomDomainName()
+	address1 := acctest.RandomEmailAddress(domain)
+	address2 := acctest.RandomEmailAddress(domain)
+	address3 := acctest.RandomEmailAddress(domain)
+
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck: func() {
 			acctest.PreCheck(t)
@@ -25,41 +32,42 @@ func testAccOrganizationalUnitDescendantAccountsDataSource_basic(t *testing.T) {
 				Config: testAccOrganizationalUnitChildAccountsDataSourceConfig,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(dataSourceName, "accounts.#", "3"),
-					resource.TestCheckResourceAttrPair(firstResourceName, "arn", dataSourceName, "accounts.0.arn"),
-					resource.TestCheckResourceAttrPair(firstResourceName, "email", dataSourceName, "accounts.0.email"),
-					resource.TestCheckResourceAttrPair(firstResourceName, "id", dataSourceName, "accounts.0.id"),
-					resource.TestCheckResourceAttrPair(firstResourceName, "joined_method", dataSourceName, "accounts.0.joined_method"),
-					resource.TestCheckResourceAttrPair(firstResourceName, "joined_timestamp", dataSourceName, "accounts.0.joined_timestamp"),
-					resource.TestCheckResourceAttrPair(firstResourceName, "name", dataSourceName, "accounts.0.name"),
-					resource.TestCheckResourceAttrPair(firstResourceName, "status", dataSourceName, "accounts.0.status"),
+					resource.TestCheckResourceAttrPair(resourceName1, "arn", dataSourceName, "accounts.0.arn"),
+					resource.TestCheckResourceAttrPair(resourceName1, "email", dataSourceName, "accounts.0.email"),
+					resource.TestCheckResourceAttrPair(resourceName1, "id", dataSourceName, "accounts.0.id"),
+					resource.TestCheckResourceAttrPair(resourceName1, "joined_method", dataSourceName, "accounts.0.joined_method"),
+					resource.TestCheckResourceAttrPair(resourceName1, "joined_timestamp", dataSourceName, "accounts.0.joined_timestamp"),
+					resource.TestCheckResourceAttrPair(resourceName1, "name", dataSourceName, "accounts.0.name"),
+					resource.TestCheckResourceAttrPair(resourceName1, "status", dataSourceName, "accounts.0.status"),
 
-					resource.TestCheckResourceAttrPair(secondResourceName, "arn", dataSourceName, "accounts.1.arn"),
-					resource.TestCheckResourceAttrPair(secondResourceName, "email", dataSourceName, "accounts.1.email"),
-					resource.TestCheckResourceAttrPair(secondResourceName, "id", dataSourceName, "accounts.1.id"),
-					resource.TestCheckResourceAttrPair(secondResourceName, "joined_method", dataSourceName, "accounts.1.joined_method"),
-					resource.TestCheckResourceAttrPair(secondResourceName, "joined_timestamp", dataSourceName, "accounts.1.joined_timestamp"),
-					resource.TestCheckResourceAttrPair(secondResourceName, "name", dataSourceName, "accounts.1.name"),
-					resource.TestCheckResourceAttrPair(secondResourceName, "status", dataSourceName, "accounts.1.status"),
+					resource.TestCheckResourceAttrPair(resourceName2, "arn", dataSourceName, "accounts.1.arn"),
+					resource.TestCheckResourceAttrPair(resourceName2, "email", dataSourceName, "accounts.1.email"),
+					resource.TestCheckResourceAttrPair(resourceName2, "id", dataSourceName, "accounts.1.id"),
+					resource.TestCheckResourceAttrPair(resourceName2, "joined_method", dataSourceName, "accounts.1.joined_method"),
+					resource.TestCheckResourceAttrPair(resourceName2, "joined_timestamp", dataSourceName, "accounts.1.joined_timestamp"),
+					resource.TestCheckResourceAttrPair(resourceName2, "name", dataSourceName, "accounts.1.name"),
+					resource.TestCheckResourceAttrPair(resourceName2, "status", dataSourceName, "accounts.1.status"),
 
-					resource.TestCheckResourceAttrPair(thirdResourceName, "arn", dataSourceName, "accounts.2.arn"),
-					resource.TestCheckResourceAttrPair(thirdResourceName, "email", dataSourceName, "accounts.2.email"),
-					resource.TestCheckResourceAttrPair(thirdResourceName, "id", dataSourceName, "accounts.2.id"),
-					resource.TestCheckResourceAttrPair(thirdResourceName, "joined_method", dataSourceName, "accounts.2.joined_method"),
-					resource.TestCheckResourceAttrPair(thirdResourceName, "joined_timestamp", dataSourceName, "accounts.2.joined_timestamp"),
-					resource.TestCheckResourceAttrPair(thirdResourceName, "name", dataSourceName, "accounts.2.name"),
-					resource.TestCheckResourceAttrPair(thirdResourceName, "status", dataSourceName, "accounts.2.status"),
+					resource.TestCheckResourceAttrPair(resourceName3, "arn", dataSourceName, "accounts.2.arn"),
+					resource.TestCheckResourceAttrPair(resourceName3, "email", dataSourceName, "accounts.2.email"),
+					resource.TestCheckResourceAttrPair(resourceName3, "id", dataSourceName, "accounts.2.id"),
+					resource.TestCheckResourceAttrPair(resourceName3, "joined_method", dataSourceName, "accounts.2.joined_method"),
+					resource.TestCheckResourceAttrPair(resourceName3, "joined_timestamp", dataSourceName, "accounts.2.joined_timestamp"),
+					resource.TestCheckResourceAttrPair(resourceName3, "name", dataSourceName, "accounts.2.name"),
+					resource.TestCheckResourceAttrPair(resourceName3, "status", dataSourceName, "accounts.2.status"),
 				),
 			},
 		},
 	})
 }
 
-const testAccOrganizationalUnitChildAccountsDataSourceConfig = `
+func testAccOrganizationalUnitChildAccountsDataSourceConfig(rAddress1 string, rAddress2 string, rAddress3 string) string {
+	return fmt.Sprintf(`
 resource "aws_organizations_organization" "test" {}
 
 resource "aws_organizations_account" "test1" {
   name  = "test1"
-  email = "ahubler+test1@rapidsos.org"
+  email = %[1]q
   parent_id = aws_organizations_organization.test.roots[0].id
 }
 
@@ -70,7 +78,7 @@ resource "aws_organizations_organizational_unit" "test1" {
 
 resource "aws_organizations_account" "test2" {
   name  = "test2"
-  email = "ahubler+test2@rapidsos.org"
+  email = %[2]q
   parent_id = aws_organizations_organizational_unit.test1.id
 }
 
@@ -81,11 +89,12 @@ resource "aws_organizations_organizational_unit" "test2" {
 
 resource "aws_organizations_account" "test3" {
   name  = "test3"
-  email = "ahubler+test3@rapidsos.org"
+  email = %[3]q
   parent_id = aws_organizations_organizational_unit.test2.id
 }
 
 data "aws_organizations_organizational_unit_descendant_accounts" "test" {
   parent_id = aws_organizations_organization.test.roots[0].id
 }
-`
+`, rAddress1, rAddress2, rAddress3)
+}
