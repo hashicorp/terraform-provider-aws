fc6ae4eab573ab1defe4c433c2c992abd6a4eedf
Justin Retzolk <44710313+justinretzolk@users.noreply.github.com>
2022-04-22 12:08:26 -0500

    Switch service labels to be dynamically generated

--- infrastructure/repository/labels-service.tf
+++ infrastructure/repository/labels-service.tf
@@ -1,234 +1,50 @@
-#
-# For future consideration, this list could be automatically generated
-# via the AWS SDK service list.
-#
-
-variable "service_labels" {
-  default = [
-    "accessanalyzer",
-    "account",
-    "acm",
-    "acmpca",
-    "alexaforbusiness",
-    "amp",
-    "amplify",
-    "apigateway",
-    "apigatewaymanagementapi",
-    "apigatewayv2",
-    "appconfig",
-    "appflow",
-    "appintegrations",
-    "applicationautoscaling",
-    "applicationdiscoveryservice",
-    "applicationinsights",
-    "appmesh",
-    "apprunner",
-    "appstream",
-    "appsync",
-    "athena",
-    "auditmanager",
-    "autoscaling",
-    "autoscalingplans",
-    "backup",
-    "batch",
-    "braket",
-    "budgets",
-    "chime",
-    "cloud9",
-    "cloudcontrolapi",
-    "clouddirectory",
-    "cloudformation",
-    "cloudfront",
-    "cloudhsm",
-    "cloudhsmv2",
-    "cloudsearch",
-    "cloudtrail",
-    "cloudwatch",
-    "cloudwatchevidently",
-    "cloudwatchlogs",
-    "cloudwatchrum",
-    "codeartifact",
-    "codebuild",
-    "codecommit",
-    "codedeploy",
-    "codeguruprofiler",
-    "codegurureviewer",
-    "codepipeline",
-    "codestar",
-    "codestarconnections",
-    "codestarnotifications",
-    "cognito",
-    "cognitoidentityprovider",
-    "comprehend",
-    "comprehendmedical",
-    "computeoptimizer",
-    "configservice",
-    "connect",
-    "costandusagereportservice",
-    "costexplorer",
-    "databasemigrationservice",
-    "dataexchange",
-    "datapipeline",
-    "datasync",
-    "dax",
-    "detective",
-    "devicefarm",
-    "directconnect",
-    "directoryservice",
-    "dlm",
-    "docdb",
-    "dynamodb",
-    "ec2-classic",
-    "ec2",
-    "ecr",
-    "ecrpublic",
-    "ecs",
-    "efs",
-    "eks",
-    "elastictranscoder",
-    "elasticache",
-    "elasticbeanstalk",
-    "elasticinference",
-    "elasticsearch",
-    "elb",
-    "elbv2",
-    "emr",
-    "emrcontainers",
-    "events",
-    "firehose",
-    "fms",
-    "forecastservice",
-    "frauddetector",
-    "fsx",
-    "gamelift",
-    "glacier",
-    "globalaccelerator",
-    "glue",
-    "grafana",
-    "greengrass",
-    "groundstation",
-    "guardduty",
-    "honeycode",
-    "iam",
-    "identitystore",
-    "imagebuilder",
-    "inspector",
-    "iot",
-    "iotanalytics",
-    "iotevents",
-    "iotsecuretunneling",
-    "iotsitewise",
-    "iotthingsgraph",
-    "ivs",
-    "kafka",
-    "kafkaconnect",
-    "kendra",
-    "keyspaces",
-    "kinesis",
-    "kinesisanalytics",
-    "kinesisanalyticsv2",
-    "kinesisvideo",
-    "kms",
-    "lakeformation",
-    "lambda",
-    "lexmodels",
-    "licensemanager",
-    "lightsail",
-    "location",
-    "machinelearning",
-    "macie",
-    "macie2",
-    "managedblockchain",
-    "marketplacecatalog",
-    "mediaconnect",
-    "mediaconvert",
-    "medialive",
-    "mediapackage",
-    "mediapackagevod",
-    "mediastore",
-    "mediatailor",
-    "memorydb",
-    "meteringmarketplace",
-    "mobile",
-    "mq",
-    "mwaa",
-    "neptune",
-    "networkfirewall",
-    "networkmanager",
-    "opensearch",
-    "opsworks",
-    "opsworkscm",
-    "organizations",
-    "outposts",
-    "personalize",
-    "pi",
-    "pinpoint",
-    "pinpointemail",
-    "pinpointsmsvoice",
-    "polly",
-    "pricing",
-    "qldb",
-    "quicksight",
-    "ram",
-    "rds",
-    "redshift",
-    "resourcegroups",
-    "resourcegroupstaggingapi",
-    "robomaker",
-    "route53",
-    "route53domains",
-    "route53recoverycontrolconfig",
-    "route53recoveryreadiness",
-    "route53resolver",
-    "s3",
-    "s3control",
-    "s3outposts",
-    "sagemaker",
-    "savingsplans",
-    "schemas",
-    "secretsmanager",
-    "securityhub",
-    "serverlessrepo",
-    "servicecatalog",
-    "servicediscovery",
-    "servicequotas",
-    "ses",
-    "sesv2",
-    "sfn",
-    "shield",
-    "signer",
-    "simpledb",
-    "sms",
-    "snowball",
-    "sns",
-    "sqs",
-    "ssm",
-    "ssoadmin",
-    "storagegateway",
-    "sts",
-    "support",
-    "swf",
-    "synthetics",
-    "textract",
-    "timestreamwrite",
-    "transcribeservice",
-    "timestreamwrite",
-    "transfer",
-    "translate",
-    "waf",
-    "wafv2",
-    "workdocs",
-    "worklink",
-    "workmail",
-    "workspaces",
-    "xray",
-  ]
-  description = "Set of AWS Go SDK service labels"
-  type        = set(string)
+data "github_branch" "main" {
+  branch     = "main"
+  repository = "terraform-provider-aws"
 }
 
+data "github_tree" "main" {
+  recursive  = true
+  repository = data.github_branch.main.repository
+  tree_sha   = data.github_branch.main.sha
+}
+
+locals {
+
+  # A breakdown on this becuase while I realize it's a pretty ugly one-liner,
+  # I wasn't able to come up with a prettier way to do it, outside of just
+  # breaking the `compact()` and `toset()` functions out into separate locals.
+  #
+  # The `for` bit in here takes the list of tree entries from the data source
+  # uses `regexall` to check to see if the path for the entry is a directory
+  # under the `internal/service/` directory. `regexall()` returns a list of
+  # every match, so by testing whether the length is greater than 0, we can
+  # determine if the filepath matches our regex.
+  #
+  # If the path matches the regex, we then trim the `internal/service/` prefix
+  # before adding the path to our resulting list. If the path does not match,
+  # we pass `null` instead.
+  #
+  # As a note, it would probably be more efficient to *not* trim 'service/`,
+  # however, we would need to do some state surgery to move over to that, since
+  # we were previously supplying a static list of service names rather than
+  # using these data sources.
+  #
+  # Passing `null` means that we can wrap the whole list to `compact()` to
+  # remove all of the `null` values and leave only the list of tag values.
+  # Further, passing this list to `toset()` means that we have something that
+  # can be used by `for_each` in the label resource below.
+
+  services = toset(compact([
+    for entry in data.github_tree.main.entries :
+    length(regexall("^internal\\/service\\/[[:alnum:]]+$", entry.path)) > 0 ? trimprefix(entry.path, "internal/service/") : null
+  ]))
+
+}
+
+
 resource "github_issue_label" "service" {
-  for_each = var.service_labels
+  for_each = var.services
 
   repository  = "terraform-provider-aws"
   name        = "service/${each.value}"
