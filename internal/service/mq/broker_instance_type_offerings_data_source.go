4c85bf6d9f0b44471615d542a8b69178419a6402
drfaust92 <ilia.lazebnik@gmail.com>
2022-04-25 18:15:22 +0300

    mq offering ds

---
+++ internal/service/mq/broker_instance_type_offerings_data_source.go
@@ -0,0 +1,150 @@
+package mq
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/mq"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/flex"
+)
+
+func DataSourceBrokerInstanceTypeOfferings() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourcemqBrokerInstanceTypeOfferingsRead,
+
+		Schema: map[string]*schema.Schema{
+			"engine_type": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ValidateFunc: validation.StringInSlice(mq.EngineType_Values(), false),
+			},
+			"host_instance_type": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"storage_type": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ValidateFunc: validation.StringInSlice(mq.BrokerStorageType_Values(), false),
+			},
+			"broker_instance_options": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"availability_zones": {
+							Type:     schema.TypeSet,
+							Computed: true,
+							Elem:     &schema.Schema{Type: schema.TypeString},
+						},
+						"engine_type": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"host_instance_type": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"storage_type": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"supported_deployment_modes": {
+							Type:     schema.TypeSet,
+							Computed: true,
+							Elem:     &schema.Schema{Type: schema.TypeString},
+						},
+						"supported_engine_versions": {
+							Type:     schema.TypeList,
+							Computed: true,
+							Elem:     &schema.Schema{Type: schema.TypeString},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func dataSourcemqBrokerInstanceTypeOfferingsRead(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*conns.AWSClient).MQConn
+
+	input := &mq.DescribeBrokerInstanceOptionsInput{}
+
+	if v, ok := d.GetOk("host_instance_type"); ok {
+		input.HostInstanceType = aws.String(v.(string))
+	}
+
+	if v, ok := d.GetOk("engine_type"); ok {
+		input.EngineType = aws.String(v.(string))
+	}
+
+	if v, ok := d.GetOk("storage_type"); ok {
+		input.StorageType = aws.String(v.(string))
+	}
+
+	output, err := conn.DescribeBrokerInstanceOptions(input)
+
+	if err != nil {
+		return fmt.Errorf("error listing MQ Broker Instance Type Offerings: %w", err)
+	}
+
+	if output == nil {
+		return fmt.Errorf("empty response while reading MQ Broker Instance Type Offerings")
+	}
+
+	d.SetId(meta.(*conns.AWSClient).Region)
+
+	if err := d.Set("broker_instance_options", flattenBrokerInstanceOptions(output.BrokerInstanceOptions)); err != nil {
+		return fmt.Errorf("error setting broker_instance_options: %w", err)
+	}
+
+	return nil
+}
+
+func flattenBrokerInstanceOptions(bios []*mq.BrokerInstanceOption) []interface{} {
+	if len(bios) == 0 {
+		return nil
+	}
+
+	var tfList []interface{}
+
+	for _, bio := range bios {
+		if bio == nil {
+			continue
+		}
+
+		tfMap := map[string]interface{}{}
+
+		if bio.EngineType != nil {
+			tfMap["engine_type"] = aws.StringValue(bio.EngineType)
+		}
+
+		if bio.StorageType != nil {
+			tfMap["storage_type"] = aws.StringValue(bio.StorageType)
+		}
+
+		if bio.HostInstanceType != nil {
+			tfMap["host_instance_type"] = aws.StringValue(bio.HostInstanceType)
+		}
+
+		// if bio.AvailabilityZones != nil {
+		// 	tfMap["availability_zones"] = flex.FlattenStringSet(bio.AvailabilityZones)
+		// }
+
+		if bio.SupportedDeploymentModes != nil {
+			tfMap["supported_deployment_modes"] = flex.FlattenStringSet(bio.SupportedDeploymentModes)
+		}
+
+		if bio.SupportedEngineVersions != nil {
+			tfMap["supported_engine_versions"] = flex.FlattenStringList(bio.SupportedEngineVersions)
+		}
+
+		tfList = append(tfList, tfMap)
+	}
+
+	return tfList
+}
