b69130ca51bba2c628ad9e01e5c181b7a8271cdd
Aegon95 <akhil.ram5127@gmail.com>
2022-04-26 22:40:16 +0530

    Added new datasource for listing instance profiles for a role

---
+++ internal/service/iam/instance_profiles_data_source.go
@@ -0,0 +1,74 @@
+package iam
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/iam"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+)
+
+func DataSourceInstanceProfiles() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceInstanceProfilesRead,
+
+		Schema: map[string]*schema.Schema{
+			"role_name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"arns": {
+				Type:     schema.TypeSet,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"paths": {
+				Type:     schema.TypeSet,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"names": {
+				Type:     schema.TypeSet,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+		},
+	}
+}
+
+func dataSourceInstanceProfilesRead(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*conns.AWSClient).IAMConn
+
+	roleName := d.Get("role_name").(string)
+
+	req := &iam.ListInstanceProfilesForRoleInput{
+		RoleName: &roleName,
+	}
+
+	log.Printf("[DEBUG] Reading IAM Instance Profiles for given role %s: %s", roleName, req)
+	resp, err := conn.ListInstanceProfilesForRole(req)
+	if err != nil {
+		return fmt.Errorf("Error getting instance profiles: %w", err)
+	}
+	if resp == nil {
+		return fmt.Errorf("no IAM instance profiles found for role %s", roleName)
+	}
+
+	instanceProfiles := resp.InstanceProfiles
+
+	var arns, paths, names []string
+	for _, profile := range instanceProfiles {
+		arns = append(arns, *profile.Arn)
+		paths = append(paths, *profile.Path)
+		names = append(names, *profile.InstanceProfileName)
+	}
+
+	d.SetId(aws.StringValue(&roleName))
+	d.Set("arns", arns)
+	d.Set("paths", paths)
+	d.Set("names", names)
+
+	return nil
+}
---
+++ internal/service/iam/instance_profiles_data_source_test.go
@@ -0,0 +1,58 @@
+package iam_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/service/iam"
+	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+)
+
+func TestAccIAMInstanceProfilesDataSource_basic(t *testing.T) {
+	datasourceName := "data.aws_iam_instance_profiles.test"
+	resourceName := "aws_iam_instance_profile.test"
+
+	roleName := fmt.Sprintf("tf-acc-ds-instance-profile-role-%d", sdkacctest.RandInt())
+	profileName := fmt.Sprintf("tf-acc-ds-instance-profile-%d", sdkacctest.RandInt())
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:   func() { acctest.PreCheck(t) },
+		ErrorCheck: acctest.ErrorCheck(t, iam.EndpointsID),
+		Providers:  acctest.Providers,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccInstanceProfilesDataSourceConfig(roleName, profileName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(datasourceName, "arns.#", "1"),
+					resource.TestCheckResourceAttr(datasourceName, "paths.#", "1"),
+					resource.TestCheckResourceAttr(datasourceName, "names.#", "1"),
+					resource.TestCheckResourceAttrPair(datasourceName, "arns.*", resourceName, "arn"),
+					resource.TestCheckResourceAttrPair(datasourceName, "paths.*", resourceName, "path"),
+					resource.TestCheckResourceAttrPair(datasourceName, "names.*", resourceName, "name"),
+				),
+			},
+		},
+	})
+}
+
+func testAccInstanceProfilesDataSourceConfig(roleName, profileName string) string {
+	return fmt.Sprintf(`
+resource "aws_iam_role" "test" {
+  name               = "%s"
+  assume_role_policy = "{\"Version\":\"2012-10-17\",\"Statement\":[{\"Effect\":\"Allow\",\"Principal\":{\"Service\":[\"ec2.amazonaws.com\"]},\"Action\":[\"sts:AssumeRole\"]}]}"
+}
+
+resource "aws_iam_instance_profile" "test" {
+  name = "%s"
+  role = aws_iam_role.test.name
+  path = "/testpath/"
+}
+
+data "aws_iam_instance_profiles" "test" {
+	role_name = aws_iam_role.test.name
+	depends_on = [aws_iam_role.test, aws_iam_instance_profile.test]
+}
+`, roleName, profileName)
+}
