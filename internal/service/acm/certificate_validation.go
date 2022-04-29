37957902d44e43b03a1afccde18512856f3e4172
Joel Courtney <euphemize@gmail.com>
2022-04-29 13:35:54 +1000

    missing read steps

--- internal/service/acm/certificate_validation.go
+++ internal/service/acm/certificate_validation.go
@@ -111,6 +111,20 @@ func resourceCertificateValidationRead(d *schema.ResourceData, meta interface{})
 		return fmt.Errorf("reading ACM Certificate (%s): %w", arn, err)
 	}
 
+	if certificate == nil {
+		return fmt.Errorf("error describing ACM Certificate (%s): empty response", arn)
+	}
+
+	if status := aws.StringValue(certificate.Status); status != acm.CertificateStatusIssued {
+		if d.IsNewResource() {
+			return fmt.Errorf("ACM Certificate (%s) status not issued: %s", arn, status)
+		}
+
+		log.Printf("[WARN] ACM Certificate (%s) status not issued (%s), removing from state", arn, status)
+		d.SetId("")
+		return nil
+	}
+
 	d.Set("certificate_arn", certificate.CertificateArn)
 	d.SetId(aws.TimeValue(certificate.IssuedAt).String())
 
