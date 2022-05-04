1f259af498577c4d5649406198105927e1dbeb4c
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-05-04 09:26:53 -0400

    r/aws_iot_topic_rule: Tidy up 'TestAccIoTTopicRule_lambda'.
    Acceptance test output:
    
    % make testacc TESTARGS='-run=TestAccIoTTopicRule_lambda' PKG=iot
    ==> Checking that code complies with gofmt requirements...
    TF_ACC=1 go test ./internal/service/iot/... -v -count 1 -parallel 20  -run=TestAccIoTTopicRule_lambda -timeout 180m
    === RUN   TestAccIoTTopicRule_lambda
    === PAUSE TestAccIoTTopicRule_lambda
    === CONT  TestAccIoTTopicRule_lambda
    --- PASS: TestAccIoTTopicRule_lambda (16.61s)
    PASS
    ok  	github.com/hashicorp/terraform-provider-aws/internal/service/iot	20.630s

--- internal/service/iot/topic_rule_test.go
+++ internal/service/iot/topic_rule_test.go
@@ -751,7 +751,7 @@ func TestAccIoTTopicRule_kinesis(t *testing.T) {
 }
 
 func TestAccIoTTopicRule_lambda(t *testing.T) {
-	rName := sdkacctest.RandString(5)
+	rName := testAccTopicRuleName()
 	resourceName := "aws_iot_topic_rule.rule"
 
 	resource.ParallelTest(t, resource.TestCase{
@@ -761,9 +761,28 @@ func TestAccIoTTopicRule_lambda(t *testing.T) {
 		CheckDestroy: testAccCheckTopicRuleDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccTopicRule_lambda(rName),
+				Config: testAccTopicRuleLambdaConfig(rName),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckTopicRuleExists("aws_iot_topic_rule.rule"),
+					testAccCheckTopicRuleExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "cloudwatch_alarm.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "cloudwatch_logs.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "cloudwatch_metric.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "dynamodb.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "dynamodbv2.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "elasticsearch.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "error_action.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "firehose.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "iot_analytics.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "iot_events.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "kafka.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "kinesis.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "lambda.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "republish.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "s3.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "sns.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "sqs.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "step_functions.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "timestream.#", "0"),
 				),
 			},
 			{
@@ -1436,15 +1455,14 @@ resource "aws_iot_topic_rule" "rule" {
 `, rName))
 }
 
-func testAccTopicRule_lambda(rName string) string {
+func testAccTopicRuleLambdaConfig(rName string) string {
 	return fmt.Sprintf(`
 data "aws_region" "current" {}
 
 data "aws_partition" "current" {}
 
 resource "aws_iot_topic_rule" "rule" {
-  name        = "test_rule_%[1]s"
-  description = "Example rule"
+  name        = %[1]q
   enabled     = true
   sql         = "SELECT * FROM 'topic/test'"
   sql_version = "2015-10-08"
