a6bee5e0abc4b7e6ba5d73e19374ddc359111c83
Kamil Turek <kamil.turek@hotmail.com>
2022-05-06 18:13:32 +0200

    Use output.UpdateTime instead of output.CreateTime

--- internal/service/location/map.go
+++ internal/service/location/map.go
@@ -144,7 +144,7 @@ func resourceMapRead(d *schema.ResourceData, meta interface{}) error {
 	d.Set("description", output.Description)
 	d.Set("map_arn", output.MapArn)
 	d.Set("map_name", output.MapName)
-	d.Set("update_time", aws.TimeValue(output.CreateTime).Format(time.RFC3339))
+	d.Set("update_time", aws.TimeValue(output.UpdateTime).Format(time.RFC3339))
 
 	tags := KeyValueTags(output.Tags).IgnoreAWS().IgnoreConfig(ignoreTagsConfig)
 
