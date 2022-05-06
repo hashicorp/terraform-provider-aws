f0582476cbb0b84d752c2f83fa0df31d80968fa9
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-05-06 17:01:19 -0400

    Add 'SemVerGreaterThanOrEqual'.

--- internal/verify/semver.go
+++ internal/verify/semver.go
@@ -8,6 +8,7 @@ import (
 // according to Semantic Versioning rules (https://semver.org/).
 func SemVerLessThan(s1, s2 string) bool {
 	v1, v2, err := parseVersions(s1, s2)
+
 	if err != nil {
 		return false
 	}
@@ -15,21 +16,16 @@ func SemVerLessThan(s1, s2 string) bool {
 	return v1.LessThan(v2)
 }
 
-// SemVerGreaterThan returns whether or not the first version string is greater than the second
-// according to Semantic Versioning rules (https://semver.org/).
-func SemVerGreaterThan(s1, s2 string) bool {
-	return SemVerLessThan(s2, s1)
-}
-
-// SemVerEqual returns whether or not the first version string is equal to the second
-// according to Semantic Versioning rules (https://semver.org/).
-func SemVerEqual(s1, s2 string) bool {
+// SemVerGreaterThanOrEqual returns whether or not the first version string is greater than or equal
+// to the second according to Semantic Versioning rules (https://semver.org/).
+func SemVerGreaterThanOrEqual(s1, s2 string) bool {
 	v1, v2, err := parseVersions(s1, s2)
+
 	if err != nil {
 		return false
 	}
 
-	return v1.Equal(v2)
+	return v1.GreaterThanOrEqual(v2)
 }
 
 func parseVersions(s1, s2 string) (*gversion.Version, *gversion.Version, error) {
--- internal/verify/semver_test.go
+++ internal/verify/semver_test.go
@@ -23,7 +23,7 @@ func TestSemVerLessThan(t *testing.T) {
 	}
 }
 
-func TestSemVerGreaterThan(t *testing.T) {
+func TestSemVerGreaterThanOrEqual(t *testing.T) {
 	for _, tc := range []struct {
 		s1 string
 		s2 string
@@ -31,33 +31,13 @@ func TestSemVerGreaterThan(t *testing.T) {
 	}{
 		{"1.0", "2.0", false},
 		{"3.0", "2.0", true},
-		{"4.0", "4.0", false},
+		{"4.0", "4.0", true},
 		{"2", "10", false},
 		{"abc", "xyz", false},
 	} {
-		lt := SemVerGreaterThan(tc.s1, tc.s2)
+		lt := SemVerGreaterThanOrEqual(tc.s1, tc.s2)
 		if tc.lt != lt {
-			t.Fatalf("SemVerGreaterThan(%q, %q) should be: %t", tc.s1, tc.s2, tc.lt)
-		}
-	}
-}
-
-func TestSemVerSemVerEqual(t *testing.T) {
-	for _, tc := range []struct {
-		s1 string
-		s2 string
-		lt bool
-	}{
-		{"1.0", "2.0", false},
-		{"4.0", "3.0", false},
-		{"3.0", "3.0", true},
-		{"4.0", "4.0", true},
-		{"10", "10", true},
-		{"abc", "xyz", false},
-	} {
-		lt := SemVerEqual(tc.s1, tc.s2)
-		if tc.lt != lt {
-			t.Fatalf("SemVerEqual(%q, %q) should be: %t", tc.s1, tc.s2, tc.lt)
+			t.Fatalf("SemVerGreaterThanOrEqual(%q, %q) should be: %t", tc.s1, tc.s2, tc.lt)
 		}
 	}
 }
