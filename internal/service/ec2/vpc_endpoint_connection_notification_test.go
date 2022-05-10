d565f41e7702854bf92a8d91d50f73710d0fce03
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-05-10 11:35:38 -0400

    Run 'make fmt'.

--- internal/service/ec2/vpc_endpoint_connection_notification_test.go
+++ internal/service/ec2/vpc_endpoint_connection_notification_test.go
@@ -12,7 +12,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
-  tfec2 "github.com/hashicorp/terraform-provider-aws/internal/service/ec2"
+	tfec2 "github.com/hashicorp/terraform-provider-aws/internal/service/ec2"
 )
 
 func TestAccVPCEndpointConnectionNotification_basic(t *testing.T) {
