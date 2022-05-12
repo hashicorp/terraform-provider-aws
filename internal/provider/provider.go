5c51c5aa3ebffc4ec8850b30793f8305b2ed81e3
Glenn <31919569+GlennChia@users.noreply.github.com>
2022-05-13 03:30:01 +0800

    r/aws_appintegrations_event_integration (#23904)
    * fix(appint): remap to appintegrationsservice
    
    map appintegrations to appintegrationsservice
    
    * feat(appint): add generate file and tags_gen
    
    * feat(appint): eventIntegration schema
    
    * feat(appint): eventIntegration read
    
    * feat(appint): eventIntegration create
    
    * feat(appint): eventIntegration update
    
    * feat(appint): eventIntegration delete
    
    * feat(appint): add eventIntegration to provider
    
    * test(appint): eventIntegration basic update desc
    
    * test(appint): eventIntegration disappears
    
    * test(appint): eventIntegration update tags
    
    * docs(appint): add eventIntegration resource doc
    
    * ci(appint): add changelog for eventIntegration
    
    * fix(appint): add CustomizeDiff to fix update tags
    
    CustomizeDiff: verify.SetTagsDiff,
    
    * test(appint): change sourceName to point to var
    
    * chore(appint): rm unused flags from generate go
    
    * add partition prechecks
    
    Co-authored-by: Simon Davis <sdavis@hashicorp.com>

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -26,6 +26,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/service/appautoscaling"
 	"github.com/hashicorp/terraform-provider-aws/internal/service/appconfig"
 	"github.com/hashicorp/terraform-provider-aws/internal/service/appflow"
+	"github.com/hashicorp/terraform-provider-aws/internal/service/appintegrations"
 	"github.com/hashicorp/terraform-provider-aws/internal/service/appmesh"
 	"github.com/hashicorp/terraform-provider-aws/internal/service/apprunner"
 	"github.com/hashicorp/terraform-provider-aws/internal/service/appstream"
@@ -961,6 +962,8 @@ func Provider() *schema.Provider {
 			"aws_appautoscaling_target":           appautoscaling.ResourceTarget(),
 
 			"aws_appflow_flow": appflow.ResourceFlow(),
+
+			"aws_appintegrations_event_integration": appintegrations.ResourceEventIntegration(),
 
 			"aws_appmesh_gateway_route":   appmesh.ResourceGatewayRoute(),
 			"aws_appmesh_mesh":            appmesh.ResourceMesh(),
