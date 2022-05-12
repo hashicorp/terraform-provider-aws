9a5db7e8ec931a46fe2a2502023a8a7ec4936f00
Dave DeRicco <ddericco@amazon.com>
2022-05-11 17:47:54 -0700

    Cleanup unneeded comments

--- internal/service/networkfirewall/firewall_policy_data_source.go
+++ internal/service/networkfirewall/firewall_policy_data_source.go
@@ -17,13 +17,7 @@ func DataSourceFirewallPolicy() *schema.Resource {
 		ReadContext: dataSourceFirewallPolicyRead,
 		Schema: map[string]*schema.Schema{
 			"arn": {
-<<<<<<< HEAD
 				Type:         schema.TypeString,
-=======
-				Type: schema.TypeString,
-				//Computed: true,
-				// Assuming ARN is the only acceptable input (it's not)
->>>>>>> 7505fe3b1a (Add changelog, WIP support for name input)
 				AtLeastOneOf: []string{"arn", "name"},
 				Optional:     true,
 			},
