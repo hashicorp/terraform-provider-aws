34e920af1f1859dd1b0150a2b76707b159dfae41
GlennChia <glennchia7@gmail.com>
2022-05-13 21:21:16 +0000

    feat(connect): HierarchyGroup datasrc reads

--- internal/service/connect/user_hierarchy_group_data_source.go
+++ internal/service/connect/user_hierarchy_group_data_source.go
@@ -1,13 +1,21 @@
 package connect
 
 import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/connect"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 func DataSourceUserHierarchyGroup() *schema.Resource {
 	return &schema.Resource{
+		ReadContext: dataSourceUserHierarchyGroupRead,
 		Schema: map[string]*schema.Schema{
 			"arn": {
 				Type:     schema.TypeString,
@@ -70,4 +78,96 @@ func DataSourceUserHierarchyGroup() *schema.Resource {
 			"tags": tftags.TagsSchemaComputed(),
 		},
 	}
+}
+
+func dataSourceUserHierarchyGroupRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn := meta.(*conns.AWSClient).ConnectConn
+	ignoreTagsConfig := meta.(*conns.AWSClient).IgnoreTagsConfig
+
+	instanceID := d.Get("instance_id").(string)
+
+	input := &connect.DescribeUserHierarchyGroupInput{
+		InstanceId: aws.String(instanceID),
+	}
+
+	if v, ok := d.GetOk("hierarchy_group_id"); ok {
+		input.HierarchyGroupId = aws.String(v.(string))
+	} else if v, ok := d.GetOk("name"); ok {
+		name := v.(string)
+		hierarchyGroupSummary, err := dataSourceGetConnectUserHierarchyGroupSummaryByName(ctx, conn, instanceID, name)
+
+		if err != nil {
+			return diag.FromErr(fmt.Errorf("error finding Connect Hierarchy Group Summary by name (%s): %w", name, err))
+		}
+
+		if hierarchyGroupSummary == nil {
+			return diag.FromErr(fmt.Errorf("error finding Connect Hierarchy Group Summary by name (%s): not found", name))
+		}
+
+		input.HierarchyGroupId = hierarchyGroupSummary.Id
+	}
+
+	resp, err := conn.DescribeUserHierarchyGroupWithContext(ctx, input)
+
+	if err != nil {
+		return diag.FromErr(fmt.Errorf("error getting Connect Hierarchy Group: %w", err))
+	}
+
+	if resp == nil || resp.HierarchyGroup == nil {
+		return diag.FromErr(fmt.Errorf("error getting Connect Hierarchy Group: empty response"))
+	}
+
+	hierarchyGroup := resp.HierarchyGroup
+
+	d.Set("arn", hierarchyGroup.Arn)
+	d.Set("hierarchy_group_id", hierarchyGroup.Id)
+	d.Set("instance_id", instanceID)
+	d.Set("level_id", hierarchyGroup.LevelId)
+	d.Set("name", hierarchyGroup.Name)
+
+	if err := d.Set("hierarchy_path", flattenUserHierarchyPath(hierarchyGroup.HierarchyPath)); err != nil {
+		return diag.FromErr(fmt.Errorf("error setting Connect User Hierarchy Group hierarchy_path (%s): %w", d.Id(), err))
+	}
+
+	if err := d.Set("tags", KeyValueTags(hierarchyGroup.Tags).IgnoreAWS().IgnoreConfig(ignoreTagsConfig).Map()); err != nil {
+		return diag.FromErr(fmt.Errorf("error setting tags: %s", err))
+	}
+
+	d.SetId(fmt.Sprintf("%s:%s", instanceID, aws.StringValue(hierarchyGroup.Id)))
+
+	return nil
+}
+
+func dataSourceGetConnectUserHierarchyGroupSummaryByName(ctx context.Context, conn *connect.Connect, instanceID, name string) (*connect.HierarchyGroupSummary, error) {
+	var result *connect.HierarchyGroupSummary
+
+	input := &connect.ListUserHierarchyGroupsInput{
+		InstanceId: aws.String(instanceID),
+		MaxResults: aws.Int64(ListUserHierarchyGroupsMaxResults),
+	}
+
+	err := conn.ListUserHierarchyGroupsPagesWithContext(ctx, input, func(page *connect.ListUserHierarchyGroupsOutput, lastPage bool) bool {
+		if page == nil {
+			return !lastPage
+		}
+
+		for _, qs := range page.UserHierarchyGroupSummaryList {
+			if qs == nil {
+				continue
+			}
+
+			if aws.StringValue(qs.Name) == name {
+				result = qs
+				return false
+			}
+		}
+
+		return !lastPage
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
 }
