a331bca7911ce8255aff7dcac17a0a87dcad0dd3
Chris Gilmer <cgilmer@lucasfilm.com>
2022-05-18 09:06:53 -0700

    Update importer to use StateContext

--- internal/service/imagebuilder/container_recipe.go
+++ internal/service/imagebuilder/container_recipe.go
@@ -24,7 +24,7 @@ func ResourceContainerRecipe() *schema.Resource {
 		Update: resourceContainerRecipeUpdate,
 		Delete: resourceContainerRecipeDelete,
 		Importer: &schema.ResourceImporter{
-			State: schema.ImportStatePassthrough,
+			StateContext: schema.ImportStatePassthroughContext,
 		},
 		Schema: map[string]*schema.Schema{
 			"arn": {
