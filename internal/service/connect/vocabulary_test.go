c7c17163569521122df5a88b0ec49afd2418dfc6
GlennChia <glennchia7@gmail.com>
2022-05-18 10:51:11 +0000

    test(connect): vocab destroy

--- internal/service/connect/vocabulary_test.go
+++ internal/service/connect/vocabulary_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/connect"
+	"github.com/hashicorp/aws-sdk-go-base/v2/awsv1shim/v2/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
@@ -44,4 +45,42 @@ func testAccCheckVocabularyExists(resourceName string, function *connect.Describ
 
 		return nil
 	}
+}
+
+func testAccCheckVocabularyDestroy(s *terraform.State) error {
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "aws_connect_vocabulary" {
+			continue
+		}
+
+		conn := acctest.Provider.Meta().(*conns.AWSClient).ConnectConn
+
+		instanceID, vocabularyID, err := tfconnect.VocabularyParseID(rs.Primary.ID)
+
+		if err != nil {
+			return err
+		}
+
+		params := &connect.DescribeVocabularyInput{
+			InstanceId:   aws.String(instanceID),
+			VocabularyId: aws.String(vocabularyID),
+		}
+
+		resp, err := conn.DescribeVocabulary(params)
+
+		if tfawserr.ErrCodeEquals(err, connect.ErrCodeResourceNotFoundException) {
+			continue
+		}
+
+		if err != nil {
+			return err
+		}
+
+		// API returns an empty list for Vocabulary if there are none
+		if resp.Vocabulary == nil {
+			continue
+		}
+	}
+
+	return nil
 }
