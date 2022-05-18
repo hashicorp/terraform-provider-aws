7483ff5a471c8975c4913e5f6be0ace920e9b87a
GlennChia <glennchia7@gmail.com>
2022-05-18 10:42:11 +0000

    feat(connect): wait for vocab deleted

--- internal/service/connect/wait.go
+++ internal/service/connect/wait.go
@@ -19,6 +19,9 @@ const (
 	connectBotAssociationCreateTimeout = 5 * time.Minute
 
 	connectVocabularyCreatedTimeout = 5 * time.Minute
+	// It takes about 90 minutes for Amazon Connect to delete a vocabulary.
+	// https://docs.aws.amazon.com/connect/latest/adminguide/add-custom-vocabulary.html
+	connectVocabularyDeletedTimeout = 100 * time.Minute
 )
 
 func waitInstanceCreated(ctx context.Context, conn *connect.Connect, instanceId string) (*connect.DescribeInstanceOutput, error) {
@@ -62,6 +65,23 @@ func waitVocabularyCreated(ctx context.Context, conn *connect.Connect, timeout t
 	stateConf := &resource.StateChangeConf{
 		Pending: []string{connect.VocabularyStateCreationInProgress},
 		Target:  []string{connect.VocabularyStateActive, connect.VocabularyStateCreationFailed},
+		Refresh: statusVocabulary(ctx, conn, instanceId, vocabularyId),
+		Timeout: timeout,
+	}
+
+	outputRaw, err := stateConf.WaitForState()
+
+	if v, ok := outputRaw.(*connect.DescribeVocabularyOutput); ok {
+		return v, err
+	}
+
+	return nil, err
+}
+
+func waitVocabularyDeleted(ctx context.Context, conn *connect.Connect, timeout time.Duration, instanceId, vocabularyId string) (*connect.DescribeVocabularyOutput, error) {
+	stateConf := &resource.StateChangeConf{
+		Pending: []string{connect.VocabularyStateDeleteInProgress},
+		Target:  []string{connect.ErrCodeResourceNotFoundException},
 		Refresh: statusVocabulary(ctx, conn, instanceId, vocabularyId),
 		Timeout: timeout,
 	}
