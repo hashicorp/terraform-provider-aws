eebedd5ba3c0e20f0796c27970f20e03ded28deb
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-05-22 17:46:23 -0400

    r/aws_autoscaling_group: Tidy up 'TestAccAutoScalingGroup_MixedInstancesPolicyInstancesDistribution_onDemandBaseCapacity'.
    Acceptance test output:
    
    % make testacc TESTARGS='-run=TestAccAutoScalingGroup_MixedInstancesPolicyInstancesDistribution_onDemandBaseCapacity' PKG=autoscaling ACCTEST_PARALLELISM=2
    ==> Checking that code complies with gofmt requirements...
    TF_ACC=1 go test ./internal/service/autoscaling/... -v -count 1 -parallel 2  -run=TestAccAutoScalingGroup_MixedInstancesPolicyInstancesDistribution_onDemandBaseCapacity -timeout 180m
    === RUN   TestAccAutoScalingGroup_MixedInstancesPolicyInstancesDistribution_onDemandBaseCapacity
    === PAUSE TestAccAutoScalingGroup_MixedInstancesPolicyInstancesDistribution_onDemandBaseCapacity
    === CONT  TestAccAutoScalingGroup_MixedInstancesPolicyInstancesDistribution_onDemandBaseCapacity
    --- PASS: TestAccAutoScalingGroup_MixedInstancesPolicyInstancesDistribution_onDemandBaseCapacity (148.50s)
    PASS
    ok  	github.com/hashicorp/terraform-provider-aws/internal/service/autoscaling	152.484s

--- internal/service/autoscaling/group_test.go
+++ internal/service/autoscaling/group_test.go
@@ -1300,7 +1300,7 @@ func TestAccAutoScalingGroup_MixedInstancesPolicyInstancesDistribution_onDemandB
 		CheckDestroy:      testAccCheckGroupDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccGroupConfig_MixedInstancesPolicy_InstancesDistribution_OnDemandBaseCapacity(rName, 1),
+				Config: testAccGroupMixedInstancesPolicyInstancesDistributionOnDemandBaseCapacityConfig(rName, 1),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckGroupExists(resourceName, &group),
 					resource.TestCheckResourceAttr(resourceName, "mixed_instances_policy.#", "1"),
@@ -1310,7 +1310,7 @@ func TestAccAutoScalingGroup_MixedInstancesPolicyInstancesDistribution_onDemandB
 			},
 			testAccGroupImportStep(resourceName),
 			{
-				Config: testAccGroupConfig_MixedInstancesPolicy_InstancesDistribution_OnDemandBaseCapacity(rName, 2),
+				Config: testAccGroupMixedInstancesPolicyInstancesDistributionOnDemandBaseCapacityConfig(rName, 2),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckGroupExists(resourceName, &group),
 					resource.TestCheckResourceAttr(resourceName, "mixed_instances_policy.#", "1"),
@@ -1319,7 +1319,7 @@ func TestAccAutoScalingGroup_MixedInstancesPolicyInstancesDistribution_onDemandB
 				),
 			},
 			{
-				Config: testAccGroupConfig_MixedInstancesPolicy_InstancesDistribution_OnDemandBaseCapacity(rName, 0),
+				Config: testAccGroupMixedInstancesPolicyInstancesDistributionOnDemandBaseCapacityConfig(rName, 0),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckGroupExists(resourceName, &group),
 					resource.TestCheckResourceAttr(resourceName, "mixed_instances_policy.#", "1"),
@@ -1344,7 +1344,7 @@ func TestAccAutoScalingGroup_MixedInstancesPolicyInstancesDistribution_updateToZ
 		CheckDestroy:      testAccCheckGroupDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccGroupConfig_MixedInstancesPolicy_InstancesDistribution_OnDemandBaseCapacity(rName, 1),
+				Config: testAccGroupMixedInstancesPolicyInstancesDistributionOnDemandBaseCapacityConfig(rName, 1),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckGroupExists(resourceName, &group),
 					resource.TestCheckResourceAttr(resourceName, "mixed_instances_policy.#", "1"),
@@ -1354,7 +1354,7 @@ func TestAccAutoScalingGroup_MixedInstancesPolicyInstancesDistribution_updateToZ
 			},
 			testAccGroupImportStep(resourceName),
 			{
-				Config: testAccGroupConfig_MixedInstancesPolicy_InstancesDistribution_OnDemandBaseCapacity(rName, 0),
+				Config: testAccGroupMixedInstancesPolicyInstancesDistributionOnDemandBaseCapacityConfig(rName, 0),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckGroupExists(resourceName, &group),
 					resource.TestCheckResourceAttr(resourceName, "mixed_instances_policy.#", "1"),
@@ -4464,19 +4464,18 @@ resource "aws_autoscaling_group" "test" {
 `, rName, onDemandAllocationStrategy))
 }
 
-func testAccGroupConfig_MixedInstancesPolicy_InstancesDistribution_OnDemandBaseCapacity(rName string, onDemandBaseCapacity int) string {
-	return testAccGroupConfig_MixedInstancesPolicy_Base(rName) +
-		fmt.Sprintf(`
+func testAccGroupMixedInstancesPolicyInstancesDistributionOnDemandBaseCapacityConfig(rName string, onDemandBaseCapacity int) string {
+	return acctest.ConfigCompose(testAccGroupLaunchTemplateBaseConfig(rName, "t3.micro"), fmt.Sprintf(`
 resource "aws_autoscaling_group" "test" {
   availability_zones = [data.aws_availability_zones.available.names[0]]
   desired_capacity   = 0
   max_size           = 2
   min_size           = 0
-  name               = %q
+  name               = %[1]q
 
   mixed_instances_policy {
     instances_distribution {
-      on_demand_base_capacity = %d
+      on_demand_base_capacity = %[2]d
     }
 
     launch_template {
@@ -4487,13 +4486,14 @@ resource "aws_autoscaling_group" "test" {
       override {
         instance_type = "t2.micro"
       }
+
       override {
         instance_type = "t3.small"
       }
     }
   }
 }
-`, rName, onDemandBaseCapacity)
+`, rName, onDemandBaseCapacity))
 }
 
 func testAccGroupConfig_MixedInstancesPolicy_InstancesDistribution_OnDemandPercentageAboveBaseCapacity(rName string, onDemandPercentageAboveBaseCapacity int) string {
