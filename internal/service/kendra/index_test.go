4232e1fa29c40695977001a11a4f3ded41063e0f
GlennChia <glennchia7@gmail.com>
2022-05-22 08:12:00 +0000

    test(kendra): index exists

---
+++ internal/service/kendra/index_test.go
@@ -0,0 +1,36 @@
+package kendra_test
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/kendra"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+)
+
+func testAccCheckIndexExists(name string, index *kendra.DescribeIndexOutput) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[name]
+
+		if !ok {
+			return fmt.Errorf("Not found: %s", name)
+		}
+
+		conn := acctest.Provider.Meta().(*conns.AWSClient).KendraConn
+		input := &kendra.DescribeIndexInput{
+			Id: aws.String(rs.Primary.ID),
+		}
+		resp, err := conn.DescribeIndex(input)
+
+		if err != nil {
+			return err
+		}
+
+		*index = *resp
+
+		return nil
+	}
+}
