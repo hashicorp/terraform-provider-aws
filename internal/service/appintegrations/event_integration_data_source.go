d0cbf2699ca2e024ca93f56f6b290d479a1b4e4b
GlennChia <glennchia7@gmail.com>
2022-05-24 05:39:00 +0000

    feat(appint): eventIntegration datasrc read

--- internal/service/appintegrations/event_integration_data_source.go
+++ internal/service/appintegrations/event_integration_data_source.go
@@ -1,12 +1,20 @@
 package appintegrations
 
 import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/appintegrationsservice"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 func DataSourceEventIntegration() *schema.Resource {
 	return &schema.Resource{
+		ReadContext: dataSourceEventIntegrationRead,
 		Schema: map[string]*schema.Schema{
 			"arn": {
 				Type:     schema.TypeString,
@@ -39,4 +47,35 @@ func DataSourceEventIntegration() *schema.Resource {
 			"tags": tftags.TagsSchemaComputed(),
 		},
 	}
+}
+
+func dataSourceEventIntegrationRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn := meta.(*conns.AWSClient).AppIntegrationsConn
+	ignoreTagsConfig := meta.(*conns.AWSClient).IgnoreTagsConfig
+
+	name := d.Get("name").(string)
+
+	resp, err := conn.GetEventIntegrationWithContext(ctx, &appintegrationsservice.GetEventIntegrationInput{
+		Name: aws.String(name),
+	})
+	if err != nil {
+		return diag.FromErr(fmt.Errorf("error getting AppIntegrations Event Integration: %w", err))
+	}
+
+	d.SetId(aws.StringValue(resp.Name))
+
+	d.Set("arn", resp.EventIntegrationArn)
+	d.Set("description", resp.Description)
+	d.Set("eventbridge_bus", resp.EventBridgeBus)
+	d.Set("name", resp.Name)
+
+	if err := d.Set("event_filter", flattenEventFilter(resp.EventFilter)); err != nil {
+		return diag.FromErr(fmt.Errorf("error setting event_filter: %w", err))
+	}
+
+	if err := d.Set("tags", KeyValueTags(resp.Tags).IgnoreAWS().IgnoreConfig(ignoreTagsConfig).Map()); err != nil {
+		return diag.FromErr(fmt.Errorf("error setting tags: %s", err))
+	}
+
+	return nil
 }
