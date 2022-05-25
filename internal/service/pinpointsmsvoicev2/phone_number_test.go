86e1c31bb6a93fe3aed54c2a3b2016e984a5200b
James Marino <james@marino.io>
2022-05-25 23:56:50 +1000

    fix: linting fixes

--- internal/service/pinpointsmsvoicev2/phone_number_test.go
+++ internal/service/pinpointsmsvoicev2/phone_number_test.go
@@ -12,7 +12,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
-
 	tfpinpointsmsvoicev2 "github.com/hashicorp/terraform-provider-aws/internal/service/pinpointsmsvoicev2"
 )
 
