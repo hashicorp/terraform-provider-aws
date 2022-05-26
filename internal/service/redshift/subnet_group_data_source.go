9135136d2ea758331878764600bf369d67f0e903
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-05-26 08:09:55 -0400

    d/aws_redshift_subnet_group: Tidy up error handling.

--- internal/service/redshift/subnet_group_data_source.go
+++ internal/service/redshift/subnet_group_data_source.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/arn"
+	"github.com/aws/aws-sdk-go/service/redshift"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
@@ -44,29 +45,26 @@ func dataSourceSubnetGroupRead(d *schema.ResourceData, meta interface{}) error {
 	subnetgroup, err := FindSubnetGroupByName(conn, d.Get("name").(string))
 
 	if err != nil {
-		return fmt.Errorf("error reading Redshift Subnet Group (%s): %w", d.Id(), err)
+		return fmt.Errorf("reading Redshift Subnet Group (%s): %w", d.Id(), err)
 	}
 
 	d.SetId(aws.StringValue(subnetgroup.ClusterSubnetGroupName))
-
-	d.Set("name", subnetgroup.ClusterSubnetGroupName)
-	d.Set("description", subnetgroup.Description)
-	d.Set("subnet_ids", subnetIdsToSlice(subnetgroup.Subnets))
-
-	//lintignore:AWSR002
-	if err := d.Set("tags", KeyValueTags(subnetgroup.Tags).IgnoreAWS().IgnoreConfig(ignoreTagsConfig).Map()); err != nil {
-		return fmt.Errorf("error setting tags: %w", err)
-	}
-
 	arn := arn.ARN{
 		Partition: meta.(*conns.AWSClient).Partition,
-		Service:   "redshift",
+		Service:   redshift.ServiceName,
 		Region:    meta.(*conns.AWSClient).Region,
 		AccountID: meta.(*conns.AWSClient).AccountID,
 		Resource:  fmt.Sprintf("subnetgroup:%s", d.Id()),
 	}.String()
-
 	d.Set("arn", arn)
+	d.Set("description", subnetgroup.Description)
+	d.Set("name", subnetgroup.ClusterSubnetGroupName)
+	d.Set("subnet_ids", subnetIdsToSlice(subnetgroup.Subnets))
+
+	//lintignore:AWSR002
+	if err := d.Set("tags", KeyValueTags(subnetgroup.Tags).IgnoreAWS().IgnoreConfig(ignoreTagsConfig).Map()); err != nil {
+		return fmt.Errorf("setting tags: %w", err)
+	}
 
 	return nil
 }
