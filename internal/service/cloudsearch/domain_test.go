faa835b9b34f47bd6982c39755b477b2f38415f5
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-05-26 14:23:18 -0400

    '*' is not valid syntax for 'source_fields'.

--- internal/service/cloudsearch/domain_test.go
+++ internal/service/cloudsearch/domain_test.go
@@ -183,7 +183,7 @@ func TestAccCloudSearchDomain_sourceFields(t *testing.T) {
 				Config: testAccDomainSourceFieldsUpdatedConfig(rName),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					testAccDomainExists(resourceName, &v),
-					resource.TestCheckResourceAttr(resourceName, "index_field.#", "3"),
+					resource.TestCheckResourceAttr(resourceName, "index_field.#", "4"),
 					resource.TestCheckTypeSetElemNestedAttrs(resourceName, "index_field.*", map[string]string{
 						"name":          "int_test",
 						"type":          "int",
@@ -197,9 +197,15 @@ func TestAccCloudSearchDomain_sourceFields(t *testing.T) {
 						"source_fields": "",
 					}),
 					resource.TestCheckTypeSetElemNestedAttrs(resourceName, "index_field.*", map[string]string{
+						"name":          "int_test_3",
+						"type":          "int",
+						"default_value": "8",
+						"source_fields": "",
+					}),
+					resource.TestCheckTypeSetElemNestedAttrs(resourceName, "index_field.*", map[string]string{
 						"name":          "int_test_source",
 						"type":          "int-array",
-						"source_fields": "*",
+						"source_fields": "int_test_3",
 					}),
 				),
 			},
@@ -463,10 +469,16 @@ resource "aws_cloudsearch_domain" "test" {
   }
 
   index_field {
+    name          = "int_test_3"
+    type          = "int"
+    default_value = "8"
+  }
+
+  index_field {
     name   = "int_test_source"
     type   = "int-array"
 
-    source_fields = "*"
+    source_fields = "int_test_3"
   }
 }
 `, rName)
