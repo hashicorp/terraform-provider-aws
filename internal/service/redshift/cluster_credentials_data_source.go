793daa98554a327166462abfcac72928f1f9a77f
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-01 09:19:07 -0400

    d/aws_redshift_cluster_credentials: Alphabetize attributes.

--- internal/service/redshift/cluster_credentials_data_source.go
+++ internal/service/redshift/cluster_credentials_data_source.go
@@ -25,22 +25,22 @@ func DataSourceClusterCredentials() *schema.Resource {
 				Type:     schema.TypeString,
 				Required: true,
 			},
+			"db_groups": {
+				Type:     schema.TypeSet,
+				Optional: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
 			"db_name": {
 				Type:     schema.TypeString,
 				Optional: true,
-			},
-			"db_user": {
-				Type:     schema.TypeString,
-				Required: true,
 			},
 			"db_password": {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
-			"db_groups": {
-				Type:     schema.TypeSet,
-				Optional: true,
-				Elem:     &schema.Schema{Type: schema.TypeString},
+			"db_user": {
+				Type:     schema.TypeString,
+				Required: true,
 			},
 			"duration_seconds": {
 				Type:         schema.TypeInt,
@@ -59,32 +59,32 @@ func DataSourceClusterCredentials() *schema.Resource {
 func dataSourceClusterCredentialsRead(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*conns.AWSClient).RedshiftConn
 
-	clusterId := d.Get("cluster_identifier").(string)
+	clusterID := d.Get("cluster_identifier").(string)
 	input := &redshift.GetClusterCredentialsInput{
-		ClusterIdentifier: aws.String(clusterId),
-		DbUser:            aws.String(d.Get("db_user").(string)),
 		AutoCreate:        aws.Bool(d.Get("auto_create").(bool)),
+		ClusterIdentifier: aws.String(clusterID),
+		DbUser:            aws.String(d.Get("db_user").(string)),
 		DurationSeconds:   aws.Int64(int64(d.Get("duration_seconds").(int))),
-	}
-
-	if v, ok := d.GetOk("db_name"); ok {
-		input.DbName = aws.String(v.(string))
 	}
 
 	if v, ok := d.GetOk("db_groups"); ok && v.(*schema.Set).Len() > 0 {
 		input.DbGroups = flex.ExpandStringSet(v.(*schema.Set))
 	}
 
+	if v, ok := d.GetOk("db_name"); ok {
+		input.DbName = aws.String(v.(string))
+	}
+
 	creds, err := conn.GetClusterCredentials(input)
 
 	if err != nil {
-		return fmt.Errorf("reading Redshift Cluster Credentials for Cluster (%s): %w", clusterId, err)
+		return fmt.Errorf("reading Redshift Cluster Credentials for Cluster (%s): %w", clusterID, err)
 	}
 
-	d.SetId(clusterId)
+	d.SetId(clusterID)
 
-	d.Set("db_user", creds.DbUser)
 	d.Set("db_password", creds.DbPassword)
+	d.Set("db_user", creds.DbUser)
 	d.Set("expiration", aws.TimeValue(creds.Expiration).Format(time.RFC3339))
 
 	return nil
