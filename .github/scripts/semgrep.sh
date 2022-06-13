cad622cf9e330479d53d8d7fd6a4c4cadad27c5a
Dirk Avery <dirk.avery@gmail.com>
2022-06-13 14:23:07 -0400

    ci: Switch to script file

--- .github/scripts/semgrep.sh
+++ .github/scripts/semgrep.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-local idx=$1
+idx=$1
 
-local results=$( semgrep -c .semgrep-service-name"${idx}".yml 2>&1 )
+results=$( semgrep -c .semgrep-service-name"${idx}".yml 2>&1 )
 while [[ "${results}" == *Invalid_argument* ]]; do
   echo "${results}"
   results=$( semgrep -c .semgrep-service-name"${idx}".yml 2>&1 )
 done
 if [[ ! "${results}" == *" 0 findings"* ]]; then
-  echo "${results}"
-  return 1
+  >&2 echo "${results}"
 fi
 echo "${results}"
