4591c34e1557c1b0673013a37c8d25d9ddd914ac
Dirk Avery <dirk.avery@gmail.com>
2022-06-13 14:37:51 -0400

    ci: Only retry on 0 findings

--- .github/scripts/semgrep.sh
+++ .github/scripts/semgrep.sh
@@ -3,7 +3,7 @@
 idx=$1
 
 results=$( semgrep -c .semgrep-service-name"${idx}".yml 2>&1 )
-while [[ "${results}" == *Invalid_argument* ]]; do
+while [[ "${results}" == *Invalid_argument* ]] && [[ "${results}" == *" 0 findings"* ]]; do
   echo "${results}"
   results=$( semgrep -c .semgrep-service-name"${idx}".yml 2>&1 )
 done
