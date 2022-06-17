fd1b3e85d2e4b22ae0d431e958f79e56c178d4a7
Dirk Avery <dirk.avery@gmail.com>
2022-06-17 17:46:13 -0400

    ci/golangci: Fix shell

--- .github/scripts/golangci-lint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd tools && go install github.com/golangci/golangci-lint/cmd/golangci-lint
-results=$( golangci-lint run ./internal/... 2>&1 )
-echo "${results}"
-
-cd providerlint && results=$( golangci-lint run ./... 2>&1 )
-echo "${results}"
