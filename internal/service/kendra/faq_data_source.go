138b34fcf82d147e9e1577f191a07e012641a7de
GlennChia <glennchia7@gmail.com>
2022-06-22 04:10:13 +0000

    feat(kendra): faq datasrc reads

--- internal/service/kendra/faq_data_source.go
+++ internal/service/kendra/faq_data_source.go
@@ -1,15 +1,23 @@
 package kendra
 
 import (
+	"context"
+	"fmt"
 	"regexp"
+	"time"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/arn"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 func DataSourceFaq() *schema.Resource {
 	return &schema.Resource{
+		ReadContext: dataSourceFaqRead,
 		Schema: map[string]*schema.Schema{
 			"arn": {
 				Type:     schema.TypeString,
@@ -89,4 +97,61 @@ func DataSourceFaq() *schema.Resource {
 			"tags": tftags.TagsSchemaComputed(),
 		},
 	}
+}
+
+func dataSourceFaqRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn := meta.(*conns.AWSClient).KendraConn
+	ignoreTagsConfig := meta.(*conns.AWSClient).IgnoreTagsConfig
+
+	id := d.Get("faq_id").(string)
+	indexId := d.Get("index_id").(string)
+
+	resp, err := FindFaqByID(ctx, conn, id, indexId)
+
+	if err != nil {
+		return diag.Errorf("getting Kendra Faq (%s): %s", d.Id(), err)
+	}
+
+	if resp == nil {
+		return diag.Errorf("getting Kendra Faq (%s): empty response", id)
+	}
+
+	arn := arn.ARN{
+		Partition: meta.(*conns.AWSClient).Partition,
+		Service:   "kendra",
+		Region:    meta.(*conns.AWSClient).Region,
+		AccountID: meta.(*conns.AWSClient).AccountID,
+		Resource:  fmt.Sprintf("index/%s/faq/%s", indexId, id),
+	}.String()
+
+	d.Set("arn", arn)
+	d.Set("created_at", aws.ToTime(resp.CreatedAt).Format(time.RFC3339))
+	d.Set("description", resp.Description)
+	d.Set("error_message", resp.ErrorMessage)
+	d.Set("faq_id", resp.Id)
+	d.Set("file_format", resp.FileFormat)
+	d.Set("index_id", resp.IndexId)
+	d.Set("language_code", resp.LanguageCode)
+	d.Set("name", resp.Name)
+	d.Set("role_arn", resp.RoleArn)
+	d.Set("status", resp.Status)
+	d.Set("updated_at", aws.ToTime(resp.UpdatedAt).Format(time.RFC3339))
+
+	if err := d.Set("s3_path", flattenS3Path(resp.S3Path)); err != nil {
+		return diag.FromErr(err)
+	}
+
+	tags, err := ListTags(ctx, conn, arn)
+	if err != nil {
+		return diag.Errorf("error listing tags for resource (%s): %s", arn, err)
+	}
+	tags = tags.IgnoreAWS().IgnoreConfig(ignoreTagsConfig)
+
+	if err := d.Set("tags", tags.IgnoreAWS().IgnoreConfig(ignoreTagsConfig).Map()); err != nil {
+		return diag.Errorf("error setting tags: %s", err)
+	}
+
+	d.SetId(fmt.Sprintf("%s/%s", id, indexId))
+
+	return nil
 }
