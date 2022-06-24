f747f1cacae41c8c43cb33f6e8c2967e593b87e9
GlennChia <glennchia7@gmail.com>
2022-06-24 07:04:43 +0000

    feat(kendra): thesaurus datasrc schema

---
+++ internal/service/kendra/thesaurus_data_source.go
@@ -0,0 +1,96 @@
+package kendra
+
+import (
+	"regexp"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
+)
+
+func DataSourceThesaurus() *schema.Resource {
+	return &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"arn": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"created_at": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"description": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"error_message": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"file_size_bytes": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"index_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ValidateFunc: validation.StringMatch(
+					regexp.MustCompile(`[a-zA-Z0-9][a-zA-Z0-9-]{35}`),
+					"Starts with an alphanumeric character. Subsequently, can contain alphanumeric characters and hyphens. Fixed length of 36.",
+				),
+			},
+			"name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"role_arn": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"source_s3_path": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"bucket": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"key": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"status": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"synonym_rule_count": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"term_count": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"thesaurus_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ValidateFunc: validation.All(
+					validation.StringLenBetween(1, 100),
+					validation.StringMatch(
+						regexp.MustCompile(`[a-zA-Z0-9][a-zA-Z0-9_-]*`),
+						"Starts with an alphanumeric character. Subsequently, can contain alphanumeric characters and hyphens.",
+					),
+				),
+			},
+			"updated_at": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"tags": tftags.TagsSchemaComputed(),
+		},
+	}
+}
