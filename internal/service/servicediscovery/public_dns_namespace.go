5f387d6e2915be8c80cd2c11a6a52fff9a49f317
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-24 15:11:46 -0400

    r/aws_service_discovery_public_dns_namespace: Use 'FindNamespaceByID'.
    Acceptance test output:
    
    % make testacc TESTARGS='-run=TestAccServiceDiscoveryPublicDNSNamespace_' PKG=servicediscovery ACCTEST_PARALLELISM=2
    ==> Checking that code complies with gofmt requirements...
    TF_ACC=1 go test ./internal/service/servicediscovery/... -v -count 1 -parallel 2  -run=TestAccServiceDiscoveryPublicDNSNamespace_ -timeout 180m
    === RUN   TestAccServiceDiscoveryPublicDNSNamespace_basic
    === PAUSE TestAccServiceDiscoveryPublicDNSNamespace_basic
    === RUN   TestAccServiceDiscoveryPublicDNSNamespace_disappears
    === PAUSE TestAccServiceDiscoveryPublicDNSNamespace_disappears
    === RUN   TestAccServiceDiscoveryPublicDNSNamespace_description
    === PAUSE TestAccServiceDiscoveryPublicDNSNamespace_description
    === RUN   TestAccServiceDiscoveryPublicDNSNamespace_tags
    === PAUSE TestAccServiceDiscoveryPublicDNSNamespace_tags
    === CONT  TestAccServiceDiscoveryPublicDNSNamespace_basic
    === CONT  TestAccServiceDiscoveryPublicDNSNamespace_description
    --- PASS: TestAccServiceDiscoveryPublicDNSNamespace_description (106.83s)
    === CONT  TestAccServiceDiscoveryPublicDNSNamespace_tags
    --- PASS: TestAccServiceDiscoveryPublicDNSNamespace_basic (109.66s)
    === CONT  TestAccServiceDiscoveryPublicDNSNamespace_disappears
    --- PASS: TestAccServiceDiscoveryPublicDNSNamespace_disappears (102.40s)
    --- PASS: TestAccServiceDiscoveryPublicDNSNamespace_tags (133.84s)
    PASS
    ok  	github.com/hashicorp/terraform-provider-aws/internal/service/servicediscovery	247.485s

--- internal/service/servicediscovery/public_dns_namespace.go
+++ internal/service/servicediscovery/public_dns_namespace.go
@@ -2,14 +2,15 @@ package servicediscovery
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/servicediscovery"
-	"github.com/hashicorp/aws-sdk-go-base/v2/awsv1shim/v2/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
+	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
@@ -25,27 +26,27 @@ func ResourcePublicDNSNamespace() *schema.Resource {
 		},
 
 		Schema: map[string]*schema.Schema{
-			"name": {
-				Type:         schema.TypeString,
-				Required:     true,
-				ForceNew:     true,
-				ValidateFunc: validNamespaceName,
+			"arn": {
+				Type:     schema.TypeString,
+				Computed: true,
 			},
 			"description": {
 				Type:     schema.TypeString,
 				Optional: true,
 				ForceNew: true,
 			},
-			"tags":     tftags.TagsSchema(),
-			"tags_all": tftags.TagsSchemaComputed(),
-			"arn": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
 			"hosted_zone": {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+			"name": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validNamespaceName,
+			},
+			"tags":     tftags.TagsSchema(),
+			"tags_all": tftags.TagsSchemaComputed(),
 		},
 
 		CustomizeDiff: verify.SetTagsDiff,
@@ -71,26 +72,23 @@ func resourcePublicDNSNamespaceCreate(d *schema.ResourceData, meta interface{}) 
 		input.Tags = Tags(tags.IgnoreAWS())
 	}
 
+	log.Printf("[DEBUG] Creating Service Discovery Public DNS Namespace: %s", input)
 	output, err := conn.CreatePublicDnsNamespace(input)
 
 	if err != nil {
-		return fmt.Errorf("error creating Service Discovery Public DNS Namespace (%s): %w", name, err)
-	}
-
-	if output == nil || output.OperationId == nil {
-		return fmt.Errorf("error creating Service Discovery Public DNS Namespace (%s): creation response missing Operation ID", name)
+		return fmt.Errorf("creating Service Discovery Public DNS Namespace (%s): %w", name, err)
 	}
 
 	operation, err := WaitOperationSuccess(conn, aws.StringValue(output.OperationId))
 
 	if err != nil {
-		return fmt.Errorf("error waiting for Service Discovery Public DNS Namespace (%s) creation: %w", name, err)
+		return fmt.Errorf("waiting for Service Discovery Public DNS Namespace (%s) create: %w", name, err)
 	}
 
 	namespaceID, ok := operation.Targets[servicediscovery.OperationTargetTypeNamespace]
 
 	if !ok {
-		return fmt.Errorf("error creating Service Discovery Public DNS Namespace (%s): operation response missing Namespace ID", name)
+		return fmt.Errorf("creating Service Discovery Public DNS Namespace (%s): operation response missing Namespace ID", name)
 	}
 
 	d.SetId(aws.StringValue(namespaceID))
@@ -103,42 +101,43 @@ func resourcePublicDNSNamespaceRead(d *schema.ResourceData, meta interface{}) er
 	defaultTagsConfig := meta.(*conns.AWSClient).DefaultTagsConfig
 	ignoreTagsConfig := meta.(*conns.AWSClient).IgnoreTagsConfig
 
-	input := &servicediscovery.GetNamespaceInput{
-		Id: aws.String(d.Id()),
+	ns, err := FindNamespaceByID(conn, d.Id())
+
+	if !d.IsNewResource() && tfresource.NotFound(err) {
+		log.Printf("[WARN] Service Discovery Public DNS Namespace %s not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
 	}
 
-	resp, err := conn.GetNamespace(input)
 	if err != nil {
-		if tfawserr.ErrCodeEquals(err, servicediscovery.ErrCodeNamespaceNotFound) {
-			d.SetId("")
-			return nil
-		}
-		return err
+		return fmt.Errorf("reading Service Discovery Public DNS Namespace (%s): %w", d.Id(), err)
 	}
 
-	arn := aws.StringValue(resp.Namespace.Arn)
-	d.Set("name", resp.Namespace.Name)
-	d.Set("description", resp.Namespace.Description)
+	arn := aws.StringValue(ns.Arn)
 	d.Set("arn", arn)
-	if resp.Namespace.Properties != nil {
-		d.Set("hosted_zone", resp.Namespace.Properties.DnsProperties.HostedZoneId)
+	d.Set("description", ns.Description)
+	if ns.Properties != nil && ns.Properties.DnsProperties != nil {
+		d.Set("hosted_zone", ns.Properties.DnsProperties.HostedZoneId)
+	} else {
+		d.Set("hosted_zone", nil)
 	}
+	d.Set("name", ns.Name)
 
 	tags, err := ListTags(conn, arn)
 
 	if err != nil {
-		return fmt.Errorf("error listing tags for resource (%s): %s", arn, err)
+		return fmt.Errorf("listing tags for Service Discovery Public DNS Namespace (%s): %w", arn, err)
 	}
 
 	tags = tags.IgnoreAWS().IgnoreConfig(ignoreTagsConfig)
 
 	//lintignore:AWSR002
 	if err := d.Set("tags", tags.RemoveDefaultConfig(defaultTagsConfig).Map()); err != nil {
-		return fmt.Errorf("error setting tags: %w", err)
+		return fmt.Errorf("setting tags: %w", err)
 	}
 
 	if err := d.Set("tags_all", tags.Map()); err != nil {
-		return fmt.Errorf("error setting tags_all: %w", err)
+		return fmt.Errorf("setting tags_all: %w", err)
 	}
 
 	return nil
@@ -149,30 +148,30 @@ func resourcePublicDNSNamespaceUpdate(d *schema.ResourceData, meta interface{}) 
 
 	if d.HasChange("tags_all") {
 		o, n := d.GetChange("tags_all")
+
 		if err := UpdateTags(conn, d.Get("arn").(string), o, n); err != nil {
-			return fmt.Errorf("error updating Service Discovery Public DNS Namespace (%s) tags: %s", d.Id(), err)
+			return fmt.Errorf("updating Service Discovery Public DNS Namespace (%s) tags: %s", d.Id(), err)
 		}
 	}
 
-	return resourceHTTPNamespaceRead(d, meta)
+	return resourcePublicDNSNamespaceRead(d, meta)
 }
 
 func resourcePublicDNSNamespaceDelete(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*conns.AWSClient).ServiceDiscoveryConn
 
-	input := &servicediscovery.DeleteNamespaceInput{
+	log.Printf("[INFO] Deleting Service Discovery Public DNS Namespace: %s", d.Id())
+	output, err := conn.DeleteNamespace(&servicediscovery.DeleteNamespaceInput{
 		Id: aws.String(d.Id()),
-	}
-
-	output, err := conn.DeleteNamespace(input)
+	})
 
 	if err != nil {
-		return fmt.Errorf("error deleting Service Discovery Public DNS Namespace (%s): %w", d.Id(), err)
+		return fmt.Errorf("deleting Service Discovery Public DNS Namespace (%s): %w", d.Id(), err)
 	}
 
 	if output != nil && output.OperationId != nil {
 		if _, err := WaitOperationSuccess(conn, aws.StringValue(output.OperationId)); err != nil {
-			return fmt.Errorf("error waiting for Service Discovery Public DNS Namespace (%s) deletion: %w", d.Id(), err)
+			return fmt.Errorf("waiting for Service Discovery Public DNS Namespace (%s) delete: %w", d.Id(), err)
 		}
 	}
 
