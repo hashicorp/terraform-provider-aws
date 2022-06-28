aa8700bc1c83027abc0d61c0017c259f9172c351
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-28 15:29:18 -0400

    SDKv2 provider is one of (right now the only) the muxed providers.

--- internal/provider/factory.go
+++ internal/provider/factory.go
@@ -5,13 +5,13 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
 	"github.com/hashicorp/terraform-plugin-mux/tf5muxserver"
-	// "github.com/hashicorp/terraform-provider-aws/internal/tf5provider"
+	"github.com/hashicorp/terraform-provider-aws/internal/provider/sdkv2"
 )
 
 // ProtoV5ProviderServerFactory returns a terraform-plugin-go protocol v5 provider factory function.
 func ProtoV5ProviderServerFactory(ctx context.Context) (func() tfprotov5.ProviderServer, error) {
 	providers := []func() tfprotov5.ProviderServer{
-		// tf5provider.Provider().GRPCProvider,
+		sdkv2.Provider().GRPCProvider,
 	}
 
 	muxServer, err := tf5muxserver.NewMuxServer(ctx, providers...)
