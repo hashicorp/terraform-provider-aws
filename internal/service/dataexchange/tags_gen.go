ab13f2dc375000f130ee106b3a143ef2adc27206
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 14:32:44 -0400

    gen/tags: Use interface type as generated AWS Go SDK v1 client type for dataexchange.

--- internal/service/dataexchange/tags_gen.go
+++ internal/service/dataexchange/tags_gen.go
@@ -6,13 +6,14 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dataexchange"
+	"github.com/aws/aws-sdk-go/service/dataexchange/dataexchangeiface"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 // ListTags lists dataexchange service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func ListTags(conn *dataexchange.DataExchange, identifier string) (tftags.KeyValueTags, error) {
+func ListTags(conn dataexchangeiface.DataExchangeAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &dataexchange.ListTagsForResourceInput{
 		ResourceArn: aws.String(identifier),
 	}
@@ -41,7 +42,7 @@ func KeyValueTags(tags map[string]*string) tftags.KeyValueTags {
 // UpdateTags updates dataexchange service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn *dataexchange.DataExchange, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn dataexchangeiface.DataExchangeAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
