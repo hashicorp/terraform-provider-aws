7b6d3642b5cb8acd2622a50fd593a8c55354f521
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 15:07:17 -0400

    gen/tags: Generate 'WithContext' variants of AWS SDK for Go v1 tagging functions for directconnect.

--- internal/service/directconnect/tags_gen.go
+++ internal/service/directconnect/tags_gen.go
@@ -2,6 +2,7 @@
 package directconnect
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -14,11 +15,15 @@ import (
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
 func ListTags(conn directconnectiface.DirectConnectAPI, identifier string) (tftags.KeyValueTags, error) {
+	return ListTagsWithContext(context.Background(), conn, identifier)
+}
+
+func ListTagsWithContext(ctx context.Context, conn directconnectiface.DirectConnectAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &directconnect.DescribeTagsInput{
 		ResourceArns: aws.StringSlice([]string{identifier}),
 	}
 
-	output, err := conn.DescribeTags(input)
+	output, err := conn.DescribeTagsWithContext(ctx, input)
 
 	if err != nil {
 		return tftags.New(nil), err
@@ -59,7 +64,10 @@ func KeyValueTags(tags []*directconnect.Tag) tftags.KeyValueTags {
 // UpdateTags updates directconnect service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn directconnectiface.DirectConnectAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn directconnectiface.DirectConnectAPI, identifier string, oldTags interface{}, newTags interface{}) error {
+	return UpdateTagsWithContext(context.Background(), conn, identifier, oldTags, newTags)
+}
+func UpdateTagsWithContext(ctx context.Context, conn directconnectiface.DirectConnectAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
@@ -69,7 +77,7 @@ func UpdateTags(conn directconnectiface.DirectConnectAPI, identifier string, old
 			TagKeys:     aws.StringSlice(removedTags.IgnoreAWS().Keys()),
 		}
 
-		_, err := conn.UntagResource(input)
+		_, err := conn.UntagResourceWithContext(ctx, input)
 
 		if err != nil {
 			return fmt.Errorf("error untagging resource (%s): %w", identifier, err)
@@ -82,7 +90,7 @@ func UpdateTags(conn directconnectiface.DirectConnectAPI, identifier string, old
 			Tags:        Tags(updatedTags.IgnoreAWS()),
 		}
 
-		_, err := conn.TagResource(input)
+		_, err := conn.TagResourceWithContext(ctx, input)
 
 		if err != nil {
 			return fmt.Errorf("error tagging resource (%s): %w", identifier, err)
