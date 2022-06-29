ddb1e27dd949a4ac82dd15c695512a383b907277
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 14:32:42 -0400

    gen/tags: Use interface type as generated AWS Go SDK v1 client type for appflow.

--- internal/service/appflow/tags_gen.go
+++ internal/service/appflow/tags_gen.go
@@ -6,13 +6,14 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/appflow"
+	"github.com/aws/aws-sdk-go/service/appflow/appflowiface"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 // ListTags lists appflow service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func ListTags(conn *appflow.Appflow, identifier string) (tftags.KeyValueTags, error) {
+func ListTags(conn appflowiface.AppflowAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &appflow.ListTagsForResourceInput{
 		ResourceArn: aws.String(identifier),
 	}
@@ -41,7 +42,7 @@ func KeyValueTags(tags map[string]*string) tftags.KeyValueTags {
 // UpdateTags updates appflow service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn *appflow.Appflow, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn appflowiface.AppflowAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
