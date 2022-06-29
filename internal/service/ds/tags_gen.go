c15747757350722beb4a629ca9a8cb21d8220220
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 14:32:45 -0400

    gen/tags: Use interface type as generated AWS Go SDK v1 client type for ds.

--- internal/service/ds/tags_gen.go
+++ internal/service/ds/tags_gen.go
@@ -6,13 +6,14 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/directoryservice"
+	"github.com/aws/aws-sdk-go/service/directoryservice/directoryserviceiface"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 // ListTags lists ds service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func ListTags(conn *directoryservice.DirectoryService, identifier string) (tftags.KeyValueTags, error) {
+func ListTags(conn directoryserviceiface.DirectoryServiceAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &directoryservice.ListTagsForResourceInput{
 		ResourceId: aws.String(identifier),
 	}
@@ -58,7 +59,7 @@ func KeyValueTags(tags []*directoryservice.Tag) tftags.KeyValueTags {
 // UpdateTags updates ds service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn *directoryservice.DirectoryService, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn directoryserviceiface.DirectoryServiceAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
