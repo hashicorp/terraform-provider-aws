8d2e13e53e36f31521840491b5a22c7e547a00e7
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 14:32:46 -0400

    gen/tags: Use interface type as generated AWS Go SDK v1 client type for elbv2.

--- internal/service/elbv2/tags_gen.go
+++ internal/service/elbv2/tags_gen.go
@@ -6,13 +6,14 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/elbv2"
+	"github.com/aws/aws-sdk-go/service/elbv2/elbv2iface"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 // ListTags lists elbv2 service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func ListTags(conn *elbv2.ELBV2, identifier string) (tftags.KeyValueTags, error) {
+func ListTags(conn elbv2iface.ELBV2API, identifier string) (tftags.KeyValueTags, error) {
 	input := &elbv2.DescribeTagsInput{
 		ResourceArns: aws.StringSlice([]string{identifier}),
 	}
@@ -58,7 +59,7 @@ func KeyValueTags(tags []*elbv2.Tag) tftags.KeyValueTags {
 // UpdateTags updates elbv2 service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn *elbv2.ELBV2, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn elbv2iface.ELBV2API, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
