faefae643182a11e84e69590640a59ecce5194b2
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 14:32:53 -0400

    gen/tags: Use interface type as generated AWS Go SDK v1 client type for sfn.

--- internal/service/sfn/tags_gen.go
+++ internal/service/sfn/tags_gen.go
@@ -6,13 +6,14 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/sfn"
+	"github.com/aws/aws-sdk-go/service/sfn/sfniface"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 // ListTags lists sfn service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func ListTags(conn *sfn.SFN, identifier string) (tftags.KeyValueTags, error) {
+func ListTags(conn sfniface.SFNAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &sfn.ListTagsForResourceInput{
 		ResourceArn: aws.String(identifier),
 	}
@@ -58,7 +59,7 @@ func KeyValueTags(tags []*sfn.Tag) tftags.KeyValueTags {
 // UpdateTags updates sfn service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn *sfn.SFN, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn sfniface.SFNAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
