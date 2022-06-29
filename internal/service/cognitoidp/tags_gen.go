df1fd123b2ac0a44549f91c07d67121d9e71503d
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 14:32:44 -0400

    gen/tags: Use interface type as generated AWS Go SDK v1 client type for cognitoidp.

--- internal/service/cognitoidp/tags_gen.go
+++ internal/service/cognitoidp/tags_gen.go
@@ -6,13 +6,14 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
+	"github.com/aws/aws-sdk-go/service/cognitoidentityprovider/cognitoidentityprovideriface"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 // ListTags lists cognitoidp service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func ListTags(conn *cognitoidentityprovider.CognitoIdentityProvider, identifier string) (tftags.KeyValueTags, error) {
+func ListTags(conn cognitoidentityprovideriface.CognitoIdentityProviderAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &cognitoidentityprovider.ListTagsForResourceInput{
 		ResourceArn: aws.String(identifier),
 	}
@@ -41,7 +42,7 @@ func KeyValueTags(tags map[string]*string) tftags.KeyValueTags {
 // UpdateTags updates cognitoidp service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn *cognitoidentityprovider.CognitoIdentityProvider, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn cognitoidentityprovideriface.CognitoIdentityProviderAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
