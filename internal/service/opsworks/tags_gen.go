8b6ed50e56a4bf8c74399502a47e1f248906d048
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 14:32:50 -0400

    gen/tags: Use interface type as generated AWS Go SDK v1 client type for opsworks.

--- internal/service/opsworks/tags_gen.go
+++ internal/service/opsworks/tags_gen.go
@@ -6,13 +6,14 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/opsworks"
+	"github.com/aws/aws-sdk-go/service/opsworks/opsworksiface"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 // ListTags lists opsworks service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func ListTags(conn *opsworks.OpsWorks, identifier string) (tftags.KeyValueTags, error) {
+func ListTags(conn opsworksiface.OpsWorksAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &opsworks.ListTagsInput{
 		ResourceArn: aws.String(identifier),
 	}
@@ -41,7 +42,7 @@ func KeyValueTags(tags map[string]*string) tftags.KeyValueTags {
 // UpdateTags updates opsworks service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn *opsworks.OpsWorks, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn opsworksiface.OpsWorksAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
