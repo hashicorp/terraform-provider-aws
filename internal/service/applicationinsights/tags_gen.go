010c01e5e3423e3435bfce3c6fb47a6a6edd34f3
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 14:32:42 -0400

    gen/tags: Use interface type as generated AWS Go SDK v1 client type for applicationinsights.

--- internal/service/applicationinsights/tags_gen.go
+++ internal/service/applicationinsights/tags_gen.go
@@ -6,13 +6,14 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/applicationinsights"
+	"github.com/aws/aws-sdk-go/service/applicationinsights/applicationinsightsiface"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 // ListTags lists applicationinsights service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func ListTags(conn *applicationinsights.ApplicationInsights, identifier string) (tftags.KeyValueTags, error) {
+func ListTags(conn applicationinsightsiface.ApplicationInsightsAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &applicationinsights.ListTagsForResourceInput{
 		ResourceARN: aws.String(identifier),
 	}
@@ -58,7 +59,7 @@ func KeyValueTags(tags []*applicationinsights.Tag) tftags.KeyValueTags {
 // UpdateTags updates applicationinsights service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn *applicationinsights.ApplicationInsights, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn applicationinsightsiface.ApplicationInsightsAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
