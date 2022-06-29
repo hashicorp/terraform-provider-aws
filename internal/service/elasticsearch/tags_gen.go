ed4755a6e5caeabe83b951919a1d982ce425b5a2
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 15:07:18 -0400

    gen/tags: Generate 'WithContext' variants of AWS SDK for Go v1 tagging functions for elasticsearch.

--- internal/service/elasticsearch/tags_gen.go
+++ internal/service/elasticsearch/tags_gen.go
@@ -2,6 +2,7 @@
 package elasticsearch
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -14,11 +15,15 @@ import (
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
 func ListTags(conn elasticsearchserviceiface.ElasticsearchServiceAPI, identifier string) (tftags.KeyValueTags, error) {
+	return ListTagsWithContext(context.Background(), conn, identifier)
+}
+
+func ListTagsWithContext(ctx context.Context, conn elasticsearchserviceiface.ElasticsearchServiceAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &elasticsearchservice.ListTagsInput{
 		ARN: aws.String(identifier),
 	}
 
-	output, err := conn.ListTags(input)
+	output, err := conn.ListTagsWithContext(ctx, input)
 
 	if err != nil {
 		return tftags.New(nil), err
@@ -59,7 +64,10 @@ func KeyValueTags(tags []*elasticsearchservice.Tag) tftags.KeyValueTags {
 // UpdateTags updates elasticsearch service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn elasticsearchserviceiface.ElasticsearchServiceAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn elasticsearchserviceiface.ElasticsearchServiceAPI, identifier string, oldTags interface{}, newTags interface{}) error {
+	return UpdateTagsWithContext(context.Background(), conn, identifier, oldTags, newTags)
+}
+func UpdateTagsWithContext(ctx context.Context, conn elasticsearchserviceiface.ElasticsearchServiceAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
@@ -69,7 +77,7 @@ func UpdateTags(conn elasticsearchserviceiface.ElasticsearchServiceAPI, identifi
 			TagKeys: aws.StringSlice(removedTags.IgnoreAWS().Keys()),
 		}
 
-		_, err := conn.RemoveTags(input)
+		_, err := conn.RemoveTagsWithContext(ctx, input)
 
 		if err != nil {
 			return fmt.Errorf("error untagging resource (%s): %w", identifier, err)
@@ -82,7 +90,7 @@ func UpdateTags(conn elasticsearchserviceiface.ElasticsearchServiceAPI, identifi
 			TagList: Tags(updatedTags.IgnoreAWS()),
 		}
 
-		_, err := conn.AddTags(input)
+		_, err := conn.AddTagsWithContext(ctx, input)
 
 		if err != nil {
 			return fmt.Errorf("error tagging resource (%s): %w", identifier, err)
