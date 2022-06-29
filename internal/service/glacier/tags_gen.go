8211d19b25481a32b787f4038975f7ec6829f67f
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 14:32:47 -0400

    gen/tags: Use interface type as generated AWS Go SDK v1 client type for glacier.

--- internal/service/glacier/tags_gen.go
+++ internal/service/glacier/tags_gen.go
@@ -6,13 +6,14 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/glacier"
+	"github.com/aws/aws-sdk-go/service/glacier/glacieriface"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 // ListTags lists glacier service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func ListTags(conn *glacier.Glacier, identifier string) (tftags.KeyValueTags, error) {
+func ListTags(conn glacieriface.GlacierAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &glacier.ListTagsForVaultInput{
 		VaultName: aws.String(identifier),
 	}
@@ -41,7 +42,7 @@ func KeyValueTags(tags map[string]*string) tftags.KeyValueTags {
 // UpdateTags updates glacier service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn *glacier.Glacier, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn glacieriface.GlacierAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
