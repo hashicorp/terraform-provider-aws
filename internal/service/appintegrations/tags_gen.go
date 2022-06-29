513152a1fd0214e11647e4236b0bdd57d909498d
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 15:07:14 -0400

    gen/tags: Generate 'WithContext' variants of AWS SDK for Go v1 tagging functions for appintegrations.

--- internal/service/appintegrations/tags_gen.go
+++ internal/service/appintegrations/tags_gen.go
@@ -2,6 +2,7 @@
 package appintegrations
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -25,7 +26,10 @@ func KeyValueTags(tags map[string]*string) tftags.KeyValueTags {
 // UpdateTags updates appintegrations service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn appintegrationsserviceiface.AppIntegrationsServiceAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn appintegrationsserviceiface.AppIntegrationsServiceAPI, identifier string, oldTags interface{}, newTags interface{}) error {
+	return UpdateTagsWithContext(context.Background(), conn, identifier, oldTags, newTags)
+}
+func UpdateTagsWithContext(ctx context.Context, conn appintegrationsserviceiface.AppIntegrationsServiceAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
@@ -35,7 +39,7 @@ func UpdateTags(conn appintegrationsserviceiface.AppIntegrationsServiceAPI, iden
 			TagKeys:     aws.StringSlice(removedTags.IgnoreAWS().Keys()),
 		}
 
-		_, err := conn.UntagResource(input)
+		_, err := conn.UntagResourceWithContext(ctx, input)
 
 		if err != nil {
 			return fmt.Errorf("error untagging resource (%s): %w", identifier, err)
@@ -48,7 +52,7 @@ func UpdateTags(conn appintegrationsserviceiface.AppIntegrationsServiceAPI, iden
 			Tags:        Tags(updatedTags.IgnoreAWS()),
 		}
 
-		_, err := conn.TagResource(input)
+		_, err := conn.TagResourceWithContext(ctx, input)
 
 		if err != nil {
 			return fmt.Errorf("error tagging resource (%s): %w", identifier, err)
