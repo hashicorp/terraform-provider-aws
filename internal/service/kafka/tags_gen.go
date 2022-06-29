51119161fd237a4e68bf6994281872aa746242a3
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 14:32:48 -0400

    gen/tags: Use interface type as generated AWS Go SDK v1 client type for kafka.

--- internal/service/kafka/tags_gen.go
+++ internal/service/kafka/tags_gen.go
@@ -6,13 +6,14 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/kafka"
+	"github.com/aws/aws-sdk-go/service/kafka/kafkaiface"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 // ListTags lists kafka service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func ListTags(conn *kafka.Kafka, identifier string) (tftags.KeyValueTags, error) {
+func ListTags(conn kafkaiface.KafkaAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &kafka.ListTagsForResourceInput{
 		ResourceArn: aws.String(identifier),
 	}
@@ -41,7 +42,7 @@ func KeyValueTags(tags map[string]*string) tftags.KeyValueTags {
 // UpdateTags updates kafka service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn *kafka.Kafka, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn kafkaiface.KafkaAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
