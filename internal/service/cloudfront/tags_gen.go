4f7da858de4af44f9a65b931f4bbc601557210aa
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 14:32:43 -0400

    gen/tags: Use interface type as generated AWS Go SDK v1 client type for cloudfront.

--- internal/service/cloudfront/tags_gen.go
+++ internal/service/cloudfront/tags_gen.go
@@ -6,13 +6,14 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/cloudfront"
+	"github.com/aws/aws-sdk-go/service/cloudfront/cloudfrontiface"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 // ListTags lists cloudfront service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func ListTags(conn *cloudfront.CloudFront, identifier string) (tftags.KeyValueTags, error) {
+func ListTags(conn cloudfrontiface.CloudFrontAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &cloudfront.ListTagsForResourceInput{
 		Resource: aws.String(identifier),
 	}
@@ -58,7 +59,7 @@ func KeyValueTags(tags []*cloudfront.Tag) tftags.KeyValueTags {
 // UpdateTags updates cloudfront service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn *cloudfront.CloudFront, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn cloudfrontiface.CloudFrontAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
