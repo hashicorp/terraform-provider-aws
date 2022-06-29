c91564d2b1e683a7ac62612a954a7846bb43146c
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 14:32:52 -0400

    gen/tags: Use interface type as generated AWS Go SDK v1 client type for securityhub.

--- internal/service/securityhub/tags_gen.go
+++ internal/service/securityhub/tags_gen.go
@@ -6,13 +6,14 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/securityhub"
+	"github.com/aws/aws-sdk-go/service/securityhub/securityhubiface"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 // ListTags lists securityhub service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func ListTags(conn *securityhub.SecurityHub, identifier string) (tftags.KeyValueTags, error) {
+func ListTags(conn securityhubiface.SecurityHubAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &securityhub.ListTagsForResourceInput{
 		ResourceArn: aws.String(identifier),
 	}
@@ -41,7 +42,7 @@ func KeyValueTags(tags map[string]*string) tftags.KeyValueTags {
 // UpdateTags updates securityhub service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn *securityhub.SecurityHub, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn securityhubiface.SecurityHubAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
