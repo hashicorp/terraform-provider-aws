5a243ac51790a26c2f611597f9b97597f8d1712f
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 14:32:53 -0400

    gen/tags: Use interface type as generated AWS Go SDK v1 client type for storagegateway.

--- internal/service/storagegateway/tags_gen.go
+++ internal/service/storagegateway/tags_gen.go
@@ -6,13 +6,14 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/storagegateway"
+	"github.com/aws/aws-sdk-go/service/storagegateway/storagegatewayiface"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 // ListTags lists storagegateway service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func ListTags(conn *storagegateway.StorageGateway, identifier string) (tftags.KeyValueTags, error) {
+func ListTags(conn storagegatewayiface.StorageGatewayAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &storagegateway.ListTagsForResourceInput{
 		ResourceARN: aws.String(identifier),
 	}
@@ -58,7 +59,7 @@ func KeyValueTags(tags []*storagegateway.Tag) tftags.KeyValueTags {
 // UpdateTags updates storagegateway service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn *storagegateway.StorageGateway, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn storagegatewayiface.StorageGatewayAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
