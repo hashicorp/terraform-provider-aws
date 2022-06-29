38a4653250f1a6c1f9010185fcaccfe6e7c0c39f
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 14:32:47 -0400

    gen/tags: Use interface type as generated AWS Go SDK v1 client type for imagebuilder.

--- internal/service/imagebuilder/tags_gen.go
+++ internal/service/imagebuilder/tags_gen.go
@@ -6,13 +6,14 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/imagebuilder"
+	"github.com/aws/aws-sdk-go/service/imagebuilder/imagebuilderiface"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 // ListTags lists imagebuilder service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func ListTags(conn *imagebuilder.Imagebuilder, identifier string) (tftags.KeyValueTags, error) {
+func ListTags(conn imagebuilderiface.ImagebuilderAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &imagebuilder.ListTagsForResourceInput{
 		ResourceArn: aws.String(identifier),
 	}
@@ -41,7 +42,7 @@ func KeyValueTags(tags map[string]*string) tftags.KeyValueTags {
 // UpdateTags updates imagebuilder service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn *imagebuilder.Imagebuilder, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn imagebuilderiface.ImagebuilderAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
