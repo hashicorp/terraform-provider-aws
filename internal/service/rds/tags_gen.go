08251da7e9de6eb94eaefcdbe678ece81445dc16
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 14:32:51 -0400

    gen/tags: Use interface type as generated AWS Go SDK v1 client type for rds.

--- internal/service/rds/tags_gen.go
+++ internal/service/rds/tags_gen.go
@@ -6,13 +6,14 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/rds"
+	"github.com/aws/aws-sdk-go/service/rds/rdsiface"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 // ListTags lists rds service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func ListTags(conn *rds.RDS, identifier string) (tftags.KeyValueTags, error) {
+func ListTags(conn rdsiface.RDSAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &rds.ListTagsForResourceInput{
 		ResourceName: aws.String(identifier),
 	}
@@ -58,7 +59,7 @@ func KeyValueTags(tags []*rds.Tag) tftags.KeyValueTags {
 // UpdateTags updates rds service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn *rds.RDS, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn rdsiface.RDSAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
