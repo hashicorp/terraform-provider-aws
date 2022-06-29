e35f9451fecaa2382f600958fe59cc98b56a0d3b
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 15:07:17 -0400

    gen/tags: Generate 'WithContext' variants of AWS SDK for Go v1 tagging functions for dms.

--- internal/service/dms/tags_gen.go
+++ internal/service/dms/tags_gen.go
@@ -2,6 +2,7 @@
 package dms
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -14,11 +15,15 @@ import (
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
 func ListTags(conn databasemigrationserviceiface.DatabaseMigrationServiceAPI, identifier string) (tftags.KeyValueTags, error) {
+	return ListTagsWithContext(context.Background(), conn, identifier)
+}
+
+func ListTagsWithContext(ctx context.Context, conn databasemigrationserviceiface.DatabaseMigrationServiceAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &databasemigrationservice.ListTagsForResourceInput{
 		ResourceArn: aws.String(identifier),
 	}
 
-	output, err := conn.ListTagsForResource(input)
+	output, err := conn.ListTagsForResourceWithContext(ctx, input)
 
 	if err != nil {
 		return tftags.New(nil), err
@@ -59,7 +64,10 @@ func KeyValueTags(tags []*databasemigrationservice.Tag) tftags.KeyValueTags {
 // UpdateTags updates dms service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn databasemigrationserviceiface.DatabaseMigrationServiceAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn databasemigrationserviceiface.DatabaseMigrationServiceAPI, identifier string, oldTags interface{}, newTags interface{}) error {
+	return UpdateTagsWithContext(context.Background(), conn, identifier, oldTags, newTags)
+}
+func UpdateTagsWithContext(ctx context.Context, conn databasemigrationserviceiface.DatabaseMigrationServiceAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
@@ -69,7 +77,7 @@ func UpdateTags(conn databasemigrationserviceiface.DatabaseMigrationServiceAPI, 
 			TagKeys:     aws.StringSlice(removedTags.IgnoreAWS().Keys()),
 		}
 
-		_, err := conn.RemoveTagsFromResource(input)
+		_, err := conn.RemoveTagsFromResourceWithContext(ctx, input)
 
 		if err != nil {
 			return fmt.Errorf("error untagging resource (%s): %w", identifier, err)
@@ -82,7 +90,7 @@ func UpdateTags(conn databasemigrationserviceiface.DatabaseMigrationServiceAPI, 
 			Tags:        Tags(updatedTags.IgnoreAWS()),
 		}
 
-		_, err := conn.AddTagsToResource(input)
+		_, err := conn.AddTagsToResourceWithContext(ctx, input)
 
 		if err != nil {
 			return fmt.Errorf("error tagging resource (%s): %w", identifier, err)
