7e76e737f581d39fd2f849de195595223572ff9c
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 14:32:48 -0400

    gen/tags: Use interface type as generated AWS Go SDK v1 client type for keyspaces.

--- internal/service/keyspaces/tags_gen.go
+++ internal/service/keyspaces/tags_gen.go
@@ -6,13 +6,14 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/keyspaces"
+	"github.com/aws/aws-sdk-go/service/keyspaces/keyspacesiface"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 // ListTags lists keyspaces service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func ListTags(conn *keyspaces.Keyspaces, identifier string) (tftags.KeyValueTags, error) {
+func ListTags(conn keyspacesiface.KeyspacesAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &keyspaces.ListTagsForResourceInput{
 		ResourceArn: aws.String(identifier),
 	}
@@ -58,7 +59,7 @@ func KeyValueTags(tags []*keyspaces.Tag) tftags.KeyValueTags {
 // UpdateTags updates keyspaces service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn *keyspaces.Keyspaces, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn keyspacesiface.KeyspacesAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
