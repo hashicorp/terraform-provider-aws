cf8d765653a0ef088a63c6ef21da17257f86389b
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 15:07:23 -0400

    gen/tags: Generate 'WithContext' variants of AWS SDK for Go v1 tagging functions for ram.

--- internal/service/ram/tags_gen.go
+++ internal/service/ram/tags_gen.go
@@ -2,6 +2,7 @@
 package ram
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -42,7 +43,10 @@ func KeyValueTags(tags []*ram.Tag) tftags.KeyValueTags {
 // UpdateTags updates ram service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn ramiface.RAMAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn ramiface.RAMAPI, identifier string, oldTags interface{}, newTags interface{}) error {
+	return UpdateTagsWithContext(context.Background(), conn, identifier, oldTags, newTags)
+}
+func UpdateTagsWithContext(ctx context.Context, conn ramiface.RAMAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
@@ -52,7 +56,7 @@ func UpdateTags(conn ramiface.RAMAPI, identifier string, oldTagsMap interface{},
 			TagKeys:          aws.StringSlice(removedTags.IgnoreAWS().Keys()),
 		}
 
-		_, err := conn.UntagResource(input)
+		_, err := conn.UntagResourceWithContext(ctx, input)
 
 		if err != nil {
 			return fmt.Errorf("error untagging resource (%s): %w", identifier, err)
@@ -65,7 +69,7 @@ func UpdateTags(conn ramiface.RAMAPI, identifier string, oldTagsMap interface{},
 			Tags:             Tags(updatedTags.IgnoreAWS()),
 		}
 
-		_, err := conn.TagResource(input)
+		_, err := conn.TagResourceWithContext(ctx, input)
 
 		if err != nil {
 			return fmt.Errorf("error tagging resource (%s): %w", identifier, err)
