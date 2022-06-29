9aaf52b41edf5020047fdddda4d43cf5f9328247
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 14:32:42 -0400

    gen/tags: Use interface type as generated AWS Go SDK v1 client type for athena.

--- internal/service/athena/tags_gen.go
+++ internal/service/athena/tags_gen.go
@@ -6,13 +6,14 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/athena"
+	"github.com/aws/aws-sdk-go/service/athena/athenaiface"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 // ListTags lists athena service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func ListTags(conn *athena.Athena, identifier string) (tftags.KeyValueTags, error) {
+func ListTags(conn athenaiface.AthenaAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &athena.ListTagsForResourceInput{
 		ResourceARN: aws.String(identifier),
 	}
@@ -58,7 +59,7 @@ func KeyValueTags(tags []*athena.Tag) tftags.KeyValueTags {
 // UpdateTags updates athena service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn *athena.Athena, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn athenaiface.AthenaAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
