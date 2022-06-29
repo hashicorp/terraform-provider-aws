58369b794239b0bf1896d44d37ba7d3abeaa09d0
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 14:32:45 -0400

    gen/tags: Use interface type as generated AWS Go SDK v1 client type for dlm.

--- internal/service/dlm/tags_gen.go
+++ internal/service/dlm/tags_gen.go
@@ -6,13 +6,14 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dlm"
+	"github.com/aws/aws-sdk-go/service/dlm/dlmiface"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 // ListTags lists dlm service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func ListTags(conn *dlm.DLM, identifier string) (tftags.KeyValueTags, error) {
+func ListTags(conn dlmiface.DLMAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &dlm.ListTagsForResourceInput{
 		ResourceArn: aws.String(identifier),
 	}
@@ -41,7 +42,7 @@ func KeyValueTags(tags map[string]*string) tftags.KeyValueTags {
 // UpdateTags updates dlm service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn *dlm.DLM, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn dlmiface.DLMAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
