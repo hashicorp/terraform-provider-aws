0356183e5f2686238956c406df7e52b04491edaa
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 14:32:52 -0400

    gen/tags: Use interface type as generated AWS Go SDK v1 client type for servicediscovery.

--- internal/service/servicediscovery/tags_gen.go
+++ internal/service/servicediscovery/tags_gen.go
@@ -6,13 +6,14 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/servicediscovery"
+	"github.com/aws/aws-sdk-go/service/servicediscovery/servicediscoveryiface"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 // ListTags lists servicediscovery service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func ListTags(conn *servicediscovery.ServiceDiscovery, identifier string) (tftags.KeyValueTags, error) {
+func ListTags(conn servicediscoveryiface.ServiceDiscoveryAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &servicediscovery.ListTagsForResourceInput{
 		ResourceARN: aws.String(identifier),
 	}
@@ -58,7 +59,7 @@ func KeyValueTags(tags []*servicediscovery.Tag) tftags.KeyValueTags {
 // UpdateTags updates servicediscovery service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn *servicediscovery.ServiceDiscovery, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn servicediscoveryiface.ServiceDiscoveryAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
