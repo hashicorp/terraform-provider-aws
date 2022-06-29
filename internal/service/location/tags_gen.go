b4f6b6782816829c61b61af21098be9183498700
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 14:32:49 -0400

    gen/tags: Use interface type as generated AWS Go SDK v1 client type for location.

--- internal/service/location/tags_gen.go
+++ internal/service/location/tags_gen.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/locationservice"
+	"github.com/aws/aws-sdk-go/service/locationservice/locationserviceiface"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
@@ -24,7 +25,7 @@ func KeyValueTags(tags map[string]*string) tftags.KeyValueTags {
 // UpdateTags updates location service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn *locationservice.LocationService, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn locationserviceiface.LocationServiceAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
