e0270a941de35a6e95cb2faf7dec346fcae93043
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 14:32:54 -0400

    gen/tags: Use interface type as generated AWS Go SDK v1 client type for worklink.

--- internal/service/worklink/tags_gen.go
+++ internal/service/worklink/tags_gen.go
@@ -6,13 +6,14 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/worklink"
+	"github.com/aws/aws-sdk-go/service/worklink/worklinkiface"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 // ListTags lists worklink service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func ListTags(conn *worklink.WorkLink, identifier string) (tftags.KeyValueTags, error) {
+func ListTags(conn worklinkiface.WorkLinkAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &worklink.ListTagsForResourceInput{
 		ResourceArn: aws.String(identifier),
 	}
@@ -41,7 +42,7 @@ func KeyValueTags(tags map[string]*string) tftags.KeyValueTags {
 // UpdateTags updates worklink service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn *worklink.WorkLink, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn worklinkiface.WorkLinkAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
