32c901ee7d876ea6c8e13cef3326926d928f1853
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 14:32:47 -0400

    gen/tags: Use interface type as generated AWS Go SDK v1 client type for iotanalytics.

--- internal/service/iotanalytics/tags_gen.go
+++ internal/service/iotanalytics/tags_gen.go
@@ -6,13 +6,14 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/iotanalytics"
+	"github.com/aws/aws-sdk-go/service/iotanalytics/iotanalyticsiface"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 // ListTags lists iotanalytics service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func ListTags(conn *iotanalytics.IoTAnalytics, identifier string) (tftags.KeyValueTags, error) {
+func ListTags(conn iotanalyticsiface.IoTAnalyticsAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &iotanalytics.ListTagsForResourceInput{
 		ResourceArn: aws.String(identifier),
 	}
@@ -58,7 +59,7 @@ func KeyValueTags(tags []*iotanalytics.Tag) tftags.KeyValueTags {
 // UpdateTags updates iotanalytics service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn *iotanalytics.IoTAnalytics, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn iotanalyticsiface.IoTAnalyticsAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
