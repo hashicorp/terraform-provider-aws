52b95b95b4f10459ad11490c7a3c8b084bd48ec5
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 14:32:42 -0400

    gen/tags: Use interface type as generated AWS Go SDK v1 client type for appstream.

--- internal/service/appstream/tags_gen.go
+++ internal/service/appstream/tags_gen.go
@@ -6,13 +6,14 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/appstream"
+	"github.com/aws/aws-sdk-go/service/appstream/appstreamiface"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 // ListTags lists appstream service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func ListTags(conn *appstream.AppStream, identifier string) (tftags.KeyValueTags, error) {
+func ListTags(conn appstreamiface.AppStreamAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &appstream.ListTagsForResourceInput{
 		ResourceArn: aws.String(identifier),
 	}
@@ -41,7 +42,7 @@ func KeyValueTags(tags map[string]*string) tftags.KeyValueTags {
 // UpdateTags updates appstream service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn *appstream.AppStream, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn appstreamiface.AppStreamAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
