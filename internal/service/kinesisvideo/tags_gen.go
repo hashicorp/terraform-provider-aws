7d73ccb5dd499731f271016a413c9712925855ce
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 15:07:20 -0400

    gen/tags: Generate 'WithContext' variants of AWS SDK for Go v1 tagging functions for kinesisvideo.

--- internal/service/kinesisvideo/tags_gen.go
+++ internal/service/kinesisvideo/tags_gen.go
@@ -2,6 +2,7 @@
 package kinesisvideo
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -14,11 +15,15 @@ import (
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
 func ListTags(conn kinesisvideoiface.KinesisVideoAPI, identifier string) (tftags.KeyValueTags, error) {
+	return ListTagsWithContext(context.Background(), conn, identifier)
+}
+
+func ListTagsWithContext(ctx context.Context, conn kinesisvideoiface.KinesisVideoAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &kinesisvideo.ListTagsForStreamInput{
 		StreamARN: aws.String(identifier),
 	}
 
-	output, err := conn.ListTagsForStream(input)
+	output, err := conn.ListTagsForStreamWithContext(ctx, input)
 
 	if err != nil {
 		return tftags.New(nil), err
@@ -42,7 +47,10 @@ func KeyValueTags(tags map[string]*string) tftags.KeyValueTags {
 // UpdateTags updates kinesisvideo service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn kinesisvideoiface.KinesisVideoAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn kinesisvideoiface.KinesisVideoAPI, identifier string, oldTags interface{}, newTags interface{}) error {
+	return UpdateTagsWithContext(context.Background(), conn, identifier, oldTags, newTags)
+}
+func UpdateTagsWithContext(ctx context.Context, conn kinesisvideoiface.KinesisVideoAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
@@ -52,7 +60,7 @@ func UpdateTags(conn kinesisvideoiface.KinesisVideoAPI, identifier string, oldTa
 			TagKeyList: aws.StringSlice(removedTags.IgnoreAWS().Keys()),
 		}
 
-		_, err := conn.UntagStream(input)
+		_, err := conn.UntagStreamWithContext(ctx, input)
 
 		if err != nil {
 			return fmt.Errorf("error untagging resource (%s): %w", identifier, err)
@@ -65,7 +73,7 @@ func UpdateTags(conn kinesisvideoiface.KinesisVideoAPI, identifier string, oldTa
 			Tags:      Tags(updatedTags.IgnoreAWS()),
 		}
 
-		_, err := conn.TagStream(input)
+		_, err := conn.TagStreamWithContext(ctx, input)
 
 		if err != nil {
 			return fmt.Errorf("error tagging resource (%s): %w", identifier, err)
