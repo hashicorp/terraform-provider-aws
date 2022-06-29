19fece02ad847e7d576c4eab000511df60ca2a31
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 14:32:46 -0400

    gen/tags: Use interface type as generated AWS Go SDK v1 client type for fms.

--- internal/service/fms/tags_gen.go
+++ internal/service/fms/tags_gen.go
@@ -6,13 +6,14 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/fms"
+	"github.com/aws/aws-sdk-go/service/fms/fmsiface"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 // ListTags lists fms service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func ListTags(conn *fms.FMS, identifier string) (tftags.KeyValueTags, error) {
+func ListTags(conn fmsiface.FMSAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &fms.ListTagsForResourceInput{
 		ResourceArn: aws.String(identifier),
 	}
@@ -58,7 +59,7 @@ func KeyValueTags(tags []*fms.Tag) tftags.KeyValueTags {
 // UpdateTags updates fms service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn *fms.FMS, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn fmsiface.FMSAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
