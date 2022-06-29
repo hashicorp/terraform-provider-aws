297c54b0efdf21ca8041c4d799f719a45ea5d645
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 14:32:41 -0400

    gen/tags: Use interface type as generated AWS Go SDK v1 client type for amplify.

--- internal/service/amplify/tags_gen.go
+++ internal/service/amplify/tags_gen.go
@@ -6,13 +6,14 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/amplify"
+	"github.com/aws/aws-sdk-go/service/amplify/amplifyiface"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 // ListTags lists amplify service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func ListTags(conn *amplify.Amplify, identifier string) (tftags.KeyValueTags, error) {
+func ListTags(conn amplifyiface.AmplifyAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &amplify.ListTagsForResourceInput{
 		ResourceArn: aws.String(identifier),
 	}
@@ -41,7 +42,7 @@ func KeyValueTags(tags map[string]*string) tftags.KeyValueTags {
 // UpdateTags updates amplify service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn *amplify.Amplify, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn amplifyiface.AmplifyAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
