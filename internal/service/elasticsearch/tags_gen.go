d2b0f9ef142335514d5e9841bc1874d7e60d1b6d
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 14:32:46 -0400

    gen/tags: Use interface type as generated AWS Go SDK v1 client type for elasticsearch.

--- internal/service/elasticsearch/tags_gen.go
+++ internal/service/elasticsearch/tags_gen.go
@@ -6,13 +6,14 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/elasticsearchservice"
+	"github.com/aws/aws-sdk-go/service/elasticsearchservice/elasticsearchserviceiface"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 // ListTags lists elasticsearch service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func ListTags(conn *elasticsearchservice.ElasticsearchService, identifier string) (tftags.KeyValueTags, error) {
+func ListTags(conn elasticsearchserviceiface.ElasticsearchServiceAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &elasticsearchservice.ListTagsInput{
 		ARN: aws.String(identifier),
 	}
@@ -58,7 +59,7 @@ func KeyValueTags(tags []*elasticsearchservice.Tag) tftags.KeyValueTags {
 // UpdateTags updates elasticsearch service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn *elasticsearchservice.ElasticsearchService, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn elasticsearchserviceiface.ElasticsearchServiceAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
