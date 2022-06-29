a539d872e150f75cd47bba852e20f04a7aeb1515
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 15:07:16 -0400

    gen/tags: Generate 'WithContext' variants of AWS SDK for Go v1 tagging functions for datapipeline.

--- internal/service/datapipeline/tags_gen.go
+++ internal/service/datapipeline/tags_gen.go
@@ -2,6 +2,7 @@
 package datapipeline
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -42,7 +43,10 @@ func KeyValueTags(tags []*datapipeline.Tag) tftags.KeyValueTags {
 // UpdateTags updates datapipeline service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn datapipelineiface.DataPipelineAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn datapipelineiface.DataPipelineAPI, identifier string, oldTags interface{}, newTags interface{}) error {
+	return UpdateTagsWithContext(context.Background(), conn, identifier, oldTags, newTags)
+}
+func UpdateTagsWithContext(ctx context.Context, conn datapipelineiface.DataPipelineAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
@@ -52,7 +56,7 @@ func UpdateTags(conn datapipelineiface.DataPipelineAPI, identifier string, oldTa
 			TagKeys:    aws.StringSlice(removedTags.IgnoreAWS().Keys()),
 		}
 
-		_, err := conn.RemoveTags(input)
+		_, err := conn.RemoveTagsWithContext(ctx, input)
 
 		if err != nil {
 			return fmt.Errorf("error untagging resource (%s): %w", identifier, err)
@@ -65,7 +69,7 @@ func UpdateTags(conn datapipelineiface.DataPipelineAPI, identifier string, oldTa
 			Tags:       Tags(updatedTags.IgnoreAWS()),
 		}
 
-		_, err := conn.AddTags(input)
+		_, err := conn.AddTagsWithContext(ctx, input)
 
 		if err != nil {
 			return fmt.Errorf("error tagging resource (%s): %w", identifier, err)
