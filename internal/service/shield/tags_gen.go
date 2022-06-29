78a8fa566c591610dfaf75a020d3ef2bd1c031b9
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 14:32:53 -0400

    gen/tags: Use interface type as generated AWS Go SDK v1 client type for shield.

--- internal/service/shield/tags_gen.go
+++ internal/service/shield/tags_gen.go
@@ -6,13 +6,14 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/shield"
+	"github.com/aws/aws-sdk-go/service/shield/shieldiface"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 // ListTags lists shield service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func ListTags(conn *shield.Shield, identifier string) (tftags.KeyValueTags, error) {
+func ListTags(conn shieldiface.ShieldAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &shield.ListTagsForResourceInput{
 		ResourceARN: aws.String(identifier),
 	}
@@ -58,7 +59,7 @@ func KeyValueTags(tags []*shield.Tag) tftags.KeyValueTags {
 // UpdateTags updates shield service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn *shield.Shield, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn shieldiface.ShieldAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
