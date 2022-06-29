f0151bb7d1e4bedabc29d61f8c6f6ec2181fe537
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 14:32:52 -0400

    gen/tags: Use interface type as generated AWS Go SDK v1 client type for schemas.

--- internal/service/schemas/tags_gen.go
+++ internal/service/schemas/tags_gen.go
@@ -6,13 +6,14 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/schemas"
+	"github.com/aws/aws-sdk-go/service/schemas/schemasiface"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 // ListTags lists schemas service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func ListTags(conn *schemas.Schemas, identifier string) (tftags.KeyValueTags, error) {
+func ListTags(conn schemasiface.SchemasAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &schemas.ListTagsForResourceInput{
 		ResourceArn: aws.String(identifier),
 	}
@@ -41,7 +42,7 @@ func KeyValueTags(tags map[string]*string) tftags.KeyValueTags {
 // UpdateTags updates schemas service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn *schemas.Schemas, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn schemasiface.SchemasAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
