bbdb9581a6aa008f30dc22a9ac3b41f4ddf2b9aa
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 14:32:45 -0400

    gen/tags: Use interface type as generated AWS Go SDK v1 client type for eks.

--- internal/service/eks/tags_gen.go
+++ internal/service/eks/tags_gen.go
@@ -6,13 +6,14 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/eks"
+	"github.com/aws/aws-sdk-go/service/eks/eksiface"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 // ListTags lists eks service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func ListTags(conn *eks.EKS, identifier string) (tftags.KeyValueTags, error) {
+func ListTags(conn eksiface.EKSAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &eks.ListTagsForResourceInput{
 		ResourceArn: aws.String(identifier),
 	}
@@ -41,7 +42,7 @@ func KeyValueTags(tags map[string]*string) tftags.KeyValueTags {
 // UpdateTags updates eks service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn *eks.EKS, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn eksiface.EKSAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
