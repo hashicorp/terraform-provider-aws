b7b61b21bc10c39bc7f36f9867f623f4aee2a267
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-29 14:32:42 -0400

    gen/tags: Use interface type as generated AWS Go SDK v1 client type for appsync.

--- internal/service/appsync/tags_gen.go
+++ internal/service/appsync/tags_gen.go
@@ -6,13 +6,14 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/appsync"
+	"github.com/aws/aws-sdk-go/service/appsync/appsynciface"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 // ListTags lists appsync service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func ListTags(conn *appsync.AppSync, identifier string) (tftags.KeyValueTags, error) {
+func ListTags(conn appsynciface.AppSyncAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &appsync.ListTagsForResourceInput{
 		ResourceArn: aws.String(identifier),
 	}
@@ -41,7 +42,7 @@ func KeyValueTags(tags map[string]*string) tftags.KeyValueTags {
 // UpdateTags updates appsync service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn *appsync.AppSync, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(conn appsynciface.AppSyncAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
