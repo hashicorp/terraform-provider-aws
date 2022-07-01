f7ce1d1eb9ad5c8b7a7e27c08201ce5e63df77a9
Kamil Turek <kamil.turek@hotmail.com>
2022-07-01 21:56:46 +0200

    Generate ListTags

--- internal/service/location/tags_gen.go
+++ internal/service/location/tags_gen.go
@@ -11,6 +11,27 @@ import (
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
+// ListTags lists location service tags.
+// The identifier is typically the Amazon Resource Name (ARN), although
+// it may also be a different identifier depending on the service.
+func ListTags(conn locationserviceiface.LocationServiceAPI, identifier string) (tftags.KeyValueTags, error) {
+	return ListTagsWithContext(context.Background(), conn, identifier)
+}
+
+func ListTagsWithContext(ctx context.Context, conn locationserviceiface.LocationServiceAPI, identifier string) (tftags.KeyValueTags, error) {
+	input := &locationservice.ListTagsForResourceInput{
+		ResourceArn: aws.String(identifier),
+	}
+
+	output, err := conn.ListTagsForResourceWithContext(ctx, input)
+
+	if err != nil {
+		return tftags.New(nil), err
+	}
+
+	return KeyValueTags(output.Tags), nil
+}
+
 // map[string]*string handling
 
 // Tags returns location service tags.
