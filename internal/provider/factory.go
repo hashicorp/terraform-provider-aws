6a294865a66626ca4e03025543ef957952b88e9c
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-07-06 10:38:16 -0400

    Mux in the downgraded skeleton Terraform Plugin Framework provider.

--- internal/provider/factory.go
+++ internal/provider/factory.go
@@ -3,15 +3,25 @@ package provider
 import (
 	"context"
 
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
 	"github.com/hashicorp/terraform-plugin-mux/tf5muxserver"
+	"github.com/hashicorp/terraform-plugin-mux/tf6to5server"
+	"github.com/hashicorp/terraform-provider-aws/internal/provider/fw"
 )
 
 // ProtoV5ProviderServerFactory returns a muxed terraform-plugin-go protocol v5 provider factory function.
 // This factory function is suitable for use with the terraform-plugin-go Serve function.
 func ProtoV5ProviderServerFactory(ctx context.Context) (func() tfprotov5.ProviderServer, error) {
+	downgradedFrameworkProvider, err := tf6to5server.DowngradeServer(ctx, providerserver.NewProtocol6(fw.New()))
+
+	if err != nil {
+		return nil, err
+	}
+
 	providers := []func() tfprotov5.ProviderServer{
 		Provider().GRPCProvider,
+		func() tfprotov5.ProviderServer { return downgradedFrameworkProvider },
 	}
 
 	muxServer, err := tf5muxserver.NewMuxServer(ctx, providers...)
