cba7260470dc6fb2eca54567053aad22e2c073c4
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-07-18 10:53:32 -0400

    TestAccLambdaFunction_runtimes: Fix 'InvalidParameterValueException: The runtime parameter of python2.7 is no longer supported for creating or updating AWS Lambda functions. We recommend you use the new runtime (python3.9) while creating or updating functions'.

--- internal/service/lambda/function_test.go
+++ internal/service/lambda/function_test.go
@@ -2131,6 +2131,8 @@ func TestAccLambdaFunction_runtimes(t *testing.T) {
 	for _, runtime := range lambda.Runtime_Values() {
 		// EOL runtimes.
 		switch runtime {
+		case lambda.RuntimePython27:
+			fallthrough
 		case lambda.RuntimeRuby25:
 			fallthrough
 		case lambda.RuntimeNodejs43Edge:
