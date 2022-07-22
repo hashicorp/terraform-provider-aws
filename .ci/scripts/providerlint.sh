7376ea23de0bad44d42f44af4f2cc0172c1c3644
Dirk Avery <dirk.avery@gmail.com>
2022-07-21 20:56:13 -0400

    tflint better

--- .ci/scripts/providerlint.sh
+++ .ci/scripts/providerlint.sh
@@ -6,3 +6,18 @@ while [[ "${results}" == *Killed* ]]; do
   results=$( make providerlint 2>&1 )
 done
 echo "${results}"
+
+rules=(
+    # Syntax checks
+    "--only=terraform_comment_syntax"
+    "--only=terraform_deprecated_index"
+    "--only=terraform_deprecated_interpolation"
+    # Ensure valid instance types
+    "--only=aws_db_instance_invalid_type"
+    # Ensure modern instance types
+    "--only=aws_db_instance_previous_type"
+    "--only=aws_instance_previous_type"
+    # Ensure engine types are valid
+    "--only=aws_db_instance_invalid_engine"
+    "--only=aws_mq_broker_invalid_engine_type"
+)
