99c2099007ab0b4d15d8071bde279c95ca74b0b3
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-07-23 17:25:24 -0400

    'acctest.ProviderFactories' -> 'acctest.ProtoV5ProviderFactories' for meta.

--- internal/service/meta/arn_data_source_test.go
+++ internal/service/meta/arn_data_source_test.go
@@ -38,9 +38,9 @@ func TestAccMetaARNDataSource_s3Bucket(t *testing.T) {
 	dataSourceName := "data.aws_arn.test"
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:          func() { acctest.PreCheck(t) },
-		ErrorCheck:        acctest.ErrorCheck(t, tfmeta.PseudoServiceID),
-		ProviderFactories: acctest.ProviderFactories,
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		ErrorCheck:               acctest.ErrorCheck(t, tfmeta.PseudoServiceID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccARNDataSourceConfig_basic(arn),
