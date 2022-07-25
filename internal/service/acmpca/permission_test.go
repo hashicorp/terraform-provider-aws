70e45fa221ab37eafeb8868051b9b7bb1168ed3f
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-07-25 10:49:52 -0400

    r/aws_acmpca_permission: Use 'FindPermission' in acceptance tests.

--- internal/service/acmpca/permission_test.go
+++ internal/service/acmpca/permission_test.go
@@ -4,13 +4,13 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/acmpca"
-	"github.com/hashicorp/aws-sdk-go-base/v2/awsv1shim/v2/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	tfacmpca "github.com/hashicorp/terraform-provider-aws/internal/service/acmpca"
+	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 )
 
 func TestAccACMPCAPermission_basic(t *testing.T) {
@@ -49,52 +49,55 @@ func testAccCheckPermissionDestroy(s *terraform.State) error {
 			continue
 		}
 
-		input := &acmpca.ListPermissionsInput{
-			CertificateAuthorityArn: aws.String(rs.Primary.Attributes["certificate_authority_arn"]),
-		}
-
-		output, err := conn.ListPermissions(input)
+		caARN, principal, sourceAccount, err := tfacmpca.PermissionParseResourceID(rs.Primary.ID)
 
 		if err != nil {
-			if tfawserr.ErrCodeEquals(err, acmpca.ErrCodeResourceNotFoundException) ||
-				tfawserr.ErrCodeEquals(err, acmpca.ErrCodeInvalidStateException) {
-				return nil
-			}
 			return err
 		}
 
-		if output != nil {
-			return fmt.Errorf("ACMPCA Permission %q still exists", rs.Primary.ID)
+		_, err = tfacmpca.FindPermission(conn, caARN, principal, sourceAccount)
+
+		if tfresource.NotFound(err) {
+			continue
 		}
+
+		if err != nil {
+			return err
+		}
+
+		return fmt.Errorf("ACM PCA Permission %s still exists", rs.Primary.ID)
 	}
 
 	return nil
 
 }
 
-func testAccCheckPermissionExists(resourceName string, permission *acmpca.Permission) resource.TestCheckFunc {
+func testAccCheckPermissionExists(n string, v *acmpca.Permission) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
-		rs, ok := s.RootModule().Resources[resourceName]
+		rs, ok := s.RootModule().Resources[n]
 		if !ok {
-			return fmt.Errorf("Not found: %s", resourceName)
+			return fmt.Errorf("Not found: %s", n)
 		}
 
-		conn := acctest.Provider.Meta().(*conns.AWSClient).ACMPCAConn
-		input := &acmpca.ListPermissionsInput{
-			CertificateAuthorityArn: aws.String(rs.Primary.Attributes["certificate_authority_arn"]),
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No ACM PCA Permission ID is set")
 		}
 
-		output, err := conn.ListPermissions(input)
+		caARN, principal, sourceAccount, err := tfacmpca.PermissionParseResourceID(rs.Primary.ID)
 
 		if err != nil {
 			return err
 		}
 
-		if output == nil || output.Permissions == nil {
-			return fmt.Errorf("ACMPCA Permission %q does not exist", rs.Primary.ID)
+		conn := acctest.Provider.Meta().(*conns.AWSClient).ACMPCAConn
+
+		output, err := tfacmpca.FindPermission(conn, caARN, principal, sourceAccount)
+
+		if err != nil {
+			return err
 		}
 
-		*permission = *output.Permissions[0]
+		*v = *output
 
 		return nil
 	}
