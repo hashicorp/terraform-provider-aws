6ae0e8d87db2392009b49eb27fc5593178cce559
GlennChia <glennchia7@gmail.com>
2022-07-28 04:28:59 +0000

    test(kendra): datasource web_crawler max url per m

--- internal/service/kendra/kendra_test.go
+++ internal/service/kendra/kendra_test.go
@@ -15,6 +15,7 @@ func TestAccKendra_serial(t *testing.T) {
 			"Configuration_S3_ExclusionInclusionPatternsPrefixes":                                   testAccDataSource_Configuration_S3_ExclusionInclusionPatternsPrefixes,
 			"Configuration_WebCrawler_CrawlDepth":                                                   testAccDataSource_Configuration_WebCrawler_CrawlDepth,
 			"Configuration_WebCrawler_MaxLinksPerPage":                                              testAccDataSource_Configuration_WebCrawler_MaxLinksPerPage,
+			"Configuration_WebCrawler_MaxUrlsPerMinuteCrawlRate":                                    testAccDataSource_Configuration_WebCrawler_MaxUrlsPerMinuteCrawlRate,
 			"Configuration_WebCrawler_UrlsSeedUrls":                                                 testAccDataSource_Configuration_WebCrawler_UrlsSeedUrls,
 			"CustomDocumentEnrichmentConfiguration_ExtractionHookConfiguration_InvocationCondition": testAccDataSource_CustomDocumentEnrichmentConfiguration_ExtractionHookConfiguration_InvocationCondition,
 			"CustomDocumentEnrichmentConfiguration_ExtractionHookConfiguration_LambdaARN":           testAccDataSource_CustomDocumentEnrichmentConfiguration_ExtractionHookConfiguration_LambdaARN,
