ab1fbd674e1a4e6e42009eea69fc43bd0afee7ed
Mehmet Gungoren <mehgungoren@gmail.com>
2022-07-29 23:37:36 +0300

    fix import linting

--- internal/service/rekognition/collection_data_source.go
+++ internal/service/rekognition/collection_data_source.go
@@ -2,6 +2,7 @@ package rekognition
 
 import (
 	"fmt"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/rekognition"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
