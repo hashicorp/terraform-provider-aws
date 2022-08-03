febab4af2eb18aa60d4295b405dfdf0192a79deb
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-08-03 15:45:32 -0400

    r/aws_autoscaling_attachment: Tidy up resource Read.

--- internal/service/autoscaling/attachment.go
+++ internal/service/autoscaling/attachment.go
@@ -20,11 +20,11 @@ func ResourceAttachment() *schema.Resource {
 
 		Schema: map[string]*schema.Schema{
 			"alb_target_group_arn": {
-				Type:          schema.TypeString,
-				ForceNew:      true,
-				Optional:      true,
-				Deprecated:    "Use lb_target_group_arn instead",
-				ConflictsWith: []string{"lb_target_group_arn"},
+				Type:         schema.TypeString,
+				ForceNew:     true,
+				Optional:     true,
+				Deprecated:   "Use lb_target_group_arn instead",
+				ExactlyOneOf: []string{"alb_target_group_arn", "elb", "lb_target_group_arn"},
 			},
 			"autoscaling_group_name": {
 				Type:     schema.TypeString,
@@ -32,15 +32,16 @@ func ResourceAttachment() *schema.Resource {
 				Required: true,
 			},
 			"elb": {
-				Type:     schema.TypeString,
-				ForceNew: true,
-				Optional: true,
+				Type:         schema.TypeString,
+				ForceNew:     true,
+				Optional:     true,
+				ExactlyOneOf: []string{"alb_target_group_arn", "elb", "lb_target_group_arn"},
 			},
 			"lb_target_group_arn": {
-				Type:          schema.TypeString,
-				ForceNew:      true,
-				Optional:      true,
-				ConflictsWith: []string{"alb_target_group_arn"},
+				Type:         schema.TypeString,
+				ForceNew:     true,
+				Optional:     true,
+				ExactlyOneOf: []string{"alb_target_group_arn", "elb", "lb_target_group_arn"},
 			},
 		},
 	}
@@ -60,16 +61,14 @@ func resourceAttachmentCreate(d *schema.ResourceData, meta interface{}) error {
 		if _, err := conn.AttachLoadBalancers(input); err != nil {
 			return fmt.Errorf("attaching Auto Scaling Group (%s) load balancer (%s): %w", asgName, lbName, err)
 		}
-	}
+	} else {
+		var targetGroupARN string
+		if v, ok := d.GetOk("alb_target_group_arn"); ok {
+			targetGroupARN = v.(string)
+		} else if v, ok := d.GetOk("lb_target_group_arn"); ok {
+			targetGroupARN = v.(string)
+		}
 
-	var targetGroupARN string
-	if v, ok := d.GetOk("alb_target_group_arn"); ok {
-		targetGroupARN = v.(string)
-	} else if v, ok := d.GetOk("lb_target_group_arn"); ok {
-		targetGroupARN = v.(string)
-	}
-
-	if targetGroupARN != "" {
 		input := &autoscaling.AttachLoadBalancerTargetGroupsInput{
 			AutoScalingGroupName: aws.String(asgName),
 			TargetGroupARNs:      aws.StringSlice([]string{targetGroupARN}),
@@ -90,60 +89,29 @@ func resourceAttachmentRead(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*conns.AWSClient).AutoScalingConn
 	asgName := d.Get("autoscaling_group_name").(string)
 
-	asg, err := FindGroupByName(conn, asgName)
+	var err error
+
+	if v, ok := d.GetOk("elb"); ok {
+		lbName := v.(string)
+		err = FindAttachmentByLoadBalancerName(conn, asgName, lbName)
+	} else {
+		var targetGroupARN string
+		if v, ok := d.GetOk("alb_target_group_arn"); ok {
+			targetGroupARN = v.(string)
+		} else if v, ok := d.GetOk("lb_target_group_arn"); ok {
+			targetGroupARN = v.(string)
+		}
+		err = FindAttachmentByTargetGroupARN(conn, asgName, targetGroupARN)
+	}
 
 	if !d.IsNewResource() && tfresource.NotFound(err) {
-		log.Printf("[WARN] Auto Scaling Group %s not found, removing from state", d.Id())
+		log.Printf("[WARN] Auto Scaling Group Attachment %s not found, removing from state", d.Id())
 		d.SetId("")
 		return nil
 	}
 
 	if err != nil {
-		return fmt.Errorf("reading Auto Scaling Group (%s): %w", asgName, err)
-	}
-
-	if v, ok := d.GetOk("elb"); ok {
-		found := false
-		lbName := v.(string)
-
-		for _, v := range asg.LoadBalancerNames {
-			if aws.StringValue(v) == lbName {
-				d.Set("elb", lbName)
-				found = true
-				break
-			}
-		}
-
-		if !found {
-			log.Printf("[WARN] Association for %s was not found in ASG association", lbName)
-			d.SetId("")
-		}
-	}
-
-	var key, targetGroupARN string
-	if v, ok := d.GetOk("alb_target_group_arn"); ok {
-		key = "alb_target_group_arn"
-		targetGroupARN = v.(string)
-	} else if v, ok := d.GetOk("lb_target_group_arn"); ok {
-		key = "lb_target_group_arn"
-		targetGroupARN = v.(string)
-	}
-
-	if targetGroupARN != "" {
-		found := false
-
-		for _, v := range asg.TargetGroupARNs {
-			if aws.StringValue(v) == targetGroupARN {
-				d.Set(key, targetGroupARN)
-				found = true
-				break
-			}
-		}
-
-		if !found {
-			log.Printf("[WARN] Association for %s was not found in ASG association", targetGroupARN)
-			d.SetId("")
-		}
+		return fmt.Errorf("reading Auto Scaling Group Attachment (%s): %w", d.Id(), err)
 	}
 
 	return nil
@@ -163,16 +131,14 @@ func resourceAttachmentDelete(d *schema.ResourceData, meta interface{}) error {
 		if _, err := conn.DetachLoadBalancers(input); err != nil {
 			return fmt.Errorf("detaching Auto Scaling Group (%s) load balancer (%s): %w", asgName, lbName, err)
 		}
-	}
+	} else {
+		var targetGroupARN string
+		if v, ok := d.GetOk("alb_target_group_arn"); ok {
+			targetGroupARN = v.(string)
+		} else if v, ok := d.GetOk("lb_target_group_arn"); ok {
+			targetGroupARN = v.(string)
+		}
 
-	var targetGroupARN string
-	if v, ok := d.GetOk("alb_target_group_arn"); ok {
-		targetGroupARN = v.(string)
-	} else if v, ok := d.GetOk("lb_target_group_arn"); ok {
-		targetGroupARN = v.(string)
-	}
-
-	if targetGroupARN != "" {
 		input := &autoscaling.DetachLoadBalancerTargetGroupsInput{
 			AutoScalingGroupName: aws.String(asgName),
 			TargetGroupARNs:      aws.StringSlice([]string{targetGroupARN}),
