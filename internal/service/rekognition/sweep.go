4dacd208a2ac9d68a1a075ce018cd36837c9565b
Mehmet Gungoren <mehgungoren@gmail.com>
2022-08-04 22:00:21 +0300

    add rekognition sweep

---
+++ internal/service/rekognition/sweep.go
@@ -0,0 +1,67 @@
+//go:build sweep
+// +build sweep
+
+package rekognition
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/rekognition"
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
+	"log"
+)
+
+func init() {
+	resource.AddTestSweepers("aws_rekognition_collection", &resource.Sweeper{
+		Name: "aws_rekognition_collection",
+		F:    sweepCollections,
+	})
+}
+
+func sweepCollections(region string) error{
+	client, err := sweep.SharedRegionalSweepClient(region)
+
+	if err != nil {
+		return fmt.Errorf("error getting client: %s", err)
+	}
+
+	conn := client.(*conns.AWSClient).RekognitionConn
+	sweepResources := make([]*sweep.SweepResource, 0)
+	var errs *multierror.Error
+
+	err = conn.ListCollectionsPages(&rekognition.ListCollectionsInput{}, func(resp *rekognition.ListCollectionsOutput, lastPage bool) bool {
+		if len(resp.CollectionIds) == 0 {
+			log.Print("[DEBUG] No Rekognition Collections to sweep")
+			return !lastPage
+		}
+
+		for _, c := range resp.CollectionIds {
+			r := ResourceCollection()
+			d := r.Data(nil)
+			d.SetId(aws.StringValue(c))
+
+			sweepResources = append(sweepResources, sweep.NewSweepResource(r, d, client))
+		}
+
+		return !lastPage
+	})
+
+	if err != nil {
+		errs = multierror.Append(errs, fmt.Errorf("error describing Rekognition Collections: %w", err))
+		// in case work can be done, don't jump out yet
+	}
+
+	if err = sweep.SweepOrchestrator(sweepResources); err != nil {
+		errs = multierror.Append(errs, fmt.Errorf("error sweeping Rekognition Collections for %s: %w", region, err))
+	}
+
+	if sweep.SkipSweepError(errs.ErrorOrNil()) {
+		log.Printf("[WARN] Skipping Rekognition Collections sweep for %s: %s", region, err)
+		return nil
+	}
+
+	return errs.ErrorOrNil()
+}
