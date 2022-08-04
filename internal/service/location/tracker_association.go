67109dd5ca543f5506fdaaa3763fada54e23cda4
Kamil Turek <kamil.turek@hotmail.com>
2022-08-04 21:07:01 +0200

    Remove update timeout

--- internal/service/location/tracker_association.go
+++ internal/service/location/tracker_association.go
@@ -33,7 +33,6 @@ func ResourceTrackerAssociation() *schema.Resource {
 
 		Timeouts: &schema.ResourceTimeout{
 			Create: schema.DefaultTimeout(30 * time.Minute),
-			Update: schema.DefaultTimeout(30 * time.Minute),
 			Delete: schema.DefaultTimeout(30 * time.Minute),
 		},
 
