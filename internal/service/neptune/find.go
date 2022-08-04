79087761ec1b096595a3799bb1b14519f869fb99
larssnellman <lsnellman@gmail.com>
2022-08-04 14:51:50 -0400

    initial commit to add support for Neptune Global Databases - #26132

--- internal/service/neptune/find.go
+++ internal/service/neptune/find.go
@@ -1,10 +1,13 @@
 package neptune
 
 import (
+	"context"
+	"errors"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/neptune"
 	"github.com/hashicorp/aws-sdk-go-base/v2/awsv1shim/v2/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"log"
 )
 
 func FindEndpointByID(conn *neptune.Neptune, id string) (*neptune.DBClusterEndpoint, error) {
@@ -47,4 +50,114 @@ func FindEndpointByID(conn *neptune.Neptune, id string) (*neptune.DBClusterEndpo
 	}
 
 	return endpoints[0], nil
+}
+
+func findGlobalClusterByARN(ctx context.Context, conn *neptune.Neptune, dbClusterARN string) (*neptune.GlobalCluster, error) {
+
+	// Input currently has no filter support, maybe this will change in the future.
+	input := &neptune.DescribeGlobalClustersInput{}
+
+	for {
+		log.Printf("[DEBUG] Reading Neptune Global Cluster (%s): %s", dbClusterARN, input)
+		output, err := conn.DescribeGlobalClustersWithContext(ctx, input)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, gc := range output.GlobalClusters {
+			if gc == nil {
+				continue
+			}
+			for _, gcm := range gc.GlobalClusterMembers {
+				if gcm == nil {
+					continue
+				}
+
+				if aws.StringValue(gcm.DBClusterArn) == dbClusterARN {
+					log.Printf("found global cluster %s", gc)
+					return gc, nil
+				} else {
+					log.Printf("failed to find the global cluster still, %s, %s", dbClusterARN, aws.StringValue(gc.GlobalClusterArn))
+				}
+
+			}
+
+		}
+
+		if output.Marker == nil {
+			break
+		}
+
+		input.Marker = output.Marker
+	}
+
+	// We didn't find the global cluster
+	return nil, nil
+}
+
+func FindGlobalClusterById(ctx context.Context, conn *neptune.Neptune, globalClusterID string) (*neptune.GlobalCluster, error) {
+
+	input := &neptune.DescribeGlobalClustersInput{
+		GlobalClusterIdentifier: aws.String(globalClusterID),
+	}
+
+	for {
+		log.Printf("[DEBUG] Reading Neptune Global Cluster (%s): %s", globalClusterID, input)
+		output, err := conn.DescribeGlobalClustersWithContext(ctx, input)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, gc := range output.GlobalClusters {
+
+			if gc == nil {
+				continue
+			}
+			log.Printf("[DEBUG] Found random Neptune Global Cluster (%s): %s", aws.StringValue(gc.GlobalClusterIdentifier), gc)
+
+			if aws.StringValue(gc.GlobalClusterIdentifier) == aws.StringValue(input.GlobalClusterIdentifier) {
+				log.Printf("[DEBUG] This did match Neptune Global Cluster (%s): %s", aws.StringValue(input.GlobalClusterIdentifier), gc)
+
+				return gc, nil
+			}
+		}
+
+		if output.Marker == nil {
+			break
+		}
+
+		input.Marker = output.Marker
+	}
+
+	return nil, nil
+}
+
+func findClusterByClusterARN(ctx context.Context, conn *neptune.Neptune, arn string) (*neptune.DBCluster, error) {
+	input := &neptune.DescribeDBClustersInput{}
+	for {
+		log.Printf("[DEBUG] Reading Neptune Cluster (%s): %s", arn, input)
+		output, err := conn.DescribeDBClustersWithContext(ctx, input)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, dbc := range output.DBClusters {
+			if dbc == nil {
+				continue
+			}
+
+			if aws.StringValue(dbc.DBClusterArn) == arn {
+				return dbc, nil
+			}
+		}
+
+		if output.Marker == nil {
+			break
+		}
+
+		input.Marker = output.Marker
+	}
+
+	// We didn't find the cluster
+	return nil, errors.New(neptune.ErrCodeDBClusterNotFoundFault)
 }
