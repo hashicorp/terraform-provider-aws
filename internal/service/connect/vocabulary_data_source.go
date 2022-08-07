84c0efe36d06a72efb719bbfaea337a82f873662
GlennChia <glennchia7@gmail.com>
2022-08-07 09:31:40 +0000

    feat(connect): vocabulary datasrc reads

--- internal/service/connect/vocabulary_data_source.go
+++ internal/service/connect/vocabulary_data_source.go
@@ -1,13 +1,22 @@
 package connect
 
 import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/connect"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
 func DataSourceVocabulary() *schema.Resource {
 	return &schema.Resource{
+		ReadContext: dataSourceVocabularyRead,
 		Schema: map[string]*schema.Schema{
 			"arn": {
 				Type:     schema.TypeString,
@@ -53,4 +62,97 @@ func DataSourceVocabulary() *schema.Resource {
 			"tags": tftags.TagsSchemaComputed(),
 		},
 	}
+}
+
+func dataSourceVocabularyRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn := meta.(*conns.AWSClient).ConnectConn
+	ignoreTagsConfig := meta.(*conns.AWSClient).IgnoreTagsConfig
+
+	instanceID := d.Get("instance_id").(string)
+
+	input := &connect.DescribeVocabularyInput{
+		InstanceId: aws.String(instanceID),
+	}
+
+	if v, ok := d.GetOk("vocabulary_id"); ok {
+		input.VocabularyId = aws.String(v.(string))
+	} else if v, ok := d.GetOk("name"); ok {
+		name := v.(string)
+		vocabularySummary, err := dataSourceGetVocabularySummaryByName(ctx, conn, instanceID, name)
+
+		if err != nil {
+			return diag.FromErr(fmt.Errorf("error finding Connect Vocabulary Summary by name (%s): %w", name, err))
+		}
+
+		if vocabularySummary == nil {
+			return diag.FromErr(fmt.Errorf("error finding Connect Vocabulary Summary by name (%s): not found", name))
+		}
+
+		input.VocabularyId = vocabularySummary.Id
+	}
+
+	resp, err := conn.DescribeVocabularyWithContext(ctx, input)
+
+	if err != nil {
+		return diag.FromErr(fmt.Errorf("error getting Connect Vocabulary: %w", err))
+	}
+
+	if resp == nil || resp.Vocabulary == nil {
+		return diag.FromErr(fmt.Errorf("error getting Connect Vocabulary: empty response"))
+	}
+
+	vocabulary := resp.Vocabulary
+
+	d.Set("arn", vocabulary.Arn)
+	d.Set("content", vocabulary.Content)
+	d.Set("failure_reason", vocabulary.FailureReason)
+	d.Set("instance_id", instanceID)
+	d.Set("language_code", vocabulary.LanguageCode)
+	d.Set("last_modified_time", vocabulary.LastModifiedTime.Format(time.RFC3339))
+	d.Set("name", vocabulary.Name)
+	d.Set("state", vocabulary.State)
+	d.Set("vocabulary_id", vocabulary.Id)
+
+	if err := d.Set("tags", KeyValueTags(vocabulary.Tags).IgnoreAWS().IgnoreConfig(ignoreTagsConfig).Map()); err != nil {
+		return diag.FromErr(fmt.Errorf("error setting tags: %s", err))
+	}
+
+	d.SetId(fmt.Sprintf("%s:%s", instanceID, aws.StringValue(vocabulary.Id)))
+
+	return nil
+}
+
+func dataSourceGetVocabularySummaryByName(ctx context.Context, conn *connect.Connect, instanceID, name string) (*connect.VocabularySummary, error) {
+	var result *connect.VocabularySummary
+
+	input := &connect.SearchVocabulariesInput{
+		InstanceId:     aws.String(instanceID),
+		MaxResults:     aws.Int64(SearchVocabulariesMaxResults),
+		NameStartsWith: aws.String(name),
+	}
+
+	err := conn.SearchVocabulariesPagesWithContext(ctx, input, func(page *connect.SearchVocabulariesOutput, lastPage bool) bool {
+		if page == nil {
+			return !lastPage
+		}
+
+		for _, qs := range page.VocabularySummaryList {
+			if qs == nil {
+				continue
+			}
+
+			if aws.StringValue(qs.Name) == name {
+				result = qs
+				return false
+			}
+		}
+
+		return !lastPage
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
 }
