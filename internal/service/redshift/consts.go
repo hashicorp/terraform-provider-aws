7d0ea0678355a2060e59ae05467b865e9a039547
Dirk Avery <dirk.avery@gmail.com>
2022-08-08 14:02:29 -0400

    gofmt for Go 1.19

--- internal/service/redshift/consts.go
+++ internal/service/redshift/consts.go
@@ -12,6 +12,7 @@ const (
 )
 
 // https://docs.aws.amazon.com/redshift/latest/mgmt/working-with-clusters.html#rs-mgmt-cluster-status.
+//
 //nolint:deadcode,varcheck // These constants are missing from the AWS SDK
 const (
 	clusterStatusAvailable              = "available"
