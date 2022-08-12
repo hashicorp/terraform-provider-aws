2a6ca9283466627e76f5e524d74cc1fd25dd94ee
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-08-12 09:47:19 -0400

    r/aws_opsworks_stack: Add 'TestAccOpsWorksStack_classic'.
    Acceptance test output:
    
    % make testacc TESTARGS='-run=TestAccOpsWorksStack_classic' PKG=opsworks ACCTEST_PARALLELISM=2
    ==> Checking that code complies with gofmt requirements...
    TF_ACC=1 go test ./internal/service/opsworks/... -v -count 1 -parallel 2  -run=TestAccOpsWorksStack_classic -timeout 180m
    === RUN   TestAccOpsWorksStack_classic
    === PAUSE TestAccOpsWorksStack_classic
    === CONT  TestAccOpsWorksStack_classic
    --- PASS: TestAccOpsWorksStack_classic (24.66s)
    PASS
    ok  	github.com/hashicorp/terraform-provider-aws/internal/service/opsworks	28.579s

--- internal/service/opsworks/stack_test.go
+++ internal/service/opsworks/stack_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/opsworks"
 	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
@@ -143,6 +144,41 @@ func TestAccOpsWorksStack_tags(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "tags.%", "1"),
 					resource.TestCheckResourceAttr(resourceName, "tags.key2", "value2"),
 				),
+			},
+		},
+	})
+}
+
+func TestAccOpsWorksStack_classic(t *testing.T) {
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	resourceName := "aws_opsworks_stack.test"
+	var v opsworks.Stack
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheck(t)
+			acctest.PreCheckEC2Classic(t)
+			acctest.PreCheckPartitionHasService(opsworks.EndpointsID, t)
+			testAccPreCheckStacks(t)
+		},
+		ErrorCheck:               acctest.ErrorCheck(t, opsworks.EndpointsID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckStackClassicDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccStackConfig_classic(rName),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccCheckStackClassicExists(resourceName, &v),
+					resource.TestCheckResourceAttrPair(resourceName, "default_availability_zone", "data.aws_availability_zones.available", "names.0"),
+					resource.TestCheckResourceAttr(resourceName, "default_subnet_id", ""),
+					resource.TestCheckResourceAttr(resourceName, "vpc_id", ""),
+				),
+			},
+			{
+				Config:            testAccStackConfig_classic(rName),
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
 			},
 		},
 	})
@@ -425,6 +461,14 @@ func testAccCheckCreateStackAttributes(rName string) resource.TestCheckFunc {
 }
 
 func testAccCheckStackExists(n string, v *opsworks.Stack) resource.TestCheckFunc {
+	return testAccCheckStackExistsWithProvider(n, v, func() *schema.Provider { return acctest.Provider })
+}
+
+func testAccCheckStackClassicExists(n string, v *opsworks.Stack) resource.TestCheckFunc {
+	return testAccCheckStackExistsWithProvider(n, v, func() *schema.Provider { return acctest.ProviderEC2Classic })
+}
+
+func testAccCheckStackExistsWithProvider(n string, v *opsworks.Stack, providerF func() *schema.Provider) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		rs, ok := s.RootModule().Resources[n]
 		if !ok {
@@ -435,7 +479,7 @@ func testAccCheckStackExists(n string, v *opsworks.Stack) resource.TestCheckFunc
 			return fmt.Errorf("No ID is set")
 		}
 
-		conn := acctest.Provider.Meta().(*conns.AWSClient).OpsWorksConn
+		conn := providerF().Meta().(*conns.AWSClient).OpsWorksConn
 
 		output, err := tfopsworks.FindStackByID(conn, rs.Primary.ID)
 
@@ -450,7 +494,15 @@ func testAccCheckStackExists(n string, v *opsworks.Stack) resource.TestCheckFunc
 }
 
 func testAccCheckStackDestroy(s *terraform.State) error {
-	conn := acctest.Provider.Meta().(*conns.AWSClient).OpsWorksConn
+	return testAccCheckStackDestroyWithProvider(s, func() *schema.Provider { return acctest.Provider })
+}
+
+func testAccCheckStackClassicDestroy(s *terraform.State) error {
+	return testAccCheckStackDestroyWithProvider(s, func() *schema.Provider { return acctest.ProviderEC2Classic })
+}
+
+func testAccCheckStackDestroyWithProvider(s *terraform.State, providerF func() *schema.Provider) error {
+	conn := providerF().Meta().(*conns.AWSClient).OpsWorksConn
 
 	for _, rs := range s.RootModule().Resources {
 		if rs.Type != "aws_opsworks_stack" {
@@ -594,6 +646,23 @@ resource "aws_opsworks_stack" "test" {
   }
 }
 `, rName, acctest.Region(), tagKey1, tagValue1, tagKey2, tagValue2))
+}
+
+func testAccStackConfig_classic(rName string) string {
+	return acctest.ConfigCompose(
+		acctest.ConfigEC2ClassicRegionProvider(),
+		acctest.ConfigAvailableAZsNoOptIn(),
+		testAccStackConfig_base(rName),
+		fmt.Sprintf(`
+resource "aws_opsworks_stack" "test" {
+  name                          = %[1]q
+  region                        = %[2]q
+  service_role_arn              = aws_iam_role.opsworks_service.arn
+  default_instance_profile_arn  = aws_iam_instance_profile.opsworks_instance.arn
+  default_availability_zone     = data.aws_availability_zones.available.names[0]
+  use_opsworks_security_groups  = false
+}
+`, rName, acctest.EC2ClassicRegion()))
 }
 
 func testAccStackConfig_baseTagsAlternateRegion(rName string) string {
