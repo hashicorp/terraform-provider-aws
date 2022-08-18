eef5fc6dc95f88c7e03efbd8d4a5078c57d404b3
Bruno Schaatsbergen <git@bschaatsbergen.com>
2022-08-18 23:58:43 +0200

    Create workspace_api_key.go

---
+++ internal/service/grafana/workspace_api_key.go
@@ -0,0 +1,97 @@
+package grafana
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/managedgrafana"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+)
+
+func ResourceWorkspaceApiKey() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceWorkspaceApiKeyCreate,
+		Read:   resourceWorkspaceApiKeyRead,
+		Delete: resourceWorkspaceApiKeyDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(10 * time.Minute),
+			Update: schema.DefaultTimeout(10 * time.Minute),
+		},
+
+		Schema: map[string]*schema.Schema{
+			"key_name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"key_role": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validation.StringInSlice(managedgrafana.Role_Values(), false),
+			},
+			"seconds_to_live": {
+				Type:     schema.TypeInt,
+				Required: true,
+			},
+			"workspace_id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+		},
+	}
+}
+
+func resourceWorkspaceApiKeyCreate(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*conns.AWSClient).GrafanaConn
+
+	input := &managedgrafana.CreateWorkspaceApiKeyInput{
+		KeyName:       aws.String(d.Get("key_name").(string)),
+		KeyRole:       aws.String(d.Get("key_role").(string)),
+		SecondsToLive: aws.Int64(int64(d.Get("seconds_to_live").(int))),
+		WorkspaceId:   aws.String(d.Get("workspace_id").(string)),
+	}
+
+	log.Printf("[DEBUG] Creating Grafana Workspace API Key: %s", input)
+	output, err := conn.CreateWorkspaceApiKey(input)
+
+	if err != nil {
+		return fmt.Errorf("error creating Grafana Workspace API Key: %w", err)
+	}
+
+	d.SetId(aws.StringValue(output.KeyName))
+
+	return resourceWorkspaceApiKeyRead(d, meta)
+}
+
+func resourceWorkspaceApiKeyRead(d *schema.ResourceData, meta interface{}) error {
+	d.Set("key_name", d.Get("key_name").(string))
+	d.Set("key_role", d.Get("key_role").(string))
+	d.Set("seconds_to_live", d.Get("seconds_to_live").(int))
+	d.Set("workspace_id", d.Get("workspace_id").(string))
+
+	return nil
+}
+
+func resourceWorkspaceApiKeyDelete(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*conns.AWSClient).GrafanaConn
+
+	log.Printf("[DEBUG] Deleting Grafana Workspace API Key: %s", d.Id())
+	_, err := conn.DeleteWorkspaceApiKey(&managedgrafana.DeleteWorkspaceApiKeyInput{
+		KeyName:     aws.String(d.Get("key_name").(string)),
+		WorkspaceId: aws.String(d.Get("workspace_id").(string)),
+	})
+
+	if err != nil {
+		return fmt.Errorf("error deleting Grafana Workspace API Key (%s): %w", d.Id(), err)
+	}
+
+	return nil
+}
