77a8aab222e3ba8d363feedc57bafdc1a0e40549
Graham Davison <gdavison@hashicorp.com>
2022-08-31 11:43:25 -0700

    Adds AWS credentials envvars to temporary config file

--- .teamcity/scripts/sweeper_role.sh
+++ .teamcity/scripts/sweeper_role.sh
@@ -2,11 +2,26 @@
 
 set -euo pipefail
 
-touch ./aws.conf
-chmod 600 ./aws.conf
-cat << EOF > ./aws.conf
+conf=$(pwd)/aws.conf
+
+function cleanup {
+  rm "$conf"
+}
+trap cleanup EXIT
+
+touch "$conf"
+chmod 600 "$conf"
+cat << EOF > "$conf"
 [profile sweeper]
 role_arn = %ACCTEST_ROLE_ARN%
+source_profile = source
+
+[profile source]
+aws_access_key_id     = %env.AWS_ACCESS_KEY_ID%
+aws_secret_access_key = %env.AWS_SECRET_ACCESS_KEY%
 EOF
 
-AWS_CONFIG_FILE=./aws.conf AWS_PROFILE=sweeper go test ./internal/sweep -v -tags=sweep -sweep="%SWEEPER_REGIONS%" -sweep-allow-failures -timeout=4h
+unset AWS_ACCESS_KEY_ID
+unset AWS_SECRET_ACCESS_KEY
+
+AWS_CONFIG_FILE="$conf" AWS_PROFILE=sweeper go test ./internal/sweep -v -tags=sweep -sweep="%SWEEPER_REGIONS%" -sweep-allow-failures -timeout=4h
