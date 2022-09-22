85624f145777a146392b578513e8a501937fd031
Iain Steers <isteers@palantir.com>
2022-09-22 10:26:05 +0100

    Add acceptance tests for configuration resource groups

--- internal/service/resourcegroups/group_test.go
+++ internal/service/resourcegroups/group_test.go
@@ -2,6 +2,7 @@ package resourcegroups_test
 
 import (
 	"fmt"
+	"regexp"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -63,6 +64,73 @@ func TestAccResourceGroupsGroup_Resource_basic(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "description", desc2),
 					resource.TestCheckResourceAttr(resourceName, "resource_query.0.query", query2+"\n"),
 				),
+			},
+		},
+	})
+}
+
+func TestAccResourceGroupsGroup_Resource_Configuration(t *testing.T) {
+	var v resourcegroups.Group
+	resourceName := "aws_resourcegroups_group.test"
+	n := fmt.Sprintf("test-group-%d", sdkacctest.RandInt())
+
+	desc1 := "Hello World"
+	desc2 := "Foo Bar"
+	configType1 := "AWS::EC2::HostManagement"
+	configType2 := "AWS::ResourceGroups::Generic"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		ErrorCheck:               acctest.ErrorCheck(t, resourcegroups.EndpointsID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckResourceGroupDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccGroupConfig_config(n, desc1, configType1, configType2, false),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckResourceGroupExists(resourceName, &v),
+					resource.TestCheckResourceAttr(resourceName, "name", n),
+					resource.TestCheckResourceAttr(resourceName, "description", desc1),
+					resource.TestCheckResourceAttr(resourceName, "configuration.0.type", configType1),
+					resource.TestCheckResourceAttr(resourceName, "configuration.1.type", configType2),
+					resource.TestCheckResourceAttr(resourceName, "configuration.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "configuration.0.parameters.#", "4"),
+					resource.TestCheckResourceAttr(resourceName, "configuration.0.parameters.0.name", "allowed-host-families"),
+					resource.TestCheckResourceAttr(resourceName, "configuration.0.parameters.0.values.0", "mac1"),
+					resource.TestCheckResourceAttrSet(resourceName, "arn"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			// Check that changing the auto-allocate value is represented
+			{
+				Config: testAccGroupConfig_config(n, desc1, configType1, configType2, true),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckResourceGroupExists(resourceName, &v),
+					resource.TestCheckResourceAttr(resourceName, "name", n),
+					resource.TestCheckResourceAttr(resourceName, "description", desc1),
+					resource.TestCheckResourceAttr(resourceName, "configuration.0.type", configType1),
+					resource.TestCheckResourceAttr(resourceName, "configuration.1.type", configType2),
+					resource.TestCheckResourceAttr(resourceName, "configuration.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "configuration.0.parameters.#", "4"),
+					resource.TestCheckResourceAttr(resourceName, "configuration.0.parameters.2.name", "auto-allocate-host"),
+					resource.TestCheckResourceAttr(resourceName, "configuration.0.parameters.2.values.0", "true"),
+					resource.TestCheckResourceAttrSet(resourceName, "arn"),
+				),
+			},
+			{
+				Config: testAccGroupConfig_config(n, desc2, configType1, configType2, true),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "description", desc2),
+				),
+			},
+			// Check that trying to change the configuration group to a resource-query group fails
+			{
+				Config:      testAccGroupConfig_basic(n, desc1, testAccResourceGroupQueryConfig),
+				ExpectError: regexp.MustCompile(`conversion between resource-query and configuration group types is not possible`),
 			},
 		},
 	})
@@ -240,4 +308,63 @@ JSON
   }
 }
 `, rName, desc, query, tag1Key, tag1Value, tag2Key, tag2Value)
+}
+
+func testAccGroupConfig_config(rName, desc, cType1, cType2 string, autoAllocateHost bool) string {
+	return fmt.Sprintf(`
+resource "aws_resourcegroups_group" "test" {
+  name        = "%s"
+  description = "%s"
+
+  configuration {
+    type = "%s"
+
+    parameters {
+      name   = "allowed-host-families"
+      values = [
+        "mac1",
+      ]
+    }
+
+    parameters {
+      name   = "any-host-based-license-configuration"
+      values = [
+        "true",
+      ]
+    }
+
+    parameters {
+      name   = "auto-allocate-host"
+      values = [
+        "%t",
+      ]
+    }
+
+    parameters {
+      name   = "auto-release-host"
+      values = [
+        "true",
+      ]
+    }
+  }
+
+  configuration {
+    type = "%s"
+
+    parameters {
+      name = "allowed-resource-types"
+      values = [
+        "AWS::EC2::Host",
+      ]
+    }
+
+    parameters {
+      name = "deletion-protection"
+      values = [
+        "UNLESS_EMPTY"
+      ]
+    }
+  }
+}
+`, rName, desc, cType1, autoAllocateHost, cType2)
 }
