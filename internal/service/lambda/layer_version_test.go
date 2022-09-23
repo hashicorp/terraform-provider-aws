f8a664c68acd70f1069cd3eb335a570872dff809
Scott Westover <scottwestover2006@gmail.com>
2022-09-23 18:07:58 -0400

    test - lambda layer_version - fixed formatting
    Fixed the terraform formatting in the test file for lambda layer version.
    
    Relates https://github.com/hashicorp/terraform-provider-aws/issues/26576
    
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

--- internal/service/lambda/layer_version_test.go
+++ internal/service/lambda/layer_version_test.go
@@ -383,7 +383,7 @@ resource "aws_lambda_layer_version" "lambda_layer_test" {
   filename   = "test-fixtures/lambdatest.zip"
   layer_name = %[1]q
 
-  compatible_runtimes = ["nodejs14.x", "nodejs16.x",]
+  compatible_runtimes = ["nodejs14.x", "nodejs16.x"]
 }
 `, rName)
 }
