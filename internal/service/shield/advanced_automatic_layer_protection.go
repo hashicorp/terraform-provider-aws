fbfae7d2fb933f65c466c20ffe748355fcffea6f
Dennis Vinterfjärd <dennis.hedlund@klarna.com>
2022-09-28 11:58:34 +0200

    Implemented crud for advanced_automatic_layer_protection, ran through all tests manually towards aws

--- internal/service/shield/advanced_automatic_layer_protection.go
+++ internal/service/shield/advanced_automatic_layer_protection.go
@@ -6,6 +6,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/shield"
 	"github.com/hashicorp/aws-sdk-go-base/v2/awsv1shim/v2/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"log"
 )
@@ -27,8 +28,9 @@ func ResourceAdvancedAutomaticLayerProtection() *schema.Resource {
 				ForceNew: true,
 			},
 			"action": {
-				Type:     schema.TypeString,
-				Required: true,
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validation.StringInSlice([]string{"block", "count"}, false),
 			},
 		},
 	}
@@ -42,21 +44,19 @@ func resourceAdvancedAutomaticLayerProtectionUpdate(d *schema.ResourceData, meta
 	}
 
 	action := &shield.ResponseAction{}
-
-	if d.Get("action").(string) == "block" {
+	switch d.Get("action").(string) {
+	case "block":
 		action.Block = &shield.BlockAction{}
-	}
-
-	if d.Get("action").(string) == "count" {
+	case "count":
 		action.Count = &shield.CountAction{}
 	}
 
 	input := &shield.UpdateApplicationLayerAutomaticResponseInput{
 		Action:      action,
-		ResourceArn: aws.String(d.Get("resource_arn").(string)),
+		ResourceArn: aws.String(d.Id()),
 	}
 
-	_, err := conn.UpdateApplicationLayerAutomaticResponseRequest(input)
+	_, err := conn.UpdateApplicationLayerAutomaticResponse(input)
 	if err != nil {
 		return fmt.Errorf("error updating Application Layer Automatic Protection: %s", err)
 	}
@@ -68,12 +68,10 @@ func resourceAdvancedAutomaticLayerProtectionCreate(d *schema.ResourceData, meta
 	conn := meta.(*conns.AWSClient).ShieldConn
 
 	action := &shield.ResponseAction{}
-
-	if d.Get("action").(string) == "block" {
+	switch d.Get("action").(string) {
+	case "block":
 		action.Block = &shield.BlockAction{}
-	}
-
-	if d.Get("action").(string) == "count" {
+	case "count":
 		action.Count = &shield.CountAction{}
 	}
 
@@ -82,21 +80,12 @@ func resourceAdvancedAutomaticLayerProtectionCreate(d *schema.ResourceData, meta
 		ResourceArn: aws.String(d.Get("resource_arn").(string)),
 	}
 
-	describeProtectionInput := &shield.DescribeProtectionInput{
-		ResourceArn: aws.String(d.Get("resource_arn").(string)),
-	}
-
-	res, err := conn.DescribeProtection(describeProtectionInput)
-	if err != nil {
-		return fmt.Errorf("error reading Protection: %s", err)
-	}
-
-	_, err = conn.EnableApplicationLayerAutomaticResponse(enableAutomaticResponseInput)
+	_, err := conn.EnableApplicationLayerAutomaticResponse(enableAutomaticResponseInput)
 	if err != nil {
 		return fmt.Errorf("error creating Application Layer Automatic Protection: %s", err)
 	}
 
-	d.SetId(aws.StringValue(res.Protection.Id))
+	d.SetId(d.Get("resource_arn").(string))
 	return resourceAdvancedAutomaticLayerProtectionRead(d, meta)
 }
 
@@ -104,7 +93,7 @@ func resourceAdvancedAutomaticLayerProtectionRead(d *schema.ResourceData, meta i
 	conn := meta.(*conns.AWSClient).ShieldConn
 
 	input := &shield.DescribeProtectionInput{
-		ProtectionId: aws.String(d.Id()),
+		ResourceArn: aws.String(d.Id()),
 	}
 
 	resp, err := conn.DescribeProtection(input)
@@ -119,21 +108,16 @@ func resourceAdvancedAutomaticLayerProtectionRead(d *schema.ResourceData, meta i
 		return fmt.Errorf("error reading Application Layer Automatic Protection (%s): %s", d.Id(), err)
 	}
 
-	a := resp.Protection.ApplicationLayerAutomaticResponseConfiguration.Action.Block.String()
-	b := resp.Protection.ApplicationLayerAutomaticResponseConfiguration.Action.Count.String()
 	var action string
-
-	if a != "" {
-		action = a
+	if resp.Protection.ApplicationLayerAutomaticResponseConfiguration.Action != nil {
+		if resp.Protection.ApplicationLayerAutomaticResponseConfiguration.Action.Block != nil {
+			action = "block"
+		}
+		if resp.Protection.ApplicationLayerAutomaticResponseConfiguration.Action.Count != nil {
+			action = "count"
+		}
 	}
-	if b != "" {
-		action = b
-	}
 
-	arn := aws.StringValue(resp.Protection.ProtectionArn)
-	d.Set("arn", arn)
-	d.Set("name", resp.Protection.Name)
-	d.Set("resource_arn", resp.Protection.ResourceArn)
 	d.Set("action", action)
 
 	return nil
@@ -143,17 +127,13 @@ func resourceAdvancedAutomaticLayerProtectionDelete(d *schema.ResourceData, meta
 	conn := meta.(*conns.AWSClient).ShieldConn
 
 	input := &shield.DisableApplicationLayerAutomaticResponseInput{
-		ResourceArn: aws.String(d.Get("resource_arn").(string)),
+		ResourceArn: aws.String(d.Id()),
 	}
 
-	output, _ := conn.DisableApplicationLayerAutomaticResponseRequest(input)
+	_, err := conn.DisableApplicationLayerAutomaticResponse(input)
 
-	if tfawserr.ErrCodeEquals(output.Error, shield.ErrCodeResourceNotFoundException) {
-		return nil
-	}
-
-	if output.Error != nil {
-		return fmt.Errorf("error deleting Application Layer Automatic Protection (%s): %s", d.Id(), output.Error)
+	if err != nil {
+		return fmt.Errorf("error deleting Application Layer Automatic Protection (%s): %s", d.Id(), err)
 	}
 	return nil
 }
