d3d3e712409371cd8a10a03c66b62d96134e9b2d
Adrian Johnson <adrian.johnson@hashicorp.com>
2022-09-29 13:36:01 -0500

    ds/aws_kms_custom_key_store: update Read

--- internal/service/kms/custom_key_store_data_source.go
+++ internal/service/kms/custom_key_store_data_source.go
@@ -1,33 +1,35 @@
 package kms
 
 import (
-	"fmt"
+	"context"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/kms"
-	"github.com/hashicorp/aws-sdk-go-base/v2/awsv1shim/v2/tfawserr"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
-	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
+	"github.com/hashicorp/terraform-provider-aws/internal/create"
+	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
 func DataSourceCustomKeyStore() *schema.Resource {
 	return &schema.Resource{
-		Read: dataSourceCustomKeyStoreRead,
+		ReadContext: dataSourceCustomKeyStoreRead,
 		Schema: map[string]*schema.Schema{
-			"id": {
-				Type:     schema.TypeString,
-				Optional: true,
-				Computed: true,
+			"custom_key_store_id": {
+				Type:          schema.TypeString,
+				Optional:      true,
+				Computed:      true,
+				ConflictsWith: []string{"custom_key_store_name"},
 			},
-			"name": {
-				Type:     schema.TypeString,
-				Optional: true,
-				Computed: true,
+			"custom_key_store_name": {
+				Type:          schema.TypeString,
+				Optional:      true,
+				Computed:      true,
+				ConflictsWith: []string{"custom_key_store_id"},
 			},
-			"cloudhsm_cluster_id": {
+			"cloud_hsm_cluster_id": {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
@@ -47,43 +49,35 @@ func DataSourceCustomKeyStore() *schema.Resource {
 	}
 }
 
-func dataSourceCustomKeyStoreRead(d *schema.ResourceData, meta interface{}) error {
+const (
+	DSNameCustomKeyStore = "Custom Key Store"
+)
+
+func dataSourceCustomKeyStoreRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	conn := meta.(*conns.AWSClient).KMSConn
 
 	input := &kms.DescribeCustomKeyStoresInput{}
 
-	if v, ok := d.GetOk("id"); ok {
+	var ksID string
+	if v, ok := d.GetOk("custom_key_store_id"); ok {
 		input.CustomKeyStoreId = aws.String(v.(string))
+		ksID = v.(string)
 	}
-	if v, ok := d.GetOk("name"); ok {
+	if v, ok := d.GetOk("custom_key_store_name"); ok {
 		input.CustomKeyStoreName = aws.String(v.(string))
+		ksID = v.(string)
 	}
 
-	output, err := conn.DescribeCustomKeyStores(input)
-
-	if tfawserr.ErrCodeEquals(err, kms.ErrCodeCustomKeyStoreNotFoundException) {
-		return &resource.NotFoundError{
-			LastError:   err,
-			LastRequest: input,
-		}
-	}
+	keyStore, err := FindCustomKeyStoreByID(ctx, conn, input)
 
 	if err != nil {
-		return fmt.Errorf("error reading KMS Custom Key Store: %w", err)
+		return create.DiagError(names.KMS, create.ErrActionReading, DSNameCustomKeyStore, ksID, err)
 	}
 
-	if output == nil || len(output.CustomKeyStores) == 0 || output.CustomKeyStores[0] == nil {
-		return tfresource.NewEmptyResultError(input)
-	}
-
-	if count := len(output.CustomKeyStores); count > 1 {
-		return tfresource.NewTooManyResultsError(count, input)
-	}
-
-	keyStore := output.CustomKeyStores[0]
 	d.SetId(aws.StringValue(keyStore.CustomKeyStoreId))
-	d.Set("name", keyStore.CustomKeyStoreName)
-	d.Set("cloudhsm_cluster_id", keyStore.CloudHsmClusterId)
+	d.Set("custom_key_store_name", keyStore.CustomKeyStoreName)
+	d.Set("custom_key_store_id", keyStore.CustomKeyStoreId)
+	d.Set("cloud_hsm_cluster_id", keyStore.CloudHsmClusterId)
 	d.Set("connection_state", keyStore.ConnectionState)
 	d.Set("creation_date", keyStore.CreationDate.Format(time.RFC3339))
 	d.Set("trust_anchor_certificate", keyStore.TrustAnchorCertificate)
