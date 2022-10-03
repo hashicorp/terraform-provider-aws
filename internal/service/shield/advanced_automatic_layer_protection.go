a393a1614b8feb97e3fc24224e3082457595f940
Dennis Vinterfjärd <dennis.hedlund@klarna.com>
2022-10-03 11:27:20 +0200

    Removing all complications around resourceAdvancedAutomaticLayerProtectionDelete ( trying to circomvent WAFOptimisticLockException )

--- internal/service/shield/advanced_automatic_layer_protection.go
+++ internal/service/shield/advanced_automatic_layer_protection.go
@@ -9,7 +9,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"log"
-	"time"
 )
 
 func ResourceAdvancedAutomaticLayerProtection() *schema.Resource {
@@ -127,28 +126,6 @@ func resourceAdvancedAutomaticLayerProtectionRead(d *schema.ResourceData, meta i
 func resourceAdvancedAutomaticLayerProtectionDelete(d *schema.ResourceData, meta interface{}) error {
 	shieldConn := meta.(*conns.AWSClient).ShieldConn
 
-	/*
-		cloudfrontConn := meta.(*conns.AWSClient).CloudFrontConn
-
-		splitResourceArn := strings.Split(d.Get("resource_arn").(string), "/")
-		cloudfrontID := splitResourceArn[len(splitResourceArn)-1]
-
-
-			cloudfrontInformation, err := cloudfrontConn.GetDistribution(&cloudfront.GetDistributionInput{
-				Id: aws.String(cloudfrontID),
-			})
-			if err != nil {
-				return fmt.Errorf("error describing Protection (%s): %s", d.Get("resource_arn").(string), err)
-			}
-
-				webAclARN, err := arn.Parse(aws.StringValue(cloudfrontInformation.Distribution.DistributionConfig.WebACLId))
-				if err != nil {
-					return fmt.Errorf("unable to parse WebACLId (%s): %s",
-						aws.StringValue(cloudfrontInformation.Distribution.DistributionConfig.WebACLId),
-						err,
-					)
-				}
-	*/
 	input := &shield.DisableApplicationLayerAutomaticResponseInput{
 		ResourceArn: aws.String(d.Id()),
 	}
@@ -158,64 +135,5 @@ func resourceAdvancedAutomaticLayerProtectionDelete(d *schema.ResourceData, meta
 		return fmt.Errorf("error deleting Application Layer Automatic Protection (%s): %s", d.Id(), err)
 	}
 
-	time.Sleep(15 * time.Second)
-	/*
-		if err := AdvancedAutomaticLayerProtectionWaitUntilDeAssociated(
-			webAclARN,
-			meta); err != nil {
-			return fmt.Errorf("error waiting for shield advanced waf rule de-association (%s): %s", d.Id(), err)
-		}
-	*/
-
 	return nil
 }
-
-/*
-// resourceWAFStateRefreshFunc blocks until AWS has removed AWS Shield WAF rule from webACL
-// before considering AdvancedAutomaticLayerProtection removed. This is to avoid WAFOptimisticLockException that
-// happens when trying to remove WAF resource immediately after AdvancedAutomaticLayerProtection.
-func AdvancedAutomaticLayerProtectionWaitUntilDeAssociated(webACLArn arn.ARN, meta interface{}) error {
-	stateConf := &resource.StateChangeConf{
-		Refresh:    resourceWAFStateRefreshFunc(webACLArn, meta),
-		Pending:    []string{"Associated"},
-		Target:     []string{"NotAssociated"},
-		Timeout:    30 * time.Second,
-		MinTimeout: 5 * time.Second,
-	}
-	_, err := stateConf.WaitForState()
-	return err
-}
-
-func resourceWAFStateRefreshFunc(webACLArn arn.ARN, meta interface{}) resource.StateRefreshFunc {
-	return func() (interface{}, string, error) {
-		conn := meta.(*conns.AWSClient).WAFV2Conn
-
-		splitWebACLArn := strings.Split(webACLArn.Resource, "/")
-		webACLId := splitWebACLArn[len(splitWebACLArn)-1]
-		webACLName := splitWebACLArn[len(splitWebACLArn)-2]
-
-		params := &wafv2.GetWebACLInput{
-			Id:    aws.String(webACLId),
-			Name:  aws.String(webACLName),
-			Scope: aws.String("CLOUDFRONT"),
-		}
-
-		resp, err := conn.GetWebACL(params)
-		if err != nil {
-			log.Printf("[WARN] Error retrieving WEBACL %q details: %s", webACLId, err)
-			return nil, "", err
-		}
-
-		if resp == nil {
-			return nil, "", nil
-		}
-
-		//TODO Implement regexp match
-		for _, rule := range resp.WebACL.Rules {
-
-		}
-
-		return resp.WebACL, "NotAssociated", nil
-	}
-}
-*/
