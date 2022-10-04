07fa085039a15d83e59075da6e01ee385cd48d8c
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-10-04 17:13:34 -0400

    r/aws_s3control_storage_lens_configuration: Start to add support for configuration.

--- internal/service/s3control/storage_lens_configuration.go
+++ internal/service/s3control/storage_lens_configuration.go
@@ -45,6 +45,106 @@ func ResourceStorageLensConfiguration() *schema.Resource {
 				Required: true,
 				ForceNew: true,
 			},
+			"storage_lens_configuration": {
+				Type:     schema.TypeList,
+				Required: true,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"account_level": {
+							Type:     schema.TypeList,
+							Required: true,
+							MaxItems: 1,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"activity_metrics": {
+										Type:     schema.TypeList,
+										Optional: true,
+										MaxItems: 1,
+										Elem: &schema.Resource{
+											Schema: map[string]*schema.Schema{
+												"enabled": {
+													Type:     schema.TypeBool,
+													Optional: true,
+												},
+											},
+										},
+									},
+									"bucket_level": {
+										Type:     schema.TypeList,
+										Required: true,
+										MaxItems: 1,
+										Elem: &schema.Resource{
+											Schema: map[string]*schema.Schema{
+												"activity_metrics": {
+													Type:     schema.TypeList,
+													Optional: true,
+													MaxItems: 1,
+													Elem: &schema.Resource{
+														Schema: map[string]*schema.Schema{
+															"enabled": {
+																Type:     schema.TypeBool,
+																Optional: true,
+															},
+														},
+													},
+												},
+												"prefix_level": {
+													Type:     schema.TypeList,
+													Optional: true,
+													MaxItems: 1,
+													Elem: &schema.Resource{
+														Schema: map[string]*schema.Schema{
+															"storage_metrics": {
+																Type:     schema.TypeList,
+																Optional: true,
+																MaxItems: 1,
+																Elem: &schema.Resource{
+																	Schema: map[string]*schema.Schema{
+																		"enabled": {
+																			Type:     schema.TypeBool,
+																			Optional: true,
+																		},
+																		"selection_criteria": {
+																			Type:     schema.TypeList,
+																			Optional: true,
+																			MaxItems: 1,
+																			Elem: &schema.Resource{
+																				Schema: map[string]*schema.Schema{
+																					"delimiter": {
+																						Type:     schema.TypeString,
+																						Optional: true,
+																					},
+																					"max_depth": {
+																						Type:     schema.TypeInt,
+																						Optional: true,
+																					},
+																					"min_storage_bytes_percentage": {
+																						Type:     schema.TypeFloat,
+																						Optional: true,
+																					},
+																				},
+																			},
+																		},
+																	},
+																},
+															},
+														},
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+						"enabled": {
+							Type:     schema.TypeBool,
+							Required: true,
+						},
+					},
+				},
+			},
 			"tags":     tftags.TagsSchema(),
 			"tags_all": tftags.TagsSchemaComputed(),
 		},
@@ -70,10 +170,16 @@ func resourceStorageLensConfigurationCreate(ctx context.Context, d *schema.Resou
 		ConfigId:  aws.String(configID),
 	}
 
+	if v, ok := d.GetOk("storage_lens_configuration"); ok && len(v.([]interface{})) > 0 && v.([]interface{})[0] != nil {
+		input.StorageLensConfiguration = expandStorageLensConfiguration(v.([]interface{})[0].(map[string]interface{}))
+		input.StorageLensConfiguration.Id = aws.String(configID)
+	}
+
 	if len(tags) > 0 {
 		input.Tags = StorageLensTags(tags.IgnoreAWS())
 	}
 
+	log.Printf("[DEBUG] Creating S3 Storage Lens Configuration: %s", input)
 	_, err := conn.PutStorageLensConfigurationWithContext(ctx, input)
 
 	if err != nil {
@@ -109,7 +215,11 @@ func resourceStorageLensConfigurationRead(ctx context.Context, d *schema.Resourc
 	}
 
 	d.Set("account_id", accountID)
+	d.Set("arn", output.StorageLensArn)
 	d.Set("config_id", configID)
+	if err := d.Set("storage_lens_configuration", []interface{}{flattenStorageLensConfiguration(output)}); err != nil {
+		return diag.Errorf("setting storage_lens_configuration: %s", err)
+	}
 
 	tags, err := storageLensConfigurationListTags(ctx, conn, accountID, configID)
 
@@ -138,6 +248,25 @@ func resourceStorageLensConfigurationUpdate(ctx context.Context, d *schema.Resou
 
 	if err != nil {
 		return diag.FromErr(err)
+	}
+
+	if d.HasChangesExcept("tags", "tags_all") {
+		input := &s3control.PutStorageLensConfigurationInput{
+			AccountId: aws.String(accountID),
+			ConfigId:  aws.String(configID),
+		}
+
+		if v, ok := d.GetOk("storage_lens_configuration"); ok && len(v.([]interface{})) > 0 && v.([]interface{})[0] != nil {
+			input.StorageLensConfiguration = expandStorageLensConfiguration(v.([]interface{})[0].(map[string]interface{}))
+			input.StorageLensConfiguration.Id = aws.String(configID)
+		}
+
+		log.Printf("[DEBUG] Updating S3 Storage Lens Configuration: %s", input)
+		_, err := conn.PutStorageLensConfigurationWithContext(ctx, input)
+
+		if err != nil {
+			return diag.Errorf("updating S3 Storage Lens Configuration (%s): %s", d.Id(), err)
+		}
 	}
 
 	if d.HasChange("tags_all") {
@@ -194,4 +323,250 @@ func StorageLensConfigurationParseResourceID(id string) (string, string, error) 
 	}
 
 	return "", "", fmt.Errorf("unexpected format for ID (%[1]s), expected account-id%[2]sconfig-id", id, storageLensConfigurationResourceIDSeparator)
+}
+
+func expandStorageLensConfiguration(tfMap map[string]interface{}) *s3control.StorageLensConfiguration {
+	if tfMap == nil {
+		return nil
+	}
+
+	apiObject := &s3control.StorageLensConfiguration{}
+
+	if v, ok := tfMap["account_level"].([]interface{}); ok && len(v) > 0 && v[0] != nil {
+		apiObject.AccountLevel = expandAccountLevel(v[0].(map[string]interface{}))
+	}
+
+	if v, ok := tfMap["enabled"].(bool); ok && v {
+		apiObject.IsEnabled = aws.Bool(v)
+	}
+
+	return apiObject
+}
+
+func expandAccountLevel(tfMap map[string]interface{}) *s3control.AccountLevel {
+	if tfMap == nil {
+		return nil
+	}
+
+	apiObject := &s3control.AccountLevel{}
+
+	if v, ok := tfMap["activity_metrics"].([]interface{}); ok && len(v) > 0 && v[0] != nil {
+		apiObject.ActivityMetrics = expandActivityMetrics(v[0].(map[string]interface{}))
+	}
+
+	if v, ok := tfMap["bucket_level"].([]interface{}); ok && len(v) > 0 && v[0] != nil {
+		apiObject.BucketLevel = expandBucketLevel(v[0].(map[string]interface{}))
+	} else {
+		apiObject.BucketLevel = &s3control.BucketLevel{}
+	}
+
+	return apiObject
+}
+
+func expandActivityMetrics(tfMap map[string]interface{}) *s3control.ActivityMetrics {
+	if tfMap == nil {
+		return nil
+	}
+
+	apiObject := &s3control.ActivityMetrics{}
+
+	if v, ok := tfMap["enabled"].(bool); ok && v {
+		apiObject.IsEnabled = aws.Bool(v)
+	}
+
+	return apiObject
+}
+
+func expandBucketLevel(tfMap map[string]interface{}) *s3control.BucketLevel {
+	if tfMap == nil {
+		return nil
+	}
+
+	apiObject := &s3control.BucketLevel{}
+
+	if v, ok := tfMap["activity_metrics"].([]interface{}); ok && len(v) > 0 && v[0] != nil {
+		apiObject.ActivityMetrics = expandActivityMetrics(v[0].(map[string]interface{}))
+	}
+
+	if v, ok := tfMap["prefix_level"].([]interface{}); ok && len(v) > 0 && v[0] != nil {
+		apiObject.PrefixLevel = expandPrefixLevel(v[0].(map[string]interface{}))
+	}
+
+	return apiObject
+}
+
+func expandPrefixLevel(tfMap map[string]interface{}) *s3control.PrefixLevel {
+	if tfMap == nil {
+		return nil
+	}
+
+	apiObject := &s3control.PrefixLevel{}
+
+	if v, ok := tfMap["activity_metrics"].([]interface{}); ok && len(v) > 0 && v[0] != nil {
+		apiObject.StorageMetrics = expandPrefixLevelStorageMetrics(v[0].(map[string]interface{}))
+	}
+
+	return apiObject
+}
+
+func expandPrefixLevelStorageMetrics(tfMap map[string]interface{}) *s3control.PrefixLevelStorageMetrics {
+	if tfMap == nil {
+		return nil
+	}
+
+	apiObject := &s3control.PrefixLevelStorageMetrics{}
+
+	if v, ok := tfMap["enabled"].(bool); ok && v {
+		apiObject.IsEnabled = aws.Bool(v)
+	}
+
+	if v, ok := tfMap["selection_criteria"].([]interface{}); ok && len(v) > 0 && v[0] != nil {
+		apiObject.SelectionCriteria = expandSelectionCriteria(v[0].(map[string]interface{}))
+	}
+
+	return apiObject
+}
+
+func expandSelectionCriteria(tfMap map[string]interface{}) *s3control.SelectionCriteria {
+	if tfMap == nil {
+		return nil
+	}
+
+	apiObject := &s3control.SelectionCriteria{}
+
+	if v, ok := tfMap["delimiter"].(string); ok && v != "" {
+		apiObject.Delimiter = aws.String(v)
+	}
+
+	if v, ok := tfMap["max_depth"].(int); ok && v != 0 {
+		apiObject.MaxDepth = aws.Int64(int64(v))
+	}
+
+	if v, ok := tfMap["min_storage_bytes_percentage"].(float64); ok && v != 0.0 {
+		apiObject.MinStorageBytesPercentage = aws.Float64(v)
+	}
+
+	return apiObject
+}
+
+func flattenStorageLensConfiguration(apiObject *s3control.StorageLensConfiguration) map[string]interface{} {
+	if apiObject == nil {
+		return nil
+	}
+
+	tfMap := map[string]interface{}{}
+
+	if v := apiObject.AccountLevel; v != nil {
+		tfMap["account_level"] = []interface{}{flattenAccountLevel(v)}
+	}
+
+	if v := apiObject.IsEnabled; v != nil {
+		tfMap["enabled"] = aws.BoolValue(v)
+	}
+
+	return tfMap
+}
+
+func flattenAccountLevel(apiObject *s3control.AccountLevel) map[string]interface{} {
+	if apiObject == nil {
+		return nil
+	}
+
+	tfMap := map[string]interface{}{}
+
+	if v := apiObject.ActivityMetrics; v != nil {
+		tfMap["activity_metrics"] = []interface{}{flattenActivityMetrics(v)}
+	}
+
+	if v := apiObject.BucketLevel; v != nil {
+		tfMap["bucket_level"] = []interface{}{flattenBucketLevel(v)}
+	}
+
+	return tfMap
+}
+
+func flattenActivityMetrics(apiObject *s3control.ActivityMetrics) map[string]interface{} {
+	if apiObject == nil {
+		return nil
+	}
+
+	tfMap := map[string]interface{}{}
+
+	if v := apiObject.IsEnabled; v != nil {
+		tfMap["enabled"] = aws.BoolValue(v)
+	}
+
+	return tfMap
+}
+
+func flattenBucketLevel(apiObject *s3control.BucketLevel) map[string]interface{} {
+	if apiObject == nil {
+		return nil
+	}
+
+	tfMap := map[string]interface{}{}
+
+	if v := apiObject.ActivityMetrics; v != nil {
+		tfMap["activity_metrics"] = []interface{}{flattenActivityMetrics(v)}
+	}
+
+	if v := apiObject.PrefixLevel; v != nil {
+		tfMap["prefix_level"] = []interface{}{flattenPrefixLevel(v)}
+	}
+
+	return tfMap
+}
+
+func flattenPrefixLevel(apiObject *s3control.PrefixLevel) map[string]interface{} {
+	if apiObject == nil {
+		return nil
+	}
+
+	tfMap := map[string]interface{}{}
+
+	if v := apiObject.StorageMetrics; v != nil {
+		tfMap["storage_metrics"] = []interface{}{flattenPrefixLevelStorageMetrics(v)}
+	}
+
+	return tfMap
+}
+
+func flattenPrefixLevelStorageMetrics(apiObject *s3control.PrefixLevelStorageMetrics) map[string]interface{} {
+	if apiObject == nil {
+		return nil
+	}
+
+	tfMap := map[string]interface{}{}
+
+	if v := apiObject.IsEnabled; v != nil {
+		tfMap["enabled"] = aws.BoolValue(v)
+	}
+
+	if v := apiObject.SelectionCriteria; v != nil {
+		tfMap["selection_criteria"] = []interface{}{flattenSelectionCriteria(v)}
+	}
+
+	return tfMap
+}
+
+func flattenSelectionCriteria(apiObject *s3control.SelectionCriteria) map[string]interface{} {
+	if apiObject == nil {
+		return nil
+	}
+
+	tfMap := map[string]interface{}{}
+
+	if v := apiObject.Delimiter; v != nil {
+		tfMap["delimiter"] = aws.StringValue(v)
+	}
+
+	if v := apiObject.MaxDepth; v != nil {
+		tfMap["max_depth"] = aws.Int64Value(v)
+	}
+
+	if v := apiObject.MinStorageBytesPercentage; v != nil {
+		tfMap["min_storage_bytes_percentage"] = aws.Float64Value(v)
+	}
+
+	return tfMap
 }
--- website/docs/r/s3control_storage_lens_configuration.html.markdown
+++ website/docs/r/s3control_storage_lens_configuration.html.markdown
@@ -17,7 +17,11 @@ resource "aws_s3control_storage_lens_configuration" "example" {
   config_id = "example-1"
 
   storage_lens_configuration {
-  
+    enabled = true
+
+    account_level {
+      bucket_level {}
+    }
   }
 }
 ```
@@ -34,6 +38,50 @@ The following arguments are supported:
 ### Storage Lens Configuration
 
 The `storage_lens_configuration` block supports the following:
+
+* `account_level` (Required) The account-level configurations of the S3 Storage Lens configuration. See [Account Level](#account-level) below for more details.
+* `enabled` (Required) Whether the S3 Storage Lens configuration is enabled.
+
+### Account Level
+
+The `account_level` block supports the following:
+
+* `activity_metrics` (Optional) S3 Storage Lens activity metrics. See [Activity Metrics](#activity-metrics) below for more details.
+* `bucket_level` (Required) S3 Storage Lens bucket-level configuration. See [Bucket Level](#bucket-level) below for more details.
+
+### Activity Metrics
+
+The `activity_metrics` block supports the following:
+
+* `enabled` (Optional) Whether the activity metrics are enabled.
+
+### Bucket Level
+
+The `bucket_level` block supports the following:
+
+* `activity_metrics` (Optional) S3 Storage Lens activity metrics. See [Activity Metrics](#activity-metrics) above for more details.
+* `prefix_level` (Optional) Prefix-level metrics for S3 Storage Lens. See [Prefix Level](#prefix-level) below for more details.
+
+### Prefix Level
+
+The `prefix_level` block supports the following:
+
+* `storage_metrics` (Optional) Prefix-level storage metrics for S3 Storage Lens. See [Prefix Level Storage Metrics](#prefix-level-storage-metrics) below for more details.
+
+### Prefix Level Storage Metrics
+
+The `storage_metrics` block supports the following:
+
+* `enabled` (Optional) Whether prefix-level storage metrics are enabled.
+* `selection_criteria` (Optional) Selection criteria. See [Selection Criteria](#selection-criteria) below for more details.
+
+### Selection Criteria
+
+The `selection_criteria` block supports the following:
+
+* `delimiter` (Optional) The delimiter of the selection criteria being used.
+* `max_depth` (Optional) The max depth of the selection criteria.
+* `min_storage_bytes_percentage` (Optional) The minimum number of storage bytes percentage whose metrics will be selected.
 
 ## Attributes Reference
 
