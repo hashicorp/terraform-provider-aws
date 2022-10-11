5bc8e2cec1706936f049467a1bda3fdc28a5f29e
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-10-11 17:27:58 -0400

    Add 'internal/fwplanmodifiers' package.

---
+++ internal/fwplanmodifiers/default_value.go
@@ -0,0 +1,38 @@
+package fwplanmodifiers
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+)
+
+type defaultValue struct {
+	value attr.Value
+}
+
+// DefaultValue return an AttributePlanModifier that sets the specified value if the planned value is Null.
+func DefaultValue(val attr.Value) tfsdk.AttributePlanModifier {
+	return defaultValue{
+		value: val,
+	}
+}
+
+func (m defaultValue) Description(context.Context) string {
+	return fmt.Sprintf("If value is not configured, defaults to %s", m.value)
+}
+
+func (m defaultValue) MarkdownDescription(ctx context.Context) string {
+	return m.Description(ctx)
+}
+
+func (m defaultValue) Modify(ctx context.Context, request tfsdk.ModifyAttributePlanRequest, response *tfsdk.ModifyAttributePlanResponse) {
+	if v, err := request.AttributePlan.ToTerraformValue(ctx); err != nil {
+		response.Diagnostics.AddAttributeError(request.AttributePath, "getting attribute value", err.Error())
+
+		return
+	} else if v.IsNull() {
+		response.AttributePlan = m.value
+	}
+}
