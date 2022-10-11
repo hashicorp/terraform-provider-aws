ec51e68be4577768562518b8aaa55698c48c63af
Luca Lombardo <lomluca12@gmail.com>
2022-10-11 16:26:13 +0200

    drop default value for acl s3 object

--- internal/service/s3/object.go
+++ internal/service/s3/object.go
@@ -51,7 +51,6 @@ func ResourceObject() *schema.Resource {
 		Schema: map[string]*schema.Schema{
 			"acl": {
 				Type:         schema.TypeString,
-				Default:      s3.ObjectCannedACLPrivate,
 				Optional:     true,
 				ValidateFunc: validation.StringInSlice(s3.ObjectCannedACL_Values(), false),
 			},
@@ -464,10 +463,13 @@ func resourceObjectUpload(d *schema.ResourceData, meta interface{}) error {
 	key := d.Get("key").(string)
 
 	input := &s3manager.UploadInput{
-		ACL:    aws.String(d.Get("acl").(string)),
 		Body:   body,
 		Bucket: aws.String(bucket),
 		Key:    aws.String(key),
+	}
+
+	if v, ok := d.GetOk("acl"); ok {
+		input.ACL = aws.String(v.(string))
 	}
 
 	if v, ok := d.GetOk("storage_class"); ok {
