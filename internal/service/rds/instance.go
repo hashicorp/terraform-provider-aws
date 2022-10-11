d5a767380233fedac0d0f99162f8a287b7ca9f14
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-10-11 08:30:30 -0400

    r/aws_db_instance: Fix CodeQL 'go/clear-text-logging'.

--- internal/service/rds/instance.go
+++ internal/service/rds/instance.go
@@ -849,7 +849,6 @@ func resourceInstanceCreate(d *schema.ResourceData, meta interface{}) error {
 			input.VpcSecurityGroupIds = flex.ExpandStringSet(v.(*schema.Set))
 		}
 
-		log.Printf("[DEBUG] Creating RDS DB Instance: %s", input)
 		_, err := tfresource.RetryWhen(propagationTimeout,
 			func() (interface{}, error) {
 				return conn.RestoreDBInstanceFromS3(input)
@@ -1434,7 +1433,6 @@ func resourceInstanceCreate(d *schema.ResourceData, meta interface{}) error {
 	if requiresModifyDbInstance {
 		modifyDbInstanceInput.DBInstanceIdentifier = aws.String(d.Id())
 
-		log.Printf("[INFO] Modifying RDS DB Instance: %s", modifyDbInstanceInput)
 		_, err := conn.ModifyDBInstance(modifyDbInstanceInput)
 
 		if err != nil {
@@ -1775,7 +1773,6 @@ func resourceInstanceUpdate(d *schema.ResourceData, meta interface{}) error {
 			}
 		}
 
-		log.Printf("[DEBUG] Updating DB Instance: %s", input)
 		_, err := tfresource.RetryWhen(d.Timeout(schema.TimeoutUpdate),
 			func() (interface{}, error) {
 				return conn.ModifyDBInstance(input)
