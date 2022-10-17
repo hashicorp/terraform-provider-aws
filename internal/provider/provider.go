2a27ddf14ac0fd185584417cb4e1c79a752fe84c
Jared Baker <jared.baker@hashicorp.com>
2022-10-17 11:29:19 -0400

    r/aws_sesv2_dedicated_ip_pool: add to resources map

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -2062,6 +2062,7 @@ func New(_ context.Context) (*schema.Provider, error) {
 			"aws_ses_template":                     ses.ResourceTemplate(),
 
 			"aws_sesv2_configuration_set": sesv2.ResourceConfigurationSet(),
+			"aws_sesv2_dedicated_ip_pool": sesv2.ResourceDedicatedIPPool(),
 
 			"aws_sfn_activity":      sfn.ResourceActivity(),
 			"aws_sfn_state_machine": sfn.ResourceStateMachine(),
