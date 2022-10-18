02032b6881b858fa375b99cfac0385a51a442c25
GlennChia <glennchia7@gmail.com>
2022-10-18 17:07:51 +0000

    feat(connect): inst storage config dsrc read

--- internal/service/connect/instance_storage_config_data_source.go
+++ internal/service/connect/instance_storage_config_data_source.go
@@ -1,13 +1,20 @@
 package connect
 
 import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/connect"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 )
 
 func DataSourceInstanceStorageConfig() *schema.Resource {
 	return &schema.Resource{
+		ReadContext: dataSourceInstanceStorageConfigRead,
 		Schema: map[string]*schema.Schema{
 			"association_id": {
 				Type:         schema.TypeString,
@@ -126,4 +133,38 @@ func DataSourceInstanceStorageConfig() *schema.Resource {
 			},
 		},
 	}
+}
+
+func dataSourceInstanceStorageConfigRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn := meta.(*conns.AWSClient).ConnectConn
+
+	associationId := d.Get("association_id").(string)
+	instanceId := d.Get("instance_id").(string)
+	resourceType := d.Get("resource_type").(string)
+
+	input := &connect.DescribeInstanceStorageConfigInput{
+		AssociationId: aws.String(associationId),
+		InstanceId:    aws.String(instanceId),
+		ResourceType:  aws.String(resourceType),
+	}
+
+	resp, err := conn.DescribeInstanceStorageConfigWithContext(ctx, input)
+
+	if err != nil {
+		return diag.Errorf("getting Connect Instance Storage Config for Connect Instance (%s,%s,%s): %s", associationId, instanceId, resourceType, err)
+	}
+
+	if resp == nil || resp.StorageConfig == nil {
+		return diag.Errorf("getting Connect Instance Storage Config: empty response")
+	}
+
+	storageConfig := resp.StorageConfig
+
+	if err := d.Set("storage_config", flattenStorageConfig(storageConfig)); err != nil {
+		return diag.Errorf("setting storage_config: %s", err)
+	}
+
+	d.SetId(fmt.Sprintf("%s:%s:%s", instanceId, associationId, resourceType))
+
+	return nil
 }
