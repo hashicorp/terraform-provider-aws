96ef614940148e45cc586bc5abc56c63e5f72a96
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-10-18 12:51:04 -0400

    r/aws_simpledb_domain: Correct ImportState.
    Acceptance test output:
    
    % make testacc TESTARGS='-run=TestAccSimpleDBDomain_' PKG=simpledb
    ==> Checking that code complies with gofmt requirements...
    TF_ACC=1 go test ./internal/service/simpledb/... -v -count 1 -parallel 20  -run=TestAccSimpleDBDomain_ -timeout 180m
    === RUN   TestAccSimpleDBDomain_basic
    === PAUSE TestAccSimpleDBDomain_basic
    === RUN   TestAccSimpleDBDomain_disappears
    === PAUSE TestAccSimpleDBDomain_disappears
    === CONT  TestAccSimpleDBDomain_basic
    === CONT  TestAccSimpleDBDomain_disappears
    --- PASS: TestAccSimpleDBDomain_disappears (14.73s)
    --- PASS: TestAccSimpleDBDomain_basic (19.45s)
    PASS
    ok  	github.com/hashicorp/terraform-provider-aws/internal/service/simpledb	23.698s

--- internal/service/simpledb/domain.go
+++ internal/service/simpledb/domain.go
@@ -7,6 +7,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/simpledb"
 	"github.com/hashicorp/aws-sdk-go-base/v2/awsv1shim/v2/tfawserr"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
 	"github.com/hashicorp/terraform-plugin-framework/types"
@@ -126,15 +127,7 @@ func (r *resourceDomain) Read(ctx context.Context, request resource.ReadRequest,
 // Update is called to update the state of the resource.
 // Config, planned state, and prior state values should be read from the UpdateRequest and new state values set on the UpdateResponse.
 func (r *resourceDomain) Update(ctx context.Context, request resource.UpdateRequest, response *resource.UpdateResponse) {
-	var data resourceDomainData
-
-	response.Diagnostics.Append(request.Plan.Get(ctx, &data)...)
-
-	if response.Diagnostics.HasError() {
-		return
-	}
-
-	response.Diagnostics.Append(response.State.Set(ctx, &data)...)
+	// Noop.
 }
 
 // Delete is called when the provider must delete the resource.
@@ -160,6 +153,15 @@ func (r *resourceDomain) Delete(ctx context.Context, request resource.DeleteRequ
 
 		return
 	}
+}
+
+// ImportState is called when the provider must import the state of a resource instance.
+// This method must return enough state so the Read method can properly refresh the full resource.
+//
+// If setting an attribute with the import identifier, it is recommended to use the ImportStatePassthroughID() call in this method.
+func (r *resourceDomain) ImportState(ctx context.Context, request resource.ImportStateRequest, response *resource.ImportStateResponse) {
+	response.Diagnostics.Append(response.State.SetAttribute(ctx, path.Root("id"), request.ID)...)
+	response.Diagnostics.Append(response.State.SetAttribute(ctx, path.Root("name"), request.ID)...)
 }
 
 type resourceDomainData struct {
