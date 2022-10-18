43ab1d2bd76c35f5cc132eda323d935abe9ea7d6
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-10-18 16:43:01 -0400

    r/aws_sqs_queue: Change 'sqs_managed_sse_enabled' to Optional+Computed.

--- internal/service/sqs/queue.go
+++ internal/service/sqs/queue.go
@@ -133,6 +133,7 @@ var (
 		"sqs_managed_sse_enabled": {
 			Type:          schema.TypeBool,
 			Optional:      true,
+			Computed:      true,
 			ConflictsWith: []string{"kms_master_key_id"},
 		},
 		"tags":     tftags.TagsSchema(),
