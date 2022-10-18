a8289439ff487be042b983af56e85a7c189aa15b
Schuyler Jager <schjager@amazon.com>
2022-10-18 11:28:29 -0700

    Fix JSON validation for Core Network Policy Docs, Valid4ByteASN capitalization in validation tests

--- internal/verify/validate_test.go
+++ internal/verify/validate_test.go
@@ -19,7 +19,7 @@ func TestValid4ByteASN(t *testing.T) {
 		"4294967295",
 	}
 	for _, v := range validAsns {
-		_, errors := valid4ByteASN(v, "bgp_asn")
+		_, errors := Valid4ByteASN(v, "bgp_asn")
 		if len(errors) != 0 {
 			t.Fatalf("%q should be a valid ASN: %q", v, errors)
 		}
@@ -33,7 +33,7 @@ func TestValid4ByteASN(t *testing.T) {
 		"9999999999",
 	}
 	for _, v := range invalidAsns {
-		_, errors := valid4ByteASN(v, "bgp_asn")
+		_, errors := Valid4ByteASN(v, "bgp_asn")
 		if len(errors) == 0 {
 			t.Fatalf("%q should be an invalid ASN", v)
 		}
