d6fb21f6f558f4f4c8644839875c4f720de70419
Glenn <glennchia7@gmail.com>
2022-10-22 04:26:37 +0800

    refactor(cwevi): type fixes, err handling in status
    Co-authored-by: Adrian Johnson <adrian.johnson@hashicorp.com>

--- internal/service/evidently/status.go
+++ internal/service/evidently/status.go
@@ -5,16 +5,16 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/cloudwatchevidently"
-	"github.com/hashicorp/aws-sdk-go-base/v2/awsv1shim/v2/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 )
 
 func statusProject(conn *cloudwatchevidently.CloudWatchEvidently, id string) resource.StateRefreshFunc {
 	return func() (interface{}, string, error) {
 		output, err := FindProjectByNameOrARN(context.Background(), conn, id)
 
-		if tfawserr.ErrCodeEquals(err, cloudwatchevidently.ErrCodeResourceNotFoundException) {
-			return output, cloudwatchevidently.ErrCodeResourceNotFoundException, nil
+		if tfresource.NotFound(err) {
+			return nil, "", nil
 		}
 
 		if err != nil {
--- internal/service/evidently/wait.go
+++ internal/service/evidently/wait.go
@@ -7,7 +7,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-func waitProjectCreated(conn *cloudwatchevidently.CloudWatchEvidently, nameOrARN string, timeout time.Duration) (*cloudwatchevidently.GetProjectOutput, error) {
+func waitProjectCreated(conn *cloudwatchevidently.CloudWatchEvidently, nameOrARN string, timeout time.Duration) (*cloudwatchevidently.Project, error) {
 	stateConf := &resource.StateChangeConf{
 		Pending: []string{},
 		Target:  []string{cloudwatchevidently.ProjectStatusAvailable},
@@ -17,14 +17,14 @@ func waitProjectCreated(conn *cloudwatchevidently.CloudWatchEvidently, nameOrARN
 
 	outputRaw, err := stateConf.WaitForState()
 
-	if output, ok := outputRaw.(*cloudwatchevidently.GetProjectOutput); ok {
+	if output, ok := outputRaw.(*cloudwatchevidently.Project); ok {
 		return output, err
 	}
 
 	return nil, err
 }
 
-func waitProjectUpdated(conn *cloudwatchevidently.CloudWatchEvidently, nameOrARN string, timeout time.Duration) (*cloudwatchevidently.GetProjectOutput, error) { //nolint:unparam
+func waitProjectUpdated(conn *cloudwatchevidently.CloudWatchEvidently, nameOrARN string, timeout time.Duration) (*cloudwatchevidently.Project, error) { //nolint:unparam
 	stateConf := &resource.StateChangeConf{
 		Pending: []string{cloudwatchevidently.ProjectStatusUpdating},
 		Target:  []string{cloudwatchevidently.ProjectStatusAvailable},
@@ -34,24 +34,24 @@ func waitProjectUpdated(conn *cloudwatchevidently.CloudWatchEvidently, nameOrARN
 
 	outputRaw, err := stateConf.WaitForState()
 
-	if output, ok := outputRaw.(*cloudwatchevidently.GetProjectOutput); ok {
+	if output, ok := outputRaw.(*cloudwatchevidently.Project); ok {
 		return output, err
 	}
 
 	return nil, err
 }
 
-func waitProjectDeleted(conn *cloudwatchevidently.CloudWatchEvidently, nameOrARN string, timeout time.Duration) (*cloudwatchevidently.GetProjectOutput, error) {
+func waitProjectDeleted(conn *cloudwatchevidently.CloudWatchEvidently, nameOrARN string, timeout time.Duration) (*cloudwatchevidently.Project, error) {
 	stateConf := &resource.StateChangeConf{
 		Pending: []string{cloudwatchevidently.ProjectStatusAvailable},
-		Target:  []string{cloudwatchevidently.ErrCodeResourceNotFoundException},
+		Target:  []string{},
 		Refresh: statusProject(conn, nameOrARN),
 		Timeout: timeout,
 	}
 
 	outputRaw, err := stateConf.WaitForState()
 
-	if output, ok := outputRaw.(*cloudwatchevidently.GetProjectOutput); ok {
+	if output, ok := outputRaw.(*cloudwatchevidently.Project); ok {
 		return output, err
 	}
 
