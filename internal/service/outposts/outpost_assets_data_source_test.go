71a76e052421e152bde0526596c54be98da4957d
Kevin Pena <kpena027@fiu.edu>
2022-10-20 17:33:02 -0700

    add host filter test

--- internal/service/outposts/outpost_assets_data_source_test.go
+++ internal/service/outposts/outpost_assets_data_source_test.go
@@ -1,6 +1,7 @@
 package outposts_test
 
 import (
+	"os"
 	"regexp"
 	"testing"
 
@@ -10,6 +11,11 @@ import (
 )
 
 func TestAccOutpostsAssetsDataSource_id(t *testing.T) {
+	key := "OUTPOST_AVAIL"
+	outpostBool := os.Getenv(key)
+	if outpostBool == "" {
+		t.Skipf("Environment variable %s is not set", key)
+	}
 	dataSourceName := "data.aws_outposts_assets.test"
 
 	resource.ParallelTest(t, resource.TestCase{
@@ -23,10 +29,50 @@ func TestAccOutpostsAssetsDataSource_id(t *testing.T) {
 					acctest.MatchResourceAttrRegionalARN(dataSourceName, "arn", "outposts", regexp.MustCompile(`outpost/.+`)),
 				),
 			},
+		},
+	})
+}
+
+func TestAccOutpostsAssetsDataSource_statusFilter(t *testing.T) {
+	key := "OUTPOST_AVAIL"
+	outpostBool := os.Getenv(key)
+	if outpostBool == "" {
+		t.Skipf("Environment variable %s is not set", key)
+	}
+	dataSourceName := "data.aws_outposts_assets.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(t); acctest.PreCheckOutpostsOutposts(t) },
+		ErrorCheck:               acctest.ErrorCheck(t, outposts.EndpointsID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		Steps: []resource.TestStep{
 			{
 				Config: testAccOutpostAssetsDataSourceConfig_statusFilter(),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(dataSourceName, "status_id_filter.0", "ACTIVE"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccOutpostsAssetsDataSource_hostFilter(t *testing.T) {
+	key := "OUTPOST_HOST_ID"
+	outpostHostId := os.Getenv(key)
+	if outpostHostId == "" {
+		t.Skipf("Environment variable %s is not set", key)
+	}
+	dataSourceName := "data.aws_outposts_assets.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(t); acctest.PreCheckOutpostsOutposts(t) },
+		ErrorCheck:               acctest.ErrorCheck(t, outposts.EndpointsID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccOutpostAssetsDataSourceConfig_hostFilter(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckTypeSetElemAttr(dataSourceName, "host_id_filter.*", outpostHostId),
 				),
 			},
 		},
@@ -53,8 +99,23 @@ data "aws_outposts_assets" "source" {
 }
 
 data "aws_outposts_assets" "test" {
-  arn = data.aws_outposts_assets.source.arn
+  arn              = data.aws_outposts_assets.source.arn
   status_id_filter = ["ACTIVE"]
+  }
+`
 }
+
+func testAccOutpostAssetsDataSourceConfig_hostFilter() string {
+	return `
+data "aws_outposts_outposts" "test" {}
+
+data "aws_outposts_assets" "source" {
+  arn = tolist(data.aws_outposts_outposts.test.arns)[0]
+}
+
+data "aws_outposts_assets" "test" {
+  arn            = data.aws_outposts_assets.source.arn
+  host_id_filter = ["h-x38g5n0yd2a0ueb61"]
+  }
 `
 }
