092e8c5becdd0ab590373e7817434a2e1caea50e
Adrian Johnson <adrian.johnson@hashicorp.com>
2022-10-24 13:03:24 -0400

    update sweeper for framework

--- internal/sweep/framework.go
+++ internal/sweep/framework.go
@@ -14,18 +14,19 @@ import (
 	"github.com/hashicorp/terraform-plugin-go/tftypes"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
+	"github.com/hashicorp/terraform-provider-aws/internal/experimental/intf"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 )
 
 // Terraform Plugin Framework variants of sweeper helpers.
 
 type SweepFrameworkResource struct {
-	factory func(context.Context) (fwresource.ResourceWithConfigure, error)
+	factory func(context.Context) (intf.ResourceWithConfigureAndImportState, error)
 	id      string // TODO Currently we can only delete a resource if "id" is the only attribute used.
 	meta    interface{}
 }
 
-func NewSweepFrameworkResource(factory func(context.Context) (fwresource.ResourceWithConfigureAndImportState, error), id string, meta interface{}) *SweepFrameworkResource {
+func NewSweepFrameworkResource(factory func(context.Context) (intf.ResourceWithConfigureAndImportState, error), id string, meta interface{}) *SweepFrameworkResource {
 	return &SweepFrameworkResource{
 		factory: factory,
 		id:      id,
@@ -57,7 +58,7 @@ func (sr *SweepFrameworkResource) Delete(ctx context.Context, rc RetryConfig) er
 
 }
 
-func DeleteFrameworkResource(factory func(context.Context) (fwresource.ResourceWithConfigure, error), id string, meta interface{}) error {
+func DeleteFrameworkResource(factory func(context.Context) (intf.ResourceWithConfigureAndImportState, error), id string, meta interface{}) error {
 	ctx := context.Background()
 
 	resource, err := factory(ctx)
