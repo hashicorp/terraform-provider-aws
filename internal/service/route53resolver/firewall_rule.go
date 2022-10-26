f9889315fa4b0933fbc8c6b8296b4a57a44eff61
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-10-26 08:54:03 -0400

    r/aws_route53_resolver_firewall_rule: 'FindFirewallRuleByTwoPartKey' return 'NotFoundError'.
    Acceptance test output:
    
    % make testacc TESTARGS='-run=TestAccRoute53ResolverFirewallRule_' PKG=route53resolver ACCTEST_PARALLELISM=3
    ==> Checking that code complies with gofmt requirements...
    TF_ACC=1 go test ./internal/service/route53resolver/... -v -count 1 -parallel 3  -run=TestAccRoute53ResolverFirewallRule_ -timeout 180m
    === RUN   TestAccRoute53ResolverFirewallRule_basic
    === PAUSE TestAccRoute53ResolverFirewallRule_basic
    === RUN   TestAccRoute53ResolverFirewallRule_block
    === PAUSE TestAccRoute53ResolverFirewallRule_block
    === RUN   TestAccRoute53ResolverFirewallRule_blockOverride
    === PAUSE TestAccRoute53ResolverFirewallRule_blockOverride
    === RUN   TestAccRoute53ResolverFirewallRule_disappears
    === PAUSE TestAccRoute53ResolverFirewallRule_disappears
    === CONT  TestAccRoute53ResolverFirewallRule_basic
    === CONT  TestAccRoute53ResolverFirewallRule_blockOverride
    === CONT  TestAccRoute53ResolverFirewallRule_block
    --- PASS: TestAccRoute53ResolverFirewallRule_blockOverride (74.80s)
    === CONT  TestAccRoute53ResolverFirewallRule_disappears
    --- PASS: TestAccRoute53ResolverFirewallRule_block (74.90s)
    --- PASS: TestAccRoute53ResolverFirewallRule_basic (76.96s)
    --- PASS: TestAccRoute53ResolverFirewallRule_disappears (50.62s)
    PASS
    ok  	github.com/hashicorp/terraform-provider-aws/internal/service/route53resolver	129.638s

--- internal/service/route53resolver/firewall_rule.go
+++ internal/service/route53resolver/firewall_rule.go
@@ -3,6 +3,7 @@ package route53resolver
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/route53resolver"
@@ -11,6 +12,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 )
 
 func ResourceFirewallRule() *schema.Resource {
@@ -19,61 +21,54 @@ func ResourceFirewallRule() *schema.Resource {
 		Read:   resourceFirewallRuleRead,
 		Update: resourceFirewallRuleUpdate,
 		Delete: resourceFirewallRuleDelete,
+
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
 		},
 
 		Schema: map[string]*schema.Schema{
-			"name": {
-				Type:         schema.TypeString,
-				Required:     true,
-				ValidateFunc: validResolverName,
-			},
-
 			"action": {
 				Type:         schema.TypeString,
 				Required:     true,
 				ValidateFunc: validation.StringInSlice(route53resolver.Action_Values(), false),
 			},
-
 			"block_override_dns_type": {
 				Type:         schema.TypeString,
 				Optional:     true,
 				ValidateFunc: validation.StringInSlice(route53resolver.BlockOverrideDnsType_Values(), false),
 			},
-
 			"block_override_domain": {
 				Type:         schema.TypeString,
 				Optional:     true,
 				ValidateFunc: validation.StringLenBetween(1, 255),
 			},
-
 			"block_override_ttl": {
 				Type:         schema.TypeInt,
 				Optional:     true,
 				ValidateFunc: validation.IntBetween(0, 604800),
 			},
-
 			"block_response": {
 				Type:         schema.TypeString,
 				Optional:     true,
 				ValidateFunc: validation.StringInSlice(route53resolver.BlockResponse_Values(), false),
 			},
-
 			"firewall_domain_list_id": {
 				Type:         schema.TypeString,
 				ForceNew:     true,
 				Required:     true,
 				ValidateFunc: validation.StringLenBetween(1, 64),
 			},
-
 			"firewall_rule_group_id": {
 				Type:         schema.TypeString,
 				ForceNew:     true,
 				Required:     true,
 				ValidateFunc: validation.StringLenBetween(1, 64),
 			},
-
+			"name": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validResolverName,
+			},
 			"priority": {
 				Type:     schema.TypeInt,
 				Required: true,
@@ -85,14 +80,16 @@ func ResourceFirewallRule() *schema.Resource {
 func resourceFirewallRuleCreate(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*conns.AWSClient).Route53ResolverConn
 
-	firewallRuleGroupId := d.Get("firewall_rule_group_id").(string)
-	firewallDomainListId := d.Get("firewall_domain_list_id").(string)
+	firewallDomainListID := d.Get("firewall_domain_list_id").(string)
+	firewallRuleGroupID := d.Get("firewall_rule_group_id").(string)
+	id := firewallRuleCreateResourceID(firewallRuleGroupID, firewallDomainListID)
+	name := d.Get("name").(string)
 	input := &route53resolver.CreateFirewallRuleInput{
-		CreatorRequestId:     aws.String(resource.PrefixedUniqueId("tf-r53-resolver-firewall-rule-")),
-		Name:                 aws.String(d.Get("name").(string)),
 		Action:               aws.String(d.Get("action").(string)),
-		FirewallRuleGroupId:  aws.String(firewallRuleGroupId),
-		FirewallDomainListId: aws.String(firewallDomainListId),
+		CreatorRequestId:     aws.String(resource.PrefixedUniqueId("tf-r53-resolver-firewall-rule-")),
+		FirewallRuleGroupId:  aws.String(firewallRuleGroupID),
+		FirewallDomainListId: aws.String(firewallDomainListID),
+		Name:                 aws.String(name),
 		Priority:             aws.Int64(int64(d.Get("priority").(int))),
 	}
 
@@ -112,13 +109,13 @@ func resourceFirewallRuleCreate(d *schema.ResourceData, meta interface{}) error 
 		input.BlockResponse = aws.String(v.(string))
 	}
 
-	log.Printf("[DEBUG] Creating Route 53 Resolver DNS Firewall rule: %#v", input)
 	_, err := conn.CreateFirewallRule(input)
+
 	if err != nil {
-		return fmt.Errorf("error creating Route 53 Resolver DNS Firewall rule: %w", err)
+		return fmt.Errorf("creating Route53 Resolver Firewall Rule (%s): %w", name, err)
 	}
 
-	d.SetId(FirewallRuleCreateID(firewallRuleGroupId, firewallDomainListId))
+	d.SetId(id)
 
 	return resourceFirewallRuleRead(d, meta)
 }
@@ -126,33 +123,33 @@ func resourceFirewallRuleCreate(d *schema.ResourceData, meta interface{}) error 
 func resourceFirewallRuleRead(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*conns.AWSClient).Route53ResolverConn
 
-	rule, err := FindFirewallRuleByID(conn, d.Id())
+	firewallRuleGroupID, firewallDomainListID, err := FirewallRuleParseResourceID(d.Id())
 
-	if tfawserr.ErrCodeEquals(err, route53resolver.ErrCodeResourceNotFoundException) {
-		log.Printf("[WARN] Route53 Resolver DNS Firewall rule (%s) not found, removing from state", d.Id())
+	if err != nil {
+		return err
+	}
+
+	firewallRule, err := FindFirewallRuleByTwoPartKey(conn, firewallRuleGroupID, firewallDomainListID)
+
+	if !d.IsNewResource() && tfresource.NotFound(err) {
+		log.Printf("[WARN] Route53 Resolver Firewall Rule (%s) not found, removing from state", d.Id())
 		d.SetId("")
 		return nil
 	}
 
 	if err != nil {
-		return fmt.Errorf("error getting Route 53 Resolver DNS Firewall rule (%s): %w", d.Id(), err)
+		return fmt.Errorf("reading Route53 Resolver Firewall Rule (%s): %w", d.Id(), err)
 	}
 
-	if rule == nil {
-		log.Printf("[WARN] Route 53 Resolver DNS Firewall rule (%s) not found, removing from state", d.Id())
-		d.SetId("")
-		return nil
-	}
-
-	d.Set("name", rule.Name)
-	d.Set("action", rule.Action)
-	d.Set("block_override_dns_type", rule.BlockOverrideDnsType)
-	d.Set("block_override_domain", rule.BlockOverrideDomain)
-	d.Set("block_override_ttl", rule.BlockOverrideTtl)
-	d.Set("block_response", rule.BlockResponse)
-	d.Set("firewall_rule_group_id", rule.FirewallRuleGroupId)
-	d.Set("firewall_domain_list_id", rule.FirewallDomainListId)
-	d.Set("priority", rule.Priority)
+	d.Set("action", firewallRule.Action)
+	d.Set("block_override_dns_type", firewallRule.BlockOverrideDnsType)
+	d.Set("block_override_domain", firewallRule.BlockOverrideDomain)
+	d.Set("block_override_ttl", firewallRule.BlockOverrideTtl)
+	d.Set("block_response", firewallRule.BlockResponse)
+	d.Set("firewall_rule_group_id", firewallRule.FirewallRuleGroupId)
+	d.Set("firewall_domain_list_id", firewallRule.FirewallDomainListId)
+	d.Set("name", firewallRule.Name)
+	d.Set("priority", firewallRule.Priority)
 
 	return nil
 }
@@ -160,11 +157,17 @@ func resourceFirewallRuleRead(d *schema.ResourceData, meta interface{}) error {
 func resourceFirewallRuleUpdate(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*conns.AWSClient).Route53ResolverConn
 
+	firewallRuleGroupID, firewallDomainListID, err := FirewallRuleParseResourceID(d.Id())
+
+	if err != nil {
+		return err
+	}
+
 	input := &route53resolver.UpdateFirewallRuleInput{
-		Name:                 aws.String(d.Get("name").(string)),
 		Action:               aws.String(d.Get("action").(string)),
-		FirewallRuleGroupId:  aws.String(d.Get("firewall_rule_group_id").(string)),
-		FirewallDomainListId: aws.String(d.Get("firewall_domain_list_id").(string)),
+		FirewallDomainListId: aws.String(firewallDomainListID),
+		FirewallRuleGroupId:  aws.String(firewallRuleGroupID),
+		Name:                 aws.String(d.Get("name").(string)),
 		Priority:             aws.Int64(int64(d.Get("priority").(int))),
 	}
 
@@ -184,10 +187,10 @@ func resourceFirewallRuleUpdate(d *schema.ResourceData, meta interface{}) error 
 		input.BlockResponse = aws.String(v.(string))
 	}
 
-	log.Printf("[DEBUG] Updating Route 53 Resolver DNS Firewall rule: %#v", input)
-	_, err := conn.UpdateFirewallRule(input)
+	_, err = conn.UpdateFirewallRule(input)
+
 	if err != nil {
-		return fmt.Errorf("error updating Route 53 Resolver DNS Firewall rule (%s): %w", d.Id(), err)
+		return fmt.Errorf("updating Route53 Resolver Firewall Rule (%s): %w", d.Id(), err)
 	}
 
 	return resourceFirewallRuleRead(d, meta)
@@ -196,9 +199,16 @@ func resourceFirewallRuleUpdate(d *schema.ResourceData, meta interface{}) error 
 func resourceFirewallRuleDelete(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*conns.AWSClient).Route53ResolverConn
 
-	_, err := conn.DeleteFirewallRule(&route53resolver.DeleteFirewallRuleInput{
-		FirewallRuleGroupId:  aws.String(d.Get("firewall_rule_group_id").(string)),
-		FirewallDomainListId: aws.String(d.Get("firewall_domain_list_id").(string)),
+	firewallRuleGroupID, firewallDomainListID, err := FirewallRuleParseResourceID(d.Id())
+
+	if err != nil {
+		return err
+	}
+
+	log.Printf("[DEBUG] Deleting Route53 Resolver Firewall Rule: %s", d.Id())
+	_, err = conn.DeleteFirewallRule(&route53resolver.DeleteFirewallRuleInput{
+		FirewallDomainListId: aws.String(firewallDomainListID),
+		FirewallRuleGroupId:  aws.String(firewallRuleGroupID),
 	})
 
 	if tfawserr.ErrCodeEquals(err, route53resolver.ErrCodeResourceNotFoundException) {
@@ -206,8 +216,67 @@ func resourceFirewallRuleDelete(d *schema.ResourceData, meta interface{}) error 
 	}
 
 	if err != nil {
-		return fmt.Errorf("error deleting Route 53 Resolver DNS Firewall rule (%s): %w", d.Id(), err)
+		return fmt.Errorf("deleting Route53 Resolver Firewall Rule (%s): %w", d.Id(), err)
 	}
 
 	return nil
+}
+
+const firewallRuleIDSeparator = ":"
+
+func firewallRuleCreateResourceID(firewallRuleGroupID, firewallDomainListID string) string {
+	parts := []string{firewallRuleGroupID, firewallDomainListID}
+	id := strings.Join(parts, firewallRuleIDSeparator)
+
+	return id
+}
+
+func FirewallRuleParseResourceID(id string) (string, string, error) {
+	parts := strings.SplitN(id, firewallRuleIDSeparator, 2)
+
+	if len(parts) < 2 || parts[0] == "" || parts[1] == "" {
+		return "", "", fmt.Errorf("unexpected format for ID (%[1]s), expected firewall_rule_group_id%[2]sfirewall_domain_list_id", id, firewallRuleIDSeparator)
+	}
+
+	return parts[0], parts[1], nil
+}
+
+func FindFirewallRuleByTwoPartKey(conn *route53resolver.Route53Resolver, firewallRuleGroupID, firewallDomainListID string) (*route53resolver.FirewallRule, error) {
+	input := &route53resolver.ListFirewallRulesInput{
+		FirewallRuleGroupId: aws.String(firewallRuleGroupID),
+	}
+	var output *route53resolver.FirewallRule
+
+	err := conn.ListFirewallRulesPages(input, func(page *route53resolver.ListFirewallRulesOutput, lastPage bool) bool {
+		if page == nil {
+			return !lastPage
+		}
+
+		for _, v := range page.FirewallRules {
+			if aws.StringValue(v.FirewallDomainListId) == firewallDomainListID {
+				output = v
+
+				return false
+			}
+		}
+
+		return !lastPage
+	})
+
+	if tfawserr.ErrCodeEquals(err, route53resolver.ErrCodeResourceNotFoundException) {
+		return nil, &resource.NotFoundError{
+			LastError:   err,
+			LastRequest: input,
+		}
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	if output == nil {
+		return nil, &resource.NotFoundError{LastRequest: input}
+	}
+
+	return output, nil
 }
