af7a5c4e945fa5701dc373579426d11eedc20e72
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-10-26 19:13:26 -0400

    Fix terrafmt error.

--- internal/service/route53resolver/rule_test.go
+++ internal/service/route53resolver/rule_test.go
@@ -543,16 +543,16 @@ resource "aws_vpc" "test" {
 }
 
 resource "aws_subnet" "test" {
-	count = 3
+  count = 3
   
-	vpc_id            = aws_vpc.test.id
-	availability_zone = data.aws_availability_zones.available.names[count.index]
-	cidr_block        = cidrsubnet(aws_vpc.test.cidr_block, 8, count.index)
+  vpc_id            = aws_vpc.test.id
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  cidr_block        = cidrsubnet(aws_vpc.test.cidr_block, 8, count.index)
   
-	tags = {
-	  Name = %[1]q
-	}
+  tags = {
+    Name = %[1]q
   }
+}
 
 resource "aws_security_group" "test" {
   count = 2
