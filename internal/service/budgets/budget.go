db6774534a4324cd52a6ca3937ccbe09a7d95a89
Simon Davis <sdavis@hashicorp.com>
2022-10-26 10:34:14 +1000

    nosempgrep for pointer conversion

--- internal/service/budgets/budget.go
+++ internal/service/budgets/budget.go
@@ -558,7 +558,7 @@ func flattenAutoAdjustData(autoAdjustData *budgets.AutoAdjustData) []map[string]
 		"last_auto_adjust_time": aws.TimeValue(autoAdjustData.LastAutoAdjustTime).Format(time.RFC3339),
 	}
 
-	if *autoAdjustData.HistoricalOptions != (budgets.HistoricalOptions{}) {
+	if *autoAdjustData.HistoricalOptions != (budgets.HistoricalOptions{}) { // nosemgrep: ci.prefer-aws-go-sdk-pointer-conversion-conditional
 		attrs["historical_options"] = flattenHistoricalOptions(autoAdjustData.HistoricalOptions)
 	}
 
