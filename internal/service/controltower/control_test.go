ed41c0d2c83f1bbfc4e35f3e82739f7573bcb962
Albert Silva <albsilv@amazon.com>
2022-10-26 15:33:38 -0400

    enhance testing

--- internal/service/controltower/control_test.go
+++ internal/service/controltower/control_test.go
@@ -10,55 +10,116 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	tfct "github.com/hashicorp/terraform-provider-aws/internal/service/controltower"
 )
 
-func TestAccControlTowerControl_basic(t *testing.T) {
+func TestAccControlTowerControl_serial(t *testing.T) {
+	testCases := map[string]map[string]func(t *testing.T){
+		"ControlTowerControl": {
+			"basic":      testAccControlTowerControl_basic,
+			"disappears": testAccControlTowerControl_disappears,
+		},
+	}
+
+	for group, m := range testCases {
+		m := m
+		t.Run(group, func(t *testing.T) {
+			for name, tc := range m {
+				tc := tc
+				t.Run(name, func(t *testing.T) {
+					tc(t)
+				})
+			}
+		})
+	}
+}
+
+func testAccControlTowerControl_basic(t *testing.T) {
+	var control controltower.EnabledControlSummary
 	resourceName := "aws_controltower_control.test"
 	controlName := "AWS-GR_EC2_VOLUME_INUSE_CHECK"
 	ouName := "Security"
 
-	resource.ParallelTest(t, resource.TestCase{
+	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
 			acctest.PreCheck(t)
 			acctest.PreCheckControlTowerDeployed(t)
 		},
 		ErrorCheck:               acctest.ErrorCheck(t, controltower.EndpointsID),
-		CheckDestroy:             testAccCheckControlDisable,
+		CheckDestroy:             testAccCheckControlDestroy,
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccControlConfig_basic(controlName, ouName),
 				Check: resource.ComposeTestCheckFunc(
+					testAccCheckControlExists(resourceName, &control),
 					acctest.MatchResourceAttrGlobalARN(resourceName, "target_identifier", "organizations", regexp.MustCompile(`ou/.+`)),
+					resource.TestCheckResourceAttrSet(resourceName, "control_identifier"),
 				),
 			},
 		},
 	})
 }
 
-func testAccControlConfig_basic(controlName string, ouName string) string {
-	return fmt.Sprintf(`
-data "aws_region" "current" {}
+func testAccControlTowerControl_disappears(t *testing.T) {
+	var control controltower.EnabledControlSummary
+	resourceName := "aws_controltower_control.test"
+	controlName := "AWS-GR_EC2_VOLUME_INUSE_CHECK"
+	ouName := "Security"
 
-data "aws_partition" "current" {}
-
-data "aws_organizations_organization" "test" {}
-
-data "aws_organizations_organizational_units" "test" {
-  parent_id = data.aws_organizations_organization.test.roots[0].id
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheck(t)
+			acctest.PreCheckControlTowerDeployed(t)
+		},
+		ErrorCheck:               acctest.ErrorCheck(t, controltower.EndpointsID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckControlDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccControlConfig_basic(controlName, ouName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckControlExists(resourceName, &control),
+					acctest.CheckResourceDisappears(acctest.Provider, tfct.ResourceControl(), resourceName),
+				),
+				ExpectNonEmptyPlan: true,
+			},
+		},
+	})
 }
 
-resource "aws_controltower_control" "test" {
-  control_identifier = "arn:${data.aws_partition.current.partition}:controltower:${data.aws_region.current.name}::control/%[1]s"
-  target_identifier = [
-    for x in data.aws_organizations_organizational_units.test.children :
-    x.arn if x.name == "%[2]s"
-  ][0]
-}
-`, controlName, ouName)
+func testAccCheckControlExists(n string, control *controltower.EnabledControlSummary) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No Control ID is set")
+		}
+
+		conn := acctest.Provider.Meta().(*conns.AWSClient).ControlTowerConn
+
+		input := &controltower.ListEnabledControlsInput{
+			TargetIdentifier: &rs.Primary.ID,
+		}
+		output, err := conn.ListEnabledControls(input)
+
+		if err != nil {
+			return err
+		}
+		for _, c := range output.EnabledControls {
+			if *c.ControlIdentifier == rs.Primary.Attributes["control_identifier"] {
+				*control = *c
+				return nil
+			}
+		}
+
+		return fmt.Errorf("Expected Control Tower Control to be created, but wasn't found")
+	}
 }
 
-func testAccCheckControlDisable(s *terraform.State) error {
+func testAccCheckControlDestroy(s *terraform.State) error {
 	for _, rs := range s.RootModule().Resources {
 		if rs.Type != "aws_controltower_control" {
 			continue
@@ -82,4 +143,26 @@ func testAccCheckControlDisable(s *terraform.State) error {
 	}
 
 	return nil
+}
+
+func testAccControlConfig_basic(controlName string, ouName string) string {
+	return fmt.Sprintf(`
+data "aws_region" "current" {}
+
+data "aws_partition" "current" {}
+
+data "aws_organizations_organization" "test" {}
+
+data "aws_organizations_organizational_units" "test" {
+  parent_id = data.aws_organizations_organization.test.roots[0].id
+}
+
+resource "aws_controltower_control" "test" {
+  control_identifier = "arn:${data.aws_partition.current.partition}:controltower:${data.aws_region.current.name}::control/%[1]s"
+  target_identifier = [
+    for x in data.aws_organizations_organizational_units.test.children :
+    x.arn if x.name == "%[2]s"
+  ][0]
+}
+`, controlName, ouName)
 }
