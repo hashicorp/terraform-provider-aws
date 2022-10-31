3eaebddd490ac8a5e53024572d587111db2d2895
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-10-31 07:46:58 -0400

    Revert "r/aws_swf_domain: Implement using terraform-plugin-framework."
    This reverts commit 7923eb26291f9b33367d3b27606a5e15c9383d8c.

--- internal/tags/framework.go
+++ internal/tags/framework.go
@@ -7,13 +7,6 @@ import (
 
 // Terraform Plugin Framework variants of tags schemas.
 
-func TagsAttribute() tfsdk.Attribute {
-	return tfsdk.Attribute{
-		Type:     types.MapType{ElemType: types.StringType},
-		Optional: true,
-	}
-}
-
 func TagsAttributeComputed() tfsdk.Attribute {
 	return tfsdk.Attribute{
 		Type:     types.MapType{ElemType: types.StringType},
