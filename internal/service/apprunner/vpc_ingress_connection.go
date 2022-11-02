d120ac243a993b143a49fad1692234d998f22a56
Bruno Schaatsbergen <git@bschaatsbergen.com>
2022-11-02 19:52:29 +0100

    Set `configuration` to required

--- internal/service/apprunner/vpc_ingress_connection.go
+++ internal/service/apprunner/vpc_ingress_connection.go
@@ -42,7 +42,7 @@ func ResourceVPCIngressConnection() *schema.Resource {
 			},
 			"configuration": {
 				Type:     schema.TypeList,
-				Optional: true,
+				Required: true,
 				MaxItems: 1,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
