bc87de321fd6f055206fe32908c4d28b47f32761
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-11-04 16:17:28 -0400

    Use 'fwplanmodifiers.NormalizeEmptyMap' for 'tags' attributes.

--- internal/tags/framework.go
+++ internal/tags/framework.go
@@ -3,6 +3,7 @@ package tags
 import (
 	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-provider-aws/internal/fwplanmodifiers"
 )
 
 // Terraform Plugin Framework variants of tags schemas.
@@ -11,6 +12,9 @@ func TagsAttribute() tfsdk.Attribute {
 	return tfsdk.Attribute{
 		Type:     types.MapType{ElemType: types.StringType},
 		Optional: true,
+		PlanModifiers: []tfsdk.AttributePlanModifier{
+			fwplanmodifiers.NormalizeEmptyMap(),
+		},
 	}
 }
 
