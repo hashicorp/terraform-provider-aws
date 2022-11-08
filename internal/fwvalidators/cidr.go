91e59ec998c204ea45f69c84ddc7ec3e54febf3c
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-11-08 15:46:32 -0500

    Add 'fwvalidators.IPv6CIDRNetworkAddress'.

--- internal/fwvalidators/cidr.go
+++ internal/fwvalidators/cidr.go
@@ -49,3 +49,45 @@ func (validator ipv4CIDRNetworkAddressValidator) Validate(ctx context.Context, r
 func IPv4CIDRNetworkAddress() tfsdk.AttributeValidator {
 	return ipv4CIDRNetworkAddressValidator{}
 }
+
+// ipv6CIDRNetworkAddressValidator validates that a string Attribute's value is a valid IPv6 CIDR that represents a network address.
+type ipv6CIDRNetworkAddressValidator struct{}
+
+// Description describes the validation in plain text formatting.
+func (validator ipv6CIDRNetworkAddressValidator) Description(_ context.Context) string {
+	return "value must be a valid IPv6 CIDR that represents a network address"
+}
+
+// MarkdownDescription describes the validation in Markdown formatting.
+func (validator ipv6CIDRNetworkAddressValidator) MarkdownDescription(ctx context.Context) string {
+	return validator.Description(ctx)
+}
+
+// Validate performs the validation.
+func (validator ipv6CIDRNetworkAddressValidator) Validate(ctx context.Context, request tfsdk.ValidateAttributeRequest, response *tfsdk.ValidateAttributeResponse) {
+	s, ok := validateString(ctx, request, response)
+
+	if !ok {
+		return
+	}
+
+	if err := verify.ValidateIPv6CIDRBlock(s); err != nil {
+		response.Diagnostics.Append(diag.NewAttributeErrorDiagnostic(
+			request.AttributePath,
+			validator.Description(ctx),
+			err.Error(),
+		))
+
+		return
+	}
+}
+
+// IPv6CIDRNetworkAddress returns an AttributeValidator which ensures that any configured
+// attribute value:
+//
+//   - Is a string, which represents a valid IPv6 CIDR network address.
+//
+// Null (unconfigured) and unknown (known after apply) values are skipped.
+func IPv6CIDRNetworkAddress() tfsdk.AttributeValidator {
+	return ipv6CIDRNetworkAddressValidator{}
+}
--- internal/fwvalidators/cidr_test.go
+++ internal/fwvalidators/cidr_test.go
@@ -33,7 +33,7 @@ func TestIPv4CIDRNetworkAddressValidator(t *testing.T) {
 			val:         types.String{Value: "test-value"},
 			expectError: true,
 		},
-		"valid string": {
+		"valid IPv4 CIDR": {
 			val: types.String{Value: "10.2.2.0/24"},
 		},
 		"invalid IPv4 CIDR": {
@@ -41,7 +41,7 @@ func TestIPv4CIDRNetworkAddressValidator(t *testing.T) {
 			expectError: true,
 		},
 		"valid IPv6 CIDR": {
-			val:         types.String{Value: "2000::/15"},
+			val:         types.String{Value: "2001:db8::/122"},
 			expectError: true,
 		},
 	}
@@ -56,6 +56,63 @@ func TestIPv4CIDRNetworkAddressValidator(t *testing.T) {
 			}
 			response := tfsdk.ValidateAttributeResponse{}
 			fwvalidators.IPv4CIDRNetworkAddress().Validate(context.Background(), request, &response)
+
+			if !response.Diagnostics.HasError() && test.expectError {
+				t.Fatal("expected error, got no error")
+			}
+
+			if response.Diagnostics.HasError() && !test.expectError {
+				t.Fatalf("got unexpected error: %s", response.Diagnostics)
+			}
+		})
+	}
+}
+
+func TestIPv6CIDRNetworkAddressValidator(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		val         attr.Value
+		expectError bool
+	}
+	tests := map[string]testCase{
+		"not a String": {
+			val:         types.Bool{Value: true},
+			expectError: true,
+		},
+		"unknown String": {
+			val: types.String{Unknown: true},
+		},
+		"null String": {
+			val: types.String{Null: true},
+		},
+		"invalid String": {
+			val:         types.String{Value: "test-value"},
+			expectError: true,
+		},
+		"valid IPv6 CIDR": {
+			val: types.String{Value: "2001:db8::/122"},
+		},
+		"invalid IPv6 CIDR": {
+			val:         types.String{Value: "2001::/15"},
+			expectError: true,
+		},
+		"valid IPv4 CIDR": {
+			val:         types.String{Value: "10.2.2.0/24"},
+			expectError: true,
+		},
+	}
+
+	for name, test := range tests {
+		name, test := name, test
+		t.Run(name, func(t *testing.T) {
+			request := tfsdk.ValidateAttributeRequest{
+				AttributePath:           path.Root("test"),
+				AttributePathExpression: path.MatchRoot("test"),
+				AttributeConfig:         test.val,
+			}
+			response := tfsdk.ValidateAttributeResponse{}
+			fwvalidators.IPv6CIDRNetworkAddress().Validate(context.Background(), request, &response)
 
 			if !response.Diagnostics.HasError() && test.expectError {
 				t.Fatal("expected error, got no error")
