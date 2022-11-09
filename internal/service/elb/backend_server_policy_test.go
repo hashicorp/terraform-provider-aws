85a8e9379bf9bdd72a438d1d05e7ef375e9468a7
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-11-09 15:32:10 -0500

    Add testing.T parameter to 'TLSRSAPublicKeyPEM' in tests for elb.

--- internal/service/elb/backend_server_policy_test.go
+++ internal/service/elb/backend_server_policy_test.go
@@ -19,8 +19,8 @@ import (
 func TestAccELBBackendServerPolicy_basic(t *testing.T) {
 	privateKey1 := acctest.TLSRSAPrivateKeyPEM(t, 2048)
 	privateKey2 := acctest.TLSRSAPrivateKeyPEM(t, 2048)
-	publicKey1 := acctest.TLSRSAPublicKeyPEM(privateKey1)
-	publicKey2 := acctest.TLSRSAPublicKeyPEM(privateKey2)
+	publicKey1 := acctest.TLSRSAPublicKeyPEM(t, privateKey1)
+	publicKey2 := acctest.TLSRSAPublicKeyPEM(t, privateKey2)
 	certificate1 := acctest.TLSRSAX509SelfSignedCertificatePEM(privateKey1, "example.com")
 	rString := sdkacctest.RandString(8)
 	lbName := fmt.Sprintf("tf-acc-lb-bsp-basic-%s", rString)
