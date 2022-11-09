f9daed43a369bb868fdc5cab63273d45ce62f90a
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-11-09 10:00:53 -0500

    d/aws_route53_resolver_firewall_config: Tidy up acceptance test configuration.

--- internal/service/route53resolver/firewall_config_data_source_test.go
+++ internal/service/route53resolver/firewall_config_data_source_test.go
@@ -1,41 +1,47 @@
 package route53resolver_test
 
 import (
-	"regexp"
+	"fmt"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/service/route53resolver"
+	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 )
 
 func TestAccRoute53ResolverFirewallConfigDataSource_basic(t *testing.T) {
 	dataSourceName := "data.aws_route53_resolver_firewall_config.test"
+	resourceName := "aws_route53_resolver_firewall_config.test"
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
 
 	resource.Test(t, resource.TestCase{
-		PreCheck:          func() { acctest.PreCheck(t) },
-		ErrorCheck:        acctest.ErrorCheck(t, route53resolver.EndpointsID),
-		ProviderFactories: acctest.ProviderFactories,
+		PreCheck:                 func() { acctest.PreCheck(t); testAccPreCheck(t) },
+		ErrorCheck:               acctest.ErrorCheck(t, route53resolver.EndpointsID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccFirewallConfigDataSourceConfig_basic(),
+				Config: testAccFirewallConfigDataSourceConfig_basic(rName),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestMatchResourceAttr(dataSourceName, "firewall_fail_open", regexp.MustCompile(`ENABLED|DISABLED`)),
-					resource.TestCheckResourceAttrSet(dataSourceName, "id"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "owner_id"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "resource_id"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "firewall_fail_open", resourceName, "firewall_fail_open"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "owner_id", resourceName, "owner_id"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "resource_id", resourceName, "resource_id"),
 				),
 			},
 		},
 	})
 }
 
-func testAccFirewallConfigDataSourceConfig_basic() string {
-	return `
+func testAccFirewallConfigDataSourceConfig_basic(rName string) string {
+	return fmt.Sprintf(`
 resource "aws_vpc" "test" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
+
+  tags = {
+    Name = %[1]q
+  }
 }
 
 resource "aws_route53_resolver_firewall_config" "test" {
@@ -46,6 +52,5 @@ resource "aws_route53_resolver_firewall_config" "test" {
 data "aws_route53_resolver_firewall_config" "test" {
   resource_id = aws_vpc.test.id
 }
-
-`
+`, rName)
 }
