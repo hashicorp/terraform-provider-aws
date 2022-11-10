992b3c91d787a52481261e22c411656932ee112a
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-11-10 16:01:04 -0500

    STS: Use 'framework.DataSourceWithConfigure'.
    Acceptance test output:
    
    % make testacc TESTARGS='-run=TestAccSTSCallerIdentityDataSource_' PKG=sts
    ==> Checking that code complies with gofmt requirements...
    TF_ACC=1 go test ./internal/service/sts/... -v -count 1 -parallel 20  -run=TestAccSTSCallerIdentityDataSource_ -timeout 180m
    === RUN   TestAccSTSCallerIdentityDataSource_basic
    === PAUSE TestAccSTSCallerIdentityDataSource_basic
    === CONT  TestAccSTSCallerIdentityDataSource_basic
    --- PASS: TestAccSTSCallerIdentityDataSource_basic (12.94s)
    PASS
    ok  	github.com/hashicorp/terraform-provider-aws/internal/service/sts	19.792s

--- internal/service/sts/caller_identity_data_source.go
+++ internal/service/sts/caller_identity_data_source.go
@@ -9,7 +9,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
 	"github.com/hashicorp/terraform-plugin-framework/types"
-	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/framework"
 	"github.com/hashicorp/terraform-provider-aws/internal/fwtypes"
 )
 
@@ -23,7 +23,7 @@ func newDataSourceCallerIdentity(context.Context) (datasource.DataSourceWithConf
 }
 
 type dataSourceCallerIdentity struct {
-	meta *conns.AWSClient
+	framework.DataSourceWithConfigure
 }
 
 // Metadata should return the full name of the data source, such as
@@ -59,15 +59,6 @@ func (d *dataSourceCallerIdentity) GetSchema(context.Context) (tfsdk.Schema, dia
 	return schema, nil
 }
 
-// Configure enables provider-level data or clients to be set in the
-// provider-defined DataSource type. It is separately executed for each
-// ReadDataSource RPC.
-func (d *dataSourceCallerIdentity) Configure(_ context.Context, request datasource.ConfigureRequest, response *datasource.ConfigureResponse) {
-	if v, ok := request.ProviderData.(*conns.AWSClient); ok {
-		d.meta = v
-	}
-}
-
 // Read is called when the provider must read data source values in order to update state.
 // Config values should be read from the ReadRequest and new state values set on the ReadResponse.
 func (d *dataSourceCallerIdentity) Read(ctx context.Context, request datasource.ReadRequest, response *datasource.ReadResponse) {
@@ -79,7 +70,9 @@ func (d *dataSourceCallerIdentity) Read(ctx context.Context, request datasource.
 		return
 	}
 
-	output, err := FindCallerIdentity(ctx, d.meta.STSConn)
+	conn := d.Meta().STSConn
+
+	output, err := FindCallerIdentity(ctx, conn)
 
 	if err != nil {
 		response.Diagnostics.AddError("reading STS Caller Identity", err.Error())
