61f812bb854aca4f0b06f3103366e4ee8bdf3f30
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-11-10 15:31:34 -0500

    Add 'internal/framework' package.

---
+++ internal/framework/base.go
@@ -0,0 +1,44 @@
+package framework
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+)
+
+type withMeta struct {
+	meta *conns.AWSClient
+}
+
+func (w *withMeta) Meta() *conns.AWSClient {
+	return w.meta
+}
+
+// ResourceWithConfigure is a structure to be embedded within a Resource that implements the ResourceWithConfigure interface.
+type ResourceWithConfigure struct {
+	withMeta
+}
+
+// Configure enables provider-level data or clients to be set in the
+// provider-defined Resource type.
+func (r *ResourceWithConfigure) Configure(_ context.Context, request resource.ConfigureRequest, response *resource.ConfigureResponse) {
+	if v, ok := request.ProviderData.(*conns.AWSClient); ok {
+		r.meta = v
+	}
+}
+
+// DataSourceWithConfigure is a structure to be embedded within a DataSource that implements the DataSourceWithConfigure interface.
+type DataSourceWithConfigure struct {
+	withMeta
+}
+
+// Configure enables provider-level data or clients to be set in the
+// provider-defined DataSource type. It is separately executed for each
+// ReadDataSource RPC.
+func (d *DataSourceWithConfigure) Configure(_ context.Context, request datasource.ConfigureRequest, response *datasource.ConfigureResponse) {
+	if v, ok := request.ProviderData.(*conns.AWSClient); ok {
+		d.meta = v
+	}
+}
