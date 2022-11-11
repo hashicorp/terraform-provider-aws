146f125c4848fee89cebafaaa6463fd65c9cc600
Roberth Kulbin <6707630+roberth-k@users.noreply.github.com>
2022-11-11 13:57:03 +0000

    new service: EventBridge Scheduler

--- internal/conns/config.go
+++ internal/conns/config.go
@@ -16,6 +16,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/rolesanywhere"
 	"github.com/aws/aws-sdk-go-v2/service/route53domains"
 	"github.com/aws/aws-sdk-go-v2/service/s3control"
+	"github.com/aws/aws-sdk-go-v2/service/scheduler"
 	"github.com/aws/aws-sdk-go-v2/service/sesv2"
 	"github.com/aws/aws-sdk-go-v2/service/ssm"
 	"github.com/aws/aws-sdk-go-v2/service/transcribe"
@@ -262,6 +263,12 @@ func (c *Config) ConfigureProvider(ctx context.Context, client *AWSClient) (*AWS
 	client.S3ControlClient = s3control.NewFromConfig(cfg, func(o *s3control.Options) {
 		if endpoint := c.Endpoints[names.S3Control]; endpoint != "" {
 			o.EndpointResolver = s3control.EndpointResolverFromURL(endpoint)
+		}
+	})
+
+	client.SchedulerClient = scheduler.NewFromConfig(cfg, func(o *scheduler.Options) {
+		if endpoint := c.Endpoints[names.Scheduler]; endpoint != "" {
+			o.EndpointResolver = scheduler.EndpointResolverFromURL(endpoint)
 		}
 	})
 
