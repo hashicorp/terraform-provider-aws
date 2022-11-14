44ba0f7888ed99526e1e938508005fb93c6fadde
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-11-14 16:37:18 -0500

    d/aws_cloudwatch_log_group: Alphabetize attributes.
    Acceptance test output:
    
    % make testacc TESTARGS='-run=TestAccLogsGroupDataSource_' PKG=logs ACCTEST_PARALLELISM=2
    ==> Checking that code complies with gofmt requirements...
    TF_ACC=1 go test ./internal/service/logs/... -v -count 1 -parallel 2  -run=TestAccLogsGroupDataSource_ -timeout 180m
    === RUN   TestAccLogsGroupDataSource_basic
    === PAUSE TestAccLogsGroupDataSource_basic
    === CONT  TestAccLogsGroupDataSource_basic
    --- PASS: TestAccLogsGroupDataSource_basic (18.36s)
    PASS
    ok  	github.com/hashicorp/terraform-provider-aws/internal/service/logs	23.186s

--- internal/service/logs/group_data_source.go
+++ internal/service/logs/group_data_source.go
@@ -14,10 +14,6 @@ func DataSourceGroup() *schema.Resource {
 		Read: dataSourceGroupRead,
 
 		Schema: map[string]*schema.Schema{
-			"name": {
-				Type:     schema.TypeString,
-				Required: true,
-			},
 			"arn": {
 				Type:     schema.TypeString,
 				Computed: true,
@@ -26,12 +22,16 @@ func DataSourceGroup() *schema.Resource {
 				Type:     schema.TypeInt,
 				Computed: true,
 			},
-			"retention_in_days": {
-				Type:     schema.TypeInt,
-				Computed: true,
-			},
 			"kms_key_id": {
 				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"retention_in_days": {
+				Type:     schema.TypeInt,
 				Computed: true,
 			},
 			"tags": tftags.TagsSchemaComputed(),
@@ -40,28 +40,26 @@ func DataSourceGroup() *schema.Resource {
 }
 
 func dataSourceGroupRead(d *schema.ResourceData, meta interface{}) error {
-	name := d.Get("name").(string)
 	conn := meta.(*conns.AWSClient).LogsConn
 	ignoreTagsConfig := meta.(*conns.AWSClient).IgnoreTagsConfig
 
+	name := d.Get("name").(string)
 	logGroup, err := FindLogGroupByName(conn, name)
+
 	if err != nil {
-		return err
-	}
-	if logGroup == nil {
-		return fmt.Errorf("No log group named %s found\n", name)
+		return fmt.Errorf("reading CloudWatch Logs Log Group (%s): %w", name, err)
 	}
 
 	d.SetId(name)
 	d.Set("arn", TrimLogGroupARNWildcardSuffix(aws.StringValue(logGroup.Arn)))
 	d.Set("creation_time", logGroup.CreationTime)
-	d.Set("retention_in_days", logGroup.RetentionInDays)
 	d.Set("kms_key_id", logGroup.KmsKeyId)
+	d.Set("retention_in_days", logGroup.RetentionInDays)
 
 	tags, err := ListTags(conn, name)
 
 	if err != nil {
-		return fmt.Errorf("listing tags for CloudWatch Logs Group (%s): %w", name, err)
+		return fmt.Errorf("listing tags for CloudWatch Logs Log Group (%s): %w", name, err)
 	}
 
 	if err := d.Set("tags", tags.IgnoreAWS().IgnoreConfig(ignoreTagsConfig).Map()); err != nil {
