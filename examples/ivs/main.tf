f279e30e7d1a3f3a43353e5684d0ee1722194fb9
Kevin Eady <eakevinh@amazon.de>
2022-11-14 13:56:34 +0100

    Fix ivs example tflint errors - terraform "required_version" attribute is required - fix formatting

--- examples/ivs/main.tf
+++ examples/ivs/main.tf
@@ -1,3 +1,7 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
 provider "aws" {
   region = var.aws_region
 }
@@ -11,13 +15,13 @@ resource "aws_ivs_recording_configuration" "example" {
   name = "tf-ivs-recording-configuration"
   destination_configuration {
     s3 {
-        bucket_name = aws_s3_bucket.example.id
+      bucket_name = aws_s3_bucket.example.id
     }
   }
 }
 
 resource "aws_ivs_channel" "example" {
-  name = "tf-ivs-channel"
+  name                        = "tf-ivs-channel"
   recording_configuration_arn = aws_ivs_recording_configuration.example.arn
 }
 
