8a30707644ec44f8023f6af9f19647eac914d220
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-12-01 09:14:59 -0500

    Update main.go

--- internal/generate/awsclient/main.go
+++ internal/generate/awsclient/main.go
@@ -34,7 +34,7 @@ type TemplateData struct {
 func main() {
 	g := common.NewGenerator()
 
-	g.Infof("generating internal/conns/%s", filename)
+	g.Infof("Generating internal/conns/%s", filename)
 
 	data, err := common.ReadAllNamesData(namesDataFile)
 
