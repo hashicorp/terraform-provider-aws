c4246e8449b0eb1f80bb07e9b75d7fdfec6a02e1
pureiboi <17396188+pureiboi@users.noreply.github.com>
2022-12-06 22:07:09 +0800

    fix - vpc_config: '': source data must be an array or slice, got map - domain_join_info : '': source data must be an array or slice, got map

--- internal/service/appstream/image_builder.go
+++ internal/service/appstream/image_builder.go
@@ -277,12 +277,21 @@ func resourceImageBuilderRead(ctx context.Context, d *schema.ResourceData, meta 
 	if err = d.Set("access_endpoint", flattenAccessEndpoints(imageBuilder.AccessEndpoints)); err != nil {
 		return diag.FromErr(fmt.Errorf("error setting `%s` for AppStream ImageBuilder (%s): %w", "access_endpoints", d.Id(), err))
 	}
-	if err = d.Set("domain_join_info", flattenDomainInfo(imageBuilder.DomainJoinInfo)); err != nil {
-		return diag.FromErr(fmt.Errorf("error setting `%s` for AppStream ImageBuilder (%s): %w", "domain_join_info", d.Id(), err))
+	if imageBuilder.DomainJoinInfo != nil {
+		if err = d.Set("domain_join_info", []interface{}{flattenDomainInfo(imageBuilder.DomainJoinInfo)}); err != nil {
+			return diag.FromErr(fmt.Errorf("error setting `%s` for AppStream ImageBuilder (%s): %w", "domain_join_info", d.Id(), err))
+		}
+	} else {
+		d.Set("domain_join_info", nil)
 	}
 
-	if err = d.Set("vpc_config", flattenVPCConfig(imageBuilder.VpcConfig)); err != nil {
-		return diag.FromErr(fmt.Errorf("error setting `%s` for AppStream ImageBuilder (%s): %w", "vpc_config", d.Id(), err))
+
+	if imageBuilder.VpcConfig != nil {
+		if err = d.Set("vpc_config", []interface{}{flattenVPCConfig(imageBuilder.VpcConfig)}); err != nil {
+			return diag.FromErr(fmt.Errorf("error setting `%s` for AppStream ImageBuilder (%s): %w", "vpc_config", d.Id(), err))
+		}
+	} else {
+		d.Set("vpc_config", nil)
 	}
 
 	d.Set("name", imageBuilder.Name)
