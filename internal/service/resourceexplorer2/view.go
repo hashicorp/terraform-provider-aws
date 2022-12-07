50a80822302d3ecbe04609ca0fc887e7c273cbe9
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-12-07 17:10:05 -0500

    r/aws_resourceexplorer2_view: Implement Create.

--- internal/service/resourceexplorer2/view.go
+++ internal/service/resourceexplorer2/view.go
@@ -3,20 +3,25 @@ package resourceexplorer2
 import (
 	"context"
 	"fmt"
+	"regexp"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/resourceexplorer2"
 	awstypes "github.com/aws/aws-sdk-go-v2/service/resourceexplorer2/types"
 	"github.com/hashicorp/terraform-plugin-framework-validators/resourcevalidator"
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	sdkresource "github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-provider-aws/internal/create"
+	"github.com/hashicorp/terraform-provider-aws/internal/enum"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/fwdiag"
 	"github.com/hashicorp/terraform-provider-aws/internal/flex"
@@ -58,6 +63,10 @@ func (r *resourceView) Schema(ctx context.Context, request resource.SchemaReques
 					stringplanmodifier.RequiresReplace(),
 					stringplanmodifier.UseStateForUnknown(),
 				},
+				Validators: []validator.String{
+					stringvalidator.LengthAtMost(64),
+					stringvalidator.RegexMatches(regexp.MustCompile(`^[a-zA-Z0-9\-]$`), `can include letters, digits, and the dash (-) character`),
+				},
 			},
 			"name_prefix": schema.StringAttribute{
 				Optional: true,
@@ -66,9 +75,37 @@ func (r *resourceView) Schema(ctx context.Context, request resource.SchemaReques
 					stringplanmodifier.RequiresReplace(),
 					stringplanmodifier.UseStateForUnknown(),
 				},
+				Validators: []validator.String{
+					stringvalidator.LengthAtMost(64 - sdkresource.UniqueIDSuffixLength),
+					stringvalidator.RegexMatches(regexp.MustCompile(`^[a-zA-Z0-9\-]$`), `can include letters, digits, and the dash (-) character`),
+				},
 			},
 			"tags":     tftags.TagsAttribute(),
 			"tags_all": tftags.TagsAttributeComputedOnly(),
+		},
+		Blocks: map[string]schema.Block{
+			"filters": schema.SingleNestedBlock{
+				Attributes: map[string]schema.Attribute{
+					"filter_string": schema.StringAttribute{
+						Required: true,
+						Validators: []validator.String{
+							stringvalidator.LengthAtMost(2048),
+						},
+					},
+				},
+			},
+			"included_property": schema.ListNestedBlock{
+				NestedObject: schema.NestedBlockObject{
+					Attributes: map[string]schema.Attribute{
+						"name": schema.StringAttribute{
+							Required: true,
+							Validators: []validator.String{
+								enum.FrameworkValidate[propertyName](),
+							},
+						},
+					},
+				},
+			},
 		},
 	}
 }
@@ -91,6 +128,22 @@ func (r *resourceView) Create(ctx context.Context, request resource.CreateReques
 	input := &resourceexplorer2.CreateViewInput{
 		ClientToken: aws.String(sdkresource.UniqueId()),
 		ViewName:    aws.String(name),
+	}
+
+	if v, diags := r.expandSearchFilter(ctx, data.Filters); diags.HasError() {
+		response.Diagnostics.Append(diags...)
+
+		return
+	} else {
+		input.Filters = v
+	}
+
+	if v, diags := r.expandIncludedProperties(ctx, data.IncludedProperties); diags.HasError() {
+		response.Diagnostics.Append(diags...)
+
+		return
+	} else {
+		input.IncludedProperties = v
 	}
 
 	if len(tags) > 0 {
@@ -243,13 +296,73 @@ func (r *resourceView) ConfigValidators(context.Context) []resource.ConfigValida
 	}
 }
 
+func (r *resourceView) expandSearchFilter(ctx context.Context, tfObject types.Object) (*awstypes.SearchFilter, diag.Diagnostics) {
+	if tfObject.IsNull() || tfObject.IsUnknown() {
+		return nil, nil
+	}
+
+	var data viewSearchFilterData
+
+	diags := tfObject.As(ctx, &data, types.ObjectAsOptions{})
+
+	if diags.HasError() {
+		return nil, diags
+	}
+
+	apiObject := &awstypes.SearchFilter{
+		FilterString: flex.StringFromFramework(ctx, data.FilterString),
+	}
+
+	return apiObject, nil
+}
+
+func (r *resourceView) expandIncludedProperties(ctx context.Context, tfList types.List) ([]awstypes.IncludedProperty, diag.Diagnostics) {
+	if tfList.IsNull() || tfList.IsUnknown() {
+		return nil, nil
+	}
+
+	var data []viewIncludedPropertyData
+
+	diags := tfList.ElementsAs(ctx, &data, false)
+
+	if diags.HasError() {
+		return nil, diags
+	}
+
+	var apiObjects []awstypes.IncludedProperty
+
+	for _, v := range data {
+		apiObjects = append(apiObjects, r.expandIncludedProperty(ctx, v))
+	}
+
+	return apiObjects, nil
+}
+
+func (r *resourceView) expandIncludedProperty(ctx context.Context, data viewIncludedPropertyData) awstypes.IncludedProperty {
+	apiObject := awstypes.IncludedProperty{
+		Name: flex.StringFromFramework(ctx, data.Name),
+	}
+
+	return apiObject
+}
+
 type resourceViewData struct {
-	ARN        types.String `tfsdk:"arn"`
-	ID         types.String `tfsdk:"id"`
-	Name       types.String `tfsdk:"name"`
-	NamePrefix types.String `tfsdk:"name_prefix"`
-	Tags       types.Map    `tfsdk:"tags"`
-	TagsAll    types.Map    `tfsdk:"tags_all"`
+	ARN                types.String `tfsdk:"arn"`
+	Filters            types.Object `tfsdk:"filters"`
+	ID                 types.String `tfsdk:"id"`
+	IncludedProperties types.List   `tfsdk:"included_property"`
+	Name               types.String `tfsdk:"name"`
+	NamePrefix         types.String `tfsdk:"name_prefix"`
+	Tags               types.Map    `tfsdk:"tags"`
+	TagsAll            types.Map    `tfsdk:"tags_all"`
+}
+
+type viewSearchFilterData struct {
+	FilterString types.String `tfsdk:"filter_string"`
+}
+
+type viewIncludedPropertyData struct {
+	Name types.String `tfsdk:"name"`
 }
 
 func findViewByARN(ctx context.Context, conn *resourceexplorer2.Client, arn string) (*resourceexplorer2.GetViewOutput, error) {
@@ -275,4 +388,17 @@ func findViewByARN(ctx context.Context, conn *resourceexplorer2.Client, arn stri
 	}
 
 	return output, nil
+}
+
+type propertyName string
+
+// Enum values for propertyName.
+const (
+	propertyNameTags propertyName = "Tags"
+)
+
+func (propertyName) Values() []propertyName {
+	return []propertyName{
+		propertyNameTags,
+	}
 }
