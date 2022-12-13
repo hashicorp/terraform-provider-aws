ab3befaa869d1c5977d1be5dcdcaf33adf3653f4
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-12-13 09:31:16 -0500

    d/aws_lambda_functions: Fix importlint 'Import groups are not in the proper order: ["Std" "Third party" "Third party"]'.

--- internal/service/lambda/functions_data_source_test.go
+++ internal/service/lambda/functions_data_source_test.go
@@ -1,16 +1,16 @@
 package lambda_test
 
 import (
-	"regexp"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/service/lambda"
-
+	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 )
 
 func TestAccLambdaFunctionsDataSource_basic(t *testing.T) {
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
 	dataSourceName := "data.aws_lambda_functions.test"
 
 	resource.ParallelTest(t, resource.TestCase{
@@ -19,18 +19,20 @@ func TestAccLambdaFunctionsDataSource_basic(t *testing.T) {
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccLambdaFunctionsDataSourceConfig_basic(),
+				Config: testAccFunctionsDataSourceConfig_basic(rName),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestMatchResourceAttr(dataSourceName, "function_names.#", regexp.MustCompile("[^0].*$")),
-					resource.TestMatchResourceAttr(dataSourceName, "function_arns.#", regexp.MustCompile("[^0].*$")),
+					acctest.CheckResourceAttrGreaterThanValue(dataSourceName, "function_arns.#", "0"),
+					acctest.CheckResourceAttrGreaterThanValue(dataSourceName, "function_names.#", "0"),
 				),
 			},
 		},
 	})
 }
 
-func testAccLambdaFunctionsDataSourceConfig_basic() string {
-	return `
-data "aws_lambda_functions" "test" {}
-`
+func testAccFunctionsDataSourceConfig_basic(rName string) string {
+	return acctest.ConfigCompose(testAccFunctionConfig_basic(rName, rName, rName, rName), `
+data "aws_lambda_functions" "test" {
+  depends_on = [aws_lambda_function.test]
+}
+`)
 }
