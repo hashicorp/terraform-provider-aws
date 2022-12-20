fbab66cad4c48d6aa2f6d772223abd3b3861fd48
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-12-20 18:22:18 -0500

    Use AWS SDK client accessors instead of field names for servicediscovery.

--- internal/service/servicediscovery/http_namespace_data_source.go
+++ internal/service/servicediscovery/http_namespace_data_source.go
@@ -39,7 +39,7 @@ func DataSourceHTTPNamespace() *schema.Resource {
 }
 
 func dataSourceHTTPNamespaceRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	conn := meta.(*conns.AWSClient).ServiceDiscoveryConn
+	conn := meta.(*conns.AWSClient).ServiceDiscoveryConn()
 	ignoreTagsConfig := meta.(*conns.AWSClient).IgnoreTagsConfig
 
 	name := d.Get("name").(string)
