54974c18ccb55ff9952b6cbfa8c8c64d460e0ec8
Adrian Johnson <adrian.johnson@hashicorp.com>
2023-01-03 12:36:07 -0500

    r/aws_wafv2_web_acl: fmt tests

--- internal/service/wafv2/web_acl_test.go
+++ internal/service/wafv2/web_acl_test.go
@@ -2910,15 +2910,13 @@ resource "aws_wafv2_web_acl" "test" {
         name        = "AWSManagedRulesATPRuleSet"
         vendor_name = "AWS"
 
-		managed_rule_group_config {
-			login_path =  "/login"
-			password_field = "/password"
-			payload_type = "JSON"
-			username_field = "/username"
-		}
-
+        managed_rule_group_config {
+          login_path     = "/login"
+          password_field = "/password"
+          payload_type   = "JSON"
+          username_field = "/username"
+        }
       }
-	  
     }
 
     visibility_config {
