ce360903947ad10c2952f644b40b4729d7856b00
GlennChia <glennchia7@gmail.com>
2023-01-07 09:19:44 +0000

    test(cwevi): launch metric_monitors

--- internal/service/evidently/launch_test.go
+++ internal/service/evidently/launch_test.go
@@ -195,6 +195,95 @@ func TestAccEvidentlyLaunch_updateGroups(t *testing.T) {
 	})
 }
 
+func TestAccEvidentlyLaunch_updateMetricMonitors(t *testing.T) {
+	var launch cloudwatchevidently.Launch
+
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	rName2 := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	rName3 := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	startTime := time.Now().AddDate(0, 0, 2).Format("2006-01-02T15:04:05Z")
+	resourceName := "aws_evidently_launch.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheck(t)
+			acctest.PreCheckPartitionHasService(cloudwatchevidently.EndpointsID, t)
+		},
+		ErrorCheck:               acctest.ErrorCheck(t, cloudwatchevidently.EndpointsID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckLaunchDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccLaunchConfig_oneMetricMonitor(rName, rName2, rName3, startTime),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckLaunchExists(resourceName, &launch),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.0.metric_definition.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.0.metric_definition.0.entity_id_key", "entity_id_key1"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.0.metric_definition.0.event_pattern", "{\"Price\":[{\"numeric\":[\">\",10,\"<=\",20]}]}"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.0.metric_definition.0.name", "name1"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.0.metric_definition.0.unit_label", "unit_label1"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.0.metric_definition.0.value_key", "value_key1"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccLaunchConfig_twoMetricMonitors(rName, rName2, rName3, startTime),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckLaunchExists(resourceName, &launch),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.0.metric_definition.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.0.metric_definition.0.entity_id_key", "entity_id_key1a"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.0.metric_definition.0.event_pattern", "{\"Price\":[{\"numeric\":[\">\",11,\"<=\",22]}]}"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.0.metric_definition.0.name", "name1a"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.0.metric_definition.0.unit_label", "unit_label1a"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.0.metric_definition.0.value_key", "value_key1a"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.1.metric_definition.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.1.metric_definition.0.entity_id_key", "entity_id_key2"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.1.metric_definition.0.event_pattern", "{\"Price\":[{\"numeric\":[\">\",9,\"<=\",19]}]}"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.1.metric_definition.0.name", "name2"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.1.metric_definition.0.unit_label", "unit_label2"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.1.metric_definition.0.value_key", "value_key2"),
+				),
+			},
+			{
+				Config: testAccLaunchConfig_threeMetricMonitors(rName, rName2, rName3, startTime),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckLaunchExists(resourceName, &launch),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.#", "3"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.0.metric_definition.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.0.metric_definition.0.entity_id_key", "entity_id_key1b"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.0.metric_definition.0.event_pattern", "{\"Price\":[{\"numeric\":[\">\",15,\"<=\",25]}]}"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.0.metric_definition.0.name", "name1b"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.0.metric_definition.0.unit_label", "unit_label1b"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.0.metric_definition.0.value_key", "value_key1b"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.1.metric_definition.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.1.metric_definition.0.entity_id_key", "entity_id_key2a"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.1.metric_definition.0.event_pattern", "{\"Price\":[{\"numeric\":[\">\",8,\"<=\",18]}]}"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.1.metric_definition.0.name", "name2a"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.1.metric_definition.0.unit_label", "unit_label2a"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.1.metric_definition.0.value_key", "value_key2a"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.2.metric_definition.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.2.metric_definition.0.entity_id_key", "entity_id_key3"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.2.metric_definition.0.name", "name3"),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.2.metric_definition.0.value_key", "value_key3"),
+				),
+			},
+			{
+				Config: testAccLaunchConfig_basic(rName, rName2, rName3, startTime),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckLaunchExists(resourceName, &launch),
+					resource.TestCheckResourceAttr(resourceName, "metric_monitors.#", "0"),
+				),
+			},
+		},
+	})
+}
+
 func TestAccEvidentlyLaunch_tags(t *testing.T) {
 	var launch cloudwatchevidently.Launch
 
@@ -527,6 +616,142 @@ resource "aws_evidently_launch" "test" {
   }
 }
 `, rName5, startTime))
+}
+
+func testAccLaunchConfig_oneMetricMonitor(rName, rName2, rName3, startTime string) string {
+	return acctest.ConfigCompose(
+		testAccLaunchConfigBase(rName, rName2),
+		fmt.Sprintf(`
+resource "aws_evidently_launch" "test" {
+  name    = %[1]q
+  project = aws_evidently_project.test.name
+
+  groups {
+    feature   = aws_evidently_feature.test.name
+    name      = "Variation1"
+    variation = "Variation1"
+  }
+
+  metric_monitors {
+    metric_definition {
+      entity_id_key = "entity_id_key1"
+      event_pattern = "{\"Price\":[{\"numeric\":[\">\",10,\"<=\",20]}]}"
+      name          = "name1"
+      unit_label    = "unit_label1"
+      value_key     = "value_key1"
+    }
+  }
+
+  scheduled_splits_config {
+    steps {
+      group_weights = {
+        "Variation1" = 0
+      }
+      start_time = %[2]q
+    }
+  }
+}
+`, rName3, startTime))
+}
+
+func testAccLaunchConfig_twoMetricMonitors(rName, rName2, rName3, startTime string) string {
+	return acctest.ConfigCompose(
+		testAccLaunchConfigBase(rName, rName2),
+		fmt.Sprintf(`
+resource "aws_evidently_launch" "test" {
+  name    = %[1]q
+  project = aws_evidently_project.test.name
+
+  groups {
+    feature   = aws_evidently_feature.test.name
+    name      = "Variation1"
+    variation = "Variation1"
+  }
+
+  metric_monitors {
+    metric_definition {
+      entity_id_key = "entity_id_key1a"
+      event_pattern = "{\"Price\":[{\"numeric\":[\">\",11,\"<=\",22]}]}"
+      name          = "name1a"
+      unit_label    = "unit_label1a"
+      value_key     = "value_key1a"
+    }
+  }
+
+  metric_monitors {
+    metric_definition {
+      entity_id_key = "entity_id_key2"
+      event_pattern = "{\"Price\":[{\"numeric\":[\">\",9,\"<=\",19]}]}"
+      name          = "name2"
+      unit_label    = "unit_label2"
+      value_key     = "value_key2"
+    }
+  }
+
+  scheduled_splits_config {
+    steps {
+      group_weights = {
+        "Variation1" = 0
+      }
+      start_time = %[2]q
+    }
+  }
+}
+`, rName3, startTime))
+}
+
+func testAccLaunchConfig_threeMetricMonitors(rName, rName2, rName3, startTime string) string {
+	return acctest.ConfigCompose(
+		testAccLaunchConfigBase(rName, rName2),
+		fmt.Sprintf(`
+resource "aws_evidently_launch" "test" {
+  name    = %[1]q
+  project = aws_evidently_project.test.name
+
+  groups {
+    feature   = aws_evidently_feature.test.name
+    name      = "Variation1"
+    variation = "Variation1"
+  }
+
+  metric_monitors {
+    metric_definition {
+      entity_id_key = "entity_id_key1b"
+      event_pattern = "{\"Price\":[{\"numeric\":[\">\",15,\"<=\",25]}]}"
+      name          = "name1b"
+      unit_label    = "unit_label1b"
+      value_key     = "value_key1b"
+    }
+  }
+
+  metric_monitors {
+    metric_definition {
+      entity_id_key = "entity_id_key2a"
+      event_pattern = "{\"Price\":[{\"numeric\":[\">\",8,\"<=\",18]}]}"
+      name          = "name2a"
+      unit_label    = "unit_label2a"
+      value_key     = "value_key2a"
+    }
+  }
+
+  metric_monitors {
+    metric_definition {
+      entity_id_key = "entity_id_key3"
+      name          = "name3"
+      value_key     = "value_key3"
+    }
+  }
+
+  scheduled_splits_config {
+    steps {
+      group_weights = {
+        "Variation1" = 0
+      }
+      start_time = %[2]q
+    }
+  }
+}
+`, rName3, startTime))
 }
 
 func testAccLaunchConfig_tags1(rName, rName2, rName3, startTime, tag, value string) string {
