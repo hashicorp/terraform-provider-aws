d5d82e26df635200c2cb4ebb42506b10b8c6a803
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-01-09 18:26:38 -0500

    Fix golangci-lint 'paralleltest' for workspaces.

--- internal/service/workspaces/directory_test.go
+++ internal/service/workspaces/directory_test.go
@@ -411,6 +411,8 @@ func testAccDirectory_ipGroupIDs(t *testing.T) {
 }
 
 func TestExpandSelfServicePermissions(t *testing.T) {
+	t.Parallel()
+
 	cases := []struct {
 		input    []interface{}
 		expected *workspaces.SelfservicePermissions
@@ -450,6 +452,8 @@ func TestExpandSelfServicePermissions(t *testing.T) {
 }
 
 func TestFlattenSelfServicePermissions(t *testing.T) {
+	t.Parallel()
+
 	cases := []struct {
 		input    *workspaces.SelfservicePermissions
 		expected []interface{}
@@ -489,6 +493,8 @@ func TestFlattenSelfServicePermissions(t *testing.T) {
 }
 
 func TestExpandWorkspaceAccessProperties(t *testing.T) {
+	t.Parallel()
+
 	cases := []struct {
 		input    []interface{}
 		expected *workspaces.WorkspaceAccessProperties
@@ -534,6 +540,8 @@ func TestExpandWorkspaceAccessProperties(t *testing.T) {
 }
 
 func TestFlattenWorkspaceAccessProperties(t *testing.T) {
+	t.Parallel()
+
 	cases := []struct {
 		input    *workspaces.WorkspaceAccessProperties
 		expected []interface{}
@@ -579,6 +587,8 @@ func TestFlattenWorkspaceAccessProperties(t *testing.T) {
 }
 
 func TestExpandWorkspaceCreationProperties(t *testing.T) {
+	t.Parallel()
+
 	cases := []struct {
 		input    []interface{}
 		expected *workspaces.WorkspaceCreationProperties
@@ -637,6 +647,8 @@ func TestExpandWorkspaceCreationProperties(t *testing.T) {
 }
 
 func TestFlattenWorkspaceCreationProperties(t *testing.T) {
+	t.Parallel()
+
 	cases := []struct {
 		input    *workspaces.DefaultWorkspaceCreationProperties
 		expected []interface{}
