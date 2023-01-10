78d443950ffedd6ab485222df67c4c88578215a9
Dirk Avery <dirk.avery@gmail.com>
2023-01-10 11:23:59 -0500

    efs/file_system_policy: Improve diff handling

--- internal/service/efs/file_system_policy.go
+++ internal/service/efs/file_system_policy.go
@@ -37,10 +37,15 @@ func ResourceFileSystemPolicy() *schema.Resource {
 				ForceNew: true,
 			},
 			"policy": {
-				Type:             schema.TypeString,
-				Required:         true,
-				ValidateFunc:     validation.StringIsJSON,
-				DiffSuppressFunc: verify.SuppressEquivalentPolicyDiffs,
+				Type:                  schema.TypeString,
+				Required:              true,
+				ValidateFunc:          validation.StringIsJSON,
+				DiffSuppressFunc:      verify.SuppressEquivalentPolicyDiffs,
+				DiffSuppressOnRefresh: true,
+				StateFunc: func(v interface{}) string {
+					json, _ := structure.NormalizeJsonString(v)
+					return json
+				},
 			},
 		},
 	}
