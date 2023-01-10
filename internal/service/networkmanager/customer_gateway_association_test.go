e7900d8d67ce31a2737f48907d6f085889b17762
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-01-10 13:29:14 -0500

    Use 'acctest.RunSerialTests' for networkmanager.

--- internal/service/networkmanager/customer_gateway_association_test.go
+++ internal/service/networkmanager/customer_gateway_association_test.go
@@ -17,18 +17,15 @@ import (
 )
 
 func TestAccNetworkManagerCustomerGatewayAssociation_serial(t *testing.T) {
+	t.Parallel()
+
 	testCases := map[string]func(t *testing.T){
 		"basic":                      testAccCustomerGatewayAssociation_basic,
 		"disappears":                 testAccCustomerGatewayAssociation_disappears,
 		"disappears_CustomerGateway": testAccCustomerGatewayAssociation_Disappears_customerGateway,
 	}
 
-	for name, tc := range testCases {
-		tc := tc
-		t.Run(name, func(t *testing.T) {
-			tc(t)
-		})
-	}
+	acctest.RunSerialTests1Level(t, testCases, 0)
 }
 
 func testAccCustomerGatewayAssociation_basic(t *testing.T) {
