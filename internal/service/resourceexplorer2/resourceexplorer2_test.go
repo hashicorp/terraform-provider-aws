5b9dbf6548dd52e3e7e22885bd15cce27d9e389c
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-01-10 13:29:16 -0500

    Use 'acctest.RunSerialTests' for resourceexplorer2.

--- internal/service/resourceexplorer2/resourceexplorer2_test.go
+++ internal/service/resourceexplorer2/resourceexplorer2_test.go
@@ -2,9 +2,13 @@ package resourceexplorer2_test
 
 import (
 	"testing"
+
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 )
 
 func TestAccResourceExplorer2_serial(t *testing.T) {
+	t.Parallel()
+
 	testCases := map[string]map[string]func(t *testing.T){
 		"Index": {
 			"basic":      testAccIndex_basic,
@@ -14,15 +18,5 @@ func TestAccResourceExplorer2_serial(t *testing.T) {
 		},
 	}
 
-	for group, m := range testCases {
-		m := m
-		t.Run(group, func(t *testing.T) {
-			for name, tc := range m {
-				tc := tc
-				t.Run(name, func(t *testing.T) {
-					tc(t)
-				})
-			}
-		})
-	}
+	acctest.RunSerialTests2Levels(t, testCases, 0)
 }
