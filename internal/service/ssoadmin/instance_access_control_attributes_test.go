92292d09e3079baddd33fc3243d16d9469209dcb
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-01-10 13:29:20 -0500

    Use 'acctest.RunSerialTests' for ssoadmin.

--- internal/service/ssoadmin/instance_access_control_attributes_test.go
+++ internal/service/ssoadmin/instance_access_control_attributes_test.go
@@ -14,6 +14,8 @@ import (
 )
 
 func TestAccSSOAdmin_serial(t *testing.T) {
+	t.Parallel()
+
 	testCases := map[string]map[string]func(t *testing.T){
 		"InstanceAccessControlAttributes": {
 			"basic":      testAccInstanceAccessControlAttributes_basic,
@@ -23,17 +25,7 @@ func TestAccSSOAdmin_serial(t *testing.T) {
 		},
 	}
 
-	for group, m := range testCases {
-		m := m
-		t.Run(group, func(t *testing.T) {
-			for name, tc := range m {
-				tc := tc
-				t.Run(name, func(t *testing.T) {
-					tc(t)
-				})
-			}
-		})
-	}
+	acctest.RunSerialTests2Levels(t, testCases, 0)
 }
 
 func testAccInstanceAccessControlAttributes_basic(t *testing.T) {
