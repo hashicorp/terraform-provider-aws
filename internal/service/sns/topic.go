1edac8578042e9a9ebf23f8feab95313aab9df74
Dirk Avery <dirk.avery@gmail.com>
2023-01-12 15:20:51 -0500

    sns: Improve policy diffs

--- internal/service/sns/topic.go
+++ internal/service/sns/topic.go
@@ -138,11 +138,12 @@ var (
 			Computed: true,
 		},
 		"policy": {
-			Type:             schema.TypeString,
-			Optional:         true,
-			Computed:         true,
-			ValidateFunc:     validation.StringIsJSON,
-			DiffSuppressFunc: verify.SuppressEquivalentPolicyDiffs,
+			Type:                  schema.TypeString,
+			Optional:              true,
+			Computed:              true,
+			ValidateFunc:          validation.StringIsJSON,
+			DiffSuppressFunc:      verify.SuppressEquivalentPolicyDiffs,
+			DiffSuppressOnRefresh: true,
 			StateFunc: func(v interface{}) string {
 				json, _ := structure.NormalizeJsonString(v)
 				return json
