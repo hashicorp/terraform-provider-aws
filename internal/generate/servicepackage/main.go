a5dfc405d4141ad191b3c8b5428daf37e6800ca1
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-01-16 14:16:24 -0500

    Use function-level "annotations" to signal resources to be registered.

--- internal/generate/servicepackage/main.go
+++ internal/generate/servicepackage/main.go
@@ -6,17 +6,23 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
 	"os"
 	"path/filepath"
+	"regexp"
 	"sort"
+	"strings"
 
+	multierror "github.com/hashicorp/go-multierror"
 	"github.com/hashicorp/terraform-provider-aws/internal/generate/common"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
 func main() {
 	const (
-		spFile        = `service_package_gen.go`
+		spdFile       = `service_package_gen.go`
 		spsFile       = `../../provider/service_packages_gen.go`
 		namesDataFile = `../../../names/names_data.csv`
 	)
@@ -28,7 +34,7 @@ func main() {
 		g.Fatalf("error reading %s: %s", namesDataFile, err.Error())
 	}
 
-	g.Infof("Generating per-service %s", filepath.Base(spFile))
+	g.Infof("Generating per-service %s", filepath.Base(spdFile))
 
 	td := TemplateData{}
 
@@ -53,14 +59,29 @@ func main() {
 			continue
 		}
 
-		if v, err := filepath.Glob(fmt.Sprintf("%s/*.go", dir)); err != nil || len(v) == 0 {
-			continue
+		v := &visitor{
+			g: g,
+
+			frameworkDataSources: make([]string, 0),
+			frameworkResources:   make([]string, 0),
+			sdkDataSources:       make(map[string]string),
+			sdkResources:         make(map[string]string),
+		}
+
+		v.processDir(dir)
+
+		if err := v.err.ErrorOrNil(); err != nil {
+			g.Fatalf("%s", err.Error())
 		}
 
 		s := ServiceDatum{
-			ProviderPackage: p,
+			ProviderPackage:      p,
+			FrameworkDataSources: v.frameworkDataSources,
+			FrameworkResources:   v.frameworkResources,
+			SDKDataSources:       v.sdkDataSources,
+			SDKResources:         v.sdkResources,
 		}
-		d := g.NewGoFileDestination(fmt.Sprintf("../../service/%s/%s", p, spFile))
+		d := g.NewGoFileDestination(fmt.Sprintf("../../service/%s/%s", p, spdFile))
 
 		if err := d.WriteTemplate("servicepackagedata", spdTmpl, s); err != nil {
 			g.Fatalf("error generating %s service package data: %s", p, err.Error())
@@ -83,7 +104,11 @@ func main() {
 }
 
 type ServiceDatum struct {
-	ProviderPackage string
+	ProviderPackage      string
+	FrameworkDataSources []string
+	FrameworkResources   []string
+	SDKDataSources       map[string]string
+	SDKResources         map[string]string
 }
 
 type TemplateData struct {
@@ -95,3 +120,114 @@ var spdTmpl string
 
 //go:embed sps.tmpl
 var spsTmpl string
+
+// Annotation processing.
+var (
+	frameworkDataSourceAnnotation = regexp.MustCompile(`^//\s*@FrameworkDataSource\s*$`)
+	frameworkResourceAnnotation   = regexp.MustCompile(`^//\s*@FrameworkResource\s*$`)
+	sdkDataSourceAnnotation       = regexp.MustCompile(`^//\s*@SDKDataSource\("([a-z0-9_]+)"\)\s*$`)
+	sdkResourceAnnotation         = regexp.MustCompile(`^//\s*@SDKResource\("([a-z0-9_]+)"\)\s*$`)
+)
+
+type visitor struct {
+	err *multierror.Error
+	g   *common.Generator
+
+	fileName     string
+	functionName string
+	packageName  string
+
+	frameworkDataSources []string
+	frameworkResources   []string
+	sdkDataSources       map[string]string
+	sdkResources         map[string]string
+}
+
+// processDir scans a single service package directory and processes contained Go sources files.
+func (v *visitor) processDir(path string) {
+	fileSet := token.NewFileSet()
+	packageMap, err := parser.ParseDir(fileSet, path, func(fi os.FileInfo) bool {
+		// Skip tests.
+		return !strings.HasSuffix(fi.Name(), "_test.go")
+	}, parser.ParseComments)
+
+	if err != nil {
+		v.err = multierror.Append(v.err, fmt.Errorf("parsing (%s): %w", path, err))
+
+		return
+	}
+
+	for name, pkg := range packageMap {
+		v.packageName = name
+
+		for name, file := range pkg.Files {
+			v.fileName = name
+
+			v.processFile(file)
+
+			v.fileName = ""
+		}
+
+		v.packageName = ""
+	}
+}
+
+// processFile processes a single Go source file.
+func (v *visitor) processFile(file *ast.File) {
+	ast.Walk(v, file)
+}
+
+// processFuncDecl processes a single Go function.
+// The function's comments are scanned for annotations indicating a Plugin Framework or SDK resource or data source.
+func (v *visitor) processFuncDecl(funcDecl *ast.FuncDecl) {
+	v.functionName = funcDecl.Name.Name
+
+	for _, line := range funcDecl.Doc.List {
+		line := line.Text
+		functionName := fmt.Sprintf("%s.%s", v.packageName, v.functionName)
+
+		if m := frameworkDataSourceAnnotation.FindStringSubmatch(line); len(m) > 0 {
+			v.frameworkDataSources = append(v.frameworkDataSources, functionName)
+
+			break
+		} else if m := frameworkResourceAnnotation.FindStringSubmatch(line); len(m) > 0 {
+			v.frameworkResources = append(v.frameworkResources, functionName)
+
+			break
+		} else if m := sdkDataSourceAnnotation.FindStringSubmatch(line); len(m) > 0 {
+			name := m[1]
+
+			if _, ok := v.sdkDataSources[name]; ok {
+				v.err = multierror.Append(v.err, fmt.Errorf("duplicate SDK Data Source (%s): %s", name, functionName))
+			} else {
+				v.sdkDataSources[name] = functionName
+			}
+
+			break
+		} else if m := sdkResourceAnnotation.FindStringSubmatch(line); len(m) > 0 {
+			name := m[1]
+
+			if _, ok := v.sdkResources[name]; ok {
+				v.err = multierror.Append(v.err, fmt.Errorf("duplicate SDK Resource (%s): %s", name, functionName))
+			} else {
+				v.sdkResources[name] = functionName
+			}
+
+			break
+		}
+	}
+
+	v.functionName = ""
+}
+
+// Visit is called for each node visited by ast.Walk.
+func (v *visitor) Visit(node ast.Node) ast.Visitor {
+	if node != nil {
+		// Look at functions (not methods) with comments.
+		if funcDecl, ok := node.(*ast.FuncDecl); ok && funcDecl.Recv == nil && funcDecl.Doc != nil {
+			v.processFuncDecl(funcDecl)
+		}
+	}
+
+	return v
+}
