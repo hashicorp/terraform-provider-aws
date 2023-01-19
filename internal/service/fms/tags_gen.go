306ff070a9628f0f915a20c51fb229a01c5a9e12
Graham Davison <gdavison@hashicorp.com>
2023-01-18 21:50:32 -0800

    Adds `context` calls to `fms`

--- internal/service/fms/tags_gen.go
+++ internal/service/fms/tags_gen.go
@@ -14,11 +14,7 @@ import (
 // ListTags lists fms service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func ListTags(conn fmsiface.FMSAPI, identifier string) (tftags.KeyValueTags, error) {
-	return ListTagsWithContext(context.Background(), conn, identifier)
-}
-
-func ListTagsWithContext(ctx context.Context, conn fmsiface.FMSAPI, identifier string) (tftags.KeyValueTags, error) {
+func ListTags(ctx context.Context, conn fmsiface.FMSAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &fms.ListTagsForResourceInput{
 		ResourceArn: aws.String(identifier),
 	}
@@ -64,10 +60,7 @@ func KeyValueTags(tags []*fms.Tag) tftags.KeyValueTags {
 // UpdateTags updates fms service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn fmsiface.FMSAPI, identifier string, oldTags interface{}, newTags interface{}) error {
-	return UpdateTagsWithContext(context.Background(), conn, identifier, oldTags, newTags)
-}
-func UpdateTagsWithContext(ctx context.Context, conn fmsiface.FMSAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(ctx context.Context, conn fmsiface.FMSAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
