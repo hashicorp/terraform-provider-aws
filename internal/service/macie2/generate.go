1d60797bf6bc4e66f337060972892bfa3e7396bd
Graham Davison <gdavison@hashicorp.com>
2023-01-19 12:40:56 -0800

    Adds `context` calls to `macie2`

--- internal/service/macie2/generate.go
+++ internal/service/macie2/generate.go
@@ -1,4 +1,4 @@
-//go:generate go run ../../generate/tags/main.go -ServiceTagsMap
+//go:generate go run ../../generate/tags/main.go -ServiceTagsMap -ContextOnly
 // ONLY generate directives and package declaration! Do not add anything else to this file.
 
 package macie2
