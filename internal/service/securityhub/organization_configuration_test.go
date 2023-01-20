366ffae2797a066d2d5d1cdf4da23a42bb0b908b
Graham Davison <gdavison@hashicorp.com>
2023-01-19 16:30:57 -0800

    Adds `context` calls to `securityhub`

--- internal/service/securityhub/organization_configuration_test.go
+++ internal/service/securityhub/organization_configuration_test.go
@@ -1,6 +1,7 @@
 package securityhub_test
 
 import (
+	"context"
 	"fmt"
 	"testing"
 
@@ -12,6 +13,7 @@ import (
 )
 
 func testAccOrganizationConfiguration_basic(t *testing.T) {
+	ctx := acctest.Context(t)
 	resourceName := "aws_securityhub_organization_configuration.test"
 
 	resource.Test(t, resource.TestCase{
@@ -23,7 +25,7 @@ func testAccOrganizationConfiguration_basic(t *testing.T) {
 			{
 				Config: testAccOrganizationConfigurationConfig_basic(true),
 				Check: resource.ComposeTestCheckFunc(
-					testAccOrganizationConfigurationExists(resourceName),
+					testAccOrganizationConfigurationExists(ctx, resourceName),
 					resource.TestCheckResourceAttr(resourceName, "auto_enable", "true"),
 				),
 			},
@@ -35,7 +37,7 @@ func testAccOrganizationConfiguration_basic(t *testing.T) {
 			{
 				Config: testAccOrganizationConfigurationConfig_basic(false),
 				Check: resource.ComposeTestCheckFunc(
-					testAccOrganizationConfigurationExists(resourceName),
+					testAccOrganizationConfigurationExists(ctx, resourceName),
 					resource.TestCheckResourceAttr(resourceName, "auto_enable", "false"),
 				),
 			},
@@ -43,7 +45,7 @@ func testAccOrganizationConfiguration_basic(t *testing.T) {
 	})
 }
 
-func testAccOrganizationConfigurationExists(n string) resource.TestCheckFunc {
+func testAccOrganizationConfigurationExists(ctx context.Context, n string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		_, ok := s.RootModule().Resources[n]
 		if !ok {
@@ -52,7 +54,7 @@ func testAccOrganizationConfigurationExists(n string) resource.TestCheckFunc {
 
 		conn := acctest.Provider.Meta().(*conns.AWSClient).SecurityHubConn()
 
-		_, err := conn.DescribeOrganizationConfiguration(&securityhub.DescribeOrganizationConfigurationInput{})
+		_, err := conn.DescribeOrganizationConfigurationWithContext(ctx, &securityhub.DescribeOrganizationConfigurationInput{})
 
 		return err
 	}
