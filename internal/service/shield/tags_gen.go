6f5018c4d707df3311740d2883b874604c993107
Graham Davison <gdavison@hashicorp.com>
2023-01-19 16:57:24 -0800

    Adds `context` calls to `shield`

--- internal/service/shield/tags_gen.go
+++ internal/service/shield/tags_gen.go
@@ -14,11 +14,7 @@ import (
 // ListTags lists shield service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func ListTags(conn shieldiface.ShieldAPI, identifier string) (tftags.KeyValueTags, error) {
-	return ListTagsWithContext(context.Background(), conn, identifier)
-}
-
-func ListTagsWithContext(ctx context.Context, conn shieldiface.ShieldAPI, identifier string) (tftags.KeyValueTags, error) {
+func ListTags(ctx context.Context, conn shieldiface.ShieldAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &shield.ListTagsForResourceInput{
 		ResourceARN: aws.String(identifier),
 	}
@@ -64,10 +60,7 @@ func KeyValueTags(tags []*shield.Tag) tftags.KeyValueTags {
 // UpdateTags updates shield service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn shieldiface.ShieldAPI, identifier string, oldTags interface{}, newTags interface{}) error {
-	return UpdateTagsWithContext(context.Background(), conn, identifier, oldTags, newTags)
-}
-func UpdateTagsWithContext(ctx context.Context, conn shieldiface.ShieldAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(ctx context.Context, conn shieldiface.ShieldAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
