2bd5a6c7d5187a06df8ce10c54d25521de462f82
Graham Davison <gdavison@hashicorp.com>
2023-01-19 17:32:46 -0800

    Adds `context` calls to `storagegateway`

--- internal/service/storagegateway/tags_gen.go
+++ internal/service/storagegateway/tags_gen.go
@@ -14,11 +14,7 @@ import (
 // ListTags lists storagegateway service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func ListTags(conn storagegatewayiface.StorageGatewayAPI, identifier string) (tftags.KeyValueTags, error) {
-	return ListTagsWithContext(context.Background(), conn, identifier)
-}
-
-func ListTagsWithContext(ctx context.Context, conn storagegatewayiface.StorageGatewayAPI, identifier string) (tftags.KeyValueTags, error) {
+func ListTags(ctx context.Context, conn storagegatewayiface.StorageGatewayAPI, identifier string) (tftags.KeyValueTags, error) {
 	input := &storagegateway.ListTagsForResourceInput{
 		ResourceARN: aws.String(identifier),
 	}
@@ -64,10 +60,7 @@ func KeyValueTags(tags []*storagegateway.Tag) tftags.KeyValueTags {
 // UpdateTags updates storagegateway service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(conn storagegatewayiface.StorageGatewayAPI, identifier string, oldTags interface{}, newTags interface{}) error {
-	return UpdateTagsWithContext(context.Background(), conn, identifier, oldTags, newTags)
-}
-func UpdateTagsWithContext(ctx context.Context, conn storagegatewayiface.StorageGatewayAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
+func UpdateTags(ctx context.Context, conn storagegatewayiface.StorageGatewayAPI, identifier string, oldTagsMap interface{}, newTagsMap interface{}) error {
 	oldTags := tftags.New(oldTagsMap)
 	newTags := tftags.New(newTagsMap)
 
