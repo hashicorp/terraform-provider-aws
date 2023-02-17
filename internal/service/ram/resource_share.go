cafc11d130c9ac46559a1294780501c968dd5d1e
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-02-17 13:23:44 -0500

    Pass 'Context' to 'tftags.New' for ram.

--- internal/service/ram/resource_share.go
+++ internal/service/ram/resource_share.go
@@ -69,7 +69,7 @@ func resourceResourceShareCreate(ctx context.Context, d *schema.ResourceData, me
 	var diags diag.Diagnostics
 	conn := meta.(*conns.AWSClient).RAMConn()
 	defaultTagsConfig := meta.(*conns.AWSClient).DefaultTagsConfig
-	tags := defaultTagsConfig.MergeTags(tftags.New(d.Get("tags").(map[string]interface{})))
+	tags := defaultTagsConfig.MergeTags(tftags.New(ctx, d.Get("tags").(map[string]interface{})))
 
 	name := d.Get("name").(string)
 	input := &ram.CreateResourceShareInput{
