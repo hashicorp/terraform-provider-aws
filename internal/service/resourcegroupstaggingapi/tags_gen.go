488df07164d957a665699d3a1532fe7929e6fe93
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-02-17 13:23:46 -0500

    Pass 'Context' to 'tftags.New' for resourcegroupstaggingapi.

--- internal/service/resourcegroupstaggingapi/tags_gen.go
+++ internal/service/resourcegroupstaggingapi/tags_gen.go
@@ -26,12 +26,12 @@ func Tags(tags tftags.KeyValueTags) []*resourcegroupstaggingapi.Tag {
 }
 
 // KeyValueTags creates tftags.KeyValueTags from resourcegroupstaggingapi service tags.
-func KeyValueTags(tags []*resourcegroupstaggingapi.Tag) tftags.KeyValueTags {
+func KeyValueTags(ctx context.Context, tags []*resourcegroupstaggingapi.Tag) tftags.KeyValueTags {
 	m := make(map[string]*string, len(tags))
 
 	for _, tag := range tags {
 		m[aws.StringValue(tag.Key)] = tag.Value
 	}
 
-	return tftags.New(m)
+	return tftags.New(ctx, m)
 }
