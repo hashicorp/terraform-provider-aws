55ce8afd104001bed04b773bed8dbe44b35434e0
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-02-17 13:23:43 -0500

    Pass 'Context' to 'tftags.New' for organizations.

--- internal/service/organizations/policy.go
+++ internal/service/organizations/policy.go
@@ -66,7 +66,7 @@ func ResourcePolicy() *schema.Resource {
 func resourcePolicyCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	conn := meta.(*conns.AWSClient).OrganizationsConn()
 	defaultTagsConfig := meta.(*conns.AWSClient).DefaultTagsConfig
-	tags := defaultTagsConfig.MergeTags(tftags.New(d.Get("tags").(map[string]interface{})))
+	tags := defaultTagsConfig.MergeTags(tftags.New(ctx, d.Get("tags").(map[string]interface{})))
 
 	name := d.Get("name").(string)
 
