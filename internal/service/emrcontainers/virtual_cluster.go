ad615e7de442e3dd8f6b3eaca0fdf02b95a8d695
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-02-17 13:23:31 -0500

    Pass 'Context' to 'tftags.New' for emrcontainers.

--- internal/service/emrcontainers/virtual_cluster.go
+++ internal/service/emrcontainers/virtual_cluster.go
@@ -107,7 +107,7 @@ func ResourceVirtualCluster() *schema.Resource {
 func resourceVirtualClusterCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	conn := meta.(*conns.AWSClient).EMRContainersConn()
 	defaultTagsConfig := meta.(*conns.AWSClient).DefaultTagsConfig
-	tags := defaultTagsConfig.MergeTags(tftags.New(d.Get("tags").(map[string]interface{})))
+	tags := defaultTagsConfig.MergeTags(tftags.New(ctx, d.Get("tags").(map[string]interface{})))
 
 	name := d.Get("name").(string)
 	input := &emrcontainers.CreateVirtualClusterInput{
