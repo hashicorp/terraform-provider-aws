bd5b2064187bbc52ba3097f1cb000401de95d2ca
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-02-17 13:23:38 -0500

    Pass 'Context' to 'tftags.New' for kinesisanalyticsv2.

--- internal/service/kinesisanalyticsv2/application.go
+++ internal/service/kinesisanalyticsv2/application.go
@@ -933,7 +933,7 @@ func resourceApplicationCreate(ctx context.Context, d *schema.ResourceData, meta
 	var diags diag.Diagnostics
 	conn := meta.(*conns.AWSClient).KinesisAnalyticsV2Conn()
 	defaultTagsConfig := meta.(*conns.AWSClient).DefaultTagsConfig
-	tags := defaultTagsConfig.MergeTags(tftags.New(d.Get("tags").(map[string]interface{})))
+	tags := defaultTagsConfig.MergeTags(tftags.New(ctx, d.Get("tags").(map[string]interface{})))
 
 	applicationName := d.Get("name").(string)
 	input := &kinesisanalyticsv2.CreateApplicationInput{
