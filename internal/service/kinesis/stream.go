807ae6b84dadbdcc4c7481bb0268a6678ef5e593
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-02-17 13:23:37 -0500

    Pass 'Context' to 'tftags.New' for kinesis.

--- internal/service/kinesis/stream.go
+++ internal/service/kinesis/stream.go
@@ -143,7 +143,7 @@ func resourceStreamCreate(ctx context.Context, d *schema.ResourceData, meta inte
 	var diags diag.Diagnostics
 	conn := meta.(*conns.AWSClient).KinesisConn()
 	defaultTagsConfig := meta.(*conns.AWSClient).DefaultTagsConfig
-	tags := defaultTagsConfig.MergeTags(tftags.New(d.Get("tags").(map[string]interface{})))
+	tags := defaultTagsConfig.MergeTags(tftags.New(ctx, d.Get("tags").(map[string]interface{})))
 
 	name := d.Get("name").(string)
 	input := &kinesis.CreateStreamInput{
