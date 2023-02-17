e642b6c586596cc4dd5b5774974ececa8c8f5a73
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-02-17 13:23:47 -0500

    Pass 'Context' to 'tftags.New' for rum.

--- internal/service/rum/app_monitor.go
+++ internal/service/rum/app_monitor.go
@@ -145,7 +145,7 @@ func ResourceAppMonitor() *schema.Resource {
 func resourceAppMonitorCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	conn := meta.(*conns.AWSClient).RUMConn()
 	defaultTagsConfig := meta.(*conns.AWSClient).DefaultTagsConfig
-	tags := defaultTagsConfig.MergeTags(tftags.New(d.Get("tags").(map[string]interface{})))
+	tags := defaultTagsConfig.MergeTags(tftags.New(ctx, d.Get("tags").(map[string]interface{})))
 
 	name := d.Get("name").(string)
 	input := &cloudwatchrum.CreateAppMonitorInput{
