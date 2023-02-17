c17090292e2b810a08b5ce2aa2e3bcca1d47943c
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-02-17 13:23:36 -0500

    Pass 'Context' to 'tftags.New' for ivs.

--- internal/service/ivs/playback_key_pair.go
+++ internal/service/ivs/playback_key_pair.go
@@ -78,7 +78,7 @@ func resourcePlaybackKeyPairCreate(ctx context.Context, d *schema.ResourceData, 
 	}
 
 	defaultTagsConfig := meta.(*conns.AWSClient).DefaultTagsConfig
-	tags := defaultTagsConfig.MergeTags(tftags.New(d.Get("tags").(map[string]interface{})))
+	tags := defaultTagsConfig.MergeTags(tftags.New(ctx, d.Get("tags").(map[string]interface{})))
 
 	if len(tags) > 0 {
 		in.Tags = Tags(tags.IgnoreAWS())
