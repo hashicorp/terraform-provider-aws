2e2caa11a47aface47545859bfd2a9a8d6429cd5
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-02-17 13:23:31 -0500

    Pass 'Context' to 'tftags.New' for emrserverless.

--- internal/service/emrserverless/application.go
+++ internal/service/emrserverless/application.go
@@ -204,7 +204,7 @@ func resourceApplicationCreate(ctx context.Context, d *schema.ResourceData, meta
 	var diags diag.Diagnostics
 	conn := meta.(*conns.AWSClient).EMRServerlessConn()
 	defaultTagsConfig := meta.(*conns.AWSClient).DefaultTagsConfig
-	tags := defaultTagsConfig.MergeTags(tftags.New(d.Get("tags").(map[string]interface{})))
+	tags := defaultTagsConfig.MergeTags(tftags.New(ctx, d.Get("tags").(map[string]interface{})))
 
 	name := d.Get("name").(string)
 	input := &emrserverless.CreateApplicationInput{
