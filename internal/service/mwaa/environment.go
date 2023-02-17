7af7ef01eb4c2ae4cb1320f3c36d3ce7b68859f2
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-02-17 13:23:41 -0500

    Pass 'Context' to 'tftags.New' for mwaa.

--- internal/service/mwaa/environment.go
+++ internal/service/mwaa/environment.go
@@ -259,7 +259,7 @@ func ResourceEnvironment() *schema.Resource {
 func resourceEnvironmentCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	conn := meta.(*conns.AWSClient).MWAAConn()
 	defaultTagsConfig := meta.(*conns.AWSClient).DefaultTagsConfig
-	tags := defaultTagsConfig.MergeTags(tftags.New(d.Get("tags").(map[string]interface{})))
+	tags := defaultTagsConfig.MergeTags(tftags.New(ctx, d.Get("tags").(map[string]interface{})))
 
 	name := d.Get("name").(string)
 	input := &mwaa.CreateEnvironmentInput{
