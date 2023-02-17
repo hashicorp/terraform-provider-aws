db2ef8a1edce0dab349fb555c78f09ecda6adeb0
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-02-17 13:23:29 -0500

    Pass 'Context' to 'tftags.New' for ecr.

--- internal/service/ecr/repository.go
+++ internal/service/ecr/repository.go
@@ -118,7 +118,7 @@ func resourceRepositoryCreate(ctx context.Context, d *schema.ResourceData, meta 
 	var diags diag.Diagnostics
 	conn := meta.(*conns.AWSClient).ECRConn()
 	defaultTagsConfig := meta.(*conns.AWSClient).DefaultTagsConfig
-	tags := defaultTagsConfig.MergeTags(tftags.New(d.Get("tags").(map[string]interface{})))
+	tags := defaultTagsConfig.MergeTags(tftags.New(ctx, d.Get("tags").(map[string]interface{})))
 
 	input := ecr.CreateRepositoryInput{
 		ImageTagMutability:      aws.String(d.Get("image_tag_mutability").(string)),
