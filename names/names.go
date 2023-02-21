341ea5d725867a7b268c5b1dae5d63dfe8fc2e0d
Kevin Chun <poshchu@amazon.com>
2023-02-21 12:01:22 +1100

    Add SSMContacts to names/names.go

--- names/names.go
+++ names/names.go
@@ -40,6 +40,7 @@ const (
 	SchedulerEndpointID            = "scheduler"
 	SESV2EndpointID                = "sesv2"
 	SSMEndpointID                  = "ssm"
+	SSMContactsEndpointId		   = "ssm-contacts"
 	TranscribeEndpointID           = "transcribe"
 )
 
