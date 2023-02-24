9a8ed8fbf008eaf1bbd38cfbaa2908671d7ea67a
Lukas Hetzenecker <lukas.hetzenecker@tmnl.nl>
2023-02-24 17:05:59 +0100

    EC2 Transit Gateway Attachments: Add attributes

--- internal/service/ec2/transitgateway_attachment_data_source.go
+++ internal/service/ec2/transitgateway_attachment_data_source.go
@@ -55,6 +55,14 @@ func DataSourceTransitGatewayAttachment() *schema.Resource {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+			"association_state": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"association_transit_gateway_route_table_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
 		},
 	}
 }
@@ -104,6 +112,8 @@ func dataSourceTransitGatewayAttachmentRead(ctx context.Context, d *schema.Resou
 	d.Set("transit_gateway_attachment_id", transitGatewayAttachmentID)
 	d.Set("transit_gateway_id", transitGatewayAttachment.TransitGatewayId)
 	d.Set("transit_gateway_owner_id", transitGatewayAttachment.TransitGatewayOwnerId)
+	d.Set("association_state", transitGatewayAttachment.Association.State)
+	d.Set("association_transit_gateway_route_table_id", transitGatewayAttachment.Association.TransitGatewayRouteTableId)
 
 	if err := d.Set("tags", KeyValueTags(ctx, transitGatewayAttachment.Tags).IgnoreAWS().IgnoreConfig(ignoreTagsConfig).Map()); err != nil {
 		return sdkdiag.AppendErrorf(diags, "setting tags: %s", err)
