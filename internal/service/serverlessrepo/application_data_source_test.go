4f3d0a4cd221c31d93be78f4ab765bf3bf0f7a5b
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-02-24 16:37:26 -0500

    Add 'Context' argument to 'acctest.PreCheck' for serverlessrepo.

--- internal/service/serverlessrepo/application_data_source_test.go
+++ internal/service/serverlessrepo/application_data_source_test.go
@@ -16,7 +16,7 @@ func TestAccServerlessRepoApplicationDataSource_basic(t *testing.T) {
 	appARN := testAccCloudFormationApplicationID()
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:                 func() { acctest.PreCheck(t) },
+		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
 		ErrorCheck:               acctest.ErrorCheck(t, serverlessapplicationrepository.EndpointsID),
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
 		Steps: []resource.TestStep{
@@ -49,7 +49,7 @@ func TestAccServerlessRepoApplicationDataSource_versioned(t *testing.T) {
 	)
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:                 func() { acctest.PreCheck(t) },
+		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
 		ErrorCheck:               acctest.ErrorCheck(t, serverlessapplicationrepository.EndpointsID),
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
 		Steps: []resource.TestStep{
