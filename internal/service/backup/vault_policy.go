318b90e1ae8ca83ca10acb0d99f1e58e7df0f679
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-02-28 13:08:26 -0500

    Enable Plugin SDK resource self-registration for backup.

--- internal/service/backup/vault_policy.go
+++ internal/service/backup/vault_policy.go
@@ -17,6 +17,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
+// @SDKResource("aws_backup_vault_policy")
 func ResourceVaultPolicy() *schema.Resource {
 	return &schema.Resource{
 		CreateWithoutTimeout: resourceVaultPolicyPut,
