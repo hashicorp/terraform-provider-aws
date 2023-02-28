b3b805a67d07884aeceb3ca30eceae7cf042ac58
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-02-28 12:40:02 -0500

    Enable Plugin SDK data source self-registration for acm.

--- internal/service/acm/certificate_data_source.go
+++ internal/service/acm/certificate_data_source.go
@@ -15,6 +15,7 @@ import (
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 )
 
+// @SDKDataSource("aws_acm_certificate")
 func DataSourceCertificate() *schema.Resource {
 	return &schema.Resource{
 		ReadWithoutTimeout: dataSourceCertificateRead,
