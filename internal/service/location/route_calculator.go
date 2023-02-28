0ce59c6316226ebaafa3120f75b50193c3a28ab3
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-02-28 13:08:49 -0500

    Enable Plugin SDK resource self-registration for location.

--- internal/service/location/route_calculator.go
+++ internal/service/location/route_calculator.go
@@ -18,6 +18,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
+// @SDKResource("aws_location_route_calculator")
 func ResourceRouteCalculator() *schema.Resource {
 	return &schema.Resource{
 		CreateWithoutTimeout: resourceRouteCalculatorCreate,
