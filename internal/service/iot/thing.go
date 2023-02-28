51a9a092c9caad150506d19ce3ed10d01ee4eb26
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-02-28 13:08:45 -0500

    Enable Plugin SDK resource self-registration for iot.

--- internal/service/iot/thing.go
+++ internal/service/iot/thing.go
@@ -16,6 +16,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 )
 
+// @SDKResource("aws_iot_thing")
 func ResourceThing() *schema.Resource {
 	return &schema.Resource{
 		CreateWithoutTimeout: resourceThingCreate,
--- internal/service/iot/thing_group.go
+++ internal/service/iot/thing_group.go
@@ -19,6 +19,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
+// @SDKResource("aws_iot_thing_group")
 func ResourceThingGroup() *schema.Resource {
 	return &schema.Resource{
 		CreateWithoutTimeout: resourceThingGroupCreate,
