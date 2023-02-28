a9fc80e244e2938fe757341879d70f3c61534b8d
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-02-28 13:08:24 -0500

    Enable Plugin SDK resource self-registration for applicationinsights.

--- internal/service/applicationinsights/application.go
+++ internal/service/applicationinsights/application.go
@@ -19,6 +19,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
+// @SDKResource("aws_applicationinsights_application")
 func ResourceApplication() *schema.Resource {
 	return &schema.Resource{
 		CreateWithoutTimeout: resourceApplicationCreate,
