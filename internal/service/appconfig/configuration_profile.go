6098e44cbfb6db576010af67a590d0758dd331c4
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-02-28 13:08:23 -0500

    Enable Plugin SDK resource self-registration for appconfig.

--- internal/service/appconfig/configuration_profile.go
+++ internal/service/appconfig/configuration_profile.go
@@ -20,6 +20,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
+// @SDKResource("aws_appconfig_configuration_profile")
 func ResourceConfigurationProfile() *schema.Resource {
 	return &schema.Resource{
 		CreateWithoutTimeout: resourceConfigurationProfileCreate,
