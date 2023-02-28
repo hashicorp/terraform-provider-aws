f08824d929b7983bf95c280008e2333f5486460c
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-02-28 13:08:24 -0500

    Enable Plugin SDK resource self-registration for appmesh.

--- internal/service/appmesh/route.go
+++ internal/service/appmesh/route.go
@@ -22,6 +22,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
+// @SDKResource("aws_appmesh_route")
 func ResourceRoute() *schema.Resource {
 	return &schema.Resource{
 		CreateWithoutTimeout: resourceRouteCreate,
