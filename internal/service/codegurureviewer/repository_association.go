efc8600568f54cb61a6fa2a9db0fb420da3d75a7
Albert Silva <albsilv@amazon.com>
2023-03-01 07:48:10 -0500

    update resource for self-registration

--- internal/service/codegurureviewer/repository_association.go
+++ internal/service/codegurureviewer/repository_association.go
@@ -24,6 +24,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
+// @SDKResource("aws_codegurureviewer_repository_association")
 func ResourceRepositoryAssociation() *schema.Resource {
 	return &schema.Resource{
 
--- internal/service/codegurureviewer/service_package_gen.go
+++ internal/service/codegurureviewer/service_package_gen.go
@@ -26,7 +26,9 @@ func (p *servicePackage) SDKDataSources(ctx context.Context) map[string]func() *
 }
 
 func (p *servicePackage) SDKResources(ctx context.Context) map[string]func() *schema.Resource {
-	return map[string]func() *schema.Resource{}
+	return map[string]func() *schema.Resource{
+		"aws_codegurureviewer_repository_association": ResourceRepositoryAssociation,
+	}
 }
 
 func (p *servicePackage) ServicePackageName() string {
