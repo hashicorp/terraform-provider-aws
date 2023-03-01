9d6f569482d76ad033b008225fb2787d607492eb
Graham Davison <gdavison@hashicorp.com>
2023-03-01 11:55:26 -0800

    Extends IAM propagation time for OpenSearch domain creation

--- internal/service/opensearch/consts.go
+++ internal/service/opensearch/consts.go
@@ -5,5 +5,7 @@ import (
 )
 
 const (
-	propagationTimeout = 2 * time.Minute
+	// OpenSearch sometimes needs a longer IAM propagation time than most servicces,
+	// especially with acceptnace tests
+	propagationTimeout = 10 * time.Minute
 )
