efc286780a56a4368888eb71afa3dc97ec1ecbb4
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-03-07 10:45:15 -0500

    Add 'findClusters' and 'findCluster'.

--- internal/service/cloudhsmv2/find.go
+++ internal/service/cloudhsmv2/find.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/cloudhsmv2"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 )
 
 func FindClusterByID(ctx context.Context, conn *cloudhsmv2.CloudHSMV2, id string) (*cloudhsmv2.Cluster, error) {
@@ -14,21 +16,58 @@ func FindClusterByID(ctx context.Context, conn *cloudhsmv2.CloudHSMV2, id string
 		},
 	}
 
-	var result *cloudhsmv2.Cluster
+	output, err := findCluster(ctx, conn, input)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if state := aws.StringValue(output.State); state == cloudhsmv2.ClusterStateDeleted {
+		return nil, &resource.NotFoundError{
+			Message:     state,
+			LastRequest: input,
+		}
+	}
+
+	// Eventual consistency check.
+	if aws.StringValue(output.ClusterId) != id {
+		return nil, &resource.NotFoundError{
+			LastRequest: input,
+		}
+	}
+
+	return output, nil
+}
+
+func findCluster(ctx context.Context, conn *cloudhsmv2.CloudHSMV2, input *cloudhsmv2.DescribeClustersInput) (*cloudhsmv2.Cluster, error) {
+	output, err := findClusters(ctx, conn, input)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if len(output) == 0 || output[0] == nil {
+		return nil, tfresource.NewEmptyResultError(input)
+	}
+
+	if count := len(output); count > 1 {
+		return nil, tfresource.NewTooManyResultsError(count, input)
+	}
+
+	return output[0], nil
+}
+
+func findClusters(ctx context.Context, conn *cloudhsmv2.CloudHSMV2, input *cloudhsmv2.DescribeClustersInput) ([]*cloudhsmv2.Cluster, error) {
+	var output []*cloudhsmv2.Cluster
 
 	err := conn.DescribeClustersPagesWithContext(ctx, input, func(page *cloudhsmv2.DescribeClustersOutput, lastPage bool) bool {
 		if page == nil {
 			return !lastPage
 		}
 
-		for _, cluster := range page.Clusters {
-			if cluster == nil {
-				continue
-			}
-
-			if aws.StringValue(cluster.ClusterId) == id {
-				result = cluster
-				break
+		for _, v := range page.Clusters {
+			if v != nil {
+				output = append(output, v)
 			}
 		}
 
@@ -39,45 +78,32 @@ func FindClusterByID(ctx context.Context, conn *cloudhsmv2.CloudHSMV2, id string
 		return nil, err
 	}
 
-	return result, nil
+	return output, nil
 }
 
 func FindHSMByTwoPartKey(ctx context.Context, conn *cloudhsmv2.CloudHSMV2, hsmID, eniID string) (*cloudhsmv2.Hsm, error) {
 	input := &cloudhsmv2.DescribeClustersInput{}
 
-	var result *cloudhsmv2.Hsm
-
-	err := conn.DescribeClustersPagesWithContext(ctx, input, func(page *cloudhsmv2.DescribeClustersOutput, lastPage bool) bool {
-		if page == nil {
-			return !lastPage
-		}
-
-		for _, cluster := range page.Clusters {
-			if cluster == nil {
-				continue
-			}
-
-			for _, hsm := range cluster.Hsms {
-				if hsm == nil {
-					continue
-				}
-
-				// CloudHSMv2 HSM instances can be recreated, but the ENI ID will
-				// remain consistent. Without this ENI matching, HSM instances
-				// instances can become orphaned.
-				if aws.StringValue(hsm.HsmId) == hsmID || aws.StringValue(hsm.EniId) == eniID {
-					result = hsm
-					return false
-				}
-			}
-		}
-
-		return !lastPage
-	})
+	output, err := findClusters(ctx, conn, input)
 
 	if err != nil {
 		return nil, err
 	}
 
-	return result, nil
+	for _, v := range output {
+		for _, v := range v.Hsms {
+			if v == nil {
+				continue
+			}
+
+			// CloudHSMv2 HSM instances can be recreated, but the ENI ID will
+			// remain consistent. Without this ENI matching, HSM instances
+			// instances can become orphaned.
+			if aws.StringValue(v.HsmId) == hsmID || aws.StringValue(v.EniId) == eniID {
+				return v, nil
+			}
+		}
+	}
+
+	return nil, &resource.NotFoundError{}
 }
