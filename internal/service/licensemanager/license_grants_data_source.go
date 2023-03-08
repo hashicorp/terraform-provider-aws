47cb62eec1f17b17a719d5ffdb7f7193da555ac7
Brittan DeYoung <32572259+brittandeyoung@users.noreply.github.com>
2023-03-08 10:32:35 -0500

    licensemanager: Add license_grants to service gen

--- internal/service/licensemanager/license_grants_data_source.go
+++ internal/service/licensemanager/license_grants_data_source.go
@@ -13,6 +13,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
 )
 
+// @SDKDataSource("aws_licensemanager_grants")
 func DataSourceDistributedGrants() *schema.Resource {
 	return &schema.Resource{
 		ReadWithoutTimeout: dataSourceDistributedGrantsRead,
--- internal/service/licensemanager/service_package_gen.go
+++ internal/service/licensemanager/service_package_gen.go
@@ -22,6 +22,10 @@ func (p *servicePackage) FrameworkResources(ctx context.Context) []*types.Servic
 func (p *servicePackage) SDKDataSources(ctx context.Context) []*types.ServicePackageSDKDataSource {
 	return []*types.ServicePackageSDKDataSource{
 		{
+			Factory:  DataSourceDistributedGrants,
+			TypeName: "aws_licensemanager_grants",
+		},
+		{
 			Factory:  DataSourceReceivedLicense,
 			TypeName: "aws_licensemanager_received_license",
 		},
