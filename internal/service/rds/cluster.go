a34ca9bcfe45b03f909b5281ec73bf5f5a5e59b3
Tyler Lynch <awstyler@amazon.com>
2023-03-11 12:46:26 -0500

    removed GetOkExists and replaced with GetOk for manage master password

--- internal/service/rds/cluster.go
+++ internal/service/rds/cluster.go
@@ -553,7 +553,7 @@ func resourceClusterCreate(ctx context.Context, d *schema.ResourceData, meta int
 			input.KmsKeyId = aws.String(v.(string))
 		}
 
-		if v, ok := d.GetOkExists("manage_master_user_password"); ok {
+		if v, ok := d.GetOk("manage_master_user_password"); ok {
 			modifyDbClusterInput.ManageMasterUserPassword = aws.Bool(v.(bool))
 			requiresModifyDbCluster = true
 		}
