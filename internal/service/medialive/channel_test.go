9f496fca3815beb30c28f491d578ec52551fc473
Lasse Emil Hildebrandt <j1x4r4@gmail.com>
2023-03-14 18:42:58 +0100

    Added missing context

--- internal/service/medialive/channel_test.go
+++ internal/service/medialive/channel_test.go
@@ -217,7 +217,7 @@ func TestAccMediaLiveChannel_MsSmooth_outputSettings(t *testing.T) {
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck: func() {
-			acctest.PreCheck(t)
+			acctest.PreCheck(ctx, t)
 			acctest.PreCheckPartitionHasService(t, names.MediaLiveEndpointID)
 			testAccChannelsPreCheck(ctx, t)
 		},
