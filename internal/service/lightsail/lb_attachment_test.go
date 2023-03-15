95546b230f8c04a7730ee19eea106697606daa8d
Brittan DeYoung <32572259+brittandeyoung@users.noreply.github.com>
2023-03-15 12:26:52 -0400

    lightsail: Amend lb_attachment_test, update blueprint id to valid

--- internal/service/lightsail/lb_attachment_test.go
+++ internal/service/lightsail/lb_attachment_test.go
@@ -147,7 +147,7 @@ resource "aws_lightsail_lb" "test" {
 resource "aws_lightsail_instance" "test" {
   name              = %[2]q
   availability_zone = data.aws_availability_zones.available.names[0]
-  blueprint_id      = "amazon_linux"
+  blueprint_id      = "amazon_linux_2"
   bundle_id         = "nano_1_0"
 }
 resource "aws_lightsail_lb_attachment" "test" {
