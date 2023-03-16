95c8a4d5c15de1c6c79a77849570487c41b484c1
Tyler Lynch <awstyler@amazon.com>
2023-03-16 12:08:51 -0400

    Adjust KMS key prinicpal for correct format.

--- internal/service/rds/cluster_test.go
+++ internal/service/rds/cluster_test.go
@@ -2492,7 +2492,7 @@ resource "aws_kms_key" "example" {
       "Sid": "Enable IAM User Permissions",
       "Effect": "Allow",
       "Principal": {
-        "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:*"
+        "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
       },
       "Action": "kms:*",
       "Resource": "*"
