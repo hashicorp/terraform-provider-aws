54e84f5929e2cc2b543dab743de77314eca3c8dc
Gerard Solé <g.sole.ca@gmail.com>
2023-03-20 13:25:23 +0100

    r/aws_ecs_task_definition: add track_latest attribute

--- internal/service/ecs/task_definition.go
+++ internal/service/ecs/task_definition.go
@@ -276,6 +276,11 @@ func ResourceTaskDefinition() *schema.Resource {
 				ForceNew:     true,
 				ValidateFunc: verify.ValidARN,
 			},
+			"track_latest": {
+				Type:     schema.TypeBool,
+				Default:  false,
+				Optional: true,
+			},
 			"volume": {
 				Type:     schema.TypeSet,
 				Optional: true,
@@ -562,8 +567,14 @@ func resourceTaskDefinitionRead(ctx context.Context, d *schema.ResourceData, met
 	var diags diag.Diagnostics
 	conn := meta.(*conns.AWSClient).ECSConn(ctx)
 
+	trackedTaskDefinition := aws.String(d.Get("arn").(string))
+	if v, ok := d.GetOk("track_latest"); ok && v.(bool) {
+		log.Printf("[DEBUG] Tracking latest ECS Task Definition")
+		trackedTaskDefinition = aws.String(d.Get("family").(string))
+	}
+
 	input := ecs.DescribeTaskDefinitionInput{
-		TaskDefinition: aws.String(d.Get("arn").(string)),
+		TaskDefinition: trackedTaskDefinition,
 		Include:        []*string{aws.String(ecs.TaskDefinitionFieldTags)},
 	}
 
