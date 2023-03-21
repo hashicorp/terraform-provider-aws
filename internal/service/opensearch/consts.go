85d8f425f9aba98e9866736c20c0d6d95527b250
Graham Davison <gdavison@hashicorp.com>
2023-03-20 17:44:28 -0700

    Spelling fix

--- internal/service/opensearch/consts.go
+++ internal/service/opensearch/consts.go
@@ -6,6 +6,6 @@ import (
 
 const (
 	// OpenSearch sometimes needs a longer IAM propagation time than most servicces,
-	// especially with acceptnace tests
+	// especially with acceptance tests
 	propagationTimeout = 10 * time.Minute
 )
