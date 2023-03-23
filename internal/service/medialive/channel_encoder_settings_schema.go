e8b05dbae707d98e52d2ad210493de8f6ef6df1b
Jonas Hansen <jonasdkhansen@gmail.com>
2023-03-23 08:34:36 +0100

    Removed maxitems from videodescription typelist

--- internal/service/medialive/channel_encoder_settings_schema.go
+++ internal/service/medialive/channel_encoder_settings_schema.go
@@ -1318,7 +1318,6 @@ func channelEncoderSettingsSchema() *schema.Schema {
 					Type:     schema.TypeList,
 					Optional: true,
 					Computed: true,
-					MaxItems: 1,
 					Elem: &schema.Resource{
 						Schema: map[string]*schema.Schema{
 							"name": {
