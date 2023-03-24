e2e43c33151a541aac495a42c33e1506c376d1a3
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-03-24 14:35:50 -0400

    Fix terrafmt error.

--- internal/service/appmesh/virtual_router_data_source_test.go
+++ internal/service/appmesh/virtual_router_data_source_test.go
@@ -70,7 +70,7 @@ resource "aws_appmesh_virtual_router" "test" {
 }
 
 data "aws_appmesh_virtual_router" "test" {
-  name = aws_appmesh_virtual_router.test.name
+  name      = aws_appmesh_virtual_router.test.name
   mesh_name = aws_appmesh_mesh.test.name
 }
 `, meshName, vrName)
