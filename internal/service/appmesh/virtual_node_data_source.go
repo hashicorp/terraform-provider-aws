f4288af685dde296750c099f4fa06c059a111f3e
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-03-24 09:11:13 -0400

    d/aws_appmesh_virtual_node: Tidy up.

--- internal/service/appmesh/virtual_node_data_source.go
+++ internal/service/appmesh/virtual_node_data_source.go
@@ -2,9 +2,13 @@ package appmesh
 
 import (
 	"context"
+	"time"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
@@ -44,101 +48,54 @@ func DataSourceVirtualNode() *schema.Resource {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
-			"spec":         {},
-			names.AttrTags: tftags.TagsSchema(),
+			"spec":         dataSourcePropertyFromResourceProperty(resourceVirtualNodeSpecSchema()),
+			names.AttrTags: tftags.TagsSchemaComputed(),
 		},
 	}
 }
 
 func dataSourceVirtualNodeRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
-	/*
-		conn := meta.(*conns.AWSClient).AppMeshConn()
-		ignoreTagsConfig := meta.(*conns.AWSClient).IgnoreTagsConfig
+	conn := meta.(*conns.AWSClient).AppMeshConn()
+	ignoreTagsConfig := meta.(*conns.AWSClient).IgnoreTagsConfig
 
-		req := &appmesh.DescribeVirtualNodeInput{
-			MeshName:        aws.String(d.Get("mesh_name").(string)),
-			VirtualNodeName: aws.String(d.Get("name").(string)),
-		}
-		if v, ok := d.GetOk("mesh_owner"); ok {
-			req.MeshOwner = aws.String(v.(string))
-		}
+	virtualNodeName := d.Get("name").(string)
+	vn, err := FindVirtualNodeByThreePartKey(ctx, conn, d.Get("mesh_name").(string), d.Get("mesh_owner").(string), virtualNodeName)
 
-		var resp *appmesh.DescribeVirtualNodeOutput
+	if err != nil {
+		return sdkdiag.AppendErrorf(diags, "reading App Mesh Virtual Node (%s): %s", virtualNodeName, err)
+	}
 
-		err := resource.Retry(propagationTimeout, func() *resource.RetryError {
-			var err error
+	d.SetId(aws.StringValue(vn.VirtualNodeName))
+	arn := aws.StringValue(vn.Metadata.Arn)
+	d.Set("arn", arn)
+	d.Set("created_date", vn.Metadata.CreatedAt.Format(time.RFC3339))
+	d.Set("last_updated_date", vn.Metadata.LastUpdatedAt.Format(time.RFC3339))
+	d.Set("mesh_name", vn.MeshName)
+	meshOwner := aws.StringValue(vn.Metadata.MeshOwner)
+	d.Set("mesh_owner", meshOwner)
+	d.Set("name", vn.VirtualNodeName)
+	d.Set("resource_owner", vn.Metadata.ResourceOwner)
+	if err := d.Set("spec", flattenVirtualNodeSpec(vn.Spec)); err != nil {
+		return sdkdiag.AppendErrorf(diags, "setting spec: %s", err)
+	}
 
-			resp, err = conn.DescribeVirtualNode(req)
+	// https://docs.aws.amazon.com/app-mesh/latest/userguide/sharing.html#sharing-permissions
+	// Owners and consumers can list tags and can tag/untag resources in a mesh that the account created.
+	// They can't list tags and tag/untag resources in a mesh that aren't created by the account.
+	var tags tftags.KeyValueTags
 
-			if d.IsNewResource() && tfawserr.ErrCodeEquals(err, appmesh.ErrCodeNotFoundException) {
-				return resource.RetryableError(err)
-			}
-
-			if err != nil {
-				return resource.NonRetryableError(err)
-			}
-
-			return nil
-		})
-
-		if tfresource.TimedOut(err) {
-			resp, err = conn.DescribeVirtualNode(req)
-		}
-
-		if !d.IsNewResource() && tfawserr.ErrCodeEquals(err, appmesh.ErrCodeNotFoundException) {
-			log.Printf("[WARN] App Mesh Virtual Node (%s) not found, removing from state", d.Id())
-			d.SetId("")
-			return nil
-		}
+	if meshOwner == meta.(*conns.AWSClient).AccountID {
+		tags, err = ListTags(ctx, conn, arn)
 
 		if err != nil {
-			return fmt.Errorf("reading App Mesh Virtual Node: %w", err)
+			return sdkdiag.AppendErrorf(diags, "listing tags for App Mesh Virtual Node (%s): %s", arn, err)
 		}
+	}
 
-		if resp == nil || resp.VirtualNode == nil {
-			return fmt.Errorf("reading App Mesh Virtual Node: empty response")
-		}
+	if err := d.Set("tags", tags.IgnoreAWS().IgnoreConfig(ignoreTagsConfig).Map()); err != nil {
+		return sdkdiag.AppendErrorf(diags, "setting tags: %s", err)
+	}
 
-		if aws.StringValue(resp.VirtualNode.Status.Status) == appmesh.VirtualNodeStatusCodeDeleted {
-			if d.IsNewResource() {
-				return fmt.Errorf("reading App Mesh Virtual Node: %s after creation", aws.StringValue(resp.VirtualNode.Status.Status))
-			}
-
-			log.Printf("[WARN] App Mesh Virtual Node (%s) not found, removing from state", d.Id())
-			d.SetId("")
-			return nil
-		}
-
-		arn := aws.StringValue(resp.VirtualNode.Metadata.Arn)
-		d.Set("name", resp.VirtualNode.VirtualNodeName)
-		d.Set("mesh_name", resp.VirtualNode.MeshName)
-		d.Set("mesh_owner", resp.VirtualNode.Metadata.MeshOwner)
-		d.Set("arn", arn)
-		d.Set("created_date", resp.VirtualNode.Metadata.CreatedAt.Format(time.RFC3339))
-		d.Set("last_updated_date", resp.VirtualNode.Metadata.LastUpdatedAt.Format(time.RFC3339))
-		d.Set("resource_owner", resp.VirtualNode.Metadata.ResourceOwner)
-		err = d.Set("spec", flattenVirtualNodeSpec(resp.VirtualNode.Spec))
-		if err != nil {
-			return fmt.Errorf("setting spec: %w", err)
-		}
-
-		tags, err := ListTags(conn, arn)
-
-		if err != nil {
-			return fmt.Errorf("listing tags for App Mesh virtual node (%s): %w", arn, err)
-		}
-
-		tags = tags.IgnoreAWS().IgnoreConfig(ignoreTagsConfig)
-
-		//lintignore:AWSR002
-		if err := d.Set("tags", tags.RemoveDefaultConfig(defaultTagsConfig).Map()); err != nil {
-			return fmt.Errorf("setting tags: %w", err)
-		}
-
-		if err := d.Set("tags_all", tags.Map()); err != nil {
-			return fmt.Errorf("setting tags_all: %w", err)
-		}
-	*/
 	return diags
 }
