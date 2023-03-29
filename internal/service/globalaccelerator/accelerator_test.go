f7b886fae9037a8d3624a471ca4f2934200d9020
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-03-29 10:31:30 -0400

    Fix 'AccessControlListNotSupported: The bucket does not allow ACLs' error in acceptance test configuration.

--- internal/service/globalaccelerator/accelerator_test.go
+++ internal/service/globalaccelerator/accelerator_test.go
@@ -476,11 +476,6 @@ resource "aws_s3_bucket" "test" {
   force_destroy = true
 }
 
-resource "aws_s3_bucket_acl" "test" {
-  bucket = aws_s3_bucket.test.id
-  acl    = "private"
-}
-
 resource "aws_globalaccelerator_accelerator" "test" {
   name            = %[1]q
   ip_address_type = "IPV4"
