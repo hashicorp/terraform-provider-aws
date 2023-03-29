4c59054ada704aca67fb7cd44225b0cd9905ed41
Jared Baker <jared.baker@hashicorp.com>
2023-03-29 11:38:53 -0400

    r/aws_quicksight_data_set(test): s3 bucket default security changes

--- internal/service/quicksight/data_source_test.go
+++ internal/service/quicksight/data_source_test.go
@@ -482,12 +482,36 @@ resource "aws_s3_bucket" "test" {
   force_destroy = true
 }
 
+resource "aws_s3_bucket_public_access_block" "test" {
+  bucket = aws_s3_bucket.test.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_ownership_controls" "test" {
+  bucket = aws_s3_bucket.test.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
 resource "aws_s3_bucket_acl" "test" {
+  depends_on = [
+	aws_s3_bucket_public_access_block.test,
+	aws_s3_bucket_ownership_controls.test,
+  ]
+
   bucket = aws_s3_bucket.test.id
   acl    = "public-read"
 }
 
 resource "aws_s3_object" "test" {
+  depends_on = [aws_s3_bucket_acl.test]
+
   bucket  = aws_s3_bucket.test.bucket
   key     = %[1]q
   content = <<EOF
