9f148c6f32e9e7feaf829b71d01211bfe4237f10
Fabien COMTE <fabien.comte@daia.tech>
2023-03-31 00:45:39 +0200

    New resource aws_quicksight_account_subscription

--- internal/service/quicksight/service_package_gen.go
+++ internal/service/quicksight/service_package_gen.go
@@ -26,6 +26,10 @@ func (p *servicePackage) SDKDataSources(ctx context.Context) []*types.ServicePac
 func (p *servicePackage) SDKResources(ctx context.Context) []*types.ServicePackageSDKResource {
 	return []*types.ServicePackageSDKResource{
 		{
+			Factory:  ResourceAccountSubscription,
+			TypeName: "aws_quicksight_account_subscription",
+		},
+		{
 			Factory:  ResourceDataSet,
 			TypeName: "aws_quicksight_data_set",
 		},
