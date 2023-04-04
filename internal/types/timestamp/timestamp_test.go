5404375e5bfb5814690c60b006ae77032c7a2046
Adrian Johnson <adrian.johnson@hashicorp.com>
2023-04-04 11:03:55 -0500

    linter: remove leading whitespace

--- internal/types/timestamp/timestamp_test.go
+++ internal/types/timestamp/timestamp_test.go
@@ -28,7 +28,6 @@ func TestValidateOnceADayWindowFormat(t *testing.T) {
 	for name, test := range tests {
 		name, test := name, test
 		t.Run(name, func(t *testing.T) {
-
 			t.Parallel()
 
 			ts := New(test.value)
