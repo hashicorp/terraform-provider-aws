2273d5aca52515112623469a23865c12c8d96bfc
Matt Burgess <549318+mattburgess@users.noreply.github.com>
2023-04-05 22:33:59 +0100

    Add VPCLattice Service data source

--- internal/service/vpclattice/service_package_gen.go
+++ internal/service/vpclattice/service_package_gen.go
@@ -20,7 +20,12 @@ func (p *servicePackage) FrameworkResources(ctx context.Context) []*types.Servic
 }
 
 func (p *servicePackage) SDKDataSources(ctx context.Context) []*types.ServicePackageSDKDataSource {
-	return []*types.ServicePackageSDKDataSource{}
+	return []*types.ServicePackageSDKDataSource{
+		{
+			Factory:  DataSourceService,
+			TypeName: "aws_vpclattice_service",
+		},
+	}
 }
 
 func (p *servicePackage) SDKResources(ctx context.Context) []*types.ServicePackageSDKResource {
