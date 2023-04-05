3ce1aa7263480a407dec95c077248f59da4605f7
George Blue <gblue@pivotal.io>
2023-04-05 14:32:33 +0100

    fix: error when scaling elasticache_replication_group
    When adding extra nodes to an elasticache_replication_group, we saw the
    error:
    ```
    Error: modifying ElastiCache Replication Group (csbb2a2560a-acf7-4106-9618-4263082cb536) clusters: error waiting for ElastiCache Replication Group (csbb2a2560a-acf7-4106-9618-4263082cb536) replica addition: unexpected state 'snapshotting', wanted target 'available'.
                  last error: %!s(<nil>) with aws_elasticache_replication_group.redis, on main.tf line 46, in resource "aws_elasticache_replication_group" "redis": 46: resource "aws_elasticache_replication_group" "redis"
    ```
    
    Looks like a snapshot occurred while waiting for the scale up. I see
    that "snapshotting" is an allowed state in the
    `WaitReplicationGroupAvailable()` function, so for consistency it makes
    sense for it to also be allowed in the
    `WaitReplicationGroupMemberClustersAvailable()` function.
    
    Testing: tests of this functionality don't seem to go into this level of
    detail, so no tests were modified.

--- internal/service/elasticache/wait.go
+++ internal/service/elasticache/wait.go
@@ -78,6 +78,7 @@ func WaitReplicationGroupMemberClustersAvailable(ctx context.Context, conn *elas
 			CacheClusterStatusCreating,
 			CacheClusterStatusDeleting,
 			CacheClusterStatusModifying,
+			CacheClusterStatusSnapshotting,
 		},
 		Target:     []string{CacheClusterStatusAvailable},
 		Refresh:    StatusReplicationGroupMemberClusters(ctx, conn, replicationGroupID),
