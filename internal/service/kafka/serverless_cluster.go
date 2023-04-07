868e5a1e9a2d23945b94a051ee6b1603653af29b
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-04-07 16:28:11 -0400

    Revert "r/aws_msk_serverless_cluster: 'vpc_config.security_group_ids' is Computed."
    This reverts commit cc4fcb4d848f2302d308106625969065cf075a3b.

--- internal/service/kafka/serverless_cluster.go
+++ internal/service/kafka/serverless_cluster.go
@@ -95,7 +95,6 @@ func ResourceServerlessCluster() *schema.Resource {
 						"security_group_ids": {
 							Type:     schema.TypeSet,
 							Optional: true,
-							Computed: true,
 							ForceNew: true,
 							MaxItems: 5,
 							Elem: &schema.Schema{
