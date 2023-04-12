a0a63b06eee6c35d65f928806aa7deb8f1117594
Bill Booth <bill.booth@autodesk.com>
2023-04-12 13:37:12 -0700

    stop using deprected "resource" lib

--- internal/service/sagemaker/monitoring_schedule.go
+++ internal/service/sagemaker/monitoring_schedule.go
@@ -10,7 +10,7 @@ import (
 	"github.com/hashicorp/aws-sdk-go-base/v2/awsv1shim/v2/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/id"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
@@ -206,7 +206,7 @@ func FindMonitoringScheduleByName(ctx context.Context, conn *sagemaker.SageMaker
 	output, err := conn.DescribeMonitoringScheduleWithContext(ctx, input)
 
 	if tfawserr.ErrCodeEquals(err, sagemaker.ErrCodeResourceNotFound) {
-		return nil, &resource.NotFoundError{
+		return nil, &retry.NotFoundError{
 			LastError:   err,
 			LastRequest: input,
 		}
