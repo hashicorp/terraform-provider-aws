b3837593f61cf2c6f67991a529c88cfa5af0c62f
Daniel Whitehead <danieljw@amazon.com>
2023-04-12 12:20:21 -0700

    fixed resource data

--- internal/service/organizations/organizational_policies_data_source_test.go
+++ internal/service/organizations/organizational_policies_data_source_test.go
@@ -33,7 +33,7 @@ func TestAccOrganizationalPoliciesDataSource_basic(t *testing.T) {
 const testAccOrganizationalPoliciesDataSourceConfig_basic = `
 data "aws_organizations_organization" "current" {}
 
-resource "aws_organizations_organizational_unit" "test" {
+resource "aws_organizations_organizational_policies" "test" {
   target_id = data.aws_organizations_organization.current.roots[0].id
 }
 `
