8055472a90a81c1d2a154adf2e0249bdb97524ba
Daniel Whitehead <danieljw@amazon.com>
2023-04-13 14:12:49 -0700

    fixing Describe call v2

--- internal/service/organizations/policy_data_source.go
+++ internal/service/organizations/policy_data_source.go
@@ -97,7 +97,7 @@ func findPolicyByPolicyID(ctx context.Context, conn *organizations.Organizations
 	}
 	var output []*organizations.Policy
 
-	err := conn.DescribePolicyWithContext(ctx, input, func(page *organizations.DescribePolicyOutput, err error) {
+	output, err := conn.DescribePolicyWithContext(ctx, input, func(page *organizations.DescribePolicyOutput, error) {
 		output = append(output, page.Policies...)
 
 		return !lastPage
