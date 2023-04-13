2534f31ec775be515a486b4a4be20737659ed1d8
Daniel Whitehead <danieljw@amazon.com>
2023-04-13 11:17:36 -0700

    adding initial file versions for organizations policy data source

---
+++ internal/service/organizations/policy_data_source.go
@@ -0,0 +1,126 @@
+package organizations
+
+import (
+	"context"
+	"github.com/aws/aws-sdk-go/service/organizations"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+)
+
+func DataSourcePolicy() *schema.Resource {
+	return &schema.Resource{
+		ReadWithoutTimeout: dataSourcePolicyRead,
+
+		Schema: map[string]*schema.Schema{
+			"policy_id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"policy": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"content": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"policy_summary": {
+							Type:     schema.TypeList,
+							Computed: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"arn": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"aws_managed": {
+										Type:     schema.TypeBool,
+										Computed: true,
+									},
+									"description": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"id": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"name": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"type": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+const (
+	DSNamePolicy = "Organization Policy Data Source"
+)
+
+func dataSourcePolicyRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn := meta.(*conns.AWSClient).OrganizationsClient()
+	policyID := d.Get("policy_id").(string)
+
+	out, err := findPolicyByPolicyID(ctx, conn, policyID)
+	if err != nil {
+		return sdkdiag.AppendErrorf(diags, "describing Policy (%s): %s", policyID, err)
+	}
+
+	d.SetId(policyID)
+	if err := d.Set("policy", FlattenPolicy(policy)); err != nil {
+		return sdkdiag.AppendErrorf(diags, "setting policy: %s", err)
+	}
+
+	return diags
+}
+func findPolicyByPolicyID(ctx context.Context, conn *organizations.Organizations, id string) ([]*organizations.Policy, error) {
+	input := &organizations.DescribePolicyInput{
+		TargetId: aws.String(id),
+	}
+	var output []*organizations.Policy
+
+	err := conn.DescribePolicyWithContext(ctx, input, func(page *organizations.DescribePolicyOutput, lastPage bool) bool {
+		output = append(output, page.Policy...)
+
+		return !lastPage
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return output, nil
+}
+
+func FlattenOrganizationPolicy(policies []*organizations.Policy) []map[string]interface{} {
+	if len(policies) == 0 {
+		return nil
+	}
+	var result []map[string]interface{}
+	for _, policy := range policies {
+		result = append(result, map[string]interface{}{
+			"context":     aws.StringValue(policy.Context),
+			"arn":         aws.StringValue(policy.PolicySummary.Arn),
+			"aws_managed": aws.BoolValue(policy.PolicySummary.AwsManaged),
+			"description": aws.StringValue(policy.PolicySummary.Description),
+			"id":          aws.StringValue(policy.PolicySummary.Id),
+			"name":        aws.StringValue(policy.PolicySummary.Name),
+			"type":        aws.StringValue(policy.PolicySummary.Type),
+		})
+	}
+	return result
+}
---
+++ internal/service/organizations/policy_data_source_test.go
@@ -0,0 +1,43 @@
+package organizations_test
+
+import (
+	"testing"
+
+	"github.com/aws/aws-sdk-go/service/organizations"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+)
+
+func TestAccOrganizationPolicyDataSource_basic(t *testing.T) {
+	ctx := acctest.Context(t)
+	dataSourceName := "data.aws_organizations_policy.test"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+			acctest.PreCheckOrganizationManagementAccount(ctx, t)
+		},
+		ErrorCheck:               acctest.ErrorCheck(t, organizations.EndpointsID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccOrganizationPolicyDataSourceConfig_basic,
+				Check: resource.ComposeTestCheckFunc(
+					acctest.CheckResourceAttrGreaterThanValue(dataSourceName, "policy.#", "0"),
+				),
+			},
+		},
+	})
+}
+
+const testAccOrganizationPolicyDataSourceConfig_basic = `
+data "aws_organizations_organization" "current" {}
+
+data "aws_organizations_oorganizational_policies" "current" {
+  target_id = data.aws_organizations_organization.current.roots[0].id
+  filter = "SERVICE_CONTROL_POLICY"
+}
+data "aws_organizations_policies" "test" {
+	policy_id= data.aws_organizations_oorganizational_policies.current.policies[0].id
+}
+`
