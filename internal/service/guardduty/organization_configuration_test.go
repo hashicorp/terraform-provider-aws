a82b3f526a5df8faa77c4156c44b00efc6c068c8
Rob Kulbin <6707630+roberth-k@users.noreply.github.com>
2023-04-14 13:15:01 +0100

    r/aws_guardduty_organization_configuration: consolidate acceptance test base configuration

--- internal/service/guardduty/organization_configuration_test.go
+++ internal/service/guardduty/organization_configuration_test.go
@@ -183,15 +183,18 @@ func testAccOrganizationConfiguration_malwareprotection(t *testing.T) {
 	})
 }
 
-func testAccOrganizationConfigurationConfig_autoEnable(autoEnable bool) string {
-	return fmt.Sprintf(`
+const testAccOrganizationConfigurationConfigBase = `
 data "aws_caller_identity" "current" {}
 
 data "aws_partition" "current" {}
 
 resource "aws_organizations_organization" "test" {
-  aws_service_access_principals = ["guardduty.${data.aws_partition.current.dns_suffix}"]
-  feature_set                   = "ALL"
+  aws_service_access_principals = [
+    "guardduty.${data.aws_partition.current.dns_suffix}",
+    "malware-protection.guardduty.${data.aws_partition.current.dns_suffix}",
+  ]
+
+  feature_set = "ALL"
 }
 
 resource "aws_guardduty_detector" "test" {}
@@ -201,35 +204,38 @@ resource "aws_guardduty_organization_admin_account" "test" {
 
   admin_account_id = data.aws_caller_identity.current.account_id
 }
+`
 
+func testAccOrganizationConfigurationConfig_autoEnable(autoEnable bool) string {
+	return acctest.ConfigCompose(
+		testAccOrganizationConfigurationConfigBase,
+		fmt.Sprintf(`
 resource "aws_guardduty_organization_configuration" "test" {
   depends_on = [aws_guardduty_organization_admin_account.test]
 
   auto_enable = %[1]t
   detector_id = aws_guardduty_detector.test.id
 }
-`, autoEnable)
+`, autoEnable))
+}
+
+func testAccOrganizationConfigurationConfig_autoEnableOrganizationMembers(value string) string {
+	return acctest.ConfigCompose(
+		testAccOrganizationConfigurationConfigBase,
+		fmt.Sprintf(`
+resource "aws_guardduty_organization_configuration" "test" {
+  depends_on = [aws_guardduty_organization_admin_account.test]
+
+  auto_enable_organization_members = %[1]q
+  detector_id                      = aws_guardduty_detector.test.id
+}
+`, value))
 }
 
 func testAccOrganizationConfigurationConfig_s3Logs(autoEnable bool) string {
-	return fmt.Sprintf(`
-data "aws_caller_identity" "current" {}
-
-data "aws_partition" "current" {}
-
-resource "aws_organizations_organization" "test" {
-  aws_service_access_principals = ["guardduty.${data.aws_partition.current.dns_suffix}"]
-  feature_set                   = "ALL"
-}
-
-resource "aws_guardduty_detector" "test" {}
-
-resource "aws_guardduty_organization_admin_account" "test" {
-  depends_on = [aws_organizations_organization.test, aws_guardduty_detector.test]
-
-  admin_account_id = data.aws_caller_identity.current.account_id
-}
-
+	return acctest.ConfigCompose(
+		testAccOrganizationConfigurationConfigBase,
+		fmt.Sprintf(`
 resource "aws_guardduty_organization_configuration" "test" {
   depends_on = [aws_guardduty_organization_admin_account.test]
 
@@ -242,28 +248,13 @@ resource "aws_guardduty_organization_configuration" "test" {
     }
   }
 }
-`, autoEnable)
+`, autoEnable))
 }
 
 func testAccOrganizationConfigurationConfig_kubernetes(autoEnable bool) string {
-	return fmt.Sprintf(`
-data "aws_caller_identity" "current" {}
-
-data "aws_partition" "current" {}
-
-resource "aws_organizations_organization" "test" {
-  aws_service_access_principals = ["guardduty.${data.aws_partition.current.dns_suffix}"]
-  feature_set                   = "ALL"
-}
-
-resource "aws_guardduty_detector" "test" {}
-
-resource "aws_guardduty_organization_admin_account" "test" {
-  depends_on = [aws_organizations_organization.test, aws_guardduty_detector.test]
-
-  admin_account_id = data.aws_caller_identity.current.account_id
-}
-
+	return acctest.ConfigCompose(
+		testAccOrganizationConfigurationConfigBase,
+		fmt.Sprintf(`
 resource "aws_guardduty_organization_configuration" "test" {
   depends_on = [aws_guardduty_organization_admin_account.test]
 
@@ -278,28 +269,13 @@ resource "aws_guardduty_organization_configuration" "test" {
     }
   }
 }
-`, autoEnable)
+`, autoEnable))
 }
 
 func testAccOrganizationConfigurationConfig_malwareprotection(autoEnable bool) string {
-	return fmt.Sprintf(`
-data "aws_caller_identity" "current" {}
-
-data "aws_partition" "current" {}
-
-resource "aws_organizations_organization" "test" {
-  aws_service_access_principals = ["guardduty.${data.aws_partition.current.dns_suffix}", "malware-protection.guardduty.${data.aws_partition.current.dns_suffix}"]
-  feature_set                   = "ALL"
-}
-
-resource "aws_guardduty_detector" "test" {}
-
-resource "aws_guardduty_organization_admin_account" "test" {
-  depends_on = [aws_organizations_organization.test, aws_guardduty_detector.test]
-
-  admin_account_id = data.aws_caller_identity.current.account_id
-}
-
+	return acctest.ConfigCompose(
+		testAccOrganizationConfigurationConfigBase,
+		fmt.Sprintf(`
 resource "aws_guardduty_organization_configuration" "test" {
   depends_on = [aws_guardduty_organization_admin_account.test]
 
@@ -316,5 +292,5 @@ resource "aws_guardduty_organization_configuration" "test" {
     }
   }
 }
-`, autoEnable)
+`, autoEnable))
 }
