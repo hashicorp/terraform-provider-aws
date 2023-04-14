d689846f19863b6e4f4e6a5b9fdfd4cb50d2bcb4
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-04-14 11:09:34 -0400

    r/aws_cloudwatch_event_endpoint: Add 'TestAccEndpoint_description'.
    Acceptance test output:
    
    % make testacc TESTARGS='-run=TestAccEndpoint_description' PKG=events
    ==> Checking that code complies with gofmt requirements...
    TF_ACC=1 go test ./internal/service/events/... -v -count 1 -parallel 20  -run=TestAccEndpoint_description -timeout 180m
    === RUN   TestAccEndpoint_description
    === PAUSE TestAccEndpoint_description
    === CONT  TestAccEndpoint_description
    --- PASS: TestAccEndpoint_description (31.00s)
    PASS
    ok  	github.com/hashicorp/terraform-provider-aws/internal/service/events	37.712s

--- internal/service/events/endpoint_test.go
+++ internal/service/events/endpoint_test.go
@@ -129,6 +129,72 @@ func TestAccEndpoint_roleARN(t *testing.T) {
 	})
 }
 
+func TestAccEndpoint_description(t *testing.T) {
+	ctx := acctest.Context(t)
+	var v eventbridge.DescribeEndpointOutput
+	var providers []*schema.Provider
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	resourceName := "aws_cloudwatch_event_endpoint.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
+		ErrorCheck:               acctest.ErrorCheck(t, eventbridge.EndpointsID),
+		ProtoV5ProviderFactories: acctest.ProtoV5FactoriesPlusProvidersAlternate(ctx, t, &providers),
+		CheckDestroy:             testAccCheckEndpointDestroy(ctx),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccEndpointConfig_description(rName, "description 1"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccCheckEndpointExists(ctx, resourceName, &v),
+					acctest.CheckResourceAttrRegionalARN(resourceName, "arn", "events", fmt.Sprintf("endpoint/%s", rName)),
+					resource.TestCheckResourceAttr(resourceName, "description", "description 1"),
+					resource.TestCheckResourceAttrSet(resourceName, "endpoint_url"),
+					resource.TestCheckResourceAttr(resourceName, "event_bus.#", "2"),
+					resource.TestCheckResourceAttrPair(resourceName, "event_bus.0.event_bus_arn", "aws_cloudwatch_event_bus.primary", "arn"),
+					resource.TestCheckResourceAttrPair(resourceName, "event_bus.1.event_bus_arn", "aws_cloudwatch_event_bus.secondary", "arn"),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "replication_config.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "replication_config.0.state", "DISABLED"),
+					resource.TestCheckResourceAttr(resourceName, "role_arn", ""),
+					resource.TestCheckResourceAttr(resourceName, "routing_config.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "routing_config.0.failover_config.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "routing_config.0.failover_config.0.primary.#", "1"),
+					resource.TestCheckResourceAttrPair(resourceName, "routing_config.0.failover_config.0.primary.0.health_check", "aws_route53_health_check.test", "arn"),
+					resource.TestCheckResourceAttr(resourceName, "routing_config.0.failover_config.0.secondary.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "routing_config.0.failover_config.0.secondary.0.route", acctest.AlternateRegion()),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccEndpointConfig_description(rName, "description 2"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccCheckEndpointExists(ctx, resourceName, &v),
+					acctest.CheckResourceAttrRegionalARN(resourceName, "arn", "events", fmt.Sprintf("endpoint/%s", rName)),
+					resource.TestCheckResourceAttr(resourceName, "description", "description 2"),
+					resource.TestCheckResourceAttrSet(resourceName, "endpoint_url"),
+					resource.TestCheckResourceAttr(resourceName, "event_bus.#", "2"),
+					resource.TestCheckResourceAttrPair(resourceName, "event_bus.0.event_bus_arn", "aws_cloudwatch_event_bus.primary", "arn"),
+					resource.TestCheckResourceAttrPair(resourceName, "event_bus.1.event_bus_arn", "aws_cloudwatch_event_bus.secondary", "arn"),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "replication_config.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "replication_config.0.state", "DISABLED"),
+					resource.TestCheckResourceAttr(resourceName, "role_arn", ""),
+					resource.TestCheckResourceAttr(resourceName, "routing_config.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "routing_config.0.failover_config.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "routing_config.0.failover_config.0.primary.#", "1"),
+					resource.TestCheckResourceAttrPair(resourceName, "routing_config.0.failover_config.0.primary.0.health_check", "aws_route53_health_check.test", "arn"),
+					resource.TestCheckResourceAttr(resourceName, "routing_config.0.failover_config.0.secondary.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "routing_config.0.failover_config.0.secondary.0.route", acctest.AlternateRegion()),
+				),
+			},
+		},
+	})
+}
+
 func TestAccEndpoint_update(t *testing.T) {
 	ctx := acctest.Context(t)
 	var v1 eventbridge.DescribeEndpointOutput
@@ -344,6 +410,38 @@ resource "aws_cloudwatch_event_endpoint" "test" {
   }
 }
 `, rName, acctest.AlternateRegion()))
+}
+
+func testAccEndpointConfig_description(rName, description string) string {
+	return acctest.ConfigCompose(testAccEndpointConfig_base(rName), fmt.Sprintf(`
+resource "aws_cloudwatch_event_endpoint" "test" {
+  name        = %[1]q
+  description = %[2]q
+
+  event_bus {
+    event_bus_arn = aws_cloudwatch_event_bus.primary.arn
+  }
+  event_bus {
+    event_bus_arn = aws_cloudwatch_event_bus.secondary.arn
+  }
+
+  replication_config {
+    state = "DISABLED"
+  }
+
+  routing_config {
+    failover_config {
+      primary {
+        health_check= aws_route53_health_check.test.arn
+      }
+
+      secondary {
+        route = %[3]q
+      }
+    }
+  }
+}
+`, rName, description, acctest.AlternateRegion()))
 }
 
 func testAccEndpointConfig_updateAttributes(name string) string {
