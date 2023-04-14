ebf1a04cdd42f688de9ce24f29630bc35744bc60
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-04-14 11:31:11 -0400

    r/aws_cloudwatch_event_endpoint: Add 'TestAccEndpoint_updateRoutingConfig'.
    Acceptance test output:
    
    % make testacc TESTARGS='-run=TestAccEndpoint_updateRoutingConfig' PKG=events
    ==> Checking that code complies with gofmt requirements...
    TF_ACC=1 go test ./internal/service/events/... -v -count 1 -parallel 20  -run=TestAccEndpoint_updateRoutingConfig -timeout 180m
    === RUN   TestAccEndpoint_updateRoutingConfig
    === PAUSE TestAccEndpoint_updateRoutingConfig
    === CONT  TestAccEndpoint_updateRoutingConfig
    --- PASS: TestAccEndpoint_updateRoutingConfig (31.80s)
    PASS
    ok  	github.com/hashicorp/terraform-provider-aws/internal/service/events	37.089s

--- internal/service/events/endpoint_test.go
+++ internal/service/events/endpoint_test.go
@@ -195,29 +195,61 @@ func TestAccEndpoint_description(t *testing.T) {
 	})
 }
 
-func TestAccEndpoint_update(t *testing.T) {
+func TestAccEndpoint_updateRoutingConfig(t *testing.T) {
 	ctx := acctest.Context(t)
-	var v1 eventbridge.DescribeEndpointOutput
+	var v eventbridge.DescribeEndpointOutput
+	var providers []*schema.Provider
 	resourceName := "aws_cloudwatch_event_endpoint.test"
-	endpointName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
 		ErrorCheck:               acctest.ErrorCheck(t, eventbridge.EndpointsID),
-		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		ProtoV5ProviderFactories: acctest.ProtoV5FactoriesPlusProvidersAlternate(ctx, t, &providers),
 		CheckDestroy:             testAccCheckEndpointDestroy(ctx),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccEndpointConfig_basic(endpointName),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckEndpointExists(ctx, resourceName, &v1),
+				Config: testAccEndpointConfig_basic(rName),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccCheckEndpointExists(ctx, resourceName, &v),
+					acctest.CheckResourceAttrRegionalARN(resourceName, "arn", "events", fmt.Sprintf("endpoint/%s", rName)),
+					resource.TestCheckResourceAttr(resourceName, "description", ""),
+					resource.TestCheckResourceAttrSet(resourceName, "endpoint_url"),
+					resource.TestCheckResourceAttr(resourceName, "event_bus.#", "2"),
+					resource.TestCheckResourceAttrPair(resourceName, "event_bus.0.event_bus_arn", "aws_cloudwatch_event_bus.primary", "arn"),
+					resource.TestCheckResourceAttrPair(resourceName, "event_bus.1.event_bus_arn", "aws_cloudwatch_event_bus.secondary", "arn"),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "replication_config.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "replication_config.0.state", "DISABLED"),
+					resource.TestCheckResourceAttr(resourceName, "role_arn", ""),
+					resource.TestCheckResourceAttr(resourceName, "routing_config.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "routing_config.0.failover_config.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "routing_config.0.failover_config.0.primary.#", "1"),
+					resource.TestCheckResourceAttrPair(resourceName, "routing_config.0.failover_config.0.primary.0.health_check", "aws_route53_health_check.test", "arn"),
+					resource.TestCheckResourceAttr(resourceName, "routing_config.0.failover_config.0.secondary.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "routing_config.0.failover_config.0.secondary.0.route", acctest.AlternateRegion()),
 				),
 			},
 			{
-				Config: testAccEndpointConfig_updateAttributes(endpointName),
+				Config: testAccEndpointConfig_updateRoutingConfig(rName),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckEndpointExists(ctx, resourceName, &v1),
-					resource.TestCheckResourceAttr(resourceName, "description", "some description"),
+					testAccCheckEndpointExists(ctx, resourceName, &v),
+					acctest.CheckResourceAttrRegionalARN(resourceName, "arn", "events", fmt.Sprintf("endpoint/%s", rName)),
+					resource.TestCheckResourceAttr(resourceName, "description", ""),
+					resource.TestCheckResourceAttrSet(resourceName, "endpoint_url"),
+					resource.TestCheckResourceAttr(resourceName, "event_bus.#", "2"),
+					resource.TestCheckResourceAttrPair(resourceName, "event_bus.0.event_bus_arn", "aws_cloudwatch_event_bus.primary", "arn"),
+					resource.TestCheckResourceAttrPair(resourceName, "event_bus.1.event_bus_arn", "aws_cloudwatch_event_bus.secondary", "arn"),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "replication_config.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "replication_config.0.state", "DISABLED"),
+					resource.TestCheckResourceAttr(resourceName, "role_arn", ""),
+					resource.TestCheckResourceAttr(resourceName, "routing_config.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "routing_config.0.failover_config.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "routing_config.0.failover_config.0.primary.#", "1"),
+					resource.TestCheckResourceAttrPair(resourceName, "routing_config.0.failover_config.0.primary.0.health_check", "aws_route53_health_check.test2", "arn"),
+					resource.TestCheckResourceAttr(resourceName, "routing_config.0.failover_config.0.secondary.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "routing_config.0.failover_config.0.secondary.0.route", acctest.AlternateRegion()),
 				),
 			},
 		},
@@ -444,103 +476,45 @@ resource "aws_cloudwatch_event_endpoint" "test" {
 `, rName, description, acctest.AlternateRegion()))
 }
 
-func testAccEndpointConfig_updateAttributes(name string) string {
-	return fmt.Sprintf(`
-provider "aws" {
-  alias  = "us-east-2"
-  region = "us-east-2"
-}
-
-resource "aws_cloudwatch_event_bus" "test" {
-  name = %[1]q
-}
-
-resource "aws_cloudwatch_event_bus" "test_secondary" {
-  name     = %[1]q
-  provider = aws.us-east-2
-}
-
-data "aws_iam_policy_document" "test_assume" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-    type        = "Service"
-    identifiers = ["events.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role" "test" {
-  name               = %[1]q
-  assume_role_policy = data.aws_iam_policy_document.test_assume.json
-
-  inline_policy {
-    name   = %[1]q
-    policy = data.aws_iam_policy_document.test.json
-  }
-}
-
-data "aws_iam_policy_document" "test" {
-  statement {
-    actions = [
-      "events:PutRule",
-      "events:PutTargets",
-      "events:DeleteRule",
-      "events:RemoveTargets",
-    ]
-
-    resources = ["arn:aws:events:*:*:rule/%[1]s/GlobalEndpointManagedRule-*"]
-  }
-
-  statement {
-    actions = ["events:PutEvents"]
-
-    resources = [
-      aws_cloudwatch_event_bus.test.arn,
-      aws_cloudwatch_event_bus.test_secondary.arn,
-    ]
-  }
-
-  statement {
-    actions = ["iam:PassRole"]
-
-    resources = ["arn:aws:iam::*:role/%[1]s"]
-    condition {
-      test     = "StringLike"
-      variable = "iam:PassedToService"
-
-      values = ["events.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_route53_health_check" "test" {
+func testAccEndpointConfig_updateRoutingConfig(rName string) string {
+	return acctest.ConfigCompose(testAccEndpointConfig_base(rName), fmt.Sprintf(`
+resource "aws_route53_health_check" "test2" {
   fqdn             = "example.com"
-  type             = "HTTP"
-  request_interval = "30"
+  type             = "HTTPS"
+  request_interval = "10"
   disabled         = true
+  port             = 443
+
+  tags = {
+    Name = %[1]q
+  }
 }
 
 resource "aws_cloudwatch_event_endpoint" "test" {
-  name        = %[1]q
-  role_arn    = aws_iam_role.test.arn
-  event_buses = [aws_cloudwatch_event_bus.test.arn, aws_cloudwatch_event_bus.test_secondary.arn]
+  name = %[1]q
+
+  event_bus {
+    event_bus_arn = aws_cloudwatch_event_bus.primary.arn
+  }
+  event_bus {
+    event_bus_arn = aws_cloudwatch_event_bus.secondary.arn
+  }
 
   replication_config {
-    is_enabled = false
+    state = "DISABLED"
   }
 
   routing_config {
     failover_config {
       primary {
-        health_check_arn = aws_route53_health_check.test.arn
+        health_check= aws_route53_health_check.test2.arn
       }
+
       secondary {
-        route = "us-east-2"
+        route = %[2]q
       }
     }
   }
 }
-`, name)
+`, rName, acctest.AlternateRegion()))
 }
