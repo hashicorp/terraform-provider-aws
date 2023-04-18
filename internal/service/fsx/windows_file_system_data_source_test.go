69b93f4efd597e550933dbacd2eb25f81f4a7acb
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-04-18 14:20:03 -0400

    d/aws_fsx_windows_file_system: Tidy up acceptance test configurations.

--- internal/service/fsx/windows_file_system_data_source_test.go
+++ internal/service/fsx/windows_file_system_data_source_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/aws/aws-sdk-go/service/fsx"
+	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 )
@@ -16,6 +17,8 @@ func TestAccFSxWindowsFileSystemDataSource_basic(t *testing.T) {
 	ctx := acctest.Context(t)
 	resourceName := "aws_fsx_windows_file_system.test"
 	datasourceName := "data.aws_fsx_windows_file_system.test"
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	domainName := acctest.RandomDomainName()
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
@@ -24,7 +27,7 @@ func TestAccFSxWindowsFileSystemDataSource_basic(t *testing.T) {
 		CheckDestroy:             testAccCheckWindowsFileSystemDestroy(ctx),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccWindowsFileSystemDataSourceConfig_subnetIDs1(),
+				Config: testAccWindowsFileSystemDataSourceConfig_basic(rName, domainName),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrPair(datasourceName, "active_directory_id", resourceName, "active_directory_id"),
 					resource.TestCheckResourceAttrPair(datasourceName, "arn", resourceName, "arn"),
@@ -37,7 +40,7 @@ func TestAccFSxWindowsFileSystemDataSource_basic(t *testing.T) {
 					resource.TestCheckResourceAttrPair(datasourceName, "owner_id", resourceName, "owner_id"),
 					resource.TestCheckResourceAttrPair(datasourceName, "storage_capacity", resourceName, "storage_capacity"),
 					resource.TestCheckResourceAttrPair(datasourceName, "storage_type", resourceName, "storage_type"),
-					resource.TestCheckResourceAttrPair(datasourceName, "subnet_ids", resourceName, "subnet_ids"),
+					resource.TestCheckResourceAttrPair(datasourceName, "subnet_ids.#", resourceName, "subnet_ids.#"),
 					resource.TestCheckResourceAttrPair(datasourceName, "throughput_capacity", resourceName, "throughput_capacity"),
 					resource.TestCheckResourceAttrPair(datasourceName, "vpc_id", resourceName, "vpc_id"),
 				),
@@ -46,59 +49,10 @@ func TestAccFSxWindowsFileSystemDataSource_basic(t *testing.T) {
 	})
 }
 
-func testAccWindowsFileSystemDataSourceBaseConfig() string {
-	return `
-data "aws_availability_zones" "available" {
-  state = "available"
-
-  filter {
-    name   = "opt-in-status"
-    values = ["opt-in-not-required"]
-  }
-}
-
-resource "aws_vpc" "test" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_subnet" "test1" {
-  vpc_id            = aws_vpc.test.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = data.aws_availability_zones.available.names[0]
-}
-
-resource "aws_subnet" "test2" {
-  vpc_id            = aws_vpc.test.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = data.aws_availability_zones.available.names[1]
-}
-
-resource "aws_directory_service_directory" "test" {
-  edition  = "Standard"
-  name     = "corp.notexample.com"
-  password = "SuperSecretPassw0rd"
-  type     = "MicrosoftAD"
-
-  vpc_settings {
-    subnet_ids = [aws_subnet.test1.id, aws_subnet.test2.id]
-    vpc_id     = aws_vpc.test.id
-  }
-}
-`
-}
-
-func testAccWindowsFileSystemDataSourceConfig_subnetIDs1() string {
-	return testAccWindowsFileSystemDataSourceBaseConfig() + `
-resource "aws_fsx_windows_file_system" "test" {
-  active_directory_id = aws_directory_service_directory.test.id
-  skip_final_backup   = true
-  storage_capacity    = 32
-  subnet_ids          = [aws_subnet.test1.id]
-  throughput_capacity = 8
-}
-
+func testAccWindowsFileSystemDataSourceConfig_basic(rName, domain string) string {
+	return acctest.ConfigCompose(testAccWindowsFileSystemConfig_subnetIDs1(rName, domain), `
 data "aws_fsx_windows_file_system" "test" {
   id = aws_fsx_windows_file_system.test.id
 }
-`
+`)
 }
