bddc8797e1c703cde39bba9a06f55d7cdccb3ead
Author Name <jer.chauvet@gmail.com>
2023-04-18 14:03:35 +0200

    datasource: add aws_s3_bucket_server_side_encryption_configuration (#26054)

--- internal/service/s3/bucket_server_side_encryption_configuration_data_source_test.go
+++ internal/service/s3/bucket_server_side_encryption_configuration_data_source_test.go
@@ -90,7 +90,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "aes256" {
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"  
+      sse_algorithm = "AES256"
     }
   }
 }
