fa071112e5ae9e40ca0bf0c1332c145f4d3fc8d7
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-04-18 12:11:27 -0400

    d/aws_ecs_task_definition: Alphabetize attributes.

--- internal/service/ecs/task_definition_data_source.go
+++ internal/service/ecs/task_definition_data_source.go
@@ -2,7 +2,6 @@ package ecs
 
 import (
 	"context"
-	"log"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ecs"
@@ -31,6 +30,10 @@ func DataSourceTaskDefinition() *schema.Resource {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+			"execution_role_arn": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
 			"family": {
 				Type:     schema.TypeString,
 				Computed: true,
@@ -51,10 +54,6 @@ func DataSourceTaskDefinition() *schema.Resource {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
-			"execution_role_arn": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
 		},
 	}
 }
@@ -63,35 +62,27 @@ func dataSourceTaskDefinitionRead(ctx context.Context, d *schema.ResourceData, m
 	var diags diag.Diagnostics
 	conn := meta.(*conns.AWSClient).ECSConn()
 
-	params := &ecs.DescribeTaskDefinitionInput{
-		TaskDefinition: aws.String(d.Get("task_definition").(string)),
+	taskDefinitionName := d.Get("task_definition").(string)
+	input := &ecs.DescribeTaskDefinitionInput{
+		TaskDefinition: aws.String(taskDefinitionName),
 	}
-	log.Printf("[DEBUG] Reading ECS Task Definition: %s", params)
-	desc, err := conn.DescribeTaskDefinitionWithContext(ctx, params)
+
+	output, err := conn.DescribeTaskDefinitionWithContext(ctx, input)
 
 	if err != nil {
-		return sdkdiag.AppendErrorf(diags, "getting task definition %q: %s", d.Get("task_definition").(string), err)
+		return sdkdiag.AppendErrorf(diags, "reading ECS Task Definition (%s): %s", taskDefinitionName, err)
 	}
 
-	if desc == nil || desc.TaskDefinition == nil {
-		return sdkdiag.AppendErrorf(diags, "reading ECS Task Definition: empty response")
-	}
-
-	taskDefinition := desc.TaskDefinition
-
+	taskDefinition := output.TaskDefinition
 	d.SetId(aws.StringValue(taskDefinition.TaskDefinitionArn))
 	d.Set("arn", taskDefinition.TaskDefinitionArn)
 	d.Set("arn_without_revision", StripRevision(aws.StringValue(taskDefinition.TaskDefinitionArn)))
+	d.Set("execution_role_arn", taskDefinition.ExecutionRoleArn)
 	d.Set("family", taskDefinition.Family)
 	d.Set("network_mode", taskDefinition.NetworkMode)
 	d.Set("revision", taskDefinition.Revision)
 	d.Set("status", taskDefinition.Status)
 	d.Set("task_role_arn", taskDefinition.TaskRoleArn)
-	d.Set("execution_role_arn", taskDefinition.ExecutionRoleArn)
-
-	if d.Id() == "" {
-		return sdkdiag.AppendErrorf(diags, "task definition %q not found", d.Get("task_definition").(string))
-	}
 
 	return diags
 }
