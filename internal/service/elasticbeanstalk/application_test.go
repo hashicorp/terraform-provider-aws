d2cc7ac1e192ca038fc13421e66f7a39874ab5ef
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-04-19 14:12:33 -0400

    Fix terrafmt error.

--- internal/service/elasticbeanstalk/application_test.go
+++ internal/service/elasticbeanstalk/application_test.go
@@ -377,7 +377,7 @@ resource "aws_elastic_beanstalk_application" "test" {
 func testAccApplicationConfig_maxCount(rName string) string {
 	return acctest.ConfigCompose(testAccApplicationConfig_baseServiceRole(rName), fmt.Sprintf(`
 resource "aws_elastic_beanstalk_application" "test" {
-  name  = %[1]q
+  name = %[1]q
 
   appversion_lifecycle {
     service_role          = aws_iam_role.test.arn
