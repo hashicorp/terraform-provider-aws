bc41a381633546e30458241baef1247251f29853
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-04-21 14:49:26 -0400

    Fix semgrep 'ci.bare-error-returns'.

--- internal/service/ssmcontacts/helper.go
+++ internal/service/ssmcontacts/helper.go
@@ -1,23 +1,18 @@
 package ssmcontacts
 
 import (
+	"fmt"
+
 	"github.com/aws/aws-sdk-go-v2/service/ssmcontacts"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 func setContactResourceData(d *schema.ResourceData, getContactOutput *ssmcontacts.GetContactOutput) error {
-	if err := d.Set("arn", getContactOutput.ContactArn); err != nil {
-		return err
-	}
-	if err := d.Set("alias", getContactOutput.Alias); err != nil {
-		return err
-	}
-	if err := d.Set("type", getContactOutput.Type); err != nil {
-		return err
-	}
-	if err := d.Set("display_name", getContactOutput.DisplayName); err != nil {
-		return err
-	}
+	d.Set("arn", getContactOutput.ContactArn)
+	d.Set("alias", getContactOutput.Alias)
+	d.Set("type", getContactOutput.Type)
+	d.Set("display_name", getContactOutput.DisplayName)
+
 	return nil
 }
 
@@ -25,20 +20,20 @@ func setContactChannelResourceData(d *schema.ResourceData, out *ssmcontacts.GetC
 	d.Set("activation_status", out.ActivationStatus)
 	d.Set("arn", out.ContactChannelArn)
 	d.Set("contact_id", out.ContactArn)
+	if err := d.Set("delivery_address", flattenContactChannelAddress(out.DeliveryAddress)); err != nil {
+		return fmt.Errorf("setting delivery_address: %w", err)
+	}
 	d.Set("name", out.Name)
 	d.Set("type", out.Type)
-	if err := d.Set("delivery_address", flattenContactChannelAddress(out.DeliveryAddress)); err != nil {
-		return err
-	}
+
 	return nil
 }
 
 func setPlanResourceData(d *schema.ResourceData, getContactOutput *ssmcontacts.GetContactOutput) error {
-	if err := d.Set("contact_id", getContactOutput.ContactArn); err != nil {
-		return err
-	}
+	d.Set("contact_id", getContactOutput.ContactArn)
 	if err := d.Set("stage", flattenStages(getContactOutput.Plan.Stages)); err != nil {
-		return err
+		return fmt.Errorf("setting stage: %w", err)
 	}
+
 	return nil
 }
