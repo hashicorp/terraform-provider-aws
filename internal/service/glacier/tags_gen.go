e5c9b500813baa14253c4f26b51fbb8ba12e406f
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-04-21 13:39:35 -0400

    Glacier: Generate 'createTags'.

--- internal/service/glacier/tags_gen.go
+++ internal/service/glacier/tags_gen.go
@@ -78,6 +78,15 @@ func SetTagsOut(ctx context.Context, tags map[string]*string) {
 	}
 }
 
+// createTags creates glacier service tags for new resources.
+func createTags(ctx context.Context, conn glacieriface.GlacierAPI, identifier string, tags map[string]*string) error {
+	if len(tags) == 0 {
+		return nil
+	}
+
+	return UpdateTags(ctx, conn, identifier, nil, tags)
+}
+
 // UpdateTags updates glacier service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
@@ -116,15 +125,6 @@ func UpdateTags(ctx context.Context, conn glacieriface.GlacierAPI, identifier st
 	}
 
 	return nil
-}
-
-// createTags creates glacier service tags for new resources.
-func createTags(ctx context.Context, conn glacieriface.GlacierAPI, identifier string, tags []*glacier.Tag) error {
-	if len(tags) == 0 {
-		return nil
-	}
-
-	return UpdateTags(ctx, conn, identifier, nil, KeyValueTags(ctx, tags))
 }
 
 // UpdateTags updates glacier service tags.
