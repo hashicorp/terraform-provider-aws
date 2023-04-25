212cfd657a9d674ff7422b35693f1ba39a8c8349
Graham Davison <gdavison@hashicorp.com>
2023-04-25 15:10:40 -0700

    Corrects `DocDB` to `DocumentDB` in error and log messages

--- internal/service/docdb/validate_test.go
+++ internal/service/docdb/validate_test.go
@@ -17,7 +17,7 @@ func TestValidIdentifier(t *testing.T) {
 	for _, v := range validNames {
 		_, errors := validIdentifier(v, "name")
 		if len(errors) != 0 {
-			t.Fatalf("%q should be a valid DocDB Identifier: %q", v, errors)
+			t.Fatalf("%q should be a valid DocumentDB Identifier: %q", v, errors)
 		}
 	}
 
@@ -35,7 +35,7 @@ func TestValidIdentifier(t *testing.T) {
 	for _, v := range invalidNames {
 		_, errors := validIdentifier(v, "name")
 		if len(errors) == 0 {
-			t.Fatalf("%q should be an invalid DocDB Identifier", v)
+			t.Fatalf("%q should be an invalid DocumentDB Identifier", v)
 		}
 	}
 }
