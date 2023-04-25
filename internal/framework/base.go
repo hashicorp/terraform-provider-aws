b9d7c2395ff8ee28cc1b1222f9b56f7dfcf95178
Adrian Johnson <adrian.johnson@hashicorp.com>
2023-04-25 13:59:42 -0500

    tags: duplicate and computed tags on framework

--- internal/framework/base.go
+++ internal/framework/base.go
@@ -90,17 +90,15 @@ func (r *ResourceWithConfigure) SetTagsAll(ctx context.Context, request resource
 	}
 
 	if !planTags.IsUnknown() {
-		resourceTags := tftags.New(ctx, planTags)
+		if !HasUnknownElements(planTags) {
+			resourceTags := tftags.New(ctx, planTags)
 
-		if defaultTagsConfig.TagsEqual(resourceTags) {
-			response.Diagnostics.AddError(
-				`"tags" are identical to those in the "default_tags" configuration block of the provider`,
-				"please de-duplicate and try again")
+			allTags := defaultTagsConfig.MergeTags(resourceTags).IgnoreConfig(ignoreTagsConfig)
+
+			response.Diagnostics.Append(response.Plan.SetAttribute(ctx, path.Root("tags_all"), flex.FlattenFrameworkStringValueMapLegacy(ctx, allTags.Map()))...)
+		} else {
+			response.Diagnostics.Append(response.Plan.SetAttribute(ctx, path.Root("tags_all"), tftags.Unknown)...)
 		}
-
-		allTags := defaultTagsConfig.MergeTags(resourceTags).IgnoreConfig(ignoreTagsConfig)
-
-		response.Diagnostics.Append(response.Plan.SetAttribute(ctx, path.Root("tags_all"), flex.FlattenFrameworkStringValueMapLegacy(ctx, allTags.Map()))...)
 	} else {
 		response.Diagnostics.Append(response.Plan.SetAttribute(ctx, path.Root("tags_all"), tftags.Unknown)...)
 	}
@@ -209,4 +207,14 @@ func (w *WithTimeouts) DeleteTimeout(ctx context.Context, timeouts timeouts.Valu
 	}
 
 	return timeout
+}
+
+func HasUnknownElements(m types.Map) bool {
+	for _, v := range m.Elements() {
+		if v.IsUnknown() {
+			return true
+		}
+	}
+
+	return false
 }
