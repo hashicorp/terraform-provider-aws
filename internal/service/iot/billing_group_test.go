f882f8b52414f903a6fe3a22f0458f527b66f5db
Marko Jankovic <marko.jm@gmail.com>
2023-04-28 00:18:32 +0200

    Initial version

---
+++ internal/service/iot/billing_group_test.go
@@ -0,0 +1,269 @@
+package iot_test
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/service/iot"
+	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	tfiot "github.com/hashicorp/terraform-provider-aws/internal/service/iot"
+	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
+)
+
+func TestAccIoTBillingGroup_basic(t *testing.T) {
+	ctx := acctest.Context(t)
+	var BillingGroup iot.DescribeBillingGroupOutput
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	resourceName := "aws_iot_billing_group.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
+		ErrorCheck:               acctest.ErrorCheck(t, iot.EndpointsID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckBillingGroupDestroy(ctx),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccBillingGroupConfig_basic(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckBillingGroupExists(ctx, resourceName, &BillingGroup),
+					acctest.MatchResourceAttrRegionalARN(resourceName, "arn", "iot", regexp.MustCompile(fmt.Sprintf("BillingGroup/%s$", rName))),
+					resource.TestCheckResourceAttr(resourceName, "metadata.#", "1"),
+					resource.TestCheckResourceAttrSet(resourceName, "metadata.0.creation_date"),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "parent_group_name", ""),
+					resource.TestCheckResourceAttr(resourceName, "properties.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", "0"),
+					resource.TestCheckResourceAttr(resourceName, "version", "1"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccIoTBillingGroup_disappears(t *testing.T) {
+	ctx := acctest.Context(t)
+	var BillingGroup iot.DescribeBillingGroupOutput
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	resourceName := "aws_iot_billing_group.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
+		ErrorCheck:               acctest.ErrorCheck(t, iot.EndpointsID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckBillingGroupDestroy(ctx),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccBillingGroupConfig_basic(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckBillingGroupExists(ctx, resourceName, &BillingGroup),
+					acctest.CheckResourceDisappears(ctx, acctest.Provider, tfiot.ResourceBillingGroup(), resourceName),
+				),
+				ExpectNonEmptyPlan: true,
+			},
+		},
+	})
+}
+
+func TestAccIoTBillingGroup_tags(t *testing.T) {
+	ctx := acctest.Context(t)
+	var BillingGroup iot.DescribeBillingGroupOutput
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	resourceName := "aws_iot_billing_group.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
+		ErrorCheck:               acctest.ErrorCheck(t, iot.EndpointsID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckBillingGroupDestroy(ctx),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccBillingGroupConfig_tags1(rName, "key1", "value1"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckBillingGroupExists(ctx, resourceName, &BillingGroup),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", "1"),
+					resource.TestCheckResourceAttr(resourceName, "tags.key1", "value1"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccBillingGroupConfig_tags2(rName, "key1", "value1updated", "key2", "value2"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckBillingGroupExists(ctx, resourceName, &BillingGroup),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", "2"),
+					resource.TestCheckResourceAttr(resourceName, "tags.key1", "value1updated"),
+					resource.TestCheckResourceAttr(resourceName, "tags.key2", "value2"),
+				),
+			},
+			{
+				Config: testAccBillingGroupConfig_tags1(rName, "key2", "value2"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckBillingGroupExists(ctx, resourceName, &BillingGroup),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", "1"),
+					resource.TestCheckResourceAttr(resourceName, "tags.key2", "value2"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccIoTBillingGroup_properties(t *testing.T) {
+	ctx := acctest.Context(t)
+	var BillingGroup iot.DescribeBillingGroupOutput
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	resourceName := "aws_iot_billing_group.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
+		ErrorCheck:               acctest.ErrorCheck(t, iot.EndpointsID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckBillingGroupDestroy(ctx),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccBillingGroupConfig_properties(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckBillingGroupExists(ctx, resourceName, &BillingGroup),
+					resource.TestCheckResourceAttr(resourceName, "properties.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "properties.0.description", "test description 1"),
+					resource.TestCheckResourceAttr(resourceName, "version", "1"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccBillingGroupConfig_propertiesUpdated(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckBillingGroupExists(ctx, resourceName, &BillingGroup),
+					resource.TestCheckResourceAttr(resourceName, "properties.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "properties.0.description", "test description 2"),
+					resource.TestCheckResourceAttr(resourceName, "version", "2"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckBillingGroupExists(ctx context.Context, n string, v *iot.DescribeBillingGroupOutput) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No IoT Billing Group ID is set")
+		}
+
+		conn := acctest.Provider.Meta().(*conns.AWSClient).IoTConn()
+
+		output, err := tfiot.FindBillingGroupByName(ctx, conn, rs.Primary.ID)
+
+		if err != nil {
+			return err
+		}
+
+		*v = *output
+
+		return nil
+	}
+}
+
+func testAccCheckBillingGroupDestroy(ctx context.Context) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		conn := acctest.Provider.Meta().(*conns.AWSClient).IoTConn()
+
+		for _, rs := range s.RootModule().Resources {
+			if rs.Type != "aws_iot_billing_group" {
+				continue
+			}
+
+			_, err := tfiot.FindBillingGroupByName(ctx, conn, rs.Primary.ID)
+
+			if tfresource.NotFound(err) {
+				continue
+			}
+
+			if err != nil {
+				return err
+			}
+
+			return fmt.Errorf("IoT Billing Group %s still exists", rs.Primary.ID)
+		}
+
+		return nil
+	}
+}
+
+func testAccBillingGroupConfig_basic(rName string) string {
+	return fmt.Sprintf(`
+resource "aws_iot_billing_group" "test" {
+  name = %[1]q
+}
+`, rName)
+}
+
+func testAccBillingGroupConfig_tags1(rName, tagKey1, tagValue1 string) string {
+	return fmt.Sprintf(`
+resource "aws_iot_billing_group" "test" {
+  name = %[1]q
+
+  tags = {
+    %[2]q = %[3]q
+  }
+}
+`, rName, tagKey1, tagValue1)
+}
+
+func testAccBillingGroupConfig_tags2(rName, tagKey1, tagValue1, tagKey2, tagValue2 string) string {
+	return fmt.Sprintf(`
+resource "aws_iot_billing_group" "test" {
+  name = %[1]q
+
+  tags = {
+    %[2]q = %[3]q
+    %[4]q = %[5]q
+  }
+}
+`, rName, tagKey1, tagValue1, tagKey2, tagValue2)
+}
+
+func testAccBillingGroupConfig_properties(rName string) string {
+	return fmt.Sprintf(`
+resource "aws_iot_billing_group" "test" {
+  name = %[1]q
+
+  properties {
+    description = "test description 1"
+  }
+}
+`, rName)
+}
+
+func testAccBillingGroupConfig_propertiesUpdated(rName string) string {
+	return fmt.Sprintf(`
+resource "aws_iot_billing_group" "test" {
+  name = %[1]q
+
+  properties {
+    description = "test description 2"
+  }
+}
+`, rName)
+}
---
+++ website/docs/r/iot_billing_group.html.markdown
@@ -0,0 +1,53 @@
+---
+subcategory: "IoT Core"
+layout: "aws"
+page_title: "AWS: aws_iot_billing_group"
+description: |-
+  Terraform resource for managing an AWS IoT Core Billing Group.
+---
+
+# Resource: aws_iot_billing_group
+
+Terraform resource for managing an AWS IoT Core Billing Group.
+
+## Example Usage
+
+### Basic Usage
+
+```terraform
+resource "aws_iot_billing_group" "example" {
+}
+```
+
+## Argument Reference
+
+The following arguments are required:
+
+* `example_arg` - (Required) Concise argument description. Do not begin the description with "An", "The", "Defines", "Indicates", or "Specifies," as these are verbose. In other words, "Indicates the amount of storage," can be rewritten as "Amount of storage," without losing any information.
+
+The following arguments are optional:
+
+* `optional_arg` - (Optional) Concise argument description. Do not begin the description with "An", "The", "Defines", "Indicates", or "Specifies," as these are verbose. In other words, "Indicates the amount of storage," can be rewritten as "Amount of storage," without losing any information.
+
+## Attributes Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `arn` - ARN of the Billing Group. Do not begin the description with "An", "The", "Defines", "Indicates", or "Specifies," as these are verbose. In other words, "Indicates the amount of storage," can be rewritten as "Amount of storage," without losing any information.
+* `example_attribute` - Concise description. Do not begin the description with "An", "The", "Defines", "Indicates", or "Specifies," as these are verbose. In other words, "Indicates the amount of storage," can be rewritten as "Amount of storage," without losing any information.
+
+## Timeouts
+
+[Configuration options](https://developer.hashicorp.com/terraform/language/resources/syntax#operation-timeouts):
+
+* `create` - (Default `60m`)
+* `update` - (Default `180m`)
+* `delete` - (Default `90m`)
+
+## Import
+
+IoT Core Billing Group can be imported using the `example_id_arg`, e.g.,
+
+```
+$ terraform import aws_iot_billing_group.example rft-8012925589
+```
