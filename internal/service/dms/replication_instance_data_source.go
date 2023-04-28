0ae55a537aafc8e34d8ccf05c17f7e071fa7d1fe
Sharon Nam <sharon.nam@sharon.nam-PJCJ4F7JH7>
2023-04-28 09:24:03 -0700

    further work to add datasource

--- internal/service/dms/replication_instance_data_source.go
+++ internal/service/dms/replication_instance_data_source.go
@@ -1,92 +1,165 @@
-package aws
+package dms
 
 import (
-	"fmt"
-	"log"
-	"time"
+	"context"
 
 	"github.com/aws/aws-sdk-go/aws"
 	dms "github.com/aws/aws-sdk-go/service/databasemigrationservice"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/create"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
+	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
+	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
+// @SDKDataSource("aws_dms_replication_instance")
 func DataSourceReplicationInstance() *schema.Resource {
 	return &schema.Resource{
-		Read: dataSourceReplicationInstanceRead,
+		ReadWithoutTimeout: dataSourceReplicationInstanceRead,
 
 		Schema: map[string]*schema.Schema{
+			"allocated_storage": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"allow_major_version_upgrade": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+			"apply_immediately": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+			"auto_minor_version_upgrade": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+			"availability_zone": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"engine_version": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"kms_key_arn": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"multi_az": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+			"preferred_maintenance_window": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"publicly_accessible": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
 			"replication_instance_arn": {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
-
+			"replication_instance_class": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"replication_instance_id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
 			"replication_instance_private_ips": {
 				Type:     schema.TypeList,
-				Computed: true,
 				Elem:     &schema.Schema{Type: schema.TypeString},
+				Computed: true,
 			},
-
 			"replication_instance_public_ips": {
 				Type:     schema.TypeList,
-				Computed: true,
 				Elem:     &schema.Schema{Type: schema.TypeString},
+				Computed: true,
 			},
+			"replication_subnet_group_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"vpc_security_group_ids": {
+				Type:     schema.TypeSet,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Set:      schema.HashString,
+				Computed: true,
+			},
+			"tags": tftags.TagsSchemaComputed(),
 		},
 	}
 }
 
-func dataSourceReplicationInstanceRead(d *schema.ResourceData, meta interface{}) error {
-	client := meta.(*AWSClient).dmsconn
+const (
+	DSNameReplicationInstance = "Replication Instance Data Source"
+)
 
-	filters, filtersOk := d.GetOk("filter")
+func dataSourceReplicationInstanceRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn := meta.(*conns.AWSClient).DMSConn()
+	defaultTagsConfig := meta.(*conns.AWSClient).DefaultTagsConfig
+	ignoreTagsConfig := meta.(*conns.AWSClient).IgnoreTagsConfig
 
-	// Build up search parameters
-	params := &dms.DescribeReplicationInstancesInput{}
-	if filtersOk {
-		params.Filters = buildDataSourceFilters(filters.(*schema.Set))
+	rID := d.Get("replication_instance_id").(string)
+
+	instance, err := FindReplicationInstanceByID(ctx, conn, rID)
+	if err != nil {
+		return create.DiagError(names.DMS, create.ErrActionReading, DSNameReplicationTask, d.Id(), err)
 	}
 
-	log.Printf("[DEBUG] Reading Replication Instances")
-	res, err := client.DescribeReplicationInstances(params)
+	d.SetId(aws.StringValue(instance.ReplicationInstanceIdentifier))
+
+	d.Set("allocated_storage", instance.AllocatedStorage)
+	d.Set("auto_minor_version_upgrade", instance.AutoMinorVersionUpgrade)
+	d.Set("availability_zone", instance.AvailabilityZone)
+	d.Set("engine_version", instance.EngineVersion)
+	d.Set("kms_key_arn", instance.KmsKeyId)
+	d.Set("multi_az", instance.MultiAZ)
+	d.Set("preferred_maintenance_window", instance.PreferredMaintenanceWindow)
+	d.Set("publicly_accessible", instance.PubliclyAccessible)
+	d.Set("replication_instance_arn", instance.ReplicationInstanceArn)
+	d.Set("replication_instance_class", instance.ReplicationInstanceClass)
+	d.Set("replication_instance_id", instance.ReplicationInstanceIdentifier)
+
+	tags, err := ListTags(ctx, conn, aws.StringValue(instance.ReplicationInstanceArn))
 
 	if err != nil {
-		return fmt.Errorf("Error getting Replication Instances: %v", err)
+		return create.DiagError(names.DMS, create.ErrActionReading, DSNameReplicationInstance, d.Id(), err)
 	}
 
-	var instance *dms.ReplicationInstance
-	if len(res.ReplicationInstances) < 1 {
-		return nil
+	tags = tags.IgnoreAWS().IgnoreConfig(ignoreTagsConfig)
+
+	//lintignore:AWSR002
+	if err := d.Set("tags", tags.RemoveDefaultConfig(defaultTagsConfig).Map()); err != nil {
+		return create.DiagError(names.DMS, create.ErrActionSetting, DSNameReplicationInstance, d.Id(), err)
 	}
-
-	if len(res.ReplicationInstances) > 1 {
-		return fmt.Errorf("Your query returned more than one result. Please try a more " +
-			"specific search criteria.")
-	} else {
-		instance = res.ReplicationInstances[0]
-	}
-
-	log.Printf("[DEBUG] Received Replication Instances: %s", res)
-
-	d.SetId(time.Now().UTC().String())
-	d.Set("replication_instance_arn", instance.ReplicationInstanceArn)
-	d.Set("replication_instance_private_ips", instance.ReplicationInstancePrivateIpAddresses)
-	d.Set("replication_instance_public_ips", instance.ReplicationInstancePublicIpAddresses)
 
 	return nil
 }
 
-func buildDataSourceFilters(set *schema.Set) []*dms.Filter {
-	var dms_filters []*dms.Filter
-	for _, v := range set.List() {
-		m := v.(map[string]interface{})
-		var filterValues []*string
-		for _, e := range m["values"].([]interface{}) {
-			filterValues = append(filterValues, aws.String(e.(string)))
-		}
-		dms_filters = append(dms_filters, &dms.Filter{
-			Name:   aws.String(m["name"].(string)),
-			Values: filterValues,
-		})
+func FindReplicationInstanceByID(ctx context.Context, conn *dms.DatabaseMigrationService, id string) (*dms.ReplicationInstance, error) {
+	input := &dms.DescribeReplicationInstancesInput{
+		Filters: []*dms.Filter{
+			{
+				Name:   aws.String("replication-instance-id"),
+				Values: []*string{aws.String(id)},
+			},
+		},
 	}
-	return dms_filters
+	response, err := conn.DescribeReplicationInstancesWithContext(ctx, input)
+	if err != nil {
+		return nil, err
+	}
+
+	if response == nil || len(response.ReplicationInstances) == 0 || response.ReplicationInstances[0] == nil {
+		return nil, tfresource.NewEmptyResultError(input)
+	}
+
+	return response.ReplicationInstances[0], nil
 }
--- internal/service/dms/replication_instance_data_source_test.go
+++ internal/service/dms/replication_instance_data_source_test.go
@@ -1,49 +1,49 @@
-package aws
+package dms_test
 
 import (
+	"fmt"
 	"testing"
 
+	dms "github.com/aws/aws-sdk-go/service/databasemigrationservice"
+	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 )
 
 func TestAccDMSReplicationInstanceDataSource_basic(t *testing.T) {
+	ctx := acctest.Context(t)
+	replicationInstanceClass := "dms.c4.large"
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
 	resourceName := "aws_dms_replication_instance.test"
-	datasourceName := "data.aws_dms_replication_instance.test"
+	dataSourceName := "data.aws_dms_replication_instance.test"
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
-		Providers: testAccProviders,
+		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
+		ErrorCheck:               acctest.ErrorCheck(t, dms.EndpointsID),
+		ProtoV5ProviderFactories: acctest.ProtoV5FactoriesAlternate(ctx, t),
+		CheckDestroy:             testAccCheckReplicationInstanceDestroy(ctx),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccReplicationInstanceDataSourceConfig,
+				Config: testAccReplicationInstanceDataSourceConfig_basic(rName, replicationInstanceClass),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrPair(datasourceName, "replication_instance_arn", resourceName, "replication_instance_arn"),
-					resource.TestCheckResourceAttrPair(datasourceName, "replication_instance_private_ips", resourceName, "replication_instance_private_ips"),
-					resource.TestCheckResourceAttrPair(datasourceName, "replication_instance_public_ips", resourceName, "replication_instance_public_ips"),
+					testAccCheckReplicationInstanceExists(ctx, dataSourceName),
+					resource.TestCheckResourceAttrPair(dataSourceName, "replication_instance_id", resourceName, "replication_instance_id"),
 				),
 			},
 		},
 	})
 }
 
-var testAccReplicationInstanceDataSourceConfig = `
+func testAccReplicationInstanceDataSourceConfig_basic(rName, replicationInstanceClass string) string {
+	return fmt.Sprintf(`
 resource "aws_dms_replication_instance" "test" {
-  allocated_storage            = 50
-  apply_immediately            = true
-  auto_minor_version_upgrade   = true
-  availability_zone            = "us-east-1c"
-  engine_version               = "3.4.0"
-  multi_az                     = false
-  preferred_maintenance_window = "sun:10:30-sun:14:30"
-  replication_instance_class   = "dms.t2.medium"
-  replication_instance_id      = "instance-id"
-  replication_subnet_group_id  = "subnet-group-id"
+  apply_immediately          = true
+  replication_instance_class = %q
+  replication_instance_id    = %q
 }
 
 data "aws_dms_replication_instance" "test" {
-  filter {
-    name   = "replication-instance-id"
-    values = [aws_dms_replication_instance.test.id]
-  }
+  replication_instance_id = aws_dms_replication_instance.test.replication_instance_id
 }
-`
+`, replicationInstanceClass, rName)
+}
