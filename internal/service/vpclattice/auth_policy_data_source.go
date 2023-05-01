4f4164fd26f732ebf2ee25bf1ae58817a88d4e4f
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-05-01 14:24:00 -0400

    Fix importlint error.

--- internal/service/vpclattice/auth_policy_data_source.go
+++ internal/service/vpclattice/auth_policy_data_source.go
@@ -2,6 +2,7 @@ package vpclattice
 
 import (
 	"context"
+
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
