eff94aef2b4ab7b750550f08397e4a694f9cea47
Graham Davison <gdavison@hashicorp.com>
2023-05-02 16:03:38 -0700

    Removes logging

--- .teamcity/scripts/sweeper.sh
+++ .teamcity/scripts/sweeper.sh
@@ -4,12 +4,6 @@ set -euo pipefail
 
 # shellcheck disable=2157 # This isn't a constant string, it's a TeamCity variable substitution
 if [[ -n "%ACCTEST_ROLE_ARN%" ]]; then
-    echo "assuming role %ACCTEST_ROLE_ARN% for sweeper"
-
-    echo "AWS_ACCESS_KEY_ID: $(echo "%AWS_ACCESS_KEY_ID%" | sed -E "s/^.+(.{4})/****\1/")"
-    echo "AWS_SECRET_ACCESS_KEY: $(echo "%AWS_SECRET_ACCESS_KEY%" | sed -E "s/^(.{4}).+(.{4})/\1****\2/")"
-    echo "ACCTEST_ROLE_ARN: %ACCTEST_ROLE_ARN%"
-
     conf=$(pwd)/aws.conf
 
     function cleanup {
@@ -35,9 +29,5 @@ EOF
     export AWS_CONFIG_FILE="${conf}"
     export AWS_PROFILE=sweeper
 fi
-
-echo "AWS_CONFIG_FILE: ${AWS_CONFIG_FILE}"
-echo "AWS_PROFILE: ${AWS_PROFILE}"
-echo "env AWS_ACCESS_KEY_ID: $(echo "${AWS_ACCESS_KEY_ID}" | sed -E "s/^(.{4}).+(.{4})/\1****\2/")"
 
 go test ./internal/sweep -v -tags=sweep -sweep="%SWEEPER_REGIONS%" -sweep-allow-failures -timeout=4h
