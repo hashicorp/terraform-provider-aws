a710a456015774bb84b5f28b9bdcb6712e71bd4b
Graham Davison <gdavison@hashicorp.com>
2023-05-02 15:27:24 -0700

    Formatting

--- .teamcity/scripts/sweeper.sh
+++ .teamcity/scripts/sweeper.sh
@@ -3,20 +3,19 @@
 set -euo pipefail
 
 # shellcheck disable=2157 # This isn't a constant string, it's a TeamCity variable substitution
-if [[ -n "%ACCTEST_ROLE_ARN%" ]]
-then
-echo "assuming role %ACCTEST_ROLE_ARN% for sweeper"
+if [[ -n "%ACCTEST_ROLE_ARN%" ]]; then
+    echo "assuming role %ACCTEST_ROLE_ARN% for sweeper"
 
-conf=$(pwd)/aws.conf
+    conf=$(pwd)/aws.conf
 
-function cleanup {
-  rm "${conf}"
-}
-trap cleanup EXIT
+    function cleanup {
+        rm "${conf}"
+    }
+    trap cleanup EXIT
 
-touch "${conf}"
-chmod 600 "${conf}"
-cat << EOF > "${conf}"
+    touch "${conf}"
+    chmod 600 "${conf}"
+    cat <<EOF >"${conf}"
 [profile sweeper]
 role_arn       = %ACCTEST_ROLE_ARN%
 source_profile = source
@@ -26,11 +25,11 @@ aws_access_key_id     = %AWS_ACCESS_KEY_ID%
 aws_secret_access_key = %AWS_SECRET_ACCESS_KEY%
 EOF
 
-unset AWS_ACCESS_KEY_ID
-unset AWS_SECRET_ACCESS_KEY
+    unset AWS_ACCESS_KEY_ID
+    unset AWS_SECRET_ACCESS_KEY
 
-export AWS_CONFIG_FILE="${conf}"
-export AWS_PROFILE=sweeper
+    export AWS_CONFIG_FILE="${conf}"
+    export AWS_PROFILE=sweeper
 fi
 
 echo "AWS_CONFIG_FILE: ${AWS_CONFIG_FILE}"
