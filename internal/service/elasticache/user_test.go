662020ea4c35fa4cbdf744753bc8e0ecf8820408
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-05-03 12:05:17 -0400

    Fix terrafmt error.

--- internal/service/elasticache/user_test.go
+++ internal/service/elasticache/user_test.go
@@ -359,12 +359,6 @@ resource "aws_elasticache_user" "test" {
   access_string = "on ~app::* -@all +@read +@hash +@bitmap +@geo -setbit -bitfield -hset -hsetnx -hmset -hincrby -hincrbyfloat -hdel -bitop -geoadd -georadius -georadiusbymember"
   engine        = "REDIS"
   passwords     = ["password123456789"]
-
-  timeouts {
-	create = "10m"
-	delete = "10m"
-	update = "10m"
-  }
 }
 `, rName)
 }
