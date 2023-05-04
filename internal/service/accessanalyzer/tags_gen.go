297ef15a9f4d637e253379696f0a1c945301b865
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-05-04 11:26:53 -0400

    accessanalyzer: Generate AWS SDK for Go v2 tagging code.

--- internal/service/accessanalyzer/tags_gen.go
+++ internal/service/accessanalyzer/tags_gen.go
@@ -5,9 +5,8 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/accessanalyzer"
-	"github.com/aws/aws-sdk-go/service/accessanalyzer/accessanalyzeriface"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/accessanalyzer"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/types"
@@ -17,12 +16,12 @@ import (
 // ListTags lists accessanalyzer service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func ListTags(ctx context.Context, conn accessanalyzeriface.AccessAnalyzerAPI, identifier string) (tftags.KeyValueTags, error) {
+func ListTags(ctx context.Context, conn *accessanalyzer.Client, identifier string) (tftags.KeyValueTags, error) {
 	input := &accessanalyzer.ListTagsForResourceInput{
 		ResourceArn: aws.String(identifier),
 	}
 
-	output, err := conn.ListTagsForResourceWithContext(ctx, input)
+	output, err := conn.ListTagsForResource(ctx, input)
 
 	if err != nil {
 		return tftags.New(ctx, nil), err
@@ -34,7 +33,7 @@ func ListTags(ctx context.Context, conn accessanalyzeriface.AccessAnalyzerAPI, i
 // ListTags lists accessanalyzer service tags and set them in Context.
 // It is called from outside this package.
 func (p *servicePackage) ListTags(ctx context.Context, meta any, identifier string) error {
-	tags, err := ListTags(ctx, meta.(*conns.AWSClient).AccessAnalyzerConn(), identifier)
+	tags, err := ListTags(ctx, meta.(*conns.AWSClient).AccessAnalyzerClient(), identifier)
 
 	if err != nil {
 		return err
@@ -47,21 +46,21 @@ func (p *servicePackage) ListTags(ctx context.Context, meta any, identifier stri
 	return nil
 }
 
-// map[string]*string handling
+// map[string]string handling
 
 // Tags returns accessanalyzer service tags.
-func Tags(tags tftags.KeyValueTags) map[string]*string {
-	return aws.StringMap(tags.Map())
+func Tags(tags tftags.KeyValueTags) map[string]string {
+	return tags.Map()
 }
 
 // KeyValueTags creates KeyValueTags from accessanalyzer service tags.
-func KeyValueTags(ctx context.Context, tags map[string]*string) tftags.KeyValueTags {
+func KeyValueTags(ctx context.Context, tags map[string]string) tftags.KeyValueTags {
 	return tftags.New(ctx, tags)
 }
 
 // GetTagsIn returns accessanalyzer service tags from Context.
 // nil is returned if there are no input tags.
-func GetTagsIn(ctx context.Context) map[string]*string {
+func GetTagsIn(ctx context.Context) map[string]string {
 	if inContext, ok := tftags.FromContext(ctx); ok {
 		if tags := Tags(inContext.TagsIn.UnwrapOrDefault()); len(tags) > 0 {
 			return tags
@@ -72,7 +71,7 @@ func GetTagsIn(ctx context.Context) map[string]*string {
 }
 
 // SetTagsOut sets accessanalyzer service tags in Context.
-func SetTagsOut(ctx context.Context, tags map[string]*string) {
+func SetTagsOut(ctx context.Context, tags map[string]string) {
 	if inContext, ok := tftags.FromContext(ctx); ok {
 		inContext.TagsOut = types.Some(KeyValueTags(ctx, tags))
 	}
@@ -81,7 +80,7 @@ func SetTagsOut(ctx context.Context, tags map[string]*string) {
 // UpdateTags updates accessanalyzer service tags.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func UpdateTags(ctx context.Context, conn accessanalyzeriface.AccessAnalyzerAPI, identifier string, oldTagsMap, newTagsMap any) error {
+func UpdateTags(ctx context.Context, conn *accessanalyzer.Client, identifier string, oldTagsMap, newTagsMap any) error {
 	oldTags := tftags.New(ctx, oldTagsMap)
 	newTags := tftags.New(ctx, newTagsMap)
 
@@ -90,10 +89,10 @@ func UpdateTags(ctx context.Context, conn accessanalyzeriface.AccessAnalyzerAPI,
 	if len(removedTags) > 0 {
 		input := &accessanalyzer.UntagResourceInput{
 			ResourceArn: aws.String(identifier),
-			TagKeys:     aws.StringSlice(removedTags.Keys()),
+			TagKeys:     removedTags.Keys(),
 		}
 
-		_, err := conn.UntagResourceWithContext(ctx, input)
+		_, err := conn.UntagResource(ctx, input)
 
 		if err != nil {
 			return fmt.Errorf("untagging resource (%s): %w", identifier, err)
@@ -108,7 +107,7 @@ func UpdateTags(ctx context.Context, conn accessanalyzeriface.AccessAnalyzerAPI,
 			Tags:        Tags(updatedTags),
 		}
 
-		_, err := conn.TagResourceWithContext(ctx, input)
+		_, err := conn.TagResource(ctx, input)
 
 		if err != nil {
 			return fmt.Errorf("tagging resource (%s): %w", identifier, err)
@@ -121,5 +120,5 @@ func UpdateTags(ctx context.Context, conn accessanalyzeriface.AccessAnalyzerAPI,
 // UpdateTags updates accessanalyzer service tags.
 // It is called from outside this package.
 func (p *servicePackage) UpdateTags(ctx context.Context, meta any, identifier string, oldTags, newTags any) error {
-	return UpdateTags(ctx, meta.(*conns.AWSClient).AccessAnalyzerConn(), identifier, oldTags, newTags)
+	return UpdateTags(ctx, meta.(*conns.AWSClient).AccessAnalyzerClient(), identifier, oldTags, newTags)
 }
