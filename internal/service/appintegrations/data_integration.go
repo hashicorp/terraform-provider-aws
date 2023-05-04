6cfe545ba4950ed176ddb3353394fc41bb781560
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-05-04 15:38:56 -0400

    Fix golangci-lint 'SA1019: resource.UniqueId is deprecated: Use helper/id package instead.'.

--- internal/service/appintegrations/data_integration.go
+++ internal/service/appintegrations/data_integration.go
@@ -9,7 +9,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/appintegrationsservice"
 	"github.com/hashicorp/aws-sdk-go-base/v2/awsv1shim/v2/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/id"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
@@ -108,7 +108,7 @@ func resourceDataIntegrationCreate(ctx context.Context, d *schema.ResourceData, 
 
 	name := d.Get("name").(string)
 	input := &appintegrationsservice.CreateDataIntegrationInput{
-		ClientToken:    aws.String(resource.UniqueId()),
+		ClientToken:    aws.String(id.UniqueId()),
 		KmsKey:         aws.String(d.Get("kms_key").(string)),
 		Name:           aws.String(name),
 		ScheduleConfig: expandScheduleConfig(d.Get("schedule_config").([]interface{})),
