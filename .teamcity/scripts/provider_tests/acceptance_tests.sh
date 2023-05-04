50743f9e092deb6d3b7c90de291c4d15d7f5bebc
Graham Davison <gdavison@hashicorp.com>
2023-05-04 11:39:01 -0700

    Assumes role in full acceptance test configurations

--- .teamcity/scripts/provider_tests/acceptance_tests.sh
+++ .teamcity/scripts/provider_tests/acceptance_tests.sh
@@ -2,6 +2,34 @@
 
 set -euo pipefail
 
+# shellcheck disable=2157 # This isn't a constant string, it's a TeamCity variable substitution
+if [[ -n "%ACCTEST_ROLE_ARN%" ]]; then
+    conf=$(pwd)/aws.conf
+
+    function cleanup {
+        rm "${conf}"
+    }
+    trap cleanup EXIT
+
+    touch "${conf}"
+    chmod 600 "${conf}"
+    cat <<EOF >"${conf}"
+[profile primary]
+role_arn       = %ACCTEST_ROLE_ARN%
+source_profile = source
+
+[profile source]
+aws_access_key_id     = %AWS_ACCESS_KEY_ID%
+aws_secret_access_key = %AWS_SECRET_ACCESS_KEY%
+EOF
+
+    unset AWS_ACCESS_KEY_ID
+    unset AWS_SECRET_ACCESS_KEY
+
+    export AWS_CONFIG_FILE="${conf}"
+    export AWS_PROFILE=primary
+fi
+
 # All of internal except for internal/service. This list should be generated.
 TF_ACC=1 go test \
     ./internal/acctest/... \
