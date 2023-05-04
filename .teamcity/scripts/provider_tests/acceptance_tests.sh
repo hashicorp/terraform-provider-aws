6790ae83eb11e7cb23b8585f7ed39d2dacd9a351
Graham Davison <gdavison@hashicorp.com>
2023-05-04 15:02:53 -0700

    Adds logging

--- .teamcity/scripts/provider_tests/acceptance_tests.sh
+++ .teamcity/scripts/provider_tests/acceptance_tests.sh
@@ -4,6 +4,12 @@ set -euo pipefail
 
 # shellcheck disable=2157 # This isn't a constant string, it's a TeamCity variable substitution
 if [[ -n "%ACCTEST_ROLE_ARN%" ]]; then
+    echo "assuming role %ACCTEST_ROLE_ARN% for acctests"
+
+    echo "AWS_ACCESS_KEY_ID: $(echo "%AWS_ACCESS_KEY_ID%" | sed -E "s/^.+(.{4})/****\1/")"
+    echo "AWS_SECRET_ACCESS_KEY: $(echo "%AWS_SECRET_ACCESS_KEY%" | sed -E "s/^(.{4}).+(.{4})/\1****\2/")"
+    echo "ACCTEST_ROLE_ARN: %ACCTEST_ROLE_ARN%"
+
     conf=$(pwd)/aws.conf
 
     function cleanup {
@@ -16,9 +22,9 @@ if [[ -n "%ACCTEST_ROLE_ARN%" ]]; then
     cat <<EOF >"${conf}"
 [profile primary]
 role_arn       = %ACCTEST_ROLE_ARN%
-source_profile = source
+source_profile = primary_user
 
-[profile source]
+[profile primary_user]
 aws_access_key_id     = %AWS_ACCESS_KEY_ID%
 aws_secret_access_key = %AWS_SECRET_ACCESS_KEY%
 EOF
