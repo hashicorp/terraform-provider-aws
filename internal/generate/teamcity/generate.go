767005019929d00673691178df8a716dec612246
Graham Davison <gdavison@hashicorp.com>
2023-05-04 16:50:55 -0700

    Generates list of provider acceptance tests

--- internal/generate/teamcity/generate.go
+++ internal/generate/teamcity/generate.go
@@ -1,4 +1,5 @@
 //go:generate go run services.go
+//go:generate go run provider_tests.go
 // ONLY generate directives and package declaration! Do not add anything else to this file.
 
 package teamcity
