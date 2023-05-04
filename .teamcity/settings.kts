7c721e459588c8af97bd2525079ae1a07d8b23b8
Graham Davison <gdavison@hashicorp.com>
2023-05-04 10:41:14 -0700

    Always sets `ACCTEST_ALTERNATE_ROLE_ARN`

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -115,8 +115,8 @@ project {
             if (awsSecretAccessKey != "") {
                 password("AWS_ALTERNATE_SECRET_ACCESS_KEY", alternateAWSSecretAccessKey, display = ParameterDisplay.HIDDEN)
             }
-            text("ACCTEST_ALTERNATE_ROLE_ARN", alternateAccTestRoleARN, display = ParameterDisplay.HIDDEN)
         }
+        text("ACCTEST_ALTERNATE_ROLE_ARN", alternateAccTestRoleARN, display = ParameterDisplay.HIDDEN)
 
         // Define this parameter even when not set to allow individual builds to set the value
         text("env.TF_ACC_TERRAFORM_VERSION", DslContext.getParameter("terraform_version", ""))
