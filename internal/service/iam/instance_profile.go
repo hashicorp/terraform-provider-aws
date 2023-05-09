7253b4183c1f87dbcf5dc6b841b2bb76128940dc
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-05-09 17:31:38 -0400

    r/aws_iam_instance_profile: Modernize.

--- internal/service/iam/instance_profile.go
+++ internal/service/iam/instance_profile.go
@@ -113,18 +113,22 @@ func resourceInstanceProfileCreate(ctx context.Context, d *schema.ResourceData, 
 		return sdkdiag.AppendErrorf(diags, "creating IAM Instance Profile (%s): %s", name, err)
 	}
 
-	flattenInstanceProfile(ctx, d, output.InstanceProfile) // sets id
+	d.SetId(aws.StringValue(output.InstanceProfile.InstanceProfileName))
 
-	waiterRequest := &iam.GetInstanceProfileInput{
-		InstanceProfileName: aws.String(name),
-	}
-	// don't return until the IAM service reports that the instance profile is ready.
-	// this ensures that terraform resources which rely on the instance profile will 'see'
-	// that the instance profile exists.
-	err = conn.WaitUntilInstanceProfileExistsWithContext(ctx, waiterRequest)
+	_, err = tfresource.RetryWhenNotFound(ctx, propagationTimeout, func() (interface{}, error) {
+		return FindInstanceProfileByName(ctx, conn, d.Id())
+	})
 
 	if err != nil {
-		return sdkdiag.AppendErrorf(diags, "timed out while waiting for instance profile %s: %s", name, err)
+		return sdkdiag.AppendErrorf(diags, "waiting for IAM Instance Profile (%s) create: %s", d.Id(), err)
+	}
+
+	if v, ok := d.GetOk("role"); ok {
+		err := instanceProfileAddRole(ctx, conn, d.Id(), v.(string))
+
+		if err != nil {
+			return sdkdiag.AppendFromErr(diags, err)
+		}
 	}
 
 	// For partitions not supporting tag-on-create, attempt tag after create.
@@ -133,7 +137,7 @@ func resourceInstanceProfileCreate(ctx context.Context, d *schema.ResourceData, 
 
 		// If default tags only, continue. Otherwise, error.
 		if v, ok := d.GetOk(names.AttrTags); (!ok || len(v.(map[string]interface{})) == 0) && errs.IsUnsupportedOperationInPartitionError(conn.PartitionID, err) {
-			return append(diags, resourceInstanceProfileUpdate(ctx, d, meta)...)
+			return append(diags, resourceInstanceProfileRead(ctx, d, meta)...)
 		}
 
 		if err != nil {
@@ -141,47 +145,53 @@ func resourceInstanceProfileCreate(ctx context.Context, d *schema.ResourceData, 
 		}
 	}
 
-	return append(diags, resourceInstanceProfileUpdate(ctx, d, meta)...)
+	return append(diags, resourceInstanceProfileRead(ctx, d, meta)...)
 }
 
 func resourceInstanceProfileRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
 	conn := meta.(*conns.AWSClient).IAMConn()
 
-	request := &iam.GetInstanceProfileInput{
-		InstanceProfileName: aws.String(d.Id()),
-	}
+	instanceProfile, err := FindInstanceProfileByName(ctx, conn, d.Id())
 
-	result, err := conn.GetInstanceProfileWithContext(ctx, request)
-	if !d.IsNewResource() && tfawserr.ErrCodeEquals(err, iam.ErrCodeNoSuchEntityException) {
+	if !d.IsNewResource() && tfresource.NotFound(err) {
 		log.Printf("[WARN] IAM Instance Profile (%s) not found, removing from state", d.Id())
 		d.SetId("")
 		return diags
 	}
+
 	if err != nil {
 		return sdkdiag.AppendErrorf(diags, "reading IAM Instance Profile (%s): %s", d.Id(), err)
 	}
 
-	instanceProfile := result.InstanceProfile
-	if instanceProfile.Roles != nil && len(instanceProfile.Roles) > 0 {
+	if len(instanceProfile.Roles) > 0 {
 		roleName := aws.StringValue(instanceProfile.Roles[0].RoleName)
-		input := &iam.GetRoleInput{
-			RoleName: aws.String(roleName),
-		}
+		_, err := FindRoleByName(ctx, conn, roleName)
 
-		_, err := conn.GetRoleWithContext(ctx, input)
 		if err != nil {
-			if tfawserr.ErrCodeEquals(err, iam.ErrCodeNoSuchEntityException) {
+			if tfresource.NotFound(err) {
 				err := instanceProfileRemoveRole(ctx, conn, d.Id(), roleName)
+
 				if err != nil {
-					return sdkdiag.AppendErrorf(diags, "removing role %s to IAM Instance Profile (%s): %s", roleName, d.Id(), err)
+					return sdkdiag.AppendFromErr(diags, err)
 				}
 			}
-			return sdkdiag.AppendErrorf(diags, "reading IAM Role %s attached to IAM Instance Profile (%s): %s", roleName, d.Id(), err)
+
+			return sdkdiag.AppendErrorf(diags, "reading IAM Role (%s) attached to IAM Instance Profile (%s): %s", roleName, d.Id(), err)
 		}
 	}
 
-	flattenInstanceProfile(ctx, d, instanceProfile)
+	d.Set("arn", instanceProfile.Arn)
+	d.Set("create_date", instanceProfile.CreateDate.Format(time.RFC3339))
+	d.Set("name", instanceProfile.InstanceProfileName)
+	d.Set("name_prefix", create.NamePrefixFromName(aws.StringValue(instanceProfile.InstanceProfileName)))
+	d.Set("path", instanceProfile.Path)
+	if len(instanceProfile.Roles) > 0 {
+		d.Set("role", instanceProfile.Roles[0].RoleName) //there will only be 1 role returned
+	}
+	d.Set("unique_id", instanceProfile.InstanceProfileId)
+
+	SetTagsOut(ctx, instanceProfile.Tags)
 
 	return diags
 }
@@ -191,19 +201,21 @@ func resourceInstanceProfileUpdate(ctx context.Context, d *schema.ResourceData, 
 	conn := meta.(*conns.AWSClient).IAMConn()
 
 	if d.HasChange("role") {
-		oldRole, newRole := d.GetChange("role")
+		o, n := d.GetChange("role")
 
-		if oldRole.(string) != "" {
-			err := instanceProfileRemoveRole(ctx, conn, d.Id(), oldRole.(string))
+		if o := o.(string); o != "" {
+			err := instanceProfileRemoveRole(ctx, conn, d.Id(), o)
+
 			if err != nil {
-				return sdkdiag.AppendErrorf(diags, "removing role %s to IAM Instance Profile (%s): %s", oldRole.(string), d.Id(), err)
+				return sdkdiag.AppendFromErr(diags, err)
 			}
 		}
 
-		if newRole.(string) != "" {
-			err := instanceProfileAddRole(ctx, conn, d.Id(), newRole.(string))
+		if n := n.(string); n != "" {
+			err := instanceProfileAddRole(ctx, conn, d.Id(), n)
+
 			if err != nil {
-				return sdkdiag.AppendErrorf(diags, "adding role %s to IAM Instance Profile (%s): %s", newRole.(string), d.Id(), err)
+				return sdkdiag.AppendFromErr(diags, err)
 			}
 		}
 	}
@@ -213,9 +225,8 @@ func resourceInstanceProfileUpdate(ctx context.Context, d *schema.ResourceData, 
 
 		err := instanceProfileUpdateTags(ctx, conn, d.Id(), o, n)
 
-		// Some partitions (i.e., ISO) may not support tagging, giving error
-		if verify.ErrorISOUnsupported(conn.PartitionID, err) {
-			log.Printf("[WARN] failed updating tags for IAM Instance Profile (%s): %s", d.Id(), err)
+		// Some partitions (e.g. ISO) may not support tagging.
+		if errs.IsUnsupportedOperationInPartitionError(conn.PartitionID, err) {
 			return diags
 		}
 
@@ -231,18 +242,24 @@ func resourceInstanceProfileDelete(ctx context.Context, d *schema.ResourceData, 
 	var diags diag.Diagnostics
 	conn := meta.(*conns.AWSClient).IAMConn()
 
-	if err := instanceProfileRemoveAllRoles(ctx, d, conn); err != nil {
-		return sdkdiag.AppendErrorf(diags, "deleting IAM Instance Profile (%s): %s", d.Id(), err)
+	if v, ok := d.GetOk("role"); ok {
+		err := instanceProfileRemoveRole(ctx, conn, d.Id(), v.(string))
+
+		if err != nil {
+			return sdkdiag.AppendFromErr(diags, err)
+		}
 	}
 
-	request := &iam.DeleteInstanceProfileInput{
+	log.Printf("[INFO] Deleting IAM Instance Profile: %s", d.Id())
+	_, err := conn.DeleteInstanceProfileWithContext(ctx, &iam.DeleteInstanceProfileInput{
 		InstanceProfileName: aws.String(d.Id()),
+	})
+
+	if tfawserr.ErrCodeEquals(err, iam.ErrCodeNoSuchEntityException) {
+		return diags
 	}
-	_, err := conn.DeleteInstanceProfileWithContext(ctx, request)
+
 	if err != nil {
-		if tfawserr.ErrCodeEquals(err, iam.ErrCodeNoSuchEntityException) {
-			return diags
-		}
 		return sdkdiag.AppendErrorf(diags, "deleting IAM Instance Profile (%s): %s", d.Id(), err)
 	}
 
@@ -250,70 +267,73 @@ func resourceInstanceProfileDelete(ctx context.Context, d *schema.ResourceData, 
 }
 
 func instanceProfileAddRole(ctx context.Context, conn *iam.IAM, profileName, roleName string) error {
-	request := &iam.AddRoleToInstanceProfileInput{
+	input := &iam.AddRoleToInstanceProfileInput{
 		InstanceProfileName: aws.String(profileName),
 		RoleName:            aws.String(roleName),
 	}
 
-	err := retry.RetryContext(ctx, propagationTimeout, func() *retry.RetryError {
-		_, err := conn.AddRoleToInstanceProfileWithContext(ctx, request)
-		// IAM unfortunately does not provide a better error code or message for eventual consistency
-		// InvalidParameterValue: Value (XXX) for parameter iamInstanceProfile.name is invalid. Invalid IAM Instance Profile name
-		// NoSuchEntity: The request was rejected because it referenced an entity that does not exist. The error message describes the entity. HTTP Status Code: 404
-		if tfawserr.ErrMessageContains(err, "InvalidParameterValue", "Invalid IAM Instance Profile name") || tfawserr.ErrMessageContains(err, iam.ErrCodeNoSuchEntityException, "The role with name") {
-			return retry.RetryableError(err)
-		}
-		if err != nil {
-			return retry.NonRetryableError(err)
-		}
-		return nil
-	})
-	if tfresource.TimedOut(err) {
-		_, err = conn.AddRoleToInstanceProfileWithContext(ctx, request)
-	}
+	_, err := tfresource.RetryWhen(ctx, propagationTimeout,
+		func() (interface{}, error) {
+			return conn.AddRoleToInstanceProfileWithContext(ctx, input)
+		},
+		func(err error) (bool, error) {
+			// IAM unfortunately does not provide a better error code or message for eventual consistency
+			// InvalidParameterValue: Value (XXX) for parameter iamInstanceProfile.name is invalid. Invalid IAM Instance Profile name
+			// NoSuchEntity: The request was rejected because it referenced an entity that does not exist. The error message describes the entity. HTTP Status Code: 404
+			if tfawserr.ErrMessageContains(err, "InvalidParameterValue", "Invalid IAM Instance Profile name") || tfawserr.ErrMessageContains(err, iam.ErrCodeNoSuchEntityException, "The role with name") {
+				return true, err
+			}
+
+			return false, err
+		},
+	)
+
 	if err != nil {
-		return fmt.Errorf("adding IAM Role %s to Instance Profile %s: %w", roleName, profileName, err)
-	}
-
-	return err
-}
-
-func instanceProfileRemoveRole(ctx context.Context, conn *iam.IAM, profileName, roleName string) error {
-	request := &iam.RemoveRoleFromInstanceProfileInput{
-		InstanceProfileName: aws.String(profileName),
-		RoleName:            aws.String(roleName),
-	}
-
-	_, err := conn.RemoveRoleFromInstanceProfileWithContext(ctx, request)
-	if tfawserr.ErrCodeEquals(err, iam.ErrCodeNoSuchEntityException) {
-		return nil
-	}
-	return err
-}
-
-func instanceProfileRemoveAllRoles(ctx context.Context, d *schema.ResourceData, conn *iam.IAM) error {
-	if role, ok := d.GetOk("role"); ok {
-		err := instanceProfileRemoveRole(ctx, conn, d.Id(), role.(string))
-		if err != nil {
-			return fmt.Errorf("removing role (%s): %w", role, err)
-		}
+		return fmt.Errorf("adding IAM Role (%s) to IAM Instance Profile (%s): %w", roleName, profileName, err)
 	}
 
 	return nil
 }
 
-func flattenInstanceProfile(ctx context.Context, d *schema.ResourceData, result *iam.InstanceProfile) {
-	d.SetId(aws.StringValue(result.InstanceProfileName))
-	d.Set("arn", result.Arn)
-	d.Set("create_date", result.CreateDate.Format(time.RFC3339))
-	d.Set("name", result.InstanceProfileName)
-	d.Set("name_prefix", create.NamePrefixFromName(aws.StringValue(result.InstanceProfileName)))
-	d.Set("path", result.Path)
-	d.Set("unique_id", result.InstanceProfileId)
-
-	if result.Roles != nil && len(result.Roles) > 0 {
-		d.Set("role", result.Roles[0].RoleName) //there will only be 1 role returned
+func instanceProfileRemoveRole(ctx context.Context, conn *iam.IAM, profileName, roleName string) error {
+	input := &iam.RemoveRoleFromInstanceProfileInput{
+		InstanceProfileName: aws.String(profileName),
+		RoleName:            aws.String(roleName),
 	}
 
-	SetTagsOut(ctx, result.Tags)
+	_, err := conn.RemoveRoleFromInstanceProfileWithContext(ctx, input)
+
+	if tfawserr.ErrCodeEquals(err, iam.ErrCodeNoSuchEntityException) {
+		return nil
+	}
+
+	if err != nil {
+		return fmt.Errorf("removing IAM Role (%s) from IAM Instance Profile (%s): %w", roleName, profileName, err)
+	}
+
+	return nil
+}
+
+func FindInstanceProfileByName(ctx context.Context, conn *iam.IAM, name string) (*iam.InstanceProfile, error) {
+	input := &iam.GetInstanceProfileInput{
+		InstanceProfileName: aws.String(name),
+	}
+
+	output, err := conn.GetInstanceProfileWithContext(ctx, input)
+
+	if tfawserr.ErrCodeEquals(err, iam.ErrCodeNoSuchEntityException) {
+		return nil, &retry.NotFoundError{
+			LastError:   err,
+			LastRequest: input,
+		}
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	if output == nil || output.InstanceProfile == nil {
+		return nil, tfresource.NewEmptyResultError(input)
+	}
+
+	return output.InstanceProfile, nil
 }
