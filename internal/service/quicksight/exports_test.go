84312cc01d441aa748753f778e0d37c707555436
Jared Baker <jar-b@users.noreply.github.com>
2023-05-10 13:22:30 -0400

    New Resource: `aws_quicksight_template_alias` (#31310)
    * r/aws_quicksight_template_alias: resource implementation
    
    * chore: changelog

--- internal/service/quicksight/exports_test.go
+++ internal/service/quicksight/exports_test.go
@@ -7,4 +7,5 @@ var (
 	ResourceIngestion           = newResourceIngestion
 	ResourceNamespace           = newResourceNamespace
 	ResourceRefreshSchedule     = newResourceRefreshSchedule
+	ResourceTemplateAlias       = newResourceTemplateAlias
 )
