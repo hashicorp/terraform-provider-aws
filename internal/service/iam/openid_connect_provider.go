2ab58fe441255e0cab62eabb37461a3fff15a971
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-05-10 08:32:59 -0400

    r/aws_iam_instance_profile: Modernize.

--- internal/service/iam/openid_connect_provider.go
+++ internal/service/iam/openid_connect_provider.go
@@ -8,6 +8,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/iam"
 	"github.com/hashicorp/aws-sdk-go-base/v2/awsv1shim/v2/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
@@ -15,6 +16,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
 	"github.com/hashicorp/terraform-provider-aws/internal/flex"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
+	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
@@ -116,23 +118,22 @@ func resourceOpenIDConnectProviderRead(ctx context.Context, d *schema.ResourceDa
 	var diags diag.Diagnostics
 	conn := meta.(*conns.AWSClient).IAMConn()
 
-	input := &iam.GetOpenIDConnectProviderInput{
-		OpenIDConnectProviderArn: aws.String(d.Id()),
-	}
-	output, err := conn.GetOpenIDConnectProviderWithContext(ctx, input)
-	if !d.IsNewResource() && tfawserr.ErrCodeEquals(err, iam.ErrCodeNoSuchEntityException) {
+	output, err := FindOpenIDConnectProviderByARN(ctx, conn, d.Id())
+
+	if !d.IsNewResource() && tfresource.NotFound(err) {
 		log.Printf("[WARN] IAM OIDC Provider (%s) not found, removing from state", d.Id())
 		d.SetId("")
 		return diags
 	}
+
 	if err != nil {
 		return sdkdiag.AppendErrorf(diags, "reading IAM OIDC Provider (%s): %s", d.Id(), err)
 	}
 
 	d.Set("arn", d.Id())
-	d.Set("url", output.Url)
 	d.Set("client_id_list", aws.StringValueSlice(output.ClientIDList))
 	d.Set("thumbprint_list", aws.StringValueSlice(output.ThumbprintList))
+	d.Set("url", output.Url)
 
 	SetTagsOut(ctx, output.Tags)
 
@@ -150,6 +151,7 @@ func resourceOpenIDConnectProviderUpdate(ctx context.Context, d *schema.Resource
 		}
 
 		_, err := conn.UpdateOpenIDConnectProviderThumbprintWithContext(ctx, input)
+
 		if err != nil {
 			return sdkdiag.AppendErrorf(diags, "updating IAM OIDC Provider (%s) thumbprint: %s", d.Id(), err)
 		}
@@ -160,9 +162,8 @@ func resourceOpenIDConnectProviderUpdate(ctx context.Context, d *schema.Resource
 
 		err := openIDConnectProviderUpdateTags(ctx, conn, d.Id(), o, n)
 
-		// Some partitions (i.e., ISO) may not support tagging, giving error
-		if verify.ErrorISOUnsupported(conn.PartitionID, err) {
-			log.Printf("[WARN] failed updating tags for IAM OIDC Provider (%s): %s", d.Id(), err)
+		// Some partitions (e.g. ISO) may not support tagging.
+		if errs.IsUnsupportedOperationInPartitionError(conn.PartitionID, err) {
 			return append(diags, resourceOpenIDConnectProviderRead(ctx, d, meta)...)
 		}
 
@@ -178,16 +179,42 @@ func resourceOpenIDConnectProviderDelete(ctx context.Context, d *schema.Resource
 	var diags diag.Diagnostics
 	conn := meta.(*conns.AWSClient).IAMConn()
 
-	input := &iam.DeleteOpenIDConnectProviderInput{
+	log.Printf("[INFO] Deleting IAM OIDC Provider: %s", d.Id())
+	_, err := conn.DeleteOpenIDConnectProviderWithContext(ctx, &iam.DeleteOpenIDConnectProviderInput{
 		OpenIDConnectProviderArn: aws.String(d.Id()),
-	}
-	_, err := conn.DeleteOpenIDConnectProviderWithContext(ctx, input)
+	})
+
 	if tfawserr.ErrCodeEquals(err, iam.ErrCodeNoSuchEntityException) {
 		return diags
 	}
+
 	if err != nil {
 		return sdkdiag.AppendErrorf(diags, "deleting IAM OIDC Provider (%s): %s", d.Id(), err)
 	}
 
 	return diags
+}
+
+func FindOpenIDConnectProviderByARN(ctx context.Context, conn *iam.IAM, arn string) (*iam.GetOpenIDConnectProviderOutput, error) {
+	input := &iam.GetOpenIDConnectProviderInput{
+		OpenIDConnectProviderArn: aws.String(arn),
+	}
+
+	output, err := conn.GetOpenIDConnectProviderWithContext(ctx, input)
+
+	if tfawserr.ErrCodeEquals(err, iam.ErrCodeNoSuchEntityException) {
+		return nil, &retry.NotFoundError{
+			LastError:   err,
+			LastRequest: input,
+		}
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	if output == nil {
+		return nil, tfresource.NewEmptyResultError(input)
+	}
+
+	return output, nil
 }
