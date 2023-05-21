a1a02b8a296cd32d1f2adcd06631a93477b0702c
Pat Myron <PatMyron@users.noreply.github.com>
2023-05-21 15:14:26 -0400

    source enum validations from AWS Go SDK

--- internal/service/configservice/organization_custom_policy_rule.go
+++ internal/service/configservice/organization_custom_policy_rule.go
@@ -78,13 +78,7 @@ func ResourceOrganizationCustomPolicyRule() *schema.Resource {
 			"maximum_execution_frequency": {
 				Type:     schema.TypeString,
 				Optional: true,
-				ValidateFunc: validation.StringInSlice([]string{
-					configservice.MaximumExecutionFrequencyOneHour,
-					configservice.MaximumExecutionFrequencyThreeHours,
-					configservice.MaximumExecutionFrequencySixHours,
-					configservice.MaximumExecutionFrequencyTwelveHours,
-					configservice.MaximumExecutionFrequencyTwentyFourHours,
-				}, false),
+				ValidateFunc: validation.StringInSlice(configservice.MaximumExecutionFrequency_Values(), false),
 			},
 			"name": {
 				Type:         schema.TypeString,
