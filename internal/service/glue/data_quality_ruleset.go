c7b0d553199e69cded5dce4f012c1a7650da1d76
GlennChia <glennchia7@gmail.com>
2023-05-26 16:03:42 +0000

    feat(glue): dataQualityRuleset read

--- internal/service/glue/data_quality_ruleset.go
+++ internal/service/glue/data_quality_ruleset.go
@@ -1,9 +1,21 @@
 package glue
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/arn"
+	"github.com/aws/aws-sdk-go/service/glue"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
+	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
@@ -12,6 +24,7 @@ import (
 // @Tags(identifierAttribute="arn")
 func ResourceDataQualityRuleset() *schema.Resource {
 	return &schema.Resource{
+		ReadWithoutTimeout: resourceDataQualityRulesetRead,
 		Importer: &schema.ResourceImporter{
 			StateContext: schema.ImportStatePassthroughContext,
 		},
@@ -83,4 +96,57 @@ func ResourceDataQualityRuleset() *schema.Resource {
 			},
 		},
 	}
+}
+
+func resourceDataQualityRulesetRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	conn := meta.(*conns.AWSClient).GlueConn()
+
+	name := d.Id()
+
+	dataQualityRuleset, err := FindDataQualityRulesetByName(ctx, conn, name)
+	if !d.IsNewResource() && tfresource.NotFound(err) {
+		log.Printf("[WARN] Glue Data Quality Ruleset (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return diags
+	}
+
+	if err != nil {
+		return sdkdiag.AppendErrorf(diags, "reading Glue Data Quality Ruleset (%s): %s", d.Id(), err)
+	}
+
+	dataQualityRulesetArn := arn.ARN{
+		Partition: meta.(*conns.AWSClient).Partition,
+		Service:   "glue",
+		Region:    meta.(*conns.AWSClient).Region,
+		AccountID: meta.(*conns.AWSClient).AccountID,
+		Resource:  fmt.Sprintf("dataQualityRuleset/%s", aws.StringValue(dataQualityRuleset.Name)),
+	}.String()
+
+	d.Set("arn", dataQualityRulesetArn)
+	d.Set("created_on", dataQualityRuleset.CreatedOn.Format(time.RFC3339))
+	d.Set("name", dataQualityRuleset.Name)
+	d.Set("description", dataQualityRuleset.Description)
+	d.Set("last_modified_on", dataQualityRuleset.CreatedOn.Format(time.RFC3339))
+	d.Set("recommendation_run_id", dataQualityRuleset.RecommendationRunId)
+	d.Set("ruleset", dataQualityRuleset.Ruleset)
+
+	if err := d.Set("target_table", flattenTargetTable(dataQualityRuleset.TargetTable)); err != nil {
+		return sdkdiag.AppendErrorf(diags, "setting target_table: %s", err)
+	}
+
+	return diags
+}
+
+func flattenTargetTable(apiObject *glue.DataQualityTargetTable) []interface{} {
+	if apiObject == nil {
+		return []interface{}{}
+	}
+
+	tfMap := map[string]interface{}{
+		"database_name": aws.StringValue(apiObject.DatabaseName),
+		"table_name":    aws.StringValue(apiObject.TableName),
+	}
+
+	return []interface{}{tfMap}
 }
