52c760884dfd425c1ce3bc0c2c393552875299a1
Bruno Schaatsbergen <git@bschaatsbergen.com>
2023-06-01 07:31:48 +0200

    feat: create TestAccECRPullThroughCacheRuleDataSource_basic

---
+++ internal/service/ecr/pull_through_cache_rule_data_source_test.go
@@ -0,0 +1,35 @@
+package ecr_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/service/ecr"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+)
+
+func TestAccECRPullThroughCacheRuleDataSource_basic(t *testing.T) {
+	ctx := acctest.Context(t)
+	ecr_repository_prefix := "ecr-public"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
+		ErrorCheck:               acctest.ErrorCheck(t, ecr.EndpointsID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccPullThroughCacheRuleDataSourceConfig_basic(ecr_repository_prefix),
+				Check:  resource.ComposeTestCheckFunc(),
+			},
+		},
+	})
+}
+
+func testAccPullThroughCacheRuleDataSourceConfig_basic(ecr_repository_prefix string) string {
+	return fmt.Sprintf(`
+data "aws_ecr_pull_through_cache_rule" "by_tag" {
+  ecr_repository_prefix = %q
+}
+`, ecr_repository_prefix)
+}
