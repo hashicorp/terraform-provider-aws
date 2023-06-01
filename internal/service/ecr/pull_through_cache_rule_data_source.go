6b67ffd3a2dbab026d0d91f01f96c668da33f6fe
Bruno Schaatsbergen <git@bschaatsbergen.com>
2023-06-01 07:27:22 +0200

    feat: create DataSourcePullThroughCacheRule

---
+++ internal/service/ecr/pull_through_cache_rule_data_source.go
@@ -0,0 +1,64 @@
+package ecr
+
+import (
+	"context"
+	"log"
+	"regexp"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
+)
+
+// @SDKDataSource("aws_ecr_pull_through_cache_rule")
+func DataSourcePullThroughCacheRule() *schema.Resource {
+	return &schema.Resource{
+		ReadWithoutTimeout: dataSourcePullThroughCacheRuleRead,
+		Schema: map[string]*schema.Schema{
+			"ecr_repository_prefix": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+				ValidateFunc: validation.All(
+					validation.StringLenBetween(2, 20),
+					validation.StringMatch(
+						regexp.MustCompile(`^[a-z0-9]+(?:[._-][a-z0-9]+)*$`),
+						"must only include alphanumeric, underscore, period, or hyphen characters"),
+				),
+			},
+			"registry_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"upstream_registry_url": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+		},
+	}
+}
+
+func dataSourcePullThroughCacheRuleRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn := meta.(*conns.AWSClient).ECRConn()
+
+	rule, err := FindPullThroughCacheRuleByRepositoryPrefix(ctx, conn, d.Id())
+
+	if !d.IsNewResource() && tfresource.NotFound(err) {
+		log.Printf("[WARN] ECR Pull Through Cache Rule (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	if err != nil {
+		return diag.Errorf("error reading ECR Pull Through Cache Rule (%s): %s", d.Id(), err)
+	}
+
+	d.Set("ecr_repository_prefix", rule.EcrRepositoryPrefix)
+	d.Set("registry_id", rule.RegistryId)
+	d.Set("upstream_registry_url", rule.UpstreamRegistryUrl)
+
+	return nil
+}
