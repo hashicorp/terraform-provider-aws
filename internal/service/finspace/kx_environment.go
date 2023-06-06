379c908caf26cee99c0b1fbd70dfa661ec248b27
Kavan Bansal <bankavan@amazon.com>
2023-06-06 15:57:32 -0400

    Adds finspace_kx_environment resource

---
+++ internal/service/finspace/kx_environment.go
@@ -0,0 +1,587 @@
+package finspace
+
+import (
+	"context"
+	"errors"
+	"log"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/finspace"
+	"github.com/aws/aws-sdk-go-v2/service/finspace/types"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/id"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/create"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
+	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
+	"github.com/hashicorp/terraform-provider-aws/names"
+)
+
+// @SDKResource("aws_finspace_kx_environment")
+// @Tags(identifierAttribute="arn")
+func ResourceKxEnvironment() *schema.Resource {
+	return &schema.Resource{
+		CreateWithoutTimeout: resourceKxEnvironmentCreate,
+		ReadWithoutTimeout:   resourceKxEnvironmentRead,
+		UpdateWithoutTimeout: resourceKxEnvironmentUpdate,
+		DeleteWithoutTimeout: resourceKxEnvironmentDelete,
+
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(30 * time.Minute),
+			Update: schema.DefaultTimeout(30 * time.Minute),
+			Delete: schema.DefaultTimeout(30 * time.Minute),
+		},
+
+		Schema: map[string]*schema.Schema{
+			"id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"arn": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"status": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"availability_zones": {
+				Type: schema.TypeList,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+				Computed: true,
+			},
+			"infrastructure_account_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"created_timestamp": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"last_modified_timestamp": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"name": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validation.StringLenBetween(1, 255),
+			},
+			"description": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ValidateFunc: validation.StringLenBetween(1, 1000),
+			},
+			"kms_key_id": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: verify.ValidARN,
+			},
+			"transit_gateway_configuration": {
+				Type:     schema.TypeList,
+				Optional: true,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"transit_gateway_id": {
+							Type:         schema.TypeString,
+							Required:     true,
+							ValidateFunc: validation.StringLenBetween(1, 32),
+						},
+						"routable_cidr_space": {
+							Type:         schema.TypeString,
+							Required:     true,
+							ValidateFunc: validation.IsCIDR,
+						},
+					},
+				},
+			},
+			"custom_dns_configuration": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"custom_dns_server_name": {
+							Type:         schema.TypeString,
+							Required:     true,
+							ValidateFunc: validation.StringLenBetween(3, 255),
+						},
+						"custom_dns_server_ip": {
+							Type:         schema.TypeString,
+							Required:     true,
+							ValidateFunc: validation.IsIPAddress,
+						},
+					},
+				},
+			},
+			"tags":     tftags.TagsSchema(),
+			"tags_all": tftags.TagsSchemaComputed(),
+		},
+		CustomizeDiff: verify.SetTagsDiff,
+	}
+}
+
+const (
+	ResNameKxEnvironment = "Kx Environment"
+)
+
+func resourceKxEnvironmentCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*conns.AWSClient).FinSpaceClient()
+
+	in := &finspace.CreateKxEnvironmentInput{
+		Name:        aws.String(d.Get("name").(string)),
+		ClientToken: aws.String(id.UniqueId()),
+		Tags:        GetTagsIn(ctx),
+	}
+
+	if v, ok := d.GetOk("description"); ok {
+		in.Description = aws.String(v.(string))
+	}
+
+	if v, ok := d.GetOk("kms_key_id"); ok {
+		in.KmsKeyId = aws.String(v.(string))
+	}
+
+	out, err := client.CreateKxEnvironment(ctx, in)
+	if err != nil {
+		return create.DiagError(names.FinSpace, create.ErrActionCreating, ResNameKxEnvironment, d.Get("name").(string), err)
+	}
+
+	if out == nil || out.EnvironmentId == nil {
+		return create.DiagError(names.FinSpace, create.ErrActionCreating, ResNameKxEnvironment, d.Get("name").(string), errors.New("empty output"))
+	}
+
+	d.SetId(aws.ToString(out.EnvironmentId))
+
+	if _, err := waitKxEnvironmentCreated(ctx, client, d.Id(), d.Timeout(schema.TimeoutCreate)); err != nil {
+		return create.DiagError(names.FinSpace, create.ErrActionWaitingForCreation, ResNameKxEnvironment, d.Id(), err)
+	}
+
+	if _, err := updateKxEnvironmentNetwork(ctx, d, client); err != nil {
+		return create.DiagError(names.FinSpace, create.ErrActionCreating, ResNameKxEnvironment, d.Id(), err)
+	}
+
+	return resourceKxEnvironmentRead(ctx, d, meta)
+}
+
+func resourceKxEnvironmentRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn := meta.(*conns.AWSClient).FinSpaceClient()
+
+	out, err := findKxEnvironmentByID(ctx, conn, d.Id())
+
+	if !d.IsNewResource() && tfresource.NotFound(err) {
+		log.Printf("[WARN] FinSpace KxEnvironment (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	if err != nil {
+		return create.DiagError(names.FinSpace, create.ErrActionReading, ResNameKxEnvironment, d.Id(), err)
+	}
+
+	d.Set("id", out.EnvironmentId)
+	d.Set("arn", out.EnvironmentArn)
+	d.Set("name", out.Name)
+	d.Set("description", out.Description)
+	d.Set("kms_key_id", out.KmsKeyId)
+	d.Set("status", out.Status)
+	d.Set("availability_zones", out.AvailabilityZoneIds)
+	d.Set("infrastructure_account_id", out.DedicatedServiceAccountId)
+	d.Set("created_timestamp", out.CreationTimestamp.String())
+	d.Set("last_modified_timestamp", out.UpdateTimestamp.String())
+
+	if err := d.Set("transit_gateway_configuration", flattenTransitGatewayConfiguration(out.TransitGatewayConfiguration)); err != nil {
+		return create.DiagError(names.FinSpace, create.ErrActionSetting, ResNameKxEnvironment, d.Id(), err)
+	}
+
+	if err := d.Set("custom_dns_configuration", flattenCustomDnsConfigurations(out.CustomDNSConfiguration)); err != nil {
+		return create.DiagError(names.FinSpace, create.ErrActionSetting, ResNameKxEnvironment, d.Id(), err)
+	}
+
+	return nil
+}
+
+func resourceKxEnvironmentUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn := meta.(*conns.AWSClient).FinSpaceClient()
+
+	update := false
+
+	in := &finspace.UpdateKxEnvironmentInput{
+		EnvironmentId: aws.String(d.Id()),
+		Name:          aws.String(d.Get("name").(string)),
+	}
+
+	if d.HasChanges("description") {
+		in.Description = aws.String(d.Get("description").(string))
+	}
+
+	if d.HasChanges("name") || d.HasChanges("description") {
+		update = true
+		log.Printf("[DEBUG] Updating FinSpace KxEnvironment (%s): %#v", d.Id(), in)
+		_, err := conn.UpdateKxEnvironment(ctx, in)
+		if err != nil {
+			return create.DiagError(names.FinSpace, create.ErrActionUpdating, ResNameKxEnvironment, d.Id(), err)
+		}
+	}
+
+	if d.HasChanges("transit_gateway_configuration") || d.HasChanges("custom_dns_configuration") {
+		update = true
+		if _, err := updateKxEnvironmentNetwork(ctx, d, conn); err != nil {
+			return create.DiagError(names.FinSpace, create.ErrActionUpdating, ResNameKxEnvironment, d.Id(), err)
+		}
+	}
+
+	if !update {
+		return nil
+	}
+	return resourceKxEnvironmentRead(ctx, d, meta)
+}
+
+func resourceKxEnvironmentDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn := meta.(*conns.AWSClient).FinSpaceClient()
+
+	log.Printf("[INFO] Deleting FinSpace KxEnvironment %s", d.Id())
+
+	out, getErr := conn.GetKxEnvironment(ctx, &finspace.GetKxEnvironmentInput{
+		EnvironmentId: aws.String(d.Id()),
+	})
+	var nfe *types.ResourceNotFoundException
+	if (getErr != nil && errors.As(getErr, &nfe)) || out.Status == types.EnvironmentStatusDeleted {
+		log.Printf("[DEBUG] FinSpace KxEnvironment %s already deleted. Nothing to delete.", d.Id())
+		return nil
+	}
+	_, err := conn.DeleteKxEnvironment(ctx, &finspace.DeleteKxEnvironmentInput{
+		EnvironmentId: aws.String(d.Id()),
+	})
+
+	if err != nil {
+		return create.DiagError(names.FinSpace, create.ErrActionDeleting, ResNameKxEnvironment, d.Id(), err)
+	}
+
+	if _, err := waitKxEnvironmentDeleted(ctx, conn, d.Id(), d.Timeout(schema.TimeoutDelete)); err != nil {
+		return create.DiagError(names.FinSpace, create.ErrActionWaitingForDeletion, ResNameKxEnvironment, d.Id(), err)
+	}
+
+	return nil
+}
+
+/*
+*
+
+	As of 2023-02-09 updating network configuration requires 2 separate requests if both DNS and transit gateway
+	configuration is set.
+*/
+func updateKxEnvironmentNetwork(ctx context.Context, d *schema.ResourceData, client *finspace.Client) (*finspace.UpdateKxEnvironmentNetworkOutput, error) {
+
+	transitGatewayConfigIn := &finspace.UpdateKxEnvironmentNetworkInput{
+		EnvironmentId: aws.String(d.Id()),
+		ClientToken:   aws.String(id.UniqueId()),
+	}
+
+	customDnsConfigIn := &finspace.UpdateKxEnvironmentNetworkInput{
+		EnvironmentId: aws.String(d.Id()),
+		ClientToken:   aws.String(id.UniqueId()),
+	}
+
+	updateTransitGatewayConfig := false
+	updateCustomDnsConfig := false
+
+	if v, ok := d.GetOk("transit_gateway_configuration"); ok && len(v.([]interface{})) > 0 && v.([]interface{})[0] != nil &&
+		d.HasChanges("transit_gateway_configuration") {
+		transitGatewayConfigIn.TransitGatewayConfiguration = expandTransitGatewayConfiguration(v.([]interface{}))
+		updateTransitGatewayConfig = true
+	}
+
+	if v, ok := d.GetOk("custom_dns_configuration"); ok && len(v.([]interface{})) > 0 && v.([]interface{})[0] != nil &&
+		d.HasChanges("custom_dns_configuration") {
+		customDnsConfigIn.CustomDNSConfiguration = expandCustomDnsConfigurations(v.([]interface{}))
+		updateCustomDnsConfig = true
+	}
+
+	var out *finspace.UpdateKxEnvironmentNetworkOutput
+	var err error
+
+	if updateTransitGatewayConfig {
+		out, err = client.UpdateKxEnvironmentNetwork(ctx, transitGatewayConfigIn)
+		if err != nil {
+			return nil, err
+		}
+
+		if _, err := waitTransitGatewayConfigurationUpdated(ctx, client, d.Id(), d.Timeout(schema.TimeoutUpdate)); err != nil {
+			return nil, err
+		}
+	}
+
+	if updateCustomDnsConfig {
+		out, err = client.UpdateKxEnvironmentNetwork(ctx, customDnsConfigIn)
+		if err != nil {
+			return nil, err
+		}
+
+		if _, err := waitCustomDnsConfigurationUpdated(ctx, client, d.Id(), d.Timeout(schema.TimeoutUpdate)); err != nil {
+			return nil, err
+		}
+	}
+
+	return out, nil
+}
+
+func waitKxEnvironmentCreated(ctx context.Context, conn *finspace.Client, id string, timeout time.Duration) (*finspace.GetKxEnvironmentOutput, error) {
+	stateConf := &retry.StateChangeConf{
+		Pending:                   []string{string(types.EnvironmentStatusCreateRequested), string(types.EnvironmentStatusCreating)},
+		Target:                    []string{string(types.EnvironmentStatusCreated)},
+		Refresh:                   statusKxEnvironment(ctx, conn, id),
+		Timeout:                   timeout,
+		NotFoundChecks:            20,
+		ContinuousTargetOccurence: 2,
+	}
+
+	outputRaw, err := stateConf.WaitForStateContext(ctx)
+	if out, ok := outputRaw.(*finspace.GetKxEnvironmentOutput); ok {
+		return out, err
+	}
+
+	return nil, err
+}
+
+func waitTransitGatewayConfigurationUpdated(ctx context.Context, conn *finspace.Client, id string, timeout time.Duration) (*finspace.GetKxEnvironmentOutput, error) {
+	stateConf := &retry.StateChangeConf{
+		Pending: []string{string(types.TgwStatusUpdateRequested), string(types.TgwStatusUpdating)},
+		Target:  []string{string(types.TgwStatusSuccessfullyUpdated)},
+		Refresh: statusTransitGatewayConfiguration(ctx, conn, id),
+		Timeout: timeout,
+	}
+
+	outputRaw, err := stateConf.WaitForStateContext(ctx)
+	if out, ok := outputRaw.(*finspace.GetKxEnvironmentOutput); ok {
+		return out, err
+	}
+
+	return nil, err
+}
+
+func waitCustomDnsConfigurationUpdated(ctx context.Context, conn *finspace.Client, id string, timeout time.Duration) (*finspace.GetKxEnvironmentOutput, error) {
+	stateConf := &retry.StateChangeConf{
+		Pending: []string{string(types.DnsStatusUpdateRequested), string(types.DnsStatusUpdating)},
+		Target:  []string{string(types.DnsStatusSuccessfullyUpdated)},
+		Refresh: statusCustomDnsConfiguration(ctx, conn, id),
+		Timeout: timeout,
+	}
+
+	outputRaw, err := stateConf.WaitForStateContext(ctx)
+	if out, ok := outputRaw.(*finspace.GetKxEnvironmentOutput); ok {
+		return out, err
+	}
+
+	return nil, err
+}
+
+func waitKxEnvironmentDeleted(ctx context.Context, conn *finspace.Client, id string, timeout time.Duration) (*finspace.GetKxEnvironmentOutput, error) {
+	stateConf := &retry.StateChangeConf{
+		Pending: []string{string(types.EnvironmentStatusDeleteRequested), string(types.EnvironmentStatusDeleting)},
+		Target:  []string{string(types.EnvironmentStatusDeleted)},
+		Refresh: statusKxEnvironment(ctx, conn, id),
+		Timeout: timeout,
+	}
+
+	outputRaw, err := stateConf.WaitForStateContext(ctx)
+	if out, ok := outputRaw.(*finspace.GetKxEnvironmentOutput); ok {
+		return out, err
+	}
+
+	return nil, err
+}
+
+func statusKxEnvironment(ctx context.Context, conn *finspace.Client, id string) retry.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		out, err := findKxEnvironmentByID(ctx, conn, id)
+		if tfresource.NotFound(err) {
+			return nil, "", nil
+		}
+
+		if err != nil {
+			return nil, "", err
+		}
+
+		return out, string(out.Status), nil
+	}
+}
+
+func statusTransitGatewayConfiguration(ctx context.Context, conn *finspace.Client, id string) retry.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		out, err := findKxEnvironmentByID(ctx, conn, id)
+		if tfresource.NotFound(err) {
+			return nil, "", nil
+		}
+
+		if err != nil {
+			return nil, "", err
+		}
+
+		return out, string(out.TgwStatus), nil
+	}
+}
+
+func statusCustomDnsConfiguration(ctx context.Context, conn *finspace.Client, id string) retry.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		out, err := findKxEnvironmentByID(ctx, conn, id)
+		if tfresource.NotFound(err) {
+			return nil, "", nil
+		}
+
+		if err != nil {
+			return nil, "", err
+		}
+
+		return out, string(out.DnsStatus), nil
+	}
+}
+
+func findKxEnvironmentByID(ctx context.Context, conn *finspace.Client, id string) (*finspace.GetKxEnvironmentOutput, error) {
+	in := &finspace.GetKxEnvironmentInput{
+		EnvironmentId: aws.String(id),
+	}
+	out, err := conn.GetKxEnvironment(ctx, in)
+	if err != nil {
+		var nfe *types.ResourceNotFoundException
+		if errors.As(err, &nfe) {
+			return nil, &retry.NotFoundError{
+				LastError:   err,
+				LastRequest: in,
+			}
+		}
+
+		return nil, err
+	}
+
+	if out == nil || out.EnvironmentArn == nil {
+		return nil, tfresource.NewEmptyResultError(in)
+	}
+
+	return out, nil
+}
+
+func expandTransitGatewayConfiguration(tfList []interface{}) *types.TransitGatewayConfiguration {
+	if len(tfList) == 0 || tfList[0] == nil {
+		return nil
+	}
+
+	tfMap := tfList[0].(map[string]interface{})
+
+	a := &types.TransitGatewayConfiguration{}
+
+	if v, ok := tfMap["transit_gateway_id"].(string); ok && v != "" {
+		a.TransitGatewayID = aws.String(v)
+	}
+
+	if v, ok := tfMap["routable_cidr_space"].(string); ok && v != "" {
+		a.RoutableCIDRSpace = aws.String(v)
+	}
+
+	return a
+}
+
+func expandCustomDnsConfiguration(tfMap map[string]interface{}) *types.CustomDNSServer {
+	if tfMap == nil {
+		return nil
+	}
+
+	a := &types.CustomDNSServer{}
+
+	if v, ok := tfMap["custom_dns_server_name"].(string); ok && v != "" {
+		a.CustomDNSServerName = aws.String(v)
+	}
+
+	if v, ok := tfMap["custom_dns_server_ip"].(string); ok && v != "" {
+		a.CustomDNSServerIP = aws.String(v)
+	}
+
+	return a
+}
+
+func expandCustomDnsConfigurations(tfList []interface{}) []types.CustomDNSServer {
+	if len(tfList) == 0 {
+		return nil
+	}
+
+	var s []types.CustomDNSServer
+
+	for _, r := range tfList {
+		m, ok := r.(map[string]interface{})
+
+		if !ok {
+			continue
+		}
+
+		a := expandCustomDnsConfiguration(m)
+
+		if a == nil {
+			continue
+		}
+
+		s = append(s, *a)
+	}
+
+	return s
+}
+
+func flattenTransitGatewayConfiguration(apiObject *types.TransitGatewayConfiguration) []interface{} {
+	if apiObject == nil {
+		return nil
+	}
+
+	m := map[string]interface{}{}
+
+	if v := apiObject.TransitGatewayID; v != nil {
+		m["transit_gateway_id"] = aws.ToString(v)
+	}
+
+	if v := apiObject.RoutableCIDRSpace; v != nil {
+		m["routable_cidr_space"] = aws.ToString(v)
+	}
+
+	return []interface{}{m}
+}
+
+func flattenCustomDnsConfiguration(apiObject *types.CustomDNSServer) map[string]interface{} {
+	if apiObject == nil {
+		return nil
+	}
+
+	m := map[string]interface{}{}
+
+	if v := apiObject.CustomDNSServerName; v != nil {
+		m["custom_dns_server_name"] = aws.ToString(v)
+	}
+
+	if v := apiObject.CustomDNSServerIP; v != nil {
+		m["custom_dns_server_ip"] = aws.ToString(v)
+	}
+
+	return m
+}
+
+func flattenCustomDnsConfigurations(apiObjects []types.CustomDNSServer) []interface{} {
+	if len(apiObjects) == 0 {
+		return nil
+	}
+
+	var l []interface{}
+
+	for _, apiObject := range apiObjects {
+		l = append(l, flattenCustomDnsConfiguration(&apiObject))
+	}
+
+	return l
+}
---
+++ website/docs/r/finspace_kx_environment.html.markdown
@@ -0,0 +1,113 @@
+---
+subcategory: "FinSpace"
+layout: "aws"
+page_title: "AWS: aws_finspace_kx_environment"
+description: |-
+  Terraform resource for managing an AWS FinSpace Kx Environment.
+---
+
+# Resource: aws_finspace_kx_environment
+
+Terraform resource for managing an AWS FinSpace Kx Environment.
+
+## Example Usage
+
+### Basic Usage
+
+```terraform
+resource "aws_kms_key" "example" {
+  description             = "Sample KMS Key"
+  deletion_window_in_days = 7
+}
+
+resource "aws_finspace_kx_environment" "example" {
+  name     = "my-tf-kx-environment"
+  kms_key_id =aws_kms_key.example.arn
+}
+```
+
+### With Network Setup
+
+```terraform
+resource "aws_kms_key" "example" {
+  description             = "Sample KMS Key"
+  deletion_window_in_days = 7
+}
+
+resource "aws_ec2_transit_gateway" "example" {
+  description = "example"
+}
+
+resource "aws_finspace_kx_environment" "example_env" {
+  name = "my-tf-kx-environment"
+  description = "Environment description"
+  kms_key_id = aws_kms_key.test.arn
+  
+  transit_gateway_configuration {
+    transit_gateway_id = aws_ec2_transit_gateway.example.id
+    routable_cidr_space = "100.64.0.0/26"
+  }
+
+  custom_dns_configuration {
+    custom_dns_server_name = "example.finspace.amazonaws.com"
+    custom_dns_server_ip = "10.0.0.76"
+  }
+}
+```
+
+## Argument Reference
+
+The following arguments are required:
+
+* `name` - (Required) Name of the KX environment that you want to create.
+* `kms_key_id` - (Required) KMS key ID to encrypt your data in the FinSpace environment.
+
+The following arguments are optional:
+
+* `description` - (Optional) Description for the KX environment.
+* `transit_gateway_configuration` - (Optional) Transit gateway and network configuration that is used to connect the KX environment to an internal network. Defined below.
+* `custom_dns_configuration` - (Optional) List of DNS server name and server IP. This is used to set up Route-53 outbound resolvers. Defined below.
+* `tags` - (Optional) List of key-value pairs to label the KX environment.
+
+### transit_gateway_configuration
+
+The transit_gateway_configuration block supports the following arguments:
+
+* `transit_gateway_id` - (Required) Identifier of the transit gateway created by the customer to connect outbound traffics from KX network to your internal network.
+* `routable_cidr_space` - (Required) Routing CIDR on behalf of KX environment. It could be any “/26 range in the 100.64.0.0 CIDR space. After providing, it will be added to the customer’s transit gateway routing table so that the traffics could be routed to KX network.
+
+### custom_dns_configuration
+
+The custom_dns_configuration block supports the following arguments:
+
+* `custom_dns_server_name` - (Required) Name of the DNS server.
+* `custom_dns_server_ip` - (Required) IP address of the DNS server.
+
+## Attributes Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `arn` - Amazon Resource Name (ARN) identifier of the KX environment.
+* `id` - Unique identifier for the KX environment.
+* `availability_zones` - AWS Availability Zone IDs that this environment is available in. Important when selecting VPC subnets to use in cluster creation.
+* `infrastructure_account_id` - Unique identifier for the AWS environment infrastructure account.
+* `created_timestamp` - Timestamp at which the environment is created in FinSpace. Value determined as epoch time in seconds. For example, the value for Monday, November 1, 2021 12:00:00 PM UTC is specified as 1635768000.
+* `last_modified_timestamp` - Last timestamp at which the environment was updated in FinSpace. Value determined as epoch time in seconds. For example, the value for Monday, November 1, 2021 12:00:00 PM UTC is specified as 1635768000.
+* `status` - Status of environment creation
+* `tags_all` - Map of tags assigned to the resource.
+
+## Timeouts
+
+[Configuration options](https://developer.hashicorp.com/terraform/language/resources/syntax#operation-timeouts):
+
+* `create` - (Default `30m`)
+* `update` - (Default `30m`)
+* `delete` - (Default `30m`)
+
+## Import
+
+FinSpace kdb environments can be imported using the `id`, e.g.,
+
+```
+$ terraform import aws_finspace_kx_environment.a n3ceo7wqxoxcti5tujqwzs
+```
