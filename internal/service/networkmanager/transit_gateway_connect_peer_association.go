ad024b6395f6aa78c62de690dd42891410377812
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-06-06 16:42:43 -0400

    Remove leading 'error ' from diag.Errorf("error ...") - networkmanager.

--- internal/service/networkmanager/transit_gateway_connect_peer_association.go
+++ internal/service/networkmanager/transit_gateway_connect_peer_association.go
@@ -80,13 +80,13 @@ func resourceTransitGatewayConnectPeerAssociationCreate(ctx context.Context, d *
 	_, err := conn.AssociateTransitGatewayConnectPeerWithContext(ctx, input)
 
 	if err != nil {
-		return diag.Errorf("error creating Network Manager Transit Gateway Connect Peer Association (%s): %s", id, err)
+		return diag.Errorf("creating Network Manager Transit Gateway Connect Peer Association (%s): %s", id, err)
 	}
 
 	d.SetId(id)
 
 	if _, err := waitTransitGatewayConnectPeerAssociationCreated(ctx, conn, globalNetworkID, connectPeerARN, d.Timeout(schema.TimeoutCreate)); err != nil {
-		return diag.Errorf("error waiting for Network Manager Transit Gateway Connect Peer Association (%s) create: %s", d.Id(), err)
+		return diag.Errorf("waiting for Network Manager Transit Gateway Connect Peer Association (%s) create: %s", d.Id(), err)
 	}
 
 	return resourceTransitGatewayConnectPeerAssociationRead(ctx, d, meta)
@@ -110,7 +110,7 @@ func resourceTransitGatewayConnectPeerAssociationRead(ctx context.Context, d *sc
 	}
 
 	if err != nil {
-		return diag.Errorf("error reading Network Manager Transit Gateway Connect Peer Association (%s): %s", d.Id(), err)
+		return diag.Errorf("reading Network Manager Transit Gateway Connect Peer Association (%s): %s", d.Id(), err)
 	}
 
 	d.Set("device_id", output.DeviceId)
