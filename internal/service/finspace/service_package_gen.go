ab6d36e271e0a898f85bc6524974f2744dfcb868
Kavan Bansal <bankavan@amazon.com>
2023-06-06 16:29:53 -0400

    Ran make servicepackages

--- internal/service/finspace/service_package_gen.go
+++ internal/service/finspace/service_package_gen.go
@@ -24,7 +24,15 @@ func (p *servicePackage) SDKDataSources(ctx context.Context) []*types.ServicePac
 }
 
 func (p *servicePackage) SDKResources(ctx context.Context) []*types.ServicePackageSDKResource {
-	return []*types.ServicePackageSDKResource{}
+	return []*types.ServicePackageSDKResource{
+		{
+			Factory:  ResourceKxDatabase,
+			TypeName: "aws_finspace_kx_database",
+			Tags: &types.ServicePackageResourceTags{
+				IdentifierAttribute: "arn",
+			},
+		},
+	}
 }
 
 func (p *servicePackage) ServicePackageName() string {
