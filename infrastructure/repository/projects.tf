a8977a9966980dd3fd295c029c6a0a328d7ac739
Justin Retzolk <44710313+justinretzolk@users.noreply.github.com>
2023-06-12 11:16:43 -0500

    Simplify infrastructure requirements

--- infrastructure/repository/projects.tf
+++ infrastructure/repository/projects.tf
@@ -1,22 +1,6 @@
 // This configuration file sets up Actions variables for use when automating GitHub Projects.
 // Currently, this consists only of the main team project proof of concept, but more may be added at a later date.
 
-// Main Team Project
-// -----------------
-
-// Project ID (vars.team_project in workflow files)
-resource "github_actions_variable" "team_project_id" {
-  repository    = "terraform-provider-aws"
-  variable_name = "team_project"
-  value         = "PVT_kwDOAAuecM4AF-7h"
-}
-
-// Project's "Status" field's ID (vars.team_project_field_status in workflow files)
-resource "github_actions_variable" "team_project_field_status_id" {
-  repository    = "terraform-provider-aws"
-  variable_name = "team_project_field_status"
-  value         = "PVTSSF_lADOAAuecM4AF-7hzgDcsQA"
-}
 
 // Project's "Status" field's options IDs (vars.team_project_status_${option_name_snake_case}
 // This set of variables will take the names of each possible value for the "Status" column, convert them to snake case
