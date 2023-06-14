8c5ee8e748a175921d2b0f44deb553033495b18e
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-06-14 16:18:12 -0400

    logs: Lowercase non-default tagging functions.

--- internal/service/logs/group.go
+++ internal/service/logs/group.go
@@ -135,7 +135,7 @@ func resourceGroupRead(ctx context.Context, d *schema.ResourceData, meta interfa
 	d.Set("name_prefix", create.NamePrefixFromName(aws.StringValue(lg.LogGroupName)))
 	d.Set("retention_in_days", lg.RetentionInDays)
 
-	tags, err := ListLogGroupTags(ctx, conn, d.Id())
+	tags, err := listLogGroupTags(ctx, conn, d.Id())
 
 	if err != nil {
 		return diag.Errorf("listing tags for CloudWatch Logs Log Group (%s): %s", d.Id(), err)
@@ -198,7 +198,7 @@ func resourceGroupUpdate(ctx context.Context, d *schema.ResourceData, meta inter
 	if d.HasChange("tags_all") {
 		o, n := d.GetChange("tags_all")
 
-		if err := UpdateLogGroupTags(ctx, conn, d.Id(), o, n); err != nil {
+		if err := updateLogGroupTags(ctx, conn, d.Id(), o, n); err != nil {
 			return diag.Errorf("updating CloudWatch Logs Log Group (%s) tags: %s", d.Id(), err)
 		}
 	}
