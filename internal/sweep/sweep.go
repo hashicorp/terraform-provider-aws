5d0771a92f3670757c014c52de63d232103ed893
Graham Davison <gdavison@hashicorp.com>
2023-06-15 15:46:51 -0700

    Splits SDK-related sweepers to separate file

--- internal/sweep/sweep.go
+++ internal/sweep/sweep.go
@@ -4,22 +4,16 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"log"
 	"net"
 	"os"
 	"strconv"
-	"strings"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws/endpoints"
 	"github.com/hashicorp/aws-sdk-go-base/v2/awsv1shim/v2/tfawserr"
 	multierror "github.com/hashicorp/go-multierror"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/envvar"
-	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 )
 
@@ -99,43 +93,6 @@ func SharedRegionalSweepClientWithContext(ctx context.Context, region string) (*
 
 type Sweepable interface {
 	Delete(ctx context.Context, timeout time.Duration, optFns ...tfresource.OptionsFunc) error
-}
-
-type SweepResource struct {
-	d        *schema.ResourceData
-	meta     *conns.AWSClient
-	resource *schema.Resource
-}
-
-func NewSweepResource(resource *schema.Resource, d *schema.ResourceData, meta *conns.AWSClient) *SweepResource {
-	return &SweepResource{
-		d:        d,
-		meta:     meta,
-		resource: resource,
-	}
-}
-
-func (sr *SweepResource) Delete(ctx context.Context, timeout time.Duration, optFns ...tfresource.OptionsFunc) error {
-	err := tfresource.Retry(ctx, timeout, func() *retry.RetryError {
-		err := DeleteResource(ctx, sr.resource, sr.d, sr.meta)
-
-		if err != nil {
-			if strings.Contains(err.Error(), "Throttling") {
-				log.Printf("[INFO] While sweeping resource (%s), encountered throttling error (%s). Retrying...", sr.d.Id(), err)
-				return retry.RetryableError(err)
-			}
-
-			return retry.NonRetryableError(err)
-		}
-
-		return nil
-	}, optFns...)
-
-	if tfresource.TimedOut(err) {
-		err = DeleteResource(ctx, sr.resource, sr.d, sr.meta)
-	}
-
-	return err
 }
 
 func SweepOrchestratorWithContext(ctx context.Context, sweepables []Sweepable, optFns ...tfresource.OptionsFunc) error {
@@ -226,38 +183,6 @@ func SkipSweepError(err error) bool {
 		return dnsErr.IsNotFound
 	}
 	return false
-}
-
-func DeleteResource(ctx context.Context, resource *schema.Resource, d *schema.ResourceData, meta any) error {
-	if resource.DeleteContext != nil || resource.DeleteWithoutTimeout != nil {
-		var diags diag.Diagnostics
-
-		if resource.DeleteContext != nil {
-			diags = resource.DeleteContext(ctx, d, meta)
-		} else {
-			diags = resource.DeleteWithoutTimeout(ctx, d, meta)
-		}
-
-		return sdkdiag.DiagnosticsError(diags)
-	}
-
-	return resource.Delete(d, meta)
-}
-
-func ReadResource(ctx context.Context, resource *schema.Resource, d *schema.ResourceData, meta any) error {
-	if resource.ReadContext != nil || resource.ReadWithoutTimeout != nil {
-		var diags diag.Diagnostics
-
-		if resource.ReadContext != nil {
-			diags = resource.ReadContext(ctx, d, meta)
-		} else {
-			diags = resource.ReadWithoutTimeout(ctx, d, meta)
-		}
-
-		return sdkdiag.DiagnosticsError(diags)
-	}
-
-	return resource.Read(d, meta)
 }
 
 func Partition(region string) string {
