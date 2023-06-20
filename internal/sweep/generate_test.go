f6ab467c588c99e595195427612612f766a6c244
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-06-20 16:47:47 -0400

    internal/sweep: Generate list of service packages.

---
+++ internal/sweep/generate_test.go
@@ -0,0 +1,4 @@
+//go:generate go run ../generate/servicepackages/main.go -- service_packages_gen_test.go
+// ONLY generate directives and package declaration! Do not add anything else to this file.
+
+package sweep_test
