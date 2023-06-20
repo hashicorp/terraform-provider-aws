4b6e3fbcb869c6ab269d1fdea7c07e5fd5b1457c
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-06-20 09:50:34 -0400

    Modify per-service 'generate.go's to generate 'service_package_gen.go'.

--- internal/service/comprehend/generate.go
+++ internal/service/comprehend/generate.go
@@ -1,6 +1,7 @@
 //go:generate go run ../../generate/tags/main.go -ServiceTagsSlice -ListTags -UpdateTags -AWSSDKVersion=2
 //go:generate go run ./test-fixtures/generate/document_classifier/main.go
 //go:generate go run ./test-fixtures/generate/entity_recognizer/main.go
+//go:generate go run ../../generate/servicepackage/main.go
 // ONLY generate directives and package declaration! Do not add anything else to this file.
 
 package comprehend
