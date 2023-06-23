cb00d3f1d566febe5c294002017352c934bdc5d3
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-06-23 10:53:32 -0400

    Run 'make gen'.

--- internal/service/s3control/service_package_gen.go
+++ internal/service/s3control/service_package_gen.go
@@ -10,6 +10,7 @@ import (
 	aws_sdkv1 "github.com/aws/aws-sdk-go/aws"
 	session_sdkv1 "github.com/aws/aws-sdk-go/aws/session"
 	s3control_sdkv1 "github.com/aws/aws-sdk-go/service/s3control"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/types"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
@@ -112,4 +113,6 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	}), nil
 }
 
-var ServicePackage = &servicePackage{}
+func ServicePackage(ctx context.Context) conns.ServicePackage {
+	return &servicePackage{}
+}
