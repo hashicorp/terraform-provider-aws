08b5346b9b8130c9eba9c8b487fe0fe1bda56801
Fabian Ponce <fponce@toasttab.com>
2023-06-23 08:56:29 -0400

    fmt

--- internal/service/ec2/vpc_nat_gateway_test.go
+++ internal/service/ec2/vpc_nat_gateway_test.go
@@ -383,8 +383,9 @@ resource "aws_nat_gateway" "test" {
   connectivity_type = "private"
   private_ip        = "10.0.0.8"
   subnet_id         = aws_subnet.test[0].id
-
-  secondary_private_ips = [ %[2]q ]
+  secondary_private_ips = [ 
+    %[2]q
+  ]
 
   tags = {
     Name = %[1]q
