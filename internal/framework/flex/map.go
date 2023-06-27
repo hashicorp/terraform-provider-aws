65ed2283b1a18a92892740b684d18ce5fedd89fa
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-06-27 15:54:46 -0400

    Add 'flex.ExpandFrameworkStringMap'.

--- internal/framework/flex/map.go
+++ internal/framework/flex/map.go
@@ -7,6 +7,34 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
+func ExpandFrameworkStringMap(ctx context.Context, set types.Map) map[string]*string {
+	if set.IsNull() || set.IsUnknown() {
+		return nil
+	}
+
+	var m map[string]*string
+
+	if set.ElementsAs(ctx, &m, false).HasError() {
+		return nil
+	}
+
+	return m
+}
+
+func ExpandFrameworkStringValueMap(ctx context.Context, set types.Map) map[string]string {
+	if set.IsNull() || set.IsUnknown() {
+		return nil
+	}
+
+	var m map[string]string
+
+	if set.ElementsAs(ctx, &m, false).HasError() {
+		return nil
+	}
+
+	return m
+}
+
 // FlattenFrameworkStringValueMapLegacy has no Plugin SDK equivalent as schema.ResourceData.Set can be passed string value maps directly.
 // A nil map is converted to an empty (non-null) Map.
 func FlattenFrameworkStringValueMapLegacy(_ context.Context, m map[string]string) types.Map {
--- internal/framework/flex/map_test.go
+++ internal/framework/flex/map_test.go
@@ -4,11 +4,64 @@ import (
 	"context"
 	"testing"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/google/go-cmp/cmp"
 	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-provider-aws/internal/framework/flex"
 )
+
+func TestExpandFrameworkStringMap(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		input    types.Map
+		expected map[string]*string
+	}
+	tests := map[string]testCase{
+		"null": {
+			input:    types.MapNull(types.StringType),
+			expected: nil,
+		},
+		"unknown": {
+			input:    types.MapUnknown(types.StringType),
+			expected: nil,
+		},
+		"two elements": {
+			input: types.MapValueMust(types.StringType, map[string]attr.Value{
+				"one": types.StringValue("GET"),
+				"two": types.StringValue("HEAD"),
+			}),
+			expected: aws.StringMap(map[string]string{
+				"one": "GET",
+				"two": "HEAD",
+			}),
+		},
+		"zero elements": {
+			input:    types.MapValueMust(types.StringType, map[string]attr.Value{}),
+			expected: aws.StringMap(map[string]string{}),
+		},
+		"invalid element type": {
+			input: types.MapValueMust(types.BoolType, map[string]attr.Value{
+				"one": types.BoolValue(true),
+			}),
+			expected: nil,
+		},
+	}
+
+	for name, test := range tests {
+		name, test := name, test
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := flex.ExpandFrameworkStringMap(context.Background(), test.input)
+
+			if diff := cmp.Diff(got, test.expected); diff != "" {
+				t.Errorf("unexpected diff (+wanted, -got): %s", diff)
+			}
+		})
+	}
+}
 
 func TestExpandFrameworkStringValueMap(t *testing.T) {
 	t.Parallel()
