d766e505c6126aaa27f34423ae8bdaea53607872
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-07-03 11:04:03 -0400

    r/aws_timestreamwrite_database: Migrate to AWS SDK for Go v2.

---
+++ internal/service/timestreamwrite/export_test.go
@@ -0,0 +1,8 @@
+package timestreamwrite
+
+// Exports for use in tests only.
+var (
+	ResourceDatabase = resourceDatabase
+
+	FindDatabaseByName = findDatabaseByName
+)
--- internal/service/timestreamwrite/service_package_gen.go
+++ internal/service/timestreamwrite/service_package_gen.go
@@ -29,7 +29,7 @@ func (p *servicePackage) SDKDataSources(ctx context.Context) []*types.ServicePac
 func (p *servicePackage) SDKResources(ctx context.Context) []*types.ServicePackageSDKResource {
 	return []*types.ServicePackageSDKResource{
 		{
-			Factory:  ResourceDatabase,
+			Factory:  resourceDatabase,
 			TypeName: "aws_timestreamwrite_database",
 			Name:     "Database",
 			Tags: &types.ServicePackageResourceTags{
