48337faed244a7d1b0e61e44f2b0f6c0a3ae6688
Lanre Afodunrinbi <afod@amazon.com>
2023-07-10 10:35:58 +0100

    implemented new resource aws opensearch vpc endpoint connection

---
+++ internal/service/opensearch/vpc_endpoint_connection.go
@@ -0,0 +1,264 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package opensearch
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/opensearchservice"
+	"github.com/hashicorp/aws-sdk-go-base/v2/awsv1shim/v2/tfawserr"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
+)
+
+// @SDKResource("aws_opensearch_vpc_endpoint_connection")
+func ResourceVPCEndpoint() *schema.Resource {
+	return &schema.Resource{
+		CreateWithoutTimeout: resourceVPCEndpointCreate,
+		ReadWithoutTimeout:   resourceVPCEndpointRead,
+		UpdateWithoutTimeout: resourceVPCEndpointPut,
+		DeleteWithoutTimeout: resourceVPCEndpointDelete,
+
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(60 * time.Minute),
+			Update: schema.DefaultTimeout(60 * time.Minute),
+			Delete: schema.DefaultTimeout(90 * time.Minute),
+		},
+
+		Schema: map[string]*schema.Schema{
+			"domain_arn": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: verify.ValidARN,
+			},
+			"vpc_options": {
+				Type:     schema.TypeList,
+				Optional: true,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"availability_zones": {
+							Type:     schema.TypeSet,
+							Computed: true,
+							Elem:     &schema.Schema{Type: schema.TypeString},
+							Set:      schema.HashString,
+						},
+						"security_group_ids": {
+							Type:     schema.TypeSet,
+							Optional: true,
+							Elem:     &schema.Schema{Type: schema.TypeString},
+							Set:      schema.HashString,
+						},
+						"subnet_ids": {
+							Type:     schema.TypeSet,
+							Optional: true,
+							Elem:     &schema.Schema{Type: schema.TypeString},
+							Set:      schema.HashString,
+						},
+						"vpc_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"connection_status": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceVPCEndpointCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	conn := meta.(*conns.AWSClient).OpenSearchConn(ctx)
+
+	// Create the VPC Endpoint
+	input := &opensearchservice.CreateVpcEndpointInput{
+		DomainArn: aws.String(d.Get("domain_arn").(string)),
+	}
+
+	if v, ok := d.GetOk("vpc_options"); ok {
+		options := v.([]interface{})
+		if options[0] == nil {
+			return sdkdiag.AppendErrorf(diags, "At least one field is expected inside vpc_options")
+		}
+
+		s := options[0].(map[string]interface{})
+		input.VpcOptions = expandVPCOptions(s)
+	}
+
+	log.Printf("[DEBUG] Create VPC Endpoint options: %#v", input)
+
+	resp, err := conn.CreateVpcEndpointWithContext(ctx, input)
+	if err != nil {
+		return diag.Errorf("creating vpc endpoint : %s", err)
+	}
+
+	// Get the ID and store it
+	d.SetId(aws.StringValue(resp.VpcEndpoint.VpcEndpointId))
+	log.Printf("[INFO] open search vpc endpoint ID: %s", d.Id())
+
+	err = vpcEndpointConnectionWaitUntilActive(ctx, conn, d.Id(), d.Timeout(schema.TimeoutCreate))
+	if err != nil {
+		return diag.Errorf("waiting for vpc endpoint to become active: %s", err)
+	}
+
+	return append(diags, resourceVPCEndpointRead(ctx, d, meta)...)
+}
+
+func resourceVPCEndpointRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn := meta.(*conns.AWSClient).OpenSearchConn(ctx)
+
+	endpointRaw, status, err := vpcEndpointConnectionRefreshState(ctx, conn, d.Id())()
+
+	if err != nil {
+		return diag.Errorf("reading vpc endpoint Connection: %s", err)
+	}
+
+	endpoint := endpointRaw.(*opensearchservice.VpcEndpoint)
+	log.Printf("[DEBUG] vpc endpoint Connection response: %#v", endpoint)
+
+	d.Set("connection_status", status)
+	d.Set("domain_arn", endpoint.DomainArn)
+
+	if endpoint.VpcOptions == nil {
+		return diag.Errorf("reading vpc endpoint Connection vpc options ")
+	}
+
+	d.Set("vpc_options", flattenVPCDerivedInfo(endpoint.VpcOptions))
+	return nil
+}
+
+func resourceVPCEndpointPut(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	conn := meta.(*conns.AWSClient).OpenSearchConn(ctx)
+
+	// Update the VPC Endpoint
+	input := &opensearchservice.UpdateVpcEndpointInput{
+		VpcEndpointId: aws.String(d.Id()),
+	}
+
+	if v, ok := d.GetOk("vpc_options"); ok {
+		options := v.([]interface{})
+		if options[0] == nil {
+			return sdkdiag.AppendErrorf(diags, "At least one field is expected inside vpc_options")
+		}
+
+		s := options[0].(map[string]interface{})
+		input.VpcOptions = expandVPCOptions(s)
+	}
+
+	log.Printf("[DEBUG] Updating vpc endpoint Connection %s", input)
+
+	_, err := conn.UpdateVpcEndpointWithContext(ctx, input)
+
+	if err != nil {
+		return sdkdiag.AppendErrorf(diags, "updating vpc endpoint Connection (%s): %s", d.Id(), err)
+	}
+
+	err = vpcEndpointConnectionWaitUntilUpdate(ctx, conn, d.Id(), d.Timeout(schema.TimeoutUpdate))
+	if err != nil {
+		return diag.Errorf("waiting for vpc endpoint to become active: %s", err)
+	}
+
+	return append(diags, resourceVPCEndpointRead(ctx, d, meta)...)
+
+}
+func resourceVPCEndpointDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn := meta.(*conns.AWSClient).OpenSearchConn(ctx)
+
+	req := &opensearchservice.DeleteVpcEndpointInput{
+		VpcEndpointId: aws.String(d.Id()),
+	}
+
+	_, err := conn.DeleteVpcEndpointWithContext(ctx, req)
+
+	if tfawserr.ErrCodeEquals(err, "ResourceNotFoundException") {
+		return nil
+	}
+
+	if err != nil {
+		return diag.Errorf("deleting vpc endpoint Connection (%s): %s", d.Id(), err)
+	}
+
+	return nil
+}
+
+func vpcEndpointConnectionRefreshState(ctx context.Context, conn *opensearchservice.OpenSearchService, id string) retry.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		resp, err := conn.DescribeVpcEndpointsWithContext(ctx, &opensearchservice.DescribeVpcEndpointsInput{
+			VpcEndpointIds: []*string{aws.String(id)},
+		})
+		if err != nil {
+			return nil, "", err
+		}
+
+		if resp == nil || resp.VpcEndpoints == nil ||
+			len(resp.VpcEndpoints) == 0 || resp.VpcEndpoints[0] == nil {
+			// Sometimes AWS just has consistency issues and doesn't see
+			// our connection yet. Return an empty state.
+			return nil, "", nil
+		}
+		endpoint := resp.VpcEndpoints[0]
+		if endpoint.Status == nil {
+			// Sometimes AWS just has consistency issues and doesn't see
+			// our connection yet. Return an empty state.
+			return nil, "", nil
+		}
+		statusCode := aws.StringValue(endpoint.Status)
+
+		return endpoint, statusCode, nil
+	}
+}
+
+func vpcEndpointConnectionWaitUntilActive(ctx context.Context, conn *opensearchservice.OpenSearchService, id string, timeout time.Duration) error {
+	log.Printf("[DEBUG] Waiting for VPC Endpoint Connection (%s) to become available.", id)
+	stateConf := &retry.StateChangeConf{
+		Pending: []string{
+			opensearchservice.VpcEndpointStatusCreating,
+		},
+		Target: []string{
+			opensearchservice.VpcEndpointStatusActive,
+		},
+		Refresh: vpcEndpointConnectionRefreshState(ctx, conn, id),
+		Timeout: timeout,
+	}
+	if _, err := stateConf.WaitForStateContext(ctx); err != nil {
+		return fmt.Errorf("waiting for VPC Endpoint  Connection (%s) to become available: %s", id, err)
+	}
+	return nil
+}
+
+func vpcEndpointConnectionWaitUntilUpdate(ctx context.Context, conn *opensearchservice.OpenSearchService, id string, timeout time.Duration) error {
+	log.Printf("[DEBUG] Waiting for VPC Endpoint Connection (%s) to become available.", id)
+	stateConf := &retry.StateChangeConf{
+		Pending: []string{
+			opensearchservice.VpcEndpointStatusUpdating,
+		},
+		Target: []string{
+			opensearchservice.VpcEndpointStatusActive,
+		},
+		Refresh: vpcEndpointConnectionRefreshState(ctx, conn, id),
+		Timeout: timeout,
+	}
+	if _, err := stateConf.WaitForStateContext(ctx); err != nil {
+		return fmt.Errorf("waiting for VPC Endpoint  Connection (%s) to become available: %s", id, err)
+	}
+	return nil
+}
---
+++ internal/service/opensearch/vpc_endpoint_connection_test.go
@@ -0,0 +1,256 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package opensearch_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/service/opensearchservice"
+	sdkacctest "github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+	tfopensearch "github.com/hashicorp/terraform-provider-aws/internal/service/opensearch"
+)
+
+func TestAccOpenSearchVPCEndpointConnection_basic(t *testing.T) {
+	ctx := acctest.Context(t)
+	var domain opensearchservice.DomainStatus
+	ri := sdkacctest.RandString(10)
+	name := fmt.Sprintf("tf-test-%s", ri)
+	resourceName := "aws_opensearch_vpc_endpoint_connection.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
+		ErrorCheck:               acctest.ErrorCheck(t, opensearchservice.EndpointsID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckDomainDestroy(ctx),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccVPCEndpointConnectionConfig(name),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDomainExists(ctx, "aws_opensearch_domain.domain_1", &domain),
+					resource.TestCheckResourceAttr(resourceName, "connection_status", "ACTIVE"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccOpenSearchVPCEndpointConnection_update(t *testing.T) {
+	ctx := acctest.Context(t)
+	var domain opensearchservice.DomainStatus
+	ri := sdkacctest.RandString(10)
+	name := fmt.Sprintf("tf-test-%s", ri)
+	resourceName := "aws_opensearch_vpc_endpoint_connection.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
+		ErrorCheck:               acctest.ErrorCheck(t, opensearchservice.EndpointsID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckDomainDestroy(ctx),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccVPCEndpointConnectionConfig(name),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDomainExists(ctx, "aws_opensearch_domain.domain_1", &domain),
+					resource.TestCheckResourceAttr(resourceName, "vpc_options.security_group_ids.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "connection_status", "ACTIVE"),
+				),
+			},
+			{
+				Config: testAccVPCEndpointConnectionConfigUpdate(name),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDomainExists(ctx, "aws_opensearch_domain.domain_1", &domain),
+					resource.TestCheckResourceAttr(resourceName, "vpc_options.security_group_ids.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "connection_status", "ACTIVE"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccOpenSearchVPCEndpointConnection_disappears(t *testing.T) {
+	ctx := acctest.Context(t)
+	var domain opensearchservice.DomainStatus
+	ri := sdkacctest.RandString(10)
+	name := fmt.Sprintf("tf-test-%s", ri)
+	resourceName := "aws_opensearch_vpc_endpoint_connection.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
+		ErrorCheck:               acctest.ErrorCheck(t, opensearchservice.EndpointsID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckDomainDestroy(ctx),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccVPCEndpointConnectionConfig(name),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDomainExists(ctx, "aws_opensearch_domain.domain_1", &domain),
+					acctest.CheckResourceDisappears(ctx, acctest.Provider, tfopensearch.ResourceVPCEndpoint(), resourceName),
+				),
+			},
+		},
+	})
+}
+
+func testAccVPCEndpointConnectionConfig(name string) string {
+	// Satisfy the pw requirements
+	//pw := fmt.Sprintf("Aa1-%s", sdkacctest.RandString(10))
+
+	return acctest.ConfigCompose(
+		acctest.ConfigAvailableAZsNoOptIn(),
+		fmt.Sprintf(`
+resource "aws_vpc" "test" {
+  cidr_block = "192.168.0.0/22"
+
+  tags = {
+    Name = %[1]q
+  }
+}
+
+resource "aws_subnet" "test" {
+  vpc_id            = aws_vpc.test.id
+  availability_zone = data.aws_availability_zones.available.names[0]
+  cidr_block        = "192.168.0.0/24"
+
+  tags = {
+    Name = %[1]q
+  }
+}
+
+resource "aws_subnet" "test2" {
+  vpc_id            = aws_vpc.test.id
+  availability_zone = data.aws_availability_zones.available.names[1]
+  cidr_block        = "192.168.1.0/24"
+
+  tags = {
+    Name = %[1]q
+  }
+}
+
+resource "aws_security_group" "test" {
+  vpc_id = aws_vpc.test.id
+}
+
+resource "aws_security_group" "test2" {
+  vpc_id = aws_vpc.test.id
+}
+
+resource "aws_opensearch_domain" "domain_1" {
+  domain_name = %[1]q
+
+  ebs_options {
+    ebs_enabled = true
+    volume_size = 10
+  }
+
+  cluster_config {
+    instance_count         = 2
+    zone_awareness_enabled = true
+    instance_type          = "t3.small.search"
+  }
+
+  vpc_options {
+    security_group_ids = [aws_security_group.test.id, aws_security_group.test2.id]
+    subnet_ids         = [aws_subnet.test.id, aws_subnet.test2.id]
+  }
+}
+
+resource "aws_opensearch_vpc_endpoint_connection" "test" {
+	domain_arn = aws_opensearch_domain.domain_1.arn
+	vpc_options {
+	  security_group_ids = [aws_security_group.test.id]
+	  subnet_ids         = [aws_subnet.test.id, aws_subnet.test2.id]
+	}
+  
+  }
+`, name))
+
+}
+
+func testAccVPCEndpointConnectionConfigUpdate(name string) string {
+	// Satisfy the pw requirements
+	//pw := fmt.Sprintf("Aa1-%s", sdkacctest.RandString(10))
+
+	return acctest.ConfigCompose(
+		acctest.ConfigAvailableAZsNoOptIn(),
+		fmt.Sprintf(`
+resource "aws_vpc" "test" {
+  cidr_block = "192.168.0.0/22"
+
+  tags = {
+    Name = %[1]q
+  }
+}
+
+resource "aws_subnet" "test" {
+  vpc_id            = aws_vpc.test.id
+  availability_zone = data.aws_availability_zones.available.names[0]
+  cidr_block        = "192.168.0.0/24"
+
+  tags = {
+    Name = %[1]q
+  }
+}
+
+resource "aws_subnet" "test2" {
+  vpc_id            = aws_vpc.test.id
+  availability_zone = data.aws_availability_zones.available.names[1]
+  cidr_block        = "192.168.1.0/24"
+
+  tags = {
+    Name = %[1]q
+  }
+}
+
+resource "aws_security_group" "test" {
+  vpc_id = aws_vpc.test.id
+}
+
+resource "aws_security_group" "test2" {
+  vpc_id = aws_vpc.test.id
+}
+
+resource "aws_opensearch_domain" "domain_1" {
+  domain_name = %[1]q
+
+  ebs_options {
+    ebs_enabled = true
+    volume_size = 10
+  }
+
+  cluster_config {
+    instance_count         = 2
+    zone_awareness_enabled = true
+    instance_type          = "t3.small.search"
+  }
+
+  vpc_options {
+    security_group_ids = [aws_security_group.test.id, aws_security_group.test2.id]
+    subnet_ids         = [aws_subnet.test.id, aws_subnet.test2.id]
+  }
+}
+
+resource "aws_opensearch_vpc_endpoint_connection" "test" {
+	domain_arn = aws_opensearch_domain.domain_1.arn
+	vpc_options {
+	  security_group_ids = [aws_security_group.test.id, aws_security_group.test2.id]
+	  subnet_ids         = [aws_subnet.test.id, aws_subnet.test2.id]
+	}
+  
+  }
+`, name))
+
+}
