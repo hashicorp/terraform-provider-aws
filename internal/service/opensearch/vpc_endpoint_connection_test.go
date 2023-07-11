d1243c3c819bfd82395fc8d2ed89cf9af5f9f041
Debojit Bhadra <debojit.bhadra@gmail.com>
2023-07-11 13:11:25 +0100

    refactor: updated test.go

--- internal/service/opensearch/vpc_endpoint_connection_test.go
+++ internal/service/opensearch/vpc_endpoint_connection_test.go
@@ -106,9 +106,7 @@ func TestAccOpenSearchVPCEndpointConnection_disappears(t *testing.T) {
 }
 
 func testAccVPCEndpointConnectionConfig(name string) string {
-	return acctest.ConfigCompose(
-		acctest.ConfigAvailableAZsNoOptIn(),
-		fmt.Sprintf(`
+	return acctest.ConfigCompose(acctest.ConfigAvailableAZsNoOptIn(), fmt.Sprintf(`
 resource "aws_vpc" "test" {
   cidr_block = "192.168.0.0/22"
 
@@ -145,7 +143,7 @@ resource "aws_security_group" "test2" {
   vpc_id = aws_vpc.test.id
 }
 
-resource "aws_opensearch_domain" "test" {
+resource "aws_opensearch_domain" "domain_1" {
   domain_name = %[1]q
 
   ebs_options {
@@ -156,93 +154,90 @@ resource "aws_opensearch_domain" "test" {
   cluster_config {
     instance_count         = 2
     zone_awareness_enabled = true
-    instance_type          = "t2.small.search"
+    instance_type          = "t3.small.search"
   }
 
   vpc_options {
     security_group_ids = [aws_security_group.test.id, aws_security_group.test2.id]
     subnet_ids         = [aws_subnet.test.id, aws_subnet.test2.id]
   }
-}
+  }
 
-resource "aws_opensearch_vpc_endpoint_connection" "foo" {
-	domain_arn = aws_opensearch_domain.domain_1.arn
-	vpc_options {
-	  security_group_ids = [aws_security_group.test.id]
-	  subnet_ids         = [aws_subnet.test.id, aws_subnet.test2.id]
-	}
+  resource "aws_opensearch_vpc_endpoint_connection" "foo" {
+  domain_arn = aws_opensearch_domain.domain_1.arn
+  vpc_options {
+    security_group_ids = [aws_security_group.test.id]
+    subnet_ids         = [aws_subnet.test.id, aws_subnet.test2.id]
+  }
 }
 `, name))
 
 }
 
 func testAccVPCEndpointConnectionConfigUpdate(name string) string {
-	return acctest.ConfigCompose(
-		acctest.ConfigAvailableAZsNoOptIn(),
-		fmt.Sprintf(`
-		  resource "aws_vpc" "test" {
-			cidr_block = "192.168.0.0/22"
+	return acctest.ConfigCompose(acctest.ConfigAvailableAZsNoOptIn(), fmt.Sprintf(`
+resource "aws_vpc" "test" {
+	cidr_block = "192.168.0.0/22"
+	
+	tags = {
+	  Name = %[1]q
+	}
+}
 		  
-			tags = {
-			  Name = %[1]q
-			}
-		  }
+resource "aws_subnet" "test" {
+	vpc_id            = aws_vpc.test.id
+	availability_zone = data.aws_availability_zones.available.names[0]
+	cidr_block        = "192.168.0.0/24"
+	
+	tags = {
+	  Name = %[1]q
+	}
+}
 		  
-		  resource "aws_subnet" "test" {
-			vpc_id            = aws_vpc.test.id
-			availability_zone = data.aws_availability_zones.available.names[0]
-			cidr_block        = "192.168.0.0/24"
+resource "aws_subnet" "test2" {
+	vpc_id            = aws_vpc.test.id
+	availability_zone = data.aws_availability_zones.available.names[1]
+	cidr_block        = "192.168.1.0/24"
+	
+	tags = {
+	  Name = %[1]q
+	}
+}
 		  
-			tags = {
-			  Name = %[1]q
-			}
-		  }
+resource "aws_security_group" "test" {
+	vpc_id = aws_vpc.test.id
+}
 		  
-		  resource "aws_subnet" "test2" {
-			vpc_id            = aws_vpc.test.id
-			availability_zone = data.aws_availability_zones.available.names[1]
-			cidr_block        = "192.168.1.0/24"
+resource "aws_security_group" "test2" {
+ vpc_id = aws_vpc.test.id
+}
 		  
-			tags = {
-			  Name = %[1]q
-			}
-		  }
+resource "aws_opensearch_domain" "domain_1" {
+	domain_name = %[1]q
+	
+	ebs_options {
+	  ebs_enabled = true
+	  volume_size = 10
+	}
+	
+	cluster_config {
+	  instance_count         = 2
+	  zone_awareness_enabled = true
+	  instance_type          = "t3.small.search"
+	}
+	
+	vpc_options {
+	  security_group_ids = [aws_security_group.test.id, aws_security_group.test2.id]
+	  subnet_ids         = [aws_subnet.test.id, aws_subnet.test2.id]
+	}
+}
 		  
-		  resource "aws_security_group" "test" {
-			vpc_id = aws_vpc.test.id
-		  }
-		  
-		  resource "aws_security_group" "test2" {
-			vpc_id = aws_vpc.test.id
-		  }
-		  
-		  resource "aws_opensearch_domain" "domain_1" {
-			domain_name = %[1]q
-		  
-			ebs_options {
-			  ebs_enabled = true
-			  volume_size = 10
-			}
-		  
-			cluster_config {
-			  instance_count         = 2
-			  zone_awareness_enabled = true
-			  instance_type          = "t3.small.search"
-			}
-		  
-			vpc_options {
-			  security_group_ids = [aws_security_group.test.id, aws_security_group.test2.id]
-			  subnet_ids         = [aws_subnet.test.id, aws_subnet.test2.id]
-			}
-		  }
-		  
-		  resource "aws_opensearch_vpc_endpoint_connection" "foo" {
-			domain_arn = aws_opensearch_domain.domain_1.arn
-			vpc_options {
-			  security_group_ids = [aws_security_group.test.id, aws_security_group.test2.id]
-			  subnet_ids         = [aws_subnet.test.id, aws_subnet.test2.id]
-			}
-		  }		  
-		`, name))
-
+resource "aws_opensearch_vpc_endpoint_connection" "foo" {
+	domain_arn = aws_opensearch_domain.domain_1.arn
+	vpc_options {
+	  security_group_ids = [aws_security_group.test.id, aws_security_group.test2.id]
+	  subnet_ids         = [aws_subnet.test.id, aws_subnet.test2.id]
+	}
+}		  
+`, name))
 }
