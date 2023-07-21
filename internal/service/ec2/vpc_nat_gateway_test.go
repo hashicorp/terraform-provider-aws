2069005a6da85b34c8368609564ca131dac8d14e
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-07-21 08:12:19 -0400

    Revert "lint"
    This reverts commit efa6796dbfb607c6ebacf85ec46360f27cd7fb37.

--- internal/service/ec2/vpc_nat_gateway_test.go
+++ internal/service/ec2/vpc_nat_gateway_test.go
@@ -383,7 +383,7 @@ resource "aws_nat_gateway" "test" {
   connectivity_type = "private"
   private_ip        = "10.0.0.8"
   subnet_id         = aws_subnet.test[0].id
-  secondary_private_ips = [
+  secondary_private_ips = [ 
     %[2]q
   ]
 
