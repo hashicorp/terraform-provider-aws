bac36791315e98d4352d47f5f2e278bb8f8644c1
Justin Retzolk <44710313+justinretzolk@users.noreply.github.com>
2023-07-24 12:49:36 -0500

    Update projects Terraform config for View field

--- infrastructure/repository/projects.tf
+++ infrastructure/repository/projects.tf
@@ -22,9 +22,28 @@ variable "team_project_field_status_values" {
   }
 }
 
+variable "team_project_field_view_values" {
+  type        = map(string)
+  description = "A mapping of the options in the view field in the main team project to their IDs"
+  default = {
+    "Working Board"          = "8d366764",
+    "Parner Contributions"   = "b0492564",
+    "External Maintainer"    = "f17c472c",
+    "S3 Backend"             = "5c656f1e",
+    "Engineering Initiative" = "a62d09b9"
+  }
+}
+
 resource "github_actions_variable" "team_project_field_status_option_ids" {
   for_each      = var.team_project_field_status_values
   repository    = "terraform-provider-aws"
   variable_name = "team_project_status_${replace(lower(each.key), " ", "_")}"
+  value         = each.value
+}
+
+resource "github_actions_variable" "team_project_field_view_option_ids" {
+  for_each      = var.team_project_field_view_values
+  repository    = "terraform-provider-aws"
+  variable_name = "team_project_view_${replace(lower(each.key), " ", "_")}"
   value         = each.value
 }
