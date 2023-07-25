e97dfac5e7f15f3bd0bb45799e7e1e8003c4e5f3
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-07-25 14:05:59 -0400

    r/aws_iam_security_token_service_preferences: No-op resource Delete and correct resource Read.

--- internal/service/iam/security_token_service_preference.go
+++ internal/service/iam/security_token_service_preference.go
@@ -3,29 +3,29 @@ package iam
 import (
 	"context"
 	"fmt"
-	"log"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/iam"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
 )
 
 // @SDKResource("aws_iam_security_token_service_preferences", name="Security Token Service Preferences")
-// @Tags
 func ResourceSecurityTokenServicePreferences() *schema.Resource {
 	return &schema.Resource{
 		CreateWithoutTimeout: resourceSecurityTokenServicePreferencesUpsert,
 		ReadWithoutTimeout:   resourceSecurityTokenServicePreferencesRead,
 		UpdateWithoutTimeout: resourceSecurityTokenServicePreferencesUpsert,
-		DeleteWithoutTimeout: resourceSecurityTokenServicePreferencesDelete,
+		DeleteWithoutTimeout: schema.NoopContext,
 
 		Schema: map[string]*schema.Schema{
 			"global_endpoint_token_version": {
-				Type:     schema.TypeString,
-				Required: true,
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validation.StringInSlice(iam.GlobalEndpointTokenVersion_Values(), false),
 			},
 		},
 	}
@@ -45,36 +45,24 @@ func resourceSecurityTokenServicePreferencesUpsert(ctx context.Context, d *schem
 		return sdkdiag.AppendErrorf(diags, "setting IAM Security Token Service Preferences: %s", err)
 	}
 
-	d.SetId(fmt.Sprintf("iam-security-token-service-preferences.%s", *input.GlobalEndpointTokenVersion))
+	if d.IsNewResource() {
+		d.SetId(meta.(*conns.AWSClient).AccountID)
+	}
 
 	return append(diags, resourceSecurityTokenServicePreferencesRead(ctx, d, meta)...)
 }
 
 func resourceSecurityTokenServicePreferencesRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
-
-	d.Set("id", d.Id())
-
-	return diags
-}
-
-func resourceSecurityTokenServicePreferencesDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	var diags diag.Diagnostics
 	conn := meta.(*conns.AWSClient).IAMConn(ctx)
 
-	log.Printf("[INFO] Deleting IAM Instance Profile: %s", d.Id())
-
-	input := &iam.SetSecurityTokenServicePreferencesInput{
-		GlobalEndpointTokenVersion: aws.String("v1Token"),
-	}
-
-	_, err := conn.SetSecurityTokenServicePreferencesWithContext(ctx, input)
+	output, err := conn.GetAccountSummaryWithContext(ctx, &iam.GetAccountSummaryInput{})
 
 	if err != nil {
-		return sdkdiag.AppendErrorf(diags, "setting IAM Security Token Service Preferences: %s", err)
+		return sdkdiag.AppendErrorf(diags, "reading IAM Account Summary: %s", err)
 	}
 
-	d.SetId("")
+	d.Set("global_endpoint_token_version", fmt.Sprintf("v%dToken", aws.Int64Value(output.SummaryMap[iam.SummaryKeyTypeGlobalEndpointTokenVersion])))
 
 	return diags
 }
