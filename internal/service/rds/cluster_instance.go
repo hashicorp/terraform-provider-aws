317f7b098f7878b4088d636188ca5c8237c72b4e
Tyler Lynch <awstyler@amazon.com>
2023-08-02 17:05:05 -0400

    addeed skip_final_snapshot to schema

--- internal/service/rds/cluster_instance.go
+++ internal/service/rds/cluster_instance.go
@@ -217,6 +217,11 @@ func ResourceClusterInstance() *schema.Resource {
 				Optional: true,
 				Default:  false,
 			},
+			"skip_final_snapshot": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+			},
 			"storage_encrypted": {
 				Type:     schema.TypeBool,
 				Computed: true,
