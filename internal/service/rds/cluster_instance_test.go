22bc72715d5487e3fc199ee0a2964ea68022ee56
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-08-06 16:11:58 -0400

    TestAccRDSClusterInstance_basic: Check 'custom_iam_instance_profile'.

--- internal/service/rds/cluster_instance_test.go
+++ internal/service/rds/cluster_instance_test.go
@@ -47,6 +47,7 @@ func TestAccRDSClusterInstance_basic(t *testing.T) {
 					resource.TestCheckResourceAttrSet(resourceName, "availability_zone"),
 					resource.TestCheckResourceAttr(resourceName, "cluster_identifier", rName),
 					resource.TestCheckResourceAttr(resourceName, "copy_tags_to_snapshot", "false"),
+					resource.TestCheckResourceAttr(resourceName, "custom_iam_instance_profile", ""),
 					resource.TestCheckResourceAttrSet(resourceName, "dbi_resource_id"),
 					resource.TestCheckResourceAttr(resourceName, "engine", "aurora-mysql"),
 					resource.TestCheckResourceAttrSet(resourceName, "engine_version"),
