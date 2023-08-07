a074e1a5e2f085b9e1fa28474066d343228a7c91
rajbnag <rajbnag@amazon.com>
2023-08-07 16:12:04 +0100

    Adding new CodeCatalyst Data Source: Dev Environment

---
+++ internal/service/codecatalyst/dev_environment_data_source_test.go
@@ -0,0 +1,66 @@
+package codecatalyst_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/service/codecatalyst"
+	sdkacctest "github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+
+	"github.com/hashicorp/terraform-provider-aws/names"
+)
+
+func TestAccCodeCatalystDevEnvironmentDataSource_basic(t *testing.T) {
+	ctx := acctest.Context(t)
+
+	var devEnvironment codecatalyst.GetDevEnvironmentOutput
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	dataSourceName := "data.aws_codecatalyst_dev_environment.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+			acctest.PreCheckPartitionHasService(t, names.CodeCatalyst)
+			testAccPreCheck(ctx, t)
+		},
+		ErrorCheck:               acctest.ErrorCheck(t, names.CodeCatalyst),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckDevEnvironmentDestroy(ctx),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDevEnvironmentDataSourceConfig_basic(rName),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccCheckDevEnvironmentExists(ctx, dataSourceName, &devEnvironment),
+					resource.TestCheckResourceAttr(dataSourceName, "space_name", "tf-cc-aws-provider"),
+					resource.TestCheckResourceAttr(dataSourceName, "project_name", "tf-cc"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDevEnvironmentDataSourceConfig_basic(rName string) string {
+	return fmt.Sprintf(`
+
+	resource "aws_codecatalyst_dev_environment" "test" {
+		alias         = %[1]q
+		space_name    = "tf-cc-aws-provider"
+		project_name  = "tf-cc"
+		instance_type = "dev.standard1.small"
+		persistent_storage {
+		  size = 16
+		}
+		ides {
+		  name = "VSCode"
+		}
+	}
+
+	data "aws_codecatalyst_dev_environment" "test" {
+		space_name    	= "tf-cc-aws-provider"
+		project_name  	= "tf-cc"
+		env_id 	  		= aws_codecatalyst_dev_environment.test.id
+	}
+`, rName)
+}
