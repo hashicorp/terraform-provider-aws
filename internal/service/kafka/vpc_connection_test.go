1b5ae0e41becea4d484f3ea5344a020b76cec4a7
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-08-07 10:12:47 -0400

    Fix terrafmt errors.

--- internal/service/kafka/vpc_connection_test.go
+++ internal/service/kafka/vpc_connection_test.go
@@ -135,15 +135,16 @@ resource "aws_msk_cluster" "test" {
     instance_type   = "kafka.m5.large"
     security_groups = [aws_security_group.test.id]
 
-	connectivity_info {
-		vpc_connectivity {
-			client_authentication {
-				sasl {
-				  iam = true
-				}
-			}
-		}
-	}
+    connectivity_info {
+      vpc_connectivity {
+        client_authentication {
+          sasl {
+            iam = true
+          }
+        }
+      }
+    }
+
     storage_info {
       ebs_storage_info {
         volume_size = 10
