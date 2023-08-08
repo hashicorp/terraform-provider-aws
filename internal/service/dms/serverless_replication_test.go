357ab0400cc84c765a24acc90b3b1e6fcbd57f50
Daniel Rieske <daniel.rieske@gmail.com>
2023-08-09 00:53:12 +0200

    feat: removed dms-vpc role

--- internal/service/dms/serverless_replication_test.go
+++ internal/service/dms/serverless_replication_test.go
@@ -392,27 +392,5 @@ resource "aws_security_group" "test" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-resource "aws_iam_role" "dms-vpc-role" {
-  name        = "dms-vpc-role-%[1]s"
-  description = "Allows DMS to manage VPC"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "dms.amazonaws.com"
-        }
-      Action = "sts:AssumeRole"
-      },
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "example" {
-  role       = aws_iam_role.dms-vpc-role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonDMSVPCManagementRole"
-}
 `, rName))
 }
