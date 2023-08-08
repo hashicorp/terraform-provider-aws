3cd01b4e6d3326f7429852192867fc831c9810cc
Iacopo Palazzi <palazi@amazon.it>
2023-08-09 00:14:37 +0200

    [Bug #32907] Fixing empty 'restore_to_point_in_time'

--- internal/service/rds/instance.go
+++ internal/service/rds/instance.go
@@ -1238,7 +1238,8 @@ func resourceInstanceCreate(ctx context.Context, d *schema.ResourceData, meta in
 			output := outputRaw.(*rds.RestoreDBInstanceFromDBSnapshotOutput)
 			resourceID = aws.StringValue(output.DBInstance.DbiResourceId)
 		}
-	} else if v, ok := d.GetOk("restore_to_point_in_time"); ok {
+	} else if v, ok := d.GetOk("restore_to_point_in_time"); ok && v.([]interface{})[0] != nil {
+
 		tfMap := v.([]interface{})[0].(map[string]interface{})
 
 		input := &rds.RestoreDBInstanceToPointInTimeInput{
