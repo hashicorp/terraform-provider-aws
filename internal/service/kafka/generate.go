cbdfa2ba0729a893339c953500b83c97e20458e8
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-08-10 09:47:58 -0400

    kafka: Trim down generated AWS SDK for Go v2 tagging code.

--- internal/service/kafka/generate.go
+++ internal/service/kafka/generate.go
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MPL-2.0
 
 //go:generate go run ../../generate/tags/main.go -ServiceTagsMap -UpdateTags
-//go:generate go run ../../generate/tags/main.go -AWSSDKVersion=2 -TagsFunc=TagsV2 -ListTagsFunc=ListTagsV2  -KeyValueTagsFunc=keyValueTagsV2 -GetTagsInFunc=getTagsInV2 -SetTagsOutFunc=setTagsOutV2 -UpdateTagsFunc=updateTagsV2 -ListTags -ServiceTagsMap -UpdateTags -KVTValues -SkipTypesImp -- tagsv2_gen.go
+//go:generate go run ../../generate/tags/main.go -AWSSDKVersion=2 -TagsFunc=TagsV2 -KeyValueTagsFunc=keyValueTagsV2 -GetTagsInFunc=getTagsInV2 -SetTagsOutFunc=setTagsOutV2 -ServiceTagsMap -KVTValues -SkipAWSImp -SkipTypesImp -- tagsv2_gen.go
 //go:generate go run ../../generate/servicepackage/main.go
 // ONLY generate directives and package declaration! Do not add anything else to this file.
 
--- internal/service/kafka/tagsv2_gen.go
+++ internal/service/kafka/tagsv2_gen.go
@@ -3,31 +3,10 @@ package kafka
 
 import (
 	"context"
-	"fmt"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/service/kafka"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/types"
-	"github.com/hashicorp/terraform-provider-aws/names"
 )
-
-// ListTagsV2 lists kafka service tags.
-// The identifier is typically the Amazon Resource Name (ARN), although
-// it may also be a different identifier depending on the service.
-func ListTagsV2(ctx context.Context, conn *kafka.Client, identifier string) (tftags.KeyValueTags, error) {
-	input := &kafka.ListTagsForResourceInput{
-		ResourceArn: aws.String(identifier),
-	}
-
-	output, err := conn.ListTagsForResource(ctx, input)
-
-	if err != nil {
-		return tftags.New(ctx, nil), err
-	}
-
-	return keyValueTagsV2(ctx, output.Tags), nil
-}
 
 // map[string]string handling
 
@@ -58,44 +37,4 @@ func setTagsOutV2(ctx context.Context, tags map[string]string) {
 	if inContext, ok := tftags.FromContext(ctx); ok {
 		inContext.TagsOut = types.Some(keyValueTagsV2(ctx, tags))
 	}
-}
-
-// updateTagsV2 updates kafka service tags.
-// The identifier is typically the Amazon Resource Name (ARN), although
-// it may also be a different identifier depending on the service.
-func updateTagsV2(ctx context.Context, conn *kafka.Client, identifier string, oldTagsMap, newTagsMap any) error {
-	oldTags := tftags.New(ctx, oldTagsMap)
-	newTags := tftags.New(ctx, newTagsMap)
-
-	removedTags := oldTags.Removed(newTags)
-	removedTags = removedTags.IgnoreSystem(names.Kafka)
-	if len(removedTags) > 0 {
-		input := &kafka.UntagResourceInput{
-			ResourceArn: aws.String(identifier),
-			TagKeys:     removedTags.Keys(),
-		}
-
-		_, err := conn.UntagResource(ctx, input)
-
-		if err != nil {
-			return fmt.Errorf("untagging resource (%s): %w", identifier, err)
-		}
-	}
-
-	updatedTags := oldTags.Updated(newTags)
-	updatedTags = updatedTags.IgnoreSystem(names.Kafka)
-	if len(updatedTags) > 0 {
-		input := &kafka.TagResourceInput{
-			ResourceArn: aws.String(identifier),
-			Tags:        TagsV2(updatedTags),
-		}
-
-		_, err := conn.TagResource(ctx, input)
-
-		if err != nil {
-			return fmt.Errorf("tagging resource (%s): %w", identifier, err)
-		}
-	}
-
-	return nil
 }
