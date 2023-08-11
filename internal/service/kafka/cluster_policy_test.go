37f140970ebbf483aa50472b1b3d2184acd7efcd
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-08-11 08:24:36 -0400

    Fix importlint errors.

--- internal/service/kafka/cluster_policy_test.go
+++ internal/service/kafka/cluster_policy_test.go
@@ -19,9 +19,8 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/create"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
-	"github.com/hashicorp/terraform-provider-aws/names"
-
 	tfkafka "github.com/hashicorp/terraform-provider-aws/internal/service/kafka"
+	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
 func TestAccKafkaClusterPolicy_basic(t *testing.T) {
