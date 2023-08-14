7b17321bf78e8d81012fa179b7815dc50a2ce11e
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-08-14 16:39:55 -0400

    r/aws_ram_sharing_with_organization: Serialize acceptance tests.

--- internal/service/ram/sharing_with_organization_test.go
+++ internal/service/ram/sharing_with_organization_test.go
@@ -4,37 +4,40 @@
 package ram_test
 
 import (
-	"context"
-	"fmt"
 	"testing"
 
-	"github.com/aws/aws-sdk-go/service/organizations"
 	"github.com/aws/aws-sdk-go/service/ram"
-	"github.com/hashicorp/aws-sdk-go-base/v2/awsv1shim/v2/tfawserr"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
-	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	tfram "github.com/hashicorp/terraform-provider-aws/internal/service/ram"
 )
 
-func TestAccRAMSharingWithOrganization_basic(t *testing.T) {
+func TestAccRAMSharingWithOrganization_serial(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]func(t *testing.T){
+		"basic":      testAccSharingWithOrganization_basic,
+		"disappears": testAccSharingWithOrganization_disappears,
+	}
+
+	acctest.RunSerialTests1Level(t, testCases, 0)
+}
+
+func testAccSharingWithOrganization_basic(t *testing.T) {
 	ctx := acctest.Context(t)
 	resourceName := "aws_ram_sharing_with_organization.test"
 
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
 			acctest.PreCheck(ctx, t)
-			acctest.PreCheckOrganizationsAccount(ctx, t)
+			acctest.PreCheckOrganizationManagementAccount(ctx, t)
 		},
 		ErrorCheck:               acctest.ErrorCheck(t, ram.EndpointsID),
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
-		// RAM sharing with organization cannot be deleted separately.
-		// Ensure parent resource is destroyed instead.
-		CheckDestroy: testAccCheckOrganizationDestroy(ctx),
+		CheckDestroy:             acctest.CheckDestroyNoop,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccSharingWithOrganizationConfig_basic(),
-				Check:  resource.ComposeTestCheckFunc(),
 			},
 			{
 				ResourceName:      resourceName,
@@ -45,49 +48,32 @@ func TestAccRAMSharingWithOrganization_basic(t *testing.T) {
 	})
 }
 
-func testAccCheckOrganizationDestroy(ctx context.Context) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		conn := acctest.Provider.Meta().(*conns.AWSClient).OrganizationsConn()
+func testAccSharingWithOrganization_disappears(t *testing.T) {
+	ctx := acctest.Context(t)
+	resourceName := "aws_ram_sharing_with_organization.test"
 
-		for _, rs := range s.RootModule().Resources {
-			if rs.Type != "aws_organizations_organization" {
-				continue
-			}
-
-			params := &organizations.DescribeOrganizationInput{}
-
-			resp, err := conn.DescribeOrganizationWithContext(ctx, params)
-
-			if tfawserr.ErrCodeEquals(err, organizations.ErrCodeAWSOrganizationsNotInUseException) {
-				return nil
-			}
-
-			if err != nil {
-				return err
-			}
-
-			if resp != nil && resp.Organization != nil {
-				return fmt.Errorf("Bad: Organization still exists: %q", rs.Primary.ID)
-			}
-		}
-
-		return nil
-	}
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+			acctest.PreCheckOrganizationManagementAccount(ctx, t)
+		},
+		ErrorCheck:               acctest.ErrorCheck(t, ram.EndpointsID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             acctest.CheckDestroyNoop,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSharingWithOrganizationConfig_basic(),
+				Check: resource.ComposeTestCheckFunc(
+					acctest.CheckResourceDisappears(ctx, acctest.Provider, tfram.ResourceSharingWithOrganization(), resourceName),
+				),
+				ExpectNonEmptyPlan: true,
+			},
+		},
+	})
 }
 
 func testAccSharingWithOrganizationConfig_basic() string {
 	return `
-data "aws_partition" "current" {}
-
-resource "aws_organizations_organization" "test" {
-  aws_service_access_principals = ["ram.${data.aws_partition.current.dns_suffix}"]
-  feature_set                   = "ALL"
-}
-
-resource "aws_ram_sharing_with_organization" "test" {
-  depends_on = [
-    aws_organizations_organization.test
-  ]
-}
+resource "aws_ram_sharing_with_organization" "test" {}
 `
 }
