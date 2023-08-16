7bc70e6b600a8f83fb345c33ebca9f2b87000bfa
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-08-16 14:12:12 -0400

    Consolidate into 'autoflex()test).go'.

--- internal/framework/flex/autoflex.go
+++ internal/framework/flex/autoflex.go
@@ -11,6 +11,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	fwtypes "github.com/hashicorp/terraform-provider-aws/internal/framework/types"
@@ -28,6 +29,24 @@ func Expand(ctx context.Context, tfObject, apiObject any) diag.Diagnostics {
 	diags.Append(walkStructFields(ctx, tfObject, apiObject, expandVisitor{})...)
 	if diags.HasError() {
 		diags.AddError("AutoFlEx", fmt.Sprintf("Expand[%T, %T]", tfObject, apiObject))
+		return diags
+	}
+
+	return diags
+}
+
+// Flatten "flattens" an AWS SDK for Go v2 API data structure into
+// a resource's "business logic" data structure, implemented using
+// Terraform Plugin Framework data types.
+// The API data structure's fields are walked and exported fields that
+// have a corresponding field in the resource's data structure (and a
+// suitable target data type) are copied.
+func Flatten(ctx context.Context, apiObject, tfObject any) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	diags.Append(walkStructFields(ctx, apiObject, tfObject, flattenVisitor{})...)
+	if diags.HasError() {
+		diags.AddError("AutoFlEx", fmt.Sprintf("Flatten[%T, %T]", apiObject, tfObject))
 		return diags
 	}
 
@@ -597,5 +616,485 @@ func (visitor expandVisitor) nestedObjectToSlice(ctx context.Context, vFrom fwty
 
 	vTo.Set(t)
 
+	return diags
+}
+
+type flattenVisitor struct{}
+
+func (visitor flattenVisitor) visit(ctx context.Context, fieldName string, vFrom, vTo reflect.Value) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	valTo, ok := vTo.Interface().(attr.Value)
+	if !ok {
+		diags.AddError("AutoFlEx", fmt.Sprintf("does not implement attr.Value: %s", vTo.Kind()))
+		return diags
+	}
+
+	tTo := valTo.Type(ctx)
+	switch vFrom.Kind() {
+	case reflect.Bool:
+		diags.Append(visitor.bool(ctx, vFrom, tTo, vTo)...)
+		return diags
+
+	case reflect.Float32, reflect.Float64:
+		diags.Append(visitor.float(ctx, vFrom, tTo, vTo)...)
+		return diags
+
+	case reflect.Int32, reflect.Int64:
+		diags.Append(visitor.int(ctx, vFrom, tTo, vTo)...)
+		return diags
+
+	case reflect.String:
+		diags.Append(visitor.string(ctx, vFrom, tTo, vTo)...)
+		return diags
+
+	case reflect.Ptr:
+		diags.Append(visitor.ptr(ctx, vFrom, tTo, vTo)...)
+		return diags
+
+	case reflect.Slice:
+		diags.Append(visitor.slice(ctx, vFrom, tTo, vTo)...)
+		return diags
+
+	case reflect.Map:
+		diags.Append(visitor.map_(ctx, vFrom, tTo, vTo)...)
+		return diags
+	}
+
+	tflog.Info(ctx, "AutoFlex Flatten; incompatible types", map[string]interface{}{
+		"from": vFrom.Kind(),
+		"to":   tTo,
+	})
+
+	return diags
+}
+
+// bool copies an AWS API bool value to a compatible Plugin Framework field.
+func (visitor flattenVisitor) bool(ctx context.Context, vFrom reflect.Value, tTo attr.Type, vTo reflect.Value) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	switch tTo := tTo.(type) {
+	case basetypes.BoolTypable:
+		v, d := tTo.ValueFromBool(ctx, types.BoolValue(vFrom.Bool()))
+		diags.Append(d...)
+		if diags.HasError() {
+			return diags
+		}
+
+		//
+		// bool -> types.Bool.
+		//
+		vTo.Set(reflect.ValueOf(v))
+		return diags
+	}
+
+	tflog.Info(ctx, "AutoFlex Flatten; incompatible types", map[string]interface{}{
+		"from": vFrom.Kind(),
+		"to":   tTo,
+	})
+
+	return diags
+}
+
+// float copies an AWS API float value to a compatible Plugin Framework field.
+func (visitor flattenVisitor) float(ctx context.Context, vFrom reflect.Value, tTo attr.Type, vTo reflect.Value) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	switch tTo := tTo.(type) {
+	case basetypes.Float64Typable:
+		v, d := tTo.ValueFromFloat64(ctx, types.Float64Value(vFrom.Float()))
+		diags.Append(d...)
+		if diags.HasError() {
+			return diags
+		}
+
+		//
+		// float32/float64 -> types.Float64.
+		//
+		vTo.Set(reflect.ValueOf(v))
+		return diags
+	}
+
+	tflog.Info(ctx, "AutoFlex Flatten; incompatible types", map[string]interface{}{
+		"from": vFrom.Kind(),
+		"to":   tTo,
+	})
+
+	return diags
+}
+
+// int copies an AWS API int value to a compatible Plugin Framework field.
+func (visitor flattenVisitor) int(ctx context.Context, vFrom reflect.Value, tTo attr.Type, vTo reflect.Value) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	switch tTo := tTo.(type) {
+	case basetypes.Int64Typable:
+		v, d := tTo.ValueFromInt64(ctx, types.Int64Value(vFrom.Int()))
+		diags.Append(d...)
+		if diags.HasError() {
+			return diags
+		}
+
+		//
+		// int32/int64 -> types.Int64.
+		//
+		vTo.Set(reflect.ValueOf(v))
+		return diags
+	}
+
+	tflog.Info(ctx, "AutoFlex Flatten; incompatible types", map[string]interface{}{
+		"from": vFrom.Kind(),
+		"to":   tTo,
+	})
+
+	return diags
+}
+
+// string copies an AWS API string value to a compatible Plugin Framework field.
+func (visitor flattenVisitor) string(ctx context.Context, vFrom reflect.Value, tTo attr.Type, vTo reflect.Value) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	switch tTo := tTo.(type) {
+	case basetypes.StringTypable:
+		v, d := tTo.ValueFromString(ctx, types.StringValue(vFrom.String()))
+		diags.Append(d...)
+		if diags.HasError() {
+			return diags
+		}
+
+		//
+		// string -> types.String.
+		//
+		vTo.Set(reflect.ValueOf(v))
+		return diags
+	}
+
+	tflog.Info(ctx, "AutoFlex Flatten; incompatible types", map[string]interface{}{
+		"from": vFrom.Kind(),
+		"to":   tTo,
+	})
+
+	return diags
+}
+
+// ptr copies an AWS API pointer value to a compatible Plugin Framework field.
+func (visitor flattenVisitor) ptr(ctx context.Context, vFrom reflect.Value, tTo attr.Type, vTo reflect.Value) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	switch vElem := vFrom.Elem(); vFrom.Type().Elem().Kind() {
+	case reflect.Bool:
+		if vFrom.IsNil() {
+			vTo.Set(reflect.ValueOf(types.BoolNull()))
+			return diags
+		}
+
+		diags.Append(visitor.bool(ctx, vElem, tTo, vTo)...)
+		return diags
+
+	case reflect.Float32, reflect.Float64:
+		if vFrom.IsNil() {
+			vTo.Set(reflect.ValueOf(types.Float64Null()))
+			return diags
+		}
+
+		diags.Append(visitor.float(ctx, vElem, tTo, vTo)...)
+		return diags
+
+	case reflect.Int32, reflect.Int64:
+		if vFrom.IsNil() {
+			vTo.Set(reflect.ValueOf(types.Int64Null()))
+			return diags
+		}
+
+		diags.Append(visitor.int(ctx, vElem, tTo, vTo)...)
+		return diags
+
+	case reflect.String:
+		if vFrom.IsNil() {
+			vTo.Set(reflect.ValueOf(types.StringNull()))
+			return diags
+		}
+
+		diags.Append(visitor.string(ctx, vElem, tTo, vTo)...)
+		return diags
+
+	case reflect.Struct:
+		if tTo, ok := tTo.(fwtypes.NestedObjectType); ok {
+			//
+			// *struct -> types.List(OfObject).
+			//
+			diags.Append(visitor.ptrToStructNestedObject(ctx, vFrom, tTo, vTo)...)
+			return diags
+		}
+	}
+
+	tflog.Info(ctx, "AutoFlex Flatten; incompatible types", map[string]interface{}{
+		"from": vFrom.Kind(),
+		"to":   tTo,
+	})
+
+	return diags
+}
+
+// slice copies an AWS API slice value to a compatible Plugin Framework field.
+func (visitor flattenVisitor) slice(ctx context.Context, vFrom reflect.Value, tTo attr.Type, vTo reflect.Value) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	switch tSliceElem := vFrom.Type().Elem(); tSliceElem.Kind() {
+	case reflect.String:
+		switch tTo := tTo.(type) {
+		case basetypes.ListTypable:
+			//
+			// []string -> types.List(OfString).
+			//
+			if vFrom.IsNil() {
+				vTo.Set(reflect.ValueOf(types.ListNull(types.StringType)))
+				return diags
+			}
+
+			v, d := tTo.ValueFromList(ctx, FlattenFrameworkStringValueList(ctx, vFrom.Interface().([]string)))
+			diags.Append(d...)
+			if diags.HasError() {
+				return diags
+			}
+
+			vTo.Set(reflect.ValueOf(v))
+			return diags
+
+		case basetypes.SetTypable:
+			//
+			// []string -> types.Set(OfString).
+			//
+			if vFrom.IsNil() {
+				vTo.Set(reflect.ValueOf(types.SetNull(types.StringType)))
+				return diags
+			}
+
+			v, d := tTo.ValueFromSet(ctx, FlattenFrameworkStringValueSet(ctx, vFrom.Interface().([]string)))
+			diags.Append(d...)
+			if diags.HasError() {
+				return diags
+			}
+
+			vTo.Set(reflect.ValueOf(v))
+			return diags
+		}
+
+	case reflect.Ptr:
+		switch tSliceElem.Elem().Kind() {
+		case reflect.String:
+			switch tTo := tTo.(type) {
+			case basetypes.ListTypable:
+				//
+				// []*string -> types.List(OfString).
+				//
+				if vFrom.IsNil() {
+					vTo.Set(reflect.ValueOf(types.ListNull(types.StringType)))
+					return diags
+				}
+
+				v, d := tTo.ValueFromList(ctx, FlattenFrameworkStringList(ctx, vFrom.Interface().([]*string)))
+				diags.Append(d...)
+				if diags.HasError() {
+					return diags
+				}
+
+				vTo.Set(reflect.ValueOf(v))
+				return diags
+
+			case basetypes.SetTypable:
+				//
+				// []string -> types.Set(OfString).
+				//
+				if vFrom.IsNil() {
+					vTo.Set(reflect.ValueOf(types.SetNull(types.StringType)))
+					return diags
+				}
+
+				v, d := tTo.ValueFromSet(ctx, FlattenFrameworkStringSet(ctx, vFrom.Interface().([]*string)))
+				diags.Append(d...)
+				if diags.HasError() {
+					return diags
+				}
+
+				vTo.Set(reflect.ValueOf(v))
+				return diags
+			}
+
+		case reflect.Struct:
+			if tTo, ok := tTo.(fwtypes.NestedObjectType); ok {
+				//
+				// []*struct -> types.List(OfObject).
+				//
+				diags.Append(visitor.sliceOfStructNestedObject(ctx, vFrom, tTo, vTo)...)
+				return diags
+			}
+		}
+
+	case reflect.Struct:
+		if tTo, ok := tTo.(fwtypes.NestedObjectType); ok {
+			//
+			// []struct -> types.List(OfObject).
+			//
+			diags.Append(visitor.sliceOfStructNestedObject(ctx, vFrom, tTo, vTo)...)
+			return diags
+		}
+	}
+
+	tflog.Info(ctx, "AutoFlex Flatten; incompatible types", map[string]interface{}{
+		"from": vFrom.Kind(),
+		"to":   tTo,
+	})
+
+	return diags
+}
+
+// map_ copies an AWS API map value to a compatible Plugin Framework field.
+func (visitor flattenVisitor) map_(ctx context.Context, vFrom reflect.Value, tTo attr.Type, vTo reflect.Value) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	switch tMapKey := vFrom.Type().Key(); tMapKey.Kind() {
+	case reflect.String:
+		switch tMapElem := vFrom.Type().Elem(); tMapElem.Kind() {
+		case reflect.String:
+			switch tTo := tTo.(type) {
+			case basetypes.MapTypable:
+				//
+				// map[string]string -> types.Map(OfString).
+				//
+				if vFrom.IsNil() {
+					vTo.Set(reflect.ValueOf(types.MapNull(types.StringType)))
+					return diags
+				}
+
+				v, d := tTo.ValueFromMap(ctx, FlattenFrameworkStringValueMap(ctx, vFrom.Interface().(map[string]string)))
+				diags.Append(d...)
+				if diags.HasError() {
+					return diags
+				}
+
+				vTo.Set(reflect.ValueOf(v))
+				return diags
+			}
+
+		case reflect.Ptr:
+			switch tMapElem.Elem().Kind() {
+			case reflect.String:
+				switch tTo := tTo.(type) {
+				case basetypes.MapTypable:
+					//
+					// map[string]*string -> types.Map(OfString).
+					//
+					if vFrom.IsNil() {
+						vTo.Set(reflect.ValueOf(types.MapNull(types.StringType)))
+						return diags
+					}
+
+					v, d := tTo.ValueFromMap(ctx, FlattenFrameworkStringMap(ctx, vFrom.Interface().(map[string]*string)))
+					diags.Append(d...)
+					if diags.HasError() {
+						return diags
+					}
+
+					vTo.Set(reflect.ValueOf(v))
+					return diags
+				}
+			}
+		}
+	}
+
+	tflog.Info(ctx, "AutoFlex Flatten; incompatible types", map[string]interface{}{
+		"from": vFrom.Kind(),
+		"to":   tTo,
+	})
+
+	return diags
+}
+
+// ptrToStructNestedObject copies an AWS API *struct value to a compatible Plugin Framework NestedObjectValue field.
+func (visitor flattenVisitor) ptrToStructNestedObject(ctx context.Context, vFrom reflect.Value, tTo fwtypes.NestedObjectType, vTo reflect.Value) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	if vFrom.IsNil() {
+		val, d := tTo.NullValue(ctx)
+		diags.Append(d...)
+		if diags.HasError() {
+			return diags
+		}
+
+		vTo.Set(reflect.ValueOf(val))
+		return diags
+	}
+
+	// Create a new target structure and walk its fields.
+	to, d := tTo.NewObjectPtr(ctx)
+	diags.Append(d...)
+	if diags.HasError() {
+		return diags
+	}
+
+	diags.Append(walkStructFields(ctx, vFrom.Elem().Interface(), to, visitor)...)
+	if diags.HasError() {
+		return diags
+	}
+
+	// Set the target structure as a nested Object.
+	val, d := tTo.ValueFromObjectPtr(ctx, to)
+	diags.Append(d...)
+	if diags.HasError() {
+		return diags
+	}
+
+	vTo.Set(reflect.ValueOf(val))
+	return diags
+}
+
+// sliceOfStructNestedObject copies an AWS API []struct value to a compatible Plugin Framework NestedObjectValue field.
+func (visitor flattenVisitor) sliceOfStructNestedObject(ctx context.Context, vFrom reflect.Value, tTo fwtypes.NestedObjectType, vTo reflect.Value) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	if vFrom.IsNil() {
+		val, d := tTo.NullValue(ctx)
+		diags.Append(d...)
+		if diags.HasError() {
+			return diags
+		}
+
+		vTo.Set(reflect.ValueOf(val))
+		return diags
+	}
+
+	// Create a new target slice and flatten each element.
+	n := vFrom.Len()
+	to, d := tTo.NewObjectSlice(ctx, n, n)
+	diags.Append(d...)
+	if diags.HasError() {
+		return diags
+	}
+
+	t := reflect.ValueOf(to)
+	for i := 0; i < n; i++ {
+		target, d := tTo.NewObjectPtr(ctx)
+		diags.Append(d...)
+		if diags.HasError() {
+			return diags
+		}
+
+		diags.Append(walkStructFields(ctx, vFrom.Index(i).Interface(), target, visitor)...)
+		if diags.HasError() {
+			return diags
+		}
+
+		t.Index(i).Set(reflect.ValueOf(target))
+	}
+
+	// Set the target structure as a nested Object.
+	val, d := tTo.ValueFromObjectSlice(ctx, to)
+	diags.Append(d...)
+	if diags.HasError() {
+		return diags
+	}
+
+	vTo.Set(reflect.ValueOf(val))
 	return diags
 }
--- internal/framework/flex/autoflex_test.go
+++ internal/framework/flex/autoflex_test.go
@@ -413,3 +413,360 @@ func TestGenericExpand(t *testing.T) {
 		})
 	}
 }
+
+func TestGenericFlatten(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+	testString := "test"
+	testCases := []struct {
+		TestName   string
+		Source     any
+		Target     any
+		WantErr    bool
+		WantTarget any
+	}{
+		{
+			TestName: "nil Source and Target",
+			WantErr:  true,
+		},
+		{
+			TestName: "non-pointer Target",
+			Source:   TestFlex00{},
+			Target:   0,
+			WantErr:  true,
+		},
+		{
+			TestName: "non-struct Source",
+			Source:   testString,
+			Target:   &TestFlex00{},
+			WantErr:  true,
+		},
+		{
+			TestName: "non-struct Target",
+			Source:   TestFlex00{},
+			Target:   &testString,
+			WantErr:  true,
+		},
+		{
+			TestName:   "empty struct Source and Target",
+			Source:     TestFlex00{},
+			Target:     &TestFlex00{},
+			WantTarget: &TestFlex00{},
+		},
+		{
+			TestName:   "empty struct pointer Source and Target",
+			Source:     &TestFlex00{},
+			Target:     &TestFlex00{},
+			WantTarget: &TestFlex00{},
+		},
+		{
+			TestName:   "single string struct pointer Source and empty Target",
+			Source:     &TestFlexAWS01{Field1: "a"},
+			Target:     &TestFlex00{},
+			WantTarget: &TestFlex00{},
+		},
+		{
+			TestName: "does not implement attr.Value Target",
+			Source:   &TestFlexAWS01{Field1: "a"},
+			Target:   &TestFlexAWS01{},
+			WantErr:  true,
+		},
+		{
+			TestName:   "single empty string Source and single string Target",
+			Source:     &TestFlexAWS01{},
+			Target:     &TestFlexTF01{},
+			WantTarget: &TestFlexTF01{Field1: types.StringValue("")},
+		},
+		{
+			TestName:   "single string Source and single string Target",
+			Source:     &TestFlexAWS01{Field1: "a"},
+			Target:     &TestFlexTF01{},
+			WantTarget: &TestFlexTF01{Field1: types.StringValue("a")},
+		},
+		{
+			TestName:   "single nil *string Source and single string Target",
+			Source:     &TestFlexAWS02{},
+			Target:     &TestFlexTF01{},
+			WantTarget: &TestFlexTF01{Field1: types.StringNull()},
+		},
+		{
+			TestName:   "single *string Source and single string Target",
+			Source:     &TestFlexAWS02{Field1: aws.String("a")},
+			Target:     &TestFlexTF01{},
+			WantTarget: &TestFlexTF01{Field1: types.StringValue("a")},
+		},
+		{
+			TestName:   "single string Source and single int64 Target",
+			Source:     &TestFlexAWS01{Field1: "a"},
+			Target:     &TestFlexTF02{},
+			WantTarget: &TestFlexTF02{},
+		},
+		{
+			TestName: "zero value primtive types Source and primtive types Target",
+			Source:   &TestFlexAWS04{},
+			Target:   &TestFlexTF03{},
+			WantTarget: &TestFlexTF03{
+				Field1:  types.StringValue(""),
+				Field2:  types.StringNull(),
+				Field3:  types.Int64Value(0),
+				Field4:  types.Int64Null(),
+				Field5:  types.Int64Value(0),
+				Field6:  types.Int64Null(),
+				Field7:  types.Float64Value(0),
+				Field8:  types.Float64Null(),
+				Field9:  types.Float64Value(0),
+				Field10: types.Float64Null(),
+				Field11: types.BoolValue(false),
+				Field12: types.BoolNull(),
+			},
+		},
+		{
+			TestName: "primtive types Source and primtive types Target",
+			Source: &TestFlexAWS04{
+				Field1:  "field1",
+				Field2:  aws.String("field2"),
+				Field3:  3,
+				Field4:  aws.Int32(-4),
+				Field5:  5,
+				Field6:  aws.Int64(-6),
+				Field7:  7.7,
+				Field8:  aws.Float32(-8.8),
+				Field9:  9.99,
+				Field10: aws.Float64(-10.101),
+				Field11: true,
+				Field12: aws.Bool(false),
+			},
+			Target: &TestFlexTF03{},
+			WantTarget: &TestFlexTF03{
+				Field1: types.StringValue("field1"),
+				Field2: types.StringValue("field2"),
+				Field3: types.Int64Value(3),
+				Field4: types.Int64Value(-4),
+				Field5: types.Int64Value(5),
+				Field6: types.Int64Value(-6),
+				// float32 -> float64 precision problems.
+				Field7:  types.Float64Value(float64(float32(7.7))),
+				Field8:  types.Float64Value(float64(float32(-8.8))),
+				Field9:  types.Float64Value(9.99),
+				Field10: types.Float64Value(-10.101),
+				Field11: types.BoolValue(true),
+				Field12: types.BoolValue(false),
+			},
+		},
+		{
+			TestName: "zero value slice/map of primtive primtive types Source and List/Set/Map of primtive types Target",
+			Source:   &TestFlexAWS05{},
+			Target:   &TestFlexTF04{},
+			WantTarget: &TestFlexTF04{
+				Field1: types.ListNull(types.StringType),
+				Field2: types.ListNull(types.StringType),
+				Field3: types.SetNull(types.StringType),
+				Field4: types.SetNull(types.StringType),
+				Field5: types.MapNull(types.StringType),
+				Field6: types.MapNull(types.StringType),
+			},
+		},
+		{
+			TestName: "slice/map of primtive primtive types Source and List/Set/Map of primtive types Target",
+			Source: &TestFlexAWS05{
+				Field1: []string{"a", "b"},
+				Field2: aws.StringSlice([]string{"a", "b"}),
+				Field3: []string{"a", "b"},
+				Field4: aws.StringSlice([]string{"a", "b"}),
+				Field5: map[string]string{"A": "a", "B": "b"},
+				Field6: aws.StringMap(map[string]string{"A": "a", "B": "b"}),
+			},
+			Target: &TestFlexTF04{},
+			WantTarget: &TestFlexTF04{
+				Field1: types.ListValueMust(types.StringType, []attr.Value{
+					types.StringValue("a"),
+					types.StringValue("b"),
+				}),
+				Field2: types.ListValueMust(types.StringType, []attr.Value{
+					types.StringValue("a"),
+					types.StringValue("b"),
+				}),
+				Field3: types.SetValueMust(types.StringType, []attr.Value{
+					types.StringValue("a"),
+					types.StringValue("b"),
+				}),
+				Field4: types.SetValueMust(types.StringType, []attr.Value{
+					types.StringValue("a"),
+					types.StringValue("b"),
+				}),
+				Field5: types.MapValueMust(types.StringType, map[string]attr.Value{
+					"A": types.StringValue("a"),
+					"B": types.StringValue("b"),
+				}),
+				Field6: types.MapValueMust(types.StringType, map[string]attr.Value{
+					"A": types.StringValue("a"),
+					"B": types.StringValue("b"),
+				}),
+			},
+		},
+		{
+			TestName:   "nil *struct Source and single list Target",
+			Source:     &TestFlexAWS06{},
+			Target:     &TestFlexTF05{},
+			WantTarget: &TestFlexTF05{Field1: fwtypes.NewListNestedObjectValueOfNull[TestFlexTF01](ctx)},
+		},
+		{
+			TestName:   "*struct Source and single list Target",
+			Source:     &TestFlexAWS06{Field1: &TestFlexAWS01{Field1: "a"}},
+			Target:     &TestFlexTF05{},
+			WantTarget: &TestFlexTF05{Field1: fwtypes.NewListNestedObjectValueOfPtr(ctx, &TestFlexTF01{Field1: types.StringValue("a")})},
+		},
+		{
+			TestName:   "*struct Source and single set Target",
+			Source:     &TestFlexAWS06{Field1: &TestFlexAWS01{Field1: "a"}},
+			Target:     &TestFlexTF06{},
+			WantTarget: &TestFlexTF06{Field1: fwtypes.NewSetNestedObjectValueOfPtr(ctx, &TestFlexTF01{Field1: types.StringValue("a")})},
+		},
+		{
+			TestName:   "nil []struct and null list Target",
+			Source:     &TestFlexAWS08{},
+			Target:     &TestFlexTF05{},
+			WantTarget: &TestFlexTF05{Field1: fwtypes.NewListNestedObjectValueOfNull[TestFlexTF01](ctx)},
+		},
+		{
+			TestName:   "nil []struct and null set Target",
+			Source:     &TestFlexAWS08{},
+			Target:     &TestFlexTF06{},
+			WantTarget: &TestFlexTF06{Field1: fwtypes.NewSetNestedObjectValueOfNull[TestFlexTF01](ctx)},
+		},
+		{
+			TestName:   "empty []struct and empty list Target",
+			Source:     &TestFlexAWS08{Field1: []TestFlexAWS01{}},
+			Target:     &TestFlexTF05{},
+			WantTarget: &TestFlexTF05{Field1: fwtypes.NewListNestedObjectValueOfValueSlice(ctx, []TestFlexTF01{})},
+		},
+		{
+			TestName:   "empty []struct and empty struct Target",
+			Source:     &TestFlexAWS08{Field1: []TestFlexAWS01{}},
+			Target:     &TestFlexTF06{},
+			WantTarget: &TestFlexTF06{Field1: fwtypes.NewSetNestedObjectValueOfValueSlice(ctx, []TestFlexTF01{})},
+		},
+		{
+			TestName: "non-empty []struct and non-empty list Target",
+			Source: &TestFlexAWS08{Field1: []TestFlexAWS01{
+				{Field1: "a"},
+				{Field1: "b"},
+			}},
+			Target: &TestFlexTF05{},
+			WantTarget: &TestFlexTF05{Field1: fwtypes.NewListNestedObjectValueOfValueSlice(ctx, []TestFlexTF01{
+				{Field1: types.StringValue("a")},
+				{Field1: types.StringValue("b")},
+			})},
+		},
+		{
+			TestName: "non-empty []struct and non-empty set Target",
+			Source: &TestFlexAWS08{Field1: []TestFlexAWS01{
+				{Field1: "a"},
+				{Field1: "b"},
+			}},
+			Target: &TestFlexTF06{},
+			WantTarget: &TestFlexTF06{Field1: fwtypes.NewSetNestedObjectValueOfValueSlice(ctx, []TestFlexTF01{
+				{Field1: types.StringValue("a")},
+				{Field1: types.StringValue("b")},
+			})},
+		},
+		{
+			TestName:   "nil []*struct and null list Target",
+			Source:     &TestFlexAWS07{},
+			Target:     &TestFlexTF05{},
+			WantTarget: &TestFlexTF05{Field1: fwtypes.NewListNestedObjectValueOfNull[TestFlexTF01](ctx)},
+		},
+		{
+			TestName:   "nil []*struct and null set Target",
+			Source:     &TestFlexAWS07{},
+			Target:     &TestFlexTF06{},
+			WantTarget: &TestFlexTF06{Field1: fwtypes.NewSetNestedObjectValueOfNull[TestFlexTF01](ctx)},
+		},
+		{
+			TestName:   "empty []*struct and empty list Target",
+			Source:     &TestFlexAWS07{Field1: []*TestFlexAWS01{}},
+			Target:     &TestFlexTF05{},
+			WantTarget: &TestFlexTF05{Field1: fwtypes.NewListNestedObjectValueOfSlice(ctx, []*TestFlexTF01{})},
+		},
+		{
+			TestName:   "empty []*struct and empty set Target",
+			Source:     &TestFlexAWS07{Field1: []*TestFlexAWS01{}},
+			Target:     &TestFlexTF06{},
+			WantTarget: &TestFlexTF06{Field1: fwtypes.NewSetNestedObjectValueOfSlice(ctx, []*TestFlexTF01{})},
+		},
+		{
+			TestName: "non-empty []*struct and non-empty list Target",
+			Source: &TestFlexAWS07{Field1: []*TestFlexAWS01{
+				{Field1: "a"},
+				{Field1: "b"},
+			}},
+			Target: &TestFlexTF05{},
+			WantTarget: &TestFlexTF05{Field1: fwtypes.NewListNestedObjectValueOfSlice(ctx, []*TestFlexTF01{
+				{Field1: types.StringValue("a")},
+				{Field1: types.StringValue("b")},
+			})},
+		},
+		{
+			TestName: "non-empty []*struct and non-empty set Target",
+			Source: &TestFlexAWS07{Field1: []*TestFlexAWS01{
+				{Field1: "a"},
+				{Field1: "b"},
+			}},
+			Target: &TestFlexTF06{},
+			WantTarget: &TestFlexTF06{Field1: fwtypes.NewSetNestedObjectValueOfSlice(ctx, []*TestFlexTF01{
+				{Field1: types.StringValue("a")},
+				{Field1: types.StringValue("b")},
+			})},
+		},
+		{
+			TestName: "complex Source and complex Target",
+			Source: &TestFlexAWS09{
+				Field1: "m",
+				Field2: &TestFlexAWS06{Field1: &TestFlexAWS01{Field1: "n"}},
+				Field3: aws.StringMap(map[string]string{"X": "x", "Y": "y"}),
+				Field4: []TestFlexAWS03{{Field1: 100}, {Field1: 2000}, {Field1: 30000}},
+			},
+			Target: &TestFlexTF07{},
+			WantTarget: &TestFlexTF07{
+				Field1: types.StringValue("m"),
+				Field2: fwtypes.NewListNestedObjectValueOfPtr(ctx, &TestFlexTF05{
+					Field1: fwtypes.NewListNestedObjectValueOfPtr(ctx, &TestFlexTF01{
+						Field1: types.StringValue("n"),
+					}),
+				}),
+				Field3: types.MapValueMust(types.StringType, map[string]attr.Value{
+					"X": types.StringValue("x"),
+					"Y": types.StringValue("y"),
+				}),
+				Field4: fwtypes.NewSetNestedObjectValueOfValueSlice(ctx, []TestFlexTF02{
+					{Field1: types.Int64Value(100)},
+					{Field1: types.Int64Value(2000)},
+					{Field1: types.Int64Value(30000)},
+				}),
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		testCase := testCase
+		t.Run(testCase.TestName, func(t *testing.T) {
+			t.Parallel()
+
+			err := Flatten(ctx, testCase.Source, testCase.Target)
+			gotErr := err != nil
+
+			if gotErr != testCase.WantErr {
+				t.Errorf("gotErr = %v, wantErr = %v", gotErr, testCase.WantErr)
+			}
+
+			if gotErr {
+				if !testCase.WantErr {
+					t.Errorf("err = %q", err)
+				}
+			} else if diff := cmp.Diff(testCase.Target, testCase.WantTarget); diff != "" {
+				t.Errorf("unexpected diff (+wanted, -got): %s", diff)
+			}
+		})
+	}
+}
