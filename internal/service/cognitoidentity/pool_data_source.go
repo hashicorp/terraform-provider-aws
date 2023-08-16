0aa68950c411ae58094001c993c2470568ea2bb6
drewmullen <mullen.drew@gmail.com>
2023-08-16 16:01:19 -0400

    initial commit, make gen failing

---
+++ internal/service/cognitoidentity/pool_data_source.go
@@ -0,0 +1,161 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package cognitoidentity
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/arn"
+
+	"github.com/aws/aws-sdk-go/service/cognitoidentity"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/create"
+
+	// tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
+
+	"github.com/hashicorp/terraform-provider-aws/names"
+)
+
+// @SDKDataSource("aws_cognito_identity_pool", name="Pool")
+func DataSourcePool() *schema.Resource {
+	return &schema.Resource{
+		ReadWithoutTimeout: dataSourcePoolRead,
+
+		Schema: map[string]*schema.Schema{
+			"identity_pool_name": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validIdentityPoolName,
+			},
+
+			"arn": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"cognito_identity_providers": {
+				Type:     schema.TypeSet,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"client_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"provider_name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"server_side_token_check": {
+							Type:     schema.TypeBool,
+							Computed: true,
+						},
+					},
+				},
+			},
+
+			"developer_provider_name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"allow_unauthenticated_identities": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+
+			"allow_classic_flow": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+
+			"openid_connect_provider_arns": {
+				Type:     schema.TypeSet,
+				Computed: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+
+			"saml_provider_arns": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+
+			"supported_login_providers": {
+				Type:     schema.TypeMap,
+				Computed: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+
+			// "tags": tftags.TagsSchemaComputed(), // TIP: Many, but not all, data sources have `tags` attributes.
+		},
+	}
+}
+
+const (
+	DSNamePool = "Pool Data Source"
+)
+
+func dataSourcePoolRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	conn := meta.(*conns.AWSClient).CognitoIdentityConn(ctx)
+
+	name := d.Get("identity_pool_name").(string)
+
+	ip, err := findPoolByName(ctx, conn, name)
+	if err != nil {
+		return append(diags, create.DiagError(names.CognitoIdentity, create.ErrActionReading, DSNamePool, name, err)...)
+	}
+
+	d.SetId(*ip.IdentityPoolId)
+
+	arn := arn.ARN{
+		Partition: meta.(*conns.AWSClient).Partition,
+		Region:    meta.(*conns.AWSClient).Region,
+		Service:   "cognito-identity",
+		AccountID: meta.(*conns.AWSClient).AccountID,
+		Resource:  fmt.Sprintf("identitypool/%s", d.Id()),
+	}
+	d.Set("arn", arn.String())
+	d.Set("identity_pool_name", ip.IdentityPoolName)
+
+	return diags
+}
+
+func findPoolByName(ctx context.Context, conn *cognitoidentity.CognitoIdentity, name string) (*cognitoidentity.IdentityPool, error) {
+	var poolID string
+
+	pools, err := conn.ListIdentityPoolsWithContext(ctx, &cognitoidentity.ListIdentityPoolsInput{})
+	if err != nil {
+		return nil, err
+	}
+
+	for _, p := range pools.IdentityPools {
+		if aws.StringValue(p.IdentityPoolName) == name {
+			poolID = aws.StringValue(p.IdentityPoolId)
+		} else {
+			return nil, fmt.Errorf("no identity pool found with name %q", name)
+		}
+	}
+
+	pool, err := conn.DescribeIdentityPoolWithContext(ctx, &cognitoidentity.DescribeIdentityPoolInput{
+		IdentityPoolId: aws.String(poolID),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return pool, nil
+
+}
---
+++ internal/service/cognitoidentity/pool_data_source_test.go
@@ -0,0 +1,71 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package cognitoidentity_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/service/cognitoidentity"
+	sdkacctest "github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+)
+
+func TestAccCognitoIdentityPoolDataSource_basic(t *testing.T) {
+	ctx := acctest.Context(t)
+
+	if testing.Short() {
+		t.Skip("skipping long-running test in short mode")
+	}
+
+	// var ip cognitoidentity.IdentityPool
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	dataSourceName := "data.aws_cognito_identity_pool.test"
+	resourceName := "aws_cognito_identity_pool.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+			acctest.PreCheckPartitionHasService(t, cognitoidentity.EndpointsID)
+			testAccPreCheck(ctx, t)
+		},
+		ErrorCheck:               acctest.ErrorCheck(t, cognitoidentity.EndpointsID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckPoolDestroy(ctx),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccPoolDataSourceConfig_basic(rName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrPair(dataSourceName, "arn", resourceName, "arn"),
+
+					// testAccCheckPoolExists(ctx, dataSourceName, &ip),
+					// resource.TestCheckResourceAttrPair(dataSourceName, "address_family", resourceName, "address_family"),
+					// resource.TestCheckResourceAttr(dataSourceName, "auto_minor_version_upgrade", "false"),
+					// resource.TestCheckResourceAttrSet(dataSourceName, "maintenance_window_start_time.0.day_of_week"),
+					// resource.TestCheckTypeSetElemNestedAttrs(dataSourceName, "user.*", map[string]string{
+					// 	"console_access": "false",
+					// 	"groups.#":       "0",
+					// 	"username":       "Test",
+					// 	"password":       "TestTest1234",
+					// }),
+					// acctest.MatchResourceAttrRegionalARN(dataSourceName, "arn", "cognitoidentity", regexp.MustCompile(`pool:+.`)),
+				),
+			},
+		},
+	})
+}
+
+func testAccPoolDataSourceConfig_basic(rName string) string {
+	return fmt.Sprintf(`
+resource "aws_cognito_identity_pool" "test" {
+	identity_pool_name               = "%s"
+	allow_unauthenticated_identities = false
+}
+
+data "aws_cognito_identity_pool" "test" {
+	identity_pool_name = aws_cognito_identity_pool.test.name
+}
+`, rName)
+}
