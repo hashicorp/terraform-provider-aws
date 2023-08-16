4cc635f4c084dcedcf906c20a4d60b8dad8fce49
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-08-16 14:05:05 -0400

    Fix
            Error: InternalValidate
    
              with provider["registry.terraform.io/hashicorp/aws"],
              on <empty> line 0:
              (source code not available)
    
            Internal validation of the provider failed! This is always a bug
            with the provider itself, and not a user issue. Please report
            this bug:
    
            1 error occurred:
            	* data source aws_imagebuilder_image_pipeline: image_scanning_configuration:
            MaxItems is for configurable attributes,there's nothing to configure on
            computed-only field

--- internal/service/imagebuilder/image_pipeline_data_source.go
+++ internal/service/imagebuilder/image_pipeline_data_source.go
@@ -66,7 +66,6 @@ func DataSourceImagePipeline() *schema.Resource {
 			"image_scanning_configuration": {
 				Type:     schema.TypeList,
 				Computed: true,
-				MaxItems: 1,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"ecr_configuration": {
