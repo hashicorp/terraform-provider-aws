9356b326e4dd9cda8568cef5434f714dbddcc7c0
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-08-18 13:15:43 -0400

    r/aws_datasync_location_fsx_ontap_file_system: Add 'FindFSxONTAPLocationByARN'.

--- internal/service/datasync/location_fsx_ontap_file_system.go
+++ internal/service/datasync/location_fsx_ontap_file_system.go
@@ -14,12 +14,14 @@ import (
 	"github.com/aws/aws-sdk-go/service/datasync"
 	"github.com/hashicorp/aws-sdk-go-base/v2/awsv1shim/v2/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
 	"github.com/hashicorp/terraform-provider-aws/internal/flex"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
+	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
@@ -190,10 +192,6 @@ func ResourceLocationFSxOntapFileSystem() *schema.Resource {
 	}
 }
 
-const (
-	ResNameLocationFSxOntapFileSystem = "Location FSx Ontap File System"
-)
-
 func resourceLocationFSxOntapFileSystemCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
 	conn := meta.(*conns.AWSClient).DataSyncConn(ctx)
@@ -210,8 +208,9 @@ func resourceLocationFSxOntapFileSystemCreate(ctx context.Context, d *schema.Res
 	}
 
 	output, err := conn.CreateLocationFsxOntapWithContext(ctx, input)
+
 	if err != nil {
-		return sdkdiag.AppendErrorf(diags, "creating DataSync Location Fsx Ontap File System: %s", err)
+		return sdkdiag.AppendErrorf(diags, "creating DataSync Location FSx for NetApp ONTAP File System: %s", err)
 	}
 
 	d.SetId(aws.StringValue(output.LocationArn))
@@ -223,46 +222,34 @@ func resourceLocationFSxOntapFileSystemRead(ctx context.Context, d *schema.Resou
 	var diags diag.Diagnostics
 	conn := meta.(*conns.AWSClient).DataSyncConn(ctx)
 
-	input := &datasync.DescribeLocationFsxOntapInput{
-		LocationArn: aws.String(d.Id()),
-	}
+	output, err := FindFSxONTAPLocationByARN(ctx, conn, d.Id())
 
-	log.Printf("[DEBUG] Reading DataSync Location Fsx Ontap: %#v", input)
-	output, err := conn.DescribeLocationFsxOntapWithContext(ctx, input)
-
-	if tfawserr.ErrMessageContains(err, datasync.ErrCodeInvalidRequestException, "not found") {
-		log.Printf("[WARN] DataSync Location Fsx Ontap %q not found - removing from state", d.Id())
+	if !d.IsNewResource() && tfresource.NotFound(err) {
+		log.Printf("[WARN] DataSync Location FSx for NetApp ONTAP File System (%s) not found, removing from state", d.Id())
 		d.SetId("")
 		return diags
 	}
 
 	if err != nil {
-		return sdkdiag.AppendErrorf(diags, "reading DataSync Location Fsx Ontap (%s): %s", d.Id(), err)
+		return sdkdiag.AppendErrorf(diags, "reading DataSync Location FSx for NetApp ONTAP File System (%s): %s", d.Id(), err)
 	}
 
 	subdirectory, err := subdirectoryFromLocationURI(aws.StringValue(output.LocationUri))
 
 	if err != nil {
-		return sdkdiag.AppendErrorf(diags, "reading DataSync Location Fsx Ontap (%s): %s", d.Id(), err)
+		return sdkdiag.AppendFromErr(diags, err)
 	}
 
 	d.Set("arn", output.LocationArn)
+	d.Set("creation_time", output.CreationTime.Format(time.RFC3339))
 	d.Set("fsx_filesystem_arn", output.FsxFilesystemArn)
-	d.Set("storage_virtual_machine_arn", output.StorageVirtualMachineArn)
-	d.Set("subdirectory", subdirectory)
-	d.Set("uri", output.LocationUri)
-
-	if err := d.Set("creation_time", output.CreationTime.Format(time.RFC3339)); err != nil {
-		return sdkdiag.AppendErrorf(diags, "setting creation_time: %s", err)
-	}
-
 	if err := d.Set("protocol", flattenProtocol(output.Protocol)); err != nil {
 		return sdkdiag.AppendErrorf(diags, "setting protocol: %s", err)
 	}
-
-	if err := d.Set("security_group_arns", flex.FlattenStringSet(output.SecurityGroupArns)); err != nil {
-		return sdkdiag.AppendErrorf(diags, "setting security_group_arns: %s", err)
-	}
+	d.Set("security_group_arns", aws.StringValueSlice(output.SecurityGroupArns))
+	d.Set("storage_virtual_machine_arn", output.StorageVirtualMachineArn)
+	d.Set("subdirectory", subdirectory)
+	d.Set("uri", output.LocationUri)
 
 	return diags
 }
@@ -283,7 +270,7 @@ func resourceLocationFSxOntapFileSystemDelete(ctx context.Context, d *schema.Res
 		LocationArn: aws.String(d.Id()),
 	}
 
-	log.Printf("[DEBUG] Deleting DataSync Location Fsx Ontap File System: %#v", input)
+	log.Printf("[DEBUG] Deleting DataSync Location FSx for NetApp ONTAP File System: %s", d.Id())
 	_, err := conn.DeleteLocationWithContext(ctx, input)
 
 	if tfawserr.ErrMessageContains(err, datasync.ErrCodeInvalidRequestException, "not found") {
@@ -291,8 +278,33 @@ func resourceLocationFSxOntapFileSystemDelete(ctx context.Context, d *schema.Res
 	}
 
 	if err != nil {
-		return sdkdiag.AppendErrorf(diags, "deleting DataSync Location Fsx Ontap (%s): %s", d.Id(), err)
+		return sdkdiag.AppendErrorf(diags, "deleting DataSync Location FSx for NetApp ONTAP File System (%s): %s", d.Id(), err)
 	}
 
 	return diags
+}
+
+func FindFSxONTAPLocationByARN(ctx context.Context, conn *datasync.DataSync, arn string) (*datasync.DescribeLocationFsxOntapOutput, error) {
+	input := &datasync.DescribeLocationFsxOntapInput{
+		LocationArn: aws.String(arn),
+	}
+
+	output, err := conn.DescribeLocationFsxOntapWithContext(ctx, input)
+
+	if tfawserr.ErrMessageContains(err, datasync.ErrCodeInvalidRequestException, "not found") {
+		return nil, &retry.NotFoundError{
+			LastError:   err,
+			LastRequest: input,
+		}
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	if output == nil {
+		return nil, tfresource.NewEmptyResultError(input)
+	}
+
+	return output, nil
 }
