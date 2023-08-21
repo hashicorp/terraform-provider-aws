43bcd35de67000121f11abd3275d41cc061be1f8
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-08-21 14:54:40 -0400

    r/aws_ssoadmin_managed_policy_attachment: Add configurable timeouts.

--- internal/service/ssoadmin/find.go
+++ internal/service/ssoadmin/find.go
@@ -12,36 +12,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 )
 
-// FindManagedPolicy returns the managed policy attached to a permission set within a specified SSO instance.
-// Returns an error if no managed policy is found.
-func FindManagedPolicy(ctx context.Context, conn *ssoadmin.SSOAdmin, managedPolicyArn, permissionSetArn, instanceArn string) (*ssoadmin.AttachedManagedPolicy, error) {
-	input := &ssoadmin.ListManagedPoliciesInPermissionSetInput{
-		PermissionSetArn: aws.String(permissionSetArn),
-		InstanceArn:      aws.String(instanceArn),
-	}
-
-	var attachedPolicy *ssoadmin.AttachedManagedPolicy
-	err := conn.ListManagedPoliciesInPermissionSetPagesWithContext(ctx, input, func(page *ssoadmin.ListManagedPoliciesInPermissionSetOutput, lastPage bool) bool {
-		if page == nil {
-			return !lastPage
-		}
-
-		for _, policy := range page.AttachedManagedPolicies {
-			if policy == nil {
-				continue
-			}
-
-			if aws.StringValue(policy.Arn) == managedPolicyArn {
-				attachedPolicy = policy
-				return false
-			}
-		}
-		return !lastPage
-	})
-
-	return attachedPolicy, err
-}
-
 // FindPermissionsBoundary returns the permissions boundary attached to a permission set within a specified SSO instance.
 // Returns an error if no permissions boundary is found.
 func FindPermissionsBoundary(ctx context.Context, conn *ssoadmin.SSOAdmin, permissionSetArn, instanceArn string) (*ssoadmin.PermissionsBoundary, error) {
