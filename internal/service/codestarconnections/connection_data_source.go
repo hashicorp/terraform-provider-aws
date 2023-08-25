f4dfab59d5329230a8886d29e75aa3615764d07e
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-08-25 09:30:23 -0400

    Fix golangci-lint 'exportloopref'.

--- internal/service/codestarconnections/connection_data_source.go
+++ internal/service/codestarconnections/connection_data_source.go
@@ -82,6 +82,8 @@ func dataSourceConnectionRead(ctx context.Context, d *schema.ResourceData, meta 
 			}
 
 			for _, v := range page.Connections {
+				v := v
+
 				if aws.ToString(v.ConnectionName) == name {
 					connection = &v
 					break
