59236168c2cacb454a6616c9ed92206498e0ef37
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-09-06 10:57:22 -0400

    Fix terrafmt errors.

--- internal/service/shield/drt_access_log_bucket_association_test.go
+++ internal/service/shield/drt_access_log_bucket_association_test.go
@@ -201,12 +201,12 @@ resource "aws_iam_role" "test" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "drt.shield.amazonaws.com"
+      "Sid" : "",
+      "Effect" : "Allow",
+      "Principal" : {
+        "Service" : "drt.shield.amazonaws.com"
       },
-      "Action": "sts:AssumeRole"
+      "Action" : "sts:AssumeRole"
     }]
   })
 }
@@ -249,12 +249,12 @@ resource "aws_iam_role" "test" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "drt.shield.amazonaws.com"
+      "Sid" : "",
+      "Effect" : "Allow",
+      "Principal" : {
+        "Service" : "drt.shield.amazonaws.com"
       },
-      "Action": "sts:AssumeRole"
+      "Action" : "sts:AssumeRole"
     }]
   })
 }
