d307b51c147d277f7ed4b22980da85f26adf009c
Shawn Chasse <shawnsmc@amazon.com>
2023-09-12 15:00:01 -0400

    Add Shield ApplicationLayerAutomaticResponse support

--- internal/service/shield/application_layer_automatic_response_test.go
+++ internal/service/shield/application_layer_automatic_response_test.go
@@ -1,43 +1,14 @@
 package shield_test
-// **PLEASE DELETE THIS AND ALL TIP COMMENTS BEFORE SUBMITTING A PR FOR REVIEW!**
-//
-// TIP: ==== INTRODUCTION ====
-// Thank you for trying the skaff tool!
-//
-// You have opted to include these helpful comments. They all include "TIP:"
-// to help you find and remove them when you're done with them.
-//
-// While some aspects of this file are customized to your input, the
-// scaffold tool does *not* look at the AWS API and ensure it has correct
-// function, structure, and variable names. It makes guesses based on
-// commonalities. You will need to make significant adjustments.
-//
-// In other words, as generated, this is a rough outline of the work you will
-// need to do. If something doesn't make sense for your situation, get rid of
-// it.
 
 import (
-	// TIP: ==== IMPORTS ====
-	// This is a common set of imports but not customized to your code since
-	// your code hasn't been written yet. Make sure you, your IDE, or
-	// goimports -w <file> fixes these imports.
-	//
-	// The provider linter wants your imports to be in two groups: first,
-	// standard library (i.e., "fmt" or "strings"), second, everything else.
-	//
-	// Also, AWS Go SDK v2 may handle nested structures differently than v1,
-	// using the services/shield/types package. If so, you'll
-	// need to import types and reference the nested types, e.g., as
-	// types.<Type Name>.
 	"context"
+	"errors"
 	"fmt"
-	"regexp"
-	"strings"
 	"testing"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/service/shield"
 	"github.com/aws/aws-sdk-go-v2/service/shield/types"
+	"github.com/aws/aws-sdk-go/service/shield"
 	sdkacctest "github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
@@ -47,96 +18,9 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 	"github.com/hashicorp/terraform-provider-aws/names"
 
-	// TIP: You will often need to import the package that this test file lives
-	// in. Since it is in the "test" context, it must import the package to use
-	// any normal context constants, variables, or functions.
 	tfshield "github.com/hashicorp/terraform-provider-aws/internal/service/shield"
 )
 
-// TIP: File Structure. The basic outline for all test files should be as
-// follows. Improve this resource's maintainability by following this
-// outline.
-//
-// 1. Package declaration (add "_test" since this is a test file)
-// 2. Imports
-// 3. Unit tests
-// 4. Basic test
-// 5. Disappears test
-// 6. All the other tests
-// 7. Helper functions (exists, destroy, check, etc.)
-// 8. Functions that return Terraform configurations
-
-// TIP: ==== UNIT TESTS ====
-// This is an example of a unit test. Its name is not prefixed with
-// "TestAcc" like an acceptance test.
-//
-// Unlike acceptance tests, unit tests do not access AWS and are focused on a
-// function (or method). Because of this, they are quick and cheap to run.
-//
-// In designing a resource's implementation, isolate complex bits from AWS bits
-// so that they can be tested through a unit test. We encourage more unit tests
-// in the provider.
-//
-// Cut and dry functions using well-used patterns, like typical flatteners and
-// expanders, don't need unit testing. However, if they are complex or
-// intricate, they should be unit tested.
-func TestApplicationLayerAutomaticResponseExampleUnitTest(t *testing.T) {
-	t.Parallel()
-
-	testCases := []struct {
-		TestName string
-		Input    string
-		Expected string
-		Error    bool
-	}{
-		{
-			TestName: "empty",
-			Input:    "",
-			Expected: "",
-			Error:    true,
-		},
-		{
-			TestName: "descriptive name",
-			Input:    "some input",
-			Expected: "some output",
-			Error:    false,
-		},
-		{
-			TestName: "another descriptive name",
-			Input:    "more input",
-			Expected: "more output",
-			Error:    false,
-		},
-	}
-
-	for _, testCase := range testCases {
-		testCase := testCase
-		t.Run(testCase.TestName, func(t *testing.T) {
-			t.Parallel()
-			got, err := tfshield.FunctionFromResource(testCase.Input)
-
-			if err != nil && !testCase.Error {
-				t.Errorf("got error (%s), expected no error", err)
-			}
-
-			if err == nil && testCase.Error {
-				t.Errorf("got (%s) and no error, expected error", got)
-			}
-
-			if got != testCase.Expected {
-				t.Errorf("got %s, expected %s", got, testCase.Expected)
-			}
-		})
-	}
-}
-
-// TIP: ==== ACCEPTANCE TESTS ====
-// This is an example of a basic acceptance test. This should test as much of
-// standard functionality of the resource as possible, and test importing, if
-// applicable. We prefix its name with "TestAcc", the service, and the
-// resource name.
-//
-// Acceptance test access AWS and cost money to run.
 func TestAccShieldApplicationLayerAutomaticResponse_basic(t *testing.T) {
 	ctx := acctest.Context(t)
 	// TIP: This is a long-running test guard for tests that run longer than
@@ -145,17 +29,14 @@ func TestAccShieldApplicationLayerAutomaticResponse_basic(t *testing.T) {
 		t.Skip("skipping long-running test in short mode")
 	}
 
-	var applicationlayerautomaticresponse shield.DescribeApplicationLayerAutomaticResponseResponse
+	var applicationlayerautomaticresponse shield.DescribeProtectionOutput
 	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
 	resourceName := "aws_shield_application_layer_automatic_response.test"
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck: func() {
 			acctest.PreCheck(ctx, t)
-			acctest.PreCheckPartitionHasService(t, names.ShieldEndpointID)
-			testAccPreCheck(ctx, t)
 		},
-		ErrorCheck:               acctest.ErrorCheck(t, names.ShieldEndpointID),
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
 		CheckDestroy:             testAccCheckApplicationLayerAutomaticResponseDestroy(ctx),
 		Steps: []resource.TestStep{
@@ -163,22 +44,7 @@ func TestAccShieldApplicationLayerAutomaticResponse_basic(t *testing.T) {
 				Config: testAccApplicationLayerAutomaticResponseConfig_basic(rName),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckApplicationLayerAutomaticResponseExists(ctx, resourceName, &applicationlayerautomaticresponse),
-					resource.TestCheckResourceAttr(resourceName, "auto_minor_version_upgrade", "false"),
-					resource.TestCheckResourceAttrSet(resourceName, "maintenance_window_start_time.0.day_of_week"),
-					resource.TestCheckTypeSetElemNestedAttrs(resourceName, "user.*", map[string]string{
-						"console_access": "false",
-						"groups.#":       "0",
-						"username":       "Test",
-						"password":       "TestTest1234",
-					}),
-					acctest.MatchResourceAttrRegionalARN(resourceName, "arn", "shield", regexp.MustCompile(`applicationlayerautomaticresponse:+.`)),
 				),
-			},
-			{
-				ResourceName:            resourceName,
-				ImportState:             true,
-				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"apply_immediately", "user"},
 			},
 		},
 	})
@@ -190,33 +56,24 @@ func TestAccShieldApplicationLayerAutomaticResponse_disappears(t *testing.T) {
 		t.Skip("skipping long-running test in short mode")
 	}
 
-	var applicationlayerautomaticresponse shield.DescribeApplicationLayerAutomaticResponseResponse
+	var applicationlayerautomaticresponse shield.DescribeProtectionOutput
 	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
 	resourceName := "aws_shield_application_layer_automatic_response.test"
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck: func() {
 			acctest.PreCheck(ctx, t)
-			acctest.PreCheckPartitionHasService(t, names.ShieldEndpointID)
-			testAccPreCheck(t)
+			testAccPreCheck(ctx, t)
 		},
-		ErrorCheck:               acctest.ErrorCheck(t, names.ShieldEndpointID),
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
 		CheckDestroy:             testAccCheckApplicationLayerAutomaticResponseDestroy(ctx),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccApplicationLayerAutomaticResponseConfig_basic(rName, testAccApplicationLayerAutomaticResponseVersionNewer),
+				Config: testAccApplicationLayerAutomaticResponseConfig_basic(rName),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckApplicationLayerAutomaticResponseExists(ctx, resourceName, &applicationlayerautomaticresponse),
-					// TIP: The Plugin-Framework disappears helper is similar to the Plugin-SDK version,
-					// but expects a new resource factory function as the third argument. To expose this
-					// private function to the testing package, you may need to add a line like the following
-					// to exports_test.go:
-					//
-					//   var ResourceApplicationLayerAutomaticResponse = newResourceApplicationLayerAutomaticResponse
 					acctest.CheckFrameworkResourceDisappears(ctx, acctest.Provider, tfshield.ResourceApplicationLayerAutomaticResponse, resourceName),
 				),
-				ExpectNonEmptyPlan: true,
 			},
 		},
 	})
@@ -224,26 +81,28 @@ func TestAccShieldApplicationLayerAutomaticResponse_disappears(t *testing.T) {
 
 func testAccCheckApplicationLayerAutomaticResponseDestroy(ctx context.Context) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
-		conn := acctest.Provider.Meta().(*conns.AWSClient).ShieldClient(ctx)
+		conn := acctest.Provider.Meta().(*conns.AWSClient).ShieldConn(ctx)
 
 		for _, rs := range s.RootModule().Resources {
 			if rs.Type != "aws_shield_application_layer_automatic_response" {
 				continue
 			}
 
-			input := &shield.DescribeApplicationLayerAutomaticResponseInput{
-				ApplicationLayerAutomaticResponseId: aws.String(rs.Primary.ID),
+			input := &shield.DescribeProtectionInput{
+				ResourceArn: aws.String(rs.Primary.ID),
 			}
-			_, err := conn.DescribeApplicationLayerAutomaticResponse(ctx, &shield.DescribeApplicationLayerAutomaticResponseInput{
-				ApplicationLayerAutomaticResponseId: aws.String(rs.Primary.ID),
-			})
-			if errs.IsA[*types.ResourceNotFoundException](err){
-				return nil
-			}
-			if err != nil {
+			resp, err := conn.DescribeProtectionWithContext(ctx, input)
+			if errs.IsA[*types.ResourceNotFoundException](err) {
 				return nil
 			}
 
+			if err != nil || resp == nil {
+				return nil
+			}
+
+			if resp != nil && *resp.Protection.ApplicationLayerAutomaticResponseConfiguration.Status == "DISABLED" {
+				return nil
+			}
 			return create.Error(names.Shield, create.ErrActionCheckingDestroyed, tfshield.ResNameApplicationLayerAutomaticResponse, rs.Primary.ID, errors.New("not destroyed"))
 		}
 
@@ -251,7 +110,7 @@ func testAccCheckApplicationLayerAutomaticResponseDestroy(ctx context.Context) r
 	}
 }
 
-func testAccCheckApplicationLayerAutomaticResponseExists(ctx context.Context, name string, applicationlayerautomaticresponse *shield.DescribeApplicationLayerAutomaticResponseResponse) resource.TestCheckFunc {
+func testAccCheckApplicationLayerAutomaticResponseExists(ctx context.Context, name string, applicationlayerautomaticresponse *shield.DescribeProtectionOutput) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		rs, ok := s.RootModule().Resources[name]
 		if !ok {
@@ -262,9 +121,9 @@ func testAccCheckApplicationLayerAutomaticResponseExists(ctx context.Context, na
 			return create.Error(names.Shield, create.ErrActionCheckingExistence, tfshield.ResNameApplicationLayerAutomaticResponse, name, errors.New("not set"))
 		}
 
-		conn := acctest.Provider.Meta().(*conns.AWSClient).ShieldClient(ctx)
-		resp, err := conn.DescribeApplicationLayerAutomaticResponse(ctx, &shield.DescribeApplicationLayerAutomaticResponseInput{
-			ApplicationLayerAutomaticResponseId: aws.String(rs.Primary.ID),
+		conn := acctest.Provider.Meta().(*conns.AWSClient).ShieldConn(ctx)
+		resp, err := conn.DescribeProtectionWithContext(ctx, &shield.DescribeProtectionInput{
+			ResourceArn: aws.String(rs.Primary.ID),
 		})
 
 		if err != nil {
@@ -277,53 +136,97 @@ func testAccCheckApplicationLayerAutomaticResponseExists(ctx context.Context, na
 	}
 }
 
-func testAccPreCheck(ctx context.Context, t *testing.T) {
-	conn := acctest.Provider.Meta().(*conns.AWSClient).ShieldClient(ctx)
-
-	input := &shield.ListApplicationLayerAutomaticResponsesInput{}
-	_, err := conn.ListApplicationLayerAutomaticResponses(ctx, input)
-
-	if acctest.PreCheckSkipError(err) {
-		t.Skipf("skipping acceptance testing: %s", err)
-	}
-	if err != nil {
-		t.Fatalf("unexpected PreCheck error: %s", err)
-	}
-}
-
-func testAccCheckApplicationLayerAutomaticResponseNotRecreated(before, after *shield.DescribeApplicationLayerAutomaticResponseResponse) resource.TestCheckFunc {
+func testAccCheckApplicationLayerAutomaticResponseNotRecreated(before, after *shield.DescribeProtectionOutput) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
-		if before, after := aws.ToString(before.ApplicationLayerAutomaticResponseId), aws.ToString(after.ApplicationLayerAutomaticResponseId); before != after {
-			return create.Error(names.Shield, create.ErrActionCheckingNotRecreated, tfshield.ResNameApplicationLayerAutomaticResponse, aws.ToString(before.ApplicationLayerAutomaticResponseId), errors.New("recreated"))
+		if before, after := aws.ToString(before.Protection.ApplicationLayerAutomaticResponseConfiguration.Status), aws.ToString(after.Protection.ApplicationLayerAutomaticResponseConfiguration.Status); before != after {
+			return create.Error(names.Shield, create.ErrActionCheckingNotRecreated, tfshield.ResNameApplicationLayerAutomaticResponse, before, errors.New("recreated"))
 		}
 
 		return nil
 	}
 }
 
-func testAccApplicationLayerAutomaticResponseConfig_basic(rName, version string) string {
+func testAccApplicationLayerAutomaticResponseConfig_basic(rName string) string {
 	return fmt.Sprintf(`
-resource "aws_security_group" "test" {
-  name = %[1]q
+resource "aws_wafv2_web_acl" "test" {
+  name        = %[1]q
+  description = %[1]q
+  scope       = "CLOUDFRONT"
+  default_action {
+    allow {}
+  }
+  visibility_config {
+    cloudwatch_metrics_enabled = false
+    metric_name                = %[1]q
+    sampled_requests_enabled   = false
+  }
+  lifecycle {
+    ignore_changes = [
+      rule,
+    ]
+  }
+}
+resource "aws_cloudfront_distribution" "test" {
+  origin {
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols = [
+        "TLSv1",
+        "TLSv1.1",
+        "TLSv1.2",
+      ]
+    }
+    # This is a fake origin and it's set to this name to indicate that.
+    domain_name = "%[1]s.com"
+    origin_id   = %[1]q
+  }
+  enabled             = false
+  wait_for_deployment = false
+  web_acl_id 		  =  aws_wafv2_web_acl.test.arn
+  default_cache_behavior {
+    allowed_methods  = ["HEAD", "DELETE", "POST", "GET", "OPTIONS", a"PUT", "PATCH"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = %[1]q
+    forwarded_values {
+      query_string = false
+      headers      = ["*"]
+      cookies {
+        forward = "none"
+      }
+    }
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 0
+    max_ttl                = 0
+  }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+  tags = {
+    Name = %[1]q
+  }
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
 }
 
+resource "aws_shield_protection" "test" {
+  name         = %[1]q
+  resource_arn = aws_cloudfront_distribution.test.arn
+}
 resource "aws_shield_application_layer_automatic_response" "test" {
-  application_layer_automatic_response_name             = %[1]q
-  engine_type             = "ActiveShield"
-  engine_version          = %[2]q
-  host_instance_type      = "shield.t2.micro"
-  security_groups         = [aws_security_group.test.id]
-  authentication_strategy = "simple"
-  storage_type            = "efs"
+  resource_arn = aws_cloudfront_distribution.test.arn
+  action = "COUNT"
 
-  logs {
-    general = true
-  }
-
-  user {
-    username = "Test"
-    password = "TestTest1234"
-  }
+  depends_on = [ 
+    aws_shield_protection.test,
+    aws_cloudfront_distribution.test,
+    aws_wafv2_web_acl.test
+  ]
 }
-`, rName, version)
+`, rName)
 }
--- website/docs/r/shield_application_layer_automatic_response.html.markdown
+++ website/docs/r/shield_application_layer_automatic_response.html.markdown
@@ -5,24 +5,28 @@ page_title: "AWS: aws_shield_application_layer_automatic_response"
 description: |-
   Terraform resource for managing an AWS Shield Application Layer Automatic Response.
 ---
-<!---
-TIP: A few guiding principles for writing documentation:
-1. Use simple language while avoiding jargon and figures of speech.
-2. Focus on brevity and clarity to keep a reader's attention.
-3. Use active voice and present tense whenever you can.
-4. Document your feature as it exists now; do not mention the future or past if you can help it.
-5. Use accessible and inclusive language.
---->`
+
 # Resource: aws_shield_application_layer_automatic_response
 
-Terraform resource for managing an AWS Shield Application Layer Automatic Response.
+Terraform resource for managing an AWS Shield Application Layer Automatic Response for automatic DDoS mitigation.
 
 ## Example Usage
 
 ### Basic Usage
 
 ```terraform
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
+data "aws_partition" "current" {}
+
+var "distribution_id" {
+  type : "string"
+  description : "The Cloudfront Distribution on which to enable the Application Layer Automatic Response.
+}
+
 resource "aws_shield_application_layer_automatic_response" "example" {
+  resource_arn = "arn:${data.aws_partition.current.partition}:cloudfront:${data.aws_caller_identity.current.account_id}:distribution/${distribution_id}
+  action = "COUNT"
 }
 ```
 
@@ -30,31 +34,10 @@ resource "aws_shield_application_layer_automatic_response" "example" {
 
 The following arguments are required:
 
-* `example_arg` - (Required) Concise argument description. Do not begin the description with "An", "The", "Defines", "Indicates", or "Specifies," as these are verbose. In other words, "Indicates the amount of storage," can be rewritten as "Amount of storage," without losing any information.
+* `resource_arn` - (Required) ARN of the resource to protect (Cloudfront Distributions and ALBs only at this time).
 
-The following arguments are optional:
-
-* `optional_arg` - (Optional) Concise argument description. Do not begin the description with "An", "The", "Defines", "Indicates", or "Specifies," as these are verbose. In other words, "Indicates the amount of storage," can be rewritten as "Amount of storage," without losing any information.
+* `action` - (Required) One of `COUNT` or `BLOCK` 
 
 ## Attribute Reference
 
-This resource exports the following attributes in addition to the arguments above:
-
-* `arn` - ARN of the Application Layer Automatic Response. Do not begin the description with "An", "The", "Defines", "Indicates", or "Specifies," as these are verbose. In other words, "Indicates the amount of storage," can be rewritten as "Amount of storage," without losing any information.
-* `example_attribute` - Concise description. Do not begin the description with "An", "The", "Defines", "Indicates", or "Specifies," as these are verbose. In other words, "Indicates the amount of storage," can be rewritten as "Amount of storage," without losing any information.
-
-## Timeouts
-
-[Configuration options](https://developer.hashicorp.com/terraform/language/resources/syntax#operation-timeouts):
-
-* `create` - (Default `60m`)
-* `update` - (Default `180m`)
-* `delete` - (Default `90m`)
-
-## Import
-
-Import Shield Application Layer Automatic Response using the `example_id_arg`. For example:
-
-```
-$ terraform import aws_shield_application_layer_automatic_response.example rft-8012925589
-```
+This resource exports no additional attributes.
