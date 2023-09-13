49c8dd79db35fe1b664d717872f7864fd22f8241
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-09-13 09:32:51 -0400

    r/aws_s3_bucket_accelerate_configuration: Migrate to AWS SDK for Go v2.

--- internal/service/s3/exports_test.go
+++ internal/service/s3/exports_test.go
@@ -5,9 +5,10 @@ package s3
 
 // Exports for use in tests only.
 var (
-	DeleteAllObjectVersions  = deleteAllObjectVersions
-	EmptyBucket              = emptyBucket
-	FindBucketPolicy         = findBucketPolicy
-	FindObjectByBucketAndKey = findObjectByBucketAndKey
-	SDKv1CompatibleCleanKey  = sdkv1CompatibleCleanKey
+	DeleteAllObjectVersions           = deleteAllObjectVersions
+	EmptyBucket                       = emptyBucket
+	FindBucketAccelerateConfiguration = findBucketAccelerateConfiguration
+	FindBucketPolicy                  = findBucketPolicy
+	FindObjectByBucketAndKey          = findObjectByBucketAndKey
+	SDKv1CompatibleCleanKey           = sdkv1CompatibleCleanKey
 )
