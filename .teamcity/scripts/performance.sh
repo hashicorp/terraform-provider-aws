cf396db962caa839920ed9815f902508bd792bea
Dirk Avery <dirk.avery@gmail.com>
2023-09-14 13:20:41 -0400

    Change settings

--- .teamcity/scripts/performance.sh
+++ .teamcity/scripts/performance.sh
@@ -56,21 +56,27 @@ function ssmtest {
 
 function analysis {
     go install github.com/google/pprof@latest
-    perf_main_memalloc1=$( pprof -top -flat -sample_index=alloc_space -unit=mb memvpc1.prof | head -3 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)MB total.*/\1/g' )
-    perf_main_meminuse1=$( pprof -top -flat -sample_index=inuse_space -unit=mb memvpc1.prof | head -3 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)MB total.*/\1/g' )
-    perf_main_cputime1=$( pprof -top -flat -sample_index=cpu cpuvpc1.prof | head -5 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)s total.*/\1/g' ) 2>/dev/null
+    #perf_main_memalloc1=$( pprof -top -flat -sample_index=alloc_space -unit=mb memvpcmain.prof | head -3 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)MB total.*/\1/g' )
+    perf_main_memalloc1=$( pprof -top -flat -sample_index=alloc_space -unit=mb memvpcmain.prof | head -3 )
+    echo "perf_main_memalloc1:$perf_main_memalloc1"
+    perf_main_memalloc1=$( echo -n $perf_main_memalloc1 | tr '\n' ' ' )
+    echo "perf_main_memalloc1:$perf_main_memalloc1"
+    perf_main_memalloc1=$( echo -n $perf_main_memalloc1 | sed -E 's/.*%% of ([0-9.]+)MB total.*/\1/g' )
+    echo "perf_main_memalloc1:$perf_main_memalloc1"
+    perf_main_meminuse1=$( pprof -top -flat -sample_index=inuse_space -unit=mb memvpcmain.prof | head -3 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)MB total.*/\1/g' )
+    perf_main_cputime1=$( pprof -top -flat -sample_index=cpu cpuvpcmain.prof | head -5 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)s total.*/\1/g' ) 2>/dev/null
 
-    perf_main_memalloc2=$( pprof -top -flat -sample_index=alloc_space -unit=mb memssm1.prof | head -3 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)MB total.*/\1/g' )
-    perf_main_meminuse2=$( pprof -top -flat -sample_index=inuse_space -unit=mb memssm1.prof | head -3 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)MB total.*/\1/g' )
-    perf_main_cputime2=$( pprof -top -flat -sample_index=cpu cpussm1.prof | head -5 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)s total.*/\1/g' ) 2>/dev/null
+    perf_main_memalloc2=$( pprof -top -flat -sample_index=alloc_space -unit=mb memssmmain.prof | head -3 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)MB total.*/\1/g' )
+    perf_main_meminuse2=$( pprof -top -flat -sample_index=inuse_space -unit=mb memssmmain.prof | head -3 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)MB total.*/\1/g' )
+    perf_main_cputime2=$( pprof -top -flat -sample_index=cpu cpussmmain.prof | head -5 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)s total.*/\1/g' ) 2>/dev/null
 
-    perf_latest_memalloc1=$( pprof -top -flat -sample_index=alloc_space -unit=mb memvpc2.prof | head -3 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)MB total.*/\1/g' )
-    perf_latest_meminuse1=$( pprof -top -flat -sample_index=inuse_space -unit=mb memvpc2.prof | head -3 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)MB total.*/\1/g' )
-    perf_latest_cputime1=$( pprof -top -flat -sample_index=cpu cpuvpc2.prof | head -5 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)s total.*/\1/g' ) 2>/dev/null
+    perf_latest_memalloc1=$( pprof -top -flat -sample_index=alloc_space -unit=mb memvpclatest.prof | head -3 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)MB total.*/\1/g' )
+    perf_latest_meminuse1=$( pprof -top -flat -sample_index=inuse_space -unit=mb memvpclatest.prof | head -3 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)MB total.*/\1/g' )
+    perf_latest_cputime1=$( pprof -top -flat -sample_index=cpu cpuvpclatest.prof | head -5 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)s total.*/\1/g' ) 2>/dev/null
 
-    perf_latest_memalloc2=$( pprof -top -flat -sample_index=alloc_space -unit=mb memssm2.prof | head -3 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)MB total.*/\1/g' )
-    perf_latest_meminuse2=$( pprof -top -flat -sample_index=inuse_space -unit=mb memssm2.prof | head -3 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)MB total.*/\1/g' )
-    perf_latest_cputime2=$( pprof -top -flat -sample_index=cpu cpussm2.prof | head -5 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)s total.*/\1/g' ) 2>/dev/null
+    perf_latest_memalloc2=$( pprof -top -flat -sample_index=alloc_space -unit=mb memssmlatest.prof | head -3 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)MB total.*/\1/g' )
+    perf_latest_meminuse2=$( pprof -top -flat -sample_index=inuse_space -unit=mb memssmlatest.prof | head -3 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)MB total.*/\1/g' )
+    perf_latest_cputime2=$( pprof -top -flat -sample_index=cpu cpussmlatest.prof | head -5 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)s total.*/\1/g' ) 2>/dev/null
 
     alloc=$( bc -l <<< "((${perf_main_memalloc1}/${perf_latest_memalloc1})-1) + ((${perf_main_memalloc2}/${perf_latest_memalloc2})-1)/2" )
     inuse=$( bc -l <<< "((${perf_main_meminuse1}/${perf_latest_meminuse1})-1) + ((${perf_main_meminuse2}/${perf_latest_meminuse2})-1)/2" )
@@ -81,28 +87,28 @@ function analysis {
     printf ";CPUtime:%%.4f%%%%\n" "${cputime}"
 }
 
-if [ -f "memvpc1.prof" -a -f "memssm1.prof" -a -f "memvpc2.prof" -a -f "memssm2.prof" ]; then
+if [ -f "memvpcmain.prof" -a -f "memssmmain.prof" -a -f "memvpclatest.prof" -a -f "memssmlatest.prof" ]; then
     echo "Tests complete. Analyzing results..."
     analysis
 fi
 
-if [ -f "memvpc1.prof" -a -f "memssm1.prof" -a -f "memvpc2.prof" -a ! -f "memssm2.prof" ]; then
-    echo "SSM 2 test not yet run. Running..."
-    ssmtest ssm2
+if [ -f "memvpcmain.prof" -a -f "memssmmain.prof" -a -f "memvpclatest.prof" -a ! -f "memssmlatest.prof" ]; then
+    echo "Running SSM latest version ($(basename $(curl -Ls -o /dev/null -w %%{url_effective} https://github.com/hashicorp/terraform-provider-aws/releases/latest))) test..."
+    ssmtest ssmlatest
 fi
 
-if [ -f "memvpc1.prof" -a -f "memssm1.prof" -a ! -f "memvpc2.prof" ]; then
-    echo "VPC 2 test not yet run. Running..."
+if [ -f "memvpcmain.prof" -a -f "memssmmain.prof" -a ! -f "memvpclatest.prof" ]; then
+    echo "Running VPC latest version ($(basename $(curl -Ls -o /dev/null -w %%{url_effective} https://github.com/hashicorp/terraform-provider-aws/releases/latest))) test..."
     git checkout $(basename $(curl -Ls -o /dev/null -w %%{url_effective} https://github.com/hashicorp/terraform-provider-aws/releases/latest))
-    vpctest vpc2
+    vpctest vpclatest
 fi
 
-if [ -f "memvpc1.prof" -a ! -f "memssm1.prof" ]; then
-    echo "SSM 1 test not yet run. Running..."
-    ssmtest ssm1
+if [ -f "memvpcmain.prof" -a ! -f "memssmmain.prof" ]; then
+    echo "Running SSM main branch test..."
+    ssmtest ssmmain
 fi
 
-if [ ! -f "memvpc1.prof" ]; then
-    echo "VPC 1 test not yet run. Running..."
-    vpctest vpc1
+if [ ! -f "memvpcmain.prof" ]; then
+    echo "Running VPC main branch test..."
+    vpctest vpcmain
 fi
