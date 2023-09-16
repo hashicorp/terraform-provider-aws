e24415482c186821b4dc4cafe3361b0f59aaf710
Dirk Avery <dirk.avery@gmail.com>
2023-09-15 21:29:20 -0400

    teamcity/sanity: Handle text file busy

--- .teamcity/scripts/sanity.sh
+++ .teamcity/scripts/sanity.sh
@@ -31,98 +31,94 @@ fi
 
 unset TF_LOG
 
+function tester {
+    local service=$1
+    local tests=$2
+
+    local results=`TF_ACC=1 go test ./internal/service/"${service}"/... -v -parallel 4 -run="${tests}" -timeout 60m 2>&1`
+    local exit_code=$?
+
+    if [[ "${results}" == *"text file busy"* ]]; then
+        echo "FAILED attempt to run tests: ${results}"
+        echo "Trying again..."
+        sleep 5
+        tester "${service}" "${tests}"
+    fi
+
+    if [ "${exit_code}" -ne 0 ]; then
+        echo "${results}"
+        exit "${exit_code}"
+    fi
+}
+
 if [ ! -f "iamsanity.test" ]; then
-    TF_ACC=1 go test \
-        ./internal/service/iam/... \
-        -v -parallel 10 -run='TestAccIAMRole_basic|TestAccIAMRole_namePrefix|TestAccIAMRole_disappears|TestAccIAMRole_InlinePolicy_basic|TestAccIAMPolicyDocumentDataSource_basic|TestAccIAMPolicyDocumentDataSource_sourceConflicting|TestAccIAMPolicyDocumentDataSource_sourceJSONValidJSON|TestAccIAMRolePolicyAttachment_basic|TestAccIAMRolePolicyAttachment_disappears|TestAccIAMRolePolicyAttachment_Disappears_role|TestAccIAMPolicy_basic|TestAccIAMPolicy_policy|TestAccIAMPolicy_tags|TestAccIAMRolePolicy_basic|TestAccIAMRolePolicy_unknownsInPolicy|TestAccIAMInstanceProfile_basic|TestAccIAMInstanceProfile_tags' -timeout 60m
+    tester "iam" 'TestAccIAMRole_basic|TestAccIAMRole_namePrefix|TestAccIAMRole_disappears|TestAccIAMRole_InlinePolicy_basic|TestAccIAMPolicyDocumentDataSource_basic|TestAccIAMPolicyDocumentDataSource_sourceConflicting|TestAccIAMPolicyDocumentDataSource_sourceJSONValidJSON|TestAccIAMRolePolicyAttachment_basic|TestAccIAMRolePolicyAttachment_disappears|TestAccIAMRolePolicyAttachment_Disappears_role|TestAccIAMPolicy_basic|TestAccIAMPolicy_policy|TestAccIAMPolicy_tags|TestAccIAMRolePolicy_basic|TestAccIAMRolePolicy_unknownsInPolicy|TestAccIAMInstanceProfile_basic|TestAccIAMInstanceProfile_tags'
     touch iamsanity.test
     exit 0
 fi
 
 if [ ! -f "logssanity.test" ]; then
-    TF_ACC=1 go test \
-        ./internal/service/logs/... \
-        -v -parallel 10 -run='TestAccLogsGroup_basic|TestAccLogsGroup_multiple' -timeout 60m
+    tester "logs" 'TestAccLogsGroup_basic|TestAccLogsGroup_multiple'
     touch logssanity.test
     exit 0
 fi
 
 if [ ! -f "ec2sanity.test" ]; then
-    TF_ACC=1 go test \
-        ./internal/service/ec2/... \
-        -v -parallel 10 -run='TestAccVPCSecurityGroup_basic|TestAccVPCSecurityGroup_ipRangesWithSameRules|TestAccVPCSecurityGroup_vpcAllEgress|TestAccVPCSecurityGroupRule_race|TestAccVPCSecurityGroupRule_protocolChange|TestAccVPCDataSource_basic|TestAccVPCSubnet_basic|TestAccVPC_tenancy|TestAccVPCRouteTableAssociation_Subnet_basic|TestAccVPCRouteTable_basic' -timeout 60m
+    tester "ec2" 'TestAccVPCSecurityGroup_basic|TestAccVPCSecurityGroup_ipRangesWithSameRules|TestAccVPCSecurityGroup_vpcAllEgress|TestAccVPCSecurityGroupRule_race|TestAccVPCSecurityGroupRule_protocolChange|TestAccVPCDataSource_basic|TestAccVPCSubnet_basic|TestAccVPC_tenancy|TestAccVPCRouteTableAssociation_Subnet_basic|TestAccVPCRouteTable_basic'
     touch ec2sanity.test
     exit 0
 fi
 
 if [ ! -f "ecssanity.test" ]; then
-    TF_ACC=1 go test \
-        ./internal/service/ecs/... \
-        -v -parallel 10 -run='TestAccECSTaskDefinition_basic|TestAccECSService_basic' -timeout 60m
+    tester "ecs" 'TestAccECSTaskDefinition_basic|TestAccECSService_basic'
     touch ecssanity.test
     exit 0
 fi
 
 if [ ! -f "elbv2sanity.test" ]; then
-    TF_ACC=1 go test \
-        ./internal/service/elbv2/... \
-        -v -parallel 10 -run='TestAccELBV2TargetGroup_basic' -timeout 60m
+    tester "elbv2" 'TestAccELBV2TargetGroup_basic'
     touch elbv2sanity.test
     exit 0
 fi
 
 if [ ! -f "kmssanity.test" ]; then
-    TF_ACC=1 go test \
-        ./internal/service/kms/... \
-        -v -parallel 10 -run='TestAccKMSKey_basic' -timeout 60m
+    tester "kms" 'TestAccKMSKey_basic'
     touch kmssanity.test
     exit 0
 fi
 
 if [ ! -f "lambdasanity.test" ]; then
-    TF_ACC=1 go test \
-        ./internal/service/lambda/... \
-        -v -parallel 10 -run='TestAccLambdaFunction_basic|TestAccLambdaPermission_basic' -timeout 60m
+    tester "lambda" 'TestAccLambdaFunction_basic|TestAccLambdaPermission_basic'
     touch lambdasanity.test
     exit 0
 fi
 
 if [ ! -f "metasanity.test" ]; then
-    TF_ACC=1 go test \
-        ./internal/service/meta/... \
-        -v -parallel 10 -run='TestAccMetaRegionDataSource_basic|TestAccMetaRegionDataSource_endpoint|TestAccMetaPartitionDataSource_basic' -timeout 60m
+    tester "meta" 'TestAccMetaRegionDataSource_basic|TestAccMetaRegionDataSource_endpoint|TestAccMetaPartitionDataSource_basic'
     touch metasanity.test
     exit 0
 fi
 
 if [ ! -f "route53sanity.test" ]; then
-    TF_ACC=1 go test \
-        ./internal/service/route53/... \
-        -v -parallel 10 -run='TestAccRoute53Record_basic|TestAccRoute53Record_Latency_basic|TestAccRoute53ZoneDataSource_name' -timeout 60m
+    tester "route53" 'TestAccRoute53Record_basic|TestAccRoute53Record_Latency_basic|TestAccRoute53ZoneDataSource_name'
     touch route53sanity.test
     exit 0
 fi
 
 if [ ! -f "s3sanity.test" ]; then
-    TF_ACC=1 go test \
-        ./internal/service/s3/... \
-        -v -parallel 10 -run='TestAccS3Bucket_Basic_basic|TestAccS3Bucket_Security_corsUpdate|TestAccS3BucketPublicAccessBlock_basic|TestAccS3BucketPolicy_basic|TestAccS3BucketACL_updateACL' -timeout 60m
+    tester "s3" 'TestAccS3Bucket_Basic_basic|TestAccS3Bucket_Security_corsUpdate|TestAccS3BucketPublicAccessBlock_basic|TestAccS3BucketPolicy_basic|TestAccS3BucketACL_updateACL'
     touch s3sanity.test
     exit 0
 fi
 
 if [ ! -f "secretsmanagersanity.test" ]; then
-    TF_ACC=1 go test \
-        ./internal/service/secretsmanager/... \
-        -v -parallel 10 -run='TestAccSecretsManagerSecret_basic' -timeout 60m
+    tester "secretsmanager" 'TestAccSecretsManagerSecret_basic'
     touch secretsmanagersanity.test
     exit 0
 fi
 
 if [ ! -f "stssanity.test" ]; then
-    TF_ACC=1 go test \
-        ./internal/service/sts/... \
-        -v -parallel 10 -run='TestAccSTSCallerIdentityDataSource_basic' -timeout 60m
+    tester "sts" 'TestAccSTSCallerIdentityDataSource_basic'
     touch stssanity.test
     exit 0
 fi
