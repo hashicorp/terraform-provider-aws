fb33f0ecf39f97ff8bdf8ea45674816844f094b0
Piotr Kostrzeński <piotr.kostrzenski@gmail.com>
2023-09-17 16:12:56 +0200

    add example configured table

--- examples/cleanrooms/main.tf
+++ examples/cleanrooms/main.tf
@@ -32,5 +32,24 @@ resource "aws_cleanrooms_collaboration" "test_collab" {
   tags = {
     Project = "Terraform"
   }
+}
 
+resource "aws_cleanrooms_configured_table" "test_configured_table" {
+  name            = "terraform-example-table"
+  description     = "I made this table with terraform!"
+  analysis_method = "DIRECT_QUERY"
+  allowed_columns = [
+    "column1",
+    "column2",
+    "column3",
+  ]
+
+  table_reference {
+    database_name = "example_database"
+    table_name    = "example_table"
+  }
+
+  tags = {
+    Project = "Terraform"
+  }
 }
