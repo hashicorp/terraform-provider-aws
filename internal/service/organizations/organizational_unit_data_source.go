f35b873ca1fe8178939fd8302131c336e28b8963
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-09-19 16:07:04 -0400

    organizations: Rationalize 'findOrganizationalUnitsForParent' and friends.

--- internal/service/organizations/organizational_unit_data_source.go
+++ internal/service/organizations/organizational_unit_data_source.go
@@ -8,46 +8,36 @@ import (
 
 	"github.com/YakDriver/regexache"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/organizations"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
-	"github.com/hashicorp/terraform-provider-aws/internal/create"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
-	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
 // @SDKDataSource("aws_organizations_organizational_unit", name="Organizational Unit")
 func DataSourceOrganizationalUnit() *schema.Resource {
 	return &schema.Resource{
 		ReadWithoutTimeout: dataSourceOrganizationalUnitRead,
+
 		Schema: map[string]*schema.Schema{
-			"name": {
-				Type:     schema.TypeString,
-				Required: true,
-				ForceNew: true,
-			},
-			"parent_id": {
-				Type:         schema.TypeString,
-				Required:     true,
-				ForceNew:     true,
-				ValidateFunc: validation.StringMatch(regexache.MustCompile("^(r-[0-9a-z]{4,32})|(ou-[0-9a-z]{4,32}-[0-9a-z]{8,32})$"), "see https://docs.aws.amazon.com/organizations/latest/APIReference/API_CreateOrganizationalUnit.html#organizations-CreateOrganizationalUnit-request-ParentId"),
-			},
 			"arn": {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
-			"id": {
+			"name": {
 				Type:     schema.TypeString,
-				Computed: true,
+				Required: true,
+			},
+			"parent_id": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validation.StringMatch(regexache.MustCompile("^(r-[0-9a-z]{4,32})|(ou-[0-9a-z]{4,32}-[0-9a-z]{8,32})$"), "see https://docs.aws.amazon.com/organizations/latest/APIReference/API_CreateOrganizationalUnit.html#organizations-CreateOrganizationalUnit-request-ParentId"),
 			},
 		},
 	}
 }
-
-const (
-	DSNameOrganizationalUnit = "Organizational Unit Data Source"
-)
 
 func dataSourceOrganizationalUnitRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
@@ -55,22 +45,20 @@ func dataSourceOrganizationalUnitRead(ctx context.Context, d *schema.ResourceDat
 
 	name := d.Get("name").(string)
 	parentID := d.Get("parent_id").(string)
+	input := &organizations.ListOrganizationalUnitsForParentInput{
+		ParentId: aws.String(parentID),
+	}
 
-	output, err := findOrganizationalUnitsForParent(ctx, conn, parentID)
+	ou, err := findOrganizationalUnitForParent(ctx, conn, input, func(v *organizations.OrganizationalUnit) bool {
+		return aws.StringValue(v.Name) == name
+	})
+
 	if err != nil {
-		return append(diags, create.DiagError(names.Organizations, create.ErrActionReading, DSNameOrganizationalUnit, name, err)...)
-	}
-	if len(output) == 0 {
-		return sdkdiag.AppendErrorf(diags, "Organizational parent not found (%s)", parentID)
+		return sdkdiag.AppendErrorf(diags, "reading Organizations Organizational Unit (%s/%s): %s", parentID, name, err)
 	}
 
-	for _, v := range output {
-		if v.Name != nil && aws.StringValue(v.Name) == name && v.Id != nil {
-			d.SetId(aws.StringValue(v.Id))
-			d.Set("arn", v.Arn)
-			return diags
-		}
-	}
+	d.SetId(aws.StringValue(ou.Id))
+	d.Set("arn", ou.Arn)
 
-	return sdkdiag.AppendErrorf(diags, "No matching organization for (%s): %s", parentID, name)
+	return diags
 }
