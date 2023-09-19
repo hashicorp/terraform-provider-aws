291abd0833d48c912e79525647404bb9c397200b
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-09-19 08:44:03 -0400

    r/aws_opensearch_package_association: Use 'FindPackageAssociationByTwoPartKey'.

--- internal/service/opensearch/package_association.go
+++ internal/service/opensearch/package_association.go
@@ -12,17 +12,20 @@ import (
 	"github.com/aws/aws-sdk-go/service/opensearchservice"
 	"github.com/hashicorp/aws-sdk-go-base/v2/awsv1shim/v2/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
+	tfslices "github.com/hashicorp/terraform-provider-aws/internal/slices"
+	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 )
 
 // @SDKResource("aws_opensearch_package_association")
 func ResourcePackageAssociation() *schema.Resource {
 	return &schema.Resource{
-		CreateWithoutTimeout: ResourcePackageAssociationCreate,
-		ReadWithoutTimeout:   ResourcePackageAssociationRead,
-		DeleteWithoutTimeout: ResourcePackageAssociationDelete,
+		CreateWithoutTimeout: resourcePackageAssociationCreate,
+		ReadWithoutTimeout:   resourcePackageAssociationRead,
+		DeleteWithoutTimeout: resourcePackageAssociationDelete,
 
 		Importer: &schema.ResourceImporter{
 			StateContext: schema.ImportStatePassthroughContext,
@@ -47,49 +50,57 @@ func ResourcePackageAssociation() *schema.Resource {
 	}
 }
 
-func ResourcePackageAssociationCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+func resourcePackageAssociationCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
 	conn := meta.(*conns.AWSClient).OpenSearchConn(ctx)
 
+	domainName := d.Get("domain_name").(string)
+	packageID := d.Get("package_id").(string)
+	id := fmt.Sprintf("%s-%s", domainName, packageID)
 	input := &opensearchservice.AssociatePackageInput{
-		DomainName: aws.String(d.Get("domain_name").(string)),
-		PackageID:  aws.String(d.Get("package_id").(string)),
+		DomainName: aws.String(domainName),
+		PackageID:  aws.String(packageID),
 	}
 
-	output, err := conn.AssociatePackageWithContext(ctx, input)
+	_, err := conn.AssociatePackageWithContext(ctx, input)
 
 	if err != nil {
-		return sdkdiag.AppendErrorf(diags, "associating OpenSearch package: %s", err)
+		return sdkdiag.AppendErrorf(diags, "creating OpenSearch Package Association (%s): %s", id, err)
 	}
 
-	d.SetId(fmt.Sprintf("package-association:%s-%s", aws.StringValue(output.DomainPackageDetails.DomainName), aws.StringValue(output.DomainPackageDetails.PackageID)))
+	d.SetId(id)
+
+	return append(diags, resourcePackageAssociationRead(ctx, d, meta)...)
+}
+
+func resourcePackageAssociationRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	conn := meta.(*conns.AWSClient).OpenSearchConn(ctx)
+
+	pkgAssociation, err := FindPackageAssociationByTwoPartKey(ctx, conn, d.Get("domain_name").(string), d.Get("package_id").(string))
+
+	if !d.IsNewResource() && tfresource.NotFound(err) {
+		log.Printf("[WARN] OpenSearch Package Association (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return diags
+	}
+
+	if err != nil {
+		return sdkdiag.AppendErrorf(diags, "reading OpenSearch Package Association (%s): %s", d.Id(), err)
+	}
+
+	d.Set("domain_name", pkgAssociation.DomainName)
+	d.Set("package_id", pkgAssociation.PackageID)
+	d.Set("reference_path", pkgAssociation.ReferencePath)
 
 	return diags
 }
 
-func ResourcePackageAssociationRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+func resourcePackageAssociationDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
 	conn := meta.(*conns.AWSClient).OpenSearchConn(ctx)
 
-	var domainName = d.Get("domain_name").(string)
-	var packageID = d.Get("package_id").(string)
-
-	output, err := getAssociatedDomainPackage(ctx, domainName, packageID, conn)
-	if err != nil {
-		return sdkdiag.AppendErrorf(diags, "listing OpenSearch Package for domain (%s): %s", domainName, err)
-	}
-
-	d.Set("package_id", output.PackageID)
-	d.Set("package_type", output.ReferencePath)
-	d.Set("available_package_version", output.PackageVersion)
-	return diags
-}
-
-func ResourcePackageAssociationDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	var diags diag.Diagnostics
-	conn := meta.(*conns.AWSClient).OpenSearchConn(ctx)
-
-	log.Printf("[DEBUG] Deleting OpenSearch Package: %s", d.Id())
+	log.Printf("[DEBUG] Deleting OpenSearch Package Association: %s", d.Id())
 	_, err := conn.DissociatePackageWithContext(ctx, &opensearchservice.DissociatePackageInput{
 		PackageID: aws.String(d.Id()),
 	})
@@ -99,33 +110,60 @@ func ResourcePackageAssociationDelete(ctx context.Context, d *schema.ResourceDat
 	}
 
 	if err != nil {
-		return sdkdiag.AppendErrorf(diags, "dissociating OpenSearch Package (%s): %s", d.Id(), err)
+		return sdkdiag.AppendErrorf(diags, "deleting OpenSearch Package Association (%s): %s", d.Id(), err)
 	}
 
 	return diags
 }
 
-func getAssociatedDomainPackage(ctx context.Context, domainID, packageID string, conn *opensearchservice.OpenSearchService) (*opensearchservice.DomainPackageDetails, error) {
+func FindPackageAssociationByTwoPartKey(ctx context.Context, conn *opensearchservice.OpenSearchService, domainName, packageID string) (*opensearchservice.DomainPackageDetails, error) {
 	input := &opensearchservice.ListPackagesForDomainInput{
-		DomainName: aws.String(domainID),
+		DomainName: aws.String(domainName),
 	}
-	for {
-		resp, err := conn.ListPackagesForDomainWithContext(ctx, input)
-		if err != nil {
-			return nil, err
+	filter := func(v *opensearchservice.DomainPackageDetails) bool {
+		return aws.StringValue(v.PackageID) == packageID
+	}
+
+	return findPackageAssociation(ctx, conn, input, filter)
+}
+
+func findPackageAssociation(ctx context.Context, conn *opensearchservice.OpenSearchService, input *opensearchservice.ListPackagesForDomainInput, filter tfslices.Predicate[*opensearchservice.DomainPackageDetails]) (*opensearchservice.DomainPackageDetails, error) {
+	output, err := findPackageAssociations(ctx, conn, input, filter)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return tfresource.AssertSinglePtrResult(output)
+}
+
+func findPackageAssociations(ctx context.Context, conn *opensearchservice.OpenSearchService, input *opensearchservice.ListPackagesForDomainInput, filter tfslices.Predicate[*opensearchservice.DomainPackageDetails]) ([]*opensearchservice.DomainPackageDetails, error) {
+	var output []*opensearchservice.DomainPackageDetails
+
+	err := conn.ListPackagesForDomainPagesWithContext(ctx, input, func(page *opensearchservice.ListPackagesForDomainOutput, lastPage bool) bool {
+		if page == nil {
+			return !lastPage
 		}
-		if len(resp.DomainPackageDetailsList) == 0 {
-			return nil, fmt.Errorf("no packages found for domain %s", domainID)
-		}
-		for _, domainPackageDetails := range resp.DomainPackageDetailsList {
-			if aws.StringValue(domainPackageDetails.PackageID) == packageID {
-				return domainPackageDetails, nil
+
+		for _, v := range page.DomainPackageDetailsList {
+			if v != nil && filter(v) {
+				output = append(output, v)
 			}
 		}
-		if resp.NextToken == nil {
-			break
+
+		return !lastPage
+	})
+
+	if tfawserr.ErrCodeEquals(err, opensearchservice.ErrCodeResourceNotFoundException) {
+		return nil, &retry.NotFoundError{
+			LastError:   err,
+			LastRequest: input,
 		}
-		input.NextToken = resp.NextToken
 	}
-	return nil, nil
+
+	if err != nil {
+		return nil, err
+	}
+
+	return output, nil
 }
--- internal/service/opensearch/package_association_test.go
+++ internal/service/opensearch/package_association_test.go
@@ -4,20 +4,24 @@
 package opensearch_test
 
 import (
+	"context"
 	"fmt"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/service/opensearchservice"
 	sdkacctest "github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	tfopensearch "github.com/hashicorp/terraform-provider-aws/internal/service/opensearch"
+	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 )
 
 func TestAccOpenSearchPackageAssociation_basic(t *testing.T) {
 	ctx := acctest.Context(t)
-	ri := sdkacctest.RandString(10)
-	name := fmt.Sprintf("tf-test-%s", ri)
+	domainName := testAccRandomDomainName()
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
 	resourceName := "aws_opensearch_package_association.test"
 	packageResourceName := "aws_opensearch_package.test"
 	domainResourceName := "aws_opensearch_domain.test"
@@ -26,23 +30,15 @@ func TestAccOpenSearchPackageAssociation_basic(t *testing.T) {
 		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
 		ErrorCheck:               acctest.ErrorCheck(t, opensearchservice.EndpointsID),
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
-		CheckDestroy:             testAccCheckDomainDestroy(ctx),
+		CheckDestroy:             testAccCheckPackageAssociationDestroy(ctx),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccPackageAssociationConfig(name),
+				Config: testAccPackageAssociationConfig_basic(rName, domainName),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(resourceName, "package_name", name),
-					resource.TestCheckResourceAttrPair(resourceName, "package_id", packageResourceName, "id"),
+					testAccCheckPackageAssociationExists(ctx, resourceName),
 					resource.TestCheckResourceAttrPair(resourceName, "domain_name", domainResourceName, "domain_name"),
+					resource.TestCheckResourceAttrPair(resourceName, "package_id", packageResourceName, "id"),
 				),
-			},
-			{
-				ResourceName:      resourceName,
-				ImportState:       true,
-				ImportStateVerify: true,
-				ImportStateVerifyIgnore: []string{
-					"package_source", // This isn't returned by the API
-				},
 			},
 		},
 	})
@@ -50,19 +46,20 @@ func TestAccOpenSearchPackageAssociation_basic(t *testing.T) {
 
 func TestAccOpenSearchPackageAssociation_disappears(t *testing.T) {
 	ctx := acctest.Context(t)
-	ri := sdkacctest.RandString(10)
-	name := fmt.Sprintf("tf-test-%s", ri)
+	domainName := testAccRandomDomainName()
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
 	resourceName := "aws_opensearch_package_association.test"
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
 		ErrorCheck:               acctest.ErrorCheck(t, opensearchservice.EndpointsID),
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
-		CheckDestroy:             testAccCheckDomainDestroy(ctx),
+		CheckDestroy:             testAccCheckPackageAssociationDestroy(ctx),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccPackageAssociationConfig(name),
+				Config: testAccPackageAssociationConfig_basic(rName, domainName),
 				Check: resource.ComposeTestCheckFunc(
+					testAccCheckPackageAssociationExists(ctx, resourceName),
 					acctest.CheckResourceDisappears(ctx, acctest.Provider, tfopensearch.ResourcePackage(), resourceName),
 				),
 				ExpectNonEmptyPlan: true,
@@ -71,30 +68,71 @@ func TestAccOpenSearchPackageAssociation_disappears(t *testing.T) {
 	})
 }
 
-func testAccPackageAssociationConfig(name string) string {
-	return fmt.Sprintf(`
-resource "aws_s3_bucket" "opensearch_packages" {
-  bucket = "%s"
+func testAccCheckPackageAssociationExists(ctx context.Context, n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		conn := acctest.Provider.Meta().(*conns.AWSClient).OpenSearchConn(ctx)
+
+		_, err := tfopensearch.FindPackageAssociationByTwoPartKey(ctx, conn, rs.Primary.Attributes["domain_name"], rs.Primary.Attributes["package_id"])
+
+		return err
+	}
 }
 
-resource "aws_s3_object" "example_txt" {
-  bucket = aws_s3_bucket.opensearch_packages.bucket
-  key    = "%s"
+func testAccCheckPackageAssociationDestroy(ctx context.Context) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		for _, rs := range s.RootModule().Resources {
+			if rs.Type != "aws_opensearch_package_association" {
+				continue
+			}
+
+			conn := acctest.Provider.Meta().(*conns.AWSClient).OpenSearchConn(ctx)
+
+			_, err := tfopensearch.FindPackageAssociationByTwoPartKey(ctx, conn, rs.Primary.Attributes["domain_name"], rs.Primary.Attributes["package_id"])
+
+			if tfresource.NotFound(err) {
+				continue
+			}
+
+			if err != nil {
+				return err
+			}
+
+			return fmt.Errorf("OpenSearch Package Association %s still exists", rs.Primary.ID)
+		}
+
+		return nil
+	}
+}
+
+func testAccPackageAssociationConfig_basic(rName, domainName string) string {
+	return fmt.Sprintf(`
+resource "aws_s3_bucket" "test" {
+  bucket = %[1]q
+}
+
+resource "aws_s3_object" "test" {
+  bucket = aws_s3_bucket.test.bucket
+  key    = %[1]q
   source = "./test-fixtures/example-opensearch-custom-package.txt"
   etag   = filemd5("./test-fixtures/example-opensearch-custom-package.txt")
 }
 
 resource "aws_opensearch_package" "test" {
-  package_name = "%s"
+  package_name = %[1]q
   package_source {
-    s3_bucket_name = aws_s3_bucket.opensearch_packages.bucket
-    s3_key         = aws_s3_object.example_txt.key
+    s3_bucket_name = aws_s3_bucket.test.bucket
+    s3_key         = aws_s3_object.test.key
   }
   package_type = "TXT-DICTIONARY"
 }
 
 resource "aws_opensearch_domain" "test" {
-  domain_name = "%s"
+  domain_name = %[2]q
 
   cluster_config {
     instance_type = "t3.small.search" # supported in both aws and aws-us-gov
@@ -110,5 +148,5 @@ resource "aws_opensearch_package_association" "test" {
   package_id  = aws_opensearch_package.test.id
   domain_name = aws_opensearch_domain.test.domain_name
 }
-`, name, name, name, name)
+`, rName, domainName)
 }
