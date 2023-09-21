2d5da82287ae32a28ff2b30370adbf41d61d9065
Luqman Mohammed <m.luqman077@gmail.com>
2023-09-21 10:59:58 +0100

    Update wafv2 flex expand custom keys aggregate on ratebased statements

--- internal/service/wafv2/flex.go
+++ internal/service/wafv2/flex.go
@@ -1279,6 +1279,109 @@ func expandStatusCode(tfList []interface{}) *wafv2.ResponseInspectionStatusCode 
 	return &out
 }
 
+func expandRateLimitCookie(l []interface{}) *wafv2.RateLimitCookie {
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+	m := l[0].(map[string]interface{})
+	return &wafv2.RateLimitCookie{
+		Name:                aws.String(m["name"].(string)),
+		TextTransformations: expandTextTransformations(m["text_transformation"].(*schema.Set).List()),
+	}
+}
+
+func expandRateLimitHeader(l []interface{}) *wafv2.RateLimitHeader {
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+	m := l[0].(map[string]interface{})
+	return &wafv2.RateLimitHeader{
+		Name:                aws.String(m["name"].(string)),
+		TextTransformations: expandTextTransformations(m["text_transformation"].(*schema.Set).List()),
+	}
+}
+
+func expandRateLimitLabelNamespace(l []interface{}) *wafv2.RateLimitLabelNamespace {
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+	m := l[0].(map[string]interface{})
+	return &wafv2.RateLimitLabelNamespace{
+		Namespace: aws.String(m["namespace"].(string)),
+	}
+}
+
+func expandRateLimitQueryArgument(l []interface{}) *wafv2.RateLimitQueryArgument {
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+	m := l[0].(map[string]interface{})
+	return &wafv2.RateLimitQueryArgument{
+		Name:                aws.String(m["name"].(string)),
+		TextTransformations: expandTextTransformations(m["text_transformation"].(*schema.Set).List()),
+	}
+}
+
+func expandRateLimitQueryString(l []interface{}) *wafv2.RateLimitQueryString {
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+	m := l[0].(map[string]interface{})
+	return &wafv2.RateLimitQueryString{
+		TextTransformations: expandTextTransformations(m["text_transformation"].(*schema.Set).List()),
+	}
+}
+
+func expandRateLimitUriPath(l []interface{}) *wafv2.RateLimitUriPath {
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+	m := l[0].(map[string]interface{})
+	return &wafv2.RateLimitUriPath{
+		TextTransformations: expandTextTransformations(m["text_transformation"].(*schema.Set).List()),
+	}
+}
+
+func expandRateBasedStatementCustomKeys(l []interface{}) []*wafv2.RateBasedStatementCustomKey {
+	if len(l) == 0 {
+		return nil
+	}
+	out := make([]*wafv2.RateBasedStatementCustomKey, 0)
+	for _, ck := range l {
+		r := &wafv2.RateBasedStatementCustomKey{}
+		m := ck.(map[string]interface{})
+		if v, ok := m["cookie"]; ok {
+			r.Cookie = expandRateLimitCookie(v.([]interface{}))
+		}
+		if v, ok := m["forwarded_ip"]; ok && len(v.([]interface{})) > 0 {
+			r.ForwardedIP = &wafv2.RateLimitForwardedIP{}
+		}
+		if v, ok := m["http_method"]; ok && len(v.([]interface{})) > 0 {
+			r.HTTPMethod = &wafv2.RateLimitHTTPMethod{}
+		}
+		if v, ok := m["header"]; ok {
+			r.Header = expandRateLimitHeader(v.([]interface{}))
+		}
+		if v, ok := m["ip"]; ok && len(v.([]interface{})) > 0 {
+			r.IP = &wafv2.RateLimitIP{}
+		}
+		if v, ok := m["label_namespace"]; ok {
+			r.LabelNamespace = expandRateLimitLabelNamespace(v.([]interface{}))
+		}
+		if v, ok := m["query_argument"]; ok {
+			r.QueryArgument = expandRateLimitQueryArgument(v.([]interface{}))
+		}
+		if v, ok := m["query_string"]; ok {
+			r.QueryString = expandRateLimitQueryString(v.([]interface{}))
+		}
+		if v, ok := m["uri_path"]; ok {
+			r.UriPath = expandRateLimitUriPath(v.([]interface{}))
+		}
+		out = append(out, r)
+	}
+	return out
+}
+
 func expandRateBasedStatement(l []interface{}) *wafv2.RateBasedStatement {
 	if len(l) == 0 || l[0] == nil {
 		return nil
@@ -1292,6 +1395,10 @@ func expandRateBasedStatement(l []interface{}) *wafv2.RateBasedStatement {
 
 	if v, ok := m["forwarded_ip_config"]; ok {
 		r.ForwardedIPConfig = expandForwardedIPConfig(v.([]interface{}))
+	}
+
+	if v, ok := m["custom_keys"]; ok {
+		r.CustomKeys = expandRateBasedStatementCustomKeys(v.([]interface{}))
 	}
 
 	s := m["scope_down_statement"].([]interface{})
