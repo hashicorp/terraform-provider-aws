9f1aa8730b40bcb2aafd8f78bcb198828e2b5e8f
Jared Baker <jared.baker@hashicorp.com>
2023-09-27 10:24:06 -0400

    aws_servicequotas: update to aws sdk v2

--- internal/service/servicequotas/service_package_gen.go
+++ internal/service/servicequotas/service_package_gen.go
@@ -5,9 +5,8 @@ package servicequotas
 import (
 	"context"
 
-	aws_sdkv1 "github.com/aws/aws-sdk-go/aws"
-	session_sdkv1 "github.com/aws/aws-sdk-go/aws/session"
-	servicequotas_sdkv1 "github.com/aws/aws-sdk-go/service/servicequotas"
+	aws_sdkv2 "github.com/aws/aws-sdk-go-v2/aws"
+	servicequotas_sdkv2 "github.com/aws/aws-sdk-go-v2/service/servicequotas"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/types"
 	"github.com/hashicorp/terraform-provider-aws/names"
@@ -49,11 +48,15 @@ func (p *servicePackage) ServicePackageName() string {
 	return names.ServiceQuotas
 }
 
-// NewConn returns a new AWS SDK for Go v1 client for this service package's AWS API.
-func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*servicequotas_sdkv1.ServiceQuotas, error) {
-	sess := config["session"].(*session_sdkv1.Session)
+// NewClient returns a new AWS SDK for Go v2 client for this service package's AWS API.
+func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (*servicequotas_sdkv2.Client, error) {
+	cfg := *(config["aws_sdkv2_config"].(*aws_sdkv2.Config))
 
-	return servicequotas_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config["endpoint"].(string))})), nil
+	return servicequotas_sdkv2.NewFromConfig(cfg, func(o *servicequotas_sdkv2.Options) {
+		if endpoint := config["endpoint"].(string); endpoint != "" {
+			o.BaseEndpoint = aws_sdkv2.String(endpoint)
+		}
+	}), nil
 }
 
 func ServicePackage(ctx context.Context) conns.ServicePackage {
