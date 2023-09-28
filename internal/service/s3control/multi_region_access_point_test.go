43fcc479c50e36f579362b9fc4609f624ef9c62f
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-09-28 16:11:55 -0400

    Update multi_region_access_point_test.go

--- internal/service/s3control/multi_region_access_point_test.go
+++ internal/service/s3control/multi_region_access_point_test.go
@@ -418,7 +418,7 @@ resource "aws_s3_object" "test" {
   }
 }
 
-# Ensure that we can GET throught the bucket.
+# Ensure that we can GET through the bucket.
 data "aws_s3_object" "test" {
   bucket = aws_s3_bucket.test.bucket
   key    = aws_s3_object.test.key
