a904926220270912d3cbf9d452987e714f644c1a
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-09-28 07:53:55 -0400

    datasync: Handle 'domain', 'password' and 'user' in expandSMB/flattenSMB.

--- internal/service/datasync/location_fsx_ontap_file_system_test.go
+++ internal/service/datasync/location_fsx_ontap_file_system_test.go
@@ -97,6 +97,9 @@ func TestAccDataSyncLocationFSxONTAPFileSystem_disappears(t *testing.T) {
 func TestAccDataSyncLocationFSxONTAPFileSystem_smb(t *testing.T) {
 	ctx := acctest.Context(t)
 	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	netBiosName := "tftest-" + sdkacctest.RandString(7)
+	domainNetbiosName := "tftest" + sdkacctest.RandString(4)
+	domainName := domainNetbiosName + ".local"
 	var v datasync.DescribeLocationFsxOntapOutput
 	resourceName := "aws_datasync_location_fsx_ontap_file_system.test"
 
@@ -111,17 +114,17 @@ func TestAccDataSyncLocationFSxONTAPFileSystem_smb(t *testing.T) {
 		CheckDestroy:             testAccCheckLocationFSxONTAPDestroy(ctx),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccLocationFSxONTAPFileSystemConfig_smb(rName),
+				Config: testAccLocationFSxONTAPFileSystemConfig_smb(rName, netBiosName, domainNetbiosName, domainName),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					testAccCheckLocationFSxONTAPExists(ctx, resourceName, &v),
 					resource.TestCheckResourceAttr(resourceName, "protocol.#", "1"),
 					resource.TestCheckResourceAttr(resourceName, "protocol.0.nfs.#", "0"),
 					resource.TestCheckResourceAttr(resourceName, "protocol.0.smb.#", "1"),
-					resource.TestCheckResourceAttr(resourceName, "protocol.0.smb.0.domain", "aws.mydomain.com"),
-					resource.TestCheckResourceAttr(resourceName, "protocol.0.smb.0.mounts_options.#", "1"),
-					resource.TestCheckResourceAttr(resourceName, "protocol.0.smb.0.mounts_options.0.version", "SMB3"),
+					resource.TestCheckResourceAttr(resourceName, "protocol.0.smb.0.domain", domainName),
+					resource.TestCheckResourceAttr(resourceName, "protocol.0.smb.0.mount_options.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "protocol.0.smb.0.mount_options.0.version", "SMB3"),
 					resource.TestCheckResourceAttr(resourceName, "protocol.0.smb.0.password", "MyPassw0rd1"),
-					resource.TestCheckResourceAttr(resourceName, "protocol.0.smb.0.user", "datasyncsa"),
+					resource.TestCheckResourceAttr(resourceName, "protocol.0.smb.0.user", "Admin"),
 				),
 			},
 			{
@@ -276,8 +279,8 @@ func testAccLocationFSxONTAPImportStateID(n string) resource.ImportStateIdFunc {
 	}
 }
 
-func testAccFSxOntapFileSystemConfig_base(rName string) string {
-	return acctest.ConfigCompose(acctest.ConfigVPCWithSubnets(rName, 1), fmt.Sprintf(`
+func testAccFSxOntapFileSystemConfig_base(rName string, nSubnets int, deploymentType string) string {
+	return acctest.ConfigCompose(acctest.ConfigVPCWithSubnets(rName, nSubnets), fmt.Sprintf(`
 resource "aws_security_group" "test" {
   name   = %[1]q
   vpc_id = aws_vpc.test.id
@@ -304,7 +307,7 @@ resource "aws_security_group" "test" {
 resource "aws_fsx_ontap_file_system" "test" {
   storage_capacity    = 1024
   subnet_ids          = aws_subnet.test[*].id
-  deployment_type     = "SINGLE_AZ_1"
+  deployment_type     = %[2]q
   throughput_capacity = 512
   preferred_subnet_id = aws_subnet.test[0].id
 
@@ -312,7 +315,11 @@ resource "aws_fsx_ontap_file_system" "test" {
     Name = %[1]q
   }
 }
+`, rName, deploymentType))
+}
 
+func testAccFSxOntapFileSystemConfig_baseNFS(rName string) string {
+	return acctest.ConfigCompose(testAccFSxOntapFileSystemConfig_base(rName, 1, "SINGLE_AZ_1"), fmt.Sprintf(`
 resource "aws_fsx_ontap_storage_virtual_machine" "test" {
   file_system_id = aws_fsx_ontap_file_system.test.id
   name           = %[1]q
@@ -320,8 +327,24 @@ resource "aws_fsx_ontap_storage_virtual_machine" "test" {
 `, rName))
 }
 
+func testAccFSxOntapFileSystemConfig_baseSMB(rName, domainName string) string {
+	return acctest.ConfigCompose(testAccFSxOntapFileSystemConfig_base(rName, 2, "MULTI_AZ_1"), fmt.Sprintf(`
+resource "aws_directory_service_directory" "test" {
+  edition  = "Standard"
+  name     = %[1]q
+  password = "MyPassw0rd1"
+  type     = "MicrosoftAD"
+
+  vpc_settings {
+    subnet_ids = aws_subnet.test[*].id
+    vpc_id     = aws_vpc.test.id
+  }
+}
+`, domainName))
+}
+
 func testAccLocationFSxONTAPFileSystemConfig_basic(rName string) string {
-	return acctest.ConfigCompose(testAccFSxOntapFileSystemConfig_base(rName), `
+	return acctest.ConfigCompose(testAccFSxOntapFileSystemConfig_baseNFS(rName), `
 resource "aws_datasync_location_fsx_ontap_file_system" "test" {
   security_group_arns         = [aws_security_group.test.arn]
   storage_virtual_machine_arn = aws_fsx_ontap_storage_virtual_machine.test.arn
@@ -337,30 +360,48 @@ resource "aws_datasync_location_fsx_ontap_file_system" "test" {
 `)
 }
 
-func testAccLocationFSxONTAPFileSystemConfig_smb(rName string) string {
-	return acctest.ConfigCompose(testAccFSxOntapFileSystemConfig_base(rName), `
+func testAccLocationFSxONTAPFileSystemConfig_smb(rName, netBiosName, domainNetbiosName, domainName string) string {
+	return acctest.ConfigCompose(testAccFSxOntapFileSystemConfig_baseSMB(rName, domainName), fmt.Sprintf(`
+resource "aws_fsx_ontap_storage_virtual_machine" "test" {
+  file_system_id = aws_fsx_ontap_file_system.test.id
+  name           = %[1]q
+  depends_on     = [aws_directory_service_directory.test]
+
+  active_directory_configuration {
+    netbios_name = %[2]q
+    self_managed_active_directory_configuration {
+      dns_ips                                = aws_directory_service_directory.test.dns_ip_addresses
+      domain_name                            = %[3]q
+      password                               = "MyPassw0rd1"
+      username                               = "Admin"
+      organizational_unit_distinguished_name = "OU=computers,OU=%[4]s"
+      file_system_administrators_group       = "Admins"
+    }
+  }
+}
+
 resource "aws_datasync_location_fsx_ontap_file_system" "test" {
   security_group_arns         = [aws_security_group.test.arn]
   storage_virtual_machine_arn = aws_fsx_ontap_storage_virtual_machine.test.arn
 
   protocol {
     smb {
-      domain = "aws.mydomain.com"
+      domain = %[3]q
 
       mount_options {
         version = "SMB3"
       }
 
       password = "MyPassw0rd1"
-      user     = "datasyncsa"
+      user     = "Admin"
     }
   }
 }
-`)
+`, rName, netBiosName, domainName, domainNetbiosName))
 }
 
 func testAccLocationFSxONTAPFileSystemConfig_subdirectory(rName, subdirectory string) string {
-	return acctest.ConfigCompose(testAccFSxOntapFileSystemConfig_base(rName), fmt.Sprintf(`
+	return acctest.ConfigCompose(testAccFSxOntapFileSystemConfig_baseNFS(rName), fmt.Sprintf(`
 resource "aws_datasync_location_fsx_ontap_file_system" "test" {
   security_group_arns         = [aws_security_group.test.arn]
   storage_virtual_machine_arn = aws_fsx_ontap_storage_virtual_machine.test.arn
@@ -378,7 +419,7 @@ resource "aws_datasync_location_fsx_ontap_file_system" "test" {
 }
 
 func testAccLocationFSxONTAPFileSystemConfig_tags1(rName, key1, value1 string) string {
-	return acctest.ConfigCompose(testAccFSxOntapFileSystemConfig_base(rName), fmt.Sprintf(`
+	return acctest.ConfigCompose(testAccFSxOntapFileSystemConfig_baseNFS(rName), fmt.Sprintf(`
 resource "aws_datasync_location_fsx_ontap_file_system" "test" {
   security_group_arns         = [aws_security_group.test.arn]
   storage_virtual_machine_arn = aws_fsx_ontap_storage_virtual_machine.test.arn
@@ -399,7 +440,7 @@ resource "aws_datasync_location_fsx_ontap_file_system" "test" {
 }
 
 func testAccLocationFSxONTAPFileSystemConfig_tags2(rName, key1, value1, key2, value2 string) string {
-	return acctest.ConfigCompose(testAccFSxOntapFileSystemConfig_base(rName), fmt.Sprintf(`
+	return acctest.ConfigCompose(testAccFSxOntapFileSystemConfig_baseNFS(rName), fmt.Sprintf(`
 resource "aws_datasync_location_fsx_ontap_file_system" "test" {
   security_group_arns         = [aws_security_group.test.arn]
   storage_virtual_machine_arn = aws_fsx_ontap_storage_virtual_machine.test.arn
