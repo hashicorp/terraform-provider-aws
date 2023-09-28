cb9532fa2f2fbb3928111cd5b6cf6da7c4b93886
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-09-28 17:03:31 -0400

    d/aws_guardduty_detector: Tidy up acceptance tests.

--- internal/service/guardduty/detector_data_source_test.go
+++ internal/service/guardduty/detector_data_source_test.go
@@ -13,6 +13,9 @@ import (
 
 func testAccDetectorDataSource_basic(t *testing.T) {
 	ctx := acctest.Context(t)
+	datasourceName := "data.aws_guardduty_detector.test"
+	resourceName := "aws_guardduty_detector.test"
+
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
 			acctest.PreCheck(ctx, t)
@@ -23,40 +26,13 @@ func testAccDetectorDataSource_basic(t *testing.T) {
 		PreventPostDestroyRefresh: true,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDetectorDataSourceConfig_basicResource(),
-				Check:  resource.ComposeTestCheckFunc(),
-			},
-			{
-				Config: testAccDetectorDataSourceConfig_basicResource2(),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrPair("data.aws_guardduty_detector.test", "id", "aws_guardduty_detector.test", "id"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "status", "ENABLED"),
-					acctest.CheckResourceAttrGlobalARN("data.aws_guardduty_detector.test", "service_role_arn", "iam", "role/aws-service-role/guardduty.amazonaws.com/AWSServiceRoleForAmazonGuardDuty"),
-					resource.TestCheckResourceAttrPair("data.aws_guardduty_detector.test", "finding_publishing_frequency", "aws_guardduty_detector.test", "finding_publishing_frequency"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.#", "9"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.0.name", "CLOUD_TRAIL"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.0.enable", "true"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.1.name", "DNS_LOGS"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.1.enable", "true"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.2.name", "FLOW_LOGS"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.2.enable", "true"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.3.name", "S3_DATA_EVENTS"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.3.enable", "true"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.4.name", "EKS_AUDIT_LOGS"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.4.enable", "true"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.5.name", "EBS_MALWARE_PROTECTION"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.5.enable", "true"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.6.name", "RDS_LOGIN_EVENTS"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.6.enable", "true"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.7.name", "EKS_RUNTIME_MONITORING"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.7.enable", "false"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.7.additional_configuration.#", "1"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.7.additional_configuration.0.name", "EKS_ADDON_MANAGEMENT"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.7.additional_configuration.0.enable", "false"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.8.name", "LAMBDA_NETWORK_LOGS"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.8.enable", "true"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "finding_publishing_frequency", "SIX_HOURS"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "tags.%", "0"),
+				Config: testAccDetectorDataSourceConfig_basic,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					acctest.CheckResourceAttrGreaterThanValue(datasourceName, "features.#", 0),
+					resource.TestCheckResourceAttrPair(datasourceName, "finding_publishing_frequency", resourceName, "finding_publishing_frequency"),
+					resource.TestCheckResourceAttrPair(datasourceName, "id", resourceName, "id"),
+					acctest.CheckResourceAttrGlobalARN(datasourceName, "service_role_arn", "iam", "role/aws-service-role/guardduty.amazonaws.com/AWSServiceRoleForAmazonGuardDuty"),
+					resource.TestCheckResourceAttr(datasourceName, "status", "ENABLED"),
 				),
 			},
 		},
@@ -65,6 +41,9 @@ func testAccDetectorDataSource_basic(t *testing.T) {
 
 func testAccDetectorDataSource_ID(t *testing.T) {
 	ctx := acctest.Context(t)
+	datasourceName := "data.aws_guardduty_detector.test"
+	resourceName := "aws_guardduty_detector.test"
+
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
 			acctest.PreCheck(ctx, t)
@@ -74,62 +53,31 @@ func testAccDetectorDataSource_ID(t *testing.T) {
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDetectorDataSourceConfig_explicit(),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrPair("data.aws_guardduty_detector.test", "id", "aws_guardduty_detector.test", "id"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "status", "ENABLED"),
-					acctest.CheckResourceAttrGlobalARN("data.aws_guardduty_detector.test", "service_role_arn", "iam", "role/aws-service-role/guardduty.amazonaws.com/AWSServiceRoleForAmazonGuardDuty"),
-					resource.TestCheckResourceAttrPair("data.aws_guardduty_detector.test", "finding_publishing_frequency", "aws_guardduty_detector.test", "finding_publishing_frequency"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.#", "9"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.0.name", "CLOUD_TRAIL"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.0.enable", "true"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.1.name", "DNS_LOGS"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.1.enable", "true"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.2.name", "FLOW_LOGS"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.2.enable", "true"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.3.name", "S3_DATA_EVENTS"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.3.enable", "true"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.4.name", "EKS_AUDIT_LOGS"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.4.enable", "true"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.5.name", "EBS_MALWARE_PROTECTION"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.5.enable", "true"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.6.name", "RDS_LOGIN_EVENTS"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.6.enable", "true"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.7.name", "EKS_RUNTIME_MONITORING"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.7.enable", "false"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.7.additional_configuration.#", "1"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.7.additional_configuration.0.name", "EKS_ADDON_MANAGEMENT"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.7.additional_configuration.0.enable", "false"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.8.name", "LAMBDA_NETWORK_LOGS"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "features.8.enable", "true"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "finding_publishing_frequency", "SIX_HOURS"),
-					resource.TestCheckResourceAttr("data.aws_guardduty_detector.test", "tags.%", "0"),
+				Config: testAccDetectorDataSourceConfig_id,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					acctest.CheckResourceAttrGreaterThanValue(datasourceName, "features.#", 0),
+					resource.TestCheckResourceAttrPair(datasourceName, "finding_publishing_frequency", resourceName, "finding_publishing_frequency"),
+					resource.TestCheckResourceAttrPair(datasourceName, "id", resourceName, "id"),
+					acctest.CheckResourceAttrGlobalARN(datasourceName, "service_role_arn", "iam", "role/aws-service-role/guardduty.amazonaws.com/AWSServiceRoleForAmazonGuardDuty"),
+					resource.TestCheckResourceAttr(datasourceName, "status", "ENABLED"),
 				),
 			},
 		},
 	})
 }
 
-func testAccDetectorDataSourceConfig_basicResource() string {
-	return `
-resource "aws_guardduty_detector" "test" {}
-`
-}
-
-func testAccDetectorDataSourceConfig_basicResource2() string {
-	return `
+const testAccDetectorDataSourceConfig_basic = `
 resource "aws_guardduty_detector" "test" {}
 
-data "aws_guardduty_detector" "test" {}
-`
+data "aws_guardduty_detector" "test" {
+  depends_on = [aws_guardduty_detector.test]
 }
+`
 
-func testAccDetectorDataSourceConfig_explicit() string {
-	return `
+const testAccDetectorDataSourceConfig_id = `
 resource "aws_guardduty_detector" "test" {}
 
 data "aws_guardduty_detector" "test" {
   id = aws_guardduty_detector.test.id
 }
 `
-}
