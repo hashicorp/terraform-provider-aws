88f5cc6cd1414b3d7ffd06e3e33d9beae9cf8f9c
Graham Davison <gdavison@hashicorp.com>
2023-09-29 17:33:46 -0700

    Adds `ACTIVE` to expected states

--- internal/service/applicationinsights/wait.go
+++ internal/service/applicationinsights/wait.go
@@ -35,7 +35,7 @@ func waitApplicationCreated(ctx context.Context, conn *applicationinsights.Appli
 
 func waitApplicationTerminated(ctx context.Context, conn *applicationinsights.ApplicationInsights, name string) (*applicationinsights.ApplicationInfo, error) {
 	stateConf := &retry.StateChangeConf{
-		Pending: []string{"NOT_CONFIGURED", "DELETING"},
+		Pending: []string{"ACTIVE", "NOT_CONFIGURED", "DELETING"},
 		Target:  []string{},
 		Refresh: statusApplication(ctx, conn, name),
 		Timeout: ApplicationDeletedTimeout,
