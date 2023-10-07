ec04649cc8a8b9cd22998412bceb8ebb9c33721c
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-10-07 16:38:13 -0400

    r/aws_cloudfront_public_key: Tidy up resource Read.

--- internal/service/cloudfront/public_key.go
+++ internal/service/cloudfront/public_key.go
@@ -5,7 +5,6 @@ package cloudfront
 
 import (
 	"context"
-	"errors"
 	"log"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -13,11 +12,11 @@ import (
 	"github.com/hashicorp/aws-sdk-go-base/v2/awsv1shim/v2/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/id"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
-	"github.com/hashicorp/terraform-provider-aws/internal/create"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
-	"github.com/hashicorp/terraform-provider-aws/names"
+	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 )
 
 // @SDKResource("aws_cloudfront_public_key")
@@ -113,37 +112,25 @@ func resourcePublicKeyCreate(ctx context.Context, d *schema.ResourceData, meta i
 func resourcePublicKeyRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
 	conn := meta.(*conns.AWSClient).CloudFrontConn(ctx)
-	request := &cloudfront.GetPublicKeyInput{
-		Id: aws.String(d.Id()),
-	}
 
-	output, err := conn.GetPublicKeyWithContext(ctx, request)
-	if !d.IsNewResource() && tfawserr.ErrCodeEquals(err, cloudfront.ErrCodeNoSuchPublicKey) {
-		create.LogNotFoundRemoveState(names.CloudFront, create.ErrActionReading, ResNamePublicKey, d.Id())
+	output, err := findPublicKeyByID(ctx, conn, d.Id())
+
+	if !d.IsNewResource() && tfresource.NotFound(err) {
+		log.Printf("[WARN] CloudFront Public Key (%s) not found, removing from state", d.Id())
 		d.SetId("")
 		return diags
 	}
 
 	if err != nil {
-		return create.DiagError(names.CloudFront, create.ErrActionReading, ResNamePublicKey, d.Id(), err)
-	}
-
-	if !d.IsNewResource() && (output == nil || output.PublicKey == nil || output.PublicKey.PublicKeyConfig == nil) {
-		create.LogNotFoundRemoveState(names.CloudFront, create.ErrActionReading, ResNamePublicKey, d.Id())
-		d.SetId("")
-		return diags
-	}
-
-	if d.IsNewResource() && (output == nil || output.PublicKey == nil || output.PublicKey.PublicKeyConfig == nil) {
-		return create.DiagError(names.CloudFront, create.ErrActionReading, ResNamePublicKey, d.Id(), errors.New("empty response after creation"))
+		return sdkdiag.AppendErrorf(diags, "reading CloudFront Public Key (%s): %s", d.Id(), err)
 	}
 
 	publicKeyConfig := output.PublicKey.PublicKeyConfig
-	d.Set("encoded_key", publicKeyConfig.EncodedKey)
-	d.Set("name", publicKeyConfig.Name)
-	d.Set("comment", publicKeyConfig.Comment)
 	d.Set("caller_reference", publicKeyConfig.CallerReference)
+	d.Set("comment", publicKeyConfig.Comment)
+	d.Set("encoded_key", publicKeyConfig.EncodedKey)
 	d.Set("etag", output.ETag)
+	d.Set("name", publicKeyConfig.Name)
 
 	return diags
 }
@@ -199,4 +186,29 @@ func resourcePublicKeyDelete(ctx context.Context, d *schema.ResourceData, meta i
 	}
 
 	return diags
+}
+
+func findPublicKeyByID(ctx context.Context, conn *cloudfront.CloudFront, id string) (*cloudfront.GetPublicKeyOutput, error) {
+	input := &cloudfront.GetPublicKeyInput{
+		Id: aws.String(id),
+	}
+
+	output, err := conn.GetPublicKeyWithContext(ctx, input)
+
+	if tfawserr.ErrCodeEquals(err, cloudfront.ErrCodeNoSuchPublicKey) {
+		return nil, &retry.NotFoundError{
+			LastError:   err,
+			LastRequest: input,
+		}
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	if output == nil || output.PublicKey == nil || output.PublicKey.PublicKeyConfig == nil {
+		return nil, tfresource.NewEmptyResultError(input)
+	}
+
+	return output, nil
 }
