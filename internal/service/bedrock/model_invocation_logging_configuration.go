7be3a7a3ab6ef5d75c0a66d4d192315a91818dce
Richard Weerasinghe <richard@skyscrapr.io>
2023-10-07 21:50:22 +1300

    add model_invocation_logging_configuration resource

---
+++ internal/service/bedrock/model_invocation_logging_configuration.go
@@ -0,0 +1,160 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package bedrock
+
+import (
+	"context"
+	"time"
+
+	"github.com/YakDriver/regexache"
+	"github.com/aws/aws-sdk-go/service/bedrock"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
+)
+
+// @SDKResource("aws_bedrock_model_invocation_logging_configuration")
+func ResourceModelInvocationLoggingConfiguration() *schema.Resource {
+	return &schema.Resource{
+		CreateWithoutTimeout: resourceModelInvocationLoggingConfigurationCreate,
+		ReadWithoutTimeout:   resourceModelInvocationLoggingConfigurationRead,
+		UpdateWithoutTimeout: resourceModelInvocationLoggingConfigurationCreate,
+		DeleteWithoutTimeout: resourceModelInvocationLoggingConfigurationDelete,
+
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(120 * time.Minute),
+		},
+
+		Schema: map[string]*schema.Schema{
+			"logging_config": {
+				Type:     schema.TypeList,
+				Required: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"cloud_watch_config": {
+							Type:     schema.TypeList,
+							Optional: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"log_group_name": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									"role_arn": {
+										Type:         schema.TypeString,
+										Required:     true,
+										ValidateFunc: validation.StringMatch(regexache.MustCompile(`^arn:aws(-[^:]+)?:iam::([0-9]{12})?:role/.+$`), "Minimum length of 0. Maximum length of 2048."),
+									},
+									"large_data_delivery_s3_config": {
+										Type:     schema.TypeList,
+										Optional: true,
+										Elem: &schema.Resource{
+											Schema: map[string]*schema.Schema{
+												"bucket_name": {
+													Type:     schema.TypeString,
+													Required: true,
+												},
+												"key_prefix": {
+													Type:     schema.TypeString,
+													Optional: true,
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+						"embedding_data_delivery_enabled": {
+							Type:     schema.TypeBool,
+							Required: true,
+						},
+						"image_data_delivery_enabled": {
+							Type:     schema.TypeBool,
+							Required: true,
+						},
+						"s3_config": {
+							Type:     schema.TypeList,
+							Optional: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"bucket_name": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									"key_prefix": {
+										Type:     schema.TypeString,
+										Optional: true,
+									},
+								},
+							},
+						},
+						"text_data_delivery_enabled": {
+							Type:     schema.TypeBool,
+							Required: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func resourceModelInvocationLoggingConfigurationCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	conn := meta.(*conns.AWSClient).BedrockConn(ctx)
+
+	input := &bedrock.PutModelInvocationLoggingConfigurationInput{}
+
+	if v, ok := d.GetOk("logging_config"); ok && len(v.([]interface{})) > 0 && v.([]interface{})[0] != nil {
+		input.LoggingConfig = expandLoggingConfig(v.([]interface{})[0].(map[string]interface{}))
+	}
+
+	_, err := conn.PutModelInvocationLoggingConfiguration(input)
+	if err != nil {
+		return sdkdiag.AppendErrorf(diags, "failed to put model invocation logging configuration: %s", err)
+	}
+
+	d.SetId(meta.(*conns.AWSClient).Region)
+
+	return append(diags, resourceModelInvocationLoggingConfigurationRead(ctx, d, meta)...)
+}
+
+func resourceModelInvocationLoggingConfigurationRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	conn := meta.(*conns.AWSClient).BedrockConn(ctx)
+
+	output, err := conn.GetModelInvocationLoggingConfiguration(&bedrock.GetModelInvocationLoggingConfigurationInput{})
+	if err != nil {
+		return sdkdiag.AppendErrorf(diags, "failed to get model invocation logging configuration: %s", err)
+	}
+
+	d.SetId(meta.(*conns.AWSClient).Region)
+
+	if output.LoggingConfig != nil {
+		if err := d.Set("logging_config", []interface{}{flattenLoggingConfig(output.LoggingConfig)}); err != nil {
+			return diag.Errorf("setting logging)config: %s", err)
+		}
+	} else {
+		d.Set("logging_config", nil)
+	}
+	return diags
+}
+
+func resourceModelInvocationLoggingConfigurationDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	conn := meta.(*conns.AWSClient).BedrockConn(ctx)
+
+	_, err := conn.DeleteModelInvocationLoggingConfiguration(&bedrock.DeleteModelInvocationLoggingConfigurationInput{})
+	if err != nil {
+		return sdkdiag.AppendErrorf(diags, "failed to delete model invocation logging configuration: %s", err)
+	}
+
+	return diags
+}
---
+++ internal/service/bedrock/model_invocation_logging_configuration_test.go
@@ -0,0 +1,106 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package bedrock_test
+
+import (
+	"fmt"
+	"testing"
+
+	sdkacctest "github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+)
+
+func TestAccBedrockModelInvocationLoggingConfiguration_basic(t *testing.T) {
+	ctx := acctest.Context(t)
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	resourceName := "aws_bedrock_model_invocation_logging_configuration.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccBedrockModelInvocationLoggingConfiguration_basic(rName),
+				Check:  resource.ComposeAggregateTestCheckFunc(
+				// testAccCheckAddonExists(ctx, customModelResourceName, &addon),
+				// resource.TestCheckResourceAttr(customModelResourceName, "addon_name", addonName),
+				// resource.TestCheckResourceAttrSet(customModelResourceName, "addon_version"),
+				// acctest.MatchResourceAttrRegionalARN(customModelResourceName, "arn", "eks", regexache.MustCompile(fmt.Sprintf("addon/%s/%s/.+$", rName, addonName))),
+				// resource.TestCheckResourceAttr(customModelResourceName, "configuration_values", ""),
+				// resource.TestCheckNoResourceAttr(customModelResourceName, "preserve"),
+				// resource.TestCheckResourceAttr(customModelResourceName, "tags.%", "0"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccBedrockModelInvocationLoggingConfiguration_basic(rName string) string {
+	return fmt.Sprintf(`
+  data "aws_caller_identity" "current" {}
+
+resource aws_s3_bucket bedrock_logging {
+  bucket        = "bedrock-logging-%[1]s"
+  force_destroy = true
+  lifecycle {
+    ignore_changes = [
+      tags["CreatorId"], tags["CreatorName"],
+    ]
+  }
+}
+
+resource "aws_s3_bucket_policy" "bedrock_logging" {
+  bucket = aws_s3_bucket.bedrock_logging.bucket
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "bedrock.amazonaws.com"
+      },
+      "Action": [
+        "s3:*"
+      ],
+      "Resource": [
+        "${aws_s3_bucket.bedrock_logging.arn}/*"
+      ],
+      "Condition": {
+        "StringEquals": {
+          "aws:SourceAccount": "${data.aws_caller_identity.current.account_id}"
+        },
+        "ArnLike": {
+          "aws:SourceArn": "arn:aws:bedrock:us-east-1:${data.aws_caller_identity.current.account_id}:*"
+        }
+      }
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_bedrock_model_invocation_logging_configuration" "test" {
+  logging_config {
+    embedding_data_delivery_enabled = true
+    image_data_delivery_enabled = true
+    text_data_delivery_enabled = true
+    s3_config {
+      bucket_name = aws_s3_bucket.bedrock_logging.id
+      key_prefix = "bedrock"
+    }
+  }
+  depends_on = [
+    aws_s3_bucket_policy.bedrock_logging
+  ]
+}
+`, rName)
+}
