8420bb51e9b079b17782c12a61f8e157cfb6a601
GlennChia <glennchia7@gmail.com>
2023-10-10 13:25:32 +0000

    feat(veracc): inst log conf rsrc read

--- internal/service/ec2/find.go
+++ internal/service/ec2/find.go
@@ -7076,6 +7076,60 @@ func FindVerifiedAccessInstanceByID(ctx context.Context, conn *ec2_sdkv2.Client,
 	return output, nil
 }
 
+func FindVerifiedAccessInstanceLoggingConfiguration(ctx context.Context, conn *ec2_sdkv2.Client, input *ec2_sdkv2.DescribeVerifiedAccessInstanceLoggingConfigurationsInput) (*awstypes.VerifiedAccessInstanceLoggingConfiguration, error) {
+	output, err := FindVerifiedAccessInstanceLoggingConfigurations(ctx, conn, input)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return tfresource.AssertSingleValueResult(output)
+}
+
+func FindVerifiedAccessInstanceLoggingConfigurations(ctx context.Context, conn *ec2_sdkv2.Client, input *ec2_sdkv2.DescribeVerifiedAccessInstanceLoggingConfigurationsInput) ([]awstypes.VerifiedAccessInstanceLoggingConfiguration, error) {
+	var output []awstypes.VerifiedAccessInstanceLoggingConfiguration
+	paginator := ec2_sdkv2.NewDescribeVerifiedAccessInstanceLoggingConfigurationsPaginator(conn, input)
+
+	for paginator.HasMorePages() {
+		page, err := paginator.NextPage(ctx)
+
+		if tfawserr_sdkv2.ErrCodeEquals(err, errCodeInvalidVerifiedAccessInstanceIdNotFound) {
+			return nil, &retry.NotFoundError{
+				LastError:   err,
+				LastRequest: input,
+			}
+		}
+
+		if err != nil {
+			return nil, err
+		}
+
+		output = append(output, page.LoggingConfigurations...)
+	}
+
+	return output, nil
+}
+
+func FindVerifiedAccessInstanceLoggingConfigurationByInstanceID(ctx context.Context, conn *ec2_sdkv2.Client, id string) (*awstypes.VerifiedAccessInstanceLoggingConfiguration, error) {
+	input := &ec2_sdkv2.DescribeVerifiedAccessInstanceLoggingConfigurationsInput{
+		VerifiedAccessInstanceIds: []string{id},
+	}
+	output, err := FindVerifiedAccessInstanceLoggingConfiguration(ctx, conn, input)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Eventual consistency check.
+	if aws_sdkv2.ToString(output.VerifiedAccessInstanceId) != id {
+		return nil, &retry.NotFoundError{
+			LastRequest: input,
+		}
+	}
+
+	return output, nil
+}
+
 func FindVerifiedAccessInstanceTrustProviderAttachmentExists(ctx context.Context, conn *ec2_sdkv2.Client, vaiID, vatpID string) error {
 	output, err := FindVerifiedAccessInstanceByID(ctx, conn, vaiID)
 
--- internal/service/ec2/verifiedaccess_instance_logging_configuration.go
+++ internal/service/ec2/verifiedaccess_instance_logging_configuration.go
@@ -4,13 +4,24 @@
 package ec2
 
 import (
+	"context"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
+	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 // @SDKResource("aws_verifiedaccess_instance_logging_configuration", name="Verified Access Instance Logging Configuration")
 func ResourceVerifiedAccessInstanceLoggingConfiguration() *schema.Resource {
 	return &schema.Resource{
+		ReadWithoutTimeout: resourceVerifiedAccessInstanceLoggingConfigurationRead,
+
 		Importer: &schema.ResourceImporter{
 			StateContext: schema.ImportStatePassthroughContext,
 		},
@@ -106,4 +117,105 @@ func ResourceVerifiedAccessInstanceLoggingConfiguration() *schema.Resource {
 			},
 		},
 	}
+}
+
+func resourceVerifiedAccessInstanceLoggingConfigurationRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	conn := meta.(*conns.AWSClient).EC2Client(ctx)
+
+	vaiID := d.Id()
+
+	output, err := FindVerifiedAccessInstanceLoggingConfigurationByInstanceID(ctx, conn, vaiID)
+
+	if !d.IsNewResource() && tfresource.NotFound(err) {
+		log.Printf("[WARN] EC2 Verified Access Instance Logging Configuration (%s) not found, removing from state", vaiID)
+		d.SetId("")
+		return diags
+	}
+
+	if err != nil {
+		return sdkdiag.AppendErrorf(diags, "reading Verified Access Instance Logging Configuration (%s): %s", vaiID, err)
+	}
+
+	if v := output.AccessLogs; v != nil {
+		if err := d.Set("access_logs", flattenVerifiedAccessInstanceAccessLogs(v)); err != nil {
+			return sdkdiag.AppendErrorf(diags, "setting verified access instance access logs: %s", err)
+		}
+	} else {
+		d.Set("access_logs", nil)
+	}
+
+	d.Set("verifiedaccess_instance_id", vaiID)
+
+	return diags
+}
+
+func flattenVerifiedAccessInstanceAccessLogs(apiObject *types.VerifiedAccessLogs) []interface{} {
+	tfMap := map[string]interface{}{}
+
+	if v := apiObject.CloudWatchLogs; v != nil {
+		tfMap["cloudwatch_logs"] = flattenVerifiedAccessLogCloudWatchLogs(v)
+	}
+
+	if v := apiObject.IncludeTrustContext; v != nil {
+		tfMap["include_trust_context"] = aws.ToBool(v)
+	}
+
+	if v := apiObject.KinesisDataFirehose; v != nil {
+		tfMap["kinesis_data_firehose"] = flattenVerifiedAccessLogKinesisDataFirehose(v)
+	}
+
+	if v := apiObject.LogVersion; v != nil {
+		tfMap["log_version"] = aws.ToString(v)
+	}
+
+	if v := apiObject.S3; v != nil {
+		tfMap["s3"] = flattenVerifiedAccessLogS3(v)
+	}
+
+	return []interface{}{tfMap}
+}
+
+func flattenVerifiedAccessLogCloudWatchLogs(apiObject *types.VerifiedAccessLogCloudWatchLogsDestination) []interface{} {
+	tfMap := map[string]interface{}{
+		"enabled": bool(*apiObject.Enabled),
+	}
+
+	if v := apiObject.LogGroup; v != nil {
+		tfMap["log_group"] = aws.ToString(v)
+	}
+
+	return []interface{}{tfMap}
+}
+
+func flattenVerifiedAccessLogKinesisDataFirehose(apiObject *types.VerifiedAccessLogKinesisDataFirehoseDestination) []interface{} {
+	tfMap := map[string]interface{}{
+		"enabled": bool(*apiObject.Enabled),
+	}
+
+	if v := apiObject.DeliveryStream; v != nil {
+		tfMap["delivery_stream"] = aws.ToString(v)
+	}
+
+	return []interface{}{tfMap}
+}
+
+func flattenVerifiedAccessLogS3(apiObject *types.VerifiedAccessLogS3Destination) []interface{} {
+	tfMap := map[string]interface{}{
+		"enabled": bool(*apiObject.Enabled),
+	}
+
+	if v := apiObject.BucketName; v != nil {
+		tfMap["bucket_name"] = aws.ToString(v)
+	}
+
+	if v := apiObject.BucketOwner; v != nil {
+		tfMap["bucket_owner"] = aws.ToString(v)
+	}
+
+	if v := apiObject.Prefix; v != nil {
+		tfMap["prefix"] = aws.ToString(v)
+	}
+
+	return []interface{}{tfMap}
 }
