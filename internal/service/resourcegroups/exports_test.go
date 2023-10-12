c641223f913e9af62a87e0f4a159a128ca49266b
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-10-12 10:55:31 -0400

    r/aws_resourcegroups_group: Migrate to AWS SDK for Go v2.

---
+++ internal/service/resourcegroups/exports_test.go
@@ -0,0 +1,11 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package resourcegroups
+
+// Exports for use in tests only.
+var (
+	ResourceGroup = resourceGroup
+
+	FindGroupByName = findGroupByName
+)
--- internal/service/resourcegroups/service_package_gen.go
+++ internal/service/resourcegroups/service_package_gen.go
@@ -29,7 +29,7 @@ func (p *servicePackage) SDKDataSources(ctx context.Context) []*types.ServicePac
 func (p *servicePackage) SDKResources(ctx context.Context) []*types.ServicePackageSDKResource {
 	return []*types.ServicePackageSDKResource{
 		{
-			Factory:  ResourceGroup,
+			Factory:  resourceGroup,
 			TypeName: "aws_resourcegroups_group",
 			Name:     "Group",
 			Tags: &types.ServicePackageResourceTags{
