f90be26e47a6360e49ed728f7ea0e6b2591a4159
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-10-18 08:36:34 -0400

    r/aws_db_option_group: Tidy up Create.

--- internal/service/rds/option_group.go
+++ internal/service/rds/option_group.go
@@ -7,16 +7,15 @@ import (
 	"context"
 	"log"
 	"strings"
-	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/rds"
 	"github.com/hashicorp/aws-sdk-go-base/v2/awsv1shim/v2/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/id"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/create"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
@@ -35,10 +34,6 @@ func ResourceOptionGroup() *schema.Resource {
 
 		Importer: &schema.ResourceImporter{
 			StateContext: schema.ImportStatePassthroughContext,
-		},
-
-		Timeouts: &schema.ResourceTimeout{
-			Delete: schema.DefaultTimeout(15 * time.Minute),
 		},
 
 		Schema: map[string]*schema.Schema{
@@ -136,34 +131,22 @@ func resourceOptionGroupCreate(ctx context.Context, d *schema.ResourceData, meta
 	var diags diag.Diagnostics
 	conn := meta.(*conns.AWSClient).RDSConn(ctx)
 
-	var groupName string
-	if v, ok := d.GetOk("name"); ok {
-		groupName = v.(string)
-	} else if v, ok := d.GetOk("name_prefix"); ok {
-		groupName = id.PrefixedUniqueId(v.(string))
-	} else {
-		groupName = id.UniqueId()
-	}
-
-	createOpts := &rds.CreateOptionGroupInput{
+	name := create.Name(d.Get("name").(string), d.Get("name_prefix").(string))
+	input := &rds.CreateOptionGroupInput{
 		EngineName:             aws.String(d.Get("engine_name").(string)),
 		MajorEngineVersion:     aws.String(d.Get("major_engine_version").(string)),
 		OptionGroupDescription: aws.String(d.Get("option_group_description").(string)),
-		OptionGroupName:        aws.String(groupName),
+		OptionGroupName:        aws.String(name),
 		Tags:                   getTagsIn(ctx),
 	}
 
-	log.Printf("[DEBUG] Create DB Option Group: %#v", createOpts)
-	output, err := conn.CreateOptionGroupWithContext(ctx, createOpts)
+	_, err := conn.CreateOptionGroupWithContext(ctx, input)
+
 	if err != nil {
-		return sdkdiag.AppendErrorf(diags, "creating DB Option Group: %s", err)
+		return sdkdiag.AppendErrorf(diags, "creating DB Option Group (%s): %s", name, err)
 	}
 
-	d.SetId(strings.ToLower(groupName))
-	log.Printf("[INFO] DB Option Group ID: %s", d.Id())
-
-	// Set for update
-	d.Set("arn", output.OptionGroup.OptionGroupArn)
+	d.SetId(strings.ToLower(name))
 
 	return append(diags, resourceOptionGroupUpdate(ctx, d, meta)...)
 }
@@ -214,15 +197,6 @@ func resourceOptionGroupRead(ctx context.Context, d *schema.ResourceData, meta i
 	}
 
 	return diags
-}
-
-func optionInList(optionName string, list []*string) bool {
-	for _, opt := range list {
-		if aws.StringValue(opt) == optionName {
-			return true
-		}
-	}
-	return false
 }
 
 func resourceOptionGroupUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
@@ -319,6 +293,15 @@ func resourceOptionGroupDelete(ctx context.Context, d *schema.ResourceData, meta
 		return sdkdiag.AppendErrorf(diags, "Deleting DB Option Group: %s", err)
 	}
 	return diags
+}
+
+func optionInList(optionName string, list []*string) bool {
+	for _, opt := range list {
+		if aws.StringValue(opt) == optionName {
+			return true
+		}
+	}
+	return false
 }
 
 func flattenOptionNames(configured []interface{}) []*string {
