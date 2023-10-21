9e1dafebd3067f867abc420e72afcf1a29ed1bf8
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-10-21 15:37:11 -0400

    Move 'ResourceWithConfigure' to its own source file.

--- internal/framework/with_meta.go
+++ internal/framework/with_meta.go
@@ -10,10 +10,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
-	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
-	"github.com/hashicorp/terraform-provider-aws/internal/framework/flex"
-	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
@@ -36,51 +33,6 @@ func (w *withMeta) RegionalARN(service, resource string) string {
 	}.String()
 }
 
-// ResourceWithConfigure is a structure to be embedded within a Resource that implements the ResourceWithConfigure interface.
-type ResourceWithConfigure struct {
-	withMeta
-}
-
-// Configure enables provider-level data or clients to be set in the
-// provider-defined Resource type.
-func (r *ResourceWithConfigure) Configure(_ context.Context, request resource.ConfigureRequest, response *resource.ConfigureResponse) {
-	if v, ok := request.ProviderData.(*conns.AWSClient); ok {
-		r.meta = v
-	}
-}
-
-// SetTagsAll calculates the new value for the `tags_all` attribute.
-func (r *ResourceWithConfigure) SetTagsAll(ctx context.Context, request resource.ModifyPlanRequest, response *resource.ModifyPlanResponse) {
-	// If the entire plan is null, the resource is planned for destruction.
-	if request.Plan.Raw.IsNull() {
-		return
-	}
-
-	defaultTagsConfig := r.Meta().DefaultTagsConfig
-	ignoreTagsConfig := r.Meta().IgnoreTagsConfig
-
-	var planTags types.Map
-
-	response.Diagnostics.Append(request.Plan.GetAttribute(ctx, path.Root(names.AttrTags), &planTags)...)
-
-	if response.Diagnostics.HasError() {
-		return
-	}
-
-	if !planTags.IsUnknown() {
-		if !mapHasUnknownElements(planTags) {
-			resourceTags := tftags.New(ctx, planTags)
-			allTags := defaultTagsConfig.MergeTags(resourceTags).IgnoreConfig(ignoreTagsConfig)
-
-			response.Diagnostics.Append(response.Plan.SetAttribute(ctx, path.Root(names.AttrTagsAll), flex.FlattenFrameworkStringValueMapLegacy(ctx, allTags.Map()))...)
-		} else {
-			response.Diagnostics.Append(response.Plan.SetAttribute(ctx, path.Root(names.AttrTagsAll), tftags.Unknown)...)
-		}
-	} else {
-		response.Diagnostics.Append(response.Plan.SetAttribute(ctx, path.Root(names.AttrTagsAll), tftags.Unknown)...)
-	}
-}
-
 // WithImportByID is intended to be embedded in resources which import state via the "id" attribute.
 // See https://developer.hashicorp.com/terraform/plugin/framework/resources/import.
 type WithImportByID struct{}
@@ -101,14 +53,4 @@ func (d *DataSourceWithConfigure) Configure(_ context.Context, request datasourc
 	if v, ok := request.ProviderData.(*conns.AWSClient); ok {
 		d.meta = v
 	}
-}
-
-func mapHasUnknownElements(m types.Map) bool {
-	for _, v := range m.Elements() {
-		if v.IsUnknown() {
-			return true
-		}
-	}
-
-	return false
 }
