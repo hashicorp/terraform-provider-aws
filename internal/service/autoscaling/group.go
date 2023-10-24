edae1dde071cfeaa48dc35941ffc26bd3b06cb9a
Adrian Johnson <adrian.johnson@hashicorp.com>
2023-10-24 14:03:46 -0500

    aws_autoscaling_group: create custom diff for launch template

--- internal/service/autoscaling/group.go
+++ internal/service/autoscaling/group.go
@@ -253,6 +253,7 @@ func ResourceGroup() *schema.Resource {
 				Type:     schema.TypeList,
 				MaxItems: 1,
 				Optional: true,
+				Computed: true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"id": {
@@ -309,6 +310,7 @@ func ResourceGroup() *schema.Resource {
 			"mixed_instances_policy": {
 				Type:     schema.TypeList,
 				Optional: true,
+				Computed: true,
 				MaxItems: 1,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
@@ -865,13 +867,90 @@ func ResourceGroup() *schema.Resource {
 		},
 
 		CustomizeDiff: customdiff.Sequence(
-			customdiff.ComputedIf("launch_template.0.id", func(_ context.Context, diff *schema.ResourceDiff, meta interface{}) bool {
-				return diff.HasChange("launch_template.0.name")
-			}),
-			customdiff.ComputedIf("launch_template.0.name", func(_ context.Context, diff *schema.ResourceDiff, meta interface{}) bool {
-				return diff.HasChange("launch_template.0.id")
-			}),
+			launchTemplateCustomDiff,
+			// mixedInstancesPolicyLaunchTemplateCustomDiff,
+			launchTemplateDiff("mixed_instances_policy", "mixed_instances_policy.0.launch_template.0.launch_template_specification.0.launch_template_name"),
 		),
+	}
+}
+
+func launchTemplateCustomDiff(_ context.Context, diff *schema.ResourceDiff, _ interface{}) error {
+	expandMap := func(in cty.Value, attributeName, keyToSet, valueToSet string) error {
+		if !in.IsNull() && len(in.AsValueSlice()) > 0 {
+			m := in.AsValueSlice()[0].AsValueMap()
+			tfMap := make(map[string]string)
+			for k, val := range m {
+				if !val.IsNull() && val.IsKnown() {
+					tfMap[k] = val.AsString()
+				}
+			}
+
+			tfMap[keyToSet] = valueToSet
+
+			if err := diff.SetNew(attributeName, []interface{}{tfMap}); err != nil {
+				return err
+			}
+		}
+
+		return nil
+	}
+
+	if diff.HasChange("launch_template.0.name") {
+		lt := diff.GetRawPlan().GetAttr("launch_template")
+		if err := expandMap(lt, "launch_template", "id", "unknown"); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func mixedInstancesPolicyLaunchTemplateCustomDiff(_ context.Context, diff *schema.ResourceDiff, _ interface{}) error {
+	if diff.HasChange("mixed_instances_policy.0.launch_template.0.launch_template_specification.0.launch_template_name") {
+		n := diff.Get("mixed_instances_policy")
+		nmip := n.([]interface{})
+
+		//parts := strings.Split("mixed_instances_policy.0.launch_template.0.launch_template_specification.0.launch_template_name", ".")
+		launchTemplate := nmip[0].(map[string]interface{})["launch_template"].([]interface{})[0].(map[string]interface{})["launch_template_specification"].([]interface{})[0]
+		launchTemplateSpecification := launchTemplate.(map[string]interface{})
+
+		launchTemplateSpecification["launch_template_id"] = "unknown"
+
+		launchTemplate = launchTemplateSpecification
+
+		if err := diff.SetNew("mixed_instances_policy", nmip); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func launchTemplateDiff(baseAttribute, subAttribute string) schema.CustomizeDiffFunc {
+	return func(_ context.Context, diff *schema.ResourceDiff, _ interface{}) error {
+		if diff.HasChange(subAttribute) {
+			n := diff.Get(baseAttribute)
+			nmip, ok := n.([]interface{})
+			if !ok {
+				return nil
+			}
+
+			if baseAttribute == "mixed_instances_policy" {
+				launchTemplate := nmip[0].(map[string]interface{})["launch_template"].([]interface{})[0].(map[string]interface{})["launch_template_specification"].([]interface{})[0]
+				launchTemplateSpecification := launchTemplate.(map[string]interface{})
+
+				launchTemplateSpecification["launch_template_id"] = "unknown"
+
+				launchTemplate = launchTemplateSpecification
+
+				if err := diff.SetNew("mixed_instances_policy", nmip); err != nil {
+					return err
+				}
+			}
+
+		}
+
+		return nil
 	}
 }
 
@@ -3035,7 +3114,7 @@ func expandLaunchTemplateSpecificationForMixedInstancesPolicy(tfMap map[string]i
 	// API returns both ID and name, which Terraform saves to state. Next update returns:
 	// ValidationError: Valid requests must contain either launchTemplateId or LaunchTemplateName
 	// Prefer the ID if we have both.
-	if v, ok := tfMap["launch_template_id"]; ok && v != "" {
+	if v, ok := tfMap["launch_template_id"]; ok && v != "" && v != "unknown" {
 		apiObject.LaunchTemplateId = aws.String(v.(string))
 	} else if v, ok := tfMap["launch_template_name"]; ok && v != "" {
 		apiObject.LaunchTemplateName = aws.String(v.(string))
@@ -3057,7 +3136,7 @@ func expandLaunchTemplateSpecification(tfMap map[string]interface{}) *autoscalin
 
 	// DescribeAutoScalingGroups returns both name and id but LaunchTemplateSpecification
 	// allows only one of them to be set.
-	if v, ok := tfMap["id"]; ok && v != "" {
+	if v, ok := tfMap["id"]; ok && v != "" && v != "unknown" {
 		apiObject.LaunchTemplateId = aws.String(v.(string))
 	} else if v, ok := tfMap["name"]; ok && v != "" {
 		apiObject.LaunchTemplateName = aws.String(v.(string))
