acbcf6926b684bbd86bfa11146a593f22f272d7d
Jared Baker <jared.baker@hashicorp.com>
2023-10-25 15:23:34 -0400

    aws_medialive: fixes for sdk type changes

--- internal/service/medialive/channel.go
+++ internal/service/medialive/channel.go
@@ -1197,7 +1197,7 @@ func expandInputAttachmentInputSettings(tfList []interface{}) *types.InputSettin
 		out.DenoiseFilter = types.InputDenoiseFilter(v)
 	}
 	if v, ok := m["filter_strength"].(int); ok {
-		out.FilterStrength = int32(v)
+		out.FilterStrength = aws.Int32(int32(v))
 	}
 	if v, ok := m["input_filter"].(string); ok && v != "" {
 		out.InputFilter = types.InputFilter(v)
@@ -1206,7 +1206,7 @@ func expandInputAttachmentInputSettings(tfList []interface{}) *types.InputSettin
 		out.NetworkInputSettings = expandInputAttachmentInputSettingsNetworkInputSettings(v)
 	}
 	if v, ok := m["scte35_pid"].(int); ok {
-		out.Scte35Pid = int32(v)
+		out.Scte35Pid = aws.Int32(int32(v))
 	}
 	if v, ok := m["smpte2038_data_preference"].(string); ok && v != "" {
 		out.Smpte2038DataPreference = types.Smpte2038DataPreference(v)
@@ -1309,7 +1309,7 @@ func expandInputAttachmentInputSettingsAudioSelectorsSelectorSettingsAudioPidSel
 
 	var out types.AudioPidSelection
 	if v, ok := m["pid"].(int); ok {
-		out.Pid = int32(v)
+		out.Pid = aws.Int32(int32(v))
 	}
 
 	return &out
@@ -1347,7 +1347,7 @@ func expandInputAttachmentInputSettingsAudioSelectorsSelectorSettingsAudioTrackS
 
 		var o types.AudioTrack
 		if v, ok := m["track"].(int); ok {
-			o.Track = int32(v)
+			o.Track = aws.Int32(int32(v))
 		}
 
 		out = append(out, o)
@@ -1442,7 +1442,7 @@ func expandInputAttachmentInputSettingsCaptionSelectorsSelectorSettingsAncillary
 
 	var out types.AncillarySourceSettings
 	if v, ok := m["source_ancillary_channel_number"].(int); ok {
-		out.SourceAncillaryChannelNumber = int32(v)
+		out.SourceAncillaryChannelNumber = aws.Int32(int32(v))
 	}
 
 	return &out
@@ -1460,7 +1460,7 @@ func expandInputAttachmentInputSettingsCaptionSelectorsSelectorSettingsDvbSubSou
 		out.OcrLanguage = types.DvbSubOcrLanguage(v)
 	}
 	if v, ok := m["pid"].(int); ok {
-		out.Pid = int32(v)
+		out.Pid = aws.Int32(int32(v))
 	}
 
 	return &out
@@ -1481,7 +1481,7 @@ func expandInputAttachmentInputSettingsCaptionSelectorsSelectorSettingsEmbeddedS
 		out.Scte20Detection = types.EmbeddedScte20Detection(v)
 	}
 	if v, ok := m["source_608_channel_number"].(int); ok {
-		out.Source608ChannelNumber = int32(v)
+		out.Source608ChannelNumber = aws.Int32(int32(v))
 	}
 
 	return &out
@@ -1499,7 +1499,7 @@ func expandInputAttachmentInputSettingsCaptionSelectorsSelectorSettingsScte20Sou
 		out.Convert608To708 = types.Scte20Convert608To708(v)
 	}
 	if v, ok := m["source_608_channel_number"].(int); ok {
-		out.Source608ChannelNumber = int32(v)
+		out.Source608ChannelNumber = aws.Int32(int32(v))
 	}
 
 	return &out
@@ -1517,7 +1517,7 @@ func expandInputAttachmentInputSettingsCaptionSelectorsSelectorSettingsScte27Sou
 		out.OcrLanguage = types.Scte27OcrLanguage(v)
 	}
 	if v, ok := m["pid"].(int); ok {
-		out.Pid = int32(v)
+		out.Pid = aws.Int32(int32(v))
 	}
 
 	return &out
@@ -1550,16 +1550,16 @@ func expandInputAttachmentInputSettingsCaptionSelectorsSelectorSettingsTeletextS
 
 	var out types.CaptionRectangle
 	if v, ok := m["height"].(float32); ok {
-		out.Height = float64(v)
+		out.Height = aws.Float64(float64(v))
 	}
 	if v, ok := m["left_offset"].(float32); ok {
-		out.LeftOffset = float64(v)
+		out.LeftOffset = aws.Float64(float64(v))
 	}
 	if v, ok := m["top_offset"].(float32); ok {
-		out.TopOffset = float64(v)
+		out.TopOffset = aws.Float64(float64(v))
 	}
 	if v, ok := m["width"].(float32); ok {
-		out.Width = float64(v)
+		out.Width = aws.Float64(float64(v))
 	}
 
 	return &out
@@ -1592,16 +1592,16 @@ func expandNetworkInputSettingsHLSInputSettings(tfList []interface{}) *types.Hls
 
 	var out types.HlsInputSettings
 	if v, ok := m["bandwidth"].(int); ok {
-		out.Bandwidth = int32(v)
+		out.Bandwidth = aws.Int32(int32(v))
 	}
 	if v, ok := m["buffer_segments"].(int); ok {
-		out.BufferSegments = int32(v)
+		out.BufferSegments = aws.Int32(int32(v))
 	}
 	if v, ok := m["retries"].(int); ok {
-		out.Retries = int32(v)
+		out.Retries = aws.Int32(int32(v))
 	}
 	if v, ok := m["retry_interval"].(int); ok {
-		out.RetryInterval = int32(v)
+		out.RetryInterval = aws.Int32(int32(v))
 	}
 	if v, ok := m["scte35_source"].(string); ok && v != "" {
 		out.Scte35Source = types.HlsScte35SourceType(v)
@@ -1622,7 +1622,7 @@ func expandInputAttachmentAutomaticInputFailoverSettings(tfList []interface{}) *
 		out.SecondaryInputId = aws.String(v)
 	}
 	if v, ok := m["error_clear_time_msec"].(int); ok {
-		out.ErrorClearTimeMsec = int32(v)
+		out.ErrorClearTimeMsec = aws.Int32(int32(v))
 	}
 	if v, ok := m["failover_conditions"].(*schema.Set); ok && v.Len() > 0 {
 		out.FailoverConditions = expandInputAttachmentAutomaticInputFailoverSettingsFailoverConditions(v.List())
@@ -1690,7 +1690,7 @@ func expandInputAttachmentAutomaticInputFailoverSettingsFailoverConditionsFailov
 		out.AudioSelectorName = aws.String(v)
 	}
 	if v, ok := m["audio_silence_threshold_msec"].(int); ok {
-		out.AudioSilenceThresholdMsec = int32(v)
+		out.AudioSilenceThresholdMsec = aws.Int32(int32(v))
 	}
 
 	return &out
@@ -1705,7 +1705,7 @@ func expandInputAttachmentAutomaticInputFailoverSettingsFailoverConditionsFailov
 
 	var out types.InputLossFailoverSettings
 	if v, ok := m["input_loss_threshold_msec"].(int); ok {
-		out.InputLossThresholdMsec = int32(v)
+		out.InputLossThresholdMsec = aws.Int32(int32(v))
 	}
 
 	return &out
@@ -1720,10 +1720,10 @@ func expandInputAttachmentAutomaticInputFailoverSettingsFailoverConditionsFailov
 
 	var out types.VideoBlackFailoverSettings
 	if v, ok := m["black_detect_threshold"].(float32); ok {
-		out.BlackDetectThreshold = float64(v)
+		out.BlackDetectThreshold = aws.Float64(float64(v))
 	}
 	if v, ok := m["video_black_threshold_msec"].(int); ok {
-		out.VideoBlackThresholdMsec = int32(v)
+		out.VideoBlackThresholdMsec = aws.Int32(int32(v))
 	}
 
 	return &out
@@ -1760,10 +1760,10 @@ func flattenInputAttachmentsInputSettings(in *types.InputSettings) []interface{}
 		"caption_selector":          flattenInputAttachmentsInputSettingsCaptionSelectors(in.CaptionSelectors),
 		"deblock_filter":            string(in.DeblockFilter),
 		"denoise_filter":            string(in.DenoiseFilter),
-		"filter_strength":           int(in.FilterStrength),
+		"filter_strength":           int(aws.ToInt32(in.FilterStrength)),
 		"input_filter":              string(in.InputFilter),
 		"network_input_settings":    flattenInputAttachmentsInputSettingsNetworkInputSettings(in.NetworkInputSettings),
-		"scte35_pid":                int(in.Scte35Pid),
+		"scte35_pid":                int(aws.ToInt32(in.Scte35Pid)),
 		"smpte2038_data_preference": string(in.Smpte2038DataPreference),
 		"source_end_behavior":       string(in.SourceEndBehavior),
 	}
@@ -1837,7 +1837,7 @@ func flattenInputAttachmentsInputSettingsAudioSelectorsSelectorSettingsAudioPidS
 	}
 
 	m := map[string]interface{}{
-		"pid": int(in.Pid),
+		"pid": int(aws.ToInt32(in.Pid)),
 	}
 
 	return []interface{}{m}
@@ -1877,7 +1877,7 @@ func flattenInputAttachmentsInputSettingsAudioSelectorsSelectorSettingsAudioTrac
 
 	for _, v := range tfList {
 		m := map[string]interface{}{
-			"track": int(v.Track),
+			"track": int(aws.ToInt32(v.Track)),
 		}
 
 		out = append(out, m)
@@ -1930,7 +1930,7 @@ func flattenInputAttachmentsInputSettingsCaptionSelectorsSelectorSettingsAncilla
 	}
 
 	m := map[string]interface{}{
-		"source_ancillary_channel_number": int(in.SourceAncillaryChannelNumber),
+		"source_ancillary_channel_number": int(aws.ToInt32(in.SourceAncillaryChannelNumber)),
 	}
 
 	return []interface{}{m}
@@ -1943,7 +1943,7 @@ func flattenInputAttachmentsInputSettingsCaptionSelectorsSelectorSettingsDvbSubS
 
 	m := map[string]interface{}{
 		"ocr_language": string(in.OcrLanguage),
-		"pid":          int(in.Pid),
+		"pid":          int(aws.ToInt32(in.Pid)),
 	}
 
 	return []interface{}{m}
@@ -1957,7 +1957,7 @@ func flattenInputAttachmentsInputSettingsCaptionSelectorsSelectorSettingsEmbedde
 	m := map[string]interface{}{
 		"convert_608_to_708":        string(in.Convert608To708),
 		"scte20_detection":          string(in.Scte20Detection),
-		"source_608_channel_number": int(in.Source608ChannelNumber),
+		"source_608_channel_number": int(aws.ToInt32(in.Source608ChannelNumber)),
 	}
 
 	return []interface{}{m}
@@ -1970,7 +1970,7 @@ func flattenInputAttachmentsInputSettingsCaptionSelectorsSelectorSettingsScte20S
 
 	m := map[string]interface{}{
 		"convert_608_to_708":        string(in.Convert608To708),
-		"source_608_channel_number": int(in.Source608ChannelNumber),
+		"source_608_channel_number": int(aws.ToInt32(in.Source608ChannelNumber)),
 	}
 
 	return []interface{}{m}
@@ -1983,7 +1983,7 @@ func flattenInputAttachmentsInputSettingsCaptionSelectorsSelectorSettingsScte27S
 
 	m := map[string]interface{}{
 		"ocr_language": string(in.OcrLanguage),
-		"pid":          int(in.Pid),
+		"pid":          int(aws.ToInt32(in.Pid)),
 	}
 
 	return []interface{}{m}
@@ -2008,10 +2008,10 @@ func flattenInputAttachmentsInputSettingsCaptionSelectorsSelectorSettingsTeletex
 	}
 
 	m := map[string]interface{}{
-		"height":      float32(in.Height),
-		"left_offset": float32(in.LeftOffset),
-		"top_offset":  float32(in.TopOffset),
-		"width":       float32(in.Width),
+		"height":      float32(aws.ToFloat64(in.Height)),
+		"left_offset": float32(aws.ToFloat64(in.LeftOffset)),
+		"top_offset":  float32(aws.ToFloat64(in.TopOffset)),
+		"width":       float32(aws.ToFloat64(in.Width)),
 	}
 
 	return []interface{}{m}
@@ -2036,10 +2036,10 @@ func flattenNetworkInputSettingsHLSInputSettings(in *types.HlsInputSettings) []i
 	}
 
 	m := map[string]interface{}{
-		"bandwidth":       int(in.Bandwidth),
-		"buffer_segments": int(in.BufferSegments),
-		"retries":         int(in.Retries),
-		"retry_interval":  int(in.RetryInterval),
+		"bandwidth":       int(aws.ToInt32(in.Bandwidth)),
+		"buffer_segments": int(aws.ToInt32(in.BufferSegments)),
+		"retries":         int(aws.ToInt32(in.Retries)),
+		"retry_interval":  int(aws.ToInt32(in.RetryInterval)),
 		"scte35_source":   string(in.Scte35Source),
 	}
 
@@ -2053,7 +2053,7 @@ func flattenInputAttachmentAutomaticInputFailoverSettings(in *types.AutomaticInp
 
 	m := map[string]interface{}{
 		"secondary_input_id":    aws.ToString(in.SecondaryInputId),
-		"error_clear_time_msec": int(in.ErrorClearTimeMsec),
+		"error_clear_time_msec": int(aws.ToInt32(in.ErrorClearTimeMsec)),
 		"failover_conditions":   flattenInputAttachmentAutomaticInputFailoverSettingsFailoverConditions(in.FailoverConditions),
 		"input_preference":      string(in.InputPreference),
 	}
@@ -2099,7 +2099,7 @@ func flattenInputAttachmentAutomaticInputFailoverSettingsFailoverConditionsFailo
 
 	m := map[string]interface{}{
 		"audio_selector_name":          aws.ToString(in.AudioSelectorName),
-		"audio_silence_threshold_msec": int(in.AudioSilenceThresholdMsec),
+		"audio_silence_threshold_msec": int(aws.ToInt32(in.AudioSilenceThresholdMsec)),
 	}
 
 	return []interface{}{m}
@@ -2111,7 +2111,7 @@ func flattenInputAttachmentAutomaticInputFailoverSettingsFailoverConditionsFailo
 	}
 
 	m := map[string]interface{}{
-		"input_loss_threshold_msec": int(in.InputLossThresholdMsec),
+		"input_loss_threshold_msec": int(aws.ToInt32(in.InputLossThresholdMsec)),
 	}
 
 	return []interface{}{m}
@@ -2123,8 +2123,8 @@ func flattenInputAttachmentAutomaticInputFailoverSettingsFailoverConditionsFailo
 	}
 
 	m := map[string]interface{}{
-		"black_detect_threshold":     float32(in.BlackDetectThreshold),
-		"video_black_threshold_msec": int(in.VideoBlackThresholdMsec),
+		"black_detect_threshold":     float32(aws.ToFloat64(in.BlackDetectThreshold)),
+		"video_black_threshold_msec": int(aws.ToInt32(in.VideoBlackThresholdMsec)),
 	}
 
 	return []interface{}{m}
