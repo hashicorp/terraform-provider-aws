273b2c91b827d389cae7c93e455d213d371274b4
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-10-31 15:12:43 -0400

    Run 'make gen'.

--- internal/service/athena/service_package_gen.go
+++ internal/service/athena/service_package_gen.go
@@ -5,9 +5,8 @@ package athena
 import (
 	"context"
 
-	aws_sdkv1 "github.com/aws/aws-sdk-go/aws"
-	session_sdkv1 "github.com/aws/aws-sdk-go/aws/session"
-	athena_sdkv1 "github.com/aws/aws-sdk-go/service/athena"
+	aws_sdkv2 "github.com/aws/aws-sdk-go-v2/aws"
+	athena_sdkv2 "github.com/aws/aws-sdk-go-v2/service/athena"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/types"
 	"github.com/hashicorp/terraform-provider-aws/names"
@@ -60,11 +59,15 @@ func (p *servicePackage) ServicePackageName() string {
 	return names.Athena
 }
 
-// NewConn returns a new AWS SDK for Go v1 client for this service package's AWS API.
-func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*athena_sdkv1.Athena, error) {
-	sess := config["session"].(*session_sdkv1.Session)
+// NewClient returns a new AWS SDK for Go v2 client for this service package's AWS API.
+func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (*athena_sdkv2.Client, error) {
+	cfg := *(config["aws_sdkv2_config"].(*aws_sdkv2.Config))
 
-	return athena_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config["endpoint"].(string))})), nil
+	return athena_sdkv2.NewFromConfig(cfg, func(o *athena_sdkv2.Options) {
+		if endpoint := config["endpoint"].(string); endpoint != "" {
+			o.BaseEndpoint = aws_sdkv2.String(endpoint)
+		}
+	}), nil
 }
 
 func ServicePackage(ctx context.Context) conns.ServicePackage {
