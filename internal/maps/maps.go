de84435e75cc5bd09a0da62cd7fa524b006b4957
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-11-01 12:03:27 -0400

    Add 'maps.ApplyToAllKeys'.

--- internal/maps/maps.go
+++ internal/maps/maps.go
@@ -3,9 +3,20 @@
 
 package maps
 
+// ApplyToAllKeys returns a new map containing the results of applying the function `f` to each key of the original map `m`.
+func ApplyToAllKeys[K1, K2 comparable, V any](m map[K1]V, f func(K1) K2) map[K2]V {
+	n := make(map[K2]V, len(m))
+
+	for k, v := range m {
+		n[f(k)] = v
+	}
+
+	return n
+}
+
 // ApplyToAllValues returns a new map containing the results of applying the function `f` to each value of the original map `m`.
-func ApplyToAllValues[K comparable, T, U any](m map[K]T, f func(T) U) map[K]U {
-	n := make(map[K]U, len(m))
+func ApplyToAllValues[K comparable, V1, V2 any](m map[K]V1, f func(V1) V2) map[K]V2 {
+	n := make(map[K]V2, len(m))
 
 	for k, v := range m {
 		n[k] = f(v)
