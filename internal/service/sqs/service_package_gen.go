2816f005aa57e197bdbe8495af0a32e8d5843be6
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-11-01 08:41:47 -0400

    Run 'make gen'.

--- internal/service/sqs/service_package_gen.go
+++ internal/service/sqs/service_package_gen.go
@@ -5,9 +5,8 @@ package sqs
 import (
 	"context"
 
-	aws_sdkv1 "github.com/aws/aws-sdk-go/aws"
-	session_sdkv1 "github.com/aws/aws-sdk-go/aws/session"
-	sqs_sdkv1 "github.com/aws/aws-sdk-go/service/sqs"
+	aws_sdkv2 "github.com/aws/aws-sdk-go-v2/aws"
+	sqs_sdkv2 "github.com/aws/aws-sdk-go-v2/service/sqs"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/types"
 	"github.com/hashicorp/terraform-provider-aws/names"
@@ -65,11 +64,15 @@ func (p *servicePackage) ServicePackageName() string {
 	return names.SQS
 }
 
-// NewConn returns a new AWS SDK for Go v1 client for this service package's AWS API.
-func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*sqs_sdkv1.SQS, error) {
-	sess := config["session"].(*session_sdkv1.Session)
+// NewClient returns a new AWS SDK for Go v2 client for this service package's AWS API.
+func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (*sqs_sdkv2.Client, error) {
+	cfg := *(config["aws_sdkv2_config"].(*aws_sdkv2.Config))
 
-	return sqs_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config["endpoint"].(string))})), nil
+	return sqs_sdkv2.NewFromConfig(cfg, func(o *sqs_sdkv2.Options) {
+		if endpoint := config["endpoint"].(string); endpoint != "" {
+			o.BaseEndpoint = aws_sdkv2.String(endpoint)
+		}
+	}), nil
 }
 
 func ServicePackage(ctx context.Context) conns.ServicePackage {
