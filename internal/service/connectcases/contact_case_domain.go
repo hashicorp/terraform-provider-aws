03f9e1e8143d1f44656814144cd547972e2cf570
Daniel Rieske <daniel.rieske@gmail.com>
2023-11-05 14:54:09 +0100

    feat: added connect case domain resource

--- internal/service/connectcases/contact_case_domain.go
+++ internal/service/connectcases/contact_case_domain.go
@@ -1,1 +1,114 @@
 package connectcases
+
+import (
+	"context"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/connectcases"
+	"github.com/aws/aws-sdk-go-v2/service/connectcases/types"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/errs"
+	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
+	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
+)
+
+// @SDKResource("aws_connectcases_contact_case_domain", name="Connect Cases Contact Case Domain")
+func ResourceContactCaseDomain() *schema.Resource {
+	return &schema.Resource{
+		CreateWithoutTimeout: resourceContactCaseDomainCreate,
+		ReadWithoutTimeout:   resourceContactCaseRead,
+		DeleteWithoutTimeout: resourceContactCaseDelete,
+
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"domain_arn": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"domain_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"domain_status": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+		CustomizeDiff: verify.SetTagsDiff,
+	}
+}
+
+func resourceContactCaseDomainCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	conn := meta.(*conns.AWSClient).ConnectCasesClient(ctx)
+	log.Print("[DEBUG] Creating Connect Case Domain")
+
+	name := d.Get("name").(string)
+	params := &connectcases.CreateDomainInput{
+		Name: aws.String(name),
+	}
+
+	output, err := conn.CreateDomain(ctx, params)
+	if err != nil {
+		return diag.Errorf("creating Connect Case Domain (%s): %s", name, err)
+	}
+
+	d.SetId(aws.ToString(output.DomainId))
+
+	return append(diags, resourceContactCaseRead(ctx, d, meta)...)
+}
+
+func resourceContactCaseRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	conn := meta.(*conns.AWSClient).ConnectCasesClient(ctx)
+
+	output, err := findConnectCasesDomainById(ctx, conn, d.Id())
+
+	if !d.IsNewResource() && tfresource.NotFound(err) {
+		log.Printf("[WARN] Connect Case Domain %s not found, removing from state", d.Id())
+		d.SetId("")
+		return diags
+	}
+
+	if err != nil {
+		return sdkdiag.AppendErrorf(diags, "reading Connect Case Domain (%s): %s", d.Id(), err)
+	}
+
+	d.Set("name", output.Name)
+	d.Set("domain_arn", output.DomainArn)
+	d.Set("domain_id", output.DomainId)
+	d.Set("domain_status", output.DomainStatus)
+
+	return diags
+}
+
+func resourceContactCaseDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn := meta.(*conns.AWSClient).ConnectCasesClient(ctx)
+
+	log.Printf("[DEBUG] Deleting Connect Case Domain: %s", d.Id())
+	_, err := conn.DeleteDomain(ctx, &connectcases.DeleteDomainInput{
+		DomainId: aws.String(d.Id()),
+	})
+
+	if errs.IsA[*types.ResourceNotFoundException](err) {
+		return nil
+	}
+
+	if err != nil {
+		return diag.Errorf("deleting Connect Case Domain (%s): %s", d.Id(), err)
+	}
+
+	return nil
+}
---
+++ internal/service/connectcases/find.go
@@ -0,0 +1,37 @@
+package connectcases
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/connectcases"
+	"github.com/aws/aws-sdk-go-v2/service/connectcases/types"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+	"github.com/hashicorp/terraform-provider-aws/internal/errs"
+	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
+)
+
+func findConnectCasesDomainById(ctx context.Context, conn *connectcases.Client, id string) (*connectcases.GetDomainOutput, error) {
+	input := &connectcases.GetDomainInput{
+		DomainId: aws.String(id),
+	}
+
+	output, err := conn.GetDomain(ctx, input)
+
+	if errs.IsA[*types.ResourceNotFoundException](err) {
+		return nil, &retry.NotFoundError{
+			LastError:   err,
+			LastRequest: input,
+		}
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	if output == nil {
+		return nil, tfresource.NewEmptyResultError(input)
+	}
+
+	return output, nil
+}
