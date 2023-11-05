d36eeb3a31c1456f2fde8bda81ce41c547c75724
Daniel Rieske <daniel.rieske@gmail.com>
2023-11-05 15:19:38 +0100

    feat: added contact case layout

--- internal/service/connectcases/contact_case_layout.go
+++ internal/service/connectcases/contact_case_layout.go
@@ -1,1 +1,132 @@
 package connectcases
+
+import (
+	"context"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/connectcases"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
+	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
+)
+
+// @SDKResource("aws_connectcases_contact_case_layout", name="Connect Cases Contact Case Layout")
+func ResourceContactCaseLayout() *schema.Resource {
+	return &schema.Resource{
+		CreateWithoutTimeout: resourceContactCaseLayoutCreate,
+		ReadWithoutTimeout:   resourceContactCaseDomainRead,
+		DeleteWithoutTimeout: schema.NoopContext,
+
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"domain_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"content": {
+				Type:     schema.TypeList,
+				Required: true,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"value": {
+							Type:     schema.TypeList,
+							Required: true,
+							MaxItems: 1,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"more_info": {
+										Type:     schema.TypeList,
+										Required: true,
+										MaxItems: 1,
+										Elem: &schema.Resource{
+											Schema: map[string]*schema.Schema{
+												"sections": {
+													Type:     schema.TypeList,
+													Optional: true,
+													Elem:     &schema.Schema{Type: schema.TypeString},
+												},
+											},
+										},
+									},
+									"top_panel": {
+										Type:     schema.TypeList,
+										Required: true,
+										MaxItems: 1,
+										Elem: &schema.Resource{
+											Schema: map[string]*schema.Schema{
+												"sections": {
+													Type:     schema.TypeList,
+													Optional: true,
+													Elem:     &schema.Schema{Type: schema.TypeString},
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		CustomizeDiff: verify.SetTagsDiff,
+	}
+}
+
+func resourceContactCaseLayoutCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	conn := meta.(*conns.AWSClient).ConnectCasesClient(ctx)
+	log.Print("[DEBUG] Creating Connect Case Layout")
+
+	name := d.Get("name").(string)
+	params := &connectcases.CreateLayoutInput{
+		DomainId: aws.String(d.Get("domain_id").(string)),
+		Name:     aws.String(name),
+	}
+
+	output, err := conn.CreateLayout(ctx, params)
+	if err != nil {
+		return diag.Errorf("creating Connect Case Layout (%s): %s", name, err)
+	}
+
+	d.SetId(aws.ToString(output.LayoutId))
+
+	return append(diags, resourceContactCaseLayoutRead(ctx, d, meta)...)
+}
+
+func resourceContactCaseLayoutRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	conn := meta.(*conns.AWSClient).ConnectCasesClient(ctx)
+
+	output, err := findConnectCasesDomainById(ctx, conn, d.Id())
+
+	if !d.IsNewResource() && tfresource.NotFound(err) {
+		log.Printf("[WARN] Connect Case Domain %s not found, removing from state", d.Id())
+		d.SetId("")
+		return diags
+	}
+
+	if err != nil {
+		return sdkdiag.AppendErrorf(diags, "reading Connect Case Domain (%s): %s", d.Id(), err)
+	}
+
+	d.Set("name", output.Name)
+	d.Set("domain_arn", output.DomainArn)
+	d.Set("domain_id", output.DomainId)
+	d.Set("domain_status", output.DomainStatus)
+
+	return diags
+}
