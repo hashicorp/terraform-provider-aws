f8066d3d930dbf49955ea88f596e47cf384ebf52
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-11-07 10:32:58 -0500

    Restore 'NewConn' for sts package.

--- internal/service/sts/service_package.go
+++ internal/service/sts/service_package.go
@@ -8,7 +8,22 @@ import (
 
 	aws_sdkv2 "github.com/aws/aws-sdk-go-v2/aws"
 	sts_sdkv2 "github.com/aws/aws-sdk-go-v2/service/sts"
+	aws_sdkv1 "github.com/aws/aws-sdk-go/aws"
+	session_sdkv1 "github.com/aws/aws-sdk-go/aws/session"
+	sts_sdkv1 "github.com/aws/aws-sdk-go/service/sts"
 )
+
+// NewConn returns a new AWS SDK for Go v1 client for this service package's AWS API.
+func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*sts_sdkv1.STS, error) {
+	sess := config["session"].(*session_sdkv1.Session)
+	cfg := &aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config["endpoint"].(string))}
+
+	if stsRegion := config["sts_region"].(string); stsRegion != "" {
+		cfg.Region = aws_sdkv1.String(stsRegion)
+	}
+
+	return sts_sdkv1.New(sess.Copy(cfg)), nil
+}
 
 // NewClient returns a new AWS SDK for Go v2 client for this service package's AWS API.
 func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (*sts_sdkv2.Client, error) {
