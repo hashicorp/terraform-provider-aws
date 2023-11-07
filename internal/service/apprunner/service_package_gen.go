030f895882e5aaddb2b64a9085bae74a0732fcbf
Brittan DeYoung <32572259+brittandeyoung@users.noreply.github.com>
2023-11-07 11:39:33 -0500

    apprunner: Amend service_package_gen, migrate to aws-sdk-go-v2

--- internal/service/apprunner/service_package_gen.go
+++ internal/service/apprunner/service_package_gen.go
@@ -5,9 +5,8 @@ package apprunner
 import (
 	"context"
 
-	aws_sdkv1 "github.com/aws/aws-sdk-go/aws"
-	session_sdkv1 "github.com/aws/aws-sdk-go/aws/session"
-	apprunner_sdkv1 "github.com/aws/aws-sdk-go/service/apprunner"
+	aws_sdkv2 "github.com/aws/aws-sdk-go-v2/aws"
+	apprunner_sdkv2 "github.com/aws/aws-sdk-go-v2/service/apprunner"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/types"
 	"github.com/hashicorp/terraform-provider-aws/names"
@@ -88,11 +87,15 @@ func (p *servicePackage) ServicePackageName() string {
 	return names.AppRunner
 }
 
-// NewConn returns a new AWS SDK for Go v1 client for this service package's AWS API.
-func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*apprunner_sdkv1.AppRunner, error) {
-	sess := config["session"].(*session_sdkv1.Session)
+// NewClient returns a new AWS SDK for Go v2 client for this service package's AWS API.
+func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (*apprunner_sdkv2.Client, error) {
+	cfg := *(config["aws_sdkv2_config"].(*aws_sdkv2.Config))
 
-	return apprunner_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config["endpoint"].(string))})), nil
+	return apprunner_sdkv2.NewFromConfig(cfg, func(o *apprunner_sdkv2.Options) {
+		if endpoint := config["endpoint"].(string); endpoint != "" {
+			o.BaseEndpoint = aws_sdkv2.String(endpoint)
+		}
+	}), nil
 }
 
 func ServicePackage(ctx context.Context) conns.ServicePackage {
