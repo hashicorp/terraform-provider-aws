7d3f05f5c8fb06d41098ed96eacd13378e0e8a71
Brittan DeYoung <32572259+brittandeyoung@users.noreply.github.com>
2023-11-07 11:36:46 -0500

    apprunner: Amend connection, migrate to aws-sdk-go-v2

--- internal/service/apprunner/connection.go
+++ internal/service/apprunner/connection.go
@@ -7,13 +7,14 @@ import (
 	"context"
 	"log"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/apprunner"
-	"github.com/hashicorp/aws-sdk-go-base/v2/awsv1shim/v2/tfawserr"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/apprunner"
+	"github.com/aws/aws-sdk-go-v2/service/apprunner/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 	"github.com/hashicorp/terraform-provider-aws/names"
@@ -47,7 +48,7 @@ func ResourceConnection() *schema.Resource {
 			"provider_type": {
 				Type:         schema.TypeString,
 				Required:     true,
-				ValidateFunc: validation.StringInSlice(apprunner.ProviderType_Values(), false),
+				ValidateFunc: validation.StringInSlice(flattenProviderTypeValues(types.ProviderType("").Values()), false),
 				ForceNew:     true,
 			},
 
@@ -65,16 +66,16 @@ func ResourceConnection() *schema.Resource {
 }
 
 func resourceConnectionCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	conn := meta.(*conns.AWSClient).AppRunnerConn(ctx)
+	conn := meta.(*conns.AWSClient).AppRunnerClient(ctx)
 
 	name := d.Get("connection_name").(string)
 	input := &apprunner.CreateConnectionInput{
 		ConnectionName: aws.String(name),
-		ProviderType:   aws.String(d.Get("provider_type").(string)),
+		ProviderType:   types.ProviderType(d.Get("provider_type").(string)),
 		Tags:           getTagsIn(ctx),
 	}
 
-	output, err := conn.CreateConnectionWithContext(ctx, input)
+	output, err := conn.CreateConnection(ctx, input)
 
 	if err != nil {
 		return diag.Errorf("creating App Runner Connection (%s): %s", name, err)
@@ -84,17 +85,17 @@ func resourceConnectionCreate(ctx context.Context, d *schema.ResourceData, meta 
 		return diag.Errorf("creating App Runner Connection (%s): empty output", name)
 	}
 
-	d.SetId(aws.StringValue(output.Connection.ConnectionName))
+	d.SetId(aws.ToString(output.Connection.ConnectionName))
 
 	return resourceConnectionRead(ctx, d, meta)
 }
 
 func resourceConnectionRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	conn := meta.(*conns.AWSClient).AppRunnerConn(ctx)
+	conn := meta.(*conns.AWSClient).AppRunnerClient(ctx)
 
-	c, err := FindConnectionSummaryByName(ctx, conn, d.Id())
+	c, err := FindConnectionsummaryByName(ctx, conn, d.Id())
 
-	if !d.IsNewResource() && tfawserr.ErrCodeEquals(err, apprunner.ErrCodeResourceNotFoundException) {
+	if !d.IsNewResource() && errs.IsA[*types.ResourceNotFoundException](err) {
 		log.Printf("[WARN] App Runner Connection (%s) not found, removing from state", d.Id())
 		d.SetId("")
 		return nil
@@ -113,7 +114,7 @@ func resourceConnectionRead(ctx context.Context, d *schema.ResourceData, meta in
 		return nil
 	}
 
-	arn := aws.StringValue(c.ConnectionArn)
+	arn := aws.ToString(c.ConnectionArn)
 
 	d.Set("arn", arn)
 	d.Set("connection_name", c.ConnectionName)
@@ -129,27 +130,37 @@ func resourceConnectionUpdate(ctx context.Context, d *schema.ResourceData, meta 
 }
 
 func resourceConnectionDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	conn := meta.(*conns.AWSClient).AppRunnerConn(ctx)
+	conn := meta.(*conns.AWSClient).AppRunnerClient(ctx)
 
 	input := &apprunner.DeleteConnectionInput{
 		ConnectionArn: aws.String(d.Get("arn").(string)),
 	}
 
-	_, err := conn.DeleteConnectionWithContext(ctx, input)
+	_, err := conn.DeleteConnection(ctx, input)
 
 	if err != nil {
-		if tfawserr.ErrCodeEquals(err, apprunner.ErrCodeResourceNotFoundException) {
+		if errs.IsA[*types.ResourceNotFoundException](err) {
 			return nil
 		}
 		return diag.Errorf("deleting App Runner Connection (%s): %s", d.Id(), err)
 	}
 
 	if err := WaitConnectionDeleted(ctx, conn, d.Id()); err != nil {
-		if tfawserr.ErrCodeEquals(err, apprunner.ErrCodeResourceNotFoundException) {
+		if errs.IsA[*types.ResourceNotFoundException](err) {
 			return nil
 		}
 		return diag.Errorf("waiting for App Runner Connection (%s) deletion: %s", d.Id(), err)
 	}
 
 	return nil
+}
+
+func flattenProviderTypeValues(t []types.ProviderType) []string {
+	var out []string
+
+	for _, v := range t {
+		out = append(out, string(v))
+	}
+
+	return out
 }
