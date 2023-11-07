98c6eecbf8d3aeaf94e17f2ce94f3941d67401f4
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-11-07 08:54:09 -0500

    Add 'NewClient' for sts package.

--- internal/service/sts/service_package.go
+++ internal/service/sts/service_package.go
@@ -6,19 +6,19 @@ package sts
 import (
 	"context"
 
-	aws_sdkv1 "github.com/aws/aws-sdk-go/aws"
-	session_sdkv1 "github.com/aws/aws-sdk-go/aws/session"
-	sts_sdkv1 "github.com/aws/aws-sdk-go/service/sts"
+	aws_sdkv2 "github.com/aws/aws-sdk-go-v2/aws"
+	sts_sdkv2 "github.com/aws/aws-sdk-go-v2/service/sts"
 )
 
-// NewConn returns a new AWS SDK for Go v1 client for this service package's AWS API.
-func (p *servicePackage) NewConn(ctx context.Context, m map[string]any) (*sts_sdkv1.STS, error) {
-	sess := m["session"].(*session_sdkv1.Session)
-	config := &aws_sdkv1.Config{Endpoint: aws_sdkv1.String(m["endpoint"].(string))}
+// NewClient returns a new AWS SDK for Go v2 client for this service package's AWS API.
+func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (*sts_sdkv2.Client, error) {
+	cfg := *(config["aws_sdkv2_config"].(*aws_sdkv2.Config))
 
-	if stsRegion := m["sts_region"].(string); stsRegion != "" {
-		config.Region = aws_sdkv1.String(stsRegion)
-	}
-
-	return sts_sdkv1.New(sess.Copy(config)), nil
+	return sts_sdkv2.NewFromConfig(cfg, func(o *sts_sdkv2.Options) {
+		if endpoint := config["endpoint"].(string); endpoint != "" {
+			o.BaseEndpoint = aws_sdkv2.String(endpoint)
+		} else if stsRegion := config["sts_region"].(string); stsRegion != "" {
+			o.Region = stsRegion
+		}
+	}), nil
 }
