f5bd4965fc694741b45a113db640d2d4eebd1edf
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-11-09 15:04:48 -0500

    r/aws_cloudwatch_log_destination_policy: Migrate to AWS SDK for Go v2.

--- internal/service/logs/destination_policy.go
+++ internal/service/logs/destination_policy.go
@@ -7,8 +7,8 @@ import (
 	"context"
 	"log"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/structure"
@@ -55,7 +55,7 @@ func resourceDestinationPolicy() *schema.Resource {
 }
 
 func resourceDestinationPolicyPut(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	conn := meta.(*conns.AWSClient).LogsConn(ctx)
+	conn := meta.(*conns.AWSClient).LogsClient(ctx)
 
 	name := d.Get("destination_name").(string)
 	input := &cloudwatchlogs.PutDestinationPolicyInput{
@@ -67,7 +67,7 @@ func resourceDestinationPolicyPut(ctx context.Context, d *schema.ResourceData, m
 		input.ForceUpdate = aws.Bool(v.(bool))
 	}
 
-	_, err := conn.PutDestinationPolicyWithContext(ctx, input)
+	_, err := conn.PutDestinationPolicy(ctx, input)
 
 	if err != nil {
 		return diag.Errorf("putting CloudWatch Logs Destination Policy (%s): %s", name, err)
@@ -81,7 +81,7 @@ func resourceDestinationPolicyPut(ctx context.Context, d *schema.ResourceData, m
 }
 
 func resourceDestinationPolicyRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	conn := meta.(*conns.AWSClient).LogsConn(ctx)
+	conn := meta.(*conns.AWSClient).LogsClient(ctx)
 
 	destination, err := FindDestinationByName(ctx, conn, d.Id())
 
