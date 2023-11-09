63e71ae738fb2b7bfa60c272e0c3679bc7187b8f
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-11-09 17:20:44 -0500

    r/aws_cloudwatch_log_subscription_filter: Migrate to AWS SDK for Go v2.

--- internal/service/logs/subscription_filter.go
+++ internal/service/logs/subscription_filter.go
@@ -11,15 +11,17 @@ import (
 	"strings"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
-	"github.com/hashicorp/aws-sdk-go-base/v2/awsv1shim/v2/tfawserr"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs"
+	"github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/create"
+	"github.com/hashicorp/terraform-provider-aws/internal/enum"
+	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
@@ -44,10 +46,10 @@ func resourceSubscriptionFilter() *schema.Resource {
 				ValidateFunc: verify.ValidARN,
 			},
 			"distribution": {
-				Type:         schema.TypeString,
-				Optional:     true,
-				Default:      cloudwatchlogs.DistributionByLogStream,
-				ValidateFunc: validation.StringInSlice(cloudwatchlogs.Distribution_Values(), false),
+				Type:             schema.TypeString,
+				Optional:         true,
+				Default:          types.DistributionByLogStream,
+				ValidateDiagFunc: enum.Validate[types.Distribution](),
 			},
 			"filter_pattern": {
 				Type:         schema.TypeString,
@@ -76,7 +78,7 @@ func resourceSubscriptionFilter() *schema.Resource {
 }
 
 func resourceSubscriptionFilterPut(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	conn := meta.(*conns.AWSClient).LogsConn(ctx)
+	conn := meta.(*conns.AWSClient).LogsClient(ctx)
 
 	logGroupName := d.Get("log_group_name").(string)
 	name := d.Get("name").(string)
@@ -88,7 +90,7 @@ func resourceSubscriptionFilterPut(ctx context.Context, d *schema.ResourceData, 
 	}
 
 	if v, ok := d.GetOk("distribution"); ok {
-		input.Distribution = aws.String(v.(string))
+		input.Distribution = types.Distribution(v.(string))
 	}
 
 	if v, ok := d.GetOk("role_arn"); ok {
@@ -97,18 +99,18 @@ func resourceSubscriptionFilterPut(ctx context.Context, d *schema.ResourceData, 
 
 	_, err := tfresource.RetryWhen(ctx, 5*time.Minute,
 		func() (interface{}, error) {
-			return conn.PutSubscriptionFilterWithContext(ctx, input)
+			return conn.PutSubscriptionFilter(ctx, input)
 		},
 		func(err error) (bool, error) {
-			if tfawserr.ErrMessageContains(err, cloudwatchlogs.ErrCodeInvalidParameterException, "Could not deliver test message to specified") {
+			if errs.IsAErrorMessageContains[*types.InvalidParameterException](err, "Could not deliver test message to specified") {
 				return true, err
 			}
 
-			if tfawserr.ErrMessageContains(err, cloudwatchlogs.ErrCodeInvalidParameterException, "Could not execute the lambda function") {
+			if errs.IsAErrorMessageContains[*types.InvalidParameterException](err, "Could not execute the lambda function") {
 				return true, err
 			}
 
-			if tfawserr.ErrMessageContains(err, cloudwatchlogs.ErrCodeOperationAbortedException, "Please try again") {
+			if errs.IsAErrorMessageContains[*types.OperationAbortedException](err, "Please try again") {
 				return true, err
 			}
 
@@ -125,7 +127,7 @@ func resourceSubscriptionFilterPut(ctx context.Context, d *schema.ResourceData, 
 }
 
 func resourceSubscriptionFilterRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	conn := meta.(*conns.AWSClient).LogsConn(ctx)
+	conn := meta.(*conns.AWSClient).LogsClient(ctx)
 
 	subscriptionFilter, err := FindSubscriptionFilterByTwoPartKey(ctx, conn, d.Get("log_group_name").(string), d.Get("name").(string))
 
@@ -150,15 +152,15 @@ func resourceSubscriptionFilterRead(ctx context.Context, d *schema.ResourceData,
 }
 
 func resourceSubscriptionFilterDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	conn := meta.(*conns.AWSClient).LogsConn(ctx)
+	conn := meta.(*conns.AWSClient).LogsClient(ctx)
 
 	log.Printf("[INFO] Deleting CloudWatch Logs Subscription Filter: %s", d.Id())
-	_, err := conn.DeleteSubscriptionFilterWithContext(ctx, &cloudwatchlogs.DeleteSubscriptionFilterInput{
+	_, err := conn.DeleteSubscriptionFilter(ctx, &cloudwatchlogs.DeleteSubscriptionFilterInput{
 		FilterName:   aws.String(d.Get("name").(string)),
 		LogGroupName: aws.String(d.Get("log_group_name").(string)),
 	})
 
-	if tfawserr.ErrCodeEquals(err, cloudwatchlogs.ErrCodeResourceNotFoundException) {
+	if errs.IsA[*types.ResourceNotFoundException](err) {
 		return nil
 	}
 
@@ -193,43 +195,33 @@ func subscriptionFilterID(log_group_name string) string {
 	return fmt.Sprintf("cwlsf-%d", create.StringHashcode(buf.String()))
 }
 
-func FindSubscriptionFilterByTwoPartKey(ctx context.Context, conn *cloudwatchlogs.CloudWatchLogs, logGroupName, name string) (*cloudwatchlogs.SubscriptionFilter, error) {
+func FindSubscriptionFilterByTwoPartKey(ctx context.Context, conn *cloudwatchlogs.Client, logGroupName, name string) (*types.SubscriptionFilter, error) {
 	input := &cloudwatchlogs.DescribeSubscriptionFiltersInput{
 		FilterNamePrefix: aws.String(name),
 		LogGroupName:     aws.String(logGroupName),
 	}
-	var output *cloudwatchlogs.SubscriptionFilter
 
-	err := conn.DescribeSubscriptionFiltersPagesWithContext(ctx, input, func(page *cloudwatchlogs.DescribeSubscriptionFiltersOutput, lastPage bool) bool {
-		if page == nil {
-			return !lastPage
-		}
+	pages := cloudwatchlogs.NewDescribeSubscriptionFiltersPaginator(conn, input)
+	for pages.HasMorePages() {
+		page, err := pages.NextPage(ctx)
 
-		for _, v := range page.SubscriptionFilters {
-			if aws.StringValue(v.FilterName) == name {
-				output = v
-
-				return false
+		if errs.IsA[*types.ResourceNotFoundException](err) {
+			return nil, &retry.NotFoundError{
+				LastError:   err,
+				LastRequest: input,
 			}
 		}
 
-		return !lastPage
-	})
+		if err != nil {
+			return nil, err
+		}
 
-	if tfawserr.ErrCodeEquals(err, cloudwatchlogs.ErrCodeResourceNotFoundException) {
-		return nil, &retry.NotFoundError{
-			LastError:   err,
-			LastRequest: input,
+		for _, v := range page.SubscriptionFilters {
+			if aws.ToString(v.FilterName) == name {
+				return &v, nil
+			}
 		}
 	}
 
-	if err != nil {
-		return nil, err
-	}
-
-	if output == nil {
-		return nil, tfresource.NewEmptyResultError(input)
-	}
-
-	return output, nil
+	return nil, tfresource.NewEmptyResultError(input)
 }
