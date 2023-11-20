e547350e8451111bc1d7d39390970fffe0adaaee
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-11-20 10:40:56 -0500

    S3 object deletion: Fix compilation errors.

--- internal/service/s3/delete.go
+++ internal/service/s3/delete.go
@@ -88,10 +88,10 @@ func deletePageOfObjectVersions(ctx context.Context, conn *s3.Client, bucket str
 
 	input := &s3.DeleteObjectsInput{
 		Bucket:                    aws.String(bucket),
-		BypassGovernanceRetention: force,
+		BypassGovernanceRetention: aws.Bool(force),
 		Delete: &types.Delete{
 			Objects: toDelete,
-			Quiet:   true, // Only report errors.
+			Quiet:   aws.Bool(true), // Only report errors.
 		},
 	}
 
@@ -181,7 +181,7 @@ func deletePageOfDeleteMarkers(ctx context.Context, conn *s3.Client, bucket stri
 		Bucket: aws.String(bucket),
 		Delete: &types.Delete{
 			Objects: toDelete,
-			Quiet:   true, // Only report errors.
+			Quiet:   aws.Bool(true), // Only report errors.
 		},
 	}
 
@@ -386,7 +386,7 @@ func deleteObjectVersion(ctx context.Context, conn *s3.Client, b, k, v string, f
 		input.VersionId = aws.String(v)
 	}
 	if force {
-		input.BypassGovernanceRetention = true
+		input.BypassGovernanceRetention = aws.Bool(true)
 	}
 
 	log.Printf("[INFO] Deleting S3 Bucket (%s) Object (%s) Version (%s)", b, k, v)
