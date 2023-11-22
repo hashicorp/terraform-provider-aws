0e21fdb3587e985652f28584e452792f57ce7fad
Adrian Johnson <adrian.johnson@hashicorp.com>
2023-11-22 17:58:53 -0500

    evidently: update sweepers

--- internal/service/evidently/sweep.go
+++ internal/service/evidently/sweep.go
@@ -7,12 +7,12 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/cloudwatchevidently"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/evidently"
 	"github.com/hashicorp/go-multierror"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
-	"github.com/hashicorp/terraform-provider-aws/internal/sweep/awsv1"
+	"github.com/hashicorp/terraform-provider-aws/internal/sweep/awsv2"
 )
 
 func RegisterSweepers() {
@@ -28,34 +28,27 @@ func sweepProject(region string) error {
 	if err != nil {
 		return fmt.Errorf("Error getting client: %w", err)
 	}
-	conn := client.EvidentlyConn(ctx)
-	input := &cloudwatchevidently.ListProjectsInput{}
+	conn := client.EvidentlyClient(ctx)
+	input := &evidently.ListProjectsInput{}
 	var sweeperErrs *multierror.Error
 	sweepResources := make([]sweep.Sweepable, 0)
 
-	err = conn.ListProjectsPagesWithContext(ctx, input, func(page *cloudwatchevidently.ListProjectsOutput, lastPage bool) bool {
-		if page == nil {
-			return !lastPage
+	pages := evidently.NewListProjectsPaginator(conn, input)
+
+	for pages.HasMorePages() {
+		page, err := pages.NextPage(ctx)
+		if awsv2.SkipSweepError(err) {
+			log.Printf("[WARN] Skipping Evidently Project sweep for %s: %s", region, err)
+			return nil
 		}
 
 		for _, project := range page.Projects {
 			r := ResourceProject()
 			d := r.Data(nil)
-			d.SetId(aws.StringValue(project.Name))
+			d.SetId(aws.ToString(project.Name))
 
 			sweepResources = append(sweepResources, sweep.NewSweepResource(r, d, client))
 		}
-
-		return !lastPage
-	})
-
-	if awsv1.SkipSweepError(err) {
-		log.Printf("[WARN] Skipping Evidently Project sweep for %s: %s", region, err)
-		return sweeperErrs.ErrorOrNil()
-	}
-
-	if err != nil {
-		sweeperErrs = multierror.Append(sweeperErrs, fmt.Errorf("error listing Evidently Projects for %s: %w", region, err))
 	}
 
 	if err := sweep.SweepOrchestrator(ctx, sweepResources); err != nil {
