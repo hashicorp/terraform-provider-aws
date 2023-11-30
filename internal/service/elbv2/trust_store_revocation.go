a1a569d1a1c7d48b32cd79299725268f516da9cb
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-11-30 16:57:24 -0500

    r/aws_lb_trust_store_revocation: Tidy up.

--- internal/service/elbv2/trust_store_revocation.go
+++ internal/service/elbv2/trust_store_revocation.go
@@ -5,10 +5,8 @@ package elbv2
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"strconv"
-	"strings"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -19,33 +17,32 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
+	"github.com/hashicorp/terraform-provider-aws/internal/flex"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 // @SDKResource("aws_lb_trust_store_revocation", name="Trust Store Revocation")
-// @SDKResource("aws_alb_trust_store_revocation", name="Trust Store Revocation")
 func ResourceTrustStoreRevocation() *schema.Resource {
 	return &schema.Resource{
 		CreateWithoutTimeout: resourceTrustStoreRevocationCreate,
 		ReadWithoutTimeout:   resourceTrustStoreRevocationRead,
-		UpdateWithoutTimeout: nil,
 		DeleteWithoutTimeout: resourceTrustStoreRevocationDelete,
 
 		Importer: &schema.ResourceImporter{
-			StateContext: resourceTrustStoreRevocationImport,
+			StateContext: schema.ImportStatePassthroughContext,
 		},
+
 		Timeouts: &schema.ResourceTimeout{
-			Read: schema.DefaultTimeout(10 * time.Minute),
+			Create: schema.DefaultTimeout(2 * time.Minute),
 		},
 
 		Schema: map[string]*schema.Schema{
-			"trust_store_arn": {
-				Type:         schema.TypeString,
-				Required:     true,
-				ForceNew:     true,
-				ValidateFunc: verify.ValidARN,
+			"revocation_id": {
+				Type:     schema.TypeInt,
+				Computed: true,
 			},
 			"revocations_s3_bucket": {
 				Type:         schema.TypeString,
@@ -65,60 +62,52 @@ func ResourceTrustStoreRevocation() *schema.Resource {
 				ForceNew:     true,
 				ValidateFunc: validation.NoZeroValues,
 			},
-			"revocation_id": {
-				Type:     schema.TypeInt,
-				Computed: true,
+			"trust_store_arn": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: verify.ValidARN,
 			},
 		},
 	}
 }
 
+const (
+	trustStoreRevocationResourceIDPartCount = 2
+)
+
 func resourceTrustStoreRevocationCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
 	conn := meta.(*conns.AWSClient).ELBV2Conn(ctx)
 
-	trustStoreARN := d.Get("trust_store_arn").(string)
-
-	_, err := FindTrustStoreByARN(ctx, conn, trustStoreARN)
-
-	if err != nil {
-		return sdkdiag.AppendErrorf(diags, "creating ELBv2 Trust Store Revocations, Trust Store not found: %s: %s", trustStoreARN, err)
-	}
-
-	input := &elbv2.AddTrustStoreRevocationsInput{
-		TrustStoreArn:      aws.String(trustStoreARN),
-		RevocationContents: make([]*elbv2.RevocationContent, 1),
-	}
-
 	s3Bucket := d.Get("revocations_s3_bucket").(string)
 	s3Key := d.Get("revocations_s3_key").(string)
-
-	input.RevocationContents[0] = &elbv2.RevocationContent{
-		S3Bucket: aws.String(s3Bucket),
-		S3Key:    aws.String(s3Key),
+	trustStoreARN := d.Get("trust_store_arn").(string)
+	input := &elbv2.AddTrustStoreRevocationsInput{
+		RevocationContents: []*elbv2.RevocationContent{{
+			S3Bucket: aws.String(s3Bucket),
+			S3Key:    aws.String(s3Key),
+		}},
+		TrustStoreArn: aws.String(trustStoreARN),
 	}
 
-	if d.Get("revocations_s3_object_version").(string) != "" {
-		input.RevocationContents[0].S3ObjectVersion = aws.String(d.Get("revocations_s3_object_version").(string))
+	if v, ok := d.GetOk("revocations_s3_object_version"); ok {
+		input.RevocationContents[0].S3ObjectVersion = aws.String(v.(string))
 	}
 
 	output, err := conn.AddTrustStoreRevocationsWithContext(ctx, input)
 
 	if err != nil {
-		sdkdiag.AppendErrorf(diags, "creating ELBv2 Trust Store Revocations from %s s3://%s/%s: %s", trustStoreARN, s3Bucket, s3Key, err)
-	}
-
-	if len(output.TrustStoreRevocations) == 0 {
-		return sdkdiag.AppendErrorf(diags, "creatingTrust Store Revocations: no revocations returned in response")
+		sdkdiag.AppendErrorf(diags, "creating ELBv2 Trust Store (%s) Revocation (s3://%s/%s): %s", trustStoreARN, s3Bucket, s3Key, err)
 	}
 
 	revocationID := aws.Int64Value(output.TrustStoreRevocations[0].RevocationId)
-	d.SetId(TrustStoreRevocationCreateID(aws.StringValue(output.TrustStoreRevocations[0].TrustStoreArn), revocationID))
+	id := errs.Must(flex.FlattenResourceId([]string{trustStoreARN, strconv.FormatInt(revocationID, 10)}, trustStoreRevocationResourceIDPartCount, false))
 
-	d.Set("revocation_id", revocationID)
+	d.SetId(id)
 
-	_, err = tfresource.RetryWhenNotFound(ctx, propagationTimeout, func() (interface{}, error) {
-		return FindTrustStoreRevocation(ctx, conn, d.Id())
+	_, err = tfresource.RetryWhenNotFound(ctx, d.Timeout(schema.TimeoutCreate), func() (interface{}, error) {
+		return FindTrustStoreRevocationByTwoPartKey(ctx, conn, trustStoreARN, revocationID)
 	})
 
 	if err != nil {
@@ -132,9 +121,14 @@ func resourceTrustStoreRevocationRead(ctx context.Context, d *schema.ResourceDat
 	var diags diag.Diagnostics
 	conn := meta.(*conns.AWSClient).ELBV2Conn(ctx)
 
-	_, err := tfresource.RetryWhenNewResourceNotFound(ctx, propagationTimeout, func() (interface{}, error) {
-		return FindTrustStoreRevocation(ctx, conn, d.Id())
-	}, d.IsNewResource())
+	parts, err := flex.ExpandResourceId(d.Id(), trustStoreRevocationResourceIDPartCount, false)
+	if err != nil {
+		return sdkdiag.AppendFromErr(diags, err)
+	}
+
+	trustStoreARN := parts[0]
+	revocationID := errs.Must(strconv.ParseInt(parts[1], 10, 64))
+	revocation, err := FindTrustStoreRevocationByTwoPartKey(ctx, conn, trustStoreARN, revocationID)
 
 	if !d.IsNewResource() && tfresource.NotFound(err) {
 		log.Printf("[WARN] ELBv2 Trust Store Revocation %s not found, removing from state", d.Id())
@@ -145,6 +139,9 @@ func resourceTrustStoreRevocationRead(ctx context.Context, d *schema.ResourceDat
 	if err != nil {
 		return sdkdiag.AppendErrorf(diags, "reading ELBv2 Trust Store Revocation (%s): %s", d.Id(), err)
 	}
+
+	d.Set("revocation_id", revocation.RevocationId)
+	d.Set("trust_store_arn", revocation.TrustStoreArn)
 
 	return diags
 }
@@ -157,105 +154,69 @@ func resourceTrustStoreRevocationDelete(ctx context.Context, d *schema.ResourceD
 		trustStoreDeleteTimeout = 2 * time.Minute
 	)
 
-	parsed, err := parseTrustStoreRevocationID(d.Id())
-
+	parts, err := flex.ExpandResourceId(d.Id(), trustStoreRevocationResourceIDPartCount, false)
 	if err != nil {
-		log.Printf("[DEBUG] error parsing Trust Store Revocation Id: %s", d.Id())
-		return sdkdiag.AppendErrorf(diags, "deleting Trust Store: %s", err)
+		return sdkdiag.AppendFromErr(diags, err)
 	}
 
-	input := &elbv2.RemoveTrustStoreRevocationsInput{
-		TrustStoreArn: aws.String(parsed.TrustStoreARN),
-		RevocationIds: make([]*int64, 1),
-	}
-	input.RevocationIds[0] = aws.Int64(parsed.RevocationID)
+	trustStoreARN := parts[0]
+	revocationID := errs.Must(strconv.ParseInt(parts[1], 10, 64))
 
-	log.Printf("[DEBUG] Deleting Trust Store Revocation (%s): %s", d.Id(), input)
-	err = retry.RetryContext(ctx, trustStoreDeleteTimeout, func() *retry.RetryError {
-		_, err := conn.RemoveTrustStoreRevocationsWithContext(ctx, input)
-
-		if err != nil {
-			return retry.NonRetryableError(err)
-		}
-
-		return nil
+	log.Printf("[DEBUG] Deleting ELBv2 Trust Store Revocation: %s", d.Id())
+	_, err = conn.RemoveTrustStoreRevocationsWithContext(ctx, &elbv2.RemoveTrustStoreRevocationsInput{
+		RevocationIds: aws.Int64Slice([]int64{revocationID}),
+		TrustStoreArn: aws.String(trustStoreARN),
 	})
 
-	if tfresource.TimedOut(err) {
-		_, err = conn.RemoveTrustStoreRevocationsWithContext(ctx, input)
-	}
-
 	if err != nil {
-		return sdkdiag.AppendErrorf(diags, "deleting Trust Store Revocation: %s", err)
+		return sdkdiag.AppendErrorf(diags, "deleting ELBv2 Trust Store Revocation (%s): %s", d.Id(), err)
 	}
 
 	return diags
 }
 
-const trustStoreRevocationIDSeparator = "_"
-
-func parseTrustStoreRevocationID(id string) (*TrustStoreRevocationID, error) {
-	invalidIDError := func(msg string) error {
-		return fmt.Errorf("unexpected format for ID (%q), expected TRUSTSTOREARN_REVOCATIONID: %s", id, msg)
-	}
-
-	parts := strings.Split(id, trustStoreRevocationIDSeparator)
-
-	if len(parts) != 2 {
-		return nil, invalidIDError("id should have two parts")
-	}
-
-	revocationID, err := strconv.ParseInt(parts[1], 10, 64)
-
-	if err != nil {
-		return nil, invalidIDError("failed to parse revocationID")
-	}
-
-	result := &TrustStoreRevocationID{
-		TrustStoreARN: parts[0],
-		RevocationID:  revocationID,
-	}
-
-	return result, nil
-}
-
-func resourceTrustStoreRevocationImport(_ context.Context, d *schema.ResourceData, _ interface{}) ([]*schema.ResourceData, error) {
-	parsed, err := parseTrustStoreRevocationID(d.Id())
-	if err != nil {
-		return nil, err
-	}
-
-	d.Set("trust_store_arn", parsed.TrustStoreARN)
-	d.Set("revocation_id", parsed.RevocationID)
-
-	return []*schema.ResourceData{d}, nil
-}
-
-type TrustStoreRevocationID struct {
-	TrustStoreARN string
-	RevocationID  int64
-}
-
-func FindTrustStoreRevocation(ctx context.Context, conn *elbv2.ELBV2, id string) (*elbv2.DescribeTrustStoreRevocation, error) {
-	parsed, err := parseTrustStoreRevocationID(id)
-	if err != nil {
-		log.Printf("[DEBUG] error parsing Trust Store Revocation Id: %s", id)
-		return nil, err
-	}
-
+func FindTrustStoreRevocationByTwoPartKey(ctx context.Context, conn *elbv2.ELBV2, trustStoreARN string, revocationID int64) (*elbv2.DescribeTrustStoreRevocation, error) {
 	input := &elbv2.DescribeTrustStoreRevocationsInput{
-		TrustStoreArn: aws.String(parsed.TrustStoreARN),
+		RevocationIds: aws.Int64Slice([]int64{revocationID}),
+		TrustStoreArn: aws.String(trustStoreARN),
 	}
-	var matched []*elbv2.DescribeTrustStoreRevocation
+	output, err := findTrustStoreRevocation(ctx, conn, input)
 
-	err = conn.DescribeTrustStoreRevocationsPagesWithContext(ctx, input, func(page *elbv2.DescribeTrustStoreRevocationsOutput, lastPage bool) bool {
+	if err != nil {
+		return nil, err
+	}
+
+	// Eventual consistency check.
+	if aws.StringValue(output.TrustStoreArn) != trustStoreARN || aws.Int64Value(output.RevocationId) != revocationID {
+		return nil, &retry.NotFoundError{
+			LastRequest: input,
+		}
+	}
+
+	return output, nil
+}
+
+func findTrustStoreRevocation(ctx context.Context, conn *elbv2.ELBV2, input *elbv2.DescribeTrustStoreRevocationsInput) (*elbv2.DescribeTrustStoreRevocation, error) {
+	output, err := findTrustStoreRevocations(ctx, conn, input)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return tfresource.AssertSinglePtrResult(output)
+}
+
+func findTrustStoreRevocations(ctx context.Context, conn *elbv2.ELBV2, input *elbv2.DescribeTrustStoreRevocationsInput) ([]*elbv2.DescribeTrustStoreRevocation, error) {
+	var output []*elbv2.DescribeTrustStoreRevocation
+
+	err := conn.DescribeTrustStoreRevocationsPagesWithContext(ctx, input, func(page *elbv2.DescribeTrustStoreRevocationsOutput, lastPage bool) bool {
 		if page == nil {
 			return !lastPage
 		}
 
 		for _, v := range page.TrustStoreRevocations {
-			if v != nil && aws.Int64Value(v.RevocationId) == parsed.RevocationID {
-				matched = append(matched, v)
+			if v != nil {
+				output = append(output, v)
 			}
 		}
 
@@ -273,24 +234,5 @@ func FindTrustStoreRevocation(ctx context.Context, conn *elbv2.ELBV2, id string)
 		return nil, err
 	}
 
-	count := len(matched)
-
-	if count == 0 {
-		return nil, nil
-	}
-
-	if count > 1 {
-		return nil, tfresource.NewTooManyResultsError(count, input)
-	}
-
-	return matched[0], nil
-}
-
-func TrustStoreRevocationCreateID(trustStoreARN string, revocationID int64) string {
-	return fmt.Sprintf(
-		"%s%s%d",
-		trustStoreARN,
-		trustStoreRevocationIDSeparator,
-		revocationID,
-	)
+	return output, nil
 }
--- website/docs/r/lb_trust_store_revocation.html.markdown
+++ website/docs/r/lb_trust_store_revocation.html.markdown
@@ -1,7 +1,7 @@
 ---
 subcategory: "ELB (Elastic Load Balancing)"
 layout: "aws"
-page_title: "AWS: aws_alb_trust_store_revocation"
+page_title: "AWS: aws_lb_trust_store_revocation"
 description: |-
   Provides a Trust Store Revocation resource for use with Load Balancers.
 ---
@@ -9,8 +9,6 @@ description: |-
 # Resource: aws_lb_trust_store_revocation
 
 Provides a ELBv2 Trust Store Revocation for use with Application Load Balancer Listener resources.
-
-~> **Note:** `aws_alb_trust_store_revocation` is known as `aws_lb_trust_store_revocation`. The functionality is identical.
 
 ## Example Usage
 
@@ -49,21 +47,21 @@ This resource supports the following arguments:
 This resource exports the following attributes in addition to the arguments above:
 
 * `revocation_id` - AWS assigned RevocationId, (number).
-* `id` - "combination of the Trust Store ARN and RevocationId `${trust_store_arn}_{revocation_id}`"
+* `id` - "combination of the Trust Store ARN and RevocationId `${trust_store_arn},{revocation_id}`"
 
 ## Import
 
-In Terraform v1.5.0 and later, use an [`import` block](https://developer.hashicorp.com/terraform/language/import) to import Trust Stores using their ARN. For example:
+In Terraform v1.5.0 and later, use an [`import` block](https://developer.hashicorp.com/terraform/language/import) to import Trust Store Revocations using their ARN. For example:
 
 ```terraform
 import {
   to = aws_lb_trust_store_revocation.example
-  id = "arn:aws:elasticloadbalancing:us-west-2:187416307283:truststore/my-trust-store/20cfe21448b66314_6"
+  id = "arn:aws:elasticloadbalancing:us-west-2:187416307283:truststore/my-trust-store/20cfe21448b66314,6"
 }
 ```
 
-Using `terraform import`, import Target Groups using their ARN. For example:
+Using `terraform import`, import Trust Store Revocations using their ARN. For example:
 
 ```console
-% terraform import aws_lb_trust_store_revocation.example arn:aws:elasticloadbalancing:us-west-2:187416307283:truststore/my-trust-store/20cfe21448b66314_6
+% terraform import aws_lb_trust_store_revocation.example arn:aws:elasticloadbalancing:us-west-2:187416307283:truststore/my-trust-store/20cfe21448b66314,6
 ```
