7a1af7dc7c9d492aa2ada82d6a6512be827717eb
Bryant Biggs <bryantbiggs@gmail.com>
2023-12-06 08:19:46 -0500

    ecs: Generate AWS SDK for Go v2 tagging code

--- internal/service/ecs/generate.go
+++ internal/service/ecs/generate.go
@@ -3,7 +3,7 @@
 
 //go:generate go run ../../generate/listpages/main.go -ListOps=DescribeCapacityProviders
 //go:generate go run ../../generate/tagresource/main.go
-//go:generate go run ../../generate/tags/main.go -GetTag -ListTags -ServiceTagsSlice -UpdateTags -CreateTags -ParentNotFoundErrCode=InvalidParameterException "-ParentNotFoundErrMsg=The specified cluster is inactive. Specify an active cluster and try again."
+//go:generate go run ../../generate/tags/main.go -AWSSDKVersion=2 -GetTag -ListTags -UpdateTags -CreateTags -ServiceTagsMap -KVTValues -SkipTypesImp -ParentNotFoundErrCode=InvalidParameterException "-ParentNotFoundErrMsg=The specified cluster is inactive. Specify an active cluster and try again."
 //go:generate go run ../../generate/servicepackage/main.go
 // ONLY generate directives and package declaration! Do not add anything else to this file.
 
