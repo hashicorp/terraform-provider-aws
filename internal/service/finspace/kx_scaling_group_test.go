b1db62e1f396f61a056e39583a0eeb6f113f42b0
Jared Baker <jared.baker@hashicorp.com>
2023-12-14 09:25:10 -0500

    r/aws_finspace_kx_scaling_group(test): add _tags

--- internal/service/finspace/kx_scaling_group_test.go
+++ internal/service/finspace/kx_scaling_group_test.go
@@ -88,6 +88,62 @@ func TestAccFinSpaceKxScalingGroup_disappears(t *testing.T) {
 	})
 }
 
+func TestAccFinSpaceKxScalingGroup_tags(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping long-running test in short mode")
+	}
+
+	ctx := acctest.Context(t)
+	var scalingGroup finspace.GetKxScalingGroupOutput
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	resourceName := "aws_finspace_kx_scaling_group.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+			acctest.PreCheckPartitionHasService(t, finspace.ServiceID)
+		},
+		ErrorCheck:               acctest.ErrorCheck(t, finspace.ServiceID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckKxScalingGroupDestroy(ctx),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccKxScalingGroupConfig_tags1(rName, "key1", "value1"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckKxScalingGroupExists(ctx, resourceName, &scalingGroup),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", "1"),
+					resource.TestCheckResourceAttr(resourceName, "tags.key1", "value1"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccKxScalingGroupConfig_tags2(rName, "key1", "value1updated", "key2", "value2"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckKxScalingGroupExists(ctx, resourceName, &scalingGroup),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", "2"),
+					resource.TestCheckResourceAttr(resourceName, "tags.key1", "value1updated"),
+					resource.TestCheckResourceAttr(resourceName, "tags.key2", "value2"),
+				),
+			},
+			{
+				Config: testAccKxScalingGroupConfig_tags1(rName, "key2", "value2"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckKxScalingGroupExists(ctx, resourceName, &scalingGroup),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", "1"),
+					resource.TestCheckResourceAttr(resourceName, "tags.key2", "value2"),
+				),
+			},
+		},
+	})
+}
+
 func testAccCheckKxScalingGroupDestroy(ctx context.Context) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		conn := acctest.Provider.Meta().(*conns.AWSClient).FinSpaceClient(ctx)
@@ -262,4 +318,39 @@ resource "aws_finspace_kx_scaling_group" "test" {
   host_type            = "kx.sg.4xlarge"
 }
 `, rName))
+}
+
+func testAccKxScalingGroupConfig_tags1(rName, key1, value1 string) string {
+	return acctest.ConfigCompose(
+		testAccKxScalingGroupConfigBase(rName),
+		fmt.Sprintf(`
+resource "aws_finspace_kx_scaling_group" "test" {
+  name                 = %[1]q
+  environment_id       = aws_finspace_kx_environment.test.id
+  availability_zone_id = aws_finspace_kx_environment.test.availability_zones[0]
+  host_type            = "kx.sg.4xlarge"
+
+  tags = {
+    %[2]q = %[3]q
+  }
+}
+`, rName, key1, value1))
+}
+
+func testAccKxScalingGroupConfig_tags2(rName, key1, value1, key2, value2 string) string {
+	return acctest.ConfigCompose(
+		testAccKxScalingGroupConfigBase(rName),
+		fmt.Sprintf(`
+resource "aws_finspace_kx_scaling_group" "test" {
+  name                 = %[1]q
+  environment_id       = aws_finspace_kx_environment.test.id
+  availability_zone_id = aws_finspace_kx_environment.test.availability_zones[0]
+  host_type            = "kx.sg.4xlarge"
+
+  tags = {
+    %[2]q = %[3]q
+    %[4]q = %[5]q
+  }
+}
+`, rName, key1, value1, key2, value2))
 }
