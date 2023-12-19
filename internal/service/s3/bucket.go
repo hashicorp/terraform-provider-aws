f39bca27b5d92086f9e55461a4e90c2e1e61bb7c
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-12-19 08:22:19 -0500

    Update bucket.go

--- internal/service/s3/bucket.go
+++ internal/service/s3/bucket.go
@@ -31,7 +31,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/structure"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/create"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
