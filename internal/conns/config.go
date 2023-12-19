79dcd6f7c489c6f90f48f27ce107781c3590c632
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-12-19 15:20:25 -0500

    'conns.ReverseDNS' -> 'names.ReverseDNS'.

--- internal/conns/config.go
+++ internal/conns/config.go
@@ -199,7 +199,7 @@ func (c *Config) ConfigureProvider(ctx context.Context, client *AWSClient) (*AWS
 	client.IgnoreTagsConfig = c.IgnoreTagsConfig
 	client.Partition = partition
 	client.Region = c.Region
-	client.ReverseDNSPrefix = ReverseDNS(DNSSuffix)
+	client.ReverseDNSPrefix = names.ReverseDNS(DNSSuffix)
 	client.SetHTTPClient(sess.Config.HTTPClient) // Must be called while client.Session is nil.
 	client.Session = sess
 	client.TerraformVersion = c.TerraformVersion
--- names/names.go
+++ names/names.go
@@ -189,6 +189,17 @@ func PartitionForRegion(region string) string {
 	}
 }
 
+// ReverseDNS switches a DNS hostname to reverse DNS and vice-versa.
+func ReverseDNS(hostname string) string {
+	parts := strings.Split(hostname, ".")
+
+	for i, j := 0, len(parts)-1; i < j; i, j = i+1, j-1 {
+		parts[i], parts[j] = parts[j], parts[i]
+	}
+
+	return strings.Join(parts, ".")
+}
+
 // Type ServiceDatum corresponds closely to columns in `names_data.csv` and are
 // described in detail in README.md.
 type ServiceDatum struct {
