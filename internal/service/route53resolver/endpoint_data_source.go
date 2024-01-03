0ec3dbca223f5179047a1bdbdddaa8043f2e8229
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-01-03 08:27:53 -0500

    d/aws_route53_resolver_endpoint: Add `protocols` attribute.

--- internal/service/route53resolver/endpoint_data_source.go
+++ internal/service/route53resolver/endpoint_data_source.go
@@ -54,6 +54,11 @@ func DataSourceEndpoint() *schema.Resource {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+			"protocols": {
+				Type:     schema.TypeSet,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Computed: true,
+			},
 			"resolver_endpoint_id": {
 				Type:     schema.TypeString,
 				Optional: true,
@@ -115,6 +120,7 @@ func dataSourceEndpointRead(ctx context.Context, d *schema.ResourceData, meta in
 	d.Set("arn", ep.Arn)
 	d.Set("direction", ep.Direction)
 	d.Set("name", ep.Name)
+	d.Set("protocols", aws.StringValueSlice(ep.Protocols))
 	d.Set("resolver_endpoint_id", ep.Id)
 	d.Set("status", ep.Status)
 	d.Set("vpc_id", ep.HostVPCId)
