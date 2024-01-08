d4e252b05913bcc390865199026a0e1aa31d06e2
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-01-08 08:23:46 -0500

    d/aws_mq_broker: Migrate to AWS SDK for Go v2.

--- internal/service/mq/service_package_gen.go
+++ internal/service/mq/service_package_gen.go
@@ -25,8 +25,9 @@ func (p *servicePackage) FrameworkResources(ctx context.Context) []*types.Servic
 func (p *servicePackage) SDKDataSources(ctx context.Context) []*types.ServicePackageSDKDataSource {
 	return []*types.ServicePackageSDKDataSource{
 		{
-			Factory:  DataSourceBroker,
+			Factory:  dataSourceBroker,
 			TypeName: "aws_mq_broker",
+			Name:     "Broker",
 		},
 		{
 			Factory:  dataSourceBrokerEngineTypes,
