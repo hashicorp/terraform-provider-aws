ff35d82b03a4558fc7b408354c1842a86a58c49e
Mikael Suvi <mikael.suvi@gmail.com>
2024-01-21 00:01:47 +0000

    Fix duplicate

--- internal/service/qbusiness/retriever.go
+++ internal/service/qbusiness/retriever.go
@@ -118,7 +118,6 @@ func ResourceRetriever() *schema.Resource {
 			names.AttrTags:    tftags.TagsSchema(),
 			names.AttrTagsAll: tftags.TagsSchemaComputed(),
 		},
-		CustomizeDiff: verify.SetTagsDiff,
 	}
 }
 
