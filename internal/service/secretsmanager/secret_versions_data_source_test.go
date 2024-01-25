4de08347c322f88b65dc91daf0792b5516b08f92
Wilson de Carvalho <796900+wcmjunior@users.noreply.github.com>
2024-01-25 11:14:37 -0800

    Add test for remaining field

--- internal/service/secretsmanager/secret_versions_data_source_test.go
+++ internal/service/secretsmanager/secret_versions_data_source_test.go
@@ -5,6 +5,7 @@ package secretsmanager_test
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 
 	"github.com/YakDriver/regexache"
@@ -34,20 +35,41 @@ func TestAccSecretsManagerSecretVersionsDataSource_basic(t *testing.T) {
 			{
 				Config: testAccSecretVersionsDataSourceConfig_oneVersion(rName),
 				Check: resource.ComposeTestCheckFunc(
-					testAccSecretVersionsCheckDataSource(datasourceName, resource1Name, ""),
+					testAccSecretVersionsCheckDataSource(datasourceName, resource1Name, "", 1),
 				),
 			},
 			{
 				Config: testAccSecretVersionsDataSourceConfig_twoVersions(rName),
 				Check: resource.ComposeTestCheckFunc(
-					testAccSecretVersionsCheckDataSource(datasourceName, resource1Name, resource2Name),
+					testAccSecretVersionsCheckDataSource(datasourceName, resource1Name, resource2Name, 2),
 				),
 			},
 		},
 	})
 }
 
-func testAccSecretVersionsCheckDataSource(datasourceName, resource1Name, resource2Name string) resource.TestCheckFunc {
+func TestAccSecretsManagerSecretVersionsDataSource_maxResults(t *testing.T) {
+	ctx := acctest.Context(t)
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	resource1Name := "aws_secretsmanager_secret_version.test1"
+	datasourceName := "data.aws_secretsmanager_secret_versions.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(ctx, t); testAccPreCheck(ctx, t) },
+		ErrorCheck:               acctest.ErrorCheck(t, names.SecretsManagerEndpointID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSecretVersionsDataSourceConfig_maxResults(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccSecretVersionsCheckDataSource(datasourceName, resource1Name, "", 1),
+				),
+			},
+		},
+	})
+}
+
+func testAccSecretVersionsCheckDataSource(datasourceName, resource1Name, resource2Name string, versionCount int) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		dataSource, ok := s.RootModule().Resources[datasourceName]
 		if !ok {
@@ -62,6 +84,14 @@ func testAccSecretVersionsCheckDataSource(datasourceName, resource1Name, resourc
 		resource2, ok := s.RootModule().Resources[resource2Name]
 		if resource2Name != "" && !ok {
 			return fmt.Errorf("root module has no resource called %s", resource2Name)
+		}
+
+		if dataSource.Primary.Attributes["versions.#"] != strconv.Itoa(versionCount) {
+			return fmt.Errorf(
+				"versions.# is %s; want %d",
+				dataSource.Primary.Attributes["versions.#"],
+				versionCount,
+			)
 		}
 
 		checkAttributes := func(attrNames []string, resourceAttributes map[string]string) error {
@@ -103,12 +133,6 @@ func testAccSecretVersionsCheckDataSource(datasourceName, resource1Name, resourc
 			err = checkAttributes(attrNamesResource2, resource2.Primary.Attributes)
 			if err != nil {
 				return err
-			}
-			if dataSource.Primary.Attributes["versions.#"] != "2" {
-				return fmt.Errorf(
-					"versions.# is %s; want 2",
-					dataSource.Primary.Attributes["versions.#"],
-				)
 			}
 		}
 
@@ -160,6 +184,31 @@ resource "aws_secretsmanager_secret_version" "test2" {
 data "aws_secretsmanager_secret_versions" "test" {
   depends_on = [aws_secretsmanager_secret_version.test1, aws_secretsmanager_secret_version.test2]
   secret_id = aws_secretsmanager_secret.test.id
+}
+`, rName)
+}
+
+func testAccSecretVersionsDataSourceConfig_maxResults(rName string) string {
+	return fmt.Sprintf(`
+resource "aws_secretsmanager_secret" "test" {
+  name = "%[1]s"
+}
+
+resource "aws_secretsmanager_secret_version" "test1" {
+  secret_id     = aws_secretsmanager_secret.test.id
+  secret_string = "test-string"
+}
+
+resource "aws_secretsmanager_secret_version" "test2" {
+  depends_on = [aws_secretsmanager_secret_version.test1]
+  secret_id     = aws_secretsmanager_secret.test.id
+  secret_string = "test-string2"
+}
+
+data "aws_secretsmanager_secret_versions" "test" {
+  depends_on = [aws_secretsmanager_secret_version.test1, aws_secretsmanager_secret_version.test2]
+  secret_id = aws_secretsmanager_secret.test.id
+  max_results = 1
 }
 `, rName)
 }
