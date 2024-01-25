9ea78ed0013f67f69be2e8181ec139376cf5bce5
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-01-25 12:39:54 -0500

    Add 'base.WithNoOpUpdate'.

--- internal/framework/base.go
+++ internal/framework/base.go
@@ -107,6 +107,12 @@ func (w *WithImportByID) ImportState(ctx context.Context, request resource.Impor
 	resource.ImportStatePassthroughID(ctx, path.Root(names.AttrID), request, response)
 }
 
+// WithImportByID is intended to be embedded in resources which have no need of an Update method.
+type WithNoOpUpdate struct{}
+
+func (w *WithNoOpUpdate) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+}
+
 // DataSourceWithConfigure is a structure to be embedded within a DataSource that implements the DataSourceWithConfigure interface.
 type DataSourceWithConfigure struct {
 	withMeta
