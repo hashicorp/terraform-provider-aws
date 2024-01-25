bcc59ab2528f89244a5a8b0fab960848694086d8
teddylear <20077627+teddylear@users.noreply.github.com>
2024-01-24 22:26:58 -0500

    add comment about failing test case

--- internal/service/iam/role_test.go
+++ internal/service/iam/role_test.go
@@ -707,8 +707,7 @@ func TestAccIAMRole_InlinePolicy_badJSON(t *testing.T) {
 	})
 }
 
-// TODO: have to do some plan modification for this, have to read up on the docs more
-// Ideally only show diffs for each policy that actually changed
+// TODO: So close on this, for some reason namePrefix is showing up when Plan is modified... very annoying
 
 // // Reference: https://github.com/hashicorp/terraform-provider-aws/issues/19444
 // func TestAccIAMRole_InlinePolicy_ignoreOrder(t *testing.T) {
