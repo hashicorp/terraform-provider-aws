654f50385462f97fafa56b23646cf9e79a732bea
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-01-27 17:55:10 -0500

    d/aws_codeartifact_authorization_token: Migrate to AWS SDK for Go v2.

--- internal/service/codeartifact/service_package_gen.go
+++ internal/service/codeartifact/service_package_gen.go
@@ -25,8 +25,9 @@ func (p *servicePackage) FrameworkResources(ctx context.Context) []*types.Servic
 func (p *servicePackage) SDKDataSources(ctx context.Context) []*types.ServicePackageSDKDataSource {
 	return []*types.ServicePackageSDKDataSource{
 		{
-			Factory:  DataSourceAuthorizationToken,
+			Factory:  dataSourceAuthorizationToken,
 			TypeName: "aws_codeartifact_authorization_token",
+			Name:     "Authoiration Token",
 		},
 		{
 			Factory:  dataSourceRepositoryEndpoint,
