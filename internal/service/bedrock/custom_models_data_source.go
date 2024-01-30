5b67d649fb612a945510a297976d1d60e8f8a18b
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-01-30 09:21:02 -0500

    r/aws_bedrock_custom_models: Use 'fwtypes.Timestamp' for 'creation_time'.

--- internal/service/bedrock/custom_models_data_source.go
+++ internal/service/bedrock/custom_models_data_source.go
@@ -41,7 +41,7 @@ func (d *customModelsDataSource) Schema(ctx context.Context, request datasource.
 					AttrTypes: map[string]attr.Type{
 						"base_model_arn":  types.StringType,
 						"base_model_name": types.StringType,
-						"creation_time":   types.StringType,
+						"creation_time":   fwtypes.TimestampType,
 						"model_arn":       types.StringType,
 						"model_name":      types.StringType,
 					},
@@ -90,9 +90,9 @@ type customModelsDataSourceModel struct {
 }
 
 type customModelSummaryModel struct {
-	BaseModelARN  types.String `tfsdk:"base_model_arn"`
-	BaseModelName types.String `tfsdk:"base_model_name"`
-	CreationTime  types.String `tfsdk:"creation_time"`
-	ModelARN      types.String `tfsdk:"model_arn"`
-	ModelName     types.String `tfsdk:"model_name"`
+	BaseModelARN  types.String      `tfsdk:"base_model_arn"`
+	BaseModelName types.String      `tfsdk:"base_model_name"`
+	CreationTime  fwtypes.Timestamp `tfsdk:"creation_time"`
+	ModelARN      types.String      `tfsdk:"model_arn"`
+	ModelName     types.String      `tfsdk:"model_name"`
 }
