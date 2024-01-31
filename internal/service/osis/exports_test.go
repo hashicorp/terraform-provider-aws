2450db3951c7fb8441605bc2734b068133ac1e16
Farhan Angullia <farhanangullia@gmail.com>
2024-02-01 01:34:37 +0800

    Add osis exports

---
+++ internal/service/osis/exports_test.go
@@ -0,0 +1,10 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package osis
+
+// Exports for use in tests only.
+var (
+	ResourcePipeline = newResourcePipeline
+	FindPipelineByID = findPipelineByID
+)
