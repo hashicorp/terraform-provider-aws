198d9f705e3171ccb72cabf417c6d291926f62b0
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-02-01 14:57:51 -0500

    Run 'make gen'.

--- internal/service/cloud9/service_package_gen.go
+++ internal/service/cloud9/service_package_gen.go
@@ -5,9 +5,8 @@ package cloud9
 import (
 	"context"
 
-	aws_sdkv1 "github.com/aws/aws-sdk-go/aws"
-	session_sdkv1 "github.com/aws/aws-sdk-go/aws/session"
-	cloud9_sdkv1 "github.com/aws/aws-sdk-go/service/cloud9"
+	aws_sdkv2 "github.com/aws/aws-sdk-go-v2/aws"
+	cloud9_sdkv2 "github.com/aws/aws-sdk-go-v2/service/cloud9"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/types"
 	"github.com/hashicorp/terraform-provider-aws/names"
@@ -48,11 +47,15 @@ func (p *servicePackage) ServicePackageName() string {
 	return names.Cloud9
 }
 
-// NewConn returns a new AWS SDK for Go v1 client for this service package's AWS API.
-func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*cloud9_sdkv1.Cloud9, error) {
-	sess := config["session"].(*session_sdkv1.Session)
+// NewClient returns a new AWS SDK for Go v2 client for this service package's AWS API.
+func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (*cloud9_sdkv2.Client, error) {
+	cfg := *(config["aws_sdkv2_config"].(*aws_sdkv2.Config))
 
-	return cloud9_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config["endpoint"].(string))})), nil
+	return cloud9_sdkv2.NewFromConfig(cfg, func(o *cloud9_sdkv2.Options) {
+		if endpoint := config["endpoint"].(string); endpoint != "" {
+			o.BaseEndpoint = aws_sdkv2.String(endpoint)
+		}
+	}), nil
 }
 
 func ServicePackage(ctx context.Context) conns.ServicePackage {
