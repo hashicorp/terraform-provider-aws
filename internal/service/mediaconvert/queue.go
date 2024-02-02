2b30ad66b0800ce0bccf068c15c96138391b30d0
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-02-02 08:35:07 -0500

    r/aws_media_convert_queue: Tidy up Read.

--- internal/service/mediaconvert/queue.go
+++ internal/service/mediaconvert/queue.go
@@ -13,11 +13,13 @@ import (
 	"github.com/aws/aws-sdk-go/service/mediaconvert"
 	"github.com/hashicorp/aws-sdk-go-base/v2/awsv1shim/v2/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
+	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
@@ -135,31 +137,28 @@ func resourceQueueRead(ctx context.Context, d *schema.ResourceData, meta interfa
 		return sdkdiag.AppendErrorf(diags, "getting Media Convert Account Client: %s", err)
 	}
 
-	getOpts := &mediaconvert.GetQueueInput{
-		Name: aws.String(d.Id()),
-	}
+	queue, err := FindQueueByName(ctx, conn, d.Id())
 
-	resp, err := conn.GetQueueWithContext(ctx, getOpts)
-	if !d.IsNewResource() && tfawserr.ErrCodeEquals(err, mediaconvert.ErrCodeNotFoundException) {
+	if !d.IsNewResource() && tfresource.NotFound(err) {
 		log.Printf("[WARN] Media Convert Queue (%s) not found, removing from state", d.Id())
 		d.SetId("")
 		return diags
 	}
+
 	if err != nil {
-		return sdkdiag.AppendErrorf(diags, "getting Media Convert Queue: %s", err)
+		return sdkdiag.AppendErrorf(diags, "reading Media Convert Queue (%s): %s", d.Id(), err)
 	}
 
-	d.Set("arn", resp.Queue.Arn)
-	d.Set("name", resp.Queue.Name)
-	d.Set("description", resp.Queue.Description)
-	d.Set("pricing_plan", resp.Queue.PricingPlan)
-	d.Set("status", resp.Queue.Status)
-
-	if err := d.Set("reservation_plan_settings", flattenReservationPlan(resp.Queue.ReservationPlan)); err != nil {
-		return sdkdiag.AppendErrorf(diags, "setting Media Convert Queue reservation_plan_settings: %s", err)
+	d.Set("arn", queue.Arn)
+	d.Set("description", queue.Description)
+	d.Set("name", queue.Name)
+	d.Set("pricing_plan", queue.PricingPlan)
+	if err := d.Set("reservation_plan_settings", flattenReservationPlan(queue.ReservationPlan)); err != nil {
+		return sdkdiag.AppendErrorf(diags, "setting reservation_plan_settings: %s", err)
 	}
+	d.Set("status", queue.Status)
 
-	tags, err := listTags(ctx, conn, aws.StringValue(resp.Queue.Arn))
+	tags, err := listTags(ctx, conn, aws.StringValue(queue.Arn))
 
 	if err != nil {
 		return sdkdiag.AppendErrorf(diags, "listing tags for Media Convert Queue (%s): %s", d.Id(), err)
@@ -229,6 +228,31 @@ func resourceQueueDelete(ctx context.Context, d *schema.ResourceData, meta inter
 	}
 
 	return diags
+}
+
+func FindQueueByName(ctx context.Context, conn *mediaconvert.MediaConvert, name string) (*mediaconvert.Queue, error) {
+	input := &mediaconvert.GetQueueInput{
+		Name: aws.String(name),
+	}
+
+	output, err := conn.GetQueueWithContext(ctx, input)
+
+	if tfawserr.ErrCodeEquals(err, mediaconvert.ErrCodeNotFoundException) {
+		return nil, &retry.NotFoundError{
+			LastError:   err,
+			LastRequest: input,
+		}
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	if output == nil || output.Queue == nil {
+		return nil, tfresource.NewEmptyResultError(input)
+	}
+
+	return output.Queue, nil
 }
 
 func GetAccountClient(ctx context.Context, awsClient *conns.AWSClient) (*mediaconvert.MediaConvert, error) {
