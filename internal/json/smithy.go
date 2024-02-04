f1bee74445a42a16696303134ced06e2cddbe5c0
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-02-04 10:39:56 -0500

    Add 'json.SmithyDocumentFromString'.

--- internal/json/smithy.go
+++ internal/json/smithy.go
@@ -9,6 +9,18 @@ import (
 	smithydocument "github.com/aws/smithy-go/document"
 )
 
+func SmithyDocumentFromString[T smithydocument.Marshaler](s string, f func(any) T) (T, error) {
+	var v map[string]interface{}
+
+	err := json.Unmarshal([]byte(s), &v)
+	if err != nil {
+		var zero T
+		return zero, err
+	}
+
+	return f(v), nil
+}
+
 // SmithyDocumentToString converts a [Smithy document](https://smithy.io/2.0/spec/simple-types.html#document) to a JSON string.
 func SmithyDocumentToString(document smithydocument.Unmarshaler) (string, error) {
 	var v map[string]interface{}
