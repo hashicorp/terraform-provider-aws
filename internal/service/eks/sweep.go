7921e95ced3a0bce1231e5624c19f7f14e27635d
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-02-05 11:53:29 -0500

    r/aws_eks_addon: Prevent sweeper infinite loops.

--- internal/service/eks/sweep.go
+++ internal/service/eks/sweep.go
@@ -86,13 +86,13 @@ func sweepAddons(region string) error {
 				page, err := pages.NextPage(ctx)
 
 				if awsv2.SkipSweepError(err) {
-					continue
+					break
 				}
 
 				// There are EKS clusters that are listed (and are in the AWS Console) but can't be found.
 				// ¯\_(ツ)_/¯
 				if errs.IsA[*types.ResourceNotFoundException](err) {
-					continue
+					break
 				}
 
 				if err != nil {
