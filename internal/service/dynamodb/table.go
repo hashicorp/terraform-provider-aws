f97b51ee8ffb7bc8e7344dfdfccf2c1647faaae1
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-02-06 16:36:22 -0500

    Fix importlint error.

--- internal/service/dynamodb/table.go
+++ internal/service/dynamodb/table.go
@@ -22,7 +22,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/create"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
