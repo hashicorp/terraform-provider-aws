644116022aad9b8740046d129b69cbbc97da21f4
Justin Popa <481655+justinpopa@users.noreply.github.com>
2024-02-06 16:45:41 -0500

    Added validator for name, descriptions for schema attributes

--- internal/service/cloudfront/key_value_store.go
+++ internal/service/cloudfront/key_value_store.go
@@ -7,16 +7,19 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"regexp"
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/cloudfront"
 	awstypes "github.com/aws/aws-sdk-go-v2/service/cloudfront/types"
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-provider-aws/internal/create"
@@ -56,7 +59,8 @@ func (r *resourceKeyValueStore) Schema(ctx context.Context, req resource.SchemaR
 		Attributes: map[string]schema.Attribute{
 			"arn": framework.ARNAttributeComputedOnly(),
 			"comment": schema.StringAttribute{
-				Optional: true,
+				Optional:    true,
+				Description: "A comment to describe the key value store",
 			},
 			"id": framework.IDAttribute(),
 			"name": schema.StringAttribute{
@@ -64,12 +68,22 @@ func (r *resourceKeyValueStore) Schema(ctx context.Context, req resource.SchemaR
 				PlanModifiers: []planmodifier.String{
 					stringplanmodifier.RequiresReplace(),
 				},
+				Validators: []validator.String{
+					stringvalidator.LengthBetween(1, 64),
+					stringvalidator.RegexMatches(
+						regexp.MustCompile(`^[a-zA-Z0-9-_]{1,64}$`),
+						"must contain only alphanumeric characters, hyphens, and underscores",
+					),
+				},
+				Description: "The name of the key value store",
 			},
 			"last_modified_time": schema.StringAttribute{
-				Computed: true,
+				Computed:    true,
+				Description: "The date and time the key value store was last modified",
 			},
 			"etag": schema.StringAttribute{
-				Computed: true,
+				Computed:    true,
+				Description: "The current version of the key value store",
 			},
 		},
 	}
