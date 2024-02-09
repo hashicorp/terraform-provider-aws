04fc08cb31f390941ca0e58a6a17e16de18e5802
Ali Allomani <17993914+AliAllomani@users.noreply.github.com>
2024-02-09 17:39:27 +0100

    New resource: aws_account_region

--- internal/service/account/region.go
+++ internal/service/account/region.go
@@ -5,11 +5,156 @@ package account
 
 import (
 	"context"
+	"log"
+	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/account"
+	accounttypes "github.com/aws/aws-sdk-go-v2/service/account/types"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
+
+// @SDKResource("aws_account_region")
+func resourceRegion() *schema.Resource {
+	return &schema.Resource{
+		CreateWithoutTimeout: resourceRegionUpdate,
+		ReadWithoutTimeout:   resourceRegionRead,
+		UpdateWithoutTimeout: resourceRegionUpdate,
+		DeleteWithoutTimeout: schema.NoopContext,
+
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"account_id": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ForceNew:     true,
+				ValidateFunc: verify.ValidAccountID,
+			},
+			"region": {
+				Type:     schema.TypeString,
+				ForceNew: true,
+			},
+			"enabled": {
+				Type:     schema.TypeBool,
+				Default:  true,
+				Optional: true,
+			},
+			"opt_in_status": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceRegionUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	conn := meta.(*conns.AWSClient).AccountClient(ctx)
+
+	accountID := meta.(*conns.AWSClient).AccountID
+	if v, ok := d.GetOk("account_id"); ok {
+		accountID = v.(string)
+	}
+
+	region := d.Get("region").(string)
+
+	id := AccountRegionResourceID(accountID, region)
+
+	var err error
+
+	output, err := FindRegionOptInStatus(ctx, conn, accountID, region)
+
+	if err != nil {
+		return sdkdiag.AppendErrorf(diags, "reading Account Region (%s): %s", d.Id(), err)
+	}
+
+	if v := d.Get("enabled").(bool); v {
+		if output.RegionOptStatus != accounttypes.RegionOptStatusEnabledByDefault {
+			_, err := conn.EnableRegion(ctx, &account.EnableRegionInput{
+				RegionName: aws.String(region),
+				AccountId:  aws.String(accountID),
+			})
+
+			if err != nil {
+				return sdkdiag.AppendErrorf(diags, "Enabling account region (%s): %s", id, err)
+			}
+		}
+	} else {
+
+		if output.RegionOptStatus == accounttypes.RegionOptStatusEnabledByDefault {
+			return sdkdiag.AppendErrorf(diags, "cannot disable region (%s) that is enabled by default", id)
+		}
+
+		_, err = conn.DisableRegion(ctx, &account.DisableRegionInput{
+			RegionName: aws.String(region),
+			AccountId:  aws.String(accountID),
+		})
+
+		if err != nil {
+			return sdkdiag.AppendErrorf(diags, "Disabling account region (%s): %s", id, err)
+		}
+	}
+
+	if d.IsNewResource() {
+		d.SetId(id)
+	}
+
+	return append(diags, resourceRegionRead(ctx, d, meta)...)
+}
+
+func resourceRegionRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	conn := meta.(*conns.AWSClient).AccountClient(ctx)
+
+	accountID := meta.(*conns.AWSClient).AccountID
+	if v, ok := d.GetOk("account_id"); ok {
+		accountID = v.(string)
+	}
+
+	region := d.Get("region").(string)
+
+	output, err := FindRegionOptInStatus(ctx, conn, accountID, region)
+
+	if !d.IsNewResource() && tfresource.NotFound(err) {
+		log.Printf("[WARN] Account Region (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return diags
+	}
+
+	if err != nil {
+		return sdkdiag.AppendErrorf(diags, "reading Account Region (%s): %s", d.Id(), err)
+	}
+
+	if status := output.RegionOptStatus; status == accounttypes.RegionOptStatusEnabled || status == accounttypes.RegionOptStatusEnabling || status == accounttypes.RegionOptStatusEnabledByDefault {
+		d.Set("enabled", true)
+	} else {
+		d.Set("enabled", false)
+	}
+
+	d.Set("opt_in_status", string(output.RegionOptStatus))
+	d.Set("account_id", d.Get("account_id"))
+
+	return diags
+}
+
+const AccountRegionResourceIDSeparator = ","
+
+func AccountRegionResourceID(accountID, region string) string {
+	parts := []string{accountID, region}
+	id := strings.Join(parts, AccountRegionResourceIDSeparator)
+
+	return id
+}
 
 func FindRegionOptInStatus(ctx context.Context, conn *account.Client, accountID, region string) (*account.GetRegionOptStatusOutput, error) {
 	input := &account.GetRegionOptStatusInput{
---
+++ internal/service/account/region_test.go
@@ -0,0 +1,72 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package account_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	tfaccount "github.com/hashicorp/terraform-provider-aws/internal/service/account"
+	"github.com/hashicorp/terraform-provider-aws/names"
+)
+
+func testAccRegion_basic(t *testing.T) {
+	ctx := acctest.Context(t)
+	resourceName := "aws_account_region.test"
+	region := "ap-southeast-3"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(ctx, t); testAccPreCheck(ctx, t) },
+		ErrorCheck:               acctest.ErrorCheck(t, names.AccountEndpointID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             acctest.CheckDestroyNoop,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccRegionConfig_basic(region, "true"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccCheckPrimaryContactExists(ctx, resourceName),
+					resource.TestCheckResourceAttr(resourceName, "enabled", "true"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccRegionExists(ctx context.Context, n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No Account Region ID is set")
+		}
+
+		conn := acctest.Provider.Meta().(*conns.AWSClient).AccountClient(ctx)
+
+		_, err := tfaccount.FindRegionOptInStatus(ctx, conn, rs.Primary.Attributes["account_id"], rs.Primary.Attributes["region"])
+
+		return err
+	}
+}
+
+func testAccRegionConfig_basic(region string, enabled string) string {
+	return fmt.Sprintf(`
+resource "aws_account_region" "test" {
+  region     = %[1]q
+  enabled    = %[2]q
+}
+`, region, enabled)
+}
