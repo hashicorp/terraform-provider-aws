0ea6b2e80b5f241e692fadfe9b1eedaa9b4c39fe
Bruno Schaatsbergen <git@bschaatsbergen.com>
2024-02-11 16:12:05 +0100

    chore: fix precheck

--- internal/service/apprunner/start_deployment_test.go
+++ internal/service/apprunner/start_deployment_test.go
@@ -20,7 +20,9 @@ func TestAccAppRunnerStartDeployment_basic(t *testing.T) {
 	resourceName := "aws_apprunner_start_deployment.test"
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:                 func() { acctest.PreCheck(ctx, t); testAccPreCheck(ctx, t) },
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+		},
 		ErrorCheck:               acctest.ErrorCheck(t, names.AppRunnerEndpointID),
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
 		Steps: []resource.TestStep{
