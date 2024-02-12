8c4d1ffacfd11822ca0251b720310aee4bc8dc35
Dave DeRicco <30156588+ddericco@users.noreply.github.com>
2024-02-12 11:42:38 -0500

    Add docs

--- internal/service/networkfirewall/tls_inspection_configuration_test.go
+++ internal/service/networkfirewall/tls_inspection_configuration_test.go
@@ -4,18 +4,6 @@
 package networkfirewall_test
 
 import (
-	// TIP: ==== IMPORTS ====
-	// This is a common set of imports but not customized to your code since
-	// your code hasn't been written yet. Make sure you, your IDE, or
-	// goimports -w <file> fixes these imports.
-	//
-	// The provider linter wants your imports to be in two groups: first,
-	// standard library (i.e., "fmt" or "strings"), second, everything else.
-	//
-	// Also, AWS Go SDK v2 may handle nested structures differently than v1,
-	// using the services/networkfirewall/types package. If so, you'll
-	// need to import types and reference the nested types, e.g., as
-	// types.<Type Name>.
 	"context"
 	"errors"
 	"fmt"
@@ -32,98 +20,9 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/create"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
-	"github.com/hashicorp/terraform-provider-aws/names"
-
-	// TIP: You will often need to import the package that this test file lives
-	// in. Since it is in the "test" context, it must import the package to use
-	// any normal context constants, variables, or functions.
 	tfnetworkfirewall "github.com/hashicorp/terraform-provider-aws/internal/service/networkfirewall"
+	"github.com/hashicorp/terraform-provider-aws/names"
 )
-
-// TIP: File Structure. The basic outline for all test files should be as
-// follows. Improve this resource's maintainability by following this
-// outline.
-//
-// 1. Package declaration (add "_test" since this is a test file)
-// 2. Imports
-// 3. Unit tests
-// 4. Basic test
-// 5. Disappears test
-// 6. All the other tests
-// 7. Helper functions (exists, destroy, check, etc.)
-// 8. Functions that return Terraform configurations
-
-// TIP: ==== UNIT TESTS ====
-// This is an example of a unit test. Its name is not prefixed with
-// "TestAcc" like an acceptance test.
-//
-// Unlike acceptance tests, unit tests do not access AWS and are focused on a
-// function (or method). Because of this, they are quick and cheap to run.
-//
-// In designing a resource's implementation, isolate complex bits from AWS bits
-// so that they can be tested through a unit test. We encourage more unit tests
-// in the provider.
-//
-// Cut and dry functions using well-used patterns, like typical flatteners and
-// expanders, don't need unit testing. However, if they are complex or
-// intricate, they should be unit tested.
-// func TestTLSInspectionConfigurationExampleUnitTest(t *testing.T) {
-// 	t.Parallel()
-
-// 	testCases := []struct {
-// 		TestName string
-// 		Input    string
-// 		Expected string
-// 		Error    bool
-// 	}{
-// 		{
-// 			TestName: "empty",
-// 			Input:    "",
-// 			Expected: "",
-// 			Error:    true,
-// 		},
-// 		{
-// 			TestName: "descriptive name",
-// 			Input:    "some input",
-// 			Expected: "some output",
-// 			Error:    false,
-// 		},
-// 		{
-// 			TestName: "another descriptive name",
-// 			Input:    "more input",
-// 			Expected: "more output",
-// 			Error:    false,
-// 		},
-// 	}
-
-// 	for _, testCase := range testCases {
-// 		testCase := testCase
-// 		t.Run(testCase.TestName, func(t *testing.T) {
-// 			t.Parallel()
-// 			got, err := tfnetworkfirewall.FunctionFromResource(testCase.Input)
-
-// 			if err != nil && !testCase.Error {
-// 				t.Errorf("got error (%s), expected no error", err)
-// 			}
-
-// 			if err == nil && testCase.Error {
-// 				t.Errorf("got (%s) and no error, expected error", got)
-// 			}
-
-// 			if got != testCase.Expected {
-// 				t.Errorf("got %s, expected %s", got, testCase.Expected)
-// 			}
-// 		})
-// 	}
-// }
-
-// TIP: ==== ACCEPTANCE TESTS ====
-// This is an example of a basic acceptance test. This should test as much of
-// standard functionality of the resource as possible, and test importing, if
-// applicable. We prefix its name with "TestAcc", the service, and the
-// resource name.
-//
-// Acceptance test access AWS and cost money to run.
 
 // NOTE: acceptance tests require environment variable ACM_CERTIFICATE_ARN
 // to be set and the ACM certificate to be validated during testing.
