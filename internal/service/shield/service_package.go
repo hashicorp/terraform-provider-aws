ef75488dc69d50aa992e8bfe2f30c328bc09f4fd
Adrian Johnson <adrian.johnson@hashicorp.com>
2024-02-15 16:10:41 -0600

    chore: fix lint errors

--- internal/service/shield/service_package.go
+++ internal/service/shield/service_package.go
@@ -10,18 +10,13 @@ import (
 	shield_sdkv2 "github.com/aws/aws-sdk-go-v2/service/shield"
 )
 
-const (
-	partitionID     = "aws"
-	usEast1RegionID = "us-east-1"
-)
-
 // NewClient returns a new AWS SDK for Go v2 client for this service package's AWS API.
 func (p *servicePackage) NewClient(_ context.Context, config map[string]any) (*shield_sdkv2.Client, error) {
 	cfg := *(config["aws_sdkv2_config"].(*aws_sdkv2.Config))
 
 	// Force "global" services to correct Regions.
-	if config["partition"].(string) == partitionID {
-		cfg.Region = usEast1RegionID
+	if config["partition"].(string) == "aws" {
+		cfg.Region = "us-east-1"
 	}
 
 	return shield_sdkv2.NewFromConfig(cfg, func(o *shield_sdkv2.Options) {
