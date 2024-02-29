cd2af32c3396281439d3cd7ca48bd703472ba921
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-02-29 08:07:55 -0500

    Use AWS parition and Region constants from 'names'.

--- internal/service/costoptimizationhub/service_package.go
+++ internal/service/costoptimizationhub/service_package.go
@@ -6,21 +6,21 @@ package costoptimizationhub
 import (
 	"context"
 
-	aws_sdkv2 "github.com/aws/aws-sdk-go-v2/aws"
-	costoptimizationhub_sdkv2 "github.com/aws/aws-sdk-go-v2/service/costoptimizationhub"
-	endpoints_sdkv1 "github.com/aws/aws-sdk-go/aws/endpoints"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/costoptimizationhub"
+	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
 // NewClient returns a new AWS SDK for Go v2 client for this service package's AWS API.
-func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (*costoptimizationhub_sdkv2.Client, error) {
-	cfg := *(config["aws_sdkv2_config"].(*aws_sdkv2.Config))
+func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (*costoptimizationhub.Client, error) {
+	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
-	return costoptimizationhub_sdkv2.NewFromConfig(cfg, func(o *costoptimizationhub_sdkv2.Options) {
+	return costoptimizationhub.NewFromConfig(cfg, func(o *costoptimizationhub.Options) {
 		if endpoint := config["endpoint"].(string); endpoint != "" {
-			o.BaseEndpoint = aws_sdkv2.String(endpoint)
-		} else if config["partition"].(string) == endpoints_sdkv1.AwsPartitionID {
+			o.BaseEndpoint = aws.String(endpoint)
+		} else if config["partition"].(string) == names.StandardPartitionID {
 			// Cost Optimization Hub endpoint is available only in us-east-1 Region.
-			o.Region = endpoints_sdkv1.UsEast1RegionID
+			o.Region = names.USEast1RegionID
 		}
 	}), nil
 }
