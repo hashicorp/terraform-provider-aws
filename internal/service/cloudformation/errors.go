753fbd4a86619fc46145d386697745b1e456b782
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-03-11 08:57:14 -0400

    Replace 'go-multierro'r with 'errors.Join' - cloudformation.

--- internal/service/cloudformation/errors.go
+++ internal/service/cloudformation/errors.go
@@ -4,26 +4,26 @@
 package cloudformation
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/cloudformation"
-	multierror "github.com/hashicorp/go-multierror"
 )
 
 const (
 	errCodeValidationError = "ValidationError"
 )
 
-func StackSetOperationError(apiObjects []*cloudformation.StackSetOperationResultSummary) error {
-	var errors *multierror.Error
+func stackSetOperationError(apiObjects []*cloudformation.StackSetOperationResultSummary) error {
+	var errs []error
 
 	for _, apiObject := range apiObjects {
 		if apiObject == nil {
 			continue
 		}
 
-		errors = multierror.Append(errors, fmt.Errorf("Account (%s) Region (%s) Status (%s) Status Reason: %s",
+		errs = append(errs, fmt.Errorf("Account (%s), Region (%s), %s: %s",
 			aws.StringValue(apiObject.Account),
 			aws.StringValue(apiObject.Region),
 			aws.StringValue(apiObject.Status),
@@ -31,5 +31,5 @@ func StackSetOperationError(apiObjects []*cloudformation.StackSetOperationResult
 		))
 	}
 
-	return errors.ErrorOrNil()
+	return errors.Join(errs...)
 }
