c1391260389d993f2ad5b3dc2016e07c188a9409
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-03-12 08:22:58 -0400

    ec2: 'BuildTagFilterListV2' not used.

--- internal/service/ec2/filters.go
+++ internal/service/ec2/filters.go
@@ -52,39 +52,6 @@ func newTagFilterList(tags []*ec2_sdkv1.Tag) []*ec2_sdkv1.Filter {
 	return filters
 }
 
-// BuildTagFilterListV2 takes a []*ec2.Tag and produces a []*ec2.Filter that
-// represents exact matches for all of the tag key/value pairs given in
-// the tag set.
-//
-// The purpose of this function is to create values to pass in for
-// the "Filters" attribute on most of the "Describe..." API functions
-// in the EC2 API, to implement filtering by tag values e.g. in Terraform
-// data sources that retrieve data about EC2 objects.
-//
-// It is conventional for an EC2 data source to include an attribute called
-// "tags" which conforms to the schema returned by the tftags.TagsSchema() function.
-// The value of this can then be converted to a tags slice using tagsFromMap,
-// and the result finally passed in to this function.
-//
-// In Terraform configuration this would then look like this, to constrain
-// results by name:
-//
-//	tags {
-//	  Name = "my-awesome-subnet"
-//	}
-func BuildTagFilterListV2(tags []*awstypes.Tag) []awstypes.Filter {
-	filters := make([]awstypes.Filter, len(tags))
-
-	for i, tag := range tags {
-		filters[i] = awstypes.Filter{
-			Name:   aws_sdkv1.String(fmt.Sprintf("tag:%s", *tag.Key)),
-			Values: []string{*tag.Value},
-		}
-	}
-
-	return filters
-}
-
 // attributeFiltersFromMultimap returns an array of EC2 Filter objects to be used when listing resources.
 //
 // The keys of the specified map are the resource attributes names used in the filter - see the documentation
