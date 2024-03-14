df6bb85e9f3f4fd7ffcd5b172f854c8eb444b00e
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-03-14 09:14:41 -0400

    Use 'IsAWSAccountID'.

--- internal/types/aws_account_id.go
+++ internal/types/aws_account_id.go
@@ -9,5 +9,6 @@ import (
 
 // IsAWSAccountID returns whether or not the specified string is a valid AWS account ID.
 func IsAWSAccountID(s string) bool {
+	// https://docs.aws.amazon.com/accounts/latest/reference/manage-acct-identifiers.html.
 	return regexache.MustCompile(`^\d{12}$`).MatchString(s)
 }
