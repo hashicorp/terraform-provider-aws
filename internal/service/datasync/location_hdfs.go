5dde47b004854c4645a2324b5583877d6ad7ac7f
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-03-19 09:31:34 -0400

    r/aws_datasync_location_hdfs: Mark `qop_configuration` as Computed.

--- internal/service/datasync/location_hdfs.go
+++ internal/service/datasync/location_hdfs.go
@@ -119,17 +119,20 @@ func resourceLocationHDFS() *schema.Resource {
 			"qop_configuration": {
 				Type:     schema.TypeList,
 				Optional: true,
+				Computed: true,
 				MaxItems: 1,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"data_transfer_protection": {
 							Type:         schema.TypeString,
 							Optional:     true,
+							Computed:     true,
 							ValidateFunc: validation.StringInSlice(datasync.HdfsDataTransferProtection_Values(), false),
 						},
 						"rpc_protection": {
 							Type:         schema.TypeString,
 							Optional:     true,
+							Computed:     true,
 							ValidateFunc: validation.StringInSlice(datasync.HdfsRpcProtection_Values(), false),
 						},
 					},
