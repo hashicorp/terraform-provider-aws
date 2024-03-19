753a1dbe432f17fd88dfdc917226dd35c3a561e6
mvishnub <mvishnub@amazon.com>
2024-03-19 08:53:38 +0000

    make gen commit

--- internal/service/costoptimizationhub/service_package_gen.go
+++ internal/service/costoptimizationhub/service_package_gen.go
@@ -17,7 +17,12 @@ func (p *servicePackage) FrameworkDataSources(ctx context.Context) []*types.Serv
 }
 
 func (p *servicePackage) FrameworkResources(ctx context.Context) []*types.ServicePackageFrameworkResource {
-	return []*types.ServicePackageFrameworkResource{}
+	return []*types.ServicePackageFrameworkResource{
+		{
+			Factory: newResourceEnrollmentStatus,
+			Name:    "Enrollment Status",
+		},
+	}
 }
 
 func (p *servicePackage) SDKDataSources(ctx context.Context) []*types.ServicePackageSDKDataSource {
