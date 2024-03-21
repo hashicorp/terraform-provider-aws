33c7ad2805b5febe706c1457c376c12f9fdc7c50
Graham Davison <gdavison@hashicorp.com>
2024-03-21 11:39:43 -0700

    Adds tests for `aws_batch_compute_environment` and `aws_batch_job_definition`. Fixes resource name for `aws_batch_scheduling_policy`.

--- internal/service/batch/scheduling_policy.go
+++ internal/service/batch/scheduling_policy.go
@@ -22,7 +22,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
-// @SDKResource("aws_batch_scheduling_policy", name="Job Definition")
+// @SDKResource("aws_batch_scheduling_policy", name="Scheduling Policy")
 // @Tags(identifierAttribute="arn")
 func ResourceSchedulingPolicy() *schema.Resource {
 	return &schema.Resource{
