a65d1622a4d1f8a24aedec2606ae62ac8d6dda6b
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-04-01 10:50:51 -0400

    d/aws_emr_release_labels: Reduce visibility.

--- internal/service/emr/service_package_gen.go
+++ internal/service/emr/service_package_gen.go
@@ -33,8 +33,9 @@ func (p *servicePackage) FrameworkResources(ctx context.Context) []*types.Servic
 func (p *servicePackage) SDKDataSources(ctx context.Context) []*types.ServicePackageSDKDataSource {
 	return []*types.ServicePackageSDKDataSource{
 		{
-			Factory:  DataSourceReleaseLabels,
+			Factory:  dataSourceReleaseLabels,
 			TypeName: "aws_emr_release_labels",
+			Name:     "Release Labels",
 		},
 	}
 }
