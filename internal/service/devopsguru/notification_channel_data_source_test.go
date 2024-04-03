d1ce462d4479cfe60a2fdaa2201ebb814a702f8a
Jared Baker <jared.baker@hashicorp.com>
2024-04-03 15:30:39 -0400

    r/aws_devopsguru(test): add service level pre-check
    This pre-check captures operation errors observed when calling DevOps Guru in certain non-commercial partitions, such as GovCloud.
    
    Before:
    
    ```
    Error: creating Amazon DevOps Guru Notification Channel: operation error DevOps Guru: AddNotificationChannel, https response error StatusCode: 0, RequestID: , request send failed, Put "https://devops-guru.us-gov-east-1.amazonaws.com/channels": dial tcp: lookup devops-guru.us-gov-east-1.amazonaws.com: no such host
    ```
    
    GovCloud:
    
    ```console
    % make testacc PKG=devopsguru TESTS=TestAccDevOpsGuru_serial
    ==> Checking that code complies with gofmt requirements...
    TF_ACC=1 go1.21.8 test ./internal/service/devopsguru/... -v -count 1 -parallel 20 -run='TestAccDevOpsGuru_serial'  -timeout 360m
    === RUN   TestAccDevOpsGuru_serial
    === PAUSE TestAccDevOpsGuru_serial
    === CONT  TestAccDevOpsGuru_serial
        devopsguru_test.go:72: skipping acceptance testing: operation error DevOps Guru: DescribeAccountHealth, https response error StatusCode: 0, RequestID: , request send failed, Get "https://devops-guru.us-gov-east-1.amazonaws.com/accounts/health": dial tcp: lookup devops-guru.us-gov-east-1.amazonaws.com: no such host
    --- SKIP: TestAccDevOpsGuru_serial (0.17s)
    PASS
    ok      github.com/hashicorp/terraform-provider-aws/internal/service/devopsguru 5.651s
    ```
    
    Commercial:
    
    ```console
    % make testacc PKG=devopsguru TESTS=TestAccDevOpsGuru_serial
    ==> Checking that code complies with gofmt requirements...
    TF_ACC=1 go1.21.8 test ./internal/service/devopsguru/... -v -count 1 -parallel 20 -run='TestAccDevOpsGuru_serial'  -timeout 360m
    
    --- PASS: TestAccDevOpsGuru_serial (222.18s)
        --- PASS: TestAccDevOpsGuru_serial/ServiceIntegration (38.19s)
            --- PASS: TestAccDevOpsGuru_serial/ServiceIntegration/basic (18.19s)
            --- PASS: TestAccDevOpsGuru_serial/ServiceIntegration/kms (20.00s)
        --- PASS: TestAccDevOpsGuru_serial/EventSourcesConfig (20.63s)
            --- PASS: TestAccDevOpsGuru_serial/EventSourcesConfig/basic (11.15s)
            --- PASS: TestAccDevOpsGuru_serial/EventSourcesConfig/disappears (9.48s)
        --- PASS: TestAccDevOpsGuru_serial/NotificationChannel (38.04s)
            --- PASS: TestAccDevOpsGuru_serial/NotificationChannel/basic (13.16s)
            --- PASS: TestAccDevOpsGuru_serial/NotificationChannel/disappears (11.58s)
            --- PASS: TestAccDevOpsGuru_serial/NotificationChannel/filters (13.29s)
        --- PASS: TestAccDevOpsGuru_serial/NotificationChannelDataSource (11.37s)
            --- PASS: TestAccDevOpsGuru_serial/NotificationChannelDataSource/basic (11.37s)
        --- PASS: TestAccDevOpsGuru_serial/ResourceCollection (103.72s)
            --- PASS: TestAccDevOpsGuru_serial/ResourceCollection/disappears (9.60s)
            --- PASS: TestAccDevOpsGuru_serial/ResourceCollection/tags (11.27s)
            --- PASS: TestAccDevOpsGuru_serial/ResourceCollection/tagsAllResources (11.38s)
            --- PASS: TestAccDevOpsGuru_serial/ResourceCollection/basic (11.37s)
            --- PASS: TestAccDevOpsGuru_serial/ResourceCollection/cloudformation (60.10s)
        --- PASS: TestAccDevOpsGuru_serial/ResourceCollectionDataSource (9.42s)
            --- PASS: TestAccDevOpsGuru_serial/ResourceCollectionDataSource/basic (9.42s)
    PASS
    ok      github.com/hashicorp/terraform-provider-aws/internal/service/devopsguru 227.638s
    ```

--- internal/service/devopsguru/notification_channel_data_source_test.go
+++ internal/service/devopsguru/notification_channel_data_source_test.go
@@ -23,6 +23,7 @@ func testAccNotificationChannelDataSource_basic(t *testing.T) {
 		PreCheck: func() {
 			acctest.PreCheck(ctx, t)
 			acctest.PreCheckPartitionHasService(t, names.DevOpsGuruEndpointID)
+			testAccPreCheck(ctx, t)
 		},
 		ErrorCheck:               acctest.ErrorCheck(t, names.DevOpsGuruServiceID),
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
