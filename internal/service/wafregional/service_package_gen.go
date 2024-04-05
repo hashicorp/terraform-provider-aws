01bcb8497e1c72e69b92c3eb555b020edac08193
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-04-05 09:02:32 -0400

    d/aws_wafregional_ipset: Reduce visibility.

--- internal/service/wafregional/service_package_gen.go
+++ internal/service/wafregional/service_package_gen.go
@@ -26,8 +26,9 @@ func (p *servicePackage) FrameworkResources(ctx context.Context) []*types.Servic
 func (p *servicePackage) SDKDataSources(ctx context.Context) []*types.ServicePackageSDKDataSource {
 	return []*types.ServicePackageSDKDataSource{
 		{
-			Factory:  DataSourceIPSet,
+			Factory:  dataSourceIPSet,
 			TypeName: "aws_wafregional_ipset",
+			Name:     "IPSet",
 		},
 		{
 			Factory:  DataSourceRateBasedRule,
