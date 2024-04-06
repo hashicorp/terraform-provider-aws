3380d20ea14f7247c21bc394c5dd3395dff897e3
nikhil <nplusg1993@gmail.com>
2024-04-06 21:16:11 +0100

    r/aws_glue_job: update test and documentation

--- internal/service/glue/job_test.go
+++ internal/service/glue/job_test.go
@@ -653,6 +653,34 @@ func TestAccGlueJob_workerType(t *testing.T) {
 				),
 			},
 			{
+				Config: testAccJobConfig_workerType(rName, "G.025X"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckJobExists(ctx, resourceName, &job),
+					resource.TestCheckResourceAttr(resourceName, "worker_type", "G.025X"),
+				),
+			},
+			{
+				Config: testAccJobConfig_workerType(rName, "G.4X"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckJobExists(ctx, resourceName, &job),
+					resource.TestCheckResourceAttr(resourceName, "worker_type", "G.4X"),
+				),
+			},
+			{
+				Config: testAccJobConfig_workerType(rName, "G.8X"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckJobExists(ctx, resourceName, &job),
+					resource.TestCheckResourceAttr(resourceName, "worker_type", "G.8X"),
+				),
+			},
+			{
+				Config: testAccJobConfig_workerType(rName, "Z.2X"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckJobExists(ctx, resourceName, &job),
+					resource.TestCheckResourceAttr(resourceName, "worker_type", "Z.2X"),
+				),
+			},
+			{
 				ResourceName:      resourceName,
 				ImportState:       true,
 				ImportStateVerify: true,
