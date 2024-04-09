7385756f65a6d4304b3cdae98b046aec10ec76cc
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-04-09 15:58:46 -0400

    r/aws_fms_admin_account: Retries are done via service client.

--- internal/service/fms/service_package_gen.go
+++ internal/service/fms/service_package_gen.go
@@ -5,8 +5,6 @@ package fms
 import (
 	"context"
 
-	aws_sdkv2 "github.com/aws/aws-sdk-go-v2/aws"
-	fms_sdkv2 "github.com/aws/aws-sdk-go-v2/service/fms"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/types"
 	"github.com/hashicorp/terraform-provider-aws/names"
@@ -46,17 +44,6 @@ func (p *servicePackage) SDKResources(ctx context.Context) []*types.ServicePacka
 
 func (p *servicePackage) ServicePackageName() string {
 	return names.FMS
-}
-
-// NewClient returns a new AWS SDK for Go v2 client for this service package's AWS API.
-func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (*fms_sdkv2.Client, error) {
-	cfg := *(config["aws_sdkv2_config"].(*aws_sdkv2.Config))
-
-	return fms_sdkv2.NewFromConfig(cfg, func(o *fms_sdkv2.Options) {
-		if endpoint := config["endpoint"].(string); endpoint != "" {
-			o.BaseEndpoint = aws_sdkv2.String(endpoint)
-		}
-	}), nil
 }
 
 func ServicePackage(ctx context.Context) conns.ServicePackage {
