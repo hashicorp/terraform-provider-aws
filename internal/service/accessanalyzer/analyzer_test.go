f9e067f4b2af6dd98083e95db04bd26e68d721e8
Graham Davison <gdavison@hashicorp.com>
2024-04-09 14:44:17 -0700

    Adds tests for `accessanalyzer`

--- internal/service/accessanalyzer/analyzer_test.go
+++ internal/service/accessanalyzer/analyzer_test.go
@@ -77,53 +77,6 @@ func testAccAnalyzer_disappears(t *testing.T) {
 	})
 }
 
-func testAccAnalyzer_tags(t *testing.T) {
-	ctx := acctest.Context(t)
-	var analyzer types.AnalyzerSummary
-
-	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
-	resourceName := "aws_accessanalyzer_analyzer.test"
-
-	resource.Test(t, resource.TestCase{
-		PreCheck:                 func() { acctest.PreCheck(ctx, t); testAccPreCheck(ctx, t) },
-		ErrorCheck:               acctest.ErrorCheck(t, names.AccessAnalyzerServiceID),
-		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
-		CheckDestroy:             testAccCheckAnalyzerDestroy(ctx),
-		Steps: []resource.TestStep{
-			{
-				Config: testAccAnalyzerConfig_tags1(rName, "key1", "value1"),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckAnalyzerExists(ctx, resourceName, &analyzer),
-					resource.TestCheckResourceAttr(resourceName, "tags.%", "1"),
-					resource.TestCheckResourceAttr(resourceName, "tags.key1", "value1"),
-				),
-			},
-			{
-				ResourceName:      resourceName,
-				ImportState:       true,
-				ImportStateVerify: true,
-			},
-			{
-				Config: testAccAnalyzerConfig_tags2(rName, "key1", "value1updated", "key2", "value2"),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckAnalyzerExists(ctx, resourceName, &analyzer),
-					resource.TestCheckResourceAttr(resourceName, "tags.%", "2"),
-					resource.TestCheckResourceAttr(resourceName, "tags.key1", "value1updated"),
-					resource.TestCheckResourceAttr(resourceName, "tags.key2", "value2"),
-				),
-			},
-			{
-				Config: testAccAnalyzerConfig_tags1(rName, "key2", "value2"),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckAnalyzerExists(ctx, resourceName, &analyzer),
-					resource.TestCheckResourceAttr(resourceName, "tags.%", "1"),
-					resource.TestCheckResourceAttr(resourceName, "tags.key2", "value2"),
-				),
-			},
-		},
-	})
-}
-
 func testAccAnalyzer_Type_Organization(t *testing.T) {
 	ctx := acctest.Context(t)
 	var analyzer types.AnalyzerSummary
@@ -247,6 +200,14 @@ resource "aws_accessanalyzer_analyzer" "test" {
 `, rName)
 }
 
+func testAccAnalyzerConfig_tags0(rName string) string {
+	return fmt.Sprintf(`
+resource "aws_accessanalyzer_analyzer" "test" {
+  analyzer_name = %[1]q
+}
+`, rName)
+}
+
 func testAccAnalyzerConfig_tags1(rName, tagKey1, tagValue1 string) string {
 	return fmt.Sprintf(`
 resource "aws_accessanalyzer_analyzer" "test" {
@@ -270,6 +231,18 @@ resource "aws_accessanalyzer_analyzer" "test" {
   }
 }
 `, rName, tagKey1, tagValue1, tagKey2, tagValue2)
+}
+
+func testAccAnalyzerConfig_tagsNull(rName, tagKey1 string) string {
+	return fmt.Sprintf(`
+resource "aws_accessanalyzer_analyzer" "test" {
+  analyzer_name = %[1]q
+
+  tags = {
+    %[2]q = null
+  }
+}
+`, rName, tagKey1)
 }
 
 func testAccAnalyzerConfig_typeOrganization(rName string) string {
