e1e8df44129273dd16033d595b280b1706405a38
Justin Retzolk <44710313+justinretzolk@users.noreply.github.com>
2024-04-12 10:33:11 -0500

    Remove secrets management from Terraform config

---
+++ infrastructure/repository/removed.tf
@@ -0,0 +1,43 @@
+# TODO: delete after removal
+# https://developer.hashicorp.com/terraform/language/resources/syntax#removing-resources
+
+removed {
+  from = github_actions_variable.team_project_field_status_option_ids
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = github_actions_variable.team_project_field_view_option_ids
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = github_actions_secret.core_contributors
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = github_actions_secret.maintainers
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = github_actions_secret.partners
+
+  lifecycle {
+    destroy = false
+  }
+}
+
