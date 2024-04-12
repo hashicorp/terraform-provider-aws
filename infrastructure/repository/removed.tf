872249e7b9ae947c86d19309a09d9367327de15e
Justin Retzolk <44710313+justinretzolk@users.noreply.github.com>
2024-04-12 10:45:17 -0500

    Add header

--- infrastructure/repository/removed.tf
+++ infrastructure/repository/removed.tf
@@ -1,3 +1,7 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+
 # TODO: delete after removal
 # https://developer.hashicorp.com/terraform/language/resources/syntax#removing-resources
 
