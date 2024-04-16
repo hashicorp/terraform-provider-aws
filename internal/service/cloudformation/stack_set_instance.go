41df1352fcb18f53da8aae7f61a4b35e18064506
alexknez <alexknez@amazon.com>
2024-04-16 17:17:58 +0100

    Added OperationPreferences in Delete operation

--- internal/service/cloudformation/stack_set_instance.go
+++ internal/service/cloudformation/stack_set_instance.go
@@ -472,6 +472,10 @@ func resourceStackSetInstanceDelete(ctx context.Context, d *schema.ResourceData,
 		input.DeploymentTargets = dt
 	}
 
+	if v, ok := d.GetOk("operation_preferences"); ok && len(v.([]interface{})) > 0 && v.([]interface{})[0] != nil {
+		input.OperationPreferences = expandOperationPreferences(v.([]interface{})[0].(map[string]interface{}))
+	}
+
 	log.Printf("[DEBUG] Deleting CloudFormation StackSet Instance: %s", d.Id())
 	outputRaw, err := tfresource.RetryWhenIsA[*awstypes.OperationInProgressException](ctx, d.Timeout(schema.TimeoutDelete), func() (interface{}, error) {
 		return conn.DeleteStackInstances(ctx, input)
