e9f3584bd9584d7f6c56a99baa671001c51f85f5
Sharon Nam <sharon.nam@sharon.nam-PJCJ4F7JH7>
2024-04-16 16:58:18 -0700

    Fix config format

--- internal/service/bcmdataexports/export_test.go
+++ internal/service/bcmdataexports/export_test.go
@@ -142,16 +142,16 @@ resource "aws_s3_bucket_policy" "bucket" {
     Id = %[1]q
     Statement = [{
       Action = [
-      "s3:PutObject",
-      "s3:GetBucketPolicy"
+        "s3:PutObject",
+        "s3:GetBucketPolicy"
       ]
       Effect = "Allow"
-      Sid = "EnableAWSDataExportsToWriteToS3AndCheckPolicy"
+      Sid    = "EnableAWSDataExportsToWriteToS3AndCheckPolicy"
       Principal = {
-      Service = [
-        "billingreports.amazonaws.com",
-        "bcm-data-exports.amazonaws.com"
-      ]
+        Service = [
+          "billingreports.amazonaws.com",
+          "bcm-data-exports.amazonaws.com"
+        ]
       }
       Resource = [
         aws_s3_bucket.test.arn,
@@ -163,7 +163,7 @@ resource "aws_s3_bucket_policy" "bucket" {
 }
 
 resource "aws_s3_bucket" "test" {
-  bucket = %[1]q
+  bucket        = %[1]q
   force_destroy = true
 }
 
@@ -174,24 +174,24 @@ resource "aws_bcmdataexports_export" "test" {
       query_statement = "SELECT identity_line_item_id, identity_time_interval, line_item_product_code,line_item_unblended_cost FROM COST_AND_USAGE_REPORT"
       table_configurations = {
         COST_AND_USAGE_REPORT = {
-          TIME_GRANULARITY = "HOURLY",
-          INCLUDE_RESOURCES = "FALSE",
+          TIME_GRANULARITY                      = "HOURLY",
+          INCLUDE_RESOURCES                     = "FALSE",
           INCLUDE_MANUAL_DISCOUNT_COMPATIBILITY = "FALSE",
-          INCLUDE_SPLIT_COST_ALLOCATION_DATA = "FALSE",
+          INCLUDE_SPLIT_COST_ALLOCATION_DATA    = "FALSE",
         }
       }
     }
     destination_configurations {
       s3_destination {
-          s3_bucket = aws_s3_bucket.test.bucket
-          s3_prefix = aws_s3_bucket.test.bucket_prefix
-          s3_region = aws_s3_bucket.test.region
-          s3_output_configurations {
-            overwrite = "OVERWRITE_REPORT"
-            format = "TEXT_OR_CSV"
-            compression = "GZIP"
-            output_type = "CUSTOM"
-          }
+        s3_bucket = aws_s3_bucket.test.bucket
+        s3_prefix = aws_s3_bucket.test.bucket_prefix
+        s3_region = aws_s3_bucket.test.region
+        s3_output_configurations {
+          overwrite   = "OVERWRITE_REPORT"
+          format      = "TEXT_OR_CSV"
+          compression = "GZIP"
+          output_type = "CUSTOM"
+        }
       }
     }
 
