abeab42e99fc1439944d6173a5fc3961a4e6cc06
nikhil <nplusg1993@gmail.com>
2024-04-18 19:55:34 +0100

    f-support for network_card_index

--- internal/service/kafka/replicator.go
+++ internal/service/kafka/replicator.go
@@ -163,7 +163,6 @@ func resourceReplicator() *schema.Resource {
 										Type:     schema.TypeList,
 										Optional: true,
 										Computed: true,
-										ForceNew: true,
 										MaxItems: 1,
 										Elem: &schema.Resource{
 											Schema: map[string]*schema.Schema{
