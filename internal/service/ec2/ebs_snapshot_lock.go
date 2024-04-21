dcc34d5fc81a2fbb863e43e1013fa26004c67861
Oussama Bounaim <1151352+obounaim@users.noreply.github.com>
2024-04-21 13:16:51 +0000

    Update EBS snapshot lock logic

--- internal/service/ec2/ebs_snapshot_lock.go
+++ internal/service/ec2/ebs_snapshot_lock.go
@@ -7,12 +7,14 @@ import (
 	"context"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/enum"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
 )
 
@@ -35,45 +37,45 @@ func ResourceEBSSnapshotLock() *schema.Resource {
 				ForceNew: true,
 			},
 			"lock_mode": {
-				Type:         schema.TypeString,
-				Required:     true,
-				ForceNew:     true,
-				ValidateFunc: validation.StringInSlice(ec2.LockMode_Values(), false),
+				Type:             schema.TypeString,
+				Required:         true,
+				ForceNew:         true,
+				ValidateDiagFunc: enum.Validate[types.LockMode](),
 			},
 			"cool_off_period": {
-				Type:         schema.TypeInt,
-				ForceNew:     true,
-				ValidateFunc: validation.IntBetween(1, 72),
+				Type:             schema.TypeInt,
+				ForceNew:         true,
+				ValidateDiagFunc: validation.ToDiagFunc(validation.IntBetween(1, 72)),
 			},
 			"lock_duration": {
-				Type:         schema.TypeInt,
-				ForceNew:     true,
-				ValidateFunc: validation.IntBetween(1, 36500),
+				Type:             schema.TypeInt,
+				ForceNew:         true,
+				ValidateDiagFunc: validation.ToDiagFunc(validation.IntBetween(1, 36500)),
 			},
 			"expiration_date": {
-				Type:         schema.TypeString,
-				ValidateFunc: validation.IsRFC3339Time,
-				ForceNew:     true,
+				Type:             schema.TypeString,
+				ValidateDiagFunc: validation.ToDiagFunc(validation.IsRFC3339Time),
+				ForceNew:         true,
 			},
 			"lock_created_on": {
-				Type:         schema.TypeString,
-				ValidateFunc: validation.IsRFC3339Time,
-				computed: 	  true,
+				Type:             schema.TypeString,
+				ValidateDiagFunc: validation.ToDiagFunc(validation.IsRFC3339Time),
+				Computed:         true,
 			},
 			"cool_off_period_expires_on": {
-				Type:         schema.TypeString,
-				ValidateFunc: validation.IsRFC3339Time,
-				computed: 	  true,
+				Type:             schema.TypeString,
+				ValidateDiagFunc: validation.ToDiagFunc(validation.IsRFC3339Time),
+				Computed:         true,
 			},
 			"lock_duration_start_time": {
-				Type:         schema.TypeString,
-				ValidateFunc: validation.IsRFC3339Time,
-				computed: 	  true,
+				Type:             schema.TypeString,
+				ValidateDiagFunc: validation.ToDiagFunc(validation.IsRFC3339Time),
+				Computed:         true,
 			},
 			"lock_state": {
-				Type:         schema.TypeString,
-				computed: 	  true,
-				ValidateFunc: validation.StringInSlice(ec2.LockState_Values(), false),
+				Type:             schema.TypeString,
+				Computed:         true,
+				ValidateDiagFunc: enum.Validate[types.LockState](),
 			},
 		},
 	}
@@ -81,19 +83,19 @@ func ResourceEBSSnapshotLock() *schema.Resource {
 
 func resourceEBSSnapshotLockCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
-	conn := meta.(*conns.AWSClient).EC2Conn(ctx)
+	conn := meta.(*conns.AWSClient).EC2Client(ctx)
 
 	input := &ec2.LockSnapshotInput{
 		SnapshotId: aws.String(d.Get("snapshot_id").(string)),
-		LockMode:   aws.String(d.Get("lock_mode").(string)),
+		LockMode:   types.LockMode(d.Get("lock_mode").(string)),
 	}
 
 	if v, ok := d.GetOk("cool_off_period"); ok {
-		input.CoolOffPeriod = aws.Int64(v.(int64))
+		input.CoolOffPeriod = aws.Int32(v.(int32))
 	}
 
 	if v, ok := d.GetOk("lock_duration"); ok {
-		input.LockDuration = aws.Int64(v.(int64))
+		input.LockDuration = aws.Int32(v.(int32))
 	}
 
 	if v, ok := d.GetOk("expiration_date"); ok {
@@ -101,62 +103,62 @@ func resourceEBSSnapshotLockCreate(ctx context.Context, d *schema.ResourceData, 
 		input.ExpirationDate = aws.Time(t)
 	}
 
-	resp, err := conn.LockSnapshotWithContext(ctx, input)
+	resp, err := conn.LockSnapshot(ctx, input)
 	if err != nil {
 		return sdkdiag.AppendErrorf(diags, "Creating EBS snapshot lock: %s", err)
 	}
 
-	d.Set("lock_created_on", resp.LockCreatedOn.String() )
-	d.Set("cool_off_period_expires_on", resp.CoolOffPeriodExpiresOn.String())
-	d.Set("lock_duration_start_time", resp.LockDurationStartTime.String())
-	d.Set("lock_state", aws.StringValue(resp.LockState))
+	d.Set("lock_created_on", resp.LockCreatedOn)
+	d.Set("cool_off_period_expires_on", resp.CoolOffPeriodExpiresOn)
+	d.Set("lock_duration_start_time", resp.LockDurationStartTime)
+	d.Set("lock_state", resp.LockState)
 
 	return append(diags, resourceEBSSnapshotLockRead(ctx, d, meta)...)
 }
 
 func resourceEBSSnapshotLockRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
-	conn := meta.(*conns.AWSClient).EC2Conn(ctx)
+	conn := meta.(*conns.AWSClient).EC2Client(ctx)
 
 	input := &ec2.DescribeLockedSnapshotsInput{
-		SnapshotIds: aws.StringSlice([]string{
+		SnapshotIds: []string{
 			d.Get("snapshot_id").(string),
-		}),
+		},
 	}
 
-	resp, err := conn.DescribeLockedSnapshotsWithContext(ctx, input)
+	resp, err := conn.DescribeLockedSnapshots(ctx, input)
 
 	if err != nil {
 		return sdkdiag.AppendErrorf(diags, "EBS snapshot lock not found: %s", err)
 	}
 
-	d.Set("snapshot_id", aws.StringValue(resp.Snapshots[0].SnapshotId))
-	d.Set("lock_mode", aws.StringValue(resp.Snapshots[0].))
-	d.Set("cool_off_period", aws.StringValue(resp.Snapshots[0].CoolOffPeriod))
-	d.Set("lock_duration", aws.StringValue(resp.Snapshots[0].LockDuration))
-	d.Set("expiration_date", aws.StringValue(resp.Snapshots[0].))
-	d.Set("lock_created_on", resp.LockCreatedOn.String())
-	d.Set("cool_off_period_expires_on", resp.CoolOffPeriodExpiresOn.String())
-	d.Set("lock_duration_start_time", resp.LockDurationStartTime.String())
-	d.Set("lock_state", aws.StringValue(resp.LockState))
+	d.Set("cool_off_period", resp.Snapshots[0].CoolOffPeriod)
+	d.Set("cool_off_period_expires_on", resp.Snapshots[0].CoolOffPeriodExpiresOn)
+	d.Set("lock_created_on", resp.Snapshots[0].LockCreatedOn)
+	d.Set("lock_duration", resp.Snapshots[0].LockDuration)
+	d.Set("lock_duration_start_time", resp.Snapshots[0].LockDurationStartTime)
+	d.Set("expiration_date", resp.Snapshots[0].LockExpiresOn)
+	d.Set("lock_state", resp.Snapshots[0].LockState)
+	d.Set("snapshot_id", resp.Snapshots[0].SnapshotId)
 
 	return diags
 }
 
 func resourceEBSSnapshotLockUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
-	conn := meta.(*conns.AWSClient).EC2Conn(ctx)
+	conn := meta.(*conns.AWSClient).EC2Client(ctx)
+
 	input := &ec2.LockSnapshotInput{
 		SnapshotId: aws.String(d.Get("snapshot_id").(string)),
-		LockMode:   aws.String(d.Get("lock_mode").(string)),
+		LockMode:   types.LockMode(d.Get("lock_mode").(string)),
 	}
 
 	if v, ok := d.GetOk("cool_off_period"); ok {
-		input.CoolOffPeriod = aws.Int64(v.(int64))
+		input.CoolOffPeriod = aws.Int32(v.(int32))
 	}
 
 	if v, ok := d.GetOk("lock_duration"); ok {
-		input.LockDuration = aws.Int64(v.(int64))
+		input.LockDuration = aws.Int32(v.(int32))
 	}
 
 	if v, ok := d.GetOk("expiration_date"); ok {
@@ -164,24 +166,28 @@ func resourceEBSSnapshotLockUpdate(ctx context.Context, d *schema.ResourceData, 
 		input.ExpirationDate = aws.Time(t)
 	}
 
-	resp, err := conn.LockSnapshotWithContext(ctx, input)
-
+	resp, err := conn.LockSnapshot(ctx, input)
 	if err != nil {
 		return sdkdiag.AppendErrorf(diags, "Creating EBS snapshot lock: %s", err)
 	}
+
+	d.Set("lock_created_on", resp.LockCreatedOn)
+	d.Set("cool_off_period_expires_on", resp.CoolOffPeriodExpiresOn)
+	d.Set("lock_duration_start_time", resp.LockDurationStartTime)
+	d.Set("lock_state", resp.LockState)
 
 	return diags
 }
 
 func resourceEBSSnapshotLockDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
-	conn := meta.(*conns.AWSClient).EC2Conn(ctx)
+	conn := meta.(*conns.AWSClient).EC2Client(ctx)
 
 	input := &ec2.UnlockSnapshotInput{
-		SnapshotId: aws.String("snapshot_id")
+		SnapshotId: aws.String(d.Get("snapshot_id").(string)),
 	}
 
-	resp, err := conn.UnlockSnapshotWithContext(ctx, input)
+	resp, err := conn.UnlockSnapshot(ctx, input)
 
 	if err != nil {
 		return sdkdiag.AppendErrorf(diags, "Unlock EBS snapshot: %s", err)
