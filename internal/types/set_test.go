aacd972e0e47be54d2470b20dbb65ec3a4a2a7df
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-04-22 15:44:17 -0400

    Complete 'internal/flex/Set' -> 'internal/types/Set'.

--- internal/types/set_test.go
+++ internal/types/set_test.go
@@ -7,10 +7,9 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
-func TestSetDifference(t *testing.T) {
+func TestSetDifference_ints(t *testing.T) {
 	t.Parallel()
 
 	testCases := []struct {
@@ -57,7 +56,56 @@ func TestSetDifference(t *testing.T) {
 
 			got := testCase.set1.Difference(testCase.set2)
 
-			if diff := cmp.Diff(got, testCase.expected, cmpopts.SortSlices(func(v1, v2 int) bool { return v1 < v2 })); diff != "" {
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected diff (+wanted, -got): %s", diff)
+			}
+		})
+	}
+}
+
+func TestSetDifference_strings(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		original Set[string]
+		new      Set[string]
+		expected Set[string]
+	}
+	tests := map[string]testCase{
+		"nil": {
+			original: nil,
+			new:      nil,
+			expected: nil,
+		},
+		"equal": {
+			original: Set[string]{"one"},
+			new:      Set[string]{"one"},
+			expected: nil,
+		},
+		"difference": {
+			original: Set[string]{"one", "two", "four"},
+			new:      Set[string]{"one", "two", "three"},
+			expected: Set[string]{"four"},
+		},
+		"difference_remove": {
+			original: Set[string]{"one", "two"},
+			new:      Set[string]{"one"},
+			expected: Set[string]{"two"},
+		},
+		"difference_add": {
+			original: Set[string]{"one"},
+			new:      Set[string]{"one", "two"},
+			expected: nil,
+		},
+	}
+
+	for name, test := range tests {
+		name, test := name, test
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := test.original.Difference(test.new)
+			if diff := cmp.Diff(got, test.expected); diff != "" {
 				t.Errorf("unexpected diff (+wanted, -got): %s", diff)
 			}
 		})
