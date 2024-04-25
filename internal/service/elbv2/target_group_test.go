b6f593652d4763a71b27273990281d6f9ce03d7f
Mert Acikportali <macikportali@newrelic.com>
2024-04-25 12:47:18 +0200

    Remove GLB precheck

--- internal/service/elbv2/target_group_test.go
+++ internal/service/elbv2/target_group_test.go
@@ -2602,12 +2602,7 @@ func TestAccELBV2TargetGroup_targetGroupHealthState(t *testing.T) {
 	resourceName := "aws_lb_target_group.test"
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck: func() {
-			acctest.PreCheck(ctx, t)
-			if testAccPreCheckGatewayLoadBalancer(ctx, t) {
-				t.Fatal("PreCheck failed!")
-			}
-		},
+		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
 		ErrorCheck:               acctest.ErrorCheck(t, names.ELBV2ServiceID),
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
 		CheckDestroy:             testAccCheckTargetGroupDestroy(ctx),
