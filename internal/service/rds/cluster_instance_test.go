976c297e6113f0a58fd9404684b4265bf2121b7f
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-04-25 10:23:21 -0400

    testAccClusterInstanceConfig_publiclyAccessible: Use 'acctest.ConfigVPCWithSubnetsEnableDNSHostnames'.

--- internal/service/rds/cluster_instance_test.go
+++ internal/service/rds/cluster_instance_test.go
@@ -1360,29 +1360,7 @@ resource "aws_db_parameter_group" "test" {
 
 func testAccClusterInstanceConfig_publiclyAccessible(rName string, publiclyAccessible bool) string {
 	return acctest.ConfigCompose(
-		acctest.ConfigAvailableAZsNoOptInDefaultExclude(),
-		fmt.Sprintf(`
-resource "aws_vpc" "test" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = %[1]q
-  }
-}
-
-resource "aws_subnet" "test" {
-  count = %[2]d
-
-  vpc_id            = aws_vpc.test.id
-  availability_zone = data.aws_availability_zones.available.names[count.index]
-  cidr_block        = cidrsubnet(aws_vpc.test.cidr_block, 8, count.index)
-
-  tags = {
-    Name = %[1]q
-  }
-}
-`, rName, 2),
+		acctest.ConfigVPCWithSubnetsEnableDNSHostnames(rName, 2),
 		testAccClusterInstanceConfig_orderableEngineBase("aurora-mysql", false),
 		fmt.Sprintf(`
 resource "aws_rds_cluster_instance" "test" {
