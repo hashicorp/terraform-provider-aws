37279851d844bc5d2c201e35b83dacd27ae54141
bar-silver <bar.silver@anaplan.com>
2024-04-25 18:12:58 +0300

    Update imports

--- internal/service/redshiftserverless/scheduled_action.go
+++ internal/service/redshiftserverless/scheduled_action.go
@@ -10,7 +10,7 @@ import (
 
 	"github.com/YakDriver/regexache"
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/redshift"
+	"github.com/aws/aws-sdk-go/service/redshiftserverless"
 	"github.com/hashicorp/aws-sdk-go-base/v2/awsv1shim/v2/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
