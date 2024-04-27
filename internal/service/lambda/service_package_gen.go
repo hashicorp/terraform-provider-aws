787b72ef57ce4c38bc690a916d44fd20735eac44
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-04-27 16:37:38 -0400

    d/aws_lambda_alias: Migrate to AWS SDK for Go v2.

--- internal/service/lambda/service_package_gen.go
+++ internal/service/lambda/service_package_gen.go
@@ -25,7 +25,7 @@ func (p *servicePackage) FrameworkResources(ctx context.Context) []*types.Servic
 func (p *servicePackage) SDKDataSources(ctx context.Context) []*types.ServicePackageSDKDataSource {
 	return []*types.ServicePackageSDKDataSource{
 		{
-			Factory:  DataSourceAlias,
+			Factory:  dataSourceAlias,
 			TypeName: "aws_lambda_alias",
 		},
 		{
