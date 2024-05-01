3f954442143aecf62396ef42b3094d889f77cd0d
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-05-01 11:34:39 -0400

    aws_fsx_openzfs_snapshot: Fixup acceptance test configurations.

--- internal/service/fsx/openzfs_snapshot_test.go
+++ internal/service/fsx/openzfs_snapshot_test.go
@@ -298,7 +298,7 @@ func testAccOpenZFSSnapshotConfig_base(rName string) string {
 	return acctest.ConfigCompose(acctest.ConfigVPCWithSubnets(rName, 1), fmt.Sprintf(`
 resource "aws_fsx_openzfs_file_system" "test" {
   storage_capacity    = 64
-  subnet_ids          = [aws_subnet.test1.id]
+  subnet_ids          = [aws_subnet.[0].id]
   deployment_type     = "SINGLE_AZ_1"
   throughput_capacity = 64
 
