6d5a506c0f99bdee3bf7ba00c322db1f3f906d8f
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-05-05 17:38:54 -0400

    listBackups: Migrate to AWS SDK for Go v2.

--- internal/service/dynamodb/generate.go
+++ internal/service/dynamodb/generate.go
@@ -4,6 +4,7 @@
 //go:generate go run ../../generate/tagresource/main.go
 //go:generate go run ../../generate/tags/main.go -AWSSDKVersion=2 -GetTag -ListTags -ListTagsOp=ListTagsOfResource -ServiceTagsSlice -UpdateTags -ParentNotFoundErrCode=ResourceNotFoundException
 //go:generate go run ../../generate/servicepackage/main.go
+//go:generate go run ../../generate/listpages/main.go -AWSSDKVersion=2 -ListOps=ListBackups -InputPaginator=ExclusiveStartBackupArn -OutputPaginator=LastEvaluatedBackupArn -- list_backups_pages_gen.go
 // ONLY generate directives and package declaration! Do not add anything else to this file.
 
 package dynamodb
