48b6f7894cc07618d65e61cb553276eddc4d9616
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-05-06 15:27:08 -0400

    cloudformation: Custom retryer.

--- internal/service/cloudformation/service_package_gen.go
+++ internal/service/cloudformation/service_package_gen.go
@@ -5,8 +5,6 @@ package cloudformation
 import (
 	"context"
 
-	aws_sdkv2 "github.com/aws/aws-sdk-go-v2/aws"
-	cloudformation_sdkv2 "github.com/aws/aws-sdk-go-v2/service/cloudformation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/types"
 	"github.com/hashicorp/terraform-provider-aws/names"
@@ -72,17 +70,6 @@ func (p *servicePackage) SDKResources(ctx context.Context) []*types.ServicePacka
 
 func (p *servicePackage) ServicePackageName() string {
 	return names.CloudFormation
-}
-
-// NewClient returns a new AWS SDK for Go v2 client for this service package's AWS API.
-func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (*cloudformation_sdkv2.Client, error) {
-	cfg := *(config["aws_sdkv2_config"].(*aws_sdkv2.Config))
-
-	return cloudformation_sdkv2.NewFromConfig(cfg, func(o *cloudformation_sdkv2.Options) {
-		if endpoint := config["endpoint"].(string); endpoint != "" {
-			o.BaseEndpoint = aws_sdkv2.String(endpoint)
-		}
-	}), nil
 }
 
 func ServicePackage(ctx context.Context) conns.ServicePackage {
