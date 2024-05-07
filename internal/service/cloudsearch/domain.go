239747ada84a811e67a9f230ebbad5f625f9a1b5
Dirk Avery <dirk.avery@gmail.com>
2024-05-07 14:10:05 -0400

    cloudsearch: Prefer constants over string

--- internal/service/cloudsearch/domain.go
+++ internal/service/cloudsearch/domain.go
@@ -24,6 +24,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
 	"github.com/hashicorp/terraform-provider-aws/internal/flex"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
+	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
 // @SDKResource("aws_cloudsearch_domain", name="Domain")
@@ -45,7 +46,7 @@ func resourceDomain() *schema.Resource {
 		},
 
 		Schema: map[string]*schema.Schema{
-			"arn": {
+			names.AttrARN: {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
@@ -102,7 +103,7 @@ func resourceDomain() *schema.Resource {
 							Optional: true,
 							Default:  false,
 						},
-						"name": {
+						names.AttrName: {
 							Type:         schema.TypeString,
 							Required:     true,
 							ValidateFunc: validateIndexName,
@@ -127,7 +128,7 @@ func resourceDomain() *schema.Resource {
 							Optional:     true,
 							ValidateFunc: validation.StringDoesNotMatch(regexache.MustCompile(`score`), "Cannot be set to reserved field score"),
 						},
-						"type": {
+						names.AttrType: {
 							Type:             schema.TypeString,
 							Required:         true,
 							ValidateDiagFunc: enum.Validate[types.IndexFieldType](),
@@ -140,7 +141,7 @@ func resourceDomain() *schema.Resource {
 				Optional: true,
 				Computed: true,
 			},
-			"name": {
+			names.AttrName: {
 				Type:         schema.TypeString,
 				Required:     true,
 				ForceNew:     true,
@@ -184,7 +185,7 @@ func resourceDomainCreate(ctx context.Context, d *schema.ResourceData, meta inte
 	var diags diag.Diagnostics
 	conn := meta.(*conns.AWSClient).CloudSearchClient(ctx)
 
-	name := d.Get("name").(string)
+	name := d.Get(names.AttrName).(string)
 	input := &cloudsearch.CreateDomainInput{
 		DomainName: aws.String(name),
 	}
@@ -278,14 +279,14 @@ func resourceDomainRead(ctx context.Context, d *schema.ResourceData, meta interf
 		return sdkdiag.AppendErrorf(diags, "reading CloudSearch Domain (%s): %s", d.Id(), err)
 	}
 
-	d.Set("arn", domain.ARN)
+	d.Set(names.AttrARN, domain.ARN)
 	if domain.DocService != nil {
 		d.Set("document_service_endpoint", domain.DocService.Endpoint)
 	} else {
 		d.Set("document_service_endpoint", nil)
 	}
 	d.Set("domain_id", domain.DomainId)
-	d.Set("name", domain.DomainName)
+	d.Set(names.AttrName, domain.DomainName)
 	if domain.SearchService != nil {
 		d.Set("search_service_endpoint", domain.SearchService.Endpoint)
 	} else {
@@ -321,7 +322,7 @@ func resourceDomainRead(ctx context.Context, d *schema.ResourceData, meta interf
 	}
 
 	indexResults, err := conn.DescribeIndexFields(ctx, &cloudsearch.DescribeIndexFieldsInput{
-		DomainName: aws.String(d.Get("name").(string)),
+		DomainName: aws.String(d.Get(names.AttrName).(string)),
 	})
 
 	if err != nil {
@@ -413,7 +414,7 @@ func resourceDomainUpdate(ctx context.Context, d *schema.ResourceData, meta inte
 				continue
 			}
 
-			fieldName, _ := tfMap["name"].(string)
+			fieldName, _ := tfMap[names.AttrName].(string)
 
 			if fieldName == "" {
 				continue
@@ -768,11 +769,11 @@ func expandIndexField(tfMap map[string]interface{}) (*types.IndexField, bool, er
 
 	apiObject := &types.IndexField{}
 
-	if v, ok := tfMap["name"].(string); ok && v != "" {
+	if v, ok := tfMap[names.AttrName].(string); ok && v != "" {
 		apiObject.IndexFieldName = aws.String(v)
 	}
 
-	if v, ok := tfMap["type"].(string); ok && v != "" {
+	if v, ok := tfMap[names.AttrType].(string); ok && v != "" {
 		apiObject.IndexFieldType = types.IndexFieldType(v)
 	}
 
@@ -1040,11 +1041,11 @@ func flattenIndexFieldStatus(apiObject types.IndexFieldStatus) (map[string]inter
 	tfMap := map[string]interface{}{}
 
 	if v := field.IndexFieldName; v != nil {
-		tfMap["name"] = aws.ToString(v)
+		tfMap[names.AttrName] = aws.ToString(v)
 	}
 
 	fieldType := field.IndexFieldType
-	tfMap["type"] = fieldType
+	tfMap[names.AttrType] = fieldType
 
 	switch fieldType {
 	case types.IndexFieldTypeDate:
