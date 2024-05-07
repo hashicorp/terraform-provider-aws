1f16d3fdaa00a4f079df1500d6ccb0a471758532
Dirk Avery <dirk.avery@gmail.com>
2024-05-07 10:40:15 -0400

    imagebuilder: Format after replacement

--- internal/service/imagebuilder/container_recipe_data_source.go
+++ internal/service/imagebuilder/container_recipe_data_source.go
@@ -14,6 +14,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
+	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
 // @SDKDataSource("aws_imagebuilder_container_recipe")
