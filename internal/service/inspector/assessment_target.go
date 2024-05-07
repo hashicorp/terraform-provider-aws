fdd9df13e6b134621348d495f5155c2009bfb366
Dirk Avery <dirk.avery@gmail.com>
2024-05-07 10:40:15 -0400

    inspector: Format after replacement

--- internal/service/inspector/assessment_target.go
+++ internal/service/inspector/assessment_target.go
@@ -17,6 +17,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
+	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
 // @SDKResource("aws_inspector_assessment_target")
