da64f9ca089dcd995e32416e59a8e8af768b2de0
Dirk Avery <dirk.avery@gmail.com>
2024-05-08 18:12:50 -0400

    dms: Format after const replacement

--- internal/service/dms/replication_config.go
+++ internal/service/dms/replication_config.go
@@ -627,7 +627,7 @@ func flattenComputeConfig(apiObject *dms.ComputeConfig) []interface{} {
 	}
 
 	tfMap := map[string]interface{}{
-		names.AttrAvailabilityZone:            aws.StringValue(apiObject.AvailabilityZone),
+		names.AttrAvailabilityZone:     aws.StringValue(apiObject.AvailabilityZone),
 		"dns_name_servers":             aws.StringValue(apiObject.DnsNameServers),
 		names.AttrKMSKeyID:             aws.StringValue(apiObject.KmsKeyId),
 		"max_capacity_units":           aws.Int64Value(apiObject.MaxCapacityUnits),
