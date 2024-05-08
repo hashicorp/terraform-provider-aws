30c4ce8c41d067f42d039c27ce6974e35cb439a1
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-05-08 15:03:56 -0400

    Add tool to display AWS SDK for Go v1 vs. v2 retry delays.

---
+++ tools/awssdkretries/main.go
@@ -0,0 +1,43 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws/retry"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/client"
+	"github.com/aws/aws-sdk-go/aws/request"
+)
+
+func main() {
+	maxBackoff := 300 * time.Second
+	maxRetries := 25
+	v2 := retry.NewExponentialJitterBackoff(maxBackoff)
+	v1 := client.DefaultRetryer{
+		NumMaxRetries:    maxRetries,
+		MinRetryDelay:    client.DefaultRetryerMinRetryDelay,
+		MinThrottleDelay: client.DefaultRetryerMinThrottleDelay,
+		MaxRetryDelay:    maxBackoff,
+		MaxThrottleDelay: client.DefaultRetryerMaxThrottleDelay,
+	}
+
+	err := awserr.New("ThrottlingException", "Rate exceeded", nil)
+	req := request.Request{
+		Error: err,
+		HTTPResponse: &http.Response{
+			StatusCode: 400,
+		},
+	}
+	for i := 0; i < maxRetries; i++ {
+		d1 := v1.RetryRules(&req)
+		req.RetryCount++
+		d2, _ := v2.BackoffDelay(i, err)
+
+		fmt.Printf("%d v1: %s, v2: %s\n", i, d1.String(), d2.String())
+	}
+}
