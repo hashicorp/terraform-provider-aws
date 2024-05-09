0b878415350aa2c5da08cc31c8881e90dba3586e
Dirk Avery <dirk.avery@gmail.com>
2024-05-09 16:36:45 -0400

    dlm: Use constant for endpoint strings

--- internal/service/dlm/service_package_gen.go
+++ internal/service/dlm/service_package_gen.go
@@ -48,7 +48,7 @@ func (p *servicePackage) ServicePackageName() string {
 func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*dlm_sdkv1.DLM, error) {
 	sess := config["session"].(*session_sdkv1.Session)
 
-	return dlm_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config["endpoint"].(string))})), nil
+	return dlm_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config[names.AttrEndpoint].(string))})), nil
 }
 
 func ServicePackage(ctx context.Context) conns.ServicePackage {
