97d8abe8072f1a708f742481af395bdc646026e9
Dirk Avery <dirk.avery@gmail.com>
2024-05-09 16:36:30 -0400

    appautoscaling: Use constant for endpoint strings

--- internal/service/appautoscaling/service_package_gen.go
+++ internal/service/appautoscaling/service_package_gen.go
@@ -57,7 +57,7 @@ func (p *servicePackage) ServicePackageName() string {
 func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*applicationautoscaling_sdkv1.ApplicationAutoScaling, error) {
 	sess := config["session"].(*session_sdkv1.Session)
 
-	return applicationautoscaling_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config["endpoint"].(string))})), nil
+	return applicationautoscaling_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config[names.AttrEndpoint].(string))})), nil
 }
 
 func ServicePackage(ctx context.Context) conns.ServicePackage {
