d7a3f31de072f7fe813a3385ab9c57672750182f
Daniel Rieske <daniel.rieske@gmail.com>
2024-05-09 20:03:49 +0200

    chore: upgrade lambda runtime

--- internal/service/ec2/vpnclient_endpoint_test.go
+++ internal/service/ec2/vpnclient_endpoint_test.go
@@ -821,7 +821,7 @@ resource "aws_lambda_function" "test1" {
   function_name = "AWSClientVPN-%[1]s-1"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "index.handler"
-  runtime       = "nodejs14.x"
+  runtime       = "nodejs20.x"
 }
 
 resource "aws_lambda_function" "test2" {
@@ -829,7 +829,7 @@ resource "aws_lambda_function" "test2" {
   function_name = "AWSClientVPN-%[1]s-2"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "index.handler"
-  runtime       = "nodejs14.x"
+  runtime       = "nodejs20.x"
 }
 
 locals {
