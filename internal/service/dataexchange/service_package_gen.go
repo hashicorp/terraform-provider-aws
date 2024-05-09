655a8cc75e26106e83728c91d869b9d6749ded08
Dirk Avery <dirk.avery@gmail.com>
2024-05-09 16:36:43 -0400

    dataexchange: Use constant for endpoint strings

--- internal/service/dataexchange/service_package_gen.go
+++ internal/service/dataexchange/service_package_gen.go
@@ -56,7 +56,7 @@ func (p *servicePackage) ServicePackageName() string {
 func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*dataexchange_sdkv1.DataExchange, error) {
 	sess := config["session"].(*session_sdkv1.Session)
 
-	return dataexchange_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config["endpoint"].(string))})), nil
+	return dataexchange_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config[names.AttrEndpoint].(string))})), nil
 }
 
 func ServicePackage(ctx context.Context) conns.ServicePackage {
