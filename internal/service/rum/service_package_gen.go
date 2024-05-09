3539bab3dbe4b0d5956683603f666bb46917bc38
Dirk Avery <dirk.avery@gmail.com>
2024-05-09 16:37:02 -0400

    rum: Use constant for endpoint strings

--- internal/service/rum/service_package_gen.go
+++ internal/service/rum/service_package_gen.go
@@ -52,7 +52,7 @@ func (p *servicePackage) ServicePackageName() string {
 func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*cloudwatchrum_sdkv1.CloudWatchRUM, error) {
 	sess := config["session"].(*session_sdkv1.Session)
 
-	return cloudwatchrum_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config["endpoint"].(string))})), nil
+	return cloudwatchrum_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config[names.AttrEndpoint].(string))})), nil
 }
 
 func ServicePackage(ctx context.Context) conns.ServicePackage {
