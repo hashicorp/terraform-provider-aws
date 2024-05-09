3565e35435be1b26dc6e48c8f30a8889e90ec0ce
Dirk Avery <dirk.avery@gmail.com>
2024-05-09 16:57:32 -0400

    pinpoint: Use constants for strings

--- internal/service/pinpoint/service_package_gen.go
+++ internal/service/pinpoint/service_package_gen.go
@@ -86,7 +86,7 @@ func (p *servicePackage) ServicePackageName() string {
 
 // NewConn returns a new AWS SDK for Go v1 client for this service package's AWS API.
 func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*pinpoint_sdkv1.Pinpoint, error) {
-	sess := config["session"].(*session_sdkv1.Session)
+	sess := config[names.AttrSession].(*session_sdkv1.Session)
 
 	return pinpoint_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config[names.AttrEndpoint].(string))})), nil
 }
