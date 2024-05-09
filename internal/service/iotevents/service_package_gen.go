e26ac3e42f73d69869b5b4a5b34f3c26640b60c7
Dirk Avery <dirk.avery@gmail.com>
2024-05-09 16:36:51 -0400

    iotevents: Use constant for endpoint strings

--- internal/service/iotevents/service_package_gen.go
+++ internal/service/iotevents/service_package_gen.go
@@ -39,7 +39,7 @@ func (p *servicePackage) ServicePackageName() string {
 func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*iotevents_sdkv1.IoTEvents, error) {
 	sess := config["session"].(*session_sdkv1.Session)
 
-	return iotevents_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config["endpoint"].(string))})), nil
+	return iotevents_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config[names.AttrEndpoint].(string))})), nil
 }
 
 func ServicePackage(ctx context.Context) conns.ServicePackage {
