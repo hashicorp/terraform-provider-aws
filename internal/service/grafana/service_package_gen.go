21157024904e0df9cad03a88d71d150a8d1f698b
Dirk Avery <dirk.avery@gmail.com>
2024-05-09 16:36:49 -0400

    grafana: Use constant for endpoint strings

--- internal/service/grafana/service_package_gen.go
+++ internal/service/grafana/service_package_gen.go
@@ -69,7 +69,7 @@ func (p *servicePackage) ServicePackageName() string {
 func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*managedgrafana_sdkv1.ManagedGrafana, error) {
 	sess := config["session"].(*session_sdkv1.Session)
 
-	return managedgrafana_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config["endpoint"].(string))})), nil
+	return managedgrafana_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config[names.AttrEndpoint].(string))})), nil
 }
 
 func ServicePackage(ctx context.Context) conns.ServicePackage {
