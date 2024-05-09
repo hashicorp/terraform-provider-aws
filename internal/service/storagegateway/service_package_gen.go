514fcef0ad32ab2fc551205a1bd30d82599543b1
Dirk Avery <dirk.avery@gmail.com>
2024-05-09 16:37:06 -0400

    storagegateway: Use constant for endpoint strings

--- internal/service/storagegateway/service_package_gen.go
+++ internal/service/storagegateway/service_package_gen.go
@@ -113,7 +113,7 @@ func (p *servicePackage) ServicePackageName() string {
 func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*storagegateway_sdkv1.StorageGateway, error) {
 	sess := config["session"].(*session_sdkv1.Session)
 
-	return storagegateway_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config["endpoint"].(string))})), nil
+	return storagegateway_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config[names.AttrEndpoint].(string))})), nil
 }
 
 func ServicePackage(ctx context.Context) conns.ServicePackage {
