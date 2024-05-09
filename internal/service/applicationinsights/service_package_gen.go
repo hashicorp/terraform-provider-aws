3b270762ec37a369528c387ee80c519fce02a00b
Dirk Avery <dirk.avery@gmail.com>
2024-05-09 16:36:31 -0400

    applicationinsights: Use constant for endpoint strings

--- internal/service/applicationinsights/service_package_gen.go
+++ internal/service/applicationinsights/service_package_gen.go
@@ -48,7 +48,7 @@ func (p *servicePackage) ServicePackageName() string {
 func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*applicationinsights_sdkv1.ApplicationInsights, error) {
 	sess := config["session"].(*session_sdkv1.Session)
 
-	return applicationinsights_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config["endpoint"].(string))})), nil
+	return applicationinsights_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config[names.AttrEndpoint].(string))})), nil
 }
 
 func ServicePackage(ctx context.Context) conns.ServicePackage {
