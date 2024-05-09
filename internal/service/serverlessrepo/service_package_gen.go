e3d59ed55328eba314b89f7244ad9fd697ce8e1c
Dirk Avery <dirk.avery@gmail.com>
2024-05-09 16:37:03 -0400

    serverlessrepo: Use constant for endpoint strings

--- internal/service/serverlessrepo/service_package_gen.go
+++ internal/service/serverlessrepo/service_package_gen.go
@@ -51,7 +51,7 @@ func (p *servicePackage) ServicePackageName() string {
 func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*serverlessapplicationrepository_sdkv1.ServerlessApplicationRepository, error) {
 	sess := config["session"].(*session_sdkv1.Session)
 
-	return serverlessapplicationrepository_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config["endpoint"].(string))})), nil
+	return serverlessapplicationrepository_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config[names.AttrEndpoint].(string))})), nil
 }
 
 func ServicePackage(ctx context.Context) conns.ServicePackage {
