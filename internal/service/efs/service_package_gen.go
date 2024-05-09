3c1c9893e7663d4f3892cd0273ffb241c37fe8a5
Dirk Avery <dirk.avery@gmail.com>
2024-05-09 16:36:46 -0400

    efs: Use constant for endpoint strings

--- internal/service/efs/service_package_gen.go
+++ internal/service/efs/service_package_gen.go
@@ -91,7 +91,7 @@ func (p *servicePackage) ServicePackageName() string {
 func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*efs_sdkv1.EFS, error) {
 	sess := config["session"].(*session_sdkv1.Session)
 
-	return efs_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config["endpoint"].(string))})), nil
+	return efs_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config[names.AttrEndpoint].(string))})), nil
 }
 
 func ServicePackage(ctx context.Context) conns.ServicePackage {
