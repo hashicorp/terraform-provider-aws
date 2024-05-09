d0a8e7194ee61902ce924414dd915f0ca1d41a48
Dirk Avery <dirk.avery@gmail.com>
2024-05-09 16:36:47 -0400

    emr: Use constant for endpoint strings

--- internal/service/emr/service_package_gen.go
+++ internal/service/emr/service_package_gen.go
@@ -99,7 +99,7 @@ func (p *servicePackage) ServicePackageName() string {
 func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*emr_sdkv1.EMR, error) {
 	sess := config["session"].(*session_sdkv1.Session)
 
-	return emr_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config["endpoint"].(string))})), nil
+	return emr_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config[names.AttrEndpoint].(string))})), nil
 }
 
 // NewClient returns a new AWS SDK for Go v2 client for this service package's AWS API.
@@ -107,7 +107,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws_sdkv2.Config))
 
 	return emr_sdkv2.NewFromConfig(cfg, func(o *emr_sdkv2.Options) {
-		if endpoint := config["endpoint"].(string); endpoint != "" {
+		if endpoint := config[names.AttrEndpoint].(string); endpoint != "" {
 			o.BaseEndpoint = aws_sdkv2.String(endpoint)
 		}
 	}), nil
