d5ef0c63aae58f755cef24530984cdda588ba3b9
Dirk Avery <dirk.avery@gmail.com>
2024-05-09 16:57:30 -0400

    licensemanager: Use constants for strings

--- internal/service/licensemanager/service_package_gen.go
+++ internal/service/licensemanager/service_package_gen.go
@@ -71,7 +71,7 @@ func (p *servicePackage) ServicePackageName() string {
 
 // NewConn returns a new AWS SDK for Go v1 client for this service package's AWS API.
 func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*licensemanager_sdkv1.LicenseManager, error) {
-	sess := config["session"].(*session_sdkv1.Session)
+	sess := config[names.AttrSession].(*session_sdkv1.Session)
 
 	return licensemanager_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config[names.AttrEndpoint].(string))})), nil
 }
