a252c857eefb4526a161afdc40744316435d80e2
Dirk Avery <dirk.avery@gmail.com>
2024-05-09 16:36:32 -0400

    appstream: Use constant for endpoint strings

--- internal/service/appstream/service_package_gen.go
+++ internal/service/appstream/service_package_gen.go
@@ -80,7 +80,7 @@ func (p *servicePackage) ServicePackageName() string {
 func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*appstream_sdkv1.AppStream, error) {
 	sess := config["session"].(*session_sdkv1.Session)
 
-	return appstream_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config["endpoint"].(string))})), nil
+	return appstream_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config[names.AttrEndpoint].(string))})), nil
 }
 
 func ServicePackage(ctx context.Context) conns.ServicePackage {
