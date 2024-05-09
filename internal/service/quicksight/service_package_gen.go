3032808b0443f66eebfdf0ab7dc46a6a01e0f6a5
Dirk Avery <dirk.avery@gmail.com>
2024-05-09 16:36:59 -0400

    quicksight: Use constant for endpoint strings

--- internal/service/quicksight/service_package_gen.go
+++ internal/service/quicksight/service_package_gen.go
@@ -172,7 +172,7 @@ func (p *servicePackage) ServicePackageName() string {
 func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*quicksight_sdkv1.QuickSight, error) {
 	sess := config["session"].(*session_sdkv1.Session)
 
-	return quicksight_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config["endpoint"].(string))})), nil
+	return quicksight_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config[names.AttrEndpoint].(string))})), nil
 }
 
 func ServicePackage(ctx context.Context) conns.ServicePackage {
