e01c313e7d86ced711ef34b943e92a3c841ae31d
Dirk Avery <dirk.avery@gmail.com>
2024-05-09 16:57:29 -0400

    inspector: Use constants for strings

--- internal/service/inspector/service_package_gen.go
+++ internal/service/inspector/service_package_gen.go
@@ -59,7 +59,7 @@ func (p *servicePackage) ServicePackageName() string {
 
 // NewConn returns a new AWS SDK for Go v1 client for this service package's AWS API.
 func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*inspector_sdkv1.Inspector, error) {
-	sess := config["session"].(*session_sdkv1.Session)
+	sess := config[names.AttrSession].(*session_sdkv1.Session)
 
 	return inspector_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config[names.AttrEndpoint].(string))})), nil
 }
