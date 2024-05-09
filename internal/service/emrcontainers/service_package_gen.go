701f1c0dceca7ef77d50c3ebf33414f06f03cb60
Dirk Avery <dirk.avery@gmail.com>
2024-05-09 16:36:48 -0400

    emrcontainers: Use constant for endpoint strings

--- internal/service/emrcontainers/service_package_gen.go
+++ internal/service/emrcontainers/service_package_gen.go
@@ -61,7 +61,7 @@ func (p *servicePackage) ServicePackageName() string {
 func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*emrcontainers_sdkv1.EMRContainers, error) {
 	sess := config["session"].(*session_sdkv1.Session)
 
-	return emrcontainers_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config["endpoint"].(string))})), nil
+	return emrcontainers_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config[names.AttrEndpoint].(string))})), nil
 }
 
 func ServicePackage(ctx context.Context) conns.ServicePackage {
