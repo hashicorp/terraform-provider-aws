cf2124271802b017733a4a8f901b608617132054
Dirk Avery <dirk.avery@gmail.com>
2024-05-09 16:36:49 -0400

    glue: Use constant for endpoint strings

--- internal/service/glue/service_package_gen.go
+++ internal/service/glue/service_package_gen.go
@@ -177,7 +177,7 @@ func (p *servicePackage) ServicePackageName() string {
 func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*glue_sdkv1.Glue, error) {
 	sess := config["session"].(*session_sdkv1.Session)
 
-	return glue_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config["endpoint"].(string))})), nil
+	return glue_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config[names.AttrEndpoint].(string))})), nil
 }
 
 func ServicePackage(ctx context.Context) conns.ServicePackage {
