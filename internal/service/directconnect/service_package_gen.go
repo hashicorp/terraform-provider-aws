29c0150111b11ef838109b1a88445e4970150513
Dirk Avery <dirk.avery@gmail.com>
2024-05-09 16:57:26 -0400

    directconnect: Use constants for strings

--- internal/service/directconnect/service_package_gen.go
+++ internal/service/directconnect/service_package_gen.go
@@ -167,7 +167,7 @@ func (p *servicePackage) ServicePackageName() string {
 
 // NewConn returns a new AWS SDK for Go v1 client for this service package's AWS API.
 func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*directconnect_sdkv1.DirectConnect, error) {
-	sess := config["session"].(*session_sdkv1.Session)
+	sess := config[names.AttrSession].(*session_sdkv1.Session)
 
 	return directconnect_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config[names.AttrEndpoint].(string))})), nil
 }
