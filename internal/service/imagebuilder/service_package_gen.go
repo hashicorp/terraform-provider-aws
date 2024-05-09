dc9a0807e36b22c9fad9ce4825f14a14c7d5eb5c
Dirk Avery <dirk.avery@gmail.com>
2024-05-09 16:36:50 -0400

    imagebuilder: Use constant for endpoint strings

--- internal/service/imagebuilder/service_package_gen.go
+++ internal/service/imagebuilder/service_package_gen.go
@@ -160,7 +160,7 @@ func (p *servicePackage) ServicePackageName() string {
 func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*imagebuilder_sdkv1.Imagebuilder, error) {
 	sess := config["session"].(*session_sdkv1.Session)
 
-	return imagebuilder_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config["endpoint"].(string))})), nil
+	return imagebuilder_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config[names.AttrEndpoint].(string))})), nil
 }
 
 func ServicePackage(ctx context.Context) conns.ServicePackage {
