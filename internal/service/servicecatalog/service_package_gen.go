ca1302ab2e9fd6c1d52d9001e2ca59d44234c60a
Dirk Avery <dirk.avery@gmail.com>
2024-05-09 16:37:03 -0400

    servicecatalog: Use constant for endpoint strings

--- internal/service/servicecatalog/service_package_gen.go
+++ internal/service/servicecatalog/service_package_gen.go
@@ -123,7 +123,7 @@ func (p *servicePackage) ServicePackageName() string {
 func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*servicecatalog_sdkv1.ServiceCatalog, error) {
 	sess := config["session"].(*session_sdkv1.Session)
 
-	return servicecatalog_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config["endpoint"].(string))})), nil
+	return servicecatalog_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config[names.AttrEndpoint].(string))})), nil
 }
 
 func ServicePackage(ctx context.Context) conns.ServicePackage {
