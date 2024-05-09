5ac04b94ad79bd7bd4e28388fb927df845591d1a
Dirk Avery <dirk.avery@gmail.com>
2024-05-09 16:57:32 -0400

    redshift: Use constants for strings

--- internal/service/redshift/service_package_gen.go
+++ internal/service/redshift/service_package_gen.go
@@ -214,7 +214,7 @@ func (p *servicePackage) ServicePackageName() string {
 
 // NewConn returns a new AWS SDK for Go v1 client for this service package's AWS API.
 func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*redshift_sdkv1.Redshift, error) {
-	sess := config["session"].(*session_sdkv1.Session)
+	sess := config[names.AttrSession].(*session_sdkv1.Session)
 
 	return redshift_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config[names.AttrEndpoint].(string))})), nil
 }
