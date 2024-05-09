0814135825f1376c55a17d63e35e5eb0c1e638e2
Dirk Avery <dirk.avery@gmail.com>
2024-05-09 16:36:43 -0400

    datapipeline: Use constant for endpoint strings

--- internal/service/datapipeline/service_package_gen.go
+++ internal/service/datapipeline/service_package_gen.go
@@ -61,7 +61,7 @@ func (p *servicePackage) ServicePackageName() string {
 func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*datapipeline_sdkv1.DataPipeline, error) {
 	sess := config["session"].(*session_sdkv1.Session)
 
-	return datapipeline_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config["endpoint"].(string))})), nil
+	return datapipeline_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config[names.AttrEndpoint].(string))})), nil
 }
 
 func ServicePackage(ctx context.Context) conns.ServicePackage {
