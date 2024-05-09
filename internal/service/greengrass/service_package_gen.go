3b6c4d7426a7645f3392428c2cbb7327b5a5af71
Dirk Avery <dirk.avery@gmail.com>
2024-05-09 16:36:50 -0400

    greengrass: Use constant for endpoint strings

--- internal/service/greengrass/service_package_gen.go
+++ internal/service/greengrass/service_package_gen.go
@@ -39,7 +39,7 @@ func (p *servicePackage) ServicePackageName() string {
 func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*greengrass_sdkv1.Greengrass, error) {
 	sess := config["session"].(*session_sdkv1.Session)
 
-	return greengrass_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config["endpoint"].(string))})), nil
+	return greengrass_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config[names.AttrEndpoint].(string))})), nil
 }
 
 func ServicePackage(ctx context.Context) conns.ServicePackage {
