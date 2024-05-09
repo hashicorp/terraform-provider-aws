77b5011b156a8b658f4f124f79af9a09d2204990
Dirk Avery <dirk.avery@gmail.com>
2024-05-09 16:37:02 -0400

    s3outposts: Use constant for endpoint strings

--- internal/service/s3outposts/service_package_gen.go
+++ internal/service/s3outposts/service_package_gen.go
@@ -44,7 +44,7 @@ func (p *servicePackage) ServicePackageName() string {
 func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*s3outposts_sdkv1.S3Outposts, error) {
 	sess := config["session"].(*session_sdkv1.Session)
 
-	return s3outposts_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config["endpoint"].(string))})), nil
+	return s3outposts_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config[names.AttrEndpoint].(string))})), nil
 }
 
 func ServicePackage(ctx context.Context) conns.ServicePackage {
