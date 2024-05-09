0f7aa386de487d9de85e213f6ae23b56b9b49cf8
Dirk Avery <dirk.avery@gmail.com>
2024-05-09 16:57:27 -0400

    ecs: Use constants for strings

--- internal/service/ecs/service_package_gen.go
+++ internal/service/ecs/service_package_gen.go
@@ -115,7 +115,7 @@ func (p *servicePackage) ServicePackageName() string {
 
 // NewConn returns a new AWS SDK for Go v1 client for this service package's AWS API.
 func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*ecs_sdkv1.ECS, error) {
-	sess := config["session"].(*session_sdkv1.Session)
+	sess := config[names.AttrSession].(*session_sdkv1.Session)
 
 	return ecs_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config[names.AttrEndpoint].(string))})), nil
 }
