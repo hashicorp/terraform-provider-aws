0b1850d5c06409e02ece3a6407dfac54784e5771
Dirk Avery <dirk.avery@gmail.com>
2024-05-09 16:37:07 -0400

    waf: Use constant for endpoint strings

--- internal/service/waf/service_package_gen.go
+++ internal/service/waf/service_package_gen.go
@@ -125,7 +125,7 @@ func (p *servicePackage) ServicePackageName() string {
 func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*waf_sdkv1.WAF, error) {
 	sess := config["session"].(*session_sdkv1.Session)
 
-	return waf_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config["endpoint"].(string))})), nil
+	return waf_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config[names.AttrEndpoint].(string))})), nil
 }
 
 func ServicePackage(ctx context.Context) conns.ServicePackage {
