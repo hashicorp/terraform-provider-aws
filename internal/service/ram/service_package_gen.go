e84400342ae5f6a9b97d3ff926b8b30f05c6e128
Dirk Avery <dirk.avery@gmail.com>
2024-05-09 16:36:59 -0400

    ram: Use constant for endpoint strings

--- internal/service/ram/service_package_gen.go
+++ internal/service/ram/service_package_gen.go
@@ -75,7 +75,7 @@ func (p *servicePackage) ServicePackageName() string {
 func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*ram_sdkv1.RAM, error) {
 	sess := config["session"].(*session_sdkv1.Session)
 
-	return ram_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config["endpoint"].(string))})), nil
+	return ram_sdkv1.New(sess.Copy(&aws_sdkv1.Config{Endpoint: aws_sdkv1.String(config[names.AttrEndpoint].(string))})), nil
 }
 
 func ServicePackage(ctx context.Context) conns.ServicePackage {
