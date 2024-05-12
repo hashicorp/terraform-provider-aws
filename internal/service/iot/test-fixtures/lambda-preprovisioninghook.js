f99fd2745ed4205b2bcd64cabb15660fab5227f3
Daniel Rieske <daniel.rieske@gmail.com>
2024-05-12 13:56:28 +0200

    fix: added copyright headers

--- internal/service/iot/test-fixtures/lambda-preprovisioninghook.js
+++ internal/service/iot/test-fixtures/lambda-preprovisioninghook.js
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 //https://docs.aws.amazon.com/iot/latest/developerguide/pre-provisioning-hook.html#pre-provisioning-example
 exports.handler = function (event, context, callback) {
     console.log(JSON.stringify(event, null, 2));
