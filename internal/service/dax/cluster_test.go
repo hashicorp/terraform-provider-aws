d56feb991803ae6d1c286b7e8dbb445af5d6425f
Dirk Avery <dirk.avery@gmail.com>
2024-05-13 17:23:22 -0400

    dax: Use constants for strings

--- internal/service/dax/cluster_test.go
+++ internal/service/dax/cluster_test.go
@@ -45,7 +45,7 @@ func TestAccDAXCluster_basic(t *testing.T) {
 						resourceName, "cluster_endpoint_encryption_type", "NONE"),
 					resource.TestMatchResourceAttr(
 						resourceName, "cluster_name", regexache.MustCompile(`^tf-\w+$`)),
-					resource.TestCheckResourceAttrPair(resourceName, "iam_role_arn", iamRoleResourceName, names.AttrARN),
+					resource.TestCheckResourceAttrPair(resourceName, names.AttrIAMRoleARN, iamRoleResourceName, names.AttrARN),
 					resource.TestCheckResourceAttr(
 						resourceName, "node_type", "dax.t3.small"),
 					resource.TestCheckResourceAttr(
