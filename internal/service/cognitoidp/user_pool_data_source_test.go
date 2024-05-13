c5d4bed5dda430d3a64fac647382f0278f492101
Daniel Quackenbush <25692880+danquack@users.noreply.github.com>
2024-05-13 13:28:47 -0400

    remove extra whitespace

--- internal/service/cognitoidp/user_pool_data_source_test.go
+++ internal/service/cognitoidp/user_pool_data_source_test.go
@@ -14,7 +14,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
-
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
