7bd226e1a9370e9d12c6d1ccd8df4cee653ca69f
Dirk Avery <dirk.avery@gmail.com>
2024-05-14 00:47:01 -0400

    guardduty: Use constants for test strings

--- internal/service/guardduty/ipset_test.go
+++ internal/service/guardduty/ipset_test.go
@@ -86,7 +86,7 @@ func testAccIPSet_tags(t *testing.T) {
 				Config: testAccIPSetConfig_tags1(rName, "key1", "value1"),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckIPSetExists(ctx, resourceName),
-					resource.TestCheckResourceAttr(resourceName, "tags.%", "1"),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", acctest.CtOne),
 					resource.TestCheckResourceAttr(resourceName, "tags.key1", "value1"),
 				),
 			},
@@ -108,7 +108,7 @@ func testAccIPSet_tags(t *testing.T) {
 				Config: testAccIPSetConfig_tags1(rName, "key2", "value2"),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckIPSetExists(ctx, resourceName),
-					resource.TestCheckResourceAttr(resourceName, "tags.%", "1"),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", acctest.CtOne),
 					resource.TestCheckResourceAttr(resourceName, "tags.key2", "value2"),
 				),
 			},
