0746942bd4f33a878e59094909a9443a44e5d7ae
Dirk Avery <dirk.avery@gmail.com>
2024-05-14 16:55:28 -0400

    amplify: Use constants for test strings

--- internal/service/amplify/branch_test.go
+++ internal/service/amplify/branch_test.go
@@ -106,11 +106,11 @@ func testAccBranch_tags(t *testing.T) {
 		CheckDestroy:             testAccCheckBranchDestroy(ctx),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccBranchConfig_tags1(rName, "key1", "value1"),
+				Config: testAccBranchConfig_tags1(rName, acctest.CtKey1, acctest.CtValue1),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckBranchExists(ctx, resourceName, &branch),
 					resource.TestCheckResourceAttr(resourceName, acctest.CtTagsPercent, acctest.CtOne),
-					resource.TestCheckResourceAttr(resourceName, "tags.key1", "value1"),
+					resource.TestCheckResourceAttr(resourceName, "tags.key1", acctest.CtValue1),
 				),
 			},
 			{
@@ -119,20 +119,20 @@ func testAccBranch_tags(t *testing.T) {
 				ImportStateVerify: true,
 			},
 			{
-				Config: testAccBranchConfig_tags2(rName, "key1", "value1updated", "key2", "value2"),
+				Config: testAccBranchConfig_tags2(rName, acctest.CtKey1, "value1updated", acctest.CtKey2, acctest.CtValue2),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckBranchExists(ctx, resourceName, &branch),
 					resource.TestCheckResourceAttr(resourceName, acctest.CtTagsPercent, acctest.CtTwo),
 					resource.TestCheckResourceAttr(resourceName, "tags.key1", "value1updated"),
-					resource.TestCheckResourceAttr(resourceName, "tags.key2", "value2"),
+					resource.TestCheckResourceAttr(resourceName, "tags.key2", acctest.CtValue2),
 				),
 			},
 			{
-				Config: testAccBranchConfig_tags1(rName, "key2", "value2"),
+				Config: testAccBranchConfig_tags1(rName, acctest.CtKey2, acctest.CtValue2),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckBranchExists(ctx, resourceName, &branch),
 					resource.TestCheckResourceAttr(resourceName, acctest.CtTagsPercent, acctest.CtOne),
-					resource.TestCheckResourceAttr(resourceName, "tags.key2", "value2"),
+					resource.TestCheckResourceAttr(resourceName, "tags.key2", acctest.CtValue2),
 				),
 			},
 		},
