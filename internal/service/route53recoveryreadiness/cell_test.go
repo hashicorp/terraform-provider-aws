93ce8fa024166623d22fefe87e6f6e241de45d0f
Dirk Avery <dirk.avery@gmail.com>
2024-05-14 16:55:53 -0400

    route53recoveryreadiness: Use constants for test strings

--- internal/service/route53recoveryreadiness/cell_test.go
+++ internal/service/route53recoveryreadiness/cell_test.go
@@ -137,11 +137,11 @@ func TestAccRoute53RecoveryReadinessCell_tags(t *testing.T) {
 		CheckDestroy:             testAccCheckCellDestroy(ctx),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCellConfig_tags1(rName, "key1", "value1"),
+				Config: testAccCellConfig_tags1(rName, acctest.CtKey1, acctest.CtValue1),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckCellExists(ctx, resourceName),
 					resource.TestCheckResourceAttr(resourceName, acctest.CtTagsPercent, acctest.CtOne),
-					resource.TestCheckResourceAttr(resourceName, "tags.key1", "value1"),
+					resource.TestCheckResourceAttr(resourceName, "tags.key1", acctest.CtValue1),
 				),
 			},
 			{
@@ -150,20 +150,20 @@ func TestAccRoute53RecoveryReadinessCell_tags(t *testing.T) {
 				ImportStateVerify: true,
 			},
 			{
-				Config: testAccCellConfig_tags2(rName, "key1", "value1updated", "key2", "value2"),
+				Config: testAccCellConfig_tags2(rName, acctest.CtKey1, "value1updated", acctest.CtKey2, acctest.CtValue2),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckCellExists(ctx, resourceName),
 					resource.TestCheckResourceAttr(resourceName, acctest.CtTagsPercent, acctest.CtTwo),
 					resource.TestCheckResourceAttr(resourceName, "tags.key1", "value1updated"),
-					resource.TestCheckResourceAttr(resourceName, "tags.key2", "value2"),
+					resource.TestCheckResourceAttr(resourceName, "tags.key2", acctest.CtValue2),
 				),
 			},
 			{
-				Config: testAccCellConfig_tags1(rName, "key2", "value2"),
+				Config: testAccCellConfig_tags1(rName, acctest.CtKey2, acctest.CtValue2),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckCellExists(ctx, resourceName),
 					resource.TestCheckResourceAttr(resourceName, acctest.CtTagsPercent, acctest.CtOne),
-					resource.TestCheckResourceAttr(resourceName, "tags.key2", "value2"),
+					resource.TestCheckResourceAttr(resourceName, "tags.key2", acctest.CtValue2),
 				),
 			},
 		},
