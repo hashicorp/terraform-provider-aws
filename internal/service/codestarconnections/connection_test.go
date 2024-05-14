e627b29ca38bc3fc5745935b6e7da35c8a4e0c34
Dirk Avery <dirk.avery@gmail.com>
2024-05-14 00:46:47 -0400

    codestarconnections: Use constants for test strings

--- internal/service/codestarconnections/connection_test.go
+++ internal/service/codestarconnections/connection_test.go
@@ -137,7 +137,7 @@ func TestAccCodeStarConnectionsConnection_tags(t *testing.T) {
 				Config: testAccConnectionConfig_tags1(rName, "key1", "value1"),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					testAccCheckConnectionExists(ctx, resourceName, &v),
-					resource.TestCheckResourceAttr(resourceName, "tags.%", "1"),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", acctest.CtOne),
 					resource.TestCheckResourceAttr(resourceName, "tags.key1", "value1"),
 				),
 			},
@@ -159,7 +159,7 @@ func TestAccCodeStarConnectionsConnection_tags(t *testing.T) {
 				Config: testAccConnectionConfig_tags1(rName, "key2", "value2"),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					testAccCheckConnectionExists(ctx, resourceName, &v),
-					resource.TestCheckResourceAttr(resourceName, "tags.%", "1"),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", acctest.CtOne),
 					resource.TestCheckResourceAttr(resourceName, "tags.key2", "value2"),
 				),
 			},
