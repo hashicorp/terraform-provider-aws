8cc33d4eb85620b08bdda3375a1abdbfadbd3007
Dirk Avery <dirk.avery@gmail.com>
2024-05-14 01:55:50 -0400

    amplify: Use constants for test strings

--- internal/service/amplify/domain_association_test.go
+++ internal/service/amplify/domain_association_test.go
@@ -140,7 +140,7 @@ func testAccDomainAssociation_update(t *testing.T) {
 					acctest.MatchResourceAttrRegionalARN(resourceName, names.AttrARN, "amplify", regexache.MustCompile(`apps/.+/domains/.+`)),
 					resource.TestCheckResourceAttr(resourceName, names.AttrDomainName, domainName),
 					resource.TestCheckResourceAttr(resourceName, "enable_auto_sub_domain", "true"),
-					resource.TestCheckResourceAttr(resourceName, "sub_domain.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "sub_domain.#", acctest.CtTwo),
 					resource.TestCheckTypeSetElemNestedAttrs(resourceName, "sub_domain.*", map[string]string{
 						"branch_name":    rName,
 						names.AttrPrefix: "",
