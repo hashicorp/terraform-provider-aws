37205427cccb8656e81b52c6d41907a02d72eef3
Dirk Avery <dirk.avery@gmail.com>
2024-05-14 00:46:49 -0400

    dataexchange: Use constants for test strings

--- internal/service/dataexchange/data_set_test.go
+++ internal/service/dataexchange/data_set_test.go
@@ -77,7 +77,7 @@ func TestAccDataExchangeDataSet_tags(t *testing.T) {
 				Config: testAccDataSetConfig_tags1(rName, "key1", "value1"),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckDataSetExists(ctx, resourceName, &proj),
-					resource.TestCheckResourceAttr(resourceName, "tags.%", "1"),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", acctest.CtOne),
 					resource.TestCheckResourceAttr(resourceName, "tags.key1", "value1"),
 				),
 			},
@@ -99,7 +99,7 @@ func TestAccDataExchangeDataSet_tags(t *testing.T) {
 				Config: testAccDataSetConfig_tags1(rName, "key2", "value2"),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckDataSetExists(ctx, resourceName, &proj),
-					resource.TestCheckResourceAttr(resourceName, "tags.%", "1"),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", acctest.CtOne),
 					resource.TestCheckResourceAttr(resourceName, "tags.key2", "value2"),
 				),
 			},
