1c71f4e2ce1629088f56286498d9f89b59618194
Dirk Avery <dirk.avery@gmail.com>
2024-05-14 00:47:04 -0400

    keyspaces: Use constants for test strings

--- internal/service/keyspaces/keyspace_test.go
+++ internal/service/keyspaces/keyspace_test.go
@@ -90,7 +90,7 @@ func TestAccKeyspacesKeyspace_tags(t *testing.T) {
 				Config: testAccKeyspaceConfig_tags1(rName, "key1", "value1"),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckKeyspaceExists(ctx, resourceName),
-					resource.TestCheckResourceAttr(resourceName, "tags.%", "1"),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", acctest.CtOne),
 					resource.TestCheckResourceAttr(resourceName, "tags.key1", "value1"),
 				),
 			},
@@ -112,7 +112,7 @@ func TestAccKeyspacesKeyspace_tags(t *testing.T) {
 				Config: testAccKeyspaceConfig_tags1(rName, "key2", "value2"),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckKeyspaceExists(ctx, resourceName),
-					resource.TestCheckResourceAttr(resourceName, "tags.%", "1"),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", acctest.CtOne),
 					resource.TestCheckResourceAttr(resourceName, "tags.key2", "value2"),
 				),
 			},
