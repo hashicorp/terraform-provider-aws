bcd0de1f57575fb06a2b8ffe5058b706623bcb82
Dirk Avery <dirk.avery@gmail.com>
2024-05-14 00:47:15 -0400

    scheduler: Use constants for test strings

--- internal/service/scheduler/schedule_group_test.go
+++ internal/service/scheduler/schedule_group_test.go
@@ -187,7 +187,7 @@ func TestAccSchedulerScheduleGroup_tags(t *testing.T) {
 				Config: testAccScheduleGroupConfig_tags1(rName, "key1", "value1"),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckScheduleGroupExists(ctx, resourceName, &scheduleGroup),
-					resource.TestCheckResourceAttr(resourceName, "tags.%", "1"),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", acctest.CtOne),
 					resource.TestCheckResourceAttr(resourceName, "tags.key1", "value1"),
 				),
 			},
@@ -214,7 +214,7 @@ func TestAccSchedulerScheduleGroup_tags(t *testing.T) {
 				Config: testAccScheduleGroupConfig_tags1(rName, "key2", "value2"),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckScheduleGroupExists(ctx, resourceName, &scheduleGroup),
-					resource.TestCheckResourceAttr(resourceName, "tags.%", "1"),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", acctest.CtOne),
 					resource.TestCheckResourceAttr(resourceName, "tags.key2", "value2"),
 				),
 			},
