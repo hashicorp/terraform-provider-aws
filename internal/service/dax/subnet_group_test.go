49e9185ebc2c4ccf293e9a8d3fb2718c66818f97
Dirk Avery <dirk.avery@gmail.com>
2024-05-14 17:51:08 -0400

    dax: Use constants for test strings

--- internal/service/dax/subnet_group_test.go
+++ internal/service/dax/subnet_group_test.go
@@ -44,7 +44,7 @@ func TestAccDAXSubnetGroup_basic(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckSubnetGroupExists(ctx, "aws_dax_subnet_group.test"),
 					resource.TestCheckResourceAttr("aws_dax_subnet_group.test", names.AttrDescription, "update"),
-					resource.TestCheckResourceAttr("aws_dax_subnet_group.test", "subnet_ids.#", "3"),
+					resource.TestCheckResourceAttr("aws_dax_subnet_group.test", "subnet_ids.#", acctest.CtThree),
 					resource.TestCheckResourceAttrSet("aws_dax_subnet_group.test", names.AttrVPCID),
 				),
 			},
