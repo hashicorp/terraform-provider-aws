16e76e6c90e25ff9d340fdf8892e90aa374fd1e4
Dirk Avery <dirk.avery@gmail.com>
2024-05-14 16:55:54 -0400

    s3control: Use constants for test strings

--- internal/service/s3control/access_grants_instance_test.go
+++ internal/service/s3control/access_grants_instance_test.go
@@ -81,11 +81,11 @@ func testAccAccessGrantsInstance_tags(t *testing.T) {
 		CheckDestroy:             testAccCheckAccessGrantsInstanceDestroy(ctx),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccAccessGrantsInstanceConfig_tags1("key1", "value1"),
+				Config: testAccAccessGrantsInstanceConfig_tags1(acctest.CtKey1, acctest.CtValue1),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckAccessGrantsInstanceExists(ctx, resourceName),
 					resource.TestCheckResourceAttr(resourceName, acctest.CtTagsPercent, acctest.CtOne),
-					resource.TestCheckResourceAttr(resourceName, "tags.key1", "value1"),
+					resource.TestCheckResourceAttr(resourceName, "tags.key1", acctest.CtValue1),
 				),
 			},
 			{
@@ -94,20 +94,20 @@ func testAccAccessGrantsInstance_tags(t *testing.T) {
 				ImportStateVerify: true,
 			},
 			{
-				Config: testAccAccessGrantsInstanceConfig_tags2("key1", "value1updated", "key2", "value2"),
+				Config: testAccAccessGrantsInstanceConfig_tags2(acctest.CtKey1, "value1updated", acctest.CtKey2, acctest.CtValue2),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckAccessGrantsInstanceExists(ctx, resourceName),
 					resource.TestCheckResourceAttr(resourceName, acctest.CtTagsPercent, acctest.CtTwo),
 					resource.TestCheckResourceAttr(resourceName, "tags.key1", "value1updated"),
-					resource.TestCheckResourceAttr(resourceName, "tags.key2", "value2"),
+					resource.TestCheckResourceAttr(resourceName, "tags.key2", acctest.CtValue2),
 				),
 			},
 			{
-				Config: testAccAccessGrantsInstanceConfig_tags1("key2", "value2"),
+				Config: testAccAccessGrantsInstanceConfig_tags1(acctest.CtKey2, acctest.CtValue2),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckAccessGrantsInstanceExists(ctx, resourceName),
 					resource.TestCheckResourceAttr(resourceName, acctest.CtTagsPercent, acctest.CtOne),
-					resource.TestCheckResourceAttr(resourceName, "tags.key2", "value2"),
+					resource.TestCheckResourceAttr(resourceName, "tags.key2", acctest.CtValue2),
 				),
 			},
 		},
