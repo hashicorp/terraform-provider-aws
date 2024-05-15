8ca8c020d9353be7ecae1230f6c0f430208a8635
Dirk Avery <dirk.avery@gmail.com>
2024-05-14 22:17:26 -0400

    ivs: Use constants for test strings

--- internal/service/ivs/ivs_test.go
+++ internal/service/ivs/ivs_test.go
@@ -15,10 +15,10 @@ func TestAccIVS_serial(t *testing.T) {
 
 	testCases := map[string]map[string]func(t *testing.T){
 		"PlaybackKeyPair": {
-			"basic":        testAccPlaybackKeyPair_basic,
-			"update":       testAccPlaybackKeyPair_update,
-			names.AttrTags: testAccPlaybackKeyPair_tags,
-			"disappears":   testAccPlaybackKeyPair_disappears,
+			acctest.CtBasic: testAccPlaybackKeyPair_basic,
+			"update":        testAccPlaybackKeyPair_update,
+			names.AttrTags:  testAccPlaybackKeyPair_tags,
+			"disappears":    testAccPlaybackKeyPair_disappears,
 		},
 	}
 
