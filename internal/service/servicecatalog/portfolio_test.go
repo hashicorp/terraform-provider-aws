25c63f379a7a171c7aa598fca38fcb2a29252b5b
Dirk Avery <dirk.avery@gmail.com>
2024-05-14 22:17:41 -0400

    servicecatalog: Use constants for test strings

--- internal/service/servicecatalog/portfolio_test.go
+++ internal/service/servicecatalog/portfolio_test.go
@@ -41,7 +41,7 @@ func TestAccServiceCatalogPortfolio_basic(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, names.AttrName, name),
 					resource.TestCheckResourceAttr(resourceName, names.AttrDescription, "test-2"),
 					resource.TestCheckResourceAttr(resourceName, names.AttrProviderName, "test-3"),
-					resource.TestCheckResourceAttr(resourceName, acctest.CtTagsPercent, acctest.CtZero),
+					resource.TestCheckResourceAttr(resourceName, acctest.CtTagsPercent, acctest.Ct0),
 				),
 			},
 			{
