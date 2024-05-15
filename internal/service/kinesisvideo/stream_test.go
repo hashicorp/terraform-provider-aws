cea8d47a034b7920515f987112eb65b1702d27b2
Dirk Avery <dirk.avery@gmail.com>
2024-05-14 22:17:28 -0400

    kinesisvideo: Use constants for test strings

--- internal/service/kinesisvideo/stream_test.go
+++ internal/service/kinesisvideo/stream_test.go
@@ -84,7 +84,7 @@ func TestAccKinesisVideoStream_options(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckStreamExists(ctx, resourceName, &stream),
 					acctest.MatchResourceAttrRegionalARN(resourceName, names.AttrARN, "kinesisvideo", regexache.MustCompile(fmt.Sprintf("stream/terraform-kinesis-video-stream-test-%d/.+", rInt))),
-					resource.TestCheckResourceAttr(resourceName, "data_retention_in_hours", acctest.CtOne),
+					resource.TestCheckResourceAttr(resourceName, "data_retention_in_hours", acctest.Ct1),
 					resource.TestCheckResourceAttr(resourceName, "media_type", "video/h264"),
 					resource.TestCheckResourceAttr(resourceName, names.AttrDeviceName, fmt.Sprintf("kinesis-video-device-name-%s", rName1)),
 					resource.TestCheckResourceAttrPair(
@@ -126,7 +126,7 @@ func TestAccKinesisVideoStream_tags(t *testing.T) {
 				Config: testAccStreamConfig_tags1(rInt, acctest.CtKey1, acctest.CtValue1),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckStreamExists(ctx, resourceName, &stream),
-					resource.TestCheckResourceAttr(resourceName, acctest.CtTagsPercent, acctest.CtOne),
+					resource.TestCheckResourceAttr(resourceName, acctest.CtTagsPercent, acctest.Ct1),
 					resource.TestCheckResourceAttr(resourceName, acctest.CtTagsKey1, acctest.CtValue1),
 				),
 			},
@@ -134,7 +134,7 @@ func TestAccKinesisVideoStream_tags(t *testing.T) {
 				Config: testAccStreamConfig_tags2(rInt, acctest.CtKey1, acctest.CtValue1, acctest.CtKey2, acctest.CtValue2),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckStreamExists(ctx, resourceName, &stream),
-					resource.TestCheckResourceAttr(resourceName, acctest.CtTagsPercent, acctest.CtTwo),
+					resource.TestCheckResourceAttr(resourceName, acctest.CtTagsPercent, acctest.Ct2),
 					resource.TestCheckResourceAttr(resourceName, acctest.CtTagsKey1, acctest.CtValue1),
 					resource.TestCheckResourceAttr(resourceName, acctest.CtTagsKey2, acctest.CtValue2),
 				),
@@ -148,7 +148,7 @@ func TestAccKinesisVideoStream_tags(t *testing.T) {
 				Config: testAccStreamConfig_tags1(rInt, acctest.CtKey2, acctest.CtValue2),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckStreamExists(ctx, resourceName, &stream),
-					resource.TestCheckResourceAttr(resourceName, acctest.CtTagsPercent, acctest.CtOne),
+					resource.TestCheckResourceAttr(resourceName, acctest.CtTagsPercent, acctest.Ct1),
 					resource.TestCheckResourceAttr(resourceName, acctest.CtTagsKey2, acctest.CtValue2),
 				),
 			},
