2ab7ee4d7bc959ccd3117e5a724781f84a987b7e
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-05-15 13:16:26 -0400

    organizations: Custom API client.

--- internal/service/organizations/service_package_gen.go
+++ internal/service/organizations/service_package_gen.go
@@ -5,8 +5,6 @@ package organizations
 import (
 	"context"
 
-	aws_sdkv2 "github.com/aws/aws-sdk-go-v2/aws"
-	organizations_sdkv2 "github.com/aws/aws-sdk-go-v2/service/organizations"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/types"
 	"github.com/hashicorp/terraform-provider-aws/names"
@@ -132,17 +130,6 @@ func (p *servicePackage) SDKResources(ctx context.Context) []*types.ServicePacka
 
 func (p *servicePackage) ServicePackageName() string {
 	return names.Organizations
-}
-
-// NewClient returns a new AWS SDK for Go v2 client for this service package's AWS API.
-func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (*organizations_sdkv2.Client, error) {
-	cfg := *(config["aws_sdkv2_config"].(*aws_sdkv2.Config))
-
-	return organizations_sdkv2.NewFromConfig(cfg, func(o *organizations_sdkv2.Options) {
-		if endpoint := config[names.AttrEndpoint].(string); endpoint != "" {
-			o.BaseEndpoint = aws_sdkv2.String(endpoint)
-		}
-	}), nil
 }
 
 func ServicePackage(ctx context.Context) conns.ServicePackage {
