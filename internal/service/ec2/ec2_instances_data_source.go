5d77cf122ac4ecf4fc9c1ad444d5a3888dae6879
Daniel Rieske <daniel.rieske@gmail.com>
2024-05-18 21:35:47 +0200

    feat: migrated d/ec2_instances to SDKv2

--- internal/service/ec2/ec2_instances_data_source.go
+++ internal/service/ec2/ec2_instances_data_source.go
@@ -7,12 +7,13 @@ import (
 	"context"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	awstypes "github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/enum"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
 	"github.com/hashicorp/terraform-provider-aws/internal/flex"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
@@ -40,8 +41,8 @@ func DataSourceInstances() *schema.Resource {
 				Type:     schema.TypeSet,
 				Optional: true,
 				Elem: &schema.Schema{
-					Type:         schema.TypeString,
-					ValidateFunc: validation.StringInSlice(ec2.InstanceStateName_Values(), false),
+					Type:             schema.TypeString,
+					ValidateDiagFunc: enum.Validate[awstypes.InstanceStateName](),
 				},
 			},
 			"ipv6_addresses": {
@@ -65,27 +66,27 @@ func DataSourceInstances() *schema.Resource {
 
 func dataSourceInstancesRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
-	conn := meta.(*conns.AWSClient).EC2Conn(ctx)
+	conn := meta.(*conns.AWSClient).EC2Client(ctx)
 
 	input := &ec2.DescribeInstancesInput{}
 
 	if v, ok := d.GetOk("instance_state_names"); ok && v.(*schema.Set).Len() > 0 {
-		input.Filters = append(input.Filters, &ec2.Filter{
+		input.Filters = append(input.Filters, awstypes.Filter{
 			Name:   aws.String("instance-state-name"),
-			Values: flex.ExpandStringSet(v.(*schema.Set)),
+			Values: flex.ExpandStringValueSet(v.(*schema.Set)),
 		})
 	} else {
-		input.Filters = append(input.Filters, &ec2.Filter{
+		input.Filters = append(input.Filters, awstypes.Filter{
 			Name:   aws.String("instance-state-name"),
-			Values: aws.StringSlice([]string{ec2.InstanceStateNameRunning}),
+			Values: enum.Slice(awstypes.InstanceStateNameRunning),
 		})
 	}
 
-	input.Filters = append(input.Filters, newTagFilterList(
-		Tags(tftags.New(ctx, d.Get("instance_tags").(map[string]interface{}))),
+	input.Filters = append(input.Filters, newTagFilterListV2(
+		TagsV2(tftags.New(ctx, d.Get("instance_tags").(map[string]interface{}))),
 	)...)
 
-	input.Filters = append(input.Filters, newCustomFilterList(
+	input.Filters = append(input.Filters, newCustomFilterListV2(
 		d.Get(names.AttrFilter).(*schema.Set),
 	)...)
 
@@ -93,7 +94,7 @@ func dataSourceInstancesRead(ctx context.Context, d *schema.ResourceData, meta i
 		input.Filters = nil
 	}
 
-	output, err := FindInstances(ctx, conn, input)
+	output, err := findInstances(ctx, conn, input)
 
 	if err != nil {
 		return sdkdiag.AppendErrorf(diags, "reading EC2 Instances: %s", err)
@@ -102,14 +103,14 @@ func dataSourceInstancesRead(ctx context.Context, d *schema.ResourceData, meta i
 	var instanceIDs, privateIPs, publicIPs, ipv6Addresses []string
 
 	for _, v := range output {
-		instanceIDs = append(instanceIDs, aws.StringValue(v.InstanceId))
-		if privateIP := aws.StringValue(v.PrivateIpAddress); privateIP != "" {
+		instanceIDs = append(instanceIDs, aws.ToString(v.InstanceId))
+		if privateIP := aws.ToString(v.PrivateIpAddress); privateIP != "" {
 			privateIPs = append(privateIPs, privateIP)
 		}
-		if publicIP := aws.StringValue(v.PublicIpAddress); publicIP != "" {
+		if publicIP := aws.ToString(v.PublicIpAddress); publicIP != "" {
 			publicIPs = append(publicIPs, publicIP)
 		}
-		if ipv6Address := aws.StringValue(v.Ipv6Address); ipv6Address != "" {
+		if ipv6Address := aws.ToString(v.Ipv6Address); ipv6Address != "" {
 			ipv6Addresses = append(ipv6Addresses, ipv6Address)
 		}
 	}
