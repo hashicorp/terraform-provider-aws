01df1e692e1d21b6995c5fce6f46cdec711d1daf
Graham Davison <gdavison@hashicorp.com>
2024-05-22 10:45:00 -0700

    `aws_vpc`

--- internal/service/ec2/vpc_.go
+++ internal/service/ec2/vpc_.go
@@ -39,7 +39,7 @@ const (
 
 // @SDKResource("aws_vpc", name="VPC")
 // @Tags(identifierAttribute="id")
-// @Testing(tagsTest=false)
+// @Testing(existsType="github.com/aws/aws-sdk-go-v2/service/ec2/types;awstypes;awstypes.Vpc")
 func ResourceVPC() *schema.Resource {
 	//lintignore:R011
 	return &schema.Resource{
