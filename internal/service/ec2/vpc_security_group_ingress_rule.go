dc5edd3d64ae67c63224528e5b967210c8b77e98
Graham Davison <gdavison@hashicorp.com>
2024-05-22 13:06:36 -0700

    `aws_vpc_security_group_ingress_rule`

--- internal/service/ec2/vpc_security_group_ingress_rule.go
+++ internal/service/ec2/vpc_security_group_ingress_rule.go
@@ -42,7 +42,7 @@ import (
 
 // @FrameworkResource("aws_vpc_security_group_ingress_rule", name="Security Group Ingress Rule")
 // @Tags(identifierAttribute="id")
-// @Testing(tagsTest=false)
+// @Testing(existsType="github.com/aws/aws-sdk-go/service/ec2;ec2.SecurityGroupRule")
 func newSecurityGroupIngressRuleResource(context.Context) (resource.ResourceWithConfigure, error) {
 	r := &securityGroupIngressRuleResource{}
 	r.securityGroupRule = r
