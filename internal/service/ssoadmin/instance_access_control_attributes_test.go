171fc069df275d0bff89cafa469f8fa500b0d06c
Dirk Avery <dirk.avery@gmail.com>
2024-05-24 12:00:17 -0400

    ssoadmin: Fix imports and formatting

--- internal/service/ssoadmin/instance_access_control_attributes_test.go
+++ internal/service/ssoadmin/instance_access_control_attributes_test.go
@@ -22,10 +22,10 @@ func TestAccSSOAdmin_serial(t *testing.T) {
 
 	testCases := map[string]map[string]func(t *testing.T){
 		"InstanceAccessControlAttributes": {
-			acctest.CtBasic: testAccInstanceAccessControlAttributes_basic,
-			acctest.CtDisappears:    testAccInstanceAccessControlAttributes_disappears,
-			"multiple":      testAccInstanceAccessControlAttributes_multiple,
-			"update":        testAccInstanceAccessControlAttributes_update,
+			acctest.CtBasic:      testAccInstanceAccessControlAttributes_basic,
+			acctest.CtDisappears: testAccInstanceAccessControlAttributes_disappears,
+			"multiple":           testAccInstanceAccessControlAttributes_multiple,
+			"update":             testAccInstanceAccessControlAttributes_update,
 		},
 	}
 
