bc517b87e24e89bdc33663396b6bab153f8fef62
Dirk Avery <dirk.avery@gmail.com>
2024-05-24 12:00:14 -0400

    quicksight: Fix imports and formatting

--- internal/service/quicksight/quicksight_test.go
+++ internal/service/quicksight/quicksight_test.go
@@ -14,8 +14,8 @@ func TestAccQuickSight_serial(t *testing.T) {
 
 	testCases := map[string]map[string]func(t *testing.T){
 		"AccountSubscription": {
-			acctest.CtBasic: testAccAccountSubscription_basic,
-			acctest.CtDisappears:    testAccAccountSubscription_disappears,
+			acctest.CtBasic:      testAccAccountSubscription_basic,
+			acctest.CtDisappears: testAccAccountSubscription_disappears,
 		},
 	}
 
