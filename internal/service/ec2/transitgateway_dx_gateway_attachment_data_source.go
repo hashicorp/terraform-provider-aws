b516f50bc56ac30cac2c33b0ae8bd21982a993cb
Daniel Rieske <daniel.rieske@gmail.com>
2024-05-26 00:26:24 +0200

    feat: migrated d/ec2_transitgateway_dx_gateway_attachment to SDKv2

--- internal/service/ec2/transitgateway_dx_gateway_attachment_data_source.go
+++ internal/service/ec2/transitgateway_dx_gateway_attachment_data_source.go
@@ -7,8 +7,9 @@ import (
 	"context"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	awstypes "github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
@@ -44,49 +45,49 @@ func DataSourceTransitGatewayDxGatewayAttachment() *schema.Resource {
 
 func dataSourceTransitGatewayDxGatewayAttachmentRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
-	conn := meta.(*conns.AWSClient).EC2Conn(ctx)
+	conn := meta.(*conns.AWSClient).EC2Client(ctx)
 	ignoreTagsConfig := meta.(*conns.AWSClient).IgnoreTagsConfig
 
 	input := &ec2.DescribeTransitGatewayAttachmentsInput{
-		Filters: newAttributeFilterList(map[string]string{
-			"resource-type": ec2.TransitGatewayAttachmentResourceTypeDirectConnectGateway,
+		Filters: newAttributeFilterListV2(map[string]string{
+			"resource-type": string(awstypes.TransitGatewayAttachmentResourceTypeDirectConnectGateway),
 		}),
 	}
 
-	input.Filters = append(input.Filters, newCustomFilterList(
+	input.Filters = append(input.Filters, newCustomFilterListV2(
 		d.Get(names.AttrFilter).(*schema.Set),
 	)...)
 
 	if v, ok := d.GetOk(names.AttrTags); ok {
-		input.Filters = append(input.Filters, newTagFilterList(
-			Tags(tftags.New(ctx, v.(map[string]interface{}))),
+		input.Filters = append(input.Filters, newTagFilterListV2(
+			TagsV2(tftags.New(ctx, v.(map[string]interface{}))),
 		)...)
 	}
 
 	// to preserve original functionality
 	if v, ok := d.GetOk("dx_gateway_id"); ok {
-		input.Filters = append(input.Filters, newAttributeFilterList(map[string]string{
+		input.Filters = append(input.Filters, newAttributeFilterListV2(map[string]string{
 			"resource-id": v.(string),
 		})...)
 	}
 
 	if v, ok := d.GetOk(names.AttrTransitGatewayID); ok {
-		input.Filters = append(input.Filters, newAttributeFilterList(map[string]string{
+		input.Filters = append(input.Filters, newAttributeFilterListV2(map[string]string{
 			"transit-gateway-id": v.(string),
 		})...)
 	}
 
-	transitGatewayAttachment, err := FindTransitGatewayAttachment(ctx, conn, input)
+	transitGatewayAttachment, err := findTransitGatewayAttachment(ctx, conn, input)
 
 	if err != nil {
 		return sdkdiag.AppendFromErr(diags, tfresource.SingularDataSourceFindError("EC2 Transit Gateway Direct Connect Gateway Attachment", err))
 	}
 
-	d.SetId(aws.StringValue(transitGatewayAttachment.TransitGatewayAttachmentId))
+	d.SetId(aws.ToString(transitGatewayAttachment.TransitGatewayAttachmentId))
 	d.Set("dx_gateway_id", transitGatewayAttachment.ResourceId)
 	d.Set(names.AttrTransitGatewayID, transitGatewayAttachment.TransitGatewayId)
 
-	if err := d.Set(names.AttrTags, KeyValueTags(ctx, transitGatewayAttachment.Tags).IgnoreAWS().IgnoreConfig(ignoreTagsConfig).Map()); err != nil {
+	if err := d.Set(names.AttrTags, keyValueTagsV2(ctx, transitGatewayAttachment.Tags).IgnoreAWS().IgnoreConfig(ignoreTagsConfig).Map()); err != nil {
 		return sdkdiag.AppendErrorf(diags, "setting tags: %s", err)
 	}
 
