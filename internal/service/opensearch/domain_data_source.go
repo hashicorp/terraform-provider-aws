ef4360d3743150609b21bb0973cd82af5fe6ac91
Dirk Avery <dirk.avery@gmail.com>
2024-05-31 16:37:42 -0400

    opensearch: Use constants for strings

--- internal/service/opensearch/domain_data_source.go
+++ internal/service/opensearch/domain_data_source.go
@@ -287,7 +287,7 @@ func DataSourceDomain() *schema.Resource {
 				Computed: true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
-						"cloudwatch_log_group_arn": {
+						names.AttrCloudWatchLogGroupARN: {
 							Type:     schema.TypeString,
 							Computed: true,
 						},
