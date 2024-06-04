c53504b3c9ca0f2bf7af7b3c57a09004db3c8bc8
Dirk Avery <dirk.avery@gmail.com>
2024-06-04 19:14:02 -0400

    drs: Fix generate

--- internal/service/drs/generate.go
+++ internal/service/drs/generate.go
@@ -1,9 +1,8 @@
 // Copyright (c) HashiCorp, Inc.
 // SPDX-License-Identifier: MPL-2.0
 
+//go:generate go run ../../generate/tags/main.go -AWSSDKVersion=2 -ListTags -ListTagsInIDElem=ResourceArn -ServiceTagsMap -SkipTypesImp -KVTValues -TagOp=TagResource -TagInIDElem=ResourceArn -UntagOp=UntagResource -CreateTags -ListTags -UpdateTags
 //go:generate go run ../../generate/servicepackage/main.go
-//go:generate go run ../../generate/tagstests/main.go
-//go:generate go run ../../generate/tags/main.go -AWSSDKVersion=2 -ListTagsForResource -ListTagsInIDElem=ResourceArn -ServiceTagsMap -TagOp=TagResource -TagInIDElem=ResourceId -UntagOp=UntagResource -ListTags -UpdateTags
 // ONLY generate directives and package declaration! Do not add anything else to this file.
 
 package drs
