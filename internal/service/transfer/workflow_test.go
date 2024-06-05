8cc552667b91e1263ee0081dd942cbde41825276
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-06-05 10:37:44 -0400

    Fix 'TestAccTransferWorkflow_allSteps'.

--- internal/service/transfer/workflow_test.go
+++ internal/service/transfer/workflow_test.go
@@ -443,7 +443,7 @@ resource "aws_lambda_function" "test" {
   function_name = %[1]q
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "index.handler"
-  runtime       = "nodejs14.x"
+  runtime       = "nodejs20.x"
 }
 
 resource "aws_efs_file_system" "test" {
