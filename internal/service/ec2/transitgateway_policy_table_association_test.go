e85f0f3b0951a2a03acf3fda9cfb8e49407d3979
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-06-05 16:03:49 -0400

    r/aws_ec2_transit_gateway_policy_table_association: Fix acceptance test configuration.

--- internal/service/ec2/transitgateway_policy_table_association_test.go
+++ internal/service/ec2/transitgateway_policy_table_association_test.go
@@ -174,11 +174,15 @@ resource "aws_networkmanager_global_network" "test" {
 
 resource "aws_networkmanager_core_network" "test" {
   global_network_id = aws_networkmanager_global_network.test.id
-  policy_document   = data.aws_networkmanager_core_network_policy_document.test.json
 
   tags = {
     Name = %[1]q
   }
+}
+
+resource "aws_networkmanager_core_network_policy_attachment" "test" {
+  core_network_id = aws_networkmanager_core_network.test.id
+  policy_document = data.aws_networkmanager_core_network_policy_document.test.json
 }
 
 data "aws_networkmanager_core_network_policy_document" "test" {
@@ -197,7 +201,7 @@ data "aws_networkmanager_core_network_policy_document" "test" {
 }
 
 resource "aws_networkmanager_transit_gateway_peering" "test" {
-  core_network_id     = aws_networkmanager_core_network.test.id
+  core_network_id     = aws_networkmanager_core_network_policy_attachment.test.core_network_id
   transit_gateway_arn = aws_ec2_transit_gateway.test.arn
 
   tags = {
