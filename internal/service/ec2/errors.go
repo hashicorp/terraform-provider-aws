5695aedc5a1730012b277b40c67699db891e9e33
Adrian Johnson <adrian.johnson@hashicorp.com>
2024-06-06 11:28:36 -0500

    fmt

--- internal/service/ec2/errors.go
+++ internal/service/ec2/errors.go
@@ -82,7 +82,7 @@ const (
 	errCodeInvalidPrefixListIDNotFound                             = "InvalidPrefixListID.NotFound"
 	errCodeInvalidPrefixListIdNotFound                             = "InvalidPrefixListId.NotFound"
 	errCodeInvalidPublicIpv4PoolIDNotFound                         = "InvalidPublicIpv4PoolID.NotFound" // nosemgrep:ci.caps5-in-const-name,ci.caps5-in-var-name
-	errCodeInvalidReservationNotFound                        = "InvalidReservationID.NotFound"
+	errCodeInvalidReservationNotFound                              = "InvalidReservationID.NotFound"
 	errCodeInvalidRouteNotFound                                    = "InvalidRoute.NotFound"
 	errCodeInvalidRouteTableIDNotFound                             = "InvalidRouteTableID.NotFound"
 	errCodeInvalidRouteTableIdNotFound                             = "InvalidRouteTableId.NotFound"
