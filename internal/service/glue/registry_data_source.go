b6bff222490c59044f12a31e8711b2868c6f6d0c
Viriathus1 <49337323+Viriathus1@users.noreply.github.com>
2024-06-07 14:43:01 +1000

    implement read handler

--- internal/service/glue/registry_data_source.go
+++ internal/service/glue/registry_data_source.go
@@ -12,7 +12,6 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/create"
 	"github.com/hashicorp/terraform-provider-aws/internal/framework"
 	"github.com/hashicorp/terraform-provider-aws/internal/framework/flex"
-	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
@@ -46,35 +45,19 @@ func (d *dataSourceRegistry) Schema(ctx context.Context, req datasource.SchemaRe
 			"registry_name": schema.StringAttribute{
 				Computed: true,
 			},
-			names.AttrTags: tftags.TagsAttributeComputedOnly(),
 		},
 	}
 }
 
-// TIP: ==== ASSIGN CRUD METHODS ====
-// Data sources only have a read method.
 func (d *dataSourceRegistry) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
-	// TIP: ==== DATA SOURCE READ ====
-	// Generally, the Read function should do the following things. Make
-	// sure there is a good reason if you don't do one of these.
-	//
-	// 1. Get a client connection to the relevant service
-	// 2. Fetch the config
-	// 3. Get information about a resource from AWS
-	// 4. Set the ID, arguments, and attributes
-	// 5. Set the tags
-	// 6. Set the state
-	// TIP: -- 1. Get a client connection to the relevant service
-	conn := d.Meta().GlueClient(ctx)
+	conn := d.Meta().GlueConn(ctx)
 
-	// TIP: -- 2. Fetch the config
 	var data dataSourceRegistryData
 	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// TIP: -- 3. Get information about a resource from AWS
 	out, err := FindRegistryByID(ctx, conn, data.ID.ValueString())
 
 	if err != nil {
@@ -85,50 +68,17 @@ func (d *dataSourceRegistry) Read(ctx context.Context, req datasource.ReadReques
 		return
 	}
 
-	// TIP: -- 4. Set the ID, arguments, and attributes
-	//
-	// For simple data types (i.e., schema.StringAttribute, schema.BoolAttribute,
-	// schema.Int64Attribute, and schema.Float64Attribue), simply setting the
-	// appropriate data struct field is sufficient. The flex package implements
-	// helpers for converting between Go and Plugin-Framework types seamlessly. No
-	// error or nil checking is necessary.
-	//
-	// However, there are some situations where more handling is needed such as
-	// complex data types (e.g., schema.ListAttribute, schema.SetAttribute). In
-	// these cases the flatten function may have a diagnostics return value, which
-	// should be appended to resp.Diagnostics.
-	data.ARN = flex.StringToFramework(ctx, out.Arn)
-	data.ID = flex.StringToFramework(ctx, out.RegistryId)
-	data.Name = flex.StringToFramework(ctx, out.RegistryName)
-	data.Type = flex.StringToFramework(ctx, out.RegistryType)
+	resp.Diagnostics.Append(flex.Flatten(ctx, out, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
 
-	// TIP: Setting a complex type.
-	complexArgument, diag := flattenComplexArgument(ctx, out.ComplexArgument)
-	resp.Diagnostics.Append(diag...)
-	data.ComplexArgument = complexArgument
-
-	// TIP: -- 5. Set the tags
-
-	// TIP: -- 6. Set the state
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
-// TIP: ==== DATA STRUCTURES ====
-// With Terraform Plugin-Framework configurations are deserialized into
-// Go types, providing type safety without the need for type assertions.
-// These structs should match the schema definition exactly, and the `tfsdk`
-// tag value should match the attribute name.
-//
-// Nested objects are represented in their own data struct. These will
-// also have a corresponding attribute type mapping for use inside flex
-// functions.
-//
-// See more:
-// https://developer.hashicorp.com/terraform/plugin/framework/handling-data/accessing-values
 type dataSourceRegistryData struct {
-	ARN         types.String `tfsdk:"arn"`
-	Description types.String `tfsdk:"description"`
-	ID          types.String `tfsdk:"id"`
-	Name        types.String `tfsdk:"name"`
-	Type        types.String `tfsdk:"type"`
+	RegistryArn  types.String `tfsdk:"arn"`
+	Description  types.String `tfsdk:"description"`
+	ID           types.String `tfsdk:"id"`
+	RegistryName types.String `tfsdk:"registry_name"`
 }
