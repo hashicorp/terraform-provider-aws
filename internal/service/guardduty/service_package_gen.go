031b61f59ac8b27e19700ce84c4ace19b1462d5f
GlennChia <glennchia7@gmail.com>
2024-06-11 08:28:39 +0000

    build(gd): malware protection plan make gen

--- internal/service/guardduty/service_package_gen.go
+++ internal/service/guardduty/service_package_gen.go
@@ -29,7 +29,15 @@ func (p *servicePackage) FrameworkDataSources(ctx context.Context) []*types.Serv
 }
 
 func (p *servicePackage) FrameworkResources(ctx context.Context) []*types.ServicePackageFrameworkResource {
-	return []*types.ServicePackageFrameworkResource{}
+	return []*types.ServicePackageFrameworkResource{
+		{
+			Factory: newResourceMalwareProtectionPlan,
+			Name:    "Malware Protection Plan",
+			Tags: &types.ServicePackageResourceTags{
+				IdentifierAttribute: names.AttrARN,
+			},
+		},
+	}
 }
 
 func (p *servicePackage) SDKDataSources(ctx context.Context) []*types.ServicePackageSDKDataSource {
