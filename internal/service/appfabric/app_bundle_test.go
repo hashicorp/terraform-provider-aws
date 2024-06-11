e00c8a5240b3bec26d1b09de1b50afb98816643a
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-06-11 08:23:11 -0700

    Add 'TestAccAppFabricAppBundle_cmk'.

--- internal/service/appfabric/app_bundle_test.go
+++ internal/service/appfabric/app_bundle_test.go
@@ -9,6 +9,7 @@ import (
 	"testing"
 
 	awstypes "github.com/aws/aws-sdk-go-v2/service/appfabric/types"
+	sdkacctest "github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
@@ -70,6 +71,37 @@ func TestAccAppFabricAppBundle_disappears(t *testing.T) {
 					acctest.CheckFrameworkResourceDisappears(ctx, acctest.Provider, tfappfabric.ResourceAppBundle, resourceName),
 				),
 				ExpectNonEmptyPlan: true,
+			},
+		},
+	})
+}
+
+func TestAccAppFabricAppBundle_cmk(t *testing.T) {
+	ctx := acctest.Context(t)
+	var appbundle awstypes.AppBundle
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	resourceName := "aws_appfabric_app_bundle.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+			acctest.PreCheckPartitionHasService(t, names.AppFabricEndpointID)
+		},
+		ErrorCheck:               acctest.ErrorCheck(t, names.AppFabricServiceID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckAppBundleDestroy(ctx),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAppBundleConfig_cmk(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAppBundleExists(ctx, resourceName, &appbundle),
+					resource.TestCheckResourceAttrSet(resourceName, "customer_managed_key_arn"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
 			},
 		},
 	})
@@ -173,6 +205,18 @@ func testAccCheckAppBundleExists(ctx context.Context, n string, v *awstypes.AppB
 const testAccAppBundleConfig_basic = `
 resource "aws_appfabric_app_bundle" "test" {}
 `
+
+func testAccAppBundleConfig_cmk(rName string) string {
+	return fmt.Sprintf(`
+resource "aws_kms_key" "test" {
+  description = %[1]q
+}
+
+resource "aws_appfabric_app_bundle" "test" {
+  customer_managed_key_arn = aws_kms_key.test.arn
+}
+`, rName)
+}
 
 func testAccAppBundleConfig_tags1(tagKey1, tagValue1 string) string {
 	return fmt.Sprintf(`
