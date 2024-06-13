0002b0ea5ed05ae01a2d3cd65cbf9e881078d9e0
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-06-13 06:30:17 -0700

    r/aws_service_discovery_public_dns_namespace: Handle 'NamespaceNotFound' on Delete.

--- internal/service/servicediscovery/public_dns_namespace.go
+++ internal/service/servicediscovery/public_dns_namespace.go
@@ -14,6 +14,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/id"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
@@ -159,6 +160,10 @@ func resourcePublicDNSNamespaceDelete(ctx context.Context, d *schema.ResourceDat
 	output, err := conn.DeleteNamespace(ctx, &servicediscovery.DeleteNamespaceInput{
 		Id: aws.String(d.Id()),
 	})
+
+	if errs.IsA[*awstypes.NamespaceNotFound](err) {
+		return diags
+	}
 
 	if err != nil {
 		return sdkdiag.AppendErrorf(diags, "deleting Service Discovery Public DNS Namespace (%s): %s", d.Id(), err)
