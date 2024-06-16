acbc5eb085427a772517c6ff25585ec5620083b3
Alan Ip <theipster@users.noreply.github.com>
2024-06-16 03:40:04 +0000

    Reinstate all tests

--- internal/service/macie2/macie2_test.go
+++ internal/service/macie2/macie2_test.go
@@ -13,6 +13,26 @@ func TestAccMacie2_serial(t *testing.T) {
 	t.Parallel()
 
 	testCases := map[string]map[string]func(t *testing.T){
+		"Account": {
+			acctest.CtBasic:                testAccAccount_basic,
+			"finding_publishing_frequency": testAccAccount_FindingPublishingFrequency,
+			"status":                       testAccAccount_WithStatus,
+			"finding_and_status":           testAccAccount_WithFindingAndStatus,
+			acctest.CtDisappears:           testAccAccount_disappears,
+		},
+		"ClassificationExportConfiguration": {
+			acctest.CtBasic: testAccClassificationExportConfiguration_basic,
+		},
+		"ClassificationJob": {
+			acctest.CtBasic:      testAccClassificationJob_basic,
+			"name_generated":     testAccClassificationJob_Name_Generated,
+			"name_prefix":        testAccClassificationJob_NamePrefix,
+			acctest.CtDisappears: testAccClassificationJob_disappears,
+			"status":             testAccClassificationJob_Status,
+			"complete":           testAccClassificationJob_complete,
+			"tags":               testAccClassificationJob_WithTags,
+			"bucket_criteria":    testAccClassificationJob_BucketCriteria,
+		},
 		"CustomDataIdentifier": {
 			acctest.CtBasic:      testAccCustomDataIdentifier_basic,
 			"name_generated":     testAccCustomDataIdentifier_Name_Generated,
@@ -20,6 +40,32 @@ func TestAccMacie2_serial(t *testing.T) {
 			"name_prefix":        testAccCustomDataIdentifier_NamePrefix,
 			"classification_job": testAccCustomDataIdentifier_WithClassificationJob,
 			"tags":               testAccCustomDataIdentifier_WithTags,
+		},
+		"FindingsFilter": {
+			acctest.CtBasic:      testAccFindingsFilter_basic,
+			"name_generated":     testAccFindingsFilter_Name_Generated,
+			"name_prefix":        testAccFindingsFilter_NamePrefix,
+			acctest.CtDisappears: testAccFindingsFilter_disappears,
+			"complete":           testAccFindingsFilter_complete,
+			"date":               testAccFindingsFilter_WithDate,
+			"number":             testAccFindingsFilter_WithNumber,
+			"tags":               testAccFindingsFilter_withTags,
+		},
+		"OrganizationAdminAccount": {
+			acctest.CtBasic:      testAccOrganizationAdminAccount_basic,
+			acctest.CtDisappears: testAccOrganizationAdminAccount_disappears,
+		},
+		"Member": {
+			acctest.CtBasic:                         testAccMember_basic,
+			acctest.CtDisappears:                    testAccMember_disappears,
+			"tags":                                  testAccMember_withTags,
+			"invitation_disable_email_notification": testAccMember_invitationDisableEmailNotification,
+			"invite":                                testAccMember_invite,
+			"invite_removed":                        testAccMember_inviteRemoved,
+			"status":                                testAccMember_status,
+		},
+		"InvitationAccepter": {
+			acctest.CtBasic: testAccInvitationAccepter_basic,
 		},
 	}
 
