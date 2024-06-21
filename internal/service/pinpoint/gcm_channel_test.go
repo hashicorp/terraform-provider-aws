787751c3c91e33e8f737532aca9fd65153f64f3d
Alec Collier <alec.collier@fleete.com>
2024-06-21 14:11:56 +0100

    fix space formatting

--- internal/service/pinpoint/gcm_channel_test.go
+++ internal/service/pinpoint/gcm_channel_test.go
@@ -155,7 +155,7 @@ resource "aws_pinpoint_app" "test_app" {}
 resource "aws_pinpoint_gcm_channel" "test_gcm_channel" {
   application_id = aws_pinpoint_app.test_app.application_id
   enabled        = "false"
-	default_authentication_method = "KEY"
+  default_authentication_method = "KEY"
   api_key        = "%s"
 }
 `, apiKey)
@@ -168,7 +168,7 @@ resource "aws_pinpoint_app" "test_app" {}
 resource "aws_pinpoint_gcm_channel" "test_gcm_channel" {
   application_id 								= aws_pinpoint_app.test_app.application_id
   enabled        								= "false"
-	default_authentication_method = "TOKEN"
+  default_authentication_method = "TOKEN"
   service_json        					= file("%s")
 }
 `, serviceJsonFile)
