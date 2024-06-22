30b7383f6cb7419ba982badc6af7370a65f6a4af
Daniel Rieske <daniel.rieske@gmail.com>
2024-06-22 14:49:56 +0200

    feat: migrated d/ec2_outposts_local_gateway_route_table to SDKv2

--- internal/service/ec2/outposts_local_gateway_route_table_data_source.go
+++ internal/service/ec2/outposts_local_gateway_route_table_data_source.go
@@ -8,8 +8,8 @@ import (
 	"log"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
@@ -19,7 +19,7 @@ import (
 )
 
 // @SDKDataSource("aws_ec2_local_gateway_route_table")
-func DataSourceLocalGatewayRouteTable() *schema.Resource {
+func dataSourceLocalGatewayRouteTable() *schema.Resource {
 	return &schema.Resource{
 		ReadWithoutTimeout: dataSourceLocalGatewayRouteTableRead,
 
@@ -61,16 +61,16 @@ func DataSourceLocalGatewayRouteTable() *schema.Resource {
 
 func dataSourceLocalGatewayRouteTableRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
-	conn := meta.(*conns.AWSClient).EC2Conn(ctx)
+	conn := meta.(*conns.AWSClient).EC2Client(ctx)
 	ignoreTagsConfig := meta.(*conns.AWSClient).IgnoreTagsConfig
 
 	req := &ec2.DescribeLocalGatewayRouteTablesInput{}
 
 	if v, ok := d.GetOk("local_gateway_route_table_id"); ok {
-		req.LocalGatewayRouteTableIds = []*string{aws.String(v.(string))}
+		req.LocalGatewayRouteTableIds = []string{v.(string)}
 	}
 
-	req.Filters = newAttributeFilterList(
+	req.Filters = newAttributeFilterListV2(
 		map[string]string{
 			"local-gateway-id": d.Get("local_gateway_id").(string),
 			"outpost-arn":      d.Get("outpost_arn").(string),
@@ -78,11 +78,11 @@ func dataSourceLocalGatewayRouteTableRead(ctx context.Context, d *schema.Resourc
 		},
 	)
 
-	req.Filters = append(req.Filters, newTagFilterList(
-		Tags(tftags.New(ctx, d.Get(names.AttrTags).(map[string]interface{}))),
+	req.Filters = append(req.Filters, newTagFilterListV2(
+		TagsV2(tftags.New(ctx, d.Get(names.AttrTags).(map[string]interface{}))),
 	)...)
 
-	req.Filters = append(req.Filters, newCustomFilterList(
+	req.Filters = append(req.Filters, newCustomFilterListV2(
 		d.Get(names.AttrFilter).(*schema.Set),
 	)...)
 	if len(req.Filters) == 0 {
@@ -90,8 +90,8 @@ func dataSourceLocalGatewayRouteTableRead(ctx context.Context, d *schema.Resourc
 		req.Filters = nil
 	}
 
-	log.Printf("[DEBUG] Reading AWS Local Gateway Route Table: %s", req)
-	resp, err := conn.DescribeLocalGatewayRouteTablesWithContext(ctx, req)
+	log.Printf("[DEBUG] Reading AWS Local Gateway Route Table: %#v", req)
+	resp, err := conn.DescribeLocalGatewayRouteTables(ctx, req)
 	if err != nil {
 		return sdkdiag.AppendErrorf(diags, "describing EC2 Local Gateway Route Tables: %s", err)
 	}
@@ -104,13 +104,13 @@ func dataSourceLocalGatewayRouteTableRead(ctx context.Context, d *schema.Resourc
 
 	localgatewayroutetable := resp.LocalGatewayRouteTables[0]
 
-	d.SetId(aws.StringValue(localgatewayroutetable.LocalGatewayRouteTableId))
+	d.SetId(aws.ToString(localgatewayroutetable.LocalGatewayRouteTableId))
 	d.Set("local_gateway_id", localgatewayroutetable.LocalGatewayId)
 	d.Set("local_gateway_route_table_id", localgatewayroutetable.LocalGatewayRouteTableId)
 	d.Set("outpost_arn", localgatewayroutetable.OutpostArn)
 	d.Set(names.AttrState, localgatewayroutetable.State)
 
-	if err := d.Set(names.AttrTags, KeyValueTags(ctx, localgatewayroutetable.Tags).IgnoreAWS().IgnoreConfig(ignoreTagsConfig).Map()); err != nil {
+	if err := d.Set(names.AttrTags, keyValueTagsV2(ctx, localgatewayroutetable.Tags).IgnoreAWS().IgnoreConfig(ignoreTagsConfig).Map()); err != nil {
 		return sdkdiag.AppendErrorf(diags, "setting tags: %s", err)
 	}
 
