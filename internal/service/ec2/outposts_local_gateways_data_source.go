144a087078d5a4e9a3d6b2bb33847c415098e48b
Daniel Rieske <daniel.rieske@gmail.com>
2024-06-22 14:54:35 +0200

    feat: migrated d/ec2_outposts_local_gateways to SDKv2

--- internal/service/ec2/outposts_local_gateways_data_source.go
+++ internal/service/ec2/outposts_local_gateways_data_source.go
@@ -7,8 +7,8 @@ import (
 	"context"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
@@ -18,7 +18,7 @@ import (
 )
 
 // @SDKDataSource("aws_ec2_local_gateways")
-func DataSourceLocalGateways() *schema.Resource {
+func dataSourceLocalGateways() *schema.Resource {
 	return &schema.Resource{
 		ReadWithoutTimeout: dataSourceLocalGatewaysRead,
 
@@ -40,15 +40,15 @@ func DataSourceLocalGateways() *schema.Resource {
 
 func dataSourceLocalGatewaysRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
-	conn := meta.(*conns.AWSClient).EC2Conn(ctx)
+	conn := meta.(*conns.AWSClient).EC2Client(ctx)
 
 	input := &ec2.DescribeLocalGatewaysInput{}
 
-	input.Filters = append(input.Filters, newTagFilterList(
-		Tags(tftags.New(ctx, d.Get(names.AttrTags).(map[string]interface{}))),
+	input.Filters = append(input.Filters, newTagFilterListV2(
+		TagsV2(tftags.New(ctx, d.Get(names.AttrTags).(map[string]interface{}))),
 	)...)
 
-	input.Filters = append(input.Filters, newCustomFilterList(
+	input.Filters = append(input.Filters, newCustomFilterListV2(
 		d.Get(names.AttrFilter).(*schema.Set),
 	)...)
 
@@ -56,7 +56,7 @@ func dataSourceLocalGatewaysRead(ctx context.Context, d *schema.ResourceData, me
 		input.Filters = nil
 	}
 
-	output, err := FindLocalGateways(ctx, conn, input)
+	output, err := findLocalGateways(ctx, conn, input)
 
 	if err != nil {
 		return sdkdiag.AppendErrorf(diags, "reading EC2 Local Gateways: %s", err)
@@ -65,7 +65,7 @@ func dataSourceLocalGatewaysRead(ctx context.Context, d *schema.ResourceData, me
 	var gatewayIDs []string
 
 	for _, v := range output {
-		gatewayIDs = append(gatewayIDs, aws.StringValue(v.LocalGatewayId))
+		gatewayIDs = append(gatewayIDs, aws.ToString(v.LocalGatewayId))
 	}
 
 	d.SetId(meta.(*conns.AWSClient).Region)
