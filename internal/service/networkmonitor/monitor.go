c8e338bb7678f5f574d6ca41b3957909d2e148f3
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-06-23 16:43:36 -0400

    Fix golangci-lint 'unparam'.

--- internal/service/networkmonitor/monitor.go
+++ internal/service/networkmonitor/monitor.go
@@ -268,7 +268,7 @@ func statusMonitor(ctx context.Context, conn *networkmonitor.Client, name string
 	}
 }
 
-func waitMonitorReady(ctx context.Context, conn *networkmonitor.Client, name string) (*networkmonitor.GetMonitorOutput, error) {
+func waitMonitorReady(ctx context.Context, conn *networkmonitor.Client, name string) (*networkmonitor.GetMonitorOutput, error) { //nolint:unparam
 	const (
 		timeout = time.Minute * 10
 	)
