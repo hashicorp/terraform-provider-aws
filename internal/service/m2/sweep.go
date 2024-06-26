67ce9a825bced4829cad1745ca0bb4f1b2fd111a
Graham Davison <gdavison@hashicorp.com>
2024-06-26 10:16:30 -0700

    Adds sweeper for `aws_m2_application`

--- internal/service/m2/sweep.go
+++ internal/service/m2/sweep.go
@@ -17,7 +17,36 @@ import (
 )
 
 func RegisterSweepers() {
+	sweep.Register("aws_m2_application", sweepApplications)
+
 	sweep.Register("aws_m2_environment", sweepEnvironments)
+}
+
+func sweepApplications(ctx context.Context, client *conns.AWSClient) ([]sweep.Sweepable, error) {
+	conn := client.M2Client(ctx)
+
+	var sweepResources []sweep.Sweepable
+
+	pages := m2.NewListApplicationsPaginator(conn, &m2.ListApplicationsInput{})
+	for pages.HasMorePages() {
+		page, err := pages.NextPage(ctx)
+		if awsv2.SkipSweepError(err) {
+			tflog.Warn(ctx, "Skipping sweeper", map[string]any{
+				"error": err.Error(),
+			})
+			return nil, nil
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		for _, application := range page.Applications {
+			sweepResources = append(sweepResources, framework.NewSweepResource(newApplicationResource, client,
+				framework.NewAttribute(names.AttrID, aws.ToString(application.ApplicationId))))
+		}
+	}
+
+	return sweepResources, nil
 }
 
 func sweepEnvironments(ctx context.Context, client *conns.AWSClient) ([]sweep.Sweepable, error) {
