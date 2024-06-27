0bc5d311668f3eff6bed9dfee6d2a415e5e99dd0
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-06-27 13:52:28 -0400

    d/aws_grafana_workspace: Migrate to AWS SDK for Go v2.

--- internal/service/grafana/service_package_gen.go
+++ internal/service/grafana/service_package_gen.go
@@ -25,8 +25,10 @@ func (p *servicePackage) FrameworkResources(ctx context.Context) []*types.Servic
 func (p *servicePackage) SDKDataSources(ctx context.Context) []*types.ServicePackageSDKDataSource {
 	return []*types.ServicePackageSDKDataSource{
 		{
-			Factory:  DataSourceWorkspace,
+			Factory:  dataSourceWorkspace,
 			TypeName: "aws_grafana_workspace",
+			Name:     "Workspace",
+			Tags:     &types.ServicePackageResourceTags{},
 		},
 	}
 }
