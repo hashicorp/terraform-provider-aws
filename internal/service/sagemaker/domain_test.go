f86f339b54e10b35fc2dc6b6e5c1e4c08d03236b
nikhil <nplusg1993@gmail.com>
2024-06-29 22:03:53 +0100

    aws_sagemaker_domain

--- internal/service/sagemaker/domain_test.go
+++ internal/service/sagemaker/domain_test.go
@@ -2055,6 +2055,16 @@ resource "aws_sagemaker_domain" "test" {
   vpc_id      = aws_vpc.test.id
   subnet_ids  = aws_subnet.test[*].id
 
+  default_user_settings {
+    execution_role = aws_iam_role.test.arn
+
+    jupyter_lab_app_settings {
+      default_resource_spec {
+        instance_type = "ml.t3.micro"
+      }
+    }
+  }
+
   default_space_settings {
     execution_role = aws_iam_role.test.arn
 
