0df968c7703e58a10add8fe9ac42f0d5108778ee
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-07-05 11:39:39 -0400

    efs: Tidy up sweepers.

--- internal/service/efs/sweep.go
+++ internal/service/efs/sweep.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/efs"
-	multierror "github.com/hashicorp/go-multierror"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 	"github.com/hashicorp/terraform-provider-aws/internal/sweep/awsv2"
@@ -45,21 +44,19 @@ func sweepAccessPoints(region string) error {
 	}
 	conn := client.EFSClient(ctx)
 	input := &efs.DescribeFileSystemsInput{}
-	var sweeperErrs *multierror.Error
 	sweepResources := make([]sweep.Sweepable, 0)
 
 	pages := efs.NewDescribeFileSystemsPaginator(conn, input)
-
 	for pages.HasMorePages() {
 		page, err := pages.NextPage(ctx)
 
 		if awsv2.SkipSweepError(err) {
 			log.Printf("[WARN] Skipping EFS Access Point sweep for %s: %s", region, err)
-			return sweeperErrs.ErrorOrNil() // In case we have completed some pages, but had errors
+			return nil
 		}
 
 		if err != nil {
-			sweeperErrs = multierror.Append(sweeperErrs, fmt.Errorf("error listing EFS File Systems (%s): %w", region, err))
+			return fmt.Errorf("error listing EFS File Systems (%s): %w", region, err)
 		}
 
 		for _, v := range page.FileSystems {
@@ -68,20 +65,15 @@ func sweepAccessPoints(region string) error {
 			}
 
 			pages := efs.NewDescribeAccessPointsPaginator(conn, input)
-
 			for pages.HasMorePages() {
 				page, err := pages.NextPage(ctx)
 
-				if awsv2.SkipSweepError(err) {
+				if err != nil {
 					continue
 				}
 
-				if err != nil {
-					sweeperErrs = multierror.Append(sweeperErrs, fmt.Errorf("error listing EFS Access Points (%s): %w", region, err))
-				}
-
 				for _, v := range page.AccessPoints {
-					r := ResourceAccessPoint()
+					r := resourceAccessPoint()
 					d := r.Data(nil)
 					d.SetId(aws.ToString(v.AccessPointId))
 
@@ -94,10 +86,10 @@ func sweepAccessPoints(region string) error {
 	err = sweep.SweepOrchestrator(ctx, sweepResources)
 
 	if err != nil {
-		sweeperErrs = multierror.Append(sweeperErrs, fmt.Errorf("error sweeping EFS Access Points (%s): %w", region, err))
+		return fmt.Errorf("error sweeping EFS Acess Points (%s): %w", region, err)
 	}
 
-	return sweeperErrs.ErrorOrNil()
+	return nil
 }
 
 func sweepFileSystems(region string) error {
@@ -111,7 +103,6 @@ func sweepFileSystems(region string) error {
 	sweepResources := make([]sweep.Sweepable, 0)
 
 	pages := efs.NewDescribeFileSystemsPaginator(conn, input)
-
 	for pages.HasMorePages() {
 		page, err := pages.NextPage(ctx)
 
@@ -125,7 +116,7 @@ func sweepFileSystems(region string) error {
 		}
 
 		for _, v := range page.FileSystems {
-			r := ResourceFileSystem()
+			r := resourceFileSystem()
 			d := r.Data(nil)
 			d.SetId(aws.ToString(v.FileSystemId))
 
@@ -150,21 +141,19 @@ func sweepMountTargets(region string) error {
 	}
 	conn := client.EFSClient(ctx)
 	input := &efs.DescribeFileSystemsInput{}
-	var sweeperErrs *multierror.Error
 	sweepResources := make([]sweep.Sweepable, 0)
 
 	pages := efs.NewDescribeFileSystemsPaginator(conn, input)
-
 	for pages.HasMorePages() {
 		page, err := pages.NextPage(ctx)
 
 		if awsv2.SkipSweepError(err) {
 			log.Printf("[WARN] Skipping EFS Mount Target sweep for %s: %s", region, err)
-			return sweeperErrs.ErrorOrNil() // In case we have completed some pages, but had errors
+			return nil
 		}
 
 		if err != nil {
-			sweeperErrs = multierror.Append(sweeperErrs, fmt.Errorf("error listing EFS File Systems (%s): %w", region, err))
+			return fmt.Errorf("error listing EFS File Systems (%s): %w", region, err)
 		}
 
 		for _, v := range page.FileSystems {
@@ -172,28 +161,21 @@ func sweepMountTargets(region string) error {
 				FileSystemId: v.FileSystemId,
 			}
 
-			err := describeMountTargetsPages(ctx, conn, input, func(page *efs.DescribeMountTargetsOutput, lastPage bool) bool {
-				if page == nil {
-					return !lastPage
+			pages := efs.NewDescribeMountTargetsPaginator(conn, input)
+			for pages.HasMorePages() {
+				page, err := pages.NextPage(ctx)
+
+				if err != nil {
+					continue
 				}
 
 				for _, v := range page.MountTargets {
-					r := ResourceMountTarget()
+					r := resourceMountTarget()
 					d := r.Data(nil)
 					d.SetId(aws.ToString(v.MountTargetId))
 
 					sweepResources = append(sweepResources, sweep.NewSweepResource(r, d, client))
 				}
-
-				return !lastPage
-			})
-
-			if awsv2.SkipSweepError(err) {
-				continue
-			}
-
-			if err != nil {
-				sweeperErrs = multierror.Append(sweeperErrs, fmt.Errorf("error listing EFS Mount Targets (%s): %w", region, err))
 			}
 		}
 	}
@@ -201,8 +183,8 @@ func sweepMountTargets(region string) error {
 	err = sweep.SweepOrchestrator(ctx, sweepResources)
 
 	if err != nil {
-		sweeperErrs = multierror.Append(sweeperErrs, fmt.Errorf("error sweeping EFS Mount Targets (%s): %w", region, err))
+		return fmt.Errorf("error sweeping EFS Mount Targets (%s): %w", region, err)
 	}
 
-	return sweeperErrs.ErrorOrNil()
+	return nil
 }
