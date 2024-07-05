ef9c55a328fb3b221f109ba508c2650d50c0d580
nikhil <nplusg1993@gmail.com>
2024-07-05 21:35:32 +0100

    aws_cloudwatch_event_target

--- internal/service/events/target.go
+++ internal/service/events/target.go
@@ -398,7 +398,8 @@ func resourceTarget() *schema.Resource {
 						"maximum_event_age_in_seconds": {
 							Type:         schema.TypeInt,
 							Optional:     true,
-							ValidateFunc: validation.IntBetween(0, 86400),
+							Default:      86400,
+							ValidateFunc: validation.IntBetween(60, 86400),
 						},
 						"maximum_retry_attempts": {
 							Type:         schema.TypeInt,
