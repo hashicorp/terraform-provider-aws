1cf2a39718f180a921c2518b372f8e07441e5015
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-07-07 17:29:05 -0400

    Run 'make gen'.

--- internal/service/ds/tags_gen.go
+++ internal/service/ds/tags_gen.go
@@ -98,12 +98,12 @@ func setTagsOut(ctx context.Context, tags []awstypes.Tag) {
 }
 
 // createTags creates ds service tags for new resources.
-func createTags(ctx context.Context, conn *directoryservice.Client, identifier string, tags []awstypes.Tag) error {
+func createTags(ctx context.Context, conn *directoryservice.Client, identifier string, tags []awstypes.Tag, optFns ...func(*directoryservice.Options)) error {
 	if len(tags) == 0 {
 		return nil
 	}
 
-	return updateTags(ctx, conn, identifier, nil, KeyValueTags(ctx, tags))
+	return updateTags(ctx, conn, identifier, nil, KeyValueTags(ctx, tags), optFns...)
 }
 
 // updateTags updates ds service tags.
