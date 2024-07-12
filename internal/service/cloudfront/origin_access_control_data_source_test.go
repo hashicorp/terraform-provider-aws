cf58e1c8fa2441609de6ccce0fcccceba2a00ef8
Adrian Johnson <adrian.johnson@hashicorp.com>
2024-07-12 11:28:54 -0500

    aws_cloudfront_origin_access_control: cleanup test

--- internal/service/cloudfront/origin_access_control_data_source_test.go
+++ internal/service/cloudfront/origin_access_control_data_source_test.go
@@ -11,9 +11,10 @@ import (
 	sdkacctest "github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
-func TestAccCloudFrontOriginAccessControlDataSource_description(t *testing.T) {
+func TestAccCloudFrontOriginAccessControlDataSource_basic(t *testing.T) {
 	ctx := acctest.Context(t)
 	dataSourceName := "data.aws_cloudfront_origin_access_control.this"
 	resourceName := "aws_cloudfront_origin_access_control.this"
@@ -26,68 +27,26 @@ func TestAccCloudFrontOriginAccessControlDataSource_description(t *testing.T) {
 		CheckDestroy:             testAccCheckOriginAccessControlDestroy(ctx),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccOriginAccessControlDataSourceConfig_description(rName, "Acceptance Test 1"),
+				Config: testAccOriginAccessControlDataSourceConfig_basic(rName),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrSet(dataSourceName, "etag"),
-					resource.TestCheckTypeSetElemAttrPair(dataSourceName, names.AttrDescription, resourceName, names.AttrDescription),
-					resource.TestCheckTypeSetElemAttrPair(dataSourceName, names.AttrID, resourceName, names.AttrID),
-					resource.TestCheckTypeSetElemAttrPair(dataSourceName, names.AttrName, resourceName, names.AttrName),
-					resource.TestCheckTypeSetElemAttrPair(dataSourceName, "origin_access_control_origin_type", resourceName, "origin_access_control_origin_type"),
-					resource.TestCheckTypeSetElemAttrPair(dataSourceName, "signing_behavior", resourceName, "signing_behavior"),
-					resource.TestCheckTypeSetElemAttrPair(dataSourceName, "signing_protocol", resourceName, "signing_protocol"),
+					resource.TestCheckResourceAttrPair(dataSourceName, names.AttrDescription, resourceName, names.AttrDescription),
+					resource.TestCheckResourceAttrPair(dataSourceName, names.AttrID, resourceName, names.AttrID),
+					resource.TestCheckResourceAttrPair(dataSourceName, names.AttrName, resourceName, names.AttrName),
+					resource.TestCheckResourceAttrPair(dataSourceName, "origin_access_control_origin_type", resourceName, "origin_access_control_origin_type"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "signing_behavior", resourceName, "signing_behavior"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "signing_protocol", resourceName, "signing_protocol"),
 				),
 			},
 		},
 	})
 }
 
-func TestAccCloudFrontOriginAccessControlDataSource_noDescription(t *testing.T) {
-	ctx := acctest.Context(t)
-	dataSourceName := "data.aws_cloudfront_origin_access_control.this"
-	resourceName := "aws_cloudfront_origin_access_control.this"
-	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
-
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:                 func() { acctest.PreCheck(ctx, t); acctest.PreCheckPartitionHasService(t, cloudfront.EndpointsID) },
-		ErrorCheck:               acctest.ErrorCheck(t, cloudfront.EndpointsID),
-		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
-		CheckDestroy:             testAccCheckOriginAccessControlDestroy(ctx),
-		Steps: []resource.TestStep{
-			{
-				Config: testAccOriginAccessControlDataSourceConfig_noDescription(rName),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrSet(dataSourceName, "etag"),
-					resource.TestCheckTypeSetElemAttrPair(dataSourceName, names.AttrID, resourceName, names.AttrID),
-					resource.TestCheckTypeSetElemAttrPair(dataSourceName, names.AttrName, resourceName, names.AttrName),
-					resource.TestCheckTypeSetElemAttrPair(dataSourceName, "origin_access_control_origin_type", resourceName, "origin_access_control_origin_type"),
-					resource.TestCheckTypeSetElemAttrPair(dataSourceName, "signing_behavior", resourceName, "signing_behavior"),
-					resource.TestCheckTypeSetElemAttrPair(dataSourceName, "signing_protocol", resourceName, "signing_protocol"),
-				),
-			},
-		},
-	})
-}
-
-func testAccOriginAccessControlDataSourceConfig_description(rName, description string) string {
+func testAccOriginAccessControlDataSourceConfig_basic(rName string) string {
 	return fmt.Sprintf(`
 resource "aws_cloudfront_origin_access_control" "this" {
   name                              = %[1]q
-  description                       = %[2]q
-  origin_access_control_origin_type = "s3"
-  signing_behavior                  = "always"
-  signing_protocol                  = "sigv4"
-}
-
-data "aws_cloudfront_origin_access_control" "this" {
-  id = aws_cloudfront_origin_access_control.this.id
-}
-`, rName, description)
-}
-
-func testAccOriginAccessControlDataSourceConfig_noDescription(rName string) string {
-	return fmt.Sprintf(`
-resource "aws_cloudfront_origin_access_control" "this" {
-  name                              = %[1]q
+  description                       = %[1]q
   origin_access_control_origin_type = "s3"
   signing_behavior                  = "always"
   signing_protocol                  = "sigv4"
