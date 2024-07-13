f3f52326c93a34fc5d79bda64c933e94b4a39a29
Daniel Rieske <daniel.rieske@gmail.com>
2024-07-13 23:06:20 +0200

    feat: migrate ec2 resources in acctest.go

--- internal/acctest/acctest.go
+++ internal/acctest/acctest.go
@@ -40,7 +40,6 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/arn"
 	"github.com/aws/aws-sdk-go/aws/endpoints"
-	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/service/outposts"
 	"github.com/aws/aws-sdk-go/service/pinpoint"
 	"github.com/hashicorp/aws-sdk-go-base/v2/awsv1shim/v2/tfawserr"
@@ -2259,14 +2258,14 @@ data "aws_ami" "amzn2-ami-minimal-hvm-ebs-%[1]s" {
 // describes the latest Amazon Linux 2 x86_64 AMI using HVM virtualization and an EBS root device.
 // The data source is named 'amzn2-ami-minimal-hvm-ebs-x86_64'.
 func ConfigLatestAmazonLinux2HVMEBSX8664AMI() string {
-	return configLatestAmazonLinux2HVMEBSAMI(ec2.ArchitectureValuesX8664)
+	return configLatestAmazonLinux2HVMEBSAMI(string(ec2types.ArchitectureValuesX8664))
 }
 
 // ConfigLatestAmazonLinux2HVMEBSARM64AMI returns the configuration for a data source that
 // describes the latest Amazon Linux 2 arm64 AMI using HVM virtualization and an EBS root device.
 // The data source is named 'amzn2-ami-minimal-hvm-ebs-arm64'.
 func ConfigLatestAmazonLinux2HVMEBSARM64AMI() string {
-	return configLatestAmazonLinux2HVMEBSAMI(ec2.ArchitectureValuesArm64)
+	return configLatestAmazonLinux2HVMEBSAMI(string(ec2types.ArchitectureValuesArm64))
 }
 
 func ConfigLambdaBase(policyName, roleName, sgName string) string {
