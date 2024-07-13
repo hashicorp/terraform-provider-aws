d7ffdb49521d1c0fe12ee614be6c192239c77f54
Daniel Rieske <daniel.rieske@gmail.com>
2024-07-13 16:47:26 +0200

    chore: reduce visibility d/prefix_list

--- internal/service/ec2/vpc_prefix_list_data_source.go
+++ internal/service/ec2/vpc_prefix_list_data_source.go
@@ -17,8 +17,8 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
-// @SDKDataSource("aws_prefix_list")
-func DataSourcePrefixList() *schema.Resource {
+// @SDKDataSource("aws_prefix_list", name="Prefix List")
+func dataSourcePrefixList() *schema.Resource {
 	return &schema.Resource{
 		ReadWithoutTimeout: dataSourcePrefixListRead,
 
