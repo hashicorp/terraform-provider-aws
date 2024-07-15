1761cf18fd8ac19cf17455453bb85479196a3372
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-07-15 13:17:47 -0400

    Revert "r/aws_codebuild_project: Tweak Update."
    This reverts commit 9fa17f715e99bd51a2278291caddfd2e1d34035a.

--- internal/service/codebuild/project.go
+++ internal/service/codebuild/project.go
@@ -948,7 +948,7 @@ func resourceProjectUpdate(ctx context.Context, d *schema.ResourceData, meta int
 		}
 	}
 
-	if d.HasChangesExcept("project_visibility", "resource_access_role", names.AttrTags, names.AttrTagsAll) {
+	if d.HasChangesExcept("project_visibility", "resource_access_role") {
 		input := &codebuild.UpdateProjectInput{
 			Name: aws.String(d.Get(names.AttrName).(string)),
 		}
