40d7c91ebe5354559d4406545f434bf158f8807b
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-07-17 12:32:45 -0400

    d/aws_ecs_cluster: Reduce visibility.

--- internal/service/ecs/service_package_gen.go
+++ internal/service/ecs/service_package_gen.go
@@ -25,8 +25,10 @@ func (p *servicePackage) FrameworkResources(ctx context.Context) []*types.Servic
 func (p *servicePackage) SDKDataSources(ctx context.Context) []*types.ServicePackageSDKDataSource {
 	return []*types.ServicePackageSDKDataSource{
 		{
-			Factory:  DataSourceCluster,
+			Factory:  dataSourceCluster,
 			TypeName: "aws_ecs_cluster",
+			Name:     "Cluster",
+			Tags:     &types.ServicePackageResourceTags{},
 		},
 		{
 			Factory:  DataSourceContainerDefinition,
