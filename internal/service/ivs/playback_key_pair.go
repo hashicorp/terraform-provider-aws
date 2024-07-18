42b648c874701abe7f7f0c12460213f61a598e94
Matt Burgess <549318+mattburgess@users.noreply.github.com>
2024-07-18 21:42:53 +0100

    r/ivs_playback_key_pair: Migrate to AWS SDK v2

--- internal/service/ivs/playback_key_pair.go
+++ internal/service/ivs/playback_key_pair.go
@@ -12,7 +12,6 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/ivs"
 	awstypes "github.com/aws/aws-sdk-go-v2/service/ivs/types"
-	"github.com/hashicorp/aws-sdk-go-base/v2/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
