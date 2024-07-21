fefbfbeaf0c5f3b1eb973f6b96568bf9fe71bf92
Hubbard Harvey <hcubedcoder@gmail.com>
2024-07-21 14:48:01 -0400

    email_identity_test: Migrate to AWS SDK v2

--- internal/service/ses/email_identity_test.go
+++ internal/service/ses/email_identity_test.go
@@ -11,9 +11,7 @@ import (
 	"testing"
 
 	"github.com/YakDriver/regexache"
-	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/ses"
-	awstypes "github.com/aws/aws-sdk-go-v2/service/ses/types"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
