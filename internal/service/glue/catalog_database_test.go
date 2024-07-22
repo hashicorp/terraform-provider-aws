8beffc775ff841544953c347a6295eaf50b9440e
Adrian Johnson <adrian.johnson@hashicorp.com>
2024-07-22 11:38:01 -0500

    aws_glue_catalog_table: add lakeformation permissions to test

--- internal/service/glue/catalog_database_test.go
+++ internal/service/glue/catalog_database_test.go
@@ -360,6 +360,14 @@ data "aws_region" "current" {}
 data "aws_partition" "current" {}
 data "aws_caller_identity" "current" {}
 
+data "aws_iam_session_context" "current" {
+  arn = data.aws_caller_identity.current.arn
+}
+
+resource "aws_lakeformation_data_lake_settings" "test" {
+  admins = [data.aws_iam_session_context.current.issuer_arn]
+}
+
 resource "aws_redshiftserverless_namespace" "test" {
   namespace_name = %[1]q
   db_name        = "test"
