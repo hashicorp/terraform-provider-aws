e9ac9ad5833ab0eb7b089970b6a9a49d9b76aff5
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-07-23 15:47:21 -0400

    Fix semgrep 'ci.semgrep.migrate.error-check-service-id'.

--- internal/service/rds/cluster_test.go
+++ internal/service/rds/cluster_test.go
@@ -1207,7 +1207,7 @@ func TestAccRDSCluster_copyTagsToSnapshot_restorePointInTime(t *testing.T) {
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
-		ErrorCheck:               acctest.ErrorCheck(t, rds.EndpointsID),
+		ErrorCheck:               acctest.ErrorCheck(t, names.RDSServiceID),
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
 		CheckDestroy:             testAccCheckClusterDestroy(ctx),
 		Steps: []resource.TestStep{
