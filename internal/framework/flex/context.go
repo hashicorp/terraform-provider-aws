0cdea3811a203cd7de967057f28f92d78ac6e6cc
Graham Davison <gdavison@hashicorp.com>
2024-07-24 12:29:05 -0700

    Correctly handles log levels

--- internal/framework/flex/context.go
+++ internal/framework/flex/context.go
@@ -5,13 +5,30 @@ package flex
 
 import (
 	"context"
+	"os"
 
+	"github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 )
 
 func RegisterLogger(ctx context.Context) context.Context {
+	// tflog.WithLevelFromEnv() does not accomodate a custom default level,
+	// so manage it ourselves here
+	level := defaultLogLevel
+	if l := hclog.LevelFromString(os.Getenv(envvar)); l != hclog.NoLevel {
+		level = l
+	}
+
+	return registerLogger(ctx, level)
+}
+
+func registerTestingLogger(ctx context.Context) context.Context {
+	return registerLogger(ctx, hclog.NoLevel)
+}
+
+func registerLogger(ctx context.Context, logLevel hclog.Level) context.Context {
 	return tflog.NewSubsystem(ctx, subsystemName,
-		tflog.WithLevelFromEnv(envvar),
+		tflog.WithLevel(logLevel),
 		tflog.WithRootFields(),
 	)
 }
