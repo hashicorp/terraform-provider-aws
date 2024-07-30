b11625a2e6aeefd3407d310d85ba2281cd131751
corymhall <43035978+corymhall@users.noreply.github.com>
2024-07-30 15:46:09 -0400

    fix(opensearchserverless): security_policy policy attribute doesn't normalize json strings
    The `policy` attribute is a string type, but requires the string to be
    valid json. The user can input json with the keys in any order, but the
    value stored in state will have the keys in order causing an error
    message on apply (see linked issue for details).
    
    This resource is very similar to the `access_policy` resource which uses
    `jsontypes.Normalize` as the type instead of `types.String`. I have
    copied most of the implementation from `access_policy` and added tests
    which test using a string value instead of `jsonencode`.
    
    fixes #38603

--- internal/service/opensearchserverless/security_policy_test.go
+++ internal/service/opensearchserverless/security_policy_test.go
@@ -56,6 +56,47 @@ func TestAccOpenSearchServerlessSecurityPolicy_basic(t *testing.T) {
 	})
 }
 
+func TestAccOpenSearchServerlessSecurityPolicy_string(t *testing.T) {
+	ctx := acctest.Context(t)
+	var securitypolicy types.SecurityPolicyDetail
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	resourceName := "aws_opensearchserverless_security_policy.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+			acctest.PreCheckPartitionHasService(t, names.OpenSearchServerlessEndpointID)
+			testAccPreCheck(ctx, t)
+		},
+		ErrorCheck:               acctest.ErrorCheck(t, names.OpenSearchServerlessServiceID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckSecurityPolicyDestroy(ctx),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSecurityPolicyConfig_string(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckSecurityPolicyExists(ctx, resourceName, &securitypolicy),
+					resource.TestCheckResourceAttr(resourceName, names.AttrType, "encryption"),
+					resource.TestCheckResourceAttr(resourceName, names.AttrDescription, rName),
+				),
+			},
+			{
+				Config:             testAccSecurityPolicyConfig_string(rName),
+				PlanOnly:           true,
+				ExpectNonEmptyPlan: false,
+			},
+			{
+				ResourceName:            resourceName,
+				ImportStateIdFunc:       testAccSecurityPolicyImportStateIdFunc(resourceName),
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"policy"}, // JSON is semantically correct but can be set in state in a different order
+
+			},
+		},
+	})
+}
+
 func TestAccOpenSearchServerlessSecurityPolicy_update(t *testing.T) {
 	ctx := acctest.Context(t)
 	var securitypolicy types.SecurityPolicyDetail
@@ -82,6 +123,42 @@ func TestAccOpenSearchServerlessSecurityPolicy_update(t *testing.T) {
 			},
 			{
 				Config: testAccSecurityPolicyConfig_update(rName, "description updated"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckSecurityPolicyExists(ctx, resourceName, &securitypolicy),
+					resource.TestCheckResourceAttr(resourceName, names.AttrType, "encryption"),
+					resource.TestCheckResourceAttr(resourceName, names.AttrDescription, "description updated"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccOpenSearchServerlessSecurityPolicy_string_update(t *testing.T) {
+	ctx := acctest.Context(t)
+	var securitypolicy types.SecurityPolicyDetail
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	resourceName := "aws_opensearchserverless_security_policy.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+			acctest.PreCheckPartitionHasService(t, names.OpenSearchServerlessEndpointID)
+			testAccPreCheck(ctx, t)
+		},
+		ErrorCheck:               acctest.ErrorCheck(t, names.OpenSearchServerlessServiceID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckSecurityPolicyDestroy(ctx),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSecurityPolicyConfig_string_update(rName, names.AttrDescription),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckSecurityPolicyExists(ctx, resourceName, &securitypolicy),
+					resource.TestCheckResourceAttr(resourceName, names.AttrType, "encryption"),
+					resource.TestCheckResourceAttr(resourceName, names.AttrDescription, names.AttrDescription),
+				),
+			},
+			{
+				Config: testAccSecurityPolicyConfig_string_update(rName, "description updated"),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckSecurityPolicyExists(ctx, resourceName, &securitypolicy),
 					resource.TestCheckResourceAttr(resourceName, names.AttrType, "encryption"),
@@ -221,6 +298,18 @@ resource "aws_opensearchserverless_security_policy" "test" {
 `, rName, collection)
 }
 
+func testAccSecurityPolicyConfig_string(rName string) string {
+	collection := fmt.Sprintf("collection/%s", rName)
+	return fmt.Sprintf(`
+resource "aws_opensearchserverless_security_policy" "test" {
+  name        = %[1]q
+  type        = "encryption"
+  description = %[1]q
+  policy      = "{\"Rules\":[{\"Resource\":[\"%[2]s\"],\"ResourceType\":\"collection\"}],\"AWSOwnedKey\":true}"
+}
+`, rName, collection)
+}
+
 func testAccSecurityPolicyConfig_update(rName, description string) string {
 	collection := fmt.Sprintf("collection/%s", rName)
 	return fmt.Sprintf(`
@@ -239,6 +328,18 @@ resource "aws_opensearchserverless_security_policy" "test" {
     ],
     "AWSOwnedKey" = true
   })
+}
+`, rName, collection, description)
+}
+
+func testAccSecurityPolicyConfig_string_update(rName, description string) string {
+	collection := fmt.Sprintf("collection/%s", rName)
+	return fmt.Sprintf(`
+resource "aws_opensearchserverless_security_policy" "test" {
+  name        = %[1]q
+  type        = "encryption"
+  description = %[3]q
+	policy      = "{\"Rules\":[{\"Resource\":[\"%[2]s\"],\"ResourceType\":\"collection\"}],\"AWSOwnedKey\": true }"
 }
 `, rName, collection, description)
 }
