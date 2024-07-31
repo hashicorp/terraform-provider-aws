8acc1916f95f1e3cbaf1954b1c2f75386398de1d
Matt Dainty <matt@bodgit-n-scarper.com>
2024-07-31 10:46:59 +0100

    d/aws_ecr_repository_creation_template: New data source.

--- internal/service/ecr/service_package_gen.go
+++ internal/service/ecr/service_package_gen.go
@@ -56,6 +56,11 @@ func (p *servicePackage) SDKDataSources(ctx context.Context) []*types.ServicePac
 				IdentifierAttribute: names.AttrARN,
 			},
 		},
+		{
+			Factory:  dataSourceRepositoryCreationTemplate,
+			TypeName: "aws_ecr_repository_creation_template",
+			Name:     "Repository Creation Template",
+		},
 	}
 }
 
