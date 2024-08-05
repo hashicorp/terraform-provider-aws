4590b1b522a65aabd6c7bb2e47fde14445a51d33
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-08-05 10:38:50 -0400

    Import 'names'.

--- internal/service/imagebuilder/sweep.go
+++ internal/service/imagebuilder/sweep.go
@@ -14,6 +14,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 	"github.com/hashicorp/terraform-provider-aws/internal/sweep/awsv2"
 	"github.com/hashicorp/terraform-provider-aws/internal/sweep/framework"
+	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
 func RegisterSweepers() {
