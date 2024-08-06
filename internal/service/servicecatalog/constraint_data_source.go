aa04df26e052c8cd159b7306320e8bd01233c61c
Daniel Rieske <daniel.rieske@gmail.com>
2024-08-06 09:09:58 +0200

    feat: f/constraint reduce visibility and added name

--- internal/service/servicecatalog/constraint_data_source.go
+++ internal/service/servicecatalog/constraint_data_source.go
@@ -15,8 +15,8 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
-// @SDKDataSource("aws_servicecatalog_constraint")
-func DataSourceConstraint() *schema.Resource {
+// @SDKDataSource("aws_servicecatalog_constraint", name="Constraint")
+func dataSourceConstraint() *schema.Resource {
 	return &schema.Resource{
 		ReadWithoutTimeout: dataSourceConstraintRead,
 
