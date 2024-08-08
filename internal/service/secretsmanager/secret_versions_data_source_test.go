aca8b57069160f1d28809e4826310e42d6115171
Sharon Nam <sharon.nam@sharon.nam-PJCJ4F7JH7>
2024-08-07 17:17:08 -0700

    Tests

--- internal/service/secretsmanager/secret_versions_data_source_test.go
+++ internal/service/secretsmanager/secret_versions_data_source_test.go
@@ -5,13 +5,10 @@ package secretsmanager_test
 
 import (
 	"fmt"
-	"strconv"
 	"testing"
 
-	"github.com/YakDriver/regexache"
 	sdkacctest "github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
-	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
@@ -19,214 +16,85 @@ import (
 func TestAccSecretsManagerSecretVersionsDataSource_basic(t *testing.T) {
 	ctx := acctest.Context(t)
 	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
-	resource1Name := "aws_secretsmanager_secret_version.test1"
-	resource2Name := "aws_secretsmanager_secret_version.test2"
-	datasourceName := "data.aws_secretsmanager_secret_versions.test"
+	resource1Name := "aws_secretsmanager_secret_version.test"
+	dataSourceName := "data.aws_secretsmanager_secret_versions.test"
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:                 func() { acctest.PreCheck(ctx, t); testAccPreCheck(ctx, t) },
-		ErrorCheck:               acctest.ErrorCheck(t, names.SecretsManagerEndpointID),
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+			testAccPreCheck(ctx, t)
+		},
+		ErrorCheck:               acctest.ErrorCheck(t, names.SecretsManagerServiceID),
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config:      testAccSecretVersionsDataSourceConfig_nonExistent,
-				ExpectError: regexache.MustCompile(`couldn't find resource`),
-			},
-			{
-				Config: testAccSecretVersionsDataSourceConfig_noVersion(rName),
+				Config: testAccSecretVersionsDataSourceConfig_basic(rName),
 				Check: resource.ComposeTestCheckFunc(
-					testAccSecretVersionsCheckDataSource(datasourceName, "", "", 0),
-				),
-			},
-			{
-				Config: testAccSecretVersionsDataSourceConfig_oneVersion(rName),
-				Check: resource.ComposeTestCheckFunc(
-					testAccSecretVersionsCheckDataSource(datasourceName, resource1Name, "", 1),
-				),
-			},
-			{
-				Config: testAccSecretVersionsDataSourceConfig_twoVersions(rName),
-				Check: resource.ComposeTestCheckFunc(
-					testAccSecretVersionsCheckDataSource(datasourceName, resource1Name, resource2Name, 2),
+					resource.TestCheckResourceAttrPair(dataSourceName, "secret_id", resource1Name, "secret_id"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "versions.#"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "versions.0.%"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "versions.0.created_time"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "versions.0.version_id"),
 				),
 			},
 		},
 	})
 }
 
-func TestAccSecretsManagerSecretVersionsDataSource_maxResults(t *testing.T) {
+func TestAccSecretsManagerSecretVersionsDataSource_emptyVer(t *testing.T) {
 	ctx := acctest.Context(t)
 	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
-	resource1Name := "aws_secretsmanager_secret_version.test1"
-	datasourceName := "data.aws_secretsmanager_secret_versions.test"
+	dataSourceName := "data.aws_secretsmanager_secret_versions.test"
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:                 func() { acctest.PreCheck(ctx, t); testAccPreCheck(ctx, t) },
-		ErrorCheck:               acctest.ErrorCheck(t, names.SecretsManagerEndpointID),
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+			testAccPreCheck(ctx, t)
+		},
+		ErrorCheck:               acctest.ErrorCheck(t, names.SecretsManagerServiceID),
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccSecretVersionsDataSourceConfig_maxResults(rName),
+				Config: testAccSecretVersionsDataSourceConfig_emptyVersion(rName),
 				Check: resource.ComposeTestCheckFunc(
-					testAccSecretVersionsCheckDataSource(datasourceName, resource1Name, "", 1),
+					resource.TestCheckResourceAttr(dataSourceName, "versions.#", "0"),
 				),
 			},
 		},
 	})
 }
 
-func testAccSecretVersionsCheckDataSource(datasourceName, resource1Name, resource2Name string, versionCount int) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		dataSource, ok := s.RootModule().Resources[datasourceName]
-		if !ok {
-			return fmt.Errorf("root module has no data source called %s", datasourceName)
-		}
-
-		resource1, ok := s.RootModule().Resources[resource1Name]
-		if resource1Name != "" && !ok {
-			return fmt.Errorf("root module has no resource called %s", resource1Name)
-		}
-
-		resource2, ok := s.RootModule().Resources[resource2Name]
-		if resource2Name != "" && !ok {
-			return fmt.Errorf("root module has no resource called %s", resource2Name)
-		}
-
-		if dataSource.Primary.Attributes["versions.#"] != strconv.Itoa(versionCount) {
-			return fmt.Errorf(
-				"versions.# is %s; want %d",
-				dataSource.Primary.Attributes["versions.#"],
-				versionCount,
-			)
-		}
-
-		checkAttributes := func(attrNames []string, resourceAttributes map[string]string) error {
-			for _, attrName := range attrNames {
-				if resourceAttributes[attrName] != dataSource.Primary.Attributes[attrName] {
-					return fmt.Errorf(
-						"%s is %s; want %s",
-						attrName,
-						resourceAttributes[attrName],
-						dataSource.Primary.Attributes[attrName],
-					)
-				}
-			}
-			return nil
-		}
-
-		if resource1Name != "" {
-			attrNamesResource1 := []string{
-				"arn",
-				"secret_id",
-				"versions.0,created_date",
-				"versions.0,version_id",
-				"versions.0,version_stages.#",
-				"versions.0,last_accessed_date",
-			}
-			if err := checkAttributes(attrNamesResource1, resource1.Primary.Attributes); err != nil {
-				return err
-			}
-		}
-
-		if resource2Name != "" {
-			attrNamesResource2 := []string{
-				"arn",
-				"secret_id",
-				"versions.1,created_date",
-				"versions.1,version_id",
-				"versions.1,version_stages.#",
-				"versions.1,last_accessed_date",
-			}
-			if err := checkAttributes(attrNamesResource2, resource2.Primary.Attributes); err != nil {
-				return err
-			}
-		}
-
-		return nil
-	}
-}
-
-const testAccSecretVersionsDataSourceConfig_nonExistent = `
-data "aws_secretsmanager_secret_versions" "test" {
-  secret_id = "tf-acc-test-does-not-exist"
-}
-`
-
-func testAccSecretVersionsDataSourceConfig_noVersion(rName string) string {
+func testAccSecretVersionsDataSourceConfig_basic(rName string) string {
 	return fmt.Sprintf(`
 resource "aws_secretsmanager_secret" "test" {
-  name = "%[1]s"
+  name = %[1]q
 }
 
-data "aws_secretsmanager_secret_versions" "test" {
-  secret_id = aws_secretsmanager_secret.test.id
-}
-`, rName)
-}
-
-func testAccSecretVersionsDataSourceConfig_oneVersion(rName string) string {
-	return fmt.Sprintf(`
-resource "aws_secretsmanager_secret" "test" {
-  name = "%[1]s"
-}
-
-resource "aws_secretsmanager_secret_version" "test1" {
-  secret_id     = aws_secretsmanager_secret.test.id
-  secret_string = "test-string"
-}
-  
-data "aws_secretsmanager_secret_versions" "test" {
-  depends_on = [aws_secretsmanager_secret_version.test1]
-  secret_id = aws_secretsmanager_secret.test.id
-}
-`, rName)
-}
-
-func testAccSecretVersionsDataSourceConfig_twoVersions(rName string) string {
-	return fmt.Sprintf(`
-resource "aws_secretsmanager_secret" "test" {
-  name = "%[1]s"
-}
-
-resource "aws_secretsmanager_secret_version" "test1" {
+resource "aws_secretsmanager_secret_version" "test" {
   secret_id     = aws_secretsmanager_secret.test.id
   secret_string = "test-string"
 }
 
 resource "aws_secretsmanager_secret_version" "test2" {
-  depends_on = [aws_secretsmanager_secret_version.test1]
+  depends_on = [aws_secretsmanager_secret_version.test]
   secret_id     = aws_secretsmanager_secret.test.id
   secret_string = "test-string2"
 }
 
 data "aws_secretsmanager_secret_versions" "test" {
-  depends_on = [aws_secretsmanager_secret_version.test1, aws_secretsmanager_secret_version.test2]
+  depends_on = [aws_secretsmanager_secret_version.test]
   secret_id = aws_secretsmanager_secret.test.id
 }
 `, rName)
 }
 
-func testAccSecretVersionsDataSourceConfig_maxResults(rName string) string {
+func testAccSecretVersionsDataSourceConfig_emptyVersion(rName string) string {
 	return fmt.Sprintf(`
 resource "aws_secretsmanager_secret" "test" {
   name = "%[1]s"
 }
-
-resource "aws_secretsmanager_secret_version" "test1" {
-  secret_id     = aws_secretsmanager_secret.test.id
-  secret_string = "test-string"
-}
-
-resource "aws_secretsmanager_secret_version" "test2" {
-  depends_on = [aws_secretsmanager_secret_version.test1]
-  secret_id     = aws_secretsmanager_secret.test.id
-  secret_string = "test-string2"
-}
-
 data "aws_secretsmanager_secret_versions" "test" {
-  depends_on = [aws_secretsmanager_secret_version.test1, aws_secretsmanager_secret_version.test2]
   secret_id = aws_secretsmanager_secret.test.id
-  max_results = 1
 }
 `, rName)
 }
