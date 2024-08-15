a6011cf232d019a4a53a97da62f221847d3d3ea9
dimagolomozy <dima.golomozy@gmail.com>
2024-08-15 23:04:07 +0300

    DT-2401: ebs-volume

--- internal/datafy/client.go
+++ internal/datafy/client.go
@@ -1,8 +1,15 @@
 package datafy
 
 import (
+	awstypes "github.com/aws/aws-sdk-go-v2/service/ec2/types"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/datafy-io/iac-gateway-lambda/iacgateway"
+	"strings"
 )
+
+func apiNotFound(err error) bool {
+	return strings.Contains(err.Error(), "404")
+}
 
 type Client struct {
 	config Config
@@ -15,4 +22,22 @@ func NewDatafyClient(config *Config) *Client {
 		config: *config,
 		client: iacgateway.NewAwsClient(config.Url, config.Token),
 	}
+}
+
+func (c *Client) GetVolume(volumeId string) (*Volume, error) {
+	volume, err := c.client.GetVolume(volumeId)
+	if err != nil {
+		if apiNotFound(err) {
+			return nil, NotFoundError
+		}
+		return nil, err
+	}
+
+	return &Volume{
+		Volume: &awstypes.Volume{
+			VolumeId: aws.String(volume.VolumeId),
+		},
+		IsManaged:     volume.IsManaged,
+		IsReplacement: volume.IsReplacement,
+	}, nil
 }
--- internal/datafy/types.go
+++ internal/datafy/types.go
@@ -1,1 +1,10 @@
 package datafy
+
+import "github.com/aws/aws-sdk-go-v2/service/ec2/types"
+
+type Volume struct {
+	*types.Volume
+
+	IsManaged     bool
+	IsReplacement bool
+}
