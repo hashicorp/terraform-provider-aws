64e07e65a39e87573ac468b8c7c74e2aead74cc9
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-08-19 10:31:07 -0400

    Revert "Cleaned up schema"
    This reverts commit 1597e71c059cffcb354f914e4d1fb7d294dab02a.

--- aws/data_source_aws_route53_zones.go
+++ aws/data_source_aws_route53_zones.go
@@ -14,7 +14,16 @@ func dataSourceAwsRoute53Zones() *schema.Resource {
 	return &schema.Resource{
 		Read: dataSourceAwsRoute53ZonesRead,
 		Schema: map[string]*schema.Schema{
+			"filter": ec2CustomFiltersSchema(),
+
 			"tags": tagsSchemaComputed(),
+
+			"ids": {
+				Type:     schema.TypeSet,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Set:      schema.HashString,
+			},
 		},
 	}
 }
