24c1ef0ca6ab1dd4fed29e169e1e988bee131ba1
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-08-19 10:30:49 -0400

    Revert "Created a basic read function"
    This reverts commit 81ab6e20ebd64ef444082e1215c3602eacb771b1.

--- aws/data_source_aws_route53_zones.go
+++ aws/data_source_aws_route53_zones.go
@@ -2,45 +2,65 @@ package aws
 
 import (
 	"fmt"
+	"log"
+
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/route53"
+	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/terraform-providers/terraform-provider-aws/aws/internal/keyvaluetags"
 )
 
 func dataSourceAwsRoute53Zones() *schema.Resource {
 	return &schema.Resource{
 		Read: dataSourceAwsRoute53ZonesRead,
 		Schema: map[string]*schema.Schema{
-			"ids": {
-				Type:     schema.TypeSet,
-				Computed: true,
-				Elem:     &schema.Schema{Type: schema.TypeString},
-				Set:      schema.HashString,
-			},
+			"tags": tagsSchemaComputed(),
 		},
 	}
 }
 
 func dataSourceAwsRoute53ZonesRead(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*AWSClient).r53conn
-	req := &route53.ListHostedZonesInput{}
-	res, err := conn.ListHostedZones(req)
+	ignoreTagsConfig := meta.(*AWSClient).IgnoreTagsConfig
+
+	req := &ec2.DescribeVpcsInput{}
+
+	if tags, tagsOk := d.GetOk("tags"); tagsOk {
+		req.Filters = append(req.Filters, buildEC2TagFilterList(
+			keyvaluetags.New(tags.(map[string]interface{})).Ec2Tags(),
+		)...)
+	}
+
+	if filters, filtersOk := d.GetOk("filter"); filtersOk {
+		req.Filters = append(req.Filters, buildEC2CustomFilterList(
+			filters.(*schema.Set),
+		)...)
+	}
+	if len(req.Filters) == 0 {
+		// Don't send an empty filters list; the EC2 API won't accept it.
+		req.Filters = nil
+	}
+
+	log.Printf("[DEBUG] DescribeVpcs %s\n", req)
+	resp, err := conn.DescribeVpcs(req)
 	if err != nil {
 		return err
 	}
 
-	if res == nil || len(res.HostedZones) == 0 {
-		return fmt.Errorf("no maching hosted zone found")
+	if resp == nil || len(resp.Vpcs) == 0 {
+		return fmt.Errorf("no matching VPC found")
 	}
 
-	zoneIds := make([]string, 0)
-	for _, zone := range res.HostedZones {
-		zoneIds = append(zoneIds, aws.StringValue(zone.Id)) // TODO There is a unexpected "/hostedzone/" prefix
+	vpcs := make([]string, 0)
+
+	for _, vpc := range resp.Vpcs {
+		vpcs = append(vpcs, aws.StringValue(vpc.VpcId))
 	}
 
 	d.SetId(meta.(*AWSClient).region)
-	if err := d.Set("ids", zoneIds); err != nil {
-		return fmt.Errorf("error setting vpc ids: %s", err)
+
+	if err := d.Set("ids", vpcs); err != nil {
+		return fmt.Errorf("Error setting vpc ids: %s", err)
 	}
 
 	return nil
