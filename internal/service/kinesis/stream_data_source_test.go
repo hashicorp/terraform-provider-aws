29608736bc13a0ab788e4394d62e6ed9ed5110b6
Anthony Wat <acwwat@gmail.com>
2024-09-08 03:07:48 -0400

    feat: Add encryption_type and kms_key_id params to aws_kenesis_stream data source

--- internal/service/kinesis/stream_data_source_test.go
+++ internal/service/kinesis/stream_data_source_test.go
@@ -50,6 +50,35 @@ func TestAccKinesisStreamDataSource_basic(t *testing.T) {
 	})
 }
 
+func TestAccKinesisStreamDataSource_encryption(t *testing.T) {
+	ctx := acctest.Context(t)
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	dataSourceName := "data.aws_kinesis_stream.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
+		ErrorCheck:               acctest.ErrorCheck(t, names.KinesisServiceID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckStreamDestroy(ctx),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccStreamDataSourceConfig_encryption(rName, 2),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(dataSourceName, names.AttrARN),
+					resource.TestCheckResourceAttrSet(dataSourceName, "creation_timestamp"),
+					resource.TestCheckResourceAttr(dataSourceName, "closed_shards.#", acctest.Ct0),
+					resource.TestCheckResourceAttr(dataSourceName, "encryption_type", "KMS"),
+					resource.TestCheckResourceAttrPair(dataSourceName, names.AttrKMSKeyID, "aws_kms_key.test", names.AttrID),
+					resource.TestCheckResourceAttr(dataSourceName, names.AttrName, rName),
+					resource.TestCheckResourceAttr(dataSourceName, "open_shards.#", acctest.Ct2),
+					resource.TestCheckResourceAttr(dataSourceName, names.AttrStatus, "ACTIVE"),
+					resource.TestCheckResourceAttr(dataSourceName, "stream_mode_details.0.stream_mode", "PROVISIONED"),
+				),
+			},
+		},
+	})
+}
+
 func testAccStreamDataSourceConfig_basic(rName string, shardCount int) string {
 	return fmt.Sprintf(`
 resource "aws_kinesis_stream" "test" {
@@ -69,6 +98,43 @@ resource "aws_kinesis_stream" "test" {
 
 data "aws_kinesis_stream" "test" {
   name = aws_kinesis_stream.test.name
+}
+`, rName, shardCount)
+}
+func testAccStreamDataSourceConfig_encryption(rName string, shardCount int) string {
+	return fmt.Sprintf(`
+resource "aws_kinesis_stream" "test" {
+  name            = %[1]q
+  shard_count     = %[2]d
+  encryption_type = "KMS"
+  kms_key_id      = aws_kms_key.test.id
+}
+
+data "aws_kinesis_stream" "test" {
+  name = aws_kinesis_stream.test.name
+}
+
+resource "aws_kms_key" "test" {
+  description             = %[1]q
+  deletion_window_in_days = 7
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "kms-tf-1",
+  "Statement": [
+    {
+      "Sid": "Enable IAM User Permissions",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Action": "kms:*",
+      "Resource": "*"
+    }
+  ]
+}
+POLICY
 }
 `, rName, shardCount)
 }
