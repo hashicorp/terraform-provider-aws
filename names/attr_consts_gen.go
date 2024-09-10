b5b706c72d49fbb826bb955efba1ada70550082c
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-09-10 16:49:32 -0400

    Run 'make gen'.

--- names/attr_consts_gen.go
+++ names/attr_consts_gen.go
@@ -36,7 +36,6 @@ const (
 	AttrClientSecret               = "client_secret"
 	AttrCloudWatchLogGroupARN      = "cloudwatch_log_group_arn"
 	AttrCloudWatchLogs             = "cloudwatch_logs"
-	AttrCloudHSMType               = "hsm_type"
 	AttrClusterIdentifier          = "cluster_identifier"
 	AttrClusterName                = "cluster_name"
 	AttrComment                    = "comment"
