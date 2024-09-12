138bd26d88c037c10795331ff4117b223b5c6770
Graham Davison <gdavison@hashicorp.com>
2024-09-11 17:50:52 -0700

    Makes AWS SDK v2 default for `tags` generator

--- internal/service/route53/generate.go
+++ internal/service/route53/generate.go
@@ -4,7 +4,7 @@
 //go:generate go run ../../generate/listpages/main.go -ListOps=ListHostedZonesByVPC,ListVPCAssociationAuthorizations
 //go:generate go run ../../generate/listpages/main.go -ListOps=ListTrafficPolicies -Paginator=TrafficPolicyIdMarker -- list_traffic_policies_pages_gen.go
 //go:generate go run ../../generate/listpages/main.go -ListOps=ListTrafficPolicyVersions -Paginator=TrafficPolicyVersionMarker -- list_traffic_policy_versions_pages_gen.go
-//go:generate go run ../../generate/tags/main.go -AWSSDKVersion=2 -ListTags -ListTagsInIDElem=ResourceId -ListTagsOutTagsElem=ResourceTagSet.Tags -ServiceTagsSlice -TagOp=ChangeTagsForResource -TagInIDElem=ResourceId -TagInTagsElem=AddTags -TagResTypeElem=ResourceType -TagResTypeElemType=TagResourceType -UntagOp=ChangeTagsForResource -UntagInTagsElem=RemoveTagKeys -UpdateTags -CreateTags
+//go:generate go run ../../generate/tags/main.go -ListTags -ListTagsInIDElem=ResourceId -ListTagsOutTagsElem=ResourceTagSet.Tags -ServiceTagsSlice -TagOp=ChangeTagsForResource -TagInIDElem=ResourceId -TagInTagsElem=AddTags -TagResTypeElem=ResourceType -TagResTypeElemType=TagResourceType -UntagOp=ChangeTagsForResource -UntagInTagsElem=RemoveTagKeys -UpdateTags -CreateTags
 //go:generate go run ../../generate/servicepackage/main.go
 // ONLY generate directives and package declaration! Do not add anything else to this file.
 
