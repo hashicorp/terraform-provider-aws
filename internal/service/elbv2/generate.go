d89212ed0153d6764d79f2ec74e676223808906f
Graham Davison <gdavison@hashicorp.com>
2024-09-11 18:02:29 -0700

    Removes unused `-AWSSDKVersion` flags

--- internal/service/elbv2/generate.go
+++ internal/service/elbv2/generate.go
@@ -5,7 +5,7 @@
 //go:generate go run ../../generate/listpages/main.go -ListOps=DescribeRules -InputPaginator=Marker -OutputPaginator=NextMarker -- list_rules_pages_gen.go
 //go:generate go run ../../generate/tags/main.go -ListTags -ListTagsOp=DescribeTags -ListTagsInIDElem=ResourceArns -ListTagsInIDNeedValueSlice=yes -ListTagsOutTagsElem=TagDescriptions[0].Tags -ServiceTagsSlice -TagOp=AddTags -TagInIDElem=ResourceArns -TagInIDNeedValueSlice=yes -UntagOp=RemoveTags -UpdateTags -CreateTags -KVTValues
 //go:generate go run ../../generate/servicepackage/main.go
-//go:generate go run ../../generate/tagstests/main.go -AWSSDKVersion=2
+//go:generate go run ../../generate/tagstests/main.go
 // ONLY generate directives and package declaration! Do not add anything else to this file.
 
 package elbv2
