7b66d40b8bfec719a15a049e6f5b07f7b8282e7b
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-09-24 10:48:09 -0400

    'acctest.PreCheckCloudSearchAccountAllowListed' -> 'cloudsearch.testAccPreCheck'.

--- internal/acctest/acctest.go
+++ internal/acctest/acctest.go
@@ -24,7 +24,6 @@ import (
 	accounttypes "github.com/aws/aws-sdk-go-v2/service/account/types"
 	"github.com/aws/aws-sdk-go-v2/service/acmpca"
 	acmpcatypes "github.com/aws/aws-sdk-go-v2/service/acmpca/types"
-	"github.com/aws/aws-sdk-go-v2/service/cloudsearch"
 	"github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider"
 	"github.com/aws/aws-sdk-go-v2/service/directoryservice"
 	dstypes "github.com/aws/aws-sdk-go-v2/service/directoryservice/types"
@@ -1063,22 +1062,6 @@ func PreCheckCognitoIdentityProvider(ctx context.Context, t *testing.T) {
 
 	if err != nil {
 		t.Fatalf("unexpected PreCheck error: %s", err)
-	}
-}
-
-func PreCheckCloudSearchAccountAllowListed(ctx context.Context, t *testing.T) {
-	t.Helper()
-
-	conn := Provider.Meta().(*conns.AWSClient).CloudSearchClient(ctx)
-
-	_, err := conn.ListDomainNames(ctx, &cloudsearch.ListDomainNamesInput{})
-
-	if errs.MessageContains(err, "NotAuthorized", "New domain creation not supported on this account") {
-		t.Skip("skipping tests; this AWS account does not support new CloudSearch domain creation")
-	}
-
-	if err != nil {
-		t.Fatalf("listing CloudSearch Domain Names: %s", err)
 	}
 }
 
