6821570e05881c2ac7d1cd8420d1d384d8d2fd79
Graham Davison <gdavison@hashicorp.com>
2024-09-24 16:00:26 -0700

    Removes spurious `.`

--- internal/service/ecs/validate.go
+++ internal/service/ecs/validate.go
@@ -14,7 +14,7 @@ func validateClusterName(v interface{}, k string) (ws []string, errors []error) 
 	return validation.All(
 		validation.StringLenBetween(1, 255),
 		validation.StringMatch(
-			regexache.MustCompile("^[a-zA-Z0-9-_.]+$"),
+			regexache.MustCompile("^[a-zA-Z0-9-_]+$"),
 			"The cluster name must be 1-255 chars, consisting of only alphanumerics, hyphens, and underscores."),
 	)(v, k)
 }
