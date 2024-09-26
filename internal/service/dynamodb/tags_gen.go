3e86c38b37be37233ac337488dde47d0b2f3f656
Adrian Johnson <adrian.johnson@hashicorp.com>
2024-09-26 14:44:03 -0500

    generate updateTagsForResource

--- internal/service/dynamodb/tags_gen.go
+++ internal/service/dynamodb/tags_gen.go
@@ -205,7 +205,7 @@ func waitTagsPropagated(ctx context.Context, conn *dynamodb.Client, id string, t
 			output = output.IgnoreConfig(inContext.IgnoreConfig)
 		}
 
-		return output.ContainsAll(tags), nil
+		return output.Equal(tags), nil
 	}
 	opts := tfresource.WaitOpts{
 		ContinuousTargetOccurence: 2,
