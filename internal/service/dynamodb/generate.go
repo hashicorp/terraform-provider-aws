a2c3340993789b13b8fc46c2b747d7e537038bce
Adrian Johnson <adrian.johnson@hashicorp.com>
2024-09-26 10:59:30 -0500

    aws_dynamodb_table: add generated tags tests

--- internal/service/dynamodb/generate.go
+++ internal/service/dynamodb/generate.go
@@ -5,6 +5,7 @@
 //go:generate go run ../../generate/tags/main.go -GetTag -ListTags -ListTagsOp=ListTagsOfResource -ServiceTagsSlice -UpdateTags -Wait -WaitContinuousOccurence 2 -WaitMinTimeout 1s -WaitTimeout 2m -WaitFuncComparator=ContainsAll -ParentNotFoundErrCode=ResourceNotFoundException
 //go:generate go run ../../generate/servicepackage/main.go
 //go:generate go run ../../generate/listpages/main.go -ListOps=ListBackups -InputPaginator=ExclusiveStartBackupArn -OutputPaginator=LastEvaluatedBackupArn -- list_backups_pages_gen.go
+//go:generate go run ../../generate/tagstests/main.go
 // ONLY generate directives and package declaration! Do not add anything else to this file.
 
 package dynamodb
