53f682d8bcfead1fecb77ef8c716f14ad8f9fdcd
Adrian Johnson <adrian.johnson@hashicorp.com>
2024-09-26 11:43:53 -0500

    tags: factor out checkFunc

--- internal/service/dynamodb/tags_gen.go
+++ internal/service/dynamodb/tags_gen.go
@@ -167,7 +167,28 @@ func updateTags(ctx context.Context, conn *dynamodb.Client, identifier string, o
 	}
 
 	if len(removedTags) > 0 || len(updatedTags) > 0 {
-		if err := waitTagsPropagated(ctx, conn, identifier, newTags, optFns...); err != nil {
+		checkFunc := func(tags tftags.KeyValueTags) func() (bool, error) {
+			return func() (bool, error) {
+				output, err := listTags(ctx, conn, identifier, optFns...)
+
+				if tfresource.NotFound(err) {
+					return false, nil
+				}
+
+				if err != nil {
+					return false, err
+				}
+
+				if inContext, ok := tftags.FromContext(ctx); ok {
+					tags = tags.IgnoreConfig(inContext.IgnoreConfig)
+					output = output.IgnoreConfig(inContext.IgnoreConfig)
+				}
+
+				return output.Equal(tags), nil
+			}
+		}
+
+		if err := waitTagsPropagated(ctx, conn, identifier, newTags, checkFunc(newTags), optFns...); err != nil {
 			return fmt.Errorf("waiting for resource (%s) tag propagation: %w", identifier, err)
 		}
 	}
@@ -184,29 +205,11 @@ func (p *servicePackage) UpdateTags(ctx context.Context, meta any, identifier st
 // waitTagsPropagated waits for dynamodb service tags to be propagated.
 // The identifier is typically the Amazon Resource Name (ARN), although
 // it may also be a different identifier depending on the service.
-func waitTagsPropagated(ctx context.Context, conn *dynamodb.Client, id string, tags tftags.KeyValueTags, optFns ...func(*dynamodb.Options)) error {
+func waitTagsPropagated(ctx context.Context, conn *dynamodb.Client, id string, tags tftags.KeyValueTags, checkFunc func() (bool, error), optFns ...func(*dynamodb.Options)) error {
 	tflog.Debug(ctx, "Waiting for tag propagation", map[string]any{
 		names.AttrTags: tags,
 	})
 
-	checkFunc := func() (bool, error) {
-		output, err := listTags(ctx, conn, id, optFns...)
-
-		if tfresource.NotFound(err) {
-			return false, nil
-		}
-
-		if err != nil {
-			return false, err
-		}
-
-		if inContext, ok := tftags.FromContext(ctx); ok {
-			tags = tags.IgnoreConfig(inContext.IgnoreConfig)
-			output = output.IgnoreConfig(inContext.IgnoreConfig)
-		}
-
-		return output.Equal(tags), nil
-	}
 	opts := tfresource.WaitOpts{
 		ContinuousTargetOccurence: 2,
 		MinTimeout:                1 * time.Second,
