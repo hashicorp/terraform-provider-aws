1d486ec3760e9ef122ca41e614e0526e10e4e604
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-01 08:40:49 -0400

    Remove 'aws_emr_cluster.custom_ami_id' change.

--- internal/service/emr/cluster.go
+++ internal/service/emr/cluster.go
@@ -100,12 +100,6 @@ func resourceCluster() *schema.Resource {
 											},
 										},
 									},
-									"custom_ami_id": {
-										Type:         schema.TypeString,
-										ForceNew:     true,
-										Optional:     true,
-										ValidateFunc: validCustomAMIID,
-									},
 									"ebs_config": {
 										Type:     schema.TypeSet,
 										Optional: true,
