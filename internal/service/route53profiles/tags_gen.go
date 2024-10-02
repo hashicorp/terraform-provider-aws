2f9ad54d0fad799c496cd88ea712bd921eebec81
Sharon Nam <sharon.nam@sharon.nam-PJCJ4F7JH7>
2024-10-02 13:29:51 -0700

    tags gen

--- internal/service/route53profiles/tags_gen.go
+++ internal/service/route53profiles/tags_gen.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/route53profiles"
+	awstypes "github.com/aws/aws-sdk-go-v2/service/route53profiles/types"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/logging"
@@ -48,21 +49,38 @@ func (p *servicePackage) ListTags(ctx context.Context, meta any, identifier stri
 	return nil
 }
 
-// map[string]string handling
+// []*SERVICE.Tag handling
 
 // Tags returns route53profiles service tags.
-func Tags(tags tftags.KeyValueTags) map[string]string {
-	return tags.Map()
+func Tags(tags tftags.KeyValueTags) []awstypes.Tag {
+	result := make([]awstypes.Tag, 0, len(tags))
+
+	for k, v := range tags.Map() {
+		tag := awstypes.Tag{
+			Key:   aws.String(k),
+			Value: aws.String(v),
+		}
+
+		result = append(result, tag)
+	}
+
+	return result
 }
 
 // KeyValueTags creates tftags.KeyValueTags from route53profiles service tags.
-func KeyValueTags(ctx context.Context, tags map[string]string) tftags.KeyValueTags {
-	return tftags.New(ctx, tags)
+func KeyValueTags(ctx context.Context, tags []awstypes.Tag) tftags.KeyValueTags {
+	m := make(map[string]*string, len(tags))
+
+	for _, tag := range tags {
+		m[aws.ToString(tag.Key)] = tag.Value
+	}
+
+	return tftags.New(ctx, m)
 }
 
 // getTagsIn returns route53profiles service tags from Context.
 // nil is returned if there are no input tags.
-func getTagsIn(ctx context.Context) map[string]string {
+func getTagsIn(ctx context.Context) []awstypes.Tag {
 	if inContext, ok := tftags.FromContext(ctx); ok {
 		if tags := Tags(inContext.TagsIn.UnwrapOrDefault()); len(tags) > 0 {
 			return tags
@@ -73,7 +91,7 @@ func getTagsIn(ctx context.Context) map[string]string {
 }
 
 // setTagsOut sets route53profiles service tags in Context.
-func setTagsOut(ctx context.Context, tags map[string]string) {
+func setTagsOut(ctx context.Context, tags []awstypes.Tag) {
 	if inContext, ok := tftags.FromContext(ctx); ok {
 		inContext.TagsOut = option.Some(KeyValueTags(ctx, tags))
 	}
