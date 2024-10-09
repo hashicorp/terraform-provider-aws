a9a660d53bb8f776b8fb23b109d01e7cc4ce2f7a
Stefan Freitag <stefan.freitag@udo.edu>
2024-10-09 21:26:57 +0200

    feat: new datasource spot datafeed subscription

---
+++ internal/service/ec2/ec2_spot_datafeed_subscription_data_source.go
@@ -0,0 +1,69 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package ec2
+
+import (
+	"context"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/create"
+	"github.com/hashicorp/terraform-provider-aws/names"
+)
+
+// @SDKDataSource("aws_spot_datafeed_subscription", name="Spot Data Feed Subscription")
+func dataSourceSpotDataFeedSubscription() *schema.Resource {
+	return &schema.Resource{
+		ReadWithoutTimeout: dataSourceSpotDataFeedSubscriptionRead,
+
+		Schema: map[string]*schema.Schema{
+			names.AttrBucket: {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			names.AttrOwnerID: {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			names.AttrPrefix: {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			names.AttrState: {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+const (
+	DSNameSpotDataFeedSubscription = "Spot Data Feed Subscription Data Source"
+)
+
+func dataSourceSpotDataFeedSubscriptionRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	conn := meta.(*conns.AWSClient).EC2Client(ctx)
+
+	name := meta.(*conns.AWSClient).AccountID
+
+	out, err := conn.DescribeSpotDatafeedSubscription(ctx, &ec2.DescribeSpotDatafeedSubscriptionInput{})
+	if err != nil {
+		return create.AppendDiagError(diags, names.EC2, create.ErrActionReading, DSNameSpotDataFeedSubscription, name, err)
+	}
+
+	d.SetId("spot-datafeed-subscription")
+
+	subscription := out.SpotDatafeedSubscription
+
+	d.Set(names.AttrBucket, subscription.Bucket)
+	d.Set(names.AttrPrefix, subscription.Prefix)
+	d.Set(names.AttrOwnerID, subscription.OwnerId)
+	d.Set(names.AttrState, subscription.State)
+
+	return diags
+}
---
+++ website/docs/d/spot_datafeed_subscription.html.markdown
@@ -0,0 +1,28 @@
+---
+subcategory: "EC2 (Elastic Compute Cloud)"
+layout: "aws"
+page_title: "AWS: aws_spot_datafeed_subscription"
+description: |-
+  Terraform data source for accessing an AWS EC2 (Elastic Compute Cloud) spot data feed subscription.
+---
+
+# Data Source: aws_spot_datafeed_subscription
+
+-> **Note:** There is only a single subscription existing per account.
+
+Terraform data source for accessing an AWS EC2 (Elastic Compute Cloud) spot data feed subscription.
+
+## Example Usage
+
+```terraform
+data "aws_spot_datafeed_subscription" "default" {}
+```
+
+## Attribute Reference
+
+This data source exports the following attributes in addition to the arguments above:
+
+* `bucket` - The name of the Amazon S3 bucket where the spot instance data feed is located.
+* `owner_id` - The Amazon Web Services account ID of the account.
+* `prefix` - The prefix for the data feed files.
+* `state` - The state of the spot instance data feed subscription, either `Active` or `Inactive`.
