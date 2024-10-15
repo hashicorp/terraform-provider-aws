2da1a22c6554b035f82cb8e0dcd3e1f7d4e2b58a
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-15 08:07:39 -0400

    R/aws_workspaces_ip_group: Ignore ResourceNotFoundException on Delete.

--- internal/service/workspaces/ip_group.go
+++ internal/service/workspaces/ip_group.go
@@ -14,6 +14,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
@@ -171,6 +172,10 @@ func resourceIPGroupDelete(ctx context.Context, d *schema.ResourceData, meta int
 	_, err = conn.DeleteIpGroup(ctx, &workspaces.DeleteIpGroupInput{
 		GroupId: aws.String(d.Id()),
 	})
+
+	if errs.IsA[*types.ResourceNotFoundException](err) {
+		return diags
+	}
 
 	if err != nil {
 		return sdkdiag.AppendErrorf(diags, "deleting WorkSpaces IP Group (%s): %s", d.Id(), err)
