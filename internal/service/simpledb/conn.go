7d736c9678b0b6b519117d152f15535f714a28b1
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-18 17:03:25 -0400

    r/aws_simpledb_domain: Add and use 'simpleDBConn'.

---
+++ internal/service/simpledb/conn.go
@@ -0,0 +1,120 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package simpledb
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/url"
+
+	"github.com/aws/aws-sdk-go/aws"
+	endpoints_sdkv1 "github.com/aws/aws-sdk-go/aws/endpoints"
+	"github.com/aws/aws-sdk-go/service/simpledb"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/errs"
+	"github.com/hashicorp/terraform-provider-aws/names"
+)
+
+var (
+	conn *simpledb.SimpleDB
+)
+
+// Adapted from
+// internal/conns/awsclient_gen.go: func (c *AWSClient) SimpleDBConn(ctx context.Context) *simpledb_sdkv1.SimpleDB
+// internal/conns/awsclient.go: func conn[T any](ctx context.Context, c *AWSClient, servicePackageName string, extra map[string]any) (T, error)
+// internal/service/simpledb/service_package_gen.go: func (p *servicePackage) NewConn(ctx context.Context, config map[string]any) (*simpledb_sdkv1.SimpleDB, error)
+
+func simpleDBConn(ctx context.Context, c *conns.AWSClient) *simpledb.SimpleDB {
+	const servicePackageName = names.SimpleDB
+	ctx = tflog.SetField(ctx, "tf_aws.service_package", servicePackageName)
+
+	const mutexKey = "simpledb-conn"
+	conns.GlobalMutexKV.Lock(mutexKey)
+	defer conns.GlobalMutexKV.Unlock(mutexKey)
+
+	if conn != nil {
+		return conn
+	}
+
+	cfg := aws.Config{}
+
+	if endpoint := c.ResolveEndpoint(ctx, servicePackageName); endpoint != "" {
+		tflog.Debug(ctx, "setting endpoint", map[string]any{
+			"tf_aws.endpoint": endpoint,
+		})
+		cfg.Endpoint = aws.String(endpoint)
+	} else {
+		cfg.EndpointResolver = newEndpointResolverSDKv1(ctx)
+	}
+
+	conn = simpledb.New(c.AwsSession(ctx).Copy(&cfg))
+
+	return conn
+}
+
+// Copied from internal/service/simpledb/service_endpoint_resolver_gen.go.
+
+var _ endpoints_sdkv1.Resolver = resolverSDKv1{}
+
+type resolverSDKv1 struct {
+	ctx context.Context
+}
+
+func newEndpointResolverSDKv1(ctx context.Context) resolverSDKv1 {
+	return resolverSDKv1{
+		ctx: ctx,
+	}
+}
+
+func (r resolverSDKv1) EndpointFor(service, region string, opts ...func(*endpoints_sdkv1.Options)) (endpoint endpoints_sdkv1.ResolvedEndpoint, err error) {
+	ctx := r.ctx
+
+	var opt endpoints_sdkv1.Options
+	opt.Set(opts...)
+
+	useFIPS := opt.UseFIPSEndpoint == endpoints_sdkv1.FIPSEndpointStateEnabled
+
+	defaultResolver := endpoints_sdkv1.DefaultResolver()
+
+	if useFIPS {
+		ctx = tflog.SetField(ctx, "tf_aws.use_fips", useFIPS)
+
+		endpoint, err = defaultResolver.EndpointFor(service, region, opts...)
+		if err != nil {
+			return endpoint, err
+		}
+
+		tflog.Debug(ctx, "endpoint resolved", map[string]any{
+			"tf_aws.endpoint": endpoint.URL,
+		})
+
+		var endpointURL *url.URL
+		endpointURL, err = url.Parse(endpoint.URL)
+		if err != nil {
+			return endpoint, err
+		}
+
+		hostname := endpointURL.Hostname()
+		_, err = net.LookupHost(hostname)
+		if err != nil {
+			if dnsErr, ok := errs.As[*net.DNSError](err); ok && dnsErr.IsNotFound {
+				tflog.Debug(ctx, "default endpoint host not found, disabling FIPS", map[string]any{
+					"tf_aws.hostname": hostname,
+				})
+				opts = append(opts, func(o *endpoints_sdkv1.Options) {
+					o.UseFIPSEndpoint = endpoints_sdkv1.FIPSEndpointStateDisabled
+				})
+			} else {
+				err = fmt.Errorf("looking up simpledb endpoint %q: %s", hostname, err)
+				return
+			}
+		} else {
+			return endpoint, err
+		}
+	}
+
+	return defaultResolver.EndpointFor(service, region, opts...)
+}
