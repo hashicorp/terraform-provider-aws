3b715f4884443438723deddc00e3dcb665e37f69
Derek Wiedenhoeft <2476173+cailen@users.noreply.github.com>
2024-10-18 13:22:30 -0400

    fix(iam):Sort policy_document json lists alphabetically

--- internal/service/iam/policy_model.go
+++ internal/service/iam/policy_model.go
@@ -114,7 +114,6 @@ func (ps IAMPolicyStatementPrincipalSet) MarshalJSON() ([]byte, error) {
 				raw[p.Type] = make([]string, 0, len(i)+1)
 				raw[p.Type] = append(raw[p.Type].([]string), v)
 			}
-			sort.Sort(sort.Reverse(sort.StringSlice(i)))
 			raw[p.Type] = append(raw[p.Type].([]string), i...)
 		case string:
 			switch v := raw[p.Type].(type) {
@@ -133,6 +132,13 @@ func (ps IAMPolicyStatementPrincipalSet) MarshalJSON() ([]byte, error) {
 		}
 	}
 
+	// Do a sort on the combined principals set
+	for k, v := range raw {
+		if av, ok := v.([]string); ok {
+			sort.Strings(av)
+			raw[k] = av
+		}
+	}
 	return json.Marshal(&raw)
 }
 
@@ -242,7 +248,7 @@ func policyDecodeConfigStringList(lI []interface{}) interface{} {
 	for i, vI := range lI {
 		ret[i] = vI.(string)
 	}
-	sort.Sort(sort.Reverse(sort.StringSlice(ret)))
+	sort.Strings(ret)
 	return ret
 }
 
