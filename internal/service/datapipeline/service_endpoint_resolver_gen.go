4f0bc25d404813edbe6d3bcbb1f0233c89f68505
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:13 -0400

    Rename endpoint resolver struct type - datapipeline.

--- internal/service/datapipeline/service_endpoint_resolver_gen.go
+++ internal/service/datapipeline/service_endpoint_resolver_gen.go
@@ -14,19 +14,19 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 )
 
-var _ datapipeline.EndpointResolverV2 = resolverSDKv2{}
+var _ datapipeline.EndpointResolverV2 = resolverV2{}
 
-type resolverSDKv2 struct {
+type resolverV2 struct {
 	defaultResolver datapipeline.EndpointResolverV2
 }
 
-func newEndpointResolverSDKv2() resolverSDKv2 {
-	return resolverSDKv2{
+func newEndpointResolverV2() resolverV2 {
+	return resolverV2{
 		defaultResolver: datapipeline.NewDefaultEndpointResolverV2(),
 	}
 }
 
-func (r resolverSDKv2) ResolveEndpoint(ctx context.Context, params datapipeline.EndpointParameters) (endpoint smithyendpoints.Endpoint, err error) {
+func (r resolverV2) ResolveEndpoint(ctx context.Context, params datapipeline.EndpointParameters) (endpoint smithyendpoints.Endpoint, err error) {
 	params = params.WithDefaults()
 	useFIPS := aws.ToBool(params.UseFIPS)
 
