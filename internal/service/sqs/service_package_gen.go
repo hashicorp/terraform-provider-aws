db0bd45636ba123509e07736e4edc6b0320a9579
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:25 -0400

    Rename endpoint resolver struct type - sqs.

--- internal/service/sqs/service_package_gen.go
+++ internal/service/sqs/service_package_gen.go
@@ -72,7 +72,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return sqs.NewFromConfig(cfg,
-		sqs.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		sqs.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
