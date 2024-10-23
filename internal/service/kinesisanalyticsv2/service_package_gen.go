ce1b05cb53920de63aaa33eba3f7ad7375ccd10b
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:18 -0400

    Rename endpoint resolver struct type - kinesisanalyticsv2.

--- internal/service/kinesisanalyticsv2/service_package_gen.go
+++ internal/service/kinesisanalyticsv2/service_package_gen.go
@@ -53,7 +53,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return kinesisanalyticsv2.NewFromConfig(cfg,
-		kinesisanalyticsv2.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		kinesisanalyticsv2.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
