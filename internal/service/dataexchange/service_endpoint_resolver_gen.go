fbfc3efd9a0887583ea532444547944086cdce0c
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:13 -0400

    Rename endpoint resolver struct type - dataexchange.

--- internal/service/dataexchange/service_endpoint_resolver_gen.go
+++ internal/service/dataexchange/service_endpoint_resolver_gen.go
@@ -14,19 +14,19 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 )
 
-var _ dataexchange.EndpointResolverV2 = resolverSDKv2{}
+var _ dataexchange.EndpointResolverV2 = resolverV2{}
 
-type resolverSDKv2 struct {
+type resolverV2 struct {
 	defaultResolver dataexchange.EndpointResolverV2
 }
 
-func newEndpointResolverSDKv2() resolverSDKv2 {
-	return resolverSDKv2{
+func newEndpointResolverV2() resolverV2 {
+	return resolverV2{
 		defaultResolver: dataexchange.NewDefaultEndpointResolverV2(),
 	}
 }
 
-func (r resolverSDKv2) ResolveEndpoint(ctx context.Context, params dataexchange.EndpointParameters) (endpoint smithyendpoints.Endpoint, err error) {
+func (r resolverV2) ResolveEndpoint(ctx context.Context, params dataexchange.EndpointParameters) (endpoint smithyendpoints.Endpoint, err error) {
 	params = params.WithDefaults()
 	useFIPS := aws.ToBool(params.UseFIPS)
 
--- internal/service/dataexchange/service_package_gen.go
+++ internal/service/dataexchange/service_package_gen.go
@@ -56,7 +56,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return dataexchange.NewFromConfig(cfg,
-		dataexchange.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		dataexchange.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
