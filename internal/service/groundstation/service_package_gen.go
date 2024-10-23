3004789816ce1380c4aaa7fd94159e32675f1ae3
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:16 -0400

    Rename endpoint resolver struct type - groundstation.

--- internal/service/groundstation/service_package_gen.go
+++ internal/service/groundstation/service_package_gen.go
@@ -39,7 +39,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return groundstation.NewFromConfig(cfg,
-		groundstation.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		groundstation.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
