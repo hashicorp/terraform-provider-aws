f9a5f69d014ee830f92cd8016770644c3a31adbc
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:09 -0400

    Rename endpoint resolver struct type - autoscalingplans.

--- internal/service/autoscalingplans/service_package_gen.go
+++ internal/service/autoscalingplans/service_package_gen.go
@@ -44,7 +44,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return autoscalingplans.NewFromConfig(cfg,
-		autoscalingplans.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		autoscalingplans.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
