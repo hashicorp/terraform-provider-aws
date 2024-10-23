6eddff7d6fc79120de1bbad59bfa650fa9b0dbc5
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:16 -0400

    Rename endpoint resolver struct type - greengrass.

--- internal/service/greengrass/service_package_gen.go
+++ internal/service/greengrass/service_package_gen.go
@@ -39,7 +39,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return greengrass.NewFromConfig(cfg,
-		greengrass.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		greengrass.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
