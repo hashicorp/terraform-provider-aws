7bc34b9c51d40dc65139458ed0ab01fac2a6aaec
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:24 -0400

    Rename endpoint resolver struct type - secretsmanager.

--- internal/service/secretsmanager/service_package_gen.go
+++ internal/service/secretsmanager/service_package_gen.go
@@ -97,7 +97,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return secretsmanager.NewFromConfig(cfg,
-		secretsmanager.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		secretsmanager.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
