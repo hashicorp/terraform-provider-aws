da14e21158cdf7f31679d8140efd9e4821134fab
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:14 -0400

    Rename endpoint resolver struct type - drs.

--- internal/service/drs/service_package_gen.go
+++ internal/service/drs/service_package_gen.go
@@ -47,7 +47,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return drs.NewFromConfig(cfg,
-		drs.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		drs.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
