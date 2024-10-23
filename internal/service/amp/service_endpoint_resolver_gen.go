3095d688d9d71bf5e704aee756f9a7118910c59c
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:08 -0400

    Rename endpoint resolver struct type - amp.

--- internal/service/amp/service_endpoint_resolver_gen.go
+++ internal/service/amp/service_endpoint_resolver_gen.go
@@ -14,19 +14,19 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 )
 
-var _ amp.EndpointResolverV2 = resolverSDKv2{}
+var _ amp.EndpointResolverV2 = resolverV2{}
 
-type resolverSDKv2 struct {
+type resolverV2 struct {
 	defaultResolver amp.EndpointResolverV2
 }
 
-func newEndpointResolverSDKv2() resolverSDKv2 {
-	return resolverSDKv2{
+func newEndpointResolverV2() resolverV2 {
+	return resolverV2{
 		defaultResolver: amp.NewDefaultEndpointResolverV2(),
 	}
 }
 
-func (r resolverSDKv2) ResolveEndpoint(ctx context.Context, params amp.EndpointParameters) (endpoint smithyendpoints.Endpoint, err error) {
+func (r resolverV2) ResolveEndpoint(ctx context.Context, params amp.EndpointParameters) (endpoint smithyendpoints.Endpoint, err error) {
 	params = params.WithDefaults()
 	useFIPS := aws.ToBool(params.UseFIPS)
 
--- internal/service/amp/service_package_gen.go
+++ internal/service/amp/service_package_gen.go
@@ -83,7 +83,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return amp.NewFromConfig(cfg,
-		amp.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		amp.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
