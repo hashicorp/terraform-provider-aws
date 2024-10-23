a653b99a25c4a751646a1bb1877b2dd1b6bde86d
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:17 -0400

    Rename endpoint resolver struct type - iam.

--- internal/service/iam/service_package_gen.go
+++ internal/service/iam/service_package_gen.go
@@ -326,7 +326,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return iam.NewFromConfig(cfg,
-		iam.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		iam.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
