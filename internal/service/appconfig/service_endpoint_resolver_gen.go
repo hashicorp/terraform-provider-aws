2181a836130cd309400dbc98a71019529857f1db
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:08 -0400

    Rename endpoint resolver struct type - appconfig.

--- internal/service/appconfig/service_endpoint_resolver_gen.go
+++ internal/service/appconfig/service_endpoint_resolver_gen.go
@@ -14,19 +14,19 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 )
 
-var _ appconfig.EndpointResolverV2 = resolverSDKv2{}
+var _ appconfig.EndpointResolverV2 = resolverV2{}
 
-type resolverSDKv2 struct {
+type resolverV2 struct {
 	defaultResolver appconfig.EndpointResolverV2
 }
 
-func newEndpointResolverSDKv2() resolverSDKv2 {
-	return resolverSDKv2{
+func newEndpointResolverV2() resolverV2 {
+	return resolverV2{
 		defaultResolver: appconfig.NewDefaultEndpointResolverV2(),
 	}
 }
 
-func (r resolverSDKv2) ResolveEndpoint(ctx context.Context, params appconfig.EndpointParameters) (endpoint smithyendpoints.Endpoint, err error) {
+func (r resolverV2) ResolveEndpoint(ctx context.Context, params appconfig.EndpointParameters) (endpoint smithyendpoints.Endpoint, err error) {
 	params = params.WithDefaults()
 	useFIPS := aws.ToBool(params.UseFIPS)
 
--- internal/service/appconfig/service_package_gen.go
+++ internal/service/appconfig/service_package_gen.go
@@ -123,7 +123,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return appconfig.NewFromConfig(cfg,
-		appconfig.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		appconfig.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
