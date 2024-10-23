d85677389a3e64215b67430c622687a3170523d5
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:22 -0400

    Rename endpoint resolver struct type - rekognition.

--- internal/service/rekognition/service_package_gen.go
+++ internal/service/rekognition/service_package_gen.go
@@ -58,7 +58,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return rekognition.NewFromConfig(cfg,
-		rekognition.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		rekognition.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
