c4977356010e9f197c6eb7bf794a33b44fdc2f64
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:24 -0400

    Rename endpoint resolver struct type - servicecatalogappregistry.

--- internal/service/servicecatalogappregistry/service_package_gen.go
+++ internal/service/servicecatalogappregistry/service_package_gen.go
@@ -49,7 +49,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return servicecatalogappregistry.NewFromConfig(cfg,
-		servicecatalogappregistry.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		servicecatalogappregistry.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
