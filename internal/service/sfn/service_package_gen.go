3b5ea31fa0650548e09703395d849bf6535d36e0
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:25 -0400

    Rename endpoint resolver struct type - sfn.

--- internal/service/sfn/service_package_gen.go
+++ internal/service/sfn/service_package_gen.go
@@ -82,7 +82,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return sfn.NewFromConfig(cfg,
-		sfn.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		sfn.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
