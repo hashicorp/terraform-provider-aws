e2b4d568d8f8a364fe8072244a8dc2f9209003b7
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:08 -0400

    Rename endpoint resolver struct type - amplify.

--- internal/service/amplify/service_package_gen.go
+++ internal/service/amplify/service_package_gen.go
@@ -71,7 +71,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return amplify.NewFromConfig(cfg,
-		amplify.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		amplify.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
