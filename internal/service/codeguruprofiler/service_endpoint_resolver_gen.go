7e73b021dd834d19b8beb38fde2b2586351806a4
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:11 -0400

    Rename endpoint resolver struct type - codeguruprofiler.

--- internal/service/codeguruprofiler/service_endpoint_resolver_gen.go
+++ internal/service/codeguruprofiler/service_endpoint_resolver_gen.go
@@ -14,19 +14,19 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 )
 
-var _ codeguruprofiler.EndpointResolverV2 = resolverSDKv2{}
+var _ codeguruprofiler.EndpointResolverV2 = resolverV2{}
 
-type resolverSDKv2 struct {
+type resolverV2 struct {
 	defaultResolver codeguruprofiler.EndpointResolverV2
 }
 
-func newEndpointResolverSDKv2() resolverSDKv2 {
-	return resolverSDKv2{
+func newEndpointResolverV2() resolverV2 {
+	return resolverV2{
 		defaultResolver: codeguruprofiler.NewDefaultEndpointResolverV2(),
 	}
 }
 
-func (r resolverSDKv2) ResolveEndpoint(ctx context.Context, params codeguruprofiler.EndpointParameters) (endpoint smithyendpoints.Endpoint, err error) {
+func (r resolverV2) ResolveEndpoint(ctx context.Context, params codeguruprofiler.EndpointParameters) (endpoint smithyendpoints.Endpoint, err error) {
 	params = params.WithDefaults()
 	useFIPS := aws.ToBool(params.UseFIPS)
 
