6460d2a9c0e068d8af5126bbc9a35b95aa720398
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:13 -0400

    Rename endpoint resolver struct type - devicefarm.

--- internal/service/devicefarm/service_package_gen.go
+++ internal/service/devicefarm/service_package_gen.go
@@ -85,7 +85,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return devicefarm.NewFromConfig(cfg,
-		devicefarm.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		devicefarm.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
