94fbe0a7a31f38eb6e2882a092725754706e6fb6
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:26 -0400

    Rename endpoint resolver struct type - transcribe.

--- internal/service/transcribe/service_package_gen.go
+++ internal/service/transcribe/service_package_gen.go
@@ -72,7 +72,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return transcribe.NewFromConfig(cfg,
-		transcribe.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		transcribe.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
