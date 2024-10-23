f148dd59ae94195018d64ed86109478ec1f56879
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:17 -0400

    Rename endpoint resolver struct type - guardduty.

--- internal/service/guardduty/service_package_gen.go
+++ internal/service/guardduty/service_package_gen.go
@@ -122,7 +122,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return guardduty.NewFromConfig(cfg,
-		guardduty.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		guardduty.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
