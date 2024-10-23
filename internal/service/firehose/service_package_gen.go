a3f379ab7012062b278bec92e414ec89620afc0c
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:16 -0400

    Rename endpoint resolver struct type - firehose.

--- internal/service/firehose/service_package_gen.go
+++ internal/service/firehose/service_package_gen.go
@@ -53,7 +53,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return firehose.NewFromConfig(cfg,
-		firehose.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		firehose.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
