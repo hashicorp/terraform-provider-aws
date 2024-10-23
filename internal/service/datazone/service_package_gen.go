3969cafbb23d2ae6f2ae3217399274cdd39548b4
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:13 -0400

    Rename endpoint resolver struct type - datazone.

--- internal/service/datazone/service_package_gen.go
+++ internal/service/datazone/service_package_gen.go
@@ -88,7 +88,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return datazone.NewFromConfig(cfg,
-		datazone.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		datazone.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
