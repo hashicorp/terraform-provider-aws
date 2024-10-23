eb12c98dbfb81a30f4c8d9ddd2f0dc83c76bd4f0
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:22 -0400

    Rename endpoint resolver struct type - redshift.

--- internal/service/redshift/service_package_gen.go
+++ internal/service/redshift/service_package_gen.go
@@ -214,7 +214,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return redshift.NewFromConfig(cfg,
-		redshift.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		redshift.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
