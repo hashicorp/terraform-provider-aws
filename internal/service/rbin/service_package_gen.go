b13777b010c05f2f7d151c66bb3be3b107b84223
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:22 -0400

    Rename endpoint resolver struct type - rbin.

--- internal/service/rbin/service_package_gen.go
+++ internal/service/rbin/service_package_gen.go
@@ -48,7 +48,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return rbin.NewFromConfig(cfg,
-		rbin.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		rbin.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
