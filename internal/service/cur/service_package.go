9a55626dcf2f95dbc90c9009f4daf40adb985b17
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:13 -0400

    Rename endpoint resolver struct type - cur.

--- internal/service/cur/service_package.go
+++ internal/service/cur/service_package.go
@@ -17,7 +17,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return costandusagereportservice.NewFromConfig(cfg,
-		costandusagereportservice.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		costandusagereportservice.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 		func(o *costandusagereportservice.Options) {
 			if config["partition"].(string) == names.StandardPartitionID {
