8f23e13b0bc1034079acdfa39be4a0da05b55046
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:12 -0400

    Rename endpoint resolver struct type - controltower.

--- internal/service/controltower/service_package_gen.go
+++ internal/service/controltower/service_package_gen.go
@@ -59,7 +59,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return controltower.NewFromConfig(cfg,
-		controltower.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		controltower.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
