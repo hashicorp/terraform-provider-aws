7df363cec96d34e390946eb00de3b2aabdb1d55b
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:16 -0400

    Rename endpoint resolver struct type - fis.

--- internal/service/fis/service_package_gen.go
+++ internal/service/fis/service_package_gen.go
@@ -46,7 +46,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return fis.NewFromConfig(cfg,
-		fis.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		fis.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
