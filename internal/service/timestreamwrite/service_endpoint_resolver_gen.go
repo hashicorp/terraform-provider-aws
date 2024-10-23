63544e97e02effbb9953ace1b20d90de59086e23
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:26 -0400

    Rename endpoint resolver struct type - timestreamwrite.

--- internal/service/timestreamwrite/service_endpoint_resolver_gen.go
+++ internal/service/timestreamwrite/service_endpoint_resolver_gen.go
@@ -14,19 +14,19 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 )
 
-var _ timestreamwrite.EndpointResolverV2 = resolverSDKv2{}
+var _ timestreamwrite.EndpointResolverV2 = resolverV2{}
 
-type resolverSDKv2 struct {
+type resolverV2 struct {
 	defaultResolver timestreamwrite.EndpointResolverV2
 }
 
-func newEndpointResolverSDKv2() resolverSDKv2 {
-	return resolverSDKv2{
+func newEndpointResolverV2() resolverV2 {
+	return resolverV2{
 		defaultResolver: timestreamwrite.NewDefaultEndpointResolverV2(),
 	}
 }
 
-func (r resolverSDKv2) ResolveEndpoint(ctx context.Context, params timestreamwrite.EndpointParameters) (endpoint smithyendpoints.Endpoint, err error) {
+func (r resolverV2) ResolveEndpoint(ctx context.Context, params timestreamwrite.EndpointParameters) (endpoint smithyendpoints.Endpoint, err error) {
 	params = params.WithDefaults()
 	useFIPS := aws.ToBool(params.UseFIPS)
 
