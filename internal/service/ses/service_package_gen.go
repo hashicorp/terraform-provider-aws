6c38a51d0285a8d242168f4ef887632acc5dd91a
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:24 -0400

    Rename endpoint resolver struct type - ses.

--- internal/service/ses/service_package_gen.go
+++ internal/service/ses/service_package_gen.go
@@ -126,7 +126,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return ses.NewFromConfig(cfg,
-		ses.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		ses.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
