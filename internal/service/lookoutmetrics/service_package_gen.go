023eced9a07018cfe372e03306390a6df8718462
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:19 -0400

    Rename endpoint resolver struct type - lookoutmetrics.

--- internal/service/lookoutmetrics/service_package_gen.go
+++ internal/service/lookoutmetrics/service_package_gen.go
@@ -39,7 +39,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return lookoutmetrics.NewFromConfig(cfg,
-		lookoutmetrics.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		lookoutmetrics.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
