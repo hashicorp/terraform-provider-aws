328135e2e3fbe80c6594b6b9e8ddfd9ec18800b8
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:10 -0400

    Rename endpoint resolver struct type - cloud9.

--- internal/service/cloud9/service_package_gen.go
+++ internal/service/cloud9/service_package_gen.go
@@ -53,7 +53,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return cloud9.NewFromConfig(cfg,
-		cloud9.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		cloud9.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
