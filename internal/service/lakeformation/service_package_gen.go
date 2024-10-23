f6cb8a529b139cbf720c1db422c40257ba22620b
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:18 -0400

    Rename endpoint resolver struct type - lakeformation.

--- internal/service/lakeformation/service_package_gen.go
+++ internal/service/lakeformation/service_package_gen.go
@@ -83,7 +83,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return lakeformation.NewFromConfig(cfg,
-		lakeformation.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		lakeformation.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
