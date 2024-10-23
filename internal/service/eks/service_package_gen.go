0bc4c1f32bef5373447e2ddff5d319018dafb9f1
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:15 -0400

    Rename endpoint resolver struct type - eks.

--- internal/service/eks/service_package_gen.go
+++ internal/service/eks/service_package_gen.go
@@ -135,7 +135,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return eks.NewFromConfig(cfg,
-		eks.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		eks.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
