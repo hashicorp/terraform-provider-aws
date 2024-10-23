5d56c4a31fed548784d6e6516aa2fe71e2895fd0
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:13 -0400

    Rename endpoint resolver struct type - dax.

--- internal/service/dax/service_package_gen.go
+++ internal/service/dax/service_package_gen.go
@@ -56,7 +56,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return dax.NewFromConfig(cfg,
-		dax.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		dax.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
