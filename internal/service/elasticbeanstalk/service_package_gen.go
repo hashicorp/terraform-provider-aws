398d1d2dde246d49cf59b39fa86a609b9798856a
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:15 -0400

    Rename endpoint resolver struct type - elasticbeanstalk.

--- internal/service/elasticbeanstalk/service_package_gen.go
+++ internal/service/elasticbeanstalk/service_package_gen.go
@@ -84,7 +84,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return elasticbeanstalk.NewFromConfig(cfg,
-		elasticbeanstalk.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		elasticbeanstalk.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
