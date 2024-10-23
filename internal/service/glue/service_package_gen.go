a0a709b4c675e0b74c5eaaded3e41558b13419b7
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:16 -0400

    Rename endpoint resolver struct type - glue.

--- internal/service/glue/service_package_gen.go
+++ internal/service/glue/service_package_gen.go
@@ -187,7 +187,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return glue.NewFromConfig(cfg,
-		glue.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		glue.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
