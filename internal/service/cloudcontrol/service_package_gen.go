a89c43967edea3376e9f09dc9bac615dca05c357
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:10 -0400

    Rename endpoint resolver struct type - cloudcontrol.

--- internal/service/cloudcontrol/service_package_gen.go
+++ internal/service/cloudcontrol/service_package_gen.go
@@ -51,7 +51,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return cloudcontrol.NewFromConfig(cfg,
-		cloudcontrol.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		cloudcontrol.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
