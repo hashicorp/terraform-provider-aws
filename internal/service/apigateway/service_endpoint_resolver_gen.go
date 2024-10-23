743ef2b11beaacfe97d05e8363b7ee4cbd112303
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:08 -0400

    Rename endpoint resolver struct type - apigateway.

--- internal/service/apigateway/service_endpoint_resolver_gen.go
+++ internal/service/apigateway/service_endpoint_resolver_gen.go
@@ -14,19 +14,19 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 )
 
-var _ apigateway.EndpointResolverV2 = resolverSDKv2{}
+var _ apigateway.EndpointResolverV2 = resolverV2{}
 
-type resolverSDKv2 struct {
+type resolverV2 struct {
 	defaultResolver apigateway.EndpointResolverV2
 }
 
-func newEndpointResolverSDKv2() resolverSDKv2 {
-	return resolverSDKv2{
+func newEndpointResolverV2() resolverV2 {
+	return resolverV2{
 		defaultResolver: apigateway.NewDefaultEndpointResolverV2(),
 	}
 }
 
-func (r resolverSDKv2) ResolveEndpoint(ctx context.Context, params apigateway.EndpointParameters) (endpoint smithyendpoints.Endpoint, err error) {
+func (r resolverV2) ResolveEndpoint(ctx context.Context, params apigateway.EndpointParameters) (endpoint smithyendpoints.Endpoint, err error) {
 	params = params.WithDefaults()
 	useFIPS := aws.ToBool(params.UseFIPS)
 
