93b16c2b989ed6fc4dc4e00423096a8214157b15
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:25 -0400

    Rename endpoint resolver struct type - ssmquicksetup.

--- internal/service/ssmquicksetup/service_package_gen.go
+++ internal/service/ssmquicksetup/service_package_gen.go
@@ -39,7 +39,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return ssmquicksetup.NewFromConfig(cfg,
-		ssmquicksetup.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		ssmquicksetup.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
