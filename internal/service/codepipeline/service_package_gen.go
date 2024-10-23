5d9380892bc3bd3c749734129e1dd71821ee642a
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:12 -0400

    Rename endpoint resolver struct type - codepipeline.

--- internal/service/codepipeline/service_package_gen.go
+++ internal/service/codepipeline/service_package_gen.go
@@ -64,7 +64,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return codepipeline.NewFromConfig(cfg,
-		codepipeline.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		codepipeline.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
