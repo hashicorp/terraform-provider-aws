8e32d44fd8ff161e814552086d841c7cffa9dc27
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:21 -0400

    Rename endpoint resolver struct type - pinpoint.

--- internal/service/pinpoint/service_package_gen.go
+++ internal/service/pinpoint/service_package_gen.go
@@ -106,7 +106,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return pinpoint.NewFromConfig(cfg,
-		pinpoint.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		pinpoint.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
