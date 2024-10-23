25258b6273badf689aba0caede73fc44d96bb973
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:10 -0400

    Rename endpoint resolver struct type - budgets.

--- internal/service/budgets/service_package_gen.go
+++ internal/service/budgets/service_package_gen.go
@@ -64,7 +64,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return budgets.NewFromConfig(cfg,
-		budgets.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		budgets.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
