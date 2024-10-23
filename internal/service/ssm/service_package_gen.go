f120905705567ed645d18db850ebfa10ee965e08
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:25 -0400

    Rename endpoint resolver struct type - ssm.

--- internal/service/ssm/service_package_gen.go
+++ internal/service/ssm/service_package_gen.go
@@ -157,7 +157,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return ssm.NewFromConfig(cfg,
-		ssm.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		ssm.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
