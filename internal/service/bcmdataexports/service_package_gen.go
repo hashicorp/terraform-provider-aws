4e3df4f5fc872b5291072dc02303bb1010988c83
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:10 -0400

    Rename endpoint resolver struct type - bcmdataexports.

--- internal/service/bcmdataexports/service_package_gen.go
+++ internal/service/bcmdataexports/service_package_gen.go
@@ -47,7 +47,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return bcmdataexports.NewFromConfig(cfg,
-		bcmdataexports.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		bcmdataexports.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
