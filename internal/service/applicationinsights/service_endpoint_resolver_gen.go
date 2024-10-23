40dcbf841c458216382f8730392621ec3ab24850
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:09 -0400

    Rename endpoint resolver struct type - applicationinsights.

--- internal/service/applicationinsights/service_endpoint_resolver_gen.go
+++ internal/service/applicationinsights/service_endpoint_resolver_gen.go
@@ -14,19 +14,19 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 )
 
-var _ applicationinsights.EndpointResolverV2 = resolverSDKv2{}
+var _ applicationinsights.EndpointResolverV2 = resolverV2{}
 
-type resolverSDKv2 struct {
+type resolverV2 struct {
 	defaultResolver applicationinsights.EndpointResolverV2
 }
 
-func newEndpointResolverSDKv2() resolverSDKv2 {
-	return resolverSDKv2{
+func newEndpointResolverV2() resolverV2 {
+	return resolverV2{
 		defaultResolver: applicationinsights.NewDefaultEndpointResolverV2(),
 	}
 }
 
-func (r resolverSDKv2) ResolveEndpoint(ctx context.Context, params applicationinsights.EndpointParameters) (endpoint smithyendpoints.Endpoint, err error) {
+func (r resolverV2) ResolveEndpoint(ctx context.Context, params applicationinsights.EndpointParameters) (endpoint smithyendpoints.Endpoint, err error) {
 	params = params.WithDefaults()
 	useFIPS := aws.ToBool(params.UseFIPS)
 
