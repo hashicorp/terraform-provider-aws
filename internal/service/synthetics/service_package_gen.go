cf92aa451fac957ee72580234b672c7697666414
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:26 -0400

    Rename endpoint resolver struct type - synthetics.

--- internal/service/synthetics/service_package_gen.go
+++ internal/service/synthetics/service_package_gen.go
@@ -70,7 +70,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return synthetics.NewFromConfig(cfg,
-		synthetics.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		synthetics.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
