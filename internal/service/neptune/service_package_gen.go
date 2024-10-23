972521f74a57fa51c141d42e2ecfdb204b54199d
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:20 -0400

    Rename endpoint resolver struct type - neptune.

--- internal/service/neptune/service_package_gen.go
+++ internal/service/neptune/service_package_gen.go
@@ -117,7 +117,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return neptune.NewFromConfig(cfg,
-		neptune.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		neptune.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
