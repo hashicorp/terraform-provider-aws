3be2a7fca2aed8b273ee1962c748159182eb430d
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:15 -0400

    Rename endpoint resolver struct type - elb.

--- internal/service/elb/service_package_gen.go
+++ internal/service/elb/service_package_gen.go
@@ -104,7 +104,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return elasticloadbalancing.NewFromConfig(cfg,
-		elasticloadbalancing.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		elasticloadbalancing.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
