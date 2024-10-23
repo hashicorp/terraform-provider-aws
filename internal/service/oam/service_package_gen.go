52be8a58cd04f9198a0d2432612db48d6a5d68e1
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:20 -0400

    Rename endpoint resolver struct type - oam.

--- internal/service/oam/service_package_gen.go
+++ internal/service/oam/service_package_gen.go
@@ -77,7 +77,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return oam.NewFromConfig(cfg,
-		oam.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		oam.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
