d1f5b70693e9c3974b52298e0a4792134ab5a747
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:12 -0400

    Rename endpoint resolver struct type - codegurureviewer.

--- internal/service/codegurureviewer/service_package_gen.go
+++ internal/service/codegurureviewer/service_package_gen.go
@@ -48,7 +48,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return codegurureviewer.NewFromConfig(cfg,
-		codegurureviewer.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		codegurureviewer.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
