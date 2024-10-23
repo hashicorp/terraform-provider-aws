ec8a4e1ae3493363145d2953fd7bf5c6804201f4
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:24 -0400

    Rename endpoint resolver struct type - securitylake.

--- internal/service/securitylake/service_endpoint_resolver_gen.go
+++ internal/service/securitylake/service_endpoint_resolver_gen.go
@@ -14,19 +14,19 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 )
 
-var _ securitylake.EndpointResolverV2 = resolverSDKv2{}
+var _ securitylake.EndpointResolverV2 = resolverV2{}
 
-type resolverSDKv2 struct {
+type resolverV2 struct {
 	defaultResolver securitylake.EndpointResolverV2
 }
 
-func newEndpointResolverSDKv2() resolverSDKv2 {
-	return resolverSDKv2{
+func newEndpointResolverV2() resolverV2 {
+	return resolverV2{
 		defaultResolver: securitylake.NewDefaultEndpointResolverV2(),
 	}
 }
 
-func (r resolverSDKv2) ResolveEndpoint(ctx context.Context, params securitylake.EndpointParameters) (endpoint smithyendpoints.Endpoint, err error) {
+func (r resolverV2) ResolveEndpoint(ctx context.Context, params securitylake.EndpointParameters) (endpoint smithyendpoints.Endpoint, err error) {
 	params = params.WithDefaults()
 	useFIPS := aws.ToBool(params.UseFIPS)
 
--- internal/service/securitylake/service_package_gen.go
+++ internal/service/securitylake/service_package_gen.go
@@ -66,7 +66,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return securitylake.NewFromConfig(cfg,
-		securitylake.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		securitylake.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
