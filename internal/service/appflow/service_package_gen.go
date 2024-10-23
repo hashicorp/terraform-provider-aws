a925f7862e10e2fd7f8d53a2d03b880d1feb8683
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:08 -0400

    Rename endpoint resolver struct type - appflow.

--- internal/service/appflow/service_package_gen.go
+++ internal/service/appflow/service_package_gen.go
@@ -53,7 +53,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return appflow.NewFromConfig(cfg,
-		appflow.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		appflow.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
