8ebd3c196c01bd2c1af633fc49f01ff5dbb0d2b1
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:12 -0400

    Rename endpoint resolver struct type - computeoptimizer.

--- internal/service/computeoptimizer/service_package_gen.go
+++ internal/service/computeoptimizer/service_package_gen.go
@@ -48,7 +48,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return computeoptimizer.NewFromConfig(cfg,
-		computeoptimizer.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		computeoptimizer.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
