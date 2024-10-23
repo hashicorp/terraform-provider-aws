619a7ebe3e0ef6541f3c2f5209f6b66c882882c3
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:24 -0400

    Rename endpoint resolver struct type - scheduler.

--- internal/service/scheduler/service_package_gen.go
+++ internal/service/scheduler/service_package_gen.go
@@ -52,7 +52,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return scheduler.NewFromConfig(cfg,
-		scheduler.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		scheduler.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
