02dd95ace42cc1c03ce8fd99d8186bf2f76eab60
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:13 -0400

    Rename endpoint resolver struct type - databrew.

--- internal/service/databrew/service_package_gen.go
+++ internal/service/databrew/service_package_gen.go
@@ -39,7 +39,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return databrew.NewFromConfig(cfg,
-		databrew.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		databrew.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
