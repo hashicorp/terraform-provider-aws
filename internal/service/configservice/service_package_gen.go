63752f2d7023d41b36d36c5bc072eeee1d694ca6
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:12 -0400

    Rename endpoint resolver struct type - configservice.

--- internal/service/configservice/service_package_gen.go
+++ internal/service/configservice/service_package_gen.go
@@ -114,7 +114,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return configservice.NewFromConfig(cfg,
-		configservice.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		configservice.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
