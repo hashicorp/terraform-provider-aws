deb5c0e635be424f6cb240285aa61daeecb08986
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:24 -0400

    Rename endpoint resolver struct type - s3outposts.

--- internal/service/s3outposts/service_package_gen.go
+++ internal/service/s3outposts/service_package_gen.go
@@ -45,7 +45,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return s3outposts.NewFromConfig(cfg,
-		s3outposts.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		s3outposts.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
