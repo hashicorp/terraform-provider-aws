eef98d2437eec60c864cbfe05bbbda204d08c764
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:09 -0400

    Rename endpoint resolver struct type - appmesh.

--- internal/service/appmesh/service_package_gen.go
+++ internal/service/appmesh/service_package_gen.go
@@ -139,7 +139,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return appmesh.NewFromConfig(cfg,
-		appmesh.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		appmesh.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
