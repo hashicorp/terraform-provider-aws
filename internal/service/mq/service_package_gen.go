ea971f7e6b67f66d879001a5b81be5083a09e805
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:20 -0400

    Rename endpoint resolver struct type - mq.

--- internal/service/mq/service_package_gen.go
+++ internal/service/mq/service_package_gen.go
@@ -72,7 +72,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return mq.NewFromConfig(cfg,
-		mq.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		mq.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
