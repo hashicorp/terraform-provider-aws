079d3236c23426a73f940259f83a8faa5c16815d
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:23 -0400

    Rename endpoint resolver struct type - s3control.

--- internal/service/s3control/service_package_gen.go
+++ internal/service/s3control/service_package_gen.go
@@ -119,7 +119,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return s3control.NewFromConfig(cfg,
-		s3control.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		s3control.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
