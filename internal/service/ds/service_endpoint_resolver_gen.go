1c7a8ebac184f73da81866fd9140a7dc317d01c3
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:14 -0400

    Rename endpoint resolver struct type - ds.

--- internal/service/ds/service_endpoint_resolver_gen.go
+++ internal/service/ds/service_endpoint_resolver_gen.go
@@ -14,19 +14,19 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 )
 
-var _ directoryservice.EndpointResolverV2 = resolverSDKv2{}
+var _ directoryservice.EndpointResolverV2 = resolverV2{}
 
-type resolverSDKv2 struct {
+type resolverV2 struct {
 	defaultResolver directoryservice.EndpointResolverV2
 }
 
-func newEndpointResolverSDKv2() resolverSDKv2 {
-	return resolverSDKv2{
+func newEndpointResolverV2() resolverV2 {
+	return resolverV2{
 		defaultResolver: directoryservice.NewDefaultEndpointResolverV2(),
 	}
 }
 
-func (r resolverSDKv2) ResolveEndpoint(ctx context.Context, params directoryservice.EndpointParameters) (endpoint smithyendpoints.Endpoint, err error) {
+func (r resolverV2) ResolveEndpoint(ctx context.Context, params directoryservice.EndpointParameters) (endpoint smithyendpoints.Endpoint, err error) {
 	params = params.WithDefaults()
 	useFIPS := aws.ToBool(params.UseFIPS)
 
--- internal/service/ds/service_package_gen.go
+++ internal/service/ds/service_package_gen.go
@@ -90,7 +90,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return directoryservice.NewFromConfig(cfg,
-		directoryservice.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		directoryservice.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
