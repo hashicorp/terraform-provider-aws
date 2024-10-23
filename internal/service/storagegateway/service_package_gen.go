7789096d210495f1a2b3e409bb5425fdc7c18eee
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:26 -0400

    Rename endpoint resolver struct type - storagegateway.

--- internal/service/storagegateway/service_package_gen.go
+++ internal/service/storagegateway/service_package_gen.go
@@ -117,7 +117,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return storagegateway.NewFromConfig(cfg,
-		storagegateway.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		storagegateway.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
