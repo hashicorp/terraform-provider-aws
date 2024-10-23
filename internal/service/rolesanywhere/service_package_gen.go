bbd82134d1634ac065c805c106128c701c967931
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:23 -0400

    Rename endpoint resolver struct type - rolesanywhere.

--- internal/service/rolesanywhere/service_package_gen.go
+++ internal/service/rolesanywhere/service_package_gen.go
@@ -56,7 +56,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return rolesanywhere.NewFromConfig(cfg,
-		rolesanywhere.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		rolesanywhere.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
