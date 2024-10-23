9006ab801396dcc13101866019d29004775fa8d2
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:27 -0400

    Rename endpoint resolver struct type - wafregional.

--- internal/service/wafregional/service_package_gen.go
+++ internal/service/wafregional/service_package_gen.go
@@ -143,7 +143,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return wafregional.NewFromConfig(cfg,
-		wafregional.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		wafregional.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
