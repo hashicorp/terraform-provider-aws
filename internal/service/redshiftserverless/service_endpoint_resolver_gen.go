c542117db8cd9f0400e944c2d291b3855b314e7f
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:22 -0400

    Rename endpoint resolver struct type - redshiftserverless.

--- internal/service/redshiftserverless/service_endpoint_resolver_gen.go
+++ internal/service/redshiftserverless/service_endpoint_resolver_gen.go
@@ -14,19 +14,19 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 )
 
-var _ redshiftserverless.EndpointResolverV2 = resolverSDKv2{}
+var _ redshiftserverless.EndpointResolverV2 = resolverV2{}
 
-type resolverSDKv2 struct {
+type resolverV2 struct {
 	defaultResolver redshiftserverless.EndpointResolverV2
 }
 
-func newEndpointResolverSDKv2() resolverSDKv2 {
-	return resolverSDKv2{
+func newEndpointResolverV2() resolverV2 {
+	return resolverV2{
 		defaultResolver: redshiftserverless.NewDefaultEndpointResolverV2(),
 	}
 }
 
-func (r resolverSDKv2) ResolveEndpoint(ctx context.Context, params redshiftserverless.EndpointParameters) (endpoint smithyendpoints.Endpoint, err error) {
+func (r resolverV2) ResolveEndpoint(ctx context.Context, params redshiftserverless.EndpointParameters) (endpoint smithyendpoints.Endpoint, err error) {
 	params = params.WithDefaults()
 	useFIPS := aws.ToBool(params.UseFIPS)
 
--- internal/service/redshiftserverless/service_package_gen.go
+++ internal/service/redshiftserverless/service_package_gen.go
@@ -97,7 +97,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return redshiftserverless.NewFromConfig(cfg,
-		redshiftserverless.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		redshiftserverless.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
