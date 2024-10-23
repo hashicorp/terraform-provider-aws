c0375b47353224b915b5c2f2d7da10b0524f1710
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:12 -0400

    Rename endpoint resolver struct type - comprehend.

--- internal/service/comprehend/service_package_gen.go
+++ internal/service/comprehend/service_package_gen.go
@@ -56,7 +56,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return comprehend.NewFromConfig(cfg,
-		comprehend.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		comprehend.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
