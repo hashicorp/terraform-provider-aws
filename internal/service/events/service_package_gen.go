059d712713c19bb563ccd6792b5eb8149f1980da
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:15 -0400

    Rename endpoint resolver struct type - events.

--- internal/service/events/service_package_gen.go
+++ internal/service/events/service_package_gen.go
@@ -107,7 +107,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return eventbridge.NewFromConfig(cfg,
-		eventbridge.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		eventbridge.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
