bcf374f0a3aa8a586ccb070a4008001173e68603
Dirk Avery <dirk.avery@gmail.com>
2024-10-23 18:10:02 -0400

    cloud9: De-constant numbers in test code

--- internal/service/cloud9/environment_ec2_test.go
+++ internal/service/cloud9/environment_ec2_test.go
@@ -75,7 +75,7 @@ func TestAccCloud9EnvironmentEC2_allFields(t *testing.T) {
 				Config: testAccEnvironmentEC2Config_allFields(rName, name1, description1, imageID),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckEnvironmentEC2Exists(ctx, resourceName, &conf),
-					resource.TestCheckResourceAttr(resourceName, "automatic_stop_time_minutes", acctest.Ct60),
+					resource.TestCheckResourceAttr(resourceName, "automatic_stop_time_minutes", "60"),
 					resource.TestCheckResourceAttr(resourceName, "connection_type", "CONNECT_SSH"),
 					resource.TestCheckResourceAttr(resourceName, names.AttrDescription, description1),
 					resource.TestCheckResourceAttr(resourceName, "image_id", imageID),
@@ -120,7 +120,7 @@ func TestAccCloud9EnvironmentEC2_tags(t *testing.T) {
 				Config: testAccEnvironmentEC2Config_tags1(rName, acctest.CtKey1, acctest.CtValue1),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckEnvironmentEC2Exists(ctx, resourceName, &conf),
-					resource.TestCheckResourceAttr(resourceName, acctest.CtTagsPercent, acctest.Ct1),
+					resource.TestCheckResourceAttr(resourceName, acctest.CtTagsPercent, "1"),
 					resource.TestCheckResourceAttr(resourceName, acctest.CtTagsKey1, acctest.CtValue1),
 				),
 			},
@@ -134,7 +134,7 @@ func TestAccCloud9EnvironmentEC2_tags(t *testing.T) {
 				Config: testAccEnvironmentEC2Config_tags2(rName, acctest.CtKey1, acctest.CtValue1Updated, acctest.CtKey2, acctest.CtValue2),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckEnvironmentEC2Exists(ctx, resourceName, &conf),
-					resource.TestCheckResourceAttr(resourceName, acctest.CtTagsPercent, acctest.Ct2),
+					resource.TestCheckResourceAttr(resourceName, acctest.CtTagsPercent, "2"),
 					resource.TestCheckResourceAttr(resourceName, acctest.CtTagsKey1, acctest.CtValue1Updated),
 					resource.TestCheckResourceAttr(resourceName, acctest.CtTagsKey2, acctest.CtValue2),
 				),
@@ -143,7 +143,7 @@ func TestAccCloud9EnvironmentEC2_tags(t *testing.T) {
 				Config: testAccEnvironmentEC2Config_tags1(rName, acctest.CtKey2, acctest.CtValue2),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckEnvironmentEC2Exists(ctx, resourceName, &conf),
-					resource.TestCheckResourceAttr(resourceName, acctest.CtTagsPercent, acctest.Ct1),
+					resource.TestCheckResourceAttr(resourceName, acctest.CtTagsPercent, "1"),
 					resource.TestCheckResourceAttr(resourceName, acctest.CtTagsKey2, acctest.CtValue2),
 				),
 			},
