831133ca15d893ea9fa7ef6ad7638e15fbb951b1
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:17 -0400

    Rename endpoint resolver struct type - iotevents.

--- internal/service/iotevents/service_package_gen.go
+++ internal/service/iotevents/service_package_gen.go
@@ -39,7 +39,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return iotevents.NewFromConfig(cfg,
-		iotevents.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		iotevents.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
