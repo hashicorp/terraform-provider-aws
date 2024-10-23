99e55a583e6f90164efdfb0e469cb3d349f09c01
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:15 -0400

    Rename endpoint resolver struct type - elasticsearch.

--- internal/service/elasticsearch/service_endpoint_resolver_gen.go
+++ internal/service/elasticsearch/service_endpoint_resolver_gen.go
@@ -14,19 +14,19 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 )
 
-var _ elasticsearchservice.EndpointResolverV2 = resolverSDKv2{}
+var _ elasticsearchservice.EndpointResolverV2 = resolverV2{}
 
-type resolverSDKv2 struct {
+type resolverV2 struct {
 	defaultResolver elasticsearchservice.EndpointResolverV2
 }
 
-func newEndpointResolverSDKv2() resolverSDKv2 {
-	return resolverSDKv2{
+func newEndpointResolverV2() resolverV2 {
+	return resolverV2{
 		defaultResolver: elasticsearchservice.NewDefaultEndpointResolverV2(),
 	}
 }
 
-func (r resolverSDKv2) ResolveEndpoint(ctx context.Context, params elasticsearchservice.EndpointParameters) (endpoint smithyendpoints.Endpoint, err error) {
+func (r resolverV2) ResolveEndpoint(ctx context.Context, params elasticsearchservice.EndpointParameters) (endpoint smithyendpoints.Endpoint, err error) {
 	params = params.WithDefaults()
 	useFIPS := aws.ToBool(params.UseFIPS)
 
--- internal/service/elasticsearch/service_package_gen.go
+++ internal/service/elasticsearch/service_package_gen.go
@@ -69,7 +69,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return elasticsearchservice.NewFromConfig(cfg,
-		elasticsearchservice.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		elasticsearchservice.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
