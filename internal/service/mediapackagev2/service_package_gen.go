98acb0311aa7c4e65b1fd64f878366ea988d0a86
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:20 -0400

    Rename endpoint resolver struct type - mediapackagev2.

--- internal/service/mediapackagev2/service_package_gen.go
+++ internal/service/mediapackagev2/service_package_gen.go
@@ -39,7 +39,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return mediapackagev2.NewFromConfig(cfg,
-		mediapackagev2.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		mediapackagev2.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
