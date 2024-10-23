bdfe4100fc7ce5bcb98c57c7f17d79de1d5ae90e
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:09 -0400

    Rename endpoint resolver struct type - applicationsignals.

--- internal/service/applicationsignals/service_package_gen.go
+++ internal/service/applicationsignals/service_package_gen.go
@@ -39,7 +39,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return applicationsignals.NewFromConfig(cfg,
-		applicationsignals.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		applicationsignals.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
