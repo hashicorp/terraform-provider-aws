dda230c2330ad56c58ca00f4f84f212c09e711da
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-23 11:18:09 -0400

    Rename endpoint resolver struct type - batch.

--- internal/service/batch/service_package_gen.go
+++ internal/service/batch/service_package_gen.go
@@ -96,7 +96,7 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 	cfg := *(config["aws_sdkv2_config"].(*aws.Config))
 
 	return batch.NewFromConfig(cfg,
-		batch.WithEndpointResolverV2(newEndpointResolverSDKv2()),
+		batch.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 	), nil
 }
