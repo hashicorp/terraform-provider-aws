da2c8ecb581a44928b2b4d2e007fcfaa2aba2b81
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-24 14:09:38 -0400

    Use 'AWSClient.PartitionID' - glue.

--- internal/service/glue/catalog_table_data_source.go
+++ internal/service/glue/catalog_table_data_source.go
@@ -367,7 +367,7 @@ func dataSourceCatalogTableRead(ctx context.Context, d *schema.ResourceData, met
 
 	table := out.Table
 	tableArn := arn.ARN{
-		Partition: meta.(*conns.AWSClient).Partition,
+		Partition: meta.(*conns.AWSClient).PartitionID(ctx),
 		Service:   "glue",
 		Region:    meta.(*conns.AWSClient).Region,
 		AccountID: meta.(*conns.AWSClient).AccountID,
