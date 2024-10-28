cf0841d4e6b958713c7d26f9b78bbb158fa36e9a
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-28 10:46:03 -0400

    Remove unused '-SkipTypesImp' from tags generation callers.

--- internal/service/dynamodb/generate.go
+++ internal/service/dynamodb/generate.go
@@ -3,7 +3,7 @@
 
 //go:generate go run ../../generate/tagresource/main.go -UpdateTagsFunc=updateTagsResource
 //go:generate go run ../../generate/tags/main.go -GetTag -ListTags -ListTagsOp=ListTagsOfResource -ServiceTagsSlice -UpdateTags -Wait -WaitContinuousOccurence 2 -WaitMinTimeout 1s -WaitTimeout 2m -ParentNotFoundErrCode=ResourceNotFoundException -CreateTags
-//go:generate go run ../../generate/tags/main.go -SkipTypesImp -UpdateTags -UpdateTagsFunc=updateTagsResource -Wait -WaitFunc=waitTagsPropagedForResource -WaitContinuousOccurence 2 -WaitMinTimeout 1s -WaitTimeout 2m -WaitFuncComparator=ContainsAll -- update_tags_for_resource_gen.go
+//go:generate go run ../../generate/tags/main.go -UpdateTags -UpdateTagsFunc=updateTagsResource -Wait -WaitFunc=waitTagsPropagedForResource -WaitContinuousOccurence 2 -WaitMinTimeout 1s -WaitTimeout 2m -WaitFuncComparator=ContainsAll -- update_tags_for_resource_gen.go
 //go:generate go run ../../generate/servicepackage/main.go
 //go:generate go run ../../generate/listpages/main.go -ListOps=ListBackups -InputPaginator=ExclusiveStartBackupArn -OutputPaginator=LastEvaluatedBackupArn -- list_backups_pages_gen.go
 //go:generate go run ../../generate/tagstests/main.go
