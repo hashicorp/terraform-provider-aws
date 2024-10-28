d1d4437dcba9c19ff42a541dd7d432fd17dcba5f
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-10-28 15:59:40 -0400

    Some 'internal/namevaluesfilters/v2' -> 'internal/namevaluesfilters'.

--- internal/namevaluesfilters/generate.go
+++ internal/namevaluesfilters/generate.go
@@ -1,7 +1,7 @@
 // Copyright (c) HashiCorp, Inc.
 // SPDX-License-Identifier: MPL-2.0
 
-//go:generate go run ../../generate/namevaluesfilters/main.go
+//go:generate go run ../generate/namevaluesfilters/main.go
 // ONLY generate directives and package declaration! Do not add anything else to this file.
 
-package v2
+package namevaluesfilters
