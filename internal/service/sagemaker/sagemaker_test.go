182998a84f9956775880bdbe366b3c72bfbdf5bc
Dirk Avery <dirk.avery@gmail.com>
2024-11-07 15:54:08 -0500

    sagemaker/test: Add new test

--- internal/service/sagemaker/sagemaker_test.go
+++ internal/service/sagemaker/sagemaker_test.go
@@ -74,6 +74,7 @@ func TestAccSageMaker_serial(t *testing.T) {
 			"rSessionAppSettings":                                     testAccDomain_rSessionAppSettings,
 			"rStudioServerProAppSettings":                             testAccDomain_rStudioServerProAppSettings,
 			"rStudioServerProDomainSettings":                          testAccDomain_rStudioServerProDomainSettings,
+			"rStudioServerProDomainSettingsUpdate":                    testAccDomain_rStudioServerProDomainSettingsUpdate,
 			"spaceSettingsKernelGatewayAppSettings":                   testAccDomain_spaceSettingsKernelGatewayAppSettings,
 			"spaceSettingsJupyterLabAppSettings":                      testAccDomain_spaceSettingsJupyterLabAppSettings,
 			"spaceSettingsSpaceStorageSettings":                       testAccDomain_spaceSettingsSpaceStorageSettings,
