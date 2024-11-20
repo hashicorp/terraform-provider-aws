5dc279988dda4f76704fc83869d1efe4b55dd093
Graham Davison <gdavison@hashicorp.com>
2024-11-20 11:07:38 -0800

    Adds sweepers for `aws_s3tables_namespace` and `aws_s3tables_table`

--- internal/service/s3tables/sweep.go
+++ internal/service/s3tables/sweep.go
@@ -16,7 +16,99 @@ import (
 )
 
 func RegisterSweepers() {
-	awsv2.Register("aws_s3tables_table_bucket", sweepTableBuckets)
+	awsv2.Register("aws_s3tables_namespace", sweepNamespaces,
+		"aws_s3tables_table",
+	)
+
+	awsv2.Register("aws_s3tables_table", sweepTables)
+
+	awsv2.Register("aws_s3tables_table_bucket", sweepTableBuckets,
+		"aws_s3tables_namespace",
+	)
+}
+
+func sweepNamespaces(ctx context.Context, client *conns.AWSClient) ([]sweep.Sweepable, error) {
+	conn := client.S3TablesClient(ctx)
+
+	var sweepResources []sweep.Sweepable
+
+	tableBuckets := s3tables.NewListTableBucketsPaginator(conn, &s3tables.ListTableBucketsInput{})
+	for tableBuckets.HasMorePages() {
+		page, err := tableBuckets.NextPage(ctx)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, bucket := range page.TableBuckets {
+			namespaces := s3tables.NewListNamespacesPaginator(conn, &s3tables.ListNamespacesInput{
+				TableBucketARN: bucket.Arn,
+			})
+			for namespaces.HasMorePages() {
+				page, err := namespaces.NextPage(ctx)
+				if err != nil {
+					return nil, err
+				}
+
+				for _, namespace := range page.Namespaces {
+					sweepResources = append(sweepResources, framework.NewSweepResource(newResourceNamespace, client,
+						framework.NewAttribute("table_bucket_arn", aws.ToString(bucket.Arn)),
+						framework.NewAttribute(names.AttrNamespace, namespace.Namespace),
+					))
+				}
+			}
+		}
+	}
+
+	return sweepResources, nil
+}
+
+func sweepTables(ctx context.Context, client *conns.AWSClient) ([]sweep.Sweepable, error) {
+	conn := client.S3TablesClient(ctx)
+
+	var sweepResources []sweep.Sweepable
+
+	tableBuckets := s3tables.NewListTableBucketsPaginator(conn, &s3tables.ListTableBucketsInput{})
+	for tableBuckets.HasMorePages() {
+		page, err := tableBuckets.NextPage(ctx)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, bucket := range page.TableBuckets {
+			namespaces := s3tables.NewListNamespacesPaginator(conn, &s3tables.ListNamespacesInput{
+				TableBucketARN: bucket.Arn,
+			})
+			for namespaces.HasMorePages() {
+				page, err := namespaces.NextPage(ctx)
+				if err != nil {
+					return nil, err
+				}
+
+				for _, namespace := range page.Namespaces {
+					tables := s3tables.NewListTablesPaginator(conn, &s3tables.ListTablesInput{
+						TableBucketARN: bucket.Arn,
+						Namespace:      aws.String(namespace.Namespace[0]),
+					})
+					for tables.HasMorePages() {
+						page, err := tables.NextPage(ctx)
+						if err != nil {
+							return nil, err
+						}
+
+						for _, table := range page.Tables {
+							sweepResources = append(sweepResources, framework.NewSweepResource(newResourceTable, client,
+								framework.NewAttribute("table_bucket_arn", aws.ToString(bucket.Arn)),
+								framework.NewAttribute(names.AttrNamespace, namespace.Namespace[0]),
+								framework.NewAttribute(names.AttrName, aws.ToString(table.Name)),
+							))
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return sweepResources, nil
 }
 
 func sweepTableBuckets(ctx context.Context, client *conns.AWSClient) ([]sweep.Sweepable, error) {
