1eb1cd4f87e08720d4daf2848fe081672cc7a345
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-11-27 14:36:16 -0500

    Make 'AWSClient.Region' a getter - elbv2.

--- internal/service/elbv2/hosted_zone_id_data_source.go
+++ internal/service/elbv2/hosted_zone_id_data_source.go
@@ -117,7 +117,7 @@ func dataSourceHostedZoneID() *schema.Resource {
 func dataSourceHostedZoneIDRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
 
-	region := meta.(*conns.AWSClient).Region
+	region := meta.(*conns.AWSClient).Region(ctx)
 	if v, ok := d.GetOk(names.AttrRegion); ok {
 		region = v.(string)
 	}
