316ab737faddafbe03f4b9163542615d04a7fbf7
Andrew Tulloch <tullandr@amazon.co.uk>
2024-11-29 16:01:55 +0000

    Make gen

--- internal/service/servicecatalogappregistry/service_package_gen.go
+++ internal/service/servicecatalogappregistry/service_package_gen.go
@@ -43,13 +43,6 @@ func (p *servicePackage) FrameworkResources(ctx context.Context) []*types.Servic
 			},
 		},
 		{
-			Factory: newResourceAttributeGroup,
-			Name:    "Attribute Group",
-			Tags: &types.ServicePackageResourceTags{
-				IdentifierAttribute: names.AttrARN,
-			},
-		},
-		{
 			Factory: newResourceApplicationAttributeGroupAssociation,
 			Name:    "Application Attribute Group Association",
 		},
