d3feca50f79f54a260a9055e693c17a53a74330b
Andrew Tulloch <tullandr@amazon.co.uk>
2024-11-29 15:48:20 +0000

    Make gen

--- internal/service/servicecatalogappregistry/testdata/Application/data.tags_defaults/main_gen.tf
+++ internal/service/servicecatalogappregistry/testdata/Application/data.tags_defaults/main_gen.tf
@@ -1,6 +1,12 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
+provider "aws" {
+  default_tags {
+    tags = var.provider_tags
+  }
+}
+
 # tflint-ignore: terraform_unused_declarations
 data "aws_servicecatalogappregistry_application" "test" {
   id = aws_servicecatalogappregistry_application.test.id
@@ -23,4 +29,9 @@ variable "resource_tags" {
   # Not setting a default, so that this must explicitly be set to `null` to specify no tags
   type     = map(string)
   nullable = true
+}
+
+variable "provider_tags" {
+  type     = map(string)
+  nullable = false
 }
