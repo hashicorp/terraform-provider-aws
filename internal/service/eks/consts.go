14704fd12799309e0184d478d44dd7497fa1cfba
Bryant Biggs <bryantbiggs@gmail.com>
2024-11-29 09:47:28 -0600

    fix: Swap out hybrid node type for auto mode type

--- internal/service/eks/consts.go
+++ internal/service/eks/consts.go
@@ -26,19 +26,19 @@ const (
 )
 
 const (
+	accessEntryTypeEC2          = "EC2"
 	accessEntryTypeEC2Linux     = "EC2_LINUX"
 	accessEntryTypeEC2Windows   = "EC2_WINDOWS"
 	accessEntryTypeFargateLinux = "FARGATE_LINUX"
-	accessEntryTypeHybridLinux  = "HYBRID_LINUX"
 	accessEntryTypeStandard     = "STANDARD"
 )
 
 func accessEntryType_Values() []string {
 	return []string{
+		accessEntryTypeEC2,
 		accessEntryTypeEC2Linux,
 		accessEntryTypeEC2Windows,
 		accessEntryTypeFargateLinux,
-		accessEntryTypeHybridLinux,
 		accessEntryTypeStandard,
 	}
 }
