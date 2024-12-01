16f44c68d01e53fa6a0dae60ddc324202c4fd90f
Bruno Schaatsbergen <git@bschaatsbergen.com>
2024-12-01 15:13:43 -0800

    memorydb: [wip] implement `multi_region_cluster` resource

--- internal/service/memorydb/multi_region_cluster.go
+++ internal/service/memorydb/multi_region_cluster.go
@@ -5,9 +5,7 @@ package memorydb
 
 import (
 	"context"
-	"fmt"
 	"log"
-	"strconv"
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
@@ -22,8 +20,6 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
 	"github.com/hashicorp/terraform-provider-aws/internal/flex"
-	"github.com/hashicorp/terraform-provider-aws/internal/sdkv2"
-	tfslices "github.com/hashicorp/terraform-provider-aws/internal/slices"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
@@ -76,28 +72,36 @@ func resourceMultiRegionCluster() *schema.Resource {
 				"multi_region_cluster_name": {
 					Type:     schema.TypeString,
 					Optional: true,
+					Computed: true,
+				},
+				"multi_region_cluster_name_suffix": {
+					Type:     schema.TypeString,
+					Required: true,
 				},
 				"multi_region_parameter_group_name": {
 					Type:     schema.TypeString,
 					Optional: true,
+					Computed: true,
 				},
 				"node_type": {
 					Type:     schema.TypeString,
-					Optional: true,
+					Required: true,
 				},
-				"number_of_shards": {
+				"num_shards": {
 					Type:     schema.TypeInt,
 					Optional: true,
+					Computed: true,
 				},
 				"status": {
 					Type:     schema.TypeString,
 					Optional: true,
+					Computed: true,
 				},
 				"tls_enabled": {
 					Type:     schema.TypeBool,
 					Optional: true,
+					Computed: true,
 					Default:  true,
-					ForceNew: true,
 				},
 				names.AttrTags:    tftags.TagsSchema(),
 				names.AttrTagsAll: tftags.TagsSchemaComputed(),
@@ -106,43 +110,16 @@ func resourceMultiRegionCluster() *schema.Resource {
 	}
 }
 
-func endpointSchema() *schema.Schema {
-	return &schema.Schema{
-		Type:     schema.TypeList,
-		Computed: true,
-		Elem: &schema.Resource{
-			Schema: map[string]*schema.Schema{
-				names.AttrAddress: {
-					Type:     schema.TypeString,
-					Computed: true,
-				},
-				names.AttrPort: {
-					Type:     schema.TypeInt,
-					Computed: true,
-				},
-			},
-		},
-	}
-}
-
 func resourceMultiRegionClusterCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
 	conn := meta.(*conns.AWSClient).MemoryDBClient(ctx)
 
-	name := create.Name(d.Get(names.AttrName).(string), d.Get(names.AttrNamePrefix).(string))
+	name := create.Name(d.Get(names.AttrName).(string), "")
 	input := &memorydb.CreateMultiRegionClusterInput{
-		ACLName:                 aws.String(d.Get("acl_name").(string)),
-		AutoMinorVersionUpgrade: aws.Bool(d.Get(names.AttrAutoMinorVersionUpgrade).(bool)),
-		MultiRegionClusterName:  aws.String(name),
-		NodeType:                aws.String(d.Get("node_type").(string)),
-		NumReplicasPerShard:     aws.Int32(int32(d.Get("num_replicas_per_shard").(int))),
-		NumShards:               aws.Int32(int32(d.Get("num_shards").(int))),
-		Tags:                    getTagsIn(ctx),
-		TLSEnabled:              aws.Bool(d.Get("tls_enabled").(bool)),
-	}
-
-	if v, ok := d.GetOk("data_tiering"); ok {
-		input.DataTiering = aws.Bool(v.(bool))
+		NodeType:                     aws.String(d.Get("node_type").(string)),
+		MultiRegionClusterNameSuffix: aws.String(d.Get("multi_region_cluster_name_suffix").(string)),
+		TLSEnabled:                   aws.Bool(d.Get("tls_enabled").(bool)),
+		Tags:                         getTagsIn(ctx),
 	}
 
 	if v, ok := d.GetOk(names.AttrDescription); ok {
@@ -157,60 +134,24 @@ func resourceMultiRegionClusterCreate(ctx context.Context, d *schema.ResourceDat
 		input.EngineVersion = aws.String(v.(string))
 	}
 
-	if v, ok := d.GetOk(names.AttrKMSKeyARN); ok {
-		input.KmsKeyId = aws.String(v.(string))
+	if v, ok := d.GetOk("multi_region_parameter_group_name"); ok {
+		input.MultiRegionParameterGroupName = aws.String(v.(string))
 	}
 
-	if v, ok := d.GetOk("maintenance_window"); ok {
-		input.MaintenanceWindow = aws.String(v.(string))
-	}
-
-	if v, ok := d.GetOk(names.AttrParameterGroupName); ok {
-		input.ParameterGroupName = aws.String(v.(string))
-	}
-
-	if v, ok := d.GetOk(names.AttrPort); ok {
+	if v, ok := d.GetOk("num_shards"); ok {
 		input.Port = aws.Int32(int32(v.(int)))
-	}
-
-	if v, ok := d.GetOk(names.AttrSecurityGroupIDs); ok {
-		input.SecurityGroupIds = flex.ExpandStringValueSet(v.(*schema.Set))
-	}
-
-	if v, ok := d.GetOk("snapshot_arns"); ok && len(v.([]interface{})) > 0 {
-		input.SnapshotArns = flex.ExpandStringValueList(v.([]interface{}))
-	}
-
-	if v, ok := d.GetOk("snapshot_name"); ok {
-		input.SnapshotName = aws.String(v.(string))
-	}
-
-	if v, ok := d.GetOk("snapshot_retention_limit"); ok {
-		input.SnapshotRetentionLimit = aws.Int32(int32(v.(int)))
-	}
-
-	if v, ok := d.GetOk("snapshot_window"); ok {
-		input.SnapshotWindow = aws.String(v.(string))
-	}
-
-	if v, ok := d.GetOk(names.AttrSNSTopicARN); ok {
-		input.SnsTopicArn = aws.String(v.(string))
-	}
-
-	if v, ok := d.GetOk("subnet_group_name"); ok {
-		input.SubnetGroupName = aws.String(v.(string))
 	}
 
 	_, err := conn.CreateMultiRegionCluster(ctx, input)
 
 	if err != nil {
-		return sdkdiag.AppendErrorf(diags, "creating MemoryDB MultiRegionCluster (%s): %s", name, err)
+		return sdkdiag.AppendErrorf(diags, "creating MemoryDB Multi Region cluster (%s): %s", name, err)
 	}
 
 	d.SetId(name)
 
 	if _, err := waitMultiRegionClusterAvailable(ctx, conn, d.Id(), d.Timeout(schema.TimeoutCreate)); err != nil {
-		return sdkdiag.AppendErrorf(diags, "waiting for MemoryDB MultiRegionCluster (%s) create: %s", d.Id(), err)
+		return sdkdiag.AppendErrorf(diags, "waiting for MemoryDB Multi Region cluster (%s) create: %s", d.Id(), err)
 	}
 
 	return append(diags, resourceMultiRegionClusterRead(ctx, d, meta)...)
@@ -223,61 +164,25 @@ func resourceMultiRegionClusterRead(ctx context.Context, d *schema.ResourceData,
 	cluster, err := findMultiRegionClusterByName(ctx, conn, d.Id())
 
 	if !d.IsNewResource() && tfresource.NotFound(err) {
-		log.Printf("[WARN] MemoryDB MultiRegionCluster (%s) not found, removing from state", d.Id())
+		log.Printf("[WARN] MemoryDB Multi Region cluster (%s) not found, removing from state", d.Id())
 		d.SetId("")
 		return diags
 	}
 
 	if err != nil {
-		return sdkdiag.AppendErrorf(diags, "reading MemoryDB MultiRegionCluster (%s): %s", d.Id(), err)
+		return sdkdiag.AppendErrorf(diags, "reading MemoryDB Multi Region cluster (%s): %s", d.Id(), err)
 	}
 
-	d.Set("acl_name", cluster.ACLName)
 	d.Set(names.AttrARN, cluster.ARN)
-	d.Set(names.AttrAutoMinorVersionUpgrade, cluster.AutoMinorVersionUpgrade)
-	if v := cluster.MultiRegionClusterEndpoint; v != nil {
-		d.Set("cluster_endpoint", flattenEndpoint(v))
-		d.Set(names.AttrPort, v.Port)
-	}
-	if v := string(cluster.DataTiering); v != "" {
-		v, err := strconv.ParseBool(v)
-		if err != nil {
-			return sdkdiag.AppendFromErr(diags, err)
-		}
-
-		d.Set("data_tiering", v)
-	}
 	d.Set(names.AttrDescription, cluster.Description)
-	d.Set("engine_patch_version", cluster.EnginePatchVersion)
 	d.Set(names.AttrEngine, cluster.Engine)
 	d.Set(names.AttrEngineVersion, cluster.EngineVersion)
-	d.Set(names.AttrKMSKeyARN, cluster.KmsKeyId) // KmsKeyId is actually an ARN here.
-	d.Set("maintenance_window", cluster.MaintenanceWindow)
-	d.Set(names.AttrName, cluster.Name)
-	d.Set(names.AttrNamePrefix, create.NamePrefixFromName(aws.ToString(cluster.Name)))
+	d.Set("multi_region_cluster_name", cluster.MultiRegionClusterName)
 	d.Set("node_type", cluster.NodeType)
-	if v, err := deriveMultiRegionClusterNumReplicasPerShard(cluster); err != nil {
-		return sdkdiag.AppendFromErr(diags, err)
-	} else {
-		d.Set("num_replicas_per_shard", v)
-	}
 	d.Set("num_shards", cluster.NumberOfShards)
-	d.Set(names.AttrParameterGroupName, cluster.ParameterGroupName)
-	d.Set(names.AttrSecurityGroupIDs, tfslices.ApplyToAll(cluster.SecurityGroups, func(v awstypes.SecurityGroupMembership) string {
-		return aws.ToString(v.SecurityGroupId)
-	}))
-	if err := d.Set("shards", flattenShards(cluster.Shards)); err != nil {
-		return sdkdiag.AppendErrorf(diags, "setting shards: %s", err)
-	}
-	d.Set("snapshot_retention_limit", cluster.SnapshotRetentionLimit)
-	d.Set("snapshot_window", cluster.SnapshotWindow)
-	if aws.ToString(cluster.SnsTopicStatus) == clusterSNSTopicStatusActive {
-		d.Set(names.AttrSNSTopicARN, cluster.SnsTopicArn)
-	} else {
-		d.Set(names.AttrSNSTopicARN, "")
-	}
-	d.Set("subnet_group_name", cluster.SubnetGroupName)
+	d.Set("multi_region_parameter_group_name", cluster.MultiRegionParameterGroupName)
 	d.Set("tls_enabled", cluster.TLSEnabled)
+	d.Set(names.AttrStatus, cluster.Status)
 
 	return diags
 }
@@ -427,7 +332,7 @@ func resourceMultiRegionClusterDelete(ctx context.Context, d *schema.ResourceDat
 func findMultiRegionClusterByName(ctx context.Context, conn *memorydb.Client, name string) (*awstypes.MultiRegionCluster, error) {
 	input := &memorydb.DescribeMultiRegionClustersInput{
 		MultiRegionClusterName: aws.String(name),
-		ShowShardDetails:       aws.Bool(true),
+		ShowClusterDetails:     aws.Bool(true),
 	}
 
 	return findMultiRegionCluster(ctx, conn, input)
@@ -499,30 +404,6 @@ func statusMultiRegionClusterParameterGroup(ctx context.Context, conn *memorydb.
 	}
 }
 
-func statusMultiRegionClusterSecurityGroups(ctx context.Context, conn *memorydb.Client, name string) retry.StateRefreshFunc {
-	return func() (interface{}, string, error) {
-		output, err := findMultiRegionClusterByName(ctx, conn, name)
-
-		if tfresource.NotFound(err) {
-			return nil, "", nil
-		}
-
-		if err != nil {
-			return nil, "", err
-		}
-
-		for _, v := range output.SecurityGroups {
-			// When at least one security group change is being applied (whether
-			// that be adding or removing an SG), say that we're still in progress.
-			if aws.ToString(v.Status) != clusterSecurityGroupStatusActive {
-				return output, clusterSecurityGroupStatusModifying, nil
-			}
-		}
-
-		return output, clusterSecurityGroupStatusActive, nil
-	}
-}
-
 func waitMultiRegionClusterAvailable(ctx context.Context, conn *memorydb.Client, name string, timeout time.Duration) (*awstypes.MultiRegionCluster, error) { //nolint:unparam
 	stateConf := &retry.StateChangeConf{
 		Pending: []string{clusterStatusCreating, clusterStatusUpdating, clusterStatusSnapshotting},
@@ -575,99 +456,4 @@ func waitMultiRegionClusterParameterGroupInSync(ctx context.Context, conn *memor
 	}
 
 	return nil, err
-}
-
-func waitMultiRegionClusterSecurityGroupsActive(ctx context.Context, conn *memorydb.Client, name string) (*awstypes.MultiRegionCluster, error) {
-	const (
-		timeout = 10 * time.Minute
-	)
-	stateConf := &retry.StateChangeConf{
-		Pending: []string{clusterSecurityGroupStatusModifying},
-		Target:  []string{clusterSecurityGroupStatusActive},
-		Refresh: statusMultiRegionClusterSecurityGroups(ctx, conn, name),
-		Timeout: timeout,
-	}
-
-	outputRaw, err := stateConf.WaitForStateContext(ctx)
-
-	if output, ok := outputRaw.(*awstypes.MultiRegionCluster); ok {
-		return output, err
-	}
-
-	return nil, err
-}
-
-var (
-	clusterShardHash     = sdkv2.SimpleSchemaSetFunc(names.AttrName)
-	clusterShardNodeHash = sdkv2.SimpleSchemaSetFunc(names.AttrName)
-)
-
-func flattenEndpoint(apiObject *awstypes.Endpoint) []interface{} {
-	if apiObject == nil {
-		return []interface{}{}
-	}
-
-	tfMap := map[string]interface{}{}
-
-	if v := aws.ToString(apiObject.Address); v != "" {
-		tfMap[names.AttrAddress] = v
-	}
-
-	if apiObject.Port != 0 {
-		tfMap[names.AttrPort] = apiObject.Port
-	}
-
-	return []interface{}{tfMap}
-}
-
-func flattenShards(apiObjects []awstypes.Shard) *schema.Set {
-	tfSet := schema.NewSet(clusterShardHash, nil)
-
-	for _, apiObject := range apiObjects {
-		nodeSet := schema.NewSet(clusterShardNodeHash, nil)
-
-		for _, apiObject := range apiObject.Nodes {
-			nodeSet.Add(map[string]interface{}{
-				names.AttrAvailabilityZone: aws.ToString(apiObject.AvailabilityZone),
-				names.AttrCreateTime:       aws.ToTime(apiObject.CreateTime).Format(time.RFC3339),
-				names.AttrEndpoint:         flattenEndpoint(apiObject.Endpoint),
-				names.AttrName:             aws.ToString(apiObject.Name),
-			})
-		}
-
-		tfSet.Add(map[string]interface{}{
-			names.AttrName: aws.ToString(apiObject.Name),
-			"num_nodes":    aws.ToInt32(apiObject.NumberOfNodes),
-			"nodes":        nodeSet,
-			"slots":        aws.ToString(apiObject.Slots),
-		})
-	}
-
-	return tfSet
-}
-
-// deriveMultiRegionClusterNumReplicasPerShard determines the replicas per shard
-// configuration of a cluster. As this cannot directly be read back, we
-// assume that it's the same as that of the largest shard.
-//
-// For the sake of caution, this search is limited to stable shards.
-func deriveMultiRegionClusterNumReplicasPerShard(cluster *awstypes.MultiRegionCluster) (int, error) {
-	var maxNumberOfNodesPerShard int32
-
-	for _, shard := range cluster.Shards {
-		if aws.ToString(shard.Status) != clusterShardStatusAvailable {
-			continue
-		}
-
-		n := aws.ToInt32(shard.NumberOfNodes)
-		if n > maxNumberOfNodesPerShard {
-			maxNumberOfNodesPerShard = n
-		}
-	}
-
-	if maxNumberOfNodesPerShard == 0 {
-		return 0, fmt.Errorf("no available shards found")
-	}
-
-	return int(maxNumberOfNodesPerShard - 1), nil
 }
