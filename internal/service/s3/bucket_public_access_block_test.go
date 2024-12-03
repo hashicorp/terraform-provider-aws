4376738845a0c9aa3d612064da6b58b8853e3d32
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-12-03 11:31:44 -0500

    r/aws_s3_bucket_public_access_block: Handle directory buckets correctly.

--- internal/service/s3/bucket_public_access_block_test.go
+++ internal/service/s3/bucket_public_access_block_test.go
@@ -290,11 +290,15 @@ func TestAccS3BucketPublicAccessBlock_directoryBucket(t *testing.T) {
 
 func testAccCheckBucketPublicAccessBlockDestroy(ctx context.Context) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
-		conn := acctest.Provider.Meta().(*conns.AWSClient).S3Client(ctx)
-
 		for _, rs := range s.RootModule().Resources {
+			conn := acctest.Provider.Meta().(*conns.AWSClient).S3Client(ctx)
+
 			if rs.Type != "aws_s3_bucket_public_access_block" {
 				continue
+			}
+
+			if tfs3.IsDirectoryBucket(rs.Primary.ID) {
+				conn = acctest.Provider.Meta().(*conns.AWSClient).S3ExpressClient(ctx)
 			}
 
 			_, err := tfs3.FindPublicAccessBlockConfiguration(ctx, conn, rs.Primary.ID)
@@ -324,6 +328,9 @@ func testAccCheckBucketPublicAccessBlockExists(ctx context.Context, n string, v 
 		conn := acctest.Provider.Meta().(*conns.AWSClient).S3Client(ctx)
 
 		output, err := tfs3.FindPublicAccessBlockConfiguration(ctx, conn, rs.Primary.ID)
+		if tfs3.IsDirectoryBucket(rs.Primary.ID) {
+			conn = acctest.Provider.Meta().(*conns.AWSClient).S3ExpressClient(ctx)
+		}
 
 		if err != nil {
 			return err
