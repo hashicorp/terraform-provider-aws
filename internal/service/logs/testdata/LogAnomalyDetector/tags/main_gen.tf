80f16442e439ef87a28513ba682113fb3cc50985
Sharon Nam <sharon.nam@hashicorp.com>
2024-12-04 00:57:34 -0800

    Tags test generated

---
+++ internal/service/logs/testdata/LogAnomalyDetector/tags/main_gen.tf
@@ -0,0 +1,35 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+resource aws_cloudwatch_log_group "test" {
+  count = 2
+
+  name = "${var.rName}-${count.index}"
+}
+
+data aws_cloudwatch_log_groups "test" {
+  log_group_name_prefix = var.rName
+
+  depends_on = [aws_cloudwatch_log_group.test[0], aws_cloudwatch_log_group.test[1]]
+}
+
+resource "aws_logs_log_anomaly_detector" "test" {
+  detector_name        = var.rName
+  log_arn_group_list   = [aws_cloudwatch_log_group.test.arn]
+  evaluation_frequency = "TEN_MIN"
+  enabled              = "false"
+
+  tags = var.resource_tags
+}
+variable "rName" {
+  description = "Name for resource"
+  type        = string
+  nullable    = false
+}
+
+variable "resource_tags" {
+  description = "Tags to set on resource. To specify no tags, set to `null`"
+  # Not setting a default, so that this must explicitly be set to `null` to specify no tags
+  type     = map(string)
+  nullable = true
+}
---
+++ internal/service/logs/testdata/LogAnomalyDetector/tagsComputed1/main_gen.tf
@@ -0,0 +1,39 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+provider "null" {}
+
+resource aws_cloudwatch_log_group "test" {
+  count = 2
+
+  name = "${var.rName}-${count.index}"
+}
+
+data aws_cloudwatch_log_groups "test" {
+  log_group_name_prefix = var.rName
+
+  depends_on = [aws_cloudwatch_log_group.test[0], aws_cloudwatch_log_group.test[1]]
+}
+
+resource "aws_logs_log_anomaly_detector" "test" {
+  detector_name        = var.rName
+  log_arn_group_list   = [aws_cloudwatch_log_group.test.arn]
+  evaluation_frequency = "TEN_MIN"
+  enabled              = "false"
+
+  tags = {
+    (var.unknownTagKey) = null_resource.test.id
+  }
+}
+resource "null_resource" "test" {}
+
+variable "rName" {
+  description = "Name for resource"
+  type        = string
+  nullable    = false
+}
+
+variable "unknownTagKey" {
+  type     = string
+  nullable = false
+}
