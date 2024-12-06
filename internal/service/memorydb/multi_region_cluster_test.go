e1fa5af6db1da91b0232e274aaf0ac02cc2f9433
Bruno Schaatsbergen <git@bschaatsbergen.com>
2024-12-06 11:56:08 -0800

    memorydb: test resource attributes

--- internal/service/memorydb/multi_region_cluster_test.go
+++ internal/service/memorydb/multi_region_cluster_test.go
@@ -425,7 +425,7 @@ resource "aws_memorydb_cluster" "test" {
   acl_name                   = aws_memorydb_acl.test.id
   auto_minor_version_upgrade = false
   name                       = %[1]q
-  node_type                  = "db.t4g.small"
+  node_type                  = "db.r7g.xlarge"
   num_shards                 = 2
   security_group_ids         = [aws_security_group.test.id]
   snapshot_retention_limit   = 7
@@ -441,6 +441,7 @@ resource "aws_memorydb_cluster" "test" {
 resource "aws_memorydb_multi_region_cluster" "test" {
   name_suffix = %[1]q
   node_type   = "db.r7g.xlarge"
+  num_shards  = 2
 
   tags = {
     Test = "test"
