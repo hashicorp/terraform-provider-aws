602bc5045a9eecb407ff631b8f2b121c50bc83fe
Andrew Tulloch <tullandr@amazon.co.uk>
2024-12-09 22:09:22 +0000

    Make gen

--- internal/service/servicecatalogappregistry/service_package_gen.go
+++ internal/service/servicecatalogappregistry/service_package_gen.go
@@ -34,13 +34,6 @@ func (p *servicePackage) FrameworkDataSources(ctx context.Context) []*types.Serv
 				IdentifierAttribute: names.AttrARN,
 			},
 		},
-		{
-			Factory: newDataSourceAttributeGroup,
-			Name:    "Attribute Group",
-			Tags: &types.ServicePackageResourceTags{
-				IdentifierAttribute: names.AttrARN,
-			},
-		},
 	}
 }
 
