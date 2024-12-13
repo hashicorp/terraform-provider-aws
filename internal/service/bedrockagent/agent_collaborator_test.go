4b552a5788b14c49cf9a5d54ea035771bfaaccaa
Andrew Tulloch <tullandr@amazon.co.uk>
2024-12-13 16:21:33 +0000

    Fix imports

--- internal/service/bedrockagent/agent_collaborator_test.go
+++ internal/service/bedrockagent/agent_collaborator_test.go
@@ -7,10 +7,10 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/aws/aws-sdk-go-v2/service/bedrockagent/types"
 	"testing"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/bedrockagent/types"
 	sdkacctest "github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
