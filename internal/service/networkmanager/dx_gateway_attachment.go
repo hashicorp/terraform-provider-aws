d36c45c464f27658c879992bf7e9e2b071668933
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-12-15 16:48:01 -0500

    r/aws_networkmanager_dx_gateway_attachment: Use 'attachmentARN'.

--- internal/service/networkmanager/dx_gateway_attachment.go
+++ internal/service/networkmanager/dx_gateway_attachment.go
@@ -175,6 +175,8 @@ func (r *directConnectGatewayAttachmentResource) Create(ctx context.Context, req
 		return
 	}
 
+	data.ARN = flex.StringValueToFramework(ctx, attachmentARN(ctx, r.Meta(), data.ID.ValueString()))
+
 	response.Diagnostics.Append(response.State.Set(ctx, data)...)
 }
 
@@ -452,6 +454,7 @@ func waitDirectConnectGatewayAttachmentRejected(ctx context.Context, conn *netwo
 }
 
 type directConnectGatewayAttachmentResourceModel struct {
+	ARN                        types.String         `tfsdk:"arn"`
 	AttachmentPolicyRuleNumber types.Int64          `tfsdk:"attachment_policy_rule_number"`
 	AttachmentType             types.String         `tfsdk:"attachment_type"`
 	CoreNetworkARN             types.String         `tfsdk:"core_network_arn"`
@@ -461,7 +464,6 @@ type directConnectGatewayAttachmentResourceModel struct {
 	ID                         types.String         `tfsdk:"id"`
 	NetworkFunctionGroupName   types.String         `tfsdk:"network_function_group_name"`
 	OwnerAccountId             types.String         `tfsdk:"owner_account_id"`
-	ResourceARN                types.String         `tfsdk:"arn"`
 	SegmentName                types.String         `tfsdk:"segment_name"`
 	State                      types.String         `tfsdk:"state"`
 	Tags                       tftags.Map           `tfsdk:"tags"`
