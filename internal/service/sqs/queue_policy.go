dc7bb6c3e69e2af0326c39b45f3fc4a779299733
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-12-18 14:00:46 -0500

    Revert "r/aws_sqs_queue_policy: Use 'sdkv2.PolicyDocumentSchema'."
    This reverts commit 6f4f170b022dbf00ce74fb9e2583ecb6ed261714.

--- internal/service/sqs/queue_policy.go
+++ internal/service/sqs/queue_policy.go
@@ -6,7 +6,8 @@ package sqs
 import (
 	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hashicorp/terraform-provider-aws/internal/sdkv2"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/structure"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
@@ -34,7 +35,17 @@ func resourceQueuePolicy() *schema.Resource {
 		SchemaVersion: 1,
 
 		Schema: map[string]*schema.Schema{
-			names.AttrPolicy: sdkv2.PolicyDocumentSchema(true),
+			names.AttrPolicy: {
+				Type:                  schema.TypeString,
+				Required:              true,
+				ValidateFunc:          validation.StringIsJSON,
+				DiffSuppressFunc:      verify.SuppressEquivalentPolicyDiffs,
+				DiffSuppressOnRefresh: true,
+				StateFunc: func(v interface{}) string {
+					json, _ := structure.NormalizeJsonString(v)
+					return json
+				},
+			},
 			"queue_url": {
 				Type:     schema.TypeString,
 				Required: true,
