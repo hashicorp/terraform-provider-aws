d367f199a4c396724eeb763987f3ac6800d10fdb
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-12-18 11:49:10 -0500

    r/aws_sqs_queue_redrive_allow_policy: Add and use 'sdkv2.PolicyDocumentSchema'.

--- internal/sdkv2/schema.go
+++ internal/sdkv2/schema.go
@@ -5,6 +5,9 @@ package sdkv2
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/structure"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 // Adapted from https://github.com/hashicorp/terraform-provider-google/google/datasource_helpers.go. Thanks!
@@ -60,4 +63,23 @@ func DataSourceSchemaFromResourceSchema(rs map[string]*schema.Schema) map[string
 	}
 
 	return ds
+}
+
+// PolicyDocumentSchema returns the standard schema for a JSON policy document.
+func PolicyDocumentSchema(required bool) *schema.Schema {
+	schema := &schema.Schema{
+		Type:                  schema.TypeString,
+		ValidateFunc:          validation.StringIsJSON,
+		DiffSuppressFunc:      verify.SuppressEquivalentPolicyDiffs,
+		DiffSuppressOnRefresh: true,
+		StateFunc: func(v interface{}) string {
+			json, _ := structure.NormalizeJsonString(v)
+			return json
+		},
+	}
+	if required {
+		schema.Required = true
+	}
+
+	return schema
 }
