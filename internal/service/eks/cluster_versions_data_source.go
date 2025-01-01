007d33621cf914e2ca81dac0fc607b3638060917
hjoshi123 <mail2hemantjoshi@pm.me>
2025-01-01 16:42:13 -0700

    feat: added new data source aws_eks_cluster_versions

--- internal/service/eks/cluster_versions_data_source.go
+++ internal/service/eks/cluster_versions_data_source.go
@@ -4,25 +4,15 @@
 package eks
 
 import (
-	// TIP: ==== IMPORTS ====
-	// This is a common set of imports but not customized to your code since
-	// your code hasn't been written yet. Make sure you, your IDE, or
-	// goimports -w <file> fixes these imports.
-	//
-	// The provider linter wants your imports to be in two groups: first,
-	// standard library (i.e., "fmt" or "strings"), second, everything else.
-	//
-	// Also, AWS Go SDK v2 may handle nested structures differently than v1,
-	// using the services/eks/types package. If so, you'll
-	// need to import types and reference the nested types, e.g., as
-	// awstypes.<Type Name>.
 	"context"
-
+	"fmt"
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+
+	"github.com/hashicorp/terraform-plugin-framework-timetypes/timetypes"
+
 	"github.com/aws/aws-sdk-go-v2/service/eks"
 	awstypes "github.com/aws/aws-sdk-go-v2/service/eks/types"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/eks"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/types"
@@ -30,21 +20,9 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/framework"
 	"github.com/hashicorp/terraform-provider-aws/internal/framework/flex"
 	fwtypes "github.com/hashicorp/terraform-provider-aws/internal/framework/types"
-	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
-// TIP: ==== FILE STRUCTURE ====
-// All data sources should follow this basic outline. Improve this data source's
-// maintainability by sticking to it.
-//
-// 1. Package declaration
-// 2. Imports
-// 3. Main data source struct with schema method
-// 4. Read method
-// 5. Other functions (flatteners, expanders, waiters, finders, etc.)
-
-// Function annotations are used for datasource registration to the Provider. DO NOT EDIT.
 // @FrameworkDataSource("aws_eks_cluster_versions", name="Cluster Versions")
 func newDataSourceClusterVersions(context.Context) (datasource.DataSourceWithConfigure, error) {
 	return &dataSourceClusterVersions{}, nil
@@ -62,136 +40,114 @@ func (d *dataSourceClusterVersions) Metadata(_ context.Context, req datasource.M
 	resp.TypeName = "aws_eks_cluster_versions"
 }
 
-// TIP: ==== SCHEMA ====
-// In the schema, add each of the arguments and attributes in snake
-// case (e.g., delete_automated_backups).
-// * Alphabetize arguments to make them easier to find.
-// * Do not add a blank line between arguments/attributes.
-//
-// Users can configure argument values while attribute values cannot be
-// configured and are used as output. Arguments have either:
-// Required: true,
-// Optional: true,
-//
-// All attributes will be computed and some arguments. If users will
-// want to read updated information or detect drift for an argument,
-// it should be computed:
-// Computed: true,
-//
-// You will typically find arguments in the input struct
-// (e.g., CreateDBInstanceInput) for the create operation. Sometimes
-// they are only in the input struct (e.g., ModifyDBInstanceInput) for
-// the modify operation.
-//
-// For more about schema options, visit
-// https://developer.hashicorp.com/terraform/plugin/framework/handling-data/schemas?page=schemas
 func (d *dataSourceClusterVersions) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
 	resp.Schema = schema.Schema{
 		Attributes: map[string]schema.Attribute{
 			"cluster_type": schema.StringAttribute{
-				Computed: true,
+				Optional: true,
 			},
-			"cluster_versions": schema.StringAttribute{
-				Computed: true,
+			"default_only": schema.BoolAttribute{
+				Optional: true,
 			},
-			"default_platform_version": schema.StringAttribute{
-				Computed: true,
-			},
-			"default_version": schema.BoolAttribute{
-				Computed: true,
-			},
-			"end_of_extended_support_date": schema.StringAttribute{
-				Computed: true,
-			},
-			"end_of_standard_support_date": schema.StringAttribute{
-				Computed: true,
-			},
-			"kubernetes_patch_version": schema.StringAttribute{
-				Computed: true,
-			},
-			"release_date": schema.StringAttribute{
-				Computed: true,
+			"cluster_versions_only": schema.ListAttribute{
+				Optional:   true,
+				CustomType: fwtypes.ListOfStringType,
 			},
 			"status": schema.StringAttribute{
-				CustomType: fwtypes.StringEnumType(awstypes.ClusterVersionStatus),
-				Computed:   true,
+				Optional:   true,
+				CustomType: fwtypes.StringEnumType[awstypes.ClusterVersionStatus](),
 			},
+			"cluster_versions": framework.DataSourceComputedListOfObjectAttribute[customDataSourceClusterVersion](ctx),
 		},
 	}
 }
 
-// TIP: ==== ASSIGN CRUD METHODS ====
-// Data sources only have a read method.
 func (d *dataSourceClusterVersions) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
-	// TIP: ==== DATA SOURCE READ ====
-	// Generally, the Read function should do the following things. Make
-	// sure there is a good reason if you don't do one of these.
-	//
-	// 1. Get a client connection to the relevant service
-	// 2. Fetch the config
-	// 3. Get information about a resource from AWS
-	// 4. Set the ID, arguments, and attributes
-	// 5. Set the tags
-	// 6. Set the state
-	// TIP: -- 1. Get a client connection to the relevant service
 	conn := d.Meta().EKSClient(ctx)
 
-	// TIP: -- 2. Fetch the config
 	var data dataSourceClusterVersionsModel
 	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
+	input := &eks.DescribeClusterVersionsInput{}
+
+	if data.ClusterType.String() != "" {
+		input.ClusterType = aws.String(data.ClusterType.ValueString())
+	}
+
+	input.DefaultOnly = aws.Bool(data.DefaultOnly.ValueBool())
+
+	if len(data.ClusterVersionsOnly.Elements()) > 0 && !data.ClusterVersions.IsNull() {
+		clVersions := make([]string, 0, len(data.ClusterVersionsOnly.Elements()))
+		for _, v := range data.ClusterVersionsOnly.Elements() {
+			clVersions = append(clVersions, v.String())
+		}
+
+		input.ClusterVersions = clVersions
+	}
+
+	if data.Status.String() != "" {
+		input.Status = awstypes.ClusterVersionStatus(data.Status.ValueString())
+	}
+
 	// TIP: -- 3. Get information about a resource from AWS
-	out, err := findClusterVersionsByName(ctx, conn, data.Name.ValueString())
+	out, err := findClusterVersions(ctx, conn, input)
 	if err != nil {
-		resp.Diagnostics.AddError(
-			create.ProblemStandardMessage(names.EKS, create.ErrActionReading, DSNameClusterVersions, data.Name.String(), err),
-			err.Error(),
-		)
+		resp.Diagnostics.AddError(fmt.Sprint(names.EKS, create.ErrActionReading, DSNameClusterVersions, err), err.Error())
 		return
 	}
 
-	// TIP: -- 4. Set the ID, arguments, and attributes
-	// Using a field name prefix allows mapping fields such as `ClusterVersionsId` to `ID`
-	resp.Diagnostics.Append(flex.Flatten(ctx, out, &data, flex.WithFieldNamePrefix("ClusterVersions"))...)
+	output := &eks.DescribeClusterVersionsOutput{
+		ClusterVersions: out,
+	}
+
+	resp.Diagnostics.Append(flex.Flatten(ctx, output, &data)...)
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// TIP: -- 5. Set the tags
-	ignoreTagsConfig := d.Meta().IgnoreTagsConfig(ctx)
-	tags := KeyValueTags(ctx, out.Tags).IgnoreAWS().IgnoreConfig(ignoreTagsConfig)
-	data.Tags = tftags.FlattenStringValueMap(ctx, tags.Map())
-
-	// TIP: -- 6. Set the state
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
-// TIP: ==== DATA STRUCTURES ====
-// With Terraform Plugin-Framework configurations are deserialized into
-// Go types, providing type safety without the need for type assertions.
-// These structs should match the schema definition exactly, and the `tfsdk`
-// tag value should match the attribute name.
-//
-// Nested objects are represented in their own data struct. These will
-// also have a corresponding attribute type mapping for use inside flex
-// functions.
-//
-// See more:
-// https://developer.hashicorp.com/terraform/plugin/framework/handling-data/accessing-values
-type dataSourceClusterVersionsModel struct {
-	ARN             types.String                                          `tfsdk:"arn"`
-	ComplexArgument fwtypes.ListNestedObjectValueOf[complexArgumentModel] `tfsdk:"complex_argument"`
-	Description     types.String                                          `tfsdk:"description"`
-	ID              types.String                                          `tfsdk:"id"`
-	Name            types.String                                          `tfsdk:"name"`
-	Tags            tftags.Map                                            `tfsdk:"tags"`
-	Type            types.String                                          `tfsdk:"type"`
+func findClusterVersions(ctx context.Context, conn *eks.Client, input *eks.DescribeClusterVersionsInput) ([]awstypes.ClusterVersionInformation, error) {
+	output := make([]awstypes.ClusterVersionInformation, 0)
+
+	fmt.Printf("Finding cluster versions\n %v %v %v", input.ClusterVersions, aws.ToString(input.ClusterType), input.Status)
+	tflog.Debug(ctx, "Finding cluster versions", map[string]interface{}{"input": input})
+
+	pages := eks.NewDescribeClusterVersionsPaginator(conn, input)
+	for pages.HasMorePages() {
+		page, err := pages.NextPage(ctx)
+		if err != nil {
+			return nil, err
+		}
+
+		output = append(output, page.ClusterVersions...)
+	}
+
+	fmt.Printf("Found cluster versions %v", output)
+	tflog.Debug(ctx, "Found cluster versions", map[string]interface{}{"output": output})
+
+	return output, nil
 }
 
-type complexArgumentModel struct {
-	NestedRequired types.String `tfsdk:"nested_required"`
-	NestedOptional types.String `tfsdk:"nested_optional"`
+type dataSourceClusterVersionsModel struct {
+	ClusterType         types.String                                                    `tfsdk:"cluster_type"`
+	DefaultOnly         types.Bool                                                      `tfsdk:"default_only"`
+	ClusterVersionsOnly fwtypes.ListValueOf[types.String]                               `tfsdk:"cluster_versions_only"`
+	Status              fwtypes.StringEnum[awstypes.ClusterVersionStatus]               `tfsdk:"status"`
+	ClusterVersions     fwtypes.ListNestedObjectValueOf[customDataSourceClusterVersion] `tfsdk:"cluster_versions"`
+}
+
+type customDataSourceClusterVersion struct {
+	ClusterType              types.String                                      `tfsdk:"cluster_type"`
+	ClusterVersion           types.String                                      `tfsdk:"cluster_version"`
+	DefaultPlatformVersion   types.String                                      `tfsdk:"default_platform_version"`
+	EndOfExtendedSupportDate timetypes.RFC3339                                 `tfsdk:"end_of_extended_support_date"`
+	EndOfStandardSupportDate timetypes.RFC3339                                 `tfsdk:"end_of_standard_support_date"`
+	KubernetesPatchVersion   types.String                                      `tfsdk:"kubernetes_patch_version"`
+	ReleaseDate              timetypes.RFC3339                                 `tfsdk:"release_date"`
+	Status                   fwtypes.StringEnum[awstypes.ClusterVersionStatus] `tfsdk:"status"`
 }
