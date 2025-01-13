2ea7a166c5e1ff793214bad8919300e41b622ef8
Vladimir Romashov <vladimir.romashov@booking.com>
2025-01-13 23:01:44 +0100

    perf: isolated DescribeOrganization as part of existing organization service

--- internal/service/organizations/organization_data_source_test.go
+++ internal/service/organizations/organization_data_source_test.go
@@ -115,6 +115,39 @@ func testAccOrganizationDataSource_delegatedAdministrator(t *testing.T) {
 	})
 }
 
+// Retrieve only DescribeOrganization response attributes.
+func testAccOrganizationDataSource_describeOrganizationOnly(t *testing.T) {
+	ctx := acctest.Context(t)
+	resourceName := "aws_organizations_organization.test"
+	dataSourceName := "data.aws_organizations_organization.test"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+			acctest.PreCheckOrganizationsAccount(ctx, t)
+		},
+		ErrorCheck:               acctest.ErrorCheck(t, names.OrganizationsServiceID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccOrganizationDataSourceConfig_describeOrganizationOnly,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrPair(resourceName, names.AttrARN, dataSourceName, names.AttrARN),
+					resource.TestCheckResourceAttrPair(resourceName, "feature_set", dataSourceName, "feature_set"),
+					resource.TestCheckResourceAttrPair(resourceName, "master_account_arn", dataSourceName, "master_account_arn"),
+					resource.TestCheckResourceAttrPair(resourceName, "master_account_email", dataSourceName, "master_account_email"),
+					resource.TestCheckResourceAttrPair(resourceName, "master_account_id", dataSourceName, "master_account_id"),
+					resource.TestCheckNoResourceAttr(dataSourceName, "accounts.#"),
+					resource.TestCheckNoResourceAttr(dataSourceName, "aws_service_access_principals.#"),
+					resource.TestCheckNoResourceAttr(dataSourceName, "enabled_policy_types.#"),
+					resource.TestCheckNoResourceAttr(dataSourceName, "non_master_accounts.#"),
+					resource.TestCheckNoResourceAttr(dataSourceName, "roots.#"),
+				),
+			},
+		},
+	})
+}
+
 const testAccOrganizationDataSourceConfig_newOrganization = `
 resource "aws_organizations_organization" "test" {}
 
@@ -174,3 +207,12 @@ data "aws_organizations_organization" "test" {
   depends_on = [aws_organizations_resource_policy.test]
 }
 `)
+
+const testAccOrganizationDataSourceConfig_describeOrganizationOnly = `
+resource "aws_organizations_organization" "test" {}
+
+data "aws_organizations_organization" "test" {
+    describe_organization_only = true
+    depends_on = [aws_organizations_organization.test]
+}
+`
