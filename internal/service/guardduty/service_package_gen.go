a309786efdce1f2d963467ca5850988fa0ef5b82
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-01-14 10:39:55 -0500

    Adding 'name=' annotation values for 'SDKDataSource' - guardduty.

--- internal/service/guardduty/service_package_gen.go
+++ internal/service/guardduty/service_package_gen.go
@@ -40,6 +40,7 @@ func (p *servicePackage) SDKDataSources(ctx context.Context) []*types.ServicePac
 		{
 			Factory:  DataSourceDetector,
 			TypeName: "aws_guardduty_detector",
+			Name:     "Detector",
 		},
 	}
 }
