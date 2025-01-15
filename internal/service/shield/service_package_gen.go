e90c87fa66bf55c3aca8a58f331b21ed98372ec4
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-01-15 13:16:15 -0500

    Emit type name values for 'EphemeralResource' and 'FrameworkResource' - shield.

--- internal/service/shield/service_package_gen.go
+++ internal/service/shield/service_package_gen.go
@@ -24,24 +24,29 @@ func (p *servicePackage) FrameworkDataSources(ctx context.Context) []*types.Serv
 func (p *servicePackage) FrameworkResources(ctx context.Context) []*types.ServicePackageFrameworkResource {
 	return []*types.ServicePackageFrameworkResource{
 		{
-			Factory: newApplicationLayerAutomaticResponseResource,
-			Name:    "Application Layer Automatic Response",
+			Factory:  newApplicationLayerAutomaticResponseResource,
+			TypeName: "aws_shield_application_layer_automatic_response",
+			Name:     "Application Layer Automatic Response",
 		},
 		{
-			Factory: newDRTAccessLogBucketAssociationResource,
-			Name:    "DRT Log Bucket Association",
+			Factory:  newDRTAccessLogBucketAssociationResource,
+			TypeName: "aws_shield_drt_access_log_bucket_association",
+			Name:     "DRT Log Bucket Association",
 		},
 		{
-			Factory: newDRTAccessRoleARNAssociationResource,
-			Name:    "DRT Role ARN Association",
+			Factory:  newDRTAccessRoleARNAssociationResource,
+			TypeName: "aws_shield_drt_access_role_arn_association",
+			Name:     "DRT Role ARN Association",
 		},
 		{
-			Factory: newProactiveEngagementResource,
-			Name:    "Proactive Engagement",
+			Factory:  newProactiveEngagementResource,
+			TypeName: "aws_shield_proactive_engagement",
+			Name:     "Proactive Engagement",
 		},
 		{
-			Factory: newResourceSubscription,
-			Name:    "Subscription",
+			Factory:  newResourceSubscription,
+			TypeName: "aws_shield_subscription",
+			Name:     "Subscription",
 		},
 	}
 }
