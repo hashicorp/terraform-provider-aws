1333aa50a91b6ca9598f3d5a168cf3172888f5e5
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-01-15 15:48:41 -0500

    Emit type name values for 'FrameworkDataSource' - s3.

--- internal/service/s3/service_package_gen.go
+++ internal/service/s3/service_package_gen.go
@@ -15,8 +15,9 @@ type servicePackage struct{}
 func (p *servicePackage) FrameworkDataSources(ctx context.Context) []*types.ServicePackageFrameworkDataSource {
 	return []*types.ServicePackageFrameworkDataSource{
 		{
-			Factory: newDirectoryBucketsDataSource,
-			Name:    "Directory Buckets",
+			Factory:  newDirectoryBucketsDataSource,
+			TypeName: "aws_s3_directory_buckets",
+			Name:     "Directory Buckets",
 		},
 	}
 }
