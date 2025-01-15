cc7649ef2d4a31cd639bc6f9a759e14ff9eb2b09
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-01-15 15:48:20 -0500

    Emit type name values for 'FrameworkDataSource' - amp.

--- internal/service/amp/service_package_gen.go
+++ internal/service/amp/service_package_gen.go
@@ -17,8 +17,9 @@ type servicePackage struct{}
 func (p *servicePackage) FrameworkDataSources(ctx context.Context) []*types.ServicePackageFrameworkDataSource {
 	return []*types.ServicePackageFrameworkDataSource{
 		{
-			Factory: newDefaultScraperConfigurationDataSource,
-			Name:    "Default Scraper Configuration",
+			Factory:  newDefaultScraperConfigurationDataSource,
+			TypeName: "aws_prometheus_default_scraper_configuration",
+			Name:     "Default Scraper Configuration",
 		},
 	}
 }
