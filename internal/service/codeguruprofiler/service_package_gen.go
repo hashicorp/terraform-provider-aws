a66430e803c293262bdcb4163b605984f325c355
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-01-15 13:15:59 -0500

    Emit type name values for 'EphemeralResource' and 'FrameworkResource' - codeguruprofiler.

--- internal/service/codeguruprofiler/service_package_gen.go
+++ internal/service/codeguruprofiler/service_package_gen.go
@@ -26,8 +26,9 @@ func (p *servicePackage) FrameworkDataSources(ctx context.Context) []*types.Serv
 func (p *servicePackage) FrameworkResources(ctx context.Context) []*types.ServicePackageFrameworkResource {
 	return []*types.ServicePackageFrameworkResource{
 		{
-			Factory: newResourceProfilingGroup,
-			Name:    "Profiling Group",
+			Factory:  newResourceProfilingGroup,
+			TypeName: "aws_codeguruprofiler_profiling_group",
+			Name:     "Profiling Group",
 			Tags: &types.ServicePackageResourceTags{
 				IdentifierAttribute: names.AttrARN,
 			},
