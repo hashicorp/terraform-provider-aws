6026b477af32503a7e98355c8e0acb6c6d69dd96
Andrew Tulloch <tullandr@amazon.co.uk>
2025-01-21 16:21:52 +0100

    Imports

--- internal/service/vpclattice/resource_configuration_test.go
+++ internal/service/vpclattice/resource_configuration_test.go
@@ -7,12 +7,12 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/aws/aws-sdk-go-v2/service/vpclattice/types"
 	"testing"
 
 	"github.com/YakDriver/regexache"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/vpclattice"
+	"github.com/aws/aws-sdk-go-v2/service/vpclattice/types"
 	sdkacctest "github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
