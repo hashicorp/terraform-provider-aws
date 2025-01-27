a0847da0b80729b27ca19b85af0123076eef7674
Adrian Johnson <adrian.johnson@hashicorp.com>
2025-01-27 12:11:17 -0600

    cleanup

--- internal/flex/write_only.go
+++ internal/flex/write_only.go
@@ -13,27 +13,18 @@ type writeOnlyAttrGetter interface {
 	GetRawConfigAt(path cty.Path) (cty.Value, diag.Diagnostics)
 	Id() string
 }
-type attType cty.Type
-
-func (a attType) ctyType() cty.Type {
-	return cty.Type(a)
-}
-
-var (
-	AttrTypeString = attType(cty.String)
-)
 
 // GetWriteOnlyValue returns the value of the write-only attribute from the config.
-func GetWriteOnlyValue(d writeOnlyAttrGetter, path string, attrType attType) (cty.Value, diag.Diagnostics) {
+func GetWriteOnlyValue(d writeOnlyAttrGetter, path cty.Path, attrType cty.Type) (cty.Value, diag.Diagnostics) {
 	var diags diag.Diagnostics
 
-	valueWO, di := d.GetRawConfigAt(cty.GetAttrPath(path))
+	valueWO, di := d.GetRawConfigAt(path)
 	if di.HasError() {
 		diags = append(diags, di...)
 		return cty.Value{}, diags
 	}
 
-	if !valueWO.Type().Equals(attrType.ctyType()) {
+	if !valueWO.Type().Equals(attrType) {
 		return cty.Value{}, sdkdiag.AppendErrorf(diags, "SSM Parameter (%s): invalid value_wo type", d.Id())
 	}
 
--- internal/service/ssm/parameter.go
+++ internal/service/ssm/parameter.go
@@ -182,7 +182,7 @@ func resourceParameterCreate(ctx context.Context, d *schema.ResourceData, meta i
 		value = v
 	}
 
-	valueWO, di := flex.GetWriteOnlyValue(d, "value_wo", flex.AttrTypeString)
+	valueWO, di := flex.GetWriteOnlyValue(d, cty.GetAttrPath("value_wo"), cty.String)
 	diags = append(diags, di...)
 	if diags.HasError() {
 		return diags
@@ -290,7 +290,7 @@ func resourceParameterRead(ctx context.Context, d *schema.ResourceData, meta int
 	hasWriteOnly := d.Get("has_value_wo").(bool)
 	rawConfig := d.GetRawConfig()
 	if !rawConfig.IsNull() {
-		valueWO, di := flex.GetWriteOnlyValue(d, "value_wo", flex.AttrTypeString)
+		valueWO, di := flex.GetWriteOnlyValue(d, cty.GetAttrPath("value_wo"), cty.String)
 		diags = append(diags, di...)
 		if diags.HasError() {
 			return diags
@@ -349,7 +349,7 @@ func resourceParameterUpdate(ctx context.Context, d *schema.ResourceData, meta i
 		}
 
 		if d.HasChanges("value_wo_version") {
-			valueWO, di := flex.GetWriteOnlyValue(d, "value_wo", flex.AttrTypeString)
+			valueWO, di := flex.GetWriteOnlyValue(d, cty.GetAttrPath("value_wo"), cty.String)
 			diags = append(diags, di...)
 			if diags.HasError() {
 				return diags
