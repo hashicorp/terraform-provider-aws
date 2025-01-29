c7094f8eafe12c227bb53ddbd2e3091395e870f3
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-01-29 12:33:39 -0500

    Remove 'Metadata' implementation for Plugin Framework resources - pinpointsmsvoicev2.

--- internal/service/pinpointsmsvoicev2/configuration_set.go
+++ internal/service/pinpointsmsvoicev2/configuration_set.go
@@ -43,10 +43,6 @@ type configurationSetResource struct {
 	framework.WithImportByID
 }
 
-func (*configurationSetResource) Metadata(_ context.Context, request resource.MetadataRequest, response *resource.MetadataResponse) {
-	response.TypeName = "aws_pinpointsmsvoicev2_configuration_set"
-}
-
 func (r *configurationSetResource) Schema(ctx context.Context, request resource.SchemaRequest, response *resource.SchemaResponse) {
 	response.Schema = schema.Schema{
 		Attributes: map[string]schema.Attribute{
