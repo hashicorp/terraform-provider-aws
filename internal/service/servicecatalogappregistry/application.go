7764a350cbc89560fd898f779f6bd3029b1f2e2e
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-01-29 12:33:42 -0500

    Remove 'Metadata' implementation for Plugin Framework resources - servicecatalogappregistry.

--- internal/service/servicecatalogappregistry/application.go
+++ internal/service/servicecatalogappregistry/application.go
@@ -41,10 +41,6 @@ type resourceApplication struct {
 	framework.ResourceWithConfigure
 }
 
-func (r *resourceApplication) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
-	resp.TypeName = "aws_servicecatalogappregistry_application"
-}
-
 func (r *resourceApplication) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
 	resp.Schema = schema.Schema{
 		Attributes: map[string]schema.Attribute{
