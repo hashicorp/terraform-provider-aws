dde53eca4b8578494bcf96d0de02c63eab6d23e7
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-02-04 08:48:38 -0500

    Remove transparent tagging-only 'ModifyPlan' implementation for Plugin Framework resources - paymentcryptography.

--- internal/service/paymentcryptography/key.go
+++ internal/service/paymentcryptography/key.go
@@ -414,9 +414,6 @@ func (r *resourceKey) Delete(ctx context.Context, request resource.DeleteRequest
 func (r *resourceKey) ImportState(ctx context.Context, request resource.ImportStateRequest, response *resource.ImportStateResponse) {
 	resource.ImportStatePassthroughID(ctx, path.Root(names.AttrID), request, response)
 }
-func (r *resourceKey) ModifyPlan(ctx context.Context, request resource.ModifyPlanRequest, response *resource.ModifyPlanResponse) {
-	r.SetTagsAll(ctx, request, response)
-}
 
 func waitKeyCreated(ctx context.Context, conn *paymentcryptography.Client, id string, timeout time.Duration) (*awstypes.Key, error) {
 	stateConf := &retry.StateChangeConf{
