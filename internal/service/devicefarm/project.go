788e45628565739c43b41bcf82bd070371677e9f
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-02-05 13:58:50 -0500

    Remove transparent tagging-only CustomizeDiff - devicefarm.

--- internal/service/devicefarm/project.go
+++ internal/service/devicefarm/project.go
@@ -19,7 +19,6 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
-	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
@@ -53,8 +52,6 @@ func resourceProject() *schema.Resource {
 			names.AttrTags:    tftags.TagsSchema(),
 			names.AttrTagsAll: tftags.TagsSchemaComputed(),
 		},
-
-		CustomizeDiff: verify.SetTagsDiff,
 	}
 }
 
