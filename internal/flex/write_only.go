b4c78449c2c0d3ccf5344f070b51661671f1ec57
Adrian Johnson <adrian.johnson@hashicorp.com>
2025-02-05 15:55:00 -0500

    flex: add write only flex functions

---
+++ internal/flex/write_only.go
@@ -0,0 +1,32 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package flex
+
+import (
+	"github.com/hashicorp/go-cty/cty"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
+)
+
+type writeOnlyAttrGetter interface {
+	GetRawConfigAt(path cty.Path) (cty.Value, diag.Diagnostics)
+	Id() string
+}
+
+// GetWriteOnlyValue returns the value of the write-only attribute from the config.
+func GetWriteOnlyValue(d writeOnlyAttrGetter, path cty.Path, attrType cty.Type) (cty.Value, diag.Diagnostics) {
+	var diags diag.Diagnostics
+
+	valueWO, di := d.GetRawConfigAt(path)
+	if di.HasError() {
+		diags = append(diags, di...)
+		return cty.Value{}, diags
+	}
+
+	if !valueWO.Type().Equals(attrType) {
+		return cty.Value{}, sdkdiag.AppendErrorf(diags, "invalid type (%s) for resource(%s)", attrType, d.Id())
+	}
+
+	return valueWO, diags
+}
