b5c900cac8e18619985c2f21920109616e285e00
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-02-05 14:34:19 -0500

    'verify.SuppressEquivalentRoundedTime' ->'sdkv2.SuppressEquivalentRoundedTime'.

--- internal/verify/diff.go
+++ internal/verify/diff.go
@@ -4,26 +4,10 @@
 package verify
 
 import (
-	"time"
-
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 // Find JSON diff functions in the json.go file.
-
-// SuppressEquivalentRoundedTime returns a difference suppression function that compares
-// two time value with the specified layout rounded to the specified duration.
-func SuppressEquivalentRoundedTime(layout string, d time.Duration) schema.SchemaDiffSuppressFunc {
-	return func(k, old, new string, _ *schema.ResourceData) bool {
-		if old, err := time.Parse(layout, old); err == nil {
-			if new, err := time.Parse(layout, new); err == nil {
-				return old.Round(d).Equal(new.Round(d))
-			}
-		}
-
-		return false
-	}
-}
 
 // SuppressMissingOptionalConfigurationBlock handles configuration block attributes in the following scenario:
 //   - The resource schema includes an optional configuration block with defaults
