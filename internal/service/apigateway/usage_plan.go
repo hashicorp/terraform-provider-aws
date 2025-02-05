ae42c2563a554dfce4ee380b8dbb11675c2e7a31
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-02-05 13:58:44 -0500

    Remove transparent tagging-only CustomizeDiff - apigateway.

--- internal/service/apigateway/usage_plan.go
+++ internal/service/apigateway/usage_plan.go
@@ -22,7 +22,6 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
-	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
@@ -142,8 +141,6 @@ func resourceUsagePlan() *schema.Resource {
 				},
 			},
 		},
-
-		CustomizeDiff: verify.SetTagsDiff,
 	}
 }
 
