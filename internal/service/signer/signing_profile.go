996f818f4ea7b2cdf9b895e2e952ba66fd34b242
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-02-05 13:59:04 -0500

    Remove transparent tagging-only CustomizeDiff - signer.

--- internal/service/signer/signing_profile.go
+++ internal/service/signer/signing_profile.go
@@ -23,7 +23,6 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
-	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
@@ -144,8 +143,6 @@ func ResourceSigningProfile() *schema.Resource {
 				Computed: true,
 			},
 		},
-
-		CustomizeDiff: verify.SetTagsDiff,
 	}
 }
 
