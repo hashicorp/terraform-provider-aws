bcea85e18eafafd3fed74971b3dc1f96035d6a19
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-02-05 13:58:55 -0500

    Remove transparent tagging-only CustomizeDiff - ivschat.

--- internal/service/ivschat/room.go
+++ internal/service/ivschat/room.go
@@ -97,8 +97,6 @@ func ResourceRoom() *schema.Resource {
 			names.AttrTags:    tftags.TagsSchema(),
 			names.AttrTagsAll: tftags.TagsSchemaComputed(),
 		},
-
-		CustomizeDiff: verify.SetTagsDiff,
 	}
 }
 
