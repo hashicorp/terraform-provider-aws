eae765dd06e4abfd4f047730b6f83ee1467dfbce
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-02-05 13:58:49 -0500

    Remove transparent tagging-only CustomizeDiff - cur.

--- internal/service/cur/report_definition.go
+++ internal/service/cur/report_definition.go
@@ -25,7 +25,6 @@ import (
 	tfslices "github.com/hashicorp/terraform-provider-aws/internal/slices"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
-	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
@@ -117,8 +116,6 @@ func resourceReportDefinition() *schema.Resource {
 			names.AttrTags:    tftags.TagsSchema(),
 			names.AttrTagsAll: tftags.TagsSchemaComputed(),
 		},
-
-		CustomizeDiff: verify.SetTagsDiff,
 	}
 }
 
