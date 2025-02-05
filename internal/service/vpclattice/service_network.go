02483418a102001d359bcd0b8551611cb311c8c1
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-02-05 13:59:06 -0500

    Remove transparent tagging-only CustomizeDiff - vpclattice.

--- internal/service/vpclattice/service_network.go
+++ internal/service/vpclattice/service_network.go
@@ -22,7 +22,6 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
-	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
@@ -60,8 +59,6 @@ func resourceServiceNetwork() *schema.Resource {
 			names.AttrTags:    tftags.TagsSchema(),
 			names.AttrTagsAll: tftags.TagsSchemaComputed(),
 		},
-
-		CustomizeDiff: verify.SetTagsDiff,
 	}
 }
 
