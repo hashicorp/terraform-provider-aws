d1360e9351ab26554c40541aaa748b189fa1ebce
Dirk Avery <dirk.avery@gmail.com>
2025-02-07 18:48:10 -0500

    Update tags for special handling

--- internal/service/sts/caller_identity_data_source.go
+++ internal/service/sts/caller_identity_data_source.go
@@ -5,6 +5,7 @@ package sts
 
 import (
 	"context"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/sts"
@@ -80,6 +81,8 @@ func (d *callerIdentityDataSource) Read(ctx context.Context, request datasource.
 
 func findCallerIdentity(ctx context.Context, conn *sts.Client) (*sts.GetCallerIdentityOutput, error) {
 	input := &sts.GetCallerIdentityInput{}
+
+	time.Sleep(15 * time.Second)
 
 	output, err := conn.GetCallerIdentity(ctx, input)
 
