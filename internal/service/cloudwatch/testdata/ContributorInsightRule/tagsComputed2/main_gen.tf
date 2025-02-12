1f52dc5913593366f1a143e8566daf4f214b07af
Sharon Nam <sharon.nam@hashicorp.com>
2025-02-12 15:29:06 -0800

    Run tag gen after template change

--- internal/service/cloudwatch/testdata/ContributorInsightRule/tagsComputed2/main_gen.tf
+++ internal/service/cloudwatch/testdata/ContributorInsightRule/tagsComputed2/main_gen.tf
@@ -4,9 +4,9 @@
 provider "null" {}
 
 resource "aws_cloudwatch_contributor_insight_rule" "test" {
-  rule_name             = var.rName
-  rule_state             = "ENABLED"
-  rule_definition  = "{\"Schema\":{\"Name\":\"CloudWatchLogRule\",\"Version\":1},\"AggregateOn\":\"Count\",\"Contribution\":{\"Filters\":[{\"In\":[\"some-keyword\"],\"Match\":\"$.message\"}],\"Keys\":[\"$.country\"]},\"LogFormat\":\"JSON\",\"LogGroupNames\":[\"/aws/lambda/api-prod\"]}"
+  rule_name       = var.rName
+  rule_state      = "ENABLED"
+  rule_definition = "{\"Schema\":{\"Name\":\"CloudWatchLogRule\",\"Version\":1},\"AggregateOn\":\"Count\",\"Contribution\":{\"Filters\":[{\"In\":[\"some-keyword\"],\"Match\":\"$.message\"}],\"Keys\":[\"$.country\"]},\"LogFormat\":\"JSON\",\"LogGroupNames\":[\"/aws/lambda/api-prod\"]}"
 
   tags = {
     (var.unknownTagKey) = null_resource.test.id
