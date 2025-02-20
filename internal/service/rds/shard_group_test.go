476f88a509ddecd652507e46e4ca1523b9d4c617
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-02-20 17:01:46 -0500

    Complete 'TestAccRDSShardGroup_basic'.

--- internal/service/rds/shard_group_test.go
+++ internal/service/rds/shard_group_test.go
@@ -47,6 +47,14 @@ func TestAccRDSShardGroup_basic(t *testing.T) {
 				},
 				ConfigStateChecks: []statecheck.StateCheck{
 					statecheck.ExpectKnownValue(resourceName, tfjsonpath.New(names.AttrARN), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue(resourceName, tfjsonpath.New("compute_redundancy"), knownvalue.Int64Exact(0)),
+					statecheck.ExpectKnownValue(resourceName, tfjsonpath.New("db_shard_group_identifier"), knownvalue.StringExact(rName)),
+					statecheck.ExpectKnownValue(resourceName, tfjsonpath.New("db_shard_group_resource_id"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue(resourceName, tfjsonpath.New(names.AttrEndpoint), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue(resourceName, tfjsonpath.New("max_acu"), knownvalue.Float64Exact(1000)),
+					statecheck.ExpectKnownValue(resourceName, tfjsonpath.New("min_acu"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue(resourceName, tfjsonpath.New(names.AttrPubliclyAccessible), knownvalue.Bool(false)),
+					statecheck.ExpectKnownValue(resourceName, tfjsonpath.New(names.AttrTags), knownvalue.Null()),
 				},
 			},
 			{
@@ -67,7 +75,7 @@ func testAccCheckShardGroupDestroy(ctx context.Context) resource.TestCheckFunc {
 				continue
 			}
 
-			_, err := tfrds.FindDBShardGroupByID(ctx, conn, rs.Primary.ID)
+			_, err := tfrds.FindDBShardGroupByID(ctx, conn, rs.Primary.Attributes["db_shard_group_identifier"])
 
 			if tfresource.NotFound(err) {
 				continue
@@ -93,7 +101,7 @@ func testAccCheckShardGroupExists(ctx context.Context, n string, v *awstypes.DBS
 
 		conn := acctest.Provider.Meta().(*conns.AWSClient).RDSClient(ctx)
 
-		output, err := tfrds.FindDBShardGroupByID(ctx, conn, rs.Primary.ID)
+		output, err := tfrds.FindDBShardGroupByID(ctx, conn, rs.Primary.Attributes["db_shard_group_identifier"])
 
 		if err != nil {
 			return err
@@ -105,6 +113,61 @@ func testAccCheckShardGroupExists(ctx context.Context, n string, v *awstypes.DBS
 	}
 }
 
+func testAccShardGroupConfig_base(rName string) string {
+	return fmt.Sprintf(`
+data "aws_partition" "current" {}
+
+resource "aws_iam_role" "test" {
+  name = %[1]q
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "monitoring.rds.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "test" {
+  policy_arn = "arn:${data.aws_partition.current.partition}:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
+  role       = aws_iam_role.test.name
+}
+
+# https://docs.aws.amazon.com/AmazonRDS/latest/AuroraUserGuide/limitless-create-cluster.html.
+resource "aws_rds_cluster" "test" {
+  cluster_identifier                    = %[1]q
+  engine                                = "aurora-postgresql"
+  engine_version                        = "16.6-limitless"
+  engine_mode                           = ""
+  storage_type                          = "aurora-iopt1"
+  cluster_scalability_type              = "limitless"
+  master_username                       = "tfacctest"
+  master_password                       = "avoid-plaintext-passwords"
+  skip_final_snapshot                   = true
+  performance_insights_enabled          = true
+  performance_insights_retention_period = 31
+  enabled_cloudwatch_logs_exports       = ["postgresql"]
+  monitoring_interval                   = 5
+  monitoring_role_arn                   = aws_iam_role.test.arn
+}
+`, rName)
+}
+
 func testAccShardGroupConfig_basic(rName string) string {
-	return ""
+	return acctest.ConfigCompose(testAccShardGroupConfig_base(rName), fmt.Sprintf(`
+resource "aws_rds_shard_group" "test" {
+  db_shard_group_identifier = %[1]q
+  db_cluster_identifier     = aws_rds_cluster.test.id
+  max_acu                   = 1000
+}
+`, rName))
 }
