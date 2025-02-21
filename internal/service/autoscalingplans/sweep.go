094f9c0db90fba9332a82a6d33267407a9b83157
Graham Davison <gdavison@hashicorp.com>
2025-02-21 12:48:51 -0800

    Uses updated sweeper registration

--- internal/service/autoscalingplans/sweep.go
+++ internal/service/autoscalingplans/sweep.go
@@ -4,35 +4,27 @@
 package autoscalingplans
 
 import (
-	"fmt"
-	"log"
+	"context"
 
 	"github.com/aws/aws-sdk-go-v2/service/autoscalingplans"
-	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 	"github.com/hashicorp/terraform-provider-aws/internal/sweep/awsv2"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
 func RegisterSweepers() {
-	resource.AddTestSweepers("aws_autoscalingplans_scaling_plan", &resource.Sweeper{
-		Name: "aws_autoscalingplans_scaling_plan",
-		F:    sweepScalingPlans,
-	})
+	awsv2.Register("aws_autoscalingplans_scaling_plan", sweepScalingPlans)
 }
 
-func sweepScalingPlans(region string) error {
-	ctx := sweep.Context(region)
-	client, err := sweep.SharedRegionalSweepClient(ctx, region)
-	if err != nil {
-		return fmt.Errorf("error getting client: %s", err)
-	}
+func sweepScalingPlans(ctx context.Context, client *conns.AWSClient) ([]sweep.Sweepable, error) {
 	conn := client.AutoScalingPlansClient(ctx)
+
 	var sweepResources []sweep.Sweepable
 	r := ResourceScalingPlan()
-	input := autoscalingplans.DescribeScalingPlansInput{}
 
-	err = describeScalingPlansPages(ctx, conn, &input, func(page *autoscalingplans.DescribeScalingPlansOutput, lastPage bool) bool {
+	input := autoscalingplans.DescribeScalingPlansInput{}
+	err := describeScalingPlansPages(ctx, conn, &input, func(page *autoscalingplans.DescribeScalingPlansOutput, lastPage bool) bool {
 		if page == nil {
 			return !lastPage
 		}
@@ -49,20 +41,5 @@ func sweepScalingPlans(region string) error {
 		return !lastPage
 	})
 
-	if awsv2.SkipSweepError(err) {
-		log.Printf("[WARN] Skipping Auto Scaling Scaling Plan sweep for %s: %s", region, err)
-		return nil
-	}
-
-	if err != nil {
-		return fmt.Errorf("error listing Auto Scaling Scaling Plans (%s): %w", region, err)
-	}
-
-	err = sweep.SweepOrchestrator(ctx, sweepResources)
-
-	if err != nil {
-		return fmt.Errorf("error sweeping Auto Scaling Scaling Plans (%s): %w", region, err)
-	}
-
-	return nil
+	return sweepResources, err
 }
