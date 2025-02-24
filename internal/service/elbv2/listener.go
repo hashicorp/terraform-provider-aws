851a81a28fc706b914bb19f75ef1673d6629f77f
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-02-24 11:37:27 -0500

    Add 'canonicalListenerProtocol'.

--- internal/service/elbv2/listener.go
+++ internal/service/elbv2/listener.go
@@ -29,6 +29,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
 	"github.com/hashicorp/terraform-provider-aws/internal/flex"
+	tfmaps "github.com/hashicorp/terraform-provider-aws/internal/maps"
 	tfslices "github.com/hashicorp/terraform-provider-aws/internal/slices"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
@@ -563,16 +564,7 @@ func suppressIfDefaultActionTypeNot(t awstypes.ActionTypeEnum) schema.SchemaDiff
 
 func suppressIfListenerProtocolNot(protocols ...awstypes.ProtocolEnum) schema.SchemaDiffSuppressFunc {
 	return func(k string, old string, new string, d *schema.ResourceData) bool {
-		protocolType := awstypes.ProtocolEnum(d.Get(names.AttrProtocol).(string))
-
-		// GENEVE protocol for GWLB listeners cannot be specified on create
-		// If protocol is blank (i.e. GWLB listener on plan) or load balancer ARN contains 'gwy',
-		// assume GENEVE protocol.
-		if protocolType == awstypes.ProtocolEnum("") || strings.Contains(d.Get("load_balancer_arn").(string), "gwy") {
-			protocolType = awstypes.ProtocolEnumGeneve
-		}
-
-		return !slices.Contains(protocols, protocolType)
+		return !slices.Contains(protocols, canonicalListenerProtocol(awstypes.ProtocolEnum(d.Get(names.AttrProtocol).(string)), d.Get("load_balancer_arn").(string)))
 	}
 }
 
@@ -672,17 +664,8 @@ func resourceListenerCreate(ctx context.Context, d *schema.ResourceData, meta in
 		}
 	}
 
-	listenerProtocolType := awstypes.ProtocolEnum(d.Get(names.AttrProtocol).(string))
-	// Protocol does not need to be explicitly set with GWLB listeners, nor is it returned by the API
-	// If protocol is not set, use the load balancer ARN to determine if listener is gateway type and set protocol appropriately
-	if listenerProtocolType == awstypes.ProtocolEnum("") && strings.Contains(lbARN, "loadbalancer/gwy/") {
-		listenerProtocolType = awstypes.ProtocolEnumGeneve
-	}
-
-	// Listener attributes like TCP idle timeout are not supported on create
-	attributes := listenerAttributes.expand(d, listenerProtocolType, false)
-
-	if len(attributes) > 0 {
+	// Listener attributes like TCP idle timeout are not supported on create.
+	if attributes := listenerAttributes.expand(d, canonicalListenerProtocol(awstypes.ProtocolEnum(d.Get(names.AttrProtocol).(string)), lbARN), false); len(attributes) > 0 {
 		if err := modifyListenerAttributes(ctx, conn, d.Id(), attributes); err != nil {
 			return sdkdiag.AppendFromErr(diags, err)
 		}
@@ -746,7 +729,8 @@ func resourceListenerUpdate(ctx context.Context, d *schema.ResourceData, meta in
 	var diags diag.Diagnostics
 	conn := meta.(*conns.AWSClient).ELBV2Client(ctx)
 
-	if d.HasChangesExcept(names.AttrTags, names.AttrTagsAll, "tcp_idle_timeout_seconds") {
+	listenerAttributeKeys := tfmaps.Keys(listenerAttributes)
+	if d.HasChangesExcept(append([]string{names.AttrTags, names.AttrTagsAll}, listenerAttributeKeys...)...) {
 		input := &elasticloadbalancingv2.ModifyListenerInput{
 			ListenerArn: aws.String(d.Id()),
 		}
@@ -793,18 +777,11 @@ func resourceListenerUpdate(ctx context.Context, d *schema.ResourceData, meta in
 		}
 	}
 
-	lbARN := d.Get("load_balancer_arn").(string)
-	listenerProtocolType := awstypes.ProtocolEnum(d.Get(names.AttrProtocol).(string))
-	// Protocol does not need to be explicitly set with GWLB listeners, nor is it returned by the API
-	// If protocol is not set, use the load balancer ARN to determine if listener is gateway type and set protocol appropriately
-	if listenerProtocolType == awstypes.ProtocolEnum("") && strings.Contains(lbARN, "loadbalancer/gwy/") {
-		listenerProtocolType = awstypes.ProtocolEnumGeneve
-	}
-
-	updatedAttributes := listenerAttributes.expand(d, listenerProtocolType, true)
-	if len(updatedAttributes) > 0 {
-		if err := modifyListenerAttributes(ctx, conn, d.Id(), updatedAttributes); err != nil {
-			return sdkdiag.AppendFromErr(diags, err)
+	if d.HasChanges(listenerAttributeKeys...) {
+		if attributes := listenerAttributes.expand(d, canonicalListenerProtocol(awstypes.ProtocolEnum(d.Get(names.AttrProtocol).(string)), d.Get("load_balancer_arn").(string)), true); len(attributes) > 0 {
+			if err := modifyListenerAttributes(ctx, conn, d.Id(), attributes); err != nil {
+				return sdkdiag.AppendFromErr(diags, err)
+			}
 		}
 	}
 
@@ -825,6 +802,16 @@ func resourceListenerDelete(ctx context.Context, d *schema.ResourceData, meta in
 	}
 
 	return diags
+}
+
+func canonicalListenerProtocol(protocol awstypes.ProtocolEnum, lbARN string) awstypes.ProtocolEnum {
+	// Protocol does not need to be explicitly set with GWLB listeners, nor is it returned by the API
+	// If protocol is not set, use the load balancer ARN to determine if listener is gateway type and set protocol appropriately
+	if protocol == awstypes.ProtocolEnum("") && strings.Contains(lbARN, "loadbalancer/gwy/") {
+		protocol = awstypes.ProtocolEnumGeneve
+	}
+
+	return protocol
 }
 
 func findListenerAttributesByARN(ctx context.Context, conn *elasticloadbalancingv2.Client, listenerARN string) ([]awstypes.ListenerAttribute, error) {
