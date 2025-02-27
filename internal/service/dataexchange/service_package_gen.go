41fe7803a1533f620f23418e04574f77c399c9eb
Graham Davison <gdavison@hashicorp.com>
2025-02-27 10:07:44 -0800

    Regenerates service package

--- internal/service/dataexchange/service_package_gen.go
+++ internal/service/dataexchange/service_package_gen.go
@@ -21,8 +21,9 @@ func (p *servicePackage) FrameworkDataSources(ctx context.Context) []*types.Serv
 func (p *servicePackage) FrameworkResources(ctx context.Context) []*types.ServicePackageFrameworkResource {
 	return []*types.ServicePackageFrameworkResource{
 		{
-			Factory: ResourceEventAction,
-			Name:    "Event Action",
+			Factory:  ResourceEventAction,
+			TypeName: "aws_dataexchange_event_action",
+			Name:     "Event Action",
 		},
 	}
 }
