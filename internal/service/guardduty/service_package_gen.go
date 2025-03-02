302926d74c10b5e59d33e4e335085f290339f710
twelsh-aw <84401379+twelsh-aw@users.noreply.github.com>
2025-03-01 21:27:55 -0500

    Add aws_guardduty_malware_scan_settings resource

--- internal/service/guardduty/service_package_gen.go
+++ internal/service/guardduty/service_package_gen.go
@@ -43,6 +43,11 @@ func (p *servicePackage) FrameworkResources(ctx context.Context) []*inttypes.Ser
 			Region: unique.Make(inttypes.ResourceRegionDefault()),
 		},
 		{
+			Factory:  newResourceMalwareScanSettings,
+			TypeName: "aws_guardduty_malware_scan_settings",
+			Name:     "Malware Scan Settings",
+		},
+		{
 			Factory:  newMemberDetectorFeatureResource,
 			TypeName: "aws_guardduty_member_detector_feature",
 			Name:     "Member Detector Feature",
