891324a0be402f811211e3fb493e73c1077196c9
Adrian Johnson <adrian.johnson@hashicorp.com>
2025-03-06 14:33:43 -0600

    aws_datazone_domain: add additional attribute

--- internal/service/datazone/domain_data_source.go
+++ internal/service/datazone/domain_data_source.go
@@ -9,8 +9,11 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/datazone"
 	awstypes "github.com/aws/aws-sdk-go-v2/service/datazone/types"
+	"github.com/hashicorp/terraform-plugin-framework-timetypes/timetypes"
+	"github.com/hashicorp/terraform-plugin-framework-validators/datasourcevalidator"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-provider-aws/internal/create"
 	"github.com/hashicorp/terraform-provider-aws/internal/framework"
@@ -37,9 +40,36 @@ func (d *dataSourceDataZoneDomain) Schema(_ context.Context, _ datasource.Schema
 	response.Schema = schema.Schema{
 		Attributes: map[string]schema.Attribute{
 			names.AttrARN: framework.ARNAttributeComputedOnly(),
-			names.AttrID:  framework.IDAttribute(),
+			names.AttrCreatedAt: schema.StringAttribute{
+				CustomType: timetypes.RFC3339Type{},
+				Computed:   true,
+			},
+			names.AttrDescription: schema.StringAttribute{
+				Computed: true,
+			},
+			"domain_version": schema.StringAttribute{
+				Computed: true,
+			},
+			names.AttrID: schema.StringAttribute{
+				Optional: true,
+				Computed: true,
+			},
+			"last_updated_at": schema.StringAttribute{
+				CustomType: timetypes.RFC3339Type{},
+				Computed:   true,
+			},
+			"managed_account_id": schema.StringAttribute{
+				Computed: true,
+			},
 			names.AttrName: schema.StringAttribute{
-				Required: true,
+				Optional: true,
+				Computed: true,
+			},
+			"portal_url": schema.StringAttribute{
+				Computed: true,
+			},
+			names.AttrStatus: schema.StringAttribute{
+				Computed: true,
 			},
 		},
 	}
@@ -54,10 +84,21 @@ func (d *dataSourceDataZoneDomain) Read(ctx context.Context, request datasource.
 		return
 	}
 
-	output, err := findDomain(ctx, conn, func(domain *awstypes.DomainSummary) bool {
-		return aws.ToString(domain.Name) == data.Name.ValueString()
-	})
+	// set filter for input attribute
+	var filter tfslices.Predicate[*awstypes.DomainSummary]
+	if !data.Name.IsNull() {
+		filter = func(domain *awstypes.DomainSummary) bool {
+			return aws.ToString(domain.Name) == data.Name.ValueString()
+		}
+	}
 
+	if !data.ID.IsNull() {
+		filter = func(domain *awstypes.DomainSummary) bool {
+			return aws.ToString(domain.Id) == data.ID.ValueString()
+		}
+	}
+
+	output, err := findDomain(ctx, conn, filter)
 	if err != nil {
 		response.Diagnostics.AddError(
 			create.ProblemStandardMessage(names.DataZone, create.ErrActionReading, DSNameDataZoneDomain, data.Name.String(), err),
@@ -72,6 +113,15 @@ func (d *dataSourceDataZoneDomain) Read(ctx context.Context, request datasource.
 	}
 
 	response.Diagnostics.Append(response.State.Set(ctx, &data)...)
+}
+
+func (d *dataSourceDataZoneDomain) ConfigValidators(_ context.Context) []datasource.ConfigValidator {
+	return []datasource.ConfigValidator{
+		datasourcevalidator.ExactlyOneOf(
+			path.MatchRoot(names.AttrName),
+			path.MatchRoot(names.AttrID),
+		),
+	}
 }
 
 func findDomain(ctx context.Context, conn *datazone.Client, filter tfslices.Predicate[*awstypes.DomainSummary]) (*awstypes.DomainSummary, error) {
@@ -104,7 +154,14 @@ func findDomains(ctx context.Context, conn *datazone.Client, filter tfslices.Pre
 }
 
 type dataSourceDomainModel struct {
-	ARN  types.String `tfsdk:"arn"`
-	ID   types.String `tfsdk:"id"`
-	Name types.String `tfsdk:"name"`
+	ARN              types.String      `tfsdk:"arn"`
+	CreatedAt        timetypes.RFC3339 `tfsdk:"created_at"`
+	Description      types.String      `tfsdk:"description"`
+	DomainVersion    types.String      `tfsdk:"domain_version"`
+	ID               types.String      `tfsdk:"id"`
+	LastUpdatedAt    timetypes.RFC3339 `tfsdk:"last_updated_at"`
+	ManagedAccountID types.String      `tfsdk:"managed_account_id"`
+	Name             types.String      `tfsdk:"name"`
+	PortalURL        types.String      `tfsdk:"portal_url"`
+	Status           types.String      `tfsdk:"status"`
 }
--- internal/service/datazone/domain_data_source_test.go
+++ internal/service/datazone/domain_data_source_test.go
@@ -43,10 +43,51 @@ func TestAccDataZoneDomainDataSource_basic(t *testing.T) {
 	})
 }
 
+func TestAccDataZoneDomainDataSource_name(t *testing.T) {
+	ctx := acctest.Context(t)
+	if testing.Short() {
+		t.Skip("skipping long-running test in short mode")
+	}
+
+	dataSourceName := "data.aws_datazone_domain.test"
+	resourceName := "aws_datazone_domain.test"
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
+		ErrorCheck:               acctest.ErrorCheck(t, names.AttrDomainName),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDomainDataSourceConfig_name(rName),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrPair(dataSourceName, names.AttrARN, resourceName, names.AttrARN),
+					resource.TestCheckResourceAttrPair(dataSourceName, names.AttrID, resourceName, names.AttrID),
+					resource.TestCheckResourceAttrPair(dataSourceName, names.AttrName, resourceName, names.AttrName),
+				),
+			},
+			{
+				Config: testAccDomainDataSourceConfig_basic(rName),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrPair(dataSourceName, names.AttrID, resourceName, names.AttrID),
+					resource.TestCheckResourceAttrPair(dataSourceName, names.AttrID, resourceName, names.AttrID),
+					resource.TestCheckResourceAttrPair(dataSourceName, names.AttrName, resourceName, names.AttrName),
+				),
+			},
+		},
+	})
+}
+
 func testAccDomainDataSourceConfig_basic(rName string) string {
 	return acctest.ConfigCompose(testAccDomainConfig_basic(rName), `
 data "aws_datazone_domain" "test" {
-  name = aws_datazone_domain.test.name
+  id = aws_datazone_domain.test.id
+}`)
 }
-`)
+
+func testAccDomainDataSourceConfig_name(rName string) string {
+	return acctest.ConfigCompose(testAccDomainConfig_basic(rName), `
+data "aws_datazone_domain" "test" {
+  name = aws_datazone_domain.test.name
+}`)
 }
