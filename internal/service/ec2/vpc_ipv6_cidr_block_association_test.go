4b765b1ab053821281845b74e4410253be30dfea
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-03-12 12:24:24 -0400

    r/aws_vpc_ipv6_cidr_block_association: Support optional import of the `ipv6_ipam_pool_id` and `ipv6_netmask_length` attributes.

--- internal/service/ec2/vpc_ipv6_cidr_block_association_test.go
+++ internal/service/ec2/vpc_ipv6_cidr_block_association_test.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/YakDriver/regexache"
 	awstypes "github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -97,6 +98,97 @@ func TestAccVPCIPv6CIDRBlockAssociation_disappears(t *testing.T) {
 	})
 }
 
+func TestAccVPCIPv6CIDRBlockAssociation_ipamBasic(t *testing.T) {
+	ctx := acctest.Context(t)
+	var associationSecondary awstypes.VpcIpv6CidrBlockAssociation
+	resourceName := "aws_vpc_ipv6_cidr_block_association.secondary_cidr"
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
+		ErrorCheck:               acctest.ErrorCheck(t, names.EC2ServiceID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckVPCIPv6CIDRBlockAssociationDestroy(ctx),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccVPCIPv6CIDRBlockAssociationConfig_ipam(rName, 56),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckVPCIPv6CIDRBlockAssociationExists(ctx, resourceName, &associationSecondary),
+				),
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceName, plancheck.ResourceActionCreate),
+					},
+				},
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(resourceName, tfjsonpath.New("ip_source"), knownvalue.StringExact("amazon")),
+					statecheck.ExpectKnownValue(resourceName, tfjsonpath.New("ipv6_address_attribute"), knownvalue.StringExact("public")),
+					statecheck.ExpectKnownValue(resourceName, tfjsonpath.New("ipv6_pool"), knownvalue.StringExact("IPAM Managed")),
+					statecheck.ExpectKnownValue(resourceName, tfjsonpath.New("ipv6_cidr_block"), knownvalue.StringRegexp(regexache.MustCompile(`/56$`))),
+				},
+			},
+			{
+				ResourceName: resourceName,
+				ImportState:  true,
+				ImportStateIdFunc: func(s *terraform.State) (string, error) {
+					rs, ok := s.RootModule().Resources[resourceName]
+					if !ok {
+						return "", fmt.Errorf("Not Found: %s", resourceName)
+					}
+
+					return fmt.Sprintf("%s,%s,%s", rs.Primary.ID, rs.Primary.Attributes["ipv6_ipam_pool_id"], rs.Primary.Attributes["ipv6_netmask_length"]), nil
+				},
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccVPCIPv6CIDRBlockAssociation_ipamBasicExplicitCIDR(t *testing.T) {
+	ctx := acctest.Context(t)
+	var associationSecondary awstypes.VpcIpv6CidrBlockAssociation
+	resourceName := "aws_vpc_ipv6_cidr_block_association.secondary_cidr"
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
+		ErrorCheck:               acctest.ErrorCheck(t, names.EC2ServiceID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckVPCIPv6CIDRBlockAssociationDestroy(ctx),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccVPCIPv6CIDRBlockAssociationConfig_ipamExplicit(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckVPCIPv6CIDRBlockAssociationExists(ctx, resourceName, &associationSecondary),
+				),
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceName, plancheck.ResourceActionCreate),
+					},
+				},
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(resourceName, tfjsonpath.New("ip_source"), knownvalue.StringExact("amazon")),
+					statecheck.ExpectKnownValue(resourceName, tfjsonpath.New("ipv6_address_attribute"), knownvalue.StringExact("public")),
+					statecheck.ExpectKnownValue(resourceName, tfjsonpath.New("ipv6_pool"), knownvalue.StringExact("IPAM Managed")),
+				},
+			},
+			{
+				ResourceName: resourceName,
+				ImportState:  true,
+				ImportStateIdFunc: func(s *terraform.State) (string, error) {
+					rs, ok := s.RootModule().Resources[resourceName]
+					if !ok {
+						return "", fmt.Errorf("Not Found: %s", resourceName)
+					}
+
+					return fmt.Sprintf("%s,%s", rs.Primary.ID, rs.Primary.Attributes["ipv6_ipam_pool_id"]), nil
+				},
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
 func testAccCheckVPCIPv6CIDRBlockAssociationDestroy(ctx context.Context) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		conn := acctest.Provider.Meta().(*conns.AWSClient).EC2Client(ctx)
@@ -165,4 +257,48 @@ resource "aws_vpc_ipv6_cidr_block_association" "tertiary_cidr" {
   assign_generated_ipv6_cidr_block = true
 }
 `, rName)
+}
+
+func testAccVPCIPv6CIDRBlockAssociationConfig_ipam(rName string, netmaskLength int) string {
+	return acctest.ConfigCompose(testAccVPCConfig_baseIPAMIPv6(rName), fmt.Sprintf(`
+resource "aws_vpc" "test" {
+  cidr_block                       = "10.1.0.0/16"
+  ipv6_ipam_pool_id                = aws_vpc_ipam_pool.test.id
+  ipv6_netmask_length              = 56
+
+  tags = {
+    Name = %[1]q
+  }
+
+  depends_on = [aws_vpc_ipam_pool_cidr.test]
+}
+
+resource "aws_vpc_ipv6_cidr_block_association" "secondary_cidr" {
+  ipv6_ipam_pool_id   = aws_vpc_ipam_pool.test.id
+  ipv6_netmask_length = %[2]d
+  vpc_id              = aws_vpc.test.id
+}
+`, rName, netmaskLength))
+}
+
+func testAccVPCIPv6CIDRBlockAssociationConfig_ipamExplicit(rName string) string {
+	return acctest.ConfigCompose(testAccVPCConfig_baseIPAMIPv6(rName), fmt.Sprintf(`
+resource "aws_vpc" "test" {
+  cidr_block                       = "10.1.0.0/16"
+  ipv6_ipam_pool_id                = aws_vpc_ipam_pool.test.id
+  ipv6_netmask_length              = 56
+
+  tags = {
+    Name = %[1]q
+  }
+
+  depends_on = [aws_vpc_ipam_pool_cidr.test]
+}
+
+resource "aws_vpc_ipv6_cidr_block_association" "secondary_cidr" {
+  ipv6_ipam_pool_id = aws_vpc_ipam_pool.test.id
+  ipv6_cidr_block   = cidrsubnets(aws_vpc_ipam_pool_cidr.test.cidr, 4, 4)[1]
+  vpc_id            = aws_vpc.test.id
+}
+`, rName))
 }
