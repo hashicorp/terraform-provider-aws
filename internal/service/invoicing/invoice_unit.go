c12efdd7145ba3538316cfa394c015001e587fe5
Ali Allomani <17993914+AliAllomani@users.noreply.github.com>
2025-03-13 17:04:03 +0100

    updates

--- internal/service/invoicing/invoice_unit.go
+++ internal/service/invoicing/invoice_unit.go
@@ -10,14 +10,17 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/invoicing"
+	"github.com/aws/aws-sdk-go-v2/service/invoicing/types"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
+	"github.com/hashicorp/terraform-provider-aws/internal/flex"
 	"github.com/hashicorp/terraform-provider-aws/internal/logging"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
@@ -45,10 +48,13 @@ func resourceInvoiceUnit() *schema.Resource {
 			"name": {
 				Type:     schema.TypeString,
 				Required: true,
+				ForceNew: true,
 			},
 			"invoice_receiver": {
-				Type:     schema.TypeString,
-				Required: true,
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: verify.ValidAccountID,
+				ForceNew:     true,
 			},
 			names.AttrARN: {
 				Type:     schema.TypeString,
@@ -56,11 +62,21 @@ func resourceInvoiceUnit() *schema.Resource {
 			},
 			names.AttrDescription: {
 				Type:     schema.TypeString,
-				Computed: true,
+				Optional: true,
 			},
 			"tax_inheritance_disabled": {
 				Type:     schema.TypeBool,
+				Optional: true,
 				Computed: true,
+			},
+			"linked_accounts": {
+				Type:     schema.TypeSet,
+				Required: true,
+				MinItems: 1,
+				Elem: &schema.Schema{
+					Type:         schema.TypeString,
+					ValidateFunc: verify.ValidAccountID,
+				},
 			},
 			names.AttrTags:    tftags.TagsSchema(),
 			names.AttrTagsAll: tftags.TagsSchemaComputed(),
@@ -74,6 +90,10 @@ func resourceInvoiceUnitCreate(ctx context.Context, d *schema.ResourceData, meta
 
 	input := invoicing.CreateInvoiceUnitInput{
 		InvoiceReceiver: aws.String(d.Get("invoice_receiver").(string)),
+		Name:            aws.String(d.Get("name").(string)),
+		Rule: &types.InvoiceUnitRule{
+			LinkedAccounts: flex.ExpandStringValueSet(d.Get("linked_accounts").(*schema.Set)),
+		},
 	}
 
 	res, err := conn.CreateInvoiceUnit(ctx, &input)
@@ -89,9 +109,27 @@ func resourceInvoiceUnitCreate(ctx context.Context, d *schema.ResourceData, meta
 
 func resourceInvoiceUnitUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
-	// conn := meta.(*conns.AWSClient).InvoicingClient(ctx)
+	conn := meta.(*conns.AWSClient).InvoicingClient(ctx)
 
-	log.Printf("[WARN] TODO", d.Id())
+	input := invoicing.UpdateInvoiceUnitInput{
+		Rule: &types.InvoiceUnitRule{
+			LinkedAccounts: flex.ExpandStringValueSet(d.Get("linked_accounts").(*schema.Set)),
+		},
+	}
+
+	input.InvoiceUnitArn = aws.String(d.Id())
+
+	if v, ok := d.GetOk(names.AttrDescription); ok {
+		input.Description = aws.String(v.(string))
+	}
+
+	if v, ok := d.GetOk("tax_inheritance_disabled"); ok {
+		input.TaxInheritanceDisabled = aws.Bool(v.(bool))
+	}
+
+	if _, err := conn.UpdateInvoiceUnit(ctx, &input); err != nil {
+		return sdkdiag.AppendErrorf(diags, "Error updating Invoice Unit (%s): %s", d.Id(), err)
+	}
 
 	return append(diags, resourceInvoiceUnitRead(ctx, d, meta)...)
 }
@@ -119,6 +157,18 @@ func resourceInvoiceUnitRead(ctx context.Context, d *schema.ResourceData, meta i
 	d.Set(names.AttrARN, res.InvoiceUnitArn)
 	d.Set(names.AttrDescription, res.Description)
 	d.Set("name", res.Name)
+	d.Set("linked_accounts", res.Rule.LinkedAccounts)
+	d.Set("tax_inheritance_disabled", res.TaxInheritanceDisabled)
+
+	rtags, err := conn.ListTagsForResource(ctx, &invoicing.ListTagsForResourceInput{
+		ResourceArn: aws.String(d.Id()),
+	})
+
+	if err != nil {
+		return sdkdiag.AppendErrorf(diags, "reading Invoice Unit tags(%s): %s", d.Id(), err)
+	}
+
+	d.Set("tags", rtags.ResourceTags)
 
 	return diags
 }
@@ -126,7 +176,14 @@ func resourceInvoiceUnitRead(ctx context.Context, d *schema.ResourceData, meta i
 func resourceInvoiceUnitDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
 
-	log.Printf("[WARN] TODO", d.Id())
+	conn := meta.(*conns.AWSClient).InvoicingClient(ctx)
+
+	if _, err := conn.DeleteInvoiceUnit(ctx, &invoicing.DeleteInvoiceUnitInput{
+		InvoiceUnitArn: aws.String(d.Id()),
+	}); err != nil {
+		return sdkdiag.AppendErrorf(diags, "Error deleting Invoice Unit (%s): %s", d.Id(), err)
+	}
+
 	d.SetId("")
 
 	return diags
