ade2427dde103ce8551e061f4078f9a3f1a34906
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-03-14 14:07:24 -0400

    r/aws_rds_cluster: Fix `InvalidParameterCombination: To enable the Advanced mode of Database Insights, modify your cluster to enable Performance Insights and set the retention period for Performance Insights to at least 465 days` errors when enabling `database_insights_mode` on existing clusters.

--- internal/service/rds/cluster.go
+++ internal/service/rds/cluster.go
@@ -1614,6 +1614,7 @@ func resourceClusterUpdate(ctx context.Context, d *schema.ResourceData, meta any
 
 		if d.HasChange("database_insights_mode") {
 			input.DatabaseInsightsMode = types.DatabaseInsightsMode(d.Get("database_insights_mode").(string))
+			input.EnablePerformanceInsights = aws.Bool(d.Get("performance_insights_enabled").(bool))
 		}
 
 		if d.HasChange("db_cluster_instance_class") {
