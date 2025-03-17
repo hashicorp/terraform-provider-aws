f06758ebf9257df3b17af80a4ebf6b3725797392
Adam Sherwood <theherk@gmail.com>
2025-03-17 12:13:33 +0100

    Use any in lieu of interface{}.

--- internal/service/rds/global_cluster_data_source.go
+++ internal/service/rds/global_cluster_data_source.go
@@ -87,7 +87,7 @@ func DataSourceGlobalCluster() *schema.Resource {
 	}
 }
 
-func dataSourceGlobalClusterRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+func dataSourceGlobalClusterRead(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
 	var diags diag.Diagnostics
 	conn := meta.(*conns.AWSClient).RDSClient(ctx)
 
