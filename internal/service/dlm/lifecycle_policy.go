srik207/https-github.com-hashicorp-terraform-provider-aws

feat: dlm: Add support for target_region parameter in cross_region_copy_rule
`target_region` should only be used for dlm policies of
`policy_type=IMAGE_MANAGEMENT`. Creating `IMAGE_MANAGEMENT` policies
using `target` raises a validation error from the AWS API.

Previously the argument was marked as deprecated from the AWS docs, but
after contacting the AWS support, the documentation was updated to
specify that `TargetRegion` should be used only for `IMAGE_MANAGEMENT`
policies, while `Target` only for `EBS_SNAPSHOT_MANAGEMENT` policies.

See the relevant docs: https://docs.aws.amazon.com/dlm/latest/APIReference/API_CrossRegionCopyRule.html

Relevant issues:

- https://discuss.hashicorp.com/t/fail-to-create-a-cross-region-copy-rule-for-dlp/46076
- https://github.com/hashicorp/terraform-provider-aws/issues/24226

This fixes #24226

internal/service/dlm/lifecycle_policy.go
@@ -335,7 +335,12 @@ func ResourceLifecyclePolicy() *schema.Resource {
 												},
 												"target": {
 													Type:         schema.TypeString,
-													Required:     true,
+													Optional:     true,
+													ValidateFunc: validation.StringMatch(regexache.MustCompile(`^[\w:\-\/\*]+$`), ""),
+												},
+												"target_region": {
+													Type:         schema.TypeString,
+													Optional:     true,
 													ValidateFunc: validation.StringMatch(regexache.MustCompile(`^[\w:\-\/\*]+$`), ""),
 												},
 											},
@@ -944,6 +949,9 @@ func expandCrossRegionCopyRules(l []interface{}) []*dlm.CrossRegionCopyRule {
 		if v, ok := m["target"].(string); ok && v != "" {
 			rule.Target = aws.String(v)
 		}
+		if v, ok := m["target_region"].(string); ok && v != "" {
+			rule.TargetRegion = aws.String(v)
+		}
 
 		rules = append(rules, rule)
 	}
@@ -970,6 +978,7 @@ func flattenCrossRegionCopyRules(rules []*dlm.CrossRegionCopyRule) []interface{}
 			"encrypted":      aws.BoolValue(rule.Encrypted),
 			"retain_rule":    flattenCrossRegionCopyRuleRetainRule(rule.RetainRule),
 			"target":         aws.StringValue(rule.Target),
+			"target_region":  aws.StringValue(rule.TargetRegion),
 		}
 
 		result = append(result, m)

internal/service/dlm/lifecycle_policy_test.go
@@ -454,6 +454,42 @@ func TestAccDLMLifecyclePolicy_crossRegionCopyRule(t *testing.T) {
 	})
 }
 
+func TestAccDLMLifecyclePolicy_crossRegionCopyRuleImageManagement(t *testing.T) {
+	ctx := acctest.Context(t)
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	resourceName := "aws_dlm_lifecycle_policy.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+			acctest.PreCheckMultipleRegion(t, 2)
+			testAccPreCheck(ctx, t)
+		},
+		ErrorCheck:               acctest.ErrorCheck(t, dlm.EndpointsID),
+		ProtoV5ProviderFactories: acctest.ProtoV5FactoriesAlternate(ctx, t),
+		CheckDestroy:             testAccCheckLifecyclePolicyDestroy(ctx),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccLifecyclePolicyConfig_crossRegionCopyRuleImageManagement(rName),
+				Check: resource.ComposeTestCheckFunc(
+					checkLifecyclePolicyExists(ctx, resourceName),
+					resource.TestCheckResourceAttr(resourceName, "policy_details.0.policy_type", "IMAGE_MANAGEMENT"),
+					resource.TestCheckResourceAttr(resourceName, "policy_details.0.schedule.0.cross_region_copy_rule.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "policy_details.0.schedule.0.cross_region_copy_rule.0.encrypted", "false"),
+					resource.TestCheckResourceAttr(resourceName, "policy_details.0.schedule.0.cross_region_copy_rule.0.retain_rule.0.interval", "15"),
+					resource.TestCheckResourceAttr(resourceName, "policy_details.0.schedule.0.cross_region_copy_rule.0.retain_rule.0.interval_unit", "DAYS"),
+					resource.TestCheckResourceAttr(resourceName, "policy_details.0.schedule.0.cross_region_copy_rule.0.target_region", acctest.AlternateRegion()),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
 func TestAccDLMLifecyclePolicy_tags(t *testing.T) {
 	ctx := acctest.Context(t)
 	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
@@ -1065,6 +1101,47 @@ resource "aws_dlm_lifecycle_policy" "test" {
         retain_rule {
           interval      = 15
           interval_unit = "MONTHS"
+        }
+      }
+    }
+
+    target_tags = {
+      Name = %[1]q
+    }
+  }
+}
+`, rName, acctest.AlternateRegion()))
+}
+
+func testAccLifecyclePolicyConfig_crossRegionCopyRuleImageManagement(rName string) string {
+	return acctest.ConfigCompose(
+		lifecyclePolicyBaseConfig(rName),
+		fmt.Sprintf(`
+resource "aws_dlm_lifecycle_policy" "test" {
+  description        = %[1]q
+  execution_role_arn = aws_iam_role.test.arn
+
+  policy_details {
+	policy_type = "IMAGE_MANAGEMENT"
+    resource_types = ["INSTANCE"]
+
+    schedule {
+      name = %[1]q
+
+      create_rule {
+        interval = 12
+      }
+
+      retain_rule {
+        count = 10
+      }
+
+      cross_region_copy_rule {
+        target_region    = %[2]q
+        encrypted 		 = false
+        retain_rule {
+          interval      = 15
+          interval_unit = "DAYS"
         }
       }
     }

hashicorp/terraform-provider-aws

internal/service/dlm/lifecycle_policy.go
@@ -330,7 +330,12 @@ func resourceLifecyclePolicy() *schema.Resource {
 												},
 												names.AttrTarget: {
 													Type:         schema.TypeString,
-													Required:     true,
+													Optional:     true,
+													ValidateFunc: validation.StringMatch(regexache.MustCompile(`^[\w:\-\/\*]+$`), ""),
+												},
+												"target_region": {
+													Type:         schema.TypeString,
+													Optional:     true,
 													ValidateFunc: validation.StringMatch(regexache.MustCompile(`^[\w:\-\/\*]+$`), ""),
 												},
 											},
@@ -951,6 +956,9 @@ func expandCrossRegionCopyRules(l []interface{}) []awstypes.CrossRegionCopyRule 
 		if v, ok := m[names.AttrTarget].(string); ok && v != "" {
 			rule.Target = aws.String(v)
 		}
+		if v, ok := m["target_region"].(string); ok && v != "" {
+			rule.TargetRegion = aws.String(v)
+		}
 
 		rules = append(rules, rule)
 	}
@@ -973,6 +981,7 @@ func flattenCrossRegionCopyRules(rules []awstypes.CrossRegionCopyRule) []interfa
 			names.AttrEncrypted: aws.ToBool(rule.Encrypted),
 			"retain_rule":       flattenCrossRegionCopyRuleRetainRule(rule.RetainRule),
 			names.AttrTarget:    aws.ToString(rule.Target),
+			"target_region":     aws.ToString(rule.TargetRegion),
 		}
 
 		result = append(result, m)

internal/service/dlm/lifecycle_policy_test.go
@@ -456,6 +456,42 @@ func TestAccDLMLifecyclePolicy_crossRegionCopyRule(t *testing.T) {
 	})
 }
 
+func TestAccDLMLifecyclePolicy_crossRegionCopyRuleImageManagement(t *testing.T) {
+	ctx := acctest.Context(t)
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	resourceName := "aws_dlm_lifecycle_policy.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+			acctest.PreCheckMultipleRegion(t, 2)
+			testAccPreCheck(ctx, t)
+		},
+		ErrorCheck:               acctest.ErrorCheck(t, names.DLMServiceID),
+		ProtoV5ProviderFactories: acctest.ProtoV5FactoriesAlternate(ctx, t),
+		CheckDestroy:             testAccCheckLifecyclePolicyDestroy(ctx),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccLifecyclePolicyConfig_crossRegionCopyRuleImageManagement(rName),
+				Check: resource.ComposeTestCheckFunc(
+					checkLifecyclePolicyExists(ctx, resourceName),
+					resource.TestCheckResourceAttr(resourceName, "policy_details.0.policy_type", "IMAGE_MANAGEMENT"),
+					resource.TestCheckResourceAttr(resourceName, "policy_details.0.schedule.0.cross_region_copy_rule.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "policy_details.0.schedule.0.cross_region_copy_rule.0.encrypted", "false"),
+					resource.TestCheckResourceAttr(resourceName, "policy_details.0.schedule.0.cross_region_copy_rule.0.retain_rule.0.interval", "15"),
+					resource.TestCheckResourceAttr(resourceName, "policy_details.0.schedule.0.cross_region_copy_rule.0.retain_rule.0.interval_unit", "DAYS"),
+					resource.TestCheckResourceAttr(resourceName, "policy_details.0.schedule.0.cross_region_copy_rule.0.target_region", acctest.AlternateRegion()),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
 func TestAccDLMLifecyclePolicy_tags(t *testing.T) {
 	ctx := acctest.Context(t)
 	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
@@ -1057,6 +1093,47 @@ resource "aws_dlm_lifecycle_policy" "test" {
         retain_rule {
           interval      = 15
           interval_unit = "MONTHS"
+        }
+      }
+    }
+
+    target_tags = {
+      Name = %[1]q
+    }
+  }
+}
+`, rName, acctest.AlternateRegion()))
+}
+
+func testAccLifecyclePolicyConfig_crossRegionCopyRuleImageManagement(rName string) string {
+	return acctest.ConfigCompose(
+		lifecyclePolicyBaseConfig(rName),
+		fmt.Sprintf(`
+resource "aws_dlm_lifecycle_policy" "test" {
+  description        = %[1]q
+  execution_role_arn = aws_iam_role.test.arn
+
+  policy_details {
+	policy_type = "IMAGE_MANAGEMENT"
+    resource_types = ["INSTANCE"]
+
+    schedule {
+      name = %[1]q
+
+      create_rule {
+        interval = 12
+      }
+
+      retain_rule {
+        count = 10
+      }
+
+      cross_region_copy_rule {
+        target_region    = %[2]q
+        encrypted 		 = false
+        retain_rule {
+          interval      = 15
+          interval_unit = "DAYS"
         }
       }
     }