9ada70ffbc7d9fa856b398d5e6958770ac11ed65
Sharon Nam <sharon.nam@hashicorp.com>
2025-03-19 12:18:20 -0700

    Add check in case for secret regions/govcloud

--- internal/service/ec2/ec2_eip.go
+++ internal/service/ec2/ec2_eip.go
@@ -278,6 +278,8 @@ func resourceEIPRead(ctx context.Context, d *schema.ResourceData, meta interface
 		d.Set("ptr_record", strings.TrimSuffix(aws.ToString(addressAttr.PtrRecord), "."))
 	case tfresource.NotFound(err):
 		d.Set("ptr_record", nil)
+	case tfawserr.ErrMessageContains(err, "InvalidAction", "not valid for this web service"):
+		// Effectively ignoring the error as we handle errors from GovCloud/Secret Regions
 	default:
 		return sdkdiag.AppendErrorf(diags, "reading EC2 EIP (%s) domain name attribute: %s", d.Id(), err)
 	}
