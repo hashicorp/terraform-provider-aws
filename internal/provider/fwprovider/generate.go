82dd2731badc673cd7c791a91ade693b3a320daa
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-03-26 12:21:37 -0400

    Additional generation of service package list.

--- internal/provider/fwprovider/generate.go
+++ internal/provider/fwprovider/generate.go
@@ -1,7 +1,7 @@
 // Copyright (c) HashiCorp, Inc.
 // SPDX-License-Identifier: MPL-2.0
 
-//go:generate go run ../generate/servicepackages/main.go -ServicePackageRoot ../service
+//go:generate go run ../../generate/servicepackages/main.go -ServicePackageRoot ../../service
 // ONLY generate directives and package declaration! Do not add anything else to this file.
 
-package provider
+package fwprovider
