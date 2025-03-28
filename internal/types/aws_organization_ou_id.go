e3c1b401bcec1b2010093524a7df20e51ae63eec
Iain Steers <iainsteers@gmail.com>
2025-03-28 23:06:21 +0000

    Add copyright and fix linting

--- internal/types/aws_organization_ou_id.go
+++ internal/types/aws_organization_ou_id.go
@@ -1,8 +1,11 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
 package types
 
 import "github.com/YakDriver/regexache"
 
 // IsAWSOrganizationOUID returns whether or not the specified string is a valid AWS Organizational Unit ID.
-func IsAWSOrganizationOUID(s string) bool {
+func IsAWSOrganizationOUID(s string) bool { // nosemgrep:ci.aws-in-func-name
 	return regexache.MustCompile(`^ou-[0-9a-z]{4,32}-[a-z0-9]{8,32}$`).MatchString(s)
 }
