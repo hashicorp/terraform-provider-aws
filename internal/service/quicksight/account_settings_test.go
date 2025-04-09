3300e31078d4b51de8bf3ef2cce2a4bb23652dd2
Travis Rutledge <trutled3@gmail.com>
2025-04-09 20:29:37 +0000

    Updated tests and schema due to conflicts with notification_email with subscription

--- internal/service/quicksight/account_settings_test.go
+++ internal/service/quicksight/account_settings_test.go
@@ -2,225 +2,88 @@
 // SPDX-License-Identifier: MPL-2.0
 
 package quicksight_test
-// **PLEASE DELETE THIS AND ALL TIP COMMENTS BEFORE SUBMITTING A PR FOR REVIEW!**
-//
-// TIP: ==== INTRODUCTION ====
-// Thank you for trying the skaff tool!
-//
-// You have opted to include these helpful comments. They all include "TIP:"
-// to help you find and remove them when you're done with them.
-//
-// While some aspects of this file are customized to your input, the
-// scaffold tool does *not* look at the AWS API and ensure it has correct
-// function, structure, and variable names. It makes guesses based on
-// commonalities. You will need to make significant adjustments.
-//
-// In other words, as generated, this is a rough outline of the work you will
-// need to do. If something doesn't make sense for your situation, get rid of
-// it.
 
 import (
-	// TIP: ==== IMPORTS ====
-	// This is a common set of imports but not customized to your code since
-	// your code hasn't been written yet. Make sure you, your IDE, or
-	// goimports -w <file> fixes these imports.
-	//
-	// The provider linter wants your imports to be in two groups: first,
-	// standard library (i.e., "fmt" or "strings"), second, everything else.
-	//
-	// Also, AWS Go SDK v2 may handle nested structures differently than v1,
-	// using the services/quicksight/types package. If so, you'll
-	// need to import types and reference the nested types, e.g., as
-	// types.<Type Name>.
 	"context"
 	"errors"
 	"fmt"
 	"testing"
 
-	"github.com/YakDriver/regexache"
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/service/quicksight"
 	"github.com/aws/aws-sdk-go-v2/service/quicksight/types"
+	awstypes "github.com/aws/aws-sdk-go-v2/service/quicksight/types"
 	sdkacctest "github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/create"
-	"github.com/hashicorp/terraform-provider-aws/internal/errs"
+	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/names"
 
-	// TIP: You will often need to import the package that this test file lives
-	// in. Since it is in the "test" context, it must import the package to use
-	// any normal context constants, variables, or functions.
 	tfquicksight "github.com/hashicorp/terraform-provider-aws/internal/service/quicksight"
 )
 
-// TIP: File Structure. The basic outline for all test files should be as
-// follows. Improve this resource's maintainability by following this
-// outline.
-//
-// 1. Package declaration (add "_test" since this is a test file)
-// 2. Imports
-// 3. Unit tests
-// 4. Basic test
-// 5. Disappears test
-// 6. All the other tests
-// 7. Helper functions (exists, destroy, check, etc.)
-// 8. Functions that return Terraform configurations
-
-// TIP: ==== UNIT TESTS ====
-// This is an example of a unit test. Its name is not prefixed with
-// "TestAcc" like an acceptance test.
-//
-// Unlike acceptance tests, unit tests do not access AWS and are focused on a
-// function (or method). Because of this, they are quick and cheap to run.
-//
-// In designing a resource's implementation, isolate complex bits from AWS bits
-// so that they can be tested through a unit test. We encourage more unit tests
-// in the provider.
-//
-// Cut and dry functions using well-used patterns, like typical flatteners and
-// expanders, don't need unit testing. However, if they are complex or
-// intricate, they should be unit tested.
-func TestAccountSettingsExampleUnitTest(t *testing.T) {
-	t.Parallel()
-
-	testCases := []struct {
-		TestName string
-		Input    string
-		Expected string
-		Error    bool
-	}{
-		{
-			TestName: "empty",
-			Input:    "",
-			Expected: "",
-			Error:    true,
-		},
-		{
-			TestName: "descriptive name",
-			Input:    "some input",
-			Expected: "some output",
-			Error:    false,
-		},
-		{
-			TestName: "another descriptive name",
-			Input:    "more input",
-			Expected: "more output",
-			Error:    false,
-		},
-	}
-
-	for _, testCase := range testCases {
-		t.Run(testCase.TestName, func(t *testing.T) {
-			t.Parallel()
-			got, err := tfquicksight.FunctionFromResource(testCase.Input)
-
-			if err != nil && !testCase.Error {
-				t.Errorf("got error (%s), expected no error", err)
-			}
-
-			if err == nil && testCase.Error {
-				t.Errorf("got (%s) and no error, expected error", got)
-			}
-
-			if got != testCase.Expected {
-				t.Errorf("got %s, expected %s", got, testCase.Expected)
-			}
-		})
-	}
-}
-
-// TIP: ==== ACCEPTANCE TESTS ====
-// This is an example of a basic acceptance test. This should test as much of
-// standard functionality of the resource as possible, and test importing, if
-// applicable. We prefix its name with "TestAcc", the service, and the
-// resource name.
-//
-// Acceptance test access AWS and cost money to run.
 func TestAccQuickSightAccountSettings_basic(t *testing.T) {
 	ctx := acctest.Context(t)
-	// TIP: This is a long-running test guard for tests that run longer than
-	// 300s (5 min) generally.
-	if testing.Short() {
-		t.Skip("skipping long-running test in short mode")
-	}
-
-	var accountsettings quicksight.DescribeAccountSettingsResponse
+	var accountsettings awstypes.AccountSettings
 	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
 	resourceName := "aws_quicksight_account_settings.test"
 
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck: func() {
-			acctest.PreCheck(ctx, t)
-			acctest.PreCheckPartitionHasService(t, names.QuickSightEndpointID)
-			testAccPreCheck(ctx, t)
-		},
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
 		ErrorCheck:               acctest.ErrorCheck(t, names.QuickSightServiceID),
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
 		CheckDestroy:             testAccCheckAccountSettingsDestroy(ctx),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccAccountSettingsConfig_basic(rName),
-				Check: resource.ComposeAggregateTestCheckFunc(
+				Config: testAccAccountSettings_basic(rName),
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectNonEmptyPlan(),
+					},
+				},
+				Check: resource.ComposeTestCheckFunc(
 					testAccCheckAccountSettingsExists(ctx, resourceName, &accountsettings),
-					resource.TestCheckResourceAttr(resourceName, "auto_minor_version_upgrade", "false"),
-					resource.TestCheckResourceAttrSet(resourceName, "maintenance_window_start_time.0.day_of_week"),
-					resource.TestCheckTypeSetElemNestedAttrs(resourceName, "user.*", map[string]string{
-						"console_access": "false",
-						"groups.#":       "0",
-						"username":       "Test",
-						"password":       "TestTest1234",
-					}),
-					// TIP: If the ARN can be partially or completely determined by the parameters passed, e.g. it contains the
-					// value of `rName`, either include the values in the regex or check for an exact match using `acctest.CheckResourceAttrRegionalARN`
-					acctest.MatchResourceAttrRegionalARN(ctx, resourceName, names.AttrARN, "quicksight", regexache.MustCompile(`accountsettings:.+$`)),
 				),
 			},
 			{
-				ResourceName:            resourceName,
-				ImportState:             true,
-				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"apply_immediately", "user"},
+				ResourceName: resourceName,
+				ImportState:  false,
+				RefreshState: true,
 			},
 		},
 	})
 }
 
-func TestAccQuickSightAccountSettings_disappears(t *testing.T) {
+func TestAccQuickSightAccountSettings_resetOnDelete_false(t *testing.T) {
 	ctx := acctest.Context(t)
-	if testing.Short() {
-		t.Skip("skipping long-running test in short mode")
-	}
-
-	var accountsettings quicksight.DescribeAccountSettingsResponse
 	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
 	resourceName := "aws_quicksight_account_settings.test"
 
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck: func() {
-			acctest.PreCheck(ctx, t)
-			acctest.PreCheckPartitionHasService(t, names.QuickSightEndpointID)
-			testAccPreCheck(ctx, t)
-		},
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
 		ErrorCheck:               acctest.ErrorCheck(t, names.QuickSightServiceID),
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
 		CheckDestroy:             testAccCheckAccountSettingsDestroy(ctx),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccAccountSettingsConfig_basic(rName, testAccAccountSettingsVersionNewer),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					testAccCheckAccountSettingsExists(ctx, resourceName, &accountsettings),
-					// TIP: The Plugin-Framework disappears helper is similar to the Plugin-SDK version,
-					// but expects a new resource factory function as the third argument. To expose this
-					// private function to the testing package, you may need to add a line like the following
-					// to exports_test.go:
-					//
-					//   var ResourceAccountSettings = newResourceAccountSettings
-					acctest.CheckFrameworkResourceDisappears(ctx, acctest.Provider, tfquicksight.ResourceAccountSettings, resourceName),
-				),
-				ExpectNonEmptyPlan: true,
+				Config: testAccAccountConfig_resetOnDelete(rName, false),
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(resourceName, tfjsonpath.New("termination_protection_enabled"), knownvalue.Bool(false)),
+					statecheck.ExpectKnownValue(resourceName, tfjsonpath.New("reset_on_delete"), knownvalue.Bool(false)),
+				},
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+				ImportStateVerifyIgnore: []string{
+					"reset_on_delete",
+				},
 			},
 		},
 	})
@@ -235,34 +98,39 @@ func testAccCheckAccountSettingsDestroy(ctx context.Context) resource.TestCheckF
 				continue
 			}
 
-
-			// TIP: ==== FINDERS ====
-			// The find function should be exported. Since it won't be used outside of the package, it can be exported
-			// in the `exports_test.go` file.
-			_, err := tfquicksight.FindAccountSettingsByID(ctx, conn, rs.Primary.ID)
+			settings, err := tfquicksight.FindAccountSettingsByID(ctx, conn, rs.Primary.ID)
 			if tfresource.NotFound(err) {
 				return nil
 			}
-			if err != nil {
-			        return create.Error(names.QuickSight, create.ErrActionCheckingDestroyed, tfquicksight.ResNameAccountSettings, rs.Primary.ID, err)
+
+			fmt.Println("testAccCheckAccountSettingsReset: ")
+			fmt.Println(settings)
+
+			if settings.AccountName == nil {
+				// Settings have not been reset
+				return nil
 			}
 
-			return create.Error(names.QuickSight, create.ErrActionCheckingDestroyed, tfquicksight.ResNameAccountSettings, rs.Primary.ID, errors.New("not destroyed"))
+			if err != nil {
+				return create.Error(names.QuickSight, create.ErrActionCheckingDestroyed, tfquicksight.ResNameAccountSettings, rs.Primary.ID, err)
+			}
+
+			return create.Error(names.QuickSight, create.ErrActionCheckingDestroyed, tfquicksight.ResNameAccountSettings, rs.Primary.ID, errors.New("Quicksight account settings were reset"))
 		}
 
 		return nil
 	}
 }
 
-func testAccCheckAccountSettingsExists(ctx context.Context, name string, accountsettings *quicksight.DescribeAccountSettingsResponse) resource.TestCheckFunc {
+func testAccCheckAccountSettingsExists(ctx context.Context, n string, v *types.AccountSettings) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
-		rs, ok := s.RootModule().Resources[name]
+		rs, ok := s.RootModule().Resources[n]
 		if !ok {
-			return create.Error(names.QuickSight, create.ErrActionCheckingExistence, tfquicksight.ResNameAccountSettings, name, errors.New("not found"))
+			return create.Error(names.QuickSight, create.ErrActionCheckingExistence, tfquicksight.ResNameAccountSettings, n, errors.New("not found"))
 		}
 
 		if rs.Primary.ID == "" {
-			return create.Error(names.QuickSight, create.ErrActionCheckingExistence, tfquicksight.ResNameAccountSettings, name, errors.New("not set"))
+			return create.Error(names.QuickSight, create.ErrActionCheckingExistence, tfquicksight.ResNameAccountSettings, n, errors.New("not set"))
 		}
 
 		conn := acctest.Provider.Meta().(*conns.AWSClient).QuickSightClient(ctx)
@@ -272,57 +140,44 @@ func testAccCheckAccountSettingsExists(ctx context.Context, name string, account
 			return create.Error(names.QuickSight, create.ErrActionCheckingExistence, tfquicksight.ResNameAccountSettings, rs.Primary.ID, err)
 		}
 
-		*accountsettings = *resp
+		*v = *resp
 
 		return nil
 	}
 }
 
-func testAccPreCheck(ctx context.Context, t *testing.T) {
-	conn := acctest.Provider.Meta().(*conns.AWSClient).QuickSightClient(ctx)
-
-	input := &quicksight.ListAccountSettingssInput{}
-
-	_, err := conn.ListAccountSettingss(ctx, input)
-
-	if acctest.PreCheckSkipError(err) {
-		t.Skipf("skipping acceptance testing: %s", err)
-	}
-	if err != nil {
-		t.Fatalf("unexpected PreCheck error: %s", err)
-	}
-}
-
-func testAccCheckAccountSettingsNotRecreated(before, after *quicksight.DescribeAccountSettingsResponse) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		if before, after := aws.ToString(before.AccountSettingsId), aws.ToString(after.AccountSettingsId); before != after {
-			return create.Error(names.QuickSight, create.ErrActionCheckingNotRecreated, tfquicksight.ResNameAccountSettings, aws.ToString(before.AccountSettingsId), errors.New("recreated"))
-		}
-
-		return nil
-	}
-}
-
-func testAccAccountSettingsConfig_basic(rName, version string) string {
+func testAccAccountSettings_base(rName string) string {
 	return fmt.Sprintf(`
-resource "aws_security_group" "test" {
-  name = %[1]q
+resource "aws_quicksight_account_subscription" "test" {
+  account_name          = %[1]q
+  authentication_method = "IAM_AND_QUICKSIGHT"
+  edition               = "ENTERPRISE"
+  notification_email    = %[2]q
 }
+
+`, rName, acctest.DefaultEmailAddress)
+}
+
+func testAccAccountSettings_basic(rName string) string {
+	return acctest.ConfigCompose(
+		testAccAccountSettings_base(rName), `
 resource "aws_quicksight_account_settings" "test" {
-  account_settings_name             = %[1]q
-  engine_type             = "ActiveQuickSight"
-  engine_version          = %[2]q
-  host_instance_type      = "quicksight.t2.micro"
-  security_groups         = [aws_security_group.test.id]
-  authentication_strategy = "simple"
-  storage_type            = "efs"
-  logs {
-    general = true
-  }
-  user {
-    username = "Test"
-    password = "TestTest1234"
-  }
+  termination_protection_enabled = false
+
+  depends_on = [aws_quicksight_account_subscription.test]
 }
-`, rName, version)
+`)
+}
+
+func testAccAccountConfig_resetOnDelete(rName string, reset bool) string {
+	return acctest.ConfigCompose(
+		testAccAccountSettings_base(rName),
+		fmt.Sprintf(`
+resource "aws_quicksight_account_settings" "test" {
+  termination_protection_enabled = false
+  reset_on_delete                = %[1]t
+
+  depends_on = [aws_quicksight_account_subscription.test]
+}
+`, reset))
 }
---
+++ website/docs/r/quicksight_account_settings.html.markdown
@@ -0,0 +1,62 @@
+---
+subcategory: "QuickSight"
+layout: "aws"
+page_title: "AWS: aws_quicksight_account_settings"
+description: |-
+  Terraform resource for managing an AWS QuickSight Account Settings.
+---
+
+# Resource: aws_quicksight_account_settings
+
+Terraform resource for managing an AWS QuickSight Account Settings.
+
+~> Due to the collision of the `notification_email` attribute of the [`aws_quicksight_account_subscription`](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/quicksight_account_subscription) resource and the [`UpdateAccountSettings`](https://docs.aws.amazon.com/quicksight/latest/APIReference/API_UpdateAccountSettings.html) API does not support updating the default namespace, this resource does not support their management.
+
+## Example Usage
+
+```terraform
+resource "aws_quicksight_account_subscription" "subscription" {
+  account_name          = "quicksight-terraform"
+  authentication_method = "IAM_AND_QUICKSIGHT"
+  edition               = "ENTERPRISE"
+  notification_email    = "notification@email.com"
+}
+
+resource "aws_quicksight_account_settings" "example" {
+  termination_protection_enabled = false
+
+  depends_on = [aws_quicksight_account_subscription.subscription]
+}
+```
+
+## Argument Reference
+
+The following arguments are required:
+
+* `termination_protection_enabled` - (Optional) A boolean value that determines whether or not an Amazon QuickSight account can be deleted. If `true`, it does not allow the account to be deleted and results in an error message if a user tries to make a DeleteAccountSubscription request. If `false`, it will allow the account to be deleted.
+* `reset_on_delete` - (Optional) If `true`, destroying the resource will reset the TerminationProtectionEnabled account settings to default of `true`, otherwise account settings are not modified.
+  Defaults to `false`.
+  Will be removed in a future major version of the provider.
+
+## Attribute Reference
+
+This resource exports the following attributes in addition to the arguments above:
+
+* `default_namespace` - The default namespace for this Amazon Web Services account. Currently, the default is default.
+
+## Import
+
+In Terraform v1.5.0 and later, use an [`import` block](https://developer.hashicorp.com/terraform/language/import) to import QuickSight Account Settings using the AWS account ID `api-gateway-account`. For example:
+
+```terraform
+import {
+  to = aws_quicksight_account_settings.example
+  id = "012345678901"
+}
+```
+
+Using `terraform import`, import QuickSight Account Settings using the AWS account ID `api-gateway-account`. For example:
+
+```console
+% terraform import aws_quicksight_account_settings.example "012345678901"
+```
