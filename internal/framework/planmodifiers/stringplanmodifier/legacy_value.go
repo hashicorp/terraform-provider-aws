bb437ed3fe98c3b285d214b04acdc4ae064614f1
Graham Davison <gdavison@hashicorp.com>
2025-04-09 13:15:17 -0500

    Tweaks skip condition order

--- internal/framework/planmodifiers/stringplanmodifier/legacy_value.go
+++ internal/framework/planmodifiers/stringplanmodifier/legacy_value.go
@@ -27,13 +27,13 @@ func (m legacyValueModifier) MarkdownDescription(ctx context.Context) string {
 }
 
 func (m legacyValueModifier) PlanModifyString(ctx context.Context, req planmodifier.StringRequest, resp *planmodifier.StringResponse) {
-	// Use value from Config if set
-	if !req.ConfigValue.IsNull() {
+	// Exit if another planmodifier has set the value
+	if !req.PlanValue.IsUnknown() {
 		return
 	}
 
-	// Exit if another planmodifier has set the value
-	if !req.PlanValue.IsUnknown() {
+	// Use value from Config if set
+	if !req.ConfigValue.IsNull() {
 		return
 	}
 
