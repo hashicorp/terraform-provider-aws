b6133b4c4bdf1b6e52f90b3f6630bbda0c0deb38
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-04-15 12:05:31 -0400

    aws_dx_gateway: Disable per-resource Region override as it's a global resource.

--- internal/service/directconnect/gateway.go
+++ internal/service/directconnect/gateway.go
@@ -27,7 +27,7 @@ import (
 )
 
 // @SDKResource("aws_dx_gateway", name="Gateway")
-// @Region(global=true)
+// @Region(overrideEnabled=false)
 func resourceGateway() *schema.Resource {
 	return &schema.Resource{
 		CreateWithoutTimeout: resourceGatewayCreate,
