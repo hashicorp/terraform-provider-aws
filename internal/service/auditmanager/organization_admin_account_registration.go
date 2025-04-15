09af073c1e56d405e532a328a755c8a08b694354
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-04-15 15:09:11 -0400

    r/aws_auditmanager_organization_admin_account_registration: Global resource.

--- internal/service/auditmanager/organization_admin_account_registration.go
+++ internal/service/auditmanager/organization_admin_account_registration.go
@@ -20,6 +20,7 @@ import (
 )
 
 // @FrameworkResource("aws_auditmanager_organization_admin_account_registration", name="Organization Admin Account Registration")
+// @Region(overrideEnabled=false)
 func newResourceOrganizationAdminAccountRegistration(_ context.Context) (resource.ResourceWithConfigure, error) {
 	return &resourceOrganizationAdminAccountRegistration{}, nil
 }
