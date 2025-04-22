fd384d6b599408be7cf2c41bc4db0b419dfa8255
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-04-22 17:12:54 -0400

    Add 'framework.WithRegionModel' to resources where necessary - vpclattice.

--- internal/service/vpclattice/service_network_resource_association.go
+++ internal/service/vpclattice/service_network_resource_association.go
@@ -45,7 +45,7 @@ func newServiceNetworkResourceAssociationResource(_ context.Context) (resource.R
 }
 
 type serviceNetworkResourceAssociationResource struct {
-	framework.ResourceWithConfigure
+	framework.ResourceWithModel[serviceNetworkResourceAssociationResourceModel]
 	framework.WithNoOpUpdate[serviceNetworkResourceAssociationResourceModel]
 	framework.WithImportByID
 	framework.WithTimeouts
@@ -275,6 +275,7 @@ func waitServiceNetworkResourceAssociationDeleted(ctx context.Context, conn *vpc
 }
 
 type serviceNetworkResourceAssociationResourceModel struct {
+	framework.WithRegionModel
 	ARN                     types.String                                   `tfsdk:"arn"`
 	ID                      types.String                                   `tfsdk:"id"`
 	DNSEntry                fwtypes.ListNestedObjectValueOf[dnsEntryModel] `tfsdk:"dns_entry"`
