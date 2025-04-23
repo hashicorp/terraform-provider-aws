c173312445a28b0a8c7b8b9c0ff739523282f818
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-04-23 15:40:13 -0400

    Add 'framework.WithRegionModel' to resources where necessary - mediapackagev2.

--- internal/service/mediapackagev2/service_package_gen.go
+++ internal/service/mediapackagev2/service_package_gen.go
@@ -23,7 +23,7 @@ func (p *servicePackage) FrameworkDataSources(ctx context.Context) []*inttypes.S
 func (p *servicePackage) FrameworkResources(ctx context.Context) []*inttypes.ServicePackageFrameworkResource {
 	return []*inttypes.ServicePackageFrameworkResource{
 		{
-			Factory:  newResourceChannelGroup,
+			Factory:  newChannelGroupResource,
 			TypeName: "aws_media_packagev2_channel_group",
 			Name:     "Channel Group",
 			Tags: unique.Make(inttypes.ServicePackageResourceTags{
