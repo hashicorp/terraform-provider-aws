90ae7401dc8f876623d00709422aff26cd7561f3
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-04-23 11:56:09 -0400

    Add 'framework.WithRegionModel' to resources where necessary - bcmdataexports.

--- internal/service/bcmdataexports/service_package_gen.go
+++ internal/service/bcmdataexports/service_package_gen.go
@@ -23,7 +23,7 @@ func (p *servicePackage) FrameworkDataSources(ctx context.Context) []*inttypes.S
 func (p *servicePackage) FrameworkResources(ctx context.Context) []*inttypes.ServicePackageFrameworkResource {
 	return []*inttypes.ServicePackageFrameworkResource{
 		{
-			Factory:  newResourceExport,
+			Factory:  newExportResource,
 			TypeName: "aws_bcmdataexports_export",
 			Name:     "Export",
 			Tags: unique.Make(inttypes.ServicePackageResourceTags{
