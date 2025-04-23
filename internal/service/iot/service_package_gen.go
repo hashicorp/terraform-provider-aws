a3dc1a180a77d4b4b44adcc3cbd5fae618504989
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-04-23 16:39:31 -0400

    Add 'framework.WithRegionModel' to resources where necessary - iot.

--- internal/service/iot/service_package_gen.go
+++ internal/service/iot/service_package_gen.go
@@ -23,7 +23,7 @@ func (p *servicePackage) FrameworkDataSources(ctx context.Context) []*inttypes.S
 func (p *servicePackage) FrameworkResources(ctx context.Context) []*inttypes.ServicePackageFrameworkResource {
 	return []*inttypes.ServicePackageFrameworkResource{
 		{
-			Factory:  newResourceBillingGroup,
+			Factory:  newBillingGroupResource,
 			TypeName: "aws_iot_billing_group",
 			Name:     "Billing Group",
 			Tags: unique.Make(inttypes.ServicePackageResourceTags{
