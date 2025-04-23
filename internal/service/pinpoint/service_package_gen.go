83bdd2808e46d9fcd9592a828d64fd9faac6e9b9
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-04-23 15:40:15 -0400

    Add 'framework.WithRegionModel' to resources where necessary - pinpoint.

--- internal/service/pinpoint/service_package_gen.go
+++ internal/service/pinpoint/service_package_gen.go
@@ -23,7 +23,7 @@ func (p *servicePackage) FrameworkDataSources(ctx context.Context) []*inttypes.S
 func (p *servicePackage) FrameworkResources(ctx context.Context) []*inttypes.ServicePackageFrameworkResource {
 	return []*inttypes.ServicePackageFrameworkResource{
 		{
-			Factory:  newResourceEmailTemplate,
+			Factory:  newEmailTemplateResource,
 			TypeName: "aws_pinpoint_email_template",
 			Name:     "Email Template",
 			Tags: unique.Make(inttypes.ServicePackageResourceTags{
