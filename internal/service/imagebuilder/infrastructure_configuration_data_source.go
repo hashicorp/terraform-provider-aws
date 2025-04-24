a51b8a706d4941bf2b6b9502a7f3540ce7cb7555
tabito <tabito.htnet.org@gmail.com>
2025-04-24 11:40:43 +0900

    import lint

--- internal/service/imagebuilder/infrastructure_configuration_data_source.go
+++ internal/service/imagebuilder/infrastructure_configuration_data_source.go
@@ -5,6 +5,7 @@ package imagebuilder
 
 import (
 	"context"
+
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
