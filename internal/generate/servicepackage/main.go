8035bad965d4066859d5480ec2e8d696a8633bef
Graham Davison <gdavison@hashicorp.com>
2025-04-25 14:20:15 -0700

    Restores resource-type–level `IsGlobal` setting to `@Region` annotation

--- internal/generate/servicepackage/main.go
+++ internal/generate/servicepackage/main.go
@@ -119,11 +119,16 @@ func main() {
 type ResourceDatum struct {
 	FactoryName                       string
 	Name                              string // Friendly name (without service name), e.g. "Topic", not "SNS Topic"
-	RegionOverrideEnabled             bool
+	IsGlobal                          bool
+	regionOverrideEnabled             bool
 	TransparentTagging                bool
 	TagsIdentifierAttribute           string
 	TagsResourceType                  string
 	ValidateRegionOverrideInPartition bool
+}
+
+func (d ResourceDatum) RegionOverrideEnabled() bool {
+	return d.regionOverrideEnabled && !d.IsGlobal
 }
 
 type ServiceDatum struct {
@@ -208,7 +213,8 @@ func (v *visitor) processFuncDecl(funcDecl *ast.FuncDecl) {
 
 	// Look first for per-resource annotations such as tagging and Region.
 	d := ResourceDatum{
-		RegionOverrideEnabled:             true,
+		IsGlobal:                          false,
+		regionOverrideEnabled:             true,
 		ValidateRegionOverrideInPartition: true,
 	}
 
@@ -218,11 +224,18 @@ func (v *visitor) processFuncDecl(funcDecl *ast.FuncDecl) {
 		if m := annotation.FindStringSubmatch(line); len(m) > 0 {
 			switch annotationName, args := m[1], common.ParseArgs(m[3]); annotationName {
 			case "Region":
+				if attr, ok := args.Keyword["global"]; ok {
+					if global, err := strconv.ParseBool(attr); err != nil {
+						v.errs = append(v.errs, fmt.Errorf("invalid Region/global value (%s): %s: %w", attr, fmt.Sprintf("%s.%s", v.packageName, v.functionName), err))
+					} else {
+						d.IsGlobal = global
+					}
+				}
 				if attr, ok := args.Keyword["overrideEnabled"]; ok {
 					if enabled, err := strconv.ParseBool(attr); err != nil {
 						v.errs = append(v.errs, fmt.Errorf("invalid Region/overrideEnabled value (%s): %s: %w", attr, fmt.Sprintf("%s.%s", v.packageName, v.functionName), err))
 					} else {
-						d.RegionOverrideEnabled = enabled
+						d.regionOverrideEnabled = enabled
 					}
 				}
 				if attr, ok := args.Keyword["validateOverrideInPartition"]; ok {
