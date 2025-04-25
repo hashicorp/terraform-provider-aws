874f685c4048b61037cac87fb57f3cf688d4d747
Madhav Vishnubhatta <mvishnub@amazon.co.uk>
2025-04-25 14:25:06 +0000

    Added documentation

--- internal/service/inspector2/filter.go
+++ internal/service/inspector2/filter.go
@@ -29,7 +29,6 @@ import (
 	fwflex "github.com/hashicorp/terraform-provider-aws/internal/framework/flex"
 	fwtypes "github.com/hashicorp/terraform-provider-aws/internal/framework/types"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
-
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
--- internal/service/inspector2/filter_test.go
+++ internal/service/inspector2/filter_test.go
@@ -21,11 +21,9 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/create"
-
+	tfinspector2 "github.com/hashicorp/terraform-provider-aws/internal/service/inspector2"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/names"
-
-	tfinspector2 "github.com/hashicorp/terraform-provider-aws/internal/service/inspector2"
 )
 
 func testAccInspector2Filter_basic(t *testing.T) {
