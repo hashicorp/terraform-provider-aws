bbfb020090dede5088e6c3f5d3005038ff337c8e
Graham Davison <gdavison@hashicorp.com>
2025-05-08 16:20:31 -0700

    Adds special case import handling for ARN Identity

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -506,7 +506,7 @@ func initialize(ctx context.Context, provider *schema.Provider) (map[string]conn
 					why:         CustomizeDiff,
 					interceptor: forceNewIfRegionChanges(),
 				})
-				if resource.Identity.Singleton {
+				if resource.Identity.Singleton || resource.Identity.ARN {
 					interceptors = append(interceptors, interceptorInvocation{
 						when:        Before,
 						why:         Import,
