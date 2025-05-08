7071295cf6633531265c49980dc94104cde63552
Graham Davison <gdavison@hashicorp.com>
2025-05-08 16:24:08 -0700

    Adds import handler to `aws_lb`

--- internal/service/elbv2/load_balancer.go
+++ internal/service/elbv2/load_balancer.go
@@ -49,7 +49,20 @@ func resourceLoadBalancer() *schema.Resource {
 		DeleteWithoutTimeout: resourceLoadBalancerDelete,
 
 		Importer: &schema.ResourceImporter{
-			StateContext: schema.ImportStatePassthroughContext,
+			StateContext: func(ctx context.Context, rd *schema.ResourceData, meta any) ([]*schema.ResourceData, error) {
+				arnARN, err := arn.Parse(rd.Id())
+				if err != nil {
+					return nil, fmt.Errorf("could not parse import ID %q as ARN: %s", rd.Id(), err)
+				}
+				if region, ok := rd.GetOk("region"); ok {
+					if region != arnARN.Region {
+						return nil, fmt.Errorf("the region passed for import %q does not match the region %q in the ARN %q", region, arnARN.Region, rd.Id())
+					}
+				} else {
+					rd.Set("region", arnARN.Region)
+				}
+				return []*schema.ResourceData{rd}, nil
+			},
 		},
 
 		CustomizeDiff: customdiff.Sequence(
