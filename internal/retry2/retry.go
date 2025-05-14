e488bf4e4ca5b41a1c88be640ad32cb8921dbdae
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-05-14 14:22:30 -0400

    Simple 'retry' functionality working.

--- internal/retry2/retry.go
+++ internal/retry2/retry.go
@@ -5,6 +5,8 @@ package retry2
 
 import (
 	"context"
+	"math"
+	"math/rand"
 	"time"
 )
 
@@ -15,11 +17,99 @@ type Timer interface {
 	After(time.Duration) <-chan time.Time
 }
 
+// DelayFunc returns the duration to wait before the next retry attempt.
+type DelayFunc func(uint) time.Duration
+
+// FixedDelay returns a delay that is the same through all iterations except the firts (when it is 0).
+func FixedDelay(delay time.Duration) DelayFunc {
+	return func(n uint) time.Duration {
+		if n == 0 {
+			return 0
+		}
+
+		return delay
+	}
+}
+
+// Do not use the default RNG since we do not want different provider instances
+// to pick the same deterministic random sequence.
+var rng = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+// ExponentialBackoffWithJitterDelay returns a duration of backoffMinDuration * backoffMultiplier**n, with added jitter.
+func ExponentialBackoffWithJitterDelay(backoffMinDuration time.Duration, backoffMultiplier float64) DelayFunc {
+	return func(n uint) time.Duration {
+		if n == 0 {
+			return 0
+		}
+
+		mult := math.Pow(backoffMultiplier, float64(n))
+		d := time.Duration(float64(backoffMinDuration) * mult)
+		const jitter = 0.4
+		mult = 1 - jitter*rng.Float64() // Subtract up to 40%.
+		return time.Duration(float64(d) * mult)
+	}
+}
+
+type sdkv2HelperRetryCompatibleDelay struct {
+	minTimeout   time.Duration
+	pollInterval time.Duration
+	wait         time.Duration
+}
+
+func (d *sdkv2HelperRetryCompatibleDelay) delay() time.Duration {
+	wait := d.wait
+
+	// First round had no wait.
+	if wait == 0 {
+		wait = 100 * time.Millisecond
+	}
+
+	wait *= 2
+
+	// If a poll interval has been specified, choose that interval.
+	// Otherwise bound the default value.
+	if d.pollInterval > 0 && d.pollInterval < 180*time.Second {
+		wait = d.pollInterval
+	} else {
+		if wait < d.minTimeout {
+			wait = d.minTimeout
+		} else if wait > 10*time.Second {
+			wait = 10 * time.Second
+		}
+	}
+
+	d.wait = wait
+
+	return wait
+}
+
+// SDKv2HelperRetryCompatibleDelay returns a Terraform Plugin SDK v2 helper/retry-compatible delay.
+func SDKv2HelperRetryCompatibleDelay(initialDelay, pollInterval, minTimeout time.Duration) DelayFunc {
+	delay := &sdkv2HelperRetryCompatibleDelay{
+		minTimeout:   minTimeout,
+		pollInterval: pollInterval,
+	}
+
+	return func(n uint) time.Duration {
+		if n == 0 {
+			return initialDelay
+		}
+
+		return delay.delay()
+	}
+}
+
+// DefaultSDKv2HelperRetryCompatibleDelay returns a Terraform Plugin SDK v2 helper/retry-compatible delay
+// with default values (from the `RetryContext` function).
+func DefaultSDKv2HelperRetryCompatibleDelay() DelayFunc {
+	return SDKv2HelperRetryCompatibleDelay(0, 0, 500*time.Millisecond)
+}
+
 // Config configures a retry loop.
 type Config struct {
-	delay    time.Duration
-	maxDelay time.Duration
-	timer    Timer
+	delay       DelayFunc
+	gracePeriod time.Duration
+	timer       Timer
 }
 
 // Option represents an option for retry.
@@ -27,17 +117,19 @@ type Option func(*Config)
 
 func emptyOption(c *Config) {}
 
-// WithDelay sets the delay between retries.
-func WithDelay(delay time.Duration) Option {
+func WithGracePeriod(d time.Duration) Option {
 	return func(c *Config) {
-		c.delay = delay
+		c.gracePeriod = d
 	}
 }
 
-// WithMaxDelay sets the maximum delay between retries.
-func WithMaxDelay(maxDelay time.Duration) Option {
+func WithDelay(d DelayFunc) Option {
+	if d == nil {
+		return emptyOption
+	}
+
 	return func(c *Config) {
-		c.maxDelay = maxDelay
+		c.delay = d
 	}
 }
 
@@ -58,31 +150,28 @@ func (t *timerImpl) After(d time.Duration) <-chan time.Time {
 }
 
 // The default Config is backwards compatible with github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry.
-func defaultConfig() *Config {
-	return &Config{
-		timer: &timerImpl{},
+func defaultConfig() Config {
+	return Config{
+		delay:       DefaultSDKv2HelperRetryCompatibleDelay(),
+		gracePeriod: 30 * time.Second,
+		timer:       &timerImpl{},
 	}
 }
 
 // RetryWithTimeout holds state for managing retry loops with a timeout.
 type RetryWithTimeout struct {
-	config   *Config
-	attempt  int
+	attempt  uint
+	config   Config
 	deadline deadline
+	timedOut bool
 }
 
 // BeginWithOptions returns a new retry loop configured with the provided options.
 func BeginWithOptions(timeout time.Duration, opts ...Option) *RetryWithTimeout {
 	config := defaultConfig()
 	for _, opt := range opts {
-		opt(config)
+		opt(&config)
 	}
-
-	if timeout <= 0 {
-		timeout = 1<<63 - 1 // time.maxDuration
-	}
-
-	// TODO Backwards compatibel grace period.
 
 	return &RetryWithTimeout{
 		config:   config,
@@ -100,18 +189,40 @@ func Begin(timeout time.Duration) *RetryWithTimeout {
 // The first call does not sleep.
 func (r *RetryWithTimeout) Continue(ctx context.Context) bool {
 	if r.deadline.Remaining() == 0 {
-		return false
+		if r.config.gracePeriod == 0 {
+			r.timedOut = true
+			return false
+		}
+
+		r.deadline = NewDeadline(r.config.gracePeriod)
+		r.config.gracePeriod = 0
 	}
 
-	if r.attempt != 0 {
-		//randomizedSleep(ctx, r.backoffDelay())
-	}
+	r.sleep(ctx, r.config.delay(r.attempt))
 	r.attempt++
 
 	return true
 }
 
-// Reset resets a Retry to its initial state.
+// Reset resets a RetryWithTimeout to its initial state.
 func (r *RetryWithTimeout) Reset() {
 	r.attempt = 0
+}
+
+// TimedOut return whether the retry timed out.
+func (r *RetryWithTimeout) TimedOut() bool {
+	return r.timedOut
+}
+
+// Sleeps for the specified duration or until context is done, whichever occurs first.
+func (r *RetryWithTimeout) sleep(ctx context.Context, d time.Duration) {
+	if d == 0 {
+		return
+	}
+
+	select {
+	case <-ctx.Done():
+		return
+	case <-r.config.timer.After(d):
+	}
 }
---
+++ internal/retry2/retry_test.go
@@ -0,0 +1,114 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package retry2
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestDefaultSDKv2HelperRetryCompatibleDelay(t *testing.T) {
+	t.Parallel()
+
+	delay := DefaultSDKv2HelperRetryCompatibleDelay()
+	want := []time.Duration{
+		0,
+		500 * time.Millisecond,
+		1 * time.Second,
+		2 * time.Second,
+		4 * time.Second,
+		8 * time.Second,
+		10 * time.Second,
+		10 * time.Second,
+		10 * time.Second,
+		10 * time.Second,
+	}
+	var got []time.Duration
+	for i := range len(want) {
+		got = append(got, delay(uint(i)))
+	}
+
+	if diff := cmp.Diff(got, want); diff != "" {
+		t.Errorf("unexpected diff (+wanted, -got): %s", diff)
+	}
+}
+
+func TestSDKv2HelperRetryCompatibleDelay(t *testing.T) {
+	t.Parallel()
+
+	delay := SDKv2HelperRetryCompatibleDelay(200*time.Millisecond, 0, 3*time.Second)
+	want := []time.Duration{
+		200 * time.Millisecond,
+		3 * time.Second,
+		6 * time.Second,
+		10 * time.Second,
+		10 * time.Second,
+	}
+	var got []time.Duration
+	for i := range len(want) {
+		got = append(got, delay(uint(i)))
+	}
+
+	if diff := cmp.Diff(got, want); diff != "" {
+		t.Errorf("unexpected diff (+wanted, -got): %s", diff)
+	}
+}
+
+func TestSDKv2HelperRetryCompatibleDelayWithPollTimeout(t *testing.T) {
+	t.Parallel()
+
+	delay := SDKv2HelperRetryCompatibleDelay(200*time.Millisecond, 20*time.Second, 3*time.Second)
+	want := []time.Duration{
+		200 * time.Millisecond,
+		20 * time.Second,
+		20 * time.Second,
+		20 * time.Second,
+		20 * time.Second,
+	}
+	var got []time.Duration
+	for i := range len(want) {
+		got = append(got, delay(uint(i)))
+	}
+
+	if diff := cmp.Diff(got, want); diff != "" {
+		t.Errorf("unexpected diff (+wanted, -got): %s", diff)
+	}
+}
+
+func TestRetryWithTimeoutDefaultGracePeriod(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+
+	var n int
+	for r := BeginWithOptions(1*time.Minute, WithDelay(FixedDelay(1*time.Second))); r.Continue(ctx); {
+		time.Sleep(35 * time.Second)
+		n++
+	}
+
+	// Want = 3 because of default 30s grace period.
+	if got, want := n, 3; got != want {
+		t.Errorf("Iterations = %v, want %v", got, want)
+	}
+}
+
+func TestRetryWithTimeoutNoGracePeriod(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+
+	var n int
+	for r := BeginWithOptions(1*time.Minute, WithDelay(FixedDelay(1*time.Second)), WithGracePeriod(0)); r.Continue(ctx); {
+		time.Sleep(35 * time.Second)
+		n++
+	}
+
+	// Want = 2 because of no grace period.
+	if got, want := n, 2; got != want {
+		t.Errorf("Iterations = %v, want %v", got, want)
+	}
+}
