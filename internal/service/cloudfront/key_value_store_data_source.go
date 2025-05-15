3c6c31ee4b529f6821c582db5ef85b3f39473a65
tabito <tabito.htnet.org@gmail.com>
2025-05-16 01:52:39 +0900

    fixed codes issued by import-lint

--- internal/service/cloudfront/key_value_store_data_source.go
+++ internal/service/cloudfront/key_value_store_data_source.go
@@ -5,13 +5,13 @@ package cloudfront
 
 import (
 	"context"
+
 	"github.com/YakDriver/regexache"
 	"github.com/hashicorp/terraform-plugin-framework-timetypes/timetypes"
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
-	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
-
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-provider-aws/internal/create"
 	"github.com/hashicorp/terraform-provider-aws/internal/framework"
