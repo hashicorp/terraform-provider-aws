499a8bb583b3196c55b9893b3be20d6b09e7b871
Kazuho Cryer-Shinozuka <malaysia.cryer@gmail.com>
2025-05-19 00:26:18 +0900

    refactor

--- internal/service/batch/compute_environment.go
+++ internal/service/batch/compute_environment.go
@@ -380,7 +380,7 @@ func resourceComputeEnvironmentRead(ctx context.Context, d *schema.ResourceData,
 	d.Set(names.AttrStatusReason, computeEnvironment.StatusReason)
 	d.Set(names.AttrType, computeEnvironment.Type)
 	if computeEnvironment.UnmanagedvCpus != nil {
-		d.Set("unmanaged_v_cpus", aws.ToInt32(computeEnvironment.UnmanagedvCpus))
+		d.Set("unmanaged_v_cpus", computeEnvironment.UnmanagedvCpus)
 	}
 	if err := d.Set("update_policy", flattenComputeEnvironmentUpdatePolicy(computeEnvironment.UpdatePolicy)); err != nil {
 		return sdkdiag.AppendErrorf(diags, "setting update_policy: %s", err)
