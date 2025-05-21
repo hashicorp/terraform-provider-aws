d0cb223df43447795d85b57a42dc51215c13a162
Graham Davison <gdavison@hashicorp.com>
2025-05-21 12:55:58 -0700

    Adds `acctest.CrossRegionImportStateIdFuncAdapter`

--- internal/acctest/state_id.go
+++ internal/acctest/state_id.go
@@ -39,3 +39,27 @@ func CrossRegionImportStateIdFunc(resourceName string) resource.ImportStateIdFun
 		return id + "@" + region, nil
 	}
 }
+
+// CrossRegionImportStateIdFuncAdapter adapts an ImportStateIdFunc by appending the region
+func CrossRegionImportStateIdFuncAdapter(resourceName string, f Func) resource.ImportStateIdFunc {
+	return func(s *terraform.State) (string, error) {
+		rs, ok := s.RootModule().Resources[resourceName]
+		if !ok {
+			return "", fmt.Errorf("Not found: %s", resourceName)
+		}
+
+		id, err := f(resourceName)(s)
+		if err != nil {
+			return "", err
+		}
+
+		region, ok := rs.Primary.Attributes["region"]
+		if !ok {
+			return "", fmt.Errorf("Attribute \"region\" not found in %s", resourceName)
+		}
+
+		return id + "@" + region, nil
+	}
+}
+
+type Func func(resourceName string) resource.ImportStateIdFunc
