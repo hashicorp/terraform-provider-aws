096fab1233a41d71cb341e4da65fd1f3d44fc354
Graham Davison <gdavison@hashicorp.com>
2025-05-26 15:09:51 -0700

    `xray`

--- internal/service/xray/service_package_gen.go
+++ internal/service/xray/service_package_gen.go
@@ -42,6 +42,10 @@ func (p *servicePackage) SDKResources(ctx context.Context) []*inttypes.ServicePa
 			TypeName: "aws_xray_encryption_config",
 			Name:     "Encryption Config",
 			Region:   unique.Make(inttypes.ResourceRegionDefault()),
+			Identity: inttypes.RegionalSingletonIdentity(),
+			Import: inttypes.Import{
+				WrappedImport: true,
+			},
 		},
 		{
 			Factory:  resourceGroup,
