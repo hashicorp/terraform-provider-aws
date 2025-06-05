f3f4874616ea7b99e004878a82699d0ba504e129
Graham Davison <gdavison@hashicorp.com>
2025-06-05 15:44:36 -0700

    Moves `ImportState` wrappers to separate file

---
+++ internal/framework/import_test.go
@@ -0,0 +1,61 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package framework_test
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/identityschema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+)
+
+type importStater interface {
+	ImportState(ctx context.Context, request resource.ImportStateRequest, response *resource.ImportStateResponse)
+}
+
+func importByID(ctx context.Context, importer importStater, resourceSchema schema.Schema, id string, identitySchema identityschema.Schema) resource.ImportStateResponse {
+	identity := emtpyIdentityFromSchema(identitySchema)
+
+	request := resource.ImportStateRequest{
+		ID:       id,
+		Identity: identity,
+	}
+	response := resource.ImportStateResponse{
+		State:    emtpyStateFromSchema(resourceSchema),
+		Identity: identity,
+	}
+	importer.ImportState(ctx, request, &response)
+
+	return response
+}
+
+func importByIDNoIdentity(ctx context.Context, importer importStater, resourceSchema schema.Schema, id string) resource.ImportStateResponse {
+	request := resource.ImportStateRequest{
+		ID:       id,
+		Identity: nil,
+	}
+	response := resource.ImportStateResponse{
+		State:    emtpyStateFromSchema(resourceSchema),
+		Identity: nil,
+	}
+	importer.ImportState(ctx, request, &response)
+
+	return response
+}
+
+func importByIdentity(ctx context.Context, importer importStater, resourceSchema schema.Schema, identitySchema identityschema.Schema, identityAttrs map[string]string) resource.ImportStateResponse {
+	identity := identityFromSchema(identitySchema, identityAttrs)
+
+	request := resource.ImportStateRequest{
+		Identity: identity,
+	}
+	response := resource.ImportStateResponse{
+		State:    emtpyStateFromSchema(resourceSchema),
+		Identity: identity,
+	}
+	importer.ImportState(ctx, request, &response)
+
+	return response
+}
--- internal/framework/with_import_by_global_arn_test.go
+++ internal/framework/with_import_by_global_arn_test.go
@@ -10,7 +10,6 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws/arn"
 	"github.com/hashicorp/terraform-plugin-framework/path"
-	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/identityschema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
@@ -322,53 +321,4 @@ func getAttributeValue(ctx context.Context, t *testing.T, state tfsdk.State, pat
 		t.Fatalf("Unexpected error getting attribute %q: %s", path, fwdiag.DiagnosticsError(diags))
 	}
 	return attrVal.ValueString()
-}
-
-type importStater interface {
-	ImportState(ctx context.Context, request resource.ImportStateRequest, response *resource.ImportStateResponse)
-}
-
-func importByID(ctx context.Context, importer importStater, resourceSchema schema.Schema, id string, identitySchema identityschema.Schema) resource.ImportStateResponse {
-	identity := emtpyIdentityFromSchema(identitySchema)
-
-	request := resource.ImportStateRequest{
-		ID:       id,
-		Identity: identity,
-	}
-	response := resource.ImportStateResponse{
-		State:    emtpyStateFromSchema(resourceSchema),
-		Identity: identity,
-	}
-	importer.ImportState(ctx, request, &response)
-
-	return response
-}
-
-func importByIDNoIdentity(ctx context.Context, importer importStater, resourceSchema schema.Schema, id string) resource.ImportStateResponse {
-	request := resource.ImportStateRequest{
-		ID:       id,
-		Identity: nil,
-	}
-	response := resource.ImportStateResponse{
-		State:    emtpyStateFromSchema(resourceSchema),
-		Identity: nil,
-	}
-	importer.ImportState(ctx, request, &response)
-
-	return response
-}
-
-func importByIdentity(ctx context.Context, importer importStater, resourceSchema schema.Schema, identitySchema identityschema.Schema, identityAttrs map[string]string) resource.ImportStateResponse {
-	identity := identityFromSchema(identitySchema, identityAttrs)
-
-	request := resource.ImportStateRequest{
-		Identity: identity,
-	}
-	response := resource.ImportStateResponse{
-		State:    emtpyStateFromSchema(resourceSchema),
-		Identity: identity,
-	}
-	importer.ImportState(ctx, request, &response)
-
-	return response
 }
