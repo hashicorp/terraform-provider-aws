89c231db37d8d090d8dc4376483d01bd364d051a
Graham Davison <gdavison@hashicorp.com>
2025-06-05 17:55:55 -0700

    Factors out common Framework ARN import

--- internal/framework/with_import_by_global_arn.go
+++ internal/framework/with_import_by_global_arn.go
@@ -5,67 +5,16 @@ package framework
 
 import (
 	"context"
-	"fmt"
 
-	"github.com/aws/aws-sdk-go-v2/aws/arn"
-	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 )
 
 // WithImportByGlobalARN is intended to be embedded in global resources which import state via the "arn" attribute.
 // See https://developer.hashicorp.com/terraform/plugin/framework/resources/import.
 type WithImportByGlobalARN struct {
-	attributeName  string
-	duplicateAttrs []string
-}
-
-func (w *WithImportByGlobalARN) SetARNAttributeName(attr string, duplicateAttrs []string) {
-	w.attributeName = attr
-	w.duplicateAttrs = duplicateAttrs
+	importByARN
 }
 
 func (w *WithImportByGlobalARN) ImportState(ctx context.Context, request resource.ImportStateRequest, response *resource.ImportStateResponse) {
-	if request.ID != "" {
-		_, err := arn.Parse(request.ID)
-		if err != nil {
-			response.Diagnostics.AddError(
-				"Invalid Resource Import ID Value",
-				"The import ID could not be parsed as an ARN.\n\n"+
-					fmt.Sprintf("Value: %q\nError: %s", request.ID, err),
-			)
-			return
-		}
-
-		response.Diagnostics.Append(response.State.SetAttribute(ctx, path.Root(w.attributeName), request.ID)...)
-		for _, attr := range w.duplicateAttrs {
-			response.Diagnostics.Append(response.State.SetAttribute(ctx, path.Root(attr), request.ID)...)
-		}
-
-		if identity := response.Identity; identity != nil {
-			response.Diagnostics.Append(identity.SetAttribute(ctx, path.Root(w.attributeName), request.ID)...)
-		}
-
-		return
-	}
-
-	if identity := request.Identity; identity != nil {
-		arnPath := path.Root(w.attributeName)
-		var arnVal string
-		identity.GetAttribute(ctx, arnPath, &arnVal)
-
-		_, err := arn.Parse(arnVal)
-		if err != nil {
-			response.Diagnostics.AddAttributeError(
-				arnPath,
-				"Invalid Import Attribute Value",
-				fmt.Sprintf("Import attribute %q is not a valid ARN, got: %s", arnPath, arnVal),
-			)
-			return
-		}
-
-		response.Diagnostics.Append(response.State.SetAttribute(ctx, path.Root(w.attributeName), arnVal)...)
-		for _, attr := range w.duplicateAttrs {
-			response.Diagnostics.Append(response.State.SetAttribute(ctx, path.Root(attr), arnVal)...)
-		}
-	}
+	w.importState(ctx, request, response)
 }
--- internal/framework/with_import_by_regional_arn.go
+++ internal/framework/with_import_by_regional_arn.go
@@ -7,7 +7,6 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/aws/aws-sdk-go-v2/aws/arn"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/types"
@@ -17,27 +16,16 @@ import (
 // WithImportByRegionalARN is intended to be embedded in resources which import state via the "arn" attribute.
 // See https://developer.hashicorp.com/terraform/plugin/framework/resources/import.
 type WithImportByRegionalARN struct {
-	attributeName  string
-	duplicateAttrs []string
-}
-
-func (w *WithImportByRegionalARN) SetARNAttributeName(attr string, duplicateAttrs []string) {
-	w.attributeName = attr
-	w.duplicateAttrs = duplicateAttrs
+	importByARN
 }
 
 func (w *WithImportByRegionalARN) ImportState(ctx context.Context, request resource.ImportStateRequest, response *resource.ImportStateResponse) {
-	if request.ID != "" {
-		arnARN, err := arn.Parse(request.ID)
-		if err != nil {
-			response.Diagnostics.AddError(
-				"Invalid Resource Import ID Value",
-				"The import ID could not be parsed as an ARN.\n\n"+
-					fmt.Sprintf("Value: %q\nError: %s", request.ID, err),
-			)
-			return
-		}
+	arnARN := w.importState(ctx, request, response)
+	if response.Diagnostics.HasError() {
+		return
+	}
 
+	if request.ID != "" {
 		var region types.String
 		response.Diagnostics.Append(response.State.GetAttribute(ctx, path.Root(names.AttrRegion), &region)...)
 		if response.Diagnostics.HasError() {
@@ -52,47 +40,11 @@ func (w *WithImportByRegionalARN) ImportState(ctx context.Context, request resou
 				)
 				return
 			}
-		} else {
-			response.Diagnostics.Append(response.State.SetAttribute(ctx, path.Root(names.AttrRegion), arnARN.Region)...)
-			if response.Diagnostics.HasError() {
-				return
-			}
 		}
-
-		response.Diagnostics.Append(response.State.SetAttribute(ctx, path.Root(w.attributeName), request.ID)...)
-		for _, attr := range w.duplicateAttrs {
-			response.Diagnostics.Append(response.State.SetAttribute(ctx, path.Root(attr), request.ID)...)
-		}
-
-		if identity := response.Identity; identity != nil {
-			response.Diagnostics.Append(identity.SetAttribute(ctx, path.Root(w.attributeName), request.ID)...)
-		}
-
-		return
 	}
 
-	if identity := request.Identity; identity != nil {
-		arnPath := path.Root(w.attributeName)
-		var arnVal string
-		identity.GetAttribute(ctx, arnPath, &arnVal)
-
-		arnARN, err := arn.Parse(arnVal)
-		if err != nil {
-			response.Diagnostics.AddAttributeError(
-				arnPath,
-				"Invalid Import Attribute Value",
-				fmt.Sprintf("Import attribute %q is not a valid ARN, got: %s", arnPath, arnVal),
-			)
-			return
-		}
-		response.Diagnostics.Append(response.State.SetAttribute(ctx, path.Root(names.AttrRegion), arnARN.Region)...)
-		if response.Diagnostics.HasError() {
-			return
-		}
-
-		response.Diagnostics.Append(response.State.SetAttribute(ctx, path.Root(w.attributeName), arnVal)...)
-		for _, attr := range w.duplicateAttrs {
-			response.Diagnostics.Append(response.State.SetAttribute(ctx, path.Root(attr), arnVal)...)
-		}
+	response.Diagnostics.Append(response.State.SetAttribute(ctx, path.Root(names.AttrRegion), arnARN.Region)...)
+	if response.Diagnostics.HasError() {
+		return
 	}
 }
