4eb7646900ad5686139e94345cc275ac5fdaf52d
Graham Davison <gdavison@hashicorp.com>
2025-06-06 12:59:19 -0700

    Adds combined `framework.WithImportByARN`

---
+++ internal/framework/with_import_by_arn2.go
@@ -0,0 +1,31 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package framework
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-provider-aws/internal/provider/fwprovider/importer"
+	inttypes "github.com/hashicorp/terraform-provider-aws/internal/types"
+)
+
+// TODO: Needs a better name
+type ImportByIdentityer interface {
+	SetIdentitySpec(identity inttypes.Identity)
+}
+
+var _ ImportByIdentityer = &WithImportByARN{}
+
+type WithImportByARN struct {
+	identity inttypes.Identity
+}
+
+func (w *WithImportByARN) SetIdentitySpec(identity inttypes.Identity) {
+	w.identity = identity
+}
+
+func (w *WithImportByARN) ImportState(ctx context.Context, request resource.ImportStateRequest, response *resource.ImportStateResponse) {
+	importer.GlobalARN(ctx, request, &w.identity, response)
+}
---
+++ internal/framework/with_import_by_arn2_test.go
@@ -0,0 +1,236 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package framework_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/aws/arn"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-provider-aws/internal/errs/fwdiag"
+	"github.com/hashicorp/terraform-provider-aws/internal/framework"
+	inttypes "github.com/hashicorp/terraform-provider-aws/internal/types"
+)
+
+func TestImportByARN_GlobalARN_ImportID_Valid(t *testing.T) {
+	ctx := context.Background()
+
+	arn := arn.ARN{
+		Partition: "aws",
+		Service:   "a-service",
+		Region:    "",
+		AccountID: "123456789012",
+		Resource:  "res-abc123",
+	}.String()
+
+	importer := globalARNImporter()
+
+	response := importByID(ctx, &importer, globalARNSchema, arn, globalARNIdentitySchema)
+	if response.Diagnostics.HasError() {
+		t.Fatalf("Unexpected error: %s", fwdiag.DiagnosticsError(response.Diagnostics))
+	}
+
+	if e, a := arn, getAttributeValue(ctx, t, response.State, path.Root("arn")); e != a {
+		t.Errorf("expected `arn` to be %q, got %q", e, a)
+	}
+	if e, a := "", getAttributeValue(ctx, t, response.State, path.Root("attr")); e != a {
+		t.Errorf("expected `attr` to be %q, got %q", e, a)
+	}
+
+	if identity := response.Identity; identity == nil {
+		t.Error("Identity should be set")
+	} else {
+		var arnVal string
+		identity.GetAttribute(ctx, path.Root("arn"), &arnVal)
+		if e, a := arn, arnVal; e != a {
+			t.Errorf("expected Identity `arn` to be %q, got %q", e, a)
+		}
+	}
+}
+
+func TestImportByARN_GlobalARN_ImportID_Valid_NoIdentity(t *testing.T) {
+	ctx := context.Background()
+
+	arn := arn.ARN{
+		Partition: "aws",
+		Service:   "a-service",
+		Region:    "",
+		AccountID: "123456789012",
+		Resource:  "res-abc123",
+	}.String()
+
+	importer := globalARNImporter()
+
+	response := importByIDNoIdentity(ctx, &importer, globalARNSchema, arn)
+	if response.Diagnostics.HasError() {
+		t.Fatalf("Unexpected error: %s", fwdiag.DiagnosticsError(response.Diagnostics))
+	}
+
+	if e, a := arn, getAttributeValue(ctx, t, response.State, path.Root("arn")); e != a {
+		t.Errorf("expected `arn` to be %q, got %q", e, a)
+	}
+	if e, a := "", getAttributeValue(ctx, t, response.State, path.Root("attr")); e != a {
+		t.Errorf("expected `attr` to be %q, got %q", e, a)
+	}
+
+	if response.Identity != nil {
+		t.Error("Identity should not be set")
+	}
+}
+
+func TestImportByARN_GlobalARN_ImportID_Invalid_NotAnARN(t *testing.T) {
+	ctx := context.Background()
+
+	importer := globalARNImporter()
+
+	response := importByID(ctx, &importer, globalARNSchema, "not a valid ARN", globalARNIdentitySchema)
+	if response.Diagnostics.HasError() {
+		if response.Diagnostics[0].Summary() != "Invalid Resource Import ID Value" {
+			t.Fatalf("Unexpected error: %s", fwdiag.DiagnosticsError(response.Diagnostics))
+		}
+	} else {
+		t.Fatal("Expected error, got none")
+	}
+}
+
+func TestImportByARN_GlobalARN_Identity_Valid(t *testing.T) {
+	ctx := context.Background()
+
+	arn := arn.ARN{
+		Partition: "aws",
+		Service:   "a-service",
+		Region:    "",
+		AccountID: "123456789012",
+		Resource:  "res-abc123",
+	}.String()
+
+	importer := globalARNImporter()
+
+	response := importByIdentity(ctx, &importer, globalARNSchema, globalARNIdentitySchema, map[string]string{
+		"arn": arn,
+	})
+	if response.Diagnostics.HasError() {
+		t.Fatalf("Unexpected error: %s", fwdiag.DiagnosticsError(response.Diagnostics))
+	}
+
+	if e, a := arn, getAttributeValue(ctx, t, response.State, path.Root("arn")); e != a {
+		t.Errorf("expected `arn` to be %q, got %q", e, a)
+	}
+	if e, a := "", getAttributeValue(ctx, t, response.State, path.Root("attr")); e != a {
+		t.Errorf("expected `attr` to be %q, got %q", e, a)
+	}
+
+	if identity := response.Identity; identity == nil {
+		t.Error("Identity should be set")
+	} else {
+		var arnVal string
+		identity.GetAttribute(ctx, path.Root("arn"), &arnVal)
+		if e, a := arn, arnVal; e != a {
+			t.Errorf("expected Identity `arn` to be %q, got %q", e, a)
+		}
+	}
+}
+
+func TestImportByARN_GlobalARN_Identity_Invalid_NotAnARN(t *testing.T) {
+	ctx := context.Background()
+
+	importer := globalARNImporter()
+
+	response := importByIdentity(ctx, &importer, globalARNSchema, globalARNIdentitySchema, map[string]string{
+		"arn": "not a valid ARN",
+	})
+	if response.Diagnostics.HasError() {
+		if response.Diagnostics[0].Summary() != "Invalid Import Attribute Value" {
+			t.Fatalf("Unexpected error: %s", fwdiag.DiagnosticsError(response.Diagnostics))
+		}
+	} else {
+		t.Fatal("Expected error, got none")
+	}
+}
+
+func TestImportByARN_GlobalARN_DuplicateAttrs_ImportID_Valid(t *testing.T) {
+	ctx := context.Background()
+
+	arn := arn.ARN{
+		Partition: "aws",
+		Service:   "a-service",
+		Region:    "",
+		AccountID: "123456789012",
+		Resource:  "res-abc123",
+	}.String()
+
+	importer := globalARNImporter()
+
+	response := importByID(ctx, &importer, globalARNWithIDSchema, arn, globalARNIdentitySchema)
+	if response.Diagnostics.HasError() {
+		t.Fatalf("Unexpected error: %s", fwdiag.DiagnosticsError(response.Diagnostics))
+	}
+
+	if e, a := arn, getAttributeValue(ctx, t, response.State, path.Root("id")); e != a {
+		t.Errorf("expected `id` to be %q, got %q", e, a)
+	}
+	if e, a := arn, getAttributeValue(ctx, t, response.State, path.Root("arn")); e != a {
+		t.Errorf("expected `arn` to be %q, got %q", e, a)
+	}
+	if e, a := arn, getAttributeValue(ctx, t, response.State, path.Root("attr")); e != a {
+		t.Errorf("expected `attr` to be %q, got %q", e, a)
+	}
+
+	if identity := response.Identity; identity == nil {
+		t.Error("Identity should be set")
+	} else {
+		var arnVal string
+		identity.GetAttribute(ctx, path.Root("arn"), &arnVal)
+		if e, a := arn, arnVal; e != a {
+			t.Errorf("expected Identity `arn` to be %q, got %q", e, a)
+		}
+	}
+}
+
+func TestImportByARN_GlobalARN_DuplicateAttrs_Identity_Valid(t *testing.T) {
+	ctx := context.Background()
+
+	arn := arn.ARN{
+		Partition: "aws",
+		Service:   "a-service",
+		Region:    "",
+		AccountID: "123456789012",
+		Resource:  "res-abc123",
+	}.String()
+
+	importer := globalARNImporter()
+
+	response := importByIdentity(ctx, &importer, globalARNWithIDSchema, globalARNIdentitySchema, map[string]string{
+		"arn": arn,
+	})
+	if response.Diagnostics.HasError() {
+		t.Fatalf("Unexpected error: %s", fwdiag.DiagnosticsError(response.Diagnostics))
+	}
+
+	if e, a := arn, getAttributeValue(ctx, t, response.State, path.Root("id")); e != a {
+		t.Errorf("expected `id` to be %q, got %q", e, a)
+	}
+	if e, a := arn, getAttributeValue(ctx, t, response.State, path.Root("arn")); e != a {
+		t.Errorf("expected `arn` to be %q, got %q", e, a)
+	}
+	if e, a := arn, getAttributeValue(ctx, t, response.State, path.Root("attr")); e != a {
+		t.Errorf("expected `attr` to be %q, got %q", e, a)
+	}
+
+	if identity := response.Identity; identity == nil {
+		t.Error("Identity should be set")
+	} else {
+		var arnVal string
+		identity.GetAttribute(ctx, path.Root("arn"), &arnVal)
+		if e, a := arn, arnVal; e != a {
+			t.Errorf("expected Identity `arn` to be %q, got %q", e, a)
+		}
+	}
+}
+
+func globalARNImporter() (importer framework.WithImportByARN) {
+	importer.SetIdentitySpec(inttypes.GlobalARNIdentity())
+	return
+}
