ee07599c175a06678a11fb1669b5fdbdcb186533
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-06-10 16:39:40 -0400

    r/aws_evs_environment: More attributes.

--- internal/service/evs/environment.go
+++ internal/service/evs/environment.go
@@ -5,32 +5,46 @@ package evs
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/evs"
 	awstypes "github.com/aws/aws-sdk-go-v2/service/evs/types"
+	"github.com/hashicorp/terraform-plugin-framework-timeouts/resource/timeouts"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	sdkid "github.com/hashicorp/terraform-plugin-sdk/v2/helper/id"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+	"github.com/hashicorp/terraform-provider-aws/internal/enum"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/fwdiag"
 	"github.com/hashicorp/terraform-provider-aws/internal/framework"
 	fwflex "github.com/hashicorp/terraform-provider-aws/internal/framework/flex"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
+	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
 // @FrameworkResource("aws_evs_environment", name="Environment")
 // @Tags(identifierAttribute="environment_arn")
+// @Testing(tagsTest=false)
 func newEnvironmentResource(_ context.Context) (resource.ResourceWithConfigure, error) {
-	return &environmentResource{}, nil
+	r := &environmentResource{}
+
+	r.SetDefaultCreateTimeout(45 * time.Minute)
+	r.SetDefaultDeleteTimeout(45 * time.Minute)
+
+	return r, nil
 }
 
 type environmentResource struct {
 	framework.ResourceWithConfigure
 	framework.WithNoOpUpdate[environmentResourceModel]
+	framework.WithTimeouts
 }
 
 func (r *environmentResource) Schema(ctx context.Context, request resource.SchemaRequest, response *resource.SchemaResponse) {
@@ -38,6 +52,14 @@ func (r *environmentResource) Schema(ctx context.Context, request resource.Schem
 		Attributes: map[string]schema.Attribute{
 			"environment_arn": framework.ARNAttributeComputedOnly(),
 			"environment_id":  framework.IDAttribute(),
+			names.AttrTags:    tftags.TagsAttribute(),
+			names.AttrTagsAll: tftags.TagsAttributeComputedOnly(),
+		},
+		Blocks: map[string]schema.Block{
+			names.AttrTimeouts: timeouts.Block(ctx, timeouts.Opts{
+				Create: true,
+				Delete: true,
+			}),
 		},
 	}
 }
@@ -55,7 +77,31 @@ func (r *environmentResource) Create(ctx context.Context, request resource.Creat
 		return
 	}
 
+	// Additional fields.
+	input.ClientToken = aws.String(sdkid.UniqueId())
+	input.Tags = getTagsIn(ctx)
+
 	conn := r.Meta().EVSClient(ctx)
+
+	output, err := conn.CreateEnvironment(ctx, &input)
+
+	if err != nil {
+		response.Diagnostics.AddError("creating EVS Environment", err.Error())
+
+		return
+	}
+
+	// Set values for unknowns.
+	env := output.Environment
+	id := aws.ToString(env.EnvironmentId)
+	data.EnvironmentARN = fwflex.StringToFramework(ctx, env.EnvironmentArn)
+	data.EnvironmentID = fwflex.StringValueToFramework(ctx, id)
+
+	if _, err := waitEnvironmentCreated(ctx, conn, id, r.CreateTimeout(ctx, data.Timeouts)); err != nil {
+		response.Diagnostics.AddError(fmt.Sprintf("waiting for EVS Environment (%s) create", id), err.Error())
+
+		return
+	}
 
 	response.Diagnostics.Append(response.State.Set(ctx, data)...)
 }
@@ -99,6 +145,28 @@ func (r *environmentResource) Delete(ctx context.Context, request resource.Delet
 	}
 
 	conn := r.Meta().EVSClient(ctx)
+
+	id := fwflex.StringValueFromFramework(ctx, data.EnvironmentID)
+	input := evs.DeleteEnvironmentInput{
+		EnvironmentId: aws.String(id),
+	}
+	_, err := conn.DeleteEnvironment(ctx, &input)
+
+	if errs.IsA[*awstypes.ResourceNotFoundException](err) {
+		return
+	}
+
+	if err != nil {
+		response.Diagnostics.AddError(fmt.Sprintf("deleting EVS Environment (%s)", id), err.Error())
+
+		return
+	}
+
+	if _, err := waitEnvironmentDeleted(ctx, conn, id, r.DeleteTimeout(ctx, data.Timeouts)); err != nil {
+		response.Diagnostics.AddError(fmt.Sprintf("waiting for EVS Environment (%s) delete", id), err.Error())
+
+		return
+	}
 }
 
 func (r *environmentResource) ImportState(ctx context.Context, request resource.ImportStateRequest, response *resource.ImportStateResponse) {
@@ -127,10 +195,73 @@ func findEnvironmentByID(ctx context.Context, conn *evs.Client, id string) (*aws
 		return nil, tfresource.NewEmptyResultError(input)
 	}
 
+	if state := output.Environment.EnvironmentState; state == awstypes.EnvironmentStateDeleted {
+		return nil, &retry.NotFoundError{
+			Message: string(state),
+		}
+	}
+
 	return output.Environment, nil
 }
 
+func statusEnvironment(ctx context.Context, conn *evs.Client, id string) retry.StateRefreshFunc {
+	return func() (any, string, error) {
+		output, err := findEnvironmentByID(ctx, conn, id)
+
+		if tfresource.NotFound(err) {
+			return nil, "", nil
+		}
+
+		if err != nil {
+			return nil, "", err
+		}
+
+		return output, string(output.EnvironmentState), nil
+	}
+}
+
+func waitEnvironmentCreated(ctx context.Context, conn *evs.Client, id string, timeout time.Duration) (*awstypes.Environment, error) {
+	stateConf := &retry.StateChangeConf{
+		Pending: enum.Slice(awstypes.EnvironmentStateCreating),
+		Target:  enum.Slice(awstypes.EnvironmentStateCreated),
+		Refresh: statusEnvironment(ctx, conn, id),
+		Timeout: timeout,
+	}
+
+	outputRaw, err := stateConf.WaitForStateContext(ctx)
+
+	if output, ok := outputRaw.(*awstypes.Environment); ok {
+		tfresource.SetLastError(err, errors.New(aws.ToString(output.StateDetails)))
+
+		return output, err
+	}
+
+	return nil, err
+}
+
+func waitEnvironmentDeleted(ctx context.Context, conn *evs.Client, id string, timeout time.Duration) (*awstypes.Environment, error) {
+	stateConf := &retry.StateChangeConf{
+		Pending: enum.Slice(awstypes.EnvironmentStateDeleting),
+		Target:  []string{},
+		Refresh: statusEnvironment(ctx, conn, id),
+		Timeout: timeout,
+	}
+
+	outputRaw, err := stateConf.WaitForStateContext(ctx)
+
+	if output, ok := outputRaw.(*awstypes.Environment); ok {
+		tfresource.SetLastError(err, errors.New(aws.ToString(output.StateDetails)))
+
+		return output, err
+	}
+
+	return nil, err
+}
+
 type environmentResourceModel struct {
-	EnvironmentARN types.String `tfsdk:"environment_arn"`
-	EnvironmentID  types.String `tfsdk:"environment_id"`
+	EnvironmentARN types.String   `tfsdk:"environment_arn"`
+	EnvironmentID  types.String   `tfsdk:"environment_id"`
+	Tags           tftags.Map     `tfsdk:"tags"`
+	TagsAll        tftags.Map     `tfsdk:"tags_all"`
+	Timeouts       timeouts.Value `tfsdk:"timeouts"`
 }
