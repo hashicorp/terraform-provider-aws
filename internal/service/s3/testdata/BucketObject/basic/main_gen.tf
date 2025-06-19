91e6411f338d0ba1d62e154a4c0912eebc0e7fde
Graham Davison <gdavison@hashicorp.com>
2025-06-19 11:44:38 -0700

    `aws_s3_bucket_object`

--- internal/service/s3/testdata/BucketObject/basic/main_gen.tf
+++ internal/service/s3/testdata/BucketObject/basic/main_gen.tf
@@ -1,21 +1,25 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
 resource "aws_s3_bucket_object" "test" {
-{{- template "region" }}
   # Must have bucket versioning enabled first
   bucket = aws_s3_bucket_versioning.test.bucket
   key    = var.rName
-
-{{- template "tags" . }}
 }
 
 resource "aws_s3_bucket" "test" {
-{{- template "region" }}
   bucket = var.rName
 }
 
 resource "aws_s3_bucket_versioning" "test" {
-{{- template "region" }}
   bucket = aws_s3_bucket.test.bucket
   versioning_configuration {
     status = "Enabled"
   }
+}
+
+variable "rName" {
+  description = "Name for resource"
+  type        = string
+  nullable    = false
 }
