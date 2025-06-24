6016295a39a9671510a80b415111784b2d023c94
Simon Davis <sdavis@hashicorp.com>
2025-06-24 08:27:59 -0700

    fix region template whitespaec

--- internal/service/elbv2/testdata/ListenerRule/tagsComputed1/main_gen.tf
+++ internal/service/elbv2/testdata/ListenerRule/tagsComputed1/main_gen.tf
@@ -6,7 +6,6 @@ provider "null" {}
 resource "aws_lb_listener_rule" "test" {
   listener_arn = aws_lb_listener.test.arn
   priority     = 100
-    
 
   action {
     type             = "forward"
@@ -28,7 +27,6 @@ resource "aws_lb_listener" "test" {
   load_balancer_arn = aws_lb.test.id
   protocol          = "HTTP"
   port              = "80"
-    
 
   default_action {
     target_group_arn = aws_lb_target_group.test.id
@@ -39,7 +37,6 @@ resource "aws_lb_listener" "test" {
 resource "aws_security_group" "test" {
   name   = var.rName
   vpc_id = aws_vpc.test.id
-    
 
   ingress {
     from_port   = 0
@@ -61,7 +58,6 @@ resource "aws_lb" "test" {
   internal        = true
   security_groups = [aws_security_group.test.id]
   subnets         = aws_subnet.test[*].id
-    
 
   idle_timeout               = 30
   enable_deletion_protection = false
@@ -72,7 +68,6 @@ resource "aws_lb_target_group" "test" {
   port     = 8080
   protocol = "HTTP"
   vpc_id   = aws_vpc.test.id
-    
 
   health_check {
     path                = "/health"
