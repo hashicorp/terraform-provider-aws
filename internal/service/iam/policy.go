47e8a56e8c4333dad0d47f942d6b76a19e0c2fac
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-06-26 11:24:27 -0400

    r/aws_iam_policy: Paginated ListTags.

--- internal/service/iam/policy.go
+++ internal/service/iam/policy.go
@@ -467,13 +467,24 @@ func findPolicyVersionsByARN(ctx context.Context, conn *iam.Client, arn string) 
 	return output, nil
 }
 
-func policyTags(ctx context.Context, conn *iam.Client, identifier string) ([]awstypes.Tag, error) {
-	output, err := conn.ListPolicyTags(ctx, &iam.ListPolicyTagsInput{
+func policyTags(ctx context.Context, conn *iam.Client, identifier string, optFns ...func(*iam.Options)) ([]awstypes.Tag, error) {
+	input := iam.ListPolicyTagsInput{
 		PolicyArn: aws.String(identifier),
-	})
-	if err != nil {
-		return nil, err
+	}
+	var output []awstypes.Tag
+
+	pages := iam.NewListPolicyTagsPaginator(conn, &input)
+	for pages.HasMorePages() {
+		page, err := pages.NextPage(ctx, optFns...)
+
+		if err != nil {
+			return nil, err
+		}
+
+		for _, v := range page.Tags {
+			output = append(output, v)
+		}
 	}
 
-	return output.Tags, nil
+	return output, nil
 }
