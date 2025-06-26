c787011e00e8b9d37460b9ce39348eedb4b260cc
Jared Baker <jared.baker@hashicorp.com>
2025-06-25 20:14:23 -0400

    r/aws_bedrockagent_agent_collaborator(test): fix _multiCollaborator test config
    ```console
    % make testacc PKG=bedrockagent TESTS=TestAccBedrockAgentAgentCollaborator_multi
    make: Verifying source code with gofmt...
    ==> Checking that code complies with gofmt requirements...
    TF_ACC=1 go1.24.4 test ./internal/service/bedrockagent/... -v -count 1 -parallel 20 -run='TestAccBedrockAgentAgentCollaborator_multi'  -timeout 360m -vet=off
    2025/06/25 20:09:19 Creating Terraform AWS Provider (SDKv2-style)...
    2025/06/25 20:09:19 Initializing Terraform AWS Provider (SDKv2-style)...
    
    --- PASS: TestAccBedrockAgentAgentCollaborator_multiCollaborator (57.29s)
    PASS
    ok      github.com/hashicorp/terraform-provider-aws/internal/service/bedrockagent       63.347s
    ```

--- internal/service/bedrockagent/agent_collaborator_test.go
+++ internal/service/bedrockagent/agent_collaborator_test.go
@@ -46,7 +46,7 @@ func TestAccBedrockAgentAgentCollaborator_basic(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testAccAgentCollaboratorConfig_basic(rName, model, description, instruction),
-				Check: resource.ComposeTestCheckFunc(
+				Check: resource.ComposeAggregateTestCheckFunc(
 					testAccCheckAgentCollaboratorExists(ctx, resourceName, &agentcollaborator),
 					resource.TestCheckResourceAttrPair(resourceName, "agent_id", superResourceName, "agent_id"),
 					resource.TestCheckResourceAttr(resourceName, "collaborator_name", rName),
@@ -88,7 +88,7 @@ func TestAccBedrockAgentAgentCollaborator_update(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testAccAgentCollaboratorConfig_basic(rName, model, description, instruction),
-				Check: resource.ComposeTestCheckFunc(
+				Check: resource.ComposeAggregateTestCheckFunc(
 					testAccCheckAgentCollaboratorExists(ctx, resourceName, &agentcollaborator),
 					resource.TestCheckResourceAttr(resourceName, "collaborator_name", rName),
 					resource.TestCheckResourceAttr(resourceName, "collaboration_instruction", instruction),
@@ -103,7 +103,7 @@ func TestAccBedrockAgentAgentCollaborator_update(t *testing.T) {
 			},
 			{
 				Config: testAccAgentCollaboratorConfig_update(rName, model, description, instruction2),
-				Check: resource.ComposeTestCheckFunc(
+				Check: resource.ComposeAggregateTestCheckFunc(
 					testAccCheckAgentCollaboratorExists(ctx, resourceName, &agentcollaborator),
 					resource.TestCheckResourceAttr(resourceName, "collaborator_name", rName),
 					resource.TestCheckResourceAttr(resourceName, "collaboration_instruction", instruction2),
@@ -135,7 +135,7 @@ func TestAccBedrockAgentAgentCollaborator_disappears(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testAccAgentCollaboratorConfig_basic(rName, model, description, instruction),
-				Check: resource.ComposeTestCheckFunc(
+				Check: resource.ComposeAggregateTestCheckFunc(
 					testAccCheckAgentCollaboratorExists(ctx, resourceName, &agentcollaborator),
 					acctest.CheckFrameworkResourceDisappearsWithStateFunc(ctx, acctest.Provider, tfbedrockagent.ResourceAgentCollaborator, resourceName, agentCollaboratorDisappearsStateFunc),
 				),
@@ -151,13 +151,17 @@ func TestAccBedrockAgentAgentCollaborator_disappears(t *testing.T) {
 // Ref: https://github.com/hashicorp/terraform-provider-aws/issues/42256
 func TestAccBedrockAgentAgentCollaborator_multiCollaborator(t *testing.T) {
 	ctx := acctest.Context(t)
-	var agentcollaborator awstypes.AgentCollaborator
+	var c1, c2, c3 awstypes.AgentCollaborator
 	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
 	resourceName := "aws_bedrockagent_agent_collaborator.test"
-	aliasResourceName := "aws_bedrockagent_agent_alias.test"
+	resourceName2 := "aws_bedrockagent_agent_collaborator.test2"
+	resourceName3 := "aws_bedrockagent_agent_collaborator.test3"
 	superResourceName := "aws_bedrockagent_agent.test_super"
+	aliasResourceName := "aws_bedrockagent_agent_alias.test"
+	aliasResourceName2 := "aws_bedrockagent_agent_alias.test2"
+	aliasResourceName3 := "aws_bedrockagent_agent_alias.test3"
 
-	instruction := "tell the other agent what to do"
+	instruction := "Do what the supervisor tells you to do. Think clearly."
 	model := "anthropic.claude-3-5-sonnet-20241022-v2:0"
 	description := "basic claude"
 
@@ -171,14 +175,20 @@ func TestAccBedrockAgentAgentCollaborator_multiCollaborator(t *testing.T) {
 		CheckDestroy:             testAccCheckAgentCollaboratorDestroy(ctx),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccAgentCollaboratorConfig_basic(rName, model, description, instruction),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckAgentCollaboratorExists(ctx, resourceName, &agentcollaborator),
+				Config: testAccAgentCollaboratorConfig_multiCollaborator(rName, model, description, instruction),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccCheckAgentCollaboratorExists(ctx, resourceName, &c1),
+					testAccCheckAgentCollaboratorExists(ctx, resourceName2, &c2),
+					testAccCheckAgentCollaboratorExists(ctx, resourceName3, &c3),
 					resource.TestCheckResourceAttrPair(resourceName, "agent_id", superResourceName, "agent_id"),
-					resource.TestCheckResourceAttr(resourceName, "collaborator_name", rName),
+					resource.TestCheckResourceAttr(resourceName, "collaborator_name", rName+"-1"),
+					resource.TestCheckResourceAttr(resourceName2, "collaborator_name", rName+"-2"),
+					resource.TestCheckResourceAttr(resourceName3, "collaborator_name", rName+"-3"),
 					resource.TestCheckResourceAttr(resourceName, "collaboration_instruction", instruction),
 					resource.TestCheckResourceAttr(resourceName, "relay_conversation_history", string(awstypes.RelayConversationHistoryToCollaborator)),
 					resource.TestCheckResourceAttrPair(resourceName, "agent_descriptor.0.alias_arn", aliasResourceName, "agent_alias_arn"),
+					resource.TestCheckResourceAttrPair(resourceName2, "agent_descriptor.0.alias_arn", aliasResourceName2, "agent_alias_arn"),
+					resource.TestCheckResourceAttrPair(resourceName3, "agent_descriptor.0.alias_arn", aliasResourceName3, "agent_alias_arn"),
 				),
 			},
 			{
@@ -338,8 +348,7 @@ resource "aws_bedrockagent_agent_alias" "test_update" {
 
 func testAccAgentCollaboratorConfig_multiCollaborator(rName, model, description, instruction string) string {
 	return acctest.ConfigCompose(
-		testAccAgentConfig_basic(rName, model, description),
-		testAccAgentAliasConfig_alias(rName),
+		testAccAgent_base(rName, model),
 		fmt.Sprintf(`
 resource "aws_bedrockagent_agent" "test_super" {
   agent_collaboration         = "SUPERVISOR"
@@ -350,6 +359,21 @@ resource "aws_bedrockagent_agent" "test_super" {
   instruction                 = file("${path.module}/test-fixtures/instruction.txt")
   foundation_model            = %[2]q
   prepare_agent               = false
+}
+
+resource "aws_bedrockagent_agent" "test" {
+  agent_name                  = "%[1]s-1"
+  agent_resource_role_arn     = aws_iam_role.test_agent.arn
+  description                 = %[3]q
+  idle_session_ttl_in_seconds = 500
+  instruction                 = file("${path.module}/test-fixtures/instruction.txt")
+  foundation_model            = %[2]q
+}
+
+resource "aws_bedrockagent_agent_alias" "test" {
+  agent_alias_name = %[1]q
+  agent_id         = aws_bedrockagent_agent.test.agent_id
+  description      = "Test Alias"
 }
 
 resource "aws_bedrockagent_agent_collaborator" "test" {
@@ -363,9 +387,18 @@ resource "aws_bedrockagent_agent_collaborator" "test" {
   }
 }
 
+resource "aws_bedrockagent_agent" "test2" {
+  agent_name                  = "%[1]s-2"
+  agent_resource_role_arn     = aws_iam_role.test_agent.arn
+  description                 = %[3]q
+  idle_session_ttl_in_seconds = 500
+  instruction                 = file("${path.module}/test-fixtures/instruction.txt")
+  foundation_model            = %[2]q
+}
+
 resource "aws_bedrockagent_agent_alias" "test2" {
-  agent_alias_name = %[1]q
-  agent_id         = aws_bedrockagent_agent.test.agent_id
+  agent_alias_name = "%[1]s-2"
+  agent_id         = aws_bedrockagent_agent.test2.agent_id
   description      = "Test Alias 2"
 }
 
@@ -380,9 +413,18 @@ resource "aws_bedrockagent_agent_collaborator" "test2" {
   }
 }
 
+resource "aws_bedrockagent_agent" "test3" {
+  agent_name                  = "%[1]s-3"
+  agent_resource_role_arn     = aws_iam_role.test_agent.arn
+  description                 = %[3]q
+  idle_session_ttl_in_seconds = 500
+  instruction                 = file("${path.module}/test-fixtures/instruction.txt")
+  foundation_model            = %[2]q
+}
+
 resource "aws_bedrockagent_agent_alias" "test3" {
-  agent_alias_name = %[1]q
-  agent_id         = aws_bedrockagent_agent.test.agent_id
+  agent_alias_name = "%[1]s-3"
+  agent_id         = aws_bedrockagent_agent.test3.agent_id
   description      = "Test Alias 3"
 }
 
