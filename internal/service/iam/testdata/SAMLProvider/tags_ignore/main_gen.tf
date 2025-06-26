400e1eb4fa11614bdaced1241f01566a0495ec30
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-06-26 19:31:33 -0400

    r/aws_iam_saml_provider: Try to fix tflint 'aws_iam_saml_provider_invalid_saml_metadata_document' again.

--- internal/service/iam/testdata/SAMLProvider/tags_ignore/main_gen.tf
+++ internal/service/iam/testdata/SAMLProvider/tags_ignore/main_gen.tf
@@ -13,7 +13,7 @@ provider "aws" {
 resource "aws_iam_saml_provider" "test" {
   name = var.rName
 
-  saml_metadata_document = templatefile("test-fixtures/saml-metadata.xml.tpl", { entity_id = "https://example.com" })
+  saml_metadata_document = templatefile("${path.root}/test-fixtures/saml-metadata.xml.tpl", { entity_id = "https://example.com" })
 
   tags = var.resource_tags
 }
