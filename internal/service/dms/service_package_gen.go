2fbc99dcfb13432e46383217e33b62ac5ce02d89
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-06-30 15:54:57 -0400

    r/aws_dms_replication_config: Add '@V60SDKv2Fix' attribute.

--- internal/service/dms/service_package_gen.go
+++ internal/service/dms/service_package_gen.go
@@ -114,7 +114,9 @@ func (p *servicePackage) SDKResources(ctx context.Context) []*inttypes.ServicePa
 			}),
 			Region: unique.Make(inttypes.ResourceRegionDefault()),
 			Identity: inttypes.RegionalARNIdentity(
-				inttypes.WithIdentityDuplicateAttrs(names.AttrID)),
+				inttypes.WithIdentityDuplicateAttrs(names.AttrID),
+				inttypes.WithV6_0SDKv2Fix(),
+			),
 			Import: inttypes.SDKv2Import{
 				WrappedImport: true,
 			},
