f064b33959598a43edc17705c504e6d3d9768ebd
Graham Davison <gdavison@hashicorp.com>
2025-07-03 22:05:08 -0700

    `terrafmt`

--- internal/service/redshift/cluster_iam_roles_test.go
+++ internal/service/redshift/cluster_iam_roles_test.go
@@ -132,13 +132,13 @@ EOF
 }
 
 resource "aws_redshift_cluster" "test" {
-  cluster_identifier                  = %[1]q
-  database_name                       = "mydb"
-  master_username                     = "foo_test"
-  master_password                     = "Mustbe8characters"
-  node_type                           = "ra3.large"
-  allow_version_upgrade               = false
-  skip_final_snapshot                 = true
+  cluster_identifier    = %[1]q
+  database_name         = "mydb"
+  master_username       = "foo_test"
+  master_password       = "Mustbe8characters"
+  node_type             = "ra3.large"
+  allow_version_upgrade = false
+  skip_final_snapshot   = true
 }
 `, rName)
 }
