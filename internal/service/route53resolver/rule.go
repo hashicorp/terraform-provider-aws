1954c1c47b6722935a65d94179e65633bdaec1f5
Stefan Freitag <stefan.freitag@udo.edu>
2025-07-09 20:36:34 +0200

    fix: add ForceNew to resource argument

--- internal/service/route53resolver/rule.go
+++ internal/service/route53resolver/rule.go
@@ -54,6 +54,7 @@ func resourceRule() *schema.Resource {
 			"delegation_record": {
 				Type:          schema.TypeString,
 				Optional:      true,
+				ForceNew:      true,
 				ValidateFunc:  validation.StringLenBetween(1, 256),
 				ConflictsWith: []string{names.AttrDomainName},
 				StateFunc:     trimTrailingPeriod,
@@ -434,8 +435,8 @@ func flattenRuleTargetIPs(targetAddresses []awstypes.TargetAddress) []any {
 }
 
 // trimTrailingPeriod is used to remove the trailing period
-// of "name", "domain name" or "delegation_record" attributes often returned from
-// the Route53 API or provided as user input.
+// of "name", "domain name" or "delegation_record" attributes
+// often returned from the Route53 API or provided as user input.
 // The single dot (".") domain name is returned as-is.
 func trimTrailingPeriod(v any) string {
 	var str string
