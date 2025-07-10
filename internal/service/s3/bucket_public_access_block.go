8d4bc5f7a351deafeb7fe0c4ae07557cc74a4afe
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-07-10 12:25:02 -0400

    Add 'context.Context' arg to function called by 'tfresource.RetryUntilNotFound' - s3.

--- internal/service/s3/bucket_public_access_block.go
+++ internal/service/s3/bucket_public_access_block.go
@@ -195,7 +195,7 @@ func resourceBucketPublicAccessBlockDelete(ctx context.Context, d *schema.Resour
 		return sdkdiag.AppendErrorf(diags, "deleting S3 Bucket Public Access Block (%s): %s", d.Id(), err)
 	}
 
-	_, err = tfresource.RetryUntilNotFound(ctx, bucketPropagationTimeout, func() (any, error) {
+	_, err = tfresource.RetryUntilNotFound(ctx, bucketPropagationTimeout, func(ctx context.Context) (any, error) {
 		return findPublicAccessBlockConfiguration(ctx, conn, bucket)
 	})
 
