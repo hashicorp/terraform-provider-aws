7dce8f1414fc6c409c9973f712a7913c39c1bef7
Jared Baker <jared.baker@hashicorp.com>
2025-07-16 16:51:20 -0400

    r/aws_globalaccelerator_custom_routing_accelerator: use var.rName in template
    The acceptance test results are as follows:
    
    --- PASS: TestAccGlobalAcceleratorCustomRoutingAccelerator_basic (167.52s)
    --- PASS: TestAccGlobalAcceleratorCustomRoutingAccelerator_disappears (174.18s)
    --- PASS: TestAccGlobalAcceleratorCustomRoutingAccelerator_Identity_Basic (185.20s)
    --- PASS: TestAccGlobalAcceleratorCustomRoutingAccelerator_tags (185.66s)
    --- PASS: TestAccGlobalAcceleratorCustomRoutingAccelerator_Identity_ExistingResource (191.36s)
    --- PASS: TestAccGlobalAcceleratorCustomRoutingAccelerator_update (230.57s)

--- internal/service/globalaccelerator/testdata/CustomRoutingAccelerator/basic/main_gen.tf
+++ internal/service/globalaccelerator/testdata/CustomRoutingAccelerator/basic/main_gen.tf
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: MPL-2.0
 
 resource "aws_globalaccelerator_custom_routing_accelerator" "test" {
-  name = "tf-acc-test-custom-routing-accelerator"
+  name = var.rName
 }
 
 variable "rName" {
