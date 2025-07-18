a68b1c78e088652d56c244196bafb9379c849d82
Jared Baker <jared.baker@hashicorp.com>
2025-07-18 16:02:24 -0400

    r/aws_glue_schema: fix template formatting

--- internal/service/glue/testdata/Schema/basic/main_gen.tf
+++ internal/service/glue/testdata/Schema/basic/main_gen.tf
@@ -4,8 +4,8 @@
 resource "aws_glue_schema" "test" {
   schema_name       = var.rName
   schema_definition = "{\"type\": \"record\", \"name\": \"r1\", \"fields\": [ {\"name\": \"f1\", \"type\": \"int\"}, {\"name\": \"f2\", \"type\": \"string\"} ]}"
-  data_format      = "AVRO"
-  compatibility    = "NONE"
+  data_format       = "AVRO"
+  compatibility     = "NONE"
 }
 
 variable "rName" {
