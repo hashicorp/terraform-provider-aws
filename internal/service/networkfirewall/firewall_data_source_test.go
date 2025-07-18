47cac56f58483d8e327a1382cc9a2d2e0388d3e2
Alex Bacchin <alexbacchin@outlook.com>
2025-07-18 22:02:38 +1000

    fixed documentation and data source testing

--- internal/service/networkfirewall/firewall_data_source_test.go
+++ internal/service/networkfirewall/firewall_data_source_test.go
@@ -167,6 +167,7 @@ func TestAccNetworkFirewallFirewallDataSource_transitGatewayAttachment(t *testin
 	resourceName := "aws_networkfirewall_firewall.test"
 	dataSourceName := "data.aws_networkfirewall_firewall.test"
 	dataSourceCallerIdentity := "data.aws_caller_identity.current"
+	dataSourceAvailabilityZones := "data.aws_availability_zones.available"
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.PreCheck(ctx, t); testAccPreCheck(ctx, t) },
@@ -181,7 +182,8 @@ func TestAccNetworkFirewallFirewallDataSource_transitGatewayAttachment(t *testin
 					resource.TestCheckResourceAttr(dataSourceName, "firewall_status.0.status", "READY"),
 					resource.TestCheckResourceAttr(dataSourceName, "firewall_status.0.transit_gateway_attachment_sync_state.0.transit_gateway_attachment_status", "READY"),
 					resource.TestCheckResourceAttr(dataSourceName, "availability_zone_change_protection", acctest.CtFalse),
-					resource.TestCheckResourceAttrSet(dataSourceName, "firewall_status.0.transit_gateway_attachment_sync_state.*.attachment_id"),
+					resource.TestCheckTypeSetElemAttrPair(dataSourceName, "availability_zone_mapping.0.availability_zone_id", dataSourceAvailabilityZones, "zone_ids.0"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "firewall_status.0.transit_gateway_attachment_sync_state.0.attachment_id"),
 					resource.TestCheckResourceAttrPair(dataSourceName, "transit_gateway_owner_account_id", dataSourceCallerIdentity, names.AttrAccountID),
 				),
 			},
--- website/docs/r/networkfirewall_network_firewall_transit_gateway_attachment_accepter.html.markdown
+++ website/docs/r/networkfirewall_network_firewall_transit_gateway_attachment_accepter.html.markdown
@@ -29,10 +29,10 @@ A full example of how to create a Transit Gateway in one AWS account, share it w
 
 ## Argument Reference
 
-The following arguments are required:
+This resource supports the following arguments:
 
 * `region` - (Optional) Region where this resource will be [managed](https://docs.aws.amazon.com/general/latest/gr/rande.html#regional-endpoints). Defaults to the Region set in the [provider configuration](https://registry.terraform.io/providers/hashicorp/aws/latest/docs#aws-configuration-reference).
-* `transit_gateway_attachment_id` - (Required) The unique identifier of the transit gateway attachment to accept. This ID is returned in the response when creating a transit gateway-attached firewall..
+* `transit_gateway_attachment_id` - (Required) The unique identifier of the transit gateway attachment to accept. This ID is returned in the response when creating a transit gateway-attached firewall.
 
 ## Attribute Reference
 
