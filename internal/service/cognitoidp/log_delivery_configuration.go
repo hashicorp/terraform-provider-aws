bce506ec578e8440dcb6575c1e6f6845b3f2c2fd
ThomasZalewski <thomas@thomaszwskismbp.lan>
2025-07-18 13:43:00 -0700

    skip test

--- internal/service/cognitoidp/log_delivery_configuration.go
+++ internal/service/cognitoidp/log_delivery_configuration.go
@@ -33,7 +33,7 @@ import (
 // @Testing(existsType="github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider/types;awstypes;awstypes.LogDeliveryConfigurationType")
 // @Testing(importStateIdFunc="testAccLogDeliveryConfigurationImportStateIdFunc")
 // @Testing(importStateIdAttribute="user_pool_id")
-// @Testing(preIdentityVersion="")
+// @Testing(preIdentityVersion="v6.3.0")
 func newLogDeliveryConfigurationResource(context.Context) (resource.ResourceWithConfigure, error) {
 	r := &LogDeliveryConfigurationResource{}
 	return r, nil
