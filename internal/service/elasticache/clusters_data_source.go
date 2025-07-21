0662e00d15cc1346e7ca8d9ebb4f13def9eb825a
Gavin Williams <gavin.williams@getchip.uk>
2025-07-21 19:43:31 +0100

    feat: Add new `aws_elasticache_clusters` data source
    This commit adds a new `aws_elasticache_clusters` data source to allow
    retieveing a list of Elasticache clusters matching the provided filters.
    
    Related: #34343

---
+++ internal/service/elasticache/clusters_data_source.go
@@ -0,0 +1,75 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package elasticache
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	awstypes "github.com/aws/aws-sdk-go-v2/service/elasticache/types"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/errs"
+	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
+	"github.com/hashicorp/terraform-provider-aws/internal/namevaluesfilters"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
+	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
+	"github.com/hashicorp/terraform-provider-aws/names"
+)
+
+// @SDKDataSource("aws_elasticache_clusters", name="Clusters")
+func dataSourceClusters() *schema.Resource {
+	return &schema.Resource{
+		ReadWithoutTimeout: dataSourceClustersRead,
+
+		Schema: map[string]*schema.Schema{
+			"cluster_arns": {
+				Type:     schema.TypeSet,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"cluster_identifiers": {
+				Type:     schema.TypeSet,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			names.AttrFilter: namevaluesfilters.Schema(),
+		},
+	}
+}
+
+func dataSourceClustersRead(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
+	var diags diag.Diagnostics
+	conn := meta.(*conns.AWSClient).ElastiCacheClient(ctx)
+
+	input := &elasticache.DescribeCacheClustersInput{}
+
+	if v, ok := d.GetOk(names.AttrFilter); ok {
+		input.Filters = namevaluesfilters.New(v.(*schema.Set)).ElastiCacheFilters()
+	}
+
+	clusters, err := findCacheClusters(ctx, conn, input, tfslices.PredicateTrue[*awstypes.CacheCluster])
+
+	if err != nil {
+		return sdkdiag.AppendErrorf(diags, "reading Elasticache Clusters: %s", err)
+	}
+
+	var clusterARNs []string
+	var cluterIdentifiers []string
+
+	for _, cluster := range clusters {
+		clusterARNs = append(clusterARNs, aws.ToString(cluster.ARN))
+		clusterIdentifiers = append(clusterIdentifiers, aws.ToString(cluster.CacheClusterId))
+	}
+
+	d.SetId(meta.(*conns.AWSClient).Region(ctx))
+	d.Set("cluster_arns", clusterARNs)
+	d.Set("cluster_identifiers", clusterIdentifiers)
+
+	return diags
+}
