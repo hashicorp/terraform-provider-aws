2365295cbac694fab67533d07812aba1dd679761
Asim <asim.kumar.biswal@oracle.com>
2025-07-29 10:58:32 +0100

    added examples of AVMC with all param and VMC with all param

--- examples/odb/exadata_infra.tf
+++ examples/odb/exadata_infra.tf
@@ -1,25 +1,25 @@
 //Copyright © 2025, Oracle and/or its affiliates. All rights reserved.
 
 //Exadata Infrastructure with customer managed maintenance window
-resource "aws_odb_cloud_exadata_infrastructure" "test" {
-  display_name         = "Ofake_odb_exadata_infra" //Required Field
-  shape                = "Exadata.X11M"  //Required Field
-  storage_count        = 3
-  compute_count        = 2
-  availability_zone_id = "use1-az6" //Required Field
-  customer_contacts_to_send_to_oci = ["abc@example.com"]
-  database_server_type = "X11M"
-  storage_server_type  = "X11M-HC"
+resource "aws_odb_cloud_exadata_infrastructure" "exa_infra_X11M_all_param" {
+  display_name                        = "Ofake_my_odb_exadata_infra" //Required Field
+  shape                               = "Exadata.X11M"  //Required Field
+  storage_count                       = 3
+  compute_count                       = 2
+  availability_zone_id                = "use1-az6" //Required Field
+  customer_contacts_to_send_to_oci    = ["abc@example.com"]
+  database_server_type                = "X11M"
+  storage_server_type                 = "X11M-HC"
   maintenance_window = { //Required
-    custom_action_timeout_in_mins    = 16
-    days_of_week = ["MONDAY", "TUESDAY"]
-    hours_of_day = [11, 16]
-    is_custom_action_timeout_enabled = true
-    lead_time_in_weeks               = 3
-    months = ["FEBRUARY", "MAY", "AUGUST", "NOVEMBER"]
-    patching_mode                    = "ROLLING"
-    preference                       = "CUSTOM_PREFERENCE"
-    weeks_of_month = [2, 4]
+    custom_action_timeout_in_mins     = 16
+    days_of_week                      = ["MONDAY", "TUESDAY"]
+    hours_of_day                      = [11, 16]
+    is_custom_action_timeout_enabled  = true
+    lead_time_in_weeks                = 3
+    months                            = ["FEBRUARY", "MAY", "AUGUST", "NOVEMBER"]
+    patching_mode                     = "ROLLING"
+    preference                        = "CUSTOM_PREFERENCE"
+    weeks_of_month                    = [2, 4]
   }
   tags = {
     "env" = "dev"
@@ -27,4 +27,22 @@ resource "aws_odb_cloud_exadata_infrastructure" "test" {
 
 }
 
-//Exadata Infrastructure with  default maintenance window
+//Exadata Infrastructure with  default maintenance window with X9M system shape. with minimum parameters
+resource "aws_odb_cloud_exadata_infrastructure" "test_X9M" {
+  display_name          = "Ofake_my_exa_X9M"
+  shape             	= "Exadata.X9M"
+  storage_count      	= 3
+  compute_count         = 2
+  availability_zone_id 	= "use1-az6"
+  maintenance_window = {
+      custom_action_timeout_in_mins     = 16
+      days_of_week                      = []
+      hours_of_day                      = []
+      is_custom_action_timeout_enabled  = true
+      lead_time_in_weeks                = 0
+      months                            = []
+      patching_mode                     = "ROLLING"
+      preference                        = "NO_PREFERENCE"
+      weeks_of_month                    = []
+    }
+}
--- examples/odb/odb_network_peering.tf
+++ examples/odb/odb_network_peering.tf
@@ -2,6 +2,6 @@
 
 resource "aws_odb_network_peering_connection" "test" {
   display_name    = "my_odb_net_peering"
-  odb_network_id  = aws_odb_network.test.id
-  peer_network_id = "vpc_id"
+  odb_network_id  = "<aws_odb_network.test.id">
+  peer_network_id = "<vpc_id>"
 }
