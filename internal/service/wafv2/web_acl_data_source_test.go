50813e8ac6b00f78afeb678838af3fe838ceaf51
Dirk Avery <dirk.avery@gmail.com>
2025-07-29 15:46:48 -0400

    Tests for new argument

--- internal/service/wafv2/web_acl_data_source_test.go
+++ internal/service/wafv2/web_acl_data_source_test.go
@@ -44,10 +44,53 @@ func TestAccWAFV2WebACLDataSource_basic(t *testing.T) {
 	})
 }
 
+func TestAccWAFV2WebACLDataSource_resource(t *testing.T) {
+	ctx := acctest.Context(t)
+	name := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	resourceName := "aws_wafv2_web_acl.test"
+	datasourceName := "data.aws_wafv2_web_acl.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(ctx, t); testAccPreCheckScopeRegional(ctx, t) },
+		ErrorCheck:               acctest.ErrorCheck(t, names.WAFV2ServiceID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccWebACLDataSourceConfig_resource(name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrPair(datasourceName, names.AttrARN, resourceName, names.AttrARN),
+					acctest.MatchResourceAttrRegionalARN(ctx, datasourceName, names.AttrARN, "wafv2", regexache.MustCompile(fmt.Sprintf("regional/webacl/%v/.+$", name))),
+					resource.TestCheckResourceAttrPair(datasourceName, names.AttrDescription, resourceName, names.AttrDescription),
+					resource.TestCheckResourceAttrPair(datasourceName, names.AttrID, resourceName, names.AttrID),
+					resource.TestCheckResourceAttrPair(datasourceName, names.AttrName, resourceName, names.AttrName),
+					resource.TestCheckResourceAttrPair(datasourceName, names.AttrScope, resourceName, names.AttrScope),
+				),
+			},
+		},
+	})
+}
+
+func TestAccWAFV2WebACLDataSource_resourceNotFound(t *testing.T) {
+	ctx := acctest.Context(t)
+	name := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(ctx, t); testAccPreCheckScopeRegional(ctx, t) },
+		ErrorCheck:               acctest.ErrorCheck(t, names.WAFV2ServiceID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config:      testAccWebACLDataSourceConfig_resourceNotFound(name),
+				ExpectError: regexache.MustCompile(`WAFv2 WebACL not found for`),
+			},
+		},
+	})
+}
+
 func testAccWebACLDataSourceConfig_name(name string) string {
 	return fmt.Sprintf(`
 resource "aws_wafv2_web_acl" "test" {
-  name  = "%s"
+  name  = %[1]q
   scope = "REGIONAL"
 
   default_action {
@@ -71,7 +114,7 @@ data "aws_wafv2_web_acl" "test" {
 func testAccWebACLDataSourceConfig_nonExistent(name string) string {
 	return fmt.Sprintf(`
 resource "aws_wafv2_web_acl" "test" {
-  name  = "%s"
+  name  = %[1]q
   scope = "REGIONAL"
 
   default_action {
@@ -88,6 +131,138 @@ resource "aws_wafv2_web_acl" "test" {
 data "aws_wafv2_web_acl" "test" {
   name  = "tf-acc-test-does-not-exist"
   scope = "REGIONAL"
+}
+`, name)
+}
+
+func testAccWebACLDataSourceConfig_resource(name string) string {
+	return fmt.Sprintf(`
+resource "aws_lb" "test" {
+  name               = %[1]q
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = aws_subnet.test[*].id
+
+  enable_deletion_protection = false
+}
+
+resource "aws_vpc" "test" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = %[1]q
+  }
+}
+
+resource "aws_subnet" "test" {
+  count = 2
+
+  vpc_id            = aws_vpc.test.id
+  cidr_block        = "10.0.${count.index}.0/24"
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "%[1]s-${count.index}"
+  }
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+resource "aws_internet_gateway" "test" {
+  vpc_id = aws_vpc.test.id
+
+  tags = {
+    Name = %[1]q
+  }
+}
+
+resource "aws_wafv2_web_acl" "test" {
+  name  = %[1]q
+  scope = "REGIONAL"
+
+  default_action {
+    block {}
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = false
+    metric_name                = "friendly-rule-metric-name"
+    sampled_requests_enabled   = false
+  }
+}
+
+resource "aws_wafv2_web_acl_association" "test" {
+  resource_arn = aws_lb.test.arn
+  web_acl_arn  = aws_wafv2_web_acl.test.arn
+}
+
+data "aws_wafv2_web_acl" "test" {
+  resource = aws_lb.test.arn
+  scope    = "REGIONAL"
+
+  depends_on = [aws_wafv2_web_acl_association.test]
+}
+`, name)
+}
+
+func testAccWebACLDataSourceConfig_resourceNotFound(name string) string {
+	return fmt.Sprintf(`
+resource "aws_lb" "test" {
+  name               = %[1]q
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = aws_subnet.test[*].id
+
+  enable_deletion_protection = false
+}
+
+resource "aws_vpc" "test" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = %[1]q
+  }
+}
+
+resource "aws_subnet" "test" {
+  count = 2
+
+  vpc_id            = aws_vpc.test.id
+  cidr_block        = "10.0.${count.index}.0/24"
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "%[1]s-${count.index}"
+  }
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+resource "aws_internet_gateway" "test" {
+  vpc_id = aws_vpc.test.id
+
+  tags = {
+    Name = %[1]q
+  }
+}
+
+data "aws_wafv2_web_acl" "test" {
+  resource = aws_lb.test.arn
+  scope    = "REGIONAL"
 }
 `, name)
 }
