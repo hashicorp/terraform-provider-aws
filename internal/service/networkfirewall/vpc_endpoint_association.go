39d037967fba38bfa48a1a08241db4e5280a4c2a
Alex Bacchin <alexbacchin@outlook.com>
2025-07-30 23:24:14 +1000

    WIP

---
+++ internal/service/networkfirewall/vpc_endpoint_association.go
@@ -0,0 +1,437 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package networkfirewall
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/networkfirewall"
+	awstypes "github.com/aws/aws-sdk-go-v2/service/networkfirewall/types"
+	"github.com/hashicorp/terraform-plugin-framework-timeouts/resource/timeouts"
+	"github.com/hashicorp/terraform-plugin-framework-validators/listvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+	"github.com/hashicorp/terraform-provider-aws/internal/create"
+	"github.com/hashicorp/terraform-provider-aws/internal/enum"
+	"github.com/hashicorp/terraform-provider-aws/internal/errs"
+	"github.com/hashicorp/terraform-provider-aws/internal/errs/fwdiag"
+	"github.com/hashicorp/terraform-provider-aws/internal/framework"
+	"github.com/hashicorp/terraform-provider-aws/internal/framework/flex"
+	fwtypes "github.com/hashicorp/terraform-provider-aws/internal/framework/types"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
+	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
+	"github.com/hashicorp/terraform-provider-aws/names"
+)
+
+// @FrameworkResource("aws_networkfirewall_vpc_endpoint_association", name="VPC Endpoint Association")
+// @Tags(identifierAttribute="firewall_arn")
+func newVPCEndpointAssociationResource(_ context.Context) (resource.ResourceWithConfigure, error) {
+	r := &resourceVPCEndpointAssociation{}
+
+	r.SetDefaultCreateTimeout(30 * time.Minute)
+	r.SetDefaultDeleteTimeout(30 * time.Minute)
+
+	return r, nil
+}
+
+const (
+	ResNameVPCEndpointAssociation = "VPC Endpoint Association"
+)
+
+type resourceVPCEndpointAssociation struct {
+	framework.ResourceWithModel[resourceVPCEndpointAssociationModel]
+	framework.WithTimeouts
+	framework.WithNoUpdate
+}
+
+func (r *resourceVPCEndpointAssociation) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			names.AttrARN: framework.ARNAttributeComputedOnly(),
+			names.AttrDescription: schema.StringAttribute{
+				Optional: true,
+			},
+			"firewall_arn": schema.StringAttribute{
+				Required: true,
+			},
+			names.AttrID: framework.IDAttributeDeprecatedWithAlternate(path.Root(names.AttrARN)),
+			names.AttrVPCID: schema.StringAttribute{
+				Required: true,
+			},
+			"vpc_endpoint_association_id": schema.StringAttribute{
+				Computed: true,
+			},
+			names.AttrTags:    tftags.TagsAttribute(),
+			names.AttrTagsAll: tftags.TagsAttributeComputedOnly(),
+		},
+		Blocks: map[string]schema.Block{
+			"subnet_mapping": schema.ListNestedBlock{
+				CustomType:  fwtypes.NewListNestedObjectTypeOf[subnetMappingModel](ctx),
+				Description: "A list of subnet mappings for the VPC endpoint association.",
+				Validators: []validator.List{
+					listvalidator.SizeAtMost(1),
+					listvalidator.SizeAtLeast(1),
+				},
+				NestedObject: schema.NestedBlockObject{
+					Attributes: map[string]schema.Attribute{
+						names.AttrIPAddressType: schema.StringAttribute{
+							CustomType: fwtypes.StringEnumType[awstypes.IPAddressType](),
+							Optional:   true,
+							Computed:   true,
+							PlanModifiers: []planmodifier.String{
+								stringplanmodifier.UseStateForUnknown(),
+							},
+						},
+						names.AttrSubnetID: schema.StringAttribute{
+							Required: true,
+						},
+					},
+				},
+			},
+			"vpc_endpoint_association_status": schema.ListNestedBlock{
+				CustomType: fwtypes.NewListNestedObjectTypeOf[vpcEndpointAssociationStatusModel](ctx),
+				Validators: []validator.List{
+					listvalidator.SizeAtMost(1),
+				},
+				NestedObject: schema.NestedBlockObject{
+					Attributes: map[string]schema.Attribute{
+						names.AttrStatus: schema.StringAttribute{
+							Computed: true,
+						},
+					},
+					Blocks: map[string]schema.Block{
+						"association_sync_state": schema.SetNestedBlock{
+							CustomType: fwtypes.NewSetNestedObjectTypeOf[AZSyncStateModel](ctx),
+							NestedObject: schema.NestedBlockObject{
+								Attributes: map[string]schema.Attribute{
+									names.AttrAvailabilityZone: schema.StringAttribute{
+										Computed: true,
+									},
+								},
+								Blocks: map[string]schema.Block{
+									"attachment": schema.ListNestedBlock{
+										CustomType: fwtypes.NewListNestedObjectTypeOf[attachmentModel](ctx),
+										NestedObject: schema.NestedBlockObject{
+											Attributes: map[string]schema.Attribute{
+												"endpoint_id": schema.StringAttribute{
+													Computed: true,
+												},
+												names.AttrSubnetID: schema.StringAttribute{
+													Computed: true,
+												},
+												names.AttrStatus: schema.StringAttribute{
+													Computed: true,
+												},
+												names.AttrStatusMessage: schema.StringAttribute{
+													Computed: true,
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			names.AttrTimeouts: timeouts.Block(ctx, timeouts.Opts{
+				Create: true,
+				Delete: true,
+			}),
+		},
+	}
+}
+
+func (r *resourceVPCEndpointAssociation) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	conn := r.Meta().NetworkFirewallClient(ctx)
+
+	var plan resourceVPCEndpointAssociationModel
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	planVpcEndpointAssociationStatusModel, diags := plan.VpcEndpointAssociationStatus.ToPtr(ctx)
+	// "VpcEndpointAssociationStatusModel.AZSyncState" cannot be handled by AutoFlex, since the parameter in the AWS API is a map
+	resp.Diagnostics.Append(diags...)
+	if diags.HasError() {
+		return
+	}
+
+	var input networkfirewall.CreateVpcEndpointAssociationInput
+	resp.Diagnostics.Append(flex.Expand(ctx, plan, &input, flex.WithIgnoredFieldNamesAppend("VpcEndpointAssociationStatus"))...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+	input.Tags = getTagsIn(ctx)
+	out, err := conn.CreateVpcEndpointAssociation(ctx, &input)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			create.ProblemStandardMessage(names.NetworkFirewall, create.ErrActionCreating, ResNameVPCEndpointAssociation, "", err),
+			err.Error(),
+		)
+		return
+	}
+	if out == nil || out.VpcEndpointAssociation == nil {
+		resp.Diagnostics.AddError(
+			create.ProblemStandardMessage(names.NetworkFirewall, create.ErrActionCreating, ResNameVPCEndpointAssociation, "", nil),
+			errors.New("empty output").Error(),
+		)
+		return
+	}
+	arn := aws.ToString(out.VpcEndpointAssociation.VpcEndpointAssociationArn)
+	resp.Diagnostics.Append(flex.Flatten(ctx, out.VpcEndpointAssociation, &plan, flex.WithIgnoredFieldNamesAppend("VpcEndpointAssociationStatus"))...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+	plan.VpcEndpointAssociationId = flex.StringToFramework(ctx, out.VpcEndpointAssociation.VpcEndpointAssociationId)
+	plan.VpcEndpointAssociationArn = flex.StringValueToFramework(ctx, arn)
+	plan.setID()
+
+	createTimeout := r.CreateTimeout(ctx, plan.Timeouts)
+
+	created, err := waitVPCEndpointAssociationCreated(ctx, conn, arn, createTimeout)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			create.ProblemStandardMessage(names.NetworkFirewall, create.ErrActionWaitingForCreation, ResNameVPCEndpointAssociation, arn, err),
+			err.Error(),
+		)
+		return
+	}
+
+	plan.flattenVpcEndpointAssociationStatus(ctx, created.VpcEndpointAssociationStatus)
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, plan)...)
+}
+
+func (r *resourceVPCEndpointAssociation) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	conn := r.Meta().NetworkFirewallClient(ctx)
+
+	var state resourceVPCEndpointAssociationModel
+	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+	out, err := findVPCEndpointAssociationByID(ctx, conn, state.ID.ValueString())
+
+	if tfresource.NotFound(err) {
+		resp.Diagnostics.Append(fwdiag.NewResourceNotFoundWarningDiagnostic(err))
+		resp.State.RemoveResource(ctx)
+		return
+	}
+	if err != nil {
+		resp.Diagnostics.AddError(
+			create.ProblemStandardMessage(names.NetworkFirewall, create.ErrActionReading, ResNameVPCEndpointAssociation, state.ID.String(), err),
+			err.Error(),
+		)
+		return
+	}
+
+	resp.Diagnostics.Append(flex.Flatten(ctx, out.VpcEndpointAssociation, &state, flex.WithIgnoredFieldNamesAppend("VpcEndpointAssociationStatus"))...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	state.flattenVpcEndpointAssociationStatus(ctx, out.VpcEndpointAssociationStatus)
+
+	setTagsOut(ctx, out.VpcEndpointAssociation.Tags)
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &state)...)
+}
+
+func (r *resourceVPCEndpointAssociation) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	conn := r.Meta().NetworkFirewallClient(ctx)
+
+	var state resourceVPCEndpointAssociationModel
+	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	input := networkfirewall.DeleteVpcEndpointAssociationInput{
+		VpcEndpointAssociationArn: state.ID.ValueStringPointer(),
+	}
+
+	_, err := conn.DeleteVpcEndpointAssociation(ctx, &input)
+	if err != nil {
+		if errs.IsA[*awstypes.ResourceNotFoundException](err) {
+			return
+		}
+
+		resp.Diagnostics.AddError(
+			create.ProblemStandardMessage(names.NetworkFirewall, create.ErrActionDeleting, ResNameVPCEndpointAssociation, state.ID.String(), err),
+			err.Error(),
+		)
+		return
+	}
+
+	deleteTimeout := r.DeleteTimeout(ctx, state.Timeouts)
+	_, err = waitVPCEndpointAssociationDeleted(ctx, conn, state.ID.ValueString(), deleteTimeout)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			create.ProblemStandardMessage(names.NetworkFirewall, create.ErrActionWaitingForDeletion, ResNameVPCEndpointAssociation, state.ID.String(), err),
+			err.Error(),
+		)
+		return
+	}
+}
+
+func (r *resourceVPCEndpointAssociation) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	resource.ImportStatePassthroughID(ctx, path.Root(names.AttrID), req, resp)
+}
+
+func waitVPCEndpointAssociationCreated(ctx context.Context, conn *networkfirewall.Client, arn string, timeout time.Duration) (*networkfirewall.DescribeVpcEndpointAssociationOutput, error) {
+	stateConf := &retry.StateChangeConf{
+		Pending:                   enum.Slice(awstypes.FirewallStatusValueProvisioning),
+		Target:                    enum.Slice(awstypes.FirewallStatusValueReady),
+		Refresh:                   statusVPCEndpointAssociation(ctx, conn, arn),
+		Timeout:                   timeout,
+		NotFoundChecks:            20,
+		ContinuousTargetOccurence: 2,
+	}
+
+	outputRaw, err := stateConf.WaitForStateContext(ctx)
+	if out, ok := outputRaw.(*networkfirewall.DescribeVpcEndpointAssociationOutput); ok {
+		return out, err
+	}
+
+	return nil, err
+}
+
+func waitVPCEndpointAssociationDeleted(ctx context.Context, conn *networkfirewall.Client, arn string, timeout time.Duration) (*networkfirewall.DescribeVpcEndpointAssociationOutput, error) {
+	stateConf := &retry.StateChangeConf{
+		Pending:                   enum.Slice(awstypes.FirewallStatusValueReady, awstypes.FirewallStatusValueDeleting),
+		Target:                    []string{},
+		Refresh:                   statusVPCEndpointAssociation(ctx, conn, arn),
+		Timeout:                   timeout,
+		ContinuousTargetOccurence: 2,
+	}
+
+	outputRaw, err := stateConf.WaitForStateContext(ctx)
+	if out, ok := outputRaw.(*networkfirewall.DescribeVpcEndpointAssociationOutput); ok {
+		return out, err
+	}
+
+	return nil, err
+}
+
+func statusVPCEndpointAssociation(ctx context.Context, conn *networkfirewall.Client, id string) retry.StateRefreshFunc {
+	return func() (any, string, error) {
+		out, err := findVPCEndpointAssociationByID(ctx, conn, id)
+		if tfresource.NotFound(err) {
+			return nil, "", nil
+		}
+
+		if err != nil {
+			return nil, "", err
+		}
+
+		return out, string(out.VpcEndpointAssociationStatus.Status), nil
+	}
+}
+
+func findVPCEndpointAssociationByID(ctx context.Context, conn *networkfirewall.Client, arn string) (*networkfirewall.DescribeVpcEndpointAssociationOutput, error) {
+	input := networkfirewall.DescribeVpcEndpointAssociationInput{
+		VpcEndpointAssociationArn: aws.String(arn),
+	}
+
+	out, err := conn.DescribeVpcEndpointAssociation(ctx, &input)
+	if err != nil {
+		if errs.IsA[*awstypes.ResourceNotFoundException](err) {
+			return nil, &retry.NotFoundError{
+				LastError:   err,
+				LastRequest: &input,
+			}
+		}
+
+		return nil, err
+	}
+
+	if out == nil {
+		return nil, tfresource.NewEmptyResultError(&input)
+	}
+
+	return out, nil
+}
+
+type resourceVPCEndpointAssociationModel struct {
+	framework.WithRegionModel
+	Description                  types.String                                                       `tfsdk:"description"`
+	FirewallArn                  types.String                                                       `tfsdk:"firewall_arn"`
+	ID                           types.String                                                       `tfsdk:"id"`
+	SubnetMapping                fwtypes.ListNestedObjectValueOf[subnetMappingModel]                `tfsdk:"subnet_mapping"`
+	Tags                         tftags.Map                                                         `tfsdk:"tags"`
+	TagsAll                      tftags.Map                                                         `tfsdk:"tags_all"`
+	Timeouts                     timeouts.Value                                                     `tfsdk:"timeouts"`
+	VpcEndpointAssociationArn    types.String                                                       `tfsdk:"arn"`
+	VpcEndpointAssociationId     types.String                                                       `tfsdk:"vpc_endpoint_association_id"`
+	VpcEndpointAssociationStatus fwtypes.ListNestedObjectValueOf[vpcEndpointAssociationStatusModel] `tfsdk:"vpc_endpoint_association_status"`
+	VPCID                        types.String                                                       `tfsdk:"vpc_id"`
+}
+
+func (model *resourceVPCEndpointAssociationModel) setID() {
+	model.ID = model.VpcEndpointAssociationArn
+}
+
+type subnetMappingModel struct {
+	SubnetId      types.String `tfsdk:"subnet_id"`
+	IPAddressType types.String `tfsdk:"ip_address_type"`
+}
+
+type vpcEndpointAssociationStatusModel struct {
+	Status               types.String                                     `tfsdk:"status"`
+	AssociationSyncState fwtypes.SetNestedObjectValueOf[AZSyncStateModel] `tfsdk:"association_sync_state"`
+}
+
+type AZSyncStateModel struct {
+	Attachment       fwtypes.ListNestedObjectValueOf[attachmentModel] `tfsdk:"attachment"`
+	AvailabilityZone types.String                                     `tfsdk:"availability_zone"`
+}
+
+type attachmentModel struct {
+	EndpointId    types.String `tfsdk:"endpoint_id"`
+	SubnetId      types.String `tfsdk:"subnet_id"`
+	Status        types.String `tfsdk:"status"`
+	StatusMessage types.String `tfsdk:"status_message"`
+}
+
+func (m *resourceVPCEndpointAssociationModel) flattenVpcEndpointAssociationStatus(ctx context.Context, vpcEndpointAssociationStatus *awstypes.VpcEndpointAssociationStatus) {
+
+	statusModel.Status = flex.StringValueToFramework(ctx, string(vpcEndpointAssociationStatus.Status))
+
+	if len(vpcEndpointAssociationStatus.AssociationSyncState) == 0 {
+		statusModel.AssociationSyncState = fwtypes.NewSetNestedObjectValueOfNull[AZSyncStateModel](ctx)
+		return
+	}
+	azSyncStates := make([]*AZSyncStateModel, 0, len(vpcEndpointAssociationStatus.AssociationSyncState))
+	for az, syncState := range vpcEndpointAssociationStatus.AssociationSyncState {
+		azSyncState := &AZSyncStateModel{
+			AvailabilityZone: flex.StringValueToFramework(ctx, az),
+		}
+		if syncState.Attachment != nil {
+			attachment := &attachmentModel{
+				EndpointId:    flex.StringToFramework(ctx, syncState.Attachment.EndpointId),
+				SubnetId:      flex.StringToFramework(ctx, syncState.Attachment.SubnetId),
+				Status:        flex.StringValueToFramework(ctx, string(syncState.Attachment.Status)),
+				StatusMessage: flex.StringToFramework(ctx, syncState.Attachment.StatusMessage),
+			}
+			azSyncState.Attachment = fwtypes.NewListNestedObjectValueOfSliceMust(ctx, []*attachmentModel{attachment})
+		} else {
+			azSyncState.Attachment = fwtypes.NewListNestedObjectValueOfNull[attachmentModel](ctx)
+		}
+
+		azSyncStates = append(azSyncStates, azSyncState)
+	}
+
+	statusModel.AssociationSyncState = fwtypes.NewSetNestedObjectValueOfSliceMust(ctx, azSyncStates)
+}
---
+++ internal/service/networkfirewall/vpc_endpoint_association_test.go
@@ -0,0 +1,229 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package networkfirewall_test
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/YakDriver/regexache"
+	"github.com/aws/aws-sdk-go-v2/service/networkfirewall"
+	sdkacctest "github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/create"
+	tfnetworkfirewall "github.com/hashicorp/terraform-provider-aws/internal/service/networkfirewall"
+	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
+	"github.com/hashicorp/terraform-provider-aws/names"
+)
+
+func TestAccNetworkFirewallVPCEndpointAssociation_basic(t *testing.T) {
+	ctx := acctest.Context(t)
+	if testing.Short() {
+		t.Skip("skipping long-running test in short mode")
+	}
+
+	var vpcendpointassociation networkfirewall.DescribeVpcEndpointAssociationOutput
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	resourceName := "aws_networkfirewall_vpc_endpoint_association.test"
+	// firewallResourceName := "aws_networkfirewall_firewall.test"
+	// vpcResourceName := "aws_vpc.target"
+	//subnetResourceName := "aws_subnet.target"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+			testAccVPCEndpointAssociationPreCheck(ctx, t)
+		},
+		ErrorCheck:               acctest.ErrorCheck(t, names.NetworkFirewallServiceID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckVPCEndpointAssociationDestroy(ctx),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccVPCEndpointAssociationConfig_basic(rName),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccCheckVPCEndpointAssociationExists(ctx, resourceName, &vpcendpointassociation),
+					// resource.TestCheckResourceAttrPair(resourceName, "firewall_arn", firewallResourceName, names.AttrARN),
+					// resource.TestCheckResourceAttrPair(resourceName, "vpc_id", vpcResourceName, names.AttrID),
+					//resource.TestCheckResourceAttrPair(resourceName, "subnet_mapping.0.subnet_id", subnetResourceName, names.AttrID),
+					resource.TestMatchTypeSetElemNestedAttrs(resourceName, "vpc_endpoint_association_status.0.association_sync_state.*", map[string]*regexp.Regexp{
+						"attachment.0.endpoint_id": regexache.MustCompile(`vpce-`),
+					}),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccNetworkFirewallVPCEndpointAssociation_disappears(t *testing.T) {
+	ctx := acctest.Context(t)
+	if testing.Short() {
+		t.Skip("skipping long-running test in short mode")
+	}
+
+	var vpcendpointassociation networkfirewall.DescribeVpcEndpointAssociationOutput
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	resourceName := "aws_networkfirewall_vpc_endpoint_association.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+			testAccPreCheck(ctx, t)
+		},
+		ErrorCheck:               acctest.ErrorCheck(t, names.NetworkFirewallServiceID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckVPCEndpointAssociationDestroy(ctx),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccVPCEndpointAssociationConfig_basic(rName),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccCheckVPCEndpointAssociationExists(ctx, resourceName, &vpcendpointassociation),
+
+					acctest.CheckFrameworkResourceDisappears(ctx, acctest.Provider, tfnetworkfirewall.ResourceVPCEndpointAssociation, resourceName),
+				),
+				ExpectNonEmptyPlan: true,
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PostApplyPostRefresh: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceName, plancheck.ResourceActionCreate),
+					},
+				},
+			},
+		},
+	})
+}
+
+func testAccCheckVPCEndpointAssociationDestroy(ctx context.Context) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		conn := acctest.Provider.Meta().(*conns.AWSClient).NetworkFirewallClient(ctx)
+
+		for _, rs := range s.RootModule().Resources {
+			if rs.Type != "aws_networkfirewall_vpc_endpoint_association" {
+				continue
+			}
+
+			_, err := tfnetworkfirewall.FindVPCEndpointAssociationByID(ctx, conn, rs.Primary.ID)
+			if tfresource.NotFound(err) {
+				return nil
+			}
+			if err != nil {
+				return create.Error(names.NetworkFirewall, create.ErrActionCheckingDestroyed, tfnetworkfirewall.ResNameVPCEndpointAssociation, rs.Primary.ID, err)
+			}
+
+			return create.Error(names.NetworkFirewall, create.ErrActionCheckingDestroyed, tfnetworkfirewall.ResNameVPCEndpointAssociation, rs.Primary.ID, errors.New("not destroyed"))
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckVPCEndpointAssociationExists(ctx context.Context, name string, vpcendpointassociation *networkfirewall.DescribeVpcEndpointAssociationOutput) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[name]
+		if !ok {
+			return create.Error(names.NetworkFirewall, create.ErrActionCheckingExistence, tfnetworkfirewall.ResNameVPCEndpointAssociation, name, errors.New("not found"))
+		}
+
+		if rs.Primary.ID == "" {
+			return create.Error(names.NetworkFirewall, create.ErrActionCheckingExistence, tfnetworkfirewall.ResNameVPCEndpointAssociation, name, errors.New("not set"))
+		}
+
+		conn := acctest.Provider.Meta().(*conns.AWSClient).NetworkFirewallClient(ctx)
+
+		resp, err := tfnetworkfirewall.FindVPCEndpointAssociationByID(ctx, conn, rs.Primary.ID)
+		if err != nil {
+			return create.Error(names.NetworkFirewall, create.ErrActionCheckingExistence, tfnetworkfirewall.ResNameVPCEndpointAssociation, rs.Primary.ID, err)
+		}
+
+		*vpcendpointassociation = *resp
+
+		return nil
+	}
+}
+
+func testAccVPCEndpointAssociationPreCheck(ctx context.Context, t *testing.T) {
+	conn := acctest.Provider.Meta().(*conns.AWSClient).NetworkFirewallClient(ctx)
+
+	input := &networkfirewall.ListVpcEndpointAssociationsInput{}
+
+	_, err := conn.ListVpcEndpointAssociations(ctx, input)
+
+	if acctest.PreCheckSkipError(err) {
+		t.Skipf("skipping acceptance testing: %s", err)
+	}
+	if err != nil {
+		t.Fatalf("unexpected PreCheck error: %s", err)
+	}
+}
+
+// func testAccVPCEndpointAssociationConfig_basic(rName string) string {
+// 	return acctest.ConfigCompose(testAccFirewallConfig_baseVPC(rName), fmt.Sprintf(`
+// resource "aws_networkfirewall_firewall" "test" {
+//   name                = %[1]q
+//   firewall_policy_arn = aws_networkfirewall_firewall_policy.test.arn
+//   vpc_id              = aws_vpc.test.id
+
+//   subnet_mapping {
+//     subnet_id = aws_subnet.test[0].id
+//   }
+// }
+
+// resource "aws_vpc" "target" {
+//   cidr_block = "10.0.0.0/16"
+
+//   tags = {
+//     Name = %[1]q
+//   }
+// }
+
+// resource "aws_subnet" "target" {
+//   vpc_id            = aws_vpc.target.id
+//   availability_zone = data.aws_availability_zones.available.names[0]
+//   cidr_block        = cidrsubnet(aws_vpc.target.cidr_block, 8, 1)
+
+//   tags = {
+//     Name = %[1]q
+//   }
+// }
+// resource "aws_networkfirewall_vpc_endpoint_association" "test" {
+//   firewall_arn = aws_networkfirewall_firewall.test.arn
+//   vpc_id       = aws_vpc.target.id
+
+//   subnet_mapping {
+//     subnet_id = aws_subnet.target.id
+//   }
+
+//   tags = {
+//     Name = %[1]q
+//   }
+// }
+// `, rName))
+// }
+
+func testAccVPCEndpointAssociationConfig_basic(rName string) string {
+	return fmt.Sprintf(`
+resource "aws_networkfirewall_vpc_endpoint_association" "test" {
+  firewall_arn = "arn:aws:network-firewall:ap-southeast-2:922421696073:firewall/tf-acc-test-3237751493501276692"
+  vpc_id       = "vpc-08d9d6d6e21fa4afc"
+
+  subnet_mapping {
+    subnet_id = "subnet-0c58d6ca32f04ce8f"
+  }
+
+  tags = {
+    Name = %[1]q
+  }
+}
+`, rName)
+}
