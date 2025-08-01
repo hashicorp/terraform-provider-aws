58f6ea70d9bdfdbfff88de881a7bbe5c6e359d6e
Asim <asim.kumar.biswal@oracle.com>
2025-08-01 16:41:41 +0100

    fix tflint review

--- examples/odb/exadata_infra.tf
+++ examples/odb/exadata_infra.tf
@@ -1,16 +1,16 @@
-//Copyright © 2025, Oracle and/or its affiliates. All rights reserved.
+# Copyright © 2025, Oracle and/or its affiliates. All rights reserved.
 
-//Exadata Infrastructure with customer managed maintenance window
+# Exadata Infrastructure with customer managed maintenance window
 resource "aws_odb_cloud_exadata_infrastructure" "exa_infra_X11M_all_param" {
-  display_name                        = "Ofake_my_odb_exadata_infra" //Required Field
-  shape                               = "Exadata.X11M"  //Required Field
+  display_name                        = "Ofake_my_odb_exadata_infra"
+  shape                               = "Exadata.X11M"
   storage_count                       = 3
   compute_count                       = 2
-  availability_zone_id                = "use1-az6" //Required Field
+  availability_zone_id                = "use1-az6"
   customer_contacts_to_send_to_oci    = ["abc@example.com"]
   database_server_type                = "X11M"
   storage_server_type                 = "X11M-HC"
-  maintenance_window = { //Required
+  maintenance_window = {
     custom_action_timeout_in_mins     = 16
     days_of_week                      = ["MONDAY", "TUESDAY"]
     hours_of_day                      = [11, 16]
@@ -27,7 +27,7 @@ resource "aws_odb_cloud_exadata_infrastructure" "exa_infra_X11M_all_param" {
 
 }
 
-//Exadata Infrastructure with  default maintenance window with X9M system shape. with minimum parameters
+# Exadata Infrastructure with  default maintenance window with X9M system shape. with minimum parameters
 resource "aws_odb_cloud_exadata_infrastructure" "test_X9M" {
   display_name          = "Ofake_my_exa_X9M"
   shape             	= "Exadata.X9M"
