fb817a536c8e0075381b5ce35f4a378a3317bcb1
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-08-06 15:47:57 -0400

    Run `make gen`.

--- internal/service/ivschat/room_identity_gen_test.go
+++ internal/service/ivschat/room_identity_gen_test.go
@@ -25,7 +25,7 @@ func TestAccIVSChatRoom_Identity_Basic(t *testing.T) {
 	var v ivschat.GetRoomOutput
 	resourceName := "aws_ivschat_room.test"
 
-	resource.ParallelTest(t, resource.TestCase{
+	acctest.ParallelTest(ctx, t, resource.TestCase{
 		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
 			tfversion.SkipBelow(tfversion.Version1_12_0),
 		},
@@ -101,7 +101,7 @@ func TestAccIVSChatRoom_Identity_RegionOverride(t *testing.T) {
 
 	resourceName := "aws_ivschat_room.test"
 
-	resource.ParallelTest(t, resource.TestCase{
+	acctest.ParallelTest(ctx, t, resource.TestCase{
 		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
 			tfversion.SkipBelow(tfversion.Version1_12_0),
 		},
@@ -216,7 +216,7 @@ func TestAccIVSChatRoom_Identity_ExistingResource(t *testing.T) {
 	var v ivschat.GetRoomOutput
 	resourceName := "aws_ivschat_room.test"
 
-	resource.ParallelTest(t, resource.TestCase{
+	acctest.ParallelTest(ctx, t, resource.TestCase{
 		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
 			tfversion.SkipBelow(tfversion.Version1_12_0),
 		},
