a9a73df1dd04d75c447f9f24427badafe13fd35c
Graham Davison <gdavison@hashicorp.com>
2025-08-08 12:30:13 -0700

    Adds test for `BackwardValues`

--- internal/slices/iter_test.go
+++ internal/slices/iter_test.go
@@ -47,3 +47,25 @@ func TestAppliedToEach(t *testing.T) {
 		})
 	}
 }
+
+// Copied and adapted from stdlib slices package
+func TestBackwardValues(t *testing.T) {
+	for size := range 10 {
+		var s []int
+		for i := range size {
+			s = append(s, i)
+		}
+		ev := size - 1
+		cnt := 0
+		for v := range BackwardValues(s) {
+			if v != ev {
+				t.Errorf("at iteration %d got  %d want %d", cnt, v, ev)
+			}
+			ev--
+			cnt++
+		}
+		if cnt != size {
+			t.Errorf("read %d values expected %d", cnt, size)
+		}
+	}
+}
