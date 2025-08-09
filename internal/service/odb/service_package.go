abcfea651853424910d4ecc01601c5d9a40f634b
Asim <asim.kumar.biswal@oracle.com>
2025-08-09 19:25:56 +0100

    service package changes fixed

--- internal/service/odb/service_package.go
+++ internal/service/odb/service_package.go
@@ -6,7 +6,6 @@ import (
 	"context"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/odb"
-	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
@@ -17,16 +16,6 @@ func (p *servicePackage) NewClient(ctx context.Context, config map[string]any) (
 		odb.WithEndpointResolverV2(newEndpointResolverV2()),
 		withBaseEndpoint(config[names.AttrEndpoint].(string)),
 		func(o *odb.Options) {
-			//if config["partition"].(string) == names.StandardPartitionID {
-			if cfg.Region != "\"us-east-1\"" {
-				tflog.Info(ctx, "overriding region", map[string]any{
-					"original_region": cfg.Region,
-					"override_region": "us-east-1",
-				})
-				o.Region = "us-east-1"
-			}
-			//}
-			o.BaseEndpoint = aws.String("https://gammaiad.capi.us-east-1.oracolo.alameda.aws.dev")
 		},
 	), nil
 }
