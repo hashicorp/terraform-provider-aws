815f8d171e8f3a2c37ae3949e52c9607d595604c
Jared Baker <jared.baker@hashicorp.com>
2025-08-12 10:08:12 -0400

    r/aws_subnet: add resource identity
    === RUN   TestAccVPCSubnet_Identity_Basic
    --- PASS: TestAccVPCSubnet_Identity_Basic (37.23s)
    === RUN   TestAccVPCSubnet_Identity_RegionOverride
    --- PASS: TestAccVPCSubnet_Identity_RegionOverride (29.57s)
    === RUN   TestAccVPCSubnet_Identity_ExistingResource
    --- PASS: TestAccVPCSubnet_Identity_ExistingResource (51.64s)
    PASS
    ok  	github.com/hashicorp/terraform-provider-aws/internal/service/ec2	58.280s

---
+++ internal/service/ec2/testdata/Subnet/region_override/main_gen.tf
@@ -0,0 +1,26 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+resource "aws_subnet" "test" {
+  region = var.region
+
+  cidr_block = "10.1.1.0/24"
+  vpc_id     = aws_vpc.test.id
+}
+
+resource "aws_vpc" "test" {
+  region = var.region
+
+  cidr_block = "10.1.0.0/16"
+
+  tags = {
+    Name = "test"
+  }
+}
+
+
+variable "region" {
+  description = "Region to deploy resource in"
+  type        = string
+  nullable    = false
+}
--- internal/service/ec2/testdata/tmpl/vpc_subnet_basic.gtpl
+++ internal/service/ec2/testdata/tmpl/vpc_subnet_basic.gtpl
@@ -2,11 +2,13 @@ resource "aws_subnet" "test" {
 {{- template "region" }}
   cidr_block = "10.1.1.0/24"
   vpc_id     = aws_vpc.test.id
-
-{{- template "tags" . }}
 }
 
 resource "aws_vpc" "test" {
 {{- template "region" }}
   cidr_block = "10.1.0.0/16"
+
+  tags = {
+    Name = "test"
+  }
 }
