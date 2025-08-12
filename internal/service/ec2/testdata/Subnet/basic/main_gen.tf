6062821cba08e3d45b2435bf35ca62369cb82c7c
Jared Baker <jar-b@users.noreply.github.com>
2025-08-12 13:08:55 -0400

    Add parameterized resource identity to `aws_subnet` (#43833)
    * r/aws_subnet: add resource identity
    
    === RUN   TestAccVPCSubnet_Identity_Basic
    --- PASS: TestAccVPCSubnet_Identity_Basic (37.23s)
    === RUN   TestAccVPCSubnet_Identity_RegionOverride
    --- PASS: TestAccVPCSubnet_Identity_RegionOverride (29.57s)
    === RUN   TestAccVPCSubnet_Identity_ExistingResource
    --- PASS: TestAccVPCSubnet_Identity_ExistingResource (51.64s)
    PASS
    ok  	github.com/hashicorp/terraform-provider-aws/internal/service/ec2	58.280s
    
    * Remove unnecessary basic template file
    
    Since vpc_subnet_tags.gtpl already exists and has been updated with the region template,
    the separate basic template is not needed. The identity tests now properly use the
    tags template as the base configuration.
    
    * chore: fix changelog number

--- internal/service/ec2/testdata/Subnet/basic/main_gen.tf
+++ internal/service/ec2/testdata/Subnet/basic/main_gen.tf
@@ -1,12 +1,12 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
 resource "aws_subnet" "test" {
-{{- template "region" }}
   cidr_block = "10.1.1.0/24"
   vpc_id     = aws_vpc.test.id
-
-{{- template "tags" . }}
 }
 
 resource "aws_vpc" "test" {
-{{- template "region" }}
   cidr_block = "10.1.0.0/16"
 }
+
