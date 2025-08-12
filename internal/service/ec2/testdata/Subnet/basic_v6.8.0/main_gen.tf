8a4f0eb94755975bd4a52c4874a24bba3d32177c
Jared Baker <jared.baker@hashicorp.com>
2025-08-12 10:13:01 -0400

    Remove unnecessary basic template file
    Since vpc_subnet_tags.gtpl already exists and has been updated with the region template,
    the separate basic template is not needed. The identity tests now properly use the
    tags template as the base configuration.

--- internal/service/ec2/testdata/Subnet/basic_v6.8.0/main_gen.tf
+++ internal/service/ec2/testdata/Subnet/basic_v6.8.0/main_gen.tf
@@ -8,10 +8,6 @@ resource "aws_subnet" "test" {
 
 resource "aws_vpc" "test" {
   cidr_block = "10.1.0.0/16"
-
-  tags = {
-    Name = "test"
-  }
 }
 
 terraform {
--- internal/service/ec2/testdata/Subnet/region_override/main_gen.tf
+++ internal/service/ec2/testdata/Subnet/region_override/main_gen.tf
@@ -12,10 +12,6 @@ resource "aws_vpc" "test" {
   region = var.region
 
   cidr_block = "10.1.0.0/16"
-
-  tags = {
-    Name = "test"
-  }
 }
 
 
