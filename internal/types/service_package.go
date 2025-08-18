6a1ad307f85ecfdf1ae9338e3656ee049d18dfe4
Graham Davison <gdavison@hashicorp.com>
2025-08-18 09:31:15 -0700

    Renames `inttypes.ServicePackageListResource` to `ServicePackageFrameworkListResource`

--- internal/types/service_package.go
+++ internal/types/service_package.go
@@ -74,7 +74,7 @@ type ServicePackageFrameworkResource struct {
 	Import   FrameworkImport
 }
 
-type ServicePackageListResource struct {
+type ServicePackageFrameworkListResource struct {
 	Factory  func() list.ListResourceWithConfigure
 	TypeName string
 	Name     string
