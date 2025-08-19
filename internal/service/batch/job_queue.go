ca2c92efab3cdf8e9ad3015d4bc3eeb64bf0a980
Graham Davison <gdavison@hashicorp.com>
2025-08-19 16:18:32 -0700

    Adds WIP List code for `aws_batch_job_queue`

--- internal/service/batch/job_queue.go
+++ internal/service/batch/job_queue.go
@@ -8,7 +8,9 @@ import (
 	"errors"
 	"fmt"
 	"iter"
+	"slices"
 	"time"
+	"unique"
 
 	"github.com/YakDriver/regexache"
 	"github.com/aws/aws-sdk-go-v2/aws"
@@ -17,6 +19,10 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework-timeouts/resource/timeouts"
 	"github.com/hashicorp/terraform-plugin-framework-validators/int64validator"
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/list"
+	listschema "github.com/hashicorp/terraform-plugin-framework/list/schema"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
@@ -24,15 +30,20 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/enum"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/fwdiag"
 	"github.com/hashicorp/terraform-provider-aws/internal/framework"
 	fwflex "github.com/hashicorp/terraform-provider-aws/internal/framework/flex"
 	fwtypes "github.com/hashicorp/terraform-provider-aws/internal/framework/types"
+	"github.com/hashicorp/terraform-provider-aws/internal/provider/interceptors"
+	tfslices "github.com/hashicorp/terraform-provider-aws/internal/slices"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
+	inttypes "github.com/hashicorp/terraform-provider-aws/internal/types"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
@@ -51,6 +62,13 @@ func newJobQueueResource(_ context.Context) (resource.ResourceWithConfigure, err
 
 	return &r, nil
 }
+
+// @List
+func JobQueueResourceAsListResource() list.ListResourceWithConfigure {
+	return &jobQueueResource{}
+}
+
+var _ list.ListResource = &jobQueueResource{}
 
 type jobQueueResource struct {
 	framework.ResourceWithModel[jobQueueResourceModel]
@@ -520,4 +538,305 @@ func listJobQueues(ctx context.Context, conn *batch.Client, input *batch.Describ
 			}
 		}
 	}
+}
+
+func (r jobQueueResource) ListResourceConfigSchema(_ context.Context, request list.ListResourceSchemaRequest, response *list.ListResourceSchemaResponse) {
+	response.Schema = listschema.Schema{
+		Attributes: map[string]listschema.Attribute{},
+	}
+}
+
+func (r jobQueueResource) List(ctx context.Context, request list.ListRequest, stream *list.ListResultsStream) {
+	var query jobQueueListModel
+
+	if request.Config.Raw.IsKnown() && !request.Config.Raw.IsNull() {
+		if diags := request.Config.Get(ctx, &query); diags.HasError() {
+			stream.Results = list.ListResultsStreamDiagnostics(diags)
+			return
+		}
+	}
+
+	awsClient := r.Meta()
+	conn := awsClient.BatchClient(ctx)
+
+	interceptors := []listResultInterceptor{
+		populateTimeoutsInterceptor{},
+		populateIdentityInterceptor{},
+		identityInterceptor{
+			attributes: r.IdentitySpec().Attributes,
+		},
+		setResourceInterceptor{},
+		tagsInterceptor{
+			HTags: interceptors.HTags(unique.Make(inttypes.ServicePackageResourceTags{
+				IdentifierAttribute: names.AttrARN,
+			})),
+		},
+	}
+
+	stream.Results = func(yield func(list.ListResult) bool) {
+		result := request.NewListResult(ctx)
+		var input batch.DescribeJobQueuesInput
+		for jobQueue, err := range listJobQueues(ctx, conn, &input) {
+			if err != nil {
+				result = list.ListResult{
+					Diagnostics: diag.Diagnostics{
+						diag.NewErrorDiagnostic(
+							"Error Listing Remote Resources",
+							fmt.Sprintf("Error: %s", err),
+						),
+					},
+				}
+				yield(result)
+				return
+			}
+
+			ctx = tftags.NewContext(ctx, awsClient.DefaultTagsConfig(ctx), awsClient.IgnoreTagsConfig(ctx))
+
+			var data jobQueueResourceModel
+
+			params := interceptorParams{
+				c:      awsClient,
+				result: &result,
+				object: &data,
+			}
+
+			params.when = Before
+			for interceptor := range slices.Values(interceptors) {
+				d := interceptor.read(ctx, params)
+				result.Diagnostics.Append(d...)
+				if d.HasError() {
+					result = list.ListResult{Diagnostics: result.Diagnostics}
+					yield(result)
+					return
+				}
+			}
+
+			if diags := fwflex.Flatten(ctx, jobQueue, &data, fwflex.WithFieldNamePrefix("JobQueue")); diags.HasError() {
+				result.Diagnostics.Append(diags...)
+			}
+			setTagsOut(ctx, jobQueue.Tags)
+
+			result.DisplayName = fmt.Sprintf("x%s (%s)x", data.JobQueueName.ValueString(), data.JobQueueARN.ValueString())
+
+			params.when = After
+			for interceptor := range tfslices.BackwardValues(interceptors) {
+				d := interceptor.read(ctx, params)
+				result.Diagnostics.Append(d...)
+				if d.HasError() {
+					result = list.ListResult{Diagnostics: result.Diagnostics}
+					yield(result)
+					return
+				}
+			}
+
+			if result.Diagnostics.HasError() {
+				result = list.ListResult{Diagnostics: result.Diagnostics}
+				yield(result)
+				return
+			}
+
+			if !yield(result) {
+				return
+			}
+		}
+	}
+}
+
+type jobQueueListModel struct {
+	// TODO: factor out
+	Region types.String `tfsdk:"region"`
+}
+
+// when represents the point in the CRUD request lifecycle that an interceptor is run.
+// Multiple values can be ORed together.
+type when uint16
+
+const (
+	Before  when = 1 << iota // Interceptor is invoked before call to method in schema
+	After                    // Interceptor is invoked after successful call to method in schema
+	OnError                  // Interceptor is invoked after unsuccessful call to method in schema
+	Finally                  // Interceptor is invoked after After or OnError
+)
+
+type interceptorParams struct {
+	c      *conns.AWSClient
+	result *list.ListResult
+	object *jobQueueResourceModel // Because tfsdk.Resource doesn't have SetAttribute
+	when   when
+}
+
+type listResultInterceptor interface {
+	read(ctx context.Context, params interceptorParams) diag.Diagnostics
+}
+
+type tagsInterceptor struct {
+	interceptors.HTags
+}
+
+// Copied from tagsResourceInterceptor.read()
+func (r tagsInterceptor) read(ctx context.Context, params interceptorParams) (diags diag.Diagnostics) {
+	sp, serviceName, resourceName, tagsInContext, ok := interceptors.InfoFromContext(ctx, params.c)
+	if !ok {
+		return
+	}
+
+	switch params.when {
+	case After:
+		// If the R handler didn't set tags, try and read them from the service API.
+		if tagsInContext.TagsOut.IsNone() {
+			// Some old resources may not have the required attribute set after Read:
+			// https://github.com/hashicorp/terraform-provider-aws/issues/31180
+			if identifier := r.GetIdentifierFramework(ctx, params.result.Resource); identifier != "" {
+				if err := r.ListTags(ctx, sp, params.c, identifier); err != nil {
+					diags.AddError(fmt.Sprintf("listing tags for %s %s (%s)", serviceName, resourceName, identifier), err.Error())
+
+					return
+				}
+			}
+		}
+
+		apiTags := tagsInContext.TagsOut.UnwrapOrDefault()
+
+		// AWS APIs often return empty lists of tags when none have been configured.
+		var stateTags tftags.Map
+		params.result.Resource.GetAttribute(ctx, path.Root(names.AttrTags), &stateTags)
+		// Remove any provider configured ignore_tags and system tags from those returned from the service API.
+		// The resource's configured tags do not include any provider configured default_tags.
+		if v := apiTags.IgnoreSystem(sp.ServicePackageName()).IgnoreConfig(params.c.IgnoreTagsConfig(ctx)).ResolveDuplicatesFramework(ctx, params.c.DefaultTagsConfig(ctx), params.c.IgnoreTagsConfig(ctx), stateTags, &diags).Map(); len(v) > 0 {
+			stateTags = tftags.NewMapFromMapValue(fwflex.FlattenFrameworkStringValueMapLegacy(ctx, v))
+		}
+		params.object.Tags = stateTags
+
+		// Computed tags_all do.
+		stateTagsAll := fwflex.FlattenFrameworkStringValueMapLegacy(ctx, apiTags.IgnoreSystem(sp.ServicePackageName()).IgnoreConfig(params.c.IgnoreTagsConfig(ctx)).Map())
+		params.object.TagsAll = tftags.NewMapFromMapValue(stateTagsAll)
+	}
+
+	return
+}
+
+// This interceptor will not be needed if Framework pre-populates the Resource as it does with CRUD operations
+type populateTimeoutsInterceptor struct{}
+
+func (r populateTimeoutsInterceptor) read(ctx context.Context, params interceptorParams) (diags diag.Diagnostics) {
+	switch params.when {
+	case Before:
+		timeoutsType, d := params.result.Resource.Schema.TypeAtPath(ctx, path.Root("timeouts"))
+		diags.Append(d...)
+		if d.HasError() {
+			return
+		}
+
+		obj, d := newEmptyObject(timeoutsType)
+		diags.Append(d...)
+		if d.HasError() {
+			return
+		}
+		params.object.Timeouts.Object = obj
+	}
+
+	return
+}
+
+// This interceptor will not be needed if Framework pre-populates the Identity as it does with CRUD operations
+type populateIdentityInterceptor struct{}
+
+func (r populateIdentityInterceptor) read(ctx context.Context, params interceptorParams) (diags diag.Diagnostics) {
+	switch params.when {
+	case Before:
+		identityType := params.result.Identity.Schema.Type()
+
+		obj, d := newEmptyObject(identityType)
+		diags.Append(d...)
+		if diags.HasError() {
+			return
+		}
+
+		diags.Append(params.result.Identity.Set(ctx, obj)...)
+		if diags.HasError() {
+			return
+		}
+	}
+
+	return
+}
+
+// This interceptor will not be needed if the Resource value is of a type that implements SetAttribute
+type setResourceInterceptor struct{}
+
+func (r setResourceInterceptor) read(ctx context.Context, params interceptorParams) (diags diag.Diagnostics) {
+	switch params.when {
+	case After:
+		diags.Append(params.result.Resource.Set(ctx, params.object)...)
+	}
+
+	return
+}
+
+type identityInterceptor struct {
+	attributes []inttypes.IdentityAttribute
+}
+
+func (r identityInterceptor) read(ctx context.Context, params interceptorParams) (diags diag.Diagnostics) {
+	awsClient := params.c
+
+	switch params.when {
+	case After:
+		for _, att := range r.attributes {
+			switch att.Name() {
+			case names.AttrAccountID:
+				diags.Append(params.result.Identity.SetAttribute(ctx, path.Root(att.Name()), awsClient.AccountID(ctx))...)
+				if diags.HasError() {
+					return
+				}
+
+			case names.AttrRegion:
+				diags.Append(params.result.Identity.SetAttribute(ctx, path.Root(att.Name()), awsClient.Region(ctx))...)
+				if diags.HasError() {
+					return
+				}
+
+			default:
+				var attrVal attr.Value
+				diags.Append(params.result.Resource.GetAttribute(ctx, path.Root(att.ResourceAttributeName()), &attrVal)...)
+				if diags.HasError() {
+					return
+				}
+
+				diags.Append(params.result.Identity.SetAttribute(ctx, path.Root(att.Name()), attrVal)...)
+				if diags.HasError() {
+					return
+				}
+			}
+		}
+	}
+
+	return
+}
+
+func newEmptyObject(typ attr.Type) (obj basetypes.ObjectValue, diags diag.Diagnostics) {
+	i, ok := typ.(attr.TypeWithAttributeTypes)
+	if !ok {
+		diags.AddError(
+			"Internal Error",
+			"An unexpected error occurred. "+
+				"This is always an error in the provider. "+
+				"Please report the following to the provider developer:\n\n"+
+				fmt.Sprintf("Expected value type to implement attr.TypeWithAttributeTypes, got: %T", typ),
+		)
+		return
+	}
+
+	attrTypes := i.AttributeTypes()
+	attrValues := make(map[string]attr.Value, len(attrTypes))
+	for attrName := range attrTypes {
+		attrValues[attrName] = types.StringNull()
+	}
+	obj, d := basetypes.NewObjectValue(attrTypes, attrValues)
+	diags.Append(d...)
+	if d.HasError() {
+		return basetypes.ObjectValue{}, diags
+	}
+
+	return obj, diags
 }
