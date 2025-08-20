0988e5fa63ee56b67f57d9641e68632c7435bee0
tabito <tabito.htnet.org@gmail.com>
2025-08-20 19:58:12 +0900

    Add customizeDiff func to check policy_type and target_tags consistency

--- internal/service/dlm/lifecycle_policy.go
+++ internal/service/dlm/lifecycle_policy.go
@@ -5,6 +5,7 @@ package dlm
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"time"
 
@@ -478,6 +479,7 @@ func resourceLifecyclePolicy() *schema.Resource {
 			names.AttrTags:    tftags.TagsSchema(),
 			names.AttrTagsAll: tftags.TagsSchemaComputed(),
 		},
+		CustomizeDiff: checkPolicyTypeTargetTagsConsistencyCustomizeDiff,
 	}
 }
 
@@ -1285,4 +1287,21 @@ func flattenParameters(parameters *awstypes.Parameters) []map[string]any {
 	}
 
 	return []map[string]any{result}
+}
+
+func checkPolicyTypeTargetTagsConsistencyCustomizeDiff(ctx context.Context, d *schema.ResourceDiff, meta any) error {
+	policyDetails := expandPolicyDetails(d.Get("policy_details").([]any))
+	if policyDetails == nil {
+		return nil
+	}
+	if policyDetails.PolicyType == awstypes.PolicyTypeValuesEbsSnapshotManagement || policyDetails.PolicyType == awstypes.PolicyTypeValuesImageManagement {
+		if len(policyDetails.TargetTags) == 0 {
+			return fmt.Errorf("target_tags must be specified for policy_type %s", policyDetails.PolicyType)
+		}
+	} else if policyDetails.PolicyType == awstypes.PolicyTypeValuesEventBasedPolicy {
+		if len(policyDetails.TargetTags) > 0 {
+			return fmt.Errorf("target_tags must not be specified for policy_type %s", policyDetails.PolicyType)
+		}
+	}
+	return nil
 }
