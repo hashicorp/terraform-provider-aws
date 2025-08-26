ce87511a0f20ba908a3d02101cbf5cc7f8176526
Cody Zhao <codyzhao@amazon.com>
2025-08-26 10:16:33 +0000

    updated tests

--- internal/service/cloudfront/distribution_test.go
+++ internal/service/cloudfront/distribution_test.go
@@ -955,6 +955,7 @@ func TestAccCloudFrontDistribution_minimalMultiTenant(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckDistributionExists(ctx, resourceName, &distribution),
 					resource.TestCheckResourceAttr(resourceName, "connection_mode", "tenant-only"),
+					resource.TestMatchResourceAttr(resourceName, "default_cache_behavior.0.cache_policy_id", regexache.MustCompile(`^[0-9a-z]+`)),
 				),
 			},
 			{
@@ -1551,6 +1552,43 @@ func TestAccCloudFrontDistribution_grpcConfig(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "ordered_cache_behavior.0.grpc_config.#", "1"),
 					resource.TestCheckResourceAttr(resourceName, "ordered_cache_behavior.0.grpc_config.0.enabled", acctest.CtTrue),
 				),
+			},
+		},
+	})
+}
+
+func TestAccCloudFrontDistribution_multiTenantWithConfig(t *testing.T) {
+	ctx := acctest.Context(t)
+	var distribution awstypes.Distribution
+	resourceName := "aws_cloudfront_distribution.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(ctx, t); acctest.PreCheckPartitionHasService(t, names.CloudFrontEndpointID) },
+		ErrorCheck:               acctest.ErrorCheck(t, names.CloudFrontServiceID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckDistributionDestroy(ctx),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDistributionConfig_multiTenantWithConfig(),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDistributionExists(ctx, resourceName, &distribution),
+					resource.TestCheckResourceAttr(resourceName, "connection_mode", "tenant-only"),
+					resource.TestCheckResourceAttr(resourceName, "tenant_config.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "tenant_config.0.parameter_definitions.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "tenant_config.0.parameter_definitions.0.name", "tenantName"),
+					resource.TestCheckResourceAttr(resourceName, "tenant_config.0.parameter_definitions.0.definition.0.string_schema.0.required", "false"),
+					resource.TestCheckResourceAttr(resourceName, "tenant_config.0.parameter_definitions.0.definition.0.string_schema.0.comment", "tenantName"),
+					resource.TestCheckResourceAttr(resourceName, "tenant_config.0.parameter_definitions.0.definition.0.string_schema.0.default_value", "root"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+				ImportStateVerifyIgnore: []string{
+					"retain_on_delete",
+					"wait_for_deployment",
+				},
 			},
 		},
 	})
@@ -4753,6 +4791,80 @@ resource "aws_cloudfront_distribution" "test" {
   }
 
   retain_on_delete     = false
+}
+`
+}
+
+func testAccDistributionConfig_multiTenantWithConfig() string {
+	return `
+resource "aws_cloudfront_cache_policy" "tf-policy" {
+  name        = "tfpolicy"
+  comment     = "test tenant cache policy"
+  default_ttl = 50
+  max_ttl     = 100
+  min_ttl     = 1
+  parameters_in_cache_key_and_forwarded_to_origin {
+    cookies_config {
+      cookie_behavior = "none"
+    }
+    headers_config {
+      header_behavior = "none"
+    }
+    query_strings_config {
+      query_string_behavior = "none"
+    }
+  }
+}
+
+resource "aws_cloudfront_distribution" "test" {
+  connection_mode = "tenant-only"
+  enabled         = false
+  comment         = "Test tenant-only distribution with config"
+
+  origin {
+    domain_name = "www.example.com"
+    origin_id   = "myCustomOrigin"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "myCustomOrigin"
+    viewer_protocol_policy = "allow-all"
+    cache_policy_id        = aws_cloudfront_cache_policy.tf-policy.id
+  }
+
+  tenant_config {
+    parameter_definitions {
+      name = "tenantName"
+      definition {
+        string_schema {
+          required      = false
+          comment       = "tenantName"
+          default_value = "root"
+        }
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  retain_on_delete = false
 }
 `
 }
