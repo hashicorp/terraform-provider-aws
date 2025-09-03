5f9b529caf0fcc0d4821a636c74fa9a25b8f6f2d
Brian Cain <briancq@amazon.com>
2025-09-03 10:55:03 -0700

    Fixup: Fix provider alias for test

--- internal/service/arcregionswitch/plan_test.go
+++ internal/service/arcregionswitch/plan_test.go
@@ -1240,7 +1240,7 @@ resource "aws_vpc" "primary" {
 }
 
 resource "aws_vpc" "secondary" {
-  provider             = awsalternate
+  provider             = "aws.secondary"
   cidr_block           = "10.2.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
