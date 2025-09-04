242fa2747fc6909a513a9dd0a135b8285ba44f28
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-09-04 15:36:14 -0400

    Fix 'TestAccAppAutoScalingPolicy_TargetTrack_metricMath'.

--- internal/service/appautoscaling/policy_test.go
+++ internal/service/appautoscaling/policy_test.go
@@ -633,7 +633,7 @@ resource "aws_ecs_service" "test" {
   cluster                            = aws_ecs_cluster.test.id
   deployment_maximum_percent         = 200
   deployment_minimum_healthy_percent = 50
-  desired_count                      = 0
+  desired_count                      = 2
   name                               = %[1]q
   task_definition                    = aws_ecs_task_definition.test.arn
 }
