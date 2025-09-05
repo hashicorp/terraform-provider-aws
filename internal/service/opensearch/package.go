bd43654636183da54c4708383696cbbcdcfa246e
tabito <tabito.htnet.org@gmail.com>
2025-09-05 19:40:11 +0900

    Implement waiter for package validation

--- internal/service/opensearch/package.go
+++ internal/service/opensearch/package.go
@@ -5,7 +5,9 @@ package opensearch
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"time"
 
 	"github.com/YakDriver/regexache"
 	"github.com/aws/aws-sdk-go-v2/aws"
@@ -117,6 +119,9 @@ func resourcePackageCreate(ctx context.Context, d *schema.ResourceData, meta any
 
 	d.SetId(aws.ToString(output.PackageDetails.PackageID))
 
+	if _, err := waitPackageValidationCompleted(ctx, conn, d.Id()); err != nil {
+		return sdkdiag.AppendErrorf(diags, "waiting for package validation (%s) completed: %s", d.Id(), err)
+	}
 	return append(diags, resourcePackageRead(ctx, d, meta)...)
 }
 
@@ -240,5 +245,48 @@ func expandPackageSource(v any) *awstypes.PackageSource {
 	return &awstypes.PackageSource{
 		S3BucketName: aws.String(v.(map[string]any)[names.AttrS3BucketName].(string)),
 		S3Key:        aws.String(v.(map[string]any)["s3_key"].(string)),
+	}
+}
+
+func waitPackageValidationCompleted(ctx context.Context, conn *opensearch.Client, id string) (*opensearch.DescribePackagesOutput, error) {
+	stateConf := &retry.StateChangeConf{
+		Pending:    []string{"COPYING", "VALIDATING"},
+		Target:     []string{"AVAILABLE"},
+		Refresh:    statusPackageValidation(ctx, conn, id),
+		Timeout:    20 * time.Minute,
+		MinTimeout: 15 * time.Second,
+		Delay:      30 * time.Second,
+	}
+
+	outputRaw, err := stateConf.WaitForStateContext(ctx)
+
+	if output, ok := outputRaw.(*opensearch.DescribePackagesOutput); ok {
+		return output, err
+	}
+
+	return nil, err
+}
+
+func statusPackageValidation(ctx context.Context, conn *opensearch.Client, id string) retry.StateRefreshFunc {
+	return func() (any, string, error) {
+		output, err := findPackageByID(ctx, conn, id)
+
+		if tfresource.NotFound(err) {
+			return nil, "", nil
+		}
+
+		if err != nil {
+			return nil, "", err
+		}
+
+		if output == nil {
+			return nil, "", nil
+		}
+
+		if output.ErrorDetails != nil {
+			return nil, string(output.PackageStatus), fmt.Errorf("package validation failed: %s, %s, %s", string(output.PackageStatus), aws.ToString(output.ErrorDetails.ErrorType), aws.ToString(output.ErrorDetails.ErrorMessage))
+		}
+
+		return output, string(output.PackageStatus), nil
 	}
 }
