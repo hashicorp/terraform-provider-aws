9339308ccaa76fa829eaf7062df804d05adab411
Brian Cain <briancq@amazon.com>
2025-09-08 16:51:56 -0700

    make gen

--- internal/service/resiliencehub/testdata/App/tags_defaults/main_gen.tf
+++ internal/service/resiliencehub/testdata/App/tags_defaults/main_gen.tf
@@ -8,12 +8,12 @@ provider "aws" {
 }
 
 resource "aws_resiliencehub_app" "test" {
-  name                    = var.rName
+  name = var.rName
   app_assessment_schedule = "Disabled"
 
   app_template {
     version = "2.0"
-
+    
     resource {
       name = "test-resource"
       type = "AWS::Lambda::Function"
@@ -21,16 +21,16 @@ resource "aws_resiliencehub_app" "test" {
         identifier = "TestResource"
       }
     }
-
+    
     app_component {
-      name           = "appcommon"
-      type           = "AWS::ResilienceHub::AppCommonAppComponent"
+      name = "appcommon"
+      type = "AWS::ResilienceHub::AppCommonAppComponent"
       resource_names = []
     }
-
+    
     app_component {
-      name           = "test-component"
-      type           = "AWS::ResilienceHub::ComputeAppComponent"
+      name = "test-component"
+      type = "AWS::ResilienceHub::ComputeAppComponent"
       resource_names = ["test-resource"]
     }
   }
