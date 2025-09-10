80a65b5a6b2fb6f16d428daef04d20e7b6934be7
Mario Román Dono <mromandono@twilio.com>
2025-09-10 17:45:31 +0200

    Add TestAccLogsTransformer_listToMap

--- internal/service/logs/transformer_test.go
+++ internal/service/logs/transformer_test.go
@@ -430,6 +430,54 @@ func TestAccLogsTransformer_grokWithSource(t *testing.T) {
 	})
 }
 
+func TestAccLogsTransformer_listToMap(t *testing.T) {
+	ctx := acctest.Context(t)
+
+	var transformer cloudwatchlogs.GetTransformerOutput
+	rName := acctest.RandomWithPrefix(t, acctest.ResourcePrefix)
+	resourceName := "aws_cloudwatch_log_transformer.test"
+	logGroupResourceName := "aws_cloudwatch_log_group.test"
+
+	acctest.ParallelTest(ctx, t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+			acctest.PreCheckPartitionHasService(t, names.CloudWatchEndpointID)
+		},
+		ErrorCheck:               acctest.ErrorCheck(t, names.LogsServiceID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckTransformerDestroy(ctx, t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccTransformerConfig_listToMap(rName),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccCheckTransformerExists(ctx, t, resourceName, &transformer),
+					resource.TestCheckResourceAttrPair(resourceName, "log_group_identifier", logGroupResourceName, names.AttrName),
+					resource.TestCheckResourceAttr(resourceName, "transformer_config.#", "3"),
+					resource.TestCheckResourceAttr(resourceName, "transformer_config.0.parse_json.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "transformer_config.1.list_to_map.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "transformer_config.1.list_to_map.0.key", "key1"),
+					resource.TestCheckResourceAttr(resourceName, "transformer_config.1.list_to_map.0.source", "source1"),
+					resource.TestCheckResourceAttr(resourceName, "transformer_config.1.list_to_map.0.flatten", acctest.CtFalse),
+					resource.TestCheckResourceAttr(resourceName, "transformer_config.2.list_to_map.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "transformer_config.2.list_to_map.0.key", "key2"),
+					resource.TestCheckResourceAttr(resourceName, "transformer_config.2.list_to_map.0.source", "source2"),
+					resource.TestCheckResourceAttr(resourceName, "transformer_config.2.list_to_map.0.flatten", acctest.CtTrue),
+					resource.TestCheckResourceAttr(resourceName, "transformer_config.2.list_to_map.0.flattened_element", "first"),
+					resource.TestCheckResourceAttr(resourceName, "transformer_config.2.list_to_map.0.target", "target1"),
+					resource.TestCheckResourceAttr(resourceName, "transformer_config.2.list_to_map.0.value_key", "valueKey1"),
+				),
+			},
+			{
+				ResourceName:                         resourceName,
+				ImportState:                          true,
+				ImportStateIdFunc:                    testAccTransformerImportStateIdFunc(resourceName),
+				ImportStateVerify:                    true,
+				ImportStateVerifyIdentifierAttribute: "log_group_identifier",
+			},
+		},
+	})
+}
+
 func testAccTransformerImportStateIdFunc(resourceName string) resource.ImportStateIdFunc {
 	return func(s *terraform.State) (string, error) {
 		rs, ok := s.RootModule().Resources[resourceName]
@@ -675,6 +723,40 @@ resource "aws_cloudwatch_log_transformer" "test" {
 	grok {
 		match  = "pattern"
 		source = "source1"
+	}
+  }
+}
+`, rName)
+}
+
+func testAccTransformerConfig_listToMap(rName string) string {
+	return fmt.Sprintf(`
+resource "aws_cloudwatch_log_group" "test" {
+  name = %[1]q
+}
+  
+resource "aws_cloudwatch_log_transformer" "test" {
+  log_group_identifier = aws_cloudwatch_log_group.test.name
+
+  transformer_config {
+	parse_json {}
+  }
+
+  transformer_config {
+    list_to_map {
+		key    = "key1"
+		source = "source1"
+	}
+  }
+
+  transformer_config {
+    list_to_map {
+		key               = "key2"
+		source            = "source2"
+		flatten           = true
+		flattened_element = "first"
+		target            = "target1"
+		value_key         = "valueKey1"
 	}
   }
 }
