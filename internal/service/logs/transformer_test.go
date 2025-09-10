c629e71805667816b66f28bda221269c66791872
Mario Román Dono <mromandono@twilio.com>
2025-09-10 17:26:12 +0200

    Add TestAccLogsTransformer_grokWithSource

--- internal/service/logs/transformer_test.go
+++ internal/service/logs/transformer_test.go
@@ -391,6 +391,45 @@ func TestAccLogsTransformer_grok(t *testing.T) {
 	})
 }
 
+func TestAccLogsTransformer_grokWithSource(t *testing.T) {
+	ctx := acctest.Context(t)
+
+	var transformer cloudwatchlogs.GetTransformerOutput
+	rName := acctest.RandomWithPrefix(t, acctest.ResourcePrefix)
+	resourceName := "aws_cloudwatch_log_transformer.test"
+	logGroupResourceName := "aws_cloudwatch_log_group.test"
+
+	acctest.ParallelTest(ctx, t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+			acctest.PreCheckPartitionHasService(t, names.CloudWatchEndpointID)
+		},
+		ErrorCheck:               acctest.ErrorCheck(t, names.LogsServiceID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckTransformerDestroy(ctx, t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccTransformerConfig_grokWithSource(rName),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccCheckTransformerExists(ctx, t, resourceName, &transformer),
+					resource.TestCheckResourceAttrPair(resourceName, "log_group_identifier", logGroupResourceName, names.AttrName),
+					resource.TestCheckResourceAttr(resourceName, "transformer_config.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "transformer_config.0.grok.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "transformer_config.0.grok.0.match", "pattern"),
+					resource.TestCheckResourceAttr(resourceName, "transformer_config.0.grok.0.source", "source1"),
+				),
+			},
+			{
+				ResourceName:                         resourceName,
+				ImportState:                          true,
+				ImportStateIdFunc:                    testAccTransformerImportStateIdFunc(resourceName),
+				ImportStateVerify:                    true,
+				ImportStateVerifyIdentifierAttribute: "log_group_identifier",
+			},
+		},
+	})
+}
+
 func testAccTransformerImportStateIdFunc(resourceName string) resource.ImportStateIdFunc {
 	return func(s *terraform.State) (string, error) {
 		rs, ok := s.RootModule().Resources[resourceName]
@@ -583,6 +622,28 @@ resource "aws_cloudwatch_log_transformer" "test" {
 `, rName)
 }
 
+func testAccTransformerConfig_deleteKeys(rName string) string {
+	return fmt.Sprintf(`
+resource "aws_cloudwatch_log_group" "test" {
+  name = %[1]q
+}
+
+resource "aws_cloudwatch_log_transformer" "test" {
+  log_group_identifier = aws_cloudwatch_log_group.test.name
+
+  transformer_config {
+	parse_json {}
+  }
+
+  transformer_config {
+    delete_keys {
+		with_keys = ["key1", "key2"]
+	}
+  }
+}
+`, rName)
+}
+
 func testAccTransformerConfig_grok(rName string) string {
 	return fmt.Sprintf(`
 resource "aws_cloudwatch_log_group" "test" {
@@ -601,8 +662,7 @@ resource "aws_cloudwatch_log_transformer" "test" {
 `, rName)
 }
 
-
-func testAccTransformerConfig_deleteKeys(rName string) string {
+func testAccTransformerConfig_grokWithSource(rName string) string {
 	return fmt.Sprintf(`
 resource "aws_cloudwatch_log_group" "test" {
   name = %[1]q
@@ -612,12 +672,9 @@ resource "aws_cloudwatch_log_transformer" "test" {
   log_group_identifier = aws_cloudwatch_log_group.test.name
 
   transformer_config {
-	parse_json {}
-  }
-
-  transformer_config {
-    delete_keys {
-		with_keys = ["key1", "key2"]
+	grok {
+		match  = "pattern"
+		source = "source1"
 	}
   }
 }
