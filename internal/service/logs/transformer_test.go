f09d5be89aee4585841cd09ec8ad2223f089bdb4
Mario Román Dono <mromandono@twilio.com>
2025-09-10 17:16:50 +0200

    Add TestAccLogsTransformer_deleteKeys

--- internal/service/logs/transformer_test.go
+++ internal/service/logs/transformer_test.go
@@ -312,6 +312,47 @@ func TestAccLogsTransformer_dateTimeConverter(t *testing.T) {
 	})
 }
 
+func TestAccLogsTransformer_deleteKeys(t *testing.T) {
+	ctx := acctest.Context(t)
+
+	var transformer cloudwatchlogs.GetTransformerOutput
+	rName := acctest.RandomWithPrefix(t, acctest.ResourcePrefix)
+	resourceName := "aws_cloudwatch_log_transformer.test"
+	logGroupResourceName := "aws_cloudwatch_log_group.test"
+
+	acctest.ParallelTest(ctx, t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+			acctest.PreCheckPartitionHasService(t, names.CloudWatchEndpointID)
+		},
+		ErrorCheck:               acctest.ErrorCheck(t, names.LogsServiceID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckTransformerDestroy(ctx, t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccTransformerConfig_deleteKeys(rName),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccCheckTransformerExists(ctx, t, resourceName, &transformer),
+					resource.TestCheckResourceAttrPair(resourceName, "log_group_identifier", logGroupResourceName, names.AttrName),
+					resource.TestCheckResourceAttr(resourceName, "transformer_config.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "transformer_config.0.parse_json.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "transformer_config.1.delete_keys.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "transformer_config.1.delete_keys.0.with_keys.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "transformer_config.1.delete_keys.0.with_keys.0", "key1"),
+					resource.TestCheckResourceAttr(resourceName, "transformer_config.1.delete_keys.0.with_keys.1", "key2"),
+				),
+			},
+			{
+				ResourceName:                         resourceName,
+				ImportState:                          true,
+				ImportStateIdFunc:                    testAccTransformerImportStateIdFunc(resourceName),
+				ImportStateVerify:                    true,
+				ImportStateVerifyIdentifierAttribute: "log_group_identifier",
+			},
+		},
+	})
+}
+
 func testAccTransformerImportStateIdFunc(resourceName string) resource.ImportStateIdFunc {
 	return func(s *terraform.State) (string, error) {
 		rs, ok := s.RootModule().Resources[resourceName]
@@ -498,6 +539,28 @@ resource "aws_cloudwatch_log_transformer" "test" {
 		source_timezone = "Europe/Paris"
 		target_format   = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
 		target_timezone = "America/Chicago"
+	}
+  }
+}
+`, rName)
+}
+
+func testAccTransformerConfig_deleteKeys(rName string) string {
+	return fmt.Sprintf(`
+resource "aws_cloudwatch_log_group" "test" {
+  name = %[1]q
+}
+
+resource "aws_cloudwatch_log_transformer" "test" {
+  log_group_identifier = aws_cloudwatch_log_group.test.name
+
+  transformer_config {
+	parse_json {}
+  }
+
+  transformer_config {
+    delete_keys {
+		with_keys = ["key1", "key2"]
 	}
   }
 }
