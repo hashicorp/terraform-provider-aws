9eb01feb6127bb605b254d65e88a1a4fc3729851
Dirk Avery <dirk.avery@gmail.com>
2025-09-17 12:02:14 -0400

    Add test guard by TF version

--- internal/service/sns/publish_action_test.go
+++ internal/service/sns/publish_action_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/hashicorp/terraform-plugin-testing/tfversion"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/names"
@@ -25,7 +26,10 @@ func TestAccSNSPublishAction_basic(t *testing.T) {
 		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
 		ErrorCheck:               acctest.ErrorCheck(t, "sns"),
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
-		CheckDestroy:             nil,
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.SkipBelow(tfversion.Version1_14_0),
+		},
+		CheckDestroy: nil,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccPublishActionConfig_basic(rName),
@@ -45,7 +49,10 @@ func TestAccSNSPublishAction_withAttributes(t *testing.T) {
 		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
 		ErrorCheck:               acctest.ErrorCheck(t, "sns"),
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
-		CheckDestroy:             nil,
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.SkipBelow(tfversion.Version1_14_0),
+		},
+		CheckDestroy: nil,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccPublishActionConfig_withAttributes(rName),
