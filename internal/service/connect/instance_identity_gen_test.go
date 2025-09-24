4451034c22c2cb11b4a09f196bab8bfca186bdb5
Jared Baker <jared.baker@hashicorp.com>
2025-09-24 13:44:50 -0400

    r/aws_connect_instance(test): re-gen identity tests after merge
    ```console
    % make t K=connect T=TestAccConnect_serial/Instance/identity
    make: Verifying source code with gofmt...
    ==> Checking that code complies with gofmt requirements...
    make: Running acceptance tests on branch: 🌿 f-ri-connect-instance 🌿...
    TF_ACC=1 go1.24.6 test ./internal/service/connect/... -v -count 1 -parallel 20 -run='TestAccConnect_serial/Instance/identity'  -timeout 360m -vet=off
    2025/09/24 13:44:41 Creating Terraform AWS Provider (SDKv2-style)...
    2025/09/24 13:44:41 Initializing Terraform AWS Provider (SDKv2-style)...
    
    --- PASS: TestAccConnect_serial (412.96s)
        --- PASS: TestAccConnect_serial/Instance (412.96s)
            --- PASS: TestAccConnect_serial/Instance/identityBasic (94.67s)
            --- PASS: TestAccConnect_serial/Instance/identityExistingResource (112.39s)
            --- PASS: TestAccConnect_serial/Instance/identityExistingResourceNoRefresh (107.62s)
            --- PASS: TestAccConnect_serial/Instance/identityRegionOverride (98.28s)
        --- PASS: TestAccConnect_serial/InstanceStorageConfig (0.00s)
    PASS
    ok      github.com/hashicorp/terraform-provider-aws/internal/service/connect    419.612s
    ```

--- internal/service/connect/instance_identity_gen_test.go
+++ internal/service/connect/instance_identity_gen_test.go
@@ -261,3 +261,61 @@ func testAccConnectInstance_Identity_ExistingResource(t *testing.T) {
 		},
 	})
 }
+
+// Resource Identity was added after v6.14.1
+func testAccConnectInstance_Identity_ExistingResource_NoRefresh_NoChange(t *testing.T) {
+	ctx := acctest.Context(t)
+
+	var v types.Instance
+	resourceName := "aws_connect_instance.test"
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+
+	acctest.Test(ctx, t, resource.TestCase{
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.SkipBelow(tfversion.Version1_12_0),
+		},
+		PreCheck:     func() { acctest.PreCheck(ctx, t) },
+		ErrorCheck:   acctest.ErrorCheck(t, names.ConnectServiceID),
+		CheckDestroy: testAccCheckInstanceDestroy(ctx),
+		AdditionalCLIOptions: &resource.AdditionalCLIOptions{
+			Plan: resource.PlanOptions{
+				NoRefresh: true,
+			},
+		},
+		Steps: []resource.TestStep{
+			// Step 1: Create pre-Identity
+			{
+				ConfigDirectory: config.StaticDirectory("testdata/Instance/basic_v6.14.1/"),
+				ConfigVariables: config.Variables{
+					acctest.CtRName: config.StringVariable(rName),
+				},
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccCheckInstanceExists(ctx, resourceName, &v),
+				),
+				ConfigStateChecks: []statecheck.StateCheck{
+					tfstatecheck.ExpectNoIdentity(resourceName),
+				},
+			},
+
+			// Step 2: Current version
+			{
+				ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+				ConfigDirectory:          config.StaticDirectory("testdata/Instance/basic/"),
+				ConfigVariables: config.Variables{
+					acctest.CtRName: config.StringVariable(rName),
+				},
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceName, plancheck.ResourceActionNoop),
+					},
+					PostApplyPostRefresh: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceName, plancheck.ResourceActionNoop),
+					},
+				},
+				ConfigStateChecks: []statecheck.StateCheck{
+					tfstatecheck.ExpectNoIdentity(resourceName),
+				},
+			},
+		},
+	})
+}
