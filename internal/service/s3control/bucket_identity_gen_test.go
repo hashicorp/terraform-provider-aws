484c5919bdc9f9d1af2b7fb57be2a806ed4204bb
Jared Baker <jared.baker@hashicorp.com>
2025-09-24 15:12:26 -0400

    r/aws_s3control_bucket(test): re-generate identity tests after merge
    ```console
    % make t K=s3control T=TestAccS3ControlBucket_Identity
    make: Verifying source code with gofmt...
    ==> Checking that code complies with gofmt requirements...
    make: Running acceptance tests on branch: 🌿 f-ri-s3control 🌿...
    TF_ACC=1 go1.24.6 test ./internal/service/s3control/... -v -count 1 -parallel 20 -run='TestAccS3ControlBucket_Identity'  -timeout 360m -vet=off
    2025/09/24 15:12:07 Creating Terraform AWS Provider (SDKv2-style)...
    2025/09/24 15:12:07 Initializing Terraform AWS Provider (SDKv2-style)...
    
    === NAME  TestAccS3ControlBucket_Identity_RegionOverride
        bucket_identity_gen_test.go:122: skipping since no Outposts found
    --- SKIP: TestAccS3ControlBucket_Identity_RegionOverride (0.90s)
    === NAME  TestAccS3ControlBucket_Identity_ExistingResource
        bucket_identity_gen_test.go:246: skipping since no Outposts found
    --- SKIP: TestAccS3ControlBucket_Identity_ExistingResource (0.90s)
    === NAME  TestAccS3ControlBucket_Identity_Basic
        bucket_identity_gen_test.go:34: skipping since no Outposts found
    --- SKIP: TestAccS3ControlBucket_Identity_Basic (0.90s)
    === NAME  TestAccS3ControlBucket_Identity_ExistingResource_NoRefresh_NoChange
        bucket_identity_gen_test.go:304: skipping since no Outposts found
    --- SKIP: TestAccS3ControlBucket_Identity_ExistingResource_NoRefresh_NoChange (0.95s)
    PASS
    ok      github.com/hashicorp/terraform-provider-aws/internal/service/s3control  7.658s
    ```

--- internal/service/s3control/bucket_identity_gen_test.go
+++ internal/service/s3control/bucket_identity_gen_test.go
@@ -287,3 +287,63 @@ func TestAccS3ControlBucket_Identity_ExistingResource(t *testing.T) {
 		},
 	})
 }
+
+// Resource Identity was added after v6.14.1
+func TestAccS3ControlBucket_Identity_ExistingResource_NoRefresh_NoChange(t *testing.T) {
+	ctx := acctest.Context(t)
+
+	resourceName := "aws_s3control_bucket.test"
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+
+	acctest.ParallelTest(ctx, t, resource.TestCase{
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.SkipBelow(tfversion.Version1_12_0),
+		},
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+			acctest.PreCheckOutpostsOutposts(ctx, t)
+		},
+		ErrorCheck:   acctest.ErrorCheck(t, names.S3ControlServiceID),
+		CheckDestroy: testAccCheckBucketDestroy(ctx),
+		AdditionalCLIOptions: &resource.AdditionalCLIOptions{
+			Plan: resource.PlanOptions{
+				NoRefresh: true,
+			},
+		},
+		Steps: []resource.TestStep{
+			// Step 1: Create pre-Identity
+			{
+				ConfigDirectory: config.StaticDirectory("testdata/Bucket/basic_v6.14.1/"),
+				ConfigVariables: config.Variables{
+					acctest.CtRName: config.StringVariable(rName),
+				},
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccCheckBucketExists(ctx, resourceName),
+				),
+				ConfigStateChecks: []statecheck.StateCheck{
+					tfstatecheck.ExpectNoIdentity(resourceName),
+				},
+			},
+
+			// Step 2: Current version
+			{
+				ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+				ConfigDirectory:          config.StaticDirectory("testdata/Bucket/basic/"),
+				ConfigVariables: config.Variables{
+					acctest.CtRName: config.StringVariable(rName),
+				},
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceName, plancheck.ResourceActionNoop),
+					},
+					PostApplyPostRefresh: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceName, plancheck.ResourceActionNoop),
+					},
+				},
+				ConfigStateChecks: []statecheck.StateCheck{
+					tfstatecheck.ExpectNoIdentity(resourceName),
+				},
+			},
+		},
+	})
+}
