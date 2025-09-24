45b0c515b174f666ed46a52802c8a99394b84db4
Jared Baker <jared.baker@hashicorp.com>
2025-09-24 13:51:28 -0400

    r/aws_sfn_alias(test): re-generate test configs after merge
    ```console
    % make t K=sfn T="TestAccSFN.*_Identity"
    make: Verifying source code with gofmt...
    ==> Checking that code complies with gofmt requirements...
    make: Running acceptance tests on branch: 🌿 f-ri-sfn 🌿...
    TF_ACC=1 go1.24.6 test ./internal/service/sfn/... -v -count 1 -parallel 20 -run='TestAccSFN.*_Identity'  -timeout 360m -vet=off
    2025/09/24 13:56:42 Creating Terraform AWS Provider (SDKv2-style)...
    2025/09/24 13:56:42 Initializing Terraform AWS Provider (SDKv2-style)...
    
    --- PASS: TestAccSFNActivity_Identity_RegionOverride (34.94s)
    --- PASS: TestAccSFNActivity_Identity_Basic (76.21s)
    --- PASS: TestAccSFNStateMachine_Identity_RegionOverride (87.41s)
    --- PASS: TestAccSFNStateMachine_Identity_ExistingResource_NoRefresh_NoChange (108.40s)
    --- PASS: TestAccSFNActivity_Identity_ExistingResource_NoRefresh_NoChange (113.45s)
    --- PASS: TestAccSFNAlias_Identity_ExistingResource_NoRefresh_NoChange (115.30s)
    --- PASS: TestAccSFNAlias_Identity_RegionOverride (127.72s)
    --- PASS: TestAccSFNActivity_Identity_ExistingResource (131.49s)
    --- PASS: TestAccSFNStateMachine_Identity_ExistingResource (139.07s)
    --- PASS: TestAccSFNAlias_Identity_ExistingResource (160.29s)
    --- PASS: TestAccSFNStateMachine_Identity_Basic (179.47s)
    --- PASS: TestAccSFNAlias_Identity_Basic (204.85s)
    PASS
    ok      github.com/hashicorp/terraform-provider-aws/internal/service/sfn        211.672s
    ```

--- internal/service/sfn/activity_identity_gen_test.go
+++ internal/service/sfn/activity_identity_gen_test.go
@@ -278,3 +278,60 @@ func TestAccSFNActivity_Identity_ExistingResource(t *testing.T) {
 		},
 	})
 }
+
+// Resource Identity was added after v6.14.1
+func TestAccSFNActivity_Identity_ExistingResource_NoRefresh_NoChange(t *testing.T) {
+	ctx := acctest.Context(t)
+
+	resourceName := "aws_sfn_activity.test"
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+
+	acctest.ParallelTest(ctx, t, resource.TestCase{
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.SkipBelow(tfversion.Version1_12_0),
+		},
+		PreCheck:     func() { acctest.PreCheck(ctx, t) },
+		ErrorCheck:   acctest.ErrorCheck(t, names.SFNServiceID),
+		CheckDestroy: testAccCheckActivityDestroy(ctx),
+		AdditionalCLIOptions: &resource.AdditionalCLIOptions{
+			Plan: resource.PlanOptions{
+				NoRefresh: true,
+			},
+		},
+		Steps: []resource.TestStep{
+			// Step 1: Create pre-Identity
+			{
+				ConfigDirectory: config.StaticDirectory("testdata/Activity/basic_v6.14.1/"),
+				ConfigVariables: config.Variables{
+					acctest.CtRName: config.StringVariable(rName),
+				},
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccCheckActivityExists(ctx, resourceName),
+				),
+				ConfigStateChecks: []statecheck.StateCheck{
+					tfstatecheck.ExpectNoIdentity(resourceName),
+				},
+			},
+
+			// Step 2: Current version
+			{
+				ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+				ConfigDirectory:          config.StaticDirectory("testdata/Activity/basic/"),
+				ConfigVariables: config.Variables{
+					acctest.CtRName: config.StringVariable(rName),
+				},
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceName, plancheck.ResourceActionNoop),
+					},
+					PostApplyPostRefresh: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceName, plancheck.ResourceActionNoop),
+					},
+				},
+				ConfigStateChecks: []statecheck.StateCheck{
+					tfstatecheck.ExpectNoIdentity(resourceName),
+				},
+			},
+		},
+	})
+}
--- internal/service/sfn/alias_identity_gen_test.go
+++ internal/service/sfn/alias_identity_gen_test.go
@@ -281,3 +281,61 @@ func TestAccSFNAlias_Identity_ExistingResource(t *testing.T) {
 		},
 	})
 }
+
+// Resource Identity was added after v6.14.1
+func TestAccSFNAlias_Identity_ExistingResource_NoRefresh_NoChange(t *testing.T) {
+	ctx := acctest.Context(t)
+
+	var v sfn.DescribeStateMachineAliasOutput
+	resourceName := "aws_sfn_alias.test"
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+
+	acctest.ParallelTest(ctx, t, resource.TestCase{
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.SkipBelow(tfversion.Version1_12_0),
+		},
+		PreCheck:     func() { acctest.PreCheck(ctx, t) },
+		ErrorCheck:   acctest.ErrorCheck(t, names.SFNServiceID),
+		CheckDestroy: testAccCheckAliasDestroy(ctx),
+		AdditionalCLIOptions: &resource.AdditionalCLIOptions{
+			Plan: resource.PlanOptions{
+				NoRefresh: true,
+			},
+		},
+		Steps: []resource.TestStep{
+			// Step 1: Create pre-Identity
+			{
+				ConfigDirectory: config.StaticDirectory("testdata/Alias/basic_v6.14.1/"),
+				ConfigVariables: config.Variables{
+					acctest.CtRName: config.StringVariable(rName),
+				},
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccCheckAliasExists(ctx, resourceName, &v),
+				),
+				ConfigStateChecks: []statecheck.StateCheck{
+					tfstatecheck.ExpectNoIdentity(resourceName),
+				},
+			},
+
+			// Step 2: Current version
+			{
+				ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+				ConfigDirectory:          config.StaticDirectory("testdata/Alias/basic/"),
+				ConfigVariables: config.Variables{
+					acctest.CtRName: config.StringVariable(rName),
+				},
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceName, plancheck.ResourceActionNoop),
+					},
+					PostApplyPostRefresh: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceName, plancheck.ResourceActionNoop),
+					},
+				},
+				ConfigStateChecks: []statecheck.StateCheck{
+					tfstatecheck.ExpectNoIdentity(resourceName),
+				},
+			},
+		},
+	})
+}
