521b5e01a691b7e2d7250e08dd3aca1766584e78
Jared Baker <jared.baker@hashicorp.com>
2025-09-24 13:40:25 -0400

    r/aws_connect_phone_number(test): re-gen identity tests after merge
    ```console
    % make t K=connect T=TestAccConnect_serial/PhoneNumber/identity
    make: Verifying source code with gofmt...
    ==> Checking that code complies with gofmt requirements...
    make: Running acceptance tests on branch: 🌿 f-ri-connect-phone-number 🌿...
    TF_ACC=1 go1.24.6 test ./internal/service/connect/... -v -count 1 -parallel 20 -run='TestAccConnect_serial/PhoneNumber/identity'  -timeout 360m -vet=off
    2025/09/24 13:31:56 Creating Terraform AWS Provider (SDKv2-style)...
    2025/09/24 13:31:56 Initializing Terraform AWS Provider (SDKv2-style)...
    
    --- PASS: TestAccConnect_serial (421.82s)
        --- PASS: TestAccConnect_serial/PhoneNumber (421.82s)
            --- PASS: TestAccConnect_serial/PhoneNumber/identityBasic (99.21s)
            --- PASS: TestAccConnect_serial/PhoneNumber/identityExistingResource (118.46s)
            --- PASS: TestAccConnect_serial/PhoneNumber/identityExistingResourceNoRefresh (113.47s)
            --- PASS: TestAccConnect_serial/PhoneNumber/identityRegionOverride (90.68s)
        --- PASS: TestAccConnect_serial/PhoneNumberContactFlowAssociation (0.00s)
    PASS
    ok      github.com/hashicorp/terraform-provider-aws/internal/service/connect    428.395s
    ```

--- internal/service/connect/phone_number_identity_gen_test.go
+++ internal/service/connect/phone_number_identity_gen_test.go
@@ -261,3 +261,61 @@ func testAccConnectPhoneNumber_Identity_ExistingResource(t *testing.T) {
 		},
 	})
 }
+
+// Resource Identity was added after v6.14.1
+func testAccConnectPhoneNumber_Identity_ExistingResource_NoRefresh_NoChange(t *testing.T) {
+	ctx := acctest.Context(t)
+
+	var v types.ClaimedPhoneNumberSummary
+	resourceName := "aws_connect_phone_number.test"
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+
+	acctest.Test(ctx, t, resource.TestCase{
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.SkipBelow(tfversion.Version1_12_0),
+		},
+		PreCheck:     func() { acctest.PreCheck(ctx, t) },
+		ErrorCheck:   acctest.ErrorCheck(t, names.ConnectServiceID),
+		CheckDestroy: testAccCheckPhoneNumberDestroy(ctx),
+		AdditionalCLIOptions: &resource.AdditionalCLIOptions{
+			Plan: resource.PlanOptions{
+				NoRefresh: true,
+			},
+		},
+		Steps: []resource.TestStep{
+			// Step 1: Create pre-Identity
+			{
+				ConfigDirectory: config.StaticDirectory("testdata/PhoneNumber/basic_v6.14.1/"),
+				ConfigVariables: config.Variables{
+					acctest.CtRName: config.StringVariable(rName),
+				},
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccCheckPhoneNumberExists(ctx, resourceName, &v),
+				),
+				ConfigStateChecks: []statecheck.StateCheck{
+					tfstatecheck.ExpectNoIdentity(resourceName),
+				},
+			},
+
+			// Step 2: Current version
+			{
+				ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+				ConfigDirectory:          config.StaticDirectory("testdata/PhoneNumber/basic/"),
+				ConfigVariables: config.Variables{
+					acctest.CtRName: config.StringVariable(rName),
+				},
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceName, plancheck.ResourceActionNoop),
+					},
+					PostApplyPostRefresh: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceName, plancheck.ResourceActionNoop),
+					},
+				},
+				ConfigStateChecks: []statecheck.StateCheck{
+					tfstatecheck.ExpectNoIdentity(resourceName),
+				},
+			},
+		},
+	})
+}
