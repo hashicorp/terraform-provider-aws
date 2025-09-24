f59b98e3e921402320ea33566d7cf38f051c884a
anurag-hashicorp <anurag.upadhaya@hashicorp.com>
2025-09-25 01:08:19 +0530

    r/aws_cleanrooms_configured_table: add resource identity
    ➜ terraform-provider-aws (f-ri-cleanrooms-configured-table) ✗ make testacc PKG=cleanrooms TESTS=TestAccCleanRoomsConfiguredTable_Identity_
    make: Verifying source code with gofmt...
    ==> Checking that code complies with gofmt requirements...
    make: Running acceptance tests on branch: 🌿 f-ri-cleanrooms-configured-table 🌿...
    TF_ACC=1 go1.24.6 test ./internal/service/cleanrooms/... -v -count 1 -parallel 20 -run='TestAccCleanRoomsConfiguredTable_Identity_'  -timeout 360m -vet=off
    2025/09/24 20:25:45 Creating Terraform AWS Provider (SDKv2-style)...
    2025/09/24 20:25:45 Initializing Terraform AWS Provider (SDKv2-style)...
    === RUN   TestAccCleanRoomsConfiguredTable_Identity_Basic
    === PAUSE TestAccCleanRoomsConfiguredTable_Identity_Basic
    === RUN   TestAccCleanRoomsConfiguredTable_Identity_RegionOverride
    === PAUSE TestAccCleanRoomsConfiguredTable_Identity_RegionOverride
    === RUN   TestAccCleanRoomsConfiguredTable_Identity_ExistingResource
    === PAUSE TestAccCleanRoomsConfiguredTable_Identity_ExistingResource
    === RUN   TestAccCleanRoomsConfiguredTable_Identity_ExistingResource_NoRefresh_NoChange
    === PAUSE TestAccCleanRoomsConfiguredTable_Identity_ExistingResource_NoRefresh_NoChange
    === CONT  TestAccCleanRoomsConfiguredTable_Identity_Basic
    === CONT  TestAccCleanRoomsConfiguredTable_Identity_ExistingResource
    === CONT  TestAccCleanRoomsConfiguredTable_Identity_RegionOverride
    === CONT  TestAccCleanRoomsConfiguredTable_Identity_ExistingResource_NoRefresh_NoChange
    --- PASS: TestAccCleanRoomsConfiguredTable_Identity_Basic (54.29s)
    --- PASS: TestAccCleanRoomsConfiguredTable_Identity_ExistingResource_NoRefresh_NoChange (56.59s)
    --- PASS: TestAccCleanRoomsConfiguredTable_Identity_RegionOverride (57.40s)
    --- PASS: TestAccCleanRoomsConfiguredTable_Identity_ExistingResource (80.42s)
    PASS
    ok  	github.com/hashicorp/terraform-provider-aws/internal/service/cleanrooms	85.109s

--- internal/service/cleanrooms/service_package_gen.go
+++ internal/service/cleanrooms/service_package_gen.go
@@ -57,7 +57,11 @@ func (p *servicePackage) SDKResources(ctx context.Context) []*inttypes.ServicePa
 			Tags: unique.Make(inttypes.ServicePackageResourceTags{
 				IdentifierAttribute: names.AttrARN,
 			}),
-			Region: unique.Make(inttypes.ResourceRegionDefault()),
+			Region:   unique.Make(inttypes.ResourceRegionDefault()),
+			Identity: inttypes.RegionalSingleParameterIdentity(names.AttrID),
+			Import: inttypes.SDKv2Import{
+				WrappedImport: true,
+			},
 		},
 	}
 }
---
+++ internal/service/cleanrooms/testdata/ConfiguredTable/basic/main_gen.tf
@@ -0,0 +1,49 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+resource "aws_s3_bucket" "test" {
+  bucket = var.rName
+}
+
+resource "aws_glue_catalog_database" "test" {
+  name = var.rName
+}
+
+resource "aws_glue_catalog_table" "test" {
+  name          = var.rName
+  database_name = var.rName
+
+  storage_descriptor {
+    location = "s3://${aws_s3_bucket.test.bucket}"
+
+    columns {
+      name = "my_column_1"
+      type = "string"
+    }
+
+    columns {
+      name = "my_column_2"
+      type = "string"
+    }
+  }
+}
+
+resource "aws_cleanrooms_configured_table" "test" {
+  name            = "test-name"
+  description     = "test description"
+  analysis_method = "DIRECT_QUERY"
+  allowed_columns = ["my_column_1", "my_column_2"]
+
+  table_reference {
+    database_name = var.rName
+    table_name    = var.rName
+  }
+
+  depends_on = [aws_glue_catalog_table.test]
+}
+
+variable "rName" {
+  description = "Name for resource"
+  type        = string
+  nullable    = false
+}
