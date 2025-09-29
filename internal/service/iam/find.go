ea1c81486e5ded81a32800f71504f6dbdf1964c1
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-09-29 09:21:10 -0400

    iam: 'FindUsers' -> 'findUsers'.

--- internal/service/iam/find.go
+++ internal/service/iam/find.go
@@ -7,7 +7,6 @@ import (
 	"context"
 	"reflect"
 
-	"github.com/YakDriver/regexache"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/iam"
 	awstypes "github.com/aws/aws-sdk-go-v2/service/iam/types"
@@ -15,34 +14,6 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 )
-
-func FindUsers(ctx context.Context, conn *iam.Client, nameRegex, pathPrefix string) ([]awstypes.User, error) {
-	input := &iam.ListUsersInput{}
-
-	if pathPrefix != "" {
-		input.PathPrefix = aws.String(pathPrefix)
-	}
-
-	var results []awstypes.User
-
-	pages := iam.NewListUsersPaginator(conn, input)
-	for pages.HasMorePages() {
-		page, err := pages.NextPage(ctx)
-		if err != nil {
-			return nil, err
-		}
-
-		for _, user := range page.Users {
-			if nameRegex != "" && !regexache.MustCompile(nameRegex).MatchString(aws.ToString(user.UserName)) {
-				continue
-			}
-
-			results = append(results, user)
-		}
-	}
-
-	return results, nil
-}
 
 func FindServiceSpecificCredential(ctx context.Context, conn *iam.Client, serviceName, userName, credID string) (*awstypes.ServiceSpecificCredentialMetadata, error) {
 	input := &iam.ListServiceSpecificCredentialsInput{
