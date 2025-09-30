c6d5514a8c1c9102d2b24208c318fa4e5ee75f69
Graham Davison <gdavison@hashicorp.com>
2025-09-29 17:36:33 -0700

    Adds sweepers for `aws_datazone_environment` and `aws_datazone_environment_profile`

--- internal/service/datazone/sweep.go
+++ internal/service/datazone/sweep.go
@@ -18,16 +18,23 @@ import (
 func RegisterSweepers() {
 	awsv2.Register("aws_datazone_domain", sweepDomains,
 		"aws_datazone_project",
+		"aws_datazone_environment_profile",
 	)
 
-	awsv2.Register("aws_datazone_project", sweepProjects)
+	awsv2.Register("aws_datazone_environment", sweepEnvironments)
+
+	awsv2.Register("aws_datazone_environment_profile", sweepEnvironmentProfiles)
+
+	awsv2.Register("aws_datazone_project", sweepProjects,
+		"aws_datazone_environment",
+	)
 }
 
 func sweepDomains(ctx context.Context, client *conns.AWSClient) ([]sweep.Sweepable, error) {
 	conn := client.DataZoneClient(ctx)
 	var sweepResources []sweep.Sweepable
 
-	input := datazone.ListDomainsInput{}
+	var input datazone.ListDomainsInput
 	pages := datazone.NewListDomainsPaginator(conn, &input)
 	for pages.HasMorePages() {
 		page, err := pages.NextPage(ctx)
@@ -47,11 +54,97 @@ func sweepDomains(ctx context.Context, client *conns.AWSClient) ([]sweep.Sweepab
 	return sweepResources, nil
 }
 
+func sweepEnvironments(ctx context.Context, client *conns.AWSClient) ([]sweep.Sweepable, error) {
+	conn := client.DataZoneClient(ctx)
+	var sweepResources []sweep.Sweepable
+
+	var domainsInput datazone.ListDomainsInput
+	pages := datazone.NewListDomainsPaginator(conn, &domainsInput)
+	for pages.HasMorePages() {
+		page, err := pages.NextPage(ctx)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, domain := range page.Items {
+			projectsInput := datazone.ListProjectsInput{
+				DomainIdentifier: domain.Id,
+			}
+			pages := datazone.NewListProjectsPaginator(conn, &projectsInput)
+			for pages.HasMorePages() {
+				page, err := pages.NextPage(ctx)
+				if err != nil {
+					return nil, err
+				}
+
+				for _, project := range page.Items {
+					environmentsInput := datazone.ListEnvironmentsInput{
+						DomainIdentifier:  domain.Id,
+						ProjectIdentifier: project.Id,
+					}
+					pages := datazone.NewListEnvironmentsPaginator(conn, &environmentsInput)
+					for pages.HasMorePages() {
+						page, err := pages.NextPage(ctx)
+						if err != nil {
+							return nil, err
+						}
+
+						for _, environment := range page.Items {
+							sweepResources = append(sweepResources, framework.NewSweepResource(newEnvironmentResource, client,
+								framework.NewAttribute(names.AttrID, environment.Id),
+								framework.NewAttribute("domain_identifier", domain.Id),
+							))
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return sweepResources, nil
+}
+
+func sweepEnvironmentProfiles(ctx context.Context, client *conns.AWSClient) ([]sweep.Sweepable, error) {
+	conn := client.DataZoneClient(ctx)
+	var sweepResources []sweep.Sweepable
+
+	var domainsInput datazone.ListDomainsInput
+	pages := datazone.NewListDomainsPaginator(conn, &domainsInput)
+	for pages.HasMorePages() {
+		page, err := pages.NextPage(ctx)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, domain := range page.Items {
+			environmentProfilesInput := datazone.ListEnvironmentProfilesInput{
+				DomainIdentifier: domain.Id,
+			}
+			pages := datazone.NewListEnvironmentProfilesPaginator(conn, &environmentProfilesInput)
+			for pages.HasMorePages() {
+				page, err := pages.NextPage(ctx)
+				if err != nil {
+					return nil, err
+				}
+
+				for _, profile := range page.Items {
+					sweepResources = append(sweepResources, framework.NewSweepResource(newEnvironmentProfileResource, client,
+						framework.NewAttribute(names.AttrID, profile.Id),
+						framework.NewAttribute("domain_identifier", domain.Id),
+					))
+				}
+			}
+		}
+	}
+
+	return sweepResources, nil
+}
+
 func sweepProjects(ctx context.Context, client *conns.AWSClient) ([]sweep.Sweepable, error) {
 	conn := client.DataZoneClient(ctx)
 	var sweepResources []sweep.Sweepable
 
-	domainsInput := datazone.ListDomainsInput{}
+	var domainsInput datazone.ListDomainsInput
 	pages := datazone.NewListDomainsPaginator(conn, &domainsInput)
 	for pages.HasMorePages() {
 		page, err := pages.NextPage(ctx)
@@ -74,7 +167,7 @@ func sweepProjects(ctx context.Context, client *conns.AWSClient) ([]sweep.Sweepa
 					sweepResources = append(sweepResources, framework.NewSweepResource(newProjectResource, client,
 						framework.NewAttribute(names.AttrID, project.Id),
 						framework.NewAttribute("domain_identifier", domain.Id),
-						framework.NewAttribute("skip_deletion_check", true),
+						framework.NewAttribute("skip_deletion_check", true), // Automatically delete associated Glossaries
 					))
 				}
 			}
