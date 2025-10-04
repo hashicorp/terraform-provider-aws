a08a1a413fd4311bad9075c23aff95e2cd55176d
Graham Davison <gdavison@hashicorp.com>
2025-10-03 19:52:26 -0700

    Adds region override for `aws_vpc`

--- internal/service/ec2/vpc_list_test.go
+++ internal/service/ec2/vpc_list_test.go
@@ -80,3 +80,71 @@ func TestAccVPC_List_Basic(t *testing.T) {
 		},
 	})
 }
+
+func TestAccVPC_List_RegionOverride(t *testing.T) {
+	ctx := acctest.Context(t)
+
+	resourceName1 := "aws_vpc.test[0]"
+	resourceName2 := "aws_vpc.test[1]"
+	resourceName3 := "aws_vpc.test[2]"
+
+	var id1, id2, id3 string
+
+	acctest.ParallelTest(ctx, t, resource.TestCase{
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.SkipBelow(tfversion.Version1_14_0),
+		},
+		PreCheck:     func() { acctest.PreCheck(ctx, t) },
+		ErrorCheck:   acctest.ErrorCheck(t, names.EC2ServiceID),
+		CheckDestroy: testAccCheckVPCDestroy(ctx),
+		Steps: []resource.TestStep{
+			// Step 1: Setup
+			{
+				ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+				ConfigDirectory:          config.StaticDirectory("testdata/VPC/list_region_override/"),
+				ConfigVariables: config.Variables{
+					"region": config.StringVariable(acctest.AlternateRegion()),
+				},
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrWith("aws_vpc.test.0", names.AttrID, getter(&id1)),
+					resource.TestCheckResourceAttrWith("aws_vpc.test.1", names.AttrID, getter(&id2)),
+					resource.TestCheckResourceAttrWith("aws_vpc.test.2", names.AttrID, getter(&id3)),
+				),
+				ConfigStateChecks: []statecheck.StateCheck{
+					tfstatecheck.ExpectRegionalARNAlternateRegionFormat(resourceName1, tfjsonpath.New(names.AttrARN), "ec2", "vpc/{id}"),
+					tfstatecheck.ExpectRegionalARNAlternateRegionFormat(resourceName2, tfjsonpath.New(names.AttrARN), "ec2", "vpc/{id}"),
+					tfstatecheck.ExpectRegionalARNAlternateRegionFormat(resourceName3, tfjsonpath.New(names.AttrARN), "ec2", "vpc/{id}"),
+				},
+			},
+
+			// Step 2: Query
+			{
+				Query:                    true,
+				ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+				ConfigDirectory:          config.StaticDirectory("testdata/VPC/list_region_override/"),
+				ConfigVariables: config.Variables{
+					"region": config.StringVariable(acctest.AlternateRegion()),
+				},
+				QueryResultChecks: []querycheck.QueryResultCheck{
+					querycheck.ExpectIdentity("aws_vpc.test", map[string]knownvalue.Check{
+						names.AttrAccountID: tfknownvalue.AccountID(),
+						names.AttrRegion:    knownvalue.StringExact(acctest.AlternateRegion()),
+						names.AttrID:        knownvalue.StringFunc(checker(&id1)),
+					}),
+
+					querycheck.ExpectIdentity("aws_vpc.test", map[string]knownvalue.Check{
+						names.AttrAccountID: tfknownvalue.AccountID(),
+						names.AttrRegion:    knownvalue.StringExact(acctest.AlternateRegion()),
+						names.AttrID:        knownvalue.StringFunc(checker(&id2)),
+					}),
+
+					querycheck.ExpectIdentity("aws_vpc.test", map[string]knownvalue.Check{
+						names.AttrAccountID: tfknownvalue.AccountID(),
+						names.AttrRegion:    knownvalue.StringExact(acctest.AlternateRegion()),
+						names.AttrID:        knownvalue.StringFunc(checker(&id3)),
+					}),
+				},
+			},
+		},
+	})
+}
