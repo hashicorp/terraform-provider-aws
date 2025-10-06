31fbd0e82ea8e7d4726f13c257541f92b65dd9e0
Graham Davison <gdavison@hashicorp.com>
2025-10-06 11:54:06 -0700

    Adds test to validate that default VPC is not included

--- internal/service/ec2/vpc_list_test.go
+++ internal/service/ec2/vpc_list_test.go
@@ -205,3 +205,68 @@ func TestAccVPC_List_Filtered(t *testing.T) {
 		},
 	})
 }
+
+func TestAccVPC_List_DefaultVPC_Exclude(t *testing.T) {
+	ctx := acctest.Context(t)
+
+	resourceName1 := "aws_vpc.test[0]"
+	resourceName2 := "aws_vpc.test[1]"
+	resourceName3 := "aws_vpc.test[2]"
+
+	var id1, id2, id3 string
+
+	acctest.ParallelTest(ctx, t, resource.TestCase{
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.SkipBelow(tfversion.Version1_14_0),
+		},
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+			testAccPreCheckDefaultVPCExists(ctx, t)
+		},
+		ErrorCheck:   acctest.ErrorCheck(t, names.EC2ServiceID),
+		CheckDestroy: testAccCheckVPCDestroy(ctx),
+		Steps: []resource.TestStep{
+			// Step 1: Setup
+			{
+				ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+				ConfigDirectory:          config.StaticDirectory("testdata/VPC/list_basic"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrWith("aws_vpc.test.0", names.AttrID, getter(&id1)),
+					resource.TestCheckResourceAttrWith("aws_vpc.test.1", names.AttrID, getter(&id2)),
+					resource.TestCheckResourceAttrWith("aws_vpc.test.2", names.AttrID, getter(&id3)),
+				),
+				ConfigStateChecks: []statecheck.StateCheck{
+					tfstatecheck.ExpectRegionalARNFormat(resourceName1, tfjsonpath.New(names.AttrARN), "ec2", "vpc/{id}"),
+					tfstatecheck.ExpectRegionalARNFormat(resourceName2, tfjsonpath.New(names.AttrARN), "ec2", "vpc/{id}"),
+					tfstatecheck.ExpectRegionalARNFormat(resourceName3, tfjsonpath.New(names.AttrARN), "ec2", "vpc/{id}"),
+				},
+			},
+
+			// Step 2: Query
+			{
+				Query:                    true,
+				ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+				ConfigDirectory:          config.StaticDirectory("testdata/VPC/list_basic"),
+				QueryResultChecks: []querycheck.QueryResultCheck{
+					querycheck.ExpectIdentity("aws_vpc.test", map[string]knownvalue.Check{
+						names.AttrAccountID: tfknownvalue.AccountID(),
+						names.AttrRegion:    knownvalue.StringExact(acctest.Region()),
+						names.AttrID:        knownvalue.StringFunc(checker(&id1)),
+					}),
+
+					querycheck.ExpectIdentity("aws_vpc.test", map[string]knownvalue.Check{
+						names.AttrAccountID: tfknownvalue.AccountID(),
+						names.AttrRegion:    knownvalue.StringExact(acctest.Region()),
+						names.AttrID:        knownvalue.StringFunc(checker(&id2)),
+					}),
+
+					querycheck.ExpectIdentity("aws_vpc.test", map[string]knownvalue.Check{
+						names.AttrAccountID: tfknownvalue.AccountID(),
+						names.AttrRegion:    knownvalue.StringExact(acctest.Region()),
+						names.AttrID:        knownvalue.StringFunc(checker(&id3)),
+					}),
+				},
+			},
+		},
+	})
+}
