5e5acbd439f954ccc96f40e05a00d53934e44a46
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-10-07 13:09:24 -0400

    r/aws_bedrockagentcore_agent_runtime_endpoint: Fix sweeper.

--- internal/service/bedrockagentcore/sweep.go
+++ internal/service/bedrockagentcore/sweep.go
@@ -17,7 +17,7 @@ import (
 )
 
 func RegisterSweepers() {
-	awsv2.Register("aws_bedrockagentcore_agent_runtime", sweepAgentRuntimes)
+	awsv2.Register("aws_bedrockagentcore_agent_runtime", sweepAgentRuntimes, "aws_bedrockagentcore_agent_runtime_endpoint")
 	awsv2.Register("aws_bedrockagentcore_agent_runtime_endpoint", sweepAgentRuntimeEndpoints)
 	awsv2.Register("aws_bedrockagentcore_gateway", sweepGateways, "aws_bedrockagentcore_gateway_target")
 	awsv2.Register("aws_bedrockagentcore_gateway_target", sweepGatewayTargets)
@@ -46,23 +46,38 @@ func sweepAgentRuntimes(ctx context.Context, client *conns.AWSClient) ([]sweep.S
 }
 
 func sweepAgentRuntimeEndpoints(ctx context.Context, client *conns.AWSClient) ([]sweep.Sweepable, error) {
-	input := bedrockagentcorecontrol.ListAgentRuntimeEndpointsInput{}
+	input := bedrockagentcorecontrol.ListAgentRuntimesInput{}
 	conn := client.BedrockAgentCoreClient(ctx)
 	var sweepResources []sweep.Sweepable
 
-	pages := bedrockagentcorecontrol.NewListAgentRuntimeEndpointsPaginator(conn, &input)
+	pages := bedrockagentcorecontrol.NewListAgentRuntimesPaginator(conn, &input)
 	for pages.HasMorePages() {
 		page, err := pages.NextPage(ctx)
 		if err != nil {
 			return nil, smarterr.NewError(err)
 		}
 
-		for _, v := range page.RuntimeEndpoints {
-			sweepResources = append(sweepResources, framework.NewSweepResource(newAgentRuntimeEndpointResource, client,
-				framework.NewAttribute("agent_runtime_id", aws.ToString(v.Id)),
-				framework.NewAttribute(names.AttrName, aws.ToString(v.Name)),
-			),
-			)
+		for _, v := range page.AgentRuntimes {
+			agentRuntimeID := aws.ToString(v.AgentRuntimeId)
+			input := bedrockagentcorecontrol.ListAgentRuntimeEndpointsInput{
+				AgentRuntimeId: aws.String(agentRuntimeID),
+			}
+
+			pages := bedrockagentcorecontrol.NewListAgentRuntimeEndpointsPaginator(conn, &input)
+			for pages.HasMorePages() {
+				page, err := pages.NextPage(ctx)
+				if err != nil {
+					return nil, smarterr.NewError(err)
+				}
+
+				for _, v := range page.RuntimeEndpoints {
+					sweepResources = append(sweepResources, framework.NewSweepResource(newAgentRuntimeEndpointResource, client,
+						framework.NewAttribute("agent_runtime_id", agentRuntimeID),
+						framework.NewAttribute(names.AttrName, aws.ToString(v.Name)),
+					),
+					)
+				}
+			}
 		}
 	}
 
