66b81f4c3c261a9d13e6bb56679bc4f7e5f77a19
Adrian Johnson <adrian.johnson@hashicorp.com>
2025-10-10 11:27:16 -0500

    aws_rds_global_cluster: implement read handler

--- internal/service/rds/global_cluster_data_source_test.go
+++ internal/service/rds/global_cluster_data_source_test.go
@@ -116,7 +116,7 @@ resource "aws_rds_cluster_instance" "secondary" {
 }
 
 data "aws_rds_global_cluster" "test" {
-  global_cluster_identifier = aws_rds_global_cluster.test.global_cluster_identifier
+  identifier = aws_rds_global_cluster.test.global_cluster_identifier
 }
 `, rName))
 }
