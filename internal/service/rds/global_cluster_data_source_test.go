bf7bc11a6f6949ed1efaf7ca823d96d923d949be
Adrian Johnson <adrian.johnson@hashicorp.com>
2025-10-10 10:08:35 -0500

    make testacc-lint-fix

--- internal/service/rds/global_cluster_data_source_test.go
+++ internal/service/rds/global_cluster_data_source_test.go
@@ -48,74 +48,75 @@ func TestAccRDSGlobalClusterDataSource_basic(t *testing.T) {
 func testAccGlobalClusterDataSourceConfig_basic(rName string) string {
 	return acctest.ConfigCompose(acctest.ConfigMultipleRegionProvider(2), fmt.Sprintf(`
 
+
 data "aws_region" "alternate" {
-	provider = "awsalternate"
+  provider = "awsalternate"
 }
-		
+
 data "aws_region" "current" {}
 resource "aws_rds_global_cluster" "test" {
-		global_cluster_identifier = "%[1]s"
-		engine                    = "aurora-postgresql"
-		engine_version            = "15.5"
-		database_name             = "example_db"
+  global_cluster_identifier = "%[1]s"
+  engine                    = "aurora-postgresql"
+  engine_version            = "15.5"
+  database_name             = "example_db"
 }
 
 resource "aws_rds_cluster" "primary" {
-		engine                    = aws_rds_global_cluster.test.engine
-		engine_version            = aws_rds_global_cluster.test.engine_version
-		cluster_identifier        = "test-primary-cluster"
-		master_username           = "username"
-		master_password           = "somepass123"
-		database_name             = "example_db"
-		global_cluster_identifier = aws_rds_global_cluster.test.id
-		skip_final_snapshot       = true
-		
-		depends_on = [
-			aws_rds_global_cluster.test
-		]
+  engine                    = aws_rds_global_cluster.test.engine
+  engine_version            = aws_rds_global_cluster.test.engine_version
+  cluster_identifier        = "test-primary-cluster"
+  master_username           = "username"
+  master_password           = "somepass123"
+  database_name             = "example_db"
+  global_cluster_identifier = aws_rds_global_cluster.test.id
+  skip_final_snapshot       = true
+
+  depends_on = [
+    aws_rds_global_cluster.test
+  ]
 }
 
 resource "aws_rds_cluster_instance" "primary" {
-		identifier           = "test-primary-cluster-instance"
-		cluster_identifier   = aws_rds_cluster.primary.id
-		instance_class       = "db.r5.large"
-		engine               = aws_rds_global_cluster.test.engine
-		engine_version       = aws_rds_global_cluster.test.engine_version
-		
-		depends_on = [
-			aws_rds_cluster.primary
-		]
+  identifier         = "test-primary-cluster-instance"
+  cluster_identifier = aws_rds_cluster.primary.id
+  instance_class     = "db.r5.large"
+  engine             = aws_rds_global_cluster.test.engine
+  engine_version     = aws_rds_global_cluster.test.engine_version
+
+  depends_on = [
+    aws_rds_cluster.primary
+  ]
 }
 
 resource "aws_rds_cluster" "secondary" {
-		provider                  = "awsalternate"
-		engine                    = aws_rds_global_cluster.test.engine
-		engine_version            = aws_rds_global_cluster.test.engine_version
-		cluster_identifier        = "test-secondary-cluster"
-		global_cluster_identifier = aws_rds_global_cluster.test.id
-		replication_source_identifier = aws_rds_cluster.primary.arn
-		source_region 			  = data.aws_region.alternate.name
-		skip_final_snapshot       = true
-		depends_on = [
-			aws_rds_cluster_instance.primary
-		]
+  provider                      = "awsalternate"
+  engine                        = aws_rds_global_cluster.test.engine
+  engine_version                = aws_rds_global_cluster.test.engine_version
+  cluster_identifier            = "test-secondary-cluster"
+  global_cluster_identifier     = aws_rds_global_cluster.test.id
+  replication_source_identifier = aws_rds_cluster.primary.arn
+  source_region                 = data.aws_region.alternate.name
+  skip_final_snapshot           = true
+  depends_on = [
+    aws_rds_cluster_instance.primary
+  ]
 }
 
 resource "aws_rds_cluster_instance" "secondary" {
-		provider             = "awsalternate"
-		identifier           = "test-secondary-cluster-instance"
-		cluster_identifier   = aws_rds_cluster.secondary.id
-		instance_class       = "db.r5.large"
-		engine               = aws_rds_global_cluster.test.engine
-		engine_version       = aws_rds_global_cluster.test.engine_version
-	  
-		depends_on = [
-			aws_rds_cluster.secondary
-		]
+  provider           = "awsalternate"
+  identifier         = "test-secondary-cluster-instance"
+  cluster_identifier = aws_rds_cluster.secondary.id
+  instance_class     = "db.r5.large"
+  engine             = aws_rds_global_cluster.test.engine
+  engine_version     = aws_rds_global_cluster.test.engine_version
+
+  depends_on = [
+    aws_rds_cluster.secondary
+  ]
 }
 
 data "aws_rds_global_cluster" "test" {
-	global_cluster_identifier = aws_rds_global_cluster.test.global_cluster_identifier
+  global_cluster_identifier = aws_rds_global_cluster.test.global_cluster_identifier
 }
 `, rName))
 }
