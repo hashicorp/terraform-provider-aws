150a6dd4314436fc8a4ba42547f5d564c584f008
Paul Santus <paul.santus@gmail.com>
2025-10-13 18:01:21 +0200

    Add aws_rdsdata_query resource

--- internal/service/rdsdata/service_package_gen.go
+++ internal/service/rdsdata/service_package_gen.go
@@ -29,7 +29,14 @@ func (p *servicePackage) FrameworkDataSources(ctx context.Context) []*inttypes.S
 }
 
 func (p *servicePackage) FrameworkResources(ctx context.Context) []*inttypes.ServicePackageFrameworkResource {
-	return []*inttypes.ServicePackageFrameworkResource{}
+	return []*inttypes.ServicePackageFrameworkResource{
+		{
+			Factory:  newResourceQuery,
+			TypeName: "aws_rdsdata_query",
+			Name:     "Query",
+			Region:   unique.Make(inttypes.ResourceRegionDefault()),
+		},
+	}
 }
 
 func (p *servicePackage) SDKDataSources(ctx context.Context) []*inttypes.ServicePackageSDKDataSource {
