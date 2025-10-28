1096f99a6e5b62cdc510a12976d8865411c8675f
Graham Davison <gdavison@hashicorp.com>
2025-10-28 12:55:36 -0700

    Updates `aws_sqs_queue_redrive_policy`

--- internal/service/sqs/service_package_gen.go
+++ internal/service/sqs/service_package_gen.go
@@ -91,7 +91,9 @@ func (p *servicePackage) SDKResources(ctx context.Context) []*inttypes.ServicePa
 			TypeName: "aws_sqs_queue_redrive_policy",
 			Name:     "Queue Redrive Policy",
 			Region:   unique.Make(inttypes.ResourceRegionDefault()),
-			Identity: inttypes.RegionalSingleParameterIdentity("queue_url"),
+			Identity: inttypes.VersionedIdentity(1, inttypes.RegionalCustomInherentRegionIdentity("queue_url", parseQueueURL,
+				inttypes.WithIdentityDuplicateAttrs(names.AttrID),
+			)),
 			Import: inttypes.SDKv2Import{
 				WrappedImport: true,
 			},
