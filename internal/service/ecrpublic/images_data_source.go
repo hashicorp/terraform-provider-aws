a9cb1efe6fd9d762c17ae8b15f71c97ce672f279
anurag-hashicorp <anurag.upadhaya@hashicorp.com>
2025-10-28 17:31:07 +0530

    Fix : Review comments
    - Update changelog, docs
    - Update datasource with flex `Flatten` and `Expand`

--- internal/service/ecrpublic/images_data_source.go
+++ internal/service/ecrpublic/images_data_source.go
@@ -9,6 +9,7 @@ import (
 	"github.com/YakDriver/regexache"
 	"github.com/aws/aws-sdk-go-v2/service/ecrpublic"
 	awstypes "github.com/aws/aws-sdk-go-v2/service/ecrpublic/types"
+	"github.com/hashicorp/terraform-plugin-framework-timetypes/timetypes"
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
@@ -22,19 +23,15 @@ import (
 )
 
 // @FrameworkDataSource("aws_ecrpublic_images", name="Images")
-func newDataSourceImages(context.Context) (datasource.DataSourceWithConfigure, error) {
+func newDataSourceImages(_ context.Context) (datasource.DataSourceWithConfigure, error) {
 	return &dataSourceImages{}, nil
 }
-
-const (
-	DSNameImages = "Images Data Source"
-)
 
 type dataSourceImages struct {
 	framework.DataSourceWithModel[dataSourceImagesModel]
 }
 
-func (d *dataSourceImages) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+func (d *dataSourceImages) Schema(ctx context.Context, _ datasource.SchemaRequest, resp *datasource.SchemaResponse) {
 	resp.Schema = schema.Schema{
 		Description: "Provides details about AWS ECR Public Images in a public repository.",
 		Attributes: map[string]schema.Attribute{
@@ -46,7 +43,7 @@ func (d *dataSourceImages) Schema(ctx context.Context, req datasource.SchemaRequ
 				},
 			},
 			"registry_id": schema.StringAttribute{
-				Description: "The AWS account ID associated with the public registry that contains the repository. If not specified, the default public registry is assumed.",
+				Description: "AWS account ID associated with the public registry that contains the repository. If not specified, the default public registry is assumed.",
 				Optional:    true,
 				Validators: []validator.String{
 					stringvalidator.RegexMatches(regexache.MustCompile(`^[0-9]{12}$`), "must be a 12-digit AWS account ID"),
@@ -85,49 +82,15 @@ func (d *dataSourceImages) Read(ctx context.Context, req datasource.ReadRequest,
 
 	conn := d.Meta().ECRPublicClient(ctx)
 
-	input := &ecrpublic.DescribeImagesInput{
-		RepositoryName: data.RepositoryName.ValueStringPointer(),
+	var input ecrpublic.DescribeImagesInput
+	resp.Diagnostics.Append(fwflex.Expand(ctx, &data, &input)...)
+	if resp.Diagnostics.HasError() {
+		return
 	}
 
-	if !data.RegistryID.IsNull() {
-		input.RegistryId = data.RegistryID.ValueStringPointer()
-	}
+	var images []awstypes.ImageDetail
 
-	if !data.ImageIDs.IsNull() {
-		imageIDs, diags := data.ImageIDs.ToSlice(ctx)
-		resp.Diagnostics.Append(diags...)
-		if resp.Diagnostics.HasError() {
-			return
-		}
-
-		if len(imageIDs) > 0 {
-			imageIds := make([]awstypes.ImageIdentifier, 0, len(imageIDs))
-
-			for _, id := range imageIDs {
-				identifier := awstypes.ImageIdentifier{}
-
-				if !id.ImageTag.IsNull() {
-					identifier.ImageTag = id.ImageTag.ValueStringPointer()
-				}
-
-				if !id.ImageDigest.IsNull() {
-					identifier.ImageDigest = id.ImageDigest.ValueStringPointer()
-				}
-
-				if identifier.ImageTag != nil || identifier.ImageDigest != nil {
-					imageIds = append(imageIds, identifier)
-				}
-			}
-
-			if len(imageIds) > 0 {
-				input.ImageIds = imageIds
-			}
-		}
-	}
-
-	images := make([]imageItemModel, 0)
-
-	paginator := ecrpublic.NewDescribeImagesPaginator(conn, input)
+	paginator := ecrpublic.NewDescribeImagesPaginator(conn, &input)
 	for paginator.HasMorePages() {
 		output, err := paginator.NextPage(ctx)
 
@@ -136,31 +99,15 @@ func (d *dataSourceImages) Read(ctx context.Context, req datasource.ReadRequest,
 			return
 		}
 
-		for _, img := range output.ImageDetails {
-			item := imageItemModel{
-				ArtifactMediaType:      fwflex.StringToFramework(ctx, img.ArtifactMediaType),
-				ImageDigest:            fwflex.StringToFramework(ctx, img.ImageDigest),
-				ImageManifestMediaType: fwflex.StringToFramework(ctx, img.ImageManifestMediaType),
-				ImageSizeInBytes:       fwflex.Int64ToFramework(ctx, img.ImageSizeInBytes),
-				RegistryId:             fwflex.StringToFramework(ctx, img.RegistryId),
-				RepositoryName:         fwflex.StringToFramework(ctx, img.RepositoryName),
-			}
+		images = append(images, output.ImageDetails...)
+	}
 
-			if img.ImagePushedAt != nil {
-				item.ImagePushedAt = types.StringValue(img.ImagePushedAt.Format("2006-01-02T15:04:05Z"))
-			} else {
-				item.ImagePushedAt = types.StringNull()
-			}
-
-			// Convert tags to ListValueOf
-			item.ImageTags = fwflex.FlattenFrameworkStringValueListOfString(ctx, img.ImageTags)
-
-			images = append(images, item)
-		}
+	resp.Diagnostics.Append(fwflex.Flatten(ctx, images, &data.Images)...)
+	if resp.Diagnostics.HasError() {
+		return
 	}
 
 	data.ID = types.StringValue(data.RepositoryName.ValueString())
-	data.Images = fwtypes.NewListNestedObjectValueOfValueSliceMust(ctx, images)
 
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
@@ -169,8 +116,8 @@ type dataSourceImagesModel struct {
 	framework.WithRegionModel
 	ID             types.String                                    `tfsdk:"id"`
 	RepositoryName types.String                                    `tfsdk:"repository_name"`
-	RegistryID     types.String                                    `tfsdk:"registry_id"`
-	ImageIDs       fwtypes.ListNestedObjectValueOf[imagesIDsModel] `tfsdk:"image_ids"`
+	RegistryId     types.String                                    `tfsdk:"registry_id"`
+	ImageIds       fwtypes.ListNestedObjectValueOf[imagesIDsModel] `tfsdk:"image_ids"`
 	Images         fwtypes.ListNestedObjectValueOf[imageItemModel] `tfsdk:"images"`
 }
 
@@ -181,11 +128,11 @@ type imagesIDsModel struct {
 
 type imageItemModel struct {
 	ArtifactMediaType      types.String                      `tfsdk:"artifact_media_type"`
-	ImageDigest            types.String                      `tfsdk:"digest"`
+	ImageDigest            types.String                      `tfsdk:"image_digest"`
 	ImageManifestMediaType types.String                      `tfsdk:"image_manifest_media_type"`
-	ImagePushedAt          types.String                      `tfsdk:"pushed_at"`
-	ImageSizeInBytes       types.Int64                       `tfsdk:"size_in_bytes"`
-	ImageTags              fwtypes.ListValueOf[types.String] `tfsdk:"tags"`
+	ImagePushedAt          timetypes.RFC3339                 `tfsdk:"image_pushed_at"`
+	ImageSizeInBytes       types.Int64                       `tfsdk:"image_size_in_bytes"`
+	ImageTags              fwtypes.ListValueOf[types.String] `tfsdk:"image_tags"`
 	RegistryId             types.String                      `tfsdk:"registry_id"`
 	RepositoryName         types.String                      `tfsdk:"repository_name"`
 }
