8ee86574eb01018337ff158bc2364952b1141c84
Graham Davison <gdavison@hashicorp.com>
2025-11-07 09:36:39 -0800

    Factors out common `IdentityVersion`

--- internal/generate/servicepackage/main.go
+++ internal/generate/servicepackage/main.go
@@ -230,7 +230,6 @@ type ResourceDatum struct {
 	SetIDAttribute                    bool
 	HasV6_0SDKv2Fix                   bool
 	HasIdentityFix                    bool
-	IdentityVersion                   int64
 	CustomInherentRegionParser        string
 	common.ResourceIdentity
 }
