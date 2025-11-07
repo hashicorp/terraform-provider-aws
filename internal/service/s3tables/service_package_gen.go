654e0a0d72d8d4877aad1aacc779864baa7f176e
Kit Ewbank <Kit_Ewbank@hotmail.com>
2025-11-07 11:25:25 -0500

    r/aws_s3tables_table_bucket: Add tagging support.

--- internal/service/s3tables/service_package_gen.go
+++ internal/service/s3tables/service_package_gen.go
@@ -39,6 +39,9 @@ func (p *servicePackage) FrameworkResources(ctx context.Context) []*inttypes.Ser
 			Factory:  newTableBucketResource,
 			TypeName: "aws_s3tables_table_bucket",
 			Name:     "Table Bucket",
+			Tags: unique.Make(inttypes.ServicePackageResourceTags{
+				IdentifierAttribute: names.AttrARN,
+			}),
 			Region:   unique.Make(inttypes.ResourceRegionDefault()),
 			Identity: inttypes.RegionalARNIdentity(),
 			Import: inttypes.FrameworkImport{
