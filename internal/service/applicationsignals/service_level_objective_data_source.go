b63488c619fb80710822d64797499074521929c4
Brendan <brendanelmes@improbable.io>
2025-11-10 14:33:22 +0000

    added most of fields to schema

--- internal/service/applicationsignals/service_level_objective_data_source.go
+++ internal/service/applicationsignals/service_level_objective_data_source.go
@@ -3,36 +3,7 @@
 
 package applicationsignals
 
-// **PLEASE DELETE THIS AND ALL TIP COMMENTS BEFORE SUBMITTING A PR FOR REVIEW!**
-//
-// TIP: ==== INTRODUCTION ====
-// Thank you for trying the skaff tool!
-//
-// You have opted to include these helpful comments. They all include "TIP:"
-// to help you find and remove them when you're done with them.
-//
-// While some aspects of this file are customized to your input, the
-// scaffold tool does *not* look at the AWS API and ensure it has correct
-// function, structure, and variable names. It makes guesses based on
-// commonalities. You will need to make significant adjustments.
-//
-// In other words, as generated, this is a rough outline of the work you will
-// need to do. If something doesn't make sense for your situation, get rid of
-// it.
-
 import (
-	// TIP: ==== IMPORTS ====
-	// This is a common set of imports but not customized to your code since
-	// your code hasn't been written yet. Make sure you, your IDE, or
-	// goimports -w <file> fixes these imports.
-	//
-	// The provider linter wants your imports to be in two groups: first,
-	// standard library (i.e., "fmt" or "strings"), second, everything else.
-	//
-	// Also, AWS Go SDK v2 may handle nested structures differently than v1,
-	// using the services/applicationsignals/types package. If so, you'll
-	// need to import types and reference the nested types, e.g., as
-	// awstypes.<Type Name>.
 	"context"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
@@ -48,17 +19,6 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
-// TIP: ==== FILE STRUCTURE ====
-// All data sources should follow this basic outline. Improve this data source's
-// maintainability by sticking to it.
-//
-// 1. Package declaration
-// 2. Imports
-// 3. Main data source struct with schema method
-// 4. Read method
-// 5. Other functions (flatteners, expanders, waiters, finders, etc.)
-
-// Function annotations are used for datasource registration to the Provider. DO NOT EDIT.
 // @FrameworkDataSource("aws_applicationsignals_service_level_objective", name="Service Level Objective")
 func newDataSourceServiceLevelObjective(context.Context) (datasource.DataSourceWithConfigure, error) {
 	return &dataSourceServiceLevelObjective{}, nil
@@ -72,132 +32,174 @@ type dataSourceServiceLevelObjective struct {
 	framework.DataSourceWithModel[dataSourceServiceLevelObjectiveModel]
 }
 
-// TIP: ==== SCHEMA ====
-// In the schema, add each of the arguments and attributes in snake
-// case (e.g., delete_automated_backups).
-// * Alphabetize arguments to make them easier to find.
-// * Do not add a blank line between arguments/attributes.
-//
-// Users can configure argument values while attribute values cannot be
-// configured and are used as output. Arguments have either:
-// Required: true,
-// Optional: true,
-//
-// All attributes will be computed and some arguments. If users will
-// want to read updated information or detect drift for an argument,
-// it should be computed:
-// Computed: true,
-//
-// You will typically find arguments in the input struct
-// (e.g., CreateDBInstanceInput) for the create operation. Sometimes
-// they are only in the input struct (e.g., ModifyDBInstanceInput) for
-// the modify operation.
-//
-// For more about schema options, visit
-// https://developer.hashicorp.com/terraform/plugin/framework/handling-data/schemas?page=schemas
 func (d *dataSourceServiceLevelObjective) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
 	resp.Schema = schema.Schema{
 		Attributes: map[string]schema.Attribute{
 			names.AttrARN: framework.ARNAttributeComputedOnly(),
+			"created_time": schema.StringAttribute{
+				Computed: true,
+			},
+			"last_updated_time": schema.StringAttribute{
+				Computed: true,
+			},
 			names.AttrDescription: schema.StringAttribute{
 				Computed: true,
 			},
-			names.AttrID: framework.IDAttribute(),
 			names.AttrName: schema.StringAttribute{
-				Required: true,
-			},
-			names.AttrType: schema.StringAttribute{
 				Computed: true,
+			},
+			"metric_source_type": schema.StringAttribute{
+				Computed: true,
+			},
+			"evaluation_type": schema.StringAttribute{
+				Computed: true,
+			},
+			"id": schema.StringAttribute{
+				Required: true,
 			},
 		},
 		Blocks: map[string]schema.Block{
-			"complex_argument": schema.ListNestedBlock{
-				// TIP: ==== CUSTOM TYPES ====
-				// Use a custom type to identify the model type of the tested object
-				CustomType: fwtypes.NewListNestedObjectTypeOf[complexArgumentModel](ctx),
-				NestedObject: schema.NestedBlockObject{
-					Attributes: map[string]schema.Attribute{
-						// TIP: Attributes that are required on a corresponding resource will be
-						// computed on the data source (unless required as part of the search criteria).
-						"nested_required": schema.StringAttribute{
-							Computed: true,
-						},
-						"nested_computed": schema.StringAttribute{
-							Computed: true,
+			"goal": schema.SingleNestedBlock{
+				CustomType: fwtypes.NewObjectTypeOf[goalModel](ctx),
+				Attributes: map[string]schema.Attribute{
+					"attainment_goal":   schema.Float64Attribute{Computed: true},
+					"warning_threshold": schema.Float64Attribute{Computed: true},
+				},
+				Blocks: map[string]schema.Block{
+					"interval": schema.SingleNestedBlock{
+						CustomType: fwtypes.NewObjectTypeOf[intervalModel](ctx),
+						Blocks: map[string]schema.Block{
+							"calendar_interval": schema.SingleNestedBlock{
+								CustomType: fwtypes.NewObjectTypeOf[calendarIntervalModel](ctx),
+								Attributes: map[string]schema.Attribute{
+									"start_time": schema.StringAttribute{Computed: true},
+									"end_time":   schema.StringAttribute{Computed: true},
+									"timezone":   schema.StringAttribute{Computed: true},
+								},
+							},
+							"rolling_interval": schema.SingleNestedBlock{
+								CustomType: fwtypes.NewObjectTypeOf[rollingIntervalModel](ctx),
+								Attributes: map[string]schema.Attribute{
+									"look_back_period": schema.Int64Attribute{Computed: true},
+								},
+							},
 						},
 					},
+				},
+			},
+			"burn_rate_configurations": schema.ListNestedBlock{
+				CustomType: fwtypes.NewListNestedObjectTypeOf[burnRateConfigurationModel](ctx),
+				NestedObject: schema.NestedBlockObject{
+					Attributes: map[string]schema.Attribute{
+						"look_back_window_minutes": schema.Int32Attribute{Computed: true},
+					},
+				},
+			},
+			"request_based_sli": schema.SingleNestedBlock{
+				CustomType: fwtypes.NewObjectTypeOf[requestBasedSliModel](ctx),
+				Attributes: map[string]schema.Attribute{
+					"metric_threshold":    schema.Float64Attribute{Computed: true},
+					"comparison_operator": schema.StringAttribute{Computed: true},
+				},
+			},
+			"sli": schema.SingleNestedBlock{
+				CustomType: fwtypes.NewObjectTypeOf[sliModel](ctx),
+				Attributes: map[string]schema.Attribute{
+					"metric_threshold":    schema.Float64Attribute{Computed: true},
+					"comparison_operator": schema.StringAttribute{Computed: true},
 				},
 			},
 		},
 	}
 }
 
-// TIP: ==== ASSIGN CRUD METHODS ====
-// Data sources only have a read method.
 func (d *dataSourceServiceLevelObjective) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
-	// TIP: ==== DATA SOURCE READ ====
-	// Generally, the Read function should do the following things. Make
-	// sure there is a good reason if you don't do one of these.
-	//
-	// 1. Get a client connection to the relevant service
-	// 2. Fetch the config
-	// 3. Get information about a resource from AWS
-	// 4. Set the ID, arguments, and attributes
-	// 5. Set the tags
-	// 6. Set the state
-	// TIP: -- 1. Get a client connection to the relevant service
 	conn := d.Meta().ApplicationSignalsClient(ctx)
 
-	// TIP: -- 2. Fetch the config
 	var data dataSourceServiceLevelObjectiveModel
 	smerr.EnrichAppend(ctx, &resp.Diagnostics, req.Config.Get(ctx, &data))
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// TIP: -- 3. Get information about a resource from AWS
-	out, err := findServiceLevelObjectiveByName(ctx, conn, data.Name.ValueString())
+	out, err := findServiceLevelObjectiveByID(ctx, conn, data.ID.ValueString())
 	if err != nil {
-		smerr.AddError(ctx, &resp.Diagnostics, err, smerr.ID, data.Name.String())
+		smerr.AddError(ctx, &resp.Diagnostics, err, smerr.ID, data.ID.String())
 		return
 	}
 
-	// TIP: -- 4. Set the ID, arguments, and attributes
-	// Using a field name prefix allows mapping fields such as `ServiceLevelObjectiveId` to `ID`
-	smerr.EnrichAppend(ctx, &resp.Diagnostics, flex.Flatten(ctx, out, &data, flex.WithFieldNamePrefix("ServiceLevelObjective")), smerr.ID, data.Name.String())
+	smerr.EnrichAppend(ctx, &resp.Diagnostics, flex.Flatten(ctx, out, &data), smerr.ID, data.ID.String())
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// TIP: -- 5. Set the tags
-
-	// TIP: -- 6. Set the state
-	smerr.EnrichAppend(ctx, &resp.Diagnostics, resp.State.Set(ctx, &data), smerr.ID, data.Name.String())
+	smerr.EnrichAppend(ctx, &resp.Diagnostics, resp.State.Set(ctx, &data), smerr.ID, data.ID.String())
 }
 
-// TIP: ==== DATA STRUCTURES ====
-// With Terraform Plugin-Framework configurations are deserialized into
-// Go types, providing type safety without the need for type assertions.
-// These structs should match the schema definition exactly, and the `tfsdk`
-// tag value should match the attribute name.
-//
-// Nested objects are represented in their own data struct. These will
-// also have a corresponding attribute type mapping for use inside flex
-// functions.
-//
-// See more:
-// https://developer.hashicorp.com/terraform/plugin/framework/handling-data/accessing-values
+func findServiceLevelObjectiveByID(ctx context.Context, conn *applicationsignals.Client, id string) (*awstypes.ServiceLevelObjective, error) {
+	input := &applicationsignals.GetServiceLevelObjectiveInput{
+		Id: aws.String(id),
+	}
+
+	output, err := conn.GetServiceLevelObjective(ctx, input)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return output.Slo, nil
+}
+
 type dataSourceServiceLevelObjectiveModel struct {
 	framework.WithRegionModel
-	ARN             types.String                                          `tfsdk:"arn"`
-	ComplexArgument fwtypes.ListNestedObjectValueOf[complexArgumentModel] `tfsdk:"complex_argument"`
-	Description     types.String                                          `tfsdk:"description"`
-	ID              types.String                                          `tfsdk:"id"`
-	Name            types.String                                          `tfsdk:"name"`
-	Type            types.String                                          `tfsdk:"type"`
+	ID               types.String                                `tfsdk:"id"`
+	ARN              types.String                                `tfsdk:"arn"`
+	Name             types.String                                `tfsdk:"name"`
+	Description      types.String                                `tfsdk:"description"`
+	MetricSourceType types.String                                `tfsdk:"metric_source_type"`
+	EvaluationType   types.String                                `tfsdk:"evaluation_type"`
+	Goal             fwtypes.ObjectValueOf[goalModel]            `tfsdk:"goal"`
+	Sli              fwtypes.ObjectValueOf[sliModel]             `tfsdk:"sli"`
+	RequestBasedSli  fwtypes.ObjectValueOf[requestBasedSliModel] `tfsdk:"request_based_sli"`
 }
 
-type complexArgumentModel struct {
-	NestedRequired types.String `tfsdk:"nested_required"`
-	NestedOptional types.String `tfsdk:"nested_optional"`
+type goalModel struct {
+	AttainmentGoal   types.Float64                        `tfsdk:"attainment_goal"`
+	WarningThreshold types.Float64                        `tfsdk:"warning_threshold"`
+	Interval         fwtypes.ObjectValueOf[intervalModel] `tfsdk:"interval"`
+}
+
+type intervalModel struct {
+	CalendarInterval fwtypes.ObjectValueOf[calendarIntervalModel] `tfsdk:"calendar_interval"`
+	RollingInterval  fwtypes.ObjectValueOf[rollingIntervalModel]  `tfsdk:"rolling_interval"`
+}
+
+type calendarIntervalModel struct {
+	StartTime types.String `tfsdk:"start_time"`
+	EndTime   types.String `tfsdk:"end_time"`
+	Timezone  types.String `tfsdk:"timezone"`
+}
+
+type rollingIntervalModel struct {
+	LookBackPeriod types.Int64 `tfsdk:"look_back_period"`
+}
+
+type sliModel struct {
+	MetricThreshold    types.Float64 `tfsdk:"metric_threshold"`
+	ComparisonOperator types.String  `tfsdk:"comparison_operator"`
+}
+
+type requestBasedSliModel struct {
+	RequestBasedSliMetric fwtypes.ObjectValueOf[requestBasedSliMetricModel] `tfsdk:"request_based_sli_metric"`
+	MetricThreshold       types.Float64                                     `tfsdk:"metric_threshold"`
+	ComparisonOperator    types.String                                      `tfsdk:"comparison_operator"`
+}
+
+type burnRateConfigurationModel struct {
+	LookBackWindowMinutes types.Int32 `tfsdk:"look_back_window_minutes"`
+}
+
+type requestBasedSliMetricModel struct {
+	MetricType    types.String `tfsdk:"metric_type"`
+	OperationName types.String `tfsdk:"operation_name"`
 }
