0bbd4306ae93d80725eacc22280e93415c806f30
Dirk Avery <dirk.avery@gmail.com>
2025-11-13 12:50:27 -0500

    Fix formatting in test

--- internal/service/s3/bucket_server_side_encryption_configuration_test.go
+++ internal/service/s3/bucket_server_side_encryption_configuration_test.go
@@ -694,8 +694,8 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "test" {
       kms_master_key_id = aws_kms_key.test.id
       sse_algorithm     = "aws:kms"
     }
-    bucket_key_enabled              = true
-    blocked_encryption_types_list   = %[2]s
+    bucket_key_enabled            = true
+    blocked_encryption_types_list = %[2]s
   }
 }
 `, rName, blockedTypes)
