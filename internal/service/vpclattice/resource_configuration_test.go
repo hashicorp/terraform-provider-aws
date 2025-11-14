43202299e7d759ed2128a3cd88f0414cdfaf59b6
Dave DeRicco <30156588+ddericco@users.noreply.github.com>
2025-11-14 19:21:49 +0000

    [WIP] Add custom_domain, domain_verification to resource config

--- internal/service/vpclattice/resource_configuration_test.go
+++ internal/service/vpclattice/resource_configuration_test.go
@@ -286,6 +286,46 @@ func TestAccVPCLatticeResourceConfiguration_arnResource(t *testing.T) {
 	})
 }
 
+func TestAccVPCLatticeResourceConfiguration_domainVerification(t *testing.T) {
+	ctx := acctest.Context(t)
+	var resourceconfiguration vpclattice.GetResourceConfigurationOutput
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	domainName := fmt.Sprintf("%s.example.com", rName)
+	customDomainName := fmt.Sprintf("test.%s.example.com", rName)
+	resourceName := "aws_vpclattice_resource_configuration.test"
+	domainVerificationResourceName := "aws_vpclattice_domain_verification.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+			acctest.PreCheckPartitionHasService(t, names.VPCLatticeEndpointID)
+			testAccPreCheck(ctx, t)
+		},
+		ErrorCheck:               acctest.ErrorCheck(t, names.VPCLatticeServiceID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckResourceConfigurationDestroy(ctx),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceConfigurationConfig_domainVerification(rName, domainName, customDomainName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckResourceConfigurationExists(ctx, resourceName, &resourceconfiguration),
+					resource.TestCheckResourceAttr(resourceName, names.AttrName, rName),
+					resource.TestCheckResourceAttr(resourceName, "custom_domain_name", customDomainName),
+					resource.TestCheckResourceAttrPair(resourceName, "domain_verification_id", domainVerificationResourceName, names.AttrID),
+					resource.TestCheckResourceAttrPair(resourceName, "domain_verification_arn", domainVerificationResourceName, names.AttrARN),
+					resource.TestCheckResourceAttr(resourceName, "domain_verification_status", string(types.VerificationStatusPending)),
+					resource.TestCheckResourceAttr(resourceName, "resource_configuration_definition.0.dns_resource.0.domain_name", customDomainName),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
 func TestAccVPCLatticeResourceConfiguration_disappears(t *testing.T) {
 	ctx := acctest.Context(t)
 	var resourceconfiguration vpclattice.GetResourceConfigurationOutput
@@ -508,4 +548,31 @@ resource "aws_db_subnet_group" "test" {
   subnet_ids = [aws_subnet.test.id, aws_subnet.test2.id]
 }
 `, rName))
+}
+
+func testAccResourceConfigurationConfig_domainVerification(rName, domainName, customDomainName string) string {
+	return acctest.ConfigCompose(testAccResourceGatewayConfig_basic(rName),
+		fmt.Sprintf(`
+resource "aws_vpclattice_domain_verification" "test" {
+  domain_name = %[2]q
+}
+
+resource "aws_vpclattice_resource_configuration" "test" {
+  name = %[1]q
+
+  resource_gateway_identifier = aws_vpclattice_resource_gateway.test.id
+  custom_domain_name          = %[3]q
+  domain_verification_id      = aws_vpclattice_domain_verification.test.id
+
+  port_ranges = ["443"]
+  protocol    = "TCP"
+
+  resource_configuration_definition {
+    dns_resource {
+      domain_name     = %[3]q
+      ip_address_type = "IPV4"
+    }
+  }
+}
+`, rName, domainName, customDomainName))
 }
