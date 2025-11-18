c0694a4f71518352a5e9dbc5c2891c01210e96f1
Graham Davison <gdavison@hashicorp.com>
2025-11-18 11:57:33 -0800

    Adds test for typed NotFound checks

--- internal/retry/stateof_test.go
+++ internal/retry/stateof_test.go
@@ -12,6 +12,7 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/google/go-cmp/cmp"
 )
 
 //
@@ -271,6 +272,39 @@ func TestWaitForStateOf_failure(t *testing.T) {
 	}
 	if obj != nil {
 		t.Fatalf("should not return obj")
+	}
+}
+
+func TestWaitForStateOf_NotFound_NotFoundChecks(t *testing.T) {
+	t.Parallel()
+
+	const notFoundCheckCount = 5
+
+	expectedErr := &NotFoundError{
+		Retries: notFoundCheckCount + 1,
+	}
+
+	conf := &StateChangeConfOf[*value, string]{
+		Pending:        []string{"pending", "incomplete"},
+		Target:         []string{"running"},
+		PollInterval:   10 * time.Millisecond,
+		Timeout:        100 * time.Millisecond,
+		NotFoundChecks: notFoundCheckCount,
+		Refresh: func(context.Context) (*value, string, error) {
+			return nil, "", nil
+		},
+	}
+
+	obj, err := conf.WaitForStateContext(t.Context())
+	if obj != nil {
+		t.Errorf("should not return obj")
+	}
+	if err == nil {
+		t.Fatal("Expected error. No error returned.")
+	}
+
+	if !cmp.Equal(expectedErr, err) {
+		t.Errorf("Errors don't match.\nExpected: %q\nGiven: %q\n", expectedErr, err)
 	}
 }
 
