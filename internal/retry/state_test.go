be3f77d354f68ee3e75ca0dc41a9b15bd5014094
Graham Davison <gdavison@hashicorp.com>
2025-11-18 11:53:02 -0800

    Adds test for untyped `ContinuousTargetOccurence` for empty target

--- internal/retry/state_test.go
+++ internal/retry/state_test.go
@@ -6,6 +6,8 @@ package retry
 import (
 	"context"
 	"errors"
+	"iter"
+	"slices"
 	"strings"
 	"sync/atomic"
 	"testing"
@@ -328,6 +330,70 @@ func TestWaitForState_NotFound_NotFoundChecks(t *testing.T) {
 
 	if !cmp.Equal(expectedErr, err) {
 		t.Errorf("Errors don't match.\nExpected: %q\nGiven: %q\n", expectedErr, err)
+	}
+}
+
+type refresh[T any, S ~string] struct {
+	obj   T
+	state S
+	err   error
+}
+
+func inconsistentResultStateRefreshFunc(t *testing.T) (StateRefreshFunc, func()) {
+	t.Helper()
+
+	sequence := []refresh[any, string]{
+		{nil, "", nil}, // 1
+		{&value{val: "value"}, "pending", nil},
+		{nil, "", nil}, {nil, "", nil}, // 2
+		{&value{val: "value"}, "pending", nil},
+		{nil, "", nil}, {nil, "", nil}, {nil, "", nil}, // 3
+	}
+
+	next, stop := iter.Pull(slices.Values(sequence))
+
+	return func(context.Context) (any, string, error) {
+		v, _ := next()
+
+		return v.obj, v.state, v.err
+	}, stop
+}
+
+func TestWaitForState_EmptyTarget_ContinuousTargetOccurence(t *testing.T) {
+	t.Parallel()
+
+	const continuousTargetOccurence = 3
+	const expectedCount = 8
+
+	var count atomic.Int32
+
+	inner, stop := inconsistentResultStateRefreshFunc(t)
+	defer stop()
+
+	refresh := func(ctx context.Context) (any, string, error) {
+		count.Add(1)
+		return inner(ctx)
+	}
+
+	conf := &StateChangeConf{
+		Pending:                   []string{"pending", "incomplete"},
+		Target:                    []string{},
+		Timeout:                   100 * time.Millisecond,
+		PollInterval:              10 * time.Millisecond,
+		ContinuousTargetOccurence: continuousTargetOccurence,
+		Refresh:                   refresh,
+	}
+
+	obj, err := conf.WaitForStateContext(t.Context())
+	if obj != nil {
+		t.Errorf("should not return obj")
+	}
+	if err != nil {
+		t.Fatalf("Unexpected error: %s", err)
+	}
+
+	if v := count.Load(); v != expectedCount {
+		t.Errorf("Expected %d refresh calls, got %d", expectedCount, v)
 	}
 }
 
