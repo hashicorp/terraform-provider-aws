92597e5b2edb006d95424b1288135abc18103d61
Graham Davison <gdavison@hashicorp.com>
2025-11-18 16:53:20 -0800

    Requires `inttypes` alias for `github.com/hashicorp/terraform-provider-aws/internal/types`

--- internal/service/ec2/diff.go
+++ internal/service/ec2/diff.go
@@ -5,11 +5,11 @@ package ec2
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hashicorp/terraform-provider-aws/internal/types"
+	inttypes "github.com/hashicorp/terraform-provider-aws/internal/types"
 )
 
 // suppressEqualCIDRBlockDiffs provides custom difference suppression for CIDR blocks
 // that have different string values but represent the same CIDR.
 func suppressEqualCIDRBlockDiffs(k, old, new string, d *schema.ResourceData) bool {
-	return types.CIDRBlocksEqual(old, new)
+	return inttypes.CIDRBlocksEqual(old, new)
 }
