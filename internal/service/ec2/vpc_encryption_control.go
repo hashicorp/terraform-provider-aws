ee6ec60d72ee041b2513f41c0ca1b71826db3de4
Graham Davison <gdavison@hashicorp.com>
2025-11-24 15:48:33 -0800

    Generate paginators

--- internal/service/ec2/vpc_encryption_control.go
+++ internal/service/ec2/vpc_encryption_control.go
@@ -360,7 +360,7 @@ func findVPCEncryptionControlsByIDs(ctx context.Context, conn *ec2.Client, ids [
 
 	var output []awstypes.VpcEncryptionControl
 
-	pages := NewDescribeVpcEncryptionControlsPaginator(conn, &input)
+	pages := newDescribeVpcEncryptionControlsPaginator(conn, &input)
 	for pages.HasMorePages() {
 		page, err := pages.NextPage(ctx)
 		if tfawserr.ErrCodeEquals(err, errCodeInvalidVPCEncryptionControlIdNotFound) {
@@ -491,98 +491,3 @@ type vpcEncryptionControlExclusionModel struct {
 	State        types.String `tfsdk:"state"`
 	StateMessage types.String `tfsdk:"state_message"`
 }
-
-// DescribeVpcEncryptionControlsPaginatorOptions is the paginator options for
-// DescribeVpcEncryptionControls
-type DescribeVpcEncryptionControlsPaginatorOptions struct {
-	// The maximum number of items to return for this request. To get the next page of
-	// items, make another request with the token returned in the output. For more
-	// information, see [Pagination].
-	//
-	// [Pagination]: https://docs.aws.amazon.com/AWSEC2/latest/APIReference/Query-Requests.html#api-pagination
-	Limit int32
-
-	// Set to true if pagination should stop if the service returns a pagination token
-	// that matches the most recent token provided to the service.
-	StopOnDuplicateToken bool
-}
-
-// DescribeVpcEncryptionControlsPaginator is a paginator for DescribeVpcEncryptionControls
-type DescribeVpcEncryptionControlsPaginator struct {
-	options   DescribeVpcEncryptionControlsPaginatorOptions
-	client    DescribeVpcEncryptionControlsAPIClient
-	params    *ec2.DescribeVpcEncryptionControlsInput
-	nextToken *string
-	firstPage bool
-}
-
-// NewDescribeVpcEncryptionControlsPaginator returns a new DescribeVpcEncryptionControlsPaginator
-func NewDescribeVpcEncryptionControlsPaginator(client DescribeVpcEncryptionControlsAPIClient, params *ec2.DescribeVpcEncryptionControlsInput, optFns ...func(*DescribeVpcEncryptionControlsPaginatorOptions)) *DescribeVpcEncryptionControlsPaginator {
-	if params == nil {
-		params = &ec2.DescribeVpcEncryptionControlsInput{}
-	}
-
-	options := DescribeVpcEncryptionControlsPaginatorOptions{}
-	if params.MaxResults != nil {
-		options.Limit = *params.MaxResults
-	}
-
-	for _, fn := range optFns {
-		fn(&options)
-	}
-
-	return &DescribeVpcEncryptionControlsPaginator{
-		options:   options,
-		client:    client,
-		params:    params,
-		firstPage: true,
-		nextToken: params.NextToken,
-	}
-}
-
-// HasMorePages returns a boolean indicating whether more pages are available
-func (p *DescribeVpcEncryptionControlsPaginator) HasMorePages() bool {
-	return p.firstPage || (p.nextToken != nil && len(*p.nextToken) != 0)
-}
-
-// NextPage retrieves the next DescribeVpcEncryptionControls page.
-func (p *DescribeVpcEncryptionControlsPaginator) NextPage(ctx context.Context, optFns ...func(*ec2.Options)) (*ec2.DescribeVpcEncryptionControlsOutput, error) {
-	if !p.HasMorePages() {
-		return nil, fmt.Errorf("no more pages available")
-	}
-
-	params := *p.params
-	params.NextToken = p.nextToken
-
-	var limit *int32
-	if p.options.Limit > 0 {
-		limit = &p.options.Limit
-	}
-	params.MaxResults = limit
-
-	result, err := p.client.DescribeVpcEncryptionControls(ctx, &params, optFns...)
-	if err != nil {
-		return nil, err
-	}
-	p.firstPage = false
-
-	prevToken := p.nextToken
-	p.nextToken = result.NextToken
-
-	if p.options.StopOnDuplicateToken &&
-		prevToken != nil &&
-		p.nextToken != nil &&
-		*prevToken == *p.nextToken {
-		p.nextToken = nil
-	}
-
-	return result, nil
-}
-
-// DescribeVpcEncryptionControlsAPIClient is a client that implements the
-// DescribeVpcEncryptionControls operation.
-type DescribeVpcEncryptionControlsAPIClient interface {
-	DescribeVpcEncryptionControls(context.Context, *ec2.DescribeVpcEncryptionControlsInput, ...func(*ec2.Options)) (*ec2.DescribeVpcEncryptionControlsOutput, error)
-}
-
-var _ DescribeVpcEncryptionControlsAPIClient = (*ec2.Client)(nil)
