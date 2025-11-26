6bc44410a8a1f1f53e6aba1cbc84351311d1ddd1
Graham Davison <gdavison@hashicorp.com>
2025-11-26 14:03:18 -0800

    Uses `retry.NotFound` in `testAccCheckVPCDestroy`

--- internal/service/ec2/vpc_test.go
+++ internal/service/ec2/vpc_test.go
@@ -24,8 +24,8 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	tfstatecheck "github.com/hashicorp/terraform-provider-aws/internal/acctest/statecheck"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/retry"
 	tfec2 "github.com/hashicorp/terraform-provider-aws/internal/service/ec2"
-	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
@@ -1164,7 +1164,7 @@ func testAccCheckVPCDestroy(ctx context.Context, t *testing.T) resource.TestChec
 
 			_, err := tfec2.FindVPCByID(ctx, conn, rs.Primary.ID)
 
-			if tfresource.NotFound(err) {
+			if retry.NotFound(err) {
 				continue
 			}
 
