e1bd0b29bb15b29f49287dec8fcf6872d00b593a
Graham Davison <gdavison@hashicorp.com>
2025-11-26 23:55:16 -0800

    `configservice`

--- internal/service/configservice/configuration_recorder.go
+++ internal/service/configservice/configuration_recorder.go
@@ -12,7 +12,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/configservice"
 	"github.com/aws/aws-sdk-go-v2/service/configservice/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+	sdkretry "github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
@@ -314,7 +314,7 @@ func findConfigurationRecorders(ctx context.Context, conn *configservice.Client,
 	output, err := conn.DescribeConfigurationRecorders(ctx, input)
 
 	if errs.IsA[*types.NoSuchConfigurationRecorderException](err) {
-		return nil, &retry.NotFoundError{
+		return nil, &sdkretry.NotFoundError{
 			LastError:   err,
 			LastRequest: input,
 		}
