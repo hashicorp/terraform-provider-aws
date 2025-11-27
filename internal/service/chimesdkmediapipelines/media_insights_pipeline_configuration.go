421db2fff3ec105ec510c8e8a3d88fb6828d68b1
Graham Davison <gdavison@hashicorp.com>
2025-11-26 17:37:04 -0800

    `chimesdkmediapipelines`

--- internal/service/chimesdkmediapipelines/media_insights_pipeline_configuration.go
+++ internal/service/chimesdkmediapipelines/media_insights_pipeline_configuration.go
@@ -14,7 +14,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/chimesdkmediapipelines"
 	awstypes "github.com/aws/aws-sdk-go-v2/service/chimesdkmediapipelines/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+	sdkretry "github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
@@ -633,7 +633,7 @@ func findMediaInsightsPipelineConfigurationByID(ctx context.Context, conn *chime
 	}
 	out, err := conn.GetMediaInsightsPipelineConfiguration(ctx, in)
 	if errs.IsA[*awstypes.NotFoundException](err) {
-		return nil, &retry.NotFoundError{
+		return nil, &sdkretry.NotFoundError{
 			LastError:   err,
 			LastRequest: in,
 		}
