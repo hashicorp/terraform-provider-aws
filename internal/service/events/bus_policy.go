fa0e6a31a2b7bbeb1126a7e887fc469ab62d4d06
Graham Davison <gdavison@hashicorp.com>
2025-11-27 00:46:20 -0800

    `events`

--- internal/service/events/bus_policy.go
+++ internal/service/events/bus_policy.go
@@ -11,7 +11,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/eventbridge"
 	"github.com/aws/aws-sdk-go-v2/service/eventbridge/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+	sdkretry "github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/structure"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
@@ -153,7 +153,7 @@ func findEventBusPolicyByName(ctx context.Context, conn *eventbridge.Client, nam
 	}
 
 	if aws.ToString(output.Policy) == "" {
-		return nil, &retry.NotFoundError{}
+		return nil, &sdkretry.NotFoundError{}
 	}
 
 	return output.Policy, nil
