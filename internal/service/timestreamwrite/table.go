3c14b4418c0fd6da40fd905de624e58ec8655fdc
Graham Davison <gdavison@hashicorp.com>
2025-11-27 09:37:53 -0800

    `timestreamwrite`

--- internal/service/timestreamwrite/table.go
+++ internal/service/timestreamwrite/table.go
@@ -14,7 +14,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/timestreamwrite"
 	"github.com/aws/aws-sdk-go-v2/service/timestreamwrite/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+	sdkretry "github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
@@ -330,7 +330,7 @@ func findTableByTwoPartKey(ctx context.Context, conn *timestreamwrite.Client, da
 	output, err := conn.DescribeTable(ctx, input)
 
 	if errs.IsA[*types.ResourceNotFoundException](err) {
-		return nil, &retry.NotFoundError{
+		return nil, &sdkretry.NotFoundError{
 			LastError:   err,
 			LastRequest: input,
 		}
