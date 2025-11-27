fad5ea37b27f3d6c3a983376f8350846dd124973
Graham Davison <gdavison@hashicorp.com>
2025-11-27 09:37:12 -0800

    `servicecatalogappregistry`

--- internal/service/servicecatalogappregistry/attribute_group.go
+++ internal/service/servicecatalogappregistry/attribute_group.go
@@ -18,7 +18,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+	sdkretry "github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-provider-aws/internal/create"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 	"github.com/hashicorp/terraform-provider-aws/internal/framework"
@@ -229,7 +229,7 @@ func findAttributeGroupByID(ctx context.Context, conn *servicecatalogappregistry
 	out, err := conn.GetAttributeGroup(ctx, in)
 	if err != nil {
 		if errs.IsA[*awstypes.ResourceNotFoundException](err) {
-			return nil, &retry.NotFoundError{
+			return nil, &sdkretry.NotFoundError{
 				LastError:   err,
 				LastRequest: in,
 			}
