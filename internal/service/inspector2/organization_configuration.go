964ac6be96891f3932c3a62432f5d13b0278d4e0
Graham Davison <gdavison@hashicorp.com>
2025-11-27 00:46:59 -0800

    `inspector2`

--- internal/service/inspector2/organization_configuration.go
+++ internal/service/inspector2/organization_configuration.go
@@ -12,7 +12,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/inspector2"
 	awstypes "github.com/aws/aws-sdk-go-v2/service/inspector2/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+	sdkretry "github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
@@ -177,7 +177,7 @@ func findOrganizationConfiguration(ctx context.Context, conn *inspector2.Client)
 	output, err := conn.DescribeOrganizationConfiguration(ctx, input)
 
 	if errs.IsAErrorMessageContains[*awstypes.AccessDeniedException](err, "Invoking account does not have access to describe the organization configuration") {
-		return nil, &retry.NotFoundError{
+		return nil, &sdkretry.NotFoundError{
 			LastError:   err,
 			LastRequest: input,
 		}
