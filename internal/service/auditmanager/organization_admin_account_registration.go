843331f9641084b2d98383c1f275c8d0804b26fa
Graham Davison <gdavison@hashicorp.com>
2025-11-26 16:55:20 -0800

    `auditmanager`

--- internal/service/auditmanager/organization_admin_account_registration.go
+++ internal/service/auditmanager/organization_admin_account_registration.go
@@ -16,7 +16,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+	sdkretry "github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/fwdiag"
 	"github.com/hashicorp/terraform-provider-aws/internal/framework"
@@ -149,7 +149,7 @@ func findOrganizationAdminAccount(ctx context.Context, conn *auditmanager.Client
 	output, err := conn.GetOrganizationAdminAccount(ctx, &input)
 
 	if errs.IsA[*awstypes.ResourceNotFoundException](err) {
-		return nil, &retry.NotFoundError{
+		return nil, &sdkretry.NotFoundError{
 			LastError:   err,
 			LastRequest: input,
 		}
