edbacf7cf85c8624de8d573ce6d95bdc7ba49bda
Graham Davison <gdavison@hashicorp.com>
2025-11-27 00:47:19 -0800

    `lightsail`

--- internal/service/lightsail/container_service.go
+++ internal/service/lightsail/container_service.go
@@ -14,7 +14,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/lightsail"
 	"github.com/aws/aws-sdk-go-v2/service/lightsail/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+	sdkretry "github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
@@ -476,7 +476,7 @@ func FindContainerServiceByName(ctx context.Context, conn *lightsail.Client, ser
 	output, err := conn.GetContainerServices(ctx, input)
 
 	if IsANotFoundError(err) {
-		return nil, &retry.NotFoundError{
+		return nil, &sdkretry.NotFoundError{
 			LastError:   err,
 			LastRequest: input,
 		}
