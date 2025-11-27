4f6664f7efa77e6bc59fb31b990814eed0349eec
Graham Davison <gdavison@hashicorp.com>
2025-11-27 09:37:21 -0800

    `sesv2`

--- internal/service/sesv2/email_identity_policy.go
+++ internal/service/sesv2/email_identity_policy.go
@@ -14,7 +14,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/sesv2"
 	"github.com/aws/aws-sdk-go-v2/service/sesv2/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+	sdkretry "github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/structure"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
@@ -235,7 +235,7 @@ func findEmailIdentityPolicies(ctx context.Context, conn *sesv2.Client, input *s
 	output, err := conn.GetEmailIdentityPolicies(ctx, input)
 
 	if errs.IsA[*types.NotFoundException](err) {
-		return nil, &retry.NotFoundError{
+		return nil, &sdkretry.NotFoundError{
 			LastError:   err,
 			LastRequest: input,
 		}
