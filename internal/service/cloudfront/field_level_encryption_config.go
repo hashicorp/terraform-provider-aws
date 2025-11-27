e1ce2062ada17a53c47bdebd6d41e80bb1236642
Graham Davison <gdavison@hashicorp.com>
2025-11-26 17:42:54 -0800

    `cloudfront`

--- internal/service/cloudfront/field_level_encryption_config.go
+++ internal/service/cloudfront/field_level_encryption_config.go
@@ -12,7 +12,7 @@ import (
 	awstypes "github.com/aws/aws-sdk-go-v2/service/cloudfront/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/id"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+	sdkretry "github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/enum"
@@ -275,7 +275,7 @@ func findFieldLevelEncryptionConfigByID(ctx context.Context, conn *cloudfront.Cl
 	output, err := conn.GetFieldLevelEncryptionConfig(ctx, input)
 
 	if errs.IsA[*awstypes.NoSuchFieldLevelEncryptionConfig](err) {
-		return nil, &retry.NotFoundError{
+		return nil, &sdkretry.NotFoundError{
 			LastError:   err,
 			LastRequest: input,
 		}
--- internal/service/cloudfront/response_headers_policy.go
+++ internal/service/cloudfront/response_headers_policy.go
@@ -11,7 +11,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/cloudfront"
 	awstypes "github.com/aws/aws-sdk-go-v2/service/cloudfront/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+	sdkretry "github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
@@ -520,7 +520,7 @@ func findResponseHeadersPolicyByID(ctx context.Context, conn *cloudfront.Client,
 	output, err := conn.GetResponseHeadersPolicy(ctx, input)
 
 	if errs.IsA[*awstypes.NoSuchResponseHeadersPolicy](err) {
-		return nil, &retry.NotFoundError{
+		return nil, &sdkretry.NotFoundError{
 			LastError:   err,
 			LastRequest: input,
 		}
