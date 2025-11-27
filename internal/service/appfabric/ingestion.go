de4de2448bea84642b9b8a3805d17641505ff3e8
Graham Davison <gdavison@hashicorp.com>
2025-11-26 16:52:17 -0800

    `appfabric`

--- internal/service/appfabric/ingestion.go
+++ internal/service/appfabric/ingestion.go
@@ -18,7 +18,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+	sdkretry "github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/fwdiag"
 	"github.com/hashicorp/terraform-provider-aws/internal/flex"
@@ -212,7 +212,7 @@ func findIngestionByTwoPartKey(ctx context.Context, conn *appfabric.Client, appB
 	output, err := conn.GetIngestion(ctx, input)
 
 	if errs.IsA[*awstypes.ResourceNotFoundException](err) {
-		return nil, &retry.NotFoundError{
+		return nil, &sdkretry.NotFoundError{
 			LastError:   err,
 			LastRequest: input,
 		}
