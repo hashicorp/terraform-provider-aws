ed04971f18bdfca436b388a23c6ef7214bfd762e
Graham Davison <gdavison@hashicorp.com>
2025-11-26 17:35:23 -0800

    `ce`

--- internal/service/ce/anomaly_subscription.go
+++ internal/service/ce/anomaly_subscription.go
@@ -12,7 +12,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/costexplorer"
 	awstypes "github.com/aws/aws-sdk-go-v2/service/costexplorer/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+	sdkretry "github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
@@ -235,7 +235,7 @@ func findAnomalySubscriptionByARN(ctx context.Context, conn *costexplorer.Client
 	output, err := conn.GetAnomalySubscriptions(ctx, input)
 
 	if errs.IsA[*awstypes.UnknownMonitorException](err) {
-		return nil, &retry.NotFoundError{
+		return nil, &sdkretry.NotFoundError{
 			LastError:   err,
 			LastRequest: input,
 		}
