228bf5e06490a48b87717c64313d3a40ab1782c1
Graham Davison <gdavison@hashicorp.com>
2025-11-27 09:37:58 -0800

    `verifiedpermissions`

--- internal/service/verifiedpermissions/schema.go
+++ internal/service/verifiedpermissions/schema.go
@@ -19,7 +19,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+	sdkretry "github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-provider-aws/internal/create"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 	"github.com/hashicorp/terraform-provider-aws/internal/framework"
@@ -266,7 +266,7 @@ func findSchemaByPolicyStoreID(ctx context.Context, conn *verifiedpermissions.Cl
 
 	out, err := conn.GetSchema(ctx, in)
 	if errs.IsA[*awstypes.ResourceNotFoundException](err) {
-		return nil, &retry.NotFoundError{
+		return nil, &sdkretry.NotFoundError{
 			LastError:   err,
 			LastRequest: in,
 		}
