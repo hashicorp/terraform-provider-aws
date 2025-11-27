69be005a14e8e0ae585857c792982bb44ff75d0a
Graham Davison <gdavison@hashicorp.com>
2025-11-27 09:38:09 -0800

    `workspacesweb`

--- internal/service/workspacesweb/network_settings.go
+++ internal/service/workspacesweb/network_settings.go
@@ -20,7 +20,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	sdkid "github.com/hashicorp/terraform-plugin-sdk/v2/helper/id"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+	sdkretry "github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/fwdiag"
 	"github.com/hashicorp/terraform-provider-aws/internal/framework"
@@ -231,7 +231,7 @@ func findNetworkSettingsByARN(ctx context.Context, conn *workspacesweb.Client, a
 	output, err := conn.GetNetworkSettings(ctx, &input)
 
 	if errs.IsA[*awstypes.ResourceNotFoundException](err) {
-		return nil, &retry.NotFoundError{
+		return nil, &sdkretry.NotFoundError{
 			LastError:   err,
 			LastRequest: input,
 		}
