5901bff1c3110269ea121c2f90f6cb62e8be2ea4
Graham Davison <gdavison@hashicorp.com>
2025-11-27 09:37:53 -0800

    `timestreamwrite`

--- internal/service/timestreamwrite/database.go
+++ internal/service/timestreamwrite/database.go
@@ -12,7 +12,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/timestreamwrite"
 	"github.com/aws/aws-sdk-go-v2/service/timestreamwrite/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+	sdkretry "github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
@@ -169,7 +169,7 @@ func findDatabaseByName(ctx context.Context, conn *timestreamwrite.Client, name 
 	output, err := conn.DescribeDatabase(ctx, input)
 
 	if errs.IsA[*types.ResourceNotFoundException](err) {
-		return nil, &retry.NotFoundError{
+		return nil, &sdkretry.NotFoundError{
 			LastError:   err,
 			LastRequest: input,
 		}
