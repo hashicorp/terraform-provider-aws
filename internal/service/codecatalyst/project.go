c90741b7b0cfec2a2c413133dfd13c5be5d0ac4e
Graham Davison <gdavison@hashicorp.com>
2025-11-26 17:44:52 -0800

    `codecatalyst`

--- internal/service/codecatalyst/project.go
+++ internal/service/codecatalyst/project.go
@@ -13,7 +13,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/codecatalyst"
 	"github.com/aws/aws-sdk-go-v2/service/codecatalyst/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+	sdkretry "github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/create"
@@ -180,7 +180,7 @@ func findProjectByName(ctx context.Context, conn *codecatalyst.Client, id string
 	}
 	out, err := conn.GetProject(ctx, in)
 	if errs.IsA[*types.AccessDeniedException](err) || errs.IsA[*types.ResourceNotFoundException](err) {
-		return nil, &retry.NotFoundError{
+		return nil, &sdkretry.NotFoundError{
 			LastError:   err,
 			LastRequest: in,
 		}
