d1ea9bc64fb7ade1c82ca33b4815bab4443f9c3f
Graham Davison <gdavison@hashicorp.com>
2025-11-27 00:48:47 -0800

    `rolesanywhere`

--- internal/service/rolesanywhere/trust_anchor.go
+++ internal/service/rolesanywhere/trust_anchor.go
@@ -13,7 +13,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/rolesanywhere"
 	awstypes "github.com/aws/aws-sdk-go-v2/service/rolesanywhere/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+	sdkretry "github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/enum"
@@ -297,7 +297,7 @@ func findTrustAnchorByID(ctx context.Context, conn *rolesanywhere.Client, id str
 	out, err := conn.GetTrustAnchor(ctx, in)
 
 	if errs.IsA[*awstypes.ResourceNotFoundException](err) {
-		return nil, &retry.NotFoundError{
+		return nil, &sdkretry.NotFoundError{
 			LastError:   err,
 			LastRequest: in,
 		}
