3690aa8d7a1d9fca80747b5ec52f64858c9ed039
Graham Davison <gdavison@hashicorp.com>
2025-11-27 00:47:38 -0800

    `macie2`

--- internal/service/macie2/findings_filter.go
+++ internal/service/macie2/findings_filter.go
@@ -15,7 +15,7 @@ import (
 	awstypes "github.com/aws/aws-sdk-go-v2/service/macie2/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/id"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+	sdkretry "github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
@@ -295,7 +295,7 @@ func findFindingsFilter(ctx context.Context, conn *macie2.Client, input *macie2.
 	output, err := conn.GetFindingsFilter(ctx, input)
 
 	if isFindingsFilterNotFoundError(err) {
-		return nil, &retry.NotFoundError{
+		return nil, &sdkretry.NotFoundError{
 			LastError:   err,
 			LastRequest: input,
 		}
