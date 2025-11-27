b9d1fc29a7648ad7393bf457a664083b1d9fa409
Graham Davison <gdavison@hashicorp.com>
2025-11-27 00:47:06 -0800

    `keyspaces`

--- internal/service/keyspaces/keyspace.go
+++ internal/service/keyspaces/keyspace.go
@@ -13,7 +13,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/keyspaces"
 	"github.com/aws/aws-sdk-go-v2/service/keyspaces/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+	sdkretry "github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
@@ -204,7 +204,7 @@ func findKeyspaceByName(ctx context.Context, conn *keyspaces.Client, name string
 	output, err := conn.GetKeyspace(ctx, &input)
 
 	if errs.IsA[*types.ResourceNotFoundException](err) {
-		return nil, &retry.NotFoundError{
+		return nil, &sdkretry.NotFoundError{
 			LastError:   err,
 			LastRequest: input,
 		}
