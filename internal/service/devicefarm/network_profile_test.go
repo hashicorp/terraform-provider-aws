299575af276249609443c40ff905cf0180fc9dc7
Graham Davison <gdavison@hashicorp.com>
2025-11-28 16:31:58 -0800

    `devicefarm`

--- internal/service/devicefarm/network_profile_test.go
+++ internal/service/devicefarm/network_profile_test.go
@@ -16,8 +16,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/retry"
 	tfdevicefarm "github.com/hashicorp/terraform-provider-aws/internal/service/devicefarm"
-	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
@@ -222,7 +222,7 @@ func testAccCheckNetworkProfileDestroy(ctx context.Context) resource.TestCheckFu
 
 			// Try to find the resource
 			_, err := tfdevicefarm.FindNetworkProfileByARN(ctx, conn, rs.Primary.ID)
-			if tfresource.NotFound(err) {
+			if retry.NotFound(err) {
 				continue
 			}
 
