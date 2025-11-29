6be8bd2506fdfe9e64ad4a47174251c8949b0f55
Graham Davison <gdavison@hashicorp.com>
2025-11-28 16:34:13 -0800

    `timestreamwrite`

--- internal/service/timestreamwrite/database.go
+++ internal/service/timestreamwrite/database.go
@@ -18,6 +18,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
+	"github.com/hashicorp/terraform-provider-aws/internal/retry"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
@@ -102,7 +103,7 @@ func resourceDatabaseRead(ctx context.Context, d *schema.ResourceData, meta any)
 
 	db, err := findDatabaseByName(ctx, conn, d.Id())
 
-	if !d.IsNewResource() && tfresource.NotFound(err) {
+	if !d.IsNewResource() && retry.NotFound(err) {
 		log.Printf("[WARN] Timestream Database %s not found, removing from state", d.Id())
 		d.SetId("")
 		return diags
