e852c08f8d8db7fffed10d2c475c43c4aa289286
Graham Davison <gdavison@hashicorp.com>
2025-11-28 17:17:29 -0800

    Removes `tfresource.NotFound`

--- internal/tfresource/errors.go
+++ internal/tfresource/errors.go
@@ -9,15 +9,6 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/retry"
 )
 
-// NotFound returns true if the error represents a "resource not found" condition.
-// Specifically, NotFound returns true if the error or a wrapped error is of type
-// retry.NotFoundError.
-//
-// Deprecated: NotFound is an alias to a function of the same name in internal/retry
-// which handles both Plugin SDK V2 and internal error types. For net-new usage,
-// prefer calling retry.NotFound directly.
-var NotFound = retry.NotFound
-
 // TimedOut returns true if the error represents a "wait timed out" condition.
 // Specifically, TimedOut returns true if the error matches all these conditions:
 //   - err is of type retry.TimeoutError
