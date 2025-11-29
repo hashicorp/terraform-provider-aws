bf71b91791c59be4988db255261c14bed372f9e2
Graham Davison <gdavison@hashicorp.com>
2025-11-28 16:33:27 -0800

    `redshift`

--- internal/service/redshift/hsm_client_certificate_test.go
+++ internal/service/redshift/hsm_client_certificate_test.go
@@ -14,8 +14,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/retry"
 	tfredshift "github.com/hashicorp/terraform-provider-aws/internal/service/redshift"
-	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
@@ -83,7 +83,7 @@ func testAccCheckHSMClientCertificateDestroy(ctx context.Context) resource.TestC
 
 			_, err := tfredshift.FindHSMClientCertificateByID(ctx, conn, rs.Primary.ID)
 
-			if tfresource.NotFound(err) {
+			if retry.NotFound(err) {
 				continue
 			}
 
