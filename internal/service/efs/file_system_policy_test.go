0053447c4da49d4bb8e7a3a46f35f56b48452e46
Graham Davison <gdavison@hashicorp.com>
2025-11-28 16:32:15 -0800

    `efs`

--- internal/service/efs/file_system_policy_test.go
+++ internal/service/efs/file_system_policy_test.go
@@ -15,8 +15,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/retry"
 	tfefs "github.com/hashicorp/terraform-provider-aws/internal/service/efs"
-	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
@@ -207,7 +207,7 @@ func testAccCheckFileSystemPolicyDestroy(ctx context.Context) resource.TestCheck
 
 			_, err := tfefs.FindFileSystemPolicyByID(ctx, conn, rs.Primary.ID)
 
-			if tfresource.NotFound(err) {
+			if retry.NotFound(err) {
 				continue
 			}
 
--- internal/service/efs/mount_target_test.go
+++ internal/service/efs/mount_target_test.go
@@ -15,8 +15,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/retry"
 	tfefs "github.com/hashicorp/terraform-provider-aws/internal/service/efs"
-	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
@@ -278,7 +278,7 @@ func testAccCheckMountTargetDestroy(ctx context.Context) resource.TestCheckFunc 
 
 			_, err := tfefs.FindMountTargetByID(ctx, conn, rs.Primary.ID)
 
-			if tfresource.NotFound(err) {
+			if retry.NotFound(err) {
 				continue
 			}
 
