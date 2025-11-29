57c814d1d712e363e900d869dd247c68c2cb32c2
Graham Davison <gdavison@hashicorp.com>
2025-11-28 16:31:37 -0800

    `cloudwatch`

--- internal/service/cloudwatch/contributor_managed_insight_rule_test.go
+++ internal/service/cloudwatch/contributor_managed_insight_rule_test.go
@@ -16,8 +16,8 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/create"
+	"github.com/hashicorp/terraform-provider-aws/internal/retry"
 	tfcloudwatch "github.com/hashicorp/terraform-provider-aws/internal/service/cloudwatch"
-	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
@@ -140,7 +140,7 @@ func testAccCheckContributorManagedInsightRuleDestroy(ctx context.Context) resou
 				rs.Primary.Attributes["template_name"],
 			)
 
-			if tfresource.NotFound(err) {
+			if retry.NotFound(err) {
 				return nil
 			}
 
