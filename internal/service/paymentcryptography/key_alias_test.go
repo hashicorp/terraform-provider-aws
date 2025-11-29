67ff9256c2da029ce7e439ddf1dbde6643348942
Graham Davison <gdavison@hashicorp.com>
2025-11-28 16:33:17 -0800

    `paymentcryptography`

--- internal/service/paymentcryptography/key_alias_test.go
+++ internal/service/paymentcryptography/key_alias_test.go
@@ -14,8 +14,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/retry"
 	tfkeyalias "github.com/hashicorp/terraform-provider-aws/internal/service/paymentcryptography"
-	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
@@ -170,7 +170,7 @@ func testAccCheckKeyAliasDestroy(ctx context.Context) resource.TestCheckFunc {
 
 			_, err := tfkeyalias.FindKeyAliasByName(ctx, conn, rs.Primary.ID)
 
-			if tfresource.NotFound(err) {
+			if retry.NotFound(err) {
 				continue
 			}
 
