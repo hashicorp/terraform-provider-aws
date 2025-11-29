72abff510bb9eccaa8bddc1a7a1bb137f813affe
Graham Davison <gdavison@hashicorp.com>
2025-11-28 16:32:31 -0800

    `globalaccelerator`

--- internal/service/globalaccelerator/cross_account_attachment.go
+++ internal/service/globalaccelerator/cross_account_attachment.go
@@ -24,6 +24,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/framework"
 	fwflex "github.com/hashicorp/terraform-provider-aws/internal/framework/flex"
 	fwtypes "github.com/hashicorp/terraform-provider-aws/internal/framework/types"
+	"github.com/hashicorp/terraform-provider-aws/internal/retry"
 	tfslices "github.com/hashicorp/terraform-provider-aws/internal/slices"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
@@ -146,7 +147,7 @@ func (r *crossAccountAttachmentResource) Read(ctx context.Context, request resou
 
 	output, err := findCrossAccountAttachmentByARN(ctx, conn, data.ID.ValueString())
 
-	if tfresource.NotFound(err) {
+	if retry.NotFound(err) {
 		response.Diagnostics.Append(fwdiag.NewResourceNotFoundWarningDiagnostic(err))
 		response.State.RemoveResource(ctx)
 
