3f67948f55f4c3e68d55664a0a0e6404a7ea61c8
Graham Davison <gdavison@hashicorp.com>
2025-11-28 16:31:01 -0800

    `appconfig`

--- internal/service/appconfig/extension_association_test.go
+++ internal/service/appconfig/extension_association_test.go
@@ -14,8 +14,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/retry"
 	tfappconfig "github.com/hashicorp/terraform-provider-aws/internal/service/appconfig"
-	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
@@ -139,7 +139,7 @@ func testAccCheckExtensionAssociationDestroy(ctx context.Context) resource.TestC
 
 			_, err := tfappconfig.FindExtensionByID(ctx, conn, rs.Primary.ID)
 
-			if tfresource.NotFound(err) {
+			if retry.NotFound(err) {
 				continue
 			}
 
