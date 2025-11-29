6a1ee830636600b3b540794c96387bca4d80fc21
Graham Davison <gdavison@hashicorp.com>
2025-11-28 16:32:27 -0800

    `fis`

--- internal/service/fis/experiment_template.go
+++ internal/service/fis/experiment_template.go
@@ -23,6 +23,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
 	"github.com/hashicorp/terraform-provider-aws/internal/flex"
+	"github.com/hashicorp/terraform-provider-aws/internal/retry"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
@@ -413,7 +414,7 @@ func resourceExperimentTemplateRead(ctx context.Context, d *schema.ResourceData,
 
 	experimentTemplate, err := findExperimentTemplateByID(ctx, conn, d.Id())
 
-	if !d.IsNewResource() && tfresource.NotFound(err) {
+	if !d.IsNewResource() && retry.NotFound(err) {
 		log.Printf("[WARN] FIS Experiment Template (%s) not found, removing from state", d.Id())
 		d.SetId("")
 		return diags
--- internal/service/fis/target_account_configuration_test.go
+++ internal/service/fis/target_account_configuration_test.go
@@ -19,8 +19,8 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/create"
+	"github.com/hashicorp/terraform-provider-aws/internal/retry"
 	tffis "github.com/hashicorp/terraform-provider-aws/internal/service/fis"
-	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
@@ -144,7 +144,7 @@ func testAccCheckTargetAccountConfigurationDestroy(ctx context.Context) resource
 			accountId := aws.String(rs.Primary.Attributes[names.AttrAccountID])
 			experimentId := aws.String(rs.Primary.Attributes["experiment_template_id"])
 			_, err := tffis.FindTargetAccountConfigurationByID(ctx, conn, accountId, experimentId)
-			if tfresource.NotFound(err) {
+			if retry.NotFound(err) {
 				return nil
 			}
 			if err != nil {
