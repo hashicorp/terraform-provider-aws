c4263983d11e2e44260a30178a92655b93e28ede
Graham Davison <gdavison@hashicorp.com>
2025-11-28 16:32:16 -0800

    `eks`

--- internal/service/eks/identity_provider_config_test.go
+++ internal/service/eks/identity_provider_config_test.go
@@ -15,8 +15,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/retry"
 	tfeks "github.com/hashicorp/terraform-provider-aws/internal/service/eks"
-	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
@@ -214,7 +214,7 @@ func testAccCheckIdentityProviderConfigDestroy(ctx context.Context) resource.Tes
 
 			_, err = tfeks.FindOIDCIdentityProviderConfigByTwoPartKey(ctx, conn, clusterName, configName)
 
-			if tfresource.NotFound(err) {
+			if retry.NotFound(err) {
 				continue
 			}
 
