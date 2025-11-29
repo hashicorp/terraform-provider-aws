a3bf03d37e3620b36d1440a926c8f5f0b087585f
Graham Davison <gdavison@hashicorp.com>
2025-11-28 16:33:16 -0800

    `osis`

--- internal/service/osis/pipeline.go
+++ internal/service/osis/pipeline.go
@@ -33,6 +33,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/framework"
 	fwflex "github.com/hashicorp/terraform-provider-aws/internal/framework/flex"
 	fwtypes "github.com/hashicorp/terraform-provider-aws/internal/framework/types"
+	"github.com/hashicorp/terraform-provider-aws/internal/retry"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/names"
@@ -269,7 +270,7 @@ func (r *pipelineResource) Read(ctx context.Context, request resource.ReadReques
 	name := data.PipelineName.ValueString()
 	pipeline, err := findPipelineByName(ctx, conn, name)
 
-	if tfresource.NotFound(err) {
+	if retry.NotFound(err) {
 		response.Diagnostics.Append(fwdiag.NewResourceNotFoundWarningDiagnostic(err))
 		response.State.RemoveResource(ctx)
 
@@ -397,7 +398,7 @@ func statusPipeline(ctx context.Context, conn *osis.Client, name string) sdkretr
 	return func() (any, string, error) {
 		output, err := findPipelineByName(ctx, conn, name)
 
-		if tfresource.NotFound(err) {
+		if retry.NotFound(err) {
 			return nil, "", nil
 		}
 
