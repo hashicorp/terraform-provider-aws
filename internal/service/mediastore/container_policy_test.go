cd9985bcc4aea7b3ba96fefc7d62c3c7c7a9ad7f
Graham Davison <gdavison@hashicorp.com>
2025-11-28 16:33:00 -0800

    `mediastore`

--- internal/service/mediastore/container_policy_test.go
+++ internal/service/mediastore/container_policy_test.go
@@ -14,8 +14,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/retry"
 	tfmediastore "github.com/hashicorp/terraform-provider-aws/internal/service/mediastore"
-	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
@@ -93,7 +93,7 @@ func testAccCheckContainerPolicyDestroy(ctx context.Context) resource.TestCheckF
 
 			_, err := tfmediastore.FindContainerPolicyByContainerName(ctx, conn, rs.Primary.ID)
 
-			if tfresource.NotFound(err) {
+			if retry.NotFound(err) {
 				continue
 			}
 
