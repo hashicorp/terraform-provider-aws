348d2960f8782b711ee41ba998e16e7f1a8aefd6
Graham Davison <gdavison@hashicorp.com>
2025-11-28 16:31:17 -0800

    `bedrock`

--- internal/service/bedrock/model_invocation_logging_configuration.go
+++ internal/service/bedrock/model_invocation_logging_configuration.go
@@ -21,6 +21,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/framework"
 	fwflex "github.com/hashicorp/terraform-provider-aws/internal/framework/flex"
 	fwtypes "github.com/hashicorp/terraform-provider-aws/internal/framework/types"
+	"github.com/hashicorp/terraform-provider-aws/internal/retry"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
@@ -172,7 +173,7 @@ func (r *modelInvocationLoggingConfigurationResource) Read(ctx context.Context, 
 
 	output, err := findModelInvocationLoggingConfiguration(ctx, conn)
 
-	if tfresource.NotFound(err) {
+	if retry.NotFound(err) {
 		response.Diagnostics.Append(fwdiag.NewResourceNotFoundWarningDiagnostic(err))
 		response.State.RemoveResource(ctx)
 		return
