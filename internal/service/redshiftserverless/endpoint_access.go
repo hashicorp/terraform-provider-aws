66a10ce5b117138d57e2a7b09880c7bd44795c37
Graham Davison <gdavison@hashicorp.com>
2025-11-28 16:33:29 -0800

    `redshiftserverless`

--- internal/service/redshiftserverless/endpoint_access.go
+++ internal/service/redshiftserverless/endpoint_access.go
@@ -19,6 +19,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
 	"github.com/hashicorp/terraform-provider-aws/internal/flex"
+	"github.com/hashicorp/terraform-provider-aws/internal/retry"
 	tfslices "github.com/hashicorp/terraform-provider-aws/internal/slices"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
@@ -170,7 +171,7 @@ func resourceEndpointAccessRead(ctx context.Context, d *schema.ResourceData, met
 
 	endpointAccess, err := findEndpointAccessByName(ctx, conn, d.Id())
 
-	if !d.IsNewResource() && tfresource.NotFound(err) {
+	if !d.IsNewResource() && retry.NotFound(err) {
 		log.Printf("[WARN] Redshift Serverless Endpoint Access (%s) not found, removing from state", d.Id())
 		d.SetId("")
 		return diags
@@ -275,7 +276,7 @@ func statusEndpointAccess(ctx context.Context, conn *redshiftserverless.Client, 
 	return func() (any, string, error) {
 		output, err := findEndpointAccessByName(ctx, conn, name)
 
-		if tfresource.NotFound(err) {
+		if retry.NotFound(err) {
 			return nil, "", nil
 		}
 
