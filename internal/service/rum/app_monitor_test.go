5852bab3c9f896597328644601aa8cc464339e65
Graham Davison <gdavison@hashicorp.com>
2025-11-28 16:33:41 -0800

    `rum`

--- internal/service/rum/app_monitor_test.go
+++ internal/service/rum/app_monitor_test.go
@@ -14,8 +14,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/retry"
 	tfcloudwatchrum "github.com/hashicorp/terraform-provider-aws/internal/service/rum"
-	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
@@ -271,7 +271,7 @@ func testAccCheckAppMonitorDestroy(ctx context.Context) resource.TestCheckFunc {
 
 			_, err := tfcloudwatchrum.FindAppMonitorByName(ctx, conn, rs.Primary.ID)
 
-			if tfresource.NotFound(err) {
+			if retry.NotFound(err) {
 				continue
 			}
 
