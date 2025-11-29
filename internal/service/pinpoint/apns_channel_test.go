48586abc919ad16f619cc95c1e2ee5d025202369
Graham Davison <gdavison@hashicorp.com>
2025-11-28 16:33:18 -0800

    `pinpoint`

--- internal/service/pinpoint/apns_channel_test.go
+++ internal/service/pinpoint/apns_channel_test.go
@@ -16,8 +16,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/retry"
 	tfpinpoint "github.com/hashicorp/terraform-provider-aws/internal/service/pinpoint"
-	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
@@ -201,7 +201,7 @@ func testAccCheckAPNSChannelDestroy(ctx context.Context) resource.TestCheckFunc 
 
 			_, err := tfpinpoint.FindAPNSChannelByApplicationId(ctx, conn, rs.Primary.ID)
 
-			if tfresource.NotFound(err) {
+			if retry.NotFound(err) {
 				continue
 			}
 
--- internal/service/pinpoint/event_stream.go
+++ internal/service/pinpoint/event_stream.go
@@ -16,6 +16,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
+	"github.com/hashicorp/terraform-provider-aws/internal/retry"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 	"github.com/hashicorp/terraform-provider-aws/names"
@@ -90,7 +91,7 @@ func resourceEventStreamRead(ctx context.Context, d *schema.ResourceData, meta a
 
 	output, err := findEventStreamByApplicationId(ctx, conn, d.Id())
 
-	if !d.IsNewResource() && tfresource.NotFound(err) {
+	if !d.IsNewResource() && retry.NotFound(err) {
 		log.Printf("[WARN] Pinpoint Event Stream (%s) not found, removing from state", d.Id())
 		d.SetId("")
 		return diags
