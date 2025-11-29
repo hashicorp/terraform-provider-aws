5292c92f4031583462a8fac22f334dacf213298d
Graham Davison <gdavison@hashicorp.com>
2025-11-28 16:31:11 -0800

    `auditmanager`

--- internal/service/auditmanager/framework.go
+++ internal/service/auditmanager/framework.go
@@ -24,6 +24,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/framework"
 	fwflex "github.com/hashicorp/terraform-provider-aws/internal/framework/flex"
 	fwtypes "github.com/hashicorp/terraform-provider-aws/internal/framework/types"
+	"github.com/hashicorp/terraform-provider-aws/internal/retry"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/names"
@@ -148,7 +149,7 @@ func (r *frameworkResource) Read(ctx context.Context, request resource.ReadReque
 
 	output, err := findFrameworkByID(ctx, conn, data.ID.ValueString())
 
-	if tfresource.NotFound(err) {
+	if retry.NotFound(err) {
 		response.Diagnostics.Append(fwdiag.NewResourceNotFoundWarningDiagnostic(err))
 		response.State.RemoveResource(ctx)
 
