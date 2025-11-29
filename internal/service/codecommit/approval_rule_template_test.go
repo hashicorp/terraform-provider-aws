f433864c72ac5e860aa6c364156e4fa8c44a00c3
Graham Davison <gdavison@hashicorp.com>
2025-11-28 16:31:41 -0800

    `codecommit`

--- internal/service/codecommit/approval_rule_template_test.go
+++ internal/service/codecommit/approval_rule_template_test.go
@@ -13,8 +13,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/retry"
 	tfcodecommit "github.com/hashicorp/terraform-provider-aws/internal/service/codecommit"
-	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
@@ -180,7 +180,7 @@ func testAccCheckApprovalRuleTemplateDestroy(ctx context.Context) resource.TestC
 
 			_, err := tfcodecommit.FindApprovalRuleTemplateByName(ctx, conn, rs.Primary.ID)
 
-			if tfresource.NotFound(err) {
+			if retry.NotFound(err) {
 				continue
 			}
 
