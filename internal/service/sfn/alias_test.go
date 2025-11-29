c2cba26d2a17f55ebf3d13b2c279e3f4da82d0c2
Graham Davison <gdavison@hashicorp.com>
2025-11-28 16:34:02 -0800

    `sfn`

--- internal/service/sfn/alias_test.go
+++ internal/service/sfn/alias_test.go
@@ -14,8 +14,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/retry"
 	tfsfn "github.com/hashicorp/terraform-provider-aws/internal/service/sfn"
-	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
@@ -112,7 +112,7 @@ func testAccCheckAliasDestroy(ctx context.Context) resource.TestCheckFunc {
 
 			_, err := tfsfn.FindAliasByARN(ctx, conn, rs.Primary.ID)
 
-			if tfresource.NotFound(err) {
+			if retry.NotFound(err) {
 				continue
 			}
 
