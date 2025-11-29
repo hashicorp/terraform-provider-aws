600bbdeefd8c421c9edf9dea1ab430e8467fb857
Graham Davison <gdavison@hashicorp.com>
2025-11-28 16:32:54 -0800

    `licensemanager`

--- internal/service/licensemanager/grant_accepter_test.go
+++ internal/service/licensemanager/grant_accepter_test.go
@@ -16,8 +16,8 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/envvar"
+	"github.com/hashicorp/terraform-provider-aws/internal/retry"
 	tflicensemanager "github.com/hashicorp/terraform-provider-aws/internal/service/licensemanager"
-	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
@@ -121,7 +121,7 @@ func testAccCheckGrantAccepterDestroyWithProvider(ctx context.Context) acctest.T
 
 			_, err := tflicensemanager.FindReceivedGrantByARN(ctx, conn, rs.Primary.ID)
 
-			if tfresource.NotFound(err) {
+			if retry.NotFound(err) {
 				continue
 			}
 
