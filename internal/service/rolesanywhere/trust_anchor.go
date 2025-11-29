4692cb3a4a98b88c662a87b0b1eb7dbc874b8b55
Graham Davison <gdavison@hashicorp.com>
2025-11-28 16:33:33 -0800

    `rolesanywhere`

--- internal/service/rolesanywhere/trust_anchor.go
+++ internal/service/rolesanywhere/trust_anchor.go
@@ -19,6 +19,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/enum"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
+	"github.com/hashicorp/terraform-provider-aws/internal/retry"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
@@ -209,7 +210,7 @@ func resourceTrustAnchorRead(ctx context.Context, d *schema.ResourceData, meta a
 
 	trustAnchor, err := findTrustAnchorByID(ctx, conn, d.Id())
 
-	if !d.IsNewResource() && tfresource.NotFound(err) {
+	if !d.IsNewResource() && retry.NotFound(err) {
 		log.Printf("[WARN] RolesAnywhere Trust Anchor (%s) not found, removing from state", d.Id())
 		d.SetId("")
 		return diags
