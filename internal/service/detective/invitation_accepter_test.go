6aa3b631845aa3cb5db3da6e0e8ead969b8242a5
Graham Davison <gdavison@hashicorp.com>
2025-11-28 16:31:57 -0800

    `detective`

--- internal/service/detective/invitation_accepter_test.go
+++ internal/service/detective/invitation_accepter_test.go
@@ -12,8 +12,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/retry"
 	tfdetective "github.com/hashicorp/terraform-provider-aws/internal/service/detective"
-	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
@@ -72,7 +72,7 @@ func testAccCheckInvitationAccepterDestroy(ctx context.Context) resource.TestChe
 
 			_, err := tfdetective.FindInvitationByGraphARN(ctx, conn, rs.Primary.ID)
 
-			if tfresource.NotFound(err) {
+			if retry.NotFound(err) {
 				continue
 			}
 
