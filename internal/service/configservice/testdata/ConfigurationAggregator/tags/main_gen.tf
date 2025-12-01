578d58d4f47588e474a40c4c4239f871adee70ae
Graham Davison <gdavison@hashicorp.com>
2025-12-01 11:10:37 -0800

    Adds tests for `aws_config_configuration_aggregator`

---
+++ internal/service/configservice/testdata/ConfigurationAggregator/tags/main_gen.tf
@@ -0,0 +1,30 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+resource "aws_config_configuration_aggregator" "test" {
+  name = var.rName
+
+  account_aggregation_source {
+    account_ids = [data.aws_caller_identity.current.account_id]
+    regions     = [data.aws_region.current.region]
+  }
+
+  tags = var.resource_tags
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+variable "rName" {
+  description = "Name for resource"
+  type        = string
+  nullable    = false
+}
+
+variable "resource_tags" {
+  description = "Tags to set on resource. To specify no tags, set to `null`"
+  # Not setting a default, so that this must explicitly be set to `null` to specify no tags
+  type     = map(string)
+  nullable = true
+}
