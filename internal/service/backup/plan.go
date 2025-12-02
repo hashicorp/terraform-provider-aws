f93fb3b508145892e99f61514e7f6cab282d677c
Patrick Decat <pdecat@gmail.com>
2025-12-02 16:20:55 +0100

    fix: set default value of schedule attribute of aws_backup_plan rule
    Resolves #23976

--- internal/service/backup/plan.go
+++ internal/service/backup/plan.go
@@ -27,6 +27,7 @@ import (
 )
 
 const (
+	defaultPlanRuleScheduleExpression         = "cron(0 5 ? * * *)"
 	defaultPlanRuleScheduleExpressionTimezone = "Etc/UTC"
 )
 
@@ -167,6 +168,7 @@ func resourcePlan() *schema.Resource {
 						names.AttrSchedule: {
 							Type:     schema.TypeString,
 							Optional: true,
+							Default:  defaultPlanRuleScheduleExpression,
 						},
 						"schedule_expression_timezone": {
 							Type:     schema.TypeString,
