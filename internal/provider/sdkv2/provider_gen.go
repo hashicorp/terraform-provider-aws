235adfbd1923802c17bef5cbeabd18d5670d6fbe
Isaiah Peralta <98777083+iperalta7@users.noreply.github.com>
2025-12-03 11:42:56 -0500

    run make gen

--- internal/provider/sdkv2/provider_gen.go
+++ internal/provider/sdkv2/provider_gen.go
@@ -203,6 +203,14 @@ func endpointsSchema() *schema.Schema {
 					Description: "Use this to override the default service endpoint URL",
 				},
 
+				// arczonalshift
+
+				"arczonalshift": {
+					Type:        schema.TypeString,
+					Optional:    true,
+					Description: "Use this to override the default service endpoint URL",
+				},
+
 				// athena
 
 				"athena": {
