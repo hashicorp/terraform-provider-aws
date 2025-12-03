8269fcdb2993ebaa41a8b9521259b837f84fa9d9
Tabito Hara <105637744+tabito-hara@users.noreply.github.com>
2025-12-04 01:32:40 +0900

    Update internal/service/logs/group.go
    Co-authored-by: Kit Ewbank <Kit_Ewbank@hotmail.com>

--- internal/service/logs/group.go
+++ internal/service/logs/group.go
@@ -56,6 +56,7 @@ func resourceGroup() *schema.Resource {
 			"deletion_protection_enabled": {
 				Type:     schema.TypeBool,
 				Optional: true,
+				Computed: true,
 			},
 			names.AttrKMSKeyID: {
 				Type:     schema.TypeString,
