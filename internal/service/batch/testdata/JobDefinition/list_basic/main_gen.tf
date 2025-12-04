503f790fd841518f0dbc10e211862c6aeca3bcee
Dirk Avery <dirk.avery@gmail.com>
2025-12-04 15:33:21 -0500

    resource/aws_batch_job_definition: Add list resource tests

---
+++ internal/service/batch/testdata/JobDefinition/list_basic/main_gen.tf
@@ -0,0 +1,20 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+resource "aws_batch_job_definition" "test" {
+  count = 2
+
+  name             = "${var.rName}-${count.index}"
+  type             = "container"
+  container_properties = jsonencode({
+    image = "busybox"
+    vcpus = 1
+    memory = 128
+  })
+}
+
+variable "rName" {
+  description = "Name for resource"
+  type        = string
+  nullable    = false
+}
