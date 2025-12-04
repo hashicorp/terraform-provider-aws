957b169546f4e94d954527e22067cdc2282c380d
Dirk Avery <dirk.avery@gmail.com>
2025-12-04 18:16:41 -0500

    you basic

--- internal/service/batch/job_definition_list_test.go
+++ internal/service/batch/job_definition_list_test.go
@@ -19,7 +19,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
-func TestAccBatchJobDefinition_List_Basic(t *testing.T) {
+func TestAccBatchJobDefinition_List_basic(t *testing.T) {
 	ctx := acctest.Context(t)
 
 	resourceName1 := "aws_batch_job_definition.test[0]"
