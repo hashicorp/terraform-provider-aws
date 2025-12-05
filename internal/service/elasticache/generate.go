3e629c14dfe13d62b6fd76f0547c9b8b6aeb0f6f
Dirk Avery <dirk.avery@gmail.com>
2025-12-05 12:09:14 -0500

    Update copyright headers (batch 54/63)
    Files: 5301-5400 of 6292

--- internal/service/elasticache/generate.go
+++ internal/service/elasticache/generate.go
@@ -1,4 +1,4 @@
-// Copyright (c) HashiCorp, Inc.
+// Copyright IBM Corp. 2014, 2025
 // SPDX-License-Identifier: MPL-2.0
 
 //go:generate go run ../../generate/tags/main.go -CreateTags -ListTags -ListTagsInIDElem=ResourceName -ListTagsOutTagsElem=TagList -ServiceTagsSlice -TagOp=AddTagsToResource -TagInIDElem=ResourceName -UntagOp=RemoveTagsFromResource -UpdateTags -RetryTagOps -RetryTagsListTagsType=ListTagsForResourceOutput -RetryErrorCode=awstypes.InvalidReplicationGroupStateFault "-RetryErrorMessage=not in available state" -RetryTimeout=15m
