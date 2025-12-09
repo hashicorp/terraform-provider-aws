ac41b48c94d73cf73b080a9306f9fcf0b6e5fd36
Graham Davison <gdavison@hashicorp.com>
2025-12-09 10:41:40 -0800

    `ecs`

--- internal/service/ecs/service.go
+++ internal/service/ecs/service.go
@@ -2081,7 +2081,7 @@ func findServices(ctx context.Context, conn *ecs.Client, input *ecs.DescribeServ
 	}
 
 	if output == nil {
-		return nil, tfresource.NewEmptyResultError(input)
+		return nil, tfresource.NewEmptyResultError()
 	}
 
 	// When an ECS Service is not found by DescribeServices(), it will return a Failure struct with Reason = "MISSING"
@@ -2370,7 +2370,7 @@ func findServiceDeployments(ctx context.Context, conn *ecs.Client, input *ecs.De
 	}
 
 	if output == nil {
-		return nil, tfresource.NewEmptyResultError(input)
+		return nil, tfresource.NewEmptyResultError()
 	}
 
 	return output.ServiceDeployments, nil
