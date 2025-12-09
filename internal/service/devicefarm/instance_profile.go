8123df52406dabb94781800969f1471e56dfd0b4
Graham Davison <gdavison@hashicorp.com>
2025-12-09 10:41:24 -0800

    `devicefarm`

--- internal/service/devicefarm/instance_profile.go
+++ internal/service/devicefarm/instance_profile.go
@@ -218,7 +218,7 @@ func findInstanceProfileByARN(ctx context.Context, conn *devicefarm.Client, arn 
 	}
 
 	if output == nil || output.InstanceProfile == nil {
-		return nil, tfresource.NewEmptyResultError(input)
+		return nil, tfresource.NewEmptyResultError()
 	}
 
 	return output.InstanceProfile, nil
