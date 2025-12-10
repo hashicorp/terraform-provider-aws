bf334446841c7a3a74076c41d3408280fe8fb95f
Dirk Avery <dirk.avery@gmail.com>
2025-12-09 19:23:22 -0500

    Add copyright headers to services batch 9/19 (100 files)
    Updated copyright headers using copyplop for services a-d and t-z

--- internal/service/cloudfront/generate.go
+++ internal/service/cloudfront/generate.go
@@ -1,4 +1,4 @@
-// Copyright (c) HashiCorp, Inc.
+// Copyright IBM Corp. 2014, 2025
 // SPDX-License-Identifier: MPL-2.0
 
 //go:generate go run ../../generate/listpages/main.go -ListOps=ListCachePolicies -InputPaginator=Marker -OutputPaginator=CachePolicyList.NextMarker -- list_cache_policies_pages_gen.go
