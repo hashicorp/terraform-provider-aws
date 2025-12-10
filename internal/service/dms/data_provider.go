b343dd98db03343404b84a2d34f28c4005f8c360
Paul Santus <paul.santus@gmail.com>
2025-12-10 01:21:06 +0100

    Fix retry import alias per importas linter

--- internal/service/dms/data_provider.go
+++ internal/service/dms/data_provider.go
@@ -12,7 +12,7 @@ import (
 	dms "github.com/aws/aws-sdk-go-v2/service/databasemigrationservice"
 	awstypes "github.com/aws/aws-sdk-go-v2/service/databasemigrationservice/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+	sdkretry "github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
@@ -240,7 +240,7 @@ func findDataProviderByARN(ctx context.Context, conn *dms.Client, arn string) (*
 	output, err := conn.DescribeDataProviders(ctx, input)
 
 	if errs.IsA[*awstypes.ResourceNotFoundFault](err) {
-		return nil, &retry.NotFoundError{
+		return nil, &sdkretry.NotFoundError{
 			LastError:   err,
 			LastRequest: input,
 		}
