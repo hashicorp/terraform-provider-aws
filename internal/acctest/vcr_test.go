14c0ab143bd31fbc81d8fcf4b95049ffe28c5625
Graham Davison <gdavison@hashicorp.com>
2025-12-17 16:36:23 -0800

    Adds `acctest.RandIntRange`

--- internal/acctest/vcr_test.go
+++ internal/acctest/vcr_test.go
@@ -52,3 +52,25 @@ func TestRandomWithPrefix(t *testing.T) {
 		t.Errorf("REPLAY_ONLY: %s, RECORD_ONLY: %s", rep2, rec2)
 	}
 }
+
+func TestRandIntRange(t *testing.T) {
+	ctx := acctest.Context(t)
+
+	t.Setenv("VCR_PATH", t.TempDir())
+
+	t.Setenv("VCR_MODE", "RECORD_ONLY")
+	rec1 := acctest.RandIntRange(t, 1, 100)
+	rec2 := acctest.RandIntRange(t, 1, 100)
+	acctest.CloseVCRRecorder(ctx, t)
+
+	t.Setenv("VCR_MODE", "REPLAY_ONLY")
+	rep1 := acctest.RandIntRange(t, 1, 100)
+	rep2 := acctest.RandIntRange(t, 1, 100)
+
+	if rep1 != rec1 {
+		t.Errorf("REPLAY_ONLY: %d, RECORD_ONLY: %d", rep1, rec1)
+	}
+	if rep2 != rec2 {
+		t.Errorf("REPLAY_ONLY: %d, RECORD_ONLY: %d", rep2, rec2)
+	}
+}
