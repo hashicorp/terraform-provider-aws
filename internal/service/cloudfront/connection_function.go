a0bd200328c00a34e7cd0933ce718c0c6d77767a
Madhav Vishnubhatta <mvishnub@amazon.co.uk>
2025-12-19 12:01:21 +0000

    Added changelog

--- internal/service/cloudfront/connection_function.go
+++ internal/service/cloudfront/connection_function.go
@@ -13,7 +13,6 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/cloudfront"
 	awstypes "github.com/aws/aws-sdk-go-v2/service/cloudfront/types"
 	"github.com/hashicorp/terraform-plugin-framework-timeouts/resource/timeouts"
-	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
