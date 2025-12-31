92c6179504932a280f04dd119d8992fc2fdbae8e
tabito <tabito.htnet.org@gmail.com>
2026-01-01 00:06:02 +0900

    Add a test case to verify tunneln_inside_cidr is updated in-place

--- internal/service/ec2/vpnsite_connection_test.go
+++ internal/service/ec2/vpnsite_connection_test.go
@@ -15,6 +15,7 @@ import (
 	awstypes "github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	sdkacctest "github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
@@ -528,10 +529,28 @@ func TestAccSiteVPNConnection_tunnel1InsideCIDR(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testAccVPNConnectionConfig_tunnel1InsideCIDR(rName, rBgpAsn, "169.254.8.0/30", "169.254.9.0/30"),
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceName, plancheck.ResourceActionCreate),
+					},
+				},
 				Check: resource.ComposeAggregateTestCheckFunc(
 					testAccVPNConnectionExists(ctx, resourceName, &vpn),
 					resource.TestCheckResourceAttr(resourceName, "tunnel1_inside_cidr", "169.254.8.0/30"),
 					resource.TestCheckResourceAttr(resourceName, "tunnel2_inside_cidr", "169.254.9.0/30"),
+				),
+			},
+			{
+				Config: testAccVPNConnectionConfig_tunnel1InsideCIDR(rName, rBgpAsn, "169.254.10.0/30", "169.254.11.0/30"),
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceName, plancheck.ResourceActionUpdate),
+					},
+				},
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccVPNConnectionExists(ctx, resourceName, &vpn),
+					resource.TestCheckResourceAttr(resourceName, "tunnel1_inside_cidr", "169.254.10.0/30"),
+					resource.TestCheckResourceAttr(resourceName, "tunnel2_inside_cidr", "169.254.11.0/30"),
 				),
 			},
 			// NOTE: Import does not currently have access to the Terraform configuration,
