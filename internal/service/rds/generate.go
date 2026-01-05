140f97d46ea49f726a4f5b3e827529bf96c04911
Dirk Avery <dirk.avery@gmail.com>
2026-01-05 14:10:08 -0500

    Update copyright year to 2026 (batch 76/108)

--- internal/service/rds/generate.go
+++ internal/service/rds/generate.go
@@ -1,4 +1,4 @@
-// Copyright IBM Corp. 2014, 2025
+// Copyright IBM Corp. 2014, 2026
 // SPDX-License-Identifier: MPL-2.0
 
 //go:generate go run ../../generate/tags/main.go -ListTags -ListTagsInIDElem=ResourceName -ListTagsOutTagsElem=TagList -ServiceTagsSlice -TagOp=AddTagsToResource -TagInIDElem=ResourceName -UntagOp=RemoveTagsFromResource -UpdateTags
