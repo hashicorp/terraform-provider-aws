6a2c4133fbd1d98e6eab91ba923462dda5973f81
Stefan Freitag <stefan.freitag@udo.edu>
2026-01-10 11:25:27 +0100

    fix: add missing properties to structs

--- internal/service/ecr/lifecycle_policy_document_data_source_test.go
+++ internal/service/ecr/lifecycle_policy_document_data_source_test.go
@@ -6,6 +6,7 @@ package ecr_test
 import (
 	"testing"
 
+	"github.com/YakDriver/regexache"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/names"
@@ -24,6 +25,28 @@ func TestAccECRLifecyclePolicyDocumentDataSource_basic(t *testing.T) {
 				Config: testAccLifecyclePolicyDocumentDataSourceConfig_basic,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrSet(dataSourceName, names.AttrJSON),
+				),
+			},
+		},
+	})
+}
+
+func TestAccECRLifecyclePolicyDocumentDataSource_storageClassTransition(t *testing.T) {
+	ctx := acctest.Context(t)
+	dataSourceName := "data.aws_ecr_lifecycle_policy_document.test"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
+		ErrorCheck:               acctest.ErrorCheck(t, names.ECRServiceID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccLifecyclePolicyDocumentDataSourceConfig_storageClassTransition,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(dataSourceName, names.AttrJSON),
+					resource.TestMatchResourceAttr(dataSourceName, names.AttrJSON, regexache.MustCompile(`"targetStorageClass":\s*"archive"`)),
+					resource.TestMatchResourceAttr(dataSourceName, names.AttrJSON, regexache.MustCompile(`"storageClass":\s*"archive"`)),
+					resource.TestMatchResourceAttr(dataSourceName, names.AttrJSON, regexache.MustCompile(`"sinceImageTransitioned"`)),
 				),
 			},
 		},
@@ -95,6 +118,60 @@ data "aws_ecr_lifecycle_policy_document" "test" {
       count_type    = "sinceImageTransitioned"
       count_unit    = "days"
       count_number  = 365
+    }
+
+    action {
+      type = "expire"
+    }
+  }
+}
+`
+
+const testAccLifecyclePolicyDocumentDataSourceConfig_storageClassTransition = `
+data "aws_ecr_lifecycle_policy_document" "test" {
+  rule {
+    priority    = 1
+    description = "Expire untagged images after 7 days"
+
+    selection {
+      tag_status   = "untagged"
+      count_type   = "sinceImagePushed"
+      count_unit   = "days"
+      count_number = 7
+    }
+
+    action {
+      type = "expire"
+    }
+  }
+
+  rule {
+    priority    = 2
+    description = "Archive unused images after 90 days"
+
+    selection {
+      tag_status   = "any"
+      count_type   = "sinceImagePulled"
+      count_unit   = "days"
+      count_number = 90
+    }
+
+    action {
+      type                 = "transition"
+      target_storage_class = "archive"
+    }
+  }
+
+  rule {
+    priority    = 3
+    description = "Delete archive images after 90 days"
+
+    selection {
+      tag_status    = "any"
+      count_type    = "sinceImageTransitioned"
+      count_unit    = "days"
+      storage_class = "archive"
+      count_number  = 90
     }
 
     action {
