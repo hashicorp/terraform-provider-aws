a603227b6a666c1911d6d3c897860c0a3a097da1
Kit Ewbank <Kit_Ewbank@hotmail.com>
2026-01-12 10:12:39 -0500

    d/aws_organizations_entity_path: New data source.

---
+++ internal/service/organizations/entity_path_data_source.go
@@ -0,0 +1,59 @@
+// Copyright IBM Corp. 2014, 2026
+// SPDX-License-Identifier: MPL-2.0
+
+package organizations
+
+import (
+	"context"
+
+	"github.com/YakDriver/regexache"
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-provider-aws/internal/framework"
+)
+
+// @FrameworkDataSource("aws_organizations_entity_path", name="Entity Path")
+func newEntityPathDataSource(context.Context) (datasource.DataSourceWithConfigure, error) {
+	return &entityPathDataSource{}, nil
+}
+
+type entityPathDataSource struct {
+	framework.DataSourceWithModel[entityPathDataSourceModel]
+}
+
+func (d *entityPathDataSource) Schema(ctx context.Context, request datasource.SchemaRequest, response *datasource.SchemaResponse) {
+	response.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"entity_id": schema.StringAttribute{
+				Required: true,
+				Validators: []validator.String{
+					stringvalidator.RegexMatches(regexache.MustCompile(`^(\d{12})|(ou-[0-9a-z]{4,32}-[a-z0-9]{8,32})$`), "must be an organizational unit (OU) or AWS account ID"),
+				},
+			},
+			"entity_path": schema.StringAttribute{
+				Computed: true,
+			},
+		},
+	}
+}
+
+func (d *entityPathDataSource) Read(ctx context.Context, request datasource.ReadRequest, response *datasource.ReadResponse) {
+	var data entityPathDataSourceModel
+	response.Diagnostics.Append(request.Config.Get(ctx, &data)...)
+	if response.Diagnostics.HasError() {
+		return
+	}
+
+	conn := d.Meta().OrganizationsClient(ctx)
+
+	// https://docs.aws.amazon.com/IAM/latest/UserGuide/access_policies_last-accessed-view-data-orgs.html#access_policies_last-accessed-viewing-orgs-entity-path.
+}
+
+type entityPathDataSourceModel struct {
+	framework.WithRegionModel
+	EntityID   types.String `tfsdk:"entity_id"`
+	EntityPath types.String `tfsdk:"entity_path"`
+}
