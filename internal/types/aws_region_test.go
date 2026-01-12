f1e833956a292173306b4811b3e22f62dbc6fd18
Dirk Avery <dirk.avery@gmail.com>
2026-01-12 17:03:23 -0500

    Fix tparallel linter error in TestCanonicalRegionPatterns
    - Add t.Parallel() to subtests to satisfy tparallel linter requirement
    - Ensures subtests run in parallel as expected by Go testing best practices

--- internal/types/aws_region_test.go
+++ internal/types/aws_region_test.go
@@ -48,6 +48,7 @@ func TestCanonicalRegionPatterns(t *testing.T) {
 
 	for patternName, pattern := range patterns {
 		t.Run(patternName, func(t *testing.T) {
+			t.Parallel()
 			regex := regexache.MustCompile("^" + pattern + "$")
 
 			for _, test := range testRegions {
