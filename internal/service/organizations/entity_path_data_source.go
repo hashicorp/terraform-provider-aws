b5e66e127b8e39a7a4fb13fb6d7d9d7f040ee3e7
Kit Ewbank <Kit_Ewbank@hotmail.com>
2026-01-12 11:58:30 -0500

    d/aws_organizations_entity_path: Documentation.

--- internal/service/organizations/entity_path_data_source.go
+++ internal/service/organizations/entity_path_data_source.go
@@ -5,14 +5,21 @@ package organizations
 
 import (
 	"context"
+	"fmt"
+	"slices"
+	"strings"
 
 	"github.com/YakDriver/regexache"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/organizations"
+	awstypes "github.com/aws/aws-sdk-go-v2/service/organizations/types"
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-provider-aws/internal/framework"
+	fwflex "github.com/hashicorp/terraform-provider-aws/internal/framework/flex"
 )
 
 // @FrameworkDataSource("aws_organizations_entity_path", name="Entity Path")
@@ -50,6 +57,42 @@ func (d *entityPathDataSource) Read(ctx context.Context, request datasource.Read
 	conn := d.Meta().OrganizationsClient(ctx)
 
 	// https://docs.aws.amazon.com/IAM/latest/UserGuide/access_policies_last-accessed-view-data-orgs.html#access_policies_last-accessed-viewing-orgs-entity-path.
+	entityID := fwflex.StringValueFromFramework(ctx, data.EntityID)
+	childID := entityID
+	var parts []string
+	for {
+		input := organizations.ListParentsInput{
+			ChildId: aws.String(childID),
+		}
+		parent, err := findParent(ctx, conn, &input)
+		if err != nil {
+			response.Diagnostics.AddError(fmt.Sprintf("reading Organizations entity (%s) parent", childID), err.Error())
+			return
+		}
+
+		parts = append(parts, childID)
+		parentID := aws.ToString(parent.Id)
+
+		if parent.Type == awstypes.ParentTypeRoot {
+			organization, err := findOrganization(ctx, conn)
+			if err != nil {
+				response.Diagnostics.AddError("reading Organizations organization", err.Error())
+				return
+			}
+
+			parts = append(parts, parentID)                      // Root.
+			parts = append(parts, aws.ToString(organization.Id)) // Organization.
+
+			break
+		}
+
+		childID = parentID
+	}
+
+	slices.Reverse(parts)
+	data.EntityPath = fwflex.StringValueToFramework(ctx, strings.Join(parts, "/"))
+
+	response.Diagnostics.Append(response.State.Set(ctx, &data)...)
 }
 
 type entityPathDataSourceModel struct {
