a9ce9d8eb5b1eaeeb35e7a0369d8de7e375c3527
Kit Ewbank <Kit_Ewbank@hotmail.com>
2026-01-12 12:55:38 -0500

    d/aws_organizations_entity_path: Acceptance tests.

---
+++ internal/service/organizations/entity_path_data_source_test.go
@@ -0,0 +1,58 @@
+// Copyright IBM Corp. 2014, 2026
+// SPDX-License-Identifier: MPL-2.0
+
+package organizations_test
+
+import (
+	"testing"
+
+	"github.com/YakDriver/regexache"
+	sdkacctest "github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+	"github.com/hashicorp/terraform-provider-aws/names"
+)
+
+var (
+	ouEntityPathRegexp = regexache.MustCompile(`o-[a-z0-9]{10,32}/r-[0-9a-z]{4,32}/(ou-[0-9a-z]{4,32}-[a-z0-9]{8,32})+`)
+)
+
+func TestAccOrganizationsEntityPathDataSource_ou(t *testing.T) {
+	ctx := acctest.Context(t)
+	datasource1Name := "data.aws_organizations_entity_path.test1"
+	datasource2Name := "data.aws_organizations_entity_path.test2"
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+			acctest.PreCheckOrganizationManagementAccount(ctx, t)
+		},
+		ErrorCheck:               acctest.ErrorCheck(t, names.OrganizationsServiceID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccEntityPathDataSourceConfig_ou(rName),
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(datasource1Name, tfjsonpath.New("entity_path"), knownvalue.StringRegexp(ouEntityPathRegexp)),
+					statecheck.ExpectKnownValue(datasource2Name, tfjsonpath.New("entity_path"), knownvalue.StringRegexp(ouEntityPathRegexp)),
+				},
+			},
+		},
+	})
+}
+
+func testAccEntityPathDataSourceConfig_ou(rName string) string {
+	return acctest.ConfigCompose(testAccOrganizationalUnitDataSourceConfig_basic(rName), `
+data "aws_organizations_entity_path" "test1" {
+  entity_id = aws_organizations_organizational_unit.parent.id
+}
+
+data "aws_organizations_entity_path" "test2" {
+  entity_id = aws_organizations_organizational_unit.child.id
+}
+`)
+}
