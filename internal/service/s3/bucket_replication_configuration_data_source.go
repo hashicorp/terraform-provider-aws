93fc24c390e2c56f34ff5644a845df65d7a5acc5
Jared Baker <jared.baker@hashicorp.com>
2026-01-13 16:13:06 -0500

    d/aws_s3_bucket_replication_configuration: prefer `framework.DataSourceComputedListOfObjectAttribute`
    ```console
    % make t K=s3 T=TestAccS3BucketReplicationConfigurationDataSource_
    make: Verifying source code with gofmt...
    ==> Checking that code complies with gofmt requirements...
    make: Running acceptance tests on branch: 🌿 f-aws_s3_bucket_replication_configuration-data_source 🌿...
    TF_ACC=1 go1.25.5 test ./internal/service/s3/... -v -count 1 -parallel 20 -run='TestAccS3BucketReplicationConfigurationDataSource_'  -timeout 360m -vet=off
    2026/01/13 16:11:54 Creating Terraform AWS Provider (SDKv2-style)...
    2026/01/13 16:11:54 Initializing Terraform AWS Provider (SDKv2-style)...
    
    --- PASS: TestAccS3BucketReplicationConfigurationDataSource_basic (20.77s)
    PASS
    ok      github.com/hashicorp/terraform-provider-aws/internal/service/s3 27.258s
    ```

--- internal/service/s3/bucket_replication_configuration_data_source.go
+++ internal/service/s3/bucket_replication_configuration_data_source.go
@@ -6,10 +6,8 @@ package s3
 import (
 	"context"
 
-	"github.com/hashicorp/terraform-plugin-framework-validators/listvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
-	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-provider-aws/internal/create"
 	"github.com/hashicorp/terraform-provider-aws/internal/framework"
@@ -41,256 +39,7 @@ func (d *dataSourceBucketReplicationConfiguration) Schema(ctx context.Context, r
 			names.AttrRole: schema.StringAttribute{
 				Computed: true,
 			},
-		},
-		Blocks: map[string]schema.Block{
-			names.AttrRule: schema.ListNestedBlock{
-				CustomType: fwtypes.NewListNestedObjectTypeOf[dsBucketRepConfigRuleModel](ctx),
-				Validators: []validator.List{
-					listvalidator.SizeAtMost(1000),
-				},
-				NestedObject: schema.NestedBlockObject{
-					Attributes: map[string]schema.Attribute{
-						names.AttrID: schema.StringAttribute{
-							Computed: true,
-						},
-						names.AttrPrefix: schema.StringAttribute{
-							Computed: true,
-						},
-						names.AttrPriority: schema.Int64Attribute{
-							Computed: true,
-						},
-						names.AttrStatus: schema.StringAttribute{
-							Computed: true,
-						},
-					},
-					Blocks: map[string]schema.Block{
-						"delete_marker_replication": schema.ListNestedBlock{
-							CustomType: fwtypes.NewListNestedObjectTypeOf[dsBucketRepConfigDeleteMarkerReplicationModel](ctx),
-							Validators: []validator.List{
-								listvalidator.SizeAtMost(1),
-							},
-							NestedObject: schema.NestedBlockObject{
-								Attributes: map[string]schema.Attribute{
-									names.AttrStatus: schema.StringAttribute{
-										Computed: true,
-									},
-								},
-							},
-						},
-						names.AttrDestination: schema.ListNestedBlock{
-							CustomType: fwtypes.NewListNestedObjectTypeOf[dsBucketRepConfigDestinationModel](ctx),
-							Validators: []validator.List{
-								listvalidator.SizeAtMost(1),
-							},
-							NestedObject: schema.NestedBlockObject{
-								Attributes: map[string]schema.Attribute{
-									"account": schema.StringAttribute{
-										Computed: true,
-									},
-									names.AttrBucket: schema.StringAttribute{
-										Computed: true,
-									},
-									names.AttrStorageClass: schema.StringAttribute{
-										Computed: true,
-									},
-								},
-								Blocks: map[string]schema.Block{
-									"access_control_translation": schema.ListNestedBlock{
-										CustomType: fwtypes.NewListNestedObjectTypeOf[dsBucketRepConfigAccessControlTranslationModel](ctx),
-										Validators: []validator.List{
-											listvalidator.SizeAtMost(1),
-										},
-										NestedObject: schema.NestedBlockObject{
-											Attributes: map[string]schema.Attribute{
-												names.AttrOwner: schema.StringAttribute{
-													Computed: true,
-												},
-											},
-										},
-									},
-									names.AttrEncryptionConfiguration: schema.ListNestedBlock{
-										CustomType: fwtypes.NewListNestedObjectTypeOf[dsBucketRepConfigEncryptionConfigurationModel](ctx),
-										Validators: []validator.List{
-											listvalidator.SizeAtMost(1),
-										},
-										NestedObject: schema.NestedBlockObject{
-											Attributes: map[string]schema.Attribute{
-												"replica_kms_key_id": schema.StringAttribute{
-													Computed: true,
-												},
-											},
-										},
-									},
-									"metrics": schema.ListNestedBlock{
-										CustomType: fwtypes.NewListNestedObjectTypeOf[dsBucketRepConfigMetricsModel](ctx),
-										Validators: []validator.List{
-											listvalidator.SizeAtMost(1),
-										},
-										NestedObject: schema.NestedBlockObject{
-											Attributes: map[string]schema.Attribute{
-												names.AttrStatus: schema.StringAttribute{
-													Computed: true,
-												},
-											},
-											Blocks: map[string]schema.Block{
-												"event_threshold": schema.ListNestedBlock{
-													CustomType: fwtypes.NewListNestedObjectTypeOf[dsBucketRepConfigEventThresholdModel](ctx),
-													Validators: []validator.List{
-														listvalidator.SizeAtMost(1),
-													},
-													NestedObject: schema.NestedBlockObject{
-														Attributes: map[string]schema.Attribute{
-															"minutes": schema.Int64Attribute{
-																Computed: true,
-															},
-														},
-													},
-												},
-											},
-										},
-									},
-									"replication_time": schema.ListNestedBlock{
-										CustomType: fwtypes.NewListNestedObjectTypeOf[dsBucketRepConfigReplicationTimeModel](ctx),
-										Validators: []validator.List{
-											listvalidator.SizeAtMost(1),
-										},
-										NestedObject: schema.NestedBlockObject{
-											Attributes: map[string]schema.Attribute{
-												names.AttrStatus: schema.StringAttribute{
-													Computed: true,
-												},
-											},
-											Blocks: map[string]schema.Block{
-												"time": schema.ListNestedBlock{
-													CustomType: fwtypes.NewListNestedObjectTypeOf[dsBucketRepConfigTimeModel](ctx),
-													Validators: []validator.List{
-														listvalidator.SizeAtMost(1),
-													},
-													NestedObject: schema.NestedBlockObject{
-														Attributes: map[string]schema.Attribute{
-															"minutes": schema.Int64Attribute{
-																Computed: true,
-															},
-														},
-													},
-												},
-											},
-										},
-									},
-								},
-							},
-						},
-						"existing_object_replication": schema.ListNestedBlock{
-							CustomType: fwtypes.NewListNestedObjectTypeOf[dsBucketRepConfigExistingObjectReplicationModel](ctx),
-							Validators: []validator.List{
-								listvalidator.SizeAtMost(1),
-							},
-							NestedObject: schema.NestedBlockObject{
-								Attributes: map[string]schema.Attribute{
-									names.AttrStatus: schema.StringAttribute{
-										Computed: true,
-									},
-								},
-							},
-						},
-						names.AttrFilter: schema.ListNestedBlock{
-							CustomType: fwtypes.NewListNestedObjectTypeOf[dsBucketRepConfigFilterModel](ctx),
-							Validators: []validator.List{
-								listvalidator.SizeAtMost(1),
-							},
-							NestedObject: schema.NestedBlockObject{
-								Attributes: map[string]schema.Attribute{
-									names.AttrPrefix: schema.StringAttribute{
-										Computed: true,
-									},
-								},
-								Blocks: map[string]schema.Block{
-									"and": schema.ListNestedBlock{
-										CustomType: fwtypes.NewListNestedObjectTypeOf[dsBucketRepConfigFilterAndModel](ctx),
-										Validators: []validator.List{
-											listvalidator.SizeAtMost(1),
-										},
-										NestedObject: schema.NestedBlockObject{
-											Attributes: map[string]schema.Attribute{
-												names.AttrPrefix: schema.StringAttribute{
-													Computed: true,
-												},
-											},
-											Blocks: map[string]schema.Block{
-												"tag": schema.ListNestedBlock{
-													CustomType: fwtypes.NewListNestedObjectTypeOf[dsBucketRepConfigFilterTagModel](ctx),
-													NestedObject: schema.NestedBlockObject{
-														Attributes: map[string]schema.Attribute{
-															names.AttrKey: schema.StringAttribute{
-																Computed: true,
-															},
-															names.AttrValue: schema.StringAttribute{
-																Computed: true,
-															},
-														},
-													},
-												},
-											},
-										},
-									},
-									"tag": schema.ListNestedBlock{
-										CustomType: fwtypes.NewListNestedObjectTypeOf[dsBucketRepConfigFilterTagModel](ctx),
-										Validators: []validator.List{
-											listvalidator.SizeAtMost(1),
-										},
-										NestedObject: schema.NestedBlockObject{
-											Attributes: map[string]schema.Attribute{
-												names.AttrKey: schema.StringAttribute{
-													Computed: true,
-												},
-												names.AttrValue: schema.StringAttribute{
-													Computed: true,
-												},
-											},
-										},
-									},
-								},
-							},
-						},
-						"source_selection_criteria": schema.ListNestedBlock{
-							CustomType: fwtypes.NewListNestedObjectTypeOf[dsBucketRepConfigSourceSelectionCriteriaModel](ctx),
-							Validators: []validator.List{
-								listvalidator.SizeAtMost(1),
-							},
-							NestedObject: schema.NestedBlockObject{
-								Blocks: map[string]schema.Block{
-									"replica_modifications": schema.ListNestedBlock{
-										CustomType: fwtypes.NewListNestedObjectTypeOf[dsBucketRepConfigReplicaModificationsModel](ctx),
-										Validators: []validator.List{
-											listvalidator.SizeAtMost(1),
-										},
-										NestedObject: schema.NestedBlockObject{
-											Attributes: map[string]schema.Attribute{
-												names.AttrStatus: schema.StringAttribute{
-													Computed: true,
-												},
-											},
-										},
-									},
-									"sse_kms_encrypted_objects": schema.ListNestedBlock{
-										CustomType: fwtypes.NewListNestedObjectTypeOf[dsBucketRepConfigSseKmsEncryptedObjectsModel](ctx),
-										Validators: []validator.List{
-											listvalidator.SizeAtMost(1),
-										},
-										NestedObject: schema.NestedBlockObject{
-											Attributes: map[string]schema.Attribute{
-												names.AttrStatus: schema.StringAttribute{
-													Computed: true,
-												},
-											},
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-			},
+			names.AttrRule: framework.DataSourceComputedListOfObjectAttribute[dataBucketRepConfigRuleModel](ctx),
 		},
 	}
 }
@@ -323,92 +72,92 @@ func (d *dataSourceBucketReplicationConfiguration) Read(ctx context.Context, req
 
 type dataSourceBucketReplicationConfigurationModel struct {
 	framework.WithRegionModel
-	Bucket types.String                                                `tfsdk:"bucket"`
-	Role   types.String                                                `tfsdk:"role"`
-	Rule   fwtypes.ListNestedObjectValueOf[dsBucketRepConfigRuleModel] `tfsdk:"rule"`
+	Bucket types.String                                                  `tfsdk:"bucket"`
+	Role   types.String                                                  `tfsdk:"role"`
+	Rule   fwtypes.ListNestedObjectValueOf[dataBucketRepConfigRuleModel] `tfsdk:"rule"`
 }
 
-type dsBucketRepConfigRuleModel struct {
-	DeleteMarkerReplication   fwtypes.ListNestedObjectValueOf[dsBucketRepConfigDeleteMarkerReplicationModel]   `tfsdk:"delete_marker_replication"`
-	Destination               fwtypes.ListNestedObjectValueOf[dsBucketRepConfigDestinationModel]               `tfsdk:"destination"`
-	ExistingObjectReplication fwtypes.ListNestedObjectValueOf[dsBucketRepConfigExistingObjectReplicationModel] `tfsdk:"existing_object_replication"`
-	Filter                    fwtypes.ListNestedObjectValueOf[dsBucketRepConfigFilterModel]                    `tfsdk:"filter"`
-	ID                        types.String                                                                     `tfsdk:"id"`
-	Prefix                    types.String                                                                     `tfsdk:"prefix"`
-	Priority                  types.Int64                                                                      `tfsdk:"priority"`
-	Status                    types.String                                                                     `tfsdk:"status"`
-	SourceSelectionCriteria   fwtypes.ListNestedObjectValueOf[dsBucketRepConfigSourceSelectionCriteriaModel]   `tfsdk:"source_selection_criteria"`
+type dataBucketRepConfigRuleModel struct {
+	DeleteMarkerReplication   fwtypes.ListNestedObjectValueOf[dataBucketRepConfigDeleteMarkerReplicationModel]   `tfsdk:"delete_marker_replication"`
+	Destination               fwtypes.ListNestedObjectValueOf[dataBucketRepConfigDestinationModel]               `tfsdk:"destination"`
+	ExistingObjectReplication fwtypes.ListNestedObjectValueOf[dataBucketRepConfigExistingObjectReplicationModel] `tfsdk:"existing_object_replication"`
+	Filter                    fwtypes.ListNestedObjectValueOf[dataBucketRepConfigFilterModel]                    `tfsdk:"filter"`
+	ID                        types.String                                                                       `tfsdk:"id"`
+	Prefix                    types.String                                                                       `tfsdk:"prefix"`
+	Priority                  types.Int64                                                                        `tfsdk:"priority"`
+	Status                    types.String                                                                       `tfsdk:"status"`
+	SourceSelectionCriteria   fwtypes.ListNestedObjectValueOf[dataBucketRepConfigSourceSelectionCriteriaModel]   `tfsdk:"source_selection_criteria"`
 }
 
-type dsBucketRepConfigDeleteMarkerReplicationModel struct {
+type dataBucketRepConfigDeleteMarkerReplicationModel struct {
 	Status types.String `tfsdk:"status"`
 }
 
-type dsBucketRepConfigDestinationModel struct {
-	AccessControlTranslation fwtypes.ListNestedObjectValueOf[dsBucketRepConfigAccessControlTranslationModel] `tfsdk:"access_control_translation"`
-	Account                  types.String                                                                    `tfsdk:"account"`
-	Bucket                   types.String                                                                    `tfsdk:"bucket"`
-	EncryptionConfiguration  fwtypes.ListNestedObjectValueOf[dsBucketRepConfigEncryptionConfigurationModel]  `tfsdk:"encryption_configuration"`
-	Metrics                  fwtypes.ListNestedObjectValueOf[dsBucketRepConfigMetricsModel]                  `tfsdk:"metrics"`
-	ReplicationTime          fwtypes.ListNestedObjectValueOf[dsBucketRepConfigReplicationTimeModel]          `tfsdk:"replication_time"`
-	StorageClass             types.String                                                                    `tfsdk:"storage_class"`
+type dataBucketRepConfigDestinationModel struct {
+	AccessControlTranslation fwtypes.ListNestedObjectValueOf[dataBucketRepConfigAccessControlTranslationModel] `tfsdk:"access_control_translation"`
+	Account                  types.String                                                                      `tfsdk:"account"`
+	Bucket                   types.String                                                                      `tfsdk:"bucket"`
+	EncryptionConfiguration  fwtypes.ListNestedObjectValueOf[dataBucketRepConfigEncryptionConfigurationModel]  `tfsdk:"encryption_configuration"`
+	Metrics                  fwtypes.ListNestedObjectValueOf[dataBucketRepConfigMetricsModel]                  `tfsdk:"metrics"`
+	ReplicationTime          fwtypes.ListNestedObjectValueOf[dataBucketRepConfigReplicationTimeModel]          `tfsdk:"replication_time"`
+	StorageClass             types.String                                                                      `tfsdk:"storage_class"`
 }
 
-type dsBucketRepConfigAccessControlTranslationModel struct {
+type dataBucketRepConfigAccessControlTranslationModel struct {
 	Owner types.String `tfsdk:"owner"`
 }
 
-type dsBucketRepConfigEncryptionConfigurationModel struct {
+type dataBucketRepConfigEncryptionConfigurationModel struct {
 	ReplicaKmsKeyId types.String `tfsdk:"replica_kms_key_id"`
 }
 
-type dsBucketRepConfigMetricsModel struct {
-	EventThreshold fwtypes.ListNestedObjectValueOf[dsBucketRepConfigEventThresholdModel] `tfsdk:"event_threshold"`
-	Status         types.String                                                          `tfsdk:"status"`
+type dataBucketRepConfigMetricsModel struct {
+	EventThreshold fwtypes.ListNestedObjectValueOf[dataBucketRepConfigEventThresholdModel] `tfsdk:"event_threshold"`
+	Status         types.String                                                            `tfsdk:"status"`
 }
 
-type dsBucketRepConfigEventThresholdModel struct {
+type dataBucketRepConfigEventThresholdModel struct {
 	Minutes types.Int64 `tfsdk:"minutes"`
 }
 
-type dsBucketRepConfigReplicationTimeModel struct {
-	Status types.String                                                `tfsdk:"status"`
-	Time   fwtypes.ListNestedObjectValueOf[dsBucketRepConfigTimeModel] `tfsdk:"time"`
+type dataBucketRepConfigReplicationTimeModel struct {
+	Status types.String                                                  `tfsdk:"status"`
+	Time   fwtypes.ListNestedObjectValueOf[dataBucketRepConfigTimeModel] `tfsdk:"time"`
 }
 
-type dsBucketRepConfigTimeModel struct {
+type dataBucketRepConfigTimeModel struct {
 	Minutes types.Int64 `tfsdk:"minutes"`
 }
 
-type dsBucketRepConfigExistingObjectReplicationModel struct {
+type dataBucketRepConfigExistingObjectReplicationModel struct {
 	Status types.String `tfsdk:"status"`
 }
 
-type dsBucketRepConfigFilterModel struct {
-	And    fwtypes.ListNestedObjectValueOf[dsBucketRepConfigFilterAndModel] `tfsdk:"and"`
-	Tag    fwtypes.ListNestedObjectValueOf[dsBucketRepConfigFilterTagModel] `tfsdk:"tag"`
-	Prefix types.String                                                     `tfsdk:"prefix"`
+type dataBucketRepConfigFilterModel struct {
+	And    fwtypes.ListNestedObjectValueOf[dataBucketRepConfigFilterAndModel] `tfsdk:"and"`
+	Tag    fwtypes.ListNestedObjectValueOf[dataBucketRepConfigFilterTagModel] `tfsdk:"tag"`
+	Prefix types.String                                                       `tfsdk:"prefix"`
 }
 
-type dsBucketRepConfigFilterAndModel struct {
-	Prefix types.String                                                     `tfsdk:"prefix"`
-	Tag    fwtypes.ListNestedObjectValueOf[dsBucketRepConfigFilterTagModel] `tfsdk:"tag"`
+type dataBucketRepConfigFilterAndModel struct {
+	Prefix types.String                                                       `tfsdk:"prefix"`
+	Tag    fwtypes.ListNestedObjectValueOf[dataBucketRepConfigFilterTagModel] `tfsdk:"tag"`
 }
 
-type dsBucketRepConfigFilterTagModel struct {
+type dataBucketRepConfigFilterTagModel struct {
 	Key   types.String `tfsdk:"key"`
 	Value types.String `tfsdk:"value"`
 }
 
-type dsBucketRepConfigSourceSelectionCriteriaModel struct {
-	ReplicaModifications   fwtypes.ListNestedObjectValueOf[dsBucketRepConfigReplicaModificationsModel]   `tfsdk:"replica_modifications"`
-	SseKmsEncryptedObjects fwtypes.ListNestedObjectValueOf[dsBucketRepConfigSseKmsEncryptedObjectsModel] `tfsdk:"sse_kms_encrypted_objects"`
+type dataBucketRepConfigSourceSelectionCriteriaModel struct {
+	ReplicaModifications   fwtypes.ListNestedObjectValueOf[dataBucketRepConfigReplicaModificationsModel]   `tfsdk:"replica_modifications"`
+	SseKmsEncryptedObjects fwtypes.ListNestedObjectValueOf[dataBucketRepConfigSseKmsEncryptedObjectsModel] `tfsdk:"sse_kms_encrypted_objects"`
 }
 
-type dsBucketRepConfigReplicaModificationsModel struct {
+type dataBucketRepConfigReplicaModificationsModel struct {
 	Status types.String `tfsdk:"status"`
 }
 
-type dsBucketRepConfigSseKmsEncryptedObjectsModel struct {
+type dataBucketRepConfigSseKmsEncryptedObjectsModel struct {
 	Status types.String `tfsdk:"status"`
 }
