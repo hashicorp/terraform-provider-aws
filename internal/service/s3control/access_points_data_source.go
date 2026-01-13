f06b9fe32a52a96482db899fa9927fbb19e8d56d
Jared Baker <jared.baker@hashicorp.com>
2026-01-13 11:22:22 -0500

    d/aws_s3control_access_points: new data source
    This data source will allow practitioners to list S3 access points. Optionally the results can be filtered by bucket, data source ID, or data source type.
    
    ```console
    % make t K=s3control T=TestAccS3ControlAccessPointsDataSource_
    make: Verifying source code with gofmt...
    ==> Checking that code complies with gofmt requirements...
    make: Running acceptance tests on branch: 🌿 f-s3control_access_points 🌿...
    TF_ACC=1 go1.25.5 test ./internal/service/s3control/... -v -count 1 -parallel 20 -run='TestAccS3ControlAccessPointsDataSource_'  -timeout 360m -vet=off
    2026/01/13 11:03:40 Creating Terraform AWS Provider (SDKv2-style)...
    2026/01/13 11:03:40 Initializing Terraform AWS Provider (SDKv2-style)...
    
    --- PASS: TestAccS3ControlAccessPointsDataSource_bucketNoAccessPoints (15.74s)
    --- PASS: TestAccS3ControlAccessPointsDataSource_bucket (16.75s)
    --- PASS: TestAccS3ControlAccessPointsDataSource_basic (16.75s)
    PASS
    ok      github.com/hashicorp/terraform-provider-aws/internal/service/s3control  23.084s
    ```

---
+++ internal/service/s3control/access_points_data_source.go
@@ -0,0 +1,130 @@
+// Copyright IBM Corp. 2014, 2026
+// SPDX-License-Identifier: MPL-2.0
+
+package s3control
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3control"
+	awstypes "github.com/aws/aws-sdk-go-v2/service/s3control/types"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-provider-aws/internal/framework"
+	"github.com/hashicorp/terraform-provider-aws/internal/framework/flex"
+	fwtypes "github.com/hashicorp/terraform-provider-aws/internal/framework/types"
+	fwvalidators "github.com/hashicorp/terraform-provider-aws/internal/framework/validators"
+	"github.com/hashicorp/terraform-provider-aws/internal/smerr"
+	"github.com/hashicorp/terraform-provider-aws/names"
+)
+
+// @FrameworkDataSource("aws_s3control_access_points", name="Access Points")
+func newDataSourceAccessPoints(context.Context) (datasource.DataSourceWithConfigure, error) {
+	return &dataSourceAccessPoints{}, nil
+}
+
+const (
+	DSNameAccessPoints = "Access Points Data Source"
+)
+
+type dataSourceAccessPoints struct {
+	framework.DataSourceWithModel[dataSourceAccessPointsModel]
+}
+
+func (d *dataSourceAccessPoints) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"access_points": framework.DataSourceComputedListOfObjectAttribute[accessPointListDataSourceModel](ctx),
+			names.AttrAccountID: schema.StringAttribute{
+				Optional: true,
+				Validators: []validator.String{
+					fwvalidators.AWSAccountID(),
+				},
+			},
+			names.AttrBucket: schema.StringAttribute{
+				Optional: true,
+			},
+			"data_source_id": schema.StringAttribute{
+				Optional: true,
+			},
+			"data_source_type": schema.StringAttribute{
+				Optional: true,
+			},
+		},
+	}
+}
+
+func (d *dataSourceAccessPoints) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	conn := d.Meta().S3ControlClient(ctx)
+
+	var data dataSourceAccessPointsModel
+	smerr.AddEnrich(ctx, &resp.Diagnostics, req.Config.Get(ctx, &data))
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	var input s3control.ListAccessPointsInput
+	smerr.AddEnrich(ctx, &resp.Diagnostics, flex.Expand(ctx, data, &input))
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	if input.AccountId == nil {
+		input.AccountId = aws.String(d.Meta().AccountID(ctx))
+	}
+
+	out, err := findAccessPoints(ctx, conn, input)
+	if err != nil {
+		smerr.AddError(ctx, &resp.Diagnostics, err)
+		return
+	}
+
+	smerr.AddEnrich(ctx, &resp.Diagnostics, flex.Flatten(ctx, out, &data.AccessPoints))
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	smerr.AddEnrich(ctx, &resp.Diagnostics, resp.State.Set(ctx, &data))
+}
+
+func findAccessPoints(ctx context.Context, conn *s3control.Client, input s3control.ListAccessPointsInput) ([]awstypes.AccessPoint, error) {
+	paginator := s3control.NewListAccessPointsPaginator(conn, &input)
+
+	var output []awstypes.AccessPoint
+	for paginator.HasMorePages() {
+		page, err := paginator.NextPage(ctx)
+		if err != nil {
+			return nil, err
+		}
+
+		output = append(output, page.AccessPointList...)
+	}
+
+	return output, nil
+}
+
+type dataSourceAccessPointsModel struct {
+	framework.WithRegionModel
+	AccessPoints   fwtypes.ListNestedObjectValueOf[accessPointListDataSourceModel] `tfsdk:"access_points"`
+	AccountID      types.String                                                    `tfsdk:"account_id"`
+	Bucket         types.String                                                    `tfsdk:"bucket"`
+	DataSourceID   types.String                                                    `tfsdk:"data_source_id"`
+	DataSourceType types.String                                                    `tfsdk:"data_source_type"`
+}
+
+// A variation on the access points data source model which contains only
+// the attributes returned by the ListAccessPoints API response
+type accessPointListDataSourceModel struct {
+	AccessPointARN   types.String                                           `tfsdk:"access_point_arn"`
+	Alias            types.String                                           `tfsdk:"alias"`
+	Bucket           types.String                                           `tfsdk:"bucket"`
+	BucketAccountID  types.String                                           `tfsdk:"bucket_account_id"`
+	DataSourceID     types.String                                           `tfsdk:"data_source_id"`
+	DataSourceType   types.String                                           `tfsdk:"data_source_type"`
+	Name             types.String                                           `tfsdk:"name"`
+	NetworkOrigin    types.String                                           `tfsdk:"network_origin"`
+	VPCConfiguration fwtypes.ListNestedObjectValueOf[vpcConfigurationModel] `tfsdk:"vpc_configuration"`
+}
---
+++ internal/service/s3control/access_points_data_source_test.go
@@ -0,0 +1,124 @@
+// Copyright IBM Corp. 2014, 2026
+// SPDX-License-Identifier: MPL-2.0
+
+package s3control_test
+
+import (
+	"fmt"
+	"testing"
+
+	sdkacctest "github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+	"github.com/hashicorp/terraform-provider-aws/names"
+)
+
+func TestAccS3ControlAccessPointsDataSource_basic(t *testing.T) {
+	ctx := acctest.Context(t)
+	bucketName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	accessPointName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	dataSourceName := "data.aws_s3control_access_points.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
+		ErrorCheck:               acctest.ErrorCheck(t, names.S3ControlServiceID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAccessPointsDataSourceConfig_basic(bucketName, accessPointName),
+				// Use traditional Check block here as the knownvalue.ListPartial check requires
+				// the index at which the partially known object is stored. When run in parallel
+				// this test configuration may return many access points in an unpredictable order.
+				Check: resource.ComposeTestCheckFunc(
+					// Check that we have at least one access point with the expected name
+					resource.TestCheckTypeSetElemNestedAttrs(dataSourceName, "access_points.*", map[string]string{
+						names.AttrBucket: bucketName,
+						names.AttrName:   accessPointName,
+					}),
+				),
+			},
+		},
+	})
+}
+
+func TestAccS3ControlAccessPointsDataSource_bucket(t *testing.T) {
+	ctx := acctest.Context(t)
+	bucketName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	accessPointName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	dataSourceName := "data.aws_s3control_access_points.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
+		ErrorCheck:               acctest.ErrorCheck(t, names.S3ControlServiceID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAccessPointsDataSourceConfig_bucket(bucketName, accessPointName),
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(dataSourceName, tfjsonpath.New("access_points"), knownvalue.ListSizeExact(1)),
+					statecheck.ExpectKnownValue(dataSourceName, tfjsonpath.New("access_points"), knownvalue.ListExact([]knownvalue.Check{
+						knownvalue.ObjectPartial(map[string]knownvalue.Check{
+							names.AttrBucket: knownvalue.StringExact(bucketName),
+							names.AttrName:   knownvalue.StringExact(accessPointName),
+						}),
+					})),
+				},
+			},
+		},
+	})
+}
+
+func TestAccS3ControlAccessPointsDataSource_bucketNoAccessPoints(t *testing.T) {
+	ctx := acctest.Context(t)
+	bucketName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	dataSourceName := "data.aws_s3control_access_points.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
+		ErrorCheck:               acctest.ErrorCheck(t, names.S3ControlServiceID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAccessPointsDataSourceConfig_bucketNoAccessPoints(bucketName),
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(dataSourceName, tfjsonpath.New("access_points"), knownvalue.Null()),
+				},
+			},
+		},
+	})
+}
+
+func testAccAccessPointsDataSourceConfig_basic(bucketName, accessPointName string) string {
+	return acctest.ConfigCompose(
+		testAccAccessPointConfig_basic(bucketName, accessPointName),
+		`
+data "aws_s3control_access_points" "test" {
+  depends_on = ["aws_s3_access_point.test"]
+}
+`)
+}
+
+func testAccAccessPointsDataSourceConfig_bucket(bucketName, accessPointName string) string {
+	return acctest.ConfigCompose(
+		testAccAccessPointConfig_basic(bucketName, accessPointName),
+		`
+data "aws_s3control_access_points" "test" {
+  bucket = aws_s3_access_point.test.bucket
+}
+`)
+}
+
+func testAccAccessPointsDataSourceConfig_bucketNoAccessPoints(bucketName string) string {
+	return fmt.Sprintf(`
+resource "aws_s3_bucket" "test" {
+  bucket = %[1]q
+}
+
+data "aws_s3control_access_points" "test" {
+  bucket = aws_s3_bucket.test.bucket
+}
+`, bucketName)
+}
