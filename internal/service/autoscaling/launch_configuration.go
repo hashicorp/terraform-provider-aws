67883d811fcd9a8f67e2edb28a1b0ecdd1fb8df3
Dirk Avery <dirk.avery@gmail.com>
2026-01-14 16:22:50 -0500

    internal/service/autoscaling: Suppress Semgrep SHA1 warning for launch configuration user_data
    AWS AutoScaling API uses SHA1 for user_data hashing. Must use SHA1 to
    match AWS behavior. This is dictated by AWS, not our choice.

--- internal/service/autoscaling/launch_configuration.go
+++ internal/service/autoscaling/launch_configuration.go
@@ -778,7 +778,7 @@ func userDataHashSum(userData string) string {
 		v = []byte(userData)
 	}
 
-	hash := sha1.Sum(v)
+	hash := sha1.Sum(v) // nosemgrep: go.lang.security.audit.crypto.use_of_weak_crypto.use-of-sha1 -- AWS AutoScaling API uses SHA1 for user_data hashing, must match AWS behavior
 	return hex.EncodeToString(hash[:])
 }
 
