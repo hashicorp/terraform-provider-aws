8e3084152fb9298eda3e24f9e3d7b446dd954050
Graham Davison <gdavison@hashicorp.com>
2026-01-16 15:49:42 -0800

    `globalaccelerator`

--- internal/service/globalaccelerator/accelerator.go
+++ internal/service/globalaccelerator/accelerator.go
@@ -33,6 +33,7 @@ import (
 // @Tags(identifierAttribute="id")
 // @ArnIdentity
 // @Testing(preIdentityVersion="v6.3.0")
+// @Testing(existsTakesT=false, destroyTakesT=false)
 func resourceAccelerator() *schema.Resource {
 	return &schema.Resource{
 		CreateWithoutTimeout: resourceAcceleratorCreate,
