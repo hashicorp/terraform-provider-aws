1235c57f99f1f3a3943fad8aac8eb1d69aa75b55
Jared Baker <jared.baker@hashicorp.com>
2026-01-21 11:10:52 -0500

    Enable go-vcr for pipes service

--- internal/service/pipes/pipe.go
+++ internal/service/pipes/pipe.go
@@ -17,7 +17,6 @@ import (
 	awstypes "github.com/aws/aws-sdk-go-v2/service/pipes/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/id"
-	sdkretry "github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
@@ -26,6 +25,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
 	"github.com/hashicorp/terraform-provider-aws/internal/retry"
+	sdkretry "github.com/hashicorp/terraform-provider-aws/internal/retry"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	inttypes "github.com/hashicorp/terraform-provider-aws/internal/types"
@@ -349,9 +349,8 @@ func findPipeByName(ctx context.Context, conn *pipes.Client, name string) (*pipe
 	output, err := conn.DescribePipe(ctx, input)
 
 	if errs.IsA[*awstypes.NotFoundException](err) {
-		return nil, &sdkretry.NotFoundError{
-			LastError:   err,
-			LastRequest: input,
+		return nil, &retry.NotFoundError{
+			LastError: err,
 		}
 	}
 
@@ -366,8 +365,8 @@ func findPipeByName(ctx context.Context, conn *pipes.Client, name string) (*pipe
 	return output, nil
 }
 
-func statusPipe(ctx context.Context, conn *pipes.Client, name string) sdkretry.StateRefreshFunc {
-	return func() (any, string, error) {
+func statusPipe(conn *pipes.Client, name string) retry.StateRefreshFunc {
+	return func(ctx context.Context) (any, string, error) {
 		output, err := findPipeByName(ctx, conn, name)
 
 		if retry.NotFound(err) {
@@ -386,7 +385,7 @@ func waitPipeCreated(ctx context.Context, conn *pipes.Client, id string, timeout
 	stateConf := &sdkretry.StateChangeConf{
 		Pending:                   enum.Slice(awstypes.PipeStateCreating),
 		Target:                    enum.Slice(awstypes.PipeStateRunning, awstypes.PipeStateStopped),
-		Refresh:                   statusPipe(ctx, conn, id),
+		Refresh:                   statusPipe(conn, id),
 		Timeout:                   timeout,
 		NotFoundChecks:            20,
 		ContinuousTargetOccurence: 1,
@@ -406,7 +405,7 @@ func waitPipeUpdated(ctx context.Context, conn *pipes.Client, id string, timeout
 	stateConf := &sdkretry.StateChangeConf{
 		Pending:                   enum.Slice(awstypes.PipeStateUpdating),
 		Target:                    enum.Slice(awstypes.PipeStateRunning, awstypes.PipeStateStopped),
-		Refresh:                   statusPipe(ctx, conn, id),
+		Refresh:                   statusPipe(conn, id),
 		Timeout:                   timeout,
 		NotFoundChecks:            20,
 		ContinuousTargetOccurence: 1,
@@ -426,7 +425,7 @@ func waitPipeDeleted(ctx context.Context, conn *pipes.Client, id string, timeout
 	stateConf := &sdkretry.StateChangeConf{
 		Pending: enum.Slice(awstypes.PipeStateDeleting),
 		Target:  []string{},
-		Refresh: statusPipe(ctx, conn, id),
+		Refresh: statusPipe(conn, id),
 		Timeout: timeout,
 	}
 
