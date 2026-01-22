d192065ce17febf0e06c3787acc4d2a090868930
Graham Davison <gdavison@hashicorp.com>
2026-01-22 12:30:19 -0800

    Adds comments for resource types with annotation `@Testing(identityTest=false)`

--- internal/service/organizations/delegated_administrator.go
+++ internal/service/organizations/delegated_administrator.go
@@ -31,6 +31,7 @@ import (
 // @IdentityAttribute("delegated_account_id", resourceAttributeName="account_id")
 // @IdAttrFormat("{account_id}/{service_principal}")
 // @ImportIDHandler("delegatedAdministratorImportID")
+// Alternate account not working
 // @Testing(identityTest=false)
 // @Testing(existsType="github.com/aws/aws-sdk-go-v2/service/organizations/types;awstypes;awstypes.DelegatedAdministrator")
 // @Testing(serialize=true)
