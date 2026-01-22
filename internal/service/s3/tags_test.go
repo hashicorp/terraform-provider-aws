d3951fcc0a9726694f6740fae3ef03186dd20228
wellsiau-aws <wellsiau@amazon.com>
2026-01-22 15:03:41 -0800

    fix lint & changelog

--- internal/service/s3/tags_test.go
+++ internal/service/s3/tags_test.go
@@ -14,13 +14,13 @@ import (
 // TestBucketListTags_MethodNotAllowed tests that bucketListTags handles MethodNotAllowed error correctly
 func TestBucketListTags_MethodNotAllowed(t *testing.T) {
 	t.Parallel()
-	
+
 	// Test that MethodNotAllowed error is correctly identified by tfawserr.ErrCodeEquals
 	err := &smithy.GenericAPIError{
 		Code:    errCodeMethodNotAllowed,
 		Message: "The specified method is not allowed against this resource.",
 	}
-	
+
 	// Verify that tfawserr.ErrCodeEquals correctly identifies MethodNotAllowed
 	if !tfawserr.ErrCodeEquals(err, errCodeMethodNotAllowed) {
 		t.Errorf("Expected tfawserr.ErrCodeEquals to identify MethodNotAllowed error")
@@ -30,13 +30,13 @@ func TestBucketListTags_MethodNotAllowed(t *testing.T) {
 // TestUpdateTags_MethodNotAllowedErrorCode tests that MethodNotAllowed error is correctly identified
 func TestUpdateTags_MethodNotAllowedErrorCode(t *testing.T) {
 	t.Parallel()
-	
+
 	// Test that MethodNotAllowed error is correctly identified by tfawserr.ErrCodeEquals
 	err := &smithy.GenericAPIError{
 		Code:    errCodeMethodNotAllowed,
 		Message: "The specified method is not allowed against this resource.",
 	}
-	
+
 	if !tfawserr.ErrCodeEquals(err, errCodeMethodNotAllowed) {
 		t.Errorf("Expected tfawserr.ErrCodeEquals to identify MethodNotAllowed error")
 	}
@@ -45,13 +45,13 @@ func TestUpdateTags_MethodNotAllowedErrorCode(t *testing.T) {
 // TestCreateBucket_UnsupportedArgumentErrorCode tests that UnsupportedArgument error is correctly identified
 func TestCreateBucket_UnsupportedArgumentErrorCode(t *testing.T) {
 	t.Parallel()
-	
+
 	// Verify that UnsupportedArgument error is correctly identified
 	unsupportedArgErr := &smithy.GenericAPIError{
 		Code:    errCodeUnsupportedArgument,
 		Message: "The CreateBucket operation does not support the tags argument.",
 	}
-	
+
 	if !tfawserr.ErrCodeEquals(unsupportedArgErr, errCodeUnsupportedArgument) {
 		t.Errorf("Expected tfawserr.ErrCodeEquals to identify UnsupportedArgument error")
 	}
@@ -60,10 +60,10 @@ func TestCreateBucket_UnsupportedArgumentErrorCode(t *testing.T) {
 // TestCreateBucket_AuthorizationError tests authorization error detection
 func TestCreateBucket_AuthorizationError(t *testing.T) {
 	t.Parallel()
-	
+
 	// Test authorization error message detection
 	authErr := fmt.Errorf("AccessDenied: User is not authorized to perform: s3:TagResource")
-	
+
 	// This should contain the authorization error message
 	if authErr.Error() != "AccessDenied: User is not authorized to perform: s3:TagResource" {
 		t.Errorf("Expected authorization error message, got: %v", authErr)
