904ba4cfb42ed27e36f327b76c2d9feae9df3bdc
Graham Davison <gdavison@hashicorp.com>
2026-01-27 18:06:26 -0800

    `acmpca`

--- internal/service/acmpca/certificate_authority.go
+++ internal/service/acmpca/certificate_authority.go
@@ -56,9 +56,7 @@ func resourceCertificateAuthority() *schema.Resource {
 
 		Importer: &schema.ResourceImporter{
 			StateContext: func(ctx context.Context, d *schema.ResourceData, meta any) ([]*schema.ResourceData, error) {
-				identitySpec := importer.IdentitySpec(ctx)
-
-				if err := importer.RegionalARN(ctx, d, identitySpec); err != nil {
+				if err := importer.Import(ctx, d, meta); err != nil {
 					return nil, err
 				}
 
