44e8d90018be256345d2d19ef9e32dd44e278df0
Tarun teja <tarunteja@Taruns-MacBook-Pro.local>
2026-01-29 21:50:58 +0530

    fix copyright header

--- internal/service/account/regions_data_source_test.go
+++ internal/service/account/regions_data_source_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) HashiCorp, Inc.
+// Copyright IBM Corp. 2014, 2026
 // SPDX-License-Identifier: MPL-2.0
 
 // DONOTCOPY: Copying old resources spreads bad habits. Use skaff instead.
