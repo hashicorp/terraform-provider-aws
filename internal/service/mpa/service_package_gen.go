fd86875c16a02d2825db8f28aaf5574f1d662ce8
Jack Roberts <jack.roberts@krakenflex.com>
2026-01-30 20:49:56 +0000

    r/aws-mpa-identity-source: gen

--- internal/service/mpa/service_package_gen.go
+++ internal/service/mpa/service_package_gen.go
@@ -36,6 +36,15 @@ func (p *servicePackage) FrameworkResources(ctx context.Context) []*inttypes.Ser
 			}),
 			Region: unique.Make(inttypes.ResourceRegionDefault()),
 		},
+		{
+			Factory:  newIdentitySourceResource,
+			TypeName: "aws_mpa_identity_source",
+			Name:     "Identity Source",
+			Tags: unique.Make(inttypes.ServicePackageResourceTags{
+				IdentifierAttribute: names.AttrARN,
+			}),
+			Region: unique.Make(inttypes.ResourceRegionDefault()),
+		},
 	}
 }
 
---
+++ internal/service/mpa/testdata/IdentitySource/tags_ignore/main_gen.tf
@@ -0,0 +1,51 @@
+# Copyright IBM Corp. 2014, 2026
+# SPDX-License-Identifier: MPL-2.0
+
+provider "aws" {
+  default_tags {
+    tags = var.provider_tags
+  }
+  ignore_tags {
+    keys = var.ignore_tag_keys
+  }
+}
+
+data "aws_ssoadmin_instances" "test" {}
+data "aws_region" "current" {}
+
+resource "aws_mpa_identity_source" "test" {
+  name = var.rName
+
+  identity_source_parameters {
+    iam_identity_center {
+      instance_arn = tolist(data.aws_ssoadmin_instances.test.arns)[0]
+      region       = data.aws_region.current.name
+    }
+  }
+
+  tags = var.resource_tags
+}
+
+variable "rName" {
+  description = "Name for resource"
+  type        = string
+  nullable    = false
+}
+
+variable "resource_tags" {
+  description = "Tags to set on resource. To specify no tags, set to `null`"
+  # Not setting a default, so that this must explicitly be set to `null` to specify no tags
+  type     = map(string)
+  nullable = true
+}
+
+variable "provider_tags" {
+  type     = map(string)
+  nullable = true
+  default  = null
+}
+
+variable "ignore_tag_keys" {
+  type     = set(string)
+  nullable = false
+}
