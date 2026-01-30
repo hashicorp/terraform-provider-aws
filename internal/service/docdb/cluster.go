97048aaed72ba312cb30782334a7c9860e66d8d4
Dirk Avery <dirk.avery@gmail.com>
2026-01-30 13:31:16 -0500

    Fix lint issue

--- internal/service/docdb/cluster.go
+++ internal/service/docdb/cluster.go
@@ -19,7 +19,6 @@ import (
 	"github.com/hashicorp/aws-sdk-go-base/v2/tfawserr"
 	"github.com/hashicorp/go-cty/cty"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/customdiff"
 	sdkretry "github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
