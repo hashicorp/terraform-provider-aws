70cba547affc0070c09497e446fbbcbd7874710d
Paul Santus <paul.santus@gmail.com>
2026-02-02 22:56:12 +0100

    Migrate aws_dms_data_provider from SDKv2 to Plugin Framework

--- internal/service/dms/service_package_gen.go
+++ internal/service/dms/service_package_gen.go
@@ -25,7 +25,17 @@ func (p *servicePackage) FrameworkDataSources(ctx context.Context) []*inttypes.S
 }
 
 func (p *servicePackage) FrameworkResources(ctx context.Context) []*inttypes.ServicePackageFrameworkResource {
-	return []*inttypes.ServicePackageFrameworkResource{}
+	return []*inttypes.ServicePackageFrameworkResource{
+		{
+			Factory:  resourceDataProvider,
+			TypeName: "aws_dms_data_provider",
+			Name:     "Data Provider",
+			Tags: unique.Make(inttypes.ServicePackageResourceTags{
+				IdentifierAttribute: "data_provider_arn",
+			}),
+			Region: unique.Make(inttypes.ResourceRegionDefault()),
+		},
+	}
 }
 
 func (p *servicePackage) SDKDataSources(ctx context.Context) []*inttypes.ServicePackageSDKDataSource {
