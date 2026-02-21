86a8c280dc39446947b21d897fa8b1736e0cd965
Graham Davison <gdavison@hashicorp.com>
2026-02-20 17:24:38 -0800

    Adds golden logs for `TestExpandInt32toInt64`

--- internal/framework/flex/autoflex_nums_test.go
+++ internal/framework/flex/autoflex_nums_test.go
@@ -680,7 +680,7 @@ func TestExpandInt32toInt64(t *testing.T) {
 		t.Run(testName, func(t *testing.T) {
 			t.Parallel()
 
-			runAutoExpandTestCases(t, cases, runChecks{CompareDiags: true, CompareTarget: true, SkipGoldenLogs: true})
+			runAutoExpandTestCases(t, cases, runChecks{CompareDiags: true, CompareTarget: true})
 		})
 	}
 }
