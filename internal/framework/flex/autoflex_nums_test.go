8f891326354beb1998f07d52e24c9e1e9f5b6fd4
Graham Davison <gdavison@hashicorp.com>
2026-02-20 18:12:17 -0800

    Adds golden logs for `TestFlattenFloat64`

--- internal/framework/flex/autoflex_nums_test.go
+++ internal/framework/flex/autoflex_nums_test.go
@@ -894,7 +894,7 @@ func TestFlattenFloat64(t *testing.T) {
 		t.Run(testName, func(t *testing.T) {
 			t.Parallel()
 
-			runAutoFlattenTestCases(t, cases, runChecks{CompareDiags: true, CompareTarget: true, SkipGoldenLogs: true})
+			runAutoFlattenTestCases(t, cases, runChecks{CompareDiags: true, CompareTarget: true})
 		})
 	}
 }
