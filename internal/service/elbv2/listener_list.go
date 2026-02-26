2ec82352eb9739116230c01b2927d33c457ba5fe
Graham Davison <gdavison@hashicorp.com>
2026-02-26 13:25:42 -0800

    New List Resource: `aws_lb_listener`

---
+++ internal/service/elbv2/listener_list.go
@@ -0,0 +1,131 @@
+// Copyright IBM Corp. 2014, 2026
+// SPDX-License-Identifier: MPL-2.0
+
+package elbv2
+
+import (
+	"context"
+	"fmt"
+	"iter"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/elasticloadbalancingv2"
+	awstypes "github.com/aws/aws-sdk-go-v2/service/elasticloadbalancingv2/types"
+	"github.com/hashicorp/terraform-plugin-framework/list"
+	listschema "github.com/hashicorp/terraform-plugin-framework/list/schema"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"github.com/hashicorp/terraform-provider-aws/internal/errs/fwdiag"
+	"github.com/hashicorp/terraform-provider-aws/internal/framework"
+	fwtypes "github.com/hashicorp/terraform-provider-aws/internal/framework/types"
+	"github.com/hashicorp/terraform-provider-aws/internal/logging"
+	inttypes "github.com/hashicorp/terraform-provider-aws/internal/types"
+	"github.com/hashicorp/terraform-provider-aws/names"
+)
+
+// @SDKListResource("aws_lb_listener")
+func newListenerResourceAsListResource() inttypes.ListResourceForSDK {
+	l := listResourceListener{}
+	l.SetResourceSchema(resourceListener())
+	return &l
+}
+
+var _ list.ListResource = &listResourceListener{}
+
+type listResourceListener struct {
+	framework.ListResourceWithSDKv2Resource
+}
+
+func (_ *listResourceListener) ListResourceConfigSchema(ctx context.Context, request list.ListResourceSchemaRequest, response *list.ListResourceSchemaResponse) {
+	response.Schema = listschema.Schema{
+		Attributes: map[string]listschema.Attribute{
+			"load_balancer_arn": listschema.StringAttribute{
+				CustomType:  fwtypes.ARNType,
+				Required:    true,
+				Description: "ARN of the Load Balancer to list Listeners from.",
+			},
+		},
+	}
+}
+
+func (l *listResourceListener) List(ctx context.Context, request list.ListRequest, stream *list.ListResultsStream) {
+	conn := l.Meta().ELBV2Client(ctx)
+
+	var query listListenerModel
+	if request.Config.Raw.IsKnown() && !request.Config.Raw.IsNull() {
+		if diags := request.Config.Get(ctx, &query); diags.HasError() {
+			stream.Results = list.ListResultsStreamDiagnostics(diags)
+			return
+		}
+	}
+
+	loadBalancerARN := query.LoadBalancerARN.ValueString()
+
+	tflog.Info(ctx, "Listing ELB Listener", map[string]any{
+		logging.ResourceAttributeKey("load_balancer_arn"): loadBalancerARN,
+	})
+	stream.Results = func(yield func(list.ListResult) bool) {
+		input := elasticloadbalancingv2.DescribeListenersInput{
+			LoadBalancerArn: aws.String(loadBalancerARN),
+		}
+		for item, err := range listListeners(ctx, conn, &input) {
+			if err != nil {
+				result := fwdiag.NewListResultErrorDiagnostic(err)
+				yield(result)
+				return
+			}
+
+			arn := aws.ToString(item.ListenerArn)
+			ctx := tflog.SetField(ctx, logging.ResourceAttributeKey(names.AttrARN), arn)
+
+			result := request.NewListResult(ctx)
+			rd := l.ResourceData()
+			rd.SetId(arn)
+			rd.Set(names.AttrARN, arn)
+
+			if request.IncludeResource {
+				if err := resourceListenerFlatten(ctx, l.Meta(), &item, rd); err != nil {
+					tflog.Error(ctx, "Reading ELB Listener", map[string]any{
+						"error": err.Error(),
+					})
+					continue
+				}
+			}
+
+			result.DisplayName = arn
+
+			l.SetResult(ctx, l.Meta(), request.IncludeResource, &result, rd)
+			if result.Diagnostics.HasError() {
+				yield(result)
+				return
+			}
+
+			if !yield(result) {
+				return
+			}
+		}
+	}
+}
+
+type listListenerModel struct {
+	framework.WithRegionModel
+	LoadBalancerARN fwtypes.ARN `tfsdk:"load_balancer_arn"`
+}
+
+func listListeners(ctx context.Context, conn *elasticloadbalancingv2.Client, input *elasticloadbalancingv2.DescribeListenersInput) iter.Seq2[awstypes.Listener, error] {
+	return func(yield func(awstypes.Listener, error) bool) {
+		pages := elasticloadbalancingv2.NewDescribeListenersPaginator(conn, input)
+		for pages.HasMorePages() {
+			page, err := pages.NextPage(ctx)
+			if err != nil {
+				yield(awstypes.Listener{}, fmt.Errorf("listing ELB Listener resources: %w", err))
+				return
+			}
+
+			for _, item := range page.Listeners {
+				if !yield(item, nil) {
+					return
+				}
+			}
+		}
+	}
+}
---
+++ internal/service/elbv2/listener_list_test.go
@@ -0,0 +1,245 @@
+// Copyright IBM Corp. 2014, 2026
+// SPDX-License-Identifier: MPL-2.0
+
+package elbv2_test
+
+import (
+	"testing"
+
+	"github.com/YakDriver/regexache"
+	"github.com/hashicorp/terraform-plugin-testing/config"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/querycheck"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+	"github.com/hashicorp/terraform-plugin-testing/tfversion"
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+	tfknownvalue "github.com/hashicorp/terraform-provider-aws/internal/acctest/knownvalue"
+	tfquerycheck "github.com/hashicorp/terraform-provider-aws/internal/acctest/querycheck"
+	tfqueryfilter "github.com/hashicorp/terraform-provider-aws/internal/acctest/queryfilter"
+	tfstatecheck "github.com/hashicorp/terraform-provider-aws/internal/acctest/statecheck"
+	"github.com/hashicorp/terraform-provider-aws/names"
+)
+
+func TestAccELBV2Listener_List_basic(t *testing.T) {
+	ctx := acctest.Context(t)
+
+	resourceName1 := "aws_lb_listener.test[0]"
+	resourceName2 := "aws_lb_listener.test[1]"
+	rName := acctest.RandomWithPrefix(t, acctest.ResourcePrefix)
+
+	identity1 := tfstatecheck.Identity()
+	identity2 := tfstatecheck.Identity()
+
+	acctest.ParallelTest(ctx, t, resource.TestCase{
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.SkipBelow(tfversion.Version1_14_0),
+		},
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+		},
+		ErrorCheck:   acctest.ErrorCheck(t, names.ELBV2ServiceID),
+		CheckDestroy: testAccCheckListenerDestroy(ctx, t),
+		Steps: []resource.TestStep{
+			// Step 1: Setup
+			{
+				ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+				ConfigDirectory:          config.StaticDirectory("testdata/Listener/list_basic/"),
+				ConfigVariables: config.Variables{
+					acctest.CtRName:  config.StringVariable(rName),
+					"resource_count": config.IntegerVariable(2),
+				},
+				ConfigStateChecks: []statecheck.StateCheck{
+					identity1.GetIdentity(resourceName1),
+					statecheck.ExpectKnownValue(resourceName1, tfjsonpath.New(names.AttrARN), knownValueApplicationListenerARN(rName)),
+
+					identity2.GetIdentity(resourceName2),
+					statecheck.ExpectKnownValue(resourceName2, tfjsonpath.New(names.AttrARN), knownValueApplicationListenerARN(rName)),
+				},
+			},
+
+			// Step 2: Query
+			{
+				Query:                    true,
+				ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+				ConfigDirectory:          config.StaticDirectory("testdata/Listener/list_basic/"),
+				ConfigVariables: config.Variables{
+					acctest.CtRName:  config.StringVariable(rName),
+					"resource_count": config.IntegerVariable(2),
+				},
+				QueryResultChecks: []querycheck.QueryResultCheck{
+					tfquerycheck.ExpectIdentityFunc("aws_lb_listener.test", identity1.Checks()),
+					querycheck.ExpectResourceDisplayName("aws_lb_listener.test", tfqueryfilter.ByResourceIdentityFunc(identity1.Checks()), knownValueApplicationListenerARN(rName)),
+					tfquerycheck.ExpectNoResourceObject("aws_lb_listener.test", tfqueryfilter.ByResourceIdentityFunc(identity1.Checks())),
+
+					tfquerycheck.ExpectIdentityFunc("aws_lb_listener.test", identity2.Checks()),
+					querycheck.ExpectResourceDisplayName("aws_lb_listener.test", tfqueryfilter.ByResourceIdentityFunc(identity2.Checks()), knownValueApplicationListenerARN(rName)),
+					tfquerycheck.ExpectNoResourceObject("aws_lb_listener.test", tfqueryfilter.ByResourceIdentityFunc(identity2.Checks())),
+				},
+			},
+		},
+	})
+}
+
+func TestAccELBV2Listener_List_includeResource(t *testing.T) {
+	ctx := acctest.Context(t)
+
+	resourceName1 := "aws_lb_listener.test[0]"
+	rName := acctest.RandomWithPrefix(t, acctest.ResourcePrefix)
+
+	identity1 := tfstatecheck.Identity()
+
+	acctest.ParallelTest(ctx, t, resource.TestCase{
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.SkipBelow(tfversion.Version1_14_0),
+		},
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+		},
+		ErrorCheck:   acctest.ErrorCheck(t, names.ELBV2ServiceID),
+		CheckDestroy: testAccCheckListenerDestroy(ctx, t),
+		Steps: []resource.TestStep{
+			// Step 1: Setup
+			{
+				ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+				ConfigDirectory:          config.StaticDirectory("testdata/Listener/list_include_resource/"),
+				ConfigVariables: config.Variables{
+					acctest.CtRName:  config.StringVariable(rName),
+					"resource_count": config.IntegerVariable(1),
+					acctest.CtResourceTags: config.MapVariable(map[string]config.Variable{
+						acctest.CtKey1: config.StringVariable(acctest.CtValue1),
+					}),
+				},
+				ConfigStateChecks: []statecheck.StateCheck{
+					identity1.GetIdentity(resourceName1),
+					statecheck.ExpectKnownValue(resourceName1, tfjsonpath.New(names.AttrARN), knownValueApplicationListenerARN(rName)),
+				},
+			},
+
+			// Step 2: Query
+			{
+				Query:                    true,
+				ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+				ConfigDirectory:          config.StaticDirectory("testdata/Listener/list_include_resource/"),
+				ConfigVariables: config.Variables{
+					acctest.CtRName:  config.StringVariable(rName),
+					"resource_count": config.IntegerVariable(1),
+					acctest.CtResourceTags: config.MapVariable(map[string]config.Variable{
+						acctest.CtKey1: config.StringVariable(acctest.CtValue1),
+					}),
+				},
+				QueryResultChecks: []querycheck.QueryResultCheck{
+					tfquerycheck.ExpectIdentityFunc("aws_lb_listener.test", identity1.Checks()),
+					querycheck.ExpectResourceDisplayName("aws_lb_listener.test", tfqueryfilter.ByResourceIdentityFunc(identity1.Checks()), knownValueApplicationListenerARN(rName)),
+					querycheck.ExpectResourceKnownValues("aws_lb_listener.test", tfqueryfilter.ByResourceIdentityFunc(identity1.Checks()), []querycheck.KnownValueCheck{
+						tfquerycheck.KnownValueCheck(tfjsonpath.New("alpn_policy"), knownvalue.Null()),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New(names.AttrARN), knownValueApplicationListenerARN(rName)),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New(names.AttrCertificateARN), knownvalue.Null()),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New(names.AttrDefaultAction), knownvalue.ListSizeExact(1)),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New(names.AttrID), knownValueApplicationListenerARN(rName)),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New("load_balancer_arn"), knownvalue.NotNull()),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New("mutual_authentication"), knownvalue.ListSizeExact(0)),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New(names.AttrPort), knownvalue.NotNull()),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New(names.AttrProtocol), knownvalue.NotNull()),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New(names.AttrRegion), knownvalue.StringExact(acctest.Region())),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New("routing_http_request_x_amzn_mtls_clientcert_header_name"), knownvalue.Null()),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New("routing_http_request_x_amzn_mtls_clientcert_issuer_header_name"), knownvalue.Null()),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New("routing_http_request_x_amzn_mtls_clientcert_leaf_header_name"), knownvalue.Null()),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New("routing_http_request_x_amzn_mtls_clientcert_serial_number_header_name"), knownvalue.Null()),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New("routing_http_request_x_amzn_mtls_clientcert_subject_header_name"), knownvalue.Null()),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New("routing_http_request_x_amzn_mtls_clientcert_validity_header_name"), knownvalue.Null()),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New("routing_http_request_x_amzn_tls_cipher_suite_header_name"), knownvalue.Null()),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New("routing_http_request_x_amzn_tls_version_header_name"), knownvalue.Null()),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New("routing_http_response_access_control_allow_credentials_header_value"), knownvalue.StringExact("")),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New("routing_http_response_access_control_allow_headers_header_value"), knownvalue.StringExact("")),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New("routing_http_response_access_control_allow_methods_header_value"), knownvalue.StringExact("")),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New("routing_http_response_access_control_allow_origin_header_value"), knownvalue.StringExact("")),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New("routing_http_response_access_control_expose_headers_header_value"), knownvalue.StringExact("")),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New("routing_http_response_access_control_max_age_header_value"), knownvalue.StringExact("")),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New("routing_http_response_content_security_policy_header_value"), knownvalue.StringExact("")),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New("routing_http_response_server_enabled"), knownvalue.StringExact("")),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New("routing_http_response_strict_transport_security_header_value"), knownvalue.StringExact("")),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New("routing_http_response_x_content_type_options_header_value"), knownvalue.StringExact("")),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New("routing_http_response_x_frame_options_header_value"), knownvalue.StringExact("")),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New("ssl_policy"), knownvalue.NotNull()),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New(names.AttrTags), knownvalue.MapExact(map[string]knownvalue.Check{
+							acctest.CtKey1: knownvalue.StringExact(acctest.CtValue1),
+						})),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New(names.AttrTagsAll), knownvalue.MapExact(map[string]knownvalue.Check{
+							acctest.CtKey1: knownvalue.StringExact(acctest.CtValue1),
+						})),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New("tcp_idle_timeout_seconds"), knownvalue.Null()),
+					}),
+				},
+			},
+		},
+	})
+}
+
+func TestAccELBV2Listener_List_regionOverride(t *testing.T) {
+	ctx := acctest.Context(t)
+
+	resourceName1 := "aws_lb_listener.test[0]"
+	resourceName2 := "aws_lb_listener.test[1]"
+	rName := acctest.RandomWithPrefix(t, acctest.ResourcePrefix)
+
+	identity1 := tfstatecheck.Identity()
+	identity2 := tfstatecheck.Identity()
+
+	acctest.ParallelTest(ctx, t, resource.TestCase{
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.SkipBelow(tfversion.Version1_14_0),
+		},
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+			acctest.PreCheckMultipleRegion(t, 2)
+		},
+		ErrorCheck:   acctest.ErrorCheck(t, names.ELBV2ServiceID),
+		CheckDestroy: testAccCheckListenerDestroy(ctx, t),
+		Steps: []resource.TestStep{
+			// Step 1: Setup
+			{
+				ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+				ConfigDirectory:          config.StaticDirectory("testdata/Listener/list_region_override/"),
+				ConfigVariables: config.Variables{
+					acctest.CtRName:  config.StringVariable(rName),
+					"resource_count": config.IntegerVariable(2),
+					"region":         config.StringVariable(acctest.AlternateRegion()),
+				},
+				ConfigStateChecks: []statecheck.StateCheck{
+					identity1.GetIdentity(resourceName1),
+					statecheck.ExpectKnownValue(resourceName1, tfjsonpath.New(names.AttrARN), knownValueApplicationListenerAlternateRegionARN(rName)),
+
+					identity2.GetIdentity(resourceName2),
+					statecheck.ExpectKnownValue(resourceName2, tfjsonpath.New(names.AttrARN), knownValueApplicationListenerAlternateRegionARN(rName)),
+				},
+			},
+
+			// Step 2: Query
+			{
+				Query:                    true,
+				ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+				ConfigDirectory:          config.StaticDirectory("testdata/Listener/list_region_override/"),
+				ConfigVariables: config.Variables{
+					acctest.CtRName:  config.StringVariable(rName),
+					"resource_count": config.IntegerVariable(2),
+					"region":         config.StringVariable(acctest.AlternateRegion()),
+				},
+				QueryResultChecks: []querycheck.QueryResultCheck{
+					tfquerycheck.ExpectIdentityFunc("aws_lb_listener.test", identity1.Checks()),
+
+					tfquerycheck.ExpectIdentityFunc("aws_lb_listener.test", identity2.Checks()),
+				},
+			},
+		},
+	})
+}
+
+func knownValueApplicationListenerARN(lbName string) knownvalue.Check {
+	return tfknownvalue.RegionalARNRegexp("elasticloadbalancing", regexache.MustCompile(applicationListenerARNPattern(lbName)))
+}
+
+func knownValueApplicationListenerAlternateRegionARN(lbName string) knownvalue.Check {
+	return tfknownvalue.RegionalARNAlternateRegionRegexp("elasticloadbalancing", regexache.MustCompile(applicationListenerARNPattern(lbName)))
+}
