910ea1b0fa3c6e31c5cfa12eeed4d774b08fe2ff
Dirk Avery <dirk.avery@gmail.com>
2026-03-02 16:56:00 -0500

    Update tests

--- internal/service/wafv2/web_acl_rule_test.go
+++ internal/service/wafv2/web_acl_rule_test.go
@@ -486,8 +486,8 @@ resource "aws_wafv2_web_acl_rule" "test" {
 
   statement {
     rate_based_statement {
-      limit               = 2000
-      aggregate_key_type  = "IP"
+      limit              = 2000
+      aggregate_key_type = "IP"
     }
   }
 
