332816f2bea674483ffa05e750eb0c816b0ec5c3
Dirk Avery <dirk.avery@gmail.com>
2026-03-02 13:38:19 -0500

    Add generated identity tests

---
+++ internal/service/wafv2/testdata/WebACLRule/region_override/main_gen.tf
@@ -0,0 +1,59 @@
+# Copyright IBM Corp. 2014, 2026
+# SPDX-License-Identifier: MPL-2.0
+
+resource "aws_wafv2_web_acl" "test" {
+  region = var.region
+
+  name  = var.rName
+  scope = "REGIONAL"
+
+  default_action {
+    allow {}
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = false
+    metric_name                = var.rName
+    sampled_requests_enabled   = false
+  }
+
+  lifecycle {
+    ignore_changes = [rule]
+  }
+}
+
+resource "aws_wafv2_web_acl_rule" "test" {
+  region = var.region
+
+  name        = var.rName
+  priority    = 1
+  web_acl_arn = aws_wafv2_web_acl.test.arn
+
+  action {
+    block {}
+  }
+
+  statement {
+    geo_match_statement {
+      country_codes = ["US", "CA"]
+    }
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = false
+    metric_name                = var.rName
+    sampled_requests_enabled   = false
+  }
+}
+
+variable "rName" {
+  description = "Name for resource"
+  type        = string
+  nullable    = false
+}
+
+variable "region" {
+  description = "Region to deploy resource in"
+  type        = string
+  nullable    = false
+}
