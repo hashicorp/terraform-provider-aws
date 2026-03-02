071f559eee0f7acba56f69c3564b53c55abc69c1
Kit Ewbank <Kit_Ewbank@hotmail.com>
2026-03-02 17:06:50 -0500

    Add filter to 'finderOptions'.

--- internal/slices/options.go
+++ internal/slices/options.go
@@ -3,29 +3,42 @@
 
 package slices
 
-type finderOptions struct {
+type finderOptions[T any] struct {
+	filter           Predicate[T]
 	returnFirstMatch bool
 }
 
-func NewFinderOptions(optFns []FinderOptionsFunc) *finderOptions {
-	opts := &finderOptions{}
+func NewFinderOptions[T any](optFns ...FinderOptionsFunc[T]) finderOptions[T] {
+	var opts finderOptions[T]
 	for _, fn := range optFns {
-		fn(opts)
+		fn(&opts)
 	}
 	return opts
 }
 
-func (o *finderOptions) ReturnFirstMatch() bool {
+func (o *finderOptions[T]) ReturnFirstMatch() bool {
 	return o.returnFirstMatch
 }
 
-type FinderOptionsFunc func(*finderOptions)
+func (o *finderOptions[T]) Filter() Predicate[T] {
+	return o.filter
+}
+
+type FinderOptionsFunc[T any] func(*finderOptions[T])
 
 // WithReturnFirstMatch is a finder option to enable paginated operations to short
 // circuit after the first filter match
 //
 // This option should only be used when only a single match will ever be returned
 // from the specified filter.
-var WithReturnFirstMatch = func(o *finderOptions) {
-	o.returnFirstMatch = true
+func WithReturnFirstMatch[T any]() FinderOptionsFunc[T] {
+	return func(o *finderOptions[T]) {
+		o.returnFirstMatch = true
+	}
+}
+
+func WithFilter[T any](filter Predicate[T]) FinderOptionsFunc[T] {
+	return func(o *finderOptions[T]) {
+		o.filter = filter
+	}
 }
