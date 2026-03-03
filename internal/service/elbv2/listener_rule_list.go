1b2688e00a6feb62872cdedc345206ef18adbd03
Graham Davison <gdavison@hashicorp.com>
2026-03-02 17:10:20 -0800

    Moves `batchListTags` to conventional `tags.go`

--- internal/service/elbv2/listener_rule_list.go
+++ internal/service/elbv2/listener_rule_list.go
@@ -8,7 +8,6 @@ import (
 	"fmt"
 	"iter"
 
-	"github.com/YakDriver/smarterr"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/elasticloadbalancingv2"
 	awstypes "github.com/aws/aws-sdk-go-v2/service/elasticloadbalancingv2/types"
@@ -167,23 +166,4 @@ func listListenerRules(ctx context.Context, conn *elasticloadbalancingv2.Client,
 			}
 		}
 	}
-}
-
-func batchListTags(ctx context.Context, conn *elasticloadbalancingv2.Client, identifiers []string, optFns ...func(*elasticloadbalancingv2.Options)) (map[string][]awstypes.Tag, error) {
-	input := elasticloadbalancingv2.DescribeTagsInput{
-		ResourceArns: identifiers,
-	}
-
-	output, err := conn.DescribeTags(ctx, &input, optFns...)
-
-	if err != nil {
-		return make(map[string][]awstypes.Tag, 0), smarterr.NewError(err)
-	}
-
-	tags := make(map[string][]awstypes.Tag, len(output.TagDescriptions))
-	for _, v := range output.TagDescriptions {
-		tags[aws.ToString(v.ResourceArn)] = v.Tags
-	}
-
-	return tags, nil
 }
