b372847d8739fbee293125ce692c811357401db7
Graham Davison <gdavison@hashicorp.com>
2026-03-04 16:19:38 -0800

    Adds golden logs for `TestFlattenStructListOfStringEnum`

--- internal/framework/flex/autoflex_collections_test.go
+++ internal/framework/flex/autoflex_collections_test.go
@@ -835,7 +835,7 @@ func TestFlattenStructListOfStringEnum(t *testing.T) {
 		t.Run(testName, func(t *testing.T) {
 			t.Parallel()
 
-			runAutoFlattenTestCases(t, cases, runChecks{CompareDiags: false, CompareTarget: true, SkipGoldenLogs: true})
+			runAutoFlattenTestCases(t, cases, runChecks{CompareDiags: false, CompareTarget: true})
 		})
 	}
 }
