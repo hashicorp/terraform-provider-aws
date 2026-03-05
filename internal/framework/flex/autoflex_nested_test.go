eb6b6e5003464190367eff31939f5c11ca8481b2
Graham Davison <gdavison@hashicorp.com>
2026-03-04 16:03:20 -0800

    Adds golden logs for `TestFlattenSetOfNestedObjectField`

--- internal/framework/flex/autoflex_nested_test.go
+++ internal/framework/flex/autoflex_nested_test.go
@@ -1455,7 +1455,7 @@ func TestFlattenSetOfNestedObjectField(t *testing.T) {
 		t.Run(testName, func(t *testing.T) {
 			t.Parallel()
 
-			runAutoFlattenTestCases(t, cases, runChecks{CompareDiags: true, CompareTarget: true, SkipGoldenLogs: true})
+			runAutoFlattenTestCases(t, cases, runChecks{CompareDiags: true, CompareTarget: true})
 		})
 	}
 }
