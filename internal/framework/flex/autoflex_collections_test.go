b3d48455cb307a8e51706ce48397f5e818c0dec6
Graham Davison <gdavison@hashicorp.com>
2026-03-04 16:18:40 -0800

    Adds golden logs for `TestFlattenSimpleSetOfPrimitiveValues`

--- internal/framework/flex/autoflex_collections_test.go
+++ internal/framework/flex/autoflex_collections_test.go
@@ -765,7 +765,7 @@ func TestFlattenSimpleSetOfPrimitiveValues(t *testing.T) {
 		t.Run(testName, func(t *testing.T) {
 			t.Parallel()
 
-			runAutoFlattenTestCases(t, cases, runChecks{CompareDiags: false, CompareTarget: true, SkipGoldenLogs: true})
+			runAutoFlattenTestCases(t, cases, runChecks{CompareDiags: false, CompareTarget: true})
 		})
 	}
 }
