9b836e5fb75650f4324f15fca7779c919145f745
Kit Ewbank <Kit_Ewbank@hotmail.com>
2026-03-05 12:45:01 -0500

    Add 'iter.ApplyToAll2'.

--- internal/iter/apply_to_all.go
+++ internal/iter/apply_to_all.go
@@ -17,3 +17,14 @@ func ApplyToAll[E1, E2 any](seq iter.Seq[E1], f func(E1) E2) iter.Seq[E2] {
 		}
 	}
 }
+
+// ApplyToAll2 returns a new iterator yielding the results of applying the function `f` to each element of the original iterator `seq`.
+func ApplyToAll2[K1, K2, V1, V2 any](seq iter.Seq2[K1, V1], f func(K1, V1) (K2, V2)) iter.Seq2[K2, V2] {
+	return func(yield func(K2, V2) bool) {
+		for k, v := range seq {
+			if !yield(f(k, v)) {
+				return
+			}
+		}
+	}
+}
--- internal/iter/apply_to_all_test.go
+++ internal/iter/apply_to_all_test.go
@@ -4,6 +4,7 @@
 package iter
 
 import (
+	"maps"
 	"slices"
 	"strings"
 	"testing"
@@ -40,6 +41,45 @@ func TestApplyToAll(t *testing.T) {
 			iter := ApplyToAll(slices.Values(test.input), strings.ToUpper)
 
 			got := slices.Collect(iter)
+
+			if diff := cmp.Diff(got, test.expected); diff != "" {
+				t.Errorf("unexpected diff (+wanted, -got): %s", diff)
+			}
+		})
+	}
+}
+
+func TestApplyToAll2(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		input    map[string]int
+		expected map[string]int64
+	}
+	tests := map[string]testCase{
+		"three elements": {
+			input:    map[string]int{"one": 1, "two": 2, "three": 3},
+			expected: map[string]int64{"ONE": -1, "TWO": -2, "THREE": -3},
+		},
+		"one element": {
+			input:    map[string]int{"Four": 4},
+			expected: map[string]int64{"FOUR": -4},
+		},
+		"zero elements": {
+			input:    map[string]int{},
+			expected: map[string]int64{},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			iter := ApplyToAll2(maps.All(test.input), func(k string, v int) (string, int64) {
+				return strings.ToUpper(k), -int64(v)
+			})
+
+			got := maps.Collect(iter)
 
 			if diff := cmp.Diff(got, test.expected); diff != "" {
 				t.Errorf("unexpected diff (+wanted, -got): %s", diff)
