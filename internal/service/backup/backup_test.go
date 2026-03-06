9e5af8cde4ea34c00cc5a4b025324c190af3519b
Jared Baker <jared.baker@hashicorp.com>
2026-03-06 14:31:37 -0500

    backup: fix test compilation

--- internal/service/backup/backup_test.go
+++ internal/service/backup/backup_test.go
@@ -5,16 +5,17 @@ package backup_test
 
 import (
 	"fmt"
+	"testing"
 
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 )
 
 // randomFrameworkName returns a resource name that matches the pattern for Framework names
-func randomFrameworkName() string {
+func randomFrameworkName(t *testing.T) string {
 	return fmt.Sprintf("tf_acc_test_%s", acctest.RandString(t, 7))
 }
 
 // randomReportPlanName returns a resource name that matches the pattern for Report Plan names
-func randomReportPlanName() string {
+func randomReportPlanName(t *testing.T) string {
 	return fmt.Sprintf("tf_acc_test_%s", acctest.RandString(t, 7))
 }
