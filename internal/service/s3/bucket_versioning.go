0d4fc4705f7982272af5c5373e18e9f25f189d38
Graham Davison <gdavison@hashicorp.com>
2026-03-05 17:11:58 -0800

    Updates import alias

--- internal/service/s3/bucket_versioning.go
+++ internal/service/s3/bucket_versioning.go
@@ -13,7 +13,7 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
-	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+	awstypes "github.com/aws/aws-sdk-go-v2/service/s3/types"
 	"github.com/hashicorp/aws-sdk-go-base/v2/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/customdiff"
@@ -69,7 +69,7 @@ func resourceBucketVersioning() *schema.Resource {
 							Type:             schema.TypeString,
 							Optional:         true,
 							Computed:         true,
-							ValidateDiagFunc: enum.Validate[types.MFADelete](),
+							ValidateDiagFunc: enum.Validate[awstypes.MFADelete](),
 						},
 						names.AttrStatus: {
 							Type:         schema.TypeString,
@@ -93,7 +93,7 @@ func resourceBucketVersioning() *schema.Resource {
 				oldStatusRaw, newStatusRaw := diff.GetChange("versioning_configuration.0.status")
 				oldStatus, newStatus := oldStatusRaw.(string), newStatusRaw.(string)
 
-				if newStatus == bucketVersioningStatusDisabled && (oldStatus == string(types.BucketVersioningStatusEnabled) || oldStatus == string(types.BucketVersioningStatusSuspended)) {
+				if newStatus == bucketVersioningStatusDisabled && (oldStatus == string(awstypes.BucketVersioningStatusEnabled) || oldStatus == string(awstypes.BucketVersioningStatusSuspended)) {
 					return fmt.Errorf("versioning_configuration.status cannot be updated from '%s' to '%s'", oldStatus, newStatus)
 				}
 
@@ -259,10 +259,10 @@ func resourceBucketVersioningDelete(ctx context.Context, d *schema.ResourceData,
 
 	input := s3.PutBucketVersioningInput{
 		Bucket: aws.String(bucket),
-		VersioningConfiguration: &types.VersioningConfiguration{
+		VersioningConfiguration: &awstypes.VersioningConfiguration{
 			// Status must be provided thus to "remove" this resource,
 			// we suspend versioning
-			Status: types.BucketVersioningStatusSuspended,
+			Status: awstypes.BucketVersioningStatusSuspended,
 		},
 	}
 	if expectedBucketOwner != "" {
@@ -289,7 +289,7 @@ func resourceBucketVersioningDelete(ctx context.Context, d *schema.ResourceData,
 	return diags
 }
 
-func expandBucketVersioningConfiguration(l []any) *types.VersioningConfiguration {
+func expandBucketVersioningConfiguration(l []any) *awstypes.VersioningConfiguration {
 	if len(l) == 0 || l[0] == nil {
 		return nil
 	}
@@ -299,14 +299,14 @@ func expandBucketVersioningConfiguration(l []any) *types.VersioningConfiguration
 		return nil
 	}
 
-	result := &types.VersioningConfiguration{}
+	result := &awstypes.VersioningConfiguration{}
 
 	if v, ok := tfMap["mfa_delete"].(string); ok && v != "" {
-		result.MFADelete = types.MFADelete(v)
+		result.MFADelete = awstypes.MFADelete(v)
 	}
 
 	if v, ok := tfMap[names.AttrStatus].(string); ok && v != "" {
-		result.Status = types.BucketVersioningStatus(v)
+		result.Status = awstypes.BucketVersioningStatus(v)
 	}
 
 	return result
@@ -403,5 +403,5 @@ const (
 )
 
 func bucketVersioningStatus_Values() []string {
-	return tfslices.AppendUnique(enum.Values[types.BucketVersioningStatus](), bucketVersioningStatusDisabled)
+	return tfslices.AppendUnique(enum.Values[awstypes.BucketVersioningStatus](), bucketVersioningStatusDisabled)
 }
