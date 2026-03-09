0807a96d702407ed384077dffe0de476ae6194a4
Jared Baker <jared.baker@hashicorp.com>
2026-03-09 12:54:33 -0400

    d/aws_iam_outbound_web_identity_federation(test): serialize
    ```console
    % make t K=iam T=TestAccIAMOutboundWebIdentityFederation
    make: Verifying source code with gofmt...
    ==> Checking that code complies with gofmt requirements...
    make: Running acceptance tests on branch: 🌿 f-aws_iam_outbound_web_identity_federation 🌿...
    TF_ACC=1 go1.25.8 test ./internal/service/iam/... -v -count 1 -parallel 20 -run='TestAccIAMOutboundWebIdentityFederation'  -timeout 360m -vet=off
    2026/03/09 12:49:00 Creating Terraform AWS Provider (SDKv2-style)...
    2026/03/09 12:49:00 Initializing Terraform AWS Provider (SDKv2-style)...
    
    --- PASS: TestAccIAMOutboundWebIdentityFederation_serial (58.33s)
        --- PASS: TestAccIAMOutboundWebIdentityFederation_serial/dataSource (11.02s)
        --- PASS: TestAccIAMOutboundWebIdentityFederation_serial/basic (14.45s)
        --- PASS: TestAccIAMOutboundWebIdentityFederation_serial/disappears (12.09s)
        --- PASS: TestAccIAMOutboundWebIdentityFederation_serial/Identity (20.76s)
            --- PASS: TestAccIAMOutboundWebIdentityFederation_serial/Identity/basic (20.76s)
    PASS
    ok      github.com/hashicorp/terraform-provider-aws/internal/service/iam        65.259s
    ```

--- internal/service/iam/outbound_web_identity_federation_data_source_test.go
+++ internal/service/iam/outbound_web_identity_federation_data_source_test.go
@@ -6,31 +6,28 @@ package iam_test
 import (
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-testing/compare"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
-	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
 	"github.com/hashicorp/terraform-plugin-testing/statecheck"
 	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
-func TestAccIAMOutboundWebIdentityFederationDataSource_basic(t *testing.T) {
+func testAccIAMOutboundWebIdentityFederationDataSource_basic(t *testing.T) {
 	ctx := acctest.Context(t)
 	dataSourceName := "data.aws_iam_outbound_web_identity_federation.test"
 	resourceName := "aws_iam_outbound_web_identity_federation.test"
 
-	acctest.ParallelTest(ctx, t, resource.TestCase{
+	acctest.Test(ctx, t, resource.TestCase{
 		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
 		ErrorCheck:               acctest.ErrorCheck(t, names.IAMServiceID),
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccOutboundWebIdentityFederationDataSourceConfig_basic,
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttrPair(dataSourceName, "issuer_identifier", resourceName, "issuer_identifier"),
-				),
 				ConfigStateChecks: []statecheck.StateCheck{
-					statecheck.ExpectKnownValue(dataSourceName, tfjsonpath.New("issuer_identifier"), knownvalue.NotNull()),
+					statecheck.CompareValuePairs(dataSourceName, tfjsonpath.New("issuer_identifier"), resourceName, tfjsonpath.New("issuer_identifier"), compare.ValuesSame()),
 				},
 			},
 		},
