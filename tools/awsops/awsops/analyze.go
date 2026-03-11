425f3ebfdeb193728a96e58985471c7403696d6a
Jared Baker <jared.baker@hashicorp.com>
2026-03-11 15:18:42 -0400

    chore: more readability

--- tools/awsops/awsops/analyze.go
+++ tools/awsops/awsops/analyze.go
@@ -137,3 +137,37 @@ func analyzePackage(dir string) (map[string]ResourceOps, error) {
 
 	return results, nil
 }
+
+// discoverResources scans package comments for @SDKResource and @FrameworkResource annotations.
+func discoverResources(files map[string]*ast.File, dir string) []resourceInfo {
+	var resources []resourceInfo
+
+	for filename, file := range files {
+		for _, cg := range file.Comments {
+			text := cg.Text()
+			for _, line := range strings.Split(text, "\n") {
+				line = strings.TrimSpace(line)
+				if m := sdkResourceRe.FindStringSubmatch(line); m != nil {
+					resources = append(resources, resourceInfo{
+						Name:      m[1],
+						Type:      "sdk",
+						File:      filename,
+						Package:   file.Name.Name,
+						Directory: dir,
+					})
+				}
+				if m := fwResourceRe.FindStringSubmatch(line); m != nil {
+					resources = append(resources, resourceInfo{
+						Name:      m[1],
+						Type:      "framework",
+						File:      filename,
+						Package:   file.Name.Name,
+						Directory: dir,
+					})
+				}
+			}
+		}
+	}
+
+	return resources
+}
