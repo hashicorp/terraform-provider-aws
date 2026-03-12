hashicorp/terraform-provider-aws

commit c69a5527740beaf640cdb3d7ad6160e47f845b5a

r/aws_cloudfrontkeyvaluestore_key: Use AutoFlEx.

internal/service/cloudfrontkeyvaluestore/key.go
@@ -5,55 +5,47 @@ package cloudfrontkeyvaluestore
 
 import (
 	"context"
-	"errors"
+	"fmt"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/cloudfrontkeyvaluestore"
 	awstypes "github.com/aws/aws-sdk-go-v2/service/cloudfrontkeyvaluestore/types"
-	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
-	"github.com/hashicorp/terraform-provider-aws/internal/create"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
-	intflex "github.com/hashicorp/terraform-provider-aws/internal/flex"
+	"github.com/hashicorp/terraform-provider-aws/internal/errs/fwdiag"
+	"github.com/hashicorp/terraform-provider-aws/internal/flex"
 	"github.com/hashicorp/terraform-provider-aws/internal/framework"
-	"github.com/hashicorp/terraform-provider-aws/internal/framework/flex"
+	fwflex "github.com/hashicorp/terraform-provider-aws/internal/framework/flex"
+	fwtypes "github.com/hashicorp/terraform-provider-aws/internal/framework/types"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
-var (
-	ResourceKey = newResourceKey
-)
-
 // @FrameworkResource(name="Key")
-func newResourceKey(_ context.Context) (resource.ResourceWithConfigure, error) {
-	r := &resourceKey{}
+func newKeyResource(_ context.Context) (resource.ResourceWithConfigure, error) {
+	r := &keyResource{}
 
 	return r, nil
 }
 
-const (
-	ResNameKey        = "Key"
-	ResIDKeyPartCount = 2
-)
-
-type resourceKey struct {
+type keyResource struct {
 	framework.ResourceWithConfigure
+	framework.WithImportByID
 }
 
-func (r *resourceKey) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
-	resp.TypeName = "aws_cloudfrontkeyvaluestore_key"
+func (*keyResource) Metadata(_ context.Context, request resource.MetadataRequest, response *resource.MetadataResponse) {
+	response.TypeName = "aws_cloudfrontkeyvaluestore_key"
 }
 
-func (r *resourceKey) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
-	resp.Schema = schema.Schema{
+func (r *keyResource) Schema(ctx context.Context, request resource.SchemaRequest, response *resource.SchemaResponse) {
+	response.Schema = schema.Schema{
 		Attributes: map[string]schema.Attribute{
-			"id": framework.IDAttribute(),
+			names.AttrID: framework.IDAttribute(),
 			"key": schema.StringAttribute{
 				Required:            true,
 				MarkdownDescription: "The key to put.",
@@ -62,6 +54,7 @@ func (r *resourceKey) Schema(ctx context.Context, req resource.SchemaRequest, re
 				},
 			},
 			"key_value_store_arn": schema.StringAttribute{
+				CustomType:          fwtypes.ARNType,
 				Required:            true,
 				MarkdownDescription: "The Amazon Resource Name (ARN) of the Key Value Store.",
 				PlanModifiers: []planmodifier.String{
@@ -80,265 +73,247 @@ func (r *resourceKey) Schema(ctx context.Context, req resource.SchemaRequest, re
 	}
 }
 
-func (r *resourceKey) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+func (r *keyResource) Create(ctx context.Context, request resource.CreateRequest, response *resource.CreateResponse) {
+	var data keyResourceModel
+	response.Diagnostics.Append(request.Plan.Get(ctx, &data)...)
+	if response.Diagnostics.HasError() {
+		return
+	}
+
 	conn := r.Meta().CloudFrontKeyValueStoreClient(ctx)
 
-	var plan resourceKeyData
-	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	etag, err := findEtagByARN(ctx, conn, plan.KeyValueStoreARN.ValueString())
+	etag, err := findETagByARN(ctx, conn, data.KvsARN.ValueString())
 
 	if err != nil {
-		resp.Diagnostics.AddError(
-			create.ProblemStandardMessage(names.CloudFrontKeyValueStore, create.ErrActionCreating, ResNameKey, plan.Key.String(), err),
-			err.Error(),
-		)
+		response.Diagnostics.AddError(fmt.Sprintf("reading CloudFront KeyValueStore ETag (%s)", data.KvsARN.ValueString()), err.Error())
+
 		return
 	}
 
-	in := &cloudfrontkeyvaluestore.PutKeyInput{
-		IfMatch: etag,
-		Key:     aws.String(plan.Key.ValueString()),
-		KvsARN:  aws.String(plan.KeyValueStoreARN.ValueString()),
-		Value:   aws.String(plan.Value.ValueString()),
-	}
-
-	out, err := conn.PutKey(ctx, in)
-	if err != nil {
-		resp.Diagnostics.AddError(
-			create.ProblemStandardMessage(names.CloudFrontKeyValueStore, create.ErrActionCreating, ResNameKey, plan.Key.String(), err),
-			err.Error(),
-		)
-		return
-	}
-	if out == nil {
-		resp.Diagnostics.AddError(
-			create.ProblemStandardMessage(names.CloudFrontKeyValueStore, create.ErrActionCreating, ResNameKey, plan.Key.String(), nil),
-			errors.New("empty output").Error(),
-		)
+	input := &cloudfrontkeyvaluestore.PutKeyInput{}
+	response.Diagnostics.Append(fwflex.Expand(ctx, data, input)...)
+	if response.Diagnostics.HasError() {
 		return
 	}
 
-	plan.TotalSizeInBytes = flex.Int64ToFramework(ctx, out.TotalSizeInBytes)
-	err = plan.setID(ctx)
+	// Additional fields.
+	input.IfMatch = etag
+
+	output, err := conn.PutKey(ctx, input)
 
 	if err != nil {
-		resp.Diagnostics.AddError(
-			create.ProblemStandardMessage(names.CloudFrontKeyValueStore, create.ErrActionCreating, ResNameKey, plan.Key.String(), err),
-			err.Error(),
-		)
+		response.Diagnostics.AddError(fmt.Sprintf("creating CloudFront KeyValueStore (%s) Key (%s)", data.KvsARN.ValueString(), data.Key.ValueString()), err.Error())
+
 		return
 	}
 
-	resp.Diagnostics.Append(resp.State.Set(ctx, plan)...)
+	// Set values for unknowns.
+	data.TotalSizeInBytes = fwflex.Int64ToFramework(ctx, output.TotalSizeInBytes)
+	data.setID()
+
+	response.Diagnostics.Append(response.State.Set(ctx, data)...)
 }
 
-func (r *resourceKey) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
-	conn := r.Meta().CloudFrontKeyValueStoreClient(ctx)
-
-	var state resourceKeyData
-	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
-	if resp.Diagnostics.HasError() {
+func (r *keyResource) Read(ctx context.Context, request resource.ReadRequest, response *resource.ReadResponse) {
+	var data keyResourceModel
+	response.Diagnostics.Append(request.State.Get(ctx, &data)...)
+	if response.Diagnostics.HasError() {
 		return
 	}
 
-	kvsARN, out, err := FindKeyByID(ctx, conn, state.ID.ValueString())
+	if err := data.InitFromID(); err != nil {
+		response.Diagnostics.AddError("parsing resource ID", err.Error())
+
+		return
+	}
+
+	conn := r.Meta().CloudFrontKeyValueStoreClient(ctx)
+
+	output, err := findKeyByTwoPartKey(ctx, conn, data.KvsARN.ValueString(), data.Key.ValueString())
+
 	if tfresource.NotFound(err) {
-		resp.State.RemoveResource(ctx)
+		response.Diagnostics.Append(fwdiag.NewResourceNotFoundWarningDiagnostic(err))
+		response.State.RemoveResource(ctx)
+
 		return
 	}
-	if err != nil {
-		resp.Diagnostics.AddError(
-			create.ProblemStandardMessage(names.CloudFrontKeyValueStore, create.ErrActionSetting, ResNameKey, state.ID.String(), err),
-			err.Error(),
-		)
-		return
-	}
-
-	state.Key = flex.StringToFramework(ctx, out.Key)
-	state.KeyValueStoreARN = flex.StringValueToFramework(ctx, kvsARN)
-	state.TotalSizeInBytes = flex.Int64ToFramework(ctx, out.TotalSizeInBytes)
-	state.Value = flex.StringToFramework(ctx, out.Value)
-
-	resp.Diagnostics.Append(resp.State.Set(ctx, &state)...)
-}
-
-func (r *resourceKey) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
-	conn := r.Meta().CloudFrontKeyValueStoreClient(ctx)
-
-	var plan, state resourceKeyData
-	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
-	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	if !plan.Value.Equal(state.Value) {
-		etag, err := findEtagByARN(ctx, conn, plan.KeyValueStoreARN.ValueString())
-
-		if err != nil {
-			resp.Diagnostics.AddError(
-				create.ProblemStandardMessage(names.CloudFrontKeyValueStore, create.ErrActionUpdating, ResNameKey, plan.ID.String(), err),
-				err.Error(),
-			)
-			return
-		}
-
-		in := &cloudfrontkeyvaluestore.PutKeyInput{
-			IfMatch: etag,
-			Key:     aws.String(plan.Key.ValueString()),
-			KvsARN:  aws.String(plan.KeyValueStoreARN.ValueString()),
-			Value:   aws.String(plan.Value.ValueString()),
-		}
-
-		out, err := conn.PutKey(ctx, in)
-		if err != nil {
-			resp.Diagnostics.AddError(
-				create.ProblemStandardMessage(names.CloudFrontKeyValueStore, create.ErrActionUpdating, ResNameKey, plan.ID.String(), err),
-				err.Error(),
-			)
-			return
-		}
-		if out == nil {
-			resp.Diagnostics.AddError(
-				create.ProblemStandardMessage(names.CloudFrontKeyValueStore, create.ErrActionUpdating, ResNameKey, plan.ID.String(), nil),
-				errors.New("empty output").Error(),
-			)
-			return
-		}
-
-		plan.TotalSizeInBytes = flex.Int64ToFramework(ctx, out.TotalSizeInBytes)
-		err = plan.setID(ctx)
-
-		if err != nil {
-			resp.Diagnostics.AddError(
-				create.ProblemStandardMessage(names.CloudFrontKeyValueStore, create.ErrActionUpdating, ResNameKey, plan.ID.String(), err),
-				err.Error(),
-			)
-			return
-		}
-	}
-
-	resp.Diagnostics.Append(resp.State.Set(ctx, &plan)...)
-}
-
-func (r *resourceKey) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
-	conn := r.Meta().CloudFrontKeyValueStoreClient(ctx)
-
-	var state resourceKeyData
-	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	etag, err := findEtagByARN(ctx, conn, state.KeyValueStoreARN.ValueString())
 
 	if err != nil {
-		resp.Diagnostics.AddError(
-			create.ProblemStandardMessage(names.CloudFrontKeyValueStore, create.ErrActionDeleting, ResNameKey, state.ID.String(), err),
-			err.Error(),
-		)
+		response.Diagnostics.AddError(fmt.Sprintf("reading CloudFront KeyValueStore Key (%s)", data.ID.ValueString()), err.Error())
+
 		return
 	}
 
-	in := &cloudfrontkeyvaluestore.DeleteKeyInput{
+	// Set attributes for import.
+	response.Diagnostics.Append(fwflex.Flatten(ctx, output, &data)...)
+	if response.Diagnostics.HasError() {
+		return
+	}
+
+	response.Diagnostics.Append(response.State.Set(ctx, &data)...)
+}
+
+func (r *keyResource) Update(ctx context.Context, request resource.UpdateRequest, response *resource.UpdateResponse) {
+	var old, new keyResourceModel
+	response.Diagnostics.Append(request.State.Get(ctx, &old)...)
+	if response.Diagnostics.HasError() {
+		return
+	}
+	response.Diagnostics.Append(request.Plan.Get(ctx, &new)...)
+	if response.Diagnostics.HasError() {
+		return
+	}
+
+	conn := r.Meta().CloudFrontKeyValueStoreClient(ctx)
+
+	if !new.Value.Equal(old.Value) {
+		etag, err := findETagByARN(ctx, conn, new.KvsARN.ValueString())
+
+		if err != nil {
+			response.Diagnostics.AddError(fmt.Sprintf("reading CloudFront KeyValueStore ETag (%s)", new.KvsARN.ValueString()), err.Error())
+
+			return
+		}
+
+		input := &cloudfrontkeyvaluestore.PutKeyInput{}
+		response.Diagnostics.Append(fwflex.Expand(ctx, new, input)...)
+		if response.Diagnostics.HasError() {
+			return
+		}
+
+		// Additional fields.
+		input.IfMatch = etag
+
+		output, err := conn.PutKey(ctx, input)
+
+		if err != nil {
+			response.Diagnostics.AddError(fmt.Sprintf("updating CloudFront KeyValueStore (%s) Key (%s)", new.KvsARN.ValueString(), new.Key.ValueString()), err.Error())
+
+			return
+		}
+
+		// Set values for unknowns.
+		new.TotalSizeInBytes = fwflex.Int64ToFramework(ctx, output.TotalSizeInBytes)
+	}
+
+	response.Diagnostics.Append(response.State.Set(ctx, &new)...)
+}
+
+func (r *keyResource) Delete(ctx context.Context, request resource.DeleteRequest, response *resource.DeleteResponse) {
+	var data keyResourceModel
+	response.Diagnostics.Append(request.State.Get(ctx, &data)...)
+	if response.Diagnostics.HasError() {
+		return
+	}
+
+	conn := r.Meta().CloudFrontKeyValueStoreClient(ctx)
+
+	etag, err := findETagByARN(ctx, conn, data.KvsARN.ValueString())
+
+	if err != nil {
+		response.Diagnostics.AddError(fmt.Sprintf("reading CloudFront KeyValueStore ETag (%s)", data.KvsARN.ValueString()), err.Error())
+
+		return
+	}
+
+	_, err = conn.DeleteKey(ctx, &cloudfrontkeyvaluestore.DeleteKeyInput{
 		IfMatch: etag,
-		Key:     aws.String(state.Key.ValueString()),
-		KvsARN:  aws.String(state.KeyValueStoreARN.ValueString()),
+		Key:     fwflex.StringFromFramework(ctx, data.Key),
+		KvsARN:  fwflex.StringFromFramework(ctx, data.KvsARN),
+	})
+
+	if errs.IsA[*awstypes.ResourceNotFoundException](err) {
+		return
 	}
 
-	_, err = conn.DeleteKey(ctx, in)
-
 	if err != nil {
-		if errs.IsA[*awstypes.ResourceNotFoundException](err) {
-			return
-		}
-		resp.Diagnostics.AddError(
-			create.ProblemStandardMessage(names.CloudFrontKeyValueStore, create.ErrActionDeleting, ResNameKey, state.ID.String(), err),
-			err.Error(),
-		)
+		response.Diagnostics.AddError(fmt.Sprintf("deleting CloudFront KeyValueStore Key (%s)", data.ID.ValueString()), err.Error())
+
 		return
 	}
 }
 
-func (r *resourceKey) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
-	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
-}
-
-func FindKeyByID(ctx context.Context, conn *cloudfrontkeyvaluestore.Client, id string) (kvsARN string, getKeyOutPut *cloudfrontkeyvaluestore.GetKeyOutput, err error) {
-	parts, err := intflex.ExpandResourceId(id, ResIDKeyPartCount, false)
-
-	if err != nil {
-		return "", nil, err
+func findKeyByTwoPartKey(ctx context.Context, conn *cloudfrontkeyvaluestore.Client, kvsARN, key string) (*cloudfrontkeyvaluestore.GetKeyOutput, error) {
+	input := &cloudfrontkeyvaluestore.GetKeyInput{
+		Key:    aws.String(key),
+		KvsARN: aws.String(kvsARN),
 	}
 
-	in := &cloudfrontkeyvaluestore.GetKeyInput{
-		KvsARN: aws.String(parts[0]),
-		Key:    aws.String(parts[1]),
-	}
+	output, err := conn.GetKey(ctx, input)
 
-	out, err := conn.GetKey(ctx, in)
-	if err != nil {
-		if errs.IsA[*awstypes.ResourceNotFoundException](err) {
-			return "", nil, &retry.NotFoundError{
-				LastError:   err,
-				LastRequest: in,
-			}
+	if errs.IsA[*awstypes.ResourceNotFoundException](err) {
+		return nil, &retry.NotFoundError{
+			LastError:   err,
+			LastRequest: input,
 		}
-
-		return "", nil, err
 	}
 
-	if out == nil || out.Key == nil {
-		return "", nil, tfresource.NewEmptyResultError(in)
-	}
-
-	return parts[0], out, nil
-}
-
-func findEtagByARN(ctx context.Context, conn *cloudfrontkeyvaluestore.Client, arn string) (*string, error) {
-	in := &cloudfrontkeyvaluestore.DescribeKeyValueStoreInput{
-		KvsARN: aws.String(arn),
-	}
-
-	out, err := conn.DescribeKeyValueStore(ctx, in)
 	if err != nil {
-		if errs.IsA[*awstypes.ResourceNotFoundException](err) {
-			return nil, &retry.NotFoundError{
-				LastError:   err,
-				LastRequest: in,
-			}
-		}
-
 		return nil, err
 	}
 
-	if out == nil || out.ETag == nil {
-		return nil, tfresource.NewEmptyResultError(in)
+	if output == nil || output.Key == nil {
+		return nil, tfresource.NewEmptyResultError(input)
 	}
 
-	return out.ETag, nil
+	return output, nil
 }
 
-type resourceKeyData struct {
+func findETagByARN(ctx context.Context, conn *cloudfrontkeyvaluestore.Client, arn string) (*string, error) {
+	input := &cloudfrontkeyvaluestore.DescribeKeyValueStoreInput{
+		KvsARN: aws.String(arn),
+	}
+
+	output, err := conn.DescribeKeyValueStore(ctx, input)
+
+	if errs.IsA[*awstypes.ResourceNotFoundException](err) {
+		return nil, &retry.NotFoundError{
+			LastError:   err,
+			LastRequest: input,
+		}
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	if output == nil || output.ETag == nil {
+		return nil, tfresource.NewEmptyResultError(input)
+	}
+
+	return output.ETag, nil
+}
+
+type keyResourceModel struct {
 	ID               types.String `tfsdk:"id"`
 	Key              types.String `tfsdk:"key"`
-	KeyValueStoreARN types.String `tfsdk:"key_value_store_arn"`
+	KvsARN           fwtypes.ARN  `tfsdk:"key_value_store_arn"`
 	TotalSizeInBytes types.Int64  `tfsdk:"total_size_in_bytes"`
 	Value            types.String `tfsdk:"value"`
 }
 
-func (data *resourceKeyData) setID(ctx context.Context) error {
-	id, err := intflex.FlattenResourceId([]string{data.KeyValueStoreARN.ValueString(), data.Key.ValueString()}, ResIDKeyPartCount, false)
+const (
+	keyResourceIDPartCount = 2
+)
 
+func (data *keyResourceModel) InitFromID() error {
+	id := data.ID.ValueString()
+	parts, err := flex.ExpandResourceId(id, keyResourceIDPartCount, false)
 	if err != nil {
 		return err
 	}
 
-	data.ID = flex.StringToFramework(ctx, aws.String(id))
+	v, err := fwdiag.AsErr(fwtypes.ARNValue(parts[0]))
+	if err != nil {
+		return err
+	}
+
+	data.KvsARN = v
+	data.Key = types.StringValue(parts[1])
 
 	return nil
+}
+
+func (data *keyResourceModel) setID() {
+	data.ID = types.StringValue(errs.Must(flex.FlattenResourceId([]string{data.KvsARN.ValueString(), data.Key.ValueString()}, keyResourceIDPartCount, false)))
 }

internal/service/cloudfrontkeyvaluestore/key_test.go
@@ -5,7 +5,6 @@ package cloudfrontkeyvaluestore_test
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"testing"
 
@@ -14,7 +13,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
-	"github.com/hashicorp/terraform-provider-aws/internal/create"
 	tfcloudfrontkeyvaluestore "github.com/hashicorp/terraform-provider-aws/internal/service/cloudfrontkeyvaluestore"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/names"
@@ -22,7 +20,6 @@ import (
 
 func TestAccCloudFrontKeyValueStoreKey_basic(t *testing.T) {
 	ctx := acctest.Context(t)
-
 	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
 	value := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
 	resourceName := "aws_cloudfrontkeyvaluestore_key.test"
@@ -58,7 +55,6 @@ func TestAccCloudFrontKeyValueStoreKey_basic(t *testing.T) {
 
 func TestAccCloudFrontKeyValueStoreKey_value(t *testing.T) {
 	ctx := acctest.Context(t)
-
 	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
 	value1 := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
 	value2 := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
@@ -141,44 +137,35 @@ func testAccCheckKeyDestroy(ctx context.Context) resource.TestCheckFunc {
 				continue
 			}
 
-			_, _, err := tfcloudfrontkeyvaluestore.FindKeyByID(ctx, conn, rs.Primary.ID)
+			_, err := tfcloudfrontkeyvaluestore.FindKeyByTwoPartKey(ctx, conn, rs.Primary.Attributes["key_value_store_arn"], rs.Primary.Attributes["key"])
+
 			if tfresource.NotFound(err) {
 				return nil
 			}
 
 			if err != nil {
-				return create.Error(names.CloudFrontKeyValueStore, create.ErrActionCheckingDestroyed, tfcloudfrontkeyvaluestore.ResNameKey, rs.Primary.ID, err)
+				return err
 			}
 
-			return create.Error(names.CloudFrontKeyValueStore, create.ErrActionCheckingDestroyed, tfcloudfrontkeyvaluestore.ResNameKey, rs.Primary.ID, errors.New("not destroyed"))
+			return fmt.Errorf("CloudFront KeyValueStore Key %s still exists", rs.Primary.ID)
 		}
 
 		return nil
 	}
 }
 
-func testAccCheckKeyExists(ctx context.Context, name string) resource.TestCheckFunc {
+func testAccCheckKeyExists(ctx context.Context, n string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
-		rs, ok := s.RootModule().Resources[name]
+		rs, ok := s.RootModule().Resources[n]
 		if !ok {
-			return create.Error(names.CloudFrontKeyValueStore, create.ErrActionCheckingExistence, tfcloudfrontkeyvaluestore.ResNameKey, name, errors.New("not found"))
-		}
-
-		if rs.Primary.ID == "" {
-			return create.Error(names.CloudFrontKeyValueStore, create.ErrActionCheckingExistence, tfcloudfrontkeyvaluestore.ResNameKey, name, errors.New("not set"))
+			return fmt.Errorf("Not found: %s", n)
 		}
 
 		conn := acctest.Provider.Meta().(*conns.AWSClient).CloudFrontKeyValueStoreClient(ctx)
-		_, _, err := tfcloudfrontkeyvaluestore.FindKeyByID(ctx, conn, rs.Primary.ID)
-		if tfresource.NotFound(err) {
-			return create.Error(names.CloudFrontKeyValueStore, create.ErrActionCheckingExistence, tfcloudfrontkeyvaluestore.ResNameKey, rs.Primary.ID, errors.New("Resource Not Found"))
-		}
 
-		if err != nil {
-			return create.Error(names.CloudFrontKeyValueStore, create.ErrActionCheckingExistence, tfcloudfrontkeyvaluestore.ResNameKey, rs.Primary.ID, err)
-		}
+		_, err := tfcloudfrontkeyvaluestore.FindKeyByTwoPartKey(ctx, conn, rs.Primary.Attributes["key_value_store_arn"], rs.Primary.Attributes["key"])
 
-		return nil
+		return err
 	}
 }
 
@@ -187,6 +174,7 @@ func testAccKeyConfig_basic(rName, value string) string {
 resource "aws_cloudfront_key_value_store" "test" {
   name = %[1]q
 }
+
 resource "aws_cloudfrontkeyvaluestore_key" "test" {
   key                 = %[1]q
   key_value_store_arn = aws_cloudfront_key_value_store.test.arn

internal/service/cloudfrontkeyvaluestore/exports_test.go
@@ -1,0 +1,11 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package cloudfrontkeyvaluestore
+
+// Exports for use in tests only.
+var (
+	ResourceKey = newKeyResource
+
+	FindKeyByTwoPartKey = findKeyByTwoPartKey
+)

internal/service/cloudfrontkeyvaluestore/service_package_gen.go
@@ -21,7 +21,7 @@ func (p *servicePackage) FrameworkDataSources(ctx context.Context) []*types.Serv
 func (p *servicePackage) FrameworkResources(ctx context.Context) []*types.ServicePackageFrameworkResource {
 	return []*types.ServicePackageFrameworkResource{
 		{
-			Factory: newResourceKey,
+			Factory: newKeyResource,
 			Name:    "Key",
 		},
 	}

commit 9ef8658d62610a9ba66ce730496f72099056f01c

r/aws_cloudfrontkeyvaluestore_key: Fix value JSON encoding issue
Fixes the bug where string values were being JSON-encoded, resulting in extra
quotes in the stored values. For example, `value = "asdf"` was being stored as
`"asdf"` (with extra quotes) instead of `asdf` in AWS CloudFront KeyValueStore.

**Root Cause:**
The bug was introduced in commit c69a5527740 when the resource was refactored
to use AutoFlEx (fwflex.Expand/Flatten). AutoFlEx was JSON-encoding the Value
field during Create/Update and JSON-decoding it during Read, creating a
symmetrical encode/decode cycle that masked the issue in tests.

**Changes:**
1. key.go: Manually set Value field in Create, Update, and Read operations to
   bypass AutoFlEx JSON encoding/decoding
2. key_test.go: Added testAccCheckKeyHasValue helper that directly calls AWS
   API to verify stored values (not just Terraform state)
3. key_test.go: Enhanced existing tests with AWS API validation
4. key_test.go: Added TestAccCloudFrontKeyValueStoreKey_specialCharacters to
   test values with quotes and special characters

**Testing:**
All acceptance tests pass with real AWS resources:
- TestAccCloudFrontKeyValueStoreKey_basic
- TestAccCloudFrontKeyValueStoreKey_value
- TestAccCloudFrontKeyValueStoreKey_specialCharacters

Closes #46897

internal/service/cloudfrontkeyvaluestore/key.go
@@ -113,6 +113,8 @@ func (r *keyResource) Create(ctx context.Context, request resource.CreateRequest
 
 	// Additional fields.
 	input.IfMatch = etag
+	// Manually set Value to avoid JSON encoding by AutoFlEx.
+	input.Value = aws.String(data.Value.ValueString())
 
 	output, err := conn.PutKey(ctx, input)
 
@@ -159,6 +161,11 @@ func (r *keyResource) Read(ctx context.Context, request resource.ReadRequest, re
 		return
 	}
 
+	// Manually set Value to avoid JSON decoding by AutoFlEx.
+	if output.Value != nil {
+		data.Value = types.StringValue(*output.Value)
+	}
+
 	response.Diagnostics.Append(response.State.Set(ctx, &data)...)
 }
 
@@ -200,6 +207,8 @@ func (r *keyResource) Update(ctx context.Context, request resource.UpdateRequest
 
 		// Additional fields.
 		input.IfMatch = etag
+		// Manually set Value to avoid JSON encoding by AutoFlEx.
+		input.Value = aws.String(new.Value.ValueString())
 
 		output, err := conn.PutKey(ctx, input)
 

internal/service/cloudfrontkeyvaluestore/key_test.go
@@ -39,6 +39,7 @@ func TestAccCloudFrontKeyValueStoreKey_basic(t *testing.T) {
 				Config: testAccKeyConfig_basic(rName, value),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckKeyExists(ctx, t, resourceName),
+					testAccCheckKeyHasValue(ctx, t, resourceName, value),
 					resource.TestCheckResourceAttr(resourceName, names.AttrKey, rName),
 					resource.TestCheckResourceAttrPair(resourceName, "key_value_store_arn", "aws_cloudfront_key_value_store.test", names.AttrARN),
 					resource.TestCheckResourceAttrSet(resourceName, names.AttrID),
@@ -113,6 +114,7 @@ func TestAccCloudFrontKeyValueStoreKey_value(t *testing.T) {
 				Config: testAccKeyConfig_basic(rName, value1),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckKeyExists(ctx, t, resourceName),
+					testAccCheckKeyHasValue(ctx, t, resourceName, value1),
 					resource.TestCheckResourceAttr(resourceName, names.AttrKey, rName),
 					resource.TestCheckResourceAttrPair(resourceName, "key_value_store_arn", "aws_cloudfront_key_value_store.test", names.AttrARN),
 					resource.TestCheckResourceAttrSet(resourceName, names.AttrID),
@@ -129,12 +131,46 @@ func TestAccCloudFrontKeyValueStoreKey_value(t *testing.T) {
 				Config: testAccKeyConfig_basic(rName, value2),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckKeyExists(ctx, t, resourceName),
+					testAccCheckKeyHasValue(ctx, t, resourceName, value2),
 					resource.TestCheckResourceAttr(resourceName, names.AttrKey, rName),
 					resource.TestCheckResourceAttrPair(resourceName, "key_value_store_arn", "aws_cloudfront_key_value_store.test", names.AttrARN),
 					resource.TestCheckResourceAttrSet(resourceName, names.AttrID),
 					resource.TestCheckResourceAttrSet(resourceName, "total_size_in_bytes"),
 					resource.TestCheckResourceAttr(resourceName, names.AttrValue, value2),
 				),
+			},
+		},
+	})
+}
+
+func TestAccCloudFrontKeyValueStoreKey_specialCharacters(t *testing.T) {
+	ctx := acctest.Context(t)
+	rName := acctest.RandomWithPrefix(t, acctest.ResourcePrefix)
+	// Test value with special characters that would be affected by JSON encoding
+	value := `special "quoted" value with 'quotes' and {braces}`
+	resourceName := "aws_cloudfrontkeyvaluestore_key.test"
+
+	acctest.ParallelTest(ctx, t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+			acctest.PreCheckPartitionHasService(t, names.CloudFront)
+		},
+		ErrorCheck:               acctest.ErrorCheck(t, names.CloudFront),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckKeyDestroy(ctx, t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccKeyConfig_basic(rName, value),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckKeyExists(ctx, t, resourceName),
+					testAccCheckKeyHasValue(ctx, t, resourceName, value),
+					resource.TestCheckResourceAttr(resourceName, names.AttrValue, value),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
 			},
 		},
 	})
@@ -205,6 +241,33 @@ func testAccCheckKeyExists(ctx context.Context, t *testing.T, n string) resource
 		_, err := tfcloudfrontkeyvaluestore.FindKeyByTwoPartKey(ctx, conn, rs.Primary.Attributes["key_value_store_arn"], rs.Primary.Attributes[names.AttrKey])
 
 		return err
+	}
+}
+
+func testAccCheckKeyHasValue(ctx context.Context, t *testing.T, n string, expectedValue string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		conn := acctest.ProviderMeta(ctx, t).CloudFrontKeyValueStoreClient(ctx)
+
+		output, err := tfcloudfrontkeyvaluestore.FindKeyByTwoPartKey(ctx, conn, rs.Primary.Attributes["key_value_store_arn"], rs.Primary.Attributes[names.AttrKey])
+		if err != nil {
+			return err
+		}
+
+		if output.Value == nil {
+			return fmt.Errorf("CloudFront KeyValueStore Key value is nil")
+		}
+
+		actualValue := *output.Value
+		if actualValue != expectedValue {
+			return fmt.Errorf("CloudFront KeyValueStore Key value = %q, expected %q", actualValue, expectedValue)
+		}
+
+		return nil
 	}
 }
 