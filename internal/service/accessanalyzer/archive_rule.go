63a3e5ca4e17a093b5d5d2e232f057cc1bbf61a0
Jared Baker <jared.baker@hashicorp.com>
2026-03-18 20:01:30 -0400

    accessanalyzer: prefer create.UniqueId helper

--- internal/service/accessanalyzer/archive_rule.go
+++ internal/service/accessanalyzer/archive_rule.go
@@ -16,9 +16,9 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/accessanalyzer"
 	"github.com/aws/aws-sdk-go-v2/service/accessanalyzer/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	sdkid "github.com/hashicorp/terraform-plugin-sdk/v2/helper/id"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/create"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
 	"github.com/hashicorp/terraform-provider-aws/internal/flex"
@@ -101,7 +101,7 @@ func resourceArchiveRuleCreate(ctx context.Context, d *schema.ResourceData, meta
 	id := archiveRuleCreateResourceID(analyzerName, ruleName)
 	input := accessanalyzer.CreateArchiveRuleInput{
 		AnalyzerName: aws.String(analyzerName),
-		ClientToken:  aws.String(sdkid.UniqueId()),
+		ClientToken:  aws.String(create.UniqueId(ctx)),
 		RuleName:     aws.String(ruleName),
 	}
 
@@ -163,7 +163,7 @@ func resourceArchiveRuleUpdate(ctx context.Context, d *schema.ResourceData, meta
 
 	input := accessanalyzer.UpdateArchiveRuleInput{
 		AnalyzerName: aws.String(analyzerName),
-		ClientToken:  aws.String(sdkid.UniqueId()),
+		ClientToken:  aws.String(create.UniqueId(ctx)),
 		RuleName:     aws.String(ruleName),
 	}
 
@@ -194,7 +194,7 @@ func resourceArchiveRuleDelete(ctx context.Context, d *schema.ResourceData, meta
 	log.Printf("[INFO] Deleting IAM Access Analyzer Archive Rule: %s", d.Id())
 	input := accessanalyzer.DeleteArchiveRuleInput{
 		AnalyzerName: aws.String(analyzerName),
-		ClientToken:  aws.String(sdkid.UniqueId()),
+		ClientToken:  aws.String(create.UniqueId(ctx)),
 		RuleName:     aws.String(ruleName),
 	}
 	_, err = conn.DeleteArchiveRule(ctx, &input)
