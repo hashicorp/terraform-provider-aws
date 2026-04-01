aa27ee415eda0266fcfa8f19f3fdedb4ee6b010d
Graham Davison <gdavison@hashicorp.com>
2026-04-01 14:19:32 -0700

    Adds `Name` tag to Gateway test

--- internal/service/ec2/vpc_route_table_association_list_test.go
+++ internal/service/ec2/vpc_route_table_association_list_test.go
@@ -253,3 +253,76 @@ func TestAccVPCRouteTableAssociation_List_Gateway_basic(t *testing.T) {
 		},
 	})
 }
+
+func TestAccVPCRouteTableAssociation_List_Gateway_includeResource(t *testing.T) {
+	ctx := acctest.Context(t)
+
+	resourceName1 := "aws_route_table_association.internet"
+	resourceName2 := "aws_route_table_association.vpn"
+	rName := acctest.RandomWithPrefix(t, acctest.ResourcePrefix)
+
+	identity1 := tfstatecheck.Identity()
+	identity2 := tfstatecheck.Identity()
+
+	acctest.ParallelTest(ctx, t, resource.TestCase{
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.SkipBelow(tfversion.Version1_14_0),
+		},
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+		},
+		ErrorCheck:               acctest.ErrorCheck(t, names.EC2ServiceID),
+		CheckDestroy:             testAccCheckRouteTableAssociationDestroy(ctx, t),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		Steps: []resource.TestStep{
+			// Step 1: Setup
+			{
+				ConfigDirectory: config.StaticDirectory("testdata/RouteTableAssociation/list_gateway_include_resource/"),
+				ConfigVariables: config.Variables{
+					acctest.CtRName:  config.StringVariable(rName),
+					"resource_count": config.IntegerVariable(2),
+				},
+				ConfigStateChecks: []statecheck.StateCheck{
+					identity1.GetIdentity(resourceName1),
+					statecheck.CompareValuePairs(resourceName1, tfjsonpath.New("route_table_id"), "aws_route_table.test", tfjsonpath.New(names.AttrID), compare.ValuesSame()),
+					statecheck.CompareValuePairs(resourceName1, tfjsonpath.New("gateway_id"), "aws_internet_gateway.test", tfjsonpath.New(names.AttrID), compare.ValuesSame()),
+
+					identity2.GetIdentity(resourceName2),
+					statecheck.CompareValuePairs(resourceName2, tfjsonpath.New("route_table_id"), "aws_route_table.test", tfjsonpath.New(names.AttrID), compare.ValuesSame()),
+					statecheck.CompareValuePairs(resourceName2, tfjsonpath.New("gateway_id"), "aws_vpn_gateway.test", tfjsonpath.New(names.AttrID), compare.ValuesSame()),
+				},
+			},
+
+			// Step 2: Query
+			{
+				Query:           true,
+				ConfigDirectory: config.StaticDirectory("testdata/RouteTableAssociation/list_gateway_include_resource/"),
+				ConfigVariables: config.Variables{
+					acctest.CtRName:  config.StringVariable(rName),
+					"resource_count": config.IntegerVariable(2),
+				},
+				QueryResultChecks: []querycheck.QueryResultCheck{
+					tfquerycheck.ExpectIdentityFunc("aws_route_table_association.test", identity1.Checks()),
+					querycheck.ExpectResourceDisplayName("aws_route_table_association.test", tfqueryfilter.ByResourceIdentityFunc(identity1.Checks()), knownvalue.StringRegexp(regexache.MustCompile(`^`+rName+"-Internet"+` / rtb-[a-f0-9]+ \(rtbassoc-[a-f0-9]+\)$`))),
+					querycheck.ExpectResourceKnownValues("aws_route_table_association.test", tfqueryfilter.ByResourceIdentityFunc(identity1.Checks()), []querycheck.KnownValueCheck{
+						tfquerycheck.KnownValueCheck(tfjsonpath.New("gateway_id"), knownvalue.StringRegexp(regexache.MustCompile(`^igw-[a-f0-9]+$`))),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New(names.AttrID), knownvalue.StringRegexp(regexache.MustCompile(`^rtbassoc-[a-f0-9]+$`))),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New(names.AttrRegion), knownvalue.StringExact(acctest.Region())),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New("route_table_id"), knownvalue.StringRegexp(regexache.MustCompile(`^rtb-[a-f0-9]+$`))),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New(names.AttrSubnetID), knownvalue.StringExact("")),
+					}),
+
+					tfquerycheck.ExpectIdentityFunc("aws_route_table_association.test", identity2.Checks()),
+					querycheck.ExpectResourceDisplayName("aws_route_table_association.test", tfqueryfilter.ByResourceIdentityFunc(identity2.Checks()), knownvalue.StringRegexp(regexache.MustCompile(`^`+rName+"-VPN"+` / rtb-[a-f0-9]+ \(rtbassoc-[a-f0-9]+\)$`))),
+					querycheck.ExpectResourceKnownValues("aws_route_table_association.test", tfqueryfilter.ByResourceIdentityFunc(identity2.Checks()), []querycheck.KnownValueCheck{
+						tfquerycheck.KnownValueCheck(tfjsonpath.New("gateway_id"), knownvalue.StringRegexp(regexache.MustCompile(`^vgw-[a-f0-9]+$`))),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New(names.AttrID), knownvalue.StringRegexp(regexache.MustCompile(`^rtbassoc-[a-f0-9]+$`))),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New(names.AttrRegion), knownvalue.StringExact(acctest.Region())),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New("route_table_id"), knownvalue.StringRegexp(regexache.MustCompile(`^rtb-[a-f0-9]+$`))),
+						tfquerycheck.KnownValueCheck(tfjsonpath.New(names.AttrSubnetID), knownvalue.StringExact("")),
+					}),
+				},
+			},
+		},
+	})
+}
