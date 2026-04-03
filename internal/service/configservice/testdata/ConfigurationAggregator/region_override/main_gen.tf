4121d2821403a477647b10ababaddadf509855b1
Kit Ewbank <Kit_Ewbank@hotmail.com>
2026-04-03 11:23:27 -0400

    r/aws_config_configuration_aggregator: Add RI.

---
+++ internal/service/configservice/testdata/ConfigurationAggregator/region_override/main_gen.tf
@@ -0,0 +1,29 @@
+# Copyright IBM Corp. 2014, 2026
+# SPDX-License-Identifier: MPL-2.0
+
+resource "aws_config_configuration_aggregator" "test" {
+  region = var.region
+
+  name = var.rName
+
+  account_aggregation_source {
+    account_ids = [data.aws_caller_identity.current.account_id]
+    regions     = [data.aws_region.current.region]
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+variable "rName" {
+  description = "Name for resource"
+  type        = string
+  nullable    = false
+}
+
+variable "region" {
+  description = "Region to deploy resource in"
+  type        = string
+  nullable    = false
+}
