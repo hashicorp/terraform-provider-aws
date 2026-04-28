43259d29b161c44263066bcb1d3475c716ebefb7
Stefan Freitag <stefan.freitag@udo.edu>
2026-04-28 20:46:56 +0200

    fix: fix code formatting

--- internal/service/appintegrations/data_integration_test.go
+++ internal/service/appintegrations/data_integration_test.go
@@ -376,24 +376,24 @@ resource "aws_s3_bucket" "test" {
 
 data "aws_iam_policy_document" "test" {
   statement {
-	sid    = "AllowAppIntegrations"
-	effect = "Allow"
+    sid    = "AllowAppIntegrations"
+    effect = "Allow"
 
-	principals {
-	  type        = "Service"
-	  identifiers = ["app-integrations.amazonaws.com"]
-	}
+    principals {
+      type        = "Service"
+      identifiers = ["app-integrations.amazonaws.com"]
+    }
 
-	actions = [
-	  "s3:ListBucket",
-	  "s3:GetObject",
-	  "s3:GetBucketLocation",
-	]
+    actions = [
+      "s3:ListBucket",
+      "s3:GetObject",
+      "s3:GetBucketLocation",
+    ]
 
-	resources = [
-	  aws_s3_bucket.test.arn,
-	  "${aws_s3_bucket.test.arn}/*",
-	]
+    resources = [
+      aws_s3_bucket.test.arn,
+      "${aws_s3_bucket.test.arn}/*",
+    ]
   }
 }
 
