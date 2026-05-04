28764c3eba530d2441060ed7891b938ecefe382e
Andrew Nicoll <anicoll@canva.com>
2026-05-04 13:43:22 +0930

    feat: add resource for AWS Tax Registration

---
+++ internal/service/taxsettings/exports_test.go
@@ -0,0 +1,11 @@
+// Copyright IBM Corp. 2014, 2026
+// SPDX-License-Identifier: MPL-2.0
+
+package taxsettings
+
+// Exports for use in tests only.
+var (
+	ResourceTaxRegistration = newTaxRegistrationResource
+
+	FindTaxRegistrationByAccountID = findTaxRegistrationByAccountID
+)
