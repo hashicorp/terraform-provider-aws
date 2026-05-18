6ea35d7d8237a422fe1f6b9d31e206b89429efaa
taruntej-a <tarunsraina483@gmail.com>
2026-05-19 01:29:30 +0530

    r/aws_iam_access_key: RI

--- internal/service/iam/access_key.go
+++ internal/service/iam/access_key.go
@@ -22,6 +22,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/enum"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
+	"github.com/hashicorp/terraform-provider-aws/internal/provider/sdkv2/importer"
 	"github.com/hashicorp/terraform-provider-aws/internal/retry"
 	tfslices "github.com/hashicorp/terraform-provider-aws/internal/slices"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
@@ -30,6 +31,12 @@ import (
 )
 
 // @SDKResource("aws_iam_access_key", name="Access Key")
+// @IdentityAttribute("id")
+// @Testing(existsType="github.com/aws/aws-sdk-go-v2/service/iam/types;awstypes;awstypes.AccessKeyMetadata")
+// @Testing(importIgnore="encrypted_secret;key_fingerprint;pgp_key;secret;ses_smtp_password_v4;encrypted_ses_smtp_password_v4")
+// @Testing(preIdentityVersion="v6.45.0")
+// @Testing(plannableImportAction="NoOp")
+// @CustomImport
 func resourceAccessKey() *schema.Resource {
 	return &schema.Resource{
 		CreateWithoutTimeout: resourceAccessKeyCreate,
@@ -42,6 +49,10 @@ func resourceAccessKey() *schema.Resource {
 			//   ValidationError: Must specify userName when calling with non-User credentials
 			// To prevent import from requiring this extra information, use GetAccessKeyLastUsed.
 			StateContext: func(ctx context.Context, d *schema.ResourceData, meta any) ([]*schema.ResourceData, error) {
+				if err := importer.Import(ctx, d, meta); err != nil {
+					return nil, err
+				}
+
 				conn := meta.(*conns.AWSClient).IAMClient(ctx)
 
 				input := &iam.GetAccessKeyLastUsedInput{
