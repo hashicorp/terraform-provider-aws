ac139556e577d7c1848ff49209856b8c4413071e
Jared Baker <jared.baker@hashicorp.com>
2026-05-20 14:14:41 -0400

    datasync: use `ExpectResourceAction` with `_disappears` tests

--- internal/service/datasync/task_test.go
+++ internal/service/datasync/task_test.go
@@ -96,6 +96,14 @@ func TestAccDataSyncTask_disappears(t *testing.T) {
 					acctest.CheckSDKResourceDisappears(ctx, t, tfdatasync.ResourceTask(), resourceName),
 				),
 				ExpectNonEmptyPlan: true,
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceName, plancheck.ResourceActionCreate),
+					},
+					PostApplyPostRefresh: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceName, plancheck.ResourceActionCreate),
+					},
+				},
 			},
 		},
 	})
