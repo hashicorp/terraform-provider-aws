128a2b59a1156dad9a16ad5b3d1c14e22a946989
Jared Baker <jared.baker@hashicorp.com>
2026-05-20 14:14:46 -0400

    ds: use `ExpectResourceAction` with `_disappears` tests

--- internal/service/ds/trust_test.go
+++ internal/service/ds/trust_test.go
@@ -97,6 +97,14 @@ func TestAccDSTrust_disappears(t *testing.T) {
 					acctest.CheckFrameworkResourceDisappears(ctx, t, tfds.ResourceTrust, resourceName),
 				),
 				ExpectNonEmptyPlan: true,
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceName, plancheck.ResourceActionCreate),
+					},
+					PostApplyPostRefresh: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceName, plancheck.ResourceActionCreate),
+					},
+				},
 			},
 		},
 	})
