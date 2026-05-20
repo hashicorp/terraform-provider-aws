c8697fb390751a15b033af1efa15e276432bd92d
Jared Baker <jared.baker@hashicorp.com>
2026-05-20 14:14:48 -0400

    ecs: use `ExpectResourceAction` with `_disappears` tests

--- internal/service/ecs/capacity_provider_test.go
+++ internal/service/ecs/capacity_provider_test.go
@@ -79,6 +79,14 @@ func TestAccECSCapacityProvider_disappears(t *testing.T) {
 					acctest.CheckSDKResourceDisappears(ctx, t, tfecs.ResourceCapacityProvider(), resourceName),
 				),
 				ExpectNonEmptyPlan: true,
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceName, plancheck.ResourceActionCreate),
+					},
+					PostApplyPostRefresh: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceName, plancheck.ResourceActionCreate),
+					},
+				},
 			},
 		},
 	})
