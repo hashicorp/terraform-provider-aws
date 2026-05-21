5e89d991a8dd0f912d67bc272f8212f2407f7f40
Benjamin Saint-Cyr <benstcyr@amazon.com>
2026-05-21 10:00:56 -0400

    d/aws_ec2_hosts: Add acceptance test for outpost_arn client-side filter

--- internal/service/ec2/ec2_hosts_data_source_test.go
+++ internal/service/ec2/ec2_hosts_data_source_test.go
@@ -32,6 +32,26 @@ func TestAccEC2HostsDataSource_filter(t *testing.T) {
 	})
 }
 
+func TestAccEC2HostsDataSource_outpostARN(t *testing.T) {
+	ctx := acctest.Context(t)
+	dataSourceName := "data.aws_ec2_hosts.test"
+	rName := acctest.RandomWithPrefix(t, acctest.ResourcePrefix)
+
+	acctest.ParallelTest(ctx, t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(ctx, t); acctest.PreCheckOutpostsOutposts(ctx, t) },
+		ErrorCheck:               acctest.ErrorCheck(t, names.EC2ServiceID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccHostsDataSourceConfig_outpostARN(rName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(dataSourceName, "ids.#", "1"),
+				),
+			},
+		},
+	})
+}
+
 func TestAccEC2HostsDataSource_tags(t *testing.T) {
 	ctx := acctest.Context(t)
 	dataSourceName := "data.aws_ec2_hosts.test"
@@ -50,6 +70,37 @@ func TestAccEC2HostsDataSource_tags(t *testing.T) {
 			},
 		},
 	})
+}
+
+func testAccHostsDataSourceConfig_outpostARN(rName string) string {
+	return acctest.ConfigCompose(acctest.ConfigAvailableAZsNoOptIn(), fmt.Sprintf(`
+data "aws_outposts_outposts" "test" {}
+
+data "aws_outposts_outpost" "test" {
+  id = tolist(data.aws_outposts_outposts.test.ids)[0]
+}
+
+resource "aws_ec2_host" "test" {
+  instance_family   = "r5d"
+  availability_zone = data.aws_availability_zones.available.names[1]
+  outpost_arn       = data.aws_outposts_outpost.test.arn
+
+  tags = {
+    Name = %[1]q
+  }
+}
+
+data "aws_ec2_hosts" "test" {
+  outpost_arn = data.aws_outposts_outpost.test.arn
+
+  filter {
+    name   = "instance-type"
+    values = ["r5d.*"]
+  }
+
+  depends_on = [aws_ec2_host.test]
+}
+`, rName))
 }
 
 func testAccHostsDataSourceConfig_filter(rName string) string {
