d2212ca5f0fe80d8612f062d78c69ed527225269
Jared Baker <jared.baker@hashicorp.com>
2026-05-27 09:45:17 -0400

    signer: use `ExpectResourceAction` with `_disappears` tests

--- internal/service/signer/signing_profile_test.go
+++ internal/service/signer/signing_profile_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/signer"
 	"github.com/aws/aws-sdk-go-v2/service/signer/types"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/retry"
@@ -85,6 +86,14 @@ func TestAccSignerSigningProfile_disappears(t *testing.T) {
 					acctest.CheckSDKResourceDisappears(ctx, t, tfsigner.ResourceSigningProfile(), resourceName),
 				),
 				ExpectNonEmptyPlan: true,
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction("aws_signer_signing_profile.test_sp", plancheck.ResourceActionCreate),
+					},
+					PostApplyPostRefresh: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction("aws_signer_signing_profile.test_sp", plancheck.ResourceActionCreate),
+					},
+				},
 			},
 		},
 	})
