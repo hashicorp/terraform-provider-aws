6e88e6b81ad5fbbe38c2299199697e27628d7668
Graham Davison <gdavison@hashicorp.com>
2026-05-28 10:51:32 -0700

    Reverts `resourceReplicationGroupConfigV1` to `Schema`

--- internal/service/elasticache/replication_group_migrate.go
+++ internal/service/elasticache/replication_group_migrate.go
@@ -41,247 +41,245 @@ func replicationGroupStateUpgradeFromV1(ctx context.Context, rawState map[string
 func resourceReplicationGroupConfigV1() *schema.Resource {
 	//lintignore:R011
 	return &schema.Resource{
-		SchemaFunc: func() map[string]*schema.Schema {
-			return map[string]*schema.Schema{
-				names.AttrApplyImmediately: {
-					Type:     schema.TypeBool,
-					Optional: true,
-					Computed: true,
-				},
-				names.AttrARN: {
-					Type:     schema.TypeString,
-					Computed: true,
-				},
-				"at_rest_encryption_enabled": {
-					Type:     schema.TypeBool,
-					Optional: true,
-					ForceNew: true,
-					Computed: true,
-				},
-				"auth_token": {
-					Type:      schema.TypeString,
-					Optional:  true,
-					Sensitive: true,
-				},
-				names.AttrAutoMinorVersionUpgrade: { // nosemgrep:ci.semgrep.types.valid-nullable-bool
-					Type:     nullable.TypeNullableBool,
-					Optional: true,
-					Computed: true,
-				},
-				"automatic_failover_enabled": {
-					Type:     schema.TypeBool,
-					Optional: true,
-					Default:  false,
-				},
-				"cluster_enabled": {
-					Type:     schema.TypeBool,
-					Computed: true,
-				},
-				"configuration_endpoint_address": {
-					Type:     schema.TypeString,
-					Computed: true,
-				},
-				"data_tiering_enabled": {
-					Type:     schema.TypeBool,
-					Optional: true,
-					Computed: true,
-					ForceNew: true,
-				},
-				names.AttrDescription: {
-					Type:     schema.TypeString,
-					Optional: true,
-					Computed: true,
-				},
-				names.AttrEngine: {
-					Type:         schema.TypeString,
-					Optional:     true,
-					ForceNew:     true,
-					Default:      engineRedis,
-					ValidateFunc: validation.StringInSlice([]string{engineRedis, engineValkey}, true),
-				},
-				names.AttrEngineVersion: {
-					Type:     schema.TypeString,
-					Optional: true,
-					Computed: true,
-				},
-				"engine_version_actual": {
-					Type:     schema.TypeString,
-					Computed: true,
-				},
-				"global_replication_group_id": {
-					Type:     schema.TypeString,
-					Optional: true,
-					ForceNew: true,
-					Computed: true,
-				},
-				"ip_discovery": {
-					Type:     schema.TypeString,
-					Optional: true,
-					Computed: true,
-				},
-				"log_delivery_configuration": {
-					Type:     schema.TypeSet,
-					Optional: true,
-					MaxItems: 2,
-					Elem: &schema.Resource{
-						Schema: map[string]*schema.Schema{
-							"destination_type": {
-								Type:     schema.TypeString,
-								Required: true,
-							},
-							names.AttrDestination: {
-								Type:     schema.TypeString,
-								Required: true,
-							},
-							"log_format": {
-								Type:     schema.TypeString,
-								Required: true,
-							},
-							"log_type": {
-								Type:     schema.TypeString,
-								Required: true,
-							},
+		Schema: map[string]*schema.Schema{
+			names.AttrApplyImmediately: {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Computed: true,
+			},
+			names.AttrARN: {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"at_rest_encryption_enabled": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				ForceNew: true,
+				Computed: true,
+			},
+			"auth_token": {
+				Type:      schema.TypeString,
+				Optional:  true,
+				Sensitive: true,
+			},
+			names.AttrAutoMinorVersionUpgrade: { // nosemgrep:ci.semgrep.types.valid-nullable-bool
+				Type:     nullable.TypeNullableBool,
+				Optional: true,
+				Computed: true,
+			},
+			"automatic_failover_enabled": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+			},
+			"cluster_enabled": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+			"configuration_endpoint_address": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"data_tiering_enabled": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			names.AttrDescription: {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			names.AttrEngine: {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ForceNew:     true,
+				Default:      engineRedis,
+				ValidateFunc: validation.StringInSlice([]string{engineRedis, engineValkey}, true),
+			},
+			names.AttrEngineVersion: {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"engine_version_actual": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"global_replication_group_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+				Computed: true,
+			},
+			"ip_discovery": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"log_delivery_configuration": {
+				Type:     schema.TypeSet,
+				Optional: true,
+				MaxItems: 2,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"destination_type": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						names.AttrDestination: {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"log_format": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"log_type": {
+							Type:     schema.TypeString,
+							Required: true,
 						},
 					},
 				},
-				"maintenance_window": {
-					Type:     schema.TypeString,
-					Optional: true,
-					Computed: true,
+			},
+			"maintenance_window": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"member_clusters": {
+				Type:     schema.TypeSet,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"multi_az_enabled": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+			},
+			"network_type": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"node_type": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"notification_topic_arn": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"num_cache_clusters": {
+				Type:     schema.TypeInt,
+				Computed: true,
+				Optional: true,
+			},
+			"num_node_groups": {
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			names.AttrParameterGroupName: {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			names.AttrPort: {
+				Type:     schema.TypeInt,
+				Optional: true,
+				ForceNew: true,
+			},
+			"preferred_cache_cluster_azs": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"primary_endpoint_address": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"reader_endpoint_address": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"replicas_per_node_group": {
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"replication_group_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"security_group_names": {
+				Type:     schema.TypeSet,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			names.AttrSecurityGroupIDs: {
+				Type:     schema.TypeSet,
+				Optional: true,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"snapshot_arns": {
+				Type:     schema.TypeSet,
+				Optional: true,
+				ForceNew: true,
+				// Note: Unlike aws_elasticache_cluster, this does not have a limit of 1 item.
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
 				},
-				"member_clusters": {
-					Type:     schema.TypeSet,
-					Computed: true,
-					Elem:     &schema.Schema{Type: schema.TypeString},
-				},
-				"multi_az_enabled": {
-					Type:     schema.TypeBool,
-					Optional: true,
-					Default:  false,
-				},
-				"network_type": {
-					Type:     schema.TypeString,
-					Optional: true,
-					Computed: true,
-					ForceNew: true,
-				},
-				"node_type": {
-					Type:     schema.TypeString,
-					Optional: true,
-					Computed: true,
-				},
-				"notification_topic_arn": {
-					Type:     schema.TypeString,
-					Optional: true,
-				},
-				"num_cache_clusters": {
-					Type:     schema.TypeInt,
-					Computed: true,
-					Optional: true,
-				},
-				"num_node_groups": {
-					Type:     schema.TypeInt,
-					Optional: true,
-					Computed: true,
-				},
-				names.AttrParameterGroupName: {
-					Type:     schema.TypeString,
-					Optional: true,
-					Computed: true,
-				},
-				names.AttrPort: {
-					Type:     schema.TypeInt,
-					Optional: true,
-					ForceNew: true,
-				},
-				"preferred_cache_cluster_azs": {
-					Type:     schema.TypeList,
-					Optional: true,
-					Elem:     &schema.Schema{Type: schema.TypeString},
-				},
-				"primary_endpoint_address": {
-					Type:     schema.TypeString,
-					Computed: true,
-				},
-				"reader_endpoint_address": {
-					Type:     schema.TypeString,
-					Computed: true,
-				},
-				"replicas_per_node_group": {
-					Type:     schema.TypeInt,
-					Optional: true,
-					Computed: true,
-				},
-				"replication_group_id": {
-					Type:     schema.TypeString,
-					Required: true,
-					ForceNew: true,
-				},
-				"security_group_names": {
-					Type:     schema.TypeSet,
-					Optional: true,
-					Computed: true,
-					ForceNew: true,
-					Elem:     &schema.Schema{Type: schema.TypeString},
-				},
-				names.AttrSecurityGroupIDs: {
-					Type:     schema.TypeSet,
-					Optional: true,
-					Computed: true,
-					Elem:     &schema.Schema{Type: schema.TypeString},
-				},
-				"snapshot_arns": {
-					Type:     schema.TypeSet,
-					Optional: true,
-					ForceNew: true,
-					// Note: Unlike aws_elasticache_cluster, this does not have a limit of 1 item.
-					Elem: &schema.Schema{
-						Type: schema.TypeString,
-					},
-				},
-				"snapshot_retention_limit": {
-					Type:     schema.TypeInt,
-					Optional: true,
-				},
-				"snapshot_window": {
-					Type:     schema.TypeString,
-					Optional: true,
-					Computed: true,
-				},
-				"snapshot_name": {
-					Type:     schema.TypeString,
-					Optional: true,
-					ForceNew: true,
-				},
-				"subnet_group_name": {
-					Type:     schema.TypeString,
-					Optional: true,
-					Computed: true,
-					ForceNew: true,
-				},
-				names.AttrTags:    tftags.TagsSchema(),
-				names.AttrTagsAll: tftags.TagsSchemaComputed(),
-				"transit_encryption_enabled": {
-					Type:     schema.TypeBool,
-					Optional: true,
-					ForceNew: true,
-					Computed: true,
-				},
-				"user_group_ids": {
-					Type:     schema.TypeSet,
-					Optional: true,
-					Elem:     &schema.Schema{Type: schema.TypeString},
-				},
-				names.AttrKMSKeyID: {
-					Type:     schema.TypeString,
-					ForceNew: true,
-					Optional: true,
-				},
-				names.AttrFinalSnapshotIdentifier: {
-					Type:     schema.TypeString,
-					Optional: true,
-				},
-			}
+			},
+			"snapshot_retention_limit": {
+				Type:     schema.TypeInt,
+				Optional: true,
+			},
+			"snapshot_window": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"snapshot_name": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"subnet_group_name": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			names.AttrTags:    tftags.TagsSchema(),
+			names.AttrTagsAll: tftags.TagsSchemaComputed(),
+			"transit_encryption_enabled": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				ForceNew: true,
+				Computed: true,
+			},
+			"user_group_ids": {
+				Type:     schema.TypeSet,
+				Optional: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			names.AttrKMSKeyID: {
+				Type:     schema.TypeString,
+				ForceNew: true,
+				Optional: true,
+			},
+			names.AttrFinalSnapshotIdentifier: {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
 		},
 	}
 }
