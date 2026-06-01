a5a2df7b1a8ac19013100affbfb354c319934ce7
Graham Davison <gdavison@hashicorp.com>
2026-06-01 10:50:08 -0700

    `xray`

--- internal/service/xray/group.go
+++ internal/service/xray/group.go
@@ -35,42 +35,44 @@ func resourceGroup() *schema.Resource {
 		UpdateWithoutTimeout: resourceGroupUpdate,
 		DeleteWithoutTimeout: resourceGroupDelete,
 
-		Schema: map[string]*schema.Schema{
-			names.AttrARN: {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-			names.AttrGroupName: {
-				Type:     schema.TypeString,
-				Required: true,
-				ForceNew: true,
-			},
-			"filter_expression": {
-				Type:     schema.TypeString,
-				Required: true,
-			},
-			"insights_configuration": {
-				Type:     schema.TypeList,
-				Optional: true,
-				Computed: true,
-				MaxItems: 1,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"insights_enabled": {
-							Type:     schema.TypeBool,
-							Required: true,
-						},
-						"notifications_enabled": {
-							Type:     schema.TypeBool,
-							Optional: true,
-							Computed: true,
-						},
-					},
-				},
-			},
-			names.AttrTags:    tftags.TagsSchema(),
-			names.AttrTagsAll: tftags.TagsSchemaComputed(),
-		},
+		SchemaFunc: func() map[string]*schema.Schema {
+  	return map[string]*schema.Schema{
+  			names.AttrARN: {
+  				Type:     schema.TypeString,
+  				Computed: true,
+  			},
+  			names.AttrGroupName: {
+  				Type:     schema.TypeString,
+  				Required: true,
+  				ForceNew: true,
+  			},
+  			"filter_expression": {
+  				Type:     schema.TypeString,
+  				Required: true,
+  			},
+  			"insights_configuration": {
+  				Type:     schema.TypeList,
+  				Optional: true,
+  				Computed: true,
+  				MaxItems: 1,
+  				Elem: &schema.Resource{
+  					Schema: map[string]*schema.Schema{
+  						"insights_enabled": {
+  							Type:     schema.TypeBool,
+  							Required: true,
+  						},
+  						"notifications_enabled": {
+  							Type:     schema.TypeBool,
+  							Optional: true,
+  							Computed: true,
+  						},
+  					},
+  				},
+  			},
+  			names.AttrTags:    tftags.TagsSchema(),
+  			names.AttrTagsAll: tftags.TagsSchemaComputed(),
+  		}
+  },
 	}
 }
 
