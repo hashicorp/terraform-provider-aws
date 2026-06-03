49c2e1740034cf18a3973ec127c10253b22d2177
Adrian Johnson <adrian.johnson@hashicorp.com>
2026-06-03 15:48:58 -0400

    update test templates

--- internal/service/cleanrooms/testdata/Membership/list_basic/main.tf
+++ internal/service/cleanrooms/testdata/Membership/list_basic/main.tf
@@ -1,6 +1,13 @@
 # Copyright IBM Corp. 2014, 2026
 # SPDX-License-Identifier: MPL-2.0
 
+resource "aws_cleanrooms_membership" "test" {
+  count = var.resource_count
+
+  collaboration_id = aws_cleanrooms_collaboration.test[count.index].id
+  query_log_status = "DISABLED"
+}
+
 resource "aws_cleanrooms_collaboration" "test" {
   count = var.resource_count
 
@@ -10,13 +17,6 @@ resource "aws_cleanrooms_collaboration" "test" {
   description              = "Test membership ${count.index}"
   query_log_status         = "DISABLED"
   analytics_engine         = "SPARK"
-}
-
-resource "aws_cleanrooms_membership" "test" {
-  count = var.resource_count
-
-  collaboration_id = aws_cleanrooms_collaboration.test[count.index].id
-  query_log_status = "DISABLED"
 }
 
 variable "rName" {
