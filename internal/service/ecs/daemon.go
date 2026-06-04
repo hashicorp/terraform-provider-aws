4db03858ad4fa3a86b40136f9ef9348787d3048b
Jared Baker <jared.baker@hashicorp.com>
2026-06-04 14:56:43 -0400

    r/aws_ecs_daemon: remove provider-side default

--- internal/service/ecs/daemon.go
+++ internal/service/ecs/daemon.go
@@ -22,7 +22,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/boolplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64default"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
@@ -149,7 +149,9 @@ func (r *daemonResource) Schema(ctx context.Context, request resource.SchemaRequ
 									"enable": schema.BoolAttribute{
 										Optional: true,
 										Computed: true,
-										Default:  booldefault.StaticBool(false),
+										PlanModifiers: []planmodifier.Bool{
+											boolplanmodifier.UseStateForUnknown(),
+										},
 									},
 								},
 							},
