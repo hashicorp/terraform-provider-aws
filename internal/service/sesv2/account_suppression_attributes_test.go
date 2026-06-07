3d2c8f70ff357d9ac3de1563a6329295ee4dfdd6
tabito <tabito.htnet.org@gmail.com>
2026-06-08 07:36:09 +0900

    Add CheckDestroy and disappears test

--- internal/service/sesv2/account_suppression_attributes_test.go
+++ internal/service/sesv2/account_suppression_attributes_test.go
@@ -6,15 +6,18 @@ package sesv2_test
 import (
 	"context"
 	"fmt"
+	"slices"
 	"testing"
 
 	"github.com/aws/aws-sdk-go-v2/service/sesv2/types"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
 	"github.com/hashicorp/terraform-plugin-testing/statecheck"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+	"github.com/hashicorp/terraform-provider-aws/internal/retry"
 	tfsesv2 "github.com/hashicorp/terraform-provider-aws/internal/service/sesv2"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
@@ -24,6 +27,7 @@ func TestAccSESV2AccountSuppressionAttributes_serial(t *testing.T) {
 
 	testCases := map[string]func(t *testing.T){
 		acctest.CtBasic:        testAccAccountSuppressionAttributes_basic,
+		acctest.CtDisappears:   testAccAccountSuppressionAttributes_disappears,
 		"update":               testAccAccountSuppressionAttributes_update,
 		"validationAttributes": testAccAccountSuppressionAttributes_validationAttributes,
 	}
@@ -39,7 +43,7 @@ func testAccAccountSuppressionAttributes_basic(t *testing.T) {
 		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
 		ErrorCheck:               acctest.ErrorCheck(t, names.SESV2ServiceID),
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
-		CheckDestroy:             acctest.CheckDestroyNoop,
+		CheckDestroy:             testAccCheckAccountSuppressionAttributesDestroy(ctx, t),
 		Steps: []resource.TestStep{
 			{
 				Config: testAccAccountSuppressionAttributesConfig_basic,
@@ -64,6 +68,35 @@ func testAccAccountSuppressionAttributes_basic(t *testing.T) {
 	})
 }
 
+func testAccAccountSuppressionAttributes_disappears(t *testing.T) {
+	ctx := acctest.Context(t)
+	resourceName := "aws_sesv2_account_suppression_attributes.test"
+
+	acctest.Test(ctx, t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
+		ErrorCheck:               acctest.ErrorCheck(t, names.SESV2ServiceID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckAccountSuppressionAttributesDestroy(ctx, t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAccountSuppressionAttributesConfig_basic,
+				Check: resource.ComposeTestCheckFunc(
+					acctest.CheckFrameworkResourceDisappears(ctx, t, tfsesv2.ResourceAccountSuppressionAttributes, resourceName),
+				),
+				ExpectNonEmptyPlan: true,
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceName, plancheck.ResourceActionCreate),
+					},
+					PostApplyPostRefresh: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceName, plancheck.ResourceActionUpdate),
+					},
+				},
+			},
+		},
+	})
+}
+
 func testAccAccountSuppressionAttributes_update(t *testing.T) {
 	ctx := acctest.Context(t)
 	resourceName := "aws_sesv2_account_suppression_attributes.test"
@@ -72,7 +105,7 @@ func testAccAccountSuppressionAttributes_update(t *testing.T) {
 		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
 		ErrorCheck:               acctest.ErrorCheck(t, names.SESV2ServiceID),
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
-		CheckDestroy:             acctest.CheckDestroyNoop,
+		CheckDestroy:             testAccCheckAccountSuppressionAttributesDestroy(ctx, t),
 		Steps: []resource.TestStep{
 			{
 				Config: testAccAccountSuppressionAttributesConfig_basic,
@@ -109,7 +142,7 @@ func testAccAccountSuppressionAttributes_validationAttributes(t *testing.T) {
 		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
 		ErrorCheck:               acctest.ErrorCheck(t, names.SESV2ServiceID),
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
-		CheckDestroy:             acctest.CheckDestroyNoop,
+		CheckDestroy:             testAccCheckAccountSuppressionAttributesDestroy(ctx, t),
 		Steps: []resource.TestStep{
 			{
 				Config: testAccAccountSuppressionAttributesConfig_validationAttributes(string(types.FeatureStatusEnabled), string(types.SuppressionConfidenceVerdictThresholdHigh)),
@@ -239,6 +272,45 @@ func testAccCheckAccountSuppressionAttributesExists(ctx context.Context, t *test
 		_, err := tfsesv2.FindAccountSuppressionAttributes(ctx, conn)
 
 		return err
+	}
+}
+
+func testAccCheckAccountSuppressionAttributesDestroy(ctx context.Context, t *testing.T) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		conn := acctest.ProviderMeta(ctx, t).SESV2Client(ctx)
+
+		for _, rs := range s.RootModule().Resources {
+			if rs.Type != "aws_sesv2_account_suppression_attributes" {
+				continue
+			}
+
+			output, err := tfsesv2.FindAccountSuppressionAttributes(ctx, conn)
+			if retry.NotFound(err) {
+				continue
+			}
+
+			if err != nil {
+				return err
+			}
+
+			if len(output.SuppressedReasons) != 2 ||
+				!slices.Contains(output.SuppressedReasons, types.SuppressionListReasonBounce) ||
+				!slices.Contains(output.SuppressedReasons, types.SuppressionListReasonComplaint) {
+				return fmt.Errorf("SESv2 Account Suppression Attributes %s was not reset to default suppressed reasons", rs.Primary.ID)
+			}
+
+			if output.ValidationAttributes == nil || output.ValidationAttributes.ConditionThreshold == nil {
+				continue
+			}
+
+			if output.ValidationAttributes.ConditionThreshold.ConditionThresholdEnabled == types.FeatureStatusDisabled {
+				continue
+			}
+
+			return fmt.Errorf("SESv2 Account Suppression Attributes %s was not reset to disabled condition threshold", rs.Primary.ID)
+		}
+
+		return nil
 	}
 }
 
