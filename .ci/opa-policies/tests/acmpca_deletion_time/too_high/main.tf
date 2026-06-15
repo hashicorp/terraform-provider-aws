103b3ddc432e9030746e7379d1e61cd34a00576c
Graham Davison <gdavison@hashicorp.com>
2026-06-15 14:03:27 -0700

    Copyright headers

--- .ci/opa-policies/tests/acmpca_deletion_time/too_high/main.tf
+++ .ci/opa-policies/tests/acmpca_deletion_time/too_high/main.tf
@@ -1,3 +1,6 @@
+# Copyright IBM Corp. 2014, 2026
+# SPDX-License-Identifier: MPL-2.0
+
 resource "aws_acmpca_certificate_authority" "test" {
   permanent_deletion_time_in_days = 30
   type                            = "ROOT"
