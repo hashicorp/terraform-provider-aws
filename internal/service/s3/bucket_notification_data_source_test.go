ba4b61bdc4f20a7d7699ae45a2d8d1359866d506
Dirk Avery <31492422+YakDriver@users.noreply.github.com>
2026-06-18 17:24:36 -0400

    d/aws_s3_bucket_notification(test): add basic acceptance test
    Reuses the existing testAccBucketNotificationConfig_lambdaFunction
    helper from the resource tests; layers a data block on top and asserts
    the data source surfaces the lambda_function block, eventbridge=false,
    and empty queue/topic lists.
    
    Refs #501 #31512

---
+++ internal/service/s3/bucket_notification_data_source_test.go
@@ -0,0 +1,52 @@
+// Copyright IBM Corp. 2014, 2026
+// SPDX-License-Identifier: MPL-2.0
+
+package s3_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+	"github.com/hashicorp/terraform-provider-aws/names"
+)
+
+func TestAccS3BucketNotificationDataSource_basic(t *testing.T) {
+	ctx := acctest.Context(t)
+	rName := acctest.RandomWithPrefix(t, acctest.ResourcePrefix)
+	dataSourceName := "data.aws_s3_bucket_notification.test"
+	lambdaResourceName := "aws_lambda_function.test"
+
+	acctest.ParallelTest(ctx, t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
+		ErrorCheck:               acctest.ErrorCheck(t, names.S3ServiceID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccBucketNotificationDataSourceConfig_basic(rName),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(dataSourceName, "eventbridge", acctest.CtFalse),
+					resource.TestCheckResourceAttr(dataSourceName, "lambda_function.#", "1"),
+					resource.TestCheckResourceAttr(dataSourceName, "lambda_function.0.id", "notification-lambda"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "lambda_function.0.lambda_function_arn", lambdaResourceName, names.AttrARN),
+					resource.TestCheckResourceAttr(dataSourceName, "lambda_function.0.events.#", "2"),
+					resource.TestCheckResourceAttr(dataSourceName, "lambda_function.0.filter_prefix", "tf-acc-test/"),
+					resource.TestCheckResourceAttr(dataSourceName, "lambda_function.0.filter_suffix", ".png"),
+					resource.TestCheckResourceAttr(dataSourceName, "queue.#", "0"),
+					resource.TestCheckResourceAttr(dataSourceName, "topic.#", "0"),
+				),
+			},
+		},
+	})
+}
+
+func testAccBucketNotificationDataSourceConfig_basic(rName string) string {
+	return fmt.Sprintf(`
+%[1]s
+
+data "aws_s3_bucket_notification" "test" {
+  bucket = aws_s3_bucket_notification.test.bucket
+}
+`, testAccBucketNotificationConfig_lambdaFunction(rName))
+}
