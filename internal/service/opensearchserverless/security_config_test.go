3511f684867bd7cd6b96c54519b9e7db528a2498
tabito <tabito.htnet.org@gmail.com>
2026-06-20 09:25:41 +0900

    Add acceptance tests for iam_federation_options and iam_identity_center_options

--- internal/service/opensearchserverless/security_config_test.go
+++ internal/service/opensearchserverless/security_config_test.go
@@ -134,6 +134,155 @@ func TestAccOpenSearchServerlessSecurityConfig_disappears(t *testing.T) {
 	})
 }
 
+func TestAccOpenSearchServerlessSecurityConfig_iamFederationOptions(t *testing.T) {
+	ctx := acctest.Context(t)
+	var securityconfig types.SecurityConfigDetail
+	rName := acctest.RandomWithPrefix(t, acctest.ResourcePrefix)
+	resourceName := "aws_opensearchserverless_security_config.test"
+
+	acctest.ParallelTest(ctx, t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+			acctest.PreCheckPartitionHasService(t, names.OpenSearchServerlessEndpointID)
+			testAccPreCheckSecurityConfig(ctx, t)
+		},
+		ErrorCheck:               acctest.ErrorCheck(t, names.OpenSearchServerlessServiceID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckSecurityConfigDestroy(ctx, t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSecurityConfig_iamFederationOptionsWithGroupAttributeOnly(rName, "test-1"),
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceName, plancheck.ResourceActionCreate),
+					},
+				},
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckSecurityConfigExists(ctx, t, resourceName, &securityconfig),
+					resource.TestCheckResourceAttr(resourceName, names.AttrType, string(types.SecurityConfigTypeIamfederation)),
+					resource.TestCheckResourceAttr(resourceName, names.AttrName, rName),
+					resource.TestCheckResourceAttr(resourceName, "iam_federation_options.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "iam_federation_options.0.group_attribute", "test-1"),
+					resource.TestCheckNoResourceAttr(resourceName, "iam_federation_options.0.user_attribute"),
+				),
+			},
+			{
+				Config: testAccSecurityConfig_iamFederationOptions(rName, "test-1", "test-a"),
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceName, plancheck.ResourceActionUpdate),
+					},
+				},
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckSecurityConfigExists(ctx, t, resourceName, &securityconfig),
+					resource.TestCheckResourceAttr(resourceName, names.AttrType, string(types.SecurityConfigTypeIamfederation)),
+					resource.TestCheckResourceAttr(resourceName, names.AttrName, rName),
+					resource.TestCheckResourceAttr(resourceName, "iam_federation_options.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "iam_federation_options.0.group_attribute", "test-1"),
+					resource.TestCheckResourceAttr(resourceName, "iam_federation_options.0.user_attribute", "test-a"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccSecurityConfig_iamFederationOptions(rName, "test-2", "test-b"),
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceName, plancheck.ResourceActionUpdate),
+					},
+				},
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckSecurityConfigExists(ctx, t, resourceName, &securityconfig),
+					resource.TestCheckResourceAttr(resourceName, names.AttrType, string(types.SecurityConfigTypeIamfederation)),
+					resource.TestCheckResourceAttr(resourceName, names.AttrName, rName),
+					resource.TestCheckResourceAttr(resourceName, "iam_federation_options.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "iam_federation_options.0.group_attribute", "test-2"),
+					resource.TestCheckResourceAttr(resourceName, "iam_federation_options.0.user_attribute", "test-b"),
+				),
+			},
+			{
+				Config: testAccSecurityConfig_iamFederationOptionsWithGroupAttributeOnly(rName, "test-1"),
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceName, plancheck.ResourceActionUpdate),
+					},
+				},
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckSecurityConfigExists(ctx, t, resourceName, &securityconfig),
+					resource.TestCheckResourceAttr(resourceName, names.AttrType, string(types.SecurityConfigTypeIamfederation)),
+					resource.TestCheckResourceAttr(resourceName, names.AttrName, rName),
+					resource.TestCheckResourceAttr(resourceName, "iam_federation_options.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "iam_federation_options.0.group_attribute", "test-1"),
+					resource.TestCheckNoResourceAttr(resourceName, "iam_federation_options.0.user_attribute"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccOpenSearchServerlessSecurityConfig_iamIdentityCenterOptions(t *testing.T) {
+	ctx := acctest.Context(t)
+	var securityconfig types.SecurityConfigDetail
+	rName := acctest.RandomWithPrefix(t, acctest.ResourcePrefix)
+	resourceName := "aws_opensearchserverless_security_config.test"
+
+	acctest.ParallelTest(ctx, t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+			acctest.PreCheckPartitionHasService(t, names.OpenSearchServerlessEndpointID)
+			acctest.PreCheckSSOAdminInstances(ctx, t)
+			testAccPreCheckSecurityConfig(ctx, t)
+		},
+		ErrorCheck:               acctest.ErrorCheck(t, names.OpenSearchServerlessServiceID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckSecurityConfigDestroy(ctx, t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSecurityConfig_iamIdentityCenterOptionsWithoutGroupAndUserAttribute(rName),
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceName, plancheck.ResourceActionCreate),
+					},
+				},
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckSecurityConfigExists(ctx, t, resourceName, &securityconfig),
+					resource.TestCheckResourceAttr(resourceName, names.AttrType, string(types.SecurityConfigTypeIamidentitycenter)),
+					resource.TestCheckResourceAttr(resourceName, names.AttrName, rName),
+					resource.TestCheckResourceAttr(resourceName, "iam_identity_center_options.#", "1"),
+					resource.TestCheckResourceAttrSet(resourceName, "iam_identity_center_options.0.instance_arn"),
+					resource.TestCheckResourceAttr(resourceName, "iam_identity_center_options.0.group_attribute", string(types.IamIdentityCenterGroupAttributeGroupId)),
+					resource.TestCheckResourceAttr(resourceName, "iam_identity_center_options.0.user_attribute", string(types.IamIdentityCenterUserAttributeUserId)),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccSecurityConfig_iamIdentityCenterOptions(rName, string(types.IamIdentityCenterGroupAttributeGroupName), string(types.IamIdentityCenterUserAttributeUserName)),
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceName, plancheck.ResourceActionUpdate),
+					},
+				},
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckSecurityConfigExists(ctx, t, resourceName, &securityconfig),
+					resource.TestCheckResourceAttr(resourceName, names.AttrType, string(types.SecurityConfigTypeIamidentitycenter)),
+					resource.TestCheckResourceAttr(resourceName, names.AttrName, rName),
+					resource.TestCheckResourceAttr(resourceName, "iam_identity_center_options.#", "1"),
+					resource.TestCheckResourceAttrSet(resourceName, "iam_identity_center_options.0.instance_arn"),
+					resource.TestCheckResourceAttr(resourceName, "iam_identity_center_options.0.group_attribute", string(types.IamIdentityCenterGroupAttributeGroupName)),
+					resource.TestCheckResourceAttr(resourceName, "iam_identity_center_options.0.user_attribute", string(types.IamIdentityCenterUserAttributeUserName)),
+				),
+			},
+		},
+	})
+}
+
 func TestAccOpenSearchServerlessSecurityConfig_upgradeV6_0_0(t *testing.T) {
 	ctx := acctest.Context(t)
 	var securityconfig types.SecurityConfigDetail
@@ -290,4 +439,67 @@ resource "aws_opensearchserverless_security_config" "test" {
   }
 }
 `, rName, samlOptions, sessionTimeout)
+}
+
+func testAccSecurityConfig_iamFederationOptions(rName, groupAttribute, userAttribute string) string {
+	return fmt.Sprintf(`
+data "aws_ssoadmin_instances" "test" {}
+
+resource "aws_opensearchserverless_security_config" "test" {
+  name = %[1]q
+  type = "iamfederation"
+
+  iam_federation_options {
+    group_attribute = %[2]q
+    user_attribute  = %[3]q
+  }
+}
+`, rName, groupAttribute, userAttribute)
+}
+
+func testAccSecurityConfig_iamFederationOptionsWithGroupAttributeOnly(rName, groupAttribute string) string {
+	return fmt.Sprintf(`
+data "aws_ssoadmin_instances" "test" {}
+
+resource "aws_opensearchserverless_security_config" "test" {
+  name = %[1]q
+  type = "iamfederation"
+
+  iam_federation_options {
+    group_attribute = %[2]q
+  }
+}
+`, rName, groupAttribute)
+}
+
+func testAccSecurityConfig_iamIdentityCenterOptions(rName, groupAttribute, userAttribute string) string {
+	return fmt.Sprintf(`
+data "aws_ssoadmin_instances" "test" {}
+
+resource "aws_opensearchserverless_security_config" "test" {
+  name = %[1]q
+  type = "iamidentitycenter"
+
+  iam_identity_center_options {
+    instance_arn    = tolist(data.aws_ssoadmin_instances.test.arns)[0]
+    group_attribute = %[2]q
+    user_attribute  = %[3]q
+  }
+}
+`, rName, groupAttribute, userAttribute)
+}
+
+func testAccSecurityConfig_iamIdentityCenterOptionsWithoutGroupAndUserAttribute(rName string) string {
+	return fmt.Sprintf(`
+data "aws_ssoadmin_instances" "test" {}
+
+resource "aws_opensearchserverless_security_config" "test" {
+  name = %[1]q
+  type = "iamidentitycenter"
+
+  iam_identity_center_options {
+    instance_arn    = tolist(data.aws_ssoadmin_instances.test.arns)[0]
+  }
+}
+`, rName)
 }
