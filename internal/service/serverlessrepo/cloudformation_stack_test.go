75dbf4c70ae39819aa4f7aa1e8ec1a115a9969fa
Dirk Avery <31492422+YakDriver@users.noreply.github.com>
2026-06-23 17:46:42 -0400

    Move unit test

--- internal/service/serverlessrepo/cloudformation_stack_test.go
+++ internal/service/serverlessrepo/cloudformation_stack_test.go
@@ -14,6 +14,8 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/cloudformation"
 	cloudformationtypes "github.com/aws/aws-sdk-go-v2/service/cloudformation/types"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	awstypes "github.com/aws/aws-sdk-go-v2/service/serverlessapplicationrepository/types"
+	"github.com/google/go-cmp/cmp"
 	"github.com/hashicorp/aws-sdk-go-base/v2/endpoints"
 	"github.com/hashicorp/aws-sdk-go-base/v2/tfawserr"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -26,6 +28,82 @@ import (
 
 // Since aws_serverlessapplicationrepository_cloudformation_stack creates CloudFormation stacks,
 // the aws_cloudformation_stack sweeper will clean these up as well.
+
+func TestFlattenNonDefaultCloudFormationParameters(t *testing.T) {
+	t.Parallel()
+
+	for _, tc := range []struct {
+		name             string
+		cfParams         []cloudformationtypes.Parameter
+		parameterDefs    []awstypes.ParameterDefinition
+		configuredParams map[string]any
+		expected         map[string]any
+	}{
+		{
+			name: "non-default value included",
+			cfParams: []cloudformationtypes.Parameter{
+				{ParameterKey: aws.String("Param1"), ParameterValue: aws.String("custom")},
+			},
+			parameterDefs: []awstypes.ParameterDefinition{
+				{Name: aws.String("Param1"), DefaultValue: aws.String("default")},
+			},
+			configuredParams: map[string]any{},
+			expected:         map[string]any{"Param1": "custom"},
+		},
+		{
+			name: "NoEcho preserves configured value",
+			cfParams: []cloudformationtypes.Parameter{
+				{ParameterKey: aws.String("Secret"), ParameterValue: aws.String("****")},
+			},
+			parameterDefs: []awstypes.ParameterDefinition{
+				{Name: aws.String("Secret"), NoEcho: aws.Bool(true)},
+			},
+			configuredParams: map[string]any{"Secret": "my-secret"},
+			expected:         map[string]any{"Secret": "my-secret"},
+		},
+		{
+			name: "NoEcho not in config is excluded",
+			cfParams: []cloudformationtypes.Parameter{
+				{ParameterKey: aws.String("Secret"), ParameterValue: aws.String("****")},
+			},
+			parameterDefs: []awstypes.ParameterDefinition{
+				{Name: aws.String("Secret"), NoEcho: aws.Bool(true)},
+			},
+			configuredParams: map[string]any{},
+			expected:         map[string]any{},
+		},
+		{
+			name: "configured param matching default is included",
+			cfParams: []cloudformationtypes.Parameter{
+				{ParameterKey: aws.String("Param1"), ParameterValue: aws.String("default")},
+			},
+			parameterDefs: []awstypes.ParameterDefinition{
+				{Name: aws.String("Param1"), DefaultValue: aws.String("default")},
+			},
+			configuredParams: map[string]any{"Param1": "default"},
+			expected:         map[string]any{"Param1": "default"},
+		},
+		{
+			name: "unconfigured param matching default is excluded",
+			cfParams: []cloudformationtypes.Parameter{
+				{ParameterKey: aws.String("Param1"), ParameterValue: aws.String("default")},
+			},
+			parameterDefs: []awstypes.ParameterDefinition{
+				{Name: aws.String("Param1"), DefaultValue: aws.String("default")},
+			},
+			configuredParams: map[string]any{},
+			expected:         map[string]any{},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			result := tfserverlessrepo.FlattenNonDefaultCloudFormationParameters(tc.cfParams, tc.parameterDefs, tc.configuredParams)
+			if diff := cmp.Diff(tc.expected, result); diff != "" {
+				t.Errorf("unexpected result (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
 
 func TestAccServerlessRepoCloudFormationStack_basic(t *testing.T) {
 	ctx := acctest.Context(t)
