27d185fe5e7b3d00e363f29b01a02fb70d6476e8
Graham Davison <gdavison@hashicorp.com>
2026-06-30 11:22:44 -0700

    Adds comment

--- internal/service/bedrock/testdata/UseCaseForModelAccess/basic/main_gen.tf
+++ internal/service/bedrock/testdata/UseCaseForModelAccess/basic/main_gen.tf
@@ -1,6 +1,7 @@
 # Copyright IBM Corp. 2014, 2026
 # SPDX-License-Identifier: MPL-2.0
 
+# Use import-on-create as this resource type is a global singleton and cannot be deleted
 resource "aws_bedrock_use_case_for_model_access" "test" {
   form_data = data.aws_bedrock_use_case_for_model_access.test.form_data
 }
--- internal/service/bedrock/testdata/tmpl/use_case_for_model_access_basic.gtpl
+++ internal/service/bedrock/testdata/tmpl/use_case_for_model_access_basic.gtpl
@@ -1,3 +1,4 @@
+# Use import-on-create as this resource type is a global singleton and cannot be deleted
 resource "aws_bedrock_use_case_for_model_access" "test" {
   form_data = data.aws_bedrock_use_case_for_model_access.test.form_data
 }
