049f9476502d18256bd8465faff45c58f9ffbb96
taruntej-a <tarunsraina483@gmail.com>
2026-07-02 02:42:03 +0530

    r/aws_ssoadmin_region: add plancheck

--- internal/service/ssoadmin/region_test.go
+++ internal/service/ssoadmin/region_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/ssoadmin"
 	"github.com/aws/aws-sdk-go-v2/service/ssoadmin/types"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/create"
@@ -88,6 +89,14 @@ func testAccSSOAdminRegion_disappears(t *testing.T) {
 					acctest.CheckFrameworkResourceDisappears(ctx, t, tfssoadmin.ResourceRegion, resourceName),
 				),
 				ExpectNonEmptyPlan: true,
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceName, plancheck.ResourceActionCreate),
+					},
+					PostApplyPostRefresh: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceName, plancheck.ResourceActionCreate),
+					},
+				},
 			},
 		},
 	})
