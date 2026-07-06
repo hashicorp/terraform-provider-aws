440497e39a766945e79f2e10f527a54be1058678
taruntej-a <tarunsraina483@gmail.com>
2026-07-06 22:56:10 +0530

    r/aws_codepipeline: make gen

--- internal/service/codepipeline/service_package_gen.go
+++ internal/service/codepipeline/service_package_gen.go
@@ -41,7 +41,11 @@ func (p *servicePackage) SDKResources(ctx context.Context) []*inttypes.ServicePa
 			Tags: unique.Make(inttypes.ServicePackageResourceTags{
 				IdentifierAttribute: names.AttrARN,
 			}),
-			Region: inttypes.ResourceRegionDefault(),
+			Region:   inttypes.ResourceRegionDefault(),
+			Identity: inttypes.RegionalSingleParameterIdentity(inttypes.StringIdentityAttribute(names.AttrName, true)),
+			Import: inttypes.SDKv2Import{
+				WrappedImport: true,
+			},
 		},
 		{
 			Factory:  resourceCustomActionType,
