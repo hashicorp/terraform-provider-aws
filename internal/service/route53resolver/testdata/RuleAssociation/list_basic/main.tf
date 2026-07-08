38271d05df2d4e8189e314551232a2e861efb1e3
Graham Davison <gdavison@hashicorp.com>
2026-07-08 14:56:34 -0700

    Fixes resource order in `basic` list configuration

--- internal/service/route53resolver/testdata/RuleAssociation/list_basic/main.tf
+++ internal/service/route53resolver/testdata/RuleAssociation/list_basic/main.tf
@@ -1,10 +1,12 @@
 # Copyright IBM Corp. 2014, 2026
 # SPDX-License-Identifier: MPL-2.0
 
-resource "aws_vpc" "test" {
-  cidr_block           = "10.6.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+resource "aws_route53_resolver_rule_association" "test" {
+  count = var.resource_count
+
+  name             = "${var.rName}-${count.index}"
+  resolver_rule_id = aws_route53_resolver_rule.test[count.index].id
+  vpc_id           = aws_vpc.test.id
 }
 
 resource "aws_route53_resolver_rule" "test" {
@@ -15,12 +17,10 @@ resource "aws_route53_resolver_rule" "test" {
   rule_type   = "SYSTEM"
 }
 
-resource "aws_route53_resolver_rule_association" "test" {
-  count = var.resource_count
-
-  name             = "${var.rName}-${count.index}"
-  resolver_rule_id = aws_route53_resolver_rule.test[count.index].id
-  vpc_id           = aws_vpc.test.id
+resource "aws_vpc" "test" {
+  cidr_block           = "10.6.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 }
 
 variable "rName" {
