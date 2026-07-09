1f89178a5cd65490e357c4a6a3dada14e38614ba
Graham Davison <gdavison@hashicorp.com>
2026-07-08 17:16:54 -0700

    Adds `includeResource` test

---
+++ internal/service/secretsmanager/testdata/SecretVersion/list_include_resource/main.tf
@@ -0,0 +1,25 @@
+# Copyright IBM Corp. 2014, 2026
+# SPDX-License-Identifier: MPL-2.0
+
+resource "aws_secretsmanager_secret_version" "test" {
+  count = var.resource_count
+
+  secret_id     = aws_secretsmanager_secret.test.id
+  secret_string = "test-string-${count.index}"
+}
+
+resource "aws_secretsmanager_secret" "test" {
+  name = var.rName
+}
+
+variable "rName" {
+  description = "Name for resource"
+  type        = string
+  nullable    = false
+}
+
+variable "resource_count" {
+  description = "Number of resources to create"
+  type        = number
+  nullable    = false
+}
