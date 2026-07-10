d7708027b296aae4adac8e0fa99c262747c68cba
Kit Ewbank <Kit_Ewbank@hotmail.com>
2026-07-10 12:40:44 -0400

    Add 'tfstringvalidator.ConflictsWithWhenNotEquals'.

---
+++ internal/framework/validators/stringvalidator/conflicts_with_when_not_equals.go
@@ -0,0 +1,29 @@
+// Copyright IBM Corp. 2014, 2026
+// SPDX-License-Identifier: MPL-2.0
+
+package stringvalidator
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-provider-aws/internal/framework/validators/internal"
+)
+
+// ConflictsWithWhenNotEquals checks that each path.Expression has a null
+// configuration value when the stringy attribute being validated does not have
+// the specified value.
+//
+// Relative path.Expressions are resolved using the attribute being
+// validated.
+func ConflictsWithWhenNotEquals[T ~string](value T, expressions ...path.Expression) validator.String {
+	return internal.ConflictsWithWhenValidator{
+		When: func(_ context.Context, v attr.Value) bool {
+			return !v.Equal(types.StringValue(string(value)))
+		},
+		PathExpressions: expressions,
+	}
+}
