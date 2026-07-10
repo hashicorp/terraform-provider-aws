04a32546244fe5fe037af2bc17ba6682ca8a09ec
Kit Ewbank <Kit_Ewbank@hotmail.com>
2026-07-10 11:47:59 -0400

    Add 'tfstringvalidator.ConflictsWithWhenEquals'.

---
+++ internal/framework/validators/internal/conflicts_with_when_equals.go
@@ -0,0 +1,106 @@
+// Copyright IBM Corp. 2014, 2026
+// SPDX-License-Identifier: MPL-2.0
+
+package internal
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-framework-validators/helpers/validatordiag"
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+)
+
+var (
+	_ validator.String = (*ConflictsWithWhenEqualsValidator)(nil)
+)
+
+type conflictsWithWhenEqualsValidatorRequest struct {
+	Config         tfsdk.Config
+	ConfigValue    attr.Value
+	Path           path.Path
+	PathExpression path.Expression
+}
+
+type conflictsWithWhenEqualsValidatorResponse struct {
+	Diagnostics diag.Diagnostics
+}
+
+type ConflictsWithWhenEqualsValidator struct {
+	Value           attr.Value
+	PathExpressions path.Expressions
+}
+
+func (v ConflictsWithWhenEqualsValidator) Description(ctx context.Context) string {
+	return v.MarkdownDescription(ctx)
+}
+
+func (v ConflictsWithWhenEqualsValidator) MarkdownDescription(ctx context.Context) string {
+	return fmt.Sprintf("Ensure that when this attribute equals %[1]q, the following are not also configured: %[2]q", v.Value, v.PathExpressions)
+}
+
+func (v ConflictsWithWhenEqualsValidator) ValidateString(ctx context.Context, request validator.StringRequest, response *validator.StringResponse) {
+	validateRequest := conflictsWithWhenEqualsValidatorRequest{
+		Config:         request.Config,
+		ConfigValue:    request.ConfigValue,
+		Path:           request.Path,
+		PathExpression: request.PathExpression,
+	}
+	var validateResponse conflictsWithWhenEqualsValidatorResponse
+
+	v.validate(ctx, validateRequest, &validateResponse)
+
+	response.Diagnostics.Append(validateResponse.Diagnostics...)
+}
+
+func (v ConflictsWithWhenEqualsValidator) validate(ctx context.Context, request conflictsWithWhenEqualsValidatorRequest, response *conflictsWithWhenEqualsValidatorResponse) {
+	if request.ConfigValue.IsNull() || request.ConfigValue.IsUnknown() {
+		return
+	}
+
+	if !request.ConfigValue.Equal(v.Value) {
+		return
+	}
+
+	var responseDiags diag.Diagnostics
+
+	for _, expression := range request.PathExpression.MergeExpressions(v.PathExpressions...) {
+		matchedPaths, diags := request.Config.PathMatches(ctx, expression)
+		response.Diagnostics.Append(diags...)
+		if diags.HasError() {
+			continue
+		}
+
+		for _, mp := range matchedPaths {
+			// Skip self.
+			if mp.Equal(request.Path) {
+				continue
+			}
+
+			var mpVal attr.Value
+			response.Diagnostics.Append(request.Config.GetAttribute(ctx, mp, &mpVal)...)
+			if response.Diagnostics.HasError() {
+				return
+			}
+
+			// Defer if any target is unknown; we cannot decide yet.
+			if mpVal.IsUnknown() {
+				return
+			}
+
+			if !mpVal.IsNull() {
+				// Collect all errors.
+				responseDiags.Append(validatordiag.InvalidAttributeCombinationDiagnostic(
+					request.Path,
+					fmt.Sprintf("Attribute %[1]q must not be configured when %[2]q is %[3]s", mp, request.Path, v.Value),
+				))
+			}
+		}
+	}
+
+	response.Diagnostics.Append(responseDiags...)
+}
---
+++ internal/framework/validators/stringvalidator/conflicts_with_when_equals.go
@@ -0,0 +1,24 @@
+// Copyright IBM Corp. 2014, 2026
+// SPDX-License-Identifier: MPL-2.0
+
+package stringvalidator
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-provider-aws/internal/framework/validators/internal"
+)
+
+// ConflictsWithWhenEquals checks that each path.Expression has a null
+// configuration value when the stringy attribute being validated has the known
+// specified value.
+//
+// Relative path.Expressions are resolved using the attribute being
+// validated.
+func ConflictsWithWhenEquals[T ~string](value T, expressions ...path.Expression) validator.String {
+	return internal.ConflictsWithWhenEqualsValidator{
+		Value:           types.StringValue(string(value)),
+		PathExpressions: expressions,
+	}
+}
