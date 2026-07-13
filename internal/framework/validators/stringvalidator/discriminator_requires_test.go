2a7898c253d7a38431003f88640bf0316c2b6e90
Kit Ewbank <Kit_Ewbank@hotmail.com>
2026-07-13 12:31:28 -0400

    Fix import-lint errors.

--- internal/framework/validators/stringvalidator/discriminator_requires_test.go
+++ internal/framework/validators/stringvalidator/discriminator_requires_test.go
@@ -11,7 +11,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-go/tftypes"
-
 	tfstringvalidator "github.com/hashicorp/terraform-provider-aws/internal/framework/validators/stringvalidator"
 )
 
