89b9a9dfc3e93e65441e4c42c470946c779ab659
Adrian Johnson <adrian.johnson@hashicorp.com>
2026-07-15 15:38:53 -0400

    add gh token to env

--- .teamcity/scripts/pullrequest_tests/test_results.sh
+++ .teamcity/scripts/pullrequest_tests/test_results.sh
@@ -2,7 +2,6 @@
 # Copyright IBM Corp. 2014, 2026
 # SPDX-License-Identifier: MPL-2.0
 
-sleep 10
 # Authenticate internally using TeamCity's system-provided tokens
 results=$(curl -s -u "%system.teamcity.auth.userId%:%system.teamcity.auth.password%" \
      -H "Accept: application/json" \
@@ -11,9 +10,9 @@ results=$(curl -s -u "%system.teamcity.auth.userId%:%system.teamcity.auth.passwo
 
 echo "${results}"
 
-# "%BRANCH_NAME%" is in the format "refs/pull/48516/merge"
-#pr_number="$(echo "%BRANCH_NAME%" | sed -E 's#refs/pull/([0-9]+)/merge#\1#')"
-#
-#body="$(printf '```console\n%s\n```' "${results}")"
-#
-#"%system.teamcity.build.checkoutDir%/tools/gh" pr comment "${pr_number}" --body "${body}"
+"%BRANCH_NAME%" is in the format "refs/pull/48516/merge"
+pr_number="$(echo "%BRANCH_NAME%" | sed -E 's#refs/pull/([0-9]+)/merge#\1#')"
+
+body="$(printf '```console\n%s\n```' "${results}")"
+
+"%system.teamcity.build.checkoutDir%/tools/gh" pr comment "${pr_number}" --body "${body}"
