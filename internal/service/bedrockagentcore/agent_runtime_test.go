649d721ea1d09f20a6af20fcc1641695c272f369
Kit Ewbank <Kit_Ewbank@hotmail.com>
2026-07-15 10:33:09 -0400

    Move 'TestCustomJWTAuthorizerPrivateEndpointRoundTrip'.

--- internal/service/bedrockagentcore/agent_runtime_test.go
+++ internal/service/bedrockagentcore/agent_runtime_test.go
@@ -10,8 +10,11 @@ import (
 	"testing"
 
 	"github.com/YakDriver/regexache"
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/bedrockagentcorecontrol"
 	awstypes "github.com/aws/aws-sdk-go-v2/service/bedrockagentcorecontrol/types"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
 	"github.com/hashicorp/terraform-plugin-testing/plancheck"
@@ -20,10 +23,69 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	tfknownvalue "github.com/hashicorp/terraform-provider-aws/internal/acctest/knownvalue"
+	fwflex "github.com/hashicorp/terraform-provider-aws/internal/framework/flex"
 	"github.com/hashicorp/terraform-provider-aws/internal/retry"
 	tfbedrockagentcore "github.com/hashicorp/terraform-provider-aws/internal/service/bedrockagentcore"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
+
+// TestCustomJWTAuthorizerPrivateEndpointRoundTrip proves that AutoFlex builds the
+// correct SDK payload for the private_endpoint / private_endpoint_overrides fields
+// (including the union nested inside an override list element) by round-tripping
+// SDK -> model -> SDK.
+func TestCustomJWTAuthorizerPrivateEndpointRoundTrip(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+
+	in := awstypes.CustomJWTAuthorizerConfiguration{
+		DiscoveryUrl:    aws.String("https://example.com/.well-known/openid-configuration"),
+		AllowedAudience: []string{"aud"},
+		PrivateEndpoint: &awstypes.PrivateEndpointMemberManagedVpcResource{
+			Value: awstypes.ManagedVpcResource{
+				VpcIdentifier:         aws.String("vpc-12345678"),
+				SubnetIds:             []string{"subnet-12345678", "subnet-abcdef01"},
+				EndpointIpAddressType: awstypes.EndpointIpAddressTypeIpv4,
+				SecurityGroupIds:      []string{"sg-12345678"},
+				RoutingDomain:         aws.String("example.com"),
+			},
+		},
+		PrivateEndpointOverrides: []awstypes.PrivateEndpointOverride{
+			{
+				Domain: aws.String("override.example.com"),
+				PrivateEndpoint: &awstypes.PrivateEndpointMemberSelfManagedLatticeResource{
+					Value: &awstypes.SelfManagedLatticeResourceMemberResourceConfigurationIdentifier{
+						Value: "rcfg-0123456789abcdef0",
+					},
+				},
+			},
+		},
+	}
+
+	var model tfbedrockagentcore.CustomJWTAuthorizerConfigurationModel
+	if diags := fwflex.Flatten(ctx, in, &model); diags.HasError() {
+		t.Fatalf("Flatten: %v", diags)
+	}
+
+	var out awstypes.CustomJWTAuthorizerConfiguration
+	if diags := fwflex.Expand(ctx, model, &out); diags.HasError() {
+		t.Fatalf("Expand: %v", diags)
+	}
+
+	opts := cmp.Options{
+		cmpopts.IgnoreUnexported(
+			awstypes.CustomJWTAuthorizerConfiguration{},
+			awstypes.ManagedVpcResource{},
+			awstypes.PrivateEndpointMemberManagedVpcResource{},
+			awstypes.PrivateEndpointMemberSelfManagedLatticeResource{},
+			awstypes.SelfManagedLatticeResourceMemberResourceConfigurationIdentifier{},
+			awstypes.PrivateEndpointOverride{},
+		),
+		cmpopts.SortSlices(func(a, b string) bool { return a < b }),
+	}
+	if diff := cmp.Diff(in, out, opts...); diff != "" {
+		t.Errorf("SDK -> model -> SDK round-trip mismatch (-in +out):\n%s", diff)
+	}
+}
 
 func testAccRandomAgentRuntimeName(t *testing.T) string {
 	return strings.ReplaceAll(acctest.RandomWithPrefix(t, acctest.ResourcePrefix), "-", "_")
