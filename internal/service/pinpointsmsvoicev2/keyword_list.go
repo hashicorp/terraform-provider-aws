d761080800cbbb3c3de4a7d964e9e84c85029b20
Josh Roose <54345520+joshRooz@users.noreply.github.com>
2026-07-15 12:03:05 -0700

    feat(pinpointsmsvoicev2): implement Keyword list resource

--- internal/service/pinpointsmsvoicev2/keyword_list.go
+++ internal/service/pinpointsmsvoicev2/keyword_list.go
@@ -2,65 +2,25 @@
 // SPDX-License-Identifier: MPL-2.0
 
 package pinpointsmsvoicev2
-// **PLEASE DELETE THIS AND ALL TIP COMMENTS BEFORE SUBMITTING A PR FOR REVIEW!**
-//
-// TIP: ==== INTRODUCTION ====
-// Thank you for trying the skaff tool!
-//
-// You have opted to include these helpful comments. They all include "TIP:"
-// to help you find and remove them when you're done with them.
-//
-// While some aspects of this file are customized to your input, the
-// scaffold tool does *not* look at the AWS API and ensure it has correct
-// function, structure, and variable names. It makes guesses based on
-// commonalities. You will need to make significant adjustments.
-//
-// In other words, as generated, this is a rough outline of the work you will
-// need to do. If something doesn't make sense for your situation, get rid of
-// it.
 
 import (
-	// TIP: ==== IMPORTS ====
-	// This is a common set of imports but not customized to your code since
-	// your code hasn't been written yet. Make sure you, your IDE, or
-	// goimports -w <file> fixes these imports.
-	//
-	// The provider linter wants your imports to be in two groups: first,
-	// standard library (i.e., "fmt" or "strings"), second, everything else.
-	//
-	// Also, AWS Go SDK v2 may handle nested structures differently than v1,
-	// using the services/pinpointsmsvoicev2/types package. If so, you'll
-	// need to import types and reference the nested types, e.g., as
-	// awstypes.<Type Name>.
 	"context"
-	"fmt"
-	"iter"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/pinpointsmsvoicev2"
 	awstypes "github.com/aws/aws-sdk-go-v2/service/pinpointsmsvoicev2/types"
 	"github.com/hashicorp/terraform-plugin-framework/list"
+	listschema "github.com/hashicorp/terraform-plugin-framework/list/schema"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/fwdiag"
 	"github.com/hashicorp/terraform-provider-aws/internal/framework"
-	"github.com/hashicorp/terraform-provider-aws/internal/logging"
-	"github.com/hashicorp/terraform-provider-aws/names"
 	fwflex "github.com/hashicorp/terraform-provider-aws/internal/framework/flex"
-	// listschema "github.com/hashicorp/terraform-plugin-framework/list/schema"
+	fwtypes "github.com/hashicorp/terraform-provider-aws/internal/framework/types"
+	"github.com/hashicorp/terraform-provider-aws/internal/logging"
+	"github.com/hashicorp/terraform-provider-aws/internal/smerr"
 )
 
-// TIP: ==== FILE STRUCTURE ====
-// All list resources should follow this basic outline. Improve this list resource's
-// maintainability by sticking to it.
-//
-// 1. Package declaration
-// 2. Imports
-// 3. Main data source struct with schema method
-// 4. Read method
-// 5. Other functions (flatteners, expanders, waiters, finders, etc.)
-
-// Function annotations are used for list resource registration to the Provider. DO NOT EDIT.
 // @FrameworkListResource("aws_pinpointsmsvoicev2_keyword")
 func newKeywordResourceAsListResource() list.ListResourceWithConfigure {
 	return &keywordListResource{}
@@ -72,36 +32,23 @@ type keywordListResource struct {
 	keywordResource
 	framework.WithList
 }
-// TIP: ==== LIST RESOURCE SCHEMA ===
-// This is only needed if the resource type requires any attributes for listing, such as a parent ID.
-// Otherwise, it can be removed.
-// func (l *keywordListResource) ListResourceConfigSchema(ctx context.Context, request list.ListResourceSchemaRequest, response *list.ListResourceSchemaResponse) {
-// 	response.Schema = listschema.Schema{
-// 		Attributes: map[string]listschema.Attribute{
-// 			"parent_id": listschema.StringAttribute{
-// 				Required:    true,
-// 				Description: "ID of the Parent to list Keywords from.",
-// 			},
-// 		},
-// 	}
-// }
+
+func (l *keywordListResource) ListResourceConfigSchema(ctx context.Context, request list.ListResourceSchemaRequest, response *list.ListResourceSchemaResponse) {
+	response.Schema = listschema.Schema{
+		Attributes: map[string]listschema.Attribute{
+			"origination_identity": listschema.StringAttribute{
+				Required:    true,
+				Description: "Origination identity to list keywords for. Value is the ID or ARN of a phone number, pool, or sender ID.",
+			},
+		},
+		Blocks: map[string]listschema.Block{},
+	}
+}
 
 func (l *keywordListResource) List(ctx context.Context, request list.ListRequest, stream *list.ListResultsStream) {
-	// TIP: ==== LIST RESOURCE LIST ====
-	// Generally, the List function should do the following things. Make
-	// sure there is a good reason if you don't do one of these.
-	//
-	// 1. Get a client connection to the relevant service
-	// 2. Fetch the config
-	// 3. Get information about a resource from AWS
-	// 4. Set the ID, arguments, and attributes
-	// 5. Set the tags
-	// 6. Set the state
-	// TIP: -- 1. Get a client connection to the relevant service
 	conn := l.Meta().PinpointSMSVoiceV2Client(ctx)
-	
-	// TIP: -- 2. Fetch the config
-	var query listKeywordModel
+
+	var query keywordListModel
 	if request.Config.Raw.IsKnown() && !request.Config.Raw.IsNull() {
 		if diags := request.Config.Get(ctx, &query); diags.HasError() {
 			stream.Results = list.ListResultsStreamDiagnostics(diags)
@@ -109,45 +56,47 @@ func (l *keywordListResource) List(ctx context.Context, request list.ListRequest
 		}
 	}
 
-	
-	// TIP: -- 3. Retrieve required attributes
-	// If the resource type requires any attributes for listing, such as a parent ID, retrieve them here.
-	parentID := query.ParentID.ValueString()
+	input := pinpointsmsvoicev2.DescribeKeywordsInput{
+		OriginationIdentity: query.OriginationIdentity.ValueStringPointer(),
+	}
 
-	tflog.Info(ctx, "Listing Resources", map[string]any{
-		logging.ResourceAttributeKey("parent_id"): parentID,
-	})
-	
-	// TIP: -- 4. Get information about a resource from AWS
+	tflog.Info(ctx, "Listing End User Messaging SMS Keywords")
+
 	stream.Results = func(yield func(list.ListResult) bool) {
-		input := pinpointsmsvoicev2.ListKeywordsInput{
-			ParentId: parentID,
+		keywords, originationIdentityARN, err := findKeywords(ctx, conn, &input)
+		if err != nil {
+			result := fwdiag.NewListResultErrorDiagnostic(err)
+			yield(result)
+			return
 		}
-		for item, err := range listKeywords(ctx, conn, &input) {
-			if err != nil {
-				result := fwdiag.NewListResultErrorDiagnostic(err)
-				yield(result)
-				return
-			}
-			// TIP: -- 5. Set identifying attributes for logging
-			// Set one or more logging fields with attributes that will identify the resource.
-			// Typically, these will be the attributes used in the Resource Identity
-			arn := aws.ToString(item.KeywordArn)
-			ctx := tflog.SetField(ctx, logging.ResourceAttributeKey(names.AttrARN), arn)
+
+		for i := range keywords {
+			item := &keywords[i]
+			keyword := aws.ToString(item.Keyword)
+
+			ctx := tflog.SetField(ctx, logging.ResourceAttributeKey("origination_identity"), query.OriginationIdentity.ValueString())
+			ctx = tflog.SetField(ctx, logging.ResourceAttributeKey("keyword"), keyword)
 
 			result := request.NewListResult(ctx)
-			
+
 			var data keywordResourceModel
-			// TIP: -- 6. Set the ID, arguments, and attributes
-			// Using a field name prefix allows mapping fields such as `KeywordId` to `ID`
 			l.SetResult(ctx, l.Meta(), request.IncludeResource, &data, &result, func() {
-				result.Diagnostics.Append(l.flatten(ctx, &item, &data)...)
+				smerr.AddEnrich(ctx, &result.Diagnostics, fwflex.Flatten(ctx, item, &data))
 				if result.Diagnostics.HasError() {
 					return
 				}
 
-				// TIP: -- 7. Set the display name
-				result.DisplayName = aws.ToString(item.KeywordName)
+				data.OriginationIdentity = query.OriginationIdentity
+				data.OriginationIdentityARN = fwtypes.ARNValue(aws.ToString(originationIdentityARN))
+
+				// keyword_action is AWS-managed for the mandatory keywords HELP and STOP
+				// and cannot be set (see the resource ValidateConfig). Null it so any
+				// generated configuration omits it and remains valid on apply.
+				if isMandatoryKeyword(keyword) {
+					data.KeywordAction = fwtypes.StringEnumNull[awstypes.KeywordAction]()
+				}
+
+				result.DisplayName = keyword
 			})
 
 			if !yield(result) {
@@ -157,57 +106,7 @@ func (l *keywordListResource) List(ctx context.Context, request list.ListRequest
 	}
 }
 
-// TIP: ==== DATA STRUCTURES ====
-// With Terraform Plugin-Framework configurations are deserialized into
-// Go types, providing type safety without the need for type assertions.
-// These structs should match the schema definition exactly, and the `tfsdk`
-// tag value should match the attribute name.
-//
-// Nested objects are represented in their own data struct. These will
-// also have a corresponding attribute type mapping for use inside flex
-// functions.
-//
-// See more:
-// https://developer.hashicorp.com/terraform/plugin/framework/handling-data/accessing-values
-type listKeywordModel struct {
+type keywordListModel struct {
 	framework.WithRegionModel
-	// TIP: -- 1. Include required attributes
-	// If the resource type requires any attributes for listing, such as a parent ID, include them here.
-	ParentID types.String `tfsdk:"parent_id"`
+	OriginationIdentity types.String `tfsdk:"origination_identity"`
 }
-
-// TIP: ==== LISTING FUNCTION ====
-// This listing function is written using an iterator pattern to handle pagination
-func listKeywords(ctx context.Context, conn *pinpointsmsvoicev2.Client, input *pinpointsmsvoicev2.ListKeywordsInput) iter.Seq2[awstypes.Keyword, error] {
-	return func(yield func(awstypes.Keyword, error) bool) {
-		pages := pinpointsmsvoicev2.NewListKeywordsPaginator(conn, input)
-		for pages.HasMorePages() {
-			page, err := pages.NextPage(ctx)
-			if err != nil {
-				yield(awstypes.Keyword{}, fmt.Errorf("listing End User Messaging SMS Keyword resources: %w", err))
-				return
-			}
-
-			for _, item := range page.Keywords {
-				if !yield(item, nil) {
-					return
-				}
-			}
-		}
-	}
-}
-
-// TIP: ==== RESOURCE FLATTENING FUNCTION ====
-// This function should be placed in the resource type's source file ("keyword.go"). It may already be present.
-// It is intended to perform the flattening of the results of the API call or calls used to populate a resource's values.
-// It should replace the flattening portion of the resource type's Read function (`keywordResource.Read`) and take the API results
-// as parameters.
-// The replaced section of the Read function should be
-//	response.Diagnostics.Append(r.flatten(ctx, output, &data)...)
-//	if response.Diagnostics.HasError() {
-//		return
-//	}
-// func (r *keywordResource) flatten(ctx context.Context, keyword *awstypes.Keyword, data *keywordResourceModel) (diags diag.Diagnostics) {
-// 	diags.Append(fwflex.Flatten(ctx, keyword, data)...)
-// 	return diags
-// }
