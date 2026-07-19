fb938cee67b4db5fd89c5bf5e77f1052154efc5f
Kit Ewbank <Kit_Ewbank@hotmail.com>
2026-07-19 17:24:37 -0400

    Revert "Potential fix for pull request finding"
    This reverts commit 5c9139e222276e6cba4e8d53f62da952f1bce187.

--- internal/service/ec2/vpc_flow_log.go
+++ internal/service/ec2/vpc_flow_log.go
@@ -140,7 +140,7 @@ func resourceFlowLog() *schema.Resource {
 								Type:             schema.TypeString,
 								Required:         true,
 								ForceNew:         true,
-							ValidateDiagFunc: validation.ToDiagFunc(validation.StringInSlice([]string{"instance", "network-interface", "auto-scaling-group"}, false)),
+								ValidateDiagFunc: enum.Validate[awstypes.TaggableResourceType](),
 							},
 							"tag_keys": {
 								Type:     schema.TypeList,
