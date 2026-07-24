f80315360c27445a53abbbd0c780f4f1812162d8
Graham Davison <gdavison@hashicorp.com>
2026-07-24 13:26:26 -0700

    Adds Resource Identity tests for `aws_osis_pipeline_resource_policy`

--- internal/service/osis/pipeline_resource_policy.go
+++ internal/service/osis/pipeline_resource_policy.go
@@ -26,7 +26,7 @@ import (
 // @FrameworkResource("aws_osis_pipeline_resource_policy", name="Pipeline Resource Policy")
 // @ArnIdentity("resource_arn")
 // @Testing(hasNoPreExistingResource=true)
-// @Testing(identityTest=false)
+// @Testing(generator="randomPipelineName(t)")
 func newPipelineResourcePolicyResource(_ context.Context) (resource.ResourceWithConfigure, error) {
 	r := &pipelineResourcePolicyResource{}
 
