144296367afed26526eba7dc2c05480dadb9efe2
sergiopicca <s.picca96@gmail.com>
2026-07-28 23:42:28 +0200

    feat(taxsettings): Remove scaffolding comments and NoOpDelete

--- internal/service/taxsettings/tax_inheritance.go
+++ internal/service/taxsettings/tax_inheritance.go
@@ -5,36 +5,7 @@
 
 package taxsettings
 
-// **PLEASE DELETE THIS AND ALL TIP COMMENTS BEFORE SUBMITTING A PR FOR REVIEW!**
-//
-// TIP: ==== INTRODUCTION ====
-// Thank you for trying the skaff tool!
-//
-// You have opted to include these helpful comments. They all include "TIP:"
-// to help you find and remove them when you're done with them.
-//
-// While some aspects of this file are customized to your input, the
-// scaffold tool does *not* look at the AWS API and ensure it has correct
-// function, structure, and variable names. It makes guesses based on
-// commonalities. You will need to make significant adjustments.
-//
-// In other words, as generated, this is a rough outline of the work you will
-// need to do. If something doesn't make sense for your situation, get rid of
-// it.
-
 import (
-	// TIP: ==== IMPORTS ====
-	// This is a common set of imports but not customized to your code since
-	// your code hasn't been written yet. Make sure you, your IDE, or
-	// goimports -w <file> fixes these imports.
-	//
-	// The provider linter wants your imports to be in two groups: first,
-	// standard library (i.e., "fmt" or "strings"), second, everything else.
-	//
-	// Also, AWS Go SDK v2 may handle nested structures differently than v1,
-	// using the services/taxsettings/types package. If so, you'll
-	// need to import types and reference the nested types, e.g., as
-	// awstypes.<Type Name>.
 	"context"
 	"time"
 
@@ -58,42 +29,14 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
-// TIP: ==== FILE STRUCTURE ====
-// All resources should follow this basic outline. Improve this resource's
-// maintainability by sticking to it.
-//
-// 1. Package declaration
-// 2. Imports
-// 3. Main resource struct with schema method
-// 4. Create, read, update, delete methods (in that order)
-// 5. Other functions (flatteners, expanders, waiters, finders, etc.)
-
-// Function annotations are used for resource registration to the Provider. DO NOT EDIT.
 // @FrameworkResource("aws_taxsettings_tax_inheritance", name="Tax Inheritance")
 // @Region(global=true)
 // @SingletonIdentity
-//
-// TIP: ==== GENERATED ACCEPTANCE TESTS ====
-// Resource Identity and tagging make use of automatically generated acceptance tests.
-// For more information about automatically generated acceptance tests, see
-// https://hashicorp.github.io/terraform-provider-aws/acc-test-generation/
-//
-// Some common annotations are included below:
-// @Testing(existsType="github.com/aws/aws-sdk-go-v2/service/taxsettings;taxsettings.DescribeTaxInheritanceResponse")
-// @Testing(preCheck="testAccPreCheck")
-// @Testing(importIgnore="...;...")
-// @Testing(hasNoPreExistingResource=true)
 func newTaxInheritanceResource(_ context.Context) (resource.ResourceWithConfigure, error) {
 	r := &taxInheritanceResource{}
 
-	// TIP: ==== CONFIGURABLE TIMEOUTS ====
-	// Users can configure timeout lengths but you need to use the times they
-	// provide. Access the timeout they configure (or the defaults) using,
-	// e.g., r.CreateTimeout(ctx, plan.Timeouts) (see below). The times here are
-	// the defaults if they don't configure timeouts.
-	r.SetDefaultCreateTimeout(30 * time.Minute)
-	r.SetDefaultUpdateTimeout(30 * time.Minute)
-	r.SetDefaultDeleteTimeout(30 * time.Minute)
+	r.SetDefaultCreateTimeout(10 * time.Minute)
+	r.SetDefaultUpdateTimeout(10 * time.Minute)
 
 	return r, nil
 }
@@ -106,52 +49,9 @@ type taxInheritanceResource struct {
 	framework.ResourceWithModel[taxInheritanceResourceModel]
 	framework.WithTimeouts
 	framework.WithImportByIdentity
+	framework.WithNoOpDelete
 }
 
-// TIP: ==== SCHEMA ====
-// In the schema, add each of the attributes in snake case (e.g.,
-// delete_automated_backups).
-//
-// Formatting rules:
-// * Alphabetize attributes to make them easier to find.
-// * Do not add a blank line between attributes.
-//
-// Attribute basics:
-//   - If a user can provide a value ("configure a value") for an
-//     attribute (e.g., instances = 5), we call the attribute an
-//     "argument."
-//   - You change the way users interact with attributes using:
-//   - Required
-//   - Optional
-//   - Computed
-//   - There are only four valid combinations:
-//
-// 1. Required only - the user must provide a value
-// Required: true,
-//
-//  2. Optional only - the user can configure or omit a value; do not
-//     use Default or DefaultFunc
-//
-// Optional: true,
-//
-//  3. Computed only - the provider can provide a value but the user
-//     cannot, i.e., read-only
-//
-// Computed: true,
-//
-//  4. Optional AND Computed - the provider or user can provide a value;
-//     use this combination if you are using Default
-//
-// Optional: true,
-// Computed: true,
-//
-// You will typically find arguments in the input struct
-// (e.g., CreateDBInstanceInput) for the create operation. Sometimes
-// they are only in the input struct (e.g., ModifyDBInstanceInput) for
-// the modify operation.
-//
-// For more about schema options, visit
-// https://developer.hashicorp.com/terraform/plugin/framework/handling-data/schemas?page=schemas
 func (r *taxInheritanceResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
 	resp.Schema = schema.Schema{
 		Attributes: map[string]schema.Attribute{
@@ -163,30 +63,12 @@ func (r *taxInheritanceResource) Schema(ctx context.Context, req resource.Schema
 			names.AttrTimeouts: timeouts.Block(ctx, timeouts.Opts{
 				Create: true,
 				Update: true,
-				Delete: true,
 			}),
 		},
 	}
 }
 
 func (r *taxInheritanceResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
-	// TIP: ==== RESOURCE CREATE ====
-	// Generally, the Create function should do the following things. Make
-	// sure there is a good reason if you don't do one of these.
-	//
-	// 1. Get a client connection to the relevant service
-	// 2. Fetch the plan
-	// 3. Populate a create input structure
-	// 4. Call the AWS create/put function
-	// 5. Using the output from the create function, set the minimum arguments
-	//    and attributes for the Read function to work, as well as any computed
-	//    only attributes.
-	// 6. Use a waiter to wait for create to complete
-	// 7. Save the request plan to response state
-
-	// TIP: -- 1. Get a client connection to the relevant service
-
-	// TIP: -- 2. Fetch the plan
 	var plan taxInheritanceResourceModel
 	smerr.AddEnrich(ctx, &resp.Diagnostics, req.Plan.Get(ctx, &plan))
 	if resp.Diagnostics.HasError() {
@@ -198,36 +80,19 @@ func (r *taxInheritanceResource) Create(ctx context.Context, req resource.Create
 		return
 	}
 
-	// TIP: -- 7. Save the request plan to response state
 	smerr.AddEnrich(ctx, &resp.Diagnostics, resp.State.Set(ctx, plan))
 }
 
 func (r *taxInheritanceResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
-	// TIP: ==== RESOURCE READ ====
-	// Generally, the Read function should do the following things. Make
-	// sure there is a good reason if you don't do one of these.
-	//
-	// 1. Get a client connection to the relevant service
-	// 2. Fetch the state
-	// 3. Get the resource from AWS
-	// 4. Remove resource from state if it is not found
-	// 5. Set the arguments and attributes
-	// 6. Set the state
-
-	// TIP: -- 1. Get a client connection to the relevant service
 	conn := r.Meta().TaxSettingsClient(ctx)
 
-	// TIP: -- 2. Fetch the state
 	var state taxInheritanceResourceModel
 	smerr.AddEnrich(ctx, &resp.Diagnostics, req.State.Get(ctx, &state))
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// TIP: -- 3. Get the resource from AWS using an API Get, List, or Describe-
-	// type function, or, better yet, using a finder.
-	heritageStatus, err := getTaxInheritanceHeritageStatus(ctx, conn)
-	// TIP: -- 4. Remove resource from state if it is not found
+	heritageStatus, err := findTaxInheritanceHeritageStatus(ctx, conn)
 	if retry.NotFound(err) {
 		resp.Diagnostics.Append(fwdiag.NewResourceNotFoundWarningDiagnostic(err))
 		resp.State.RemoveResource(ctx)
@@ -238,38 +103,12 @@ func (r *taxInheritanceResource) Read(ctx context.Context, req resource.ReadRequ
 		return
 	}
 
-	// TIP: -- 5. Set the arguments and attributes
-	smerr.AddEnrich(ctx, &resp.Diagnostics, flex.Flatten(ctx, heritageStatus, &state))
-	if resp.Diagnostics.HasError() {
-		return
-	}
+	state.HeritageStatus = types.StringValue(string(*heritageStatus))
 
-	// TIP: -- 6. Set the state
 	smerr.AddEnrich(ctx, &resp.Diagnostics, resp.State.Set(ctx, &state))
 }
 
 func (r *taxInheritanceResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
-	// TIP: ==== RESOURCE UPDATE ====
-	// Not all resources have Update functions. There are a few reasons:
-	// a. The AWS API does not support changing a resource
-	// b. All arguments have RequiresReplace() plan modifiers
-	// c. The AWS API uses a create call to modify an existing resource
-	//
-	// In the cases of a. and b., the resource will not have an update method
-	// defined. In the case of c., Update and Create can be refactored to call
-	// the same underlying function.
-	//
-	// The rest of the time, there should be an Update function and it should
-	// do the following things. Make sure there is a good reason if you don't
-	// do one of these.
-	//
-	// 1. Get a client connection to the relevant service
-	// 2. Fetch the plan and state
-	// 3. Populate a modify input structure and check for changes
-	// 4. Call the AWS modify/update function
-	// 5. Use a waiter to wait for update to complete
-	// 6. Save the request plan to response state
-	// TIP: -- 1. Get a client connection to the relevant service
 	var plan taxInheritanceResourceModel
 	smerr.AddEnrich(ctx, &resp.Diagnostics, req.Plan.Get(ctx, &plan))
 	if resp.Diagnostics.HasError() {
@@ -281,56 +120,70 @@ func (r *taxInheritanceResource) Update(ctx context.Context, req resource.Update
 		return
 	}
 
-	// TIP: -- 7. Save the request plan to response state
 	smerr.AddEnrich(ctx, &resp.Diagnostics, resp.State.Set(ctx, plan))
 }
 
-func (r *taxInheritanceResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
-	// TIP: ==== RESOURCE DELETE ====
-	// Most resources have Delete functions. There are rare situations
-	// where you might not need a delete:
-	// a. The AWS API does not provide a way to delete the resource
-	// b. The point of your resource is to perform an action (e.g., reboot a
-	//    server) and deleting serves no purpose.
-	//
-	// The Delete function should do the following things. Make sure there
-	// is a good reason if you don't do one of these.
-	//
-	// 1. Get a client connection to the relevant service
-	// 2. Fetch the state
-	// 3. Populate a delete input structure
-	// 4. Call the AWS delete function
-	// 5. Use a waiter to wait for delete to complete
-	// TIP: -- 1. Get a client connection to the relevant service
-	var plan taxInheritanceResourceModel
-	smerr.AddEnrich(ctx, &resp.Diagnostics, req.State.Get(ctx, &plan))
-	plan.HeritageStatus = types.StringValue(string(awstypes.HeritageStatusOptOut))
-	if resp.Diagnostics.HasError() {
-		return
+func (r *taxInheritanceResource) putTaxInheritanceHeritageStatus(ctx context.Context, data *taxInheritanceResourceModel) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	conn := r.Meta().TaxSettingsClient(ctx)
+
+	var input taxsettings.PutTaxInheritanceInput
+	diags.Append(flex.Expand(ctx, data, &input)...)
+	if diags.HasError() {
+		return diags
 	}
 
-	resp.Diagnostics.Append(r.putTaxInheritanceHeritageStatus(ctx, &plan)...)
-	if resp.Diagnostics.HasError() {
-		return
+	// A read check is introduced to avoid unnecessary updates, i.e. when
+	// the desired heritage status matches the remote.
+	heritageStatus, err := findTaxInheritanceHeritageStatus(ctx, conn)
+	if err != nil {
+		diags.AddError("put heritage status for tax inheritance", err.Error())
+		return diags
 	}
 
-	// TIP: -- 7. Save the request plan to response state
-	smerr.AddEnrich(ctx, &resp.Diagnostics, resp.State.Set(ctx, plan))
+	if input.HeritageStatus == *heritageStatus {
+		return diags
+	}
+
+	// The heritage status can only be updated once within 15 minutes. The API returns a conflict exception
+	// (409 http status code) if this constraint is not respected.
+	_, err = conn.PutTaxInheritance(ctx, &input)
+	if err != nil {
+		diags.AddError("put heritage status for tax inheritance", err.Error())
+		return diags
+	}
+
+	createTimeout := r.CreateTimeout(ctx, data.Timeouts)
+	_, err = waitHeritageStatusUpdated(ctx, conn, data.HeritageStatus.ValueString(), createTimeout)
+	if err != nil {
+		diags.AddError("waiting heritage status update for tax inheritance", err.Error())
+		return diags
+	}
+
+	return diags
 }
 
-// TIP: ==== TERRAFORM IMPORTING ====
-// The built-in import function, and Import ID Handler, if any, should handle populating the required
-// attributes from the Import ID or Resource Identity.
-// In some cases, additional attributes must be set when importing.
-// Adding a custom ImportState function can handle those.
-//
-// See more:
-// https://hashicorp.github.io/terraform-provider-aws/add-resource-identity-support/
-// func (r *taxInheritanceResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
-// 	r.WithImportByIdentity.ImportState(ctx, req, resp)
-//
-// 	// Set needed attribute values here
-// }
+func findTaxInheritanceHeritageStatus(ctx context.Context, conn *taxsettings.Client) (*awstypes.HeritageStatus, error) {
+	input := taxsettings.GetTaxInheritanceInput{}
+
+	out, err := conn.GetTaxInheritance(ctx, &input)
+	if err != nil {
+		if errs.IsA[*awstypes.ResourceNotFoundException](err) {
+			return nil, smarterr.NewError(&retry.NotFoundError{
+				LastError: err,
+			})
+		}
+
+		return nil, smarterr.NewError(err)
+	}
+
+	if out == nil || out.HeritageStatus == "" {
+		return nil, smarterr.NewError(tfresource.NewEmptyResultError())
+	}
+
+	return &out.HeritageStatus, nil
+}
 
 func waitHeritageStatusUpdated(ctx context.Context, conn *taxsettings.Client, expectedHeritageStatus string, timeout time.Duration) (*awstypes.HeritageStatus, error) {
 	stateConf := &retry.StateChangeConf{
@@ -352,7 +205,7 @@ func waitHeritageStatusUpdated(ctx context.Context, conn *taxsettings.Client, ex
 
 func statusTaxInheritance(conn *taxsettings.Client) retry.StateRefreshFunc {
 	return func(ctx context.Context) (any, string, error) {
-		out, err := getTaxInheritanceHeritageStatus(ctx, conn)
+		out, err := findTaxInheritanceHeritageStatus(ctx, conn)
 		if retry.NotFound(err) {
 			return nil, "", nil
 		}
@@ -365,71 +218,6 @@ func statusTaxInheritance(conn *taxsettings.Client) retry.StateRefreshFunc {
 	}
 }
 
-func (r *taxInheritanceResource) putTaxInheritanceHeritageStatus(ctx context.Context, data *taxInheritanceResourceModel) diag.Diagnostics {
-	var diags diag.Diagnostics
-
-	conn := r.Meta().TaxSettingsClient(ctx)
-
-	var input taxsettings.PutTaxInheritanceInput
-	diags.Append(flex.Expand(ctx, data, &input)...)
-	if diags.HasError() {
-		return diags
-	}
-
-	_, err := conn.PutTaxInheritance(ctx, &input)
-	if err != nil {
-		diags.AddError("put heritage status for tax inheritance", err.Error())
-		return diags
-	}
-
-	createTimeout := r.CreateTimeout(ctx, data.Timeouts)
-	_, err = waitHeritageStatusUpdated(ctx, conn, data.HeritageStatus.String(), createTimeout)
-	if err != nil {
-		diags.AddError("waiting heritage status update for tax inheritance", err.Error())
-		return diags
-	}
-
-	return diags
-}
-
-// TIP: ==== FINDERS ====
-// The find function is not strictly necessary. You could do the API
-// request from the status function. However, we have found that find often
-// comes in handy in other places besides the status function. As a result, it
-// is good practice to define it separately.
-func getTaxInheritanceHeritageStatus(ctx context.Context, conn *taxsettings.Client) (*awstypes.HeritageStatus, error) {
-	input := taxsettings.GetTaxInheritanceInput{}
-
-	out, err := conn.GetTaxInheritance(ctx, &input)
-	if err != nil {
-		if errs.IsA[*awstypes.ResourceNotFoundException](err) {
-			return nil, smarterr.NewError(&retry.NotFoundError{
-				LastError: err,
-			})
-		}
-
-		return nil, smarterr.NewError(err)
-	}
-
-	if out == nil || out.HeritageStatus == "" {
-		return nil, smarterr.NewError(tfresource.NewEmptyResultError())
-	}
-
-	return &out.HeritageStatus, nil
-}
-
-// TIP: ==== DATA STRUCTURES ====
-// With Terraform Plugin-Framework configurations are deserialized into
-// Go types, providing type safety without the need for type assertions.
-// These structs should match the schema definition exactly, and the `tfsdk`
-// tag value should match the attribute name.
-//
-// Nested objects are represented in their own data struct. These will
-// also have a corresponding attribute type mapping for use inside flex
-// functions.
-//
-// See more:
-// https://developer.hashicorp.com/terraform/plugin/framework/handling-data/accessing-values
 type taxInheritanceResourceModel struct {
 	HeritageStatus types.String   `tfsdk:"heritage_status"`
 	Timeouts       timeouts.Value `tfsdk:"timeouts"`
