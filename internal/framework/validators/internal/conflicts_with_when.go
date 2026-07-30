8acd6601b6dd3f2b36c348f9f046ceb5b22f8152
Kit Ewbank <Kit_Ewbank@hotmail.com>
2026-07-30 10:19:59 -0400

    'whenValidator' -> 'allOfWhenValidator'.

--- internal/framework/validators/internal/conflicts_with_when.go
+++ internal/framework/validators/internal/conflicts_with_when.go
@@ -19,14 +19,14 @@ var (
 )
 
 func ConflictsWithWhenValidator(when When, expressions ...path.Expression) conflictsWithWhenValidator {
-	return conflictsWithWhenValidator{whenValidator{
+	return conflictsWithWhenValidator{allOfWhenValidator{
 		when:            when,
 		pathExpressions: expressions,
 	}}
 }
 
 type conflictsWithWhenValidator struct {
-	whenValidator
+	allOfWhenValidator
 }
 
 func (v conflictsWithWhenValidator) Description(ctx context.Context) string {
@@ -52,7 +52,7 @@ func (v conflictsWithWhenValidator) ValidateString(ctx context.Context, request 
 }
 
 func (v conflictsWithWhenValidator) validate(ctx context.Context, request ValidatorRequest, response *ValidatorResponse) {
-	v.whenValidator.validate(ctx, request, response, v.eval)
+	v.allOfWhenValidator.validate(ctx, request, response, v.eval)
 }
 
 func (v conflictsWithWhenValidator) eval(_ context.Context, requestPath path.Path, matchedPath path.Path, matchedValue attr.Value) diag.Diagnostics {
