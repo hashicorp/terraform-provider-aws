7f99925c0690bede360444ba222743bfd4b086cc
raystrand <raman.shrestha@ibm.com>
2026-08-04 16:55:32 -0500

    needs the depends_on

--- internal/service/rds/snapshots_data_source_test.go
+++ internal/service/rds/snapshots_data_source_test.go
@@ -8,6 +8,9 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
@@ -20,7 +23,6 @@ func TestAccRDSSnapshotsDataSource_basic(t *testing.T) {
 
 	rName := acctest.RandomWithPrefix(t, acctest.ResourcePrefix)
 	dataSourceName := "data.aws_rds_snapshots.test"
-	resourceName := "aws_db_snapshot.test"
 
 	acctest.ParallelTest(ctx, t, resource.TestCase{
 		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
@@ -30,16 +32,20 @@ func TestAccRDSSnapshotsDataSource_basic(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testAccSnapshotsDataSourceConfig_basic(rName),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr(dataSourceName, "snapshots.#", "1"),
-					resource.TestCheckResourceAttrPair(dataSourceName, "snapshots.0.db_instance_identifier", resourceName, "db_instance_identifier"),
-					resource.TestCheckResourceAttrPair(dataSourceName, "snapshots.0.db_snapshot_identifier", resourceName, "db_snapshot_identifier"),
-					resource.TestCheckResourceAttrPair(dataSourceName, "snapshots.0.db_snapshot_arn", resourceName, "db_snapshot_arn"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "snapshots.0.snapshot_create_time"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "snapshots.0.status"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "snapshots.0.engine"),
-					resource.TestCheckResourceAttr(dataSourceName, "snapshots.0.tags.Name", rName),
-				),
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(dataSourceName, tfjsonpath.New("snapshots"), knownvalue.ListExact([]knownvalue.Check{
+						knownvalue.ObjectPartial(map[string]knownvalue.Check{
+							"db_instance_identifier": knownvalue.StringExact(rName),
+							"db_snapshot_identifier": knownvalue.StringExact(rName),
+							"snapshot_create_time":   knownvalue.NotNull(),
+							"status":                 knownvalue.NotNull(),
+							"engine":                 knownvalue.NotNull(),
+							names.AttrTags: knownvalue.MapExact(map[string]knownvalue.Check{
+								"Name": knownvalue.StringExact(rName),
+							}),
+						}),
+					})),
+				},
 			},
 		},
 	})
@@ -101,6 +107,8 @@ resource "aws_db_snapshot" "test" {
 resource "aws_db_snapshot" "wrong" {
   db_instance_identifier = aws_db_instance.test.identifier
   db_snapshot_identifier = "%[1]s-wrong"
+
+  depends_on = [aws_db_snapshot.test]
 }
 
 data "aws_rds_snapshots" "test" {
