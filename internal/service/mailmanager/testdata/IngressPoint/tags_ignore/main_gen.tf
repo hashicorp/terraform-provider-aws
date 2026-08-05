2a10786fa687be58f27780501d1be778adfd6c2b
taruntej-a <tarunsraina483@gmail.com>
2026-08-06 03:30:58 +0530

    r/aws_mailmanager_ingress_point: make gen

---
+++ internal/service/mailmanager/testdata/IngressPoint/tags_ignore/main_gen.tf
@@ -0,0 +1,68 @@
+# Copyright IBM Corp. 2014, 2026
+# SPDX-License-Identifier: MPL-2.0
+
+provider "aws" {
+  default_tags {
+    tags = var.provider_tags
+  }
+  ignore_tags {
+    keys = var.ignore_tag_keys
+  }
+}
+
+resource "aws_mailmanager_ingress_point" "test" {
+  name              = var.rName
+  type              = "OPEN"
+  rule_set_id       = aws_mailmanager_rule_set.test.id
+  traffic_policy_id = aws_mailmanager_traffic_policy.test.id
+
+  tags = var.resource_tags
+}
+
+resource "aws_mailmanager_traffic_policy" "test" {
+  default_action = "ALLOW"
+  name           = var.rName
+
+  policy_statement {
+    action = "DENY"
+
+    condition {
+      ip_expression {
+        operator = "CIDR_MATCHES"
+        values   = ["192.0.2.0/24"]
+
+        evaluate {
+          attribute = "SENDER_IP"
+        }
+      }
+    }
+  }
+}
+
+resource "aws_mailmanager_rule_set" "test" {
+  name = var.rName
+}
+
+variable "rName" {
+  description = "Name for resource"
+  type        = string
+  nullable    = false
+}
+
+variable "resource_tags" {
+  description = "Tags to set on resource. To specify no tags, set to `null`"
+  # Not setting a default, so that this must explicitly be set to `null` to specify no tags
+  type     = map(string)
+  nullable = true
+}
+
+variable "provider_tags" {
+  type     = map(string)
+  nullable = true
+  default  = null
+}
+
+variable "ignore_tag_keys" {
+  type     = set(string)
+  nullable = false
+}
