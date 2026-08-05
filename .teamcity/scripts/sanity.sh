d908c3e59136d8ef81f3666182fa952e0f0c4690
Graham Davison <gdavison@hashicorp.com>
2026-08-05 14:51:42 -0700

    Adds `shellcheck` exclusions for `%ACCTEST_ROLE_ARN%`

--- .teamcity/scripts/sanity.sh
+++ .teamcity/scripts/sanity.sh
@@ -4,6 +4,7 @@
 
 set -euo pipefail
 
+# shellcheck disable=2157 # This isn't a constant string, it's a TeamCity variable substitution
 if [[ -n "%ACCTEST_ROLE_ARN%" ]]; then
     conf=$(pwd)/aws.conf
 
