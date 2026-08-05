4686a2f5ff67abbcc802fda718d9e63b16101689
taruntej-a <tarunsraina483@gmail.com>
2026-08-05 17:55:31 +0530

    r/aws_mailmanager_ingress_point: make gen

--- internal/service/mailmanager/service_package_gen.go
+++ internal/service/mailmanager/service_package_gen.go
@@ -29,6 +29,19 @@ func (p *servicePackage) FrameworkDataSources(ctx context.Context) []*inttypes.S
 func (p *servicePackage) FrameworkResources(ctx context.Context) []*inttypes.ServicePackageFrameworkResource {
 	return []*inttypes.ServicePackageFrameworkResource{
 		{
+			Factory:  newIngressPointResource,
+			TypeName: "aws_mailmanager_ingress_point",
+			Name:     "Ingress Point",
+			Tags: unique.Make(inttypes.ServicePackageResourceTags{
+				IdentifierAttribute: names.AttrARN,
+			}),
+			Region:   inttypes.ResourceRegionDefault(),
+			Identity: inttypes.RegionalSingleParameterIdentity(inttypes.StringIdentityAttribute(names.AttrID, true)),
+			Import: inttypes.FrameworkImport{
+				WrappedImport: true,
+			},
+		},
+		{
 			Factory:  newRuleSetResource,
 			TypeName: "aws_mailmanager_rule_set",
 			Name:     "Rule Set",
