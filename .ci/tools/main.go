1ce10e8a5384f78bd644dbe96a55a1274e175f41
Justin Retzolk <44710313+justinretzolk@users.noreply.github.com>
2026-08-06 16:40:32 -0500

    Remove go-changelog from tools

--- .ci/tools/main.go
+++ .ci/tools/main.go
@@ -11,7 +11,6 @@ import (
 	_ "github.com/YakDriver/swissshepherd"
 	_ "github.com/client9/misspell/cmd/misspell"
 	_ "github.com/golangci/golangci-lint/v2/cmd/golangci-lint"
-	_ "github.com/hashicorp/go-changelog/cmd/changelog-build"
 	_ "github.com/katbyte/terrafmt"
 	_ "github.com/pavius/impi/cmd/impi"
 	_ "github.com/rhysd/actionlint/cmd/actionlint"
