5f59f0cda81ba71d92691a7f388f9ed6206dbc74
Dirk Avery <31492422+YakDriver@users.noreply.github.com>
2026-08-06 15:29:22 -0400

    neptunegraph: use ConfigPlanChecks in private graph endpoint disappears test

--- internal/service/neptunegraph/private_graph_endpoint_test.go
+++ internal/service/neptunegraph/private_graph_endpoint_test.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/service/neptunegraph"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/retry"
@@ -75,6 +76,14 @@ func TestAccNeptuneGraphPrivateGraphEndpoint_disappears(t *testing.T) {
 					acctest.CheckFrameworkResourceDisappears(ctx, t, tfneptunegraph.ResourcePrivateGraphEndpoint, resourceName),
 				),
 				ExpectNonEmptyPlan: true,
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceName, plancheck.ResourceActionCreate),
+					},
+					PostApplyPostRefresh: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceName, plancheck.ResourceActionCreate),
+					},
+				},
 			},
 		},
 	})
