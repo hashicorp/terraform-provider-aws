0e1ea67db20adaeda3346008d906d64ec03a0f0a
Graham Davison <gdavison@hashicorp.com>
2026-08-11 11:51:55 -0700

    Do not update Resource Identity on Read if it is not mutable

--- internal/provider/sdkv2/identity_interceptor_test.go
+++ internal/provider/sdkv2/identity_interceptor_test.go
@@ -98,12 +98,13 @@ func TestIdentityInterceptor_Create(t *testing.T) {
 	}
 }
 
-func TestIdentityInterceptor_Read_RemovedFromState(t *testing.T) {
+func TestIdentityInterceptor_Read(t *testing.T) {
 	t.Parallel()
 
 	accountID := "123456789012"
 	region := "us-west-2" //lintignore:AWSAT003
-	name := "a_name"
+	attributeName := "attr_name"
+	identityName := "id_name"
 
 	resourceSchema := map[string]*schema.Schema{
 		"name": {
@@ -117,47 +118,183 @@ func TestIdentityInterceptor_Read_RemovedFromState(t *testing.T) {
 		"region": sdkv2.RegionOptionalComputed(),
 	}
 
-	identitySpec := regionalSingleParameterizedIdentitySpec("name")
-	identitySchema := identity.NewIdentitySchema(identitySpec)
+	client := mockClient{
+		accountID: accountID,
+		region:    region,
+	}
 
-	invocation := newIdentityInterceptor(&identitySpec)
-	interceptor := invocation.interceptor.(identityInterceptor)
+	testCases := map[string]struct {
+		id             string
+		identityValues map[string]string
+		expectedName   string
+	}{
+		"existing resource": {
+			id: "some_id",
+			identityValues: map[string]string{
+				names.AttrAccountID: accountID,
+				names.AttrRegion:    region,
+				"name":              identityName,
+			},
+			expectedName: identityName,
+		},
+		"removed from state": {
+			id: "",
+			identityValues: map[string]string{
+				names.AttrAccountID: accountID,
+				names.AttrRegion:    region,
+				"name":              identityName,
+			},
+			expectedName: identityName,
+		},
+		"for import": {
+			id:             "some_id",
+			identityValues: nil,
+			expectedName:   attributeName,
+		},
+	}
+
+	for tname, tc := range testCases {
+		t.Run(tname, func(t *testing.T) {
+			t.Parallel()
+			ctx := t.Context()
+
+			identitySpec := regionalSingleParameterizedIdentitySpec("name")
+			identitySchema := identity.NewIdentitySchema(identitySpec)
+
+			invocation := newIdentityInterceptor(&identitySpec)
+			interceptor := invocation.interceptor.(identityInterceptor)
+
+			d := schema.TestResourceDataWithIdentityRaw(t, resourceSchema, identitySchema, tc.identityValues)
+			d.SetId(tc.id)
+			d.Set("name", attributeName)
+			d.Set("region", region)
+			d.Set("type", "some_type")
+
+			opts := crudInterceptorOptions{
+				c:    client,
+				d:    d,
+				when: After,
+				why:  Read,
+			}
+
+			interceptor.run(ctx, opts)
+
+			identity, err := d.Identity()
+			if err != nil {
+				t.Fatalf("unexpected error getting identity: %v", err)
+			}
+
+			if e, a := accountID, identity.Get(names.AttrAccountID); e != a {
+				t.Errorf("expected account ID %q, got %q", e, a)
+			}
+			if e, a := region, identity.Get(names.AttrRegion); e != a {
+				t.Errorf("expected region %q, got %q", e, a)
+			}
+			if e, a := tc.expectedName, identity.Get("name"); e != a {
+				t.Errorf("expected name %q, got %q", e, a)
+			}
+		})
+	}
+}
+
+func TestIdentityInterceptor_Read_MutableIdentity(t *testing.T) {
+	t.Parallel()
+
+	accountID := "123456789012"
+	region := "us-west-2" //lintignore:AWSAT003
+	attributeName := "attr_name"
+	identityName := "id_name"
+
+	resourceSchema := map[string]*schema.Schema{
+		"name": {
+			Type:     schema.TypeString,
+			Required: true,
+		},
+		"type": {
+			Type:     schema.TypeString,
+			Optional: true,
+		},
+		"region": sdkv2.RegionOptionalComputed(),
+	}
 
 	client := mockClient{
 		accountID: accountID,
 		region:    region,
 	}
 
-	ctx := t.Context()
-
-	d := schema.TestResourceDataWithIdentityRaw(t, resourceSchema, identitySchema, nil)
-	d.SetId("")
-	d.Set("name", name)
-	d.Set("region", region)
-	d.Set("type", "some_type")
-
-	opts := crudInterceptorOptions{
-		c:    client,
-		d:    d,
-		when: After,
-		why:  Read,
+	testCases := map[string]struct {
+		id             string
+		identityValues map[string]string
+		expectedName   string
+	}{
+		"existing resource": {
+			id: "some_id",
+			identityValues: map[string]string{
+				names.AttrAccountID: accountID,
+				names.AttrRegion:    region,
+				"name":              identityName,
+			},
+			expectedName: attributeName,
+		},
+		"removed from state": {
+			id: "",
+			identityValues: map[string]string{
+				names.AttrAccountID: accountID,
+				names.AttrRegion:    region,
+				"name":              identityName,
+			},
+			expectedName: identityName,
+		},
+		"for import": {
+			id:             "some_id",
+			identityValues: nil,
+			expectedName:   attributeName,
+		},
 	}
 
-	interceptor.run(ctx, opts)
+	for tname, tc := range testCases {
+		t.Run(tname, func(t *testing.T) {
+			t.Parallel()
+			ctx := t.Context()
 
-	identity, err := d.Identity()
-	if err != nil {
-		t.Fatalf("unexpected error getting identity: %v", err)
-	}
+			identitySpec := regionalSingleParameterizedIdentitySpec("name",
+				inttypes.WithMutableIdentity(),
+			)
+			identitySchema := identity.NewIdentitySchema(identitySpec)
 
-	if identity.Get(names.AttrAccountID) != "" {
-		t.Errorf("expected no account ID, got %q", identity.Get(names.AttrAccountID))
-	}
-	if identity.Get(names.AttrRegion) != "" {
-		t.Errorf("expected no region, got %q", identity.Get(names.AttrRegion))
-	}
-	if identity.Get("name") != "" {
-		t.Errorf("expected no name, got %q", identity.Get("name"))
+			invocation := newIdentityInterceptor(&identitySpec)
+			interceptor := invocation.interceptor.(identityInterceptor)
+
+			d := schema.TestResourceDataWithIdentityRaw(t, resourceSchema, identitySchema, tc.identityValues)
+			d.SetId(tc.id)
+			d.Set("name", attributeName)
+			d.Set("region", region)
+			d.Set("type", "some_type")
+
+			opts := crudInterceptorOptions{
+				c:    client,
+				d:    d,
+				when: After,
+				why:  Read,
+			}
+
+			interceptor.run(ctx, opts)
+
+			identity, err := d.Identity()
+			if err != nil {
+				t.Fatalf("unexpected error getting identity: %v", err)
+			}
+
+			if e, a := accountID, identity.Get(names.AttrAccountID); e != a {
+				t.Errorf("expected account ID %q, got %q", e, a)
+			}
+			if e, a := region, identity.Get(names.AttrRegion); e != a {
+				t.Errorf("expected region %q, got %q", e, a)
+			}
+			if e, a := tc.expectedName, identity.Get("name"); e != a {
+				t.Errorf("expected name %q, got %q", e, a)
+			}
+		})
 	}
 }
 
