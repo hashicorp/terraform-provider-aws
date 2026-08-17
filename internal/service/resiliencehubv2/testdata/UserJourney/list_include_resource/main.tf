23b523f5a3b56d2db8f838e28dcdf84aff58a6aa
Kit Ewbank <Kit_Ewbank@hotmail.com>
2026-08-17 12:37:10 -0400

    l/aws_resiliencehubv2_user_journey: First pass.

--- internal/service/resiliencehubv2/testdata/UserJourney/list_include_resource/main.tf
+++ internal/service/resiliencehubv2/testdata/UserJourney/list_include_resource/main.tf
@@ -1,15 +1,15 @@
 # Copyright IBM Corp. 2014, 2026
 # SPDX-License-Identifier: MPL-2.0
 
-resource "aws_resiliencehubv2_system" "test" {
-  name = "${var.rName}-system"
-}
-
 resource "aws_resiliencehubv2_user_journey" "test" {
   count = var.resource_count
 
   name       = "${var.rName}-${count.index}"
   system_arn = aws_resiliencehubv2_system.test.arn
+}
+
+resource "aws_resiliencehubv2_system" "test" {
+  name = "${var.rName}-system"
 }
 
 variable "rName" {
