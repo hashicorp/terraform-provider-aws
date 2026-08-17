cbb42208773badeaded5981f32e0be3334ea8732
Robin Rodriguez <robinrz@amazon.com>
2026-08-17 18:55:03 +0000

    service/directconnect: mark BGP auth keys sensitive

--- internal/service/directconnect/hosted_transit_virtual_interface.go
+++ internal/service/directconnect/hosted_transit_virtual_interface.go
@@ -70,10 +70,11 @@ func resourceHostedTransitVirtualInterface() *schema.Resource {
 					ForceNew: true,
 				},
 				"bgp_auth_key": {
-					Type:     schema.TypeString,
-					Optional: true,
-					Computed: true,
-					ForceNew: true,
+					Type:      schema.TypeString,
+					Optional:  true,
+					Computed:  true,
+					ForceNew:  true,
+					Sensitive: true,
 				},
 				names.AttrConnectionID: {
 					Type:     schema.TypeString,
