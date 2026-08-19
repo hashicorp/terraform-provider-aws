f65cd04d49eaf67a83f17e2ccb91aa72387ba795
Graham Davison <gdavison@hashicorp.com>
2026-08-19 13:10:16 -0700

    Uses `[[ ... ]]` instead of `[ ... ]` for conditionals and `&&` instead of `-a` for "and"
    Resolves SC2292 and SC2166

--- .teamcity/scripts/performance.sh
+++ .teamcity/scripts/performance.sh
@@ -132,28 +132,28 @@ function analysis {
     printf "##teamcity[notification notifier='slack' message='*Performance changes from latest version (%%s) to main* |nAllocated memory: %%.1f%%%% (%%.1fMB to %%.1fMB) (%%s) %%s |nIn-use memory: %%.1f%%%% (%%.1fMB to %%.1fMB) (%%s) (wide-fluctuations normal) |nCPU time: %%.1f%%%% (%%.1fs to %%.1fs) (%%s) %%s' sendTo='CN0G9S7M4' connectionId='PROJECT_EXT_8']\n" "$(basename $(curl -Ls -o /dev/null -w %%{url_effective} https://github.com/hashicorp/terraform-provider-aws/releases/latest))" "${alloc}" "${alloc_mb_latest}" "${alloc_mb_main}" "${alloc_bw}" "${alloc_emoji}" "${inuse}" "${inuse_mb_latest}" "${inuse_mb_main}" "${inuse_bw}" "${cputime}" "${cputime_s_latest}" "${cputime_s_main}" "${cputime_bw}" "${cputime_emoji}"
 }
 
-if [ -f "memvpcmain.prof" -a -f "memssmmain.prof" -a -f "memvpclatest.prof" -a -f "memssmlatest.prof" ]; then
+if [[ -f "memvpcmain.prof" ]] && [[ -f "memssmmain.prof" ]] && [[ -f "memvpclatest.prof" ]] && [[ -f "memssmlatest.prof" ]]; then
     echo "Tests complete. Analyzing results..."
     analysis
 fi
 
-if [ -f "memvpcmain.prof" -a -f "memssmmain.prof" -a -f "memvpclatest.prof" -a ! -f "memssmlatest.prof" ]; then
+if [[ -f "memvpcmain.prof" ]] && [[ -f "memssmmain.prof" ]] && [[ -f "memvpclatest.prof" ]] && [[ ! -f "memssmlatest.prof" ]]; then
     echo "Running SSM latest version ($(basename $(curl -Ls -o /dev/null -w %%{url_effective} https://github.com/hashicorp/terraform-provider-aws/releases/latest))) test..."
     ssmtest ssmlatest
 fi
 
-if [ -f "memvpcmain.prof" -a -f "memssmmain.prof" -a ! -f "memvpclatest.prof" ]; then
+if [[ -f "memvpcmain.prof" ]] && [[ -f "memssmmain.prof" ]] && [[ ! -f "memvpclatest.prof" ]]; then
     echo "Running VPC latest version ($(basename $(curl -Ls -o /dev/null -w %%{url_effective} https://github.com/hashicorp/terraform-provider-aws/releases/latest))) test..."
     git checkout $(basename $(curl -Ls -o /dev/null -w %%{url_effective} https://github.com/hashicorp/terraform-provider-aws/releases/latest))
     vpctest vpclatest
 fi
 
-if [ -f "memvpcmain.prof" -a ! -f "memssmmain.prof" ]; then
+if [[ -f "memvpcmain.prof" ]] && [[ ! -f "memssmmain.prof" ]]; then
     echo "Running SSM main branch test..."
     ssmtest ssmmain
 fi
 
-if [ ! -f "memvpcmain.prof" ]; then
+if [[ ! -f "memvpcmain.prof" ]]; then
     go install github.com/google/pprof@latest
     echo "Running VPC main branch test..."
     vpctest vpcmain
