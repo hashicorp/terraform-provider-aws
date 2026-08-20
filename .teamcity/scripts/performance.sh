70dfb613dcb5780118d51f5f2604b43dd138ef2d
Graham Davison <gdavison@hashicorp.com>
2026-08-20 14:31:51 -0700

    Prevents error from `head -5` where pipe is closed, causing error in `pprof`

--- .teamcity/scripts/performance.sh
+++ .teamcity/scripts/performance.sh
@@ -74,32 +74,32 @@ function analysis {
     fi
 
     local perf_main_memalloc1
-    perf_main_memalloc1=$( pprof -top -flat -sample_index=alloc_space -unit=mb memvpcmain.prof | head -5 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)MB total.*/\1/g' )
+    perf_main_memalloc1=$( pprof -top -flat -sample_index=alloc_space -unit=mb memvpcmain.prof 2>/dev/null | grep -o '%% of [0-9.]*MB total' | sed 's/%% of //;s/MB total//' )
     local perf_main_meminuse1
-    perf_main_meminuse1=$( pprof -top -flat -sample_index=inuse_space -unit=mb memvpcmain.prof | head -5 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)MB total.*/\1/g' )
+    perf_main_meminuse1=$( pprof -top -flat -sample_index=inuse_space -unit=mb memvpcmain.prof 2>/dev/null | grep -o '%% of [0-9.]*MB total' | sed 's/%% of //;s/MB total//' )
     local perf_main_cputime1
-    perf_main_cputime1=$( pprof -top -flat -sample_index=cpu cpuvpcmain.prof 2>/dev/null | head -5 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)s total.*/\1/g' )
+    perf_main_cputime1=$( pprof -top -flat -sample_index=cpu cpuvpcmain.prof 2>/dev/null | grep -o '%% of [0-9.]*s total' | sed 's/%% of //;s/s total//' )
 
     local perf_main_memalloc2
-    perf_main_memalloc2=$( pprof -top -flat -sample_index=alloc_space -unit=mb memssmmain.prof | head -5 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)MB total.*/\1/g' )
+    perf_main_memalloc2=$( pprof -top -flat -sample_index=alloc_space -unit=mb memssmmain.prof 2>/dev/null | grep -o '%% of [0-9.]*MB total' | sed 's/%% of //;s/MB total//' )
     local perf_main_meminuse2
-    perf_main_meminuse2=$( pprof -top -flat -sample_index=inuse_space -unit=mb memssmmain.prof | head -5 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)MB total.*/\1/g' )
+    perf_main_meminuse2=$( pprof -top -flat -sample_index=inuse_space -unit=mb memssmmain.prof 2>/dev/null | grep -o '%% of [0-9.]*MB total' | sed 's/%% of //;s/MB total//' )
     local perf_main_cputime2
-    perf_main_cputime2=$( pprof -top -flat -sample_index=cpu cpussmmain.prof 2>/dev/null | head -5 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)s total.*/\1/g' )
+    perf_main_cputime2=$( pprof -top -flat -sample_index=cpu cpussmmain.prof 2>/dev/null | grep -o '%% of [0-9.]*s total' | sed 's/%% of //;s/s total//' )
 
     local perf_latest_memalloc1
-    perf_latest_memalloc1=$( pprof -top -flat -sample_index=alloc_space -unit=mb memvpclatest.prof | head -5 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)MB total.*/\1/g' )
+    perf_latest_memalloc1=$( pprof -top -flat -sample_index=alloc_space -unit=mb memvpclatest.prof 2>/dev/null | grep -o '%% of [0-9.]*MB total' | sed 's/%% of //;s/MB total//' )
     local perf_latest_meminuse1
-    perf_latest_meminuse1=$( pprof -top -flat -sample_index=inuse_space -unit=mb memvpclatest.prof | head -5 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)MB total.*/\1/g' )
+    perf_latest_meminuse1=$( pprof -top -flat -sample_index=inuse_space -unit=mb memvpclatest.prof 2>/dev/null | grep -o '%% of [0-9.]*MB total' | sed 's/%% of //;s/MB total//' )
     local perf_latest_cputime1
-    perf_latest_cputime1=$( pprof -top -flat -sample_index=cpu cpuvpclatest.prof 2>/dev/null | head -5 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)s total.*/\1/g' )
+    perf_latest_cputime1=$( pprof -top -flat -sample_index=cpu cpuvpclatest.prof 2>/dev/null | grep -o '%% of [0-9.]*s total' | sed 's/%% of //;s/s total//' )
 
     local perf_latest_memalloc2
-    perf_latest_memalloc2=$( pprof -top -flat -sample_index=alloc_space -unit=mb memssmlatest.prof | head -5 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)MB total.*/\1/g' )
+    perf_latest_memalloc2=$( pprof -top -flat -sample_index=alloc_space -unit=mb memssmlatest.prof 2>/dev/null | grep -o '%% of [0-9.]*MB total' | sed 's/%% of //;s/MB total//' )
     local perf_latest_meminuse2
-    perf_latest_meminuse2=$( pprof -top -flat -sample_index=inuse_space -unit=mb memssmlatest.prof | head -5 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)MB total.*/\1/g' )
+    perf_latest_meminuse2=$( pprof -top -flat -sample_index=inuse_space -unit=mb memssmlatest.prof 2>/dev/null | grep -o '%% of [0-9.]*MB total' | sed 's/%% of //;s/MB total//' )
     local perf_latest_cputime2
-    perf_latest_cputime2=$( pprof -top -flat -sample_index=cpu cpussmlatest.prof 2>/dev/null | head -5 | tr '\n' ' ' | sed -E 's/.*%% of ([0-9.]+)s total.*/\1/g' )
+    perf_latest_cputime2=$( pprof -top -flat -sample_index=cpu cpussmlatest.prof 2>/dev/null | grep -o '%% of [0-9.]*s total' | sed 's/%% of //;s/s total//' )
 
     local alloc
     alloc=$( bc -l <<< "(((${perf_main_memalloc1}+${perf_main_memalloc2})/2)/((${perf_latest_memalloc1}+${perf_latest_memalloc2})/2)-1)*100" )
