cf5f485918f67c9ec71611dcad0b4e6fe03f1230
Kit Ewbank <Kit_Ewbank@hotmail.com>
2026-08-26 14:27:46 -0400

    Add 'internal/framework/planmodifiers/internal'.

--- internal/framework/planmodifiers/listplanmodifier/unknown_other_value_changes.go
+++ internal/framework/planmodifiers/listplanmodifier/unknown_other_value_changes.go
@@ -4,47 +4,11 @@
 package listplanmodifier
 
 import (
-	"context"
-	"fmt"
-
-	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-provider-aws/internal/framework/planmodifiers/internal"
 )
 
 func UnknownWhenOtherValueChanges(path path.Path) planmodifier.List {
-	return unknownWhenOtherValueChanges{
-		path: path,
-	}
-}
-
-type unknownWhenOtherValueChanges struct {
-	path path.Path
-}
-
-func (m unknownWhenOtherValueChanges) Description(_ context.Context) string {
-	return fmt.Sprintf("Uses state for unknown when the value at %[1]q does not change.", m.path)
-}
-
-func (m unknownWhenOtherValueChanges) MarkdownDescription(ctx context.Context) string {
-	return m.Description(ctx)
-}
-
-func (m unknownWhenOtherValueChanges) PlanModifyList(ctx context.Context, req planmodifier.ListRequest, resp *planmodifier.ListResponse) {
-	if req.StateValue.IsNull() {
-		return
-	}
-
-	var otherPlanValue, otherStateValue attr.Value
-	resp.Diagnostics.Append(req.Plan.GetAttribute(ctx, m.path, &otherPlanValue)...)
-	resp.Diagnostics.Append(req.State.GetAttribute(ctx, m.path, &otherStateValue)...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	if !otherPlanValue.Equal(otherStateValue) {
-		return
-	}
-
-	resp.PlanValue = req.StateValue
+	return internal.UnknownWhenOtherValueChanges(path)
 }
