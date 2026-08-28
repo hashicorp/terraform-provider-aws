47970f332817d0381431ad5af6491e87478f009c
Kit Ewbank <Kit_Ewbank@hotmail.com>
2026-08-28 15:55:43 -0400

    Remove 'rName' variable.

--- internal/service/accountaccess/testdata/Application/list_basic/main.tf
+++ internal/service/accountaccess/testdata/Application/list_basic/main.tf
@@ -5,17 +5,4 @@ data "aws_ssoadmin_instances" "test" {}
 
 resource "aws_accountaccess_application" "test" {
   identity_center_instance_arn = tolist(data.aws_ssoadmin_instances.test.arns)[0]
-
-  lifecycle {
-    precondition {
-      condition     = length(var.rName) > 0
-      error_message = "The generated test name must not be empty."
-    }
-  }
-}
-
-variable "rName" {
-  description = "Name for resource"
-  type        = string
-  nullable    = false
 }
