e4c0819c08a958df04344e2f454fc2e0faa8ae3a
6democratickim9 <shcomon24@gmail.com>
2026-08-29 23:24:53 +0900

    fix(observabilityadmin): omit regions for all-regions updates

---
+++ internal/service/observabilityadmin/telemetry_rule_for_organization_flex_test.go
@@ -0,0 +1,50 @@
+// Copyright IBM Corp. 2014, 2026
+// SPDX-License-Identifier: MPL-2.0
+
+package observabilityadmin
+
+import (
+	"slices"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	awstypes "github.com/aws/aws-sdk-go-v2/service/observabilityadmin/types"
+)
+
+func TestNormalizeTelemetryRuleRegionSelection(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		rule        *awstypes.TelemetryRule
+		wantRegions []string
+	}{
+		"nil rule": {},
+		"all regions": {
+			rule: &awstypes.TelemetryRule{
+				AllRegions: aws.Bool(true),
+				Regions:    []string{"us-east-1", "us-west-2"},
+			},
+		},
+		"selected regions": {
+			rule: &awstypes.TelemetryRule{
+				Regions: []string{"us-east-1"},
+			},
+			wantRegions: []string{"us-east-1"},
+		},
+	}
+
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			normalizeTelemetryRuleRegionSelection(testCase.rule)
+
+			if testCase.rule == nil {
+				return
+			}
+			if got, want := testCase.rule.Regions, testCase.wantRegions; !slices.Equal(got, want) {
+				t.Errorf("Regions = %v, want %v", got, want)
+			}
+		})
+	}
+}
