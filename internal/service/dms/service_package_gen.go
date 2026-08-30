29ee9a213c33359005fd05a4afb4c4ac18dfa2cb
taruntej-a <tarunsraina483@gmail.com>
2026-08-30 16:20:29 +0530

    r/aws_dms_instance_profile: make gen

--- internal/service/dms/service_package_gen.go
+++ internal/service/dms/service_package_gen.go
@@ -36,7 +36,21 @@ func (p *servicePackage) FrameworkDataSources(ctx context.Context) []*inttypes.S
 }
 
 func (p *servicePackage) FrameworkResources(ctx context.Context) []*inttypes.ServicePackageFrameworkResource {
-	return []*inttypes.ServicePackageFrameworkResource{}
+	return []*inttypes.ServicePackageFrameworkResource{
+		{
+			Factory:  newInstanceProfileResource,
+			TypeName: "aws_dms_instance_profile",
+			Name:     "Instance Profile",
+			Tags: unique.Make(inttypes.ServicePackageResourceTags{
+				IdentifierAttribute: names.AttrARN,
+			}),
+			Region:   inttypes.ResourceRegionDefault(),
+			Identity: inttypes.RegionalARNIdentity(inttypes.WithIdentityDuplicateAttrs(names.AttrID)),
+			Import: inttypes.FrameworkImport{
+				WrappedImport: true,
+			},
+		},
+	}
 }
 
 func (p *servicePackage) SDKDataSources(ctx context.Context) []*inttypes.ServicePackageSDKDataSource {
