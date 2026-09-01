9b46f2ae5f863581cb287884447fd401d9b3dcad
Jared Baker <jared.baker@hashicorp.com>
2026-09-01 15:19:43 -0400

    r/aws_s3_bucket_object_lock_configuration: restore `ForceNew` behavior
    The default and only valid value for `object_lock_enabled` is "Enabled". This reverts to the pre-existing logic since the value cannot be changed.

--- internal/service/s3/bucket_object_lock_configuration.go
+++ internal/service/s3/bucket_object_lock_configuration.go
@@ -56,6 +56,7 @@ func resourceBucketObjectLockConfiguration() *schema.Resource {
 				"object_lock_enabled": {
 					Type:             schema.TypeString,
 					Optional:         true,
+					ForceNew:         true,
 					Default:          types.ObjectLockEnabledEnabled,
 					ValidateDiagFunc: enum.Validate[types.ObjectLockEnabled](),
 				},
