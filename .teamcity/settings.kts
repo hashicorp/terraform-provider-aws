a4c96fa142d69ad492a92b9e1e2cef3f3f82aa32
Graham Davison <gdavison@hashicorp.com>
2026-09-02 16:41:56 -0700

    Smoke tests do not need exclusive lock

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -652,7 +652,7 @@ object Sanity : BuildType({
 
         feature {
             type = "JetBrains.SharedResources"
-            param("locks-param", "${DslContext.getParameter("aws_account.lock_id")} writeLock")
+            param("locks-param", "${DslContext.getParameter("aws_account.lock_id")} readLock")
         }
         feature {
             type = "JetBrains.SharedResources"
