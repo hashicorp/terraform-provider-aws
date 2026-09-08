c76ba042d16cd4a4c086e8d78e1533786d03e66e
Kit Ewbank <Kit_Ewbank@hotmail.com>
2026-09-08 10:22:24 -0400

    Remove unused code.

--- internal/service/agentregistry/registry_list.go
+++ internal/service/agentregistry/registry_list.go
@@ -38,14 +38,6 @@ type registryListResource struct {
 func (l *registryListResource) List(ctx context.Context, request list.ListRequest, stream *list.ListResultsStream) {
 	conn := l.Meta().AgentRegistryClient(ctx)
 
-	var query listRegistryModel
-	if request.Config.Raw.IsKnown() && !request.Config.Raw.IsNull() {
-		if diags := request.Config.Get(ctx, &query); diags.HasError() {
-			stream.Results = list.ListResultsStreamDiagnostics(diags)
-			return
-		}
-	}
-
 	stream.Results = func(yield func(list.ListResult) bool) {
 		var input agentregistrycontrol.ListRegistriesInput
 		for item, err := range listRegistries(ctx, conn, &input) {
