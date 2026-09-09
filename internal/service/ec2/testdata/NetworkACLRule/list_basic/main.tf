bdfd69a8cc20f4c3299b68d24107ca4a1435c988
Subham Mukhopadhyay <subham.mukhopadhyay@hashicorp.com>
2026-09-09 19:44:46 +0530

    initial list implementation

---
+++ internal/service/ec2/testdata/NetworkACLRule/list_basic/main.tf
@@ -0,0 +1,43 @@
+# Copyright IBM Corp. 2014, 2026
+# SPDX-License-Identifier: MPL-2.0
+
+resource "aws_network_acl_rule" "test" {
+  count = var.resource_count
+
+  network_acl_id = aws_network_acl.test.id
+  rule_number    = 1233 + count.index
+  egress         = false
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 22
+  to_port        = 22
+}
+
+resource "aws_vpc" "test" {
+  cidr_block = "10.1.0.0/16"
+
+  tags = {
+    Name = var.rName
+  }
+}
+
+resource "aws_network_acl" "test" {
+  vpc_id = aws_vpc.test.id
+
+  tags = {
+    Name = var.rName
+  }
+}
+
+variable "rName" {
+  description = "Name for resource"
+  type        = string
+  nullable    = false
+}
+
+variable "resource_count" {
+  description = "Number of resources to create"
+  type        = number
+  nullable    = false
+}
