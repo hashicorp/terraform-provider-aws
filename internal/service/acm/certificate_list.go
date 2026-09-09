147c05f1384e8601cf4ef784e0f8bf0b9448cedc
Jared Baker <jared.baker@hashicorp.com>
2026-09-09 15:31:41 -0400

    acm: remove unused list config parsing

--- internal/service/acm/certificate_list.go
+++ internal/service/acm/certificate_list.go
@@ -35,21 +35,9 @@ type certificateListResource struct {
 	framework.ListResourceWithSDKv2Resource
 }
 
-type certificateListResourceModel struct {
-	framework.WithRegionModel
-}
-
 func (l *certificateListResource) List(ctx context.Context, request list.ListRequest, stream *list.ListResultsStream) {
 	awsClient := l.Meta()
 	conn := awsClient.ACMClient(ctx)
-
-	var query certificateListResourceModel
-	if request.Config.Raw.IsKnown() && !request.Config.Raw.IsNull() {
-		if diags := request.Config.Get(ctx, &query); diags.HasError() {
-			stream.Results = list.ListResultsStreamDiagnostics(diags)
-			return
-		}
-	}
 
 	tflog.Info(ctx, "Listing ACM Certificates")
 
