7386ec48b5effd4710b589f3dcb599bee478a8db
Graham Davison <gdavison@hashicorp.com>
2026-09-11 11:06:57 -0700

    Refactors `TestAttributeTypes` to a table-driven test

--- internal/framework/types/attrtypes_test.go
+++ internal/framework/types/attrtypes_test.go
@@ -9,71 +9,78 @@ import (
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	fwtypes "github.com/hashicorp/terraform-provider-aws/internal/framework/types"
 )
 
+type attributeTypesTestStruct1 struct{}
+
+type attributeTypesTestStruct2 struct {
+	ARN             types.String `tfsdk:"arn"`
+	ID              types.Int64  `tfsdk:"id"`
+	IncludeProperty types.Bool   `tfsdk:"include_property"`
+}
+
+type attributeTypesTestStruct3 struct {
+	F1 types.String `tfsdk:"f1"`
+	attributeTypesTestStruct2
+	F2 types.Int32 `tfsdk:"f2"`
+}
+
 func TestAttributeTypes(t *testing.T) {
 	t.Parallel()
 
-	type struct1 struct{}
-
-	type struct2 struct {
-		ARN             types.String `tfsdk:"arn"`
-		ID              types.Int64  `tfsdk:"id"`
-		IncludeProperty types.Bool   `tfsdk:"include_property"`
+	testCases := map[string]struct {
+		attributeTypes func(context.Context) (map[string]attr.Type, diag.Diagnostics)
+		expectErr      bool
+		expected       map[string]attr.Type
+	}{
+		"empty struct": {
+			attributeTypes: fwtypes.AttributeTypes[attributeTypesTestStruct1],
+			expected:       map[string]attr.Type{},
+		},
+		"non-struct type": {
+			attributeTypes: fwtypes.AttributeTypes[int],
+			expectErr:      true,
+		},
+		"flat struct": {
+			attributeTypes: fwtypes.AttributeTypes[attributeTypesTestStruct2],
+			expected: map[string]attr.Type{
+				"arn":              types.StringType,
+				"id":               types.Int64Type,
+				"include_property": types.BoolType,
+			},
+		},
+		"embedded struct": {
+			attributeTypes: fwtypes.AttributeTypes[attributeTypesTestStruct3],
+			expected: map[string]attr.Type{
+				"f1":               types.StringType,
+				"arn":              types.StringType,
+				"id":               types.Int64Type,
+				"include_property": types.BoolType,
+				"f2":               types.Int32Type,
+			},
+		},
 	}
 
-	type struct3 struct {
-		F1 types.String `tfsdk:"f1"`
-		struct2
-		F2 types.Int32 `tfsdk:"f2"`
-	}
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
 
-	ctx := context.Background()
-	got := fwtypes.AttributeTypesMust[struct1](ctx)
-	wanted := map[string]attr.Type{}
+			ctx := context.Background()
+			got, diags := testCase.attributeTypes(ctx)
 
-	if diff := cmp.Diff(got, wanted); diff != "" {
-		t.Errorf("unexpected diff (+wanted, -got): %s", diff)
-	}
+			if got, want := diags.HasError(), testCase.expectErr; got != want {
+				t.Fatalf("expectErr = %t, got diagnostics: %v", want, diags)
+			}
+			if testCase.expectErr {
+				return
+			}
 
-	_, err := fwtypes.AttributeTypes[int](ctx)
-
-	if err == nil {
-		t.Fatalf("expected error")
-	}
-
-	got, err = fwtypes.AttributeTypes[struct2](ctx)
-
-	if err != nil {
-		t.Fatalf("unexpected error")
-	}
-
-	wanted = map[string]attr.Type{
-		"arn":              types.StringType,
-		"id":               types.Int64Type,
-		"include_property": types.BoolType,
-	}
-
-	if diff := cmp.Diff(got, wanted); diff != "" {
-		t.Errorf("unexpected diff (+wanted, -got): %s", diff)
-	}
-
-	got, err = fwtypes.AttributeTypes[struct3](ctx)
-	if err != nil {
-		t.Fatalf("unexpected error")
-	}
-
-	wanted = map[string]attr.Type{
-		"f1":               types.StringType,
-		"arn":              types.StringType,
-		"id":               types.Int64Type,
-		"include_property": types.BoolType,
-		"f2":               types.Int32Type,
-	}
-
-	if diff := cmp.Diff(got, wanted); diff != "" {
-		t.Errorf("unexpected diff (+wanted, -got): %s", diff)
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected diff (+expected, -got): %s", diff)
+			}
+		})
 	}
 }
