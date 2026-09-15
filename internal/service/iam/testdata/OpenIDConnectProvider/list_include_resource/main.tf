9e3e49b0a867e512e2481a252cd7673026dbb1c6
Jared Baker <jared.baker@hashicorp.com>
2026-09-15 13:14:23 -0400

    l/aws_iam_openid_connect_provider: new list resource

---
+++ internal/service/iam/testdata/OpenIDConnectProvider/list_include_resource/main.tf
@@ -0,0 +1,32 @@
+# Copyright IBM Corp. 2014, 2026
+# SPDX-License-Identifier: MPL-2.0
+
+resource "aws_iam_openid_connect_provider" "test" {
+  count = var.resource_count
+
+  url = "https://accounts.testle.com/${var.rName}-${count.index}"
+
+  client_id_list = ["266362248691-re108qaeld573ia0l6clj2i5ac7r7291.apps.testleusercontent.com"]
+
+  thumbprint_list = ["cf23df2207d99a74fbe169e3eba035e633b65d94"]
+
+  tags = var.resource_tags
+}
+
+variable "rName" {
+  description = "Name for resource"
+  type        = string
+  nullable    = false
+}
+
+variable "resource_count" {
+  description = "Number of resources to create"
+  type        = number
+  nullable    = false
+}
+
+variable "resource_tags" {
+  description = "Tags to set on resource"
+  type        = map(string)
+  nullable    = false
+}
