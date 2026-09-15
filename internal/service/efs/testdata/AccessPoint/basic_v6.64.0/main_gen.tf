60a19780be48a2d9fee0e1f4fa84eff3f64c5143
Kit Ewbank <Kit_Ewbank@hotmail.com>
2026-09-15 16:06:52 -0400

    r/aws_efs_access_point: Add Resource Identity support.

---
+++ internal/service/efs/testdata/AccessPoint/basic_v6.64.0/main_gen.tf
@@ -0,0 +1,25 @@
+# Copyright IBM Corp. 2014, 2026
+# SPDX-License-Identifier: MPL-2.0
+
+resource "aws_efs_access_point" "test" {
+  file_system_id = aws_efs_file_system.test.id
+}
+
+resource "aws_efs_file_system" "test" {
+}
+
+variable "rName" {
+  description = "Name for resource"
+  type        = string
+  nullable    = false
+}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.64.0"
+    }
+  }
+}
+
+provider "aws" {}
---
+++ internal/service/efs/testdata/AccessPoint/region_override/main_gen.tf
@@ -0,0 +1,25 @@
+# Copyright IBM Corp. 2014, 2026
+# SPDX-License-Identifier: MPL-2.0
+
+resource "aws_efs_access_point" "test" {
+  region = var.region
+
+  file_system_id = aws_efs_file_system.test.id
+}
+
+resource "aws_efs_file_system" "test" {
+  region = var.region
+
+}
+
+variable "rName" {
+  description = "Name for resource"
+  type        = string
+  nullable    = false
+}
+
+variable "region" {
+  description = "Region to deploy resource in"
+  type        = string
+  nullable    = false
+}
