21900cc3dfa7b1ee0c2216cad2ccad2ab38e2c2a
Jared Baker <jared.baker@hashicorp.com>
2026-09-17 13:08:53 -0400

    provider: switch to protocol v6
    Swaps out the v5 provider server for v6. This changes removes compaibility with Terraform versions prior to `1.1.6`. See the [Muxing](https://developer.hashicorp.com/terraform/plugin/framework/migrating/mux#terraform-1-x-compatibility) example documentation for additional details.
    
    This commit does not cover migrating acceptance tests to use protocol v6, only the provider internals.

--- internal/provider/factory.go
+++ internal/provider/factory.go
@@ -7,18 +7,19 @@ import (
 	"context"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
-	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
-	"github.com/hashicorp/terraform-plugin-mux/tf5muxserver"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-mux/tf5to6server"
+	"github.com/hashicorp/terraform-plugin-mux/tf6muxserver"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal"
 	"github.com/hashicorp/terraform-provider-aws/internal/provider/framework"
 	"github.com/hashicorp/terraform-provider-aws/internal/provider/sdkv2"
 )
 
-// ProtoV5ProviderServerFactory returns a muxed terraform-plugin-go protocol v5 provider factory function.
+// ProtoV6ProviderServerFactory returns a muxed terraform-plugin-go protocol v6 provider factory function.
 // This factory function is suitable for use with the terraform-plugin-go Serve function.
 // The primary (Plugin SDK) provider server is also returned (useful for testing).
-func ProtoV5ProviderServerFactory(ctx context.Context) (func() tfprotov5.ProviderServer, *schema.Provider, error) {
+func ProtoV6ProviderServerFactory(ctx context.Context) (func() tfprotov6.ProviderServer, *schema.Provider, error) {
 	internal.RegisterSmarterrFS()
 
 	primary, err := sdkv2.NewProvider(ctx)
@@ -27,22 +28,29 @@ func ProtoV5ProviderServerFactory(ctx context.Context) (func() tfprotov5.Provide
 		return nil, nil, err
 	}
 
+	upgradedSDKServer, err := tf5to6server.UpgradeServer(
+		ctx,
+		primary.GRPCProvider,
+	)
+
 	secondary, err := framework.NewProvider(ctx, primary)
 
 	if err != nil {
 		return nil, nil, err
 	}
 
-	servers := []func() tfprotov5.ProviderServer{
-		primary.GRPCProvider,
-		providerserver.NewProtocol5(secondary),
+	servers := []func() tfprotov6.ProviderServer{
+		func() tfprotov6.ProviderServer {
+			return upgradedSDKServer
+		},
+		providerserver.NewProtocol6(secondary),
 	}
 
-	muxServer, err := tf5muxserver.NewMuxServer(ctx, servers...)
+	muxServer, err := tf6muxserver.NewMuxServer(ctx, servers...)
 
 	if err != nil {
 		return nil, nil, err
 	}
 
-	return muxServer.ProviderServer, primary, nil
+	return muxServer.ProviderServer, primary, nil // TODO: primary is returned for testing - assert tests still work with upgradedSDKServer
 }
--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"log"
 	"runtime/debug"
 
-	"github.com/hashicorp/terraform-plugin-go/tfprotov5/tf5server"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6/tf6server"
 	"github.com/hashicorp/terraform-provider-aws/internal/provider"
 	"github.com/hashicorp/terraform-provider-aws/version"
 )
@@ -26,19 +26,19 @@ func main() {
 		log.Printf("Starting %s@%s (%s)...", buildInfo.Main.Path, version.ProviderVersion, buildInfo.GoVersion)
 	}
 
-	serverFactory, _, err := provider.ProtoV5ProviderServerFactory(context.Background())
+	serverFactory, _, err := provider.ProtoV6ProviderServerFactory(context.Background())
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var serveOpts []tf5server.ServeOpt
+	var serveOpts []tf6server.ServeOpt
 
 	if *debugFlag {
-		serveOpts = append(serveOpts, tf5server.WithManagedDebug())
+		serveOpts = append(serveOpts, tf6server.WithManagedDebug())
 	}
 
-	err = tf5server.Serve(
+	err = tf6server.Serve(
 		"registry.terraform.io/hashicorp/aws",
 		serverFactory,
 		serveOpts...,
