8a469880be84dff2612c6aabd47ba330a30d4ccb
Jared Baker <jar-b@users.noreply.github.com>
2026-09-24 10:36:41 -0400

    Merge pull request #50128 from hashicorp/td-bedrock-inference-profile
    r/aws_bedrock_inference_profile(test): replace hardcoded foundation model

--- internal/service/bedrock/testdata/InferenceProfile/tags/main_gen.tf
+++ internal/service/bedrock/testdata/InferenceProfile/tags/main_gen.tf
@@ -5,10 +5,14 @@ resource "aws_bedrock_inference_profile" "test" {
   name = var.rName
 
   model_source {
-    copy_from = "arn:aws:bedrock:us-west-2::foundation-model/anthropic.claude-3-5-sonnet-20240620-v1:0"
+    copy_from = data.aws_bedrock_foundation_model.test.model_arn
   }
 
   tags = var.resource_tags
+}
+
+data "aws_bedrock_foundation_model" "test" {
+  model_id = "amazon.nova-lite-v1:0"
 }
 
 variable "rName" {
